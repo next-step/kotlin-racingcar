bc806611a72b387fbd64a38c08635ba47a68d650
Jiwhun Kim <jiwhun.kim@outlook.com>
2020-07-23 22:37:54 +0900

    feature(person) : Person.java 를 Person.kt 로 변경
    feature(person) : lint 적용으로 formatting

---
+++ src/test/kotlin/PersonTests.kt
@@ -0,0 +1,46 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+
+class PersonTests {
+    @Test
+    fun `named arguments`() {
+        val people = listOf(
+            Person("김지훈", 35, "Jiwhun Kim"),
+            Person("김지훈", 35, nickname = "Jiwhun Kim"),
+            Person(name = "김지훈", nickname = "Jiwhun Kim", age = 35)
+        )
+        assertThat(people).allSatisfy {
+            assertThat(it.name).isEqualTo("김지훈")
+            assertThat(it.age).isEqualTo(35)
+            assertThat(it.nickname).isEqualTo("Jiwhun Kim")
+        }
+    }
+
+    @Test
+    fun `nullalbe types`() {
+        val person = Person("김지훈", null, null)
+        assertAll(
+            { assertThat(person.name).isEqualTo("김지훈") },
+            { assertThat(person.age).isNull() },
+            { assertThat(person.nickname).isNull() }
+        )
+    }
+
+    @Test
+    fun `default arguments`() {
+        val person = Person("김지훈")
+        assertAll(
+            { assertThat(person.name).isEqualTo("김지훈") },
+            { assertThat(person.age).isNull() },
+            { assertThat(person.nickname).isNull() }
+        )
+    }
+
+    @Test
+    fun `data classes`() {
+        val person1 = Person("김지훈", 35, "Jiwhun Kim")
+        val person2 = Person("김지훈", 35, "Jiwhun Kim")
+        assertThat(person1).isEqualTo(person2)
+    }
+}
