da024bcf221d8047f9c70f57ec45f70f05c64758
hyunsung.shin <cordorshs@gmail.com>
2020-07-23 22:24:40 +0900

    test(kotlin): add learning test

---
+++ src/test/kotlin/PersonTest.kt
@@ -0,0 +1,48 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+
+class PersonTest {
+
+    @Test
+    fun `Check person value`() {
+        val person = Person("hyunsung", 30, "nanamare")
+        Assertions.assertEquals("hyunsung", person.name)
+        Assertions.assertEquals(30, person.age)
+        Assertions.assertEquals("nanamare", person.nickname)
+    }
+
+    @Test
+    fun `Compare person object`() {
+        val src = Person("hyunsung", 30, "nanamare")
+        val des = Person("hyunsung", 30, "nanamare")
+        val people = listOf(
+            Person("hyunsung", 30, "nanamare"),
+            Person("hyunsung", 30, "nanamare"),
+            Person("hyunsung", 30, "nanamare")
+        )
+
+        assertThat(src).satisfies {
+            assertThat(src.name).isEqualTo(des.name)
+            assertThat(src.age).isEqualTo(des.age)
+            assertThat(src.nickname).isEqualTo(des.nickname)
+        }
+
+        assertThat(people).satisfies {
+            assertThat(src.name).isEqualTo(des.name)
+            assertThat(src.age).isEqualTo(des.age)
+            assertThat(src.nickname).isEqualTo(des.nickname)
+        }
+    }
+
+    @Test
+    fun `Check nullable types`() {
+        val person = Person("hyunsung", null, null)
+        assertAll(
+            { assertThat(person.name).isEqualTo("hyunsung") },
+            { assertThat(person.age).isNull() },
+            { assertThat(person.nickname).isNull() }
+        )
+    }
+}
