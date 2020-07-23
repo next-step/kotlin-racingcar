b28a29e892237e12b88bf0f9aad73b32864c280d
wotjd243 <wotjd243@naver.com>
2020-07-23 15:55:32 +0900

    feat(racingcar): set up the project

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    java
+    kotlin("jvm") version "1.3.72"
+    id("org.jlleitschuh.gradle.ktlint") version "9.3.0"
+}
+
+group = "camp.nextstep.edu"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation("org.junit.jupiter", "junit-jupiter", "5.6.2")
+    testImplementation("org.assertj", "assertj-core", "3.16.1")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    test {
+        useJUnitPlatform()
+    }
+    ktlint {
+        verbose.set(true)
+        disabledRules.addAll("import-ordering")
+    }
+}
