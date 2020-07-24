28502b9c1b889e299c320ef3c8af78c8d1107d9e
Parkjoopika <parkjoohan0603@gmail.com>
2020-07-24 20:40:28 +0900

    add find +, -, *, /

--- src/main/kotlin/study/StrCalculator.kt
+++ src/main/kotlin/study/StrCalculator.kt
@@ -2,7 +2,7 @@ package study
 
 class StrCalculator(str: String) {
     val numList = str.split("+", "-", "*", "/").map(String::toInt)
-    val fpoList = str.split("0", "1", "2", "3", "4", "5", "6", "7", "8", "9").filter { it != "" }
+    val symbolList = str.split("0", "1", "2", "3", "4", "5", "6", "7", "8", "9").filter { it != "" }
 
     fun plus(list: List<Int>): Int {
         return list.sum()
@@ -18,5 +18,21 @@ class StrCalculator(str: String) {
 
     fun division(list: List<Int>): Int {
         return list[0] / list[1]
+    }
+
+    fun isPlus(times: Int): Boolean {
+        return symbolList[times] == "+"
+    }
+
+    fun isMinus(times: Int): Boolean {
+        return symbolList[times] == "-"
+    }
+
+    fun isMultiplication(times: Int): Boolean {
+        return symbolList[times] == "*"
+    }
+
+    fun isDivision(times: Int): Boolean {
+        return symbolList[times] == "/"
     }
 }
--- src/test/kotlin/study/StrCalculatorTest.kt
+++ src/test/kotlin/study/StrCalculatorTest.kt
@@ -35,7 +35,7 @@ class StrCalculatorTest {
         val strCal = StrCalculator(str)
         val testList = listOf(1, 2)
         assertThat(strCal.numList).isEqualTo(testList)
-        assertThat(strCal.fpoList).isEqualTo(listOf("+"))
+        assertThat(strCal.symbolList).isEqualTo(listOf("+"))
     }
 
     @Test
@@ -50,5 +50,19 @@ class StrCalculatorTest {
         assertThat(minus).isEqualTo(1)
         assertThat(multiplication).isEqualTo(2)
         assertThat(division).isEqualTo(2)
+    }
+
+    @Test
+    fun find_FPO() {
+        val str = "1+2-3*4/5"
+        val strCal = StrCalculator(str)
+        val plusSymbol = strCal.isPlus(0)
+        val minusSymbol = strCal.isMinus(1)
+        val multiplicationSymbol = strCal.isMultiplication(2)
+        val divisionSymbol = strCal.isDivision(3)
+        assertThat(plusSymbol).isTrue()
+        assertThat(minusSymbol).isTrue()
+        assertThat(multiplicationSymbol).isTrue()
+        assertThat(divisionSymbol).isTrue()
     }
 }
