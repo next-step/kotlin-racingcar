9014b0c9cd97ab649988a13db70e60fdce044fc5
myeongjang <jacobmjjo@gmail.com>
2020-07-25 01:51:42 +0900

    start step2 , before test Calculator test

---
+++ src/test/kotlin/Calculator.kt
@@ -0,0 +1,9 @@
+class Calculator {
+    fun sum(x: Int, y: Int): Int = x + y
+    fun mul(x: Int, y: Int): Int = x * y
+    fun div(x: Int, y: Int): Int = x / y
+    fun sub(x: Int, y: Int): Int = x - y
+}
+
+
+
