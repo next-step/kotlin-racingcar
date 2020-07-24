6b924bb7e48969e5f2acf4f56af1a0093609f996
bky373 <bo373@naver.com>
2020-07-25 08:40:57 +0900

    feat(stringcalculator): initial commit
    Added StringCalculator to the step2
    Tested this by using JUnit in StringCalculatorTest

---
+++ src/test/kotlin/StringCalculator.kt
@@ -0,0 +1,63 @@
+import java.lang.ArithmeticException
+
+enum class Operator(val sign: String) {
+    PLUS("+"), MINUS("-"), MULTIPLY("*"), DIVIDE("/")
+}
+
+private const val REMAINDER_ZERO = 0
+private const val REMAINDER_ONE = 1
+
+class StringCalculator {
+    private var result: Int = 0
+
+    fun calculate(inputStr: String?): Int {
+        isCorrectInput(inputStr)
+
+        val strList = inputStr!!.trim().split(" ")
+
+        val numbers = ArrayList<Int>()
+        val operators = ArrayList<String>()
+
+        separateNumsAndOperators(strList, numbers, operators)
+
+        result = numbers.removeAt(0)
+
+        calculateInOrder(operators, numbers)
+
+        return result
+    }
+
+    private fun isCorrectInput(inputStr: String?) {
+        if (inputStr.isNullOrEmpty() or (inputStr == " ")) throw IllegalArgumentException("값을 입력해주세요.")
+    }
+
+    private fun separateNumsAndOperators(strList: List<String>, numbers: ArrayList<Int>, operators: ArrayList<String>) {
+        for (i in strList.indices) {
+            if (i % 2 == REMAINDER_ZERO) numbers.add(strList.get(i).toInt())
+            if (i % 2 == REMAINDER_ONE) operators.add(strList.get(i))
+        }
+    }
+
+    private fun calculateInOrder(operators: ArrayList<String>, numbers: ArrayList<Int>) {
+        for (i in operators.indices) {
+            when (operators.removeAt(0)) {
+                Operator.PLUS.sign -> plus(numbers.removeAt(0))
+                Operator.MINUS.sign -> minus(numbers.removeAt(0))
+                Operator.MULTIPLY.sign -> multiply(numbers.removeAt(0))
+                Operator.DIVIDE.sign -> divide(numbers.removeAt(0))
+                else -> throw IllegalArgumentException("올바르지 않은 연산자가 있습니다.")
+            }
+        }
+    }
+
+    private fun plus(num: Int) { result += num }
+
+    private fun minus(num: Int) { result -= num }
+
+    private fun multiply(num: Int) { result *= num }
+
+    private fun divide(num: Int) {
+        if (num == 0) throw ArithmeticException("0으로 나눌 수 없습니다.")
+        result /= num
+    }
+}
---
+++ src/test/kotlin/StringCalculatorTest.kt
@@ -0,0 +1,67 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
+import org.junit.jupiter.api.Test
+
+class StringCalculatorTest {
+
+    @Test
+    fun `add`() {
+        val result1 = StringCalculator().calculate("3 + 4")
+        val result2 = StringCalculator().calculate("10 + 7 + 3")
+        assertThat(result1).isEqualTo(7)
+        assertThat(result2).isEqualTo(20)
+    }
+
+    @Test
+    fun `subtract`() {
+        val result1 = StringCalculator().calculate("3 - 4")
+        val result2 = StringCalculator().calculate("10 - 7 - 3")
+        assertThat(result1).isEqualTo(-1)
+        assertThat(result2).isEqualTo(0)
+    }
+
+    @Test
+    fun `multiply`() {
+        val result1 = StringCalculator().calculate("3 * 4")
+        val result2 = StringCalculator().calculate("10 * 7 * 3")
+        assertThat(result1).isEqualTo(12)
+        assertThat(result2).isEqualTo(210)
+    }
+
+    @Test
+    fun `divide`() {
+        val result1 = StringCalculator().calculate("0 / 2")
+        val result2 = StringCalculator().calculate("100 / 10 / 2")
+        assertThat(result1).isEqualTo(0)
+        assertThat(result2).isEqualTo(5)
+    }
+
+    @Test
+    fun `useAllOperators`() {
+        val result1 = StringCalculator().calculate("3 + 4 - 2 * 10 / 10")
+        val result2 = StringCalculator().calculate("3 + 3 - 2")
+        assertThat(result1).isEqualTo(5)
+        assertThat(result2).isEqualTo(4)
+    }
+
+    @Test
+    fun `isNullOrEmpty`() {
+        assertThatExceptionOfType(IllegalArgumentException::class.java)
+            .isThrownBy { StringCalculator().calculate("") }
+            .withMessage("값을 입력해주세요.")
+    }
+
+    @Test
+    fun `isNotCorrectOperator`() {
+        assertThatExceptionOfType(IllegalArgumentException::class.java)
+            .isThrownBy { StringCalculator().calculate("2 $ 3 + 4") }
+            .withMessage("올바르지 않은 연산자가 있습니다.")
+    }
+
+    @Test
+    fun `isDividedByZero`() {
+        assertThatExceptionOfType(ArithmeticException::class.java)
+            .isThrownBy { StringCalculator().calculate("2 / 0") }
+            .withMessage("0으로 나눌 수 없습니다.")
+    }
+}
