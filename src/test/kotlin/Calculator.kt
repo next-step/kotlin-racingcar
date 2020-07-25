e7c93b725e36724ee29c0b3e12955dfa94f99376
myeongjang <jacobmjjo@gmail.com>
2020-07-25 21:20:08 +0900

    try to write code Calculator that String to Int. But can't do well

--- src/test/kotlin/Calculator.kt
+++ src/test/kotlin/Calculator.kt
@@ -1,9 +1,45 @@
-class Calculator {
-    fun sum(x: Int, y: Int): Int = x + y
-    fun mul(x: Int, y: Int): Int = x * y
-    fun div(x: Int, y: Int): Int = x / y
-    fun sub(x: Int, y: Int): Int = x - y
+import org.assertj.core.data.Index
+
+
+enum class Operate(val row: String) {
+    SUM("+") {
+        fun cal(a: Int, b: Int) = a + b
+    },
+    SUB("-") {
+        fun cal(a: Int, b: Int) = a - b
+    },
+    MUL("*") {
+        fun cal(a: Int, b: Int) = a * b
+    },
+    DIV("/") {
+        fun cal(a: Int, b: Int): Int{
+            if  (b == 0) throw ArithmeticException("0으로 나누면 값이 무한이 됩니다.")
+            return a / b
+        }
+    }
 }
 
 
+class Calculator() {
 
+    fun OperateSymbol(input: String?) {
+        if (input.isNullOrBlank())
+            throw IllegalArgumentException("공백이나 null값은 혀용이 되지 않습니다.")
+    }
+}
+
+
+fun main() {
+    val s : String = "2 + 3 * 4 / 2"
+
+
+
+
+        }
+
+    }
+
+
+    val cal = Calculator()
+
+}
--- src/test/kotlin/CalculatorTest.kt
+++ src/test/kotlin/CalculatorTest.kt
@@ -1,2 +1,12 @@
-class CalculatorTest {
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+
+class CalculatorTest{
+    @Test
+    @DisplayName("연산 체크")
+    fun checkOperate(){
+        assertThat(Operate.SUM.cal(3,4)).isEqualTo(7)
+    }
 }
