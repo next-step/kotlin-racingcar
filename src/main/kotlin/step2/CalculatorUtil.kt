ff85a3470e7edaab7c934cea95b23bb6024004a2
kimgaeul <kimgaeul4@gmail.com>
2020-07-26 00:06:14 +0900

    feat(step2): Pattern Instance 재사용 방식 적용

--- src/main/kotlin/step2/CalculatorUtil.kt
+++ src/main/kotlin/step2/CalculatorUtil.kt
@@ -1,12 +1,16 @@
 package step2
 
+import java.util.regex.Pattern
+
 class CalculatorUtil {
     companion object {
 
-        private const val Delimiter = " "
+        private const val DELIMITER = " "
+        private val OPERATOR_REGEX = Pattern.compile("[+|\\-|*|/]")
+        private val NON_NUMERIC_REGEX = Pattern.compile("[^0-9]")
 
         fun validateAndReturn(string: String): List<String> {
-            val split = string.split(Delimiter)
+            val split = string.split(DELIMITER)
             validateInputSize(split)
             checkInputOperator(split)
             checkInputNumber(split)
@@ -15,24 +19,24 @@ class CalculatorUtil {
 
         fun checkInputOperator(split: List<String>) {
             for (s in split.slice(1..split.lastIndex step 2)) {
-                if (!s.matches("[+|\\-|*|/]".toRegex()))
+                if (!OPERATOR_REGEX.matcher(s).matches())
                     throw IllegalArgumentException("허용되지 않은 연산자가 있습니다.")
             }
         }
 
         fun checkInputNumber(split: List<String>) {
             for (s in split.slice(0..split.lastIndex step 2)) {
-                if (s.matches("[^0-9]".toRegex()))
+                if (NON_NUMERIC_REGEX.matcher(s).matches())
                     throw IllegalArgumentException("숫자 타입이 아닌 문자가 있습니다.")
             }
         }
 
         fun validateInputSize(split: List<String>) {
-            if (split.isEmpty())
+            if (split.isEmpty()) {
                 throw IllegalArgumentException("input data 가 비어있습니다.")
+            }
             if (split.size % 2 == 0)
                 throw IllegalArgumentException("잘못된 포멧입니다")
         }
     }
-
 }
