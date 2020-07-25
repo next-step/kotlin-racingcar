1ce6c32a8ed51e161109ae06fc8a566ff3d746d4
influx.pine <influx.pine@gmail.com>
2020-07-25 23:39:53 +0900

    feat: 자동차 테스트 기능 정의

---
+++ src/test/kotlin/CarTest.kt
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class CarTest {
+
+    @DisplayName("자동차 전진 테스트")
+    @Test
+    fun forward() {
+    }
+
+    @DisplayName("현재 위치 노출 테스트")
+    @Test
+    fun print() {
+    }
+}
