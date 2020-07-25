37b465e8bfcf7941a9516137ae0df02382f1db3a
zion830 <zion830@honglab.org>
2020-07-26 04:05:07 +0900

    feat(calculator): 계산기 구현

---
+++ src/main/kotlin/calculator/Calculator.kt
@@ -0,0 +1,17 @@
+package calculator
+
+import calculator.Operation.none
+
+object Calculator {
+
+    fun execute(expression: List<String>): Double {
+        var accumulator = expression[0].toDouble()
+
+        for (i in 2 until expression.size step 2) {
+            val operation = Operation.of(expression[i - 1]) ?: none
+            accumulator = operation(accumulator, expression[i].toDouble())
+        }
+
+        return accumulator
+    }
+}
