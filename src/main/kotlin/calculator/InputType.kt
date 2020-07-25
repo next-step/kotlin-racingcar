2e3bb4a0841be65ca416bcc7823d243e6f2d0788
zion830 <zion830@honglab.org>
2020-07-26 03:08:27 +0900

    feat(calculator): 연산식 구성요소와 사칙연산 구현
    연산식의 구성 요소는 크게 숫자와 연산자로 구분된다. 연산자는 +, -, *, / 네가지를 지원한다.

--- src/main/kotlin/calculator/InputType.kt
+++ src/main/kotlin/calculator/InputType.kt
@@ -6,10 +6,15 @@ enum class InputType {
     NONE;
 
     companion object {
+
         fun of(value: String) = when {
             isNumber(value) -> NUMBER
             isOperator(value) -> OPERATOR
             else -> NONE
         }
+
+        private fun isNumber(value: String) = value.toDoubleOrNull() != null
+
+        private fun isOperator(value: String) = Operation.of(value) != null
     }
 }
---
+++ src/main/kotlin/calculator/Operation.kt
@@ -0,0 +1,21 @@
+package calculator
+
+object Operation {
+
+    fun of(operator: String) = operators[operator]
+
+    private val add: (Double, Double) -> Double = { base, second -> base + second }
+
+    private val sub: (Double, Double) -> Double = { base, second -> base - second }
+
+    private val multi: (Double, Double) -> Double = { base, second -> base * second }
+
+    private val div: (Double, Double) -> Double = { base, second ->
+        when (second) {
+            0.0 -> Double.NaN
+            else -> base / second
+        }
+    }
+
+    private val operators = hashMapOf(Pair("+", add), Pair("-", sub), Pair("*", multi), Pair("/", div))
+}
