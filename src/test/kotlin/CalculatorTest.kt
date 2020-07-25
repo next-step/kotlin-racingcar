f00838837dbdef67edca8ac35e68cd4c16632cec
BeokBeok <kekemusa37@nate.com>
2020-07-25 11:56:08 +0900

    추가 : 사칙 연산이 아닌 경우 TC

--- src/test/kotlin/CalculatorTest.kt
+++ src/test/kotlin/CalculatorTest.kt
@@ -1,4 +1,5 @@
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.Test
 
 class CalculatorTest {
@@ -7,5 +8,13 @@ class CalculatorTest {
         val expression = "2+3*4/2"
         val result = Calculator().calculateExpression(expression)
         assertThat(result).isEqualTo(10)
+    }
+
+    @Test
+    fun `사칙 연산 기호가 아닌 경우`() {
+        assertThatIllegalArgumentException().isThrownBy {
+            val expression = "2:3*4/2"
+            Calculator().calculateExpression(expression)
+        }
     }
 }
