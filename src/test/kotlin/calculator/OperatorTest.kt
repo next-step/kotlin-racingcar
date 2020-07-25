5ae6dc69cf4ea9672811f37b44eaa11913b741e7
javajigi <javajigi@gmail.com>
2020-07-25 10:59:33 +0900

    refactor(calculate): 사칙 연산 계산, remove duplicate test code

--- src/test/kotlin/calculator/OperatorTest.kt
+++ src/test/kotlin/calculator/OperatorTest.kt
@@ -1,16 +1,25 @@
 package calculator
 
+import calculator.Operator.Companion.valueOfSign
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
 class OperatorTest {
     @Test
+    fun `사칙연산 기호`() {
+        assertThat(valueOfSign("+")).isEqualTo(Operator.PLUS)
+        assertThat(valueOfSign("-")).isEqualTo(Operator.MINUS)
+        assertThat(valueOfSign("*")).isEqualTo(Operator.MULTIPLY)
+        assertThat(valueOfSign("/")).isEqualTo(Operator.DIVIDE)
+        assertThatThrownBy { valueOfSign("&") }.isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @Test
     fun `사칙연산`() {
-        assertThat(Operator.valueOfSign("+")).isEqualTo(Operator.PLUS)
-        assertThat(Operator.valueOfSign("-")).isEqualTo(Operator.MINUS)
-        assertThat(Operator.valueOfSign("*")).isEqualTo(Operator.MULTIPLY)
-        assertThat(Operator.valueOfSign("/")).isEqualTo(Operator.DIVIDE)
-        assertThatThrownBy { Operator.valueOfSign("&") }.isInstanceOf(IllegalArgumentException::class.java)
+        assertThat(valueOfSign("+").operation(3, 4)).isEqualTo(7)
+        assertThat(valueOfSign("-").operation(2, 1)).isEqualTo(1)
+        assertThat(valueOfSign("*").operation(3, 4)).isEqualTo(12)
+        assertThat(valueOfSign("/").operation(6, 3)).isEqualTo(2)
     }
 }
--- src/test/kotlin/calculator/StringCalculatorTest.kt
+++ src/test/kotlin/calculator/StringCalculatorTest.kt
@@ -7,35 +7,10 @@ import org.junit.jupiter.api.Test
 
 class StringCalculatorTest {
     @Test
-    fun `덧셈`() {
-        assertThat(calculate("1 + 2")).isEqualTo(3)
-    }
-
-    @Test
-    fun `뺄셈`() {
-        assertThat(calculate("2 - 1")).isEqualTo(1)
-    }
-
-    @Test
-    fun `곱셈`() {
-        assertThat(calculate("2 * 3")).isEqualTo(6)
-    }
-
-    @Test
-    fun `나눗셈`() {
-        assertThat(calculate("4 / 2")).isEqualTo(2)
-    }
-
-    @Test
     fun `입력 값이 null 또는 빈 문자열`() {
         assertThatThrownBy { calculate(null) }.isInstanceOf(IllegalArgumentException::class.java)
         assertThatThrownBy { calculate("") }.isInstanceOf(IllegalArgumentException::class.java)
         assertThatThrownBy { calculate(" ") }.isInstanceOf(IllegalArgumentException::class.java)
-    }
-
-    @Test
-    fun `사칙연산이 아닌 경우`() {
-        assertThatThrownBy { calculate("4 & 2") }.isInstanceOf(IllegalArgumentException::class.java)
     }
 
     @Test
