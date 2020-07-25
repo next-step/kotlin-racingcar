1d2542fe2e5c13028a348de4a81d792b56536759
BeokBeok <kekemusa37@nate.com>
2020-07-25 11:21:17 +0900

    추가 : Calculator Test class

---
+++ src/test/kotlin/CalculatorTest.kt
@@ -0,0 +1,11 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class CalculatorTest {
+    @Test
+    fun calculate() {
+        val expression = "2+3*4/2"
+        val result = Calculator().calculateExpression(expression)
+        assertThat(result).isEqualTo(10)
+    }
+}
