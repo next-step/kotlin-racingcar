cd87efe71d48732b256dc02c95e2f3387879bd79
kimgaeul <kimgaeul4@gmail.com>
2020-07-26 00:26:39 +0900

    feat(step2): given/when/then 패턴에 맞게 테스트 이름 변경

--- src/test/kotlin/step2/CalculatorUtilTest.kt
+++ src/test/kotlin/step2/CalculatorUtilTest.kt
@@ -1,27 +1,26 @@
 package step2
 
 import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
-import org.assertj.core.api.AssertionsForClassTypes.assertThat
 import org.junit.jupiter.api.Test
 
 class CalculatorUtilTest {
 
     @Test
-    fun `잘못된 operator`() {
+    fun `잘못된 operator인 경우 operator validation 시 IllegalArgumentException 발생`() {
         assertThatIllegalArgumentException().isThrownBy {
             CalculatorUtil.checkInputOperator("6 * 7 ^ 3".split(" "))
         }
     }
 
     @Test
-    fun `숫자 타입이 아닌 input`() {
+    fun `숫자 타입이 아닌 경우 data validation 시 IllegalArgumentException 발생`() {
         assertThatIllegalArgumentException().isThrownBy {
             CalculatorUtil.checkInputNumber("6 * 7 - A".split(" "))
         }
     }
 
     @Test
-    fun `문자열의 길이가 홀수가 아닌 경우`() {
+    fun `문자열의 길이가 홀수인 경우 data validation 시 IllegalArgumentException 발생`() {
         assertThatIllegalArgumentException().isThrownBy {
             CalculatorUtil.validateInputSize("6 * 7 *".split(" "))
         }
