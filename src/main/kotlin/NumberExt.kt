1eec7b7bb64e23785949646e4f1fcda7f3c35126
zion830 <zion830@honglab.org>
2020-07-26 04:16:40 +0900

    test(calculator): 계산기 테스트 추가

--- src/main/kotlin/NumberExt.kt
+++ src/main/kotlin/NumberExt.kt
@@ -1,1 +1,5 @@
 fun Int.isOdd() = this % 2 == 1
+
+fun Double.isInt() = this % 1 == 0.0
+
+fun Double.toPrintString(): String = if (isInt()) this.toInt().toString() else this.toString()
---
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -0,0 +1,13 @@
+package calculator
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class CalculatorTest {
+
+    @Test
+    fun execute() {
+        assertEquals(8.5, Calculator.execute(ExpressionParser.parse("3 + 4 / 2 + 5")))
+        assertEquals(Double.NaN, Calculator.execute(ExpressionParser.parse("4/0")))
+    }
+}
