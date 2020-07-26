51cb0ea2ae8f65ed6935968969017f482195a068
bky373 <bo373@naver.com>
2020-07-26 11:23:46 +0900

    style(stringcalculator): rename some variables and functions

--- src/test/kotlin/StringCalculator.kt
+++ src/test/kotlin/StringCalculator.kt
@@ -1,18 +1,21 @@
 import java.lang.ArithmeticException
 
-enum class Operator(val sign: String, val calculate : (num1: Int, num2: Int) -> Int) {
-    PLUS("+", { num1, num2 -> num1 + num2 } ),
-    MINUS("-" , { num1, num2 -> num1 - num2 }),
-    MULTIPLY("*" , { num1, num2 -> num1 * num2 }),
-    DIVIDE("/", { num1, num2 ->
-        if(num2 == 0) throw ArithmeticException("0으로 나눌 수 없습니다.")
-        num1 / num2
-    });
+enum class Operator(val sign: String, val calculate: (num1: Int, num2: Int) -> Int) {
+    PLUS("+", { num1, num2 -> num1 + num2 }),
+    MINUS("-", { num1, num2 -> num1 - num2 }),
+    MULTIPLY("*", { num1, num2 -> num1 * num2 }),
+    DIVIDE(
+        "/",
+        { num1, num2 ->
+            if (num2 == 0) throw ArithmeticException("0으로 나눌 수 없습니다.")
+            num1 / num2
+        }
+    );
 
     companion object {
-        fun findBySign(currentSign: String) : Operator =
-            values().find { operator -> operator.sign == currentSign}
-            ?: throw throw IllegalArgumentException("올바르지 않은 연산자 ${currentSign}이(가) 있습니다.")
+        fun findBySign(currentSign: String): Operator =
+            values().find { operator -> operator.sign == currentSign }
+                ?: throw throw IllegalArgumentException("올바르지 않은 연산자 ${currentSign}이(가) 있습니다.")
     }
 }
 
@@ -25,40 +28,44 @@ class StringCalculator {
         private var result: Int = 0
     }
 
-    fun calculate(input: String?): Int {
-        isCorrectInputFormat(input)
+    fun calculate(expression: String?): Int {
+        isCorrectFormat(expression)
 
-        val inputSplit = input!!.trim().split(" ")
+        val expressionSplit = expression!!.trim().split(" ")
 
         val numbers = ArrayList<Int>()
         val operators = ArrayList<String>()
 
-        separateNumsAndOperators(inputSplit, numbers, operators)
+        separateNumsAndOperators(expressionSplit, numbers, operators)
 
         calculateInOrder(operators, numbers)
 
         return result
     }
 
-    private fun isCorrectInputFormat(inputStr: String?) {
-        if (inputStr.isNullOrEmpty() or (inputStr == " ")) throw IllegalArgumentException("값을 입력해주세요.")
+    private fun isCorrectFormat(expression: String?) {
+        if (expression.isNullOrEmpty() or (expression == " ")) throw IllegalArgumentException("값을 입력해주세요.")
     }
 
-    // TODO.05 람다식 사용해보기 (람다식을 인자로 list의 원소를 filtering해보기)
-    private fun separateNumsAndOperators(inputSplit: List<String>, numbers: ArrayList<Int>, operators: ArrayList<String>) {
-        for (i in inputSplit.indices) {
-            if (i % 2 == REMAINDER_ZERO) numbers.add(inputSplit[i].toInt())
-            if (i % 2 == REMAINDER_ONE) operators.add(inputSplit[i])
+    // TODO 람다식 사용해보기? (람다식을 인자로 list의 원소를 filtering해보기)
+    private fun separateNumsAndOperators(
+        expressionSplit: List<String>,
+        numbers: ArrayList<Int>,
+        operators: ArrayList<String>
+    ) {
+        for (i in expressionSplit.indices) {
+            if (i % 2 == REMAINDER_ZERO) numbers.add(expressionSplit[i].toInt())
+            if (i % 2 == REMAINDER_ONE) operators.add(expressionSplit[i])
         }
     }
 
     private fun calculateInOrder(operators: ArrayList<String>, numbers: ArrayList<Int>) {
         result = numbers.removeAt(0)
         if (numbers.isEmpty()) return
+
         for (i in operators.indices) {
             val currentSign = operators.removeAt(0)
             result = Operator.findBySign(currentSign).calculate(result, numbers.removeAt(0))
         }
     }
 }
-
--- src/test/kotlin/StringCalculatorTest.kt
+++ src/test/kotlin/StringCalculatorTest.kt
@@ -25,26 +25,26 @@ class StringCalculatorTest {
     }
 
     @Test
-    fun `useAllOperators`() {
+    fun `use all operators`() {
         assertThat(StringCalculator().calculate("3 + 4 - 2 * 10 / 10")).isEqualTo(5)
     }
 
     @Test
-    fun `isNullOrEmpty`() {
+    fun `is null or empty`() {
         assertThatExceptionOfType(IllegalArgumentException::class.java)
             .isThrownBy { StringCalculator().calculate("") }
             .withMessage("값을 입력해주세요.")
     }
 
     @Test
-    fun `isNotCorrectOperator`() {
+    fun `is not correct operator`() {
         assertThatExceptionOfType(IllegalArgumentException::class.java)
             .isThrownBy { StringCalculator().calculate("2 # 3 + 4") }
             .withMessage("올바르지 않은 연산자 #이(가) 있습니다.")
     }
 
     @Test
-    fun `isDividedByZero`() {
+    fun `is divided by zero`() {
         assertThatExceptionOfType(ArithmeticException::class.java)
             .isThrownBy { StringCalculator().calculate("2 / 0") }
             .withMessage("0으로 나눌 수 없습니다.")
