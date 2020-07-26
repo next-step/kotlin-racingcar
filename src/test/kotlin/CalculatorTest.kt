d4753c7f8cdb0412fcc2a97cb7444d7f5b3279f7
Ryan <kekemusa37@nate.com>
2020-07-26 10:57:40 +0900

    [FEAT] 2단계 - 문자열 계산기 (#30)
    * 추가 : Calculator class
    
    * 추가 : Calculator Test class
    
    * 수정 : 사칙 연산이 아닌 경우 IllegalArgumentException 전달
    
    * 변경 : 테스트명(calculate -> happyCase)
    
    * 추가 : 사칙 연산이 아닌 경우 TC
    
    * 추가 : 식에 공백이 들어간 경우 TC
    
    * 추가 : 식에 널인 경우 TC
    
    * 추가 : 0으로 나눌 경우 예외처리
    
    * 추가 : 0으로 나눌 경우 TC
    
    * 변경 : class to object
    
    * 변경 : 함수명 (calculateExpression -> calEquation, calculate -> calOperator)
    
    * 적용 : 불완전한 식에 대한 예외처리
    
    * 추가 : 불완전한 식에 대한 TC
    
    * 정리 : 중괄호 추가 및 변수명 변경
    
    * 추가 : 식에 공백이 들어간 경우 예외 처리
    
    * 업데이트 : 식에 공백이 들어간 경우 TC
    
    * 변경 : calOperator 접근자 및 operatorList 타입
    
    * 추가 : 연산 관련 Test
    
    * 추가 : 더하기 TC
    
    * 추가 : 빼기 TC
    
    * 추가 : 곱하기 TC
    
    * 추가 : 나누기 TC
    
    * 이동 : 0 으로 나누는 TC
    
    * 추가 : separateNumAndOpList 함수
    
    * 추가 : numberBuilder 에 값이 없는 경우 예외처리
    
    * 보강 : 불완전한 식 관련 TC
    
    * 변경 : 리턴 타입 (Int -> Double)
    
    * 변경 : 계산식에 사용되는 모든 숫자 타입 변경 (Int -> Double)
    
    * 추가 : Operator enum
    
    * 적용 : 연산자 연산을 enum 으로 연산
    
    * 정리 : throw 관련 if 문 중괄호

---
+++ src/test/kotlin/CalculatorTest.kt
@@ -0,0 +1,46 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class CalculatorTest {
+
+    @Test
+    fun happyCase() {
+        val equation = "2+3*4/2"
+        val result = Calculator.calEquation(equation)
+        assertThat(result).isEqualTo(10.0)
+    }
+
+    @Test
+    fun `사칙 연산 기호가 아닌 경우`() {
+        assertThatIllegalArgumentException().isThrownBy {
+            val equation = "2:3*4/2"
+            Calculator.calEquation(equation)
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["2 + 3 * 4 / 2", "", " "])
+    fun `식에 공백이 들어간 경우`(input: String) {
+        assertThatIllegalArgumentException().isThrownBy {
+            Calculator.calEquation(input)
+        }
+    }
+
+    @Test
+    fun `식이 널인 경우`() {
+        assertThatIllegalArgumentException().isThrownBy {
+            Calculator.calEquation(null)
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["2+3*", "2+3**3", "*2+3**3"])
+    fun `불완전한 식을 계산할 경우`(input: String) {
+        assertThatIllegalArgumentException().isThrownBy {
+            Calculator.calEquation(input)
+        }
+    }
+}
