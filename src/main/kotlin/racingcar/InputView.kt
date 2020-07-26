e12362ba079797abd22e8e2e68c036f7ce04373d
bky373 <bo373@naver.com>
2020-07-26 22:50:04 +0900

    feat(racingcar): create inputView object and input fuction

---
+++ src/main/kotlin/racingcar/InputView.kt
@@ -0,0 +1,11 @@
+package racingcar
+
+object InputView {
+    fun setCarNumsAndChanceToMove() {
+        println("자동차 대수는 몇 대인가요?")
+        val number = readLine()!!.toInt()
+        println("시도할 횟수는 몇 회인가요?")
+        val chanceToMove = readLine()!!.toInt()
+    }
+}
+
