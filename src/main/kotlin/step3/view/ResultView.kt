caf2a6a9d7595a254003d6bad294d5fb5b4fb8d3
jejun <jjin.dev@gmail.com>
2020-07-27 22:51:30 +0900

    Refactor(step3): repeat 함수 도입, 테스트 코드가 성공을 반환하도록 Line break 위치 이동

--- src/main/kotlin/step3/view/ResultView.kt
+++ src/main/kotlin/step3/view/ResultView.kt
@@ -5,19 +5,16 @@ import step3.Car
 object ResultView {
     fun output(cars: List<Car>) {
         print("output for turn\n")
+
         for (element in cars) {
             print(visualize(element.distance))
+            print("\n")
         }
     }
 
     fun visualize(distance: Int): String {
         val block = "-"
-        var blocks = ""
 
-        for (i in 0 until distance) {
-            blocks += block
-        }
-        blocks += "\n"
-        return blocks
+        return block.repeat(distance)
     }
 }
