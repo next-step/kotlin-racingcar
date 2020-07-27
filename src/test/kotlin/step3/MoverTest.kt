ff4bf68fe9ed5c9ca33bd134612ecd5462d4a565
Malibin <nightmare73@naver.com>
2020-07-27 21:38:23 +0900

    refactor(step3): CarMover > Mover테스트로 변경

--- src/test/kotlin/step3/MoverTest.kt
+++ src/test/kotlin/step3/MoverTest.kt
@@ -4,13 +4,13 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
-internal class CarMoverTest {
+internal class MoverTest {
 
     @ValueSource(booleans = [true, false])
     @ParameterizedTest
     fun `원하는 Boolean값이 나오는지 확인`(expectedBoolean: Boolean) {
         // given
-        val carMover = object : CarMover() {
+        val carMover = object : Mover {
             override fun canMove(): Boolean {
                 return expectedBoolean
             }
