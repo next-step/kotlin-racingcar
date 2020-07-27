ab48f425c666f1cb4d21c30d32592f989772add4
bky373 <bo373@naver.com>
2020-07-27 13:24:04 +0900

    refactor(racingcar): change type of distance from String to Int

--- src/main/kotlin/racingcar/RacingCar.kt
+++ src/main/kotlin/racingcar/RacingCar.kt
@@ -1,13 +1,10 @@
 package racingcar
 
 class RacingCar {
-    // TODO 06. distance는 String이 아닌 Int로 -> 궁극적으론 거리 이동과 이동한 거리에 대한 출력을 분리
-    var distance: String = ""
+    var distance: Int = 0
 
     fun run(randomNum: Int) {
-        if (isRunnable(randomNum)) {
-            distance = "$distance-"
-        }
+        if (isRunnable(randomNum)) distance++
     }
 
     fun isRunnable(randomNum: Int) = randomNum in 4..9
