573726fc8fde20f85f6c5d0cfd01ea4e8021a6c7
influx.pine <influx.pine@gmail.com>
2020-07-27 19:57:02 +0900

    feat: 자동차 객체 구현 및 기본 테스트 추가

--- src/test/kotlin/CarTest.kt
+++ src/test/kotlin/CarTest.kt
@@ -1,15 +1,39 @@
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import java.lang.IllegalArgumentException
 
 class CarTest {
 
-    @DisplayName("자동차 전진 테스트")
-    @Test
-    fun forward() {
+    @DisplayName("자동차 전진 테스트(전진)")
+    @ParameterizedTest
+    @ValueSource(ints = [4, 5, 6, 7, 8, 9])
+    fun forward(number: Int) {
+        val car = Car()
+        assertThat(car.accelerate(number)).isEqualTo(1)
     }
 
-    @DisplayName("현재 위치 노출 테스트")
+    @DisplayName("자동차 전진 테스트(제자리)")
+    @ParameterizedTest
+    @ValueSource(ints = [0, 1, 2, 3])
+    fun notForward(number: Int) {
+        val car = Car()
+        assertThat(car.accelerate(number)).isEqualTo(0)
+    }
+
+    @DisplayName("자동차 전진 테스트(예외)")
     @Test
-    fun print() {
+    fun accelateException() {
+        val car = Car()
+        assertThrows<IllegalArgumentException> {
+            car.accelerate(10)
+        }
+
+        assertThrows<IllegalArgumentException> {
+            car.accelerate(-1)
+        }
     }
 }
