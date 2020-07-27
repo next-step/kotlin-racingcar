0b44c31e513fa5cce9cd5f9b80066be11008823b
bky373 <bo373@naver.com>
2020-07-27 13:14:49 +0900

    refactor(racingcar): move randomNum into 'fun run' and also isRunnable into class RacingCar

--- src/main/kotlin/racingcar/RacingCar.kt
+++ src/main/kotlin/racingcar/RacingCar.kt
@@ -1,10 +1,14 @@
 package racingcar
 
 class RacingCar {
-    var randomNum: Int = 0
+    // TODO 06. distance는 String이 아닌 Int로 -> 궁극적으론 거리 이동과 이동한 거리에 대한 출력을 분리
     var distance: String = ""
 
-    fun run() {
-        distance = "$distance-"
+    fun run(randomNum: Int) {
+        if (isRunnable(randomNum)) {
+            distance = "$distance-"
+        }
     }
+
+    fun isRunnable(randomNum: Int) = randomNum in 4..9
 }
