b34d2bb357180149d41a59efdcab3de63965a832
bky373 <bo373@naver.com>
2020-07-27 21:52:29 +0900

    refactor(racingcar): simplify code in Racing and ResultView

--- src/main/kotlin/racingcar/Racing.kt
+++ src/main/kotlin/racingcar/Racing.kt
@@ -5,13 +5,11 @@ object Racing {
     fun start(carCnt: Int, trials: Int) {
         val cars = RacingCarFactory.makeCars(carCnt)
 
-        println("\n실행 결과")
-
         for (i in 1..trials) {
             for (x in cars.indices) {
                 cars[x].run((0..9).random())
             }
-            ResultView.print(cars)
+            ResultView.print(i, cars)
         }
     }
 }
--- src/main/kotlin/racingcar/ResultView.kt
+++ src/main/kotlin/racingcar/ResultView.kt
@@ -2,12 +2,13 @@ package racingcar
 
 object ResultView {
     private const val DISTANCE_MARK = "-"
+    private const val TEXT_RACING_RESULT = "실행 결과"
 
-    fun print(cars: List<RacingCar>) {
+    fun print(trial: Int, cars: List<RacingCar>) {
+        if (trial == 1) println("\n$TEXT_RACING_RESULT")
+        
         cars.forEach { car ->
-            repeat(car.distance) {
-                print(DISTANCE_MARK)
-            }
+            repeat(car.distance) { print(DISTANCE_MARK) }
             println()
         }
         println()
