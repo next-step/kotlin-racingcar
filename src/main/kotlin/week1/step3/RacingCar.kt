0ab423cfddf395bb2510f4c4df35b0e16b7d2bae
BeokBeok <kekemusa37@nate.com>
2020-07-27 17:45:13 +0900

    feat(step3) : 구현 - main 함수

--- src/main/kotlin/week1/step3/RacingCar.kt
+++ src/main/kotlin/week1/step3/RacingCar.kt
@@ -1,4 +1,16 @@
 package week1.step3
 
 fun main() {
+    val (carCount, tryCount) = InputView.getCarAndTryCount()
+    val racingCar = Array(carCount) { Car() }
+
+    showContent(tryCount, racingCar)
+}
+
+private fun showContent(tryCount: Int, racingCar: Array<Car>) {
+    for (count in 0 until tryCount) {
+        ResultView.printEachCarMovingState(racingCar)
+        racingCar.forEach { it.updateForwardCount() }
+        println()
+    }
 }
