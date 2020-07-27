64e63ed44fd04461cd786399a32e08ceb9791a86
bky373 <bo373@naver.com>
2020-07-27 23:36:02 +0900

    style(resultview): rename functions done right before

--- src/main/kotlin/racingcar/Racing.kt
+++ src/main/kotlin/racingcar/Racing.kt
@@ -5,13 +5,13 @@ object Racing {
     fun start(carCnt: Int, trials: Int) {
         val cars = RacingCarFactory.makeCars(carCnt)
 
-        ResultView.printResultSignal()
+        ResultView.showResultTitle()
 
         for (i in 1..trials) {
             for (x in cars.indices) {
                 cars[x].run(randomNum = (0..9).random())
             }
-            ResultView.printRace(cars)
+            ResultView.showRace(cars)
         }
     }
 }
--- src/main/kotlin/racingcar/ResultView.kt
+++ src/main/kotlin/racingcar/ResultView.kt
@@ -4,9 +4,9 @@ object ResultView {
     private const val DISTANCE_MARK = "-"
     private const val TEXT_RACING_RESULT = "실행 결과"
 
-    fun printResultSignal() = println("\n$TEXT_RACING_RESULT")
+    fun showResultTitle() = println("\n$TEXT_RACING_RESULT")
 
-    fun printRace(cars: List<RacingCar>) {
+    fun showRace(cars: List<RacingCar>) {
         cars.forEach { car ->
             repeat(car.distance) { print(DISTANCE_MARK) }
             println()
