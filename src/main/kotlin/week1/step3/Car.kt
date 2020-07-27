d62a7dfd187f3a16a1d0d61bd0f5c04ce17c595d
BeokBeok <kekemusa37@nate.com>
2020-07-27 17:13:57 +0900

    feat(step3) : 추가 - InputView 클래스 및 readCarCount 함수

---
+++ src/main/kotlin/week1/step3/Car.kt
@@ -0,0 +1,18 @@
+package week1.step3
+
+class Car {
+    private var forwardCount = 1
+
+    fun updateForwardCount() {
+        if (availableForward()) {
+            forwardCount++
+        }
+    }
+
+    private fun availableForward(): Boolean = WINNER_NUMBER_RANGE.random() > WINNER_NUMBER_CUT_LINE
+
+    companion object {
+        private val WINNER_NUMBER_RANGE = 0..9
+        private const val WINNER_NUMBER_CUT_LINE = 3
+    }
+}
