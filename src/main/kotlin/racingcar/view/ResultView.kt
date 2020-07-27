4776516ca470ca033ae37f547a18417940a12c7c
zion830 <zion830@honglab.org>
2020-07-27 18:59:12 +0900

    feat(racingcar): 입력, 출력 View 구현

--- src/main/kotlin/racingcar/view/ResultView.kt
+++ src/main/kotlin/racingcar/view/ResultView.kt
@@ -1,9 +1,15 @@
 package racingcar.view
 
+import racingcar.model.Participants
+
 object ResultView {
     private const val RESULT = "실행 결과"
 
     fun printResultTitle() {
         println(RESULT)
+    }
+
+    fun printParticipantsPath(participants: Participants) {
+        println(participants.getMovingStatus())
     }
 }
