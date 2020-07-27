9e46bf0cc2d11cab7e43799931d4858cc50209fb
zion830 <zion830@honglab.org>
2020-07-27 17:52:41 +0900

    feat(racingcar): Car 도메인 객체 구현

---
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -0,0 +1,27 @@
+package racingcar.model
+
+import racingcar.strategy.MovingStrategy
+
+data class Car(
+    val id: Int,
+    private val movingStrategy: MovingStrategy
+) {
+    var position = 0
+        private set
+
+    fun getName() = "$NAME_TITLE$id"
+
+    fun getMovingPath() = MOVING_SYMBOL.repeat(position)
+
+    fun moveForward() {
+        if (movingStrategy.isMovable) {
+            position += MOVING_DISTANCE
+        }
+    }
+
+    companion object {
+        private const val NAME_TITLE = "Car No."
+        private const val MOVING_DISTANCE = 1
+        private const val MOVING_SYMBOL = "-"
+    }
+}
