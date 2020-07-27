0d2c691232e68ebe898d724718749e099b3a36b1
bky373 <bo373@naver.com>
2020-07-27 21:38:04 +0900

    refactor(racingcar): add const values of numbers which allow car run

--- src/main/kotlin/racingcar/RacingCar.kt
+++ src/main/kotlin/racingcar/RacingCar.kt
@@ -1,11 +1,16 @@
 package racingcar
 
 class RacingCar {
+    companion object {
+        private const val CAN_MOVE_FROM = 4
+        private const val CAN_MOVE_TO = 9
+    }
+
     var distance: Int = 0
 
     fun run(randomNum: Int) {
         if (isRunnable(randomNum)) distance++
     }
 
-    fun isRunnable(randomNum: Int) = randomNum in 4..9
+    private fun isRunnable(randomNum: Int) = randomNum in CAN_MOVE_FROM..CAN_MOVE_TO
 }
