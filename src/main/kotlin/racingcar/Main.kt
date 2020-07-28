036483c5e95f435aaaa9770ff603844d1d5692eb
Jonny-Cho <andante2183@gmail.com>
2020-07-28 23:07:32 +0900

    refactor(Main): add main.kt file

---
+++ src/main/kotlin/racingcar/Main.kt
@@ -0,0 +1,10 @@
+package racingcar
+
+const val FIRST_QUESTION = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
+const val SECOND_QUESTION = "시도할 횟수는 몇 회인가요?"
+
+fun main() {
+    val carNames = InputView.getCarNames(FIRST_QUESTION)
+    val tryCount = InputView.getAnswerTo(SECOND_QUESTION)
+    RacingCar(carNames).race(tryCount)
+}
