7831a5d61bd9d7553d05dd8c1ae0dc1560bffd99
Jonny-Cho <andante2183@gmail.com>
2020-07-28 23:27:03 +0900

    refactor(Car): move pure functions to top-level

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,8 +1,13 @@
 package racingcar
 
+fun getRandom(randomStartNum: Int, randomEndNum: Int) = (randomStartNum..randomEndNum).random()
+fun greaterThanOrEqualToMovableValue(randomValue: Int) = randomValue >= Car.MOVABLE_VALUE
+
 class Car(val name: String = "default car") {
     companion object {
         const val MOVABLE_VALUE = 4
+        const val RANDOM_START_NUM = 0
+        const val RANDOM_END_NUM = 9
     }
 
     // private setter 이 클래스 밖에서 이 프로퍼티의 값을 바꿀 수 없다.
@@ -20,10 +25,7 @@ class Car(val name: String = "default car") {
         currentPosition++
     }
 
-    internal fun canMove(): Boolean {
-        val randomValue = (0..9).random()
-        return canMove(randomValue)
+    private fun canMove(): Boolean {
+        return greaterThanOrEqualToMovableValue(getRandom(RANDOM_START_NUM, RANDOM_END_NUM))
     }
-
-    internal fun canMove(randomValue: Int) = randomValue >= MOVABLE_VALUE
 }
