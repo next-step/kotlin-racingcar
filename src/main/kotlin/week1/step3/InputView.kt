892a1b0acbca56502935ef16637e961faa3c1339
BeokBeok <kekemusa37@nate.com>
2020-07-29 01:22:06 +0900

    feat(step4) : 제거 - 자동차 이름이 5글자 넘게 입력 시 초과된 글자 자르는 로직

--- src/main/kotlin/week1/step3/InputView.kt
+++ src/main/kotlin/week1/step3/InputView.kt
@@ -2,16 +2,11 @@ package week1.step3
 
 object InputView {
 
-    private const val CAR_NAME_MAX_LENGTH = 5
-
     fun getCarNamesAndTryCount(): Pair<List<String>, Int> = readCarNames() to readTryCount()
 
     private fun readCarNames(): List<String> {
         println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
-        val carNames = readLine()?.split(",") ?: emptyList()
-        return carNames.filter { name ->
-            name.length <= CAR_NAME_MAX_LENGTH
-        }
+        return readLine()?.split(",") ?: emptyList()
     }
 
     private fun readTryCount(): Int {
