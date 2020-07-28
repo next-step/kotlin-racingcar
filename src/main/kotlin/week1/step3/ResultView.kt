35539e52ecd7912c681671e2f645d29d1d5528ae
BeokBeok <kekemusa37@nate.com>
2020-07-29 01:13:59 +0900

    feat(step4) : 적용 - 우승자 출력하기

--- src/main/kotlin/week1/step3/ResultView.kt
+++ src/main/kotlin/week1/step3/ResultView.kt
@@ -8,6 +8,18 @@ object ResultView {
             printCarForwardState(racingStep, position)
             printNewline(position, carNames)
         }
+        printWinner(racingStep, carNames)
+    }
+
+    private fun printWinner(racingStep: List<Int>, carNames: List<String>) {
+        val finalRound = racingStep.subList(racingStep.lastIndex - carNames.lastIndex, racingStep.size)
+        val winners = mutableListOf<String>()
+        for (index in finalRound.indices) {
+            if (finalRound[index] == finalRound.max()) {
+                winners.add(carNames[index])
+            }
+        }
+        println("${winners.joinToString()}가 최종 우승했습니다.")
     }
 
     private fun printNewline(index: Int, carNames: List<String>) {
