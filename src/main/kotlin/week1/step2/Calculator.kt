e1405198d0e51e92a683367bdae8e334eb0dcb94
BeokBeok <kekemusa37@nate.com>
2020-07-28 21:41:29 +0900

    fix(step2) : 적용 - require 단언문
    - review link : https://github.com/next-step/kotlin-racingcar/pull/30#discussion_r460466366

--- src/main/kotlin/week1/step2/Calculator.kt
+++ src/main/kotlin/week1/step2/Calculator.kt
@@ -9,7 +9,7 @@ object Calculator {
     private const val ASCII_NINE = 57
 
     fun calculateFormula(formula: String?): Double {
-        if (formula.isNullOrEmpty()) {
+        require(!formula.isNullOrEmpty()) {
             throw IllegalArgumentException("입력 값이 유효하지 않습니다.")
         }
 
