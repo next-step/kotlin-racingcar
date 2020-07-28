146541581b153ef588ff9daf21ea429512174dac
hyunsung.shin <cordorshs@gmail.com>
2020-07-28 22:31:23 +0900

    refactoring(Calculator) :  - remove Assertions redundancy keyword

--- src/test/kotlin/step2/ExpressionCheckerTest.kt
+++ src/test/kotlin/step2/ExpressionCheckerTest.kt
@@ -1,28 +1,28 @@
 package step2
 
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class ExpressionCheckerTest {
 
     @Test
     fun `number or operator is empty`() {
-        Assertions.assertThat(ExpressionChecker.isEmpty(listOf(1, 2), emptyList())).isEqualTo(true)
-        Assertions.assertThat(ExpressionChecker.isEmpty(emptyList(), listOf(Operator.MUL))).isEqualTo(true)
-        Assertions.assertThat(ExpressionChecker.isEmpty(emptyList(), emptyList())).isEqualTo(true)
+        assertThat(ExpressionChecker.isEmpty(listOf(1, 2), emptyList())).isEqualTo(true)
+        assertThat(ExpressionChecker.isEmpty(emptyList(), listOf(Operator.MUL))).isEqualTo(true)
+        assertThat(ExpressionChecker.isEmpty(emptyList(), emptyList())).isEqualTo(true)
     }
 
     @Test
     fun `valid expression size`() {
-        Assertions.assertThat(ExpressionChecker.isValidExpressionSize(listOf(1, 2), listOf(Operator.MUL)))
+        assertThat(ExpressionChecker.isValidExpressionSize(listOf(1, 2), listOf(Operator.MUL)))
             .isEqualTo(true)
     }
 
     @Test
     fun `invalid expression size`() {
-        Assertions.assertThat(ExpressionChecker.isValidExpressionSize(listOf(1, 2), listOf(Operator.MUL, Operator.ADD)))
+        assertThat(ExpressionChecker.isValidExpressionSize(listOf(1, 2), listOf(Operator.MUL, Operator.ADD)))
             .isEqualTo(false)
-        Assertions.assertThat(ExpressionChecker.isValidExpressionSize(listOf(1, 2, 4, 5), listOf(Operator.MUL)))
+        assertThat(ExpressionChecker.isValidExpressionSize(listOf(1, 2, 4, 5), listOf(Operator.MUL)))
             .isEqualTo(false)
     }
 
--- src/test/kotlin/step2/TypeExtractorTest.kt
+++ src/test/kotlin/step2/TypeExtractorTest.kt
@@ -1,6 +1,7 @@
 package step2
 
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 import step2.TypeExtractor.mappingOperator
 
@@ -8,123 +9,123 @@ class TypeExtractorTest {
 
     @Test
     fun `Extract number test - single number`() {
-        Assertions.assertThat(TypeExtractor.extractNumber("1")).isEqualTo(listOf(1))
+        assertThat(TypeExtractor.extractNumber("1")).isEqualTo(listOf(1))
     }
 
     @Test
     fun `Extract number test - multiple number`() {
-        Assertions.assertThat(TypeExtractor.extractNumber("1111")).isEqualTo(listOf(1111))
+        assertThat(TypeExtractor.extractNumber("1111")).isEqualTo(listOf(1111))
     }
 
     @Test
     fun `Extract number test - single number & character`() {
-        Assertions.assertThat(TypeExtractor.extractNumber("a1")).isEqualTo(listOf(1))
-        Assertions.assertThat(TypeExtractor.extractNumber("1a")).isEqualTo(listOf(1))
+        assertThat(TypeExtractor.extractNumber("a1")).isEqualTo(listOf(1))
+        assertThat(TypeExtractor.extractNumber("1a")).isEqualTo(listOf(1))
     }
 
     @Test
     fun `Extract number test - character precedes the number`() {
-        Assertions.assertThat(TypeExtractor.extractNumber("aa10bb20cc30")).satisfies {
-            Assertions.assertThat(it.size).isEqualTo(3)
-            Assertions.assertThat(it).isEqualTo(listOf(10, 20, 30))
+        assertThat(TypeExtractor.extractNumber("aa10bb20cc30")).satisfies {
+            assertThat(it.size).isEqualTo(3)
+            assertThat(it).isEqualTo(listOf(10, 20, 30))
         }
     }
 
     @Test
     fun `Extract number test - number precedes the character`() {
-        Assertions.assertThat(TypeExtractor.extractNumber("10aa20bb30cc")).satisfies {
-            Assertions.assertThat(it.size).isEqualTo(3)
-            Assertions.assertThat(it).isEqualTo(listOf(10, 20, 30))
+        assertThat(TypeExtractor.extractNumber("10aa20bb30cc")).satisfies {
+            assertThat(it.size).isEqualTo(3)
+            assertThat(it).isEqualTo(listOf(10, 20, 30))
         }
     }
 
     @Test
     fun `Extract Operator test - single operator`() {
-        Assertions.assertThat(TypeExtractor.extractOperator("+")).isEqualTo(listOf("+"))
-        Assertions.assertThat(TypeExtractor.extractOperator("-")).isEqualTo(listOf("-"))
-        Assertions.assertThat(TypeExtractor.extractOperator("*")).isEqualTo(listOf("*"))
-        Assertions.assertThat(TypeExtractor.extractOperator("/")).isEqualTo(listOf("/"))
-        Assertions.assertThat(TypeExtractor.extractOperator("?")).isEqualTo(emptyList<String>())
+        assertThat(TypeExtractor.extractOperator("+")).isEqualTo(listOf("+"))
+        assertThat(TypeExtractor.extractOperator("-")).isEqualTo(listOf("-"))
+        assertThat(TypeExtractor.extractOperator("*")).isEqualTo(listOf("*"))
+        assertThat(TypeExtractor.extractOperator("/")).isEqualTo(listOf("/"))
+        assertThat(TypeExtractor.extractOperator("?")).isEqualTo(emptyList<String>())
     }
 
     @Test
     fun `Extract Operator test - multiple operator`() {
-        Assertions.assertThat(TypeExtractor.extractOperator("++++++")).isEqualTo(listOf("+", "+", "+", "+", "+", "+"))
-        Assertions.assertThat(TypeExtractor.extractOperator("??????")).isEqualTo(emptyList<String>())
+        assertThat(TypeExtractor.extractOperator("++++++")).isEqualTo(listOf("+", "+", "+", "+", "+", "+"))
+        assertThat(TypeExtractor.extractOperator("??????")).isEqualTo(emptyList<String>())
     }
 
     @Test
     fun `Extract Operator test - single operator & character`() {
-        Assertions.assertThat(TypeExtractor.extractOperator("1*")).isEqualTo(listOf("*"))
-        Assertions.assertThat(TypeExtractor.extractOperator("*1")).isEqualTo(listOf("*"))
-        Assertions.assertThat(TypeExtractor.extractOperator("11")).isEqualTo(emptyList<String>())
+        assertThat(TypeExtractor.extractOperator("1*")).isEqualTo(listOf("*"))
+        assertThat(TypeExtractor.extractOperator("*1")).isEqualTo(listOf("*"))
+        assertThat(TypeExtractor.extractOperator("11")).isEqualTo(emptyList<String>())
     }
 
     @Test
     fun `Extract Operator test - multiple operator * character`() {
-        Assertions.assertThat(TypeExtractor.extractOperator("1**3*4*5")).satisfies {
-            Assertions.assertThat(it.size).isEqualTo(4)
-            Assertions.assertThat(it).isEqualTo(listOf("*", "*", "*", "*"))
+        assertThat(TypeExtractor.extractOperator("1**3*4*5")).satisfies {
+            assertThat(it.size).isEqualTo(4)
+            assertThat(it).isEqualTo(listOf("*", "*", "*", "*"))
         }
-        Assertions.assertThat(TypeExtractor.extractOperator("*1-2+3//5")).satisfies {
-            Assertions.assertThat(it.size).isEqualTo(5)
-            Assertions.assertThat(it).isEqualTo(listOf("*", "-", "+", "/", "/"))
+        assertThat(TypeExtractor.extractOperator("*1-2+3//5")).satisfies {
+            assertThat(it.size).isEqualTo(5)
+            assertThat(it).isEqualTo(listOf("*", "-", "+", "/", "/"))
         }
     }
 
     @Test
     fun `Valid expression`() {
-        Assertions.assertThat(TypeExtractor.extract("1*2*3*4*5")).satisfies {
-            Assertions.assertThat(it.first).isEqualTo(listOf(1, 2, 3, 4, 5))
-            Assertions.assertThat(it.second).isEqualTo(listOf(Operator.MUL, Operator.MUL, Operator.MUL, Operator.MUL))
+        assertThat(TypeExtractor.extract("1*2*3*4*5")).satisfies {
+            assertThat(it.first).isEqualTo(listOf(1, 2, 3, 4, 5))
+            assertThat(it.second).isEqualTo(listOf(Operator.MUL, Operator.MUL, Operator.MUL, Operator.MUL))
         }
 
-        Assertions.assertThat(TypeExtractor.extract("11+2/3*4-5")).satisfies {
-            Assertions.assertThat(it.first).isEqualTo(listOf(11, 2, 3, 4, 5))
-            Assertions.assertThat(it.second).isEqualTo(listOf(Operator.ADD, Operator.DIV, Operator.MUL, Operator.SUB))
+        assertThat(TypeExtractor.extract("11+2/3*4-5")).satisfies {
+            assertThat(it.first).isEqualTo(listOf(11, 2, 3, 4, 5))
+            assertThat(it.second).isEqualTo(listOf(Operator.ADD, Operator.DIV, Operator.MUL, Operator.SUB))
         }
     }
 
     @Test
     fun `Invalid expression`() {
-        Assertions.assertThatThrownBy { TypeExtractor.extract("*1*2*3*4*5") }
+        assertThatThrownBy { TypeExtractor.extract("*1*2*3*4*5") }
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessageContaining("Invalid expression's format")
 
-        Assertions.assertThatThrownBy { TypeExtractor.extract("1**2*3*4*5") }
+        assertThatThrownBy { TypeExtractor.extract("1**2*3*4*5") }
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessageContaining("Invalid expression's format")
 
-        Assertions.assertThatThrownBy { TypeExtractor.extract("1*2*3*4**5") }
+        assertThatThrownBy { TypeExtractor.extract("1*2*3*4**5") }
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessageContaining("Invalid expression's format")
 
-        Assertions.assertThatThrownBy { TypeExtractor.extract("/1++2-3/4**5") }
+        assertThatThrownBy { TypeExtractor.extract("/1++2-3/4**5") }
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessageContaining("Invalid expression's format")
     }
 
     @Test
     fun `Empty expression`() {
-        Assertions.assertThatThrownBy { TypeExtractor.extract("") }
+        assertThatThrownBy { TypeExtractor.extract("") }
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessageContaining("Invalid empty expression")
 
-        Assertions.assertThatThrownBy { TypeExtractor.extract("11") }
+        assertThatThrownBy { TypeExtractor.extract("11") }
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessageContaining("Invalid empty expression")
 
-        Assertions.assertThatThrownBy { TypeExtractor.extract("+") }
+        assertThatThrownBy { TypeExtractor.extract("+") }
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessageContaining("Invalid empty expression")
     }
 
     @Test
     fun `Mapping Operator enum class`() {
-        Assertions.assertThat(listOf("+", "-", "*", "/").mappingOperator())
+        assertThat(listOf("+", "-", "*", "/").mappingOperator())
             .isEqualTo(listOf(Operator.ADD, Operator.SUB, Operator.MUL, Operator.DIV))
 
-        Assertions.assertThatThrownBy { listOf("-", "-", "-", "0").mappingOperator() }
+        assertThatThrownBy { listOf("-", "-", "-", "0").mappingOperator() }
             .isInstanceOf(IllegalStateException::class.java)
             .hasMessageContaining("Invalid operator type")
     }
