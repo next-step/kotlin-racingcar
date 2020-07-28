d98b2c02683729c6659bc90a5a4e65d9c6798b9d
EthanChae <boschae36@gmail.com>
2020-07-28 23:08:26 +0900

    refactor(carRacing) - Move the function to execute the CarRacing.kt

--- src/main/kotlin/CarRacing.kt
+++ src/main/kotlin/CarRacing.kt
@@ -1,5 +1,22 @@
 // 모든 메소드가 companion object 일 경우 object 로 할 것
 // object vs class
+//  try {
+//
+//         CarRacingView.showFirstInstruction()
+//         val numberOfCars = CarRacingView.checkInput(readLine()!!)
+//         CarRacingView.showSecondInstruction()
+//         val numberOfTrial = CarRacingView.checkInput(readLine()!!)
+//
+//         println()
+//         for (i in 1..numberOfTrial) {
+//             for (j: Int in 1..numberOfCars) {
+//                 CarRacingView.showMovement(CarRacing.countOfMovement(numberOfCars))
+//             }
+//             println()
+//         }
+//     } catch (e: Exception) {
+//         println(e.message)
+//     }
 object CarRacing {
 
     private fun moveOrStop(): Boolean {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,2 @@
 fun main() {
-    try {
-
-        CarRacingView.showFirstInstruction()
-        val numberOfCars = CarRacingView.checkInput(readLine()!!)
-        CarRacingView.showSecondInstruction()
-        val numberOfTrial = CarRacingView.checkInput(readLine()!!)
-
-        println()
-        for (i in 1..numberOfTrial) {
-            for (j: Int in 1..numberOfCars) {
-                CarRacingView.showMovement(CarRacing.countOfMovement(numberOfCars))
-            }
-            println()
-        }
-    } catch (e: Exception) {
-        println(e.message)
-    }
 }
