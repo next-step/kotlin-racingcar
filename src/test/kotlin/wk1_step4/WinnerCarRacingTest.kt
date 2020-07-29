04273ab8db5b317ee540a153d9d4f8614162ae21
EthanChae <boschae36@gmail.com>
2020-07-29 13:14:06 +0900

    refactor(carRacing) - Optimize imports

--- src/test/kotlin/wk1_step4/WinnerCarRacingTest.kt
+++ src/test/kotlin/wk1_step4/WinnerCarRacingTest.kt
@@ -1,11 +1,11 @@
 package wk1_step4
 
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThatThrownBy
 
 class WinnerCarRacingTest {
 
     fun `test for car move or stop`() {
-        Assertions.assertThatThrownBy {
+        assertThatThrownBy {
             WinnerCarRacing.updateCarRacing(listOf())
         }.isInstanceOf(WinnerCarRacingException::class.java)
     }
--- src/test/kotlin/wk1_step4/WinnerCarRacingViewTest.kt
+++ src/test/kotlin/wk1_step4/WinnerCarRacingViewTest.kt
@@ -1,60 +1,60 @@
 package wk1_step4
 
-import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThatThrownBy
 
 class WinnerCarRacingViewTest {
     @Test
     fun `자동차 이름이 없거나 null 인 경우`() {
-        Assertions.assertThatThrownBy {
+        assertThatThrownBy {
             WinnerCarRacingView.checkCarsValidation("")
         }.isInstanceOf(java.lang.NullPointerException::class.java)
 
-        Assertions.assertThatThrownBy {
+        assertThatThrownBy {
             WinnerCarRacingView.checkCarsValidation(null)
         }.isInstanceOf(java.lang.NullPointerException::class.java)
     }
 
     @Test
     fun `자동차 이름에 영문, "," 외의 문자열이 포함될 경우`() {
-        Assertions.assertThatThrownBy {
+        assertThatThrownBy {
             WinnerCarRacingView.checkCarsValidation("test%ta")
         }.isInstanceOf(WinnerCarRacingException::class.java)
     }
 
     @Test
     fun `자동차 이름이 다섯글자 초과인 경우`() {
-        Assertions.assertThatThrownBy {
+        assertThatThrownBy {
             WinnerCarRacingView.parsingCars("longName,ta")
         }.isInstanceOf(WinnerCarRacingException::class.java)
     }
     @Test
     fun `글자 마지막에 ","가 있는 경우`() {
-        Assertions.assertThatThrownBy {
+        assertThatThrownBy {
             WinnerCarRacingView.parsingCars("longName,ta")
         }.isInstanceOf(WinnerCarRacingException::class.java)
     }
 
     @Test
     fun `시도 횟수가 없거나 null 인 경우`() {
-        Assertions.assertThatThrownBy {
+        assertThatThrownBy {
             WinnerCarRacingView.checkCountValidation(null)
         }.isInstanceOf(NullPointerException::class.java)
-        Assertions.assertThatThrownBy {
+        assertThatThrownBy {
             WinnerCarRacingView.checkCountValidation("")
         }.isInstanceOf(NullPointerException::class.java)
     }
 
     @Test
     fun `시도 횟수가 1보다 작을 경우`() {
-        Assertions.assertThatThrownBy {
+        assertThatThrownBy {
             WinnerCarRacingView.checkCountValidation("-1")
         }.isInstanceOf(ArithmeticException::class.java)
     }
 
     @Test
     fun `시도 횟수가 숫자가 아닌경우`() {
-        Assertions.assertThatThrownBy {
+        assertThatThrownBy {
             WinnerCarRacingView.checkCountValidation("ㄱ")
         }.isInstanceOf(NumberFormatException::class.java)
     }
