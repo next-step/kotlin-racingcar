7c6cd213fb2fffaa9b4f1cf40a6be87a4c1f2dc9
EthanChae <boschae36@gmail.com>
2020-07-29 13:21:13 +0900

    refactor(carRacing) - Divide method by function

--- src/main/kotlin/wk1_step4/WinnerCarRacing.kt
+++ src/main/kotlin/wk1_step4/WinnerCarRacing.kt
@@ -4,9 +4,12 @@ import java.lang.IndexOutOfBoundsException
 
 object WinnerCarRacing {
 
+    private fun generateRandomNumber(): Int {
+        return ((Math.random() * 10).toInt())
+    }
+
     private fun moveOrStop(car: Car) {
-        val number: Int = ((Math.random() * 10).toInt())
-        if (number > 3) car.position = car.position + 1
+        if (generateRandomNumber() > 3) car.position = car.position + 1
     }
 
     fun updateCarRacing(cars: List<Car>) {
