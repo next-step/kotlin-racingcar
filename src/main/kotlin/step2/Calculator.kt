6a5e699f224f73cea2e63c36e3d3a6011082c1d9
jejun <jjin.dev@gmail.com>
2020-07-29 21:31:16 +0900

    Refactor(step2): 리뷰 내용 반영
    - Operator를 별도의 객체로 분리 구현
    - calculate 함수 내부구조 개선

--- src/main/kotlin/step2/Calculator.kt
+++ src/main/kotlin/step2/Calculator.kt
@@ -1,68 +1,20 @@
 package step2
 
 class Calculator {
-    private val formulaSplitDelimiter = " "
 
     fun calculate(formula: String): Double {
         require(formula.isNotBlank()) { "값을 입력해 주세요" }
 
+        val formulaSplitDelimiter = " "
         val values = formula.split(formulaSplitDelimiter)
         var outcome = values[0].toDouble()
-        var operator = getOperatorType(values[1])
 
-        for (str in 2 until values.size) {
-            val value = values[str]
+        for (index in 1 until values.size step 2) {
+            val operator = Operator(values[index])
+            val number = values[index + 1].toDouble()
 
-            if (isNumber(value)) outcome = calculate(outcome, value.toDouble(), operator)
-            if (isOperator(value)) operator = getOperatorType(value)
-            if (!isValidFormula(value)) throw IllegalArgumentException("올바른 수식이 아닙니다.")
+            outcome = operator.calculate(outcome, number)
         }
         return outcome
     }
-
-    private fun isValidFormula(str: String): Boolean {
-        return isNumber(str) || isOperator(str)
-    }
-
-    private fun isNumber(str: String): Boolean {
-        return try {
-            str.toDouble()
-            true
-        } catch (e: IllegalArgumentException) {
-            false
-        }
-    }
-
-    private fun isOperator(str: String): Boolean {
-        val operatorType = getOperatorType(str)
-
-        return operatorType == OperatorType.PLUS ||
-            operatorType == OperatorType.MINUS ||
-            operatorType == OperatorType.MULTIPLE ||
-            operatorType == OperatorType.DIVIDE
-    }
-
-    private fun calculate(current: Double, change: Double, operator: OperatorType): Double {
-        if (operator == OperatorType.PLUS) return current + change
-        if (operator == OperatorType.MINUS) return current - change
-        if (operator == OperatorType.MULTIPLE) return current * change
-        if (operator == OperatorType.DIVIDE) return current / change
-        return current / change
-    }
-
-    enum class OperatorType {
-        PLUS,
-        MINUS,
-        MULTIPLE,
-        DIVIDE
-    }
-
-    fun getOperatorType(s: String): OperatorType =
-        when (s) {
-            "+" -> OperatorType.PLUS
-            "-" -> OperatorType.MINUS
-            "*" -> OperatorType.MULTIPLE
-            "/" -> OperatorType.DIVIDE
-            else -> OperatorType.DIVIDE
-        }
 }
