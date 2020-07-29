1f778426d58b286cf363a3041643ff2ab7dcbc16
bky373 <bo373@naver.com>
2020-07-29 14:21:51 +0900

    refactor(racinggame): separate view related logics from start method and add save record function

---
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -0,0 +1,16 @@
+package racingcar
+
+class RacingGame(
+    private val carNames: List<String>,
+    private val trials: Int
+) {
+    // TODO start() 테스트 어떻게 하면 좋을지 고민, start() 안의 로직은 각각 테스트했으니 할 필요가 없을지도?
+    fun start() {
+        val cars = CarFactory.makeCars(carNames)
+
+        for (i in 0 until trials) {
+            cars.forEach { it.run(flag = (0..9).random()) }
+            RecordRepository.save(nth = i, nthRecord = Record(cars))
+        }
+    }
+}
