c7a92421113bfde2001a744525b95c0cb8363ef2
hyunsung.shin <cordorshs@gmail.com>
2020-07-30 19:06:09 +0900

    feat(Calculator) :  - kotlin 제공 함수로 변경

--- src/main/kotlin/step2/Main.kt
+++ src/main/kotlin/step2/Main.kt
@@ -1,10 +1,8 @@
 package step2
 
-import java.util.Scanner
-
 fun main() {
     StringCalculator.parsing(
-        stringExpression = Scanner(System.`in`).nextLine(),
+        stringExpression = readLine(),
         result = { expressionPair ->
             println(StringCalculator.evaluate(expressionPair.first, expressionPair.second))
         }
