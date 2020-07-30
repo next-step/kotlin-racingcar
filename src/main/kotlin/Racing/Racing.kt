18206b885956b496af7f25136fb1bf045c152e49
myeongjang <jacobmjjo@gmail.com>
2020-07-31 01:23:23 +0900

    피드백 반영 시작

---
+++ src/main/kotlin/Racing/Racing.kt
@@ -0,0 +1,24 @@
+class InputView {
+    fun inputcarnumber(): Int {
+        print("Please Enter cars number")
+        return readLine()!!.toInt()
+    }
+
+    fun inputcircuitnumber(): Int {
+        print("Please Enter circuit number")
+        return readLine()!!.toInt()
+    }
+}
+
+class OutputView() {
+    fun printcircuit(number: Int) {
+        print("_".repeat(number))
+    }
+}
+
+class Car {
+    companion object{
+        fun 
+    }
+
+}
--- src/test/kotlin/RacingTest.kt
+++ src/test/kotlin/RacingTest.kt
@@ -1,12 +1,12 @@
+import Racing.Start
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
-
-class RacingTest{
+class RacingTest {
     @Test
-    @DisplayName("check Start")
-    fun starttest(){
+    @DisplayName("check Racing.Start")
+    fun starttest() {
         assertThat(Start().cars(3)).isEqualTo(mapOf("car0" to 1, "car1" to 1, "car2" to 1))
     }
 /*  프린트 테스트 방법은 습득을 못했습니다.
@@ -14,15 +14,14 @@ class RacingTest{
     @DisplayName("check print")
     fun `check print`(){
         val sample = mutableMapOf("car1" to 2)
-        assertThat(Printcircuit().circuitprint(sample)).isEqualTo("__")
+        assertThat(Racing.Printcircuit().circuitprint(sample)).isEqualTo("__")
     } */
     /*      랜덤을 테스트 하는 방법도 실행을 못했습니다.
     @Test
     @DisplayName("final check")
     fun `final check`(){
         val sample = mutableMapOf("car0" to 1, "car1" to 1, "car2" to 1)
-        assertThat(Circuit().)
+        assertThat(Racing.Circuit().)
     }
     */
 }
-
