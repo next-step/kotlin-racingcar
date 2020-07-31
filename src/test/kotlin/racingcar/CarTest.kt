efecf539f33e9f8d9880727664df7dc0e2eb86b5
BeokBeok <kekemusa37@nate.com>
2020-07-31 23:39:46 +0900

    feat(test) : [적용] 자동차에 5글자 이상 이름 입력 TC (성공)

--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -1,10 +1,15 @@
 package racingcar
 
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.Test
+import racingcar.model.Car
 
 class CarTest {
 
     @Test
     fun `자동차에 5글자 이상 이름 입력`() {
+        assertThatIllegalArgumentException().isThrownBy {
+            Car("abcdef")
+        }
     }
 }
