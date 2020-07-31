12780d3979fc4702260f42266d91262842f57b49
BeokBeok <kekemusa37@nate.com>
2020-08-01 01:23:35 +0900

    feat(refact) : [추가] printRacingStep 함수

--- src/main/kotlin/racingcar/view/ResultView.kt
+++ src/main/kotlin/racingcar/view/ResultView.kt
@@ -7,12 +7,20 @@ object ResultView {
 
         val tryCount = racingHistory[0].size
         repeat(tryCount) { tryCnt ->
-            repeat(racingCarNames.size) { racingCnt ->
-                print("${racingCarNames[racingCnt]} : ")
-                print("-".repeat(racingHistory[racingCnt][tryCnt]))
-                println()
-            }
+            printRacingStep(racingCarNames, racingHistory, tryCnt)
+        }
+    }
+
+    private fun printRacingStep(
+        racingCarNames: List<String>,
+        racingHistory: Array<Array<Int>>,
+        tryCnt: Int
+    ) {
+        repeat(racingCarNames.size) { racingCnt ->
+            print("${racingCarNames[racingCnt]} : ")
+            print("-".repeat(racingHistory[racingCnt][tryCnt]))
             println()
         }
+        println()
     }
 }
