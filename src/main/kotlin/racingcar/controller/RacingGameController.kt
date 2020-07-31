285e73113f727bf1c0d4e32cdde4e6ab5eeffb30
BeokBeok <kekemusa37@nate.com>
2020-08-01 01:25:20 +0900

    feat(step5) : [적용] 레이싱 화면 입출력

--- src/main/kotlin/racingcar/controller/RacingGameController.kt
+++ src/main/kotlin/racingcar/controller/RacingGameController.kt
@@ -1,7 +1,22 @@
 package racingcar.controller
 
+import racingcar.model.Participant
 import racingcar.view.InputView
+import racingcar.view.ResultView
 
 fun main() {
     val (racingCarNames, tryCounts) = InputView.requestRacingCarNames() to InputView.requestTryCount()
+    val participant = Participant(racingCarNames, tryCounts)
+
+    startRacing(tryCounts, participant)
+    showContents(racingCarNames, participant)
+}
+
+private fun showContents(racingCarNames: List<String>, participant: Participant) =
+    ResultView.printRacingProcess(racingCarNames, participant.movingDistanceHistory)
+
+private fun startRacing(tryCounts: Int, participant: Participant) {
+    repeat(tryCounts) {
+        participant.play(it)
+    }
 }
