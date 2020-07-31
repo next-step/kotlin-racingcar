56c3a676aa3a7215a15df485aa259ea92082391c
BeokBeok <kekemusa37@nate.com>
2020-08-01 02:03:49 +0900

    feat(test) : [추가] 우승자가 여러명인 경우 TC

--- src/test/kotlin/racingcar/ParticipantTest.kt
+++ src/test/kotlin/racingcar/ParticipantTest.kt
@@ -14,4 +14,13 @@ class ParticipantTest {
         cars[0].move(4)
         assertThat(participant.findWinner()[0].name).isEqualTo(cars[0].name)
     }
+
+    @Test
+    fun `우승자가 여러 명인 경우`() {
+        val cars = listOf(Car("1st"), Car("2nd"), Car("best"))
+        val participant = Participant(racingCars = cars, tryCount = 1)
+        cars[0].move(4)
+        cars[2].move(4)
+        assertThat(participant.findWinner().map { it.name }).isEqualTo(listOf(cars[0].name, cars[2].name))
+    }
 }
