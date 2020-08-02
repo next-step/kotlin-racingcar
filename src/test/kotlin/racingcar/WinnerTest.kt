d513887721a35c7a11a2c5a5e29a184f17b917a6
BeokBeok <kekemusa37@nate.com>
2020-08-03 00:20:37 +0900

    fix(test) : [변경] 공통적으로 사용하는 변수를 지역 변수로

--- src/test/kotlin/racingcar/WinnerTest.kt
+++ src/test/kotlin/racingcar/WinnerTest.kt
@@ -1,6 +1,7 @@
 package racingcar
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import racingcar.model.Car
 import racingcar.model.RacingGame
@@ -8,12 +9,19 @@ import racingcar.model.Winner
 
 class WinnerTest {
 
+    private lateinit var cars: List<Car>
+    private lateinit var racingGame: RacingGame
+    private lateinit var winner: Winner
+
+    @BeforeEach
+    fun setup() {
+        cars = listOf(Car("one"), Car("two"), Car("three"))
+        racingGame = RacingGame(racingCars = cars)
+        winner = Winner(racingGame)
+    }
+
     @Test
     fun `우승자가 한 명인 경우`() {
-        val cars = listOf(Car("1st"), Car("2nd"), Car("3rd"))
-        val racingGame = RacingGame(racingCars = cars)
-        val winner = Winner(racingGame)
-
         cars[0].move(4)
 
         assertThat(winner.find()).isEqualTo(cars[0].name)
@@ -21,10 +29,6 @@ class WinnerTest {
 
     @Test
     fun `우승자가 여러 명인 경우`() {
-        val cars = listOf(Car("1st"), Car("2nd"), Car("best"))
-        val racingGame = RacingGame(racingCars = cars)
-        val winner = Winner(racingGame)
-
         cars[0].move(4)
         cars[2].move(4)
 
