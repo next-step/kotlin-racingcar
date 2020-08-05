60dce5217d31f1822e377b8b440829ec02521575
LeeYounghyeon <leey9494@icloud.com>
2020-08-06 00:51:45 +0900

    refactor(racingCar): cars move를 cars에게 위임한다

--- src/main/kotlin/racingCar/RacingGame.kt
+++ src/main/kotlin/racingCar/RacingGame.kt
@@ -1,18 +1,7 @@
 package racingCar
 
-import kotlin.random.Random
-
 object RacingGame {
-    private const val MIN_NUMBER = 0
-    private const val MAX_NUMBER = 10
-
     fun playGame(cars: Cars) {
-        for (car in cars.cars) {
-            car.move(getRandomNumbers())
-        }
-    }
-
-    private fun getRandomNumbers(): Int {
-        return Random.nextInt(MIN_NUMBER, MAX_NUMBER)
+        cars.moveCars()
     }
 }
--- src/test/kotlin/racingCar/CarTest.kt
+++ src/test/kotlin/racingCar/CarTest.kt
@@ -1,29 +1,26 @@
 package racingCar
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class CarTest {
-    private lateinit var car: Car
-
-    @BeforeEach
-    fun setUp() {
-        car = Car()
-    }
+    private val car = Car()
 
     @DisplayName(value = "입력된 숫자가 4 이상이면 전진한다.")
-    @Test
-    fun `car move`() {
-        car.move(4)
+    @ParameterizedTest
+    @ValueSource(ints = [4, 5, 6, 7, 8])
+    fun `car move`(number: Int) {
+        car.move(number)
         assertThat(car.distance).isEqualTo(1)
     }
 
-    @DisplayName(value = "입력된 숫자가 4 이하면 움직이지 않는다.")
-    @Test
-    fun name() {
-        car.move(1)
+    @DisplayName(value = "입력된 숫자가 4 보다 작면 움직이지 않는다.")
+    @ParameterizedTest
+    @ValueSource(ints = [1, 2, 3])
+    fun `car not move`(number: Int) {
+        car.move(number)
         assertThat(car.distance).isEqualTo(0)
     }
 }
