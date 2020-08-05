7911e04e03dc8659ce353bc5b1add8ee653184a1
JeongChanhee <chany5238@gmail.com>
2020-08-06 00:26:20 +0900

    불필요한 파라미터 제거

--- src/main/kotlin/racingCar/domain/Car.kt
+++ src/main/kotlin/racingCar/domain/Car.kt
@@ -8,12 +8,12 @@ class Car(val name: String, var distance: Int = 0) {
 
     fun changeStateRacingCar(inputRandomNum: Int) {
         if (inputRandomNum >= STANDARD_VALUE_TO_MOVE) {
-            return move(true)
+            return move()
         }
     }
 
-    private fun move(isMove: Boolean) {
-        if (isMove) distance += 1
+    private fun move() {
+        distance += 1
     }
 
     companion object {
