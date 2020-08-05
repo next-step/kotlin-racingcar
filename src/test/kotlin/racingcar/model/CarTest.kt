5e25ea622e1df71ab32b1fc2de1b6b9a7f629a82
BeokBeok <kekemusa37@nate.com>
2020-08-05 14:50:56 +0900

    fix(test) : [추가] Car 클래스의 move 함수 테스트
    - review url : https://github.com/next-step/kotlin-racingcar/pull/91#discussion_r465463000

--- src/test/kotlin/racingcar/model/CarTest.kt
+++ src/test/kotlin/racingcar/model/CarTest.kt
@@ -2,9 +2,17 @@ package racingcar.model
 
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class CarTest {
+
+    private lateinit var car: Car
+
+    @BeforeEach
+    fun setup() {
+        car = Car("12345", 0)
+    }
 
     @Test
     fun `자동차 이름이 5글자를 초과한 경우`() {
@@ -15,13 +23,15 @@ class CarTest {
 
     @Test
     fun `차가 전진한 경우`() {
-        val car = Car("12345", 1)
+        car.move(4)
+
         assertThat(car.movingDistance).isEqualTo(1)
     }
 
     @Test
     fun `차가 전진하지 못한 경우`() {
-        val car = Car("12345", 0)
+        car.move(3)
+
         assertThat(car.movingDistance).isEqualTo(0)
     }
 }
