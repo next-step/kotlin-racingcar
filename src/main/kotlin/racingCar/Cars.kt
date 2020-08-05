b60844d971dbed5fc25cfa73c7329cafd933883e
LeeYounghyeon <leey9494@icloud.com>
2020-08-06 00:50:12 +0900

    refactor(racingCar): 자동차를 움직이는 책임을 cars에게 위임하고, cars를 immutable list로 초기화한다.

--- src/main/kotlin/racingCar/Cars.kt
+++ src/main/kotlin/racingCar/Cars.kt
@@ -1,11 +1,20 @@
 package racingCar
 
-class Cars(numberOfPlayer: Int) {
-    var cars = mutableListOf<Car>()
+import kotlin.random.Random
 
-    init {
-        for (car in 0 until numberOfPlayer) {
-            cars.add(Car())
-        }
+class Cars(numberOfPlayer: Int) {
+    val cars = (0 until numberOfPlayer).map { Car() }
+
+    fun moveCars() {
+        cars.forEach { it.move(getRandomNumbers()) }
+    }
+
+    private fun getRandomNumbers(): Int {
+        return Random.nextInt(MIN_NUMBER, MAX_NUMBER)
+    }
+
+    companion object {
+        private const val MIN_NUMBER = 0
+        private const val MAX_NUMBER = 10
     }
 }
