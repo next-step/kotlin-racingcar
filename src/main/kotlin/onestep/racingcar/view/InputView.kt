f6268a731eb2e7875adc7b2a84b4623c0aac257e
namjackson <skawo32167@naver.com>
2020-08-16 02:16:42 +0900

    feature(racingRefactoring) : Refactoring - Add InputView ExceptionCase

--- src/main/kotlin/onestep/racingcar/view/InputView.kt
+++ src/main/kotlin/onestep/racingcar/view/InputView.kt
@@ -4,26 +4,26 @@ class InputView(
     private val getNameOfCarsInput: () -> String? = { readLine() },
     private val getNumberOfTryInput: () -> String? = { readLine() }
 ) {
-    companion object {
-        private const val SEPARATOR = ","
-        private const val INPUT_NAME_OF_CAR = "경주할 자동차 이름을 입력하세요(이름은 쉼표($SEPARATOR)를 기준으로 구분)."
-        private const val INPUT_NUMBER_OF_TRY = "자동차의 전진을 시도할 수를 입력하세요."
-    }
 
     private fun getNameOfCars(): List<String> {
-        val list = showQuestion(INPUT_NAME_OF_CAR, getNameOfCarsInput).split(SEPARATOR)
-
-        return list.map {
-            require(it.length < 5) { "CarName's Length can not more than 5" }
-            it
+        return try {
+            showQuestion(INPUT_NAME_OF_CAR, getNameOfCarsInput).split(SEPARATOR)
+        } catch (e: Exception) {
+            println(INPUT_TRY_AGAIN + e.localizedMessage)
+            getNameOfCars()
         }
     }
 
     private fun getNumberOfTry(): Int {
-        val input = showQuestion(INPUT_NUMBER_OF_TRY, getNumberOfTryInput).toIntOrNull()
-            ?: throw IllegalArgumentException("Wrong Input (Not Integer)")
-        require(input > 0) { "Input Only Positive integer ( input = $input)" }
-        return input
+        return try {
+            val input = showQuestion(INPUT_NUMBER_OF_TRY, getNumberOfTryInput).toIntOrNull()
+                ?: throw IllegalArgumentException("Wrong Input (Not Integer)")
+            require(input > 0) { "Input Only Positive integer ( input = $input)" }
+            input
+        } catch (e: Exception) {
+            println(INPUT_TRY_AGAIN + e.localizedMessage)
+            getNumberOfTry()
+        }
     }
 
     private fun showQuestion(question: String, input: () -> String?): String {
@@ -32,4 +32,11 @@ class InputView(
     }
 
     fun getInputs() = Pair(getNameOfCars(), getNumberOfTry())
+
+    companion object {
+        private const val SEPARATOR = ","
+        private const val INPUT_NAME_OF_CAR = "경주할 자동차 이름을 입력하세요(이름은 쉼표($SEPARATOR)를 기준으로 구분)."
+        private const val INPUT_NUMBER_OF_TRY = "자동차의 전진을 시도할 수를 입력하세요."
+        private const val INPUT_TRY_AGAIN = "잘못 입력 하셨습니다."
+    }
 }
