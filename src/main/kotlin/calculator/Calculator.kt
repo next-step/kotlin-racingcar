69f68931fddab59b09843080c417d050ffa643a1
palmseung <palmseung@gmail.com>
2021-02-26 03:37:08 +0900

    feat(calculator): add feature to add (simple)

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -1,5 +1,20 @@
 package calculator
 
+import java.util.stream.Collectors.toList
+
+const val DELIMITER = " "
+
 fun add(input: String): Int {
-    return 0
+    val elements = input.split(DELIMITER)
+        .stream()
+        .map { element -> element.trim() }
+        .collect(toList())
+
+    val left = elements[0]
+    val right = elements[2]
+
+    val leftNumber = Integer.parseInt(left)
+    val rightNumber = Integer.parseInt(right)
+
+    return leftNumber + rightNumber
 }
