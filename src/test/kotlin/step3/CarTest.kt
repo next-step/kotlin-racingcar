962b0e2831283a67962ecb684b1279ca3060a30e
kit(김광수) <kit.t@kakaocorp.com>
2021-02-26 18:22:10 +0900

    feat: make it pass

--- src/test/kotlin/step3/CarTest.kt
+++ src/test/kotlin/step3/CarTest.kt
@@ -54,6 +54,10 @@ class CarTest {
     }
 
     data class Position(private val position: Int) {
+        init {
+            require(position >= 0)
+        }
+
         fun increase() = copy(position = position + 1)
     }
 
