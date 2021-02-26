fa1e7483bc8d09bb21f002842efdd9e1a0c13203
kit(김광수) <kit.t@kakaocorp.com>
2021-02-26 18:42:10 +0900

    feat:make it pass

--- src/test/kotlin/step3/CarsTest.kt
+++ src/test/kotlin/step3/CarsTest.kt
@@ -8,4 +8,21 @@ class CarsTest {
     fun `자동차 일급컬랙션을 생성할 수 있다`() {
         assertThat(Cars(3)).isEqualTo(Cars(3))
     }
+
+    class Cars(private val cars: Int) {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Cars
+
+            if (cars != other.cars) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            return cars
+        }
+    }
 }
