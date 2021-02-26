85dc57184de275143a3634c10083824c4d5a9c74
middlefgr <2giwon@gmail.com>
2021-02-27 01:10:07 +0900

    feature(step2): calculate 구현

--- src/main/kotlin/study/step2/Calculator.kt
+++ src/main/kotlin/study/step2/Calculator.kt
@@ -9,4 +9,35 @@ class Calculator {
     fun multiply(number: Int, number2: Int): Int = number * number2
 
     fun division(number: Int, number2: Int): Int = number / number2
+
+    fun calculateExpression(expression: List<String>): Int {
+        var acc: Int
+        var to: Int
+        var operator: String
+
+        acc = getNumber(expression[0])
+
+        for (i in 1 until expression.size step 2) {
+            operator = expression[i]
+            to = getNumber(expression[i + 1])
+
+            acc = calculate(acc, operator, to)
+        }
+
+        return acc
+    }
+
+    fun calculate(from: Int, operator: String, to: Int): Int {
+        return when (operator) {
+            CalculationsSymbol.ADD.symbol -> add(from, to)
+            CalculationsSymbol.SUB.symbol -> subtraction(from, to)
+            CalculationsSymbol.MULTIPLY.symbol -> multiply(from, to)
+            CalculationsSymbol.DIVISION.symbol -> division(from, to)
+            else -> throw IllegalArgumentException()
+        }
+    }
+
+    fun getNumber(token: String): Int {
+        return token.toIntOrNull() ?: throw IllegalArgumentException()
+    }
 }
