feb6b4491d3e4345547809c4f53a6f39f48b3aa1
taeheon.song <taeheon.song@linecorp.com>
2021-02-26 09:43:19 +0900

    feat(step2): Add StringCalculator and test

---
+++ src/test/kotlin/study/StringCalculatorTest.kt
@@ -0,0 +1,9 @@
+package study
+
+import org.junit.jupiter.api.Assertions.*
+
+/**
+ * @author tae-heon.song<taeheon.song></taeheon.song>@linecorp.com>
+ * @since 2021. 02. 26.
+ */
+internal class StringCaculatorTest
---
+++ src/test/kotlin/study/StudyTest.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2021. LINE Corporation. All rights reserved.
+ * LINE Corporation PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+ 
+package study
+
+
+/**
+  * @author tae-heon.song<taeheon.song@linecorp.com>
+  * @since 2021. 02. 26.
+  */class StudyTest {
+}
