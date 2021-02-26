0828ae1d39c09e96ec9cc430c4dbf9c582faf11a
kit(김광수) <kit.t@kakaocorp.com>
2021-02-26 18:40:53 +0900

    test: 자동차 일급컬랙션을 생성할 수 있다

---
+++ src/test/kotlin/step3/CarsTest.kt
@@ -0,0 +1,11 @@
+package step3
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class CarsTest {
+    @Test
+    fun `자동차 일급컬랙션을 생성할 수 있다`() {
+        assertThat(Cars(3)).isEqualTo(Cars(3))
+    }
+}
