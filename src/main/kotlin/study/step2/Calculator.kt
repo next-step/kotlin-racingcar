f9d3cffca686789dc27498cd26d3f6dab1bb7096
middlefgr <2giwon@gmail.com>
2021-02-26 22:01:39 +0900

    feat(step2): Calculator 추가

---
+++ src/main/kotlin/study/step2/Calculator.kt
@@ -0,0 +1,12 @@
+package study.step2
+
+class Calculator {
+
+    fun add(number: Int, number2: Int): Int = number + number2
+
+    fun subtraction(number: Int, number2: Int): Int = number - number2
+
+    fun multiply(number: Int, number2: Int): Int = number * number2
+
+    fun division(number: Int, number2: Int): Int = number / number2
+}
