643070c88a9d4a0fbede672a1b794d6b717261cd
kit(김광수) <kit.t@kakaocorp.com>
2021-02-27 00:10:10 +0900

    refactor: delete unused

--- src/main/kotlin/racingcar/Positions.kt
+++ src/main/kotlin/racingcar/Positions.kt
@@ -3,9 +3,5 @@ package racingcar
 data class Positions(private val positions: List<Position>) : List<Position> by positions {
     constructor(vararg positions: Int) : this(positions.map(::Position))
 
-    fun add(position: Position): Positions {
-        return Positions(positions + position)
-    }
-
     operator fun plus(position: Position) = Positions(positions + position)
 }
