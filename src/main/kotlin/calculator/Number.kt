b4924500b7185989f4c1e81b29fe427c3bd1d23d
improve777 <improve777@gmail.com>
2021-02-27 15:29:39 +0900

    feat(calculator): 사칙연산을 위해 operator 메서드 추가

--- src/main/kotlin/calculator/Number.kt
+++ src/main/kotlin/calculator/Number.kt
@@ -1,8 +1,26 @@
 package calculator
 
 import java.lang.NumberFormatException
+import java.util.Objects
 
-class Number private constructor(val value: Int) : Word {
+class Number private constructor(private val value: Int) : Word {
+
+    operator fun plus(other: Number) = Number(value + other.value)
+
+    operator fun minus(other: Number) = Number(value - other.value)
+
+    operator fun times(other: Number) = Number(value * other.value)
+
+    operator fun div(other: Number) = Number(value / other.value)
+
+    override fun equals(other: Any?): Boolean {
+        if (other is Number) return this.value == other.value
+        return false
+    }
+
+    override fun hashCode(): Int {
+        return Objects.hash(value)
+    }
 
     companion object {
         fun of(value: String): Number {
--- src/test/kotlin/calculator/NumberTest.kt
+++ src/test/kotlin/calculator/NumberTest.kt
@@ -1,5 +1,6 @@
 package calculator
 
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
@@ -19,5 +20,33 @@ internal class NumberTest {
             Number.of("+ ")
         }.isInstanceOf(IllegalArgumentException::class.java)
             .hasMessageContaining("the value must number")
+    }
+
+    @Test
+    fun `2 와 2 를 더하면 4 가 된다`() {
+        val num1 = Number.of("2")
+        val num2 = Number.of("2")
+        assertThat(num1 + num2).isEqualTo(Number.of("4"))
+    }
+
+    @Test
+    fun `2 와 2 를 빼면 0 이 된다`() {
+        val num1 = Number.of("2")
+        val num2 = Number.of("2")
+        assertThat(num1 - num2).isEqualTo(Number.of("0"))
+    }
+
+    @Test
+    fun `2 와 4 를 곱하면 8 이 된다`() {
+        val num1 = Number.of("2")
+        val num2 = Number.of("4")
+        assertThat(num1 * num2).isEqualTo(Number.of("8"))
+    }
+
+    @Test
+    fun `4 와 2 를 나누면 2 가 된다`() {
+        val num1 = Number.of("4")
+        val num2 = Number.of("2")
+        assertThat(num1 / num2).isEqualTo(Number.of("2"))
     }
 }
