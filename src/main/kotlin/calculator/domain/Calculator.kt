c661b886f0628b33bbb449445dcde57013ba4d75
bactoria <bactoria@gmail.com>
2021-02-28 00:30:03 +0900

    feat: 계산기 검증로직  추가

--- src/main/kotlin/calculator/domain/Calculator.kt
+++ src/main/kotlin/calculator/domain/Calculator.kt
@@ -1,6 +1,12 @@
 package calculator.domain
 
+import java.lang.IllegalArgumentException
+
 class Calculator(private val operands: List<Number>, private val operators: List<Operator>) {
+
+    init {
+        validate(operands, operators)
+    }
 
     fun calculate(): Number {
         val mutableOperands = operands.toMutableList()
@@ -14,5 +20,11 @@ class Calculator(private val operands: List<Number>, private val operators: List
         }
 
         return result
+    }
+
+    private fun validate(operands: List<Number>, operators: List<Operator>) {
+        if (operands.size != operators.size + 1) {
+            throw IllegalArgumentException("숫자는 연산자보다 갯수가 1개 더 많아야 합니다")
+        }
     }
 }
