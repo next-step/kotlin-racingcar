86218316b7c8cb8057c47988dd6afeefb90412a7
sw-oh <sangw0804@sangw0804ui-MacBookPro.local>
2021-02-28 00:57:43 +0900

    refactor(Calculator, Operation): 코드리뷰 반영
     - Operation Enum 추가
     - Null 처리 방식 변경
     - 예외처리 구현 수정
     - ArithmeticException 제거
     - 기타 코드 스타일 수정

--- src/main/kotlin/step2/Calculator.kt
+++ src/main/kotlin/step2/Calculator.kt
@@ -1,18 +1,16 @@
 package step2
 
 class Calculator {
-    fun calculate(expression: String?): Int {
+    fun calculate(expression: String): Int {
         validateInput(expression)
 
-        val terms = buildTerms(expression!!)
+        val terms = buildTerms(expression)
 
         return calculate(terms)
     }
 
-    private fun validateInput(expression: String?) {
-        if (expression.isNullOrBlank()) {
-            throw IllegalArgumentException()
-        }
+    private fun validateInput(expression: String) {
+        require(expression.isNotBlank()) { "인자로 전달되는 수식이 공백 문자열입니다!" }
     }
 
     private fun buildTerms(expression: String) = expression.split(" ")
@@ -21,19 +19,21 @@ class Calculator {
         var result = terms[0].toInt()
 
         for (i in 1 until terms.size step 2) {
-            val operator = terms[i]
+            val operatorSymbol = terms[i]
             val operand = terms[i + 1].toInt()
 
-            result = operate(result, operator, operand)
+            result = operate(result, operatorSymbol, operand)
         }
         return result
     }
 
-    private fun operate(result: Int, operator: String, operand: Int) = when (operator) {
-        "+" -> result + operand
-        "-" -> result - operand
-        "*" -> result * operand
-        "/" -> result / operand
-        else -> throw IllegalArgumentException()
+    private fun operate(result: Int, operatorSymbol: String, operand: Int): Int {
+        for (o in Operation.values()) {
+            if (operatorSymbol == o.symbol) {
+                return o.apply(result, operand)
+            }
+        }
+
+        throw IllegalArgumentException("$operatorSymbol : 알 수 없는 연산자입니다!")
     }
 }
--- src/test/kotlin/step2/CalculatorTest.kt
+++ src/test/kotlin/step2/CalculatorTest.kt
@@ -1,6 +1,7 @@
 package step2
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalStateException
 import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
@@ -9,14 +10,6 @@ import org.junit.jupiter.params.provider.MethodSource
 import org.junit.jupiter.params.provider.ValueSource
 
 internal class CalculatorTest {
-    companion object {
-        @JvmStatic
-        fun complexCalculationProvider() = listOf(
-            Arguments { arrayOf("1 + 3 * 5", 20) },
-            Arguments { arrayOf("2 - 8 / 3 - 3", -5) }
-        )
-    }
-
     private val calculator = Calculator()
 
     @Test
@@ -47,6 +40,13 @@ internal class CalculatorTest {
         assertThat(result).isEqualTo(5)
     }
 
+    @Test
+    fun `0으로 나누는 경우 IllegalStateException throw`() {
+        assertThatIllegalStateException().isThrownBy {
+            calculator.calculate("1 / 0")
+        }
+    }
+
     @ParameterizedTest
     @MethodSource("complexCalculationProvider")
     fun `사칙 연산을 모두 포함하는 기능 구현`(expression: String, answer: Int) {
@@ -69,5 +69,13 @@ internal class CalculatorTest {
         assertThatIllegalArgumentException().isThrownBy {
             calculator.calculate(input)
         }
+    }
+
+    companion object {
+        @JvmStatic
+        fun complexCalculationProvider() = listOf(
+            Arguments { arrayOf("1 + 3 * 5", 20) },
+            Arguments { arrayOf("2 - 8 / 3 - 3", -5) }
+        )
     }
 }
