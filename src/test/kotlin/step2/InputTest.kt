ac1526d8f946de60daafdc2cdfa7bc9eb5486b1b
Joseph <wjsdudtkd95@gmail.com>
2021-02-27 19:48:33 +0900

    test(step2): Input 클래스 Test

--- src/test/kotlin/step2/InputTest.kt
+++ src/test/kotlin/step2/InputTest.kt
@@ -1,3 +1,25 @@
 package step2
 
-class InputTest
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+import java.lang.IllegalArgumentException
+
+class InputTest {
+
+    private val input = Input()
+
+    @Test
+    fun `공백이나 빈 문자열 입력`() {
+        assertThatThrownBy { input.checkFormulaValidation("") }.isInstanceOf(IllegalArgumentException::class.java)
+        assertThatThrownBy { input.checkFormulaValidation(" ") }.isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @Test
+    fun `문자열을 리스트로 변환`() {
+        assertThat(input.convertFormulaToList("5 + 5 * 2")).isEqualTo(listOf("5", "+", "5", "*", "2"))
+        assertThat(input.convertFormulaToList("   5 + 5 * 2")).isEqualTo(listOf("5", "+", "5", "*", "2"))
+        assertThat(input.convertFormulaToList("5 + 5 * 2   ")).isEqualTo(listOf("5", "+", "5", "*", "2"))
+        assertThat(input.convertFormulaToList("5 + 5 * 2 -  3")).isEqualTo(listOf("5", "+", "5", "*", "2", "-", "3"))
+    }
+}
