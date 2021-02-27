bb5162f0bb5eb48331d49f77858b16a7cecb9344
bactoria <bactoria@gmail.com>
2021-02-28 00:41:59 +0900

    refactor: single expression function

--- src/main/kotlin/calculator/domain/Number.kt
+++ src/main/kotlin/calculator/domain/Number.kt
@@ -4,17 +4,11 @@ class Number(val value: Int) {
 
     constructor(value: String) : this(value.toIntOrNull() ?: throw IllegalArgumentException("숫자가 아님"))
 
-    fun plus(number: Number): Number {
-        return Number(this.value + number.value)
-    }
+    fun plus(number: Number) = Number(this.value + number.value)
 
-    fun minus(number: Number): Number {
-        return Number(this.value - number.value)
-    }
+    fun minus(number: Number) = Number(this.value - number.value)
 
-    fun multiple(number: Number): Number {
-        return Number(this.value * number.value)
-    }
+    fun multiple(number: Number) = Number(this.value * number.value)
 
     fun divide(number: Number): Number {
         if (number == Number(0)) {
