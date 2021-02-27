5c1844cca3429f2a11f9b585b625af4395ef90bc
ohgillwhan <soora_genius@naver.com>
2021-02-27 15:04:20 +0900

    refact(Operation) : 변수 operationString 이름을 char로 변경

--- src/main/kotlin/calculator/Operation.kt
+++ src/main/kotlin/calculator/Operation.kt
@@ -3,7 +3,7 @@ package calculator
 import java.lang.IllegalArgumentException
 import java.util.stream.Stream
 
-enum class Operation(val operationString: String, val operate: (Int, Int) -> Int) {
+enum class Operation(val char: String, val operate: (Int, Int) -> Int) {
     PLUS("+", { first: Int, second: Int -> first + second }),
     MINUS("-", { first: Int, second: Int -> first - second }),
     MULTIPLE("*", { first: Int, second: Int -> first * second }),
@@ -16,7 +16,7 @@ enum class Operation(val operationString: String, val operate: (Int, Int) -> Int
     companion object {
         @JvmStatic
         fun operationOf(operation: String): Operation {
-            return values().firstOrNull { item -> item.operationString == operation } ?: throw IllegalArgumentException()
+            return values().firstOrNull { item -> item.char == operation } ?: throw IllegalArgumentException()
         }
     }
 }
