55b31a688f10e82302f9cd13104d1ffa81dca334
middlefgr <2giwon@gmail.com>
2021-02-28 01:03:28 +0900

    feature(step3): Racing game 기능 추가

---
+++ src/main/kotlin/study/step3/RacingGame.kt
@@ -0,0 +1,31 @@
+package study.step3
+
+import kotlin.random.Random
+
+class RacingGame {
+
+    private lateinit var _cars: Array<Car>
+    val cars: Array<Car> get() = _cars
+
+    fun readyGame(carCount: Int) {
+        _cars = Array(carCount) { Car() }
+    }
+
+    fun raceCars(): List<Car> {
+        for (car in _cars) {
+            val randomValue = Random.nextInt(9)
+            moveCarOrNot(randomValue, car)
+        }
+        return _cars.toList()
+    }
+
+    fun moveCarOrNot(randomValue: Int, car: Car) {
+        if (randomValue >= MOVE_ALLOWABLE_VALUE) {
+            car.moveCar()
+        }
+    }
+
+    companion object {
+        private const val MOVE_ALLOWABLE_VALUE = 4
+    }
+}
