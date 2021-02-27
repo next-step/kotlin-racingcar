90f7644020f1098a3e1a6b462e97355890857104
ohgillwhan <soora_genius@naver.com>
2021-02-27 14:17:44 +0900

    refact(Calculator) : if문으로 NullOrBlank를 require로 변경

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -4,6 +4,6 @@ import java.lang.IllegalArgumentException
 
 class Calculator(private val calculate : String?) {
     fun calc() {
-        if(calculate.isNullOrBlank()) throw IllegalArgumentException("공백 또는 Null은 계산이 불가능합니다.")
+        require(!calculate.isNullOrBlank());
     }
 }
