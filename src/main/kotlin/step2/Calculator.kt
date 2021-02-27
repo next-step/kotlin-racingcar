e5c6af91a2d2394cfd6a3a7170782b2e214f17b0
Joseph <wjsdudtkd95@gmail.com>
2021-02-27 19:48:05 +0900

    refactor(step2): 입력 메소드 수정

--- src/main/kotlin/step2/Calculator.kt
+++ src/main/kotlin/step2/Calculator.kt
@@ -4,12 +4,13 @@ const val BLANK = " "
 const val POSITION_NUM1 = 0
 const val POSITION_OPERATOR = 1
 const val POSITION_NUM2 = 2
+const val TWO_STEP = 2
 
 private val inputManager: Input = Input()
 private val operator: Operator = Operator()
 
 fun main() {
-    val strFormula = inputManager.inputFormula()
+    val strFormula = inputManager.checkFormulaValidation(readLine())
     val listFormula = inputManager.convertFormulaToList(strFormula)
 
     println(calculateFormulaList(listFormula))
@@ -19,7 +20,7 @@ fun calculateFormulaList(listFormula: List<String>): Int {
     // 맨 앞에 있는 숫자를 결과값으로 먼저 초기화 시켜준다.
     var calculateResult = listFormula[POSITION_NUM1].toInt()
 
-    for (i in 0..listFormula.size - 2 step 2) {
+    for (i in 0..listFormula.size - TWO_STEP step TWO_STEP) {
 
         val operatorResult = operator.operate(
             number1 = calculateResult,
