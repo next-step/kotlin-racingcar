7f23cf41583df860d29c508ec571fe35e995d7a7
bactoria <bactoria@gmail.com>
2021-02-27 20:45:14 +0900

    feat: 계산기 추가

--- src/main/kotlin/calculator/domain/Operator.kt
+++ src/main/kotlin/calculator/domain/Operator.kt
@@ -5,4 +5,11 @@ enum class Operator(val code: String, val operate: (Int, Int) -> Int) {
     MINUS("-", { x, y -> x - y }),
     MULTIPLE("*", { x, y -> x * y }),
     DIVIDE("/", { x, y -> x / y });
+
+    companion object {
+        @JvmStatic
+        fun of(codetype: String): Operator {
+            return values().firstOrNull { it.code == codetype } ?: throw IllegalArgumentException("허용하지 않는 연산자")
+        }
+    }
 }
