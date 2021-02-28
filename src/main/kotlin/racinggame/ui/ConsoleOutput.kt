793bf96a73c366e06ef5885f55aede67124475b8
chanin <pci2676@gmail.com>
2021-02-28 17:16:50 +0900

    feat : 결과 출력을 위한 view 함수 생성

---
+++ src/main/kotlin/racinggame/ui/ConsoleOutput.kt
@@ -0,0 +1,24 @@
+package racinggame.ui
+
+import racinggame.record.Record
+import racinggame.record.Recorder
+import racinggame.record.Records
+
+fun showResult(recorder: Recorder) {
+    println("실행 결과")
+    for (records in recorder.records) {
+        records.toView()
+    }
+}
+
+fun Records.toView() {
+    for (record in records) {
+        println(record.toView())
+    }
+    println()
+}
+
+fun Record.toView() = with(StringBuilder()) {
+    repeat(distance) { append("-") }
+    toString()
+}
