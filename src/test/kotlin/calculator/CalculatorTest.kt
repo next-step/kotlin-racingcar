b02a8a4e290dc7232c94b3bf11536a1ae6f51c27
sanggu <tkdrn8578@naver.com>
2021-02-28 16:14:13 +0900

    refactor: Calculator를 object클래스로 변경에 따른 테스트코드 수정 및 ParameterTest를 사욯아여 중복된 테스트 코드 제거
     * CsvSource를 사용하면 형변환도 자동으로 해주고, 2개이상의 값을 받을 수 있음
    
    # 피드백 반영

--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -5,20 +5,20 @@ import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class CalculatorTest {
-    private val calculator = Calculator()
-
     @Test
     fun `파라미터 값이 empty인 경우 예외처리한다`() {
         assertThatExceptionOfType(IllegalArgumentException::class.java).isThrownBy {
-            calculator.validInputIsEmpty("")
+            Calculator.validInputIsEmpty("")
         }.withMessageMatching("입력 값이 존재하지 않습니다.")
     }
 
     @Test
     fun `입력한 값을 검사 후 split하여 리턴한다`() {
-        val splitedInput = calculator.input("2 + 3")
+        val splitedInput = Calculator.input("2 + 3")
 
         assertAll(
             { assertThat(splitedInput[0]).isEqualTo("2") },
@@ -29,7 +29,7 @@ class CalculatorTest {
 
     @Test
     fun `입력한 값을 split하여 리턴한다`() {
-        val splitedInput = calculator.splitByWhiteSpace("5 * 2")
+        val splitedInput = Calculator.splitByWhiteSpace("5 * 2")
 
         assertAll(
             { assertThat(splitedInput[0]).isEqualTo("5") },
@@ -38,11 +38,11 @@ class CalculatorTest {
         )
     }
 
-    @Test
-    @DisplayName("3 * 10 / 5 을 연산하여 6을 리턴한다")
-    fun `calculate_test`() {
-        val splitedInput = calculator.input("3 * 10 / 5")
-        val result = calculator.calculate(splitedInput)
+    @ParameterizedTest
+    @CsvSource("3 * 10 / 5, 6", "5 + 3 * 2 / 4", "4")
+    fun `입력받은 문자열을 calculate로 순차적으로 계산하여 결과를 리턴한다`(input: String, expectedResult: Int) {
+        val splitedInput = Calculator.input("3 * 10 / 5")
+        val result = Calculator.calculate(splitedInput)
 
         assertThat(result).isEqualTo(6)
     }
@@ -50,30 +50,23 @@ class CalculatorTest {
     @Test
     @DisplayName("5 + 3 * 2 / 4 을 연산하여 6을 리턴한다")
     fun `calculate_second_test`() {
-        val splitedInput = calculator.input("5 + 3 * 2 / 4")
-        val result = calculator.calculate(splitedInput)
+        val splitedInput = Calculator.input("5 + 3 * 2 / 4")
+        val result = Calculator.calculate(splitedInput)
 
         assertThat(result).isEqualTo(4)
     }
 
     @Test
     fun `덧셈 연산자를 입력했을 때 더하기 연산을 한다`() {
-        val result = calculator.applyOperator(0, 10, "+")
+        val result = Calculator.applyOperator(0, 10, Operator.PLUS)
 
         assertThat(result).isEqualTo(10)
     }
 
     @Test
-    fun `잚못된 입력했을 때 예외처리한다`() {
-        assertThatExceptionOfType(IllegalArgumentException::class.java).isThrownBy {
-            calculator.applyOperator(0, 10, "&")
-        }.withMessageMatching("잘못된 연산자입니다.")
-    }
-
-    @Test
     fun `덧셈 연산이 처리되어 결과를 리턴한다`() {
         // when
-        val answer = calculator.add(2, 3)
+        val answer = Calculator.add(2, 3)
 
         // then
         assertThat(answer).isEqualTo(5)
@@ -82,7 +75,7 @@ class CalculatorTest {
     @Test
     fun `뺄셈의 결과가 양수인 경우 양수의 값을 리턴한다`() {
         // when
-        val answer = calculator.minus(10, 5)
+        val answer = Calculator.minus(10, 5)
 
         // then
         assertThat(answer).isEqualTo(5)
@@ -91,7 +84,7 @@ class CalculatorTest {
     @Test
     fun `뺄셈의 결과가 음수인 경우 음수의 값을 리턴한다`() {
         // when
-        val answer = calculator.minus(5, 6)
+        val answer = Calculator.minus(5, 6)
 
         // then
         assertThat(answer).isEqualTo(-1)
@@ -100,7 +93,7 @@ class CalculatorTest {
     @Test
     fun `곱셈 연산 처리되어 결과를 리턴한다`() {
         // when
-        val answer = calculator.multiply(2, 3)
+        val answer = Calculator.multiply(2, 3)
 
         // then
         assertThat(answer).isEqualTo(6)
@@ -109,7 +102,7 @@ class CalculatorTest {
     @Test
     fun `나눗셈을 처리하여 결과를 리턴한다`() {
         // when
-        val answer = calculator.division(10, 5)
+        val answer = Calculator.division(10, 5)
 
         // then
         assertThat(answer).isEqualTo(2)
@@ -119,7 +112,7 @@ class CalculatorTest {
     fun `나눗셈을 처리 시 분모가 0인경우 예외처리한다`() {
         // then
         assertThatExceptionOfType(IllegalArgumentException::class.java).isThrownBy {
-            calculator.division(10, 0)
-        }.withMessageMatching("0은 분모가 될 수 없습니다.")
+            Calculator.division(10, 0)
+        }.withMessage("0은 분모가 될 수 없습니다.")
     }
 }
