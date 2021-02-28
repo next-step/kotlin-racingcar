68df0b931d96cd8af64592072da9953ecdd3f03d
sw-oh <sangw0804@sangw0804ui-MacBookPro.local>
2021-03-01 00:21:01 +0900

    fix(OutputView): JvmStatic 어노테이션 추가

--- src/main/kotlin/step3/OutputView.kt
+++ src/main/kotlin/step3/OutputView.kt
@@ -2,6 +2,7 @@ package step3
 
 class OutputView {
     companion object {
+        @JvmStatic
         fun handleOutput(raceCount: Int, results: List<Result>) {
             println("실행 결과")
             for (i in 0 until raceCount) {
