74cfc6ea044b82a03a0640b3109bc71719f66d35
kmdngyu <kmdngyu@gmail.com>
2021-03-01 02:26:47 +0900

    feat: 연산자 피연산자 갯수 예외 추가

--- src/main/kotlin/calculator/Expressions.kt
+++ src/main/kotlin/calculator/Expressions.kt
@@ -3,16 +3,20 @@ package calculator
 import calculator.ExpressionParser.parse
 
 class Expressions(input: String) {
-    private val numbers: List<Number>
+    private val operands: List<Operand>
     private val operators: List<Operator>
 
     init {
         val splittedInput = input.split(DELIMITER)
-        numbers = splittedInput.parse(Number.isOrderOf, Number.convertToNumber)
+        operands = splittedInput.parse(Operand.isOrderOf, Operand.CONVERT_TO_OPERAND)
         operators = splittedInput.parse(Operator.isOrderOf, Operator.convertToOperator)
+        require(operands.size - operators.size == DIFFERENCE_NUMBER_OF_OPERANDS_AND_OPERATORS) {
+            "피연산자는 연산자보다 하나 더 많아야 합니다"
+        }
     }
 
     companion object {
         private const val DELIMITER = " "
+        private const val DIFFERENCE_NUMBER_OF_OPERANDS_AND_OPERATORS = 1
     }
 }
