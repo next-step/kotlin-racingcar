28248db9fe732f47590f4fb8309b875de79d0882
choiyusung <chldbrjd@gmail.com>
2021-02-28 16:21:50 +0900

    argument 검사 추가

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -2,7 +2,9 @@ package calculator
 
 class StringCalculator(private val parsedToken: String = " ") {
 
-    fun calculate(text: String): Number {
+    fun calculate(text: String?): Number {
+        require(!text.isNullOrBlank())
+
         val expression = Expression()
         for (token in text.split(parsedToken)) {
             expression.addExpToken(token)
