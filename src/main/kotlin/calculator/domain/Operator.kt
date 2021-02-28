55b56ac53b3998c9f55530c565964492498245c7
남윤서 <devysnam@gmail.com>
2021-02-28 13:36:44 +0900

    refactor: Operator 중복 로직 제거

--- src/main/kotlin/calculator/domain/Operator.kt
+++ src/main/kotlin/calculator/domain/Operator.kt
@@ -1,21 +1,15 @@
 package calculator.calculator.domain
 
-import java.util.function.BiFunction
-
-enum class Operator(private val symbol: String, private val operate: BiFunction<Double, Double, Double>) {
-    ADD("+", { prev: Double, next: Double -> prev + next }),
-    SUB("-", { prev: Double, next: Double -> prev - next }),
-    MUL("*", { prev: Double, next: Double -> prev * next }),
-    DIV("/", { prev: Double, next: Double -> prev / next });
+enum class Operator(private val symbol: String, private val operate: (Double, Double) -> Double) {
+    ADD("+", { prev, next -> prev + next }),
+    SUB("-", { prev, next -> prev - next }),
+    MUL("*", { prev, next -> prev * next }),
+    DIV("/", { prev, next -> prev / next });
 
     companion object {
-        fun from(symbol: String): Operator {
-            require(values().any { operator -> operator.symbol == symbol }) { "존재하지 않는 기호입니다." }
-            return values().first { operator -> operator.symbol == symbol }
-        }
+        fun from(symbol: String) = values().find { op -> op.symbol == symbol }
+            ?: throw IllegalArgumentException("존재하지 않는 기호입니다.")
     }
 
-    fun calculate(prev: Double, next: Double): Double {
-        return operate.apply(prev, next)
-    }
+    fun calculate(prev: Double, next: Double) = operate.invoke(prev, next)
 }
