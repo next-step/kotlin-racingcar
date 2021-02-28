97488b6d044b30a869248354c611ea142ff84fa8
kmdngyu <kmdngyu@gmail.com>
2021-02-28 18:39:48 +0900

    feat: 문자열 입력 및 null 체크

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -1,13 +1,3 @@
 package calculator
 
-class Calculator(val inputString: String) {
-    fun calculate(): Int {
-        val expressions = Expressions(inputString)
-        val firstOperand = expressions.operands.first()
-        var result = firstOperand
-        expressions.operators.forEachIndexed { index, value ->
-            result = value.compute(result, expressions.operands[index + 1])
-        }
-        return result.value
-    }
-}
+class Calculator
--- src/test/kotlin/calculator/UserInputTest.kt
+++ src/test/kotlin/calculator/UserInputTest.kt
@@ -1,7 +1,5 @@
 package calculator
 
-import calculator.ui.UserInput
-import input.InputStrategy
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.NullAndEmptySource
