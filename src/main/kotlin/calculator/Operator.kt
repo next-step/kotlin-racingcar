7b032b53c3a8d69ddcfdfe44785673394ac8b9ed
improve777 <improve777@gmail.com>
2021-02-28 15:03:07 +0900

    refactor(calculator): 연산자를 프로퍼티로 변경

--- src/main/kotlin/calculator/Operator.kt
+++ src/main/kotlin/calculator/Operator.kt
@@ -2,11 +2,11 @@ package calculator
 
 import java.util.Stack
 
-enum class Operator(val operate: (n1: Number, n2: Number) -> Number) : Word {
-    PLUS({ n1, n2 -> n1 + n2 }),
-    MINUS({ n1, n2 -> n1 - n2 }),
-    TIMES({ n1, n2 -> n1 * n2 }),
-    DIVIDE({ n1, n2 -> n1 / n2 }),
+enum class Operator(val token: String, val operate: (n1: Number, n2: Number) -> Number) : Word {
+    PLUS("+", { n1, n2 -> n1 + n2 }),
+    MINUS("-", { n1, n2 -> n1 - n2 }),
+    TIMES("*", { n1, n2 -> n1 * n2 }),
+    DIVIDE("/", { n1, n2 -> n1 / n2 }),
     ;
 
     override fun calculate(acc: Stack<Number>) {
@@ -19,10 +19,10 @@ enum class Operator(val operate: (n1: Number, n2: Number) -> Number) : Word {
     companion object {
         fun of(value: String): Operator {
             return when (value.trim()) {
-                "+" -> PLUS
-                "-" -> MINUS
-                "*" -> TIMES
-                "/" -> DIVIDE
+                PLUS.token -> PLUS
+                MINUS.token -> MINUS
+                TIMES.token -> TIMES
+                DIVIDE.token -> DIVIDE
                 else -> throw IllegalArgumentException("the value not Operator")
             }
         }
