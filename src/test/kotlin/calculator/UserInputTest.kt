d7125671aa33e13d68a2144276bcf1506d60f48f
kmdngyu <kmdngyu@gmail.com>
2021-02-28 18:39:48 +0900

    feat: 문자열 입력 및 null 체크

---
+++ src/test/kotlin/calculator/UserInputTest.kt
@@ -0,0 +1,20 @@
+package calculator
+
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.NullAndEmptySource
+
+internal class UserInputTest {
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    fun `null 입력시 예외 발생`(input: String?) {
+        assertThatThrownBy {
+            UserInput(object : InputStrategy {
+                override fun enter(): String? {
+                    return input
+                }
+            }).operate()
+        }.isInstanceOf(IllegalArgumentException::class.java)
+    }
+}
