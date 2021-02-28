ac4de2177767e8ad5c54666a7909fa636028b54e
sanggu <tkdrn8578@naver.com>
2021-02-28 16:11:18 +0900

    feat: operator에 해당하는 string을 입력받아 해당 property에 매핑되는 Operator를 가져오는 operatorOf 추가

--- src/main/kotlin/calculator/Operator.kt
+++ src/main/kotlin/calculator/Operator.kt
@@ -1,8 +1,22 @@
 package calculator
 
+import java.lang.IllegalArgumentException
+
 enum class Operator(val operator: String) {
     PLUS("+"),
     MINUS("-"),
     MULTIPLY("*"),
     DIVISION("/");
+
+    companion object {
+        fun operatorOf(operator: String): Operator {
+            return when (operator) {
+                PLUS.operator -> PLUS
+                MINUS.operator -> MINUS
+                MULTIPLY.operator -> MULTIPLY
+                DIVISION.operator -> DIVISION
+                else -> throw IllegalArgumentException("잘못된 연산자입니다.")
+            }
+        }
+    }
 }
