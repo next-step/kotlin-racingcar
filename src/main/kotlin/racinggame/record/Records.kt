fb9ffa9493791460cda5c459dba6c72004376fd4
chanin <pci2676@gmail.com>
2021-03-01 22:36:16 +0900

    refactor : 최종 우승자를 출력하도록 수정

--- src/main/kotlin/racinggame/record/Records.kt
+++ src/main/kotlin/racinggame/record/Records.kt
@@ -1,5 +1,20 @@
 package racinggame.record
 
 class Records(val records: List<Record>) {
+    init {
+        if (records.isNullOrEmpty()) {
+            throw IllegalArgumentException("기록이 존재하지 않습니다.")
+        }
+    }
+
     fun size() = records.size
+
+    fun getTime(): Int {
+        return records[0].time
+    }
+
+    fun findWinners(): List<Record> {
+        val maxDistance = records.maxBy { it.distance }!!.distance
+        return records.filter { it.distance == maxDistance }
+    }
 }
---
+++ src/test/kotlin/racinggame/record/RecorderTest.kt
@@ -0,0 +1,60 @@
+package racinggame.record
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+
+internal class RecorderTest {
+    @Test
+    fun `우승한 기록을 찾는다`() {
+        // given
+        val records1 = Records(
+            listOf(
+                Record(1, "test1", 1),
+                Record(1, "test2", 2),
+                Record(1, "test3", 3),
+                Record(1, "test3", 3)
+            )
+        )
+        val records2 = Records(
+            listOf(
+                Record(2, "test1", 2),
+                Record(2, "test2", 3),
+                Record(2, "test3", 4),
+                Record(2, "test3", 4)
+            )
+        )
+
+        val recorder = Recorder(
+            mutableListOf(
+                records1,
+                records2
+            )
+        )
+
+        // when
+        val winners = recorder.getWinners()
+
+        // then
+        assertThat(winners).isEqualTo(
+            listOf(
+                Record(2, "test3", 4),
+                Record(2, "test3", 4)
+            )
+        )
+    }
+
+    @Test
+    fun `기록이 없는데 우승자를 찾으려고 하면 exception`() {
+        // given
+        val emptyList = mutableListOf<Records>()
+
+        // when
+        val recorder = Recorder(emptyList)
+
+        // then
+        assertThatThrownBy { recorder.getWinners() }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("녹화된 기록이 없습니다.")
+    }
+}
