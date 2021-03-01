c5aaecd403fb2d82608ff6e97d4926989dd0196d
sanggu <tkdrn8578@naver.com>
2021-03-01 15:57:22 +0900

    feat: 레이싱을 시작하기 위한 정보 입력 시 자동차 개수와 시도할 횟수를 받는 print 를 출력하는 기능 추가

---
+++ src/main/kotlin/printer/InputView.kt
@@ -0,0 +1,11 @@
+package printer
+
+object InputView {
+    fun printInputNumberOfCar() {
+        println("자동자 댗수는 몇 대인가요?")
+    }
+
+    fun printInputNumberOfAttempts() {
+        println("시도할 횟수는 몇 회인가요?")
+    }
+}
