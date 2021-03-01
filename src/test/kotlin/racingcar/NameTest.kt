c6a5d34e52b130a068cbb7a4f55f4ed2e8163382
kit(김광수) <kit.t@kakaocorp.com>
2021-03-01 22:32:57 +0900

    test: 이름은 5자를 초과할 수 없다

---
+++ src/test/kotlin/racingcar/NameTest.kt
@@ -0,0 +1,11 @@
+package racingcar
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class NameTest {
+    @Test
+    fun `이름은 5자를 초과할 수 없다`() {
+        assertThrows<IllegalArgumentException> { Name("abcedf") }
+    }
+}
