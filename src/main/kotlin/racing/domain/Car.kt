6ab04dc4e45bee325eee5f9e72cc1ef994b3871b
ka0oll <jjyoung0216@naver.com>
2021-03-01 22:00:20 +0900

    feat(racingcar) 자동차 이름 추가.

--- src/main/kotlin/racing/domain/Car.kt
+++ src/main/kotlin/racing/domain/Car.kt
@@ -1,8 +1,15 @@
 package racing.domain
 
 private const val DEFAULT_POSITION = 0
+private const val MAX_NAME_LENGTH = 5
 
-internal class Car(position: Int = DEFAULT_POSITION) {
+internal class Car(name: String = "test", position: Int = DEFAULT_POSITION) {
+
+    init {
+        require(name.trim().length <= MAX_NAME_LENGTH) {
+            "The maximum name length is 5"
+        }
+    }
 
     var position: Int = position
         private set
