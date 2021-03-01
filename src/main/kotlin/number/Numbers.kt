d609866b0537fcfecffb49f4455720cb642c496b
sanggu <tkdrn8578@naver.com>
2021-03-01 16:50:28 +0900

    feat: racing 에 사용할 0과 10사이의 숫자를 리턴하는 함수 추가

--- src/main/kotlin/number/Numbers.kt
+++ src/main/kotlin/number/Numbers.kt
@@ -1,7 +1,16 @@
 package number
 
+import kotlin.random.Random
+
 object Numbers {
+    const val MIN_RANDOM_NUMBER = 0
+    const val MAX_RANDOM_NUMBER = 10
+
     fun toInt(value: String): Int {
         return value.toIntOrNull() ?: throw IllegalArgumentException("value는 숫자여야 합니다. value : $value")
+    }
+
+    fun randomToRacing(): Int {
+        return Random.nextInt(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER)
     }
 }
