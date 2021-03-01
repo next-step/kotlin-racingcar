4b88e79dbf2b8e91d0fbd8d758fc8b3d059e02b1
sanggu <tkdrn8578@naver.com>
2021-03-02 00:26:10 +0900

    refactor: 컨벤션에 따라 companion object 위치 변경 및 cars 를 val 로 변경
     * https://kotlinlang.org/docs/coding-conventions.html#interface-implementation-layout

--- src/main/kotlin/racingcar/Cars.kt
+++ src/main/kotlin/racingcar/Cars.kt
@@ -3,7 +3,34 @@ package racingcar
 import java.util.stream.IntStream
 import kotlin.streams.toList
 
-class Cars(private var cars: List<Car>) {
+class Cars(private val cars: List<Car>) {
+    fun forwardAllByCondition(conditions: List<Int>) {
+        cars.mapIndexed { index, car ->
+            forwardByCondition(car, conditions[index])
+        }
+    }
+
+    fun forwardByCondition(car: Car, condition: Int) {
+        if (canForward(condition)) {
+            car.forward()
+        }
+    }
+
+    private fun canForward(condition: Int) = condition >= MAX_FORWARD_CONDITION
+
+    fun getSize(): Int {
+        return cars.size
+    }
+
+    fun getPositionByIndex(index: Int): Int {
+        return cars[index].position
+    }
+
+    fun getPositions(): List<Int> {
+        return cars.map { car -> car.position }
+            .toList()
+    }
+
     companion object {
         private const val MAX_FORWARD_CONDITION = 4
         private const val MIN_CAN_CREATE_CARS = 0
@@ -17,35 +44,5 @@ class Cars(private var cars: List<Car>) {
 
             return Cars(newCars)
         }
-    }
-
-    fun forwardAllByCondition(conditions: List<Int>): Cars {
-        val forwardedCars = cars.mapIndexed { index, car ->
-            forwardByCondition(car, conditions[index])
-        }.toList()
-
-        return Cars(forwardedCars)
-    }
-
-    fun forwardByCondition(car: Car, condition: Int): Car {
-        if (canForward(condition)) {
-            car.forward()
-        }
-
-        return car
-    }
-
-    private fun canForward(condition: Int) = condition >= MAX_FORWARD_CONDITION
-
-    fun getSize(): Int {
-        return cars.size
-    }
-
-    fun getPositionByIndex(index: Int): Int {
-        return cars[index].retrievePosition()
-    }
-
-    fun retrieveCars(): List<Car> {
-        return cars
     }
 }
