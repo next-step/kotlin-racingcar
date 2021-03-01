70f02f96cb5c78905e2e68990c43238d4fbcb474
ohgillwhan <soora_genius@naver.com>
2021-03-01 23:24:34 +0900

    refact(Applications) : for문을 repeat으로 변경

--- src/main/kotlin/car/Application.kt
+++ src/main/kotlin/car/Application.kt
@@ -11,7 +11,7 @@ fun main() {
     val cars = Cars(input.amount)
     val resultView = ResultView(cars)
 
-    for (i in 0 until input.tryCount) {
+    repeat(input.tryCount) {
         cars.move()
         resultView.print()
     }
