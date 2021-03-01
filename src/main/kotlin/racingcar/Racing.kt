3f13280353f145ff227aae4895970aa88612dd27
sanggu <tkdrn8578@naver.com>
2021-03-01 18:00:29 +0900

    refactor: Cars 추가에 따른 수정

--- src/main/kotlin/racingcar/Racing.kt
+++ src/main/kotlin/racingcar/Racing.kt
@@ -1,25 +1,14 @@
 package racingcar
 
 import printer.ResultView
-import java.util.stream.IntStream
-import kotlin.streams.toList
 
 fun main() {
     val numberOfCars = Reception.receiveNumberOfCar()
     val numberOfAttempts = Reception.receiveNumberOfAttempts()
 
-    // 자동차 댓수만큼 Car 를 생성한다.
-    val cars = createCars(numberOfCars)
-
+    val cars = Cars.createCars(numberOfCars)
     val racingCar = RacingCars(cars)
 
     ResultView.printInt()
-
     racingCar.racing(numberOfAttempts)
-}
-
-private fun createCars(numberOfCar: Int): List<Car> {
-    return IntStream.range(0, numberOfCar)
-        .mapToObj { Car() }
-        .toList()
 }
