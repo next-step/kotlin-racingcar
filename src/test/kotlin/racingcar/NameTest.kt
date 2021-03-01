40571e356800e90a0b11ad0efb8bc89f890f77d1
kit(김광수) <kit.t@kakaocorp.com>
2021-03-01 22:35:04 +0900

    test: 이름을 생성할 수 있다

--- src/test/kotlin/racingcar/NameTest.kt
+++ src/test/kotlin/racingcar/NameTest.kt
@@ -9,6 +9,11 @@ class NameTest {
         assertThrows<IllegalArgumentException> { Name("abcedf") }
     }
 
+    @Test
+    fun `이름을 생성할 수 있다`() {
+        assertThat(Name("pobi").stringValue()).isEqualTo("pobi")
+    }
+
     class Name(private val name: String) {
         init {
             require(name.length <= 5)
