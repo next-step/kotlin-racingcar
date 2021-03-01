ae15b015b5d7203936c62cd047862fc3275d26b4
kit(김광수) <kit.t@kakaocorp.com>
2021-03-02 00:42:49 +0900

    refactor: reorder

--- src/main/kotlin/racingcar/Name.kt
+++ src/main/kotlin/racingcar/Name.kt
@@ -3,9 +3,9 @@ package racingcar
 data class Name(private val name: String) {
     constructor(index: Int) : this(index.toString())
 
-    fun stringValue() = name
-
     init {
         require(name.length <= 5)
     }
+
+    fun stringValue() = name
 }
