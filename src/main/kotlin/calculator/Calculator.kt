9c29f88cb425f78a187e2298cb2b335469b12380
kmdngyu <kmdngyu@gmail.com>
2021-03-01 19:11:23 +0900

    feat: 코드 리뷰 반영

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -3,11 +3,11 @@ package calculator
 class Calculator(val inputString: String) {
     fun calculate(): Int {
         val expressions = Expressions(inputString)
-        var sum = expressions.operands.first().value
-        expressions.operators.withIndex()
-            .forEach {
-                sum = it.value.compute(sum, expressions.operands[it.index + 1].value)
-            }
-        return sum
+        val firstOperand = expressions.operands.first()
+        var result = firstOperand
+        expressions.operators.forEachIndexed { index, value ->
+            result = value.compute(result, expressions.operands[index + 1])
+        }
+        return result.value
     }
 }
