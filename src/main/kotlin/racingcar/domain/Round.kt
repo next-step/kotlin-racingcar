04e3fe3122227a901514d72e8b7aeed31fc024ba
bactoria <bactoria@gmail.com>
2021-03-01 17:17:20 +0900

    feat: 라운드 두번 실행될 수 없도록 검증 로직 추가

--- src/main/kotlin/racingcar/domain/Round.kt
+++ src/main/kotlin/racingcar/domain/Round.kt
@@ -5,6 +5,8 @@ class Round(private val cars: List<Car>) {
     private lateinit var gameResult: Result
 
     fun play() {
+        check(!this::gameResult.isInitialized) { "이미 진행된 라운드입니다" }
+
         cars.forEach { it.moveIfMoveable() }
         gameResult = Result(cars.map { it.currentDistance() })
     }
