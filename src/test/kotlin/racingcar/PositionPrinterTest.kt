79d00deba2eac43a9955a96d5a2e5b1fc880323b
kit(김광수) <kit.t@kakaocorp.com>
2021-03-01 23:32:51 +0900

    test: 이름과 위치를 출력한다

--- src/test/kotlin/racingcar/PositionPrinterTest.kt
+++ src/test/kotlin/racingcar/PositionPrinterTest.kt
@@ -6,16 +6,24 @@ import java.io.ByteArrayOutputStream
 
 class PositionPrinterTest {
     @Test
-    fun `위치를 출력한다`() {
+    fun `이름과 위치를 출력한다`() {
         val outputStream = ByteArrayOutputStream()
-        val positionPrinter = PositionPrinter(listOf(Positions(3, 2)), outputStream)
+        val positionPrinter = PositionPrinter(
+            listOf(
+                Positions(
+                    "pobi" to 3,
+                    "crong" to 2
+                )
+            ),
+            outputStream
+        )
 
         positionPrinter.print()
 
         Assertions.assertThat(outputStream.toString()).isEqualTo(
             """
-            ---
-            --
+            pobi : ---
+            crong : --
             
             
             """.trimIndent()
