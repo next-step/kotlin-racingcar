45859449e48cace39fc7eb490d0e9fa1ee066ba1
Wisemuji <wisemuji@gmail.com>
2021-03-02 07:21:28 +0900

    feat: Calculator 구현

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -2,7 +2,34 @@ package calculator
 
 object Calculator {
 
+    private const val DELIMITER = " "
+
     fun execute(expression: String?): Double {
-        return 0.0
+        if (expression == null || expression.isEmpty()) {
+            throw IllegalArgumentException()
+        }
+        val terms = expression.split(DELIMITER)
+        return calculate(
+            numbers = parseNumbers(terms),
+            operators = parseOperators(terms)
+        )
+    }
+
+    private fun isNumber(index: Int) = index % 2 == 0
+
+    private fun isOperator(index: Int) = index % 2 == 1
+
+    private fun parseNumbers(list: List<String>) =
+        list.filterIndexed { i, _ -> isNumber(i) }.map { it.toDouble() }
+
+    private fun parseOperators(list: List<String>) =
+        list.filterIndexed { i, _ -> isOperator(i) }.map { Operator(it) }
+
+    private fun calculate(numbers: List<Double>, operators: List<Operator>): Double {
+        var sum = numbers[0]
+        operators.forEachIndexed { index, operator ->
+            sum = operator.operation().invoke(sum, numbers[index + 1])
+        }
+        return sum
     }
 }
