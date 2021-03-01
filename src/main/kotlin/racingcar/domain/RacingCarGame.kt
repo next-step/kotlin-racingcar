62f74c8777468199f3d6e10e522678e271896691
bactoria <bactoria@gmail.com>
2021-03-01 16:54:35 +0900

    refactor: extract value

--- src/main/kotlin/racingcar/domain/RacingCarGame.kt
+++ src/main/kotlin/racingcar/domain/RacingCarGame.kt
@@ -5,7 +5,9 @@ class RacingCarGame(carCount: CarCount, roundCount: RoundCount) {
     private val rounds: List<Round>
 
     init {
-        val cars = (1..carCount.value).map { Car(RandomMoveStrategy(RandomGenerator(0, 9), 4)) }
+        val zeroToNineRandomGenerator = RandomGenerator(0, 9)
+        val fortyPercentMoveableStrategy = RandomMoveStrategy(zeroToNineRandomGenerator, 4)
+        val cars = (1..carCount.value).map { Car(fortyPercentMoveableStrategy) }
         rounds = (1..roundCount.value).map { Round(cars) }
     }
 
