ef6acf7a8bf6accc1078b55912fa8187dbb80ac8
sanggu <tkdrn8578@naver.com>
2021-03-01 17:22:26 +0900

    feat: random 숫자를 car 개수만큼 미리 생성해놓는 함수 추가

--- src/main/kotlin/number/Numbers.kt
+++ src/main/kotlin/number/Numbers.kt
@@ -1,13 +1,22 @@
 package number
 
+import java.util.stream.IntStream
 import kotlin.random.Random
+import kotlin.streams.toList
 
 object Numbers {
+    const val START_INDEX = 0
     const val MIN_RANDOM_NUMBER = 0
     const val MAX_RANDOM_NUMBER = 10
 
     fun toInt(value: String): Int {
         return value.toIntOrNull() ?: throw IllegalArgumentException("value는 숫자여야 합니다. value : $value")
+    }
+
+    fun makeRandomsToRacing(count: Int): List<Int> {
+        return IntStream.range(START_INDEX, count)
+            .mapToObj { Random.nextInt(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER) }
+            .toList()
     }
 
     fun randomToRacing(): Int {
