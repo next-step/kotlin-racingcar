308c52db3751688616b79fb87cd1ed99906a23dd
sanggu <tkdrn8578@naver.com>
2021-03-01 18:00:57 +0900

    feat: Cars 일급컬렉션 추가에 따른 로직 제거

--- src/main/kotlin/racingcar/RacingCars.kt
+++ src/main/kotlin/racingcar/RacingCars.kt
@@ -5,34 +5,13 @@ import number.Numbers.makeRandomsToRacing
 import printer.ResultView
 import java.util.stream.IntStream
 
-class RacingCars(private var cars: List<Car>) {
-    companion object {
-        private const val MAX_FORWARD_CONDITION = 4
-    }
-
+class RacingCars(private var cars: Cars) {
     fun racing(attempts: Int) {
         IntStream.range(START_INDEX, attempts)
             .forEach {
-                cars = forwardAllByCondition(makeRandomsToRacing(cars.size))
-                ResultView.printCarsPath(cars)
+                cars = cars.forwardAllByCondition(makeRandomsToRacing(cars.getSize()))
+                ResultView.printCarsPath(cars.retrieveCars())
                 println()
             }
     }
-
-    fun forwardAllByCondition(conditions: List<Int>): List<Car> {
-        return cars.mapIndexed {
-            index, car ->
-            forwardByCondition(car, conditions[index])
-        }.toList()
-    }
-
-    fun forwardByCondition(car: Car, condition: Int): Car {
-        if (canForward(condition)) {
-            car.forward()
-        }
-
-        return car
-    }
-
-    private fun canForward(condition: Int) = condition >= MAX_FORWARD_CONDITION
 }
