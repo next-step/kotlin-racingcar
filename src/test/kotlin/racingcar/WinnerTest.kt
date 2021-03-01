45c68799e5038dec1dc19297cca6644b77e7a503
kit(김광수) <kit.t@kakaocorp.com>
2021-03-02 00:11:47 +0900

    refactor: 구현클래스 이동

--- src/test/kotlin/racingcar/WinnerTest.kt
+++ src/test/kotlin/racingcar/WinnerTest.kt
@@ -21,21 +21,4 @@ class WinnerTest {
 
         assertThat(Winner(positions).names()).containsExactly("pobi", "honux")
     }
-
-    class Winner(private val positions: List<Positions>) {
-        fun names(): List<String> {
-            return positions.last()
-                .sortedByDescending { it.intValue() }
-                .takeIf { it.isNotEmpty() }
-                ?.run {
-                    winnerNames(this, first().intValue())
-                } ?: emptyList()
-        }
-
-        private fun winnerNames(
-            positions: List<NamedPosition>,
-            mostPosition: Int
-        ) = positions.takeWhile { it.intValue() >= mostPosition }
-            .map { it.name.stringValue() }
-    }
 }
