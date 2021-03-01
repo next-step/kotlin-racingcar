e28473e0896cb24ea763c730f3a2ea160ad7d82a
sanggu <tkdrn8578@naver.com>
2021-03-01 15:45:22 +0900

    feat: String 을 Int 로 변경하는 기능을 가진 Numbers.toInt 에 대한 기능 테스트 코드 추가

---
+++ src/test/kotlin/number/NumbersTest.kt
@@ -0,0 +1,19 @@
+package number
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
+import org.junit.jupiter.api.Test
+
+class NumbersTest {
+    @Test
+    fun `String을 입력받아서 Int 로 리턴한다`() {
+        assertThat(Numbers.toInt("5")).isEqualTo(5)
+    }
+
+    @Test
+    fun `숫자가 아닌 값을 입력받았을 때 예외처리한다`() {
+        assertThatExceptionOfType(IllegalArgumentException::class.java).isThrownBy {
+            assertThat(Numbers.toInt("값")).isEqualTo(5)
+        }.withMessageMatching("value는 숫자여야 합니다. value : 값")
+    }
+}
