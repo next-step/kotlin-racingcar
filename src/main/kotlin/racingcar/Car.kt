3f83b05b0a346cc6c3f3944064cc73cfdd5837a5
sanggu <tkdrn8578@naver.com>
2021-03-02 00:24:59 +0900

    refactor: position 을 외부에서 변경하지 못하도록 set 을 private 로 변경 및 초기화 방법 변경

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,17 +1,14 @@
 package racingcar
 
-class Car(var position: Int) {
-    companion object {
-        private const val START_POSITION = 0
-    }
-
-    constructor() : this(START_POSITION)
+class Car {
+    var position: Int = START_POSITION
+        private set
 
     fun forward() {
         position++
     }
 
-    fun retrievePosition(): Int {
-        return position
+    companion object {
+        private const val START_POSITION = 0
     }
 }
