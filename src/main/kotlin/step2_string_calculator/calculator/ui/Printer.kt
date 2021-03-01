ff1552815731da7f75d3c321448bf6f106121e52
dev-owner <yureka1112@gmail.com>
2021-03-01 22:12:24 +0900

    feat(UI): UI 기능 추가

---
+++ src/main/kotlin/step2_string_calculator/calculator/ui/Printer.kt
@@ -0,0 +1,9 @@
+package step2_string_calculator.calculator.ui
+
+class Printer {
+    fun print(result: Int) {
+        println("***")
+        println("RESULT : $result")
+        println("***")
+    }
+}
