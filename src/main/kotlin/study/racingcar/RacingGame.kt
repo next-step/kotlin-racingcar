ac95202fc4276f21ac61d8acb31a3c20190d9a5d
middlefgr <2giwon@gmail.com>
2021-03-02 00:31:57 +0900

    fix(step3): RacingGame 테스트 코드 추가

--- src/main/kotlin/study/racingcar/RacingGame.kt
+++ src/main/kotlin/study/racingcar/RacingGame.kt
@@ -1,7 +1,5 @@
 package study.racingcar
 
-import kotlin.random.Random
-
 class RacingGame(private val moveStrategy: MoveStrategy) {
 
     private lateinit var _cars: Array<Car>
@@ -13,14 +11,12 @@ class RacingGame(private val moveStrategy: MoveStrategy) {
 
     fun raceCars(): List<Car> {
         for (car in _cars) {
-            moveCarOrNot(car)
+            moveCarOrNot(car, moveStrategy.isMoveCar())
         }
         return _cars.toList()
     }
 
-    private fun moveCarOrNot(car: Car) {
-        if (moveStrategy.isMoveCar(Random.nextInt(9))) {
-            car.moveCar()
-        }
+    fun moveCarOrNot(car: Car, isMove: Boolean) {
+        if (isMove) car.moveCar()
     }
 }
--- src/test/kotlin/study/racingcar/RacingGameTest.kt
+++ src/test/kotlin/study/racingcar/RacingGameTest.kt
@@ -1,13 +1,36 @@
 package study.racingcar
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 internal class RacingGameTest {
 
+    private lateinit var racingGame: RacingGame
+
+    @BeforeEach
+    fun setupRacingGame() {
+        racingGame = RacingGame(RandomMoveCarStrategy())
+    }
+
     @Test
     fun `게임 준비가 완료되었는지 확인`() {
-        val racingGame = RacingGame(RandomMoveCarStrategy())
         assertThat(racingGame.readyGame(3)).isEqualTo(true)
+    }
+
+    @Test
+    fun `차를 움직이도록 요청한다`() {
+        val car = Car()
+        val oldDistance = car.distance
+        racingGame.moveCarOrNot(car, true)
+        assertThat(car.distance).isNotEqualTo(oldDistance)
+    }
+
+    @Test
+    fun `차를 움직이지 않도록 한다`() {
+        val car = Car()
+        val oldDistance = car.distance
+        racingGame.moveCarOrNot(car, false)
+        assertThat(car.distance).isEqualTo(oldDistance)
     }
 }
