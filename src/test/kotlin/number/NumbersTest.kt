471e53500e8a048ac42c1764ce2717ea853ed86c
sanggu <tkdrn8578@naver.com>
2021-03-01 16:50:09 +0900

    test: racing 에 사용할 0과 10사이의 숫자를 생성하는 함수에 대한 테스트코드 추가

--- src/test/kotlin/number/NumbersTest.kt
+++ src/test/kotlin/number/NumbersTest.kt
@@ -1,8 +1,11 @@
 package number
 
+import number.Numbers.MAX_RANDOM_NUMBER
+import number.Numbers.MIN_RANDOM_NUMBER
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 
 class NumbersTest {
     @Test
@@ -15,5 +18,15 @@ class NumbersTest {
         assertThatExceptionOfType(IllegalArgumentException::class.java).isThrownBy {
             assertThat(Numbers.toInt("값")).isEqualTo(5)
         }.withMessageMatching("value는 숫자여야 합니다. value : 값")
+    }
+
+    @Test
+    fun `Racing 에 사용할 0과 10사이의 임의의 숫자를 만든다`() {
+        val number = Numbers.randomToRacing()
+
+        assertAll(
+            { assertThat(number).isGreaterThan(MIN_RANDOM_NUMBER) },
+            { assertThat(number).isLessThan(MAX_RANDOM_NUMBER) }
+        )
     }
 }
