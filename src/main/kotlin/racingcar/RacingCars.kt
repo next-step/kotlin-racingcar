55b86dde9cc75ea1c8e37495ed8ca0b7a5c89070
sanggu <tkdrn8578@naver.com>
2021-03-02 00:28:13 +0900

    refactor: 출력하는 기능과 racing 에 대한 기능 분리 및 repeat 로 로직 변경

--- src/main/kotlin/racingcar/RacingCars.kt
+++ src/main/kotlin/racingcar/RacingCars.kt
@@ -1,17 +1,13 @@
 package racingcar
 
-import number.Numbers.START_INDEX
 import number.Numbers.makeRandomsToRacing
-import printer.ResultView
-import java.util.stream.IntStream
 
 class RacingCars(private var cars: Cars) {
-    fun racing(attempts: Int) {
-        IntStream.range(START_INDEX, attempts)
-            .forEach {
-                cars = cars.forwardAllByCondition(makeRandomsToRacing(cars.getSize()))
-                ResultView.printCarsPath(cars.retrieveCars())
-                println()
-            }
+    fun racing(attempts: Int): List<Int> {
+        repeat(attempts) {
+            cars.forwardAllByCondition(makeRandomsToRacing(cars.getSize()))
+        }
+
+        return cars.getPositions()
     }
 }
