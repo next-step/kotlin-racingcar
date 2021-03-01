f9f94d4d3f651f22b13ca788048906a559f520cb
bactoria <bactoria@gmail.com>
2021-03-01 17:02:52 +0900

    refactor: 매직넘버 제거

--- src/main/kotlin/racingcar/domain/RacingCarGame.kt
+++ src/main/kotlin/racingcar/domain/RacingCarGame.kt
@@ -1,12 +1,16 @@
 package racingcar.domain
 
+private const val MIN_RANDOM_VALUE = 0
+private const val MAX_RANDOM_VALUE = 9
+private const val THRESHOLD = 4
+
 class RacingCarGame(carCount: CarCount, roundCount: RoundCount) {
 
     private val rounds: List<Round>
 
     init {
-        val zeroToNineRandomGenerator = RandomGenerator(0, 9)
-        val fortyPercentMoveableStrategy = RandomMoveStrategy(zeroToNineRandomGenerator, 4)
+        val zeroToNineRandomGenerator = RandomGenerator(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE)
+        val fortyPercentMoveableStrategy = RandomMoveStrategy(zeroToNineRandomGenerator, THRESHOLD)
         val cars = (1..carCount.value).map { Car(fortyPercentMoveableStrategy) }
         rounds = (1..roundCount.value).map { Round(cars) }
     }
