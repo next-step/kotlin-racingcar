e10919ca4034cdd1cd6fe4294b201b2a082d0b20
kit(김광수) <kit.t@kakaocorp.com>
2021-03-01 19:34:21 +0900

    test: 쉼표로 구분된 문자를 입력받는다

--- src/test/kotlin/racingcar/UserInputTest.kt
+++ src/test/kotlin/racingcar/UserInputTest.kt
@@ -10,4 +10,10 @@ class UserInputTest {
         val userInput = UserInput.Int("질문", ByteArrayInputStream("1\n".toByteArray()))
         assertThat(userInput.answer()).isEqualTo(1)
     }
+
+    @Test
+    fun `쉼표로 구분된 문자를 입력받는다`() {
+        val userInput = UserInput.StringArray("질문", ByteArrayInputStream("pobi,crong,honux\n".toByteArray()))
+        assertThat(userInput.answer()).isEqualTo(listOf("pobi", "crong", "honux"))
+    }
 }
