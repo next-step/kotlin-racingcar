2cf1f50af23f420af657a36e2e0b4ac26a9f3b36
sanggu <tkdrn8578@naver.com>
2021-03-02 00:29:34 +0900

    refactor: printRacing 을 ResultView 로 역할 전가 및 Car 가 아닌 필요한 position 만 받는 구조로 변경

--- src/main/kotlin/printer/ResultView.kt
+++ src/main/kotlin/printer/ResultView.kt
@@ -1,7 +1,6 @@
 package printer
 
 import number.Numbers.START_INDEX
-import racingcar.Car
 import java.util.stream.IntStream
 
 object ResultView {
@@ -9,12 +8,19 @@ object ResultView {
         println("실행 결과")
     }
 
-    fun printCarsPath(cars: List<Car>) {
-        cars.forEach { car -> printCarPath(car) }
+    fun printRacing(attempts: Int, carPositions: List<Int>) {
+        repeat(attempts) {
+            printCarsPath(carPositions)
+            println()
+        }
     }
 
-    private fun printCarPath(car: Car) {
-        IntStream.range(START_INDEX, car.retrievePosition())
+    private fun printCarsPath(positions: List<Int>) {
+        positions.forEach { position -> printCarPath(position) }
+    }
+
+    private fun printCarPath(position: Int) {
+        IntStream.range(START_INDEX, position)
             .forEach {
                 print("-")
             }
