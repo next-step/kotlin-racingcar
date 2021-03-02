1f9ef9902410e081d717cfff9a7e883b74b62ed5
middlefgr <2giwon@gmail.com>
2021-03-02 23:58:16 +0900

    test(step4): 자동차 이름과 이름 입력시 5자를 초과하는지 테스트

--- src/test/kotlin/study/racingcar/CarTest.kt
+++ src/test/kotlin/study/racingcar/CarTest.kt
@@ -1,15 +1,29 @@
 package study.racingcar
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
 internal class CarTest {
 
     @Test
     fun `자동차를 움직인다`() {
-        val car = Car()
+        val car = Car("test")
         val oldDistance = car.distance
         car.moveCar()
         assertThat(oldDistance).isNotEqualTo(car.distance)
+    }
+
+    @Test
+    fun `자동차의 이름이 5자 일 경우 자동차가 생성된다`() {
+        val car = Car.makeCar("abcde")
+        assertThat(car.name.isNotEmpty()).isEqualTo(true)
+    }
+
+    @Test
+    fun `자동차 이름이 6자 일경우 자동차가 생성되지 않는다`() {
+        assertThatThrownBy {
+            Car.makeCar("abcdef")
+        }.isInstanceOf(IllegalArgumentException::class.java)
     }
 }
