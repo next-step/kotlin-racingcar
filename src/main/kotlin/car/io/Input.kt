1392d2709197229a5d9854c294dc8e952045c2fe
okk1119 <62978710+okk1119@users.noreply.github.com>
2021-03-02 09:12:57 +0900

    feat(Input) : Input에서는 이름을 , 로 구분하여 리턴할 수 있어야 한다.

--- src/main/kotlin/car/io/Input.kt
+++ src/main/kotlin/car/io/Input.kt
@@ -1,3 +1,11 @@
 package car.io
 
-data class Input(val amount: Int, val tryCount: Int)
+data class Input(val name: String, val tryCount: Int) {
+    fun nameSplitByComma(): List<String> {
+        return name.split(NAME_SPLITTER)
+    }
+
+    companion object {
+        val NAME_SPLITTER = ","
+    }
+}
