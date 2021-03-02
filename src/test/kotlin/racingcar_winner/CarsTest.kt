5aeb243c413821128091e074d0c08f4d33936046
Joseph <wjsdudtkd95@gmail.com>
2021-03-02 15:16:18 +0900

    refactor(racingcar-winner): Cars 테스트 케이스 추가

--- src/test/kotlin/racingcar_winner/CarsTest.kt
+++ src/test/kotlin/racingcar_winner/CarsTest.kt
@@ -8,20 +8,30 @@ import racingcar_winner.model.Cars
 class CarsTest {
 
     private val listOfCars = listOf<Car>(
-        Car(
-            "Phobi"
-        ), Car("John"), Car("James")
+        Car("Phobi"), Car("John"), Car("James")
     )
 
     @Test
-    fun `차들의 Progress 확인`() {
+    fun `차들이 앞으로 갔을때`() {
         listOfCars[0].tryToMoveForward(5)
         listOfCars[1].tryToMoveForward(6)
-        listOfCars[2].tryToMoveForward(4)
+        listOfCars[2].tryToMoveForward(7)
 
         val cars = Cars(listOfCars)
         val progressList = cars.getProgressOfCars().values.toList()
 
-        assertThat(progressList).isEqualTo(listOf(1,1,0))
+        assertThat(progressList).isEqualTo(listOf(1, 1, 1))
+    }
+
+    @Test
+    fun `차들이 그대로 멈출때`() {
+        listOfCars[0].tryToMoveForward(1)
+        listOfCars[1].tryToMoveForward(2)
+        listOfCars[2].tryToMoveForward(3)
+
+        val cars = Cars(listOfCars)
+        val progressList = cars.getProgressOfCars().values.toList()
+
+        assertThat(progressList).isEqualTo(listOf(0, 0, 0))
     }
 }
