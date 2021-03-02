b19dfa0b709d80059e5f8cbec101fc4d85d55184
Joseph <wjsdudtkd95@gmail.com>
2021-03-02 15:42:09 +0900

    test(racingcar-winner): Referee 클래스 테스트 (게임승자 판단)

---
+++ src/test/kotlin/racingcar_winner/RefereeTest.kt
@@ -0,0 +1,46 @@
+package racingcar_winner
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import racingcar_winner.model.Car
+import racingcar_winner.model.Cars
+import racingcar_winner.model.Referee
+
+private val PHOBI = 0
+private val JOHN = 1
+private val JAMES = 2
+
+
+class RefereeTest {
+
+    private val referee = Referee()
+    private val listOfCars = listOf<Car>(
+        Car("Phobi"), Car("John"), Car("James")
+    )
+
+    @Test
+    fun `게임의 승자 출력(동점자가 있는경우)`() {
+        listOfCars[PHOBI].tryToMoveForward(5)
+        listOfCars[PHOBI].tryToMoveForward(5)
+        listOfCars[JOHN].tryToMoveForward(5)
+        listOfCars[JOHN].tryToMoveForward(5)
+
+        // [현재 Progress] Phobi: 2, John: 2, James: 0
+        val cars = Cars(listOfCars)
+        val winners = referee.getWinners(cars)
+
+        assertThat(winners).isEqualTo(listOf("Phobi", "John"))
+    }
+
+    @Test
+    fun `게임의 승자 출력(동점자가 없는경우)`() {
+        listOfCars[PHOBI].tryToMoveForward(5)
+        listOfCars[PHOBI].tryToMoveForward(5)
+
+        // [현재 Progress] Phobi: 2, John: 0, James: 0
+        val cars = Cars(listOfCars)
+        val winners = referee.getWinners(cars)
+
+        assertThat(winners).isEqualTo(listOf("Phobi"))
+    }
+}
