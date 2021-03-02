d5e6ae5fccd4926e3ca7bd4bbb9d016db4a6e44b
bactoria <bactoria@gmail.com>
2021-03-03 08:44:07 +0900

    refactor: 무의미한 변수명 수정

--- src/main/kotlin/racingcar/domain/RacingCarGame.kt
+++ src/main/kotlin/racingcar/domain/RacingCarGame.kt
@@ -4,7 +4,7 @@ private const val MIN_RANDOM_VALUE = 0
 private const val MAX_RANDOM_VALUE = 9
 private const val THRESHOLD = 4
 
-class RacingCarGame(positiveCount: PositiveCount, roundCount: PositiveCount) {
+class RacingCarGame(carCount: PositiveCount, roundCount: PositiveCount) {
 
     private val cars: List<Car>
     private val rounds: List<Round>
@@ -12,7 +12,7 @@ class RacingCarGame(positiveCount: PositiveCount, roundCount: PositiveCount) {
     init {
         val zeroToNineRandomGenerator = RandomGenerator(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE)
         val fortyPercentMoveableStrategy = RandomMoveStrategy(zeroToNineRandomGenerator, THRESHOLD)
-        cars = (1..positiveCount.value).map { Car(fortyPercentMoveableStrategy) }
+        cars = (1..carCount.value).map { Car(fortyPercentMoveableStrategy) }
         rounds = (1..roundCount.value).map { Round() }
     }
 
