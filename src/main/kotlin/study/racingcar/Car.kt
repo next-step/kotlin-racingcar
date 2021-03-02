0e3ef3498f4171523af83eb99fdcd7ed6e5b2e31
middlefgr <2giwon@gmail.com>
2021-03-02 23:57:55 +0900

    feature(step4): 자동차 이름과 이름 입력시 5자를 초과하는지 기능 추가

--- src/main/kotlin/study/racingcar/Car.kt
+++ src/main/kotlin/study/racingcar/Car.kt
@@ -1,11 +1,27 @@
 package study.racingcar
 
-class Car {
+class Car(val name: String) {
 
     var distance: Int = 0
         private set
 
     fun moveCar() {
         distance++
+    }
+
+    companion object {
+        private const val MAX_CAR_NAME_LENGTH = 5
+
+        fun makeCar(carName: String): Car {
+            if (isExcessCarNameLength(carName)) {
+                throw IllegalArgumentException("$carName is excess $MAX_CAR_NAME_LENGTH")
+            }
+
+            return Car(carName)
+        }
+
+        fun isExcessCarNameLength(carName: String): Boolean {
+            return carName.length > MAX_CAR_NAME_LENGTH
+        }
     }
 }
