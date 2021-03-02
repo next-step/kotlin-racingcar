7657d850d2f10fb56aeaedde3a1764c8bbe7e7d4
okk1119 <62978710+okk1119@users.noreply.github.com>
2021-03-02 09:13:32 +0900

    test(Input) : 이름을 넣어주면 컴마로 구분하여 리턴해야 한다.

---
+++ src/test/kotlin/car/business/InputTest.kt
@@ -0,0 +1,16 @@
+package car.business
+
+import car.io.Input
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class InputTest {
+    @Test
+    fun `이름을 넣어주면 컴마로 구분하여 리턴해야 한다`() {
+        val input = Input(name = "오,길,환", tryCount = 3)
+
+        val names = input.nameSplitByComma()
+
+        assertThat(names).containsExactly("오","길","환")
+    }
+}
