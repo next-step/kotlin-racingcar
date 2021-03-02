fcc5efdbebf347e579d39d8e6be391961d76f0b4
sanggu <tkdrn8578@naver.com>
2021-03-02 22:27:01 +0900

    refactor: 숫자가 아닌 이름들을 입력받아 Cars 에 넘기도록 수정

--- src/main/kotlin/racingcar/Racing.kt
+++ src/main/kotlin/racingcar/Racing.kt
@@ -4,10 +4,10 @@ import printer.ResultView
 import printer.ResultView.printRacing
 
 fun main() {
-    val numberOfCars = Reception.receiveNumberOfCar()
+    val numberOfCarNames = Reception.receiveCarNames()
     val numberOfAttempts = Reception.receiveNumberOfAttempts()
 
-    val cars = Cars.createCars(numberOfCars)
+    val cars = Cars.createCars(numberOfCarNames)
     val racingCar = RacingCars(cars)
 
     ResultView.printInt()
