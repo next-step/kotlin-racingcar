9a171c872254ed537281a895600f662f4b87c625
middlefgr <2giwon@gmail.com>
2021-03-02 23:57:17 +0900

    test(step4): 입력된 자동차 이름 분리하는 parser 테스트

---
+++ src/test/kotlin/study/racingcar/CarNameParserTest.kt
@@ -0,0 +1,14 @@
+package study.racingcar
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class CarNameParserTest {
+
+    @Test
+    fun `쉼표의 개수에 +1 만큼 자동차가 생성된다`() {
+        val count = "a,b".count { it == ',' }
+        val cars = CarNameParser.requestParseCarNames("a,b")
+        assertThat(cars.size).isEqualTo(count + 1)
+    }
+}
