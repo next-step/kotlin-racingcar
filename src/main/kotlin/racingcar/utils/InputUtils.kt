f959db8a5c9e1a0002f6ded7798a6aa4131a3043
palmseung <palmseung@gmail.com>
2021-03-03 02:38:16 +0900

    refactor(racing-car): refactor to make game to iterate to get nonNullAndNotBlank input

--- src/main/kotlin/racingcar/utils/InputUtils.kt
+++ src/main/kotlin/racingcar/utils/InputUtils.kt
@@ -6,38 +6,39 @@ import racingcar.exception.InvalidInputException
 private const val QUESTION_FOR_CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
 private const val QUESTION_FOR_GAME_COUNT = "시도할 횟수는 몇 회인가요?"
 private const val DELIMITER = ","
-private const val ZERO_GAME_COUNT = 0
 
 class InputUtils {
     companion object {
         fun askQuestions(): InputDto {
             println(QUESTION_FOR_CAR_NAMES)
-            val carCount = convertToNames(readLine())
+            var readCarNames = readLine()
+            while (readCarNames.isNullOrBlank()) {
+                println(QUESTION_FOR_CAR_NAMES)
+                readCarNames = readLine()
+            }
+            val carCount = convertToNames(readCarNames!!)
 
             println(QUESTION_FOR_GAME_COUNT)
-            val gameCount = convertToGameCount(readLine())
+            var readGameCount = readLine()
+            while (readGameCount.isNullOrBlank()) {
+                println(QUESTION_FOR_GAME_COUNT)
+                readGameCount = readLine()
+            }
+            val gameCount = convertToGameCount(readGameCount!!)
 
             return InputDto(carCount, gameCount)
         }
 
-        fun convertToNames(input: String?): List<String> {
-            validateBlankInput(input)
-            return input!!.split(DELIMITER)
+        fun convertToNames(input: String): List<String> {
+            return input.split(DELIMITER)
                 .map(String::trim)
-                .toList()
         }
 
-        private fun convertToGameCount(input: String?): Int {
+        private fun convertToGameCount(input: String): Int {
             try {
                 return input!!.trim().toInt()
             } catch (e: NumberFormatException) {
                 throw InvalidInputException("게임 횟수는 숫자만 입력 가능합니다.")
-            }
-        }
-
-        private fun validateBlankInput(input: String?) {
-            if (input!!.isBlank()) {
-                throw InvalidInputException("하나 이상의 차량 이름을 입력하세요")
             }
         }
     }
