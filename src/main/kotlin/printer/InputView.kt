d976c8b0867ae02a527a4f70dbc23a4d091871c6
sanggu <tkdrn8578@naver.com>
2021-03-02 23:36:08 +0900

    refactor: 불필요한 함수 젝

--- src/main/kotlin/printer/InputView.kt
+++ src/main/kotlin/printer/InputView.kt
@@ -1,10 +1,6 @@
 package printer
 
 object InputView {
-    fun printInputNumberOfCar() {
-        println("자동자 대수는 몇 대인가요?")
-    }
-
     fun printInputNumberOfAttempts() {
         println("시도할 횟수는 몇 회인가요?")
     }
