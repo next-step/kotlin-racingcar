086a57f95c06a6ec8bdf3817068ff311de31f07c
middlefgr <2giwon@gmail.com>
2021-03-03 00:01:07 +0900

    fix(step4): 레이스게임 테스트에서 자동차 생성시 자동차 이름 추가하도록 수정, 게임준비 작업 제거

--- src/test/kotlin/study/racingcar/RacingGameTest.kt
+++ src/test/kotlin/study/racingcar/RacingGameTest.kt
@@ -14,13 +14,8 @@ internal class RacingGameTest {
     }
 
     @Test
-    fun `게임 준비가 완료되었는지 확인`() {
-        assertThat(racingGame.readyGame(3)).isEqualTo(true)
-    }
-
-    @Test
     fun `차를 움직이도록 요청한다`() {
-        val car = Car()
+        val car = Car("test")
         val oldDistance = car.distance
         racingGame.moveCarOrNot(car, true)
         assertThat(car.distance).isNotEqualTo(oldDistance)
@@ -28,7 +23,7 @@ internal class RacingGameTest {
 
     @Test
     fun `차를 움직이지 않도록 한다`() {
-        val car = Car()
+        val car = Car("test")
         val oldDistance = car.distance
         racingGame.moveCarOrNot(car, false)
         assertThat(car.distance).isEqualTo(oldDistance)
