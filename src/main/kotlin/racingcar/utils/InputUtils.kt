1fcb8980197b774ecb9c4014666b4741b522f202
palmseung <palmseung@gmail.com>
2021-03-03 03:03:23 +0900

    refactor(racing-car): refactor codes

--- src/main/kotlin/racingcar/utils/InputUtils.kt
+++ src/main/kotlin/racingcar/utils/InputUtils.kt
@@ -6,27 +6,40 @@ import racingcar.exception.InvalidInputException
 private const val QUESTION_FOR_CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
 private const val QUESTION_FOR_GAME_COUNT = "시도할 횟수는 몇 회인가요?"
 private const val DELIMITER = ","
+private const val EXCEPTION_MSG_GAME_COUNT = "게임 횟수는 숫자만 입력 가능합니다."
 
 class InputUtils {
     companion object {
         fun askQuestions(): InputDto {
-            println(QUESTION_FOR_CAR_NAMES)
-            var readCarNames = readLine()
-            while (readCarNames.isNullOrBlank()) {
-                println(QUESTION_FOR_CAR_NAMES)
-                readCarNames = readLine()
-            }
-            val carCount = convertToNames(readCarNames!!)
-
-            println(QUESTION_FOR_GAME_COUNT)
-            var readGameCount = readLine()
-            while (readGameCount.isNullOrBlank()) {
-                println(QUESTION_FOR_GAME_COUNT)
-                readGameCount = readLine()
-            }
-            val gameCount = convertToGameCount(readGameCount!!)
+            val carCount = getCarCountFromInput()
+            val gameCount = getGameCountFromInput()
 
             return InputDto(carCount, gameCount)
+        }
+
+        private fun getCarCountFromInput(): List<String> {
+            println(QUESTION_FOR_CAR_NAMES)
+            val readCarNames = iterateToGetValidInput(QUESTION_FOR_CAR_NAMES)
+
+            return convertToNames(readCarNames)
+        }
+
+        private fun getGameCountFromInput(): Int {
+            println(QUESTION_FOR_GAME_COUNT)
+            val readGameCount = iterateToGetValidInput(QUESTION_FOR_GAME_COUNT)
+
+            return convertToGameCount(readGameCount)
+        }
+
+        private fun iterateToGetValidInput(question: String): String {
+            var readLine = readLine()
+
+            while (readLine.isNullOrBlank()) {
+                println(question)
+                readLine = readLine()
+            }
+
+            return readLine
         }
 
         fun convertToNames(input: String): List<String> {
@@ -36,9 +49,9 @@ class InputUtils {
 
         private fun convertToGameCount(input: String): Int {
             try {
-                return input!!.trim().toInt()
+                return input.trim().toInt()
             } catch (e: NumberFormatException) {
-                throw InvalidInputException("게임 횟수는 숫자만 입력 가능합니다.")
+                throw InvalidInputException(EXCEPTION_MSG_GAME_COUNT)
             }
         }
     }
