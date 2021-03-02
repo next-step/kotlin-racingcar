0f89dc3432633fdaa7b6ae8a2860d16d1c1db1af
middlefgr <2giwon@gmail.com>
2021-03-02 23:59:21 +0900

    fix(step4): 레이싱게임의 멤버변수를 제거하고 레이스 후 결과값을 전달하도록 수정

--- src/main/kotlin/study/racingcar/RacingGame.kt
+++ src/main/kotlin/study/racingcar/RacingGame.kt
@@ -2,18 +2,12 @@ package study.racingcar
 
 class RacingGame(private val moveStrategy: MoveStrategy) {
 
-    private lateinit var _cars: Array<Car>
-
-    fun readyGame(carCount: Int): Boolean {
-        _cars = Array(carCount) { Car() }
-        return true
-    }
-
-    fun raceCars(): List<Car> {
-        for (car in _cars) {
+    fun raceCars(cars: List<Car>): List<Car> {
+        val mutableCars = cars.toMutableList()
+        for (car in mutableCars) {
             moveCarOrNot(car, moveStrategy.isMoveCar())
         }
-        return _cars.toList()
+        return mutableCars.toList()
     }
 
     fun moveCarOrNot(car: Car, isMove: Boolean) {
