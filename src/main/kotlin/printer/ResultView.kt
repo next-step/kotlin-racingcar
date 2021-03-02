688c06d777b726fd36385379028218dcb0896092
sanggu <tkdrn8578@naver.com>
2021-03-02 23:38:14 +0900

    feat: 경주가 끝난 후 우승자를 찍는 함수 추가 및 자동차의 이름들을 받아서 출력하도록 추가

--- src/main/kotlin/printer/ResultView.kt
+++ src/main/kotlin/printer/ResultView.kt
@@ -1,22 +1,35 @@
 package printer
 
+import racingcar.Reception.CAR_NAME_DELIMITERS
+
 object ResultView {
     fun printInt() {
         println("실행 결과")
     }
 
-    fun printRacing(carPositions: List<List<Int>>) {
+    fun printWinners(winners: List<String>) {
+        print(winners.joinToString("$CAR_NAME_DELIMITERS ") + "가 최종 우승했습니다")
+    }
+
+    fun printRacing(carNames: List<String>, carPositions: List<List<Int>>) {
         carPositions.map { carPosition ->
-            printCarsPath(carPosition)
+            printCarsPath(carNames, carPosition)
             println()
         }
     }
 
-    private fun printCarsPath(positions: List<Int>) {
-        positions.forEach { position -> printCarPath(position) }
+    private fun printCarsPath(carNames: List<String>, positions: List<Int>) {
+        positions.mapIndexed { index, position ->
+            printName(carNames[index])
+            printCarPath(position)
+        }
     }
 
     private fun printCarPath(position: Int) {
         println("-".repeat(position))
+    }
+
+    private fun printName(name: String) {
+        print("$name : ")
     }
 }
