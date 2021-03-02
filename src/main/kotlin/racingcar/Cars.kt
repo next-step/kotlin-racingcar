7652a26ec89e2bd03200cf0c7ff6a020bc347115
sanggu <tkdrn8578@naver.com>
2021-03-02 22:27:33 +0900

    refactor: 자동차의 숫자가 아닌 이름들을 입력받는 형태로 변경

--- src/main/kotlin/racingcar/Cars.kt
+++ src/main/kotlin/racingcar/Cars.kt
@@ -31,13 +31,14 @@ class Cars(private val cars: List<Car>) {
     companion object {
         private const val MAX_FORWARD_CONDITION = 4
         private const val MIN_CAN_CREATE_CARS = 0
-        private const val CREAT_CAR_START_INDEX = 1
 
-        fun createCars(numberOfCar: Int): Cars {
-            require(numberOfCar > MIN_CAN_CREATE_CARS) { "0미만의 개수는 생성할 수 없습니다." }
+        fun createCars(names: List<String>): Cars {
+            require(names.size > MIN_CAN_CREATE_CARS) { "0 미만의 개수는 생성할 수 없습니다." }
 
-            val newCars = (CREAT_CAR_START_INDEX..numberOfCar)
-                .map { Car() }
+            val newCars = names
+                .map { name ->
+                    Car(name)
+                }
                 .toList()
 
             return Cars(newCars)
