bd16277011347f392426080d210f5e2372496cbb
남윤서 <devysnam@gmail.com>
2021-03-03 11:07:58 +0900

    feat: 이름에 대한 예외 케이스 추가

--- src/main/kotlin/racing/domain/car/RacingCarFactory.kt
+++ src/main/kotlin/racing/domain/car/RacingCarFactory.kt
@@ -1,5 +1,7 @@
 package racing.domain.car
 
+private const val MIN_PARTICIPANT = 2
+
 class RacingCarFactory {
     companion object {
         fun createRacingCars(carNames: String): RacingCars {
@@ -8,10 +10,15 @@ class RacingCarFactory {
 
         private fun createRacingCar(carNames: String): List<RacingCar> {
             val tokens = carNames.split(",")
+            validateParticipant(tokens)
 
             return tokens.map { name -> RacingCarName(name) }
                 .map { racingCarName -> RacingCar(racingCarName) }
                 .toList()
+        }
+
+        private fun validateParticipant(tokens: List<String>) {
+            require(tokens.size >= MIN_PARTICIPANT) { "참가자는 2명 이상이어야 합니다." }
         }
     }
 }
