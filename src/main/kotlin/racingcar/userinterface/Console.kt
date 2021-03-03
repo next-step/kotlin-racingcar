9e47ee6bedeba941b7e7fed0c9d662377d62fc61
bactoria <bactoria@gmail.com>
2021-03-04 00:35:38 +0900

    refactor: 코틀린에서 제공하는 함수 사용

--- src/main/kotlin/racingcar/userinterface/Console.kt
+++ src/main/kotlin/racingcar/userinterface/Console.kt
@@ -9,12 +9,12 @@ class Console : UserInterface {
 
     override fun inputCarNames(): List<String> {
         println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
-        return (readLine() ?: "").split(CAR_NAME_DELIMITER)
+        return readLine().orEmpty().split(CAR_NAME_DELIMITER)
     }
 
     override fun inputRoundCount(): String {
         println("시도할 횟수는 몇 회인가요?")
-        return readLine() ?: ""
+        return readLine().orEmpty()
     }
 
     override fun outputResult(results: Results) {
