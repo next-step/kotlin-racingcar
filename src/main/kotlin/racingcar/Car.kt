0533b4c48f69a7301ea81f6753a78ade2acf58a3
kimsanggu <tkdrn8578@paymint.co.kr>
2021-03-03 18:09:45 +0900

    refactor: position 일급 컬렉션 추가에 따른 수정

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -5,15 +5,14 @@ class Car(val name: String) {
         require(name.length < MAX_NAME_LENGTH) { throw IllegalArgumentException("이름은 5글자를 초과할 수 없습니다.") }
     }
 
-    var position: Int = START_POSITION
+    var position: Position = Position()
         private set
 
     fun forward() {
-        position++
+        position.forward()
     }
 
     companion object {
-        private const val START_POSITION = 0
         private const val MAX_NAME_LENGTH = 6
     }
 }
