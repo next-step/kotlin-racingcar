16bb3ebdb9ec84942c6792e52e39146359690fd5
sanggu <tkdrn8578@naver.com>
2021-03-03 21:59:15 +0900

    test: Positions 의 max 에 대한 기능 테스트 코드 추가

---
+++ src/test/kotlin/racingcar/PositionsTest.kt
@@ -0,0 +1,15 @@
+package racingcar
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class PositionsTest {
+    @Test
+    fun `최대 position 값을 리턴한다`() {
+        val positions = Positions(listOf(Position(3), Position(5), Position(9)))
+
+        val maxPosition = positions.max()
+
+        assertThat(maxPosition.position).isEqualTo(9)
+    }
+}
