bd42d551db9fbb76783d60fbb97f9fef4dc37c60
sw-oh <sangw0804@sangw0804ui-MacBookPro.local>
2021-03-04 00:32:03 +0900

    fix(OutputView): 경기 결과 출력 로직에러 수정

--- src/main/kotlin/step3/OutputView.kt
+++ src/main/kotlin/step3/OutputView.kt
@@ -4,15 +4,15 @@ class OutputView {
     companion object {
         fun handleOutput(lapCount: Int, lapResults: LapResults) {
             println("실행 결과")
-            println(lapResults)
-            repeat(lapCount) {
-                printRace(lapResults, it)
+
+            for (lapResult in lapResults.lapResults) {
+                printLapResult(lapResult)
             }
         }
 
-        private fun printRace(lapResults: LapResults, lap: Int) {
-            for (lapResult in lapResults.lapResults) {
-                println("-".repeat(lapResult.records[lap]))
+        private fun printLapResult(lapResult: LapResult) {
+            for (record in lapResult.records) {
+                println("-".repeat(record))
             }
             println()
         }
