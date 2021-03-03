04e965625f8af559992430e6b295187eca6da918
남윤서 <devysnam@gmail.com>
2021-03-04 02:19:00 +0900

    refactor: 메소드 추출 및 이름 변경

--- src/main/kotlin/racing/domain/ranking/Ranker.kt
+++ src/main/kotlin/racing/domain/ranking/Ranker.kt
@@ -1,13 +1,19 @@
 package racing.domain.ranking
 
+import racing.ui.RacingCarDto
 import racing.ui.RacingCarDtos
 
 class Ranker {
-    fun rank(record: List<RacingCarDtos>): List<String> {
-        val lastRecord = record.last().racingCarDtos
-        val topScore = lastRecord.maxOf { racingCarDto -> racingCarDto.position }
+    fun getWinners(record: List<RacingCarDtos>): List<String> {
+        val lastRecord = extractLastRecord(record)
+        val topScore = calculateTopScore(lastRecord)
         return lastRecord.filter { racingCarDto -> racingCarDto.position == topScore }
             .map { racingCarDto -> racingCarDto.name }
             .toList()
     }
+
+    private fun extractLastRecord(record: List<RacingCarDtos>) = record.last().racingCarDtos
+
+    private fun calculateTopScore(lastRecord: List<RacingCarDto>) =
+        lastRecord.maxOf { racingCarDto -> racingCarDto.position }
 }
