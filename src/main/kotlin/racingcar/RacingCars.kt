48cb087bd91a31165b93a532657a1d63be09a221
kimsanggu <tkdrn8578@paymint.co.kr>
2021-03-03 18:15:35 +0900

    refactor: List<Int> 를 Positions 로 변경

--- src/main/kotlin/racingcar/RacingCars.kt
+++ src/main/kotlin/racingcar/RacingCars.kt
@@ -3,8 +3,8 @@ package racingcar
 import number.Numbers.makeRandomsToRacing
 
 class RacingCars(private var cars: Cars) {
-    fun racing(attempts: Int): List<List<Int>> {
-        val allRoundPositions = ArrayList<List<Int>>()
+    fun racing(attempts: Int): List<Positions> {
+        val allRoundPositions = ArrayList<Positions>()
 
         repeat(attempts) {
             cars.forwardAllByCondition(makeRandomsToRacing(cars.getSize()))
