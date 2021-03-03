6cc1c9498d9254063f5188eb119f50f00b2d4844
taeheon.song <taeheon.song@linecorp.com>
2021-03-03 23:03:48 +0900

    Test(CarName) : Test CarName

--- src/main/kotlin/racing/domain/Car.kt
+++ src/main/kotlin/racing/domain/Car.kt
@@ -13,7 +13,7 @@ class Car(
 
 data class CarName(val name: String) {
     init {
-        if (name.length > 5) {
+        require(name.length <= 5) {
             throw IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.")
         }
     }
