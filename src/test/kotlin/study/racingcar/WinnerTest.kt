406ed1a463d9a109eb28159c942d9d9d0e7dbba1
middlefgr <2giwon@gmail.com>
2021-03-03 21:22:42 +0900

    fix(step4): 테스트 코드 3대 이상일 경우도 추가

--- src/test/kotlin/study/racingcar/WinnerTest.kt
+++ src/test/kotlin/study/racingcar/WinnerTest.kt
@@ -6,6 +6,27 @@ import org.junit.jupiter.api.Test
 internal class WinnerTest {
 
     @Test
+    fun `1등이 3대 일경우 우승자의 수도 3`() {
+        val winner = Winner()
+        val car1 = Car("a")
+        car1.moveCar()
+        car1.moveCar()
+
+        val car2 = Car("b")
+        car2.moveCar()
+        car2.moveCar()
+
+        val car3 = Car("c")
+        car3.moveCar()
+        car3.moveCar()
+
+        val cars: List<Car> = listOf(car1, car2, car3)
+        val winners = winner.fetchWinners(cars)
+
+        assertThat(winners.size).isEqualTo(3)
+    }
+
+    @Test
     fun `1등이 2대 일경우 우승자의 수도 2`() {
         val winner = Winner()
         val car1 = Car("a")
