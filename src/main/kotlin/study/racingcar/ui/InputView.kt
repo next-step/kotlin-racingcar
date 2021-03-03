e67a0ff9c4e174cc9c902e0e27ea4f1d87066326
이태훈(thxwelchs) <thxwelchs@gmail.com>
2021-03-04 05:09:38 +0900

    feat(ui): InputView 구현

---
+++ src/main/kotlin/study/racingcar/ui/InputView.kt
@@ -0,0 +1,16 @@
+package study.racingcar.ui
+
+object InputView {
+    private const val INPUT_CAR_COUNT_COMMENT = "자동차 대수는 몇 대인가요?"
+    private const val INPUT_TRY_COUNT_COMMENT = "시도할 횟수는 몇 회인가요?"
+
+    fun inputCarCount(): Int {
+        println(INPUT_CAR_COUNT_COMMENT)
+        return readLine()?.toInt()!!
+    }
+
+    fun inputTryCount(): Int {
+        println(INPUT_TRY_COUNT_COMMENT)
+        return readLine()?.toInt()!!
+    }
+}
