3141024a425585f2d0c6c24538a1d3b5ac220bb7
hyunssooo <jhs695@gmail.com>
2021-03-04 20:38:56 +0900

    refactor: exception 변경

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -2,10 +2,10 @@ package racingcar.view
 
 fun inputCountOfCar(): Int {
     println("자동차 대수는 몇 대인가요?")
-    return readLine()?.toInt() ?: throw IllegalArgumentException()
+    return readLine()?.toInt() ?: throw NumberFormatException()
 }
 
 fun inputCountOfRace(): Int {
     println("시도할 횟수는 몇 회인가요?")
-    return readLine()?.toInt() ?: throw IllegalArgumentException()
+    return readLine()?.toInt() ?: throw NumberFormatException()
 }
