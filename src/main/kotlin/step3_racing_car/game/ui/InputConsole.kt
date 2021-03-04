7c1148f43c251d93ae9b3520b7bdc39e7270ce09
dev-owner <yureka1112@gmail.com>
2021-03-04 23:25:45 +0900

    refactor(RacingCar-winner): 리팩토링 InputConsole.kt

--- src/main/kotlin/step3_racing_car/game/ui/InputConsole.kt
+++ src/main/kotlin/step3_racing_car/game/ui/InputConsole.kt
@@ -17,29 +17,22 @@ class InputConsole(private val receiver: InputReceiver) {
         print("시도할 횟수는 몇 회인가요?")
         val numOfGame: String? = receiver.receive()
 
-        if (!isValid(numOfCar) or !isValid(numOfGame)) {
-            throw java.lang.IllegalArgumentException("입력이 실패했습니다.")
-        }
+        isValid(numOfCar)
+        isValid(numOfGame)
+
         return Pair(numOfCar!!.toInt(), numOfGame!!.toInt())
     }
 
-    private fun isValid(num: String?): Boolean {
-        if (num.isNullOrBlank()) {
-            throw IllegalArgumentException("입력값은 null 혹은 blank 값이 될 수 없습니다.")
-        }
+    private fun isValid(num: String?) {
+        require(!num.isNullOrBlank()) { "입력값은 null 혹은 blank 값이 될 수 없습니다." }
 
-        val numToInt: Int
-
+        var numToInt = 0
         try {
             numToInt = num.toInt()
         } catch (e: NumberFormatException) {
             throw IllegalArgumentException("입력값 : $num, 정수형을 입력해 주세요.")
         }
 
-        if (numToInt <= 0) {
-            throw IllegalArgumentException("입력값은 1 이상이어야만 합니다.")
-        }
-
-        return true
+        require(numToInt > 0) { "입력값은 1 이상이어야만 합니다." }
     }
 }
