4f2087cb7d3dba685e244140bfd8a1653e09ad32
HyeJin Kim <haaley94@gmail.com>
2021-03-04 20:05:02 +0900

    [step1] 코틀린기초 (#203)
    * [step1] 코틀린기초 (add person class and testcases)
    
    * klint 반영
    
    Co-authored-by: h.hyejinkim <h.hyejinkim@webtoonscorp.com>

---
+++ src/main/kotlin/Person.kt
@@ -0,0 +1,1 @@
+data class Person(val name: String, var age: Int? = null, var nickname: String? = null)
---
+++ src/test/kotlin/PersonTest.kt
@@ -0,0 +1,46 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+
+class PersonTest {
+    @Test
+    fun `named arguments`() {
+        val people = listOf(
+            Person("김혜진", 28, "haley"),
+            Person("김혜진", 28, nickname = "haley"),
+            Person(name = "김혜진", nickname = "haley", age = 28)
+        )
+        assertThat(people).allSatisfy {
+            assertThat(it.name).isEqualTo("김혜진")
+            assertThat(it.age).isEqualTo(28)
+            assertThat(it.nickname).isEqualTo("haley")
+        }
+    }
+
+    @Test
+    fun `nullable types`() {
+        val person = Person("김혜진", null, null)
+        assertAll(
+            { assertThat(person.name).isEqualTo("김혜진") },
+            { assertThat(person.age).isNull() },
+            { assertThat(person.nickname).isNull() }
+        )
+    }
+
+    @Test
+    fun `default arguments`() {
+        val person = Person("김혜진")
+        assertAll(
+            { assertThat(person.name).isEqualTo("김혜진") },
+            { assertThat(person.age).isNull() },
+            { assertThat(person.nickname).isNull() }
+        )
+    }
+
+    @Test
+    fun `data classes`() {
+        val person1 = Person("김혜진", 29, "haley")
+        val person2 = Person("김혜진", 29, "haley")
+        assertThat(person1).isEqualTo(person2)
+    }
+}
