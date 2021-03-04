0fa48463c6004148f15f1f809358d2634f04de02
남윤서 <devysnam@gmail.com>
2021-03-04 23:54:18 +0900

    refactor: 우승자를 구하는 방식을 Custom Getter로 변경

--- src/main/kotlin/racing/domain/ranking/Ranker.kt
+++ src/main/kotlin/racing/domain/ranking/Ranker.kt
@@ -3,14 +3,15 @@ package racing.domain.ranking
 import racing.ui.RacingCarDto
 import racing.ui.RacingCarDtos
 
-class Ranker {
-    fun getWinners(record: List<RacingCarDtos>): List<String> {
-        val lastRecord = extractLastRecord(record)
-        val topScore = calculateTopScore(lastRecord)
-        return lastRecord.filter { racingCarDto -> racingCarDto.position == topScore }
-            .map { racingCarDto -> racingCarDto.name }
-            .toList()
-    }
+class Ranker(private val record: List<RacingCarDtos>) {
+    val winners: List<String>
+        get() {
+            val lastRecord = extractLastRecord(record)
+            val topScore = calculateTopScore(lastRecord)
+            return lastRecord.filter { racingCarDto -> racingCarDto.position == topScore }
+                .map { racingCarDto -> racingCarDto.name }
+                .toList()
+        }
 
     private fun extractLastRecord(record: List<RacingCarDtos>) = record.last().racingCarDtos
 
