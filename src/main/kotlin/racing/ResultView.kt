820b19c3ed6bd7899adac9849502e55f0f1a4c9e
SeoJinHyuk14 <suhjh14@gmail.com>
2021-03-05 21:43:41 +0900

    구문 간략히 줄이기

--- src/main/kotlin/racing/ResultView.kt
+++ src/main/kotlin/racing/ResultView.kt
@@ -4,10 +4,7 @@ fun printResult(carList: List<Car>) {
     println("실행 결과")
 
     carList.forEach {
-        (0 until it.position).forEach {
-            print("-")
-        }
-        println("")
+        println("-".repeat(it.position))
     }
-    println("")
+    println()
 }
