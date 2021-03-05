58464c74967afeed71d937c2211450cecb6f2ff4
이태훈(thxwelchs) <thxwelchs@gmail.com>
2021-03-05 17:37:05 +0900

    refactor(calculation): Operator에 high order function을 적용해 연산식 구현으로 수정
    수정 사항:
    - CalculationFactory 팩토리 메소드 패턴으로 구현된 연산식 객체 제거
    - Operator에 high order function으로 대체 구현
    - 연산자 여부 기능 구현

--- src/main/kotlin/study/calculation/Calculation.kt
+++
@@ -1,5 +0,0 @@
-package study.calculation
-
-interface Calculation {
-    fun calculate(a: Int, b: Int): Int
-}
--- src/main/kotlin/study/calculation/Operator.kt
+++ src/main/kotlin/study/calculation/Operator.kt
@@ -1,15 +1,22 @@
 package study.calculation
 
-import java.lang.IllegalArgumentException
-
-enum class Operator(val oper: String, val calculation: Calculation) {
-    ADDITON("+", Addition()),
-    SUBTRACTION("-", Subtraction()),
-    MULTIPLICATION("*", Multiplication()),
-    DIVISION("/", Division());
+enum class Operator(val oper: String, val calculation: (Int, Int) -> Int) {
+    ADDITON("+", { a, b -> a + b }),
+    SUBTRACTION("-", { a, b -> a - b }),
+    MULTIPLICATION("*", { a, b -> a * b }),
+    DIVISION("/", { a, b -> a / b });
 
     companion object {
-        fun select(oper: String): Operator =
-            values().find { oper == it.oper } ?: throw IllegalArgumentException("지원하는 연산자가 아닙니다.")
+        fun select(oper: String): Operator {
+            require(isOperator(oper)) {
+                "지원하지 않는 연산자입니다."
+            }
+
+            return find(oper)!!
+        }
+
+        fun isOperator(oper: String): Boolean = find(oper) != null
+
+        fun find(oper: String): Operator? = values().find { oper == it.oper }
     }
 }
