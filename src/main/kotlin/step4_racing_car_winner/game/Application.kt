f057c17dac012d2b248ec5469c23fed1e831dd3c
dev-owner <yureka1112@gmail.com>
2021-03-06 18:58:28 +0900

    refactor(racingcar): 리뷰 전 작성 코드 삭제

--- src/main/kotlin/step4_racing_car_winner/game/Application.kt
+++
@@ -1,7 +0,0 @@
-package step4_racing_car_winner.game
-
-import step4_racing_car_winner.game.ui.CommandLineReceiver
-
-fun main() {
-    Game.start(CommandLineReceiver())
-}
--- src/main/kotlin/step4_racing_car_winner/game/Car.kt
+++
@@ -1,3 +0,0 @@
-package step4_racing_car_winner.game
-
-data class Car(val name: String, var position: Int = 0)
