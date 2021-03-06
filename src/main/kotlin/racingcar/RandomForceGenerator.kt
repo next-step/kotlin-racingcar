7eab210cd2fcea87d54bc014c5686bf51fee060e
Nam Dongmin <nam.dongmin@htbeyond.com>
2021-03-06 17:53:43 +0900

    refactor: 코틀린 표준 함수 및 클래스로 더욱 간략화

--- src/main/kotlin/racingcar/RandomForceGenerator.kt
+++ src/main/kotlin/racingcar/RandomForceGenerator.kt
@@ -1,11 +1,7 @@
 package racingcar
 
-import kotlin.random.Random
-
 class RandomForceGenerator : ForceGenerator {
-    private val random: Random = Random
-
     override fun generate(): Force {
-        return Force(random.nextInt(from = 0, until = 10))
+        return Force((0..9).random())
     }
 }
