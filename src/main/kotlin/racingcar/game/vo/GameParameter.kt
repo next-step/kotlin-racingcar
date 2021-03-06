798580983566e9947232615aed9df007d52294f6
dev-owner <yureka1112@gmail.com>
2021-03-06 18:04:17 +0900

    feat(racingcar): 자동차 이름 validation 추가

--- src/main/kotlin/racingcar/game/vo/GameParameter.kt
+++ src/main/kotlin/racingcar/game/vo/GameParameter.kt
@@ -2,10 +2,13 @@ package racingcar.game.vo
 
 import java.lang.IllegalArgumentException
 
+const val MAX_CAR_NAME = 5
+
 class GameParameter(val nameOfCar: List<String>, val numOfGame: Int) {
     init {
         nameOfCar.forEach {
             if (it.isBlank()) throw IllegalArgumentException("자동차 이름은 blank 값이 될 수 없습니다.")
+            if (it.length > MAX_CAR_NAME) throw IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.")
         }
     }
 }
