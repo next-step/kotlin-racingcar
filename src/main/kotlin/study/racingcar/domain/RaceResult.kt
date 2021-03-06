ec69100fda5f2c8d9a78a13eff17375d0198a639
이태훈(thxwelchs) <thxwelchs@gmail.com>
2021-03-06 09:40:32 +0900

    feat(domain): RaceResult 실행결과 요구사항 포맷에 맞게 수정

--- src/main/kotlin/study/racingcar/domain/RaceResult.kt
+++ src/main/kotlin/study/racingcar/domain/RaceResult.kt
@@ -1,11 +1,12 @@
 package study.racingcar.domain
 
-import study.racingcar.common.GameConfig
-
-class RaceResult(cars: List<Car>) {
-    val resultSymbols: List<String> = cars.map { toRaceSymbol(it) }
-
-    private fun toRaceSymbol(car: Car): String {
-        return (1..car.moveCount).map { GameConfig.RACE_SYMBOL }.joinToString("")
+class RaceResult {
+    var racedCars: List<Car>
+        private set
+    constructor(racedCars: List<Car>) {
+        this.racedCars = racedCars.map { Car(it.carName, it.moveCount) }
+    }
+    override fun toString(): String {
+        return racedCars.joinToString("\n")
     }
 }
