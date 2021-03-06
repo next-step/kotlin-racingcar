c1fb7170f8ab0257f4c9a12cf1234f0e18aa342d
dev-owner <yureka1112@gmail.com>
2021-03-06 18:22:14 +0900

    feat(racingcar): 우승자 출력 기능 추가

--- src/main/kotlin/racingcar/game/ui/Screen.kt
+++ src/main/kotlin/racingcar/game/ui/Screen.kt
@@ -6,15 +6,29 @@ import racingcar.game.vo.GameRoundResult
 
 object Screen {
     fun show(result: GameResult) {
+        println("실행 결과")
         result.history.forEach { roundResult ->
             showRoundGame(roundResult)
         }
+        printWinners(result)
+    }
+
+    private fun printWinners(result: GameResult) {
+        val lastRoundHistory: List<Car> = result.history.last().roundHistory
+        val maxPosition: Int? = lastRoundHistory.maxOfOrNull { it.position }
+        val maxPositionCars: List<String> = lastRoundHistory.filter {
+            it.position == maxPosition
+        }.map { it.name }
+
+        val winners = maxPositionCars.joinToString(",")
+        print("${winners}가 최종 우승했습니다.")
     }
 
     private fun showRoundGame(roundResult: GameRoundResult) {
         roundResult.roundHistory.forEach { car ->
             printResult(car)
         }
+        println()
     }
 
     private fun printResult(car: Car) {
