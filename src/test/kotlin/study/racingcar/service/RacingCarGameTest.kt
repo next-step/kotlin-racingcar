61937ed8bf117b7996c4b6afc70112b1e60abec4
이태훈(thxwelchs) <thxwelchs@gmail.com>
2021-03-06 09:43:26 +0900

    test(service) RacingCarGame 우승자 테스트

--- src/test/kotlin/study/racingcar/service/RacingCarGameTest.kt
+++ src/test/kotlin/study/racingcar/service/RacingCarGameTest.kt
@@ -1,19 +1,51 @@
 package study.racingcar.service
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import study.racingcar.domain.Car
+import study.racingcar.domain.Winners
+import study.racingcar.ui.InputView
 
 internal class RacingCarGameTest {
 
     @ParameterizedTest
-    @CsvSource("2, 3", "2, 3")
-    fun `자동차 대수와 시도 횟수에 맞게 게임이 진행되는가`(carCount: Int, tryCount: Int) {
-        val game = RacingCarGame(carCount, tryCount)
+    @CsvSource("'test1,test2,test3', 3", "'test1,test2,test3', 5")
+    fun `자동차 대수와 시도 횟수에 맞게 게임이 진행되는가`(carNames: String, tryCount: Int) {
+        val carNameList = InputView.toCarNameList(carNames)
+        val game = RacingCarGame(carNameList, tryCount)
         game.start()
 
-        val raceResults = game.getRaceResults()
-        assertThat(game.getRaceResults().size).isEqualTo(tryCount)
-        raceResults.forEach { assertThat(it.resultSymbols.size).isEqualTo(carCount) }
+        val raceResults = game.raceResults
+        assertThat(raceResults.size).isEqualTo(tryCount)
+        raceResults.forEach { assertThat(it.racedCars.size).isEqualTo(carNameList.size) }
+    }
+
+    @Test
+    fun `우승자`() {
+        val cars = listOf(
+            Car("test1").apply {
+                this.move(5)
+                this.move(5)
+                this.move(7)
+                this.move(9)
+            },
+            Car("test2").apply {
+                this.move(5)
+            },
+            Car("test3").apply {
+                this.move(5)
+                this.move(5)
+                this.move(5)
+                this.move(5)
+            }
+        )
+
+        val winners = Winners(cars).winners!!
+
+        assertThat(winners.size).isEqualTo(2)
+        assertThat(winners[0].carName).isEqualTo("test1")
+        assertThat(winners[1].carName).isEqualTo("test3")
     }
 }
