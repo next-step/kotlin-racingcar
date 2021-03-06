45d266520d600ec9b3bc709116ce077e14ea349e
이태훈(thxwelchs) <thxwelchs@gmail.com>
2021-03-06 09:36:03 +0900

    feat(domain): Car 이름 입력받기 (최대5자)

--- src/main/kotlin/study/racingcar/domain/Car.kt
+++ src/main/kotlin/study/racingcar/domain/Car.kt
@@ -3,11 +3,35 @@ package study.racingcar.domain
 import study.racingcar.common.GameConfig
 
 class Car {
+    companion object {
+        private val MAXIMUM_CAR_NAME_LENGTH = 5
+    }
+
+    constructor(carName: String) {
+        this.carName = carName
+    }
+
+    constructor(carName: String, moveCount: Int) : this(carName) {
+        this.moveCount = moveCount
+    }
+
+    var carName: String = ""
+        private set(value) {
+            require(value.length <= MAXIMUM_CAR_NAME_LENGTH) {
+                "차량의 이름은 최대 ${MAXIMUM_CAR_NAME_LENGTH}자 까지만 가능합니다."
+            }
+            field = value
+        }
     var moveCount: Int = 1
         private set
+
     fun move(randomValue: Int) {
         if (isMovable(randomValue)) moveCount++
     }
 
     private fun isMovable(randomValue: Int): Boolean = randomValue >= GameConfig.MOVABLE_VALUE
+
+    override fun toString(): String {
+        return "$carName : ${(1..moveCount).map { GameConfig.RACE_SYMBOL }.joinToString("")}"
+    }
 }
