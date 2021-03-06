46d29bbc0b48d6a745ee4261ca03bb5680a66ad1
taeheon.song <taeheon.song@linecorp.com>
2021-03-06 10:35:05 +0900

    refactor(racingCar) : fix ktlint warning

--- src/main/kotlin/racing/controller/RacingController.kt
+++ src/main/kotlin/racing/controller/RacingController.kt
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2021. LINE Corporation. All rights reserved.
- * LINE Corporation PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
-
 package racing.controller
 
 import racing.domain.MovementChecker
@@ -10,10 +5,6 @@ import racing.domain.RacingGame
 import racing.ui.InputView
 import racing.ui.ResultView
 
-/**
- * @author tae-heon.song<taeheon.song@linecorp.com>
- * @since 2021. 03. 06.
- */
 object RacingController {
     fun run() {
         val racingGameData = InputView.askQuestion()
