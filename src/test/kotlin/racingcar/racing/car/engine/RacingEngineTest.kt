6a243fcba2356c4d79f6ca5123a1d3cfb2af2fc1
Nam Dongmin <nam.dongmin@htbeyond.com>
2021-03-08 02:00:58 +0900

    test: 잘못된 테스트 이름 수정

--- src/test/kotlin/racingcar/racing/car/engine/RacingEngineTest.kt
+++ src/test/kotlin/racingcar/racing/car/engine/RacingEngineTest.kt
@@ -15,7 +15,7 @@ internal class RacingEngineTest {
         "3, 4, MOVE",
         "3, ${Int.MAX_VALUE}, MOVE"
     )
-    fun `threshold보다 낮으면 회전력(Torque)이 0 나오고, threshold보다 높으면 회전력(Torque)이 1 나온다`(
+    fun `threshold보다 낮으면 회전력(Torque)이 0 나오고, threshold보다 같거나 높으면 회전력(Torque)이 1 나온다`(
         threshold: Int,
         force: Int,
         torque: Torque
