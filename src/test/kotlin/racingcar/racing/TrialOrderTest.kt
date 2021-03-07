8a3cc33b62d6f79d8dedae9580405188edc3ae0f
Nam Dongmin <nam.dongmin@htbeyond.com>
2021-03-07 23:38:22 +0900

    style: formatting

--- src/test/kotlin/racingcar/racing/TrialOrderTest.kt
+++ src/test/kotlin/racingcar/racing/TrialOrderTest.kt
@@ -11,10 +11,10 @@ internal class TrialOrderTest {
     fun `인스턴스가 달라도 value가 같으면 동일하다`(value: Int) {
         assertThat(TrialOrder(value)).isEqualTo(TrialOrder(value))
     }
+
     @ParameterizedTest
     @ValueSource(ints = [0, -1, -278, -2042, Int.MIN_VALUE])
     fun `양수가 아니면 IllegalArgumentException throw`(value: Int) {
-
         assertThatIllegalArgumentException()
             .isThrownBy { TrialOrder(value) }
             .withMessage("The number must be positive.")
