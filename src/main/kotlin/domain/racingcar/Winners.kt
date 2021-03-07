b80721c8c7d473b3bfca8a4a9aa6193e517aa7f7
sanggu <tkdrn8578@naver.com>
2021-03-08 00:00:14 +0900

    refactor: winners의 name을 ResultView에서 사용함에 따른 접근제어자 변경 및 테스트를 위한 불필요한 메서드 제거

--- src/main/kotlin/domain/racingcar/Winners.kt
+++ src/main/kotlin/domain/racingcar/Winners.kt
@@ -1,11 +1,3 @@
 package domain.racingcar
 
-class Winners(val names: List<String>) {
-    fun getSize(): Int {
-        return names.size
-    }
-
-    fun getByIndex(index: Int): String {
-        return names[index]
-    }
-}
+class Winners(val names: List<String>)
