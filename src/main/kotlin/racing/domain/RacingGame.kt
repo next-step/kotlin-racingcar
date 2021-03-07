a4690838e2afe86c0d533f1bf3c3d2e60cd1f1c0
taeheon.song <taeheon.song@linecorp.com>
2021-03-07 19:16:56 +0900

    refactor(racingCar) : change getter of winners property to method

--- src/main/kotlin/racing/domain/RacingGame.kt
+++ src/main/kotlin/racing/domain/RacingGame.kt
@@ -11,19 +11,23 @@ class RacingGame(private val racingGameData: RacingGameData, private val movemen
 
     val cars: Cars = Cars(racingGameData.carNames)
     private val racingHistory: RacingHistory = RacingHistory()
-    private val winners
-        get() = Winners.from(cars.getFarthestCars())
 
     fun run(): RacingGameResult {
         repeat(racingGameData.tryCount) {
             val carStates = cars.moveAllCar(movementChecker)
             recordRoundResult(carStates)
         }
+
+        val winners = findWinners()
         return RacingGameResult(racingHistory, winners)
     }
 
     private fun recordRoundResult(carStates: List<CarState>) {
         val roundResult = RoundResult(carStates)
         racingHistory.recordRoundResult(roundResult)
+    }
+
+    private fun findWinners(): Winners {
+        return Winners.from(cars.getFarthestCars())
     }
 }
