6d6c0b94e897201acecfc5963d09dd19e5f40393
dev-owner <yureka1112@gmail.com>
2021-03-07 22:40:24 +0900

    refactor(racingcar): mutablelist를 외부에서 조작할 수 없도록 변경

--- src/main/kotlin/racingcar/game/domain/GameResult.kt
+++ src/main/kotlin/racingcar/game/domain/GameResult.kt
@@ -3,10 +3,11 @@ package racingcar.game.domain
 import racingcar.game.vo.GameRoundResult
 
 class GameResult {
-    val history: List<GameRoundResult> = mutableListOf()
+    private val _history = mutableListOf<GameRoundResult>()
+    val history: List<GameRoundResult>
+        get() = _history.toList()
 
     fun recordGameRoundHistory(gameRoundResult: GameRoundResult) {
-        history as MutableList
-        history.add(gameRoundResult)
+        _history.add(gameRoundResult)
     }
 }
