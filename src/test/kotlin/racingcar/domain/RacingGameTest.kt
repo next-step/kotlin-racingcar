8379389a7a4a8afcfb022cb7bb5236d1bbffb10f
kit(김광수) <kit.t@kakaocorp.com>
2021-03-07 23:28:26 +0900

    refactor: containsExactly 를 사용하여 중복제거

--- src/test/kotlin/racingcar/domain/RacingGameTest.kt
+++ src/test/kotlin/racingcar/domain/RacingGameTest.kt
@@ -6,16 +6,23 @@ import org.junit.jupiter.api.Test
 class RacingGameTest {
     @Test
     fun `이동결과를 제공한다`() {
-        val racingGame = RacingGame(CarsTest.cars(3))
+        val racingGame = RacingGame(CarsTest.cars(CAR_COUNT))
 
-        racingGame.play(5)
+        racingGame.play(PLAY_COUNT)
 
-        assertThat(racingGame.positions.size).isEqualTo(5)
+        assertThat(racingGame.positions.size).isEqualTo(PLAY_COUNT)
 
-        for ((playStep, positions) in racingGame.positions.withIndex()) {
-            assertThat(positions).allSatisfy {
-                assertThat(it).isEqualTo(Position(position = playStep + 1))
-            }
+        assertThat(racingGame.positions.withIndex()).allSatisfy {
+            assertThat(it.value).containsExactly(*Position(position = it.index + 1) copy CAR_COUNT)
         }
+    }
+
+    private infix fun Position.copy(count: Int): Array<Position> = (0 until count)
+        .map { Position(position = this.int) }
+        .toTypedArray()
+
+    companion object {
+        private const val CAR_COUNT = 3
+        private const val PLAY_COUNT = 3
     }
 }
