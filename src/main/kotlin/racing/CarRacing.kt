e529001904d0dc1b06e7af384a14804b0e219379
SeoJinHyuk14 <suhjh14@gmail.com>
2021-03-09 00:34:53 +0900

    예외 케이스 추가

--- src/main/kotlin/racing/CarRacing.kt
+++ src/main/kotlin/racing/CarRacing.kt
@@ -30,6 +30,8 @@ class CarRacing(private val cars: List<Car>, private val tryCount: Int) {
 
     fun getWinner(): List<Car> {
         val maxPosition = cars.map { it.position }.max()
+        require(maxPosition != null) { "car name이 입력되었다면, maxPosition은 null이 될수 없습니다." }
+
         return cars.filter { it.position == maxPosition }
     }
 }
