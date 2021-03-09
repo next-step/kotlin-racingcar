939b817c6a21bcbdecffbc52599d30b196bdf9c3
kimsanggu <tkdrn8578@paymint.co.kr>
2021-03-09 16:11:20 +0900

    test: 자동차 생성 시 5글자에 대한 경계값 테스트 추가

--- src/test/kotlin/domain/racingcar/CarTest.kt
+++ src/test/kotlin/domain/racingcar/CarTest.kt
@@ -4,6 +4,8 @@ import fixture.CarFixture.KIM_CAR
 import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 internal class CarTest {
 
@@ -16,10 +18,19 @@ internal class CarTest {
         assertThat(car.position).isEqualTo(Position(1))
     }
 
-    @Test
-    fun `자동차 생성 시 이름이 5글자초과인 경우 예외처리한다`() {
+    @ParameterizedTest
+    @ValueSource(strings= ["Alistart", "fiive"])
+    fun `자동차 생성 시 이름이 5글자초과인 경우 예외처리한다`(name: String) {
         Assertions.assertThatExceptionOfType(IllegalArgumentException::class.java).isThrownBy {
-            Car("Alistart")
+            Car(name)
         }.withMessageMatching("이름은 5글자를 초과할 수 없습니다.")
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings= ["four", "two", "o"])
+    fun `자동차 생성 시 이름이 5글자이하인 경우 예외가 발생하지 않는다`(name: String) {
+        val car = Car(name)
+
+        assertThat(car).isNotNull
     }
 }
