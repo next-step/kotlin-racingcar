7c52aeb7de61e4f92b64a2ae4e78a320ac072117
sanggu <tkdrn8578@naver.com>
2021-03-10 21:24:43 +0900

    refactor: wildcard 제거

--- src/main/kotlin/view/ResultView.kt
+++ src/main/kotlin/view/ResultView.kt
@@ -1,7 +1,12 @@
 package view
 
 import controller.Reception
-import domain.racingcar.*
+import domain.racingcar.Cars
+import domain.racingcar.OverallResult
+import domain.racingcar.Records
+import domain.racingcar.Winners
+import domain.racingcar.Record
+import domain.racingcar.Position
 
 object ResultView {
     fun printInt() {
