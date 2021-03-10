5368134242ac028ecf1260e49559de7f12495717
sanggu <tkdrn8578@naver.com>
2021-03-10 21:23:28 +0900

    refactor: 랜덤 값을 만드는 부분을 RandomConditions 로 이동

--- src/main/kotlin/domain/number/Numbers.kt
+++ src/main/kotlin/domain/number/Numbers.kt
@@ -1,25 +1,9 @@
 package domain.number
 
-import java.util.stream.IntStream
-import kotlin.random.Random
-import kotlin.streams.toList
-
 object Numbers {
     private const val START_INDEX = 0
-    const val MIN_RANDOM_NUMBER = 1
-    const val MAX_RANDOM_NUMBER = 10
 
     fun toInt(value: String): Int {
         return value.toIntOrNull() ?: throw IllegalArgumentException("value는 숫자여야 합니다. value : $value")
-    }
-
-    fun makeRandomsToRacing(count: Int): List<Int> {
-        return IntStream.range(START_INDEX, count)
-            .map { Random.nextInt(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER) }
-            .toList()
-    }
-
-    fun randomToRacing(): Int {
-        return Random.nextInt(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER)
     }
 }
