c95738f469b66fd570c0e80deb0503d24f3d3cfd
이태훈(thxwelchs) <thxwelchs@gmail.com>
2021-03-10 18:25:30 +0900

    refactor(domain): Cars 가 스스로 상태를 바꿀 수 있도록 기능 추가
    피드백: https://github.com/next-step/kotlin-racingcar/pull/227#discussion_r590383482

--- src/main/kotlin/study/racingcar/domain/Cars.kt
+++ src/main/kotlin/study/racingcar/domain/Cars.kt
@@ -1,7 +1,13 @@
 package study.racingcar.domain
 
+import study.racingcar.util.RacingCarRandomGenerator
+
 class Cars(racedCars: List<Car>) {
     val racedCars: List<Car> = racedCars.map { Car(it.carName, it.moveCount) }
+
+    fun moveCars() {
+        racedCars.forEach { it.move(RacingCarRandomGenerator.random()) }
+    }
 
     override fun toString(): String {
         return racedCars.joinToString("\n")
