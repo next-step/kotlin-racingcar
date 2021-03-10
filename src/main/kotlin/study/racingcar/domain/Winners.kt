943d8d16716130cda713b4cad064583bc043ad1c
이태훈(thxwelchs) <thxwelchs@gmail.com>
2021-03-10 18:24:37 +0900

    refactor(domain): Winners winners maxBy의 entry가 null 일 때 예외 던지기
    피드백: https://github.com/next-step/kotlin-racingcar/pull/227#discussion_r590367840

--- src/main/kotlin/study/racingcar/domain/Winners.kt
+++ src/main/kotlin/study/racingcar/domain/Winners.kt
@@ -4,10 +4,10 @@ class Winners(
     racedCars: List<Car>,
     val winners: List<Car> = racedCars
         .groupBy { it.moveCount }
-        .maxBy { it.key }?.value!!
+        .maxBy { it.key }?.value ?: throw IllegalStateException()
 ) {
 
     override fun toString(): String {
-        return winners.joinToString(", "){ it.carName }
+        return winners.joinToString(", ") { it.carName }
     }
 }
