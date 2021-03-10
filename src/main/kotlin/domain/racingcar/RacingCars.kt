2e6c6c51abb5b14dcca178f9631dac7644ea40e7
sanggu <tkdrn8578@naver.com>
2021-03-10 21:12:34 +0900

    refactor: conditions를 미리 만드는 구조가 아닌 전략패턴으로 변경에 따른 조건 제거

--- src/main/kotlin/domain/racingcar/RacingCars.kt
+++ src/main/kotlin/domain/racingcar/RacingCars.kt
@@ -1,10 +1,6 @@
 package domain.racingcar
 
-class RacingCars(private var cars: Cars, val conditions: Conditions) {
-    init {
-        require(cars.getSize() == conditions.getSize()) { "전진 조건의 개수와 자동차의 개수가 같지 않습니다." }
-    }
-
+class RacingCars(private var cars: Cars, private val conditions: Conditions) {
     fun racing(attempts: Int): OverallResult {
         val allRound = mutableListOf<Records>()
 
