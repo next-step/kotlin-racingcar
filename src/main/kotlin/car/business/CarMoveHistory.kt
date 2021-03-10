e9119014b306b65bfc249527773276f62791ba9f
ohgillwhan <soora_genius@naver.com>
2021-03-11 00:55:19 +0900

    refact(CarMoveHistory) : 매직스트링(넘버?) 수정

--- src/main/kotlin/car/business/CarMoveHistory.kt
+++ src/main/kotlin/car/business/CarMoveHistory.kt
@@ -2,6 +2,10 @@ package car.business
 
 data class CarMoveHistory(val car: Car, val position: Int) {
     override fun toString(): String {
-        return "${car.name} : ${"-".repeat(position)}"
+        return "${car.name} : ${POSITION_EXPRESSION.repeat(position)}"
+    }
+
+    companion object {
+        private const val POSITION_EXPRESSION = "-"
     }
 }
