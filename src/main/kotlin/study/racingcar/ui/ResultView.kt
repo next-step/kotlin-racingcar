43bc0b76b3f6d728fbfc887ea7a5692bbff5f081
이태훈(thxwelchs) <thxwelchs@gmail.com>
2021-03-10 18:30:17 +0900

    refactor(ui): ResultView 한 회당 출력, 최종결과 출력 기능 분리
    피드백: https://github.com/next-step/kotlin-racingcar/pull/227#discussion_r590385258

--- src/main/kotlin/study/racingcar/ui/ResultView.kt
+++ src/main/kotlin/study/racingcar/ui/ResultView.kt
@@ -1,15 +1,22 @@
 package study.racingcar.ui
 
 import study.racingcar.domain.Cars
+import study.racingcar.domain.RaceResults
 import study.racingcar.domain.Winners
 
 object ResultView {
     private const val RESULT_COMMENT = "실행 결과"
     private const val WINNER_COMMENT = "가 최종우승했습니다."
 
-    fun doResult(raceResults: List<Cars>, winners: Winners) {
+    private fun doResult(resultCars: Cars) {
+        println("$resultCars\n")
+    }
+
+    fun doResults(raceResults: RaceResults) {
         println(RESULT_COMMENT)
-        raceResults.forEach { println("${it}\n") }
-        println("$winners$WINNER_COMMENT")
+        raceResults.raceResultCars.forEach {
+            doResult(it)
+        }
+        println("${raceResults.winners}$WINNER_COMMENT")
     }
 }
