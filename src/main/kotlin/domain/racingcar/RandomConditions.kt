871facb7383562c62c3da45a0316d4d80b7e466b
sanggu <tkdrn8578@naver.com>
2021-03-10 21:24:35 +0900

    refactor: Random condition 을 생성하는 메서드 추가 및 그 외 제거

--- src/main/kotlin/domain/racingcar/RandomConditions.kt
+++ src/main/kotlin/domain/racingcar/RandomConditions.kt
@@ -1,25 +1,14 @@
 package domain.racingcar
 
-import domain.number.Numbers
-import java.util.stream.IntStream
 import kotlin.random.Random
-import kotlin.streams.toList
 
-class RandomConditions(conditions: List<Int>, count: Int) : Conditions {
-    var conditions = conditions
-        private set
-
-    override fun makeConditionsToRacing(size: Int) {
-        conditions = IntStream.range(START_INDEX, size)
-            .map { Random.nextInt(Numbers.MIN_RANDOM_NUMBER, Numbers.MAX_RANDOM_NUMBER) }
-            .toList()
-    }
-
-    override fun getSize(): Int {
-        return conditions.size
+class RandomConditions : Conditions {
+    override fun makeCondition(): Int {
+        return Random.nextInt(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER)
     }
 
     companion object {
-        private const val START_INDEX = 0
+        private const val MIN_RANDOM_NUMBER = 1
+        private const val MAX_RANDOM_NUMBER = 10
     }
 }
