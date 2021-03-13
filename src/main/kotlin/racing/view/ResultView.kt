d20a8fc30ea621a716f08ea836462dea394d364f
SeoJinHyuk14 <suhjh14@gmail.com>
2021-03-14 02:27:31 +0900

    pair 대신 data 클래스 이용

--- src/main/kotlin/racing/view/ResultView.kt
+++ src/main/kotlin/racing/view/ResultView.kt
@@ -2,14 +2,14 @@ package racing.view
 
 fun printResult(recordsPerCarName: Map<String, List<Int>>, tryCount: Int) {
     repeat(tryCount) { index ->
-        printRecord(recordsPerCarName.map { Pair(it.key, it.value[index]) })
+        printRecord(recordsPerCarName.map { CarRecord(it.key, it.value[index]) })
         println()
     }
 }
 
-fun printRecord(recordPerCarName: List<Pair<String, Int>>) {
+fun printRecord(recordPerCarName: List<CarRecord>) {
     recordPerCarName.forEach {
-        println("${it.first} : ${"-".repeat(it.second)}")
+        println("${it.name} : ${"-".repeat(it.position)}")
     }
 }
 
