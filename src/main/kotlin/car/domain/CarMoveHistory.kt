005addad1d29fcb44539deea2e1d058d3efb2d3c
ohgillwhan <soora_genius@naver.com>
2021-03-16 00:12:15 +0900

    refact(CarMoveHistory) : 생성자 제거

--- src/main/kotlin/car/domain/CarMoveHistory.kt
+++ src/main/kotlin/car/domain/CarMoveHistory.kt
@@ -1,8 +1,6 @@
 package car.domain
 
 data class CarMoveHistory(val carName: CarName, val position: Int) {
-    constructor(carName: String, position: Int) : this(CarName(carName), position)
-
     override fun toString(): String {
         return "${carName.name} : ${POSITION_EXPRESSION.repeat(position)}"
     }
