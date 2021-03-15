ef4d3a94796c735459d664df315d9d92c44e6b8b
ohgillwhan <soora_genius@naver.com>
2021-03-15 23:49:55 +0900

    refact(CarMoveHistory) : isWinner 제거

--- src/main/kotlin/car/domain/CarMoveHistory.kt
+++ src/main/kotlin/car/domain/CarMoveHistory.kt
@@ -4,10 +4,6 @@ data class CarMoveHistory(val carName: CarName, val position: Int) {
 
     constructor(carName: String, position: Int) : this(CarName(carName), position)
 
-    fun isWinner(winnerPosition: Int): Boolean {
-        return position == winnerPosition
-    }
-
     override fun toString(): String {
         return "${carName.name} : ${POSITION_EXPRESSION.repeat(position)}"
     }
