0541739ad6b6ea1db27d596058dccd4501d0ee4d
ohgillwhan <soora_genius@naver.com>
2021-03-18 23:46:39 +0900

    refact(Winner) : carName.name -> carName.toString() 으로 변경

--- src/main/kotlin/car/domain/winner/Winner.kt
+++ src/main/kotlin/car/domain/winner/Winner.kt
@@ -6,6 +6,6 @@ data class Winner(val carName: CarName) {
     constructor(carName: String) : this(CarName(carName))
 
     override fun toString(): String {
-        return carName.name
+        return carName.toString()
     }
 }
