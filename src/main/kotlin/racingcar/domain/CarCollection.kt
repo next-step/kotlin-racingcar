c42eb4765d3aa66fc2f3894a5cada31a3fc364ac
mjung1798 <mor2222@naver.com>
2021-03-19 00:03:03 +0900

    refactor(review): history 객체 위치 이동

--- src/main/kotlin/racingcar/domain/CarCollection.kt
+++ src/main/kotlin/racingcar/domain/CarCollection.kt
@@ -1,19 +1,13 @@
 package racingcar.domain
 
-import racingcar.history.RacingResultHistory
-
 class CarCollection(userNames: List<String>) {
 
-    private val cars: List<Car> = userNames.map { Car(it) }
-    private val racingResultHistory = RacingResultHistory()
+    val cars: List<Car> = userNames.map { Car(it) }
 
-    fun tryMoveCars(round: Int, conditionNumbers: List<Int>) {
+    fun tryMoveCars(conditionNumbers: List<Int>) {
         cars.zip(conditionNumbers)
             .forEach { (car, number) -> car.tryMove(number) }
-        racingResultHistory.logging(round, cars = cars)
     }
-
-    fun getRacingHistories() = racingResultHistory.getResults()
 
     fun size(): Int = cars.size
 
--- src/test/kotlin/racingcar/domain/CarCollectionTest.kt
+++ src/test/kotlin/racingcar/domain/CarCollectionTest.kt
@@ -4,7 +4,6 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.DisplayName
-import racingcar.history.RacingActionHistory
 
 internal class CarCollectionTest {
 
@@ -12,24 +11,16 @@ internal class CarCollectionTest {
     @DisplayName("기준 숫자인 4이상의 값과 매칭되는 차는 이동한다")
     fun tryMoveCars() {
         val carCollection = CarCollection(listOf("mj", "java", "kt"))
-        carCollection.tryMoveCars(1, arrayListOf(0, 4, 8))
-
-        val racingHistory = carCollection.getRacingHistories()[0].racingActionHistories
-
-        assertThat(racingHistory).containsSequence(
-            RacingActionHistory(0, "mj"),
-            RacingActionHistory(1, "java"),
-            RacingActionHistory(1, "kt")
-        )
+        carCollection.tryMoveCars(arrayListOf(0, 4, 8))
+        assertThat(carCollection.cars.map { it.userName }).containsSequence("mj", "java", "kt")
     }
 
     @Test
     @DisplayName("자동차의 이동 위치를 이용해 승자를 구한다.")
     fun getWinner() {
         val carCollection = CarCollection(listOf("mj", "java", "kt"))
-        carCollection.tryMoveCars(1, arrayListOf(0, 10, 10))
-        carCollection.tryMoveCars(1, arrayListOf(0, 10, 10))
-
+        carCollection.tryMoveCars(arrayListOf(0, 10, 10))
+        carCollection.tryMoveCars(arrayListOf(0, 10, 10))
         assertThat(carCollection.getWinner().map { it.userName }).contains("java", "kt")
     }
 }
