cd9028eb43fc930883db0e6f0d1b2e391670cefe
ohgillwhan <soora_genius@naver.com>
2021-03-18 23:50:27 +0900

    refact(CarTest) :Assertions 함수 변경 및 개행

--- src/test/kotlin/car/domain/CarTest.kt
+++ src/test/kotlin/car/domain/CarTest.kt
@@ -3,17 +3,19 @@ package car.domain
 import car.domain.Car.Companion.INIT_POSITION
 import car.domain.move.AlwaysMovableStrategy
 import car.domain.move.AlwaysNotMovableStrategy
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class CarTest {
     @Test
     fun `moveable 이 false면은 자동차는 움직이면 안된다`() {
         // given
-        val car: Car = Car("오길환", AlwaysNotMovableStrategy())
+        val car = Car("오길환", AlwaysNotMovableStrategy())
+
         // when
         car.move()
+
         // then
         assertThat(car.currentPosition)
             .isEqualTo(INIT_POSITION)
@@ -22,9 +24,11 @@ class CarTest {
     @Test
     fun `moveable 이 true면은 자동차는 움직여도 된다`() {
         // given
-        val car: Car = Car("오길환", AlwaysMovableStrategy())
+        val car = Car("오길환", AlwaysMovableStrategy())
+
         // when
         car.move()
+
         // then
         assertThat(car.currentPosition)
             .isEqualTo(INIT_POSITION + 1)
@@ -32,7 +36,6 @@ class CarTest {
 
     @Test
     fun `이름이 5글자 초과이면 Exception이 발생해야 한다`() {
-        Assertions.assertThatIllegalArgumentException()
-            .isThrownBy { Car("5글자가넘는이룸", AlwaysMovableStrategy()) }
+        assertThrows<IllegalArgumentException> { Car("5글자가넘는이룸", AlwaysMovableStrategy()) }
     }
 }
