ad82edb44eb776b18065bb8278cb6f91a8bdbde9
ohgillwhan <soora_genius@naver.com>
2021-03-20 14:40:53 +0900

    refact(Car) : Primary 생성자에 위치 추가할 수 있도록 수정

--- src/main/kotlin/car/domain/Car.kt
+++ src/main/kotlin/car/domain/Car.kt
@@ -4,23 +4,30 @@ import car.domain.history.CarMoveHistory
 import car.domain.history.CarMoveHistoryCollection
 import car.domain.move.MovableStrategy
 
-class Car(val carName: CarName, private val movableStrategy: MovableStrategy) {
+class Car(val carName: CarName, position: Int = INIT_POSITION, private val movableStrategy: MovableStrategy) {
     private val _histories: MutableList<CarMoveHistory> = mutableListOf()
 
-    private var _position: Int = INIT_POSITION
+    var currentPosition: Int = position
+        private set
 
-    val currentPosition: Int
-        get() = _position
     val historyCollection: CarMoveHistoryCollection
         get() = CarMoveHistoryCollection(_histories)
 
-    constructor(carName: String, movableStrategy: MovableStrategy) : this(CarName(carName), movableStrategy)
+    constructor(
+        carName: String,
+        movableStrategy: MovableStrategy
+    ) : this(CarName(carName), INIT_POSITION, movableStrategy)
+
+    constructor(
+        carName: CarName,
+        movableStrategy: MovableStrategy
+    ) : this(carName, INIT_POSITION, movableStrategy)
 
     fun move() {
         if (movableStrategy.movable) {
-            _position++
+            currentPosition++
         }
-        _histories.add(CarMoveHistory(carName, _position))
+        _histories.add(CarMoveHistory(carName, currentPosition))
     }
 
     fun isWinner(winnerPosition: Int): Boolean {
