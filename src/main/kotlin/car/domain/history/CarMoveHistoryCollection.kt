7656ca422f69946deac829d706dfb3e8ee329aae
ohgillwhan <soora_genius@naver.com>
2021-03-20 18:44:04 +0900

    refact(CarMoveHistoryCollection) : 코드를 이쁘게 정렬했다.

--- src/main/kotlin/car/domain/history/CarMoveHistoryCollection.kt
+++ src/main/kotlin/car/domain/history/CarMoveHistoryCollection.kt
@@ -1,3 +1,5 @@
 package car.domain.history
 
-class CarMoveHistoryCollection(private val moveHistories: List<CarMoveHistory>) : List<CarMoveHistory> by moveHistories
+class CarMoveHistoryCollection(
+    private val moveHistories: List<CarMoveHistory>
+) : List<CarMoveHistory> by moveHistories
