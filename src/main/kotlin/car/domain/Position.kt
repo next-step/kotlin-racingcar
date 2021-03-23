595a446e5f84964d2ff7a8e1ad3c2d6f54f1bcb7
ohgillwhan <soora_genius@naver.com>
2021-03-23 23:41:07 +0900

    refact(Position) : 매직넘버 제거

--- src/main/kotlin/car/domain/Position.kt
+++ src/main/kotlin/car/domain/Position.kt
@@ -2,7 +2,7 @@ package car.domain
 
 data class Position(val value: Int = INIT_POSITION) : Comparable<Position> {
     init {
-        require(value >= 0) { "시작 위치는 $INIT_POSITION 보다 앞서 있어야 합니다." }
+        require(value >= INIT_POSITION) { "시작 위치는 $INIT_POSITION 보다 앞서 있어야 합니다." }
     }
 
     override fun compareTo(other: Position): Int = value.compareTo(other.value)
