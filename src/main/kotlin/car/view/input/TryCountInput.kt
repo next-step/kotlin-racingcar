1b6c5d7f9e838ed9eee07ee7501effe4564735f1
ohgillwhan <soora_genius@naver.com>
2021-03-23 22:08:22 +0900

    refact(TryCountInput) : 주 생성자 답게 변경

--- src/main/kotlin/car/view/input/TryCountInput.kt
+++ src/main/kotlin/car/view/input/TryCountInput.kt
@@ -1,15 +1,19 @@
 package car.view.input
 
-class TryCountInput(count: String?) {
-    val count: Int
+class TryCountInput(val count: Int) {
     init {
-        require(!count.isNullOrEmpty()) { "실행 횟수는 필수로 입력이 되어야 합니다. " }
-
-        this.count = count.toIntOrNull() ?: throw IllegalArgumentException("실행 횟수는 숫자여야 합니다.")
         require(this.count >= MINIMUM_TRY_COUNT) { "최소 실행 횟수는 $MINIMUM_TRY_COUNT 회 이상입니다." }
     }
 
+    constructor(count: String?) : this(count.toInt())
+
     companion object {
         const val MINIMUM_TRY_COUNT = 1
+
+        private fun String?.toInt(): Int {
+            require(!this.isNullOrEmpty()) { "실행 횟수는 필수로 입력이 되어야 합니다. " }
+
+            return toIntOrNull() ?: throw IllegalArgumentException("실행 횟수는 숫자여야 합니다.")
+        }
     }
 }
