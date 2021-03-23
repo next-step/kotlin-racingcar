07403d1f3eb15ef8531c6f4e294c5d56795e10b9
ohgillwhan <soora_genius@naver.com>
2021-03-23 21:09:27 +0900

    refact(InputView) : 가독성을 위하여 공백 추가

--- src/main/kotlin/car/view/InputView.kt
+++ src/main/kotlin/car/view/InputView.kt
@@ -9,10 +9,12 @@ class InputView {
         println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
         return CarNamesInput(readLine())
     }
+
     fun inputTryCount(): TryCountInput {
         println("시도할 횟수는 몇 회인가요?")
         return TryCountInput(readLine())
     }
+
     fun inputPrettyString(): PrettyPrinterInput {
         println("어떤 문자로 이쁘게 표기할까요?")
         return PrettyPrinterInput(readLine())
