386bed053ef44ec41fc121937085bacff0fc1a59
ohgillwhan <soora_genius@naver.com>
2021-03-23 22:16:03 +0900

    refact(CarName) : 변수 private로 변경

--- src/main/kotlin/car/domain/CarName.kt
+++ src/main/kotlin/car/domain/CarName.kt
@@ -1,6 +1,6 @@
 package car.domain
 
-data class CarName(val name: String) {
+data class CarName(private val name: String) {
     init {
         require(name.length <= MAXIMUM_NAME_LENGTH) { "이름의 길이는 $MAXIMUM_NAME_LENGTH 이하여야 합니다." }
     }
