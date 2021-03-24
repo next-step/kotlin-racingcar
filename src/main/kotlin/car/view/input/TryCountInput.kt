49370d957376355f488f3939d41d74571c259fc5
okk1119 <62978710+okk1119@users.noreply.github.com>
2021-03-24 14:32:17 +0900

    refact(TryCountInput) : 외부에 노출되지 않는 객체 private으로 변경 및 부생성자 notnull로 받도록 변경

--- src/main/kotlin/car/view/input/TryCountInput.kt
+++ src/main/kotlin/car/view/input/TryCountInput.kt
@@ -5,15 +5,9 @@ data class TryCountInput(val count: Int) {
         require(this.count >= MINIMUM_TRY_COUNT) { "최소 실행 횟수는 $MINIMUM_TRY_COUNT 회 이상입니다." }
     }
 
-    constructor(count: String?) : this(count.toInt())
+    constructor(count: String) : this(count.toInt())
 
     companion object {
-        const val MINIMUM_TRY_COUNT = 1
-
-        private fun String?.toInt(): Int {
-            require(!this.isNullOrEmpty()) { "실행 횟수는 필수로 입력이 되어야 합니다. " }
-
-            return this?.toIntOrNull() ?: throw IllegalArgumentException("실행 횟수는 숫자여야 합니다.")
-        }
+        private const val MINIMUM_TRY_COUNT = 1
     }
 }
