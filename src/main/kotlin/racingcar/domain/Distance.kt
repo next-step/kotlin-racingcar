0b50d2fd8f4b3a8cde1ee879dce745e1996bdbf9
hyunssooo <jhs695@gmail.com>
2021-03-28 04:34:59 +0900

    feat: Distance Comparable 구현

--- src/main/kotlin/racingcar/domain/Distance.kt
+++ src/main/kotlin/racingcar/domain/Distance.kt
@@ -1,6 +1,6 @@
 package racingcar.domain
 
-data class Distance(val value: Int = DEFAULT_VALUE) {
+data class Distance(val value: Int = DEFAULT_VALUE) : Comparable<Distance> {
     init {
         require(value >= DEFAULT_VALUE)
     }
@@ -8,6 +8,8 @@ data class Distance(val value: Int = DEFAULT_VALUE) {
     operator fun plus(distance: Distance): Distance {
         return copy(value = value + distance.value)
     }
+
+    override fun compareTo(other: Distance) = this.value.compareTo(other.value)
 
     companion object {
         private const val DEFAULT_VALUE = 0
