c6befa76453a95f3cc6436505511b878425a62a7
Jung-ChanYoung <38939015+MyaGya@users.noreply.github.com>
2021-05-19 17:03:00 +0900

    feat: Car 클래스가 setter 를 공개하지 않도록 변경

--- src/main/kotlin/domain/Car.kt
+++ src/main/kotlin/domain/Car.kt
@@ -2,7 +2,9 @@ package domain
 
 import exception.InvalidCarNameException
 
-class Car(val name: String, var distance: Int) {
+class Car(val name: String, distance: Int) {
+    var distance: Int = distance
+        private set
 
     constructor(name: String) : this(name, 0)
     constructor(name: String, distance: Int?) : this(name, 0)
