2908ce87196c54490246142236bf191a6e31c444
Jung-ChanYoung <38939015+MyaGya@users.noreply.github.com>
2021-05-19 14:58:41 +0900

    feat: 자동차 거리가 없는 경우, 자동차가 움직이는 경우

--- src/main/kotlin/domain/Car.kt
+++ src/main/kotlin/domain/Car.kt
@@ -2,22 +2,34 @@ package domain
 
 import exception.InvalidCarNameException
 
-class Car(name: String, distance: Int) {
-    private val name: String
-    private val distance: Int
+class Car(name: String, distance: Int?) {
+    val name: String
+    val distance: Int
+
+    constructor(name: String) : this(name, 0)
+
 
     init {
         validCarName(name)
         this.name = name
-        this.distance = distance
+        when (distance) {
+            null -> this.distance = 0
+            else -> this.distance = distance
+        }
     }
 
     private fun validCarName(name: String) {
         if (name.isEmpty()) {
             throw InvalidCarNameException("not allowed empty")
         }
-//        if (name.contains(" ")) {
-//            throw InvalidCarNameException("not allowed blank")
-//        }
+        if (name.contains(" ")) {
+            throw InvalidCarNameException("not allowed blank")
+        }
     }
+
+    fun move(): Car {
+        return Car(name, distance + 1)
+    }
+
+
 }
--- src/test/kotlin/CarTest.kt
+++ src/test/kotlin/CarTest.kt
@@ -1,5 +1,6 @@
 import domain.Car
 import exception.InvalidCarNameException
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -13,5 +14,37 @@ class CarTest {
         assertThrows(InvalidCarNameException::class.java) {
             Car("", 0)
         }
+    }
+
+    @DisplayName("자동차 이름이 없는 경우")
+    @Test
+    fun blankCarName() {
+        assertThrows(InvalidCarNameException::class.java) {
+            Car("", 0)
+        }
+    }
+
+    @DisplayName("자동차의 거리가 Null 인 경우")
+    @Test
+    fun noCarDistance() {
+        val car = Car("car1", null)
+
+        assertThat(car.distance).isEqualTo(0);
+    }
+
+    @DisplayName("자동차의 거리를 입력하지 않은 경우")
+    @Test
+    fun noCarDistanceParameter() {
+        val car = Car("car1")
+
+        assertThat(car.distance).isEqualTo(0);
+    }
+
+    @DisplayName("자동차가 움직이는 경우")
+    @Test
+    fun carMove() {
+        var car = Car("car1")
+        car = car.move();
+        assertThat(car.distance).isEqualTo(1);
     }
 }
