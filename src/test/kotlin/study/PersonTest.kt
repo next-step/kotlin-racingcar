061b1da00957c9fd540fdabe95886f3865329634
Nathan Kim <jinyeob07@gmail.com>
2021-10-12 22:57:36 +0900

    study: kotlin basic

---
+++ src/test/kotlin/study/PersonTest.kt
@@ -0,0 +1,42 @@
+package study
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class PersonTest {
+    @Test
+    fun `이름 붙은 인자`() {
+        val person1 = Person(name = "김진엽", age = 20, nickname = "네이슨")
+        val person2 = Person(age = 20, nickname = "네이슨", name = "김진엽")
+        assertThat(person1.name).isEqualTo(person2.name)
+        assertThat(person1.age).isEqualTo(person2.age)
+        assertThat(person1.nickname).isEqualTo(person2.nickname)
+    }
+
+    @Test
+    fun `널 타입`() {
+        val person = Person("김진엽", 20, null)
+        assertThat(person.nickname).isNull()
+    }
+
+    @Test
+    fun `기본 인자`() {
+        val person = Person("김진엽", 20)
+        assertThat(person.nickname).isEqualTo("김진엽")
+    }
+
+    @Test
+    fun `데이터 클래스`() {
+        val person1 = Person("김진엽", 20, "네이슨")
+        val person2 = Person("김진엽", 20, "네이슨")
+        assertThat(person1).isEqualTo(person2)
+    }
+
+    @Test
+    fun `복사`() {
+        val person1 = Person("김진엽", 20, "네이슨")
+        val person2 = person1.copy(nickname = null)
+        assertThat(person1.nickname).isEqualTo("네이슨")
+        assertThat(person2.nickname).isNull()
+    }
+}
