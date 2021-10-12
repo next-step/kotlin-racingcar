7a256b7e51ef9f110da9bcc9b1b71b48deec74b3
Pyro <rhwjddhks@gmail.com>
2021-10-12 22:53:58 +0900

    refactor: PersonTest 에 softAssertion 적용

--- src/test/kotlin/study/PersonTest.kt
+++ src/test/kotlin/study/PersonTest.kt
@@ -1,16 +1,26 @@
 package study
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.SoftAssertions
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class PersonTest {
+    private lateinit var softly: SoftAssertions
+
+    @BeforeEach
+    fun setUp() {
+        softly = SoftAssertions()
+    }
+
     @Test
     fun `이름 붙은 인자`() {
         val person1 = Person("고정완", 20, "Pyro")
         val person2 = Person(nickname = "Pyro", name = "고정완", age = 20)
-        assertThat(person1.name).isEqualTo(person2.name)
-        assertThat(person1.age).isEqualTo(person2.age)
-        assertThat(person1.nickname).isEqualTo(person2.nickname)
+        softly.assertThat(person1.name).isEqualTo(person2.name)
+        softly.assertThat(person1.age).isEqualTo(person2.age)
+        softly.assertThat(person1.nickname).isEqualTo(person2.nickname)
+        softly.assertAll()
     }
 
     @Test
@@ -22,9 +32,10 @@ class PersonTest {
     @Test
     fun `primary constructor`() {
         val person = Person("고정완", 20, "Pyro")
-        assertThat(person.name).isEqualTo("고정완")
-        assertThat(person.age).isEqualTo(20)
-        assertThat(person.nickname).isEqualTo("Pyro")
+        softly.assertThat(person.name).isEqualTo("고정완")
+        softly.assertThat(person.age).isEqualTo(20)
+        softly.assertThat(person.nickname).isEqualTo("Pyro")
+        softly.assertAll()
     }
 
     @Test
@@ -44,12 +55,13 @@ class PersonTest {
     fun `복사`() {
         val person1 = Person("고정완", 20, "Pyro")
         val person2 = person1.copy(nickname = null)
-        assertThat(person1.name).isEqualTo("고정완")
-        assertThat(person1.age).isEqualTo(20)
-        assertThat(person1.nickname).isEqualTo("Pyro")
+        softly.assertThat(person1.name).isEqualTo("고정완")
+        softly.assertThat(person1.age).isEqualTo(20)
+        softly.assertThat(person1.nickname).isEqualTo("Pyro")
 
-        assertThat(person2.name).isEqualTo("고정완")
-        assertThat(person2.age).isEqualTo(20)
-        assertThat(person2.nickname).isEqualTo(null)
+        softly.assertThat(person2.name).isEqualTo("고정완")
+        softly.assertThat(person2.age).isEqualTo(20)
+        softly.assertThat(person2.nickname).isEqualTo(null)
+        softly.assertAll()
     }
 }
