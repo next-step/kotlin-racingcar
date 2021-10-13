7b5365b52adbad7d865f234ccf685e4fb41dd7f0
bangjom <kbj7227@gmail.com>
2021-10-13 13:16:23 +0900

    study: kotlin basic

---
+++ src/test/kotlin/study/DataPerson.kt
@@ -0,0 +1,3 @@
+package study
+
+data class DataPerson(val name: String, val age: Int, var nickname: String)
---
+++ src/test/kotlin/study/DefaultNullPerson.kt
@@ -0,0 +1,3 @@
+package study
+
+class DefaultNullPerson(val name: String, var age: Int = 20, var nickname: String?)
