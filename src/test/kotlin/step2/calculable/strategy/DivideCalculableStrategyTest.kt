ac5b21ea4a9be20d171ee462743990f0b7e26eac
김우재 <kwj1270@naver.com>
2021-10-13 23:15:13 +0900

    test: DivideCalculableStrategy 인스턴스 생성 및 calculate() 기능 테스트 작성

---
+++ src/test/kotlin/step2/calculable/strategy/DivideCalculableStrategyTest.kt
@@ -0,0 +1,38 @@
+package step2.calculable.strategy
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertAll
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class DivideCalculableStrategyTest {
+
+    @DisplayName("DivideCalculableStrategy 인스턴스 생성 여부 테스트")
+    @Test
+    fun constructor_test() {
+        // given and when
+        val calculable = DivideCalculableStrategy()
+
+        // then
+        assertAll(
+            { assertThat(calculable).isNotNull },
+            { assertThat(calculable).isExactlyInstanceOf(DivideCalculableStrategy::class.java) },
+        )
+    }
+
+    @DisplayName("DivideCalculableStrategy 인스턴스 calculate() 기능 테스트")
+    @ParameterizedTest
+    @CsvSource(value = ["1:1:1", "-2:2:-1", "-1:-1:1"], delimiter = ':')
+    fun calculate_test(x: Int, y: Int, expected: Int) {
+        // given
+        val calculable = DivideCalculableStrategy()
+
+        // when
+        val actual = calculable.calculate(x, y)
+
+        // then
+        assertThat(actual).isEqualTo(expected)
+    }
+}
