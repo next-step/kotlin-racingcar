91ea82bf8d4d65e699efea7f082c2cb47818720c
김우재 <kwj1270@naver.com>
2021-10-13 22:55:34 +0900

    test: Operator calculate() 기능 테스트 작성

--- src/test/kotlin/step2/domain/OperatorTest.kt
+++ src/test/kotlin/step2/domain/OperatorTest.kt
@@ -10,7 +10,7 @@ class OperatorTest {
 
     @DisplayName("Operator values() 기능 테스트")
     @ParameterizedTest(name = "연산자 : {0}, 결과 enum : {1}")
-    @MethodSource("squares")
+    @MethodSource("calculateArgument")
     fun values_test(sign: String, operator: Operator) {
         // given and when
         val actual = Operator.values(sign)
@@ -19,14 +19,34 @@ class OperatorTest {
         assertThat(actual).isEqualTo(operator)
     }
 
+    @DisplayName("Operator calculate() 기능 테스트")
+    @ParameterizedTest(name = "x 피연산자 : {0}, y 피연산자 : {1}, 결과 : {2}, 연산 enum : {3}")
+    @MethodSource("operateArgument")
+    fun calculate_test(x: Int, y: String, expected: Int, operator: Operator) {
+        // given and when
+        val actual = operator.calculate(x, y)
+
+        // then
+        assertThat(actual).isEqualTo(expected)
+    }
+
     companion object {
         @JvmStatic
-        fun squares() = listOf(
+        fun calculateArgument() = listOf(
             Arguments.of("+", Operator.PLUS),
             Arguments.of("-", Operator.MINUS),
             Arguments.of("*", Operator.MULTIPLICATION),
             Arguments.of("/", Operator.DIVISION),
             Arguments.of("%", Operator.REMAINDER)
+        )
+
+        @JvmStatic
+        fun operateArgument() = listOf(
+            Arguments.of(1, "1", 2, Operator.PLUS),
+            Arguments.of(1, "1", 0, Operator.MINUS),
+            Arguments.of(1, "1", 1, Operator.MULTIPLICATION),
+            Arguments.of(1, "1", 1, Operator.DIVISION),
+            Arguments.of(1, "1", 0, Operator.REMAINDER)
         )
     }
 }
