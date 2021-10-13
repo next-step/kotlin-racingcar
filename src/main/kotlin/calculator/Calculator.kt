2c938ee0d857e4372472a77bd7679528fd9ef903
ninjasul <ninjasul@gmail.com>
2021-10-14 08:32:25 +0900

    temp: 임시코드 commit

---
+++ src/main/kotlin/calculator/Calculator.kt
@@ -0,0 +1,11 @@
+package calculator
+
+import java.util.Scanner
+
+class Calculator {
+    fun main() {
+        println("계산할 수식을 입력하세요")
+        val scanner = Scanner(System.`in`)
+        val inputLine = scanner.nextLine()
+    }
+}
