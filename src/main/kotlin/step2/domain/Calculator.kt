1e1384c565da072b5963e4b86f04ccc6093e664c
김우재 <kwj1270@naver.com>
2021-10-15 01:28:41 +0900

    refactor: while문을 컬렉션 메서드를 이용해서 구현

--- src/main/kotlin/step2/domain/Calculator.kt
+++ src/main/kotlin/step2/domain/Calculator.kt
@@ -11,6 +11,7 @@ class Calculator {
         require(!expression.isNullOrBlank()) { EXPRESSION_NULL_MESSAGE }
         val expressionWords = expression.split(DELIMITER)
         return expressionWords.subList(1, expressionWords.size)
+            .asSequence()
             .chunked(2)
             .map { Pair(Operator.values(it[0]), it[1]) }
             .fold(expressionWords[0].toInt()) { sum, monomial -> monomial.first.calculate(sum, monomial.second) }
