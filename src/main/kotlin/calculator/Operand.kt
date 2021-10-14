c22c9873de693c9286c4cc1ce6d7c581d4bd60fc
ninjasul <ninjasul@gmail.com>
2021-10-15 01:42:14 +0900

    style: Operand.kt 스타일 수정

--- src/main/kotlin/calculator/Operand.kt
+++ src/main/kotlin/calculator/Operand.kt
@@ -1,13 +1,12 @@
 package calculator
 
-import jdk.internal.joptsimple.internal.Strings
 import java.math.BigDecimal
 
 data class Operand(val value: BigDecimal) {
     companion object {
         private const val WRONG_OPERAND_INPUT = "잘못된 피연산자 입력입니다."
         fun from(input: String?): Operand {
-            require(!(Strings.isNullOrEmpty(input))) { WRONG_OPERAND_INPUT }
+            require(!input.isNullOrEmpty()) { WRONG_OPERAND_INPUT }
             return Operand(BigDecimal(input))
         }
     }
