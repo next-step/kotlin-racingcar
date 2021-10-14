26a546240421bdb66e37e53cdeedcad0bf439752
ninjasul <ninjasul@gmail.com>
2021-10-15 02:35:25 +0900

    feat: Calculator 구현, Operator.findOperation() 메소드 구현

--- src/main/kotlin/calculator/Operator.kt
+++ src/main/kotlin/calculator/Operator.kt
@@ -8,19 +8,19 @@ enum class Operator(val operatorSymbol: String) : Operation {
             return Operand(firstOperand.value + secondOperand.value)
         }
     },
-    
+
     SUBTRACT("-") {
         override fun perform(firstOperand: Operand, secondOperand: Operand): Operand {
             return Operand(firstOperand.value - secondOperand.value)
         }
     },
-    
+
     MULTIPLY("*") {
         override fun perform(firstOperand: Operand, secondOperand: Operand): Operand {
             return Operand(firstOperand.value * secondOperand.value)
         }
     },
-    
+
     DIVIDE("/") {
         override fun perform(firstOperand: Operand, secondOperand: Operand): Operand {
             if (secondOperand.value == BigDecimal.ZERO) {
@@ -31,8 +31,18 @@ enum class Operator(val operatorSymbol: String) : Operation {
         }
     },
     ;
-    
+
     companion object {
         const val CANNOT_BE_DIVIDED_BY_ZERO = "0으로 나눌 수 없습니다."
+        private const val WRONG_OPERATOR_SYMBOL = "잘못된 연산자 기호입니다."
+        fun findOperation(operatorSymbol: String): Operator {
+            return when (operatorSymbol) {
+                "+" -> ADD
+                "-" -> SUBTRACT
+                "*" -> MULTIPLY
+                "/" -> DIVIDE
+                else -> throw IllegalArgumentException(WRONG_OPERATOR_SYMBOL)
+            }
+        }
     }
 }
