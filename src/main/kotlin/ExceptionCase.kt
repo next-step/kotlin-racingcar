90f4daaa0c4bd30d70bbc5df964fe92f898c9f1e
thgus5614@naver.com <thgus5614@naver.com>
2021-10-14 19:22:04 +0900

    add: exception case에 대한 문구 추가

---
+++ src/main/kotlin/ExceptionCase.kt
@@ -0,0 +1,7 @@
+class ExceptionCase {
+    companion object {
+        const val CASE_INPUT_NULL_OR_EMPTY = "입력값이 없습니다."
+        const val CASE_INCORRECT_SYMBOL = "사측연산 기호가 아닙니다."
+        const val CASE_INCORRECT_INPUT = "계산할 수 없는 식입니다."
+    }
+}
