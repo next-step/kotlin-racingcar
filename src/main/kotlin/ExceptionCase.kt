5dea2c91d92c943e59fa9dfbf70fab891d69ef68
thgus5614@naver.com <thgus5614@naver.com>
2021-10-14 19:40:10 +0900

    add: 메인 클레스(문자열 입력받아 사측연산 후 결과 출력하는 메소드)

--- src/main/kotlin/ExceptionCase.kt
+++ src/main/kotlin/ExceptionCase.kt
@@ -1,5 +1,6 @@
 class ExceptionCase {
     companion object {
+        const val CASE_INT_FORMAT_OR_NULL = "숫자 포멧이 아니거나 null 값입니다."
         const val CASE_INPUT_NULL_OR_EMPTY = "입력값이 없습니다."
         const val CASE_INCORRECT_SYMBOL = "사측연산 기호가 아닙니다."
         const val CASE_INCORRECT_INPUT = "계산할 수 없는 식입니다."
