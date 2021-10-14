7ccee63ced21420c72c93000d0796de2010eeeda
flamme <flamme1004@gmail.com>
2021-10-14 20:24:18 +0900

    test: 입력값이 빈 공백 문자일 경우 테스트 케이스 작성

--- src/test/kotlin/stringArithmeticCalculator/CalculatorTest.kt
+++ src/test/kotlin/stringArithmeticCalculator/CalculatorTest.kt
@@ -1,11 +1,16 @@
 package stringArithmeticCalculator
 
-import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class CalculatorTest {
 
-    @Test
-    fun `테스트`() {
-        println("테스트")
+    @ParameterizedTest
+    @ValueSource(strings = ["", " "])
+    fun `입력값이 빈 공백 문자일 경우`(input : String) {
+        assertThatExceptionOfType(IllegalArgumentException::class.java)
+            .isThrownBy{ Calculator(input) }
+            .withMessage("공백 문자는 올 수 없습니다.")
     }
 }
