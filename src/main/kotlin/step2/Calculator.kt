a678b5edfda1f5085adb15e1f6115cad3e8a1f0d
bangjom <kbj7227@gmail.com>
2021-10-14 21:28:06 +0900

    add: 계산 기능을 담당하는 함수 추가

--- src/main/kotlin/step2/Calculator.kt
+++ src/main/kotlin/step2/Calculator.kt
@@ -6,8 +6,14 @@ class Calculator {
 
     private val parser = Parser()
 
-    fun calculate(input: String) {
+    fun calculate(input: String): Double {
         val dataQueue: Queue<String> = parser.parse(input)
         var sum: Double = dataQueue.poll().toDouble()
+        while (!dataQueue.isEmpty()) {
+            val operator = dataQueue.poll()
+            val operand = dataQueue.poll().toDouble()
+            sum = Operator.apply(value = operator, sum = sum, operand = operand)
+        }
+        return sum
     }
 }
--- src/test/kotlin/step2/CalculatorTest.kt
+++ src/test/kotlin/step2/CalculatorTest.kt
@@ -1,5 +1,15 @@
 package step2
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
 internal class CalculatorTest {
     private val calculator = Calculator()
+
+    @Test
+    fun `계산 정상 테스트`() {
+        assertThat(calculator.calculate("1 + 2 - 3")).isEqualTo(0.0)
+        assertThat(calculator.calculate("1 + 1 * 5")).isEqualTo(10.0)
+        assertThat(calculator.calculate("1 + 1 * 5 / 5")).isEqualTo(2.0)
+    }
 }
