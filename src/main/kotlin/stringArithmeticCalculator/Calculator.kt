715be417cf53e12baa2cf0ab0c3acb21aa5f1873
flamme <flamme1004@gmail.com>
2021-10-14 20:19:09 +0900

    feat: Calculator 값 생성 및 input값이 공백 문자일 경우 익셉션 처리

--- src/main/kotlin/stringArithmeticCalculator/Calculator.kt
+++ src/main/kotlin/stringArithmeticCalculator/Calculator.kt
@@ -1,4 +1,7 @@
 package stringArithmeticCalculator
 
-class Calculator {
+class Calculator(private val input : String) {
+    init {
+        if(input.isBlank()) throw IllegalArgumentException("공백 문자는 올 수 없습니다.")
+    }
 }
