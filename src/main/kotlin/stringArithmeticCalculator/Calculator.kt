aee47653c77dcd5ce200969450e21c07cb4c4237
flamme <flamme1004@gmail.com>
2021-10-14 20:39:59 +0900

    feat: 더하기 구현 및 테스트 케이스 구현

--- src/main/kotlin/stringArithmeticCalculator/Calculator.kt
+++ src/main/kotlin/stringArithmeticCalculator/Calculator.kt
@@ -4,4 +4,25 @@ class Calculator(private val input : String) {
     init {
         if(input.isBlank()) throw IllegalArgumentException("공백 문자는 올 수 없습니다.")
     }
+
+    var result = 0
+
+    fun result() : Int{
+        calculate()
+        return result
+    }
+
+    private fun calculate() {
+        val list = input.split(" ")
+        result = list[0].toInt()
+        for(i in 1 until list.size) {
+            operation(list, i)
+        }
+    }
+
+    private fun operation(list: List<String>, i: Int) {
+        if (list[i] == "+") {
+            result += list[i+1].toInt()
+        }
+    }
 }
--- src/test/kotlin/stringArithmeticCalculator/CalculatorTest.kt
+++ src/test/kotlin/stringArithmeticCalculator/CalculatorTest.kt
@@ -1,10 +1,17 @@
 package stringArithmeticCalculator
 
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatExceptionOfType
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class CalculatorTest {
+
+    @Test
+    fun `덧셈`() {
+        assertThat(Calculator("2 + 3").result()).isEqualTo(5)
+    }
 
     @ParameterizedTest
     @ValueSource(strings = ["", " "])
