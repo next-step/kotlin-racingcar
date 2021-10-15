cd56be4f987fbdced87a734e75e015f037f35515
etff <wish2rich@gmail.com>
2021-10-15 17:49:00 +0900

    feature: 자동차 목록을 생성한다

---
+++ src/main/kotlin/racingcar/domain/Cars.kt
@@ -0,0 +1,31 @@
+package racingcar.domain
+
+import java.util.stream.Collectors
+
+class Cars(private val cars: List<Car>) {
+    constructor() : this(mutableListOf<Car>())
+
+    fun move(): Cars {
+        val carsRecords: MutableList<Car> = ArrayList()
+        for (car in cars) {
+            val movedCar: Car = car.moveForward()
+            carsRecords.add(movedCar)
+        }
+        return Cars(carsRecords)
+    }
+
+    fun createCars(count: Int): Cars {
+        return Cars(
+            (0 until count)
+                .map { Car() }
+                .stream()
+                .collect(
+                    Collectors.toList()
+                )
+        )
+    }
+
+    fun size(): Int {
+        return cars.size
+    }
+}
