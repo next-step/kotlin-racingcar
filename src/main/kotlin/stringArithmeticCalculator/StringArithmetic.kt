b473f683774bb0261f5e1116a5f6d846e8f0b5f6
flamme <flamme1004@gmail.com>
2021-10-15 20:40:40 +0900

    refector: 0으로 나누는 경우 로직 수정

--- src/main/kotlin/stringArithmeticCalculator/StringArithmetic.kt
+++ src/main/kotlin/stringArithmeticCalculator/StringArithmetic.kt
@@ -4,7 +4,7 @@ enum class StringArithmetic(val arithmetic: String, val func: (Int, Int) -> Int)
     PLUS("+", { a, b -> a + b }),
     MINUS("-", { a, b -> a - b }),
     MULTIPLY("*", { a, b -> a * b }),
-    MODULUS("/", { a, b -> if (a == 0 || b == 0) throw ArithmeticException("0으로는 나눠지지가 않습니다.") else a / b });
+    MODULUS("/", { a, b -> if (b == 0) throw ArithmeticException("0으로는 나눠지지가 않습니다.") else a / b });
 
     fun calculate(currentNumber: Int, nextNumber: Int) = this.func(currentNumber, nextNumber)
 
