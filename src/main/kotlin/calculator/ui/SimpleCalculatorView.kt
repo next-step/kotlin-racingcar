523e1636071eda956fddbf0746c805e9d40ec05f
안운장 <hodol@woowahan.com>
2021-10-15 10:19:00 +0900

    refactor: Step 2 피드백 반영
    - 피연산자 호출 대신 생성시에 Null을 체크하도록 변경
    - 연산자의 계산 함수 네이밍 변경
    - Scanner.nextLine()을 Kotlin의 readLine()으로 교체

--- src/main/kotlin/calculator/ui/SimpleCalculatorView.kt
+++ src/main/kotlin/calculator/ui/SimpleCalculatorView.kt
@@ -1,17 +1,14 @@
 package calculator.ui
 
-import java.util.Scanner
 import kotlin.math.floor
 
 object SimpleCalculatorView : CalculatorView {
-    private val scanner: Scanner = Scanner(System.`in`)
-
     override fun sayHowToUse() = withDivider {
         println("안녕하세요. 계산기 프로그램을 실행합니다.")
         println("올바른 형태의 사친연산 식을 입력해주세요.")
     }
 
-    override fun input(): String? = scanner.nextLine()
+    override fun input(): String? = readLine()
 
     override fun sayAnswer(answer: Double) {
         when (floor(answer) == answer) {
