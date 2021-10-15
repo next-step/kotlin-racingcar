1ef6b3c1ad10146c69c467cfe492b35347979090
etff <wish2rich@gmail.com>
2021-10-15 16:09:58 +0900

    refactor: 자동차의 차번호를 제거한다

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -1,7 +1,6 @@
 package racingcar.domain
 
 class Car(
-    val carNumber: Int,
     val position: Int = 0,
     private val movingStrategy: MovingStrategy = RandomMoveStrategy()
 ) {
@@ -12,21 +11,8 @@ class Car(
 
     fun moveForward(): Car {
         if (movingStrategy.canMove()) {
-            return Car(carNumber, position + 1)
+            return Car(position + 1)
         }
         return this
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is Car) return false
-
-        if (carNumber != other.carNumber) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return carNumber
     }
 }
--- src/test/kotlin/racingcar/domain/CarTest.kt
+++ src/test/kotlin/racingcar/domain/CarTest.kt
@@ -2,38 +2,33 @@ package racingcar.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatCode
-import org.junit.jupiter.api.Assertions.assertAll
 import org.junit.jupiter.api.Test
 
 internal class CarTest {
 
     @Test
-    fun `자동차는 번호와 위치를 가지고 있다`() {
-        val givenNumber = 1
+    fun `자동차는 위치를 가지고 있다`() {
         val givenPosition = 2
-        val car = Car(givenNumber, givenPosition)
+        val car = Car(givenPosition)
 
-        assertAll(
-            { assertThat(car.carNumber).isEqualTo(givenNumber) },
-            { assertThat(car.position).isEqualTo(givenPosition) }
-        )
+        assertThat(car.position).isEqualTo(givenPosition)
     }
 
     @Test
     fun `자동차의 초기 위치는 0이다`() {
-        val car = Car(1)
+        val car = Car()
         assertThat(car.position).isEqualTo(0)
     }
 
     @Test
     fun `자동차의 위치가 0보다 작으면 예외를 던진다`() {
-        assertThatCode { Car(1, -1) }.isInstanceOf(IllegalArgumentException::class.java)
+        assertThatCode { Car(-1) }.isInstanceOf(IllegalArgumentException::class.java)
     }
 
     @Test
     fun `자동차를 앞으로 이동할 수 있다`() {
         // given
-        val car = Car(carNumber = 1, position = 0, FixMoveStrategy())
+        val car = Car(position = 0, FixMoveStrategy())
         // when
         val actual = car.moveForward()
         // then
