216c02548712cc0a4bdd403ff5cd7c5f96c6d6cb
LimHanGyeol <hang19663@gmail.com>
2021-10-16 00:10:55 +0900

    feat: 자동차 경주 횟수 입력 구현

--- src/main/kotlin/racingcar/RacingCarApplication.kt
+++ src/main/kotlin/racingcar/RacingCarApplication.kt
@@ -1,8 +1,10 @@
 package racingcar
 
+import racingcar.domain.Count
 import racingcar.domain.Number
 import racingcar.view.InputView
 
 fun main() {
     val number = Number(InputView.inputCars())
+    val count = Count(InputView.inputCounts())
 }
--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -7,5 +7,10 @@ class InputView {
             println("자동차 대수는 몇 대인가요?")
             return readLine()!!.toInt()
         }
+
+        fun inputCounts(): Int {
+            println("시도할 횟수는 몇 회인가요?")
+            return readLine()!!.toInt()
+        }
     }
 }
