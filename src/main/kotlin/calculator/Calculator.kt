96757209ec90ca577ed9d757125cadd210c18bf7
sanghun <tkdgnswjdgk4@gmail.com>
2021-10-16 00:28:10 +0900

    calculator fix: 네이밍 수정

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -3,14 +3,14 @@ package calculator
 object Calculator {
     fun calculate(input: String): String {
         require(input.isBlank()) { throw IllegalArgumentException(Message.INPUT_EMPTY) }
-        val list = InputParser.splitSpace(input)
-        var result = list[Constant.FIRST_ITEM].toDouble()
+        val dividedInput = InputParser.splitSpace(input)
+        var result = dividedInput[Constant.FIRST_ITEM].toDouble()
 
-        list.drop(0)
-            .forEachIndexed { index, s ->
+        dividedInput.drop(0)
+            .forEachIndexed { index, item ->
                 if (index % 2 != 0) {
-                    val operator = Operator.getOperatorEnum(s)
-                    result = operator.execute(result, list[index + 1].toDouble())
+                    val operator = Operator.getOperatorEnum(item)
+                    result = operator.execute(result, dividedInput[index + 1].toDouble())
                 }
             }
 
