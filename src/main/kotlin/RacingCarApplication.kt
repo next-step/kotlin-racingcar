6be3c929b23c9acd6c236cf9a747e5638f03f7a0
thgus5614@naver.com <thgus5614@naver.com>
2021-10-16 13:56:12 +0900

    add: 메인 메소드의 자동차 경주 시작과 결과 보여주는 기능 추가

--- src/main/kotlin/RacingCarApplication.kt
+++ src/main/kotlin/RacingCarApplication.kt
@@ -1,2 +1,15 @@
+import view.InputView
+import view.ResultView
+import view.ResultView.Companion.RESULT_TITLE
+
+/**
+ * 자동차 경주 게임의 메인 class
+ * */
 fun main() {
+    val carCount = InputView().requestNumberOfCar()
+    val tryCount = InputView().requestTryCount()
+    require(carCount > 0 && tryCount > 0) { "1이상의 값을 입력주새셔야 게임을 진행할 수 있습니다." }
+
+    ResultView().printResultTitle(RESULT_TITLE)
+    ResultView().printRace(carCount, tryCount)
 }
