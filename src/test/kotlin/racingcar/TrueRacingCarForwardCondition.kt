3fedfdd6ba6fda203eddaf699404cae7cef8d4c2
laco-dev <laco2951@gmail.com>
2021-10-16 13:43:19 +0900

    :white_check_mark: Cars 테스트 추가
    1. Cars는 최소 1대 이상으로 생성 가능
    2. Cars move 함수 기능 테스트

---
+++ src/test/kotlin/racingcar/TrueRacingCarForwardCondition.kt
@@ -0,0 +1,6 @@
+package racingcar
+
+class TrueRacingCarForwardCondition : RacingCarForwardCondition {
+
+    override fun isForward(): Boolean = true
+}
---
+++ src/test/kotlin/racingcar/model/CarsTest.kt
@@ -0,0 +1,39 @@
+package racingcar.model
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.function.Executable
+import racingcar.TrueRacingCarForwardCondition
+
+class CarsTest {
+
+    @Test
+    fun `Cars는 최소 1대 이상이어야 한다`() {
+        // given & when
+        val executable = Executable {
+            Cars(CarCount(-1), TrueRacingCarForwardCondition())
+        }
+
+        // then
+        assertThrows(IllegalArgumentException::class.java, executable)
+    }
+
+    @Test
+    fun `Cars move() 기능 테스트`() {
+        // given
+        val cars = Cars(
+            carCount = CarCount(3),
+            condition = TrueRacingCarForwardCondition()
+        )
+
+        // when
+        val expected = 5
+        repeat(5) { cars.move() }
+
+        // then
+        cars.forEach { car ->
+            assertEquals(expected, car.position)
+        }
+    }
+}
