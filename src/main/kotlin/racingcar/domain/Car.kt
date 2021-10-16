f70c828141d6698fa4377a5e2e76b5eba756b9db
Pyro <rhwjddhks@gmail.com>
2021-10-16 17:38:01 +0900

    feat: 각 자동차에 이름을 부여할 수 있다. 자동차 이름은 5자를 초과할 수 없다.

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -1,8 +1,15 @@
 package racingcar.domain
 
-class Car(val name: String = "Anonymous") {
-    var position = DEFAULT_POSITION
+class Car(val name: String = "anony", position: Int = DEFAULT_POSITION) {
+    var position = position
         private set
+
+    init {
+        require(!name.isNullOrBlank())
+        if (name.length > 5) {
+            throw IllegalArgumentException("자동차 이름은 5자를 초과할 수 없다.")
+        }
+    }
 
     fun move(number: Int) {
         if (number >= FORWARD_NUMBER) {
