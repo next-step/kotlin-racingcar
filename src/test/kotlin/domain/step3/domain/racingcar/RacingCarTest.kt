5f0dcd3d6c632442faabb2acc018315d2ac1f099
김우재 <kwj1270@naver.com>
2021-10-17 01:30:47 +0900

    test: RacingCar hashCode 및 equals 테스트 작성

--- src/test/kotlin/domain/step3/domain/racingcar/RacingCarTest.kt
+++ src/test/kotlin/domain/step3/domain/racingcar/RacingCarTest.kt
@@ -1,7 +1,9 @@
 package domain.step3.domain.racingcar
 
+import domain.step3.domain.strategy.MovingStrategy
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 
 class RacingCarTest {
 
@@ -10,7 +12,7 @@ class RacingCarTest {
         val racingCar = RacingCar { true }
         val movedRacingCar = racingCar.move()
 
-        assertThat(movedRacingCar.distance).isEqualTo(1);
+        assertThat(movedRacingCar.distance).isEqualTo(1)
     }
 
     @Test
@@ -18,6 +20,18 @@ class RacingCarTest {
         val racingCar = RacingCar { false }
         val movedRacingCar = racingCar.move()
 
-        assertThat(movedRacingCar.distance).isEqualTo(0);
+        assertThat(movedRacingCar.distance).isEqualTo(0)
+    }
+
+    @Test
+    fun `RacingCar hashCode 및 equals 테스트`() {
+        val movingStrategy = MovingStrategy { true }
+        val one = RacingCar(movingStrategy = movingStrategy)
+        val other = RacingCar(movingStrategy = movingStrategy)
+
+        assertAll(
+            { assertThat(one).hasSameHashCodeAs(other) },
+            { assertThat(one).isEqualTo(other) }
+        )
     }
 }
