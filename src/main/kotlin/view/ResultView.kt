7acf1e4e40f5068f5e56102f9d560dbe740caa75
thgus5614@naver.com <thgus5614@naver.com>
2021-10-16 13:55:26 +0900

    add: 자동차 경주 결과 보여주는 view와 필요한 메소드 구현

--- src/main/kotlin/view/ResultView.kt
+++ src/main/kotlin/view/ResultView.kt
@@ -1,6 +1,48 @@
 package view
 
+import Cars
+
 /**
  * 자동차 경주에서 게임 중 혹은 결과를 출력 모음 class
  * */
-class ResultView
+class ResultView {
+    companion object {
+        const val RESULT_TITLE = "실행 결과"
+    }
+
+    fun printResultTitle(title: String) {
+        println()
+        println(title)
+    }
+
+    private fun printStartRound(round: Int) {
+        println("ROUND: $round")
+    }
+
+    private fun printEndRound() {
+        println("")
+    }
+
+    private fun printMovement(id: Int, move: Int) {
+        val step = StringBuffer("$id: ")
+        for (i in 1..move) {
+            step.append("\uD83D\uDE80")
+        }
+        println(step)
+    }
+
+    /**
+     * 경주 시작 메소드
+     * */
+    fun printRace(carCount: Int, tryCount: Int) {
+        val list = Cars(carCount).createCars()
+        for (i in 1..tryCount) {
+            printStartRound(i)
+            list.forEachIndexed { index, car ->
+                car.move()
+                printMovement(index + 1, car.movement)
+            }
+            printEndRound()
+        }
+    }
+}
