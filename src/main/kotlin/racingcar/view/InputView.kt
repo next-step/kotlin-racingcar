96620437dc36fbafbc8f599fd7cd770bb0ca0678
thgus5614@naver.com <thgus5614@naver.com>
2021-10-16 16:06:11 +0900

    add: input 받는 값 포멧 exception 추가

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -1,16 +1,18 @@
 package racingcar.view
 
+import racingcar.exception.Exception.Companion.CASE_INPUT_DATA_NOT_INT
+
 /**
  * 자동차 경주에서 게임 시작시 사용자가 입력해야하는 질문 모음 class
  * */
 class InputView {
     fun requestNumberOfCar(): Int {
         println("자동차 대수는 몇 대인가요?")
-        return readLine()!!.toInt()
+        return readLine()!!.toIntOrNull() ?: throw IllegalArgumentException(CASE_INPUT_DATA_NOT_INT)
     }
 
     fun requestTryCount(): Int {
         println("시도할 횟수는 몇 회인가요?")
-        return readLine()!!.toInt()
+        return readLine()!!.toIntOrNull() ?: throw IllegalArgumentException(CASE_INPUT_DATA_NOT_INT)
     }
 }
