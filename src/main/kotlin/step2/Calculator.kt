5cb35fc81895e60af5846392686e9053974f8f1c
pursue503 <pursue503@naver.com>
2021-10-16 22:09:53 +0900

    Step2: Added 계산기 기능 구현

---
+++ src/main/kotlin/step2/Calculator.kt
@@ -0,0 +1,21 @@
+package step2
+
+enum class Calculator(private val operator: String, val expression: (firstNum: Double, secondNum: Double) -> Double) {
+
+    SUM("+", { firstNum, secondNum -> firstNum + secondNum }),
+    SUBTRACTION("-", { firstNum, secondNum -> firstNum - secondNum }),
+    MULTIPLY("*", { firstNum, secondNum -> firstNum * secondNum }),
+    DIVIDE("/", { firstNum, secondNum -> firstNum / secondNum });
+
+    companion object {
+        fun execute(paramOperator: String, firstNum: Double, secondNum: Double): Double {
+            return when (paramOperator) {
+                SUM.operator -> SUM.expression(firstNum, secondNum)
+                SUBTRACTION.operator -> SUBTRACTION.expression(firstNum, secondNum)
+                MULTIPLY.operator -> MULTIPLY.expression(firstNum, secondNum)
+                DIVIDE.operator -> DIVIDE.expression(firstNum, secondNum)
+                else -> throw IllegalArgumentException(Message.NOT_OPERATOR.message)
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/step2/Message.kt
@@ -0,0 +1,7 @@
+package step2
+
+enum class Message(val message: String) {
+
+    NULL_AND_EMPTY("입력값이 비어있거나 NULL 일수 없습니다."),
+    NOT_OPERATOR("올바르지 않은 연산자입니다.");
+}
