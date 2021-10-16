0baab992f1e81c21df3f99b4f75f36059a471e60
chaewon.na@deliveryhero.co.kr <chaewon.na@deliveryhero.co.kr>
2021-10-16 19:05:06 +0900

    refactor: Make popFirstTwo protected

--- src/main/kotlin/stringcalculator/operations/Operation.kt
+++ src/main/kotlin/stringcalculator/operations/Operation.kt
@@ -4,7 +4,7 @@ interface Operation {
     fun execute(deque: ArrayDeque<Int>)
 
     @Throws(NoSuchElementException::class)
-    fun popFirstTwo(deque: ArrayDeque<Int>): Pair<Int, Int> {
+    fun Operation.popFirstTwo(deque: ArrayDeque<Int>): Pair<Int, Int> {
         val arg1 = deque.removeFirst()
         val arg2 = deque.removeFirst()
         return Pair(arg1, arg2)
