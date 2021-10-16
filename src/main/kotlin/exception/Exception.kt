1ccd08f2eb970ac3ec0abfaea76d72262f2fdc28
thgus5614@naver.com <thgus5614@naver.com>
2021-10-16 15:10:12 +0900

    add: exception case 별 문구 추가

---
+++ src/main/kotlin/exception/Exception.kt
@@ -0,0 +1,9 @@
+package exception
+
+class Exception {
+    companion object {
+        const val CASE_WRONG_RANDOM_VALUE = "잘못된 랜덤값입니다."
+        const val CASE_INPUT_DATA_NOT_INT = "숫자만 입력해주셔야 합니다."
+        const val CASE_INPUT_DATA_WRONG = "1 이상의 값을 입력주새셔야 게임을 진행할 수 있습니다."
+    }
+}
