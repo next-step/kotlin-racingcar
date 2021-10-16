5702109daaf7a7848afc6317719d8459569531da
thgus5614@naver.com <thgus5614@naver.com>
2021-10-16 15:10:41 +0900

    add: 랜덤값 범위 수정 및 랜덤값 반환 메소드 분리

--- src/main/kotlin/model/Car.kt
+++ src/main/kotlin/model/Car.kt
@@ -1,14 +1,20 @@
 package model
 
+import exception.Exception.Companion.CASE_WRONG_RANDOM_VALUE
 import java.util.Random
 
 data class Car(
     var movement: Int = 0
 ) {
-    fun move() {
-        when (Random().nextInt(9)) {
+    fun random(): Int {
+        return Random().nextInt(9)
+    }
+
+    fun move(random: Int) {
+        when (random) {
             in 0..3 -> stop()
-            else -> forward()
+            in 4..9 -> forward()
+            else -> throw IllegalArgumentException(CASE_WRONG_RANDOM_VALUE)
         }
     }
 
