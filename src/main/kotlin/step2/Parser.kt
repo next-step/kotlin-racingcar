db3d4a2b66682d9d944b9e7be3d09ab60ff0e3d5
bangjom <kbj7227@gmail.com>
2021-10-16 20:57:59 +0900

    refactor: requireNotNull로 변경

--- src/main/kotlin/step2/Parser.kt
+++ src/main/kotlin/step2/Parser.kt
@@ -6,8 +6,8 @@ import java.util.Queue
 class Parser(val delimiter: String) {
 
     fun parse(input: String?): Queue<String> {
-        if (input.isNullOrBlank()) {
-            throw IllegalArgumentException("입력값이 null이거나 빈 공백 문자는 안됩니다.")
+        requireNotNull(input) {
+            "입력값이 null이거나 빈 공백 문자는 안됩니다."
         }
 
         val dataList = input.split(delimiter)
