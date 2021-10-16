fbf6323570e6e46cfb0cf06496cdc6911e44e4df
thgus5614@naver.com <thgus5614@naver.com>
2021-10-16 17:41:46 +0900

    improve: print와 race 메소드 분리 및 application의 로직 재생성

--- src/main/kotlin/racingcar/Cars.kt
+++ src/main/kotlin/racingcar/Cars.kt
@@ -1,11 +1,20 @@
 package racingcar
 
 import racingcar.model.Car
+import racingcar.model.RaceCondition
+import racingcar.model.move
 
 /**
  * 자동차들의 객체 관리
  * */
-class Cars(number: Int) {
-    private val _carList = (0 until number).map { Car() }.toList()
+class Cars(raceCondition: RaceCondition) {
+    private var _carList = (0 until raceCondition.carCount).map { Car() }.toList()
     val carList = _carList
+
+    fun race(): List<Car> {
+        _carList = _carList.map { car ->
+            car.move(car.random())
+        }
+        return _carList
+    }
 }
