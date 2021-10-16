338f0ab90409d58ba0dedc710d994317f377705c
김우재 <kwj1270@naver.com>
2021-10-16 21:20:46 +0900

    test: NumberOfAttempts 인스턴스 생성시 Int 변환 실패 테스트 작성

--- src/test/kotlin/domain/step3/domain/configuration/NumberOfAttemptsTest.kt
+++ src/test/kotlin/domain/step3/domain/configuration/NumberOfAttemptsTest.kt
@@ -15,4 +15,12 @@ class NumberOfAttemptsTest {
             .isExactlyInstanceOf(IllegalArgumentException::class.java)
             .hasMessage("NumberOfAttempts 가 허용하는 범위 밖의 숫자가 입력 되었습니다.")
     }
+
+    @DisplayName("NumberOfAttempts 인스턴스 생성시 Int 변환 실패 테스트")
+    @ParameterizedTest(name = "실패될 주입 값 : {0}")
+    @ValueSource(strings = ["일", "오", "영", "마이너스 일"])
+    fun constructor_format_fail_test(numberOfAttemptsString: String) {
+        Assertions.assertThatThrownBy { NumberOfAttempts(numberOfAttemptsString) }
+            .isExactlyInstanceOf(NumberFormatException::class.java)
+    }
 }
