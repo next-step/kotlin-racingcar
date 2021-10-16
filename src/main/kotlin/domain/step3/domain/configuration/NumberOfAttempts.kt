b5d16e230a068ce15b214e9ce7bf260743d5170c
김우재 <kwj1270@naver.com>
2021-10-16 21:19:23 +0900

    feat: NumberOfAttempts 인스턴스 생성시 범위 실패 예외처리 기능 구현

--- src/main/kotlin/domain/step3/domain/configuration/NumberOfAttempts.kt
+++ src/main/kotlin/domain/step3/domain/configuration/NumberOfAttempts.kt
@@ -1,5 +1,14 @@
 package domain.step3.domain.configuration
 
-class NumberOfAttempts {
+class NumberOfAttempts(val numberOfAttempts: Int) {
+    init {
+        require(numberOfAttempts >= MINIMUM) { OUT_OF_RANGE_MESSAGE }
+    }
 
+    constructor(numberOfAttempts: String) : this(numberOfAttempts.toInt())
+
+    companion object {
+        private const val MINIMUM = 0
+        private const val OUT_OF_RANGE_MESSAGE = "NumberOfAttempts 가 허용하는 범위 밖의 숫자가 입력 되었습니다."
+    }
 }
