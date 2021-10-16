9c123208b9f6e72dc124524af05ac250ecadda53
LimHanGyeol <hang19663@gmail.com>
2021-10-16 18:12:04 +0900

    refactor: list와 _cars 네이밍 변경

--- src/main/kotlin/racingcar/domain/Cars.kt
+++ src/main/kotlin/racingcar/domain/Cars.kt
@@ -9,19 +9,17 @@ class Cars(
         private const val START_NUMBER = 1
     }
 
-    private val _cars: List<Car> = IntRange(START_NUMBER, number.value)
+    val cars: List<Car> = IntRange(START_NUMBER, number.value)
         .map { Car(RacingRuleStrategy()) }
         .toList()
 
-    val cars = _cars
-
     fun race(): List<Car> {
-        val list = mutableListOf<Car>()
-        for (car in _cars) {
+        val raceResults = mutableListOf<Car>()
+        for (car in cars) {
             val speed = car.speedUp()
             car.drive(speed)
-            list.add(car)
+            raceResults.add(car)
         }
-        return list.toList()
+        return raceResults.toList()
     }
 }
