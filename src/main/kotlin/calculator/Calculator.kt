bddcb657c35b9c25af450214f9860232d0a941b5
ddaaac <skyblue300a@gmail.com>
2021-10-17 00:52:06 +0900

    기존 Calculator를 새로운 로직으로 대체 및 메인 함수 구현

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -2,34 +2,8 @@ package calculator
 
 object Calculator {
 
-    private val operatorRegex = Regex("(?<=[+\\-*/])|(?=[+\\-*/])")
-
     fun evaluate(expression: String): Double {
-        val tokens = expression.filter { !it.isWhitespace() }
-            .split(operatorRegex)
-
-        return evaluate(tokens)
-    }
-
-    private fun evaluate(tokens: List<String>): Double {
-        if (tokens.size == 1) {
-            return tokens[0].toDoubleOrNull() ?: throw IllegalArgumentException()
-        }
-
-        val left = tokens[0].toDoubleOrNull() ?: throw IllegalArgumentException()
-        val right = tokens[2].toDoubleOrNull() ?: throw IllegalArgumentException()
-        val result = evaluate(left, tokens[1], right)
-        val newTokens = listOf(result.toString()).plus(tokens.subList(3, tokens.size))
-        return evaluate(newTokens)
-    }
-
-    private fun evaluate(left: Double, operator: String, right: Double): Double {
-        return when (operator) {
-            "+" -> left + right
-            "-" -> left - right
-            "*" -> left * right
-            "/" -> left / right
-            else -> throw IllegalArgumentException()
-        }
+        val tokens = getTokens(expression)
+        return Parser(tokens).parse().evaluate()
     }
 }
