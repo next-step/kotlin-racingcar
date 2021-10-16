07d1b87262f69928985cda50a4c86d1a57804f00
thgus5614@naver.com <thgus5614@naver.com>
2021-10-16 17:40:53 +0900

    add: raceCondition 생성하는 inputRaceCondition() 메소드 추가

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -1,17 +1,26 @@
 package racingcar.view
 
+import racingcar.exception.Exception
 import racingcar.exception.Exception.Companion.CASE_INPUT_DATA_NOT_INT
+import racingcar.model.RaceCondition
 
 /**
  * 자동차 경주에서 게임 시작시 사용자가 입력해야하는 질문 모음 class
  * */
 class InputView {
-    fun requestNumberOfCar(): Int {
+    fun inputRaceCondition(): RaceCondition {
+        val carCount = requestNumberOfCar()
+        val tryCount = requestTryCount()
+        require(carCount > 0 && tryCount > 0) { Exception.CASE_INPUT_DATA_WRONG }
+        return RaceCondition(carCount, tryCount)
+    }
+
+    private fun requestNumberOfCar(): Int {
         println("자동차 대수는 몇 대인가요?")
         return readLine()!!.toIntOrNull() ?: throw IllegalArgumentException(CASE_INPUT_DATA_NOT_INT)
     }
 
-    fun requestTryCount(): Int {
+    private fun requestTryCount(): Int {
         println("시도할 횟수는 몇 회인가요?")
         return readLine()!!.toIntOrNull() ?: throw IllegalArgumentException(CASE_INPUT_DATA_NOT_INT)
     }
