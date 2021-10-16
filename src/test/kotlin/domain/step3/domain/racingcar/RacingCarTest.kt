2b17a9817fd14735150683f3f1ade7db423a6502
김우재 <kwj1270@naver.com>
2021-10-17 01:25:00 +0900

    test: RacingCar 조건 불만족시 주행거리 테스트 작성

--- src/test/kotlin/domain/step3/domain/racingcar/RacingCarTest.kt
+++ src/test/kotlin/domain/step3/domain/racingcar/RacingCarTest.kt
@@ -6,10 +6,18 @@ import org.junit.jupiter.api.Test
 class RacingCarTest {
 
     @Test
-    fun `RacingCar 조건 만족시 이동 테스트`() {
+    fun `RacingCar 조건 만족시 주행거리 테스트`() {
         val racingCar = RacingCar { true }
         val movedRacingCar = racingCar.move()
 
         assertThat(movedRacingCar.distance).isEqualTo(1);
+    }
+
+    @Test
+    fun `RacingCar 조건 불만족시 주행거리 테스트`() {
+        val racingCar = RacingCar { false }
+        val movedRacingCar = racingCar.move()
+
+        assertThat(movedRacingCar.distance).isEqualTo(0);
     }
 }
