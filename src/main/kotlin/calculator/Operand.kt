cbda69a03607486f0d6c989eb2b7910346472698
dev.prod(박동엽)/kakaopay <dev.prod@kakaopaycorp.com>
2021-10-16 15:52:56 +0900

    feat: Operand 생성 로직 수정

--- src/main/kotlin/calculator/Operand.kt
+++ src/main/kotlin/calculator/Operand.kt
@@ -6,8 +6,9 @@ data class Operand(val value: BigDecimal) {
     companion object {
         const val WRONG_OPERAND_INPUT = "잘못된 피연산자 입력입니다."
         fun from(input: String?): Operand {
-            require(!input.isNullOrEmpty()) { WRONG_OPERAND_INPUT }
-            return Operand(BigDecimal(input))
+            val inputOperandValue = input?.toBigDecimalOrNull()
+            require(inputOperandValue != null) { WRONG_OPERAND_INPUT }
+            return Operand(inputOperandValue)
         }
     }
 }
