7472f7bed0180d4cf08ba6305d25500def44d648
dev.prod(박동엽)/kakaopay <dev.prod@kakaopaycorp.com>
2021-10-17 00:17:56 +0900

    chore: NumbersOfCars, NumberOfTrials 메시지 수정

--- src/main/kotlin/racingcar/NumberOfCars.kt
+++ src/main/kotlin/racingcar/NumberOfCars.kt
@@ -4,7 +4,8 @@ data class NumberOfCars(val value: Int?) {
     companion object {
         private const val MINIMUM_NUMBER_OF_CARS = 1
         private const val MAXIMUM_NUMBER_OF_CARS = 9
-        private const val ENTERED_WRONG_NUMBER_OF_CARS = "잘못된 자동차 대수가 입력되었습니다. $MINIMUM_NUMBER_OF_CARS 이상 $MAXIMUM_NUMBER_OF_CARS 이하의 숫자를 입력하세요."
+        private const val ENTERED_WRONG_NUMBER_OF_CARS =
+            "잘못된 자동차 대수가 입력되었습니다. $MINIMUM_NUMBER_OF_CARS 이상 $MAXIMUM_NUMBER_OF_CARS 이하의 숫자를 입력하세요."
     }
 
     init {
--- src/main/kotlin/racingcar/NumberOfTrials.kt
+++ src/main/kotlin/racingcar/NumberOfTrials.kt
@@ -1,10 +1,11 @@
 package racingcar
 
-data class NumberOfTrials(val value: Int?) {
+data class NumberOfTrials(val value: Int? = 0) {
     companion object {
         private const val MINIMUM_NUMBER_OF_TRIALS = 1
         private const val MAXIMUM_NUMBER_OF_TRIALS = 20
-        private const val ENTERED_WRONG_NUMBER_OF_TRIALS = "잘못된 시도 횟수가 입력되었습니다. $MINIMUM_NUMBER_OF_TRIALS 이상 $MAXIMUM_NUMBER_OF_TRIALS 이하의 숫자를 입력하세요."
+        private const val ENTERED_WRONG_NUMBER_OF_TRIALS =
+            "잘못된 시도 횟수가 입력되었습니다. $MINIMUM_NUMBER_OF_TRIALS 이상 $MAXIMUM_NUMBER_OF_TRIALS 이하의 숫자를 입력하세요."
     }
 
     init {
