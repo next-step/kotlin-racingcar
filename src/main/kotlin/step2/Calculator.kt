a2b2ae8546e8b2866a4babfda7e76dfdd0445b83
seunghwan <slflfl12@naver.com>
2021-10-16 23:05:23 +0900

    Calculator에 객체지향 생활 체조원칙 else 예약어 쓰지 않기 적용

--- src/main/kotlin/step2/Calculator.kt
+++ src/main/kotlin/step2/Calculator.kt
@@ -1,35 +1,22 @@
 package step2
 
-import step2.Step2Exception.CANT_PLACE_NUMBER_EXCEPTION
 import step2.Step2Exception.FIRST_VALUE_INVALID_EXCEPTION
+import step2.Step2Exception.INVALID_INPUT_EXCEPTION
 import step2.Step2Exception.IS_NOT_NULL_OR_BLANK_EXCEPTION
-import step2.Step2Exception.STRING_NEXT_NOT_NUM_EXCEPTION
 
 object Calculator {
 
     fun calculate(value: String?): Int {
         require(!value.isNullOrBlank()) { IS_NOT_NULL_OR_BLANK_EXCEPTION }
+
         val valueSplitList = value.trim().split(" ")
+        require(valueSplitList.size % 2 != 0) { INVALID_INPUT_EXCEPTION }
 
         var result = valueSplitList[0].toIntOrNull() ?: throw NumberFormatException(FIRST_VALUE_INVALID_EXCEPTION)
-        for (i in 1 until valueSplitList.size - 1) {
-            if (checkStringNextNum(valueSplitList[i], valueSplitList[i + 1])) {
-                result = calculate(result, valueSplitList[i], valueSplitList[i + 1].toInt())
-            }
+        for (i in 1 until valueSplitList.size step 2) {
+            result = calculate(result, valueSplitList[i], valueSplitList[i + 1].toInt())
         }
         return result
-    }
-
-    private fun checkStringNextNum(maybeOperator: String, maybeIntValue: String): Boolean {
-        return if (maybeOperator.toIntOrNull() == null && maybeIntValue.toIntOrNull() != null) {
-            true
-        } else if (maybeOperator.toIntOrNull() == null && maybeIntValue.toIntOrNull() == null) {
-            throw IllegalArgumentException(STRING_NEXT_NOT_NUM_EXCEPTION)
-        } else if (maybeOperator.toIntOrNull() != null && maybeIntValue.toIntOrNull() != null) {
-            throw IllegalArgumentException(CANT_PLACE_NUMBER_EXCEPTION)
-        } else {
-            return false
-        }
     }
 
     private fun calculate(num1: Int, op: String, num2: Int): Int {
