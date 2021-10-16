cd19c81a131f34ee1b87bc55a9f294389a0f1259
김우재 <kwj1270@naver.com>
2021-10-16 22:53:44 +0900

    test: Distance 인스턴스 hashCode 및 equals 테스트 작성

--- src/test/kotlin/domain/step3/domain/racingcar/DistanceDrivenTest.kt
+++ src/test/kotlin/domain/step3/domain/racingcar/DistanceDrivenTest.kt
@@ -1,7 +1,9 @@
 package domain.step3.domain.racingcar
 
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -16,16 +18,16 @@ class DistanceDrivenTest {
             .hasMessage("DistanceDriven 가 허용하는 범위 밖의 숫자가 입력 되었습니다.")
     }
 
-    // @DisplayName("Distance 인스턴스 hashCode 및 equals 테스트")
-    // @ParameterizedTest(name = "주입 값 : {0}")
-    // @ValueSource(ints = [0, 1, 10, 100])
-    // fun hasCode_and_equals_test(distanceDrivenInt: Int) {
-    //     val one = DistanceDriven(distanceDrivenInt)
-    //     val other = DistanceDriven(distanceDrivenInt)
-    //
-    //     assertAll(
-    //         { assertThat(one).hasSameHashCodeAs(other) },
-    //         { assertThat(one).isEqualTo(other) },
-    //     )
-    // }
+    @DisplayName("Distance 인스턴스 hashCode 및 equals 테스트")
+    @ParameterizedTest(name = "주입 값 : {0}")
+    @ValueSource(ints = [0, 1, 10, 100])
+    fun hasCode_and_equals_test(distanceDrivenInt: Int) {
+        val one = DistanceDriven(distanceDrivenInt)
+        val other = DistanceDriven(distanceDrivenInt)
+
+        assertAll(
+            { assertThat(one).hasSameHashCodeAs(other) },
+            { assertThat(one).isEqualTo(other) },
+        )
+    }
 }
