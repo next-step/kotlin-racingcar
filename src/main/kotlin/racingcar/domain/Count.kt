bac0af87a4119eef7bd1896bba1e48e465cb951a
LimHanGyeol <hang19663@gmail.com>
2021-10-18 00:01:17 +0900

    style: companion object 위치 변경
    * Kotlin Convention 에서
    Companion object의 위치는 맨 밑에 위치하여 변경

--- src/main/kotlin/racingcar/domain/Count.kt
+++ src/main/kotlin/racingcar/domain/Count.kt
@@ -3,14 +3,14 @@ package racingcar.domain
 class Count(
     val value: Int,
 ) {
-    companion object {
-        private const val START_CONDITION = 1
-        private const val VALIDATION_MESSAGE = "자동차 경주 횟수는 1회 이상부터 시작할 수 있습니다."
-    }
-
     init {
         if (value < START_CONDITION) {
             throw IllegalArgumentException(VALIDATION_MESSAGE)
         }
+    }
+
+    companion object {
+        private const val START_CONDITION = 1
+        private const val VALIDATION_MESSAGE = "자동차 경주 횟수는 1회 이상부터 시작할 수 있습니다."
     }
 }
--- src/main/kotlin/racingcar/domain/strategy/RacingRuleStrategy.kt
+++ src/main/kotlin/racingcar/domain/strategy/RacingRuleStrategy.kt
@@ -3,15 +3,15 @@ package racingcar.domain.strategy
 import java.util.concurrent.ThreadLocalRandom
 
 class RacingRuleStrategy : DrivingRuleStrategy {
+    override fun canDrive(carSpeed: Int): Boolean {
+        return carSpeed >= DRIVING_CONDITION
+    }
+
     companion object {
         private const val DRIVING_CONDITION = 4
 
         fun speedUp(): Int {
             return ThreadLocalRandom.current().nextInt(0, 9)
         }
-    }
-
-    override fun canDrive(carSpeed: Int): Boolean {
-        return carSpeed >= DRIVING_CONDITION
     }
 }
