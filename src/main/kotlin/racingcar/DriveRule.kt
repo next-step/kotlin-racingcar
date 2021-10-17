d776efdc51ded265c862f65a4c4dfc92b6a723db
thgus5614@naver.com <thgus5614@naver.com>
2021-10-17 16:04:49 +0900

    improve: random max value 수정

--- src/main/kotlin/racingcar/DriveRule.kt
+++ src/main/kotlin/racingcar/DriveRule.kt
@@ -4,11 +4,12 @@ import kotlin.random.Random
 
 class DriveRule : DriveRangeStrategy {
     companion object {
+        private const val NUMBER_RANDOM_MAX = 9
         const val FORWARD_NUMBER = 4
     }
 
     fun random(): Int {
-        return Random.nextInt(9)
+        return Random.nextInt(NUMBER_RANDOM_MAX)
     }
 
     override fun isForward(): Boolean {
