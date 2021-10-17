c8034fb7c60640b5c897c188ecb1cd0e3616fcda
LimHanGyeol <hang19663@gmail.com>
2021-10-18 00:30:18 +0900

    feat: 레이싱 결과 출력 시 자동차 이름 출력 구현

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -1,6 +1,7 @@
 package racingcar.view
 
 import racingcar.domain.Car
+import racingcar.domain.Name
 import racingcar.domain.Position
 
 class OutputView {
@@ -12,8 +13,13 @@ class OutputView {
         fun printRaceResults(raceResults: List<Car>) {
             println()
             for (car in raceResults) {
+                printCarName(car.name)
                 printPosition(car.position)
             }
+        }
+
+        private fun printCarName(name: Name) {
+            print("${name.value}: ")
         }
 
         private fun printPosition(position: Position) {
