0a1803e016c5bd4e505decfe47765ab6c6ada2d5
ssibongee <ssibongee@gmail.com>
2021-10-17 21:32:30 +0900

    test: 연산자 기호 관련 테스트 코드 작성

--- src/test/kotlin/calculator/domain/OperatorTest.kt
+++ src/test/kotlin/calculator/domain/OperatorTest.kt
@@ -3,6 +3,7 @@ package calculator.domain
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
 
 class OperatorTest {
 
@@ -35,5 +36,18 @@ class OperatorTest {
     fun `0으로 나누는 경우 예외를 발생시킨다`(left: Double, right: Double) {
         assertThatExceptionOfType(IllegalArgumentException::class.java)
             .isThrownBy { Operator.DIVIDE.apply(left, right) }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["&", "$", "#", "!"])
+    fun `사칙연산이 아닌 기호는 연산자가 아니다`(symbol: String) {
+        assertThat(Operator.isOperator(symbol)).isFalse
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["&", "$", "#", "!"])
+    fun `사칙연산 기호가 아닌 문자열을 연산자로 변환할 경우 예외를 발생시킨다`(symbol: String) {
+        assertThatExceptionOfType(IllegalArgumentException::class.java)
+            .isThrownBy { Operator.of(symbol) }
     }
 }
