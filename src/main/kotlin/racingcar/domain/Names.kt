c4c28f7542fcd96f42ffeaebdbbcf8469f98ae51
etff <wish2rich@gmail.com>
2021-10-17 15:56:19 +0900

    refactor: 이름을 중복되지 않도록 변경한다

--- src/main/kotlin/racingcar/domain/Names.kt
+++ src/main/kotlin/racingcar/domain/Names.kt
@@ -2,14 +2,14 @@ package racingcar.domain
 
 import racingcar.util.StringUtils
 
-data class Names(val names: List<Name>) {
+data class Names(val names: Set<Name>) {
 
     companion object {
         private const val DELIMITER = ","
 
         fun generateNames(text: String): Names {
             val splitText = StringUtils.splitText(text, DELIMITER)
-            return Names(splitText.map { Name(it) })
+            return Names(splitText.map { Name(it) }.toSet())
         }
     }
 }
