3d508260ec86ca7ebe3caa7a3a1ff174509456c1
김우재 <kwj1270@naver.com>
2021-10-17 16:09:37 +0900

    refactor: 매직 리터럴 상수로 변환

--- src/main/kotlin/domain/step2/domain/Calculator.kt
+++ src/main/kotlin/domain/step2/domain/Calculator.kt
@@ -7,13 +7,17 @@ import domain.step2.strategy.split.SplitStrategy
 
 object Calculator {
 
-    // 쪼갠 상태를 넣을까 고민했으나, 문자열 그대로 넣는것이 실세계와 비슷하다 생각해서 래퍼 클래스로 넘겼습니다.
-    // object 로 싱글톤 클래스를 만들면 동반 객체를 만들지 못합니다 -> 매직 리터럴 관리를 따로 클래스로 관리해야할까요?
+    private const val SUB_LIST_START_INDEX = 1
+    private const val CHUNKED_SIZE = 2
+    private const val OPERATOR_INDEX = 0
+    private const val OPERAND_INDEX = 0
+    private const val FOLD_START_INDEX = 0
+
     fun calculate(expression: Expression, splitStrategy: SplitStrategy): Operand {
         val expressionWords = expression.split(splitStrategy)
-        return expressionWords.subList(1, expressionWords.size).asSequence()
-            .chunked(2)
-            .map { Pair(Operator.values(it[0]), Operand(it[1])) }
-            .fold(Operand(expressionWords[0])) { sum, monomial -> monomial.first.calculate(sum, monomial.second) }
+        return expressionWords.subList(SUB_LIST_START_INDEX, expressionWords.size).asSequence()
+            .chunked(CHUNKED_SIZE)
+            .map { Pair(Operator.values(it[OPERATOR_INDEX]), Operand(it[OPERAND_INDEX])) }
+            .fold(Operand(expressionWords[FOLD_START_INDEX])) { sum, monomial -> monomial.first.calculate(sum, monomial.second) }
     }
 }
