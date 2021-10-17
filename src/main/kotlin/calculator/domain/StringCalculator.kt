c456ba223b0c0cc153a2e63e85fed0c48b1b0df8
ssibongee <ssibongee@gmail.com>
2021-10-17 21:52:29 +0900

    feat: 문자열 계산기 구현

---
+++ src/main/kotlin/calculator/domain/StringCalculator.kt
@@ -0,0 +1,29 @@
+package calculator.domain
+
+import calculator.domain.Operator.Companion.isOperator
+
+class StringCalculator {
+
+    fun execute(input: String?): Double {
+        return calculate(Expression.validate(input))
+    }
+
+    private fun calculate(input: String): Double {
+        val (_operators, _operands) = input.split(" ")
+            .partition { isOperator(it) }
+
+        val operators = _operators.toOperators()
+        val operands = _operands.toOperands()
+
+        var result = operands.first().value()
+        for (index in 1 until operands.size) {
+            result = Expression(result.toOperand(), operands[index], operators[index - 1]).calculate()
+        }
+
+        return result
+    }
+
+    private fun List<String>.toOperators() = this.map { Operator.of(it) }
+    private fun List<String>.toOperands() = this.map { Operand(it) }
+    private fun Double.toOperand() = Operand(this.toString())
+}
