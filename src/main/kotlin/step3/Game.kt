5ca3ec28b87d3118697a8165833556ec5570c246
bangjom <kbj7227@gmail.com>
2021-10-19 03:47:27 +0900

    fix: 게임진행하는 클래스 추가

---
+++ src/main/kotlin/step3/Game.kt
@@ -0,0 +1,29 @@
+package step3
+
+class Game(
+    private val roundCount: Int,
+    private val carCount: Int,
+    private val canMoveForward: Random,
+    private val movingCondition: Int
+) {
+    companion object {
+        fun isPositive(value: Int) {
+            require(value < 0) { "양수만 가능합니다." }
+        }
+    }
+
+    init {
+        isPositive(roundCount)
+        isPositive(carCount)
+    }
+
+    fun start() {
+        val cars = List(carCount) { Car(canMoveForward, movingCondition) }
+
+        OutPutView.printStart()
+        for (i in 0..roundCount) {
+            cars.forEach(Car::moveForward)
+            OutPutView.printRound(cars)
+        }
+    }
+}
---
+++ src/test/kotlin/step3/GameTest.kt
@@ -0,0 +1,20 @@
+package step3
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class GameTest {
+    @Test
+    fun `차량 갯수가 0인 테스트`() {
+        Assertions.assertThatIllegalArgumentException()
+            .isThrownBy { Game(carCount = 0, roundCount = 1, canMoveForward = canMoveForward, movingCondition = 1) }
+            .withMessage("양수만 가능합니다.")
+    }
+
+    @Test
+    fun `라운드 횟수가 0인 테스트`() {
+        Assertions.assertThatIllegalArgumentException()
+            .isThrownBy { Game(carCount = 1, roundCount = 0, canMoveForward = canMoveForward, movingCondition = 1) }
+            .withMessage("양수만 가능합니다.")
+    }
+}
