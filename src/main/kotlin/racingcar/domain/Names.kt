4a81fb8745fdcb84dc1d31b8eb9943befcd097a5
LimHanGyeol <hang19663@gmail.com>
2021-10-18 23:36:19 +0900

    refactor: 디미터 법칙을 염두하여 메서드 리팩토링

--- src/main/kotlin/racingcar/domain/Names.kt
+++ src/main/kotlin/racingcar/domain/Names.kt
@@ -9,6 +9,12 @@ class Names(
         }
     }
 
+    fun getName(index: Int): Name {
+        return names[index]
+    }
+
+    fun size(): Int = names.size
+
     companion object {
         private const val START_CONDITION = 2
         private const val VALIDATION_MESSAGE = "자동차 경주는 두 대 이상부터 시작할 수 있습니다."
