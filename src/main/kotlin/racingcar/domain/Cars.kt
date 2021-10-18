3831c4c9bce4ac5fbf990fc07da6d15fa7a153ec
etff <wish2rich@gmail.com>
2021-10-18 16:02:40 +0900

    refactor: 메서드 레퍼런스로 변경

--- src/main/kotlin/racingcar/domain/Cars.kt
+++ src/main/kotlin/racingcar/domain/Cars.kt
@@ -7,7 +7,7 @@ data class Cars(val cars: List<Car>) {
     /**
      * 자동차를 이동하고 변경된 자동차 목록을 리턴한다.
      */
-    fun move(): Cars = Cars(cars.map { it.moveForward() })
+    fun move(): Cars = Cars(cars.map(Car::moveForward))
 
     fun size(): Int {
         return cars.size
@@ -34,6 +34,6 @@ data class Cars(val cars: List<Car>) {
         /**
          * 입력받은 대수만큼 자동차 목록을 생성하여 리턴한다.
          */
-        fun createCars(carNames: Names): Cars = Cars(carNames.names.map { Car(it) })
+        fun createCars(carNames: Names): Cars = Cars(carNames.names.map(::Car))
     }
 }
