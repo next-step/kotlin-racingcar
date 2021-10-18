f684b108cc087541e18fde40e6a73122005e4764
thgus5614@naver.com <thgus5614@naver.com>
2021-10-18 21:51:01 +0900

    add: Car 객체 생성 테스트 코드 추가

---
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -0,0 +1,19 @@
+package racingcar
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import racingcar.model.Car
+
+class CarTest {
+    @Test
+    @DisplayName("자동차 객체 생성 테스트")
+    fun `check car data class`() {
+        val item = Car(name = "사람")
+
+        Assertions.assertThat(item.name).isNotNull
+        Assertions.assertThat(item.name).isEqualTo("사람")
+        Assertions.assertThat(item.movement).isNotNull
+        Assertions.assertThat(item.movement).isEqualTo(0)
+    }
+}
