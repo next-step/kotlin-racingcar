6c40e41a1d1b6e4eaf3a13f557d01aa2213c980a
ssibongee <ssibongee@gmail.com>
2021-10-19 21:31:26 +0900

    feat: 위치(Position) 클래스 구현

---
+++ src/main/kotlin/racingcar/domain/Position.kt
@@ -0,0 +1,17 @@
+package racingcar.domain
+
+class Position(val value: Int = 0) {
+    init {
+        verify(value)
+    }
+
+    private fun verify(value: Int) {
+        require(value >= MIN_VALUE) { "위치는 1이하일 수 없습니다." }
+    }
+
+    fun move(): Position = Position(value + 1)
+
+    companion object {
+        private const val MIN_VALUE = 0
+    }
+}
