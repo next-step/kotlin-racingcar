1f3c52365afee496daca44a65a9587ead836dd74
ssibongee <ssibongee@gmail.com>
2021-10-19 10:00:19 +0900

    refactor: Step2 문자열 계산기 피드백 반영 - 테스트 코드에서 실행 로직 및 가독성을 위한 개행 - 불변 필드 공개 설정 및 불필요한 함수 제거 - 코틀린 코딩 컨벤션 스타일 적용 - 불필요한 생성자 제거 - 연산자가 단순히 null일 경우 예외를 던져주도록 변경

--- src/test/kotlin/calculator/domain/OperandTest.kt
+++ src/test/kotlin/calculator/domain/OperandTest.kt
@@ -24,6 +24,6 @@ class OperandTest {
     @ParameterizedTest
     @ValueSource(strings = ["0.0", "1.0", "100", "20"])
     fun `숫자는 피연산자가 될 수 있다`(value: String) {
-        assertThat(Operand(value).value()).isEqualTo(value.toDouble())
+        assertThat(Operand(value).value).isEqualTo(value.toDouble())
     }
 }
--- src/test/kotlin/calculator/domain/StringCalculatorTest.kt
+++ src/test/kotlin/calculator/domain/StringCalculatorTest.kt
@@ -7,7 +7,11 @@ import org.junit.jupiter.params.provider.CsvSource
 class StringCalculatorTest {
 
     @ParameterizedTest
-    @CsvSource("1 + 2, 3", "2 + 3 * 4 / 2, 10", "2 * 3 + 3 / 9, 1")
+    @CsvSource(
+        "1 + 2, 3",
+        "2 + 3 * 4 / 2, 10",
+        "2 * 3 + 3 / 9, 1"
+    )
     fun `사칙연산을 수행할 수 있다`(input: String?, result: Double) {
         Assertions.assertThat(StringCalculator().execute(input)).isEqualTo(result)
     }
