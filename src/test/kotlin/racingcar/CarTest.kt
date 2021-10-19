469405cc5204bdbc2a906686dfa274ad134cac1f
seunghwan <slflfl12@naver.com>
2021-10-19 23:52:24 +0900

    test (step3) : CarTest 추가

---
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -0,0 +1,31 @@
+package racingcar
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import racingcar.car.Car
+
+class CarTest {
+
+    private lateinit var car: Car
+
+    @BeforeEach
+    fun setUpCar() {
+        car = Car()
+    }
+
+    @ParameterizedTest(name = "Test {index}: value == {0} result == {1}")
+    @CsvSource(value = ["1, 1", "2, 2", "3, 3"])
+    fun `Car move() 후에 position 테스트`(move: Int, result: Int) {
+        car.move(move)
+        assertThat(car.getPosition()).isEqualTo(result)
+    }
+
+    @ParameterizedTest(name = "Test {index}: value == {0} result == {1}")
+    @CsvSource(value = ["1, -", "2, --", "3, ---"])
+    fun `Car move() 후에 toString() 테스트`(move: Int, result: String) {
+        car.move(move)
+        assertThat(car.toString()).isEqualTo(result)
+    }
+}
