51c9bd6e043ed5c6ad98e63644802c26e4285bad
thgus5614@naver.com <thgus5614@naver.com>
2021-10-19 22:55:14 +0900

    improve: 불필요한 코드 삭제

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -17,7 +17,7 @@ class InputView {
 
     private fun requestNameOfCars(): String {
         println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)")
-        return readLine()!!.toString()
+        return readLine()!!
     }
 
     private fun requestTryCount(): Int {
