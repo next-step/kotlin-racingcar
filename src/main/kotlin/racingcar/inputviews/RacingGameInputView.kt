4bb130dd16eb0876256f9f57d1fa282ae3aa3054
chaewon.na@deliveryhero.co.kr <chaewon.na@deliveryhero.co.kr>
2021-10-20 00:09:33 +0900

    feat: Add input validation to input view

--- src/main/kotlin/racingcar/inputviews/RacingGameInputView.kt
+++ src/main/kotlin/racingcar/inputviews/RacingGameInputView.kt
@@ -19,6 +19,15 @@ class RacingGameInputView : InputView {
     }
 
     private fun receiveInt(): Int {
-        return readLine()!!.toInt()
+        val intInput = readLine()!!.toInt()
+        validateInput(intInput)
+        return intInput
+    }
+
+    @Throws(IllegalArgumentException::class)
+    private fun validateInput(input: Int) {
+        if (input < 0) {
+            throw IllegalArgumentException("입력은 0 보다 큰 정수이어야 합니다.")
+        }
     }
 }
