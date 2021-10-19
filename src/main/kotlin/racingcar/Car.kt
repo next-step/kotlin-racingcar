34c4ac8813f889e0ee2c0196f4759bff05b5f202
chaewon.na@deliveryhero.co.kr <chaewon.na@deliveryhero.co.kr>
2021-10-19 23:51:04 +0900

    refactor: Refactor car class

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -4,8 +4,16 @@ class Car(val carId: Int, private val minFuelThreshold: Int = 4) {
     var currentPosition: Int = 0
 
     fun accelerate(fuel: Int) {
-        if (fuel > minFuelThreshold) {
-            currentPosition += 1
+        if (canAccelerate(fuel)) {
+            increaseCurrentPosition()
         }
+    }
+
+    private fun canAccelerate(fuel: Int): Boolean {
+        return fuel > minFuelThreshold
+    }
+
+    private fun increaseCurrentPosition() {
+        currentPosition += 1
     }
 }
