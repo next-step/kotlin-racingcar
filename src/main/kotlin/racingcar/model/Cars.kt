0d753a4a24af8436079523d841faf02997bda199
thgus5614@naver.com <thgus5614@naver.com>
2021-10-19 22:58:08 +0900

    improve: companion object 위치 수정

--- src/main/kotlin/racingcar/model/Cars.kt
+++ src/main/kotlin/racingcar/model/Cars.kt
@@ -4,17 +4,6 @@ package racingcar.model
  * 자동차들의 객체 관리
  * */
 class Cars private constructor(private var _carList: List<Car>) {
-    companion object {
-        private const val DELIMITER = ","
-        fun splitNameOfCars(names: String): List<String> =
-            names.split(DELIMITER)
-
-        fun createCars(raceCondition: RaceCondition): Cars {
-            val carList = splitNameOfCars(raceCondition.carsName).map { Car(name = it) }
-            return Cars(carList)
-        }
-    }
-
     val carList: List<Car>
         get() = this._carList
 
@@ -27,4 +16,15 @@ class Cars private constructor(private var _carList: List<Car>) {
 
     fun getWinner(): List<String> =
         _carList.filter { car -> car.movement == maxMovement }.mapNotNull { it.name }
+
+    companion object {
+        private const val DELIMITER = ","
+        fun splitNameOfCars(names: String): List<String> =
+            names.split(DELIMITER)
+
+        fun createCars(raceCondition: RaceCondition): Cars {
+            val carList = raceCondition.carsName.map { Car(name = it) }
+            return Cars(carList)
+        }
+    }
 }
