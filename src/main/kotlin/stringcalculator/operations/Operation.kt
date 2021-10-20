bae2e611d78ca79296369b24d821e0d394e971de
chaewon.na@deliveryhero.co.kr <chaewon.na@deliveryhero.co.kr>
2021-10-20 23:38:05 +0900

    refactor: Use Operational data class instead of Pair

--- src/main/kotlin/stringcalculator/operations/Operation.kt
+++ src/main/kotlin/stringcalculator/operations/Operation.kt
@@ -1,12 +1,14 @@
 package stringcalculator.operations
 
+data class Operational(val leftNum: Int, val rightNum: Int)
+
 interface Operation {
-    fun execute(deque: ArrayDeque<Int>)
+    fun execute(operationalNums: ArrayDeque<Int>)
 
     @Throws(NoSuchElementException::class)
-    fun Operation.popFirstTwo(deque: ArrayDeque<Int>): Pair<Int, Int> {
-        val arg1 = deque.removeFirst()
-        val arg2 = deque.removeFirst()
-        return Pair(arg1, arg2)
+    fun Operation.popFirstTwo(operationalNums: ArrayDeque<Int>): Operational {
+        val leftNum = operationalNums.removeFirst()
+        val rightNum = operationalNums.removeFirst()
+        return Operational(leftNum, rightNum)
     }
 }
