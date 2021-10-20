daa32fa399ba5ca7242ff0a01405b68a03b26d7b
김우재 <kwj1270@naver.com>
2021-10-20 19:49:42 +0900

    test: 길이가 5 초과인 이름인 들어오면 생성 불가능하다

--- src/main/kotlin/domain/step4/domain/racingcar/Name.kt
+++ src/main/kotlin/domain/step4/domain/racingcar/Name.kt
@@ -2,6 +2,7 @@ package domain.step4.domain.racingcar
 
 class Name(private val name: String) {
     init {
+        require(name.isNotBlank()) { "이름 문자열은 비어있서는 안됩니다." }
         require(name.length <= 5) { "이름의 길이는 5를 초과해서는 안 됩니다." }
     }
 }
--- src/test/kotlin/domain/step4/domain/racingcar/NameTest.kt
+++ src/test/kotlin/domain/step4/domain/racingcar/NameTest.kt
@@ -1,15 +1,16 @@
 package domain.step4.domain.racingcar
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
+import java.lang.IllegalArgumentException
 
 class NameTest {
 
     @ParameterizedTest(name = "이름 : {0}")
-    @ValueSource(strings = ["a", "ab", "abc", "abcd", "abcd"])
+    @ValueSource(strings = ["a", "ab", "abc", "abcd", "abcde"])
     fun `길이가 5이하인 이름이 들어오면 생성 가능하다`(nameString: String) {
         val name = Name(nameString)
 
@@ -19,9 +20,15 @@ class NameTest {
         )
     }
 
-    @Test
-    fun `길이가 5 초과인 이름인 들어오면 생성 가능하다`() {
-
+    @ParameterizedTest(name = "이름 : {0}")
+    @ValueSource(strings = ["abcdef", "abcdefghijkmnlopqrstuvwxyz"])
+    fun `길이가 5 초과인 이름인 들어오면 생성 불가능하다`(nameString: String) {
+        assertThatThrownBy { Name(nameString) }
+            .isExactlyInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("이름의 길이는 5를 초과해서는 안 됩니다.")
     }
+
+    // "이름 문자열은 비어있서는 안됩니다."
+
 
 }
