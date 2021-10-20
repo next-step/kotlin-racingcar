8a19679cb24251e43d062761e43b143435177983
sangyongchoi <csy7792@naver.com>
2021-10-21 02:55:38 +0900

    Step4: add winners print

--- src/main/kotlin/car/racing/view/ResultView.kt
+++ src/main/kotlin/car/racing/view/ResultView.kt
@@ -8,16 +8,41 @@ class ResultView {
         println("실행결과")
 
         repeat(turnCount) { turn ->
-            cars.forEach { car ->
-                val moveDistance = car
-                    .getMoveDistanceUntilTurn(turn)
-                    .filter { it }
-                    .joinToString(SEPARATOR) { "-" }
-
-                println("${car.name} : $moveDistance")
-            }
+            printEachTurn(cars, turn)
             println()
         }
+
+        val winners = getWinners(cars, turnCount).joinToString(",")
+        println("$winners 가 최종 우승했습니다.")
+    }
+
+    private fun printEachTurn(
+        cars: List<Car>,
+        turn: Int,
+    ) {
+        cars.forEach { car ->
+            val moveDistance = car
+                .getMoveDistanceUntilTurn(turn)
+                .filter { it }
+                .joinToString(SEPARATOR) { "-" }
+
+            println("${car.name} : $moveDistance")
+        }
+    }
+
+    private fun getWinners(
+        cars: List<Car>,
+        turnCount: Int,
+    ): List<String> {
+        val turn = turnCount - 1
+        val maxMoveDistance = cars
+            .map { car -> car.getMoveDistanceUntilTurn(turn).filter { it }.size }
+            .maxOrNull() ?: 0
+
+        return cars.filter { car ->
+            val moveDistance = car.getMoveDistanceUntilTurn(turn).filter { it }.size
+            moveDistance == maxMoveDistance
+        }.map { car -> car.name }
     }
 
     companion object {
