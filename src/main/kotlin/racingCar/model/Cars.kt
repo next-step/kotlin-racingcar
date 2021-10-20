814e5ee2aef5e4c72dca9fe68c859b46b886aa52
flamme <flamme1004@gmail.com>
2021-10-20 19:45:12 +0900

    refector : map을 이용하여 한번 사용 가능한 것이 아닌 반복으로 끝나는 함수이기 때문에 foreach로 변경

--- src/main/kotlin/racingCar/model/Cars.kt
+++ src/main/kotlin/racingCar/model/Cars.kt
@@ -5,7 +5,7 @@ class Cars(stringOfCars: List<String>) {
     private val list = stringOfCars.map { Car(it) }
 
     fun move(distanceOfDrive: () -> Int) {
-        list.map { moveCar(it, distanceOfDrive) }
+        list.forEach { moveCar(it, distanceOfDrive) }
     }
 
     private fun moveCar(car: Car, distanceOfDrive: () -> Int) = car.move(distanceOfDrive.invoke())
