6002aaf267bce5fd9a9c8c350a5e9c24bd0c8b6a
flamme <flamme1004@gmail.com>
2021-10-20 19:27:38 +0900

    fix : test Case를 BDD (given / when / then) 형식으로 변경

--- src/test/kotlin/racingCar/model/CarsTest.kt
+++ src/test/kotlin/racingCar/model/CarsTest.kt
@@ -2,20 +2,30 @@ package racingCar.model
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
 
 class CarsTest {
 
     @Test
     fun `자동차 수가 똑같은지 확인한다`() {
-        assertThat(Cars(listOf("flamm", "형준", "야호")).result().size).isEqualTo(3)
+        // given
+        val cars = Cars(listOf("flamm", "형준", "야호"))
+
+        // when
+        val sizeOfCars = cars.result().size
+
+        // then
+        assertThat(sizeOfCars).isEqualTo(3)
     }
 
     @Test
     fun `모든 차가 자동차 룰에 맞추어 움직이는지 테스트`() {
+        // given
         val cars = Cars(listOf("flamm", "형준", "야호"))
+
+        // when
         cars.move { 4 }
+
+        // then
         cars.result().forEach {
             assertThat(it.second).isEqualTo(1)
         }
@@ -23,16 +33,27 @@ class CarsTest {
 
     @Test
     fun `우승자가 중복되는지 테스트`() {
+        // given
         val cars = Cars(listOf("flamm", "형준", "야호"))
+
+        // when
         cars.move { 4 }
-        assertThat(cars.victoryPlayer().isNotEmpty())
+        val victoryPlayer = cars.victoryPlayer()
+
+        // then
+        assertThat(victoryPlayer.isNotEmpty())
     }
 
     @Test
     fun `우승자가 없으면 0명인지 테스트`() {
+        // given
         val cars = Cars(listOf("flamm", "형준", "야호"))
+
+        // when
         cars.move { 3 }
-        assertThat(cars.victoryPlayer().isEmpty())
+        val victoryPlayer = cars.victoryPlayer()
+
+        // then
+        assertThat(victoryPlayer.isEmpty())
     }
 }
-
