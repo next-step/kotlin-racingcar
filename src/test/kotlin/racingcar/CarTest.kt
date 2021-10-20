bb2cc531cc01af9c5c4e153da6d818c82d8c2a36
thgus5614@naver.com <thgus5614@naver.com>
2021-10-20 20:18:22 +0900

    improve: CarName 객체 생성에 따른 테스트 코드 수정

--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -4,15 +4,16 @@ import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import racingcar.model.Car
+import racingcar.model.CarName
 
 class CarTest {
     @Test
     @DisplayName("자동차 객체 생성 테스트")
     fun `check car data class`() {
-        val item = Car(name = "사람")
+        val item = Car(name = CarName("사람"))
 
-        Assertions.assertThat(item).isEqualTo(Car(name = "사람", movement = 0))
-        Assertions.assertThat(item.name).isEqualTo("사람")
+        Assertions.assertThat(item).isEqualTo(Car(name = CarName("사람"), movement = 0))
+        Assertions.assertThat(item.name.carName).isEqualTo("사람")
         Assertions.assertThat(item.movement).isEqualTo(0)
     }
 }
--- src/test/kotlin/racingcar/CarsTest.kt
+++ src/test/kotlin/racingcar/CarsTest.kt
@@ -6,16 +6,19 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 import racingcar.exception.Exception
-import racingcar.model.Car
+import racingcar.model.CarName
 import racingcar.model.Cars
+import racingcar.model.DriveRule
 import racingcar.model.RaceCondition
 import racingcar.view.InputView.Companion.splitNameOfCars
 
 class CarsTest {
+    private val rule = DriveRule()
+
     @Test
     @DisplayName("자동차 객체 생성 테스트")
     fun `check car list count`() {
-        val list = Cars.createCars(RaceCondition(listOf("아", "안", "앙"), 5)).carList
+        val list = Cars.createCars(RaceCondition(listOf(CarName("11"), CarName("22"), CarName("33")), 5), rule).carList
         Assertions.assertThat(list.size).isEqualTo(3)
         Assertions.assertThat(list.size).isNotNull
     }
@@ -23,9 +26,9 @@ class CarsTest {
     @Test
     @DisplayName("Cars 클래스 테스트; 객체 생성 테스트 및 움직임 테스트")
     fun `check Cars class`() {
-        val cars = Cars.createCars(RaceCondition(listOf("아", "안", "앙"), 5))
+        val cars = Cars.createCars(RaceCondition(listOf(CarName("사람"), CarName("사람3"), CarName("사람5")), 5), rule)
         Assertions.assertThat(cars.carList)
-            .isEqualTo(listOf(Car(name = "아"), Car(name = "하"), Car(name = "다")))
+            .isEqualTo(listOf(CarName("사람"), CarName("사람3"), CarName("사람5")))
         Assertions.assertThat(cars.carList).isNotNull
         cars.race()
         Assertions.assertThat(cars.carList[0].movement).isLessThan(5)
@@ -55,7 +58,7 @@ class CarsTest {
     @Test
     @DisplayName("우승자는 1명 이상일 수 있다.")
     fun `check number of race winner`() {
-        val cars = Cars.createCars(RaceCondition(listOf("바람", "구름", "달"), 3))
+        val cars = Cars.createCars(RaceCondition(listOf(CarName("바람"), CarName("구름"), CarName("사달")), 3), rule)
 
         repeat(5) {
             cars.race()
