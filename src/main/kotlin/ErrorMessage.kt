b2fff4eb98918933058939d2bb5a55c17cfc15ab
Soo <onemask@naver.com>
2021-10-20 09:44:09 +0900

    [Step2] 문자열 계산기 (#303)
    * add - CalculatorTest
    
    * add Operator
    
    * add Main
    
    * add Calculator
    
    * add TestCase 추가
    
    * feat 0으로 나누었을때 예외 케이스
    
    * rename excute to execute
    
    * Refactor IllegalMessage ErrorMessage 참조하도록 수정
    
    * Refactor - 불필요한 함수 제거
    
    * Refactor - symbol 체크하여 UnsupportedOperationException return 하도록 수정
    
    * Refactor- object 로 수정
    
    * Refactor - ReadLine
    
    * Refactor - 함수 호출 수정
    
    * Add getResult 함수 추가
    
    * Rename DECIMAL to BLANK
    
    * Refactor- @ParameterizedTest
    
    * Remove - 불필요한 Operator_REGEX 제거
    
    * Add hasOperator 함수 추가
    
    * Refactor - matches 가 아닌 Operator 클래스를 이용하는 방식으로 수정
    
    * Refactor - matches가 아닌 toIntOrNull 활용

---
+++ src/main/kotlin/ErrorMessage.kt
@@ -0,0 +1,6 @@
+object ErrorMessage {
+    const val INVALID_ERROR = "올바르지 않는 입력 값입니다."
+    const val NO_ARITHMETIC_OPERATION = "사칙 연산 기호가 아닙니다."
+    const val NO_DIVISION_ZERO = "0으로 나눌 수 없습니다."
+    const val UN_SUPPORT_OPERATION = "지원하지 않는 Operation"
+}
