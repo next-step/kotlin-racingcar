18180bec9a013317e05cbe2a4aae0f1926378ed8
ssibongee <ssibongee@gmail.com>
2021-10-20 11:22:56 +0900

    feat: 현황판(BulletinBoard) 클래스 구현

---
+++ src/main/kotlin/racingcar/domain/BulletinBoard.kt
@@ -0,0 +1,18 @@
+package racingcar.domain
+
+import racingcar.ui.LINE_FEED
+import java.lang.StringBuilder
+
+class BulletinBoard {
+    private val board: MutableMap<Lap, Records> = LinkedHashMap()
+
+    fun record(participants: RacingCars, lap: Lap) {
+        board[lap] = Records(participants)
+    }
+
+    fun display(): String {
+        val result = StringBuilder()
+        board.forEach { result.append(it.value.display()).append(LINE_FEED) }
+        return result.toString()
+    }
+}
