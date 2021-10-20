af94596aa2f42604c254825b0b925769999df2fa
ssibongee <ssibongee@gmail.com>
2021-10-20 11:14:57 +0900

    feat: 경주용 자동차 리스트에 대한 일급 컬렉션 클래스(RacingCars) 구현

---
+++ src/main/kotlin/racingcar/domain/RacingCars.kt
@@ -0,0 +1,24 @@
+package racingcar.domain
+
+import racingcar.domain.strategy.MovingStrategy
+
+@JvmInline
+value class RacingCars(val list: List<RacingCar>) {
+    init {
+        verify()
+    }
+
+    private fun verify() {
+        require(list.isNotEmpty()) {
+            "한 대 이상의 참가자(경주용 자동차)가 필요합니다."
+        }
+    }
+
+    fun race() = RacingCars(list.map { it.move() })
+
+    companion object {
+        fun of(cars: Int, strategy: MovingStrategy): RacingCars {
+            return RacingCars((1..cars).map { RacingCar(strategy) })
+        }
+    }
+}
