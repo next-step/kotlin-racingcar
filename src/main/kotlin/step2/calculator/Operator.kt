10d6a6261df575df373977d0d9c6e67a4b1ba643
jaenyeong <jaenyeong.dev@gmail.com>
2021-10-21 23:03:03 +0900

    feat(Operator at el): Add plus, minus, multi, divide feature, and plus test"

--- src/main/kotlin/step2/calculator/Operator.kt
+++ src/main/kotlin/step2/calculator/Operator.kt
@@ -1,14 +1,39 @@
 package step2.calculator
 
 import java.util.Arrays
+import java.util.function.IntBinaryOperator
 
 enum class Operator(
     private val operator: String,
+    private val expression: IntBinaryOperator
 ) {
-    ADDITION("+"),
-    SUBTRACTION("-"),
-    MULTIPLICATION("*"),
-    DIVISION("/");
+    ADDITION(
+        "+",
+        IntBinaryOperator { firstOperand: Int, secondOperand: Int ->
+            firstOperand + secondOperand
+        }
+    ),
+    SUBTRACTION(
+        "-",
+        IntBinaryOperator { firstOperand: Int, secondOperand: Int ->
+            firstOperand - secondOperand
+        }
+    ),
+    MULTIPLICATION(
+        "*",
+        IntBinaryOperator { firstOperand: Int, secondOperand: Int ->
+            firstOperand * secondOperand
+        }
+    ),
+    DIVISION(
+        "/",
+        IntBinaryOperator { firstOperand: Int, secondOperand: Int ->
+            firstOperand / secondOperand
+        }
+    );
+
+    @Throws(ArithmeticException::class)
+    fun calculate(firstOperand: Int, secondOperand: Int) = this.expression.applyAsInt(firstOperand, secondOperand)
 
     companion object {
         const val INVALID_OPERATOR_ERROR_MESSAGE = "Given operator is not valid"
--- src/test/kotlin/step2/calculator/OperatorTest.kt
+++ src/test/kotlin/step2/calculator/OperatorTest.kt
@@ -8,6 +8,7 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import step2.calculator.Operator.Companion.INVALID_OPERATOR_ERROR_MESSAGE
+
 import java.util.stream.Stream
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -28,6 +29,34 @@ internal class OperatorTest {
             Operator.getOperator(givenOperator)
         }.isInstanceOf(expectedException::class.java)
             .hasMessageContaining(INVALID_OPERATOR_ERROR_MESSAGE)
+    }
+
+    @DisplayName("주어진 피연산자가 올바른 경우 덧셈 테스트")
+    @ParameterizedTest
+    @MethodSource("validParameterForPlus")
+    fun plusWhenGivenValidOperand(firstOperand: Int, secondOperand: Int, expectedResultValue: Int) {
+        // Arrange
+        val plusOperator = Operator.getOperator("+")
+
+        // Act
+        val resultValue = plusOperator.calculate(firstOperand, secondOperand)
+
+        // Assert
+        assertThat(resultValue).isEqualTo(expectedResultValue)
+    }
+
+    @DisplayName("주어진 피연산자가 올바르지 않은 경우 덧셈 테스트")
+    @ParameterizedTest
+    @MethodSource("invalidParameterForPlus")
+    fun plusWhenGivenInvalidOperand(firstOperand: Int, secondOperand: Int, expectedResultValue: Long) {
+        // Arrange
+        val plusOperator = Operator.getOperator("+")
+
+        // Act
+        val resultValue = plusOperator.calculate(firstOperand, secondOperand).toLong()
+
+        // Assert
+        assertThat(resultValue).isNotEqualTo(expectedResultValue)
     }
 
     companion object {
@@ -53,13 +82,34 @@ internal class OperatorTest {
                 Arguments.of("%", illegalArgumentException),
                 Arguments.of("^", illegalArgumentException),
                 Arguments.of("&", illegalArgumentException),
-                Arguments.of("(", illegalArgumentException),
-                Arguments.of(")", illegalArgumentException),
                 Arguments.of("_", illegalArgumentException),
                 Arguments.of(".", illegalArgumentException),
                 Arguments.of("?", illegalArgumentException),
                 Arguments.of("0", illegalArgumentException),
                 Arguments.of("1", illegalArgumentException),
+            )
+        }
+
+        @JvmStatic
+        fun validParameterForPlus(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(1, 2, 3),
+                Arguments.of(10, 22, 32),
+                Arguments.of(-1, -2, -3),
+                Arguments.of(-3, 8, 5),
+                Arguments.of(0, 0, 0),
+                Arguments.of(100_000_000, 2_000_000, 102_000_000),
+                Arguments.of(-123_123_123, -123_123_123, -246_246_246),
+                Arguments.of(1_000_000_000, 1_000_000_000, 2_000_000_000),
+                Arguments.of(-1_000_000_000, -1_000_000_000, -2_000_000_000),
+            )
+        }
+
+        @JvmStatic
+        fun invalidParameterForPlus(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(2_000_000_000, 2_000_000_000, 4_000_000_000),
+                Arguments.of(-2_000_000_000, -2_000_000_000, -4_000_000_000),
             )
         }
     }
