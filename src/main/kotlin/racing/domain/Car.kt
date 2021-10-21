54194f9108498b26969562091bb3937ed84ebc7a
bangjom <kbj7227@gmail.com>
2021-10-21 22:54:27 +0900

    feat: 자동차 이름이 5글자 초과시 예외 추가

--- src/main/kotlin/racing/domain/Car.kt
+++ src/main/kotlin/racing/domain/Car.kt
@@ -5,6 +5,10 @@ const val START_POSITION = 0
 class Car(val name: String, private val pedal: Pedal) {
     var position = START_POSITION
 
+    init {
+        require(name.length <= 5) { "자동차 이름은 5자를 초과할 수 없습니다." }
+    }
+
     fun moveForward() {
         if (pedal.press()) {
             position++
--- src/test/kotlin/racing/CarTest.kt
+++ src/test/kotlin/racing/CarTest.kt
@@ -1,6 +1,8 @@
 package racing
 
+import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 import racing.domain.Car
@@ -15,5 +17,12 @@ class CarTest {
             car.moveForward()
         }
         assertThat(car.position).isEqualTo(input)
+    }
+
+    @Test
+    fun `자동차 이름이 5자 초과인 테스트`() {
+        Assertions.assertThatIllegalArgumentException()
+            .isThrownBy { Car(name = "안녕하세요. 반갑습니다", pedal = Pedal(0)) }
+            .withMessage("자동차 이름은 5자를 초과할 수 없습니다.")
     }
 }
