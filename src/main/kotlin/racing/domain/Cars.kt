ee5141db925dcda89792b6481d520ced10dc3f23
bangjom <kbj7227@gmail.com>
2021-10-22 21:02:07 +0900

    refactor: 코틀린 컨벤션 레이아웃으로 변경

--- src/main/kotlin/racing/domain/Cars.kt
+++ src/main/kotlin/racing/domain/Cars.kt
@@ -2,15 +2,6 @@ package racing.domain
 
 @JvmInline
 value class Cars private constructor(val cars: List<Car>) {
-    companion object {
-        fun make(carNames: List<String>, pedal: Pedal): Cars {
-            require(carNames.isNotEmpty()) {
-                "1개 이상 이름이 필요합니다."
-            }
-            return Cars(carNames.map { name -> Car(name = name, pedal = pedal) })
-        }
-    }
-
     fun goForward() {
         cars.forEach(Car::moveForward)
     }
@@ -19,5 +10,14 @@ value class Cars private constructor(val cars: List<Car>) {
         val max = cars.maxOf(Car::position)
         return cars.filter { car -> car.position == max }
             .toList()
+    }
+
+    companion object {
+        fun make(carNames: List<String>, pedal: Pedal): Cars {
+            require(carNames.isNotEmpty()) {
+                "1개 이상 이름이 필요합니다."
+            }
+            return Cars(carNames.map { name -> Car(name = name, pedal = pedal) })
+        }
     }
 }
