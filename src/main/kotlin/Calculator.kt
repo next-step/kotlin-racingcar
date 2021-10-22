dc5d119c6087560f476080b2a2ea9275d279a3f8
dreamforachase <chase.lee@dreamfora.com>
2021-10-22 20:59:15 +0900

    Feat: 유효성 검사 기능 추가

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -2,7 +2,23 @@ import jdk.jshell.spi.ExecutionControl
 
 class Calculator {
     fun calculate(input : String?) : Int{
+        if (input == null) throw IllegalArgumentException()
+        if (input.isBlank()) throw IllegalArgumentException()
+        val array = input.split(" ");
+        array.forEach { parameter ->
+            if (parameter.toIntOrNull() != null) return@forEach
+            if (isValidOperator(parameter)) return@forEach
+            throw IllegalArgumentException()
+        }
         throw ExecutionControl.NotImplementedException(Exception().stackTrace[0].methodName);
+    }
+
+    private fun isValidOperator(operation: String?) : Boolean{
+        if (operation == "+") return true
+        if (operation == "-") return true
+        if (operation == "*") return true
+        if (operation == "/") return true
+        return false
     }
 
     fun add(a: Int, b: Int) :Int = a + b
