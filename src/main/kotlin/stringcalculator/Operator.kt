0859defbd37ec1269c23dc2f590821ec26fb8d30
chaewon.na@deliveryhero.co.kr <chaewon.na@deliveryhero.co.kr>
2021-10-23 01:24:57 +0900

    refactor: Change parameter name

--- src/main/kotlin/stringcalculator/Operator.kt
+++ src/main/kotlin/stringcalculator/Operator.kt
@@ -1,14 +1,14 @@
 package stringcalculator
 
-enum class Operator(val operatorString: String, val execute: (operational: Operational) -> Int) {
+enum class Operator(val value: String, val execute: (operational: Operational) -> Int) {
     ADD("+", { operational: Operational -> operational.leftNum + operational.rightNum }),
     SUBTRACT("-", { operational: Operational -> operational.leftNum - operational.rightNum }),
     MULTIPLY("*", { operational: Operational -> operational.leftNum * operational.rightNum }),
     DIVIDE("/", { operational: Operational -> operational.leftNum / operational.rightNum });
 
     companion object {
-        fun fromOperatorString(operatorString: String): Operator {
-            return values().first { it.operatorString == operatorString }
+        fun fromOperatorString(value: String): Operator {
+            return values().first { it.value == value }
         }
     }
 }
