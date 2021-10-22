e6b5fe94ac77ce55d4e568423ba3c38a7bea2e8d
ssibongee <ssibongee@gmail.com>
2021-10-22 20:32:46 +0900

    refactor: 입출력 형식 및 요구사항 변경에 따른 RacingCarView 클래스 수정 - 자동차의 대수 대신 이름을 입력 받도록 변경 - 결과를 출력할 때 우승자를 같이 출력하도록 변경

--- src/main/kotlin/racingcar/ui/RacingCarView.kt
+++ src/main/kotlin/racingcar/ui/RacingCarView.kt
@@ -3,18 +3,26 @@ package racingcar.ui
 import racingcar.domain.BulletinBoard
 
 const val LINE_FEED = "\n"
+const val DELIMITER = ","
 
 class RacingCarView {
-    fun cars() = input("자동차 대수는 몇 대인가요?")
+    fun names() = nameInput("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
 
-    fun laps() = input("시도할 횟수는 몇 회인가요?")
+    fun laps() = lapInput("시도할 횟수는 몇 회인가요?")
 
-    fun output(laps: Int, result: BulletinBoard) {
+    fun output(result: BulletinBoard) {
         println("실행 결과")
         println(result.display())
+        println("${result.winners.display()}가 최종 우승했습니다.")
     }
 
-    private fun verify(input: String?): Int {
+    private fun verifyNameInput(input: String?): List<String> {
+        require(!input.isNullOrBlank()) { "null 또는 빈 문자열을 입력으로 사용할 수 없습니다." }
+        return input.split(DELIMITER)
+
+    }
+
+    private fun verifyLapInput(input: String?): Int {
         require(!input.isNullOrBlank()) { "null 또는 빈 문자열을 입력으로 사용할 수 없습니다." }
         require(input.isInteger()) { "숫자만 입력값으로 사용할 수 있습니다. " }
         return input.toInt()
@@ -25,8 +33,13 @@ class RacingCarView {
         else -> true
     }
 
-    private fun input(message: String): Int {
+    private fun nameInput(message: String): List<String> {
         println(message)
-        return verify(readLine())
+        return verifyNameInput(readLine())
+    }
+
+    private fun lapInput(message: String): Int {
+        println(message)
+        return verifyLapInput(readLine())
     }
 }
