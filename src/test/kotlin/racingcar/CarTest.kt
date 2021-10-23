70d391afa6b56a3019602d01084da41dfb984dc4
seunghwan <slflfl12@naver.com>
2021-10-23 14:11:54 +0900

    test, refactor (step3) : CarTest 수정 (DI 메소드 주입으로 변경으로 인해)

--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -2,9 +2,11 @@ package racingcar
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import racingcar.car.Car
+import racingcar.engine.CarEngine
 
 class CarTest {
 
@@ -19,17 +21,24 @@ class CarTest {
     @CsvSource(value = ["1, 1", "2, 2", "3, 3"])
     fun `Car move() 후에 position 테스트`(move: Int, expected: Int) {
         repeat(move) {
-            car.move()
+            car.movePosition(object : CarEngine { override fun execute(): Boolean = true })
         }
         assertThat(car.getPosition()).isEqualTo(expected)
     }
 
-    @ParameterizedTest(name = "Test {index}: value == {0} result == {1}")
-    @CsvSource(value = ["1, -", "2, --", "3, ---"])
-    fun `Car move() 후에 toString() 테스트`(move: Int, expected: String) {
-        repeat(move) {
-            car.move()
-        }
-        assertThat(car.toString()).isEqualTo(expected)
+    @Test
+    fun `Car 움직임 테스트`() {
+        car.movePosition(object : CarEngine {
+            override fun execute(): Boolean = true
+        })
+        assertThat(car.getPosition()).isEqualTo(1)
+    }
+
+    @Test
+    fun `Car 움직이지 않음 테스트`() {
+        car.movePosition(object : CarEngine {
+            override fun execute(): Boolean = false
+        })
+        assertThat(car.getPosition()).isEqualTo(0)
     }
 }
