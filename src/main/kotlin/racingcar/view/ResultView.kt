6d2bd1dfa35a18b9a1cd3449bccb123183d35d2c
seunghwan <slflfl12@naver.com>
2021-10-23 14:23:28 +0900

    refactor (step3): ResultView 수정(car의 toString 변경으로 인해 position 받아와서 "_" print 해주기)

--- src/main/kotlin/racingcar/view/ResultView.kt
+++ src/main/kotlin/racingcar/view/ResultView.kt
@@ -4,9 +4,20 @@ import racingcar.car.Car
 
 object ResultView {
 
+    private const val RACING_RESULT = "실행 결과"
+
+    fun printResult() {
+        println()
+        println(RACING_RESULT)
+        println()
+    }
+
     fun printCarPositions(cars: List<Car>) {
         cars.forEach { car ->
-            println(car)
+            repeat(car.getPosition()) {
+                print("-")
+            }
+            println()
         }
         println()
     }
