25c0eb612588863f9bb3303127f1203c709dc775
seunghwan <slflfl12@naver.com>
2021-10-23 14:24:47 +0900

    refactor (step3): 코드 변경으로 인해 RacingApplication 수정 (Factory에서 Car List 생성 후 Manager에게 전달, 결과 출력)

--- src/main/kotlin/racingcar/RacingApplication.kt
+++ src/main/kotlin/racingcar/RacingApplication.kt
@@ -1,6 +1,6 @@
 package racingcar
 
-import racingcar.manager.RacingManager
+import racingcar.engine.RacingCarEngineImpl
 import racingcar.view.InputView
 import racingcar.view.ResultView
 
@@ -9,14 +9,15 @@ class RacingApplication(private val inputView: InputView = InputView()) {
     fun start() {
         val carNum = inputView.getNumberOfCars()
         val attempts = inputView.getNumberOfAttempts()
-        println()
-        println(RACING_RESULT)
-        RacingManager(carNum, attempts).run {
-            race(ResultView::printCarPositions)
-        }
-    }
+        val racingCarEngine = RacingCarEngineImpl()
+        val racingManager = RacingManagerFactory().createRacingManager(carNum, racingCarEngine)
 
-    companion object {
-        private const val RACING_RESULT = "실행 결과"
+        ResultView.printResult()
+
+        racingManager.run {
+            repeat(attempts) {
+                race(ResultView::printCarPositions)
+            }
+        }
     }
 }
