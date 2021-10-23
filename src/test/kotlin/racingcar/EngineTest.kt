50d02226e6a35f9dde22dcc8ca20144235e04844
seunghwan <slflfl12@naver.com>
2021-10-23 13:59:58 +0900

    test, refactor (step3) : EngineTest 수정 (engine에서 random으로 나온 숫자를 value로 가정하고 test)

--- src/test/kotlin/racingcar/EngineTest.kt
+++ src/test/kotlin/racingcar/EngineTest.kt
@@ -6,38 +6,26 @@ import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import racingcar.engine.CarEngine
+import racingcar.engine.RacingCarEngineImpl
+import racingcar.random.RacingRandomGenerator
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class EngineTest {
 
-    class MockCarEngine : CarEngine {
-
-        private var randomNumber = 0
-
-        override fun execute(): Boolean {
-            return randomNumber >= RACING_CAR_MOVE_CRITERIA
-        }
-
-        fun setRandomNum(num: Int) {
-            this.randomNumber = num
-        }
-
-        companion object {
-            const val RACING_CAR_MOVE_CRITERIA = 4
-        }
-    }
-
-    private lateinit var mockCarEngine: MockCarEngine
-
-    @BeforeAll
-    fun setCarEngine() {
-        mockCarEngine = MockCarEngine()
-    }
-
     @ParameterizedTest(name = "Test {index}: value == {0} result == {1}")
     @CsvSource(value = ["0, false", "1, false", "2, false", "3, false", "4, true", "5, true", "6, true"])
     fun `randomNumber 4 이상일 경우 Engine 작동하는지 테스트`(value: Int, expected: Boolean) {
-        mockCarEngine.setRandomNum(value)
-        assertThat(mockCarEngine.execute()).isEqualTo(expected)
+        // given
+        val carEngine = object : CarEngine {
+            override fun execute(): Boolean {
+                return value >= 4
+            }
+        }
+
+        // when
+        val isExecute = carEngine.execute()
+
+        // then
+        assertThat(isExecute).isEqualTo(expected)
     }
 }
