147e47b5478fa1050b3a48cb6a9dc28a9616cbfb
seunghwan <slflfl12@naver.com>
2021-10-23 14:22:41 +0900

    test, refactor (step3) : RacingManagerTest 수정 (race 후 position 테스트)

--- src/test/kotlin/racingcar/RacingManagerTest.kt
+++ src/test/kotlin/racingcar/RacingManagerTest.kt
@@ -1,31 +1,24 @@
 package racingcar
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.api.Test
+import racingcar.engine.CarEngine
 import racingcar.manager.RacingManager
 
 class RacingManagerTest {
 
     private lateinit var racingManager: RacingManager
 
-    @ParameterizedTest(name = "Test {index}: value == {0} result == {1}")
-    @CsvSource(value = ["1, 1", "2, 2", "3, 3"])
-    fun `race() 중인 Car 사이즈 테스트`(carSize: Int, expected: Int) {
-        racingManager = RacingManager(carSize, 3)
-        racingManager.race {
-            assertThat(it.size).isEqualTo(expected)
-        }
-    }
+    @Test
+    fun `race() 후 전부 움직였을 경우 position을 테스트`() {
+        // given
+        val engine = object : CarEngine { override fun execute(): Boolean = true }
+        racingManager = RacingManagerFactory().createRacingManager(3, engine)
 
-    @ParameterizedTest(name = "Test {index}: value == {0} result == {1}")
-    @CsvSource(value = ["1, 1", "2, 2", "3, 3"])
-    fun `race() 몇 회 attempt 하는지 테스트`(attempts: Int, expected: Int) {
-        racingManager = RacingManager(3, attempts)
-        var count = 0
-        racingManager.race { cars ->
-            count++
-        }
-        assertThat(count).isEqualTo(expected)
+        // when
+        racingManager.race()
+
+        // then
+        assertThat(racingManager.getPositions()).isEqualTo(listOf(1, 1, 1))
     }
 }
