cd5fa008af676d47c307c15b2605e38060687962
junyoung.choi <junyoug.choi@kurlycorp.com>
2021-10-23 22:29:50 +0900

    feat: 트랙 화면 출력, 레이싱 테스트 코드

--- src/main/kotlin/racingcar/racing/Racing.kt
+++ src/main/kotlin/racingcar/racing/Racing.kt
@@ -1,17 +1,24 @@
 package racingcar.racing
 
 import racingcar.car.Car
+import racingcar.view.OutputView.resultView
+import java.util.stream.Collectors
 
 object Racing {
     fun racingStart(carCount: Int, tryCount: Int) {
         val cars = initCars(carCount)
+        resultView(cars, tryCount)
     }
 
-    private fun initCars(carCount: Int): List<Car> {
+    fun initCars(carCount: Int): List<Car> {
         var cars = mutableListOf<Car>()
-        for (i: Int in 1 until carCount) {
+        for (i: Int in 0 until carCount) {
             cars.add(Car())
         }
         return cars
+    }
+
+    fun moveCarForward(cars: List<Car>): List<Car> {
+        return cars.stream().peek(Car::move).collect(Collectors.toList())
     }
 }
