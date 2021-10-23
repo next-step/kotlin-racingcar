fd73d78fab849ce9145e1835e03201aca53c3cf7
ssibongee <ssibongee@gmail.com>
2021-10-24 02:13:58 +0900

    refactor: 점수(Record) 및 점수 리스트(Records) 클래스의 UI로직을 분리

--- src/main/kotlin/racingcar/domain/Records.kt
+++ src/main/kotlin/racingcar/domain/Records.kt
@@ -1,17 +1,11 @@
 package racingcar.domain
 
-import racingcar.ui.LINE_FEED
-
 @JvmInline
-value class Records private constructor(private val records: List<Record>) {
-
-    fun display(): String {
-        return records.joinToString(LINE_FEED) { it.score }
-    }
+value class Records private constructor(val values: List<Record>) {
 
     companion object {
         fun of(participants: RacingCars): Records {
-            val values = participants.list.map { Record(it) }
+            val values = participants.values.map { Record(it) }
             return Records(values)
         }
     }
