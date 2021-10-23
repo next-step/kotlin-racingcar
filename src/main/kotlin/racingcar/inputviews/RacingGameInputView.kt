199e396390b37a8f2ac995bc535dc95592e6ba3f
chaewon.na@deliveryhero.co.kr <chaewon.na@deliveryhero.co.kr>
2021-10-24 02:38:53 +0900

    refactor: Fix receiveInt to handle null or non number input

--- src/main/kotlin/racingcar/inputviews/RacingGameInputView.kt
+++ src/main/kotlin/racingcar/inputviews/RacingGameInputView.kt
@@ -9,17 +9,18 @@ class RacingGameInputView : InputView {
     }
 
     private fun receiveNumberOfCars(): Int {
-        println("자동차 대수는 몇 대 인가요?")
+        println(GET_NUMBER_OF_CARS_MESSAGE)
         return receiveInt()
     }
 
     private fun receiveNumberOfRounds(): Int {
-        println("시도할 횟수는 몇 회 인가요?")
+        println(GET_NUMBER_OF_ROUNDS_MESSAGE)
         return receiveInt()
     }
 
+    @Throws(IllegalArgumentException::class)
     private fun receiveInt(): Int {
-        val intInput = readLine()!!.toInt()
+        val intInput = readLine()?.toIntOrNull() ?: throw IllegalArgumentException(NOT_A_NUMBER_ERROR_MESSAGE)
         validateInput(intInput)
         return intInput
     }
@@ -27,7 +28,14 @@ class RacingGameInputView : InputView {
     @Throws(IllegalArgumentException::class)
     private fun validateInput(input: Int) {
         if (input < 0) {
-            throw IllegalArgumentException("입력은 0 보다 큰 정수이어야 합니다.")
+            throw IllegalArgumentException(INVALID_INT_ERROR_MESSAGE)
         }
+    }
+
+    companion object {
+        const val GET_NUMBER_OF_CARS_MESSAGE = "자동차 대수는 몇 대 인가요?"
+        const val GET_NUMBER_OF_ROUNDS_MESSAGE = "시도할 횟수는 몇 회 인가요?"
+        const val NOT_A_NUMBER_ERROR_MESSAGE = "입력이 정수가 아닙니다."
+        const val INVALID_INT_ERROR_MESSAGE = "입력은 0 보다 큰 정수이어야 합니다."
     }
 }
