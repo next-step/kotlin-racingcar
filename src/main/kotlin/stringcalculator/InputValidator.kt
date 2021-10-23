99678d63d2ccdc064fd2204c8314df686ece2660
chaewon.na@deliveryhero.co.kr <chaewon.na@deliveryhero.co.kr>
2021-10-24 03:25:00 +0900

    refactor: Refactor input validator

--- src/main/kotlin/stringcalculator/InputValidator.kt
+++ src/main/kotlin/stringcalculator/InputValidator.kt
@@ -2,39 +2,34 @@ package stringcalculator
 
 class InputValidator {
     fun validate(input: String?): List<String> {
-        val validString = validateInputIsNotEmpty(input)
-        val splitString = validString.split(" ")
-
-        validateSplitString(splitString)
+        val splitString = trySplit(input)
+        checkSizeIsOdd(splitString)
+        checkFormulaIsCorrect(splitString)
         return splitString
     }
 
     @Throws(IllegalArgumentException::class)
-    private fun validateInputIsNotEmpty(input: String?): String {
+    private fun trySplit(input: String?): List<String> {
         if (input.isNullOrBlank()) {
-            throw IllegalArgumentException("입력이 비어있습니다.")
+            throw IllegalArgumentException(EMPTY_INPUT_ERROR_MESSAGE)
         }
-        return input
-    }
 
-    private fun validateSplitString(splitString: List<String>) {
-        checkInputSizeIsOdd(splitString)
-        checkInputStructure(splitString)
+        return input.split(SPLIT_DELIMITER)
     }
 
     @Throws(IllegalArgumentException::class)
-    private fun checkInputSizeIsOdd(input: List<String>) {
+    private fun checkSizeIsOdd(input: List<String>) {
         if (input.size % EVEN_DIVIDER != ODD_REMAINDER) {
-            throw IllegalArgumentException("입력 항의 갯수가 올바르지 않습니다.")
+            throw IllegalArgumentException(INVALID_SIZE_ERROR_MESSAGE)
         }
     }
 
     @Throws(IllegalArgumentException::class)
-    private fun checkInputStructure(input: List<String>) {
+    private fun checkFormulaIsCorrect(input: List<String>) {
         val controlList = input.map { it.toIntOrNull() is Int }
         val comparisonList = input.mapIndexed { index, _ -> index % EVEN_DIVIDER == EVEN_REMAINDER }
         if (controlList != comparisonList) {
-            throw IllegalArgumentException("입력된 식의 구성이 올바르지 않습니다.")
+            throw IllegalArgumentException(INVALID_FORMULA_ERROR_MESSAGE)
         }
     }
 
@@ -42,5 +37,11 @@ class InputValidator {
         const val EVEN_DIVIDER = 2
         const val ODD_REMAINDER = 1
         const val EVEN_REMAINDER = 0
+
+        const val SPLIT_DELIMITER = " "
+
+        const val EMPTY_INPUT_ERROR_MESSAGE = "입력이 비어있습니다."
+        const val INVALID_SIZE_ERROR_MESSAGE = "입력 항의 갯수가 올바르지 않습니다."
+        const val INVALID_FORMULA_ERROR_MESSAGE = "입력된 식의 구성이 올바르지 않습니다."
     }
 }
