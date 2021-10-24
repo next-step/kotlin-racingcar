e2e2d1028cffb5c7ed9ba14f316f493be2df03a2
nathankim0 <jinyeob07@gmail.com>
2021-10-24 20:15:53 +0900

    refactor: Calculator 클래스 리팩토링
    - 매직넘버 상수화
    - java.lang.ArithmeticException import 삭제
    - if-else 지옥 탈출 => Operator enum class 추가
    - Calculator 매개변수 삭제
    - kotlin require 활용
    - 함수명 수정
    - 변경되지 않는 변수 val 타입으로 변경

--- src/main/kotlin/step2/Calculator.kt
+++ src/main/kotlin/step2/Calculator.kt
@@ -1,48 +1,39 @@
 package step2
 
-import java.lang.ArithmeticException
-
-fun main() {
-    println(Calculator(readLine().toString()).caculate())
-}
-
-class Calculator(private var inputText: String) {
-    fun caculate(): Int {
+class Calculator {
+    fun calculate(inputText: String): Int {
         checkNullOrWhitespace(inputText)
-        val splitedStringList = removeWhiteSpace(inputText)
+        val splitedStringList = getListOfSplitedStringByBlank(inputText)
 
-        var result = splitedStringList[0].toInt()
+        val firstOperand = splitedStringList[0]
 
-        for (i: Int in 0..splitedStringList.size - 3 step (2)) { // ex) i == 0, 2, 4
-            result = calculateWithOperator(result, splitedStringList[i + 2].toInt(), splitedStringList[i + 1])
+        val lastIndexOfOperand = splitedStringList.size - 3
+        val offset = 2
+
+        val operatorOffset = 1
+        val yOperandOffset = 2
+
+        checkNullOrWhitespace(firstOperand)
+        var result = firstOperand.toInt()
+
+        for (index: Int in 0..lastIndexOfOperand step (offset)) {
+            val operator = splitedStringList[index + operatorOffset]
+            val y = splitedStringList[index + yOperandOffset].toInt()
+
+            result = calculateWithOperator(result, y, operator)
         }
         return result
     }
 
     private fun checkNullOrWhitespace(inputString: String) {
-        if (inputString.isNullOrBlank()) {
-            throw IllegalArgumentException("입력값이 null이거나 빈 공백 문자임.")
-        }
+        require(inputString.isNotBlank()) { "입력값이 null이거나 빈 공백 문자임." }
     }
 
-    private fun removeWhiteSpace(inputString: String): List<String> {
+    private fun getListOfSplitedStringByBlank(inputString: String): List<String> {
         return inputString.split(' ') // ex) 2, +, 3, *, 4, /, 2
     }
 
     private fun calculateWithOperator(x: Int, y: Int, operator: String): Int {
-        return if (operator == "+") {
-            x + y
-        } else if (operator == "-") {
-            x - y
-        } else if (operator == "*") {
-            x * y
-        } else if (operator == "/") {
-            if (y == 0) {
-                throw ArithmeticException("0으로 나눌 수 없음.")
-            }
-            x / y
-        } else {
-            throw IllegalArgumentException("사칙연산 기호가 아님.")
-        }
+        return Operator.pickOperation(operator).calculate(x, y)
     }
 }
---
+++ src/main/kotlin/step2/Operator.kt
@@ -0,0 +1,23 @@
+package step2
+
+enum class Operator(val operator: String, val calculate: (Int, Int) -> Int) {
+    ADDITION("+", { x, y -> x + y }),
+    SUBSTRACTION("-", { x, y -> x - y }),
+    MULTIPLICATION("*", { x, y -> x * y }),
+    DIVISION("/", { x, y ->
+        require(y != 0) { "0으로 나눌 수 없음." }
+        x / y
+    });
+
+    companion object {
+        fun pickOperation(operator: String): Operator {
+            return when (operator) {
+                "+" -> ADDITION
+                "-" -> SUBSTRACTION
+                "*" -> MULTIPLICATION
+                "/" -> DIVISION
+                else -> throw IllegalArgumentException("사칙연산 기호가 아님.")
+            }
+        }
+    }
+}
