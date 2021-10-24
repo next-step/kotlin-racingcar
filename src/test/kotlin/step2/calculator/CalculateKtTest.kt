258d617dffd909a7b8a892403ac3e8304e5e50f8
jaenyeong <jaenyeong.dev@gmail.com>
2021-10-24 19:51:12 +0900

    feat(calculateTest): Add test code when given invalid string expression

--- src/test/kotlin/step2/calculator/CalculateKtTest.kt
+++ src/test/kotlin/step2/calculator/CalculateKtTest.kt
@@ -21,7 +21,29 @@ internal class CalculateKtTest {
         Assertions.assertThat(result).isEqualTo(expectedValue)
     }
 
+    @DisplayName("주어진 문자열 수식이 올바르지 않은 경우 계산값 반환 테스트")
+    @ParameterizedTest
+    @MethodSource("invalidStringExpression")
+    fun invalidStringExpressionTest(
+        stringExpression: String,
+        expectedException: Exception,
+        containErrorMessages: String
+    ) {
+        // Arrange
+        // Act
+        // Assert
+        Assertions.assertThatThrownBy() {
+            calculate(stringExpression)
+        }.isInstanceOf(expectedException::class.java)
+            .hasMessageContaining(containErrorMessages)
+    }
+
     companion object {
+        private const val arithmeticExceptionErrorMessage = "Error occurred during calculate"
+        private const val invalidExpressionErrorMessage = " is not valid expression"
+        private val arithmeticException = ArithmeticException()
+        private val illegalArgumentException = IllegalArgumentException()
+
         @JvmStatic
         fun validStringExpression(): Stream<Arguments> {
             return Stream.of(
@@ -31,6 +53,21 @@ internal class CalculateKtTest {
                 Arguments.of("7 * 2 * 3 - 2 / 4", 10),
                 Arguments.of("777 * 2 + 6 - 560 / 4", 250),
                 Arguments.of("111111 * 10 * 3 * 5 - 666000", 16_000_650),
+            )
+        }
+
+        @JvmStatic
+        fun invalidStringExpression(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of("2 / 0", arithmeticException, arithmeticExceptionErrorMessage),
+                Arguments.of("1 + 2 3", illegalArgumentException, invalidExpressionErrorMessage),
+                Arguments.of("15", illegalArgumentException, invalidExpressionErrorMessage),
+                Arguments.of("7   4", illegalArgumentException, invalidExpressionErrorMessage),
+                Arguments.of("7 * 2 * 3 -", illegalArgumentException, invalidExpressionErrorMessage),
+                Arguments.of("/ 4", illegalArgumentException, invalidExpressionErrorMessage),
+                Arguments.of("", illegalArgumentException, invalidExpressionErrorMessage),
+                Arguments.of("                  ", illegalArgumentException, invalidExpressionErrorMessage),
+                Arguments.of("2 / 0", arithmeticException, arithmeticExceptionErrorMessage),
             )
         }
     }
