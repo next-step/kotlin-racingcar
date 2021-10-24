86405f20089732c3590dc96db8eb7831de6e59ab
soo <onemask@naver.com>
2021-10-24 17:25:48 +0900

    feat - outputTest 코드 추가

--- src/test/kotlin/step3/CarsTest.kt
+++ src/test/kotlin/step3/CarsTest.kt
@@ -2,6 +2,7 @@ package step3
 
 import org.junit.jupiter.api.Test
 import step3.ui.Input
+import step3.ui.Output
 import java.util.InputMismatchException
 
 class CarsTest {
@@ -22,9 +23,8 @@ class CarsTest {
     fun `전진하는 조건 테스트#1 - 0~9 사이의 distance 값인지`() {
         val distance = Car().distance.calculate()
         val isSuccess = distance in 0..9
-        val isFail = distance > 10
         org.junit.jupiter.api.Assertions.assertTrue(isSuccess)
-        org.junit.jupiter.api.Assertions.assertFalse(isFail)
+        org.junit.jupiter.api.Assertions.assertFalse(!isSuccess)
     }
 
     @Test
@@ -40,6 +40,26 @@ class CarsTest {
             else -> {
                 org.junit.jupiter.api.Assertions.assertFalse(isForward)
                 org.assertj.core.api.Assertions.assertThat(car.forward).isEqualTo(car.forward)
+            }
+        }
+    }
+
+    @Test
+    fun `전진한만큼 원하는 outPut이 출력되었는지`() {
+        val car = Car()
+        car.move()
+
+        val isForward = car.distance.calculate() > 4
+        when {
+            isForward -> {
+                org.assertj.core.api.Assertions.assertThat(
+                    Output.printForward(car)
+                ).isEqualTo("-")
+            }
+            else -> {
+                org.assertj.core.api.Assertions.assertThat(
+                    Output.printCar(car)
+                ).isEqualTo("")
             }
         }
     }
