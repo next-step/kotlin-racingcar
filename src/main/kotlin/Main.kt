6ff9bcf040ea53b30cfb23853904d8f6116bd916
soo <onemask@naver.com>
2021-10-24 17:26:37 +0900

    add main 로직 추가

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,10 @@
+import step3.Cars
+import step3.Round
+import step3.ui.Input
+import step3.ui.Output
+
 fun main() {
-    println(Calculator(readLine()).execute())
+    val cars = Cars(numberOfCount = Input.getCountOfCar())
+    val rond = Round(roundNumber = Input.getNumberOfRound())
+    Output.printResult(cars = cars, round = rond)
 }
