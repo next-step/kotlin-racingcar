e65493802dce1cab09d02ac9e8e44c0c7ac16281
soo <onemask@naver.com>
2021-10-24 17:15:29 +0900

    refactor - round default 값 선언 추가

--- src/main/kotlin/step3/Round.kt
+++ src/main/kotlin/step3/Round.kt
@@ -1,5 +1,7 @@
 package step3
 
-data class Round(private val roundNumber: Int) {
+data class Round(private val roundNumber: Int = DEFAULT_NUMBER_OF_ROUND) {
     val number = roundNumber
 }
+
+const val DEFAULT_NUMBER_OF_ROUND = 0
