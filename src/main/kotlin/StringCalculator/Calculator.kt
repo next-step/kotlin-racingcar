01dcbd9180fb7eb413034861b3f26168f226321d
jvhe <jeongbae1212@gmail.com>
2021-10-25 06:11:35 +0900

    Refactor: Calculator 클래스 static하게 사용할 수 있도록 변경, ktlint 스타일 적용

--- src/main/kotlin/StringCalculator/Calculator.kt
+++ src/main/kotlin/StringCalculator/Calculator.kt
@@ -1,44 +1,46 @@
 package StringCalculator
 
-class Calculator {
-    fun calculate(input : String?) : Int {
-        checkIsValidExpression(input)
+class Calculator private constructor() {
+    companion object {
+        fun calculate(input: String?): Int {
+            checkIsValidExpression(input)
 
-        throw NotImplementedError(Exception().stackTrace[0].methodName)
-    }
-
-    private fun checkIsValidExpression(input: String?) {
-        if (input == null) throw IllegalArgumentException()
-        if (input.isBlank()) throw IllegalArgumentException()
-        // 공백으로 문자열 분할
-        val list = input.split(" ").toMutableList()
-        // 빈 문자열 제거
-        list.removeIf { element -> element.isEmpty()}
-        // 연산과 수식의 합은 홀수여야 한다.
-        if (list.size % 2 == 0) throw IllegalArgumentException()
-        // 수식은 숫자와 연산자가 번갈아 와야 한다.
-        var mustBeNumber = false
-        list.forEach { parameter ->
-            mustBeNumber = !mustBeNumber
-            if (mustBeNumber && parameter.toIntOrNull() != null) return@forEach
-            if (!mustBeNumber && isValidOperator(parameter)) return@forEach
-            throw IllegalArgumentException()
+            throw NotImplementedError(Exception().stackTrace[0].methodName)
         }
+
+        private fun checkIsValidExpression(input: String?) {
+            if (input == null) throw IllegalArgumentException()
+            if (input.isBlank()) throw IllegalArgumentException()
+            // 공백으로 문자열 분할
+            val list = input.split(" ").toMutableList()
+            // 빈 문자열 제거
+            list.removeIf { element -> element.isEmpty() }
+            // 연산과 수식의 합은 홀수여야 한다.
+            if (list.size % 2 == 0) throw IllegalArgumentException()
+            // 수식은 숫자와 연산자가 번갈아 와야 한다.
+            var mustBeNumber = false
+            list.forEach { parameter ->
+                mustBeNumber = !mustBeNumber
+                if (mustBeNumber && parameter.toIntOrNull() != null) return@forEach
+                if (!mustBeNumber && isValidOperator(parameter)) return@forEach
+                throw IllegalArgumentException()
+            }
+        }
+
+        private fun isValidOperator(operation: String?): Boolean {
+            if (operation == "+") return true
+            if (operation == "-") return true
+            if (operation == "*") return true
+            if (operation == "/") return true
+            return false
+        }
+
+        fun add(a: Int, b: Int): Int = a + b
+
+        fun subtract(a: Int, b: Int) = a - b
+
+        fun multiply(a: Int, b: Int) = a * b
+
+        fun divide(a: Int, b: Int) = a / b
     }
-
-    private fun isValidOperator(operation: String?) : Boolean {
-        if (operation == "+") return true
-        if (operation == "-") return true
-        if (operation == "*") return true
-        if (operation == "/") return true
-        return false
-    }
-
-    fun add(a: Int, b: Int) :Int = a + b
-
-    fun subtract(a: Int, b: Int) = a - b
-
-    fun multiply(a: Int, b: Int) = a * b
-
-    fun divide(a: Int, b: Int) = a / b
 }
--- src/test/kotlin/StringCalculator/CalculatorTest.kt
+++ src/test/kotlin/StringCalculator/CalculatorTest.kt
@@ -1,75 +1,71 @@
 package StringCalculator
 
-import org.assertj.core.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
 class CalculatorTest {
 
     @Test
-    fun `유효값 확인` () {
-        val calculator = Calculator()
+    fun `유효값 확인`() {
         // 잘못된 입력값들
         // null
         assertThatThrownBy {
-            calculator.calculate(null)
+            Calculator.calculate(null)
         }.isInstanceOf(IllegalArgumentException::class.java)
         // empty string
         assertThatThrownBy {
-            calculator.calculate("")
+            Calculator.calculate("")
         }.isInstanceOf(IllegalArgumentException::class.java)
         // white space
         assertThatThrownBy {
-            calculator.calculate("   ")
+            Calculator.calculate("   ")
         }.isInstanceOf(IllegalArgumentException::class.java)
         // 잘못된 기호
         assertThatThrownBy {
-            calculator.calculate(" &  ")
+            Calculator.calculate(" &  ")
         }.isInstanceOf(IllegalArgumentException::class.java)
         // 연산 기호 하나만 있음
         assertThatThrownBy {
-            calculator.calculate("+")
+            Calculator.calculate("+")
         }.isInstanceOf(IllegalArgumentException::class.java)
         // 숫자의 연속
         assertThatThrownBy {
-            calculator.calculate("123 12")
+            Calculator.calculate("123 12")
         }.isInstanceOf(IllegalArgumentException::class.java)
 
         // 유효값들
         assertThatThrownBy {
-            calculator.calculate("123")
+            Calculator.calculate("123")
         }.isInstanceOf(NotImplementedError::class.java)
         assertThatThrownBy {
-            calculator.calculate("+123")
+            Calculator.calculate("+123")
         }.isInstanceOf(NotImplementedError::class.java)
         assertThatThrownBy {
-            calculator.calculate("-123")
+            Calculator.calculate("-123")
         }.isInstanceOf(NotImplementedError::class.java)
         assertThatThrownBy {
-            calculator.calculate("123 + 12")
+            Calculator.calculate("123 + 12")
         }.isInstanceOf(NotImplementedError::class.java)
     }
 
     @Test
-    fun `덧셈` () {
-        val calculator = Calculator()
-        assertThat(calculator.add(3, 2)).isEqualTo(5)
+    fun `덧셈`() {
+        assertThat(Calculator.add(3, 2)).isEqualTo(5)
     }
 
     @Test
-    fun `뺄셈` () {
-        val calculator = Calculator()
-        assertThat(calculator.subtract(3, 2)).isEqualTo(1)
+    fun `뺄셈`() {
+        assertThat(Calculator.subtract(3, 2)).isEqualTo(1)
     }
 
     @Test
-    fun `곱셈` () {
-        val calculator = Calculator()
-        assertThat(calculator.multiply(3, 2)).isEqualTo(6)
+    fun `곱셈`() {
+        assertThat(Calculator.multiply(3, 2)).isEqualTo(6)
     }
 
     @Test
-    fun `나눗셈` () {
-        val calculator = Calculator()
-        assertThat(calculator.divide(3, 2)).isEqualTo(1)
+    fun `나눗셈`() {
+        assertThat(Calculator.divide(3, 2)).isEqualTo(1)
     }
 }
