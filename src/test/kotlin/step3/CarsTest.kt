9f557b999f43f72de9cd4b8c0bae8b331a8770ab
soo <onemask@naver.com>
2021-10-24 16:32:57 +0900

    refactor - import 정리

--- src/test/kotlin/step3/CarsTest.kt
+++ src/test/kotlin/step3/CarsTest.kt
@@ -1,6 +1,5 @@
 package step3
 
-import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import step3.ui.Input
 import java.util.InputMismatchException
@@ -9,13 +8,13 @@ class CarsTest {
 
     @Test
     fun `자동차 대수 Input Test`() {
-        Assertions.assertThatThrownBy { Cars(Input.getCountOfCar()) }
+        org.assertj.core.api.Assertions.assertThatThrownBy { Cars(Input.getCountOfCar()) }
             .isInstanceOf(InputMismatchException::class.java)
     }
 
     @Test
     fun `시도할 횟수 Input Test`() {
-        Assertions.assertThatThrownBy { Round(Input.getNumberOfRound()) }
+        org.assertj.core.api.Assertions.assertThatThrownBy { Round(Input.getNumberOfRound()) }
             .isInstanceOf(InputMismatchException::class.java)
     }
 }
