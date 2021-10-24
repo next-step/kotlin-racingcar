da973f6f5850f0cadc25e93a77d0d38eb5614581
soo <onemask@naver.com>
2021-10-24 17:26:17 +0900

    add 출력 로직 추가

---
+++ src/main/kotlin/step3/ui/Output.kt
@@ -0,0 +1,38 @@
+package step3.ui
+
+import step3.Car
+import step3.Cars
+import step3.Round
+
+object Output {
+    fun printResult(cars: Cars, round: Round) {
+        println(RESULT_OF_EXECUTE)
+        printCars(cars, round)
+    }
+
+    private fun printCars(cars: Cars, round: Round) {
+        repeat(round.number) {
+            cars.carList.forEach {
+                printCar(it)
+            }
+            cars.moveCars()
+            println(END_OF_LINE)
+        }
+    }
+
+    fun printCar(car: Car) {
+        println(printForward(car))
+    }
+
+    fun printForward(car: Car): String {
+        val stringBuilder = StringBuilder()
+        (0 until car.forward).map {
+            stringBuilder.append(FORWARD_MARK)
+        }
+        return stringBuilder.toString()
+    }
+}
+
+const val FORWARD_MARK = "-"
+const val END_OF_LINE = "!-- End of line --!"
+const val RESULT_OF_EXECUTE = "실행 결과"
