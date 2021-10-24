03e1618c5213524ae5f2bacd0aab68725f0d9890
dreamforachase <chase.lee@dreamfora.com>
2021-10-24 23:08:14 +0900

    Feat: 유효값 검증 로직 구체화 및 메소드 분리

--- src/main/kotlin/StringCalculator/Calculator.kt
+++ src/main/kotlin/StringCalculator/Calculator.kt
@@ -1,20 +1,32 @@
 package StringCalculator
 
 class Calculator {
-    fun calculate(input : String?) : Int{
-        if (input == null) throw IllegalArgumentException()
-        if (input.isBlank()) throw IllegalArgumentException()
-        val array = input.split(" ").toMutableList()
-        array.removeIf { element -> element.isEmpty()}
-        array.forEach { parameter ->
-            if (parameter.toIntOrNull() != null) return@forEach
-            if (isValidOperator(parameter)) return@forEach
-            throw IllegalArgumentException()
-        }
-        throw NotImplementedError(Exception().stackTrace[0].methodName);
+    fun calculate(input : String?) : Int {
+        checkIsValidExpression(input)
+
+        throw NotImplementedError(Exception().stackTrace[0].methodName)
     }
 
-    private fun isValidOperator(operation: String?) : Boolean{
+    private fun checkIsValidExpression(input: String?) {
+        if (input == null) throw IllegalArgumentException()
+        if (input.isBlank()) throw IllegalArgumentException()
+        // 공백으로 문자열 분할
+        val list = input.split(" ").toMutableList()
+        // 빈 문자열 제거
+        list.removeIf { element -> element.isEmpty()}
+        // 연산과 수식의 합은 홀수여야 한다.
+        if (list.size % 2 == 0) throw IllegalArgumentException()
+        // 수식은 숫자와 연산자가 번갈아 와야 한다.
+        var mustBeNumber = false
+        list.forEach { parameter ->
+            mustBeNumber = !mustBeNumber
+            if (mustBeNumber && parameter.toIntOrNull() != null) return@forEach
+            if (!mustBeNumber && isValidOperator(parameter)) return@forEach
+            throw IllegalArgumentException()
+        }
+    }
+
+    private fun isValidOperator(operation: String?) : Boolean {
         if (operation == "+") return true
         if (operation == "-") return true
         if (operation == "*") return true
--- src/test/kotlin/StringCalculator/CalculatorTest.kt
+++ src/test/kotlin/StringCalculator/CalculatorTest.kt
@@ -2,37 +2,50 @@ package StringCalculator
 
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Test
-import kotlin.IllegalArgumentException
 
 class CalculatorTest {
+
     @Test
     fun `유효값 확인` () {
         val calculator = Calculator()
-        // 잘못된 유효값
+        // 잘못된 입력값들
+        // null
         assertThatThrownBy {
             calculator.calculate(null)
         }.isInstanceOf(IllegalArgumentException::class.java)
+        // empty string
         assertThatThrownBy {
             calculator.calculate("")
         }.isInstanceOf(IllegalArgumentException::class.java)
+        // white space
         assertThatThrownBy {
             calculator.calculate("   ")
         }.isInstanceOf(IllegalArgumentException::class.java)
+        // 잘못된 기호
         assertThatThrownBy {
             calculator.calculate(" &  ")
         }.isInstanceOf(IllegalArgumentException::class.java)
-        // 현재는 올바른 인풋으로 간주
+        // 연산 기호 하나만 있음
         assertThatThrownBy {
             calculator.calculate("+")
-        }.isInstanceOf(NotImplementedError::class.java)
+        }.isInstanceOf(IllegalArgumentException::class.java)
+        // 숫자의 연속
         assertThatThrownBy {
-            calculator.calculate(" 12 * 7   12")
-        }.isInstanceOf(NotImplementedError::class.java)
-        assertThatThrownBy {
-            calculator.calculate("*")
-        }.isInstanceOf(NotImplementedError::class.java)
+            calculator.calculate("123 12")
+        }.isInstanceOf(IllegalArgumentException::class.java)
+
+        // 유효값들
         assertThatThrownBy {
             calculator.calculate("123")
+        }.isInstanceOf(NotImplementedError::class.java)
+        assertThatThrownBy {
+            calculator.calculate("+123")
+        }.isInstanceOf(NotImplementedError::class.java)
+        assertThatThrownBy {
+            calculator.calculate("-123")
+        }.isInstanceOf(NotImplementedError::class.java)
+        assertThatThrownBy {
+            calculator.calculate("123 + 12")
         }.isInstanceOf(NotImplementedError::class.java)
     }
 
