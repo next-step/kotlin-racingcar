2a0f65173eac0d4056be7a35c7e06bdf5a8c2587
seunghwan <slflfl12@naver.com>
2021-10-24 21:26:02 +0900

    refactor (step4): Car에 name 추가

--- src/main/kotlin/racingcar/car/Car.kt
+++ src/main/kotlin/racingcar/car/Car.kt
@@ -2,13 +2,15 @@ package racingcar.car
 
 import racingcar.engine.CarEngine
 
-class Car(private var position: Int = 0) {
+class Car(private var name: String = "", private var position: Int = 0) {
 
     fun movePosition(carEngine: CarEngine) {
         if (carEngine.execute()) {
             position++
         }
     }
+
+    fun getName() = name
 
     fun getPosition() = position
 }
