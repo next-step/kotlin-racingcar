89a9b40770f9825982174dec96604f9ba4dd8ccd
seunghwan <slflfl12@naver.com>
2021-10-24 22:34:44 +0900

    test, refactor (step4) : test를 위해 equals() 변경, findWinners() 테스트

--- src/main/kotlin/racingcar/car/Car.kt
+++ src/main/kotlin/racingcar/car/Car.kt
@@ -10,6 +10,25 @@ class Car(private var name: String = "", private var position: Int = 0, private 
         }
     }
 
+    override fun equals(other: Any?): Boolean {
+        if(this === other) return true
+        if(javaClass != other?.javaClass) return false
+
+        other as Car
+
+        if(name != other.name) return false
+        if (position != other.position) return false
+        if(carEngine != other.carEngine) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode() + carEngine.hashCode()
+        result = 31 * result + position
+        return result
+    }
+
     fun getName() = name
 
     fun getPosition() = position
