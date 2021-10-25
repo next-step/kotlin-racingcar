3c6670cb8593ed7ec2772be6a1107ebdbcdd6eea
minyul <ggomjae@gmail.com>
2021-10-25 23:25:17 +0900

    test : 뺄셈 곱하기 나누기 에 대한 테스트 추가

--- src/test/kotlin/step2/CalculatorTest.kt
+++ src/test/kotlin/step2/CalculatorTest.kt
@@ -5,10 +5,29 @@ import org.junit.jupiter.api.Test
 
 class CalculatorTest {
 
+    private val calculator: Calculator = Calculator()
+
     @Test
     fun `덧셈` () {
-        val calculator = Calculator()
         val str = "1 + 2 + 3 + 6"
         assertThat(calculator.calculate(str)).isEqualTo(12)
+    }
+
+    @Test
+    fun `뺄셈` () {
+        val str = "1 + 2 + 5 - 6"
+        assertThat(calculator.calculate(str)).isEqualTo(5)
+    }
+
+    @Test
+    fun `곱하기` () {
+        val str = "1 + 2 + 3 * 6"
+        assertThat(calculator.calculate(str)).isEqualTo(36)
+    }
+
+    @Test
+    fun `나누기` () {
+        val str = "1 + 2 / 3 + 6"
+        assertThat(calculator.calculate(str)).isEqualTo(7)
     }
 }
