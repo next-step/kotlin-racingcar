21e5719fece734f8db41ff588a38bbfda3df6ea2
pursue503 <pursue503@naver.com>
2021-10-25 21:41:35 +0900

    Fix (Step4): InputView 자동차 이름을 받도록 변경

--- src/main/kotlin/racingcar/domain/CarRacing.kt
+++ src/main/kotlin/racingcar/domain/CarRacing.kt
@@ -8,15 +8,14 @@ class CarRacing(
     private val inputView: InputView = InputView(),
     private val resultView: ResultView = ResultView(),
     private val randomGenerator: RandomGenerator = RandomGenerator(),
-    private val carFactory: CarFactory = CarFactory()
 ) {
 
     fun start() {
 
-        val createCarNumber = inputView.inputNumberOfCars()
+        val createCarNumber = inputView.inputCarName()
         val racingCount = inputView.inputNumberOfAttempts()
 
-        val carList = carFactory.createCars(createCarNumber)
+        val carList = CarFactory.createCars(createCarNumber)
 
         for (i in 0 until racingCount) {
             for (car in carList) {
--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -4,8 +4,8 @@ import racingcar.filter.ValueFilter
 
 class InputView {
 
-    fun inputNumberOfCars(): Int {
-        println(NUMBER_OF_CARS)
+    fun inputCarName(): Int {
+        println(CAR_NAME)
         return ValueFilter.verify(readLine()!!)
     }
 
@@ -15,7 +15,7 @@ class InputView {
     }
 
     companion object {
-        private const val NUMBER_OF_CARS = "자동차 대수는 몇대인가요?"
+        private const val CAR_NAME = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)를 기준으로 구분합니다.)"
         private const val NUMBER_OF_ATTEMPTS = "시도할 횟수는 몇 회 인가요?"
     }
 }
