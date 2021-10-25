bffecdf6c4082d5683d2b839513bcf10c68c45d4
jaenyeong <jaenyeong.dev@gmail.com>
2021-10-26 00:09:23 +0900

    refactor(MathExpression): Refactor require logic to readability

--- src/main/kotlin/step2/calculator/MathExpression.kt
+++ src/main/kotlin/step2/calculator/MathExpression.kt
@@ -1,7 +1,8 @@
 package step2.calculator
 
-private const val OPERATOR_MIN_SIZE = 1
-private const val OPERAND_MIN_SIZE = 2
+private const val REQUIRED_OPERATOR_MIN_SIZE = 1
+private const val REQUIRED_OPERAND_MIN_SIZE = 2
+private const val IS_ONE_SMALLER_THAN_OPERAND_SIZE = 1
 
 data class MathExpression(
     private val _operands: List<Operand>,
@@ -9,11 +10,14 @@ data class MathExpression(
 ) {
     init {
         require(
-            _operands.size >= OPERAND_MIN_SIZE &&
-                _operators.size >= OPERATOR_MIN_SIZE &&
-                _operands.size == _operators.size + 1
+            assertOperandSizeAndOperatorSizeIsValidSize()
         )
     }
+
+    private fun assertOperandSizeAndOperatorSizeIsValidSize() =
+        _operands.size >= REQUIRED_OPERAND_MIN_SIZE &&
+            _operators.size >= REQUIRED_OPERATOR_MIN_SIZE &&
+            _operands.size == _operators.size + IS_ONE_SMALLER_THAN_OPERAND_SIZE
 
     val operands: List<Operand>
         get() = _operands.toList()
