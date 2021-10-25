aa5495e25ced223709d31119641c9c81167de624
sinoso <tpgus123123@naver.com>
2021-10-25 22:40:45 +0900

    refactor : step5 피드백 RandomGenrator 제거

--- src/main/kotlin/racingcar/domain/Circuit.kt
+++ src/main/kotlin/racingcar/domain/Circuit.kt
@@ -1,9 +1,11 @@
 package racingcar.domain
 
-import racingcar.domain.RandomGenerator.getRandomValue
+import java.util.Random
 
 class Circuit(carsNameList: List<String> = emptyList()) {
     private val circuit = ArrayList<Car>()
+
+    private val randomGenerator = Random()
 
     init {
         addCarNameListToCircuit(carsNameList)
@@ -18,9 +20,15 @@ class Circuit(carsNameList: List<String> = emptyList()) {
 
     fun getAllCarsLapResult() = circuit.map { LapResult(it.carName, it.movedDistance) }
 
+    private fun getRandomValue(bound: Int = FIXED_BOUND) = randomGenerator.nextInt(bound)
+
     private fun addCarNameListToCircuit(carNameList: List<String>) {
         carNameList.forEach {
             addCarToCircuit(Car(4, it))
         }
+    }
+
+    companion object {
+        private const val FIXED_BOUND = 10
     }
 }
