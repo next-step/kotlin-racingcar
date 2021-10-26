ed61879b393aeb52e28166494629b43c8fc9fa43
김우재 <kwj1270@naver.com>
2021-10-26 10:11:28 +0900

    refactor: let 전환 !

--- src/main/kotlin/domain/step4/domain/racingcar/Names.kt
+++ src/main/kotlin/domain/step4/domain/racingcar/Names.kt
@@ -18,11 +18,9 @@ data class Names private constructor(private val _names: List<Name>) {
         private const val EMPTY_MESSAGE = "비어있는 값은 들어올 수 없습니다."
 
         fun ofStringWithSplitStrategy(target: String, splitStrategy: SplitStrategy): Names =
-            ofList(
-                splitStrategy.split(target)
-                    .map { name -> Name(name) }
-                    .toList()
-            )
+            splitStrategy.split(target)
+                .map { name -> Name(name) }
+                .let { ofList(it) } // 없으면 빈 컬렉션 값을 사용할 수 있기 때문에 조심해야겠군요!!
 
         fun ofList(names: List<Name>): Names = Names(names.toList())
     }
