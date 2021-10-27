c07f43c21c1f37971ce3f9dc390a153beef8900d
chaewon.na@deliveryhero.co.kr <chaewon.na@deliveryhero.co.kr>
2021-10-28 01:53:01 +0900

    refactor: Refactor RacingGameInputView private methods

--- src/main/kotlin/racingcar/inputviews/RacingGameInputView.kt
+++ src/main/kotlin/racingcar/inputviews/RacingGameInputView.kt
@@ -10,45 +10,27 @@ class RacingGameInputView : InputView {
         return GameInput(carNames, numberOfRounds)
     }
 
+    @Throws(IllegalArgumentException::class)
     private fun receiveCarNames(): List<String> {
         println(GET_CAR_NAMES_MESSAGE)
-        return receiveStr().split(CAR_NAMES_SPLIT_DELIMITER)
+        val input = readLine()
+        require(!input.isNullOrBlank()) { EMPTY_STRING_ERROR_MESSAGE }
+        return input.split(CAR_NAMES_SPLIT_DELIMITER)
     }
 
     @Throws(IllegalArgumentException::class)
-    private fun receiveStr(): String {
-        val input = readLine()
-        if (input.isNullOrBlank()) {
-            throw IllegalArgumentException(EMPTY_STRING_ERROR_MESSAGE)
-        }
-        return input
-    }
-
     private fun receiveNumberOfRounds(): Int {
         println(GET_NUMBER_OF_ROUNDS_MESSAGE)
-        val input = receiveInt()
-        validateNumberOfRounds(input)
+        val input = readLine()?.toIntOrNull() ?: 0
+        require(input < 0) { INVALID_INT_ERROR_MESSAGE }
         return input
-    }
-
-    @Throws(IllegalArgumentException::class)
-    private fun receiveInt(): Int {
-        return readLine()?.toIntOrNull() ?: throw IllegalArgumentException(NOT_A_NUMBER_ERROR_MESSAGE)
-    }
-
-    @Throws(IllegalArgumentException::class)
-    private fun validateNumberOfRounds(input: Int) {
-        if (input < 0) {
-            throw IllegalArgumentException(INVALID_INT_ERROR_MESSAGE)
-        }
     }
 
     companion object {
-        const val GET_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
-        const val GET_NUMBER_OF_ROUNDS_MESSAGE = "시도할 횟수는 몇 회 인가요?"
-        const val EMPTY_STRING_ERROR_MESSAGE = "입력이 비어있습니다."
-        const val NOT_A_NUMBER_ERROR_MESSAGE = "입력이 정수가 아닙니다."
-        const val CAR_NAMES_SPLIT_DELIMITER = ","
-        const val INVALID_INT_ERROR_MESSAGE = "입력은 0 보다 큰 정수이어야 합니다."
+        private const val CAR_NAMES_SPLIT_DELIMITER = ","
+        private const val GET_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
+        private const val GET_NUMBER_OF_ROUNDS_MESSAGE = "시도할 횟수는 몇 회 인가요?"
+        private const val EMPTY_STRING_ERROR_MESSAGE = "입력이 비어있습니다."
+        private const val INVALID_INT_ERROR_MESSAGE = "입력은 0 보다 큰 정수이어야 합니다."
     }
 }
