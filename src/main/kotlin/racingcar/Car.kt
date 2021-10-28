4179c793d29149186e1e12ba8be402c64531104e
chaewon.na@deliveryhero.co.kr <chaewon.na@deliveryhero.co.kr>
2021-10-29 00:37:21 +0900

    refactor: Add error message as const val

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -5,7 +5,7 @@ class Car(val name: String, position: Int = 0) {
         private set
 
     init {
-        require(name.length <= MAX_NAME_LENGTH) { "자동차 이름은 5자를 초과할 수 없습니다." }
+        require(name.length <= MAX_NAME_LENGTH) { MAX_NAME_LENGTH_EXCEEDED_ERROR_MESSAGE }
     }
 
     fun accelerate(fuel: Int) {
@@ -19,6 +19,7 @@ class Car(val name: String, position: Int = 0) {
     }
 
     companion object {
+        private const val MAX_NAME_LENGTH_EXCEEDED_ERROR_MESSAGE = "자동차 이름은 5자를 초과할 수 없습니다."
         private const val MAX_NAME_LENGTH = 5
         private const val MIN_FUEL_THRESHOLD = 4
     }
