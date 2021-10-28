018f09d6d3bbc7c0df3471320a60f8e1f6aa9e5f
chaewon.na@deliveryhero.co.kr <chaewon.na@deliveryhero.co.kr>
2021-10-29 01:52:46 +0900

    test: Refactor test name

--- src/test/kotlin/racingcar/RacingGameTest.kt
+++ src/test/kotlin/racingcar/RacingGameTest.kt
@@ -6,12 +6,14 @@ import racingcar.dtos.GameInput
 
 class RacingGameTest {
     @Test
-    fun `입력값을 받아 실행하면 결과를 리턴한다`() {
+    fun `게임 입력값을 받아 play 하면 게임 실행 결과를 리턴한다`() {
         val carNames = listOf("car1", "car2", "car3")
         val gameInput = GameInput(carNames, 5)
 
         val gameResult = RacingGame(gameInput).play()
 
-        assertThat(gameResult.roundResults).hasSize(5).allMatch { it.cars.size == carNames.size }
+        assertThat(gameResult.roundResults)
+            .hasSize(5)
+            .allMatch { it.cars.size == carNames.size }
     }
 }
