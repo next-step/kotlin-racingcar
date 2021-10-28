8e5af3b6605d5ed417e8f6132804b035ce55a7a3
Nathan Kim <jinyeob07@gmail.com>
2021-10-28 20:49:31 +0900

    style: 오타 수정

--- src/main/kotlin/calculator/Operator.kt
+++ src/main/kotlin/calculator/Operator.kt
@@ -1,8 +1,8 @@
-package caculator
+package calculator
 
 enum class Operator(val operator: String, val calculate: (Int, Int) -> Int) {
     ADDITION("+", { x, y -> x + y }),
-    SUBSTRACTION("-", { x, y -> x - y }),
+    SUBTRACTION("-", { x, y -> x - y }),
     MULTIPLICATION("*", { x, y -> x * y }),
     DIVISION("/", { x, y ->
         require(y != 0) { "0으로 나눌 수 없음." }
@@ -13,7 +13,7 @@ enum class Operator(val operator: String, val calculate: (Int, Int) -> Int) {
         fun pickOperation(operator: String): Operator {
             return when (operator) {
                 "+" -> ADDITION
-                "-" -> SUBSTRACTION
+                "-" -> SUBTRACTION
                 "*" -> MULTIPLICATION
                 "/" -> DIVISION
                 else -> throw IllegalArgumentException("사칙연산 기호가 아님.")
