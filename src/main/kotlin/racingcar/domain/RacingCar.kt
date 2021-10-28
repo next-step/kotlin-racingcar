61a92b30add9f0698160da55625c1bee2996f620
pursue503 <pursue503@naver.com>
2021-10-28 21:01:45 +0900

    Fix (Step5): 일급 컬렉션 적용 피드백 반영

---
+++ src/main/kotlin/racingcar/domain/RacingCar.kt
@@ -0,0 +1,21 @@
+package racingcar.domain
+
+import racingcar.domain.random.RandomGenerator
+import racingcar.view.ResultView
+
+class CarList (
+    private val carList: List<Car>
+) {
+
+    fun carAction(randomGenerator: RandomGenerator, resultView: ResultView) {
+        carList.forEach { it.forward(randomGenerator.getIntRandom()) }
+        resultView.action(carList)
+        println()
+    }
+
+    fun victoryCar(): List<Car> {
+        val max = carList.maxOf { it.distance }.or(0)
+        return carList.filter { it.distance == max }.toList()
+    }
+
+}
