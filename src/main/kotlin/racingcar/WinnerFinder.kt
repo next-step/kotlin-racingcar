a6f0269c5035b0c7755d16039299e02b303c03a4
chaewon.na@deliveryhero.co.kr <chaewon.na@deliveryhero.co.kr>
2021-10-29 00:56:30 +0900

    refactor: Add a line between primary constructor and the first method

--- src/main/kotlin/racingcar/WinnerFinder.kt
+++ src/main/kotlin/racingcar/WinnerFinder.kt
@@ -3,6 +3,7 @@ package racingcar
 import racingcar.dtos.GameResult
 
 class WinnerFinder {
+
     fun find(gameResult: GameResult): List<Car> {
         val lastRoundResult = gameResult.roundResults.last()
         val maxPosition = lastRoundResult.cars.maxOf { it.position }
