2a7ed35775366d0d58787ada71b76795af38ab28
Nathan Kim <jinyeob07@gmail.com>
2021-10-30 12:24:52 +0900

    test: 우승자 수에 대한 test 코드 작성

--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -1,10 +1,24 @@
 package racingcar
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class CarTest {
+    @ParameterizedTest
+    @ValueSource(ints = [2, 1, 3])
+    fun `우승자가 1명 이상이면 validateWinnersCount 에 성공한다`(input: Int) {
+        Assertions.assertDoesNotThrow { validateWinnersCount(input) }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [0, -1])
+    fun `우승자가 1명 미만이면 validateWinnersCount 에 실패한다`(input: Int) {
+        assertThatExceptionOfType(IllegalArgumentException::class.java).isThrownBy { validateWinnersCount(input) }
+    }
+
     @ParameterizedTest
     @ValueSource(ints = [4, 5, 6, 7])
     fun `0에서 9사이의 랜덤 값이 4 이상일 경우 전진한다`(input: Int) {
@@ -25,7 +39,6 @@ class CarTest {
     @ValueSource(ints = [-1, 11, 10])
     fun `전진 시도 숫자가 0과 9사이의 값이 아니면 validateAttemptNumber 에 실패한다`(input: Int) {
         val car = Car()
-        org.assertj.core.api.Assertions.assertThatExceptionOfType(IllegalArgumentException::class.java)
-            .isThrownBy { car.validateAttemptNumber(input) }
+        assertThatExceptionOfType(IllegalArgumentException::class.java).isThrownBy { car.validateAttemptNumber(input) }
     }
 }
