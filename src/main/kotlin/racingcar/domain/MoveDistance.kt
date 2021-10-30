6d974a8dcaa5ec01890790b379e41aa06ff4a9f2
Seop(Home Mac) <edenhazard5870@gmail.com>
2021-10-30 19:32:51 +0900

    feat - 이동거리 도메인 구현

---
+++ src/main/kotlin/racingcar/domain/MoveDistance.kt
@@ -0,0 +1,12 @@
+package racingcar.domain
+
+data class MoveDistance(var distance: Int = 0) {
+
+    fun move() {
+      distance = distance.plus(ADD_MOVE_DISTANCE)
+    }
+
+    companion object {
+        private const val ADD_MOVE_DISTANCE = 1
+    }
+}
