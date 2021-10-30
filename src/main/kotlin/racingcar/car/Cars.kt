91815998600081a881dc7d90cea105dbe5fb18ab
seunghwan <slflfl12@naver.com>
2021-10-30 21:04:31 +0900

    refactor (step5) : Cars 클래스 추가

---
+++ src/main/kotlin/racingcar/car/Cars.kt
@@ -0,0 +1,22 @@
+package racingcar.car
+
+import racingcar.engine.CarEngine
+
+@JvmInline
+value class Cars(private val _cars: List<Car>) {
+
+    val cars: List<Car>
+        get() = _cars.toList()
+
+    private fun findMaxPosition(): Int? = cars.maxByOrNull { it.position }?.position
+
+    fun movePositions() = _cars.map { it.movePosition() }
+
+    fun findWinners(): List<Car> {
+        return cars.filter { it.position == findMaxPosition() }.toList()
+    }
+
+    companion object {
+        fun newInstance(carNames: List<String>, engine: CarEngine): Cars = Cars(carNames.map { Car(CarName(it), carEngine = engine) })
+    }
+}
