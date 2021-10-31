b8851bbdaeb8b467c2d50b86823f9691c532dbf2
Seop(Home Mac) <edenhazard5870@gmail.com>
2021-10-31 15:42:58 +0900

    feat - 카목록 도메인 구현

---
+++ src/main/kotlin/racingcar/domain/Cars.kt
@@ -0,0 +1,22 @@
+package racingcar.domain
+
+class Cars(registerCarNumber: Int) {
+
+    val cars: List<Car>
+
+    init {
+        cars = registerCar(registerCarNumber)
+    }
+
+    private fun registerCar(registerCarNumber: Int): List<Car> {
+        return (REGISTER_CAR_START_NUMBER..registerCarNumber).map { Car() }
+    }
+
+    fun moveOfCars(carMovingStrategy: MovingStrategy) {
+        cars.forEach { it.moveCar(carMovingStrategy) }
+    }
+
+    companion object {
+        private const val REGISTER_CAR_START_NUMBER = 1
+    }
+}
