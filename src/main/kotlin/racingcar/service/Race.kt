8e3e55e6cd29bc5b2bab4b4cf1d7a57830d234f9
Seop(Home Mac) <edenhazard5870@gmail.com>
2021-11-02 08:03:13 +0900

    refactor - for -> repeat 가독성 개선

--- src/main/kotlin/racingcar/service/Race.kt
+++ src/main/kotlin/racingcar/service/Race.kt
@@ -7,11 +7,8 @@ class Race(registerCarNumber: Int, private val moveCarNumber: Int) {
     val cars = Cars(registerCarNumber)
 
     fun confirmMoveOfCars(carMovingStrategy: Boolean) {
-        for (index in CAR_MOVE_LOOP_START_NUMBER..moveCarNumber)
+        repeat(moveCarNumber) {
             cars.moveOfCars(carMovingStrategy)
-    }
-
-    companion object {
-        private const val CAR_MOVE_LOOP_START_NUMBER = 1
+        }
     }
 }
