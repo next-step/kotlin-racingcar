5dc3c990f68ba229829095ae943b1fba8de4baea
Seop(Home Mac) <edenhazard5870@gmail.com>
2021-11-02 08:12:02 +0900

    refactor - collections map -> List API로 변경

--- src/main/kotlin/racingcar/domain/Cars.kt
+++ src/main/kotlin/racingcar/domain/Cars.kt
@@ -2,21 +2,13 @@ package racingcar.domain
 
 class Cars(registerCarNumber: Int) {
 
-    val cars: List<Car>
-
-    init {
-        cars = registerCar(registerCarNumber)
-    }
+    val cars = registerCar(registerCarNumber)
 
     private fun registerCar(registerCarNumber: Int): List<Car> {
-        return (REGISTER_CAR_START_NUMBER..registerCarNumber).map { Car() }
+        return List(registerCarNumber) { Car() }
     }
 
     fun moveOfCars(carMovingStrategy: Boolean) {
         cars.forEach { it.moveCar(carMovingStrategy) }
-    }
-
-    companion object {
-        private const val REGISTER_CAR_START_NUMBER = 1
     }
 }
