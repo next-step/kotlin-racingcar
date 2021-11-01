2c4c14d33c27ea63fa7f04680a2cc287c2c927a2
Seop(Home Mac) <edenhazard5870@gmail.com>
2021-11-02 07:44:56 +0900

    refactor - lint 코드 스타일 정리

--- src/test/kotlin/racingcar/service/RaceTest.kt
+++ src/test/kotlin/racingcar/service/RaceTest.kt
@@ -10,7 +10,7 @@ class RaceTest {
     @Test
     fun `차량들 이동 확인`() {
         // given
-        race = Race(5,5)
+        race = Race(5, 5)
 
         // when
         race.confirmMoveOfCars { true }
