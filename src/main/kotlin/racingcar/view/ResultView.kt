b3db5dab2f45adc00aac644eb8dc81d86b8fdfa3
Seop(Home Mac) <edenhazard5870@gmail.com>
2021-11-02 09:08:22 +0900

    refactor - 자동차 이동거리 출력 부분 리팩터링

--- src/main/kotlin/racingcar/view/ResultView.kt
+++ src/main/kotlin/racingcar/view/ResultView.kt
@@ -4,8 +4,6 @@ import racingcar.domain.Car
 
 object ResultView {
 
-    private const val LOOP_START_NUMBER = 0
-
     fun carPositionPrints(car: List<Car>) {
         car.forEach {
             carPositionPrint(it)
@@ -13,9 +11,6 @@ object ResultView {
     }
 
     private fun carPositionPrint(car: Car) {
-        (LOOP_START_NUMBER..car.carPosition()).forEach { _ ->
-            print("-")
-        }
-        println()
+        println("-".repeat(car.carPosition()))
     }
 }
