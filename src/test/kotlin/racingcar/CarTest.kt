caa885a8cd188af3a8f785ae12facef3d0a7a851
nathankim0 <jinyeob07@gmail.com>
2021-11-02 10:35:39 +0900

    fix: Car Test 코드 수정 (name 추가)

--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -22,7 +22,7 @@ class CarTest {
     @ParameterizedTest
     @ValueSource(ints = [4, 5, 6, 7])
     fun `0에서 9사이의 랜덤 값이 4 이상일 경우 전진한다`(input: Int) {
-        val car = Car()
+        val car = Car("Nathan")
         car.move(input)
         assertThat(car.position).isEqualTo(2)
     }
@@ -30,7 +30,7 @@ class CarTest {
     @ParameterizedTest
     @ValueSource(ints = [1, 2, 3])
     fun `0에서 9사이의 랜덤 값이 4 미만일 경우 멈춘다`(input: Int) {
-        val car = Car()
+        val car = Car("Nathan")
         car.move(input)
         assertThat(car.position).isEqualTo(1)
     }
@@ -38,7 +38,7 @@ class CarTest {
     @ParameterizedTest
     @ValueSource(ints = [-1, 11, 10])
     fun `전진 시도 숫자가 0과 9사이의 값이 아니면 validateAttemptNumber 에 실패한다`(input: Int) {
-        val car = Car()
+        val car = Car("Nathan")
         assertThatExceptionOfType(IllegalArgumentException::class.java).isThrownBy { car.validateAttemptNumber(input) }
     }
 }
