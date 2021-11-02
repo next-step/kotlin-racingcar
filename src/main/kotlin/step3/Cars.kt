b7f9a7d51a6ae2904fdea54fa4cb1457ccd6fd20
soo <onemask@naver.com>
2021-11-03 01:44:39 +0900

    refactor 불필요한 지역 변수 제거

--- src/main/kotlin/step3/Cars.kt
+++ src/main/kotlin/step3/Cars.kt
@@ -1,16 +1,15 @@
 package step3
 
-class Cars constructor(private val numberOfCount: Int = DEFAULT_NUMBER_OF_COUNT) {
+class Cars constructor(private val numberOfCount: Int = DEFAULT_NUMBER_OF_COUNT, val carList: MutableList<Car> = mutableListOf()) {
 
     fun makeCar(): List<Car> {
-        val carList = mutableListOf<Car>()
         for (i in DEFAULT_NUMBER_OF_COUNT until numberOfCount) {
             carList.add(Car(Distance()))
         }
         return carList
     }
 
-    fun moveCars(carList: List<Car>) {
+    fun moveCars() {
         carList.forEach {
             it.move()
         }
