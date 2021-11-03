9b03d7728a79f53c6094fcd547803110c15fd198
Seop(Home Mac) <edenhazard5870@gmail.com>
2021-11-04 05:29:02 +0900

    refactor - 불필요한 함수 제거

--- src/main/kotlin/racingcar/domain/Cars.kt
+++ src/main/kotlin/racingcar/domain/Cars.kt
@@ -2,11 +2,7 @@ package racingcar.domain
 
 class Cars(registerCarNumber: Int) {
 
-    val cars = registerCar(registerCarNumber)
-
-    private fun registerCar(registerCarNumber: Int): List<Car> {
-        return List(registerCarNumber) { Car() }
-    }
+    val cars = List(registerCarNumber) { Car() }
 
     fun moveOfCars(movingStrategy: MovingStrategy) {
         cars.forEach { it.moveCar(movingStrategy) }
