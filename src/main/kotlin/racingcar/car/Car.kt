6a22cfa964e4607322370fc9964ff116482a78c0
seunghwan <slflfl12@naver.com>
2021-11-03 18:29:49 +0900

    refactor (step5) : 매개변수에 이름 잘못되어있던 것 수정

--- src/main/kotlin/racingcar/car/Car.kt
+++ src/main/kotlin/racingcar/car/Car.kt
@@ -2,13 +2,13 @@ package racingcar.car
 
 import racingcar.engine.CarEngine
 
-data class Car(private val _Car_name: CarName = CarName(""), private var _position: Int = 0, private var carEngine: CarEngine) {
+data class Car(private val _carName: CarName = CarName(""), private var _position: Int = 0, private var carEngine: CarEngine) {
 
     val position: Int
         get() = _position
 
     val name: String
-        get() = _Car_name.name
+        get() = _carName.name
 
     fun movePosition() {
         if (carEngine.execute()) {
