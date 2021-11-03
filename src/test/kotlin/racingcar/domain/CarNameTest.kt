1e4c53d2a732545bac91eb323e058e76cc87b194
Seop(Home Mac) <edenhazard5870@gmail.com>
2021-11-04 07:32:55 +0900

    test - 자동차 이름 도메인 테스트 코드

---
+++ src/test/kotlin/racingcar/domain/CarNameTest.kt
@@ -0,0 +1,27 @@
+package racingcar.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class CarNameTest {
+    @Test
+    fun `자동차 이름 길이 체크`() {
+        assertThrows<IllegalArgumentException> {
+            CarName("가나다라마바사")
+        }
+    }
+
+    @Test
+    fun `자동차 이름이 빈값으로 들어오는지 체크`() {
+        assertThrows<IllegalArgumentException> {
+            CarName("")
+        }
+    }
+
+    @Test
+    fun `green case`() {
+        val actual = CarName("가나다")
+        assertThat(actual.name).isEqualTo("가나다")
+    }
+}
