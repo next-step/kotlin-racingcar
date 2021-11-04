43a861368509371f7069b52418047dd670766f95
jaenyeong <jaenyeong.dev@gmail.com>
2021-11-04 09:34:19 +0900

    feat(NumberOfCars at el): Add NumberOfCars data class

---
+++ src/main/kotlin/step3/racingcar/domain/NumberOfCars.kt
@@ -0,0 +1,11 @@
+package step3.racingcar.domain
+
+data class NumberOfCars(val value: Int) {
+    init {
+        require(MINIMUM_NUMBER_OF_CAR <= value)
+    }
+
+    companion object {
+        private const val MINIMUM_NUMBER_OF_CAR = 1
+    }
+}
