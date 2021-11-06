6508a959c8991e8d4f355efa39ee52046996bc29
soo <onemask@naver.com>
2021-11-06 16:46:11 +0900

    refactor Car 멤버 변수 생성자에서 distance 받도록 수정

--- src/main/kotlin/step3/Cars.kt
+++ src/main/kotlin/step3/Cars.kt
@@ -4,7 +4,7 @@ data class Cars(val carList: MutableList<Car> = mutableListOf()) {
 
     fun makeCar(numberOfCount: Int): List<Car> {
         for (i in DEFAULT_NUMBER_OF_COUNT until numberOfCount) {
-            carList.add(Car(Distance()))
+            carList.add(Car())
         }
         return carList
     }
