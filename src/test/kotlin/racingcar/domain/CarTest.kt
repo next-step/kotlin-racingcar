a19003470d630f8b496c0f3d17f125b5017fd812
minyul <ggomjae@gmail.com>
2021-11-08 20:51:30 +0900

    text : given, when, them 을 써서 좀 더 명시적으로 수정

--- src/test/kotlin/racingcar/domain/CarTest.kt
+++ src/test/kotlin/racingcar/domain/CarTest.kt
@@ -6,9 +6,14 @@ import org.junit.jupiter.api.Test
 class CarTest {
     
     @Test
-    fun `자동차_전진`() {
+    fun `자동차_4이상일 때_전진`() {
+        // given
         val car = Car()
+
+        // when
         val moveResult = car.move(MOVE_NUMBER)
+
+        // then
         assertThat(moveResult).isEqualTo(1)
     }
 
