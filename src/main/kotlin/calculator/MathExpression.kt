0fcff46dc01cb141b069e477becea39683194750
jaenyeong <jaenyeong.dev@gmail.com>
2021-11-10 01:49:42 +0900

    refactor(MathExpression): refactor `MathExpression` class

--- src/main/kotlin/calculator/MathExpression.kt
+++ src/main/kotlin/calculator/MathExpression.kt
@@ -1,23 +1,20 @@
 package calculator
 
-private const val REQUIRED_OPERATOR_MIN_SIZE = 1
-private const val REQUIRED_OPERAND_MIN_SIZE = 2
-private const val IS_ONE_SMALLER_THAN_OPERAND_SIZE = 1
-
 data class MathExpression(
     private val _operands: List<Operand>,
     private val _operators: List<Operator>
 ) {
     init {
-        require(
-            assertOperandSizeAndOperatorSizeIsValidSize()
-        )
+        require(assertValidOperandSizeAndOperatorSize())
     }
 
-    private fun assertOperandSizeAndOperatorSizeIsValidSize() =
-        _operands.size >= REQUIRED_OPERAND_MIN_SIZE &&
-            _operators.size >= REQUIRED_OPERATOR_MIN_SIZE &&
-            _operands.size == _operators.size + IS_ONE_SMALLER_THAN_OPERAND_SIZE
+    private fun assertValidOperandSizeAndOperatorSize(): Boolean {
+        val isValidOperandSize = _operands.size >= REQUIRED_OPERAND_MIN_SIZE
+        val isValidOperatorsSize = _operators.size >= REQUIRED_OPERATOR_MIN_SIZE
+        val isValidExpressionSize = _operands.size == _operators.size + IS_ONE_SMALLER_THAN_OPERAND_SIZE
+
+        return isValidOperandSize && isValidOperatorsSize && isValidExpressionSize
+    }
 
     val operands: List<Operand>
         get() = _operands.toList()
@@ -25,4 +22,10 @@ data class MathExpression(
         get() = _operators.toList()
     val operatorsSize: Int
         get() = _operators.size
+
+    companion object {
+        private const val REQUIRED_OPERATOR_MIN_SIZE = 1
+        private const val REQUIRED_OPERAND_MIN_SIZE = 2
+        private const val IS_ONE_SMALLER_THAN_OPERAND_SIZE = 1
+    }
 }
