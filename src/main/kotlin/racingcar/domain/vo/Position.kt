8e0a996cef7e0c6e87aa875273d4069b6092dd14
안운장 <hodol@woowahan.com>
2021-11-11 22:33:33 +0900

    feat: 위치(Position) 객체간 비교를 위한 Comparable 인터페이스 구현

--- src/main/kotlin/racingcar/domain/vo/Position.kt
+++ src/main/kotlin/racingcar/domain/vo/Position.kt
@@ -1,10 +1,14 @@
 package racingcar.domain.vo
 
 @JvmInline
-value class Position(val value: Int = 1) {
+value class Position(val value: Int = 1) : Comparable<Position> {
     init {
         require(value >= 1) { "위치는 1보다 작을 수 없습니다." }
     }
 
     fun moved(): Position = Position(value + 1)
+
+    override fun compareTo(other: Position): Int {
+        return value - other.value
+    }
 }
