edf6c17af4c615c3b21ab3fe732747a46a78b796
Seop(Home Mac) <edenhazard5870@gmail.com>
2021-11-14 00:07:33 +0900

    refactor - 우승자 이름 나열 테스트 가능형태로 변경

--- src/main/kotlin/racingcar/view/ResultView.kt
+++ src/main/kotlin/racingcar/view/ResultView.kt
@@ -12,10 +12,14 @@ object ResultView {
     }
 
     fun winnerPrints(winners: List<Car>) {
-        val winnerNames = winners.joinToString {
+        val winnerNames = racingWinnerName(winners)
+        print("${winnerNames}가 최종 우승했습니다.")
+    }
+
+    internal fun racingWinnerName(winners: List<Car>): String {
+        return winners.joinToString {
             it.carName.name
         }
-        print("${winnerNames}가 최종 우승했습니다.")
     }
 
     private fun carPositionPrint(car: Car) {
