52afd224620307b9cf84633d08b9e0061a4c6a11
Seop(Home Mac) <edenhazard5870@gmail.com>
2021-11-13 22:49:18 +0900

    refactor - 메서드 네이밍을 통해 하려고 하는 행위 명시

--- src/main/kotlin/racingcar/service/Race.kt
+++ src/main/kotlin/racingcar/service/Race.kt
@@ -1,11 +1,16 @@
 package racingcar.service
 
+import racingcar.domain.Car
 import racingcar.domain.Cars
 import racingcar.domain.MovingStrategy
 
 class Race(registerCarName: String, private val moveCarNumber: Int) {
 
     val cars = Cars(registerCarName)
+
+    fun findWinner(): List<Car> {
+        return cars.winners()
+    }
 
     fun confirmMoveOfCars(movingStrategy: MovingStrategy) {
         repeat(moveCarNumber) {
