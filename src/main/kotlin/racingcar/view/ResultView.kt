70594f791f2a096cf9b72644b6beda57d74bc72f
Seop(Home Mac) <edenhazard5870@gmail.com>
2021-11-16 07:16:34 +0900

    refactor - 이름 합치기 책임 위임으로 인한 리팩터링

--- src/main/kotlin/racingcar/view/ResultView.kt
+++ src/main/kotlin/racingcar/view/ResultView.kt
@@ -1,6 +1,7 @@
 package racingcar.view
 
 import racingcar.domain.Car
+import racingcar.domain.WinnerNames
 
 object ResultView {
 
@@ -11,15 +12,13 @@ object ResultView {
         }
     }
 
-    fun winnerPrints(winners: List<String>) {
-        val winnerNames = racingWinnerName(winners)
-        print("${winnerNames}가 최종 우승했습니다.")
+    fun winnerPrints(winnerNames: List<String>) {
+        val result = racingWinnerName(winnerNames)
+        print("${result}가 최종 우승했습니다.")
     }
 
-    internal fun racingWinnerName(winners: List<String>): String {
-        return winners.joinToString {
-            it
-        }
+    private fun racingWinnerName(winnerNames: List<String>): String {
+        return WinnerNames.combineWinnerNames(winnerNames)
     }
 
     private fun carPositionPrint(car: Car) {
