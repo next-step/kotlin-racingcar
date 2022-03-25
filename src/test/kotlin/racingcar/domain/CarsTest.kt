c8a51309e31618c12db95954849ebe08105109b4
Suhyeon Kim <wisemuji@gmail.com>
2022-03-25 18:20:15 +0900

    test: add tests for Cars class

--- src/test/kotlin/racingcar/domain/CarsTest.kt
+++ src/test/kotlin/racingcar/domain/CarsTest.kt
@@ -7,30 +7,41 @@ internal class CarsTest {
 
     @Test
     internal fun `자동차 이름들로 Cars 객체를 생성할 수 있다`() {
-        val cars = Cars(listOf("가", "나", "다"))
-        assertThat(cars).containsAnyOf(Car("가"), Car("나"), Car("다"))
+        val rawNames = listOf("가", "나", "다")
+        val cars = Cars(CarNames(rawNames))
+        assertThat(cars).hasSameSizeAs(rawNames)
+    }
+
+    @Test
+    internal fun `자동차들은 경주를 할 수 있다`() {
+        val cars = Cars(
+            cars = listOf(
+                Car(name = "가", accelerator = MoveCarAccelerator)
+            )
+        )
+        assertThat(cars).allSatisfy {
+            assertThat(it.movedDistance()).isEqualTo(0)
+        }
+        cars.race()
+        assertThat(cars).allSatisfy {
+            assertThat(it.movedDistance()).isEqualTo(1)
+        }
     }
 
     @Test
     internal fun `가장 멀리 간 자동차가 우승자고 우승자는 한 명 이상일 수 있다`() {
-        val cars = Cars(listOf("가", "나", "다"))
-        cars[0].moveOrStop(shouldMove = true)
-        cars[1].moveOrStop(shouldMove = true)
-        cars[2].moveOrStop(shouldMove = false)
+        val cars = Cars(
+            cars = listOf(
+                Car(name = "가", accelerator = MoveCarAccelerator),
+                Car(name = "나", accelerator = MoveCarAccelerator),
+                Car(name = "다", accelerator = StopCarAccelerator),
+            )
+        )
+        cars.race()
         assertThat(cars.winners())
-            .containsExactly(Car("가"), Car("나"))
-    }
-
-    @Test
-    internal fun `moveOrStopAll 테스트`() {
-        val cars = Cars(listOf("가", "나", "다"))
-        assertThat(cars.apply { moveOrStopAll { false } })
-            .allSatisfy {
-                assertThat(it.position(1)).isEqualTo(0)
-            }
-        assertThat(cars.apply { moveOrStopAll { true } })
-            .allSatisfy {
-                assertThat(it.position(2)).isEqualTo(1)
-            }
+            .containsExactly(
+                Car(name = "가", accelerator = MoveCarAccelerator),
+                Car(name = "나", accelerator = MoveCarAccelerator)
+            )
     }
 }
