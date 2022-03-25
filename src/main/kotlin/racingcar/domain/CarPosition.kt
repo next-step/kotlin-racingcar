8eb26532219231a759949d5a2ecc040fbd465506
Suhyeon Kim <wisemuji@gmail.com>
2022-03-25 18:08:24 +0900

    fix: implement CarPosition

--- src/main/kotlin/racingcar/domain/CarPosition.kt
+++ src/main/kotlin/racingcar/domain/CarPosition.kt
@@ -5,6 +5,6 @@ data class CarPosition(private var position: Int = 0) {
     fun get() = position
 
     fun applyAction(carAction: CarAction) {
-        
+        position += carAction.distance
     }
 }
