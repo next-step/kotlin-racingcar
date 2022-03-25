c2c67cb185d6cda9c5e55193c20f2b440ff9bd97
Suhyeon Kim <wisemuji@gmail.com>
2022-03-25 18:24:08 +0900

    feat: add RacingCarGame class

--- src/main/kotlin/racingcar/domain/RacingCarGame.kt
+++ src/main/kotlin/racingcar/domain/RacingCarGame.kt
@@ -1,20 +1,12 @@
 package racingcar.domain
 
-class RacingCarGame(private val cars: Cars) {
+class RacingCarGame(
+    private val cars: Cars,
+    private val attemptCount: Int,
+    private val onEachRace: (Cars) -> Unit
+) {
 
-    private fun shouldMove() = MOVE_CONDITION.random() >= MOVE_CONDITION_STANDARD
-
-    fun play(attemptCount: Int, shouldMove: () -> Boolean = ::shouldMove): Cars {
-        repeat(attemptCount) {
-            cars.moveOrStopAll(shouldMove)
-        }
+    fun play(): Cars {
         return cars
-    }
-
-    companion object {
-        private const val MOVE_CONDITION_START = 0
-        private const val MOVE_CONDITION_END = 9
-        private const val MOVE_CONDITION_STANDARD = 4
-        private val MOVE_CONDITION = (MOVE_CONDITION_START..MOVE_CONDITION_END)
     }
 }
