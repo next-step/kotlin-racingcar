52e03c45c0bd94549013353a8b9305b04362b0bd
Suhyeon Kim <wisemuji@gmail.com>
2022-04-16 19:39:05 +0900

    refactor: print CarsHistory in controller

--- src/main/kotlin/racingcar/Play.kt
+++ src/main/kotlin/racingcar/Play.kt
@@ -1,6 +1,8 @@
 package racingcar
 
+import racingcar.domain.racing.CarsHistory
 import racingcar.domain.racing.RacingCarGame
+import racingcar.domain.racing.Winners
 import racingcar.view.InputView
 import racingcar.view.ResultView
 
@@ -8,12 +10,11 @@ fun main() {
     val inputView = InputView()
     val carNames = inputView.carNames()
     val attemptCount = inputView.attemptCount()
-    val resultView = ResultView().apply { printTitle() }
-    RacingCarGame(
+    val game = RacingCarGame(
         carNames = carNames,
-        attemptCount = attemptCount,
-        onEachRace = resultView::printEachTurn
+        attemptCount = attemptCount
     )
-        .play()
-        .let(resultView::printWinner)
+    val carsHistory: CarsHistory = game.play()
+    val winners = Winners(carsHistory)
+    ResultView().printResult(carsHistory = carsHistory, winners = winners)
 }
--- src/main/kotlin/racingcar/view/ResultView.kt
+++ src/main/kotlin/racingcar/view/ResultView.kt
@@ -2,20 +2,31 @@ package racingcar.view
 
 import racingcar.domain.car.Car
 import racingcar.domain.car.Cars
+import racingcar.domain.racing.CarsHistory
+import racingcar.domain.racing.Winners
 
 class ResultView {
 
-    fun printTitle() {
+    fun printResult(carsHistory: CarsHistory, winners: Winners) {
+        printTitle()
+        printCarsHistory(carsHistory)
+        printWinners(winners)
+    }
+
+    private fun printCarsHistory(carsHistory: CarsHistory) {
+        carsHistory.forEach(::printEachTurn)
+    }
+
+    private fun printTitle() {
         println("실행 결과")
     }
 
-    fun printEachTurn(cars: Cars) {
-        cars.forEach { printCar(it) }
+    private fun printEachTurn(cars: Cars) {
+        cars.forEach(::printCar)
         println()
     }
 
-    fun printWinner(cars: Cars) {
-        val winners = cars.winners()
+    private fun printWinners(winners: List<Car>) {
         println("${winners.joinToString(", ") { it.name }}가 최종 우승했습니다.")
     }
 
