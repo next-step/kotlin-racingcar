3b743a6d02c666ba0c17afee58748e5b7cf0c269
Suhyeon Kim <wisemuji@gmail.com>
2022-04-16 19:36:44 +0900

    feat: impl CarsHistory

--- src/main/kotlin/racingcar/domain/racing/CarsHistory.kt
+++ src/main/kotlin/racingcar/domain/racing/CarsHistory.kt
@@ -5,14 +5,15 @@ import racingcar.domain.car.Cars
 
 class CarsHistory(private val carsHistory: MutableList<Cars>) : List<Cars> by carsHistory {
 
-    val latest: Cars? get() = carsHistory[0]
+    val latest: Cars? get() = carsHistory.getOrNull(carsHistory.lastIndex)
 
-    val maxDrivenCars: List<Car> get() = listOf()
+    val maxDrivenCars: List<Car> get() = latest?.filter { it.movedDistance == maxPosition } ?: listOf()
 
-    private val maxPosition: Int get() = 0
+    private val maxPosition: Int get() = latest?.maxOf { it.movedDistance } ?: 0
 
     constructor() : this(mutableListOf<Cars>())
 
     fun add(cars: Cars) {
+        carsHistory.add(cars)
     }
 }
