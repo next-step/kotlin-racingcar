9b857ec637e93efe6b66efd9487b5f870dcae49b
Suhyeon Kim <wisemuji@gmail.com>
2022-04-17 14:45:37 +0900

    fix: remove wildcard imports

--- src/test/kotlin/racingcar/domain/racing/WinnersTest.kt
+++ src/test/kotlin/racingcar/domain/racing/WinnersTest.kt
@@ -1,6 +1,6 @@
 package racingcar.domain.racing
 
-import org.assertj.core.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import racingcar.domain.car.Car
 import racingcar.domain.car.CarAction
