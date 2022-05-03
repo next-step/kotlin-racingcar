6bea14dec1bc55809931c5ece6aa7114b794ec3b
kyle.um(엄기훈)/kakao <kyle.um@kakaocorp.com>
2022-05-03 22:19:57 +0900

    study: add kotlin basic

--- src/test/kotlin/study/StringKoTest.kt
+++ src/test/kotlin/study/StringKoTest.kt
@@ -1,4 +1,14 @@
 package study
 
-class StringKoTest {
+import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.matchers.shouldBe
+
+
+class StringKoTest : AnnotationSpec() {
+    @Test
+    fun isEmpty() {
+        "".isEmpty() shouldBe true
+        " ".isEmpty() shouldBe false
+        "a".isEmpty() shouldBe false
+    }
 }
--- src/test/kotlin/study/StringTest.kt
+++ src/test/kotlin/study/StringTest.kt
@@ -1,4 +1,27 @@
 package study
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
 class StringTest {
+    @Test
+    fun isEmpty() {
+        assertThat("".isEmpty()).isTrue
+        assertThat(" ".isEmpty()).isFalse
+        assertThat("a".isEmpty()).isFalse
+    }
+
+    @Test
+    fun isBlank() {
+        assertThat("".isBlank()).isTrue
+        assertThat(" ".isBlank()).isTrue
+        assertThat("\r\n".isBlank()).isTrue
+    }
+
+    @Test
+    fun string() {
+        assertThat("kihun"[0]).isEqualTo('k')
+        assertThat("kihun"[1]).isEqualTo('i')
+        assertThat("kihun"[2]).isEqualTo('h')
+    }
 }
