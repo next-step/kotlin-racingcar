12bc6ac0b50ffdb2d762f6d6c1b3fc1a968e3952
Gilssung <45102979+Gilssung@users.noreply.github.com>
2022-05-03 22:51:09 +0900

    Step1 (#401)
    * study: 코틀린 기초
    
    * study: 코틀린 기초
    
    * study: 코틀린 기초
    
    Co-authored-by: qyu.st <pcg84637857K!>

---
+++ src/test/kotlin/study/PersonTest.kt
@@ -0,0 +1,42 @@
+package study
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class PersonTest {
+
+    @Test
+    fun `named arguments`() {
+        val people = listOf(
+            Person("qyu", 34, "큐"),
+            Person("qyu", 34, null),
+            Person("qyu", 34, "큐큐")
+        )
+        assertThat(people[0].name).isEqualTo("qyu")
+        assertThat(people[1].age).isEqualTo(34)
+        assertThat(people[2].nickname).isEqualTo("큐큐")
+    }
+
+    @Test
+    fun `널 타입`() {
+        val person = Person("qyu", 34, null)
+        assertThat(person.name).isEqualTo("qyu")
+        assertThat(person.age).isEqualTo(34)
+        assertThat(person.nickname).isNull()
+    }
+
+    @Test
+    fun `기본 인자`() {
+        val person = Person("qyu", 34)
+        assertThat(person.name).isEqualTo("qyu")
+        assertThat(person.age).isEqualTo(34)
+        assertThat(person.nickname).isEqualTo("qyu")
+    }
+
+    @Test
+    fun `데이터 클래스`() {
+        val person1 = Person("qyu", 34, null)
+        val person2 = Person("qyu", 34, null)
+        assertThat(person1).isEqualTo(person2)
+    }
+}
