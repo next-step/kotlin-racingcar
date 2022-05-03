574490382ee7708434b2bf2b2afadcdb06c22fbc
Juice <3221ju@gmail.com>
2022-05-04 00:15:26 +0900

    testcase: 계산기 기본 테스트 케이스 추가

---
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -0,0 +1,29 @@
+package calculator
+
+import org.junit.jupiter.api.Test
+
+class CalculatorTest {
+    @Test
+    fun `입력값이 null인 경우 에러가 발생한다`() {
+    }
+
+    @Test
+    fun `입력값이 공백인 경우 에러가 발생한다`() {
+    }
+
+    @Test
+    fun `입력값에 사칙연산이 아닌 기호가 포함된 경우 에러가 발생한다`() {
+    }
+
+    @Test
+    fun `입력값에 연산자가 연속으로 두번 나온 경우 에러가 발생한다`() {
+    }
+
+    @Test
+    fun `2 + 3 * 4  ÷ 2 를 입력하면 10이 나온다`() {
+    }
+
+    @Test
+    fun `공백이 포함된 입력 값인 "1  +  1   " 이 입력된 경우 2가 나온다`() {
+    }
+}
