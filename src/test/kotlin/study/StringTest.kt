7e9b04f9a7649214996f95a2642a9f88a30ac4ae
Gyeonglok Kim <goodboy302@naver.com>
2022-05-03 23:52:21 +0900

    test: Kotlin basic 학습 테스트 추가 (#392)
    반갑습니다 경록님~ 
    자동차 경주 미션 리뷰를 담당하게된 박찬인이라고 해요!
    
    미션 화이팅입니다!
    
    미션진행을 위해서 바로 머지하겠습니다!

---
+++ src/test/kotlin/study/StringTest.kt
@@ -0,0 +1,26 @@
+package study
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class StringTest : StringSpec({
+    "isEmpty 함수를 테스트한다" {
+        val sut = "".isEmpty()
+
+        sut shouldBe true
+    }
+
+    "널 타입"{
+        val person = Person("김경록", 20, null)
+
+        person.name shouldBe "김경록"
+        person.nickname shouldBe null
+    }
+
+    "기본 인자"{
+        val person = Person("김경록", 20)
+
+        person.name shouldBe "김경록"
+        person.nickname shouldBe "김경록"
+    }
+})
