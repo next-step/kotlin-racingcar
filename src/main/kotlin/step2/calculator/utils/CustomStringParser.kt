033ddd1a9a741e85bbceaffc6fbf86c5fdf0c9ef
kwanguuuu <kwangyu326@gmail.com>
2022-05-04 08:48:52 +0900

    [step2] fix: 패키지 변경 ( step 별로 구분 )

--- src/main/kotlin/step2/calculator/utils/CustomStringParser.kt
+++ src/main/kotlin/step2/calculator/utils/CustomStringParser.kt
@@ -1,4 +1,4 @@
-package utils
+package step2.calculator.utils
 
 class CustomStringParser {
 
--- src/test/kotlin/step2/calculator/StringParseTest.kt
+++ src/test/kotlin/step2/calculator/StringParseTest.kt
@@ -1,11 +1,11 @@
-package calculator
+package step2.calculator
 
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
-import utils.CustomStringParser
+import step2.calculator.utils.CustomStringParser
 
 class StringParseTest {
     @Test
