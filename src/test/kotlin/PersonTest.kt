4ae7f2560e047f9e30c9fb2ae9c5395dc3cf1ea8
Hyungsoo Jung <41500212+jngcii@users.noreply.github.com>
2022-05-03 22:37:38 +0900

    Person 클래스 및 단위테스트작성 (#396)

---
+++ src/test/kotlin/PersonTest.kt
@@ -0,0 +1,30 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class PersonTest {
+
+    @Test
+    fun `nullable types`() {
+        val person = Person("정형수", 31, null)
+
+        assertThat(person.name).isEqualTo("정형수")
+        assertThat(person.age).isEqualTo(31)
+        assertThat(person.nickname).isNull()
+    }
+
+    @Test
+    fun `default arguments`() {
+        val person = Person("정형수")
+        assertThat(person.name).isEqualTo("정형수")
+        assertThat(person.age).isEqualTo(31)
+        assertThat(person.nickname).isNull()
+    }
+
+    @Test
+    fun `data classes`() {
+        val person1 = Person("정형수", 31)
+        val person2 = Person("정형수", 31)
+        assertThat(person1).isEqualTo(person2)
+    }
+
+}
