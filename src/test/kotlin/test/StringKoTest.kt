ad24160322107974010ae87571819d97fc67dc37
kwanguuuu <kwangyu326@gmail.com>
2022-05-03 22:22:49 +0900

    feat: 학습 테스트 작성 ( String, StringKoTest, Person )

---
+++ src/test/kotlin/test/StringKoTest.kt
@@ -0,0 +1,13 @@
+package test
+
+import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.matchers.shouldBe
+
+internal class StringKoTest : AnnotationSpec() {
+
+    @Test
+    fun isEmpty() {
+        "".isEmpty() shouldBe true
+
+    }
+}
