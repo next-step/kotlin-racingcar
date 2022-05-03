4f37a031c2ceb4a023898e49722aa76483fd67eb
hoori <freedom09@inflab.com>
2022-05-03 22:41:31 +0900

    study: step1

---
+++ src/test/kotlin/study/PersonTest.kt
@@ -0,0 +1,41 @@
+package study
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class PersonTest {
+    @Test
+    fun `named args`() {
+        val people = listOf(
+            Person("구자유", 37, "후리"),
+            Person("구자유", 37, nickname = "후리"),
+            Person(name = "구자유", age = 37, "후리"),
+        )
+        assertThat(people[0].name).isEqualTo("구자유")
+        assertThat(people[1].name).isEqualTo("구자유")
+        assertThat(people[2].name).isEqualTo("구자유")
+    }
+
+    @Test
+    fun `null type`() {
+        val person = Person("구자유", 37, null)
+        assertThat(person.name).isEqualTo("구자유")
+        assertThat(person.age).isEqualTo(37)
+        assertThat(person.nickname).isNull()
+    }
+
+    @Test
+    fun `default args`() {
+        val person = Person("구자유", 37)
+        assertThat(person.name).isEqualTo("구자유")
+        assertThat(person.age).isEqualTo(37)
+        assertThat(person.nickname).isNull()
+    }
+
+    @Test
+    internal fun `data class`() {
+        val person1 = Person("구자유", 37)
+        val person2 = Person("구자유", 37)
+        assertThat(person1).isEqualTo(person2)
+    }
+}
