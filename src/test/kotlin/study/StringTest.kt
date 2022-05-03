7978d66a6b5444102c046f63a1b8bc081781e607
Juatae-Kim <jutaekim@sk.com>
2022-05-03 22:20:55 +0900

    study: kotlin basic and test with junit

---
+++ src/test/kotlin/study/StringTest.kt
@@ -0,0 +1,30 @@
+package study
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class StringTest {
+
+    @Test
+    fun isEmpty(){
+        assertThat("".isEmpty()).isTrue
+        assertThat(" ".isEmpty()).isFalse
+        assertThat("a".isEmpty()).isFalse
+    }
+
+    @Test
+    fun isBlank(){
+        assertThat("".isBlank()).isTrue
+        assertThat(" ".isBlank()).isTrue
+        assertThat("a".isBlank()).isFalse
+    }
+
+    @Test
+    fun string(){
+        assertThat("json"[0]).isEqualTo('j')
+        assertThat("json"[1]).isEqualTo('s')
+        assertThat("json"[2]).isEqualTo('o')
+    }
+
+    
+}
