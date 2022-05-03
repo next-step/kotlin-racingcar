bba1691b3528ad0376c1bc522a5d320cb43f93c5
keira <keira.j@kakaocorp.com>
2022-05-03 22:48:47 +0900

    step 1. study hard

---
+++ src/test/kotlin/StringTest.kt
@@ -0,0 +1,17 @@
+package study
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class StringTest {
+
+    @Test
+    fun isEmpty() {
+        assertThat("".isEmpty()).isTrue
+    }
+
+    @Test
+    fun isBlank() {
+        assertThat(" ".isBlank()).isTrue
+    }
+}
