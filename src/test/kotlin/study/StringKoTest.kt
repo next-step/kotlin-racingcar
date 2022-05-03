68ae5be3fad795ff7d8c47a9bcfdedc5eb74569a
glennsm <54771120+glennsm@users.noreply.github.com>
2022-05-04 00:32:43 +0900

    study: add kotlin basic (#421)

---
+++ src/test/kotlin/study/StringKoTest.kt
@@ -0,0 +1,21 @@
+package study
+
+import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.matchers.shouldBe
+
+class StringKoTest : AnnotationSpec() {
+
+    @Test
+    fun isBlank() {
+        "".isBlank() shouldBe true
+        " ".isBlank() shouldBe true
+    }
+    @Test
+    fun string() {
+        "jason"[0] shouldBe 'j'
+        "jason"[1] shouldBe 'a'
+        "jason"[2] shouldBe 's'
+        "jason"[3] shouldBe 'o'
+        "jason"[4] shouldBe 'n'
+    }
+}
