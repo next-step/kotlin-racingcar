9e447a1df702d90d950af3d21cf4bcc5e7812e93
keiraJ <37860683+keiraJ@users.noreply.github.com>
2022-05-04 20:34:22 +0900

    step 1. study hard (#415)
    Co-authored-by: keira <keira.j@kakaocorp.com>

---
+++ src/test/kotlin/StringKoTest.kt
@@ -0,0 +1,12 @@
+import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.matchers.shouldBe
+
+class StringKoTest : AnnotationSpec() {
+
+    @Test
+    fun isEmpty() {
+        "".isEmpty() shouldBe true
+        " ".isEmpty() shouldBe false
+        "a".isEmpty() shouldBe false
+    }
+}
