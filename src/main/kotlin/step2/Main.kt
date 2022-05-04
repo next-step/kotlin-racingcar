12208b33e505d9adf7de8823c8771bc368e4f9a5
austen.lee <austen.lee@kakaocorp.com>
2022-05-04 22:34:11 +0900

    step2 refactor : main에서 실행하는 검사에 Exception을 같이 전달하도록 변경

--- src/main/kotlin/step2/Main.kt
+++ src/main/kotlin/step2/Main.kt
@@ -2,9 +2,11 @@ package step2
 
 import step2.inspector.mustNot
 
+const val INVALID_INPUT_ERROR_MESSAGE = "\n적합하지 않은 입력값이 전달되었습니다.\n입력값은 공백으로만 이루어져서는 안 됩니다."
+
 fun main() {
     val expression = readlnOrNull()
-    mustNot {
+    mustNot(IllegalArgumentException(INVALID_INPUT_ERROR_MESSAGE)) {
         expression.isNullOrBlank()
     }
 }
