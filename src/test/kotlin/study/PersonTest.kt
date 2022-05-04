189022075421e6f0ec81b54e8ac98c6e85aa259d
yohan <yohan@inflab.com>
2022-05-04 09:00:36 +0900

    study: Korrect!

---
+++ src/test/kotlin/study/PersonTest.kt
@@ -0,0 +1,46 @@
+package study
+
+import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.matchers.equality.shouldBeEqualToComparingFields
+import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+
+class PersonTest : AnnotationSpec() {
+
+    @Test
+    fun `named args`() {
+        val people = listOf(
+            Person("신병주", 28, "요한"),
+            Person("신병주", 28, nickname = "요한"),
+            Person(name = "신병주", age = 28, nickname = "요한"),
+        )
+
+        people[0].name shouldBe "신병주"
+        people[1].name shouldBe "신병주"
+        people[2].name shouldBe "신병주"
+    }
+
+    @Test
+    fun `null type`() {
+        val person = Person("신병주", 28, null)
+        person.name shouldBe "신병주"
+        person.age shouldBe 28
+        person.nickname.shouldBeNull()
+    }
+
+    @Test
+    fun `default args`() {
+        val person = Person("신병주", 28)
+        person shouldBe "신병주"
+        person shouldBe 28
+        person.nickname shouldNotBe null
+    }
+
+    @Test
+    fun `data class`() {
+        val person1 = Person("신병주", 28)
+        val person2 = Person("신병주", 28)
+        person1.shouldBeEqualToComparingFields(person2)
+    }
+}
