801d08f2e6f68dcdc544e4a309369d6ab6b4af9c
austen.lee <austen.lee@kakaocorp.com>
2022-05-04 23:46:42 +0900

    step2 feat : 기존 숫자에 calculation을 적용하는 compensates 추가

--- src/main/kotlin/step2/calculator/Calculation.kt
+++ src/main/kotlin/step2/calculator/Calculation.kt
@@ -3,4 +3,10 @@ package step2.calculator
 class Calculation(
     private val operator: Operator,
     private val number: Double
-)
+) {
+    fun applyTo(accumulator: Double) = operator.operate(accumulator, number)
+}
+
+infix fun Double.compensates(calculation: Calculation): Double {
+    return calculation.applyTo(this)
+}
