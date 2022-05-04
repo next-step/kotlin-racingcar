f3a6be33d29405b002b1f79933f2b7c60fcc48bf
kwanguuuu <kwangyu326@gmail.com>
2022-05-04 12:46:22 +0900

    [step2] refactor: parser refactoring

--- src/main/kotlin/step2/calculator/utils/CustomStringParser.kt
+++ src/main/kotlin/step2/calculator/utils/CustomStringParser.kt
@@ -4,19 +4,15 @@ class CustomStringParser {
 
     companion object {
         fun parse(str: String?): List<String> {
-            validate(str)
+            if (str.isNullOrBlank()) {
+                throw IllegalArgumentException()
+            }
 
-            return str!!.split(DELIMITER)
+            return str.split(DELIMITER)
         }
 
         fun removeEmptyStrings(list: List<String>): List<String> {
             return list.filter { it.isNotBlank() }
-        }
-
-        private fun validate(str: String?) {
-            if (str.isNullOrBlank()) {
-                throw IllegalArgumentException()
-            }
         }
 
         private const val DELIMITER = " "
