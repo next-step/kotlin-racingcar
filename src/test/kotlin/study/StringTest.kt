6eae729926ff127a9929e5305750282e96889c77
Grandelwald <91423342+Grandelwald@users.noreply.github.com>
2022-05-04 13:32:43 +0900

    1단계-코틀린 기초 실습 (#429)
    * 1단계-코틀린 기초 실습
    
    * 1단계-코틀린 기초 : 명령어 입력 후 추가 커밋

---
+++ src/test/kotlin/study/StringTest.kt
@@ -0,0 +1,26 @@
+package study
+
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class StringTest {
+    @Test
+    fun isEmpty(){
+        assertThat("".isEmpty()).isTrue()
+    }
+
+    @Test
+    fun isBlank() {
+        assertThat("")
+    }
+
+    @Test
+    fun string() {
+        assertThat("jason"[0]).isEqualTo('j')
+        assertThat("jason"[1]).isEqualTo('a')
+        assertThat("jason"[2]).isEqualTo('s')
+        assertThat("jason"[3]).isEqualTo('o')
+        assertThat("jason"[4]).isEqualTo('n')
+    }
+}
