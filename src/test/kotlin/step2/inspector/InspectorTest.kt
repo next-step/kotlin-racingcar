a97fdc7402ec0f8e4fb0fe1378679e76098b2c19
austen.lee <austen.lee@kakaocorp.com>
2022-05-04 22:07:12 +0900

    step2 test : 테스트를 @ParameterizedTest와 @NullAndEmptySource를 사용하도록 수정

--- src/test/kotlin/step2/inspector/InspectorTest.kt
+++ src/test/kotlin/step2/inspector/InspectorTest.kt
@@ -1,16 +1,17 @@
 package step2.inspector
 
 import org.assertj.core.api.Assertions
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.NullAndEmptySource
 
 // Todo : JUnit 끝나면 Kotest로도 만들어 보자.
 class InspectorTest {
-    @Test
-    fun `null 여부 검사`() {
+    @ParameterizedTest
+    @NullAndEmptySource
+    fun `input이 null 혹은 empty인지 확인하고, null이나 empty가 맞다면 IllegalArgumentException을 던진다`(input: String?) {
         Assertions.assertThatThrownBy {
-            val expression: String? = null
             mustNot {
-                expression == null
+                input.isNullOrEmpty()
             }
         }.isInstanceOf(IllegalArgumentException::class.java)
     }
