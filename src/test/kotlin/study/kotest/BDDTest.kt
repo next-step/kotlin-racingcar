8bf19f9a0a7dcdebbc87bf3070470525f8b5bb68
leeyohan93 <good_1411@naver.com>
2022-05-04 23:00:33 +0900

    test: Kotest에 대한 학습테스트 진행

---
+++ src/test/kotlin/study/kotest/BDDTest.kt
@@ -0,0 +1,32 @@
+package study.kotest
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldHaveLength
+
+class BDDTest : BehaviorSpec({
+
+    given("숫자 1과 3 주어졌을 때") {
+        val firstNumber = 1
+        val secondNumber = 3
+        `when`("두 숫자를 + 연산을 수행하면") {
+            val result = firstNumber + secondNumber
+            then("결과 값은 4를 반환한다") {
+                result shouldBe 4
+            }
+        }
+    }
+
+    given("여러 세 글자 이름의 이 주어졌을 때") {
+        val names = listOf(
+            "sam",
+            "pam",
+            "tom"
+        )
+        then("길이는 3이다") {
+            names.forEach {
+                it.shouldHaveLength(3)
+            }
+        }
+    }
+})
---
+++ src/test/kotlin/study/kotest/CallbacksTest.kt
@@ -0,0 +1,20 @@
+package study.kotest
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.string.shouldHaveLength
+import java.util.*
+
+class CallbacksTest : FunSpec({
+
+    beforeEach {
+        println("Hello from $it")
+    }
+
+    test("sam 은 세 글자 이름이다") {
+        "sam".shouldHaveLength(3)
+    }
+
+    afterEach {
+        println("Goodbye from $it")
+    }
+})
