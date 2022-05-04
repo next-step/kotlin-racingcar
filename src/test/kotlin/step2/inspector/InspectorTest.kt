821fb7a95c7120361d831d8517c76d779b0a9e2a
austen.lee <austen.lee@kakaocorp.com>
2022-05-04 22:12:47 +0900

    step2 test : 정상적인 string일 경우 exception을 던지지 않는 test 추가.

--- src/test/kotlin/step2/inspector/InspectorTest.kt
+++ src/test/kotlin/step2/inspector/InspectorTest.kt
@@ -1,18 +1,30 @@
 package step2.inspector
 
 import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.NullAndEmptySource
+import org.junit.jupiter.params.provider.ValueSource
 
 // Todo : JUnit 끝나면 Kotest로도 만들어 보자.
 class InspectorTest {
     @ParameterizedTest
     @NullAndEmptySource
-    fun `input이 null 혹은 empty인지 확인하고, null이나 empty가 맞다면 IllegalArgumentException을 던진다`(input: String?) {
+    fun `input이 null이나 empty일 경우 IllegalArgumentException을 던진다`(input: String?) {
         Assertions.assertThatThrownBy {
             mustNot {
                 input.isNullOrEmpty()
             }
         }.isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1 * 2"])
+    fun `input이 null이나 empty가 아닐 경우 IllegalArgumentException을 던지지 않는다`(input: String?) {
+        assertDoesNotThrow {
+            mustNot {
+                input.isNullOrEmpty()
+            }
+        }
     }
 }
