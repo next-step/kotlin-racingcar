2bd918f89829962141d1c5f6c4816e4912d41052
austen.lee <austen.lee@kakaocorp.com>
2022-05-04 22:55:05 +0900

    step2 feat : 실행해야 할 연산자와 숫자를 보관하는 Calculation class와 test

---
+++ src/main/kotlin/step2/calculator/Calculation.kt
@@ -0,0 +1,6 @@
+package step2.calculator
+
+data class Calculation(
+    val operator: Operator,
+    val number: Int
+)
---
+++ src/test/kotlin/step2/calculator/CalculationTest.kt
@@ -0,0 +1,23 @@
+package step2.calculator
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class CalculationTest {
+    @Test
+    fun `Calculation 클래스가 프로퍼티를 제대로 보관하는가`() {
+        val plusCalculation = Calculation(
+            operator = Operator.PLUS,
+            number = 100
+        )
+        val minusCalculation = Calculation(
+            operator = Operator.MINUS,
+            number = 5
+        )
+
+        assertThat(plusCalculation.operator).isEqualTo(Operator.PLUS)
+        assertThat(plusCalculation.number).isEqualTo(100)
+        assertThat(minusCalculation.operator).isEqualTo(Operator.MINUS)
+        assertThat(minusCalculation.number).isEqualTo(5)
+    }
+}
