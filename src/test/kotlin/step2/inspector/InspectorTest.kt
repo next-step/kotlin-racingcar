a50b43ef356c299a5a43e4db3490368983b1cd8c
austen.lee <austen.lee@kakaocorp.com>
2022-05-04 22:01:51 +0900

    step2 feat : 해당 블록이 true일 경우 에러를 던지는 mustNot DSL 생성.

--- src/test/kotlin/step2/inspector/InspectorTest.kt
+++ src/test/kotlin/step2/inspector/InspectorTest.kt
@@ -1,5 +1,17 @@
 package step2.inspector
 
-class InspectorTest {
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
 
+// Todo : JUnit 끝나면 Kotest로도 만들어 보자.
+class InspectorTest {
+    @Test
+    fun `null 여부 검사`() {
+        Assertions.assertThatThrownBy {
+            val expression: String? = null
+            mustNot {
+                expression == null
+            }
+        }.isInstanceOf(IllegalArgumentException::class.java)
+    }
 }
