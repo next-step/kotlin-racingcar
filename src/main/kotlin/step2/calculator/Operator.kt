c33d2d0dedeca7d39db37ab85a79bf5922300d09
austen.lee <austen.lee@kakaocorp.com>
2022-05-04 23:21:00 +0900

    step2 feat : Operator에 연산 콜백 추가

--- src/main/kotlin/step2/calculator/Operator.kt
+++ src/main/kotlin/step2/calculator/Operator.kt
@@ -1,8 +1,19 @@
 package step2.calculator
 
-enum class Operator(val symbol: String) {
-    PLUS("+"),
-    MINUS("-"),
-    MULTIPLY("*"),
-    DIVIDE("/")
+enum class Operator(
+    val symbol: String,
+    val operate: (num1: Double, num2: Double) -> Double
+) {
+    PLUS("+", { num1, num2 ->
+        num1 + num2
+    }),
+    MINUS("-", { num1, num2 ->
+        num1 - num2
+    }),
+    MULTIPLY("*", { num1, num2 ->
+        num1 * num2
+    }),
+    DIVIDE("/", { num1, num2 ->
+        num1 / num2
+    })
 }
--- src/test/kotlin/step2/calculator/OperatorTest.kt
+++ src/test/kotlin/step2/calculator/OperatorTest.kt
@@ -16,4 +16,32 @@ class OperatorTest {
         assertThat(multiply).isEqualTo(Operator.MULTIPLY.symbol)
         assertThat(divide).isEqualTo(Operator.DIVIDE.symbol)
     }
+
+    @Test
+    fun `PLUS Operator의 operate를 통해 두 수를 더할 수 있다`() {
+        val result = Operator.PLUS.operate(1.0, 10.0)
+
+        assertThat(result).isEqualTo(1.0 + 10.0)
+    }
+
+    @Test
+    fun `MINUS Operator의 operate를 통해 두 수를 뺄 수 있다`() {
+        val result = Operator.MINUS.operate(10.0, 5.0)
+
+        assertThat(result).isEqualTo(10.0 - 5.0)
+    }
+
+    @Test
+    fun `MULTIPLY Operator의 operate를 통해 두 수를 곱할 수 있다`() {
+        val result = Operator.MULTIPLY.operate(2.0, 10.0)
+
+        assertThat(result).isEqualTo(2.0 * 10.0)
+    }
+
+    @Test
+    fun `DIVIDE Operator의 operate를 통해 두 수를 나눌 수 있다`() {
+        val result = Operator.DIVIDE.operate(10.0, 2.0)
+
+        assertThat(result).isEqualTo(10.0 / 2.0)
+    }
 }
