9add8e5a6c76a254e6d15ddda5bf568ddf02e651
jojoldu <jojoldu@gmail.com>
2022-05-06 01:02:16 +0900

    연산자 검색 (find) 추가

--- src/main/kotlin/step2/Operator.kt
+++ src/main/kotlin/step2/Operator.kt
@@ -1,8 +1,18 @@
 package step2
 
-enum class Operator(expr: String, var evaluate: (num1: Int, num2: Int) -> Int) {
-    PLUS("+", {num1: Int, num2: Int -> num1 + num2 }),
-    MINUS("-", {num1: Int, num2: Int -> num1 - num2 }),
-    MULTIPLY("*", {num1: Int, num2: Int -> num1 * num2 }),
-    DIVIDE("/", {num1: Int, num2: Int -> num1 / num2 });
+enum class Operator(expr: String, evaluate: (num1: Int, num2: Int) -> Int) {
+    PLUS("+", { num1: Int, num2: Int -> num1 + num2 }),
+    MINUS("-", { num1: Int, num2: Int -> num1 - num2 }),
+    MULTIPLY("*", { num1: Int, num2: Int -> num1 * num2 }),
+    DIVIDE("/", { num1: Int, num2: Int -> num1 / num2 });
+
+    var expr: String = expr
+        private set
+    var evaluate = evaluate
+        private set
+    companion object {
+        fun find(target: String): Operator? {
+            return Operator.values().find { it.expr == target }
+        }
+    }
 }
