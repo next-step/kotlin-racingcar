bbc221f5c2296314afaec8731506e8197b242783
link-js <link.js@kakaocorp.com>
2022-05-05 18:11:14 +0900

    test : 입력값이 null이거나 빈 공백 문자일 경우 IllegalArgumentException 테스트 코드 추가

--- src/test/kotlin/step2/StringCalculatorTest.kt
+++ src/test/kotlin/step2/StringCalculatorTest.kt
@@ -1,9 +1,22 @@
 package step2
 
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
+import org.junit.jupiter.api.Test
+
 /**
  * 문자열 사칙 연산 계산기 테스트
  * Created by Jaesungchi on 2022.05.05..
  */
 
 class StringCalculatorTest {
+    @Test
+    fun `입력값이 null이거나 빈 공백 문자일 경우 IllegalArgumentException`() {
+        val calculator = StringCalculator()
+        assertThatIllegalArgumentException().isThrownBy {
+            calculator.startCalculate(null)
+        }
+        assertThatIllegalArgumentException().isThrownBy {
+            calculator.startCalculate("")
+        }
+    }
 }
