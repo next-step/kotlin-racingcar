cf4e871d821d73210f443d6b6fca83d0a090ff07
jojoldu <jojoldu@gmail.com>
2022-05-05 23:59:20 +0900

    - 연산자 추가

--- src/main/kotlin/step2/Operator.kt
+++ src/main/kotlin/step2/Operator.kt
@@ -1,13 +1,6 @@
 package step2
 
-enum class Operator {
-    PLUS("+", {num1: Int, num2: Int -> num1 + num2 });
-
-    private val mark: String
-    var evaluate: (num1: Int, num2: Int) -> Int
-
-    constructor(expr: String, evaluate: (num1: Int, num2: Int) -> Int) {
-        this.mark = expr
-        this.evaluate = evaluate
-    }
+enum class Operator(expr: String, var evaluate: (num1: Int, num2: Int) -> Int) {
+    PLUS("+", {num1: Int, num2: Int -> num1 + num2 }),
+    MINUS("-", {num1: Int, num2: Int -> num1 - num2 });
 }
