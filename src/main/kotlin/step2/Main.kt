9810526fe9440d42cf9199de122d5b488b7fb1a3
austen.lee <austen.lee@kakaocorp.com>
2022-05-05 20:58:52 +0900

    step2 refactor : private으로 닫아도 문제 없음

--- src/main/kotlin/step2/Main.kt
+++ src/main/kotlin/step2/Main.kt
@@ -4,7 +4,7 @@ import step2.calculator.Calculator
 import step2.calculator.ExpressionParser
 import step2.inspector.mustNot
 
-const val INVALID_INPUT_ERROR_MESSAGE = "\n적합하지 않은 입력값이 전달되었습니다.\n입력값은 공백으로만 이루어져서는 안 됩니다."
+private const val INVALID_INPUT_ERROR_MESSAGE = "\n적합하지 않은 입력값이 전달되었습니다.\n입력값은 공백으로만 이루어져서는 안 됩니다."
 
 fun main() {
     val expression = readlnOrNull()
