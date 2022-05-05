0685473daeada1cf0e3211fbc5b9cab591a18d61
austen.lee <austen.lee@kakaocorp.com>
2022-05-05 23:19:06 +0900

    step2 fix : Operator에서 상수 안 쓰고 있던 부분 수정

--- src/main/kotlin/step2/calculator/Operator.kt
+++ src/main/kotlin/step2/calculator/Operator.kt
@@ -4,16 +4,16 @@ enum class Operator(
     val symbol: String,
     val operate: (num1: Double, num2: Double) -> Double
 ) {
-    PLUS("+", { num1, num2 ->
+    PLUS(Operator.SYMBOL_PLUS, { num1, num2 ->
         num1 + num2
     }),
-    MINUS("-", { num1, num2 ->
+    MINUS(Operator.SYMBOL_MINUS, { num1, num2 ->
         num1 - num2
     }),
-    MULTIPLY("*", { num1, num2 ->
+    MULTIPLY(Operator.SYMBOL_MULTIPLY, { num1, num2 ->
         num1 * num2
     }),
-    DIVIDE("/", { num1, num2 ->
+    DIVIDE(Operator.SYMBOL_DIVIDE, { num1, num2 ->
         num1 / num2
     });
 
