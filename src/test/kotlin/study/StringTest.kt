4056794f130d6beb11ce977c7313e17cc175f3ca
link-js <link.js@kakaocorp.com>
2022-05-05 17:47:36 +0900

    refactor(test) : String.Split()에 대한 학습테스트코드 작성

--- src/test/kotlin/study/StringTest.kt
+++ src/test/kotlin/study/StringTest.kt
@@ -26,4 +26,24 @@ class StringTest {
         assertThat("jason"[3]).isEqualTo('o')
         assertThat("jason"[4]).isEqualTo('n')
     }
+
+    @Test
+    fun `spilt() 학습테스트`() {
+        val normalString = "분리가 어떻게 될까요?"
+        assertThat(normalString.split(" ").size).isEqualTo(3)
+
+        val lastWordIsSpaceString = "마지막 문구가 공백이라면? "
+        assertThat(lastWordIsSpaceString.split(" ").size).isEqualTo(4)
+        assertThat(lastWordIsSpaceString.split(" ").last()).isEqualTo("")
+
+        val firstWordIsSpaceString = " 첫번째 문구가 공백이라면?"
+        assertThat(firstWordIsSpaceString.split(" ").size).isEqualTo(4)
+        assertThat(firstWordIsSpaceString.split(" ").first()).isEqualTo("")
+
+        val emptyString = "" //공백일때 나누면 몇개일까?
+        assertThat(emptyString.split(" ").size).isEqualTo(1)
+
+        val nullString: String? = null //null을 split하면 몇개일까?
+        assertThat(nullString?.split(" ")?.size).isEqualTo(null)
+    }
 }
