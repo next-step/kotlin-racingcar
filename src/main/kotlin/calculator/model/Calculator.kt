42596d0c341ee1916ae65ef5bf6f7fae4ff135fd
YooInKeun <keun0390@naver.com>
2022-05-06 00:52:12 +0900

    feat: 수식을 전달 받아 계산하는 기능 및 테스트 코드 추가

--- src/main/kotlin/calculator/model/Calculator.kt
+++ src/main/kotlin/calculator/model/Calculator.kt
@@ -18,7 +18,28 @@ enum class Calculator(
     });
 
     companion object {
-        fun calculate(num1: Int, num2: Int, symbol: String): Int {
+        fun calculate(expression: String): Int {
+            validateExpression(expression)
+
+            val split = expression.split(" ")
+            var result = split[0].toInt()
+
+            for (i in 1 until split.size step (2)) {
+                val symbol = split[i]
+                val num = split[i + 1].toInt()
+                result = Calculator.calculate(result, num, symbol)
+            }
+
+            return result
+        }
+
+        private fun validateExpression(expression: String) {
+            if (expression.isBlank()) {
+                throw IllegalArgumentException("수식은 공백이 될 수 없습니다.")
+            }
+        }
+
+        private fun calculate(num1: Int, num2: Int, symbol: String): Int {
             val operator = findOperator(symbol)
             return operator.operator.apply(num1, num2)
         }
--- src/test/kotlin/calculator/model/CalculatorTest.kt
+++ src/test/kotlin/calculator/model/CalculatorTest.kt
@@ -14,7 +14,7 @@ class CalculatorTest {
     fun `존재하지 않는 수식 기호 예외 발생`() {
         // given, when, then
         val exception = assertThrows(IllegalArgumentException::class.java) {
-            Calculator.calculate(1, 2, "&")
+            Calculator.calculate("1 & 2")
         }
         assertEquals("존재하지 않는 수식 기호입니다. (symbol: &)", exception.message)
     }
@@ -23,26 +23,24 @@ class CalculatorTest {
     fun `나누는 수가 0일 때 예외 발생`() {
         // given, when, then
         val exception = assertThrows(IllegalArgumentException::class.java) {
-            Calculator.calculate(1, 0, "/")
+            Calculator.calculate("1 / 0")
         }
         assertEquals("나누는 수가 0이 될 수 없습니다.", exception.message)
+    }
+
+    @Test
+    fun `수식이 공백일 때 예외 발생`() {
+        // given, when, then
+        val exception = assertThrows(IllegalArgumentException::class.java) {
+            Calculator.calculate(" ")
+        }
+        assertEquals("수식은 공백이 될 수 없습니다.", exception.message)
     }
 
     @ParameterizedTest
     @CsvSource("1 + 2=3", "2 + 3 * 4 / 2=10", "4 - 2 / 2 * 5 + 1=6", delimiter = '=')
     fun `계산기 정상 동작`(expression: String, result: String) {
-        // given
-        val split = expression.split(" ")
-
-        // when
-        var calculatedResult = split[0].toInt()
-        for (i in 1 until split.size step (2)) {
-            val symbol = split[i]
-            val num = split[i + 1].toInt()
-            calculatedResult = Calculator.calculate(calculatedResult, num, symbol)
-        }
-
-        // then
-        assertEquals(calculatedResult, result.toInt())
+        // when, then
+        assertEquals(Calculator.calculate(expression), result.toInt())
     }
 }
