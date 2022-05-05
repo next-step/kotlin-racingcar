62b143dd712a5c073bd55151e3e46983f579568c
link-js <link.js@kakaocorp.com>
2022-05-05 17:54:03 +0900

    add : StringCalculator 클래스 및 테스트클래스 생성

---
+++ src/main/kotlin/step2/StringCalculator.kt
@@ -0,0 +1,9 @@
+package step2
+
+/**
+ * 문자열 사칙 연산 계산기
+ * Created by Jaesungchi on 2022.05.05..
+ */
+
+class StringCalculator {
+}
---
+++ src/test/kotlin/step2/StringCalculatorTest.kt
@@ -0,0 +1,9 @@
+package step2
+
+/**
+ * 문자열 사칙 연산 계산기 테스트
+ * Created by Jaesungchi on 2022.05.05..
+ */
+
+class StringCalculatorTest {
+}
