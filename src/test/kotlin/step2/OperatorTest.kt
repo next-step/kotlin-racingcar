61bf3a9424acb2a87eff31f918d6149d51742a04
jojoldu <jojoldu@gmail.com>
2022-05-06 00:13:42 +0900

    연산자 테스트 코드 Describe로 전환

--- src/test/kotlin/step2/OperatorTest.kt
+++ src/test/kotlin/step2/OperatorTest.kt
@@ -1,42 +1,42 @@
 package step2
 
-import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 
-class OperatorTest: AnnotationSpec() {
+class OperatorTest: DescribeSpec({
 
-    @Test
-    fun `+연산자는 두 숫자를 합한다`() {
-        val sut = Operator.PLUS
+    describe("evaluate") {
+        it("+연산자는 두 숫자를 합한다") {
+            val sut = Operator.PLUS
 
-        val result = sut.evaluate(3, 4)
+            val result = sut.evaluate(3, 4)
 
-        result shouldBe 7
+            result shouldBe 7
+        }
+
+        it("-연산자는 두 숫자를 차감한다") {
+            val sut = Operator.MINUS
+
+            val result = sut.evaluate(3, 4)
+
+            result shouldBe -1
+        }
+
+        it("*연산자는 두 숫자를 곱셈한다") {
+            val sut = Operator.MULTIPLY
+
+            val result = sut.evaluate(3, 4)
+
+            result shouldBe 12
+        }
+
+        it("/연산자는 두 숫자를 나눗셈한다") {
+            val sut = Operator.DIVIDE
+
+            val result = sut.evaluate(4, 2)
+
+            result shouldBe 2
+        }
     }
-    @Test
-    fun `-연산자는 두 숫자를 차감한다`() {
-        val sut = Operator.MINUS
 
-        val result = sut.evaluate(3, 4)
-
-        result shouldBe -1
-    }
-
-    @Test
-    fun `*연산자는 두 숫자를 곱셈한다`() {
-        val sut = Operator.MULTIPLY
-
-        val result = sut.evaluate(3, 4)
-
-        result shouldBe 12
-    }
-
-    @Test
-    fun `나눗셈 연산자는 두 숫자를 나눗셈한다`() {
-        val sut = Operator.DIVIDE
-
-        val result = sut.evaluate(4, 2)
-
-        result shouldBe 2
-    }
-}
+})
