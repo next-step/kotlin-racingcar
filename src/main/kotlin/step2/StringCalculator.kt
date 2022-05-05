b1072b24db6347b70979036b10e5e3f052b96eb4
link-js <link.js@kakaocorp.com>
2022-05-05 18:03:57 +0900

    feat : main함수 추가 및 입출력 기능 생성

--- src/main/kotlin/step2/StringCalculator.kt
+++ src/main/kotlin/step2/StringCalculator.kt
@@ -5,5 +5,18 @@ package step2
  * Created by Jaesungchi on 2022.05.05..
  */
 
+fun main() {
+    val calculator = StringCalculator()
+    val input = readlnOrNull()
+    val output = calculator.startCalculate(input)
+    println(output)
+}
+
 class StringCalculator {
+    /**
+     * 문자열을 받고 계산을 시작하여 계산한값을 돌려준다.
+     */
+    fun startCalculate(input: String?): String {
+        return input ?: ""
+    }
 }
