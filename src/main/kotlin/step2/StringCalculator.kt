7f7169ef68fc791f970111d3ee129374ed04379b
link-js <link.js@kakaocorp.com>
2022-05-05 18:41:23 +0900

    feat : 문자열 나누기 및 정상 문자 확인 기능 추가

--- src/main/kotlin/step2/StringCalculator.kt
+++ src/main/kotlin/step2/StringCalculator.kt
@@ -17,9 +17,45 @@ class StringCalculator {
      * 문자열을 받고 계산을 시작하여 계산한값을 돌려준다.
      */
     fun startCalculate(input: String?): String {
-        if (input.isNullOrBlank())
-            throw IllegalArgumentException()
+        if (input.isNullOrBlank()) throw IllegalArgumentException("Input is Null or Blank.")
+
+        val inputList = splitStringBySpace(input)
+        inputList.forEachIndexed { index, text ->
+            // 짝수 index에는 숫자, 홀수 index에는 연산자가 온다.
+            if (index % 2 == 0) {
+                checkEnabledNumber(text)
+                //TODO 첫 숫자면 저장하고, 이후에는 계산한다.
+            } else {
+                checkEnabledOperator(text)
+                //TODO 연산자는 저장한다.
+            }
+        }
 
         return input
+    }
+
+    /**
+     * 문자열을 스페이스로 나눠 List로 돌려주는 메소드.
+     */
+    private fun splitStringBySpace(input: String): List<String> {
+        return input.split(" ")
+    }
+
+    /**
+     * 계산가능한 연산자인지 확인하는 메소드.
+     */
+    private fun checkEnabledOperator(input: String) {
+        when (input) {
+            "+", "-", "/", "*" -> return
+            else -> throw IllegalArgumentException("There is a non-operator character in the operator position.")
+        }
+    }
+
+    /**
+     * 계산가능한 숫자인지 확인하는 메소드.
+     */
+    private fun checkEnabledNumber(input: String) {
+        if (input.toIntOrNull() == null)
+            throw IllegalArgumentException("There is a non-digit character in the digit position.")
     }
 }
