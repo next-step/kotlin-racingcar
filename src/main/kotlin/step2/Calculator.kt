b6fc6bc35c202612f06958d9dd536f4bf751a3e9
inpyo-jeon <inpyo.developer@gmail.com>
2022-05-05 19:38:53 +0900

    [Step2] 잘못 구현한 부분 수정 - 공백 기준이 아닌 delimiter 이용하도록 변경

--- src/main/kotlin/step2/Calculator.kt
+++ src/main/kotlin/step2/Calculator.kt
@@ -3,13 +3,17 @@ package step2
 class Calculator {
 
     fun calculate(input: String): Int {
-        val stringExceptSpaces = input.replace(" ", "")
-        var result = stringExceptSpaces[0].digitToInt()
-        for (i in 1 until stringExceptSpaces.length step 2) {
-            val operator = Operator.of(stringExceptSpaces[i])
-            result = operator.operate(result, stringExceptSpaces[i+1].digitToInt())
+        val inputs = input.split(DELIMITER)
+        var result = inputs[0].toInt()
+        for (i in 1 until inputs.size step 2) {
+            val operator = Operator.of(inputs[i].first())
+            result = operator.operate(result, inputs[i+1].toInt())
         }
 
         return result
+    }
+
+    companion object {
+        const val DELIMITER = " "
     }
 }
