c00da0eb41caa3ebd5799def10d71bf2c46925c2
dongchul <sorkehdcjf@gmail.com>
2022-05-06 22:29:32 +0900

    feat: 사칙연산 테스트 코드 작성

---
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -0,0 +1,10 @@
+package calculator
+
+class StringCalculator(
+    private val expression: String
+) {
+    fun calculate(): Double {
+        return 0.0
+    }
+}
+
---
+++ src/test/kotlin/study/calculator/CalculatorTest.kt
@@ -0,0 +1,15 @@
+package study.calculator
+
+import calculator.StringCalculator
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class CalculatorTest : StringSpec({
+    "2 + 3 * 4 / 2 와 같은 문자열을 입력할 경우 2 + 3 * 4 / 2 실행 결과인 10이 반환된다" {
+        val expression = "2 + 3 * 4 / 2"
+        val actual = StringCalculator(expression).calculate()
+        val expected = 10
+
+        actual shouldBe expected
+    }
+})
