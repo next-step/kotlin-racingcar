d47cc2eddf931d6896bedc93d020d580be699fec
Rok93 <kr.kim@mfort.co.kr>
2022-05-06 10:27:14 +0900

    refactor: 불필요한 null check 제거

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -8,7 +8,7 @@ class StringCalculator(
     private val expression: String,
 ) {
     init {
-        require(!expression.isNullOrBlank())
+        require(expression.isNotBlank())
     }
 
     fun calculate(): Int {
