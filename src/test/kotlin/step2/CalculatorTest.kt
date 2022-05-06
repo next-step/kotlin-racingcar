e51cdd226ed54ea91d7d6a9dbec6f71cc3f63d35
vivian.bae <vivian.bae@kakaocorp.com>
2022-05-06 16:56:47 +0900

    step2: 테스트 케이스 실패하지 않도록 수정

--- src/test/kotlin/step2/CalculatorTest.kt
+++ src/test/kotlin/step2/CalculatorTest.kt
@@ -12,89 +12,80 @@ class CalculatorTest {
     @ParameterizedTest
     @NullAndEmptySource
     @ValueSource(strings = [" ", "\t", "\n"])
-    fun `null 또는 whitespace 문자가 주어질 경우 IllegalArgumentException 이 발생한다`(input: String) {
+    fun `null 또는 whitespace 문자가 주어질 경우 IllegalArgumentException 이 발생한다`(input: String?) {
         assertThatIllegalArgumentException().isThrownBy { Calculator.calculate(input) }
+            .withMessage("Invalid expression")
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{index}) Invalid operand: {0}")
     @ValueSource(strings = ["1 % 1", "2 + 2 @ 2", "3 ^ 3", "4 ++"])
     fun `사칙연산 기호가 아닌 문자가 주어질 경우 IllegalArgumentException 이 발생한다`(input: String) {
         assertThatIllegalArgumentException().isThrownBy { Calculator.calculate(input) }
-            .withMessage("")
+            .withMessageContaining("Invalid operand")
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{index}) Invalid number: {0}")
     @ValueSource(strings = ["a + b", "1,1 * 2"])
     fun `숫자가 아닌 문자가 주어질 경우 IllegalArgumentException 이 발생한다`(input: String) {
         assertThatIllegalArgumentException().isThrownBy { Calculator.calculate(input) }
-            .withMessage("")
+            .withMessageContaining("Invalid number")
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{index}) Invalid expression: {0}")
     @ValueSource(strings = ["1 +", "- 2", "3 * / 3", "4 4 - 4", "5+5"])
     fun `잘못된 식이 주어질 경우 IllegalArgumentException 이 발생한다`(input: String) {
         assertThatIllegalArgumentException().isThrownBy { Calculator.calculate(input) }
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{index}) {0} = {1}")
     @CsvSource(
         textBlock = """
-        #EXPRESSION             #RESULT
         12 + 34,                46
         5 + 4 + 3 + 2 + 1 + 0,  15
         7428934 + 189248,       7618182
-        -1 + -2,                -3
-    """
+        -1 + -2,                -3"""
     ) fun `사칙연산 - 덧셈`(expression: String, result: Int) {
         assertEquals(Calculator.calculate(expression), result)
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{index}) {0} = {1}")
     @CsvSource(
         textBlock = """
-        #EXPRESSION             #RESULT
         10 - 0 - 4 - 2,         4
         45 - 90,                -45
         -1 - -2,                1
-        28348 - 3747,           24601
-    """
+        28348 - 3747,           24601"""
     ) fun `사칙연산 - 뺄셈`(expression: String, result: Int) {
         assertEquals(Calculator.calculate(expression), result)
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{index}) {0} = {1}")
     @CsvSource(
         textBlock = """
-        #EXPRESSION             #RESULT
         12 * 34 * 56,           22848
         5 * 0,                  0
         25 * -2,                -50
-        -20 * -30,              600
-    """
+        -20 * -30,              600"""
     ) fun `사칙연산 - 곱셈`(expression: String, result: Int) {
         assertEquals(Calculator.calculate(expression), result)
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{index}) {0} = {1}")
     @CsvSource(
         textBlock = """
-        #EXPRESSION             #RESULT
         4 / 2,                  2
         15 / -5,                -3
-        100 / 2 / 5 / 2,        5
-    """
+        100 / 2 / 5 / 2,        5"""
     ) fun `사칙연산 - 나눗셈`(expression: String, result: Int) {
         assertEquals(Calculator.calculate(expression), result)
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{index}) {0} = {1}")
     @CsvSource(
         textBlock = """
-        #EXPRESSION             #RESULT
         1 + 5 / 2 * 8 - 1,      23
         88 * 10 / 8 + 4,        114
-        -3 * 4 - 90 + 44,       -58
-    """
+        -3 * 4 - 90 + 44,       -58"""
     ) fun `사칙연산 - 덧셈, 뺄셈, 곱셈, 나눗셈 모두 포함`(expression: String, result: Int) {
         assertEquals(Calculator.calculate(expression), result)
     }
