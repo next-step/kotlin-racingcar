2bbc114538b95b1d0660c944fe503d67f2bf5419
austen.lee <austen.lee@kakaocorp.com>
2022-05-07 00:41:48 +0900

    step2 test : assertAll로 변경

--- src/test/kotlin/step2/calculator/CalculatorTest.kt
+++ src/test/kotlin/step2/calculator/CalculatorTest.kt
@@ -2,6 +2,7 @@ package step2.calculator
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 
 class CalculatorTest {
     private val calculator: Calculator by lazy {
@@ -11,15 +12,15 @@ class CalculatorTest {
     @Test
     fun `사칙 연산 문자열을 받아 연산을 실행한 뒤, 적절한 결과값을 반환한다`() {
         val calculatorResult1 = calculator.calculate("2 + 3 * 4 / 2")
-        assertThat(calculatorResult1).isEqualTo((2.0 + 3.0) * 4.0 / 2.0)
-
         val calculatorResult2 = calculator.calculate("5 * 10 + 5 / 11")
-        assertThat(calculatorResult2).isEqualTo((5.0 * 10.0 + 5.0) / 11.0)
-
         val calculatorResult3 = calculator.calculate("3 / 11 * 5 / 144")
-        assertThat(calculatorResult3).isEqualTo(3.0 / 11.0 * 5.0 / 144.0)
-
         val calculatorResult4 = calculator.calculate("3 - 11 / 5 - 32")
-        assertThat(calculatorResult4).isEqualTo((3.0 - 11.0) / 5.0 - 32.0)
+
+        assertAll(
+            { assertThat(calculatorResult1).isEqualTo((2.0 + 3.0) * 4.0 / 2.0) },
+            { assertThat(calculatorResult2).isEqualTo((5.0 * 10.0 + 5.0) / 11.0) },
+            { assertThat(calculatorResult3).isEqualTo(3.0 / 11.0 * 5.0 / 144.0) },
+            { assertThat(calculatorResult4).isEqualTo((3.0 - 11.0) / 5.0 - 32.0) }
+        )
     }
 }
