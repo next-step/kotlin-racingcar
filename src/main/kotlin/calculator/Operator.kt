1a80d1ee3bf1b71ef0f184c695ee7a239e8b8152
Rok93 <kr.kim@mfort.co.kr>
2022-05-06 12:22:50 +0900

    refactor: 잘못된 symbol이 들어온 경우 예외 메시지가 입력된 symbol 값을 포함하도록 수정

--- src/main/kotlin/calculator/Operator.kt
+++ src/main/kotlin/calculator/Operator.kt
@@ -1,22 +1,21 @@
 package calculator
 
-import java.util.function.BiFunction
-
 enum class Operator(
     private val symbol: String,
-    private val execute: BiFunction<Int, Int, Int>,
+    private val execute: (Int, Int) -> Int,
 ) {
-    PLUS("+", BiFunction { a: Int, b: Int -> a + b }),
-    MINUS("-", BiFunction { a: Int, b: Int -> a - b }),
-    MULTIPLY("*", BiFunction { a: Int, b: Int -> a * b }),
-    DIVIDE("/", BiFunction { a: Int, b: Int -> a / b }),
+    PLUS("+", { a, b -> a + b }),
+    MINUS("-", { a, b -> a - b }),
+    MULTIPLY("*", { a, b -> a * b }),
+    DIVIDE("/", { a, b -> a / b }),
     ;
 
-    fun operate(a: Int, b: Int): Int = execute.apply(a, b)
+    fun operate(a: Int, b: Int): Int = execute(a, b)
 
     companion object {
         fun of(symbol: String): Operator {
-            return values().firstOrNull { symbol == it.symbol } ?: throw IllegalArgumentException("존재하지 않는 연산 기호입니다.")
+            return values().firstOrNull { symbol == it.symbol }
+                ?: throw IllegalArgumentException("존재하지 않는 연산 기호입니다. symbol = $symbol")
         }
     }
 }
