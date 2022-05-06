876b081bed9477be1eb766e07f53bc51f7925bef
austen.lee <austen.lee@kakaocorp.com>
2022-05-07 00:19:05 +0900

    step2 refactor : Main에서도 require을 사용하도록 변경

--- src/main/kotlin/step2/Main.kt
+++ src/main/kotlin/step2/Main.kt
@@ -2,16 +2,15 @@ package step2
 
 import step2.calculator.Calculator
 import step2.calculator.ExpressionParser
-import step2.inspector.mustNot
 
 private const val INVALID_INPUT_ERROR_MESSAGE = "\n적합하지 않은 입력값이 전달되었습니다.\n입력값은 공백으로만 이루어져서는 안 됩니다."
 
 fun main() {
     val expression = readlnOrNull()
-    mustNot(IllegalArgumentException(INVALID_INPUT_ERROR_MESSAGE)) {
-        expression.isNullOrBlank()
+    require(!expression.isNullOrBlank()) {
+        INVALID_INPUT_ERROR_MESSAGE
     }
 
     val calculator = Calculator(ExpressionParser())
-    println(calculator.calculate(expression!!))
+    println(calculator.calculate(expression))
 }
