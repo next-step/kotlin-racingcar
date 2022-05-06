5c6229193a8fd933d7d07e318bd5cd6c4fd94477
yjna2316 <yjna2316@gmail.com>
2022-05-06 23:36:49 +0900

    chore: test code

--- src/test/kotlin/study/PersonKoTestStringSpec.kt
+++ src/test/kotlin/study/PersonKoTestStringSpec.kt
@@ -1,42 +1,40 @@
 package study
 
-import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.inspectors.forAll
 import io.kotest.matchers.shouldBe
-import org.junit.jupiter.api.Test
 
-class PersonKoTest : AnnotationSpec() {
-    @Test
-    fun `이름 붙인 인자`() {
+class PersonKoTestStringSpec : StringSpec({
+    "이름 붙인 인자" {
         val people = listOf(
             Person("나윤지", 29, "yj"),
             Person("나윤지", 29, nickname = "yj"),
-            Person(name = "나윤지", age = 29, "yj"),
+            Person(name = "나윤지", age = 29, nickname = "yj")
         )
-        people[0].name shouldBe "나윤지"
-        people[1].name shouldBe "나윤지"
-        people[2].name shouldBe "나윤지"
+        people.forAll {
+            it.name shouldBe "나윤지"
+            it.age shouldBe 29
+            it.nickname shouldBe "yj"
+        }
     }
 
-    @Test
-    fun `널 타입`() {
+    "널 타입" {
         val person = Person("나윤지", 29, null)
         person.name shouldBe "나윤지"
         person.age shouldBe 29
         person.nickname shouldBe null
     }
 
-    @Test
-    fun `기본 인자`() {
-        val person = Person("나윤지", 29, null)
+    "기본 인자" {
+        val person = Person("나윤지", 29)
         person.name shouldBe "나윤지"
         person.age shouldBe 29
-        person.nickname shouldBe null
+        person.nickname shouldBe "나윤지"
     }
 
-    @Test
-    fun `데이터 클래스`() {
+    "데이터 클래스" {
         val person1 = Person("나윤지", 29)
         val person2 = Person("나윤지", 29)
         person1 shouldBe person2
     }
-}
+})
