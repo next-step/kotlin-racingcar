f2ff6c4cb300ad2130ceeb2d29fa833d519acbbb
yohan <cd80@kakao.com>
2022-05-06 22:24:24 +0900

    feat: 수식 문자열 검증을 위한 객체 추가

---
+++ src/test/kotlin/study/CalcSyntaxCheckerTest.kt
@@ -0,0 +1,22 @@
+package study
+
+import io.kotest.matchers.throwable.shouldHaveMessage
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import org.junit.jupiter.api.Assertions.assertThrowsExactly
+
+
+internal class CalcSyntaxCheckerTest {
+
+    @ParameterizedTest()
+    @ValueSource(strings = ["", " ", "\r\n", "\t"])
+    fun `빈 공백인 수식을 검증할 경우 예외 발생`(expr: String) {
+        // when
+        val result = assertThrowsExactly(IllegalArgumentException::class.java) {
+            CalcSyntaxChecker.validate(expr)
+        }
+
+        // then
+        result.shouldHaveMessage(CalcSyntaxChecker.EMPTY_EXPRESSION_EXCEPTION)
+    }
+}
