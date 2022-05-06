0e09403e5b1392485271d0a2238e13f8de63b4cf
ellen.youn <ellen.youn@kakaocorp.com>
2022-05-06 20:50:27 +0900

    feat: 예외처리 추가

--- src/main/kotlin/step2/InOrderCalculator.kt
+++ src/main/kotlin/step2/InOrderCalculator.kt
@@ -1,12 +1,19 @@
 package step2
 
 class InOrderCalculator {
+    private val ERROR_INPUT_NULL_OR_EMPTY = "입력값은 null이거나 빈 공백 문자일 수 없습니다."
+    private val ERROR_INPUT_NUMBER = "잘못된 숫자를 입력하였습니다."
+    private val ERROR_EXPRESSION = "식이 올바르지 않습니다."
+
     private val DELIMETER = " "
 
     fun calculate(expression: String?): Double {
-        val expressions = expression?.split(DELIMETER)?.toMutableList() ?: throw IllegalArgumentException()
+        if (expression.isNullOrBlank()) throw IllegalArgumentException(ERROR_INPUT_NULL_OR_EMPTY)
 
-        var result: Double = expressions[0].toDouble()
+        val expressions = expression.split(DELIMETER).toMutableList()
+        if (expressions.size % 2 == 0) throw IllegalArgumentException(ERROR_EXPRESSION)
+
+        var result: Double = expressions[0].toValidDouble()
         var operator: String? = null
 
         (1 until expressions.size).forEach { index ->
@@ -14,9 +21,19 @@ class InOrderCalculator {
                 //isOperator
                 operator = expressions[index]
             } else {
-                result = Operator.execute(result, expressions[index].toDouble(), operator)
+                result = Operator.execute(result, expressions[index].toValidDouble(), operator)
             }
         }
         return result
+    }
+
+    private fun String?.toValidDouble(): Double {
+        if (this.isNullOrBlank()) throw IllegalArgumentException(ERROR_INPUT_NUMBER)
+
+        try {
+            return this.toDouble()
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException(ERROR_INPUT_NUMBER)
+        }
     }
 }
