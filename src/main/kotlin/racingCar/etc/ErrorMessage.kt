3436143bb40247de36364320fa700b6d910c5df2
link-js <link.js@kakaocorp.com>
2022-05-07 15:11:14 +0900

    feat : Message클래스 생성 및 출력할 문구 생성

---
+++ src/main/kotlin/racingCar/etc/ErrorMessage.kt
@@ -0,0 +1,10 @@
+package racingCar.etc
+
+/**
+ * 에러관련 메세지를 모아둔 클래스
+ * Created by Jaesungchi on 2022.05.07..
+ */
+object ErrorMessage {
+    const val NO_RACING_CARS = "레이싱카가 없습니다."
+    const val ATTEMPTS_IS_ZERO = "시도할 횟수가 0회입니다."
+}
---
+++ src/main/kotlin/racingCar/etc/PrintingMessage.kt
@@ -0,0 +1,11 @@
+package racingCar.etc
+
+/**
+ * 일반적으로 출력되는 메세지를 모아둔 클래스
+ * Created by Jaesungchi on 2022.05.07..
+ */
+object PrintingMessage {
+    const val HOW_MANY_CAR = "자동차 대수는 몇 대인가요?"
+    const val HOW_MANY_TIMES_RACE = "시도할 횟수는 몇 회인가요?"
+    const val EXECUTION_RESULT = "실행 결과"
+}
