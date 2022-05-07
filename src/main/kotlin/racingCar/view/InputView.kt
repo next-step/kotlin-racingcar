24a2dc8c973e5342353b8a2902d9aed4df002d82
link-js <link.js@kakaocorp.com>
2022-05-07 15:29:20 +0900

    feat : 입출력 클래스 입출력 기능 추가

--- src/main/kotlin/racingCar/view/InputView.kt
+++ src/main/kotlin/racingCar/view/InputView.kt
@@ -1,7 +1,22 @@
 package racingCar.view
 
+import racingCar.etc.Message
+
 /**
  * 입력을 받고 로직을 시작하는 클래스
  * Created by Jaesungchi on 2022.05.07..
  */
-class InputView
+class InputView {
+    fun printHowManyCars() {
+        println(Message.PrintingMessage.HOW_MANY_CAR)
+    }
+
+    fun printHowManyTimesRace() {
+        println(Message.PrintingMessage.HOW_MANY_TIMES_RACE)
+    }
+
+    fun getNumber(): Int {
+        val input = readlnOrNull() ?: throw IllegalArgumentException(Message.ExceptionMessage.IS_NULL_OR_BLANK)
+        return input.toIntOrNull() ?: throw IllegalArgumentException(Message.ExceptionMessage.IS_NOT_INTEGER)
+    }
+}
--- src/main/kotlin/racingCar/view/ResultView.kt
+++ src/main/kotlin/racingCar/view/ResultView.kt
@@ -1,7 +1,13 @@
 package racingCar.view
 
+import racingCar.etc.Message
+
 /**
  * 결과를 출력하는 클래스
  * Created by Jaesungchi on 2022.05.07..
  */
-class ResultView
+class ResultView {
+    fun printResultMessage() {
+        println(Message.PrintingMessage.EXECUTION_RESULT)
+    }
+}
