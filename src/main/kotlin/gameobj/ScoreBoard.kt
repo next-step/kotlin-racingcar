33bd751985c69a37f1847f125a5a661d0bb53a5c
yohan <cd80@kakao.com>
2022-05-08 00:41:27 +0900

    feat: 클라이언트 코드 완성
    refactor: ScoreBoard 불변 객체로 변경
    
    refactor: 패키지명 변경
    
    feat: 사용자 입력 InputScene 구현
    
    refactor: ScoreboardTest 검증부 리팩터링
    
    refactor: Scoreboard 출력 메서드 이름 변경
    
    feat: Car 생성시 위치가 음수인 경우 예외 처리
    
    feat: ScoreBoard 객체의 출력 구현
    
    refactor: Car 객체 테스트 코드 수정
    
    feat: Car 객체 출력 구현
    
    refactor: 테스트명 오타 수정
    
    refactor: Car 객체 position 프로퍼티 접근 제한
    
    refactor: 기본 생성자 제한을 위한 Car 객체의 data 키워드 제거
    
    feat: 자동차 객체 이동 구현
    
    feat: 자동차 객체 생성자 구현
    
    refactor: 3차 코드리뷰 반영

---
+++ src/main/kotlin/gameobj/ScoreBoard.kt
@@ -0,0 +1,8 @@
+package gameobj
+
+import port.OutputPainter
+
+class ScoreBoard(private val painter: OutputPainter, private val cars: List<Car>) {
+
+    fun draw() = cars.map(Car::toString).forEach(painter::draw)
+}
---
+++ src/test/kotlin/gameobj/ScoreBoardTest.kt
@@ -0,0 +1,71 @@
+package gameobj
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+
+class StubPainter : OutputPainter {
+
+    val outputBuffer = mutableListOf<String>()
+
+    override fun draw(buffer: String) {
+        outputBuffer += buffer
+    }
+}
+
+internal class ScoreBoardTest : DescribeSpec({
+
+    describe("전광판 객체 테스트") {
+        it("입력된 모든 자동차의 현재 위치를 출력한다.") {
+            // given
+            val stubPainter = StubPainter()
+            val cars = listOf(
+                Car.spawnAt(1),
+                Car.spawnAt(2),
+                Car.spawnAt(3),
+            )
+            val scoreBoard = ScoreBoard(stubPainter, cars)
+
+            // when
+            scoreBoard.draw()
+
+            // then
+            stubPainter.outputBuffer.size shouldBe 3
+            stubPainter.outputBuffer[0] shouldBe "-"
+            stubPainter.outputBuffer[1] shouldBe "--"
+            stubPainter.outputBuffer[2] shouldBe "---"
+        }
+
+        it("위치가 0인 자동차도 출력한다.") {
+            // given
+            val stubPainter = StubPainter()
+            val cars = listOf(
+                Car.spawnAt(0),
+                Car.spawnAt(2),
+                Car.spawnAt(3),
+            )
+            val scoreBoard = ScoreBoard(stubPainter, cars)
+
+            // when
+            scoreBoard.draw()
+
+            // then
+            stubPainter.outputBuffer.size shouldBe 3
+            stubPainter.outputBuffer[0] shouldBe ""
+            stubPainter.outputBuffer[1] shouldBe "--"
+            stubPainter.outputBuffer[2] shouldBe "---"
+        }
+
+        it("입력된 자동차가 없는 경우 출력하지 않는다.") {
+            // given
+            val stubPainter = StubPainter()
+            val cars = emptyList<Car>()
+            val scoreBoard = ScoreBoard(stubPainter, cars)
+
+            // when
+            scoreBoard.draw()
+
+            // then
+            stubPainter.outputBuffer.size shouldBe 0
+        }
+    }
+})
