69d699a1f1b2f850a6f5bc3b6af1e1b192a7c313
austen.lee <austen.lee@kakaocorp.com>
2022-05-07 18:19:31 +0900

    step3 refactor : InputLayout이 InputView를 받고, 기존 receive는 inputView가 하도록

--- src/main/kotlin/step3/view/InputLayout.kt
+++ src/main/kotlin/step3/view/InputLayout.kt
@@ -2,26 +2,12 @@ package step3.view
 
 import step3.model.RacingGame
 
-class InputLayout {
+class InputLayout(
+    private val carInputView: InputView,
+    private val attemptInputView: InputView
+) {
     fun receiveUserInput() = RacingGame(
-        receiveNumberOfCars(),
-        receiveNumberOfAttempts()
+        carInputView.receive(),
+        attemptInputView.receive()
     )
-
-    private fun receiveNumberOfCars(): Int {
-        println(INPUT_MESSAGE_NUMBER_OF_CARS)
-        val inputNumberOfCars = requireNotNull(readLine())
-        return inputNumberOfCars.toInt()
-    }
-
-    private fun receiveNumberOfAttempts(): Int {
-        println(INPUT_MESSAGE_NUMBER_OF_ATTEMPTS)
-        val inputNumberOfAttempts = requireNotNull(readLine())
-        return inputNumberOfAttempts.toInt()
-    }
-
-    companion object {
-        private const val INPUT_MESSAGE_NUMBER_OF_CARS = "자동차 대수는 몇 대인가요?"
-        private const val INPUT_MESSAGE_NUMBER_OF_ATTEMPTS = "시도할 횟수는 몇 회인가요?"
-    }
 }
