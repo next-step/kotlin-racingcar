539729e7449b0f7818bdcfa1b0b04c048c18b758
link-js <link.js@kakaocorp.com>
2022-05-07 23:02:08 +0900

    feat : 레이싱 로직 추가

--- src/main/kotlin/racingCar/controller/RacingController.kt
+++ src/main/kotlin/racingCar/controller/RacingController.kt
@@ -1,5 +1,6 @@
 package racingCar.controller
 
+import racingCar.model.Car
 import java.util.Random
 
 /**
@@ -7,6 +8,29 @@ import java.util.Random
  * Created by Jaesungchi on 2022.05.07..
  */
 class RacingController {
+    private val carList: MutableList<Car> = mutableListOf()
+
+    fun getResult(): List<Car> {
+        return carList
+    }
+
+    fun startGame(countOfCar: Int, countOfRace: Int) {
+        repeat(countOfCar) {
+            carList.addAll(listOf(Car()))
+        }
+        repeat(countOfRace) {
+            processGame()
+        }
+    }
+
+    private fun processGame() {
+        carList.forEach { car ->
+            if (checkCanRunScore(rollTheDice())) {
+                car.position++
+            }
+        }
+    }
+
     private fun rollTheDice(): Int {
         val random = Random()
         return random.nextInt(DICE_SIZE)
@@ -17,7 +41,7 @@ class RacingController {
     }
 
     companion object {
-        private val DICE_SIZE = 10
-        private val SCORE_OF_CAN_GO = 4
+        private const val DICE_SIZE = 10
+        private const val SCORE_OF_CAN_GO = 4
     }
 }
