27bdc12515041622f12a646bb79f74767137b04d
link-js <link.js@kakaocorp.com>
2022-05-07 15:38:05 +0900

    feat : 주사위를 굴리고 4점을 넘는지 확인

--- src/main/kotlin/racingCar/controller/RacingController.kt
+++ src/main/kotlin/racingCar/controller/RacingController.kt
@@ -1,7 +1,23 @@
 package racingCar.controller
 
+import java.util.Random
+
 /**
  * 레이싱을 시작하고, 계산하는 클래스
  * Created by Jaesungchi on 2022.05.07..
  */
-class RacingController
+class RacingController {
+    private fun rollTheDice(): Int {
+        val random = Random()
+        return random.nextInt(DICE_SIZE)
+    }
+
+    private fun checkCanRunScore(score: Int): Boolean {
+        return score >= SCORE_OF_CAN_GO
+    }
+
+    companion object {
+        private val DICE_SIZE = 10
+        private val SCORE_OF_CAN_GO = 4
+    }
+}
