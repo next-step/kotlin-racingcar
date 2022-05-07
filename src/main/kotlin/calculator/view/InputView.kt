55bd69467163e5956bd9af44fac12f8c4dd1d7b6
YooInKeun <keun0390@naver.com>
2022-05-07 14:03:37 +0900

    refactor: 인풋 입력 받을 때, readLine() 사용하도록 변경

--- src/main/kotlin/calculator/view/InputView.kt
+++ src/main/kotlin/calculator/view/InputView.kt
@@ -1,16 +1,11 @@
 package calculator.view
 
-import java.util.Scanner
-
 object InputView {
-
-    private val scanner = Scanner(System.`in`)
-
     fun printInputMessage() {
         println("수식을 입력하세요.")
     }
 
     fun inputExpression(): String {
-        return scanner.nextLine()
+        return readLine()!!
     }
 }
