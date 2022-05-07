4364d7acaf71bf26f94105b43621f57a0d0dbc1f
이광재(Gwangjae Lee)/Biz플랫폼개발팀/SKP <mygoaway@sk.com>
2022-05-08 00:15:09 +0900

    step2 - 문자열계산기

---
+++ src/main/kotlin/step2/Calculator.kt
@@ -0,0 +1,20 @@
+package step2
+
+class Calculator {
+    fun calculate(input: String) : Int {
+        validateInput(input)
+
+        val splitInput = input.split(" ")
+        var result = splitInput[0].toInt()
+
+        for(i in 1 until splitInput.size step (2)) {
+            result = Operator.makeOperator(splitInput[i]).expression(result, splitInput[i+1].toInt())
+        }
+
+        return result
+    }
+
+    private fun validateInput(input: String) {
+        require(input.isNotBlank()) { "잘못된 입력값입니다." }
+    }
+}
---
+++ src/main/kotlin/step2/Operator.kt
@@ -0,0 +1,14 @@
+package step2
+
+enum class Operator (val operator: String, val expression: (Int, Int) -> Int){
+    PLUS("+", {result, additionalNumber -> result + additionalNumber}),
+    MINUS("-", {result, additionalNumber -> result - additionalNumber}),
+    MULTIPLY("*", {result, additionalNumber -> result * additionalNumber}),
+    DIVIDE("/", {result, additionalNumber -> if(additionalNumber == 0) throw IllegalArgumentException("숫자를 0으로 나눌 순 없습니다.") else result / additionalNumber });
+
+    companion object {
+        fun makeOperator(operator: String) : Operator {
+            return Operator.values().find { it.operator == operator } ?: throw IllegalArgumentException("사용 불가능한 연산자입니다.")
+        }
+    }
+}
