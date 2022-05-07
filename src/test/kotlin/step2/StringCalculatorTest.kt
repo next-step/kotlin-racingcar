4feb1abdb517d5f455f753095ddfd9ea408aedf5
qyu.st <qyu.st@kakaocorp.com>
2022-05-07 15:56:46 +0900

    refactor: Parameterized Test로 전환
    JUnit5의 Parameterized Test를 활용해보세요.
    https://www.baeldung.com/parameterized-tests-junit-5

--- src/test/kotlin/step2/StringCalculatorTest.kt
+++ src/test/kotlin/step2/StringCalculatorTest.kt
@@ -1,73 +1,48 @@
 package step2
 
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
 
 internal class StringCalculatorTest {
 
-    @Test
-    fun `arithmetic operation test`() {
-        val inputStr1 = "2 + 3"
-        val expected1 = 5.0
-        assertEquals(expected1, StringCalculator(inputStr1).result())
-
-        val inputStr2 = "8 - 1"
-        val expected2 = 7.0
-        assertEquals(expected2, StringCalculator(inputStr2).result())
-
-        val inputStr3 = "3 * 4"
-        val expected3 = 12.0
-        assertEquals(expected3, StringCalculator(inputStr3).result())
-
-        val inputStr4 = "8 / 4"
-        val expected4 = 2.0
-        assertEquals(expected4, StringCalculator(inputStr4).result())
+    @ParameterizedTest
+    @CsvSource(
+        "2 + 3,5.0",
+        "8 - 1,7.0",
+        "3 * 4,12.0",
+        "8 / 4,2.0"
+    )
+    fun `arithmetic operation test`(inputStr: String, expected: Double) {
+        assertEquals(expected, StringCalculator(inputStr).result())
     }
 
-    @Test
-    fun `multiple operands test`() {
-        val inputStr1 = "2 + 3 * 4 / 2"
-        val expected1 = 10.0
-        assertEquals(expected1, StringCalculator(inputStr1).result())
-
-        val inputStr2 = "21 * 63 / 3 / 7 - 10"
-        val expected2 = 53.0
-        assertEquals(expected2, StringCalculator(inputStr2).result())
-
-        val inputStr3 = "223 - 63 * 4 / 2"
-        val expected3 = 320.0
-        assertEquals(expected3, StringCalculator(inputStr3).result())
-
-        val inputStr4 = "2123 - 10 + 50 / 4"
-        val expected4 = 540.75
-        assertEquals(expected4, StringCalculator(inputStr4).result())
+    @ParameterizedTest
+    @CsvSource(
+        "2 + 3 * 4 / 2, 10.0",
+        "21 * 63 / 3 / 7 - 10, 53.0",
+        "223 - 63 * 4 / 2, 320.0",
+        "2123 - 10 + 50 / 4, 540.75"
+    )
+    fun `multiple operands test`(inputStr: String, expected: Double) {
+        assertEquals(expected, StringCalculator(inputStr).result())
     }
 
-    @Test
-    fun `non-arithmetic operator or non-digit check test`() {
-        val inputStr1 = "123 + 삼백"
-        assertThrows(IllegalArgumentException::class.java, StringCalculator(inputStr1)::result)
-
-        val inputStr2 = "123 + 3849 ^ 13"
-        assertThrows(IllegalArgumentException::class.java, StringCalculator(inputStr2)::result)
+    @ParameterizedTest
+    @ValueSource(strings = arrayOf("123 + 삼백", "123 + 3849 ^ 13"))
+    fun `non-arithmetic operator or non-digit check test`(inputStr: String) {
+        assertThrows(IllegalArgumentException::class.java, StringCalculator(inputStr)::result)
     }
 
-    @Test
-    fun `black remove test`() {
-        val inputStr1 = "2             + 3 * 4 / 2"
-        val expected1 = 10.0
-        assertEquals(expected1, StringCalculator(inputStr1).result())
-
-        val inputStr2 = "2  1 * 6   3            / 3 / 7 - 10"
-        val expected2 = 53.0
-        assertEquals(expected2, StringCalculator(inputStr2).result())
-
-        val inputStr3 = "2  2   3 - 6 3          * 4 / 2"
-        val expected3 = 320.0
-        assertEquals(expected3, StringCalculator(inputStr3).result())
-
-        val inputStr4 = "21         23 - 10 + 50 / 4"
-        val expected4 = 540.75
-        assertEquals(expected4, StringCalculator(inputStr4).result())
+    @ParameterizedTest
+    @CsvSource(
+        "2             + 3 * 4 / 2, 10.0",
+        "2  1 * 6   3            / 3 / 7 - 10, 53.0",
+        "223 - 63 * 4 / 2, 320.0",
+        "2123 - 10 + 50 / 4, 540.75"
+    )
+    fun `black remove test`(inputStr: String, expected: Double) {
+        assertEquals(expected, StringCalculator(inputStr).result())
     }
 }
