a566319fcd5a112e5616dd9bda4f5c909d01177b
PAPICO <ocipap0531@gmail.com>
2022-05-08 00:52:00 +0900

    new: kotlin 테스트 코드 연습 (#467)
    Co-authored-by: papico <papico@inflab.com>

---
+++ src/test/kotlin/study/PersonKoTest.kt
@@ -0,0 +1,41 @@
+package study
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.inspectors.forAll
+import io.kotest.matchers.shouldBe
+
+class PersonKoTest: StringSpec ({
+    "이름 붙인 인자" {
+        val people = listOf(
+            Person("조성륜", 23, "빠삐코"),
+            Person("조성륜", 23, nickname = "빠삐코"), Person(name = "조성륜",  age = 23, nickname = "빠삐코")
+        )
+
+        people.forAll {
+            it.name shouldBe "조성륜"
+            it.age shouldBe 23
+            it.nickname shouldBe "빠삐코"
+        }
+    }
+
+    "널 타입" {
+        val person = Person("조성륜", 23, null)
+        person.name shouldBe "조성륜"
+        person.age shouldBe 23
+        person.nickname shouldBe null
+    }
+
+    "기본 인자" {
+        val person = Person("조성륜", 23)
+        person.name shouldBe "조성륜"
+        person.age shouldBe 23
+        person.nickname shouldBe "조성륜"
+    }
+
+    "데이터 클래스" {
+        val person1 = Person("조성륜", 23)
+        val person2 = Person("조성륜", 23)
+
+        person1 shouldBe person2
+    }
+})
---
+++ src/test/kotlin/study/PersonTest.kt
@@ -0,0 +1,41 @@
+package study
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class PersonTest {
+    @Test
+    fun `이름 붙인 인자`() {
+        val people = listOf(
+            Person("조성륜", 23, "빠삐코"), Person("조성륜", 23,  nickname = "빠삐코"), Person(name = "조성륜", age = 23, nickname = "빠삐코")
+        )
+
+        // 리스트에 있는 것들이 전부 만족해야 함.
+        assertThat(people[0].name).isEqualTo("조성륜")
+        assertThat(people[1].name).isEqualTo("조성륜")
+        assertThat(people[2].name).isEqualTo("조성륜")
+    }
+
+    @Test
+    fun `널 타입`() {
+        val person = Person(name = "조성륜", age = 23)
+        assertThat(person.name).isEqualTo("조성륜")
+        assertThat(person.age).isEqualTo(23)
+    }
+
+    @Test
+    fun `기본 인자`() {
+        val person = Person("조성륜", 23)
+        assertThat(person.name).isEqualTo("조성륜")
+        assertThat(person.age).isEqualTo(23)
+        assertThat(person.nickname).isEqualTo("조성륜")
+    }
+
+    @Test
+    fun `데이터 클래스`() {
+        val person1 = Person("조성륜", 23)
+        val person2 = Person("조성륜", 23)
+
+        assertThat(person1).isEqualTo(person2)
+    }
+}
