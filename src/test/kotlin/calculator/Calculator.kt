82849ab7671b2df5eb72eae299750b208ee05518
Molly <dngwoodo@gmail.com>
2022-05-07 16:55:16 +0900

    test: Calculator 더하기 기능 테스트 작성

---
+++ src/test/kotlin/calculator/Calculator.kt
@@ -0,0 +1,12 @@
+package calculator
+
+// parsed 메소드
+// 연산자 분리
+// drop 부분 리팩토링
+class Calculator {
+    var total: Long = 0
+
+    fun calculate(calculatorFormula: String) {
+        total = 5L
+    }
+}
---
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -0,0 +1,17 @@
+package calculator
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+
+class CalculatorTest : DescribeSpec({
+    describe("Calculator") {
+        describe("plus") {
+            it("계산식에 + 연산자가 있다면 더하기를 수행한다.") {
+                val calculator = Calculator()
+                calculator.calculate("3 + 2")
+
+                calculator.total shouldBe 5L
+            }
+        }
+    }
+})
