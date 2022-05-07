6f20a43d678a6c0f9cfbe263d919949347cadad1
Molly <dngwoodo@gmail.com>
2022-05-07 17:35:38 +0900

    test: Calculator 뺄셈 테스트 코드 작성

--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -5,7 +5,7 @@ import io.kotest.matchers.shouldBe
 
 class CalculatorTest : DescribeSpec({
     describe("Calculator") {
-        describe("plus") {
+        describe("addition") {
             it("계산식에 + 연산자가 있다면 더하기를 수행한다.") {
                 val calculator = Calculator()
                 val secondCalculator = Calculator()
@@ -15,6 +15,19 @@ class CalculatorTest : DescribeSpec({
 
                 calculator.total shouldBe 5L
                 secondCalculator.total shouldBe 8L
+            }
+        }
+
+        describe("subtraction") {
+            it("계산식에 - 연산자가 있다면 뺄셈을 수행한다.") {
+                val calculator = Calculator()
+                val secondCalculator = Calculator()
+
+                calculator.calculate("3 - 2")
+                secondCalculator.calculate("3 - 3 - 2")
+
+                calculator.total shouldBe 1L
+                secondCalculator.total shouldBe -2L
             }
         }
     }
