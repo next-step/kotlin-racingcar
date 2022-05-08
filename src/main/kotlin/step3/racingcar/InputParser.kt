b4040782017cd0dd8d2ba2510c3152eafb414e8b
kwanguuuu <kwangyu326@gmail.com>
2022-05-09 08:28:40 +0900

    [step3] feat, test:  NumberParser 생성, input 단위 테스트 추가. docs 수정

---
+++ src/main/kotlin/step3/racingcar/InputParser.kt
@@ -0,0 +1,8 @@
+package step3.racingcar
+
+object InputParser {
+
+    fun toInt(input: String?): Int {
+        return input?.toIntOrNull() ?: throw IllegalArgumentException("잘못된 입력 입니다 `$input`")
+    }
+}
--- src/test/kotlin/step3/racingcar/NumberInputTest.kt
+++ src/test/kotlin/step3/racingcar/NumberInputTest.kt
@@ -1,8 +1,20 @@
 package step3.racingcar
 
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
 class NumberInputTest {
-
     @Test
+    fun `횟수 n을 입력 받는다 n은 정수 int type`() {
+        val intInput = "4"
+        val stringInput = "문자"
+        val nullInput = null
+        val blinkInput = ""
+
+        assertThat(InputParser.toInt(intInput)).isEqualTo(4)
+        assertThatThrownBy { InputParser.toInt(stringInput) }.isInstanceOf(IllegalArgumentException::class.java)
+        assertThatThrownBy { InputParser.toInt(nullInput) }.isInstanceOf(IllegalArgumentException::class.java)
+        assertThatThrownBy { InputParser.toInt(blinkInput) }.isInstanceOf(IllegalArgumentException::class.java)
+    }
 }
