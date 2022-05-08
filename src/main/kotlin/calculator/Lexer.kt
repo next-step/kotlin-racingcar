de8d12e735f8ff21bdba4fb460c6e4cb12c037bf
Jake Son <sonjeabin@gmail.com>
2022-05-08 17:01:17 +0900

    feat: lexer 생성 예외 상황 구현

--- src/main/kotlin/calculator/Lexer.kt
+++ src/main/kotlin/calculator/Lexer.kt
@@ -1,8 +1,16 @@
 package calculator
 
-class Lexer {
+class Lexer private constructor(
+    private val tokens: List<Token>,
+) {
     companion object {
-        fun new(input: String?): List<Token> {
+        fun new(input: String?): Lexer {
+            if (input.isNullOrBlank()) {
+                throw IllegalArgumentException("잘못된 입력입니다: $input")
+            }
+
+            throw IllegalArgumentException("잘못된 입력입니다: $input")
+
             TODO()
         }
     }
