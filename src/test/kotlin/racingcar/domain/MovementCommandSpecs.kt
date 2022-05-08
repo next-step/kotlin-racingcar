dd8047fd83c2b60541c0619fd065391ae7a0fa0a
rizzle.js <rizzle.js@kakaoent.com>
2022-05-08 19:06:22 +0900

    feature: add MovementCommand

---
+++ src/test/kotlin/racingcar/domain/MovementCommandSpecs.kt
@@ -0,0 +1,32 @@
+package racingcar.domain
+
+import io.kotest.assertions.throwables.shouldThrowExactly
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import racingcar.domain.MovementCommand.*
+
+class MovementCommandSpecs : DescribeSpec({
+
+    describe("이동 명령은") {
+        context("입력 숫자가 0 ~ 3 사이 숫자면") {
+            val number = 0
+            it("대기 명령을 의미한다") {
+                MovementCommand.of(number) shouldBe WAIT
+            }
+        }
+
+        context("입력 숫자가 4 ~ 9 사이 숫자면") {
+            val number = 4
+            it("이동 명령을 의미한다") {
+                MovementCommand.of(number) shouldBe MOVE
+            }
+        }
+
+        context("입력 숫자가 0 ~ 9를 벗어나면") {
+            val number = 100
+            it("예외를 발생시킨다") {
+                shouldThrowExactly<IllegalArgumentException> { MovementCommand.of(number) }
+            }
+        }
+    }
+})
