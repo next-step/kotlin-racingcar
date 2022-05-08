df2dc5dad7268560987ee4e1404eaae197ebb48a
leeyohan93 <good_1411@naver.com>
2022-05-09 00:40:31 +0900

    feat: 자동차 게임(RacingGame) 도메인모델 구현

---
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -0,0 +1,38 @@
+package racingcar
+
+import java.util.Random
+
+class RacingGame(
+    private val cars: List<Car>,
+    private var laps: Int
+) {
+    private val roundReports: MutableList<List<Int>> = mutableListOf()
+    private val random: Random = Random()
+
+    fun start() {
+        if (!isNotEnd()) throw IllegalStateException("이미 종료된 레이스입니다.")
+
+        cars.forEach {
+            it.move(random.nextInt(10))
+            writeReport()
+        }
+
+        laps--
+    }
+
+    private fun writeReport() {
+        roundReports.add(cars.map { it.position })
+    }
+
+    fun isNotEnd(): Boolean {
+        return laps > END_GAME_LAPS
+    }
+
+    fun report(): List<List<Int>> {
+        return roundReports
+    }
+
+    companion object {
+        private const val END_GAME_LAPS = 0
+    }
+}
