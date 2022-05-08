e49bc55687afcc183ddff85d0ce5634baa88096f
papico <papico@inflab.com>
2022-05-08 16:24:36 +0900

    new: 곱하기 구현

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -1,14 +1,29 @@
 package calculator
 
 class Calculator {
-    fun calculate (input: String): Int {
-        val inputs = input.split(" ");
-        var total = 0;
+    fun groupByIsNumber(inputs: List<String>, bool: Boolean): List<String> {
+        val elements = inputs.groupBy { (it.toDoubleOrNull() != null) };
+        return elements[bool]!!
+    }
 
-        for((index, el) in inputs.withIndex())
-            if(index % 2 == 0) {
+    fun calculate(input: String): Int {
+        val inputs = input.split(" ");
+        val numbers = groupByIsNumber(inputs, true)
+        val operations = groupByIsNumber(inputs, false);
+
+        var total = numbers[0].toInt();
+
+        for ((index, el) in numbers.drop(1).withIndex()) {
+            if (operations[index] == "+") {
                 total += el.toInt();
             }
+            if (operations[index] == "-") {
+                total -= el.toInt();
+            }
+            if (operations[index] == "*") {
+                total *= el.toInt();
+            }
+        }
 
         return total
     }
--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -3,7 +3,7 @@ package calculator
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 
-class CalculatorTest: DescribeSpec ({
+class CalculatorTest : DescribeSpec({
     describe("Calculator") {
         it("더하기를 수행한다.") {
             val calculator = Calculator();
@@ -15,5 +15,53 @@ class CalculatorTest: DescribeSpec ({
             result2 shouldBe 6
         }
 
+        it("빼기를 수행한다.") {
+            val calculator = Calculator();
+
+            val result = calculator.calculate("2 - 1");
+            val result2 = calculator.calculate("10 - 2 - 2");
+
+            result shouldBe 1
+            result2 shouldBe 6
+        }
+
+        it("곱셈를 수행한다.") {
+            val calculator = Calculator();
+
+            val result = calculator.calculate("2 * 2");
+            val result2 = calculator.calculate("10 * 2 * 2");
+
+            result shouldBe 4
+            result2 shouldBe 40
+        }
+
+        describe("groupByIsNumber") {
+            it("true 를 받으면 숫자 배열만 리턴한다.") {
+                val inputs = listOf<String>("1", "+", "2", "-", "3");
+                val inputs2 = listOf<String>("1", "+", "2", "-", "3", "4");
+
+                val calculator = Calculator();
+
+                val result = calculator.groupByIsNumber(inputs, true);
+                val result2 = calculator.groupByIsNumber(inputs2, true);
+
+                result shouldBe listOf<String>("1", "2", "3")
+                result2 shouldBe listOf<String>("1", "2", "3", "4")
+            }
+
+            it("false 를 받으면 숫자가 아닌 요소 배열만 리턴한다.") {
+                val inputs = listOf<String>("1", "+", "2", "-", "3");
+                val inputs2 = listOf<String>("1", "+", "2", "-", "3", "4");
+
+                val calculator = Calculator();
+
+                val result = calculator.groupByIsNumber(inputs, false);
+                val result2 = calculator.groupByIsNumber(inputs2, false);
+
+                result shouldBe listOf<String>("+", "-")
+                result2 shouldBe listOf<String>("+", "-")
+            }
+        }
     }
+
 })
