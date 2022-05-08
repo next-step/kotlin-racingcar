d11b1d2397dbce982487eb51db66f23af0951e5e
mygoaway <79883692+mygoaway@users.noreply.github.com>
2022-05-08 13:07:37 +0900

    Step2 - 문자열 계산기 (#468)
    * 불필요한 파일 제거
    
    * step2 - 문자열계산기
    
    Co-authored-by: 이광재(Gwangjae Lee)/Biz플랫폼개발팀/SKP <mygoaway@sk.com>

---
+++ src/test/kotlin/step2/CalculatorTest.kt
@@ -0,0 +1,45 @@
+package step2
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
+
+class CalculatorTest {
+    @Order(1)
+    @DisplayName("1) 오류없이 테스트 성공")
+    @ParameterizedTest
+    @CsvSource("5 + 2, 7",
+               "5 - 2, 3",
+               "5 * 2, 10",
+               "5 / 2, 2",
+               "2 + 3 * 4 / 2, 10")
+    fun success (input: String, result: Int){
+        val calculator = Calculator()
+        Assertions.assertThat(calculator.calculate(input)).isEqualTo(result)
+    }
+
+    @Order(2)
+    @DisplayName("2) 입력값이 null 또는 빈 공백 문자일 경우 테스트 실패")
+    @ParameterizedTest
+    @ValueSource(strings = ["", " "])
+    fun failedInputNullOrEmpty (input: String){
+        val calculator = Calculator()
+        Assertions.assertThatThrownBy { calculator.calculate(input) }
+            .isInstanceOf(IllegalArgumentException::class.java).hasMessageContaining("잘못된 입력값입니다.")
+    }
+
+    @Order(3)
+    @DisplayName("3) 0으로 나누는 경우 테스트 실패")
+    @ParameterizedTest
+    @CsvSource(
+        "5 / 0, 2",
+        "2 + 3 * 4 / 0, 10")
+    fun failedInput0 (input: String, result: Int){
+        val calculator = Calculator()
+        Assertions.assertThatThrownBy { calculator.calculate(input) }
+            .isInstanceOf(IllegalArgumentException::class.java).hasMessageContaining("숫자를 0으로 나눌 순 없습니다.")
+    }
+}
---
+++ src/test/kotlin/step2/OperatorTest.kt
@@ -0,0 +1,28 @@
+package step2
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class OperatorTest {
+    @Order(1)
+    @DisplayName("1) 사칙연산 기호가 아닌 경우 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = ["#", "^", "&", "@"])
+    fun failed (operator: String){
+        assertThatThrownBy { Operator.makeOperator(operator) }
+            .isInstanceOf(IllegalArgumentException::class.java).hasMessageContaining("사용 불가능한 연산자입니다.")
+    }
+
+    @Order(2)
+    @DisplayName("2) 사칙연산 기호가 맞는 경우 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = ["+", "-","*", "/"])
+    fun success (operator: String){
+        val makeOperator = Operator.makeOperator(operator)
+        assertThat(makeOperator.operator).isEqualTo(operator)
+    }
+}
