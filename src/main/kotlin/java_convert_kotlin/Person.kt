09584d17eece8d55d49e3baf8af3603c870172c1
Gomding <57378410+Gomding@users.noreply.github.com>
2022-05-08 21:14:08 +0900

    Step1 - 코틀린 기초 (#475)
    * study: Person 자바 예시 코드 생성
    
    * study: Person Java코드를 kotlin 코드로 변환
    
    * study: 이름 붙은 인자를 활용한 Person 객체 생성 테스트 작성
    
    * study: 널 타입에 대한 테스트 작성
    
    * study: 기본 인자를 활용한 Person 객체 생성 테스트
    
    * study: 데이터 클래스 테스트 작성

---
+++ src/main/kotlin/java_convert_kotlin/Person.kt
@@ -0,0 +1,3 @@
+package java_convert_kotlin
+
+data class Person(val name: String, val age: Int?, var nickname: String? = "찰리")
---
+++ src/test/kotlin/PersonTest.kt
@@ -0,0 +1,45 @@
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.inspectors.shouldForAll
+import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.shouldBe
+import java_convert_kotlin.Person
+
+class PersonTest : FunSpec({
+
+    test("이름 붙인 인자") {
+        val people = listOf(
+            Person("박민영", 29, "찰리"),
+            Person("박민영", 29, nickname = "찰리"),
+            Person(name = "박민영", nickname = "찰리", age = 29)
+        )
+
+        people.shouldForAll {
+            it.name shouldBe "박민영"
+            it.nickname shouldBe "찰리"
+        }
+    }
+
+    test("널 타입") {
+        val person = Person("박민영", null, null)
+
+        person.name shouldBe "박민영"
+        person.age.shouldBeNull()
+        person.nickname.shouldBeNull()
+    }
+
+    test("기본 인자") {
+        val person = Person("박민영", 29)
+
+        person.name shouldBe "박민영"
+        person.age shouldBe 29
+        person.nickname shouldBe "찰리"
+    }
+
+    test("데이터 클래스") {
+        val person1 = Person("박민영", 29, "찰리")
+        val person2 = Person("박민영", 29, "찰리")
+
+        person1.equals(person2).shouldBeTrue()
+    }
+})
