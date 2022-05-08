1d39161c49ffa9a86292f55c54f74b4f5d0acb9e
jaeju <jjj0611@gmail.com>
2022-05-09 05:25:19 +0900

    refactor: refactor four arithmetic operation function to companion object function

--- src/test/kotlin/study/StringCalculator.kt
+++ src/test/kotlin/study/StringCalculator.kt
@@ -1,8 +1,10 @@
 package study
 
 class StringCalculator {
-    fun plus(a: Double, b: Double) = a + b
-    fun minus(a: Double, b: Double) = a - b
-    fun multiply(a: Double, b: Double) = a * b
-    fun divide(a: Double, b: Double) = a / b
+    companion object {
+        fun plus(a: Double, b: Double) = a + b
+        fun minus(a: Double, b: Double) = a - b
+        fun multiply(a: Double, b: Double) = a * b
+        fun divide(a: Double, b: Double) = a / b
+    }
 }
--- src/test/kotlin/study/StringCalculatorTest.kt
+++ src/test/kotlin/study/StringCalculatorTest.kt
@@ -3,33 +3,31 @@ package study
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-class CalculatorTest {
-    private val calculator = StringCalculator()
-
+class StringCalculatorTest {
     @Test
     fun 더하기() {
-        val result = calculator.plus(1.0, 2.0)
+        val result = StringCalculator.plus(1.0, 2.0)
 
         assertThat(result).isEqualTo(3.0)
     }
 
     @Test
     fun 곱하기() {
-        val result = calculator.multiply(1.0, 2.0)
+        val result = StringCalculator.multiply(1.0, 2.0)
 
         assertThat(result).isEqualTo(2.0)
     }
 
     @Test
     fun 빼기() {
-        val result = calculator.minus(1.0, 2.0)
+        val result = StringCalculator.minus(1.0, 2.0)
 
         assertThat(result).isEqualTo(-1.0)
     }
 
     @Test
     fun 나누기() {
-        val result = calculator.divide(1.0, 2.0)
+        val result = StringCalculator.divide(1.0, 2.0)
 
         assertThat(result).isEqualTo(0.5)
     }
