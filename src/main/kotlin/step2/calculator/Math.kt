08675c730b0f37fab3516538fc4f57c2f26eba1f
Charming <loadkrnis@gmail.com>
2022-05-08 19:42:08 +0900

    [step2] 사칙연산 기능 추가

--- src/main/kotlin/step2/calculator/Math.kt
+++ src/main/kotlin/step2/calculator/Math.kt
@@ -1,19 +1,11 @@
 package step2.calculator
 
 class Math {
-  fun plus(a: Long, b: Long): Long {
-    TODO("Not yet implemented")
-  }
+  fun plus(a: Long, b: Long): Long = a + b
 
-  fun minus(a: Long, b: Long): Long {
-    TODO("Not yet implemented")
-  }
+  fun minus(a: Long, b: Long): Long = a - b
 
-  fun multiple(a: Long, b: Long): Long {
-    TODO("Not yet implemented")
-  }
+  fun multiple(a: Long, b: Long): Long = a * b
 
-  fun divide(a: Long, b: Long): Long {
-    TODO("Not yet implemented")
-  }
+  fun divide(a: Long, b: Long): Long = a / b
 }
