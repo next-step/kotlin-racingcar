7e33bd1c063363388872715912af27707a734613
Charming <loadkrnis@gmail.com>
2022-05-08 22:59:33 +0900

    [step2] 연산 기능 & 다중 연산 기능 테스트 추가

---
+++ src/main/kotlin/step2/calculator/Calculator.kt
@@ -0,0 +1,7 @@
+package step2.calculator
+
+class Calculator {
+  fun execute(input: String): Long {
+    TODO("Not yet implemented")
+  }
+}
---
+++ src/test/kotlin/step2/calculator/CalculatorTest.kt
@@ -0,0 +1,64 @@
+package step2.calculator
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
+
+class CalculatorTest : DescribeSpec({
+  describe("이항식") {
+    forAll(
+      row("8 + 4", 12),
+      row("8 - 4", 4),
+      row("8 * 4 ", 32),
+      row("8 / 4", 2)
+    ) { input, expectResult ->
+      // given
+      val calculator = Calculator()
+
+      // when
+      val result = calculator.execute(input)
+
+      // then
+      result shouldBe expectResult
+    }
+  }
+
+  describe("삼항식") {
+    it("모두 같은 연산자") {
+      forAll(
+        row("8 + 4 + 2", 12),
+        row("8 - 4 - 2", 4),
+        row("8 * 4 * 2", 64),
+        row("8 / 4 / 2", 1)
+      ) { input, expectResult ->
+        // given
+        val calculator = Calculator()
+
+        // when
+        val result = calculator.execute(input)
+
+        // then
+        result shouldBe expectResult
+      }
+    }
+
+    it("모두 다른 연산자") {
+      forAll(
+        row("8 + 4 - 2", 10),
+        row("8 - 4 * 2", 8),
+        row("8 * 4 / 2", 16),
+        row("8 / 4 + 2", 4)
+      ) { input, expectResult ->
+        // given
+        val calculator = Calculator()
+
+        // when
+        val result = calculator.execute(input)
+
+        // then
+        result shouldBe expectResult
+      }
+    }
+  }
+})
