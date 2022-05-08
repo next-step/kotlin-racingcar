ff71c69fc46cd318fb64a0fcb808c89b4d259343
larry.p <larry.p@kakaocorp.com>
2022-05-08 23:32:29 +0900

    feat: 입력/결과화면 로직 구현

---
+++ src/main/kotlin/car/racing/CarRacing.kt
@@ -0,0 +1,48 @@
+package car.racing
+
+class InputView(
+    private val reader: () -> String?,
+    private val console: (String) -> Unit
+) {
+
+    fun readDrivers(): Int {
+        console("자동차 대수는 몇 대인가요?: ")
+        return readInt()
+    }
+
+    fun readMoves(): Int {
+        console("시도할 횟수는 몇 회인가요?: ")
+        return readInt()
+    }
+
+    private fun readInt(): Int {
+        val value: String? = reader()
+        requireNotNull(value)
+        return value.toInt()
+    }
+}
+
+class ResultView(
+    private val console: (String) -> Unit
+) {
+    fun showRecords(records: List<List<Int>>) {
+        console("실행 결과:\n")
+        records.forEach(::showRecord)
+    }
+
+    private fun showRecord(record: List<Int>) {
+        record.forEach { move ->
+            repeat(move) {
+                console(DISPLAY_MOVEMENT)
+            }
+            showEmptyLine()
+        }
+        showEmptyLine()
+    }
+
+    private fun showEmptyLine() = console("\n")
+
+    companion object {
+        private const val DISPLAY_MOVEMENT = "-"
+    }
+}
