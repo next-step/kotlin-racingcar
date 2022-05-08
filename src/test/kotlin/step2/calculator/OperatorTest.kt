c593ff6eda1f3e4ad9f8a519d66919a327e2ce0e
kwanguuuu <kwangyu326@gmail.com>
2022-05-08 21:02:35 +0900

    [step2] chore: Operator test 변수명 수정

--- src/test/kotlin/step2/calculator/OperatorTest.kt
+++ src/test/kotlin/step2/calculator/OperatorTest.kt
@@ -8,16 +8,16 @@ class OperatorTest {
 
     @Test
     fun `연산자 찾기 테스트`() {
-        val test1 = "+"
-        val test2 = "-"
-        val test3 = "*"
-        val test4 = "/"
-        val test5 = "#"
+        val plus = "+"
+        val minus = "-"
+        val multiply = "*"
+        val divide = "/"
+        val shap = "#"
 
-        assertThat(Operator.findOperator(test1)).isEqualTo(Operator.PLUS)
-        assertThat(Operator.findOperator(test2)).isEqualTo(Operator.MINUS)
-        assertThat(Operator.findOperator(test3)).isEqualTo(Operator.MULTIPLY)
-        assertThat(Operator.findOperator(test4)).isEqualTo(Operator.DIVIDED)
-        assertThatThrownBy { Operator.findOperator(test5) }.isInstanceOf(IllegalArgumentException::class.java)
+        assertThat(Operator.findOperator(plus)).isEqualTo(Operator.PLUS)
+        assertThat(Operator.findOperator(minus)).isEqualTo(Operator.MINUS)
+        assertThat(Operator.findOperator(multiply)).isEqualTo(Operator.MULTIPLY)
+        assertThat(Operator.findOperator(divide)).isEqualTo(Operator.DIVIDED)
+        assertThatThrownBy { Operator.findOperator(shap) }.isInstanceOf(IllegalArgumentException::class.java)
     }
 }
