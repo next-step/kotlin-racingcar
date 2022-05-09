4ae198c9e61e167064ff3f9251da17f415773404
leeyohan93 <good_1411@naver.com>
2022-05-10 08:41:44 +0900

    refactor: Car.location 을 내부에서만 변경하도록 설정

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -3,11 +3,14 @@ package racingcar.domain
 import racingcar.domain.Location.Companion.BASE_LOCATION
 
 class Car(
-    var location: Location = BASE_LOCATION
+    private var _location: Location = BASE_LOCATION
 ) {
+    val location: Location
+        get() = _location
+
     fun move(movingStrategy: MovingStrategy) {
         if (movingStrategy.isMovable()) {
-            location = location.forward()
+            _location = _location.forward()
         }
     }
 }
