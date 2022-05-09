f47c05f3df64d808d648492ac26b154624a73df4
ujusy <lsyeon1997@ajou.ac.kr>
2022-05-10 01:14:11 +0900

    refactor: make constant boundary integer

--- src/main/kotlin/car_racing/RandomGenerator.kt
+++ src/main/kotlin/car_racing/RandomGenerator.kt
@@ -3,8 +3,9 @@ package car_racing
 import java.util.Random
 
 object RandomGenerator {
+    private const val BOUNDARY_INT = 10
 
     private val random = Random()
 
-    fun value(): Int = random.nextInt(10)
+    fun value(): Int = random.nextInt(BOUNDARY_INT)
 }
