9dce9ceeb0e3ab8e13a22990d9e1cce88401699e
d000l <questknight26@gmail.com>
2022-05-09 11:30:04 +0900

    test : 사용자 입력 테스트 작성

---
+++ src/test/kotlin/racing/RacingInputManagerKoTest.kt
@@ -0,0 +1,44 @@
+package racing
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
+import java.io.ByteArrayInputStream
+
+class RacingInputManagerKoTest : DescribeSpec({
+    describe("receive method") {
+        forAll(
+            row("5\n3", 5, 3),
+            row("3\n3", 3, 3),
+            row("13\n1", 13, 1)
+        ) { input, carNumber, moveCount ->
+            context("with user input : $input") {
+                System.setIn(ByteArrayInputStream(input.toByteArray()))
+
+                it("returns ${UserInput(carNumber, moveCount)}") {
+                    RacingInputManager().receive() shouldBe UserInput(carNumber, moveCount)
+                }
+            }
+        }
+
+        forAll(
+            row(""),
+            row("    "),
+            row("자동차10대"),
+            row("지나가던강아지 한마리"),
+            row("IDontKnowKorean")
+        ) { input ->
+            context("with wrong user Input : ( $input )") {
+                System.setIn(ByteArrayInputStream(input.toByteArray()))
+
+                it("throws IllegalArgumentException") {
+                    shouldThrow<IllegalArgumentException> {
+                        RacingInputManager().receive()
+                    }
+                }
+            }
+        }
+    }
+})
---
+++ src/test/kotlin/racing/RacingInputManagerTest.kt
@@ -0,0 +1,35 @@
+package racing
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
+import java.io.ByteArrayInputStream
+
+class RacingInputManagerTest {
+
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            "'5\n3'  ,    5, 3",
+            "'3\n3'  ,    3, 3",
+            "'13\n1' ,   13, 1"
+        ]
+    )
+    fun `사용자의 입력이 들어오면, 입력을 리턴한다`(input: String, carNumber: Int, moveCount: Int) {
+        System.setIn(ByteArrayInputStream(input.toByteArray()))
+
+        assertThat(RacingInputManager().receive()).isEqualTo(UserInput(carNumber, moveCount))
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = [" ", "    ", "자동차10대", "지나가던강아지 한마리", "IDontKnowKorean"])
+    fun `잘못된 사용자의 입력이 들어오면, IllegalArgumentException 예외가 발생한다`(input: String) {
+        System.setIn(ByteArrayInputStream(input.toByteArray()))
+
+        assertThrows<IllegalArgumentException> {
+            RacingInputManager().receive()
+        }
+    }
+}
