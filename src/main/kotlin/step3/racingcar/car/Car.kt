ae39ec5ba4000c0b6fc4e8a541527e8f7ee38ebe
kwanguuuu <kwangyu326@gmail.com>
2022-05-09 09:16:44 +0900

    [step3] test: car 객체에 이동/멈춤 기능을 구현함.
    - Position.kt 추가
    - Car에 이동관련 함수 추가

--- src/main/kotlin/step3/racingcar/car/Car.kt
+++ src/main/kotlin/step3/racingcar/car/Car.kt
@@ -1,6 +1,19 @@
 package step3.racingcar.car
 
 class Car {
+    private val drivingRecord: MutableList<Position> = mutableListOf()
+
+    fun getDrivingRecord(): List<Position> {
+        return drivingRecord.toList()
+    }
+
+    fun move() {
+        drivingRecord.add(Position.MOVE)
+    }
+
+    fun stop() {
+        drivingRecord.add(Position.STOP)
+    }
 
     fun isMovable(moveNumber: Int): Boolean {
         return moveNumber >= 4
--- src/test/kotlin/step3/racingcar/CarTest.kt
+++ src/test/kotlin/step3/racingcar/CarTest.kt
@@ -3,6 +3,7 @@ package step3.racingcar
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import step3.racingcar.car.Car
+import step3.racingcar.car.Position
 
 class CarTest {
 
@@ -15,5 +16,36 @@ class CarTest {
         assertThat(Car().isMovable(moveNumber)).isTrue
         assertThat(Car().isMovable(nonMoveNumber)).isFalse
         assertThat(Car().isMovable(boundaryNumber)).isTrue
+    }
+
+    @Test
+    fun `차 이동&멈춤의 상태를 저장한다`() {
+        // given
+        val moveTestCar = Car()
+
+        // when
+        moveTestCar.move()
+        val recored1 = moveTestCar.getDrivingRecord()
+
+        // then
+        assertThat(recored1.size).isEqualTo(1)
+        assertThat(recored1[0]).isEqualTo(Position.MOVE)
+
+        // given
+        val stopTestCar = Car()
+
+        // when
+        stopTestCar.stop()
+        val recored2 = stopTestCar.getDrivingRecord()
+
+        // then
+        assertThat(recored2.size).isEqualTo(1)
+        assertThat(recored2[0]).isEqualTo(Position.STOP)
+    }
+
+    @Test
+    fun `여러번 차 이동멈춤을 시도한다`() {
+        val car = Car()
+        // 이동멈춤 권한을 driver에게 이동
     }
 }
