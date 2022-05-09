d5bc57fcb7c3549546bcfeca8b19d54a7d1605f4
link-js <link.js@kakaocorp.com>
2022-05-09 22:08:39 +0900

    refactor : random 점수 뽑는 함수 변경

--- src/main/kotlin/racingCar/controller/RacingController.kt
+++ src/main/kotlin/racingCar/controller/RacingController.kt
@@ -3,7 +3,6 @@ package racingCar.controller
 import racingCar.constants.Message
 import racingCar.model.Car
 import racingCar.model.RaceResult
-import java.util.Random
 
 /**
  * 레이싱을 시작하고, 계산하는 클래스
@@ -39,11 +38,8 @@ class RacingController {
         }
     }
 
-    // 주사위의 숫자는 MINIMUM_SCORE 부터 EXCEEDED_SCORE -1 값까지 나온다.
-    // ex MINIMUM_SCORE = 1, EXCEEDED_SCORE = 6 일땐 [1,2,3,4,5]
     private fun rollTheDice(): Int {
-        val random = Random()
-        return random.nextInt(EXCEEDED_SCORE - MINIMUM_SCORE) + MINIMUM_SCORE
+        return (MINIMUM_SCORE..MAXIMUM_SCORE).random()
     }
 
     private fun checkCanRunScore(score: Int): Boolean {
@@ -52,7 +48,7 @@ class RacingController {
 
     companion object {
         private const val MINIMUM_SCORE = 0
-        private const val EXCEEDED_SCORE = 10
+        private const val MAXIMUM_SCORE = 9
         private const val SCORE_OF_CAN_GO = 4
         private const val CAN_VALID_NUMBER = 1
     }
