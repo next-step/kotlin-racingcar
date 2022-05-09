b951644884194b4844ede2b4d2fbebf732c68c4c
justin-s <justin.s@kakaocorp.com>
2022-05-09 20:23:26 +0900

    feat : 연산 테스트

--- src/main/kotlin/calculator/Operator.kt
+++ src/main/kotlin/calculator/Operator.kt
@@ -1,14 +1,18 @@
 package calculator
 
+import java.math.BigDecimal
 import java.util.function.BiFunction
 
 enum class Operator(
     private val type: String,
-    private val expression: BiFunction<Int, Int, Any>
+    private val expression: BiFunction<Long, Long, BigDecimal>
 ) {
-    PLUS("*", BiFunction { a: Int, b: Int -> a + b });
+    PLUS("+", { a: Long, b: Long -> BigDecimal.valueOf(a).plus(BigDecimal.valueOf(b)) }),
+    MINUS("+", { a: Long, b: Long -> BigDecimal.valueOf(a).minus(BigDecimal.valueOf(b)) }),
+    MULTI("+", { a: Long, b: Long -> BigDecimal.valueOf(a).multiply(BigDecimal.valueOf(b)) }),
+    DIVIDE("+", { a: Long, b: Long -> BigDecimal.valueOf(a).divide(BigDecimal.valueOf(b)) });
 
-    fun cal(a: Int, b: Int): Any {
+    fun calculate(a: Long, b: Long): BigDecimal {
         return expression.apply(a, b)
     }
 }
--- src/test/kotlin/calculator/OperatorTest.kt
+++ src/test/kotlin/calculator/OperatorTest.kt
@@ -6,7 +6,25 @@ import org.junit.jupiter.api.Test
 internal class OperatorTest {
     @Test
     fun `덧셈 테스트`() {
-        val cal = Operator.PLUS.cal(1, 2)
-        assertThat(cal).isEqualTo(3);
+        val cal = Operator.PLUS.calculate(1, 2)
+        assertThat(cal.intValueExact()).isEqualTo(3)
+    }
+
+    @Test
+    fun `뺄셈 테스트`() {
+        val cal = Operator.MINUS.calculate(2, 2)
+        assertThat(cal.intValueExact()).isEqualTo(0)
+    }
+
+    @Test
+    fun `곱셈 테스트`() {
+        val cal = Operator.MULTI.calculate(1, 2)
+        assertThat(cal.intValueExact()).isEqualTo(2)
+    }
+
+    @Test
+    fun `나눗셈 테스트`() {
+        val cal = Operator.DIVIDE.calculate(1, 2)
+        assertThat(cal.toDouble()).isEqualTo(0.5)
     }
 }
