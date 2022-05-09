a9c1885fc4fcf017b74d80095808c64639a50d26
austen.lee <austen.lee@kakaocorp.com>
2022-05-09 21:37:56 +0900

    step3 feat : 실행시켰을 때 0 또는 1 값의 거리를 얻을 수 있는 RandomEngine 클래스와 테스트

--- src/test/kotlin/step3/domain/EngineTest.kt
+++ src/test/kotlin/step3/domain/EngineTest.kt
@@ -1,6 +1,8 @@
 package step3.domain
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.RepeatedTest
 import org.junit.jupiter.api.Test
 
 class EngineTest {
@@ -20,5 +22,13 @@ class EngineTest {
 
         engine2.isBroken = true
         assertThat(engine2.getMovableDistance()).isEqualTo(0)
+    }
+
+    @RepeatedTest(100)
+    fun `RandomEngine은 실행시켰을 때 0 또는 1의 값의 거리를 반환한다`() {
+        val distance = RandomEngine.getMovableDistance()
+        assertTrue(
+            distance == RandomEngine.MOVE_SUCCESS || distance == RandomEngine.MOVE_FAIL
+        )
     }
 }
