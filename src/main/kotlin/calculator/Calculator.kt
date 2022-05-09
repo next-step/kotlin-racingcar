c99e30ea1c7f21caf511454aa2947adc30c791c0
teo.stra <teo.stra@kakaocorp.com>
2022-05-10 00:10:54 +0900

    step2: imple calculator

---
+++ src/main/kotlin/calculator/Calculator.kt
@@ -0,0 +1,46 @@
+package calculator
+
+class Calculator {
+    fun plus(a: Long, b: Long) = a + b
+    fun minus(a: Long, b: Long) = a - b
+    fun multiply(a: Long, b: Long) = a * b
+    fun divide(a: Long, b: Long) = a / b
+
+    fun calculate(formula: String?): Long {
+        if (formula.isNullOrBlank())
+            throw IllegalArgumentException()
+
+        val iter = formula.split(" ").iterator()
+            .takeIf { it.hasNext() }
+            ?: throw IllegalArgumentException()
+
+        var left = iter.nextLong()
+
+        while (iter.hasNext()) {
+            val operand = iter.nextOperand()
+            val right = iter.nextLong()
+
+            left = when (operand) {
+                '+' -> plus(left, right)
+                '-' -> minus(left, right)
+                '*' -> multiply(left, right)
+                '/' -> divide(left, right)
+                else -> throw IllegalArgumentException()
+            }
+        }
+
+        return left
+    }
+
+    private fun Iterator<String>.nextLong() = runCatching {
+        next().toLong()
+    }.getOrElse { throw IllegalArgumentException() }
+
+    private fun Iterator<String>.nextOperand() = runCatching {
+        next()[0]
+    }.getOrElse { throw IllegalArgumentException() }
+}
+
+fun main() {
+    println(Calculator().calculate("2 + 3 / 5"))
+}
