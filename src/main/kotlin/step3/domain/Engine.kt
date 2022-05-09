133b6deb08bc5012924e2c9d8acab40bef87d52d
austen.lee <austen.lee@kakaocorp.com>
2022-05-09 21:34:42 +0900

    step3 feat : 자동차의 전진 거리를 구할 수 있는 Engine 클래스와 테스트

---
+++ src/main/kotlin/step3/domain/Engine.kt
@@ -0,0 +1,5 @@
+package step3.domain
+
+fun interface Engine {
+    fun getMovableDistance(): Int
+}
---
+++ src/test/kotlin/step3/domain/EngineTest.kt
@@ -0,0 +1,24 @@
+package step3.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class EngineTest {
+    @Test
+    fun `엔진을 실행시켜 이동되는 거리를 구할 수 있다`() {
+        val engine1 = Engine { 3 }
+        assertThat(engine1.getMovableDistance()).isEqualTo(3)
+
+        val engine2 = object : Engine {
+            var isBroken = false
+
+            override fun getMovableDistance(): Int {
+                return if (isBroken) 0 else 10
+            }
+        }
+        assertThat(engine2.getMovableDistance()).isEqualTo(10)
+
+        engine2.isBroken = true
+        assertThat(engine2.getMovableDistance()).isEqualTo(0)
+    }
+}
