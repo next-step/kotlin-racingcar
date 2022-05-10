9eb9a28fa7cf17b7765c7cfca3a61a1fe35b2394
juice119 <juyeol@inflearn.com>
2022-05-11 01:17:21 +0900

    ref: InputDto 파라미터 이름 상수로 변경

--- src/main/kotlin/CarRacing/inputDto.kt
+++ src/main/kotlin/CarRacing/inputDto.kt
@@ -7,14 +7,16 @@ class inputDto(carCountString: String, movementTryCountString: String) {
     val movementTryCount: Int
 
     init {
-        validateForIntegerString(carCountString, "자동차 대수")
-        validateForIntegerString(movementTryCountString, "시도할 횟수")
+        validateForIntegerString(carCountString, CAR_COUNT_NAME)
+        validateForIntegerString(movementTryCountString, MOVEMENT_TRY_COUNT_NAME)
 
         carCount = parseInt(removeSpace(carCountString))
         movementTryCount = parseInt(removeSpace(movementTryCountString))
     }
 
     companion object {
+        private const val CAR_COUNT_NAME = "자동차 대수"
+        private const val MOVEMENT_TRY_COUNT_NAME = "시도할 횟수"
         private val ONLY_NUMBER_REGEX = Regex("([0-9])+")
 
         private fun removeSpace(spaceContainedString: String): String {
