0c85330726e2724338a4df794dcfe77e95005cbf
austen.lee <austen.lee@kakaocorp.com>
2022-05-10 21:49:30 +0900

    step3 test : CarTest 세분화

--- src/test/kotlin/step3/domain/CarTest.kt
+++ src/test/kotlin/step3/domain/CarTest.kt
@@ -6,14 +6,18 @@ import org.junit.jupiter.api.Test
 class CarTest {
     @Test
     fun `proceed를 통해 Engine에서 정하는 거리 만큼 Car의 proceedLevel을 높일 수 있다`() {
-        val car1 = Car { 1 }
-        car1.proceed()
-        assertThat(car1.proceedLevel).isEqualTo(1)
+        val car = Car { 1 }
 
-        car1.proceed()
-        assertThat(car1.proceedLevel).isEqualTo(2)
+        car.proceed()
+        assertThat(car.proceedLevel).isEqualTo(1)
 
-        val car2 = Car(
+        car.proceed()
+        assertThat(car.proceedLevel).isEqualTo(2)
+    }
+
+    @Test
+    fun `한 번 사용하면 고장나는 Engine을 넣은 Car는 처음 한 번만 proceedLevel이 증가한다`() {
+        val car = Car(
             object : Engine {
                 var isBroken = false
 
@@ -25,9 +29,10 @@ class CarTest {
             }
         )
 
-        car2.proceed()
-        assertThat(car2.proceedLevel).isEqualTo(10)
-        car2.proceed()
-        assertThat(car2.proceedLevel).isEqualTo(10)
+        car.proceed()
+        assertThat(car.proceedLevel).isEqualTo(10)
+
+        car.proceed()
+        assertThat(car.proceedLevel).isEqualTo(10)
     }
 }
