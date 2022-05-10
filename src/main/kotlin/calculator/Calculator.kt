56f4b275449c3f81f1812472aef268ed41efa165
Molly <dngwoodo@gmail.com>
2022-05-10 23:59:32 +0900

    refactor: split한 데이터를 활용해서 계산을 수행할 수 있도록 리팩토링
    - 멤버변수를 사용하지 않도록 변경
    - 애매한 재귀 코드 제거
    - 일관성있게 한줄짜리 코드는 private 메서드 분리 X

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -1,45 +1,24 @@
 package calculator
 
 class Calculator {
-    private var total: Float = 0F
-
     fun calculate(calculatorFormula: String?): Float {
         require(!calculatorFormula.isNullOrEmpty()) {
             throw IllegalArgumentException("유효하지 않은 계산식 입니다.")
         }
 
-        val parsedCalculateFormula = this.parseFor(calculatorFormula)
+        val parsedCalculateFormula = calculatorFormula.split(" ")
+        val restParsedCalculateFormula = parsedCalculateFormula.drop(1)
+        var total = parsedCalculateFormula[0].toFloat()
 
-        total = parsedCalculateFormula[TOTAL_INDEX].toFloat()
-        val operand = parsedCalculateFormula[OPERAND_INDEX].toFloat()
-        val operator = parsedCalculateFormula[OPERATOR_INDEX]
+        for (i: Int in restParsedCalculateFormula.indices step(2)) {
+            val operand = restParsedCalculateFormula[i + 1].toFloat()
+            val operator = restParsedCalculateFormula[i]
 
-        total = this.calculate(total, operand, operator)
-
-        if (parsedCalculateFormula.size > NUMBER_OF_ONE_CALCULATOR_FORMULA_INDEX) {
-            val restParsedCalculateFormula = parsedCalculateFormula.drop(NUMBER_OF_ONE_CALCULATOR_FORMULA_INDEX)
-            calculate("$total ${restParsedCalculateFormula.joinToString(" ")}")
+            calculate(total, operand, operator).also { total = it }
         }
 
         return total
     }
 
-    private val parseFor = { calculatorFormula: String ->
-        calculatorFormula.split(' ')
-    }
-
-    private val calculate = { a: Float, b: Float, operator: String ->
-        Operator.evaluate(a, b, operator)
-    }
-
-    companion object {
-        // const val는 컴파일 시 결정되는 상수이다.
-        // const val은 클래스의 생성자에 할당될 수 없다,
-        // const val을 함수나 클래스 내에서 사용하려면 companion object이하 중괄호 안에 선언해야 한다.
-        // main 함수에서 println(Calculator.OPERATOR_INDEX) 이런식으로 바로 접근이 가능하다.
-        private const val TOTAL_INDEX = 0
-        private const val OPERATOR_INDEX = 1
-        private const val OPERAND_INDEX = 2
-        private const val NUMBER_OF_ONE_CALCULATOR_FORMULA_INDEX = 3
-    }
+    private fun calculate(total: Float, operand: Float, operator: String): Float = Operator.evaluate(total, operand, operator)
 }
