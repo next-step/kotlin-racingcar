4089de68dad003666927bfb402fbf64a55397be8
juice119 <juyeol@inflearn.com>
2022-05-11 01:14:50 +0900

    feat: InputDto 생성자 추가

--- src/main/kotlin/CarRacing/inputDto.kt
+++ src/main/kotlin/CarRacing/inputDto.kt
@@ -1,16 +1,28 @@
 package CarRacing
 
-class inputDto(carCount: String?, movementTryCount: String?) {
+import java.lang.Integer.parseInt
+
+class inputDto(carCountString: String, movementTryCountString: String) {
+    val carCount: Int
+    val movementTryCount: Int
+
+    init {
+        validateForIntegerString(carCountString, "자동차 대수")
+        validateForIntegerString(movementTryCountString, "시도할 횟수")
+
+        carCount = parseInt(removeSpace(carCountString))
+        movementTryCount = parseInt(removeSpace(movementTryCountString))
+    }
 
     companion object {
-        val ONLY_NUMBER_REGEX = Regex("([0-9])+")
+        private val ONLY_NUMBER_REGEX = Regex("([0-9])+")
 
         private fun removeSpace(spaceContainedString: String): String {
             return spaceContainedString.replace("\\s".toRegex(), "")
         }
 
         fun validateForIntegerString(numberString: String, parameterName: String) {
-            if(!ONLY_NUMBER_REGEX.matches(numberString)) {
+            if(!ONLY_NUMBER_REGEX.matches(removeSpace(numberString))) {
                 throw IllegalArgumentException(ErrorMessage.getErrorMessageForOnlyIntegerString(parameterName, numberString))
             }
         }
--- src/test/kotlin/CarRacing/inputDtoTest.kt
+++ src/test/kotlin/CarRacing/inputDtoTest.kt
@@ -2,10 +2,52 @@ package CarRacing
 
 import io.kotest.assertions.throwables.shouldNotThrow
 import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.*
 
 class inputDtoTest: StringSpec({
+    "inputDto 는 자동차 대수, 시도할 횟수를 입력 받아 Int Property 로 저장한다" {
+        // given
+        val carCount = "3"
+        val movementTryCount = "5"
+
+        // when
+        val inputDto = inputDto(carCount, movementTryCount)
+
+        // then
+        inputDto.carCount shouldBe 3
+        inputDto.movementTryCount shouldBe 5
+    }
+
+    "inputDto 는 공백이 포함된 값을 입력 받아도 정상 저장 한다" {
+        // given
+        val carCount = "  3  0  "
+        val movementTryCount = "  5  1  3   2  "
+
+        // when
+        val inputDto = inputDto(carCount, movementTryCount)
+
+        // then
+        inputDto.carCount shouldBe 30
+        inputDto.movementTryCount shouldBe 5132
+    }
+
+    "inputDto 는 정수가 아닌 값을 넣으면 IllegalArgumentException 에러가 발생한다" {
+        // given
+        val carCount = "  test 123 !@#"
+        val movementTryCount = "  5  1  3   2  "
+
+        // when
+        val exception = shouldThrowExactly<IllegalArgumentException> {
+            inputDto(carCount, movementTryCount)
+        }
+
+        // then
+        exception.message shouldBe ErrorMessage.getErrorMessageForOnlyIntegerString("자동차 대수", carCount)
+    }
+
+
     "validateForIntegerString 정수를 확인한다." {
         // given
         val integerString = "10"
