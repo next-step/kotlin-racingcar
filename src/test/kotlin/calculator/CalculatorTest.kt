c27c99c655b271237af77c4dd8ddceeb90e7fd36
Justin.sg <justin.sg@kakaocorp.com>
2022-05-10 09:26:09 +0900

    [step2] ParameterizedTest 수정

--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -3,36 +3,58 @@ package calculator
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 internal class CalculatorTest {
 
 
-    @Test
-    fun `뎃셈 연산`() {
-        assertThat(Calculator.calculate("1 + 2 + 3")).isEqualTo(6)
-        assertThat(Calculator.calculate("10 + 30")).isEqualTo(40)
-        assertThat(Calculator.calculate("0 + 1 + 2")).isEqualTo(3)
+    @ParameterizedTest
+    @CsvSource(
+        "1 + 2 + 3, 6",
+        "10 + 30, 40"
+    )
+    fun `뎃셈 연산`(expression: String, expect: Int) {
+        assertThat(Calculator.calculate(expression)).isEqualTo(expect)
     }
 
-    @Test
-    fun `뺄셈 연산`() {
-        assertThat(Calculator.calculate("1 - 2 - 3")).isEqualTo(-4)
-        assertThat(Calculator.calculate("10 - 30")).isEqualTo(-20)
-        assertThat(Calculator.calculate("0 - 1 - 2")).isEqualTo(-3)
+    @ParameterizedTest
+    @CsvSource(
+        "1 - 2 - 3, -4",
+        "10 - 30, -20",
+        "0 - 1 - 2, -3",
+    )
+    fun `뺄셈 연산`(expression: String, expect: Int) {
+        assertThat(Calculator.calculate(expression)).isEqualTo(expect)
     }
 
 
-    @Test
-    fun `곱센 연산`() {
-        assertThat(Calculator.calculate("1 * 2 * 3")).isEqualTo(6)
-        assertThat(Calculator.calculate("10 * 30")).isEqualTo(300)
-        assertThat(Calculator.calculate("0 * 1 * 2")).isEqualTo(0)
+    @ParameterizedTest
+    @CsvSource(
+        "1 * 2 * 3, 6",
+        "10 * 30, 300",
+        "0 * 1 * 2, 0",
+    )
+    fun `곱센 연산`(expression: String, expect: Int) {
+        assertThat(Calculator.calculate(expression)).isEqualTo(expect)
     }
 
-    @Test
-    fun `나눗셈 연산`() {
-        assertThat(Calculator.calculate("100 / 10 / 10")).isEqualTo(1)
-        assertThat(Calculator.calculate("100 / 20")).isEqualTo(5)
+    @ParameterizedTest
+    @CsvSource(
+        "100 / 10 / 10, 1",
+        "100 / 20, 5"
+    )
+    fun `나눗셈 연산`(expression: String, expect: Int) {
+        assertThat(Calculator.calculate(expression)).isEqualTo(expect)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "1 + 2 + 3 - 6 + 1 * 10 / 10, 1",
+        "10 - 9 * 10 / 10 * 3, 3",
+    )
+    fun `복합 연산`(expression: String, expect: Int) {
+        assertThat(Calculator.calculate(expression)).isEqualTo(expect)
     }
 
 
