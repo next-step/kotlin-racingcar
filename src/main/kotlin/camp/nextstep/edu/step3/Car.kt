e40bda68c35e63c48c27d279ec440057bcc2e8d8
aeomhs(엄현식) <aeomhs@gmail.com>
2022-05-10 15:06:35 +0900

    test: Car 실패 테스트 코드 수정
    실제 요구사항에 맞게 Random 하게 움직여야 한다는 내용을 추가

--- src/main/kotlin/camp/nextstep/edu/step3/Car.kt
+++ src/main/kotlin/camp/nextstep/edu/step3/Car.kt
@@ -1,8 +1,18 @@
 package camp.nextstep.edu.step3
 
-class Car {
+import kotlin.random.Random
+
+class Car(id: Int) {
+
+    private val engine: Engine = Engine(id)
 
     fun raceOn(track: Track, moveCount: Int) {
+        engine.move()
         TODO()
+    }
+
+    private class Engine(id: Int) {
+        private val random = Random(id)
+        fun move(): Boolean = random.nextInt(0, 9) >= 4
     }
 }
--- src/test/kotlin/camp/nextstep/edu/step3/CarTest.kt
+++ src/test/kotlin/camp/nextstep/edu/step3/CarTest.kt
@@ -1,17 +1,43 @@
 package camp.nextstep.edu.step3
 
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import kotlin.random.Random
 
 internal class CarTest {
 
     @DisplayName("자동차는 트랙 위에서 달릴 수 있다.")
     @Test
     fun canRaceOnTrack() {
-        val car = Car()
-        val track = Track(5)
+        // Given
+        val id = 1
+        val car = Car(id)
+        val mockkRandom = mockk<Random>()
+        mockkStatic("kotlin.random.RandomKt")
+        every { mockkRandom.nextInt(0, 10) } returnsMany listOf(8, 9, 1, 2, 3)
+        every { Random(id) } returns mockkRandom
 
-        car.raceOn(track, 3)
-        TODO()
+        val trackLength = 5
+        val track = Track(trackLength)
+        assertFalse(track.isPassedAt(0))
+        assertFalse(track.isPassedAt(1))
+        assertFalse(track.isPassedAt(2))
+        assertFalse(track.isPassedAt(3))
+        assertFalse(track.isPassedAt(4))
+
+        // When
+        car.raceOn(track, 5)
+
+        // Then
+        assertTrue(track.isPassedAt(0))
+        assertTrue(track.isPassedAt(1))
+        assertFalse(track.isPassedAt(2))
+        assertFalse(track.isPassedAt(3))
+        assertFalse(track.isPassedAt(4))
     }
 }
