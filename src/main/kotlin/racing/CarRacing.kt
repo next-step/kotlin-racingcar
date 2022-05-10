39e03d4d135a008376390159be9c6ff53305633e
KM <sodp2@naver.com>
2022-05-11 00:26:17 +0900

    feat: 차 전진 함수를 조건에 맞으면 한칸만 움직이게 수정

--- src/main/kotlin/racing/CarRacing.kt
+++ src/main/kotlin/racing/CarRacing.kt
@@ -3,20 +3,12 @@ package racing
 class CarRacing {
     fun input() {
         println("자동차 대수는 몇 대인가요?")
-        val carCount = readLine()?.toInt()
+        val carCount = readLine()!!.toInt()
         println("시도할 횟수는 몇 회인가요?")
-        val moveCount = readLine()?.toInt()
+        val moveCount = readLine()!!.toInt()
 
         // 실행
     }
 
-    fun go(): Int {
-        val randomValue = (0..9).random()
-
-        return if (randomValue >= 4) {
-            randomValue
-        } else {
-            0
-        }
-    }
+    private fun go(): Boolean = (0..9).random() >= 4
 }
