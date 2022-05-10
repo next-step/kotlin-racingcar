18daea2e32baa135e46e94fd05c4accbf91f2b73
haningyu <dlsrb0412@naver.com>
2022-05-11 05:04:58 +0900

    test: 요구사항 재정리 및 테스트코드 추가.

--- src/main/kotlin/calculator/README.md
+++ src/main/kotlin/calculator/README.md
@@ -13,8 +13,11 @@
 [X] 뺄셈
 [X] 곱셈
 [X] 나눗셈
+[X] 사칙연산 복합
 ### 엣지케이스
 [X] 입력값이 null이거나 빈 공백 문자일 경우 IllegalArgumentException throw
-[ ] 시작이나 끝이 숫자가 아닌 경우 IllegalArgumentException throw
-[ ] 숫자와 허용된 기호 외에 문자가 들어가 있는 경우 IllegalArgumentException throw
-[ ] 숫자나 기호가 연속하여 나온 경우 IllegalArgumentException throw
+[X] 숫자 및 기호를 포함하여 3개 이상 입력을 안할 경우 IllegalArgumentException throw
+[X] 시작이나 끝이 숫자가 아닌 경우 IllegalArgumentException throw
+[X] 사칙연산 기호 외의 문자나 기호가 들어가 있는 경우 IllegalArgumentException throw
+[X] 숫자 외의 문자가 들어가 있는 경우 NumberFormatException throw
+[X] 숫자나 기호가 연속하여 나온 경우 IllegalArgumentException throw
--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -1,55 +1,118 @@
 package calculator
 
-import io.kotest.assertions.throwables.shouldThrowWithMessage
-import io.kotest.core.spec.style.AnnotationSpec
-import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.NullSource
+import org.junit.jupiter.params.provider.ValueSource
 
-class CalculatorTest : AnnotationSpec() {
-    @Test
-    fun `순서를 무시한 사칙연산이 이뤄지는지 확인`() {
+class CalculatorTest {
+    @DisplayName("덧셈이 제대로 이루어지는지 확인")
+    @ParameterizedTest
+    @ValueSource(strings = [ "0 + 5", "1 + 4", "2 + 3", "3 + 2", "4 + 1", "5 + 0" ])
+    fun `check plus function`(input: String) {
         val calculator = Calculator()
-        val example = "2 + 3 * 4 / 2"
-        val result = calculator.calculate(example)
+        val result = calculator.arithmeticOperation(input)
 
-        result shouldBe 10
+        assertTrue(result == 5)
     }
 
-    @Test
-    fun `공백문자열을 넘기면 exception이 발생하는지 확인`() {
+    @DisplayName("뺄셈이 제대로 이루어지는지 확인")
+    @ParameterizedTest
+    @ValueSource(strings = [ "10 - 5", "9 - 4", "8 - 3", "7 - 2", "6 - 1", "5 - 0" ])
+    fun `check minus function`(input: String) {
         val calculator = Calculator()
-        val example = "   "
-        shouldThrowWithMessage<IllegalArgumentException>("입력값이 null이거나 빈 공백이 아니여야합니다.") {
-            calculator.calculate(example)
+        val result = calculator.arithmeticOperation(input)
+
+        assertTrue(result == 5)
+    }
+
+    @DisplayName("나눗셈이 제대로 이루어지는지 확인")
+    @ParameterizedTest
+    @ValueSource(strings = [ "24 / 12", "12 / 6", "6 / 3", "4 / 2", "2 / 1" ])
+    fun `check divide function`(input: String) {
+        val calculator = Calculator()
+        val result = calculator.arithmeticOperation(input)
+
+        assertTrue(result == 2)
+    }
+
+    @DisplayName("곱셈이 제대로 이루어지는지 확인")
+    @ParameterizedTest
+    @ValueSource(strings = [ "12 * 3", "3 * 12", "6 * 6", "1 * 36", "36 * 1", "2 * 18", "18 * 2", "4 * 9"])
+    fun `check multiple function`(input: String) {
+        val calculator = Calculator()
+        val result = calculator.arithmeticOperation(input)
+
+        assertTrue(result == 36)
+    }
+
+    @DisplayName("순서를 무시한 사칙연산이 정상적으로 이뤄지는지 확인")
+    @ParameterizedTest
+    @ValueSource(strings = [ "2 + 3 * 4 / 2" ])
+    fun `check arithmeticOperation function`(input: String) {
+        val convertor = Convertor()
+        val calculator = Calculator(convertor)
+        val result = calculator.arithmeticOperation(input)
+
+        assertTrue(result == 10)
+    }
+
+    @DisplayName("입력값이 비거나 공백 문자일 경우 IllegalArgumentException throw하는지 확인")
+    @ParameterizedTest
+    @ValueSource(strings = [ "", " ", "          " ])
+    fun `throw IllegalArgumentException when input is empty or blank`(input: String) {
+        val calculator = Calculator()
+
+        assertThrows<IllegalArgumentException>("입력값이 null이거나 빈 공백이 아니여야합니다."){
+            calculator.arithmeticOperation(input)
         }
     }
 
-    @Test
-    fun `빈 문자열을 넘기면 exception이 발생하는지 확인`() {
+    @DisplayName("입력값이 null일 경우 IllegalArgumentException throw하는지 확인")
+    @ParameterizedTest
+    @NullSource
+    fun `throw IllegalArgumentException when input is null`(input: String?) {
         val calculator = Calculator()
-        val example = ""
 
-        shouldThrowWithMessage<IllegalArgumentException>("입력값이 null이거나 빈 공백이 아니여야합니다.") {
-            calculator.calculate(example)
+        assertThrows<IllegalArgumentException>("입력값이 null이거나 빈 공백이 아니여야합니다.") {
+            calculator.arithmeticOperation(input)
+        }
+    }
+    @DisplayName("시작이나 끝이 숫자가 아닌 경우 IllegalArgumentExeption throw하는지 확인")
+    @ParameterizedTest
+    @ValueSource(strings = [ "1 +", "1 + 1 +", "+ 1 + 2"])
+    fun `throw IllegalArgumentException when input's first or last word is not number`(input: String) {
+        val calculator = Calculator()
+        assertThrows<IllegalArgumentException>("수식의 시작 및 끝은 숫자여야합니다.") {
+            calculator.arithmeticOperation(input)
         }
     }
 
-    @Test
-    fun `null인 문자열을 넘기면 exception이 발생하는지 확인`() {
+    @DisplayName("허용된 사칙연산 기호 외의 문자나 기호가 들어가 있는 경우 IllegalArgumentException throw하는지 확인")
+    @ParameterizedTest
+    @ValueSource(strings = [ "1 A 2", "1 A 2 + 4", "1 % 2", "1 + 3 % 2"])
+    fun `throw IllegalArgumentException when input's sign not in plus, minus, divide, multiple`(input: String) {
         val calculator = Calculator()
-        val example = null
-
-        shouldThrowWithMessage<IllegalArgumentException>("입력값이 null이거나 빈 공백이 아니여야합니다.") {
-            calculator.calculate(example)
-        }
+        assertThrows<IllegalArgumentException>{ calculator.arithmeticOperation(input) }
+            .also { assertTrue(it.message!!.contains("은 허용되지않은 기호입니다.(+, -, *, / 만 허용됩니다.)")) }
     }
 
-    @Test
-    fun `사칙연산 외의 기호가 포함될 경우 exception이 발생하는지 확인`() {
+    @DisplayName("숫자 외의 문자가 들어가 있는 경우 NumberFormatException throw하는지 확인")
+    @ParameterizedTest
+    @ValueSource(strings = [ "2 + a / 1", "2 + 안녕 / 4", "0 + 3 * 🤣 / 1"])
+    fun `throw NumberFormatException when input hasn't a number in even position`(input: String) {
         val calculator = Calculator()
-        val example = "2 % 3 * 4 / 2"
+        assertThrows<IllegalArgumentException> { calculator.arithmeticOperation(input) }
+            .also { assertTrue(it.message!!.contains("은 숫자가 아닙니다"))  }
 
-        shouldThrowWithMessage<IllegalArgumentException>("수학기호는 +, -, *, / 만 허용됩니다.") {
-            calculator.calculate(example)
-        }
+    }
+    @DisplayName("숫자나 기호가 연속하여 나온 경우 IllegalArgumentException throw하는지 확인")
+    @ParameterizedTest
+    @ValueSource(strings = [ "1 1 1 1", "1 + + 2", "1 + 2 2 + +"])
+    fun `throw IllegalArgumentException when input has a continues number or sign`(input: String) {
+        val calculator = Calculator()
+        assertThrows<IllegalArgumentException> { calculator.arithmeticOperation(input) }
     }
 }
