30c1513f6585fb6dd2954e949f1e02c15d66f125
erie0210 <2j00923@gmail.com>
2022-05-10 12:41:14 +0900

    feat: validator에 exception 처리, 테스트 추가

--- src/main/kotlin/stringCalculator/Validator.kt
+++ src/main/kotlin/stringCalculator/Validator.kt
@@ -2,43 +2,42 @@ package stringCalculator
 
 class Validator {
 
-    fun isValidInput(input: String): Boolean {
-        return (
-            !isNullOrBlank(input) &&
-                !containsNonOperator(input) &&
-                startWithNumber(input) &&
-                endWithNumber(input) &&
-                !containsCharacter(input)
-            )
+    fun isValidInput(input: String?): Boolean {
+        require(!isNullOrBlank(input)) { INPUT_NULL_OR_BLANK }
+        require(!containsNonOperator(input)) { INVALID_OPERATOR }
+        startWithNumber(input)?.let { require(it) { INPUT_NOT_START_WITH_NUMBER } }
+        endWithNumber(input)?.let { require(it) { INPUT_NOT_END_WITH_NUMBER } }
+        require(!containsCharacter(input)) { INPUT_CONTAINS_CHARACTER }
+        return true
     }
 
-    private fun isNullOrBlank(input: String): Boolean {
+    private fun isNullOrBlank(input: String?): Boolean {
         return input.isNullOrBlank()
     }
 
-    private fun containsNonOperator(input: String): Boolean {
-        input.forEach { c: Char ->
-            if (!c.isDigit() || !isOperator(c)) return true
+    private fun containsNonOperator(input: String?): Boolean {
+        input?.forEach { c: Char ->
+            if (!c.isDigit() && !isOperator(c)) return true
         }
         return false
     }
 
-    private fun startWithNumber(input: String): Boolean {
-        return input[0].isDigit()
+    private fun startWithNumber(input: String?): Boolean? {
+        return input?.get(0)?.isDigit()
     }
 
-    private fun endWithNumber(input: String): Boolean {
-        return input.last().isDigit()
+    private fun endWithNumber(input: String?): Boolean? {
+        return input?.last()?.isDigit()
     }
 
-    private fun containsCharacter(input: String): Boolean {
-        input.forEach { c: Char ->
+    private fun containsCharacter(input: String?): Boolean {
+        input?.forEach { c: Char ->
             if (c.isLetter()) return true
         }
         return false
     }
 
-    private fun isOperator(c: Char): Boolean {
-        return (c == '+' || c == '-' || c == '*' || c == '/')
+    private fun isOperator(c: Char?): Boolean {
+        return (c == '+' || c == '-' || c == '*' || c == '/' || c == ' ')
     }
 }
--- src/test/kotlin/stringCalculator/ValidatorTest.kt
+++ src/test/kotlin/stringCalculator/ValidatorTest.kt
@@ -1,10 +1,63 @@
 package stringCalculator
 
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 
 internal class ValidatorTest {
 
+    val validator = Validator()
+
+    @AfterEach
+    fun tearDown() {
+    }
+
     @Test
-    fun isValidInput() {
+    fun `1 + 2 + 3`() {
+        val example = validator.isValidInput("1 + 2 + 3")
+        Assertions.assertThat(example).isEqualTo(true)
+    }
+
+    @Test
+    fun `blank test`() {
+        assertThatThrownBy { validator.isValidInput(" ") }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage(INPUT_NULL_OR_BLANK)
+    }
+
+    @Test
+    fun `NULL test`() {
+        assertThatThrownBy { validator.isValidInput(null) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage(INPUT_NULL_OR_BLANK)
+    }
+
+    @Test
+    fun `1 = 2`() {
+        assertThatThrownBy { validator.isValidInput("1 = 2") }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage(INVALID_OPERATOR)
+    }
+
+    @Test
+    fun `숫자로 시작하지 않는 경우 에러 처리`() {
+        assertThatThrownBy { validator.isValidInput("+ 2 * 3") }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage(INPUT_NOT_START_WITH_NUMBER)
+    }
+
+    @Test
+    fun `숫자로 끝나지 않는 경우 에러 처리`() {
+        assertThatThrownBy { validator.isValidInput("1 + 2 *") }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage(INPUT_NOT_END_WITH_NUMBER)
+    }
+
+    @Test
+    fun `문자가 들어가 있는 경우 에러 처리`() {
+        assertThatThrownBy { validator.isValidInput("1 + 2 *") }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage(INPUT_CONTAINS_CHARACTER)
     }
 }
