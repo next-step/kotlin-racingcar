08348a273213ffab260bbd83f1bba5c5b4c4ad9b
justin-s <justin.s@kakaocorp.com>
2022-05-11 00:41:54 +0900

    feat: 계산기 반복 추가

--- src/main/kotlin/calculator/StringCalculatorApplication.kt
+++ src/main/kotlin/calculator/StringCalculatorApplication.kt
@@ -1,7 +1,13 @@
 package calculator
 
 fun main() {
-    val value = readLine()
-    val customStringDelimiter = CustomStringDelimiter(value)
-    println(Expression.calculate(customStringDelimiter.getOperands(), customStringDelimiter.getOperators()))
+
+    while (true) {
+        val value = readLine()
+        if (value.equals("esc")) break
+        val customStringDelimiter = CustomStringDelimiter(value)
+        println(Expression.calculate(customStringDelimiter.getOperands(), customStringDelimiter.getOperators()))
+        println()
+    }
+
 }
