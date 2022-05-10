3f45cc76e8e3e56607005af85e5ed06e380eb4c5
aeomhs(엄현식) <aeomhs@gmail.com>
2022-05-10 16:13:05 +0900

    test: Racing 테스트 케이스 수정
    여러 개의 테스트 케이스로 나누기 애매하여.. 합침!

--- src/test/kotlin/camp/nextstep/edu/step3/RacingTest.kt
+++ src/test/kotlin/camp/nextstep/edu/step3/RacingTest.kt
@@ -4,28 +4,18 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 
 internal class RacingTest {
 
-    @DisplayName("자동차 경주를 시작하기 위한 새로운 인스턴스를 생성할 수 있다.")
+    @DisplayName("10대의 자동차가 각각의 트랙에서 경주를 할 수 있다.")
     @Test
     fun newRacing() {
-        Racing.new(10, 10)
-        TODO()
-    }
-
-    @DisplayName("자동차 경주를 시작할 수 있다.")
-    @Test
-    fun startRacing() {
-        Racing.new(10, 10).start()
-        TODO()
-    }
-
-    @DisplayName("자동차 경주가 끝나면 결과를 확인할 수 있다.")
-    @Test
-    fun getResultRacing() {
         val racing = Racing.new(10, 10)
-        racing.start()
+
+        assertDoesNotThrow {
+            racing.start()
+        }
 
         val resultMap = racing.result()
         assertNotNull(resultMap)
