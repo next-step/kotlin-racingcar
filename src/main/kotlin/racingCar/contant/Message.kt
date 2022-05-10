b615ea60535e61f6509fdff46029f79267aa66a9
erie0210 <2j00923@gmail.com>
2022-05-10 16:05:22 +0900

    docs: 요구사항 정리 및 메시지 작성

---
+++ src/main/kotlin/racingCar/contant/Message.kt
@@ -0,0 +1,7 @@
+package racingCar.contant
+
+val USER_CAR_INPUT = "자동차 갯수를 입력하세요."
+val USER_TRIAL_INPUT = "실행 횟수를 갯수를 입력하세요."
+val USER_QUIT = "게임을 종료하려면 q를 입력하세요."
+
+val ERROR_NOT_NUMBER_INPUT = "숫자를 입력하세요."
