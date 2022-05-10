24a668efa80b7e8fbf355242c1e527e84a8f912a
aeomhs(엄현식) <aeomhs@gmail.com>
2022-05-10 15:47:22 +0900

    feat: Track 구현

--- src/main/kotlin/camp/nextstep/edu/step3/Track.kt
+++ src/main/kotlin/camp/nextstep/edu/step3/Track.kt
@@ -1,12 +1,20 @@
 package camp.nextstep.edu.step3
 
-class Track(length: Int) {
+class Track(private val length: Int) {
+    private val traces: Array<Trace> = Array(length) { Trace() }
+    private var onTraceIndex: Int = 0
 
     fun markTrace() {
-        TODO()
+        if (onTraceIndex >= length) throw RuntimeException("end of track; cannot mark trace more.")
+        traces[onTraceIndex++].marked = true
     }
 
     fun isPassedAt(i: Int): Boolean {
-        TODO()
+        if (i < 0 || i >= length) throw IllegalArgumentException("$i is invalid index of track; trackLength=$length")
+        return traces[i].marked
+    }
+
+    private class Trace {
+        var marked: Boolean = false
     }
 }
