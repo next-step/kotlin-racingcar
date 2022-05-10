aee3f307369651e8e27bcd4efd7e3307baadbc52
jojoldu <jojoldu@gmail.com>
2022-05-11 08:50:17 +0900

    필드 & 파라미터명 변경

--- src/main/kotlin/calculator/Operator.kt
+++ src/main/kotlin/calculator/Operator.kt
@@ -1,19 +1,19 @@
 package calculator
 
-enum class Operator(expr: String, evaluate: (num1: Int, num2: Int) -> Int) {
+enum class Operator(symbol: String, evaluate: (num1: Int, num2: Int) -> Int) {
     PLUS("+", { num1, num2 -> num1 + num2 }),
     MINUS("-", { num1, num2 -> num1 - num2 }),
     MULTIPLY("*", { num1, num2 -> num1 * num2 }),
     DIVIDE("/", { num1, num2 -> num1 / num2 });
 
-    var display: String = expr
+    var symbol: String = symbol
         private set
     var evaluate = evaluate
         private set
 
     companion object {
-        fun find(target: String): Operator {
-            return values().find { it.display == target }
+        fun find(symbol: String): Operator {
+            return values().find { it.symbol == symbol }
                 ?: throw IllegalArgumentException("+, -, *, / 외에는 사용할 수 없습니다.")
         }
     }
