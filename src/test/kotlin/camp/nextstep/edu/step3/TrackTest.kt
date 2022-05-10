ca878f9c0f49ce9923eb95df87c8c4331de5e585
aeomhs(엄현식) <aeomhs@gmail.com>
2022-05-10 15:55:12 +0900

    test: Track 예외 처리 테스트 추가

--- src/test/kotlin/camp/nextstep/edu/step3/TrackTest.kt
+++ src/test/kotlin/camp/nextstep/edu/step3/TrackTest.kt
@@ -3,6 +3,10 @@ package camp.nextstep.edu.step3
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import java.lang.RuntimeException
 
 internal class TrackTest {
 
@@ -20,5 +24,30 @@ internal class TrackTest {
         assertEquals(true, track.isPassedAt(0))
         assertEquals(true, track.isPassedAt(1))
         assertEquals(false, track.isPassedAt(2))
+    }
+
+    @DisplayName("트랙 길이를 넘어서 흔적을 남길 수 없다.")
+    @Test
+    fun cannotMarkCarTraceOverTrack() {
+        val track = Track(3)
+
+        track.markTrace()
+        track.markTrace()
+        track.markTrace()
+
+        assertThrows<RuntimeException> {
+            track.markTrace()
+        }
+    }
+
+    @DisplayName("트랙 밖의 흔적을 확인할 수 없다.")
+    @ParameterizedTest(name = "{0} not include [0, 1, 2, 3, 4]")
+    @ValueSource(ints = [-100, -10, -1, 5, 6, 10])
+    fun cannotCheckCarTraceOverTrack(index: Int) {
+        val track = Track(5)
+
+        assertThrows<IllegalArgumentException> {
+            track.isPassedAt(index)
+        }
     }
 }
