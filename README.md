3e28e426d1b813f138404b4a9132baedbe175142
glenn.kgp <glenn.kgp@kakaocorp.com>
2022-05-10 19:54:27 +0900

    refactor: InputValidator object클래스로 변경

--- README.md
+++ README.md
@@ -84,7 +84,7 @@
 - [x] 자동차를 움직이는 로직을 외부에서 주입
 - [x] 테스트에 상수 명확하게 이해할 수 있게 처리
 - [x] 출력 문자열 상수로 변경
-- [ ] companion object to object class
+- [x] companion object to object class
 - [ ] 숫자체크 require만으로 변경
 - [ ] RacingCarGame에서 랜덤값을 주입하면 .. RacingCarGame에 결과에 대한 테스트는 어떻게할지 고민
 - [ ] gameResult _gameResults로 표현수정
--- src/main/kotlin/racingcar/InputValidator.kt
+++ src/main/kotlin/racingcar/InputValidator.kt
@@ -1,10 +1,8 @@
 package racingcar
 
-class InputValidator {
-    companion object {
-        fun checkNaturalNumber(input: String) {
-            val number = input.toIntOrNull()
-            require((number != null) && (number > 0))
-        }
+object InputValidator {
+    fun checkNaturalNumber(input: String) {
+        val number = input.toIntOrNull()
+        require((number != null) && (number > 0))
     }
 }
