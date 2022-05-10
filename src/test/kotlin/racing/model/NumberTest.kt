4ee68c8bedd60aac747c4e94f4283c37c6f57b31
YooInKeun <keun0390@naver.com>
2022-05-11 01:40:14 +0900

    refactor: assertThrows 문, 간결하게 수정

--- src/test/kotlin/racing/model/NumberTest.kt
+++ src/test/kotlin/racing/model/NumberTest.kt
@@ -1,8 +1,8 @@
 package racing.model
 
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -13,9 +13,7 @@ class NumberTest {
     @ValueSource(ints = [-1, 10])
     fun `숫자가 0에서 9 사이가 아니면 예외가 발생`(number: Int) {
         // when, then
-        val exception = assertThrows(IllegalArgumentException::class.java) {
-            Number(number)
-        }
+        val exception = assertThrows<IllegalArgumentException> { Number(number) }
         assertEquals("숫자는 0 이상 9 이하이어야 합니다. (number: $number)", exception.message)
     }
 
