906f0f630a21fa008cce16ff32a06a43cec2264d
kwanguuuu <kwangyu326@gmail.com>
2022-05-12 00:06:06 +0900

    [step3] refactor: ResultView 메서드명 변경, 출력 로직 수정

--- src/main/kotlin/step3/racingcar/view/ResultView.kt
+++ src/main/kotlin/step3/racingcar/view/ResultView.kt
@@ -1,11 +1,12 @@
 package step3.racingcar.view
 
 object ResultView {
-    private const val DASH_VIEW = "-"
+    private const val DASH_VIEW = '-'
 
-    fun show(result: Int) {
-        repeat(result) {
+    fun showRecordAsDash(driveRecord: Int) {
+        for (i in 0 until driveRecord) {
             print(DASH_VIEW)
         }
+        println()
     }
 }
