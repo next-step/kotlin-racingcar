03a99d66926f8a4d1fcb9edd54e3c21bfcebc006
juice119 <juyeol@inflearn.com>
2022-05-11 21:06:33 +0900

    feat: Car 클래스 생성

--- src/main/kotlin/carRacing/main.kt
+++ src/main/kotlin/carRacing/main.kt
@@ -4,7 +4,7 @@ fun main() {
     println("자동차 대수는 몇 대인가요?")
     val carCount: String? = readLine()
     println("시도할 횟수는 몇 회인가요?")
-    val movementTryCount:String? = readLine()
+    val movementTryCount: String? = readLine()
 
     println("carCount:" + carCount.toString())
     println("carCount:" + movementTryCount.toString())
