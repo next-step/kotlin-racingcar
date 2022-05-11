3b3a32dcf4c0da87947929e3e01dc67c566a4907
teo.stra <teo.stra@kakaocorp.com>
2022-05-11 22:51:01 +0900

    step3: internal 표시
    테스트를 위한 메소드 표시

--- src/main/kotlin/model/Race.kt
+++ src/main/kotlin/model/Race.kt
@@ -1,5 +1,7 @@
 package model
 
+import org.jetbrains.annotations.TestOnly
+
 class Race(
     numOfCars: Int
 ) {
@@ -12,5 +14,6 @@ class Race(
         return cars
     }
 
-    fun getCars() = cars
+    @TestOnly
+    internal fun getCars() = cars
 }
