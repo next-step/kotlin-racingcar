90546bcaf5164493433a32af507b8cff94309f54
link-js <link.js@kakaocorp.com>
2022-05-12 07:45:17 +0900

    refactor : input 테스트 가능하게 변경

--- src/main/kotlin/racingCar/view/InputView.kt
+++ src/main/kotlin/racingCar/view/InputView.kt
@@ -7,29 +7,25 @@ import racingCar.constants.Message
  * Created by Jaesungchi on 2022.05.07..
  */
 object InputView {
-    fun getNumberOfCars(): Int {
+    fun getNumberOfCars(readStringValue: () -> String? = { readlnOrNull() }): Int {
         printHowManyCars()
-        return getInput()
+        return changeStringToInt(readStringValue())
     }
 
     private fun printHowManyCars() {
         println(Message.PrintingMessage.HOW_MANY_CAR)
     }
 
-    fun getNumberOfRaces(): Int {
+    fun getNumberOfRaces(readStringValue: () -> String? = { readlnOrNull() }): Int {
         printHowManyTimesRace()
-        return getInput()
+        return changeStringToInt(readStringValue())
     }
 
     private fun printHowManyTimesRace() {
         println(Message.PrintingMessage.HOW_MANY_TIMES_RACE)
     }
 
-    private fun getInput(): Int {
-        return changeStringToInt(readlnOrNull())
-    }
-
-    fun changeStringToInt(input: String?): Int {
+    private fun changeStringToInt(input: String?): Int {
         if (input.isNullOrBlank()) {
             throw IllegalArgumentException(Message.ExceptionMessage.IS_NULL_OR_BLANK)
         }
--- src/test/kotlin/racingCar/view/InputViewTest.kt
+++ src/test/kotlin/racingCar/view/InputViewTest.kt
@@ -1,11 +1,8 @@
 package racingCar.view
 
 import org.assertj.core.api.Assertions
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import racingCar.constants.Message
-import java.lang.reflect.InvocationTargetException
-import java.lang.reflect.Method
 
 /**
  * input에 대한 테스트
@@ -15,21 +12,21 @@ class InputViewTest {
     @Test
     fun `글자가 입력되었을 때 에러발생`() {
         Assertions.assertThatExceptionOfType(IllegalArgumentException::class.java).isThrownBy {
-            InputView.changeStringToInt("글자")
+            InputView.getNumberOfCars { "글자" }
         }.withMessageMatching(Message.ExceptionMessage.IS_NOT_INTEGER)
     }
 
     @Test
     fun `null이 입력되었을 때 에러발생`() {
         Assertions.assertThatExceptionOfType(IllegalArgumentException::class.java).isThrownBy {
-            InputView.changeStringToInt(null)
+            InputView.getNumberOfCars { null }
         }.withMessageMatching(Message.ExceptionMessage.IS_NULL_OR_BLANK)
     }
 
     @Test
     fun `빈칸이 입력되었을 때 에러발생`() {
         Assertions.assertThatExceptionOfType(IllegalArgumentException::class.java).isThrownBy {
-            InputView.changeStringToInt(" ")
+            InputView.getNumberOfCars { " " }
         }.withMessageMatching(Message.ExceptionMessage.IS_NULL_OR_BLANK)
     }
 }
