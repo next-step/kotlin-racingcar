21d7f12a6657045119ae28f726c37599161c06b8
glenn.kgp <glenn.kgp@kakaocorp.com>
2022-05-11 17:22:01 +0900

    refactor: link check

--- src/main/kotlin/racingcar/CarSimulator.kt
+++ src/main/kotlin/racingcar/CarSimulator.kt
@@ -1,13 +1,12 @@
 package racingcar
 
-
 fun main() {
     val inputView = InputView()
     val carNames = inputView.getCarNamesInput(RacingCarGame.CAR_NAMES_INPUT_DESC)
-    val trialCount =  inputView.getNaturalNumberInput(RacingCarGame.TRIAL_NUMBER_INPUT_DESC)
+    val trialCount = inputView.getNaturalNumberInput(RacingCarGame.TRIAL_NUMBER_INPUT_DESC)
 
     val seedMaker = SeedMakerImpl()
-    val racingCarGame = RacingCarGame(trialCount,seedMaker, carNames)
+    val racingCarGame = RacingCarGame(trialCount, seedMaker, carNames)
     racingCarGame.play()
 
     val gameResult = racingCarGame.gameResults
