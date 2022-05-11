e9f9fb771b576488f99281abb286c301b5e57b40
Charming <loadkrnis@gmail.com>
2022-05-11 21:56:06 +0900

    [step3] 랜덤 숫자 생성 기능 추가

---
+++ src/test/kotlin/step3/racing/RandomGeneratorTest.kt
@@ -0,0 +1,20 @@
+package step3.racing
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.ints.shouldBeGreaterThanOrEqual
+import io.kotest.matchers.ints.shouldBeLessThanOrEqual
+
+class RandomGeneratorTest : DescribeSpec({
+  describe("getInt") {
+    it("start 부터 end 까지의 Int 형을 반환한다.") {
+      val start = 1
+      val end = 2
+
+      val result = RandomGenerator.getInt(start, end)
+
+      result shouldBeGreaterThanOrEqual start
+      result shouldBeLessThanOrEqual end
+    }
+  }
+
+})
