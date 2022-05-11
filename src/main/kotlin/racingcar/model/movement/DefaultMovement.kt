06eb54d78e31fe7d15f1861d7000109ef137cd5d
urthevvip <urthevvip@tnear.com>
2022-05-12 05:10:01 +0900

    refactor : Extracting int constants to clarify the meaning

--- src/main/kotlin/racingcar/model/movement/DefaultMovement.kt
+++ src/main/kotlin/racingcar/model/movement/DefaultMovement.kt
@@ -4,5 +4,10 @@ import kotlin.random.Random
 import kotlin.random.nextInt
 
 class DefaultMovement : Movement {
-    override fun canMoveNow() = Random.nextInt(0..9) >= 4
+    companion object {
+        private val RANGE_FOR_RANDOM_INT = 0..9
+        private const val MIN_VALUE_TO_MOVE = 4
+    }
+
+    override fun canMoveNow() = Random.nextInt(RANGE_FOR_RANDOM_INT) >= MIN_VALUE_TO_MOVE
 }
