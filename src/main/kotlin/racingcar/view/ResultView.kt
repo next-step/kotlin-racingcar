87403577c905490f9f6aa1c994a039e2cf42bf86
leeyohan93 <good_1411@naver.com>
2022-05-11 09:00:06 +0900

    refactor: View 클래스 object 설정

--- src/main/kotlin/racingcar/view/ResultView.kt
+++ src/main/kotlin/racingcar/view/ResultView.kt
@@ -2,29 +2,27 @@ package racingcar.view
 
 import racingcar.dto.RacingGameResponse
 
-class ResultView {
-    companion object {
-        private const val RESULT = "실행 결과"
-        private const val LOCATION_SYMBOL = "-"
-        private const val CAR_DELIMITER = "\n"
-        private const val LAP_DELIMITER = "\n\n"
+object ResultView {
+    private const val RESULT = "실행 결과"
+    private const val LOCATION_SYMBOL = "-"
+    private const val CAR_DELIMITER = "\n"
+    private const val LAP_DELIMITER = "\n\n"
 
-        fun print(response: RacingGameResponse) {
-            println(RESULT)
-            print(lapReports(response))
-        }
+    fun print(response: RacingGameResponse) {
+        println(RESULT)
+        print(lapReports(response))
+    }
 
-        private fun lapReports(response: RacingGameResponse): String {
-            return response.lapReports
-                .joinToString(LAP_DELIMITER) { lap(it) }
-        }
+    private fun lapReports(response: RacingGameResponse): String {
+        return response.lapReports
+            .joinToString(LAP_DELIMITER) { lap(it) }
+    }
 
-        private fun lap(lapReports: List<Int>): String {
-            return lapReports.joinToString(CAR_DELIMITER) { carLocation(it) }
-        }
+    private fun lap(lapReports: List<Int>): String {
+        return lapReports.joinToString(CAR_DELIMITER) { carLocation(it) }
+    }
 
-        private fun carLocation(location: Int): String {
-            return (1..location).joinToString("") { LOCATION_SYMBOL }
-        }
+    private fun carLocation(location: Int): String {
+        return (1..location).joinToString("") { LOCATION_SYMBOL }
     }
 }
