75f498d1ea2e86ece8ebbaf81ab8b54c6e75d790
jojoldu <jojoldu@gmail.com>
2022-05-11 10:18:43 +0900

    validate 추가

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -5,8 +5,16 @@ class Car {
         private set
 
     fun move(movePoint: Int = (0..9).random()) {
-        if (movePoint >= 0) {
+        validateMovePoint(movePoint)
+
+        if (movePoint >= 4) {
             position++
+        }
+    }
+
+    private fun validateMovePoint(movePoint: Int) {
+        if (movePoint > 9 || movePoint < 0) {
+            throw IllegalArgumentException("0 ~ 9 사이의 movePoint 만 가능합니다.")
         }
     }
 }
