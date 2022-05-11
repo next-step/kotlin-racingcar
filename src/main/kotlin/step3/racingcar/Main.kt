f633540dd6329f1dfa5206bc7580070ea542e0ba
kwanguuuu <kwangyu326@gmail.com>
2022-05-11 23:38:09 +0900

    [step3] refactor: InputView 생성, RacingGame에 존재하던 Input 관련 로직 삭제

--- src/main/kotlin/step3/racingcar/Main.kt
+++ src/main/kotlin/step3/racingcar/Main.kt
@@ -1,6 +1,10 @@
 import step3.racingcar.car.RacingGame
+import step3.racingcar.view.InputView
 
 fun main() {
-    val racingGame = RacingGame.init()
+    val numberOfCar = InputView.showNumberOfCarInitView()
+    val trialCount = InputView.showTrialCountInitView()
+
+    val racingGame = RacingGame.init(numberOfCar, trialCount)
     racingGame.play()
 }
--- src/main/kotlin/step3/racingcar/car/RacingGame.kt
+++ src/main/kotlin/step3/racingcar/car/RacingGame.kt
@@ -1,6 +1,5 @@
 package step3.racingcar.car
 
-import step3.racingcar.InputParser
 import step3.racingcar.RandomNumberGenerator
 import step3.racingcar.view.ResultView
 
@@ -9,7 +8,6 @@ class RacingGame private constructor(
     private val trialCount: Int
 ) {
 
-    // TODO: 2022/05/09 RacingGame에서 inputView의 역할을 같이 하고 있는것 같은.. 고민이 있는데 input을 어느시점에 분리하는게 좋을지 고민...
     fun play() {
         repeat(trialCount) {
             drivers.forEach {
@@ -22,15 +20,8 @@ class RacingGame private constructor(
     }
 
     companion object {
-        fun init(): RacingGame {
-            println("자동차 대수는 몇 대 인가요?")
-            val numberOfCar = InputParser.toInt(readln())
-
-            println("시도할 횟수는 몇 회 인가요?")
-            val trialCount = InputParser.toInt(readln())
-
+        fun init(numberOfCar: Int, trialCount: Int): RacingGame {
             val drivers = List(numberOfCar) { Driver(Car()) }
-
             return RacingGame(drivers, trialCount)
         }
     }
