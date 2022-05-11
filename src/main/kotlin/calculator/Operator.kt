0abd379d750fd50d9f04dc8e08efd8b79f0c6a0f
jojoldu <jojoldu@gmail.com>
2022-05-11 09:51:11 +0900

    리뷰반영 - val을 통한 getter만 오픈

--- src/main/kotlin/calculator/Operator.kt
+++ src/main/kotlin/calculator/Operator.kt
@@ -1,15 +1,10 @@
 package calculator
 
-enum class Operator(symbol: String, evaluate: (num1: Int, num2: Int) -> Int) {
+enum class Operator(val symbol: String, val evaluate: (num1: Int, num2: Int) -> Int) {
     PLUS("+", { num1, num2 -> num1 + num2 }),
     MINUS("-", { num1, num2 -> num1 - num2 }),
     MULTIPLY("*", { num1, num2 -> num1 * num2 }),
     DIVIDE("/", { num1, num2 -> num1 / num2 });
-
-    var symbol: String = symbol
-        private set
-    var evaluate = evaluate
-        private set
 
     companion object {
         fun find(symbol: String): Operator {
