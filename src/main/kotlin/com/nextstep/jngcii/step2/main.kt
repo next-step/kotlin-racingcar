8e5daffbdb443d20f146819426f5677c450a2571
jngcii <concotree@gmail.com>
2022-05-11 21:30:43 +0900

    feat : main 함수 작성

---
+++ src/main/kotlin/com/nextstep/jngcii/step2/main.kt
@@ -0,0 +1,19 @@
+package com.nextstep.jngcii.step2
+
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+    val sc: Scanner = Scanner(System.`in`)
+
+    var input: String
+
+    while (true) {
+        print("계산 식을 입력하세요 : ")
+        input = sc.nextLine()
+        if (input.isEmpty()) break
+
+        // todo : calculate
+
+        println("=======================")
+    }
+}
