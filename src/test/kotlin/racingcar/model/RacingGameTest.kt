9b5ffb8057361a28c9795d5c9aea18f1c59f8634
urthevvip <urthevvip@tnear.com>
2022-05-12 05:36:51 +0900

    refactor(test) : Use given when then pattern

--- src/test/kotlin/racingcar/model/RacingGameTest.kt
+++ src/test/kotlin/racingcar/model/RacingGameTest.kt
@@ -1,6 +1,7 @@
 package racingcar.model
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertAll
 import org.junit.jupiter.api.Test
 import racingcar.model.movement.AlwaysGoMovement
 import racingcar.model.movement.AlwaysNotGoMovement
@@ -13,9 +14,10 @@ internal class RacingGameTest {
         val numberOfCar = 3
         val stepsToTry = 5
         val movement = AlwaysGoMovement() // Always Go
+        val racingGame = RacingGame(numberOfCar, stepsToTry, movement)
 
-        RacingGame(numberOfCar, stepsToTry, movement).runGame { game ->
-            assertThat(game.carList.size).isEqualTo(numberOfCar)
+        assertThat(racingGame.carList.size).isEqualTo(numberOfCar)
+        racingGame.runGame { game ->
             game.carList.forEach { car ->
                 assertThat(car.position).isEqualTo(game.currentStep) // always == step
             }
@@ -28,9 +30,10 @@ internal class RacingGameTest {
         val numberOfCar = 10
         val stepsToTry = 5
         val movement = AlwaysNotGoMovement() // Always Not Go
+        val racingGame = RacingGame(numberOfCar, stepsToTry, movement)
 
-        RacingGame(numberOfCar, stepsToTry, movement).runGame { game ->
-            assertThat(game.carList.size).isEqualTo(numberOfCar)
+        assertThat(racingGame.carList.size).isEqualTo(numberOfCar)
+        racingGame.runGame { game ->
             game.carList.forEach { car ->
                 assertThat(car.position).isEqualTo(0) // always == 0
             }
@@ -43,19 +46,23 @@ internal class RacingGameTest {
         val numberOfCar = 10
         val stepsToTry = 5
         val racingGame = RacingGame(numberOfCar, stepsToTry)
-        racingGame.runGame { game ->
-            assertThat(game.currentStep).isLessThan(stepsToTry + 1)
-            assertThat(game.carList.size).isEqualTo(numberOfCar)
-        }
-        assertThat(racingGame.currentStep).isEqualTo(stepsToTry)
-        assertThat(racingGame.isOver).isTrue
+        racingGame.runGame()
+
+        assertAll(
+            { assertThat(racingGame.carList.size).isEqualTo(numberOfCar) },
+            { assertThat(racingGame.currentStep).isEqualTo(stepsToTry) },
+            { assertThat(racingGame.isOver).isTrue }
+        )
     }
 
     @Test
     internal fun testForNoProgressFunction() {
         val numberOfCar = 10
         val stepsToTry = 5
-        assertThat(RacingGame(numberOfCar, stepsToTry).runGame().currentStep)
+        val racingGame = RacingGame(numberOfCar, stepsToTry)
+        racingGame.runGame()
+
+        assertThat(racingGame.currentStep)
             .isEqualTo(stepsToTry)
     }
 }
