6b537467ae2258c4cc979c0b56852674ec2e124a
Soyoung Kim <soyoung0307@sk.com>
2022-05-12 11:11:36 +0900

    Step 2 : 문자열 계산기 피드백 반영

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -5,14 +5,22 @@ class Calculator {
         fun minus(left: Int, right: Int): Int = left - right
         fun multiple(left: Int, right: Int): Int = left * right
         fun division(left: Int, right: Int): Int = left / right
+    }
+}
 
+class BasicFourCalculator {
+    companion object {
         fun stringCalc(left: Int, operator: String, right: Int): Int {
             return when (operator) {
-                "+" -> sum(left, right)
-                "-" -> minus(left, right)
-                "*" -> multiple(left, right)
-                "/" -> division(left, right)
-                else -> throw IllegalArgumentException()
+                "+" -> Calculator.sum(left, right)
+                "-" -> Calculator.minus(left, right)
+                "*" -> Calculator.multiple(left, right)
+                "/" -> Calculator.division(left, right)
+                else -> {
+                    // 사칙연산 기호외의 입력으로 인한 오류
+                    println("제공하는 사칙연산 기호가 아닙니다.")
+                    throw IllegalArgumentException()
+                }
             }
         }
     }
@@ -21,50 +29,43 @@ class Calculator {
 class StringCalculator {
     companion object {
         fun calculator(expression: String?): Int {
-            ExpressionValidator.checkExpression(expression)
+            checkValidExpression(expression)
 
             var result = 0
             var operator = ""
             expression?.split(" ")?.forEachIndexed { index, str ->
                 if (index == 0) result = str.toInt()
                 else when (index % 2) {
-                    0 -> { result = Calculator.stringCalc(result, operator, str.toInt()) }
+                    0 -> { result = BasicFourCalculator.stringCalc(result, operator, str.toInt()) }
                     1 -> { operator = str }
-                    else -> throw IllegalArgumentException()
+                    else -> {
+                        // 연산 오류
+                        println("연산 중 오류가 발생했습니다.")
+                        throw UnsupportedOperationException()
+                    }
                 }
             }
             return result
         }
-    }
-}
-
-class ExpressionValidator {
-    companion object {
-        fun checkExpression(expression: String?) {
-            checkValidExpression(expression)
-            checkValidOrder(expression)
-        }
 
         private fun checkValidExpression(expression: String?) {
             if (expression.isNullOrEmpty()) throw IllegalArgumentException()
+
+            val expressionList = expression.split(" ")
+
+            requireNotNull(expressionList.last().toIntOrNull()) { "연산식의 첫번째 문자는 숫자여야 합니다." }
+            requireNotNull(expressionList.last().toIntOrNull()) { "연산식의 마지막 문자는 숫자여야 합니다." }
+
+            expressionList.forEachIndexed { index, exp ->
+                val isValid = if (index % 2 == 0) exp.toIntOrNull() != null else isValidOperator(exp)
+                if (!isValid) throw IllegalArgumentException()
+            }
         }
 
         private fun isValidOperator(op: String): Boolean {
             return when (op) {
                 "+", "-", "*", "/" -> true
                 else -> false
-            }
-        }
-
-        private fun checkValidOrder(expression: String?) {
-            val expressionList = expression?.split(" ") ?: ArrayList()
-
-            if (expressionList.first().toIntOrNull() == null) throw IllegalArgumentException()
-            if (expressionList.last().toIntOrNull() == null) throw IllegalArgumentException()
-
-            expressionList.forEachIndexed { index, exp ->
-                val isValid = if (index % 2 == 0) exp.toIntOrNull() != null else isValidOperator(exp)
-                if (!isValid) throw IllegalArgumentException()
             }
         }
     }
