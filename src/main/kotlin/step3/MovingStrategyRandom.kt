f784a6c2f4baa211a44e6a294233395394efdd33
이광재(Gwangjae Lee)/Biz플랫폼개발팀/SKP <mygoaway@sk.com>
2022-05-12 11:45:07 +0900

    feat: MovingStrategyRandom 클래스 생성 (MovingStrategy 구현 클래스)

---
+++ src/main/kotlin/step3/MovingStrategyRandom.kt
@@ -0,0 +1,19 @@
+package step3
+
+import java.util.Random
+
+class MovingStrategyRandom : MovingStrategy {
+    companion object {
+        private const val RANGE = 10
+        private const val RESTRICT = 4
+    }
+
+    override fun movable(): Boolean {
+        return randomNumber() >= RESTRICT
+    }
+
+    private fun randomNumber() : Int {
+        val random = Random()
+        return random.nextInt(RANGE)
+    }
+}
