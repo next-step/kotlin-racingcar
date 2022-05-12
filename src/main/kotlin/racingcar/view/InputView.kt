a8c04da2c5fce4009c7f34338af01e925c3cbe45
Rok93 <kr.kim@mfort.co.kr>
2022-05-12 16:28:54 +0900

    refactor: 시도횟수 입력기능의 잘못된 변수명 및 메서드 호출 수정

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -5,7 +5,7 @@ fun inputCarNumber(): Int {
         println("자동차 대수는 몇 대인가요?")
         val carNumber = readLine()
         if (carNumber.isNullOrBlank()) {
-            throw IllegalArgumentException("차량의 갯수에는 공백이나 null 입력이 불가능합니다.")
+            throw IllegalArgumentException("[ERROR] 차량의 갯수에는 공백이나 null 입력이 불가능합니다.")
         }
 
         carNumber.toInt()
@@ -18,14 +18,14 @@ fun inputCarNumber(): Int {
 fun inputTryNumber(): Int {
     return try {
         println("시도할 횟수는 몇 회인가요?")
-        val carNumber = readLine()
-        if (carNumber.isNullOrBlank()) {
-            throw IllegalArgumentException("차량의 갯수에는 공백이나 null 입력이 불가능합니다.")
+        val tryNumber = readLine()
+        if (tryNumber.isNullOrBlank()) {
+            throw IllegalArgumentException("[ERROR] 시도 횟수는 공백이나 null 입력이 불가능합니다.")
         }
 
-        carNumber.toInt()
+        tryNumber.toInt()
     } catch (e: IllegalArgumentException) {
         println(e.message)
-        inputCarNumber()
+        inputTryNumber()
     }
 }
