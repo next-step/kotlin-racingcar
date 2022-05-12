7cfa2c0d634c0c0616ffa9f2453e0048fb09af47
juice119 <juyeol@inflearn.com>
2022-05-12 10:01:21 +0900

    ref: 미사용 코드 제거

--- src/main/kotlin/carRacing/Car.kt
+++ src/main/kotlin/carRacing/Car.kt
@@ -2,12 +2,4 @@ package carRacing
 
 class Car {
     var distance = 0
-
-    fun forward() {
-        distance += MOVE_DISTANCE
-    }
-
-    companion object {
-        private const val MOVE_DISTANCE = 1
-    }
 }
