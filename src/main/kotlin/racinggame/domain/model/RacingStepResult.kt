adf87b56c489459f9154336520d1c12b676b94a6
austen.lee <austen.lee@kakaocorp.com>
2022-05-12 23:56:08 +0900

    step4 refactor : firstPlaces 프로퍼티로 변경

--- src/main/kotlin/racinggame/domain/model/RacingStepResult.kt
+++ src/main/kotlin/racinggame/domain/model/RacingStepResult.kt
@@ -2,17 +2,18 @@ package racinggame.domain.model
 
 @JvmInline
 value class RacingStepResult(val racingGameStatusList: List<IndividualRacingStepStatus>) {
-    fun getFirstPlaces(): List<String> {
-        val firstPlaceLevel = getFirstPlaceLevel()
+    val firstPlaces: List<String>
+        get() {
+            val firstPlaceLevel = getFirstPlaceLevel()
 
-        return racingGameStatusList
-            .asSequence()
-            .filter { racingGameStatus ->
-                racingGameStatus.proceedLevel == firstPlaceLevel
-            }.map { racingGameStatus ->
-                racingGameStatus.name
-            }.toList()
-    }
+            return racingGameStatusList
+                .asSequence()
+                .filter { racingGameStatus ->
+                    racingGameStatus.proceedLevel == firstPlaceLevel
+                }.map { racingGameStatus ->
+                    racingGameStatus.name
+                }.toList()
+        }
 
     private fun getFirstPlaceLevel() = racingGameStatusList.maxOf { racingGameStatus ->
         racingGameStatus.proceedLevel
