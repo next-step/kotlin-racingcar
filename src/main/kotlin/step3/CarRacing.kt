dd0fc48d75f5b21dbc467db039bc0720f9873922
이광재(Gwangjae Lee)/Biz플랫폼개발팀/SKP <mygoaway@sk.com>
2022-05-12 14:28:10 +0900

    feat: CarRacing 클래스 생성

---
+++ src/main/kotlin/step3/CarRacing.kt
@@ -0,0 +1,17 @@
+package step3
+
+class CarRacing private constructor(var carCount: Int, var moveCount: Int) {
+    var cars: Cars = Cars.from(carCount)
+
+    fun run(movingStrategy: MovingStrategy?) {
+        for (i in 0 until moveCount) {
+            cars.run(movingStrategy)
+        }
+    }
+
+    companion object {
+        fun of(carCount: Int, moveCount: Int): CarRacing {
+            return CarRacing(carCount, moveCount)
+        }
+    }
+}
