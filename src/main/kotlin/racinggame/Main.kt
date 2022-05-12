0c9bcae5d13aa95611ce7e81a973fc1135936495
austen.lee <austen.lee@kakaocorp.com>
2022-05-13 00:05:43 +0900

    step4 refactor : 입력 안내 문구 수정

--- src/main/kotlin/racinggame/Main.kt
+++ src/main/kotlin/racinggame/Main.kt
@@ -10,14 +10,14 @@ import racinggame.view.converter.StringToIntConverter
 import racinggame.view.validator.NameLengthValidator
 import racinggame.view.validator.NumberRangeValidator
 
-private const val INPUT_MESSAGE_NUMBER_OF_CARS = "자동차 대수는 몇 대인가요?"
+private const val INPUT_MESSAGE_NAME_OF_CARS = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
 private const val INPUT_MESSAGE_NUMBER_OF_ATTEMPTS = "시도할 횟수는 몇 회인가요?"
 
 fun main() {
     val racingGameLayout = RacingGameLayout(
         inputLayout = InputLayout(
             carInputView = InputView(
-                INPUT_MESSAGE_NUMBER_OF_CARS,
+                INPUT_MESSAGE_NAME_OF_CARS,
                 NameConverter(),
                 NameLengthValidator
             ),
