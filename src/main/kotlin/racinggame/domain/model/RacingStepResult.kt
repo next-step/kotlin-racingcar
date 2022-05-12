eaa9c01b4160ed1a93db00d6a9e4ebab8d805003
austen.lee <austen.lee@kakaocorp.com>
2022-05-12 23:45:37 +0900

    step4 feat : 현재 Step에서의 선두 자동차들을 구할 수 있는 getFirstPlaces 기눙

--- src/main/kotlin/racinggame/domain/model/RacingStepResult.kt
+++ src/main/kotlin/racinggame/domain/model/RacingStepResult.kt
@@ -1,4 +1,20 @@
 package racinggame.domain.model
 
 @JvmInline
-value class RacingStepResult(val racingGameStatusList: List<IndividualRacingStepStatus>)
+value class RacingStepResult(val racingGameStatusList: List<IndividualRacingStepStatus>) {
+    fun getFirstPlaces(): List<String> {
+        val firstPlaceLevel = getFirstPlaceLevel()
+
+        return racingGameStatusList
+            .asSequence()
+            .filter { racingGameStatus ->
+                racingGameStatus.proceedLevel == firstPlaceLevel
+            }.map { racingGameStatus ->
+                racingGameStatus.name
+            }.toList()
+    }
+
+    private fun getFirstPlaceLevel() = racingGameStatusList.maxOf { racingGameStatus ->
+        racingGameStatus.proceedLevel
+    }
+}
--- src/test/kotlin/racinggame/domain/model/RacingStepResultTest.kt
+++ src/test/kotlin/racinggame/domain/model/RacingStepResultTest.kt
@@ -4,23 +4,32 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class RacingStepResultTest {
-    @Test
-    fun `RacingStepResult는 현재 각 자동차들의 이름과 전진 level을 보관한다`() {
-        val racingStepResult = RacingStepResult(
+    private val racingStepResult by lazy {
+        RacingStepResult(
             listOf(
                 IndividualRacingStepStatus("java", 1),
                 IndividualRacingStepStatus("kotlin", 2),
-                IndividualRacingStepStatus("javascript", 0)
+                IndividualRacingStepStatus("javascript", 2)
             )
         )
+    }
 
+    @Test
+    fun `RacingStepResult는 현재 각 자동차들의 이름과 전진 level을 보관한다`() {
         val expected = listOf(
             IndividualRacingStepStatus("java", 1),
             IndividualRacingStepStatus("kotlin", 2),
-            IndividualRacingStepStatus("javascript", 0)
+            IndividualRacingStepStatus("javascript", 2)
         )
         racingStepResult.racingGameStatusList.forEachIndexed { index, level ->
             assertThat(level).isEqualTo(expected[index])
         }
+    }
+
+    @Test
+    fun `getFirstPlaces를 통해 현재 선두로 달리고 있는 자동차들의 이름을 구할 수 있다`() {
+        val firstPlaces = racingStepResult.getFirstPlaces()
+
+        assertThat(firstPlaces).isEqualTo(listOf("kotlin", "javascript"))
     }
 }
