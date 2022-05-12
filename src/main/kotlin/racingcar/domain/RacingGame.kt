1705f333f351d1ddff014b3a48d74c02215f6988
leeyohan93 <good_1411@naver.com>
2022-05-12 21:45:20 +0900

    refactor: RacingGameReport 생성자 파라미터로 변경

--- src/main/kotlin/racingcar/domain/RacingGame.kt
+++ src/main/kotlin/racingcar/domain/RacingGame.kt
@@ -3,9 +3,9 @@ package racingcar.domain
 class RacingGame(
     private val cars: Cars,
     private var laps: Int,
-    private val movingStrategy: MovingStrategy = DefaultStrategy()
+    private val movingStrategy: MovingStrategy = DefaultStrategy(),
+    val report: RacingGameReport = RacingGameReport()
 ) {
-    val report = RacingGameReport()
 
     fun start() {
         if (!isNotEnd()) throw IllegalStateException("이미 종료된 레이스입니다.")
