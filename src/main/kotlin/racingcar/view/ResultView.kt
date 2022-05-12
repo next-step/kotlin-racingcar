c472492f644f434b40f4708ac0ce5ad8ddc7c398
leeyohan93 <good_1411@naver.com>
2022-05-12 21:52:00 +0900

    refactor: print 메서드가 한 가지 일만 하도록 변경

--- src/main/kotlin/racingcar/view/ResultView.kt
+++ src/main/kotlin/racingcar/view/ResultView.kt
@@ -9,17 +9,17 @@ object ResultView {
     private const val LAP_DELIMITER = "\n\n"
     private const val CAR_NAME_POST_FIX = " "
     private const val WINNERS_DELIMITER = ", "
+    private const val WINNERS_PRE_FIX = "\n"
     private const val WINNERS_POST_FIX = "가 최종 우승했습니다."
 
     fun print(response: RacingGameResponse) {
         println(RESULT)
-        println(lapReports(response.lapReports))
-        println()
-        print(winners(response.winners))
+        printLapReports(response.lapReports)
+        printWinners(response.winners)
     }
 
-    private fun lapReports(lapReports: List<List<Pair<String, Int>>>): String {
-        return lapReports.joinToString(LAP_DELIMITER) { lap(it) }
+    private fun printLapReports(lapReports: List<List<Pair<String, Int>>>) {
+        println(lapReports.joinToString(LAP_DELIMITER) { lap(it) })
     }
 
     private fun lap(lapReports: List<Pair<String, Int>>): String {
@@ -34,8 +34,8 @@ object ResultView {
         return (1..location).joinToString("") { LOCATION_SYMBOL }
     }
 
-    private fun winners(winners: List<String>): String {
+    private fun printWinners(winners: List<String>) {
         val winnersText = winners.joinToString(WINNERS_DELIMITER)
-        return winnersText + WINNERS_POST_FIX
+        print(WINNERS_PRE_FIX + winnersText + WINNERS_POST_FIX)
     }
 }
