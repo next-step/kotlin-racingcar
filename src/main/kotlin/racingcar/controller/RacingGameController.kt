3f5e6b9cf6ff9cbee5702812d66a0668688cfde6
Rok93 <kr.kim@mfort.co.kr>
2022-05-12 16:05:48 +0900

    feat: 시도횟수를 입력받는 기능

--- src/main/kotlin/racingcar/controller/RacingGameController.kt
+++ src/main/kotlin/racingcar/controller/RacingGameController.kt
@@ -1,11 +1,19 @@
 package racingcar.controller
 
 import racingcar.domain.Car
+import racingcar.domain.RacingGame
+import racingcar.domain.TryNumber
 import racingcar.view.inputCarNumber
+import racingcar.view.inputTryNumber
 
 class RacingGameController {
     fun handle() {
-        val cars = createCars(inputCarNumber())
+        val racingGame = RacingGame(
+            cars = createCars(inputCarNumber()),
+            tryNumber = TryNumber(inputTryNumber()),
+        )
+
+        // val playResult = racingGame.play()
     }
 
     private fun createCars(carNumber: Int): List<Car> {
--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -2,6 +2,22 @@ package racingcar.view
 
 fun inputCarNumber(): Int {
     return try {
+        println("자동차 대수는 몇 대인가요?")
+        val carNumber = readLine()
+        if (carNumber.isNullOrBlank()) {
+            throw IllegalArgumentException("차량의 갯수에는 공백이나 null 입력이 불가능합니다.")
+        }
+
+        carNumber.toInt()
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
+        inputCarNumber()
+    }
+}
+
+fun inputTryNumber(): Int {
+    return try {
+        println("시도할 횟수는 몇 회인가요?")
         val carNumber = readLine()
         if (carNumber.isNullOrBlank()) {
             throw IllegalArgumentException("차량의 갯수에는 공백이나 null 입력이 불가능합니다.")
