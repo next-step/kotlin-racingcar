6669902139db6010ff7ac8a5c568e7d4116b28e5
justin-s <justin.s@kakaocorp.com>
2022-05-12 19:23:20 +0900

    test: 누락된 테스트 추가

--- src/test/kotlin/racingcar/RacingTest.kt
+++ src/test/kotlin/racingcar/RacingTest.kt
@@ -1,11 +1,21 @@
 package racingcar
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 internal class RacingTest {
 
     @Test
-    fun `테스트`() {
+    fun `레이싱 컬렉션 사이즈`() {
+        val racing = Racing(Cars(listOf(Car(), Car())))
 
+        val strategy = object : MoveStrategy {
+            override fun isMove(): Boolean {
+                return true
+            }
+        }
+
+        racing.start(1, strategy)
+        assertThat(racing.result()).size().isEqualTo(1)
     }
 }
