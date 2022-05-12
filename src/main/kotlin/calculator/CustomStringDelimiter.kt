e815828a08659ca79d961ccd5eda55c15a6720dc
justin-s <justin.s@kakaocorp.com>
2022-05-12 14:49:18 +0900

    refactor : collection api 및 eager evaluation 활용, 책임 분리

--- src/main/kotlin/calculator/CustomStringDelimiter.kt
+++ src/main/kotlin/calculator/CustomStringDelimiter.kt
@@ -1,39 +1,29 @@
 package calculator
 
-import kotlin.streams.toList
-
 class CustomStringDelimiter(value: String?) {
 
-    private val values: List<String>
     private val operands: MutableList<Int>
     private val operators: MutableList<String>
 
     init {
-
-        values = validate(value)
-        operators = values.stream()
-            .filter { Expression.STRING_OPERATORS.contains(it) }
+        validate(value)
+        val values = value!!.split(DELIMITER)
             .toList()
+
+        operators = values
+            .filter { Expression.STRING_OPERATORS.contains(it) }
             .toMutableList()
 
-        operands = values.stream()
+        operands = values
             .filter { !Expression.STRING_OPERATORS.contains(it) }
             .map { it.toInt() }
-            .toList()
             .toMutableList()
     }
 
-    private fun validate(value: String?): List<String> {
-        if (value == null) {
+    private fun validate(value: String?) {
+        if (value.isNullOrBlank()) {
             throw IllegalArgumentException("잘못된 값이 전달되었습니다.")
         }
-
-        if (value.isBlank() || value.isEmpty()) {
-            throw IllegalArgumentException("잘못된 값이 전달되었습니다.")
-        }
-
-        return value.split(DELIMITER)
-            .toList()
     }
 
     fun getOperands(): MutableList<Int> {
