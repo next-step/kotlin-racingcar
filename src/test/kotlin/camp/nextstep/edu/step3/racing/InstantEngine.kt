123a566c59834eb6a2802b7a27caec4293f3dca4
aeomhs(엄현식) <aeomhs@gmail.com>
2022-05-13 17:18:25 +0900

    test: 테스트용 InstantEngine 테스트 코드 추가

--- src/test/kotlin/camp/nextstep/edu/step3/racing/InstantEngine.kt
+++ src/test/kotlin/camp/nextstep/edu/step3/racing/InstantEngine.kt
@@ -1,5 +1,8 @@
 package camp.nextstep.edu.step3.racing
 
+import io.kotest.core.spec.style.ShouldSpec
+import org.assertj.core.api.Assertions.assertThat
+
 class InstantEngine(n: Int) : Car.Engine {
     private val movables = mutableListOf<Int>()
 
@@ -19,3 +22,34 @@ class InstantEngine(n: Int) : Car.Engine {
         private const val NOT_MOVABLE_SPEED = 0
     }
 }
+
+class InstantEngineTests : ShouldSpec({
+    should("0번 움직일 수 있는 엔진은 speed 메서드 호출시 매번 움직일 수 없는 값을 리턴해야 한다.") {
+        val moveTwiceEngine = InstantEngine(0)
+
+        assertThat(moveTwiceEngine.speed()).isLessThan(4)
+        assertThat(moveTwiceEngine.speed()).isLessThan(4)
+        assertThat(moveTwiceEngine.speed()).isLessThan(4)
+        assertThat(moveTwiceEngine.speed()).isLessThan(4)
+    }
+
+    should("2번 움직일 수 있는 엔진은 speed 메서드 호출시 2번 움직일 수 있는 값을 리턴해야 한다.") {
+        val moveTwiceEngine = InstantEngine(2)
+
+        assertThat(moveTwiceEngine.speed()).isGreaterThanOrEqualTo(4)
+        assertThat(moveTwiceEngine.speed()).isGreaterThanOrEqualTo(4)
+        assertThat(moveTwiceEngine.speed()).isLessThan(4)
+        assertThat(moveTwiceEngine.speed()).isLessThan(4)
+    }
+
+    should("5번 움직일 수 있는 엔진은 speed 메서드 호출시 5번 움직일 수 있는 값을 리턴해야 한다.") {
+        val moveTwiceEngine = InstantEngine(5)
+
+        assertThat(moveTwiceEngine.speed()).isGreaterThanOrEqualTo(4)
+        assertThat(moveTwiceEngine.speed()).isGreaterThanOrEqualTo(4)
+        assertThat(moveTwiceEngine.speed()).isGreaterThanOrEqualTo(4)
+        assertThat(moveTwiceEngine.speed()).isGreaterThanOrEqualTo(4)
+        assertThat(moveTwiceEngine.speed()).isGreaterThanOrEqualTo(4)
+        assertThat(moveTwiceEngine.speed()).isLessThan(4)
+    }
+})
