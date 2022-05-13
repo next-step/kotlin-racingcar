9a657e6a4432b9ea7bb5d42c602ee6786c6da4c4
saintbeller96 <rlawhdtjd9@naver.com>
2022-05-13 13:53:02 +0900

    feature: ResultView에서 Car name 출력

--- src/main/kotlin/racingcar/ui/ResultView.kt
+++ src/main/kotlin/racingcar/ui/ResultView.kt
@@ -1,15 +1,23 @@
 package racingcar.ui
 
 import racingcar.application.GameResult
+import racingcar.domain.Car
 
 object ResultView {
+
+    private const val PADDING = 6
 
     fun show(gameResult: GameResult) {
         gameResult.raceResults.forEach {
             println("[${it.phaseOfRace}번째 경주]")
             it.raceRecord.cars.forEach { car ->
-                println("-".repeat(car.currentPosition))
+                printLane(car)
             }
         }
+    }
+
+    private fun printLane(car: Car.State) {
+        val distance = "-".repeat(car.currentPosition)
+        println("${car.name.padEnd(PADDING)}: $distance")
     }
 }
