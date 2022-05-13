f3de26f787efb2a227a500dda8eaf4c7a382a678
jinwoo-k <br2k@daum.net>
2022-05-13 21:20:12 +0900

    refact: InputView class > object 변환

--- src/main/kotlin/step3/InputView.kt
+++ src/main/kotlin/step3/InputView.kt
@@ -1,24 +1,22 @@
 package step3
 
-class InputView {
-    companion object {
-        fun getNumCars(): Int {
-            println("자동차 대수는 몇 대인가요?")
+object InputView {
+    fun getNumCars(): Int {
+        println("자동차 대수는 몇 대인가요?")
+        return readInt()
+    }
+
+    fun getNumTries(): Int {
+        println("시도할 횟수는 몇 회인가요?")
+        return readInt()
+    }
+
+    fun readInt(): Int {
+        val str = readLine()
+        if (str.isNullOrEmpty() || !str.all { it.isDigit() }) {
+            println("잘못된 입력입니다. 다시 입력해주세요.")
             return readInt()
         }
-
-        fun getNumTries(): Int {
-            println("시도할 횟수는 몇 회인가요?")
-            return readInt()
-        }
-
-        fun readInt(): Int {
-            val str = readLine()
-            if (str.isNullOrEmpty() || !str.all { it.isDigit() }) {
-                println("잘못된 입력입니다. 다시 입력해주세요.")
-                return readInt()
-            }
-            return str.toInt()
-        }
+        return str.toInt()
     }
 }
