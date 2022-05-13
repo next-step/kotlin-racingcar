ff737b341acde097c2b28e60acd5fca5555894ac
Juatae-Kim <jutaekim@sk.com>
2022-05-13 09:12:01 +0900

    refactor(RacingRecord) : Add latestRecord property to RacingRecord class

--- src/main/kotlin/racingcar/model/RacingRecord.kt
+++ src/main/kotlin/racingcar/model/RacingRecord.kt
@@ -5,6 +5,9 @@ class RacingRecord(numberOfCar: Int) {
     val stepToTry: Int
         get() = recordList.maxOf { it.size }
 
+    val latestRecord: List<Int>
+        get() = recordList.map { it.last() }
+
     fun writeRecord(indexOfCar: Int, car: Car) {
         recordList[indexOfCar].add(car.position)
     }
