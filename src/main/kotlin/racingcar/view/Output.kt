a50655221d0fadd5f7899efe7b6e391c071872fd
baekseungyeol <beck33333@naver.com>
2022-05-14 02:24:46 +0900

    feat(Output) : 자동차를 출력할 때 자동차 이름을 같이 출력하는 로직 추가

--- src/main/kotlin/racingcar/view/Output.kt
+++ src/main/kotlin/racingcar/view/Output.kt
@@ -1,5 +1,7 @@
 package racingcar.view
 
+import racingcar.model.CarName
+
 import racingcar.model.Cars
 
 class Output {
@@ -11,9 +13,14 @@ class Output {
 
     fun showResultPosition(cars: Cars) {
         cars.forEach {
+            showName(it.name)
             showGauge(it.position)
         }
         println()
+    }
+
+    private fun showName(name: CarName) {
+        print("$name : ")
     }
 
     private fun showGauge(position: Int) {
