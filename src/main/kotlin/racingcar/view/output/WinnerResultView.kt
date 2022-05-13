c1c32b8a283e996b95f41fb8511c045d88d5bacb
jutaeKim <jutaekim@sk.com>
2022-05-14 07:53:44 +0900

    + feat(ResultView): Implement WinnerResultView to display car's name during the game and to display the winners

---
+++ src/main/kotlin/racingcar/view/output/WinnerResultView.kt
@@ -0,0 +1,52 @@
+package racingcar.view.output
+
+import racingcar.model.Car
+import racingcar.model.CarRecord
+import racingcar.model.RacingGame
+import racingcar.model.RacingRecord
+
+class WinnerResultView : ResultView {
+
+    override fun showResultView(racingGame: RacingGame) {
+        if (racingGame.currentStep == 1) {
+            println(MESSAGE_FOR_START_GAME)
+        }
+        racingGame.carList.forEach { println(it.toDisplayString()) }
+        println()
+
+        if (racingGame.isOver) {
+            println(MESSAGE_FOR_END_OF_GAME)
+        }
+    }
+
+    override fun showResultView(racingRecord: RacingRecord) {
+        val steps = racingRecord.stepToTry
+        println(MESSAGE_FOR_START_GAME)
+        repeat(steps) { step ->
+            printRacingRecordForTheStepAt(racingRecord, step)
+        }
+        printWinner(racingRecord.winnersRecord)
+        println(MESSAGE_FOR_END_OF_GAME)
+    }
+
+    private fun printRacingRecordForTheStepAt(racingRecord: RacingRecord, step: Int) {
+        racingRecord.carRecordList.forEach { recordForACar ->
+            println(recordForACar.recordForStep[step].toBarString())
+        }
+        println()
+    }
+
+    private fun printWinner(winnersRecord: List<CarRecord>) {
+        val winnersName = winnersRecord.map { it.car.carName }
+            .joinToString(", ")
+        println("${winnersName}가 최종 우승했습니다.")
+    }
+
+    private fun Car.toDisplayString() = "${this.carName}: ${this.position.toBarString()}}"
+    private fun Int.toBarString() = "-".repeat(this + 1)
+
+    companion object {
+        private const val MESSAGE_FOR_START_GAME = "실행 결과"
+        private const val MESSAGE_FOR_END_OF_GAME = "Game Over"
+    }
+}
