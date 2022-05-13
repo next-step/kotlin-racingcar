48656a5d32838a4b6b3d80f41bb753de8fad74f5
Juatae-Kim <jutaekim@sk.com>
2022-05-13 09:14:08 +0900

    test : Add test cases for RacingRecord and comments for given,when,then pattern

--- src/test/kotlin/racingcar/model/RacingGameTest.kt
+++ src/test/kotlin/racingcar/model/RacingGameTest.kt
@@ -11,58 +11,68 @@ internal class RacingGameTest {
     @Test
     internal fun testForAlwaysGo() {
 
+        // given
         val numberOfCar = 3
         val stepsToTry = 5
         val movement = AlwaysGoMovement() // Always Go
         val racingGame = RacingGame(numberOfCar, stepsToTry, movement)
 
-        assertThat(racingGame.carList.size).isEqualTo(numberOfCar)
-        racingGame.runGame { game ->
-            game.carList.forEach { car ->
-                assertThat(car.position).isEqualTo(game.currentStep) // always == step
+        // when
+        val gameRecord = racingGame.runGame()
+
+        // then
+        assertAll(
+            { assertThat(racingGame.carList.size).isEqualTo(numberOfCar) },
+            { assertThat(gameRecord.recordList.size).isEqualTo(numberOfCar) },
+            { assertThat(gameRecord.stepToTry).isEqualTo(stepsToTry) },
+            {
+                assertThat(gameRecord.latestRecord.joinToString(""))
+                    .isEqualTo(stepsToTry.toString().repeat(numberOfCar))
             }
-        }
+        )
     }
 
     @Test
     internal fun testForAlwaysNotGo() {
 
+        // given
         val numberOfCar = 10
         val stepsToTry = 5
         val movement = AlwaysNotGoMovement() // Always Not Go
         val racingGame = RacingGame(numberOfCar, stepsToTry, movement)
 
-        assertThat(racingGame.carList.size).isEqualTo(numberOfCar)
-        racingGame.runGame { game ->
-            game.carList.forEach { car ->
-                assertThat(car.position).isEqualTo(0) // always == 0
+        // when
+        val gameRecord = racingGame.runGame()
+
+        // then
+        assertAll(
+            { assertThat(racingGame.carList.size).isEqualTo(numberOfCar) },
+            { assertThat(gameRecord.recordList.size).isEqualTo(numberOfCar) },
+            { assertThat(gameRecord.stepToTry).isEqualTo(stepsToTry) },
+            {
+                assertThat(gameRecord.latestRecord.joinToString(""))
+                    .isEqualTo("0".repeat(numberOfCar))
             }
-        }
+        )
     }
 
     @Test
     internal fun testForRandomGame() {
 
+        // given
         val numberOfCar = 10
         val stepsToTry = 5
         val racingGame = RacingGame(numberOfCar, stepsToTry)
-        racingGame.runGame()
 
+        // when
+        val gameRecord = racingGame.runGame()
+
+        // then
         assertAll(
             { assertThat(racingGame.carList.size).isEqualTo(numberOfCar) },
             { assertThat(racingGame.currentStep).isEqualTo(stepsToTry) },
-            { assertThat(racingGame.isOver).isTrue }
+            { assertThat(racingGame.isOver).isTrue },
+            { assertThat(gameRecord.recordList.size).isEqualTo(numberOfCar) }
         )
-    }
-
-    @Test
-    internal fun testForNoProgressFunction() {
-        val numberOfCar = 10
-        val stepsToTry = 5
-        val racingGame = RacingGame(numberOfCar, stepsToTry)
-        racingGame.runGame()
-
-        assertThat(racingGame.currentStep)
-            .isEqualTo(stepsToTry)
     }
 }
