508e4f23792630391e6cbecc3c99f41b7ebd166c
vivian.bae <vivian.bae@kakaocorp.com>
2022-05-13 12:50:45 +0900

    리뷰 반영 - 테스트 코드 내에서 Car 를 구현하는 방식으로 변경

--- src/test/kotlin/racingcar/model/CarTest.kt
+++ src/test/kotlin/racingcar/model/CarTest.kt
@@ -8,7 +8,10 @@ class CarTest {
 
     @Test
     fun `무작위 값이 4보다 낮을 경우 차는 움직이지 않는다`() {
-        val car = StationaryCar()
+        val noMovingStrategy = object : MovingStrategy {
+            override fun tryMove(): Boolean = false
+        }
+        val car = Car(movingStrategy = noMovingStrategy)
         val positionBeforeFirstTry = car.position
         car.move()
         val positionAfterFirstTry = car.position
@@ -19,25 +22,15 @@ class CarTest {
 
     @Test
     fun `무작위 값이 4보다 같거나 높을 경우 차는 한 칸 움직인다`() {
-        val car = MovingCar()
+        val forwardMovingStrategy = object : MovingStrategy {
+            override fun tryMove(): Boolean = true
+        }
+        val car = Car(movingStrategy = forwardMovingStrategy)
         val positionBeforeFirstTry = car.position
         car.move()
         val positionAfterFirstTry = car.position
 
         assertEquals(defaultCarPosition, positionBeforeFirstTry)
         assertEquals(defaultCarPosition + 1, positionAfterFirstTry)
-    }
-}
-
-class StationaryCar : Car() {
-    override fun getRandomNumber(): Int = 1
-}
-
-class MovingCar : Car() {
-    override fun getRandomNumber(): Int = 5
-
-    fun moveAndReturn(): Car {
-        move()
-        return this
     }
 }
