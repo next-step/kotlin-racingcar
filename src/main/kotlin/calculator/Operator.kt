f3743b7153687087f8f56bfbe7255799fe152472
Gomding <backchida@gmail.com>
2022-05-14 20:25:51 +0900

    refactor: Operator의 연산자 문자를 나타내는 value 필드명을 symbol로 변경

--- src/main/kotlin/calculator/Operator.kt
+++ src/main/kotlin/calculator/Operator.kt
@@ -2,11 +2,11 @@ package calculator
 
 import java.util.function.BiFunction
 
-enum class Operator(val value: String, val expression: BiFunction<Number, Number, Number>) {
-    PLUS(value = "+", expression = { number1: Number, number2: Number -> number1.plus(number2) }),
-    MINUS(value = "-", expression = { number1: Number, number2: Number -> number1.minus(number2) }),
-    MULTIPLE(value = "*", expression = { number1: Number, number2: Number -> number1.multiply(number2) }),
-    DIVISION(value = "/", expression = { number1: Number, number2: Number -> number1.divide(number2) }), ;
+enum class Operator(val symbol: String, val expression: BiFunction<Number, Number, Number>) {
+    PLUS(symbol = "+", expression = { number1: Number, number2: Number -> number1.plus(number2) }),
+    MINUS(symbol = "-", expression = { number1: Number, number2: Number -> number1.minus(number2) }),
+    MULTIPLE(symbol = "*", expression = { number1: Number, number2: Number -> number1.multiply(number2) }),
+    DIVISION(symbol = "/", expression = { number1: Number, number2: Number -> number1.divide(number2) }), ;
 
     fun calculate(number1: Number, number2: Number): Number {
         return this.expression.apply(number1, number2)
@@ -14,7 +14,7 @@ enum class Operator(val value: String, val expression: BiFunction<Number, Number
 
     companion object {
         fun of(value: String): Operator {
-            return values().firstOrNull { it.value == value }
+            return values().firstOrNull { it.symbol == value }
                 ?: throw IllegalArgumentException("지원하지 않는 연산자 입니다. operator: $value")
         }
     }
