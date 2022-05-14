a81d71d7dca52d10b87dca4b5deecb8b8e8e028d
ujusy <lsyeon1997@ajou.ac.kr>
2022-05-14 23:40:55 +0900

    fix: Car Class name 인자 추가 및 예외처리 추가

--- src/main/kotlin/carRacing/Car.kt
+++ src/main/kotlin/carRacing/Car.kt
@@ -1,10 +1,21 @@
 package carRacing
 
-class Car {
+class Car(
+    val name: String
+) {
+
+    init {
+        require(name.isNotBlank()) { INVALID_CAR_NAME }
+    }
+
     var position: Int = 0
         private set
 
     fun move() {
         this.position += 1
+    }
+
+    companion object {
+        const val INVALID_CAR_NAME = "자동차 이름은 빈 문자열일 수 없다."
     }
 }
--- src/test/kotlin/carRacing/CarTest.kt
+++ src/test/kotlin/carRacing/CarTest.kt
@@ -1,14 +1,26 @@
 package carRacing
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 
 class CarTest : FreeSpec({
 
+    "init" - {
+
+        "car 이름이 빈 문자열 일 경우 IllegalArgumentException" {
+            val carName = ""
+            val exception = shouldThrow<IllegalArgumentException> {
+                Car(carName)
+            }
+            exception.message shouldBe Car.INVALID_CAR_NAME
+        }
+    }
+
     "move" - {
 
         "car의 포지션이 + 1 되어야한다." {
-            val car = Car()
+            val car = Car("test")
             val before = car.position
             car.move()
             car.position shouldBe before + 1
