c42907236c6379c1e5e1c0a4678ee237dc4ee083
dongchul <sorkehdcjf@gmail.com>
2022-05-15 02:21:37 +0900

    feat: 자동차 경주 실행 및 우승자 반환

---
+++ src/main/kotlin/car/application/CarRacing.kt
@@ -0,0 +1,24 @@
+package car.application
+
+import car.application.dto.RacingInput
+import car.application.dto.RacingResult
+import car.application.dto.RapResult
+import car.application.dto.RapResults
+import car.domain.Cars
+import car.domain.RandomMovingStrategy
+
+object CarRacing {
+    private val movingStrategy = RandomMovingStrategy()
+
+    fun race(racingInput: RacingInput): RacingResult {
+        val (carNames, tryCount) = racingInput
+        val cars = Cars(carNames)
+        return RacingResult(List(tryCount.value) { startRap(cars) }, cars.winners.map { it.name })
+    }
+
+    private fun startRap(cars: Cars): RapResults {
+        return cars.move(movingStrategy)
+            .map { RapResult(it.name, it.position) }
+            .run { RapResults(this) }
+    }
+}
