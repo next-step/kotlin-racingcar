b68a6a7085d436a6b69eb084e4ec8274efc80e98
Juice <3221ju@gmail.com>
2022-05-14 18:44:11 +0900

    fix: 에러 메시지 포맷 수정

--- src/main/kotlin/car_racing/ErrorMessage.kt
+++ src/main/kotlin/car_racing/ErrorMessage.kt
@@ -2,8 +2,8 @@ package car_racing
 
 object ErrorMessage {
 
-    fun getMinNumberError(value: Int, minValue: Int): String = "${minValue}은 올수 없습니다. 최소 값($value)이상의 값을 넣어주세요."
-    fun getMaxNumberError(value: Int, maxValue: Int): String = "${maxValue}은 올수 없습니다. 최대 값($value)이하의 값을 넣어주세요."
+    fun getMinNumberError(value: Int, minValue: Int): String = "${value}은 올수 없습니다. 최소 값($minValue)이상의 값을 넣어주세요."
+    fun getMaxNumberError(value: Int, maxValue: Int): String = "${value}은 올수 없습니다. 최대 값($maxValue)이하의 값을 넣어주세요."
     fun getErrorMessageForOnlyIntegerString(parameterName: String, parameterValue: String): String {
         return "입력 받은 \"${parameterValue}\" 은 정수가 아닙니다. ${parameterName}에 정수만을 입력해주세요. "
     }
