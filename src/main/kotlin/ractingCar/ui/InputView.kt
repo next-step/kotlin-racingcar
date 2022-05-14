d594a5792d9cf1aa8d372f2dc693c2bb899d24b6
erie0210 <2j00923@gmail.com>
2022-05-14 18:43:06 +0900

    feat: ui 추가

--- src/main/kotlin/ractingCar/ui/InputView.kt
+++ src/main/kotlin/ractingCar/ui/InputView.kt
@@ -1,4 +1,15 @@
 package ractingCar.ui
 
+import ractingCar.constants.Messages
+import ractingCar.lib.Validation
+
 class InputView {
+    fun getInput(): List<Int> {
+        println(Messages.NUMBER_OF_CAR)
+        val numOfCars = Validation().isNumber(readln())
+
+        println(Messages.NUMBER_OF_MOVES)
+        val numOfMoves = Validation().isNumber(readln())
+        return listOf<Int>(numOfCars, numOfMoves)
+    }
 }
--- src/main/kotlin/ractingCar/ui/ResultView.kt
+++ src/main/kotlin/ractingCar/ui/ResultView.kt
@@ -1,4 +1,12 @@
 package ractingCar.ui
 
+import ractingCar.entity.Car
+
 class ResultView {
+    fun getResult(carList: List<Car>) {
+        for (car in carList) {
+            car.printMoves()
+        }
+        println()
+    }
 }
