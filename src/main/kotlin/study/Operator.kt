8369ad528e9014a7bd31cf63dddbfe37d2510a57
Grandelwald <artjr97@naver.com>
2022-05-14 17:18:03 +0900

    2단계-문자열 계산기 : 각 enum 객체에 연산 함수 설정. enum 객체 전체에 대하여 synbol 값으로 enum 객체 찾는 함수 설정

--- src/main/kotlin/study/Operator.kt
+++ src/main/kotlin/study/Operator.kt
@@ -4,5 +4,17 @@ enum class Operator(val symbol: String) {
     ADD("+"),
     SUBTRACT("-"),
     MULTIPLY("*"),
-    DIVIDE("/")
+    DIVIDE("/");
+
+    fun calculateSinglePair(a: Int, b: Int): Int =
+        when (this) {
+            ADD -> a + b
+            SUBTRACT -> a - b
+            MULTIPLY -> a * b
+            DIVIDE -> a / b
+        }
+
+    companion object {
+        fun reverseMap(string: String): Operator? = Operator.values().find { it.symbol == string }
+    }
 }
