84f0d396f979152f4bf08d7dfefedad72251633a
Hyeon9mak <jinha3507@gmail.com>
2022-05-14 14:08:12 +0900

    feat: 자동차 및 자동차 전진 기능 구현

---
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -0,0 +1,17 @@
+package racingcar.domain
+
+class Car {
+    var distance: Int = 0
+        private set
+
+    fun fillGasAndForward(gas: Int) {
+        if (gas >= ENOUGH_TO_FORWARD) {
+            distance += FORWARD_DISTANCE
+        }
+    }
+
+    companion object {
+        const val ENOUGH_TO_FORWARD = 4
+        const val FORWARD_DISTANCE = 1
+    }
+}
