85a6c4cadae47aa25ec18e8ba69fc96512e79ba0
aeomhs(엄현식) <aeomhs@gmail.com>
2022-05-14 14:25:45 +0900

    feat: Racing 우승자 조회 구현

--- src/main/kotlin/camp/nextstep/edu/racingcar/racing/Racing.kt
+++ src/main/kotlin/camp/nextstep/edu/racingcar/racing/Racing.kt
@@ -3,13 +3,19 @@ package camp.nextstep.edu.racingcar.racing
 class Racing private constructor(participants: List<Car>, private val moveCount: Int) {
 
     private var ended = false
-    private val winners = setOf<Car>()
+    private var winners = setOf<Car>()
     private val carTracks = linkedMapOf(*participants.map { car -> car to Track(moveCount) }.toTypedArray())
 
     fun start() {
+        val racingResults = mutableListOf<Pair<Car, Int>>()
+
         for ((car, track) in carTracks) {
             car.raceOn(track, moveCount)
+            racingResults.add(car to track.marks())
         }
+
+        val winnersCount = racingResults.maxOf { it.second }
+        winners = racingResults.filter { it.second == winnersCount }.map { it.first }.toSet()
 
         ended = true
     }
