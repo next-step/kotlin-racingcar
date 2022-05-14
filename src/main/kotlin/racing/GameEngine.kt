2cab9a6484fde9a17464d1e4e5d6723ea35ab317
엄기훈 <pikachom@gmail.com>
2022-05-15 01:46:03 +0900

    refactor: 함수 책임 분리 및 가독성을 위한 변경

--- src/main/kotlin/racing/GameEngine.kt
+++ src/main/kotlin/racing/GameEngine.kt
@@ -4,11 +4,14 @@ object GameEngine {
     private val randomRange = 0..9
     private const val moveCriteria = 4
 
-    fun rollDiceForAllCar(carList: List<Car>) {
+    fun playTurnForAllCar(carList: List<Car>) {
         carList.forEach {
-            val diceResult = getRandomNumberBetween(randomRange)
-            if (carMoveDecision(diceResult)) it.move()
+            playTurn(it)
         }
+    }
+    private fun playTurn(car: Car) {
+        val diceResult = getRandomNumberBetween(randomRange)
+        if (carMoveDecision(diceResult)) car.move()
     }
     private fun getRandomNumberBetween(range: IntRange) = range.random()
     private fun carMoveDecision(number: Int): Boolean = number >= moveCriteria
