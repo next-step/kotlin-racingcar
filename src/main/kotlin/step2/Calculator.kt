7f1b6b476690c3593f067e60c070f67781644ea1
revfactory <revfactory@gmail.com>
2022-05-14 22:53:37 +0900

    feat: operator test

--- src/main/kotlin/step2/Calculator.kt
+++ src/main/kotlin/step2/Calculator.kt
@@ -1,18 +1,20 @@
 package step2
 
 class Calculator {
-    fun calculate(inputString: String): String {
+    fun calculate(inputString: String): Double {
         val splitList = inputString.split(" ")
-        var result = splitList[0].toInt()
+        var result = splitList[0].toDouble()
 
         for (index in 1 until splitList.size step 2) {
             result = when (splitList[index]) {
-                "+" -> result + splitList[index + 1].toInt()
-                "-" -> result - splitList[index + 1].toInt()
+                "+" -> result + splitList[index + 1].toDouble()
+                "-" -> result - splitList[index + 1].toDouble()
+                "*" -> result * splitList[index + 1].toDouble()
+                "/" -> result / splitList[index + 1].toDouble()
                 else -> throw IllegalArgumentException()
             }
         }
 
-        return result.toString()
+        return result
     }
 }
--- src/test/kotlin/step2/CalculatorTest.kt
+++ src/test/kotlin/step2/CalculatorTest.kt
@@ -8,13 +8,31 @@ class CalculatorTest : StringSpec({
     val calculator = Calculator()
 
     "plus test" {
-        calculator.calculate("1 + 1") shouldBe "2"
-        calculator.calculate("1 + 2") shouldBe "3"
-        calculator.calculate("1 + 2 + 3") shouldBe "6"
-        calculator.calculate("1 + 2 + 3 + 4") shouldBe "10"
+        calculator.calculate("1 + 1") shouldBe 2
+        calculator.calculate("1 + 2") shouldBe 3
+        calculator.calculate("1 + 2 + 3") shouldBe 6
+        calculator.calculate("1 + 2 + 3 + 4") shouldBe 10
     }
 
     "minus test" {
-        calculator.calculate("1 - 1") shouldBe "0"
+        calculator.calculate("1 - 1") shouldBe 0
+        calculator.calculate("4 - 3 - 2") shouldBe -1
+    }
+
+    "multiply test" {
+        calculator.calculate("2 * 2") shouldBe 4
+        calculator.calculate("2 * 2 * 2") shouldBe 8
+        calculator.calculate("2 * 2 * 2 * 10") shouldBe 80
+        calculator.calculate("2 * 2 * 2 * -1") shouldBe -8
+    }
+
+    "divide test" {
+        calculator.calculate("10 / 2") shouldBe 5
+        calculator.calculate("4 / 2 / 2") shouldBe 1
+        calculator.calculate("4 / 2 / 4") shouldBe 0.5
+    }
+
+    "calculate test" {
+        calculator.calculate("2 + 3 * 4 / 2") shouldBe 10
     }
 })
