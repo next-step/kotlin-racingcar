d4d2aa64ee693e545bd8b54442707fcbcefc6561
Grandelwald <artjr97@naver.com>
2022-05-14 15:09:30 +0900

    2단계-문자열 계산기 : PersonTest.kt에 assertThat() static import 완료

--- src/test/kotlin/study/step1/PersonTest.kt
+++ src/test/kotlin/study/step1/PersonTest.kt
@@ -1,6 +1,6 @@
 package study.step1
 
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class PersonTest {
@@ -11,32 +11,32 @@ class PersonTest {
             Person("이호재", 26, nickname = "그란델왈드"),
             Person(name = "이호재", age = 26, nickname = "그란델왈드")
         )
-        Assertions.assertThat(people[0].name).isEqualTo("이호재")
-        Assertions.assertThat(people[1].name).isEqualTo("이호재")
-        Assertions.assertThat(people[2].name).isEqualTo("이호재")
+        assertThat(people[0].name).isEqualTo("이호재")
+        assertThat(people[1].name).isEqualTo("이호재")
+        assertThat(people[2].name).isEqualTo("이호재")
     }
 
     @Test
     fun `널 타입`(){
         val person = Person("이호재",26,null)
-        Assertions.assertThat(person.name).isEqualTo("이호재")
-        Assertions.assertThat(person.age).isEqualTo(26)
-        Assertions.assertThat(person.nickname).isNull()
+        assertThat(person.name).isEqualTo("이호재")
+        assertThat(person.age).isEqualTo(26)
+        assertThat(person.nickname).isNull()
     }
 
     @Test
     fun `기본 인자`(){
         val person = Person("이호재",26)
-        Assertions.assertThat(person.name).isEqualTo("이호재")
-        Assertions.assertThat(person.age).isEqualTo(26)
-        Assertions.assertThat(person.nickname).isEqualTo("이호재")
+        assertThat(person.name).isEqualTo("이호재")
+        assertThat(person.age).isEqualTo(26)
+        assertThat(person.nickname).isEqualTo("이호재")
     }
 
     @Test
     fun `데이터 클래스`() {
         val person1 = Person("이호재",26)
         val person2 = Person("이호재",26)
-        Assertions.assertThat(person1).isEqualTo(person2)
+        assertThat(person1).isEqualTo(person2)
 
     }
 }
