765957d1a3573dbb5101835bee7ddf3de002f25c
Gomding <backchida@gmail.com>
2022-05-14 20:29:45 +0900

    refactor: Operator에서 BiFunction 함수형 인터페이스를 사용한 부분 고차함수로 변경

--- src/main/kotlin/calculator/Operator.kt
+++ src/main/kotlin/calculator/Operator.kt
@@ -1,15 +1,13 @@
 package calculator
 
-import java.util.function.BiFunction
-
-enum class Operator(val symbol: String, val expression: BiFunction<Number, Number, Number>) {
+enum class Operator(val symbol: String, val expression: (number1: Number, number2: Number) -> Number) {
     PLUS(symbol = "+", expression = { number1: Number, number2: Number -> number1.plus(number2) }),
     MINUS(symbol = "-", expression = { number1: Number, number2: Number -> number1.minus(number2) }),
     MULTIPLE(symbol = "*", expression = { number1: Number, number2: Number -> number1.multiply(number2) }),
     DIVISION(symbol = "/", expression = { number1: Number, number2: Number -> number1.divide(number2) }), ;
 
     fun calculate(number1: Number, number2: Number): Number {
-        return this.expression.apply(number1, number2)
+        return this.expression(number1, number2)
     }
 
     companion object {
