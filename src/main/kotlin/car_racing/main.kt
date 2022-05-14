43494e96374098c21a170302f0796ff4f8c7e063
Juice <3221ju@gmail.com>
2022-05-14 14:23:39 +0900

    feat: readline -> readln

--- src/main/kotlin/car_racing/main.kt
+++ src/main/kotlin/car_racing/main.kt
@@ -2,10 +2,9 @@ package car_racing
 
 fun main() {
     println("자동차 대수는 몇 대인가요?")
-    val carCount: String = readLine() ?: throw IllegalArgumentException("자동차 대수는 null 이 올수 없습니다")
+    val carCount: String = readln()
     println("시도할 횟수는 몇 회인가요?")
-    val movementTryCount: String = readLine() ?: throw IllegalArgumentException("시도할 횟수는 null 이 올수 없습니다")
-
+    val movementTryCount: String = readln()
     val inputDto = InputDto(carCount, movementTryCount)
     val randomNumberCarRacingManager = RandomNumberCarRacingManager(inputDto)
 
