12459809d1482a88fee38e81457d6b4aa5f88b49
Rok93 <kr.kim@mfort.co.kr>
2022-05-14 20:35:54 +0900

    refactor: 자동차가 전진 혹은 전진하지 않는 기능 엣지 케이스만 테스트하도록 변경

--- src/test/kotlin/racingcar/domain/CarTest.kt
+++ src/test/kotlin/racingcar/domain/CarTest.kt
@@ -6,30 +6,10 @@ import io.kotest.inspectors.forAll
 import io.kotest.matchers.shouldBe
 
 internal class CarTest : StringSpec({
-    "condition이 4 이상이면 자동차가 전진한다" {
+    "condition에 따라서 자동차가 전진 혹은 전진하지 않는다." {
         listOf(
-            row(4, 1),
-            row(5, 1),
-            row(6, 1),
-            row(7, 1),
-        ).forAll { (condition, expected) ->
-            // given
-            val car = Car()
-
-            // when
-            val actual = car.moveOrNot(condition)
-
-            // then
-            actual.position shouldBe expected
-        }
-    }
-
-    "condition이 4 미만이면 자동차가 전진하지 않는다." {
-        listOf(
-            row(0, 0),
-            row(1, 0),
-            row(2, 0),
             row(3, 0),
+            row(4, 1),
         ).forAll { (condition, expected) ->
             // given
             val car = Car()
