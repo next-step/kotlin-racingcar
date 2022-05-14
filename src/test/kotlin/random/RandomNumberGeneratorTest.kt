5e6675e67b6d3bb0f929ee6b6daff6dabc66e8d2
Jake Son <sonjeabin@gmail.com>
2022-05-15 08:18:57 +0900

    test: 낮은 확률로 테스트 실패하지 않도록 변경

--- src/test/kotlin/random/RandomNumberGeneratorTest.kt
+++ src/test/kotlin/random/RandomNumberGeneratorTest.kt
@@ -1,15 +1,16 @@
 package random
 
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.collections.shouldContainAll
+import io.kotest.inspectors.forAll
+import io.kotest.matchers.collections.shouldBeIn
 
 internal class RandomNumberGeneratorTest : StringSpec({
     "주어진 수 사이에 있는 임의의 정수를 반환한다" {
         val from = 1
         val to = 10
 
-        val generator = List(1000) { RandomNumberGenerator.generate(from, to) }
+        val result = List(1000) { RandomNumberGenerator.generate(from, to) }
 
-        generator shouldContainAll (1..10).toList()
+        result.forAll { it shouldBeIn (1..10).toList() }
     }
 })
