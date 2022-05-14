ca38842097f78d40e7e5cb2e32dc34b3becedbd2
Juice <3221ju@gmail.com>
2022-05-14 18:42:44 +0900

    ref: CarRacingView 변수명 수정

--- src/main/kotlin/car_racing/CarRacingView.kt
+++ src/main/kotlin/car_racing/CarRacingView.kt
@@ -1,11 +1,11 @@
 package car_racing
 
-class CarRacingView(private val carCharacter: String, private val lineCharter: String = "\n") {
+class CarRacingView(private val carCharacter: String, private val separator: String = "\n") {
 
     fun getViewString(carRacingManager: CarRacingManager): String {
         return carRacingManager.getCarList().joinToString(
             transform = { car -> getCarLineView(car) },
-            separator = lineCharter
+            separator = separator
         )
     }
 
