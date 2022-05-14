989b4e6a471359ea5d41ff0b8b7d4459cdd6bb0c
vivian.bae <vivian.bae@kakaocorp.com>
2022-05-14 16:46:47 +0900

    4단계 - Car 클래스에 이름 필드 추가

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -1,7 +1,12 @@
 package racingcar.model
 
-open class Car(position: Int = DEFAULT_POSITION, private val movingStrategy: MovingStrategy = RandomMovingStrategy()) {
-    constructor(car: Car) : this(car.position)
+class Car(
+    name: String,
+    position: Int = DEFAULT_POSITION,
+    private val movingStrategy: MovingStrategy = RandomMovingStrategy()
+) {
+    val name: String = if (isValidName(name)) name
+    else throw IllegalArgumentException("자동차의 이름은 빈 값이거나 5자가 넘어가면 안됩니다")
 
     var position: Int = position
         private set
@@ -9,6 +14,8 @@ open class Car(position: Int = DEFAULT_POSITION, private val movingStrategy: Mov
     fun move() {
         if (movingStrategy.tryMove()) position++
     }
+
+    private fun isValidName(name: String) = !(name.isBlank() || name.length > 5)
 
     companion object {
         private const val DEFAULT_POSITION: Int = 0
--- src/test/kotlin/racingcar/model/CarTest.kt
+++ src/test/kotlin/racingcar/model/CarTest.kt
@@ -1,17 +1,22 @@
 package racingcar.model
 
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EmptySource
+import org.junit.jupiter.params.provider.ValueSource
 
 class CarTest {
     private val defaultCarPosition = 0
+    private val validCarName = "myCar"
 
     @Test
     fun `무작위 값이 4보다 낮을 경우 차는 움직이지 않는다`() {
         val noMovingStrategy = object : MovingStrategy {
             override fun tryMove(): Boolean = false
         }
-        val car = Car(movingStrategy = noMovingStrategy)
+        val car = Car(validCarName, movingStrategy = noMovingStrategy)
         val positionBeforeFirstTry = car.position
         car.move()
         val positionAfterFirstTry = car.position
@@ -25,12 +30,33 @@ class CarTest {
         val forwardMovingStrategy = object : MovingStrategy {
             override fun tryMove(): Boolean = true
         }
-        val car = Car(movingStrategy = forwardMovingStrategy)
+        val car = Car(validCarName, movingStrategy = forwardMovingStrategy)
         val positionBeforeFirstTry = car.position
         car.move()
         val positionAfterFirstTry = car.position
 
         assertEquals(defaultCarPosition, positionBeforeFirstTry)
         assertEquals(defaultCarPosition + 1, positionAfterFirstTry)
+    }
+
+    @ParameterizedTest
+    @EmptySource
+    fun `자동차 이름 - 빈 값일 경우 에러가 발생한다`(input: String) {
+        assertThatIllegalArgumentException().isThrownBy { Car(input) }
+            .withMessage("자동차의 이름은 빈 값이거나 5자가 넘어가면 안됩니다")
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["666666"])
+    fun `자동차 이름 - 5자가 넘어갈 경우 에러가 발생한다`(input: String) {
+        assertThatIllegalArgumentException().isThrownBy { Car(input) }
+            .withMessage("자동차의 이름은 빈 값이거나 5자가 넘어가면 안됩니다")
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1", "22", "333", "4444", "55555"])
+    fun `자동차 이름의 길이가 1과 5사이라면 해당 이름을 가진 Car 인스턴스가 생성된다`(input: String) {
+        val car = Car(input)
+        assertEquals(input, car.name)
     }
 }
