295d8d17242e86af4179647dc2f6d04fa3345691
h-swon <heosw1107@gmail.com>
2022-05-15 02:50:15 +0900

    feat: Car 이름 프로퍼티 추가 및 예외처리

--- src/main/kotlin/racing/model/Car.kt
+++ src/main/kotlin/racing/model/Car.kt
@@ -2,14 +2,25 @@ package racing.model
 
 import racing.Movable
 
-class Car(private var position: Int) {
+class Car(private val name: String, private var position: Int = START_POSITION) {
     init {
-        require(position >= 0) { "자동차의 초기 위치는 0이상 입니다." }
+        require(position >= MIN_POSITION) { INVALID_CAR_POSITION }
+        require(name.length <= MAX_NAME_LENGTH) { INVALID_CAR_NAME_LENGTH }
+        require(name.isNotBlank()) {INVALID_CAR_NAME_BLANK}
     }
 
     fun move(movable: Movable): Int {
         if (movable.canMove())
             position++
         return position
+    }
+
+    companion object {
+        private const val START_POSITION = 0
+        private const val MIN_POSITION = 0
+        private const val MAX_NAME_LENGTH = 5
+        const val INVALID_CAR_POSITION = "자동차의 초기 위치는 0이상 입니다."
+        const val INVALID_CAR_NAME_LENGTH = "자동차의 이름은 5자를 초과할 수 없습니다."
+        const val INVALID_CAR_NAME_BLANK = "자동차의 이름은 공백이 될 수 없습니다."
     }
 }
--- src/test/kotlin/racing/CarTest.kt
+++ src/test/kotlin/racing/CarTest.kt
@@ -5,31 +5,49 @@ import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
-import racing.CarRacingGame.Companion.START_POSITION
 import racing.model.Car
-import racing.model.PositionHistory
+import racing.model.Car.Companion.INVALID_CAR_NAME_BLANK
+import racing.model.Car.Companion.INVALID_CAR_NAME_LENGTH
+import racing.model.Car.Companion.INVALID_CAR_POSITION
 
 class CarTest {
     private lateinit var car: Car
 
     @BeforeEach
     fun setUp() {
-        car = Car(START_POSITION)
+        car = Car("test")
     }
 
     @ParameterizedTest
-    @ValueSource(ints = [1, 3, 100])
-    fun `올바른 자동차 생성자 테스트`(position: Int) {
-        Car(position)
+    @CsvSource(value = ["mee, 0", "a, 4", "messi, 0"])
+    fun `올바른 자동차 생성자 테스트`(name: String, position: Int) {
+        Car(name, position)
     }
 
     @ParameterizedTest
-    @ValueSource(ints = [-1, -3, -100])
-    fun `자동차 생성자 에러 테스트`(position: Int) {
+    @ValueSource(strings = ["kylian", "k     j"])
+    fun `자동차 생성자 이름 길이 에러 테스트`(name: String) {
         assertThatIllegalArgumentException().isThrownBy {
-            Car(position)
-        }.withMessage("자동차의 초기 위치는 0이상 입니다.")
+            Car(name)
+        }.withMessage(INVALID_CAR_NAME_LENGTH)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["", " ", "    "])
+    fun `자동차 생성자 이름 공백 에러 테스트`(name: String) {
+        assertThatIllegalArgumentException().isThrownBy {
+            Car(name)
+        }.withMessage(INVALID_CAR_NAME_BLANK)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [-1, -10])
+    fun `자동차 생성자 위치 에러 테스트`(position: Int) {
+        assertThatIllegalArgumentException().isThrownBy {
+            Car("test", position)
+        }.withMessage(INVALID_CAR_POSITION)
     }
 
     @Test
