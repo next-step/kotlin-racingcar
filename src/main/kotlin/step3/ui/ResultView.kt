df2d78cc9fff14d33d1f2a5d2d053fdac46f40fc
이광재(Gwangjae Lee)/Biz플랫폼개발팀/SKP <mygoaway@sk.com>
2022-05-15 00:26:07 +0900

    feat: ResultView 최종 우승자 구하는 로직 추가 및 문구 내용 추가

--- src/main/kotlin/step3/ui/ResultView.kt
+++ src/main/kotlin/step3/ui/ResultView.kt
@@ -10,16 +10,38 @@ object ResultView {
         for(i in 0 until movCount) {
             showResultMoveHyphen(cars, i)
         }
+        printWinnerList(getWinnerList(cars))
     }
 
     private fun showResultMoveHyphen(cars: Cars, movCount: Int) {
         for (j in 0 until cars.carsList.size) {
             val position = cars.carsList[j].positionList[movCount]
+            print(cars.carsList[j].getCarName() + " : ")
             repeat(position) {
                 print(MOVE_HYPHEN)
             }
             println()
         }
         println()
+    }
+
+    private fun getWinnerList(cars: Cars):List<String> {
+        val winnerList: MutableList<String> = ArrayList<String>()
+        var max = 0
+        var winners: String? = null
+        for (j in 0 until cars.carsList.size) {
+            val position = cars.carsList[j].getPosition()
+            if(position > max) {
+                max = position
+                winnerList.add(cars.carsList[j].getCarName())
+            }
+        }
+        return winnerList
+    }
+
+    private fun printWinnerList(winnerList: List<String>) {
+        val namesList = winnerList.joinToString(", ")
+        val winnerMessage = "$namesList 가 최종 우승하였습니다."
+        print(winnerMessage)
     }
 }
