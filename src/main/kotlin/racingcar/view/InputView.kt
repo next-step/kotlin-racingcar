dee08e360707b1fda23e8587029f7d590d75cf25
hyeon9mak <jinha3507@gmail.com>
2022-05-14 13:52:14 +0900

    feat: 자동차경주 Input, Result View 메서드 작성

---
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -0,0 +1,13 @@
+package racingcar.view
+
+class InputView {
+    fun inputCountOfCars(): String {
+        println("자동차 대수는 몇 대인가요?")
+        return readln()
+    }
+
+    fun inputRacingLaps(): String {
+        println("시도할 횟수는 몇 회인가요?")
+        return readln()
+    }
+}
---
+++ src/main/kotlin/racingcar/view/ResultView.kt
@@ -0,0 +1,14 @@
+package racingcar.view
+
+class ResultView {
+    fun printRacingResult() {
+        println("실행 결과")
+
+        // iter
+        printCurrentRace()
+    }
+
+    private fun printCurrentRace() {
+        println("")
+    }
+}
