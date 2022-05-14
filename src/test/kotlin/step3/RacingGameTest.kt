c8006f477216ea0035e4cee9a82d1d9418eccefc
ellen.youn <ellen.youn@kakaocorp.com>
2022-05-14 23:37:36 +0900

    test: 입력한 숫자만큼 자동차가 생성된다.

---
+++ src/test/kotlin/step3/RacingGameTest.kt
@@ -0,0 +1,21 @@
+package step3
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
+import io.kotest.matchers.ints.shouldBeLessThanOrEqual
+import io.kotest.matchers.shouldBe
+
+class RacingGameTest : FunSpec({
+    context("입력한 숫자 만큼 자동차가 생성된다") {
+        forAll(
+            row(3, 5),
+            row(10, 10),
+            row(8, 2),
+        ) { cars, times ->
+            val racing = RacingGame()
+            racing.play(cars, times)
+            racing.carList.size shouldBe cars
+        }
+    }
+})
