597de45a601fd1430ac3c466b3d0e258f42e4f3b
erie0210 <2j00923@gmail.com>
2022-05-14 18:03:04 +0900

    feat: 자동차 클래스 추가

--- src/main/kotlin/ractingCar/entity/Car.kt
+++ src/main/kotlin/ractingCar/entity/Car.kt
@@ -1,4 +1,17 @@
 package ractingCar.entity
 
-class Car {
+class Car(var moves: Int = 0) {
+
+    fun move() {
+        val range = (0..9)
+        goForward(range.random())
+    }
+
+    fun goForward(rand: Int) {
+        if (rand >= 4) this.moves += 1
+    }
+
+    fun printMoves() {
+        println("-".repeat(this.moves))
+    }
 }
