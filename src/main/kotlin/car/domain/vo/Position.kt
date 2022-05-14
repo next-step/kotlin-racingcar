faa8c3be05f575fcf402773d61b7eea4222a3d0f
dongchul <sorkehdcjf@gmail.com>
2022-05-15 02:08:17 +0900

    feat: 자동차 경주 우승자 로직 추가

--- src/main/kotlin/car/domain/vo/Position.kt
+++ src/main/kotlin/car/domain/vo/Position.kt
@@ -1,4 +1,4 @@
-package car.domain
+package car.domain.vo
 
 private const val MIN_POSITION = 0
 
@@ -7,7 +7,7 @@ value class Position(
     val value: Int
 ) {
     init {
-        require(value >= MIN_POSITION) { "위치 객체는 음수가 될수 없습니다." }
+        require(value >= MIN_POSITION) { "위치값은 음수가 될수 없습니다." }
     }
 
     fun increase(): Position = Position(value.inc())
--- src/test/kotlin/car/domain/vo/PositionTest.kt
+++ src/test/kotlin/car/domain/vo/PositionTest.kt
@@ -1,6 +1,6 @@
-package study.car
+package car.domain.vo
 
-import car.domain.Position
+import car.domain.vo.Position
 import io.kotest.assertions.throwables.shouldNotThrow
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
