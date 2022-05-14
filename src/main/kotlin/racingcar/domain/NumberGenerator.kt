308501be391e37d78e85e4f52a1bf7cd10fc856a
leeyohan93 <good_1411@naver.com>
2022-05-14 21:06:31 +0900

    refactor: 인터페이스 구현체 파일 통합

--- src/main/kotlin/racingcar/domain/NumberGenerator.kt
+++ src/main/kotlin/racingcar/domain/NumberGenerator.kt
@@ -1,5 +1,13 @@
 package racingcar.domain
 
+import kotlin.random.Random
+
 fun interface NumberGenerator {
     fun generate(from: Int, until: Int): Int
+}
+
+object RandomNumberGenerator : NumberGenerator {
+    override fun generate(from: Int, until: Int): Int {
+        return Random.nextInt(from, until)
+    }
 }
--- src/main/kotlin/racingcar/domain/RandomNumberGenerator.kt
+++
@@ -1,9 +0,0 @@
-package racingcar.domain
-
-import kotlin.random.Random
-
-object RandomNumberGenerator : NumberGenerator {
-    override fun generate(from: Int, until: Int): Int {
-        return Random.nextInt(from, until)
-    }
-}
