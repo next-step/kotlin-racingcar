a2e8590998e614258a518bc79e21ec10cf15aced
Rok93 <kr.kim@mfort.co.kr>
2022-05-14 20:03:29 +0900

    fix: 자동차의 초기 위치 값이 0미만인 경우 예외가 발생하도록 변경

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -1,15 +1,14 @@
 package racingcar.domain
 
 private const val INITIAL_POSITION = 0
-
 private const val MOVEMENT_CONDITION_STANDARD = 4
 private const val NEXT_POSITION = 1
 
-class Car private constructor(
+class Car(
     val position: Int = INITIAL_POSITION,
 ) {
     init {
-        check(position < INITIAL_POSITION) { "자동차의 처음 position은 0이상의 숫자만 가능합니다. position = $position" }
+        check(position >= INITIAL_POSITION) { "자동차의 처음 position은 0이상의 숫자만 가능합니다. position = $position" }
     }
 
     fun moveOrNot(condition: Int): Car {
