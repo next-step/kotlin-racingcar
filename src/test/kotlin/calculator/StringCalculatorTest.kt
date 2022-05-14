374ace1651fa95cb5360cb3177db07e852d2f4b7
Gomding <backchida@gmail.com>
2022-05-14 20:57:18 +0900

    refactor: StringCalculatorTest의 테스트명 변경

--- src/test/kotlin/calculator/StringCalculatorTest.kt
+++ src/test/kotlin/calculator/StringCalculatorTest.kt
@@ -5,117 +5,115 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 class StringCalculatorTest : FunSpec({
-    context("StringCalculator의") {
-        context("calculate 함수를 호출할 때") {
-            test("입력값이 null이면 예외가 발생한다.") {
-                // given
-                val input = null
+    context("문자열 계산기를 실행할 때") {
+        test("입력값이 null이면 예외가 발생한다.") {
+            // given
+            val input = null
 
-                // when then
-                val exception = shouldThrow<IllegalArgumentException> {
-                    StringCalculator.calculate(input)
-                }
-                exception.message shouldBe "문자열 계산기의 입력값은 null이거나 빈 값일 수 없습니다. input: null"
+            // when then
+            val exception = shouldThrow<IllegalArgumentException> {
+                StringCalculator.calculate(input)
             }
+            exception.message shouldBe "문자열 계산기의 입력값은 null이거나 빈 값일 수 없습니다. input: null"
+        }
 
-            test("입력값이 빈 값이면 예외가 발생한다.") {
-                // given
-                val input = ""
+        test("입력값이 빈 값이면 예외가 발생한다.") {
+            // given
+            val input = ""
 
-                // when then
-                val exception = shouldThrow<IllegalArgumentException> {
-                    StringCalculator.calculate(input)
-                }
-                exception.message shouldBe "문자열 계산기의 입력값은 null이거나 빈 값일 수 없습니다. input: "
+            // when then
+            val exception = shouldThrow<IllegalArgumentException> {
+                StringCalculator.calculate(input)
             }
+            exception.message shouldBe "문자열 계산기의 입력값은 null이거나 빈 값일 수 없습니다. input: "
+        }
 
-            test("입력값이 공백이면 예외가 발생한다.") {
-                // given
-                val input = "   "
+        test("입력값이 공백이면 예외가 발생한다.") {
+            // given
+            val input = "   "
 
-                // when then
-                val exception = shouldThrow<IllegalArgumentException> {
-                    StringCalculator.calculate(input)
-                }
-                exception.message shouldBe "문자열 계산기의 입력값은 null이거나 빈 값일 수 없습니다. input:    "
+            // when then
+            val exception = shouldThrow<IllegalArgumentException> {
+                StringCalculator.calculate(input)
             }
+            exception.message shouldBe "문자열 계산기의 입력값은 null이거나 빈 값일 수 없습니다. input:    "
+        }
 
-            test("입력값에 숫자로 변환할 수 없는 문자열이 들어간다면 예외가 발생한다.") {
-                // given
-                val input = "1 + a"
+        test("입력값에 숫자로 변환할 수 없는 문자열이 들어간다면 예외가 발생한다.") {
+            // given
+            val input = "1 + a"
 
-                // when then
-                val exception = shouldThrow<IllegalArgumentException> {
-                    StringCalculator.calculate(input)
-                }
-                exception.message shouldBe "연산할 수 없는 문자열 입니다. value: a"
+            // when then
+            val exception = shouldThrow<IllegalArgumentException> {
+                StringCalculator.calculate(input)
             }
+            exception.message shouldBe "연산할 수 없는 문자열 입니다. value: a"
+        }
 
-            test("입력값에 지원하지 않는 연산자가 있으면 예외가 발생한다.") {
-                // given
-                val input = "1 & a"
+        test("입력값에 지원하지 않는 연산자가 있으면 예외가 발생한다.") {
+            // given
+            val input = "1 & a"
 
-                // when then
-                val exception = shouldThrow<IllegalArgumentException> {
-                    StringCalculator.calculate(input)
-                }
-                exception.message shouldBe "지원하지 않는 연산자 입니다. operator: &"
+            // when then
+            val exception = shouldThrow<IllegalArgumentException> {
+                StringCalculator.calculate(input)
             }
+            exception.message shouldBe "지원하지 않는 연산자 입니다. operator: &"
+        }
 
-            test("문자열 덧셈이 가능하다.") {
-                // given
-                val input = "1 + 1"
+        test("1 + 1을 입력하면 2가 된다.") {
+            // given
+            val input = "1 + 1"
 
-                // when
-                val result: String = StringCalculator.calculate(input)
+            // when
+            val result: String = StringCalculator.calculate(input)
 
-                // then
-                result shouldBe "2"
-            }
+            // then
+            result shouldBe "2"
+        }
 
-            test("문자열 뺄 셈이 가능하다.") {
-                // given
-                val input = "2 - 1"
+        test("2 - 1을 입력하면 1이 된다.") {
+            // given
+            val input = "2 - 1"
 
-                // when
-                val result: String = StringCalculator.calculate(input)
+            // when
+            val result: String = StringCalculator.calculate(input)
 
-                // then
-                result shouldBe "1"
-            }
+            // then
+            result shouldBe "1"
+        }
 
-            test("문자열 곱셈이 가능하다.") {
-                // given
-                val input = "3 * 2"
+        test("3 * 2를 입력하면 6이 된다.") {
+            // given
+            val input = "3 * 2"
 
-                // when
-                val result: String = StringCalculator.calculate(input)
+            // when
+            val result: String = StringCalculator.calculate(input)
 
-                // then
-                result shouldBe "6"
-            }
+            // then
+            result shouldBe "6"
+        }
 
-            test("문자열 나눗셈이 가능하다.") {
-                // given
-                val input = "10 / 2"
+        test("10 / 2를 입력하면 5가 된다.") {
+            // given
+            val input = "10 / 2"
 
-                // when
-                val result: String = StringCalculator.calculate(input)
+            // when
+            val result: String = StringCalculator.calculate(input)
 
-                // then
-                result shouldBe "5"
-            }
+            // then
+            result shouldBe "5"
+        }
 
-            test("복합 연산이 가능하다.") {
-                // given
-                val input = "2 + 3 * 4 / 2 - 1"
+        test("2 + 3 * 4 / 2 - 1 을 입력하면 9가 된다.") {
+            // given
+            val input = "2 + 3 * 4 / 2 - 1"
 
-                // when
-                val result: String = StringCalculator.calculate(input)
+            // when
+            val result: String = StringCalculator.calculate(input)
 
-                // then
-                result shouldBe "9"
-            }
+            // then
+            result shouldBe "9"
         }
     }
 })
