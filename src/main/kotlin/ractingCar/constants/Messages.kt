4043cd71c6cb111ad8db243238793926d4864742
erie0210 <2j00923@gmail.com>
2022-05-14 18:49:07 +0900

    feat: 메시지 생성

--- src/main/kotlin/ractingCar/constants/Messages.kt
+++ src/main/kotlin/ractingCar/constants/Messages.kt
@@ -1,4 +1,8 @@
 package ractingCar.constants
 
-class Messages {
+object Messages {
+    const val NUMBER_OF_CAR = "자동차 대수는 몇 대인가요?"
+    const val NUMBER_OF_MOVES = "시도할 횟수는 몇 회인가요?"
+
+    const val INPUT_NOT_NUMBER = "숫자를 입력해야합니다."
 }
