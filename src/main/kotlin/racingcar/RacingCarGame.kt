f0f6cb5248d5eed83051c70ac2b1c8bbb78b1001
papico <papico@inflab.com>
2022-05-15 18:05:38 +0900

    요구사항 구현 완료

--- src/main/kotlin/racingcar/RacingCarGame.kt
+++ src/main/kotlin/racingcar/RacingCarGame.kt
@@ -1,14 +1,21 @@
 package racingcar
 
-class RacingCarGame {
-    fun main() {
-        println("자동차 대수는 몇 대인가요?")
-        val carCount = readln()
-        println("시도할 회수는 몇 회인가요?")
-        val tryCount = readln()
+fun main() {
+    println("자동차 대수는 몇 대인가요?")
+    val carCount = readln().toIntOrNull() ?: throw IllegalArgumentException("1 이상의 정수로 입력해주세요.")
+    println("시도할 회수는 몇 회인가요?")
+    val tryCount = readln().toIntOrNull() ?: throw IllegalArgumentException("1 이상의 정수로 입력해주세요.")
 
-        println("실행 결과")
+    println("\n실행 결과")
+    val driverList = List(carCount) { Driver(Car()) }
 
+    repeat(tryCount) {
+        driverList.forEach {
+            val isMovable = (1..9).random() > 4
+            it.drive(isMovable)
 
+            println(it.getMoveResult())
+        }
+        println("")
     }
 }
