1ea66fecbf706d6293301a9905c2d67852e371e1
Gomding <backchida@gmail.com>
2022-05-15 22:31:37 +0900

    refactor(racingcar): 자동차가 입력받는 값에대한 명칭을 연료로 변경

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -2,8 +2,8 @@ package racingcar
 
 class Car(val distanceMoved: Int = 0) {
 
-    fun move(value: Int) : Car {
-        if (value >= 4) {
+    fun move(fuel: Int) : Car {
+        if (fuel >= 4) {
             return Car(this.distanceMoved.inc())
         }
         return this
--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -5,23 +5,23 @@ import io.kotest.matchers.shouldBe
 
 class CarTest : FunSpec({
     context("move 함수를 호출할 때") {
-        test("자동차에게 주어지는 값이 4이상이면 전진할 수 있다.") {
+        test("자동차에게 주어지는 연료 값이 4이상이면 전진할 수 있다.") {
             // given
             val car = Car()
 
             // when
-            val movedCar = car.move(4)
+            val movedCar = car.move(fuel = 4)
 
             // then
             movedCar.distanceMoved shouldBe 1
         }
 
-        test("자동차에게 주어지는 값이 4미만이면 멈춰있는다.") {
+        test("자동차에게 주어지는 연료 값이 4미만이면 멈춰있는다.") {
             // given
             val car = Car()
 
             // when
-            val movedCar = car.move(3)
+            val movedCar = car.move(fuel = 3)
 
             // then
             movedCar.distanceMoved shouldBe 0
