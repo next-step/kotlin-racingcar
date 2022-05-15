34401688fa6cbd419b1d0697a110257fb146de32
seojeongkuk <wjdrnr6662@gmail.com>
2022-05-15 18:17:04 +0900

    Step3: 자동차 경주 리뷰 반영
    - 리뷰 반영으로 인한 테스트 코드 추가

--- src/test/kotlin/racing/RacingCarTest.kt
+++ src/test/kotlin/racing/RacingCarTest.kt
@@ -4,20 +4,38 @@ import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 
 class RacingCarTest {
-    // @Test
-    // fun `race를 수행하면 현재 라운드가 1 증가한다`() {
-    //     val car = RacingCar(3)
-    //     car.race()
-    //
-    //     Assertions.assertThat(car.currentRound).isEqualTo(1)
-    // }
-
     @Test
-    fun `시도한 횟수만큼 race를 수행하면 자동차의 거리 결과는 0 과 1을 포함한 배열이다`() {
+    fun `race 수행 시, 기준 값 4보다 작은 값이 입력되면 자동차의 거리 결과는 0으로 이루어진 배열이다`() {
         val tryRound = 3
         val car = RacingCar(tryRound)
         for (round in 0 until tryRound) {
-            car.race(createRandom(1))
+            car.race(createRandom(3))
+        }
+
+        val distances = car.getDistances()
+
+        Assertions.assertThat(distances).containsOnly(0)
+    }
+
+    @Test
+    fun `race 수행 시, 기준 값 4보다 큰 값이 입력되면 자동차의 거리 결과는 1로 이루어진 배열이다`() {
+        val tryRound = 3
+        val car = RacingCar(tryRound)
+        for (round in 0 until tryRound) {
+            car.race(createRandom(4))
+        }
+
+        val distances = car.getDistances()
+
+        Assertions.assertThat(distances).containsOnly(1)
+    }
+
+    @Test
+    fun `시도한 횟수만큼 race를 수행하면 자동차의 거리 결과는 0 과 1을 포함한 배열이다`() {
+        val tryRound = 5
+        val car = RacingCar(tryRound)
+        for (round in 0 until tryRound) {
+            car.race(createRandom(round))
         }
 
         val distances = car.getDistances()
