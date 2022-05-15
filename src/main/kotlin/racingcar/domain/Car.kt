2f3c857279acf15d5eddaff35eb9d75faef036ba
yjna2316 <yjna2316@gmail.com>
2022-05-15 20:34:12 +0900

    feat: Car 이름이 5자가 넘어가는지 체크하는 init 메소드 추가

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -1,8 +1,13 @@
 package racingcar.domain
 
 class Car(
-    val position: Int = 0,
+    val position: Int,
+    val name: String,
 ) {
+    init {
+        require(name.length <= 5) { "자동차 이름은 5자를 초과할 수 없어요." }
+    }
+
     fun move(number: Int): Int {
         val moveOneStepOrNot = if (number >= LOWER_BOUND_TO_MOVE) MOVE else STOP
         return position.plus(moveOneStepOrNot)
