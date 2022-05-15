4216f8d331d29167d487e87c4c9bc9f4d7714739
Jake Son <sonjeabin@gmail.com>
2022-05-15 15:59:43 +0900

    refactor: 메소드 분리

--- src/main/kotlin/racingcar/painter/GameResultPainter.kt
+++ src/main/kotlin/racingcar/painter/GameResultPainter.kt
@@ -4,13 +4,12 @@ import racingcar.car.Position
 import racingcar.game.GameResult
 
 object GameResultPainter {
-    fun from(gameResult: List<GameResult>): String {
-        val toDash: Position.() -> String = {
-            "-".repeat(value)
-        }
+    private val toDash: Position.() -> String = {
+        "-".repeat(value)
+    }
 
-        return gameResult.joinToString(separator = "\n\n") {
+    fun from(gameResult: List<GameResult>): String =
+        gameResult.joinToString(separator = "\n\n") {
             it.cars.joinToString(separator = "\n") { car -> car.position().toDash() }
         }
-    }
 }
