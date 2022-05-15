c59dd531c1e0b0f50ab4e2c9cfc4c4b31a61933e
Jake Son <sonjeabin@gmail.com>
2022-05-15 15:58:11 +0900

    feat: 게임 결과 painter 구현

--- src/main/kotlin/racingcar/painter/GameResultPainter.kt
+++ src/main/kotlin/racingcar/painter/GameResultPainter.kt
@@ -1,9 +1,16 @@
 package racingcar.painter
 
+import racingcar.car.Position
 import racingcar.game.GameResult
 
 object GameResultPainter {
     fun from(gameResult: List<GameResult>): String {
-        return ""
+        val toDash: Position.() -> String = {
+            "-".repeat(value)
+        }
+
+        return gameResult.joinToString(separator = "\n\n") {
+            it.cars.joinToString(separator = "\n") { car -> car.position().toDash() }
+        }
     }
 }
--- src/test/kotlin/racingcar/painter/GameResultPainterTest.kt
+++ src/test/kotlin/racingcar/painter/GameResultPainterTest.kt
@@ -34,7 +34,7 @@ internal class GameResultPainterTest : StringSpec({
             
             ---
             --
-            ----- 
+            -----
         """.trimIndent()
     }
 })
