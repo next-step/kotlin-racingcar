35545aa0c1053c672dabedf77b4f885ef23dcd3b
h-swon <heosw1107@gmail.com>
2022-05-15 14:02:19 +0900

    feat: ResultView 게임 종료 후 우승자 출력 기능 추가

--- src/main/kotlin/racing/ui/ResultView.kt
+++ src/main/kotlin/racing/ui/ResultView.kt
@@ -9,6 +9,7 @@ class ResultView(private val totalPositionHistory: TotalPositionHistory) {
         totalPositionHistory.forEach { positionHistory ->
             printCarMoveResult(positionHistory)
         }
+        printWinnersName()
     }
 
     private fun printFirst() = println("실행 결과")
@@ -19,6 +20,10 @@ class ResultView(private val totalPositionHistory: TotalPositionHistory) {
             println(PRINT_VALUE.repeat(moveResult.position))
         }
         println()
+    }
+
+    private fun printWinnersName() {
+        println(totalPositionHistory.findWinnersName().joinToString(separator = ", ", postfix = "가 최종 우승했습니다."))
     }
 
     companion object {
--- src/test/kotlin/racing/CarRacingGameTest.kt
+++ src/test/kotlin/racing/CarRacingGameTest.kt
@@ -7,9 +7,8 @@ import racing.ui.ResultView
 class CarRacingGameTest {
     @Test
     fun `레이싱 게임 테스트`() {
-        val racingGame = CarRacingGame(CarInput(listOf("pobi","crong","honux"), 5))
+        val racingGame = CarRacingGame(CarInput(listOf("pobi", "crong", "honux"), 5))
         val result = racingGame.start()
         ResultView(result).printResult()
     }
-
 }
