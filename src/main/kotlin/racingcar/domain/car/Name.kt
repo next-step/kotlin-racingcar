27eb42b9e7cc6a58f600fc5fcc46812e75274987
Hyeon9mak <jinha3507@gmail.com>
2022-05-15 12:29:07 +0900

    feat: 자동차 이름VO 및 검증로직 추가

---
+++ src/main/kotlin/racingcar/domain/car/Name.kt
@@ -0,0 +1,26 @@
+package racingcar.domain.car
+
+data class Name(
+    val value: String
+) {
+    init {
+        validateBlank(this.value)
+        validateLength(this.value)
+    }
+
+    private fun validateLength(value: String) {
+        if (MAX_LENGTH < value.length) {
+            throw IllegalArgumentException("자동차 이름은 $MAX_LENGTH 를 넘을 수 없습니다.")
+        }
+    }
+
+    private fun validateBlank(value: String) {
+        if (value.isBlank()) {
+            throw IllegalArgumentException("자동차 이름은 공백만으로 이루어질 수 없습니다.")
+        }
+    }
+
+    companion object {
+        const val MAX_LENGTH = 5
+    }
+}
