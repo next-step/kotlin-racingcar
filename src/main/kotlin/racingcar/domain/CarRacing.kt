af1520e43eb78d900327340188f372ba620295be
yjna2316 <yjna2316@gmail.com>
2022-05-15 20:35:10 +0900

    feat: Racing Car 우승자 계산 로직 추가 및 RandomUtil 분리

--- src/main/kotlin/racingcar/domain/CarRacing.kt
+++ src/main/kotlin/racingcar/domain/CarRacing.kt
@@ -1,16 +1,22 @@
 package racingcar.domain
 
+import racingcar.util.RandomUtil.randomNumber
+
 object CarRacing {
-    fun initCars(carCount: Int): List<Car> {
-        return (0 until carCount).map { Car() }
+    fun initCars(carNames: List<String>): List<Car> {
+        return carNames.map { carName -> Car(0, carName) }
     }
 
     fun moveCars(listCar: List<Car>): List<Car> {
         return listCar.map { car ->
-            val nextPosition = car.move(randomNumber())
-            Car(position = nextPosition)
+            val movedPositon = car.move(randomNumber())
+            Car(movedPositon, car.name)
         }
     }
 
-    private fun randomNumber() = (0..9).random()
+    fun getWinners(cars: List<Car>): List<String> {
+        val sortedCar = cars.sortedByDescending { it.position }
+        val winnerPosition = sortedCar.first().position
+        return sortedCar.filter { it.position == winnerPosition }.map { it.name }
+    }
 }
---
+++ src/main/kotlin/racingcar/util/RandomUtil.kt
@@ -0,0 +1,5 @@
+package racingcar.util
+
+object RandomUtil {
+    fun randomNumber() = (0..9).random()
+}
