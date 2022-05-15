c0eee4e67a2ebb9cd929c87684c96a68dd1a3b64
Jake Son <sonjeabin@gmail.com>
2022-05-15 09:28:47 +0900

    feat: Engine 인터페이스 도입

--- src/main/kotlin/racingcar/engine/RandomEngine.kt
+++ src/main/kotlin/racingcar/engine/RandomEngine.kt
@@ -6,9 +6,9 @@ const val RANDOM_FROM = 0
 const val RANDOM_TO = 10
 const val MOVE_THRESHOLD = 4
 
-class RandomEngine {
+class RandomEngine : Engine {
 
-    fun accelerate(): Distance {
+    override fun accelerate(): Distance {
         val random = RandomNumberGenerator.generate(RANDOM_FROM, RANDOM_TO)
 
         return Distance(if (random >= MOVE_THRESHOLD) 1 else 0)
