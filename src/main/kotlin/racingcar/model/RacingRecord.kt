f5bbab59071f02be6697695a4360dd4b3f8f0df7
Juatae-Kim <jutaekim@sk.com>
2022-05-16 08:23:08 +0900

    refactor : Replace mutable list with immutable list to prevent data tampering by external code

--- src/main/kotlin/racingcar/model/RacingRecord.kt
+++ src/main/kotlin/racingcar/model/RacingRecord.kt
@@ -1,17 +1,30 @@
 package racingcar.model
 
-data class CarRecord(val car: Car, val recordForStep: MutableList<Int> = mutableListOf()) {
+data class CarRecord(val car: Car) {
+
+    val recordForStep: List<Int>
+        get() = this._recordForStep
+
     val latestPosition: Int
         get() = this.recordForStep.last()
+
+    val currentStep: Int
+        get() = this.recordForStep.size
+
+    private val _recordForStep = mutableListOf<Int>()
+
+    fun writeRecord(position: Int) {
+        this._recordForStep.add(position)
+    }
 }
 
 class RacingRecord(carList: Collection<Car>) {
     val carRecordList = carList.map { CarRecord(it) }
     val stepToTry: Int
-        get() = carRecordList.maxOf { it.recordForStep.size }
+        get() = carRecordList.maxOf { it.currentStep }
 
     val latestRecord: List<Int>
-        get() = carRecordList.map { it.recordForStep.last() }
+        get() = carRecordList.map { it.latestPosition }
 
     val winnersRecord: List<CarRecord>
         get() = this.carRecordList.filter { it.latestPosition == maxPosition }
@@ -21,6 +34,6 @@ class RacingRecord(carList: Collection<Car>) {
 
     fun writeRecord(car: Car) {
         carRecordList.find { it.car.carId == car.carId }
-            ?.recordForStep?.add(car.position)
+            ?.writeRecord(car.position)
     }
 }
