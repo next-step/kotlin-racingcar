9ae486160deef1698e7cc97306ad45416633c04b
yjna2316 <yjna2316@gmail.com>
2022-05-15 20:35:37 +0900

    feat: 자동차 경주 테스트 코드 추가

--- src/test/kotlin/racingcar/domain/CarFixture.kt
+++ src/test/kotlin/racingcar/domain/CarFixture.kt
@@ -2,15 +2,19 @@ package racingcar.domain
 
 object CarFixture {
     val initCars = listOf(
-        Car(position = 0),
-        Car(position = 0),
-        Car(position = 0),
+        Car(position = 0, name = "car1"),
+        Car(position = 0, name = "car2"),
+        Car(position = 0, name = "car3"),
     )
-    val runningCars = listOf(
-        Car(position = 0),
-        Car(position = 1),
-        Car(position = 2),
+    val racingCarOneWinner = listOf(
+        Car(position = 0, name = "car1"),
+        Car(position = 1, name = "car2"),
+        Car(position = 2, name = "car3"),
     )
-
-    val car = Car(position = 0)
+    val racingCarAllWinner = listOf(
+        Car(position = 1, name = "car1"),
+        Car(position = 1, name = "car2"),
+        Car(position = 1, name = "car3"),
+    )
+    val car = Car(position = 0, name = "myCar")
 }
--- src/test/kotlin/racingcar/domain/CarRacingTest.kt
+++ src/test/kotlin/racingcar/domain/CarRacingTest.kt
@@ -1,28 +1,55 @@
 package racingcar.domain
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.AnnotationSpec
 import io.kotest.matchers.ints.shouldBeInRange
 import io.kotest.matchers.shouldBe
 import racingcar.domain.CarFixture.initCars
-import racingcar.domain.CarFixture.runningCars
+import racingcar.domain.CarFixture.racingCarAllWinner
+import racingcar.domain.CarFixture.racingCarOneWinner
 
 class CarRacingTest : AnnotationSpec() {
     @Test
-    fun `자동차 수 만큼 Car 객체가 생성되고 각 시작 위치는 0이다`() {
-        val carCount = 3
-        val resultCars = CarRacing.initCars(carCount)
-        resultCars.size shouldBe carCount
-        initCars[0].position shouldBe resultCars[0].position
-        initCars[1].position shouldBe resultCars[1].position
-        initCars[2].position shouldBe resultCars[2].position
+    fun `입력한 자동차 이름 수 만큼 Car 객체가 생성되고, 시작위치는 0이다`() {
+        val carNames = listOf("car1", "car2", "car3")
+        val actualCars = CarRacing.initCars(carNames)
+        initCars.size shouldBe actualCars.size
+        initCars[0].position shouldBe actualCars[0].position
+        initCars[1].position shouldBe actualCars[1].position
+        initCars[2].position shouldBe actualCars[2].position
+        initCars[0].name shouldBe actualCars[0].name
+        initCars[1].name shouldBe actualCars[1].name
+        initCars[2].name shouldBe actualCars[2].name
     }
 
     @Test
     fun `자동차 경주 한 턴당 각 자동차 위치는 1칸 전진하거나 이동하지 않아야 한다`() {
-        val resultCars = CarRacing.moveCars(runningCars)
-        resultCars.size shouldBe runningCars.size
-        resultCars[0].position shouldBeInRange runningCars[0].position..runningCars[0].position.plus(1)
-        resultCars[1].position shouldBeInRange runningCars[1].position..runningCars[1].position.plus(1)
-        resultCars[2].position shouldBeInRange runningCars[2].position..runningCars[2].position.plus(1)
+        val resultCars = CarRacing.moveCars(racingCarOneWinner)
+        resultCars.size shouldBe racingCarOneWinner.size
+        resultCars[0].position shouldBeInRange racingCarOneWinner[0].position..racingCarOneWinner[0].position.plus(1)
+        resultCars[1].position shouldBeInRange racingCarOneWinner[1].position..racingCarOneWinner[1].position.plus(1)
+        resultCars[2].position shouldBeInRange racingCarOneWinner[2].position..racingCarOneWinner[2].position.plus(1)
+    }
+
+    @Test
+    fun `자동차 게임이 끝난 후 우승자 목록을 알 수 있다`() {
+        val winner = CarRacing.getWinners(racingCarOneWinner)
+        winner.size shouldBe 1
+        winner[0] shouldBe racingCarOneWinner[2].name
+    }
+
+    @Test
+    fun `우승자는 한 명 이상일 수 있다`() {
+        val listWinner = CarRacing.getWinners(racingCarAllWinner)
+        listWinner.size shouldBe racingCarAllWinner.size
+    }
+
+    @Test
+    fun `자동차 이름 길이는 5자를 초과할 수 없다`() {
+        val carNames = listOf("myCar", "thisIsMyCar")
+        val exception = shouldThrow<IllegalArgumentException> {
+            CarRacing.initCars(carNames)
+        }
+        exception.message shouldBe "자동차 이름은 5자를 초과할 수 없어요."
     }
 }
