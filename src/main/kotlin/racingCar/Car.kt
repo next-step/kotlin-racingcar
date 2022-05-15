bdf02fa40be6b3a041902616bc41a3ab2ad33d28
Molly <dngwoodo@gmil.comm>
2022-05-15 20:56:53 +0900

    Car 클래스 구현 및 테스트 코드 작성
    - move 메서드 구현

---
+++ src/main/kotlin/racingCar/Car.kt
@@ -0,0 +1,15 @@
+package racingCar
+
+interface IRandom {
+    fun generate(): Int
+}
+
+class Car(private val tryCount: Int, private val Random: IRandom) {
+    var position: Int = 0
+
+    fun move() {
+        for (i: Int in 1..tryCount) {
+            position = if (Random.generate() >= 4) position + 1 else position
+        }
+    }
+}
---
+++ src/test/kotlin/racingCar/CarTest.kt
@@ -0,0 +1,50 @@
+package racingCar
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+
+class CarTest : DescribeSpec({
+    describe("move") {
+        it("position 을 1만큼 증가시킨다.") {
+            //given
+            val car = Car(1, StubRandom(5))
+
+            // when
+            car.move()
+
+            // then
+            car.position shouldBe 1
+        }
+
+        context("시도 횟수가 여러번일 때") {
+            it("시도 횟수만큼 position 을 증가시킨다.") {
+                //given
+                val car = Car(2, StubRandom(5))
+
+                // when
+                car.move()
+
+                // then
+                car.position shouldBe 2
+            }
+        }
+
+        context("랜덤으로 생성한 숫자가 4 미만일 때") {
+            it("position 은 증가시키지 않는다.") {
+                //given
+                val car = Car(1, StubRandom(3))
+
+                // when
+                car.move()
+
+                // then
+                car.position shouldBe 0
+            }
+        }
+    }
+})
+
+
+class StubRandom(private val num: Int) : IRandom {
+    override fun generate() = num
+}
