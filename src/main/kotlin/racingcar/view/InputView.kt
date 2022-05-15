11a51bd013e3d5dcb2b9820fe500b8f9d588c8b5
yjna2316 <yjna2316@gmail.com>
2022-05-15 20:32:41 +0900

    feat: InputView, ResultView 자동차 이름 입력과 우승자 정보 출력 기능 추가

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -1,18 +1,18 @@
 package racingcar.view
 
 object InputView {
-    fun readNumOfCars(): Int {
-        println("자동차 대수는 몇 대인가요?")
+    fun readCarNames(): String {
+        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
         return readInput(readLine())
     }
 
     fun readNumOfTrials(): Int {
         println("시도할 횟수는 몇 회인가요?")
-        return readInput(readLine())
+        return readInput(readLine()).toInt()
     }
 
-    private fun readInput(inputStr: String? = null): Int {
+    private fun readInput(inputStr: String? = null): String {
         require(!inputStr.isNullOrBlank()) { "입력값이 비어있어요." }
-        return inputStr.toInt()
+        return inputStr
     }
 }
--- src/main/kotlin/racingcar/view/ResultView.kt
+++ src/main/kotlin/racingcar/view/ResultView.kt
@@ -3,7 +3,7 @@ package racingcar.view
 import racingcar.domain.Car
 
 object ResultView {
-    fun show(listCar: List<Car>) {
+    fun showPosition(listCar: List<Car>) {
         printCurrentPosition(listCar)
     }
 
@@ -11,8 +11,13 @@ object ResultView {
         println("\n[실행 결과]")
     }
 
+    fun showWinner(winnerNames: List<String>) {
+        println("${winnerNames.joinToString(", ")}가 최종 우승했습니다.")
+    }
+
     private fun printCurrentPosition(listCar: List<Car>) {
         listCar.map { car ->
+            print("${car.name} : ")
             (0 until car.position).map { print("-") }
             println("")
         }
