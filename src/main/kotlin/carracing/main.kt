a93e202821c7aa158a0c902cd459c28a353c92ec
Juice <3221ju@gmail.com>
2022-05-15 16:54:28 +0900

    ref: 상수로 적용

--- src/main/kotlin/carracing/main.kt
+++ src/main/kotlin/carracing/main.kt
@@ -2,6 +2,9 @@ package carracing
 
 const val VIEW_STRING_CAR = "-"
 const val VIEW_STRING_SEPARATOR = "\n"
+const val FORWARD_INT = 4
+const val MIN_RANDOM_INT = 1
+const val MAX_RANDOM_INT = 9
 
 fun main() {
     println("자동차 대수는 몇 대인가요?")
@@ -9,8 +12,15 @@ fun main() {
     println("시도할 횟수는 몇 회인가요?")
     val movementTryCount: String = readln()
     val inputDto = InputDto(carCount, movementTryCount)
-    val randomIntCarRacingManager =
-        RandomIntCarRacingManager.fromInputDto(inputDto, RandomIntRacingMovementRole(4, 1, 9))
+
+    val randomIntCarRacingManager = RandomIntCarRacingManager.fromInputDto(
+        inputDto,
+        RandomIntRacingMovementRole(
+            FORWARD_INT,
+            MIN_RANDOM_INT,
+            MAX_RANDOM_INT,
+        )
+    )
     val carRacingView = CarRacingView(VIEW_STRING_CAR, VIEW_STRING_SEPARATOR)
 
     repeat(inputDto.movementTryCount) {
@@ -22,7 +32,7 @@ fun main() {
 
 private fun setRandomValue(randomIntCarRacingManager: RandomIntCarRacingManager) {
     for (carIndex in 0 until randomIntCarRacingManager.getCarSize()) {
-        val randomNumber = (1..9).random()
+        val randomNumber = (MIN_RANDOM_INT..MAX_RANDOM_INT).random()
         randomIntCarRacingManager.tryMoveCar(carIndex, randomNumber)
     }
 }
