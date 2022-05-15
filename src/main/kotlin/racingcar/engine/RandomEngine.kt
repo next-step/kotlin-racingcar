72ff4f22c9d083067f0a03e4881cf411f4da2a9e
Jake Son <sonjeabin@gmail.com>
2022-05-15 10:30:14 +0900

    refactor: 상수 값 위치 변경

--- src/main/kotlin/racingcar/engine/RandomEngine.kt
+++ src/main/kotlin/racingcar/engine/RandomEngine.kt
@@ -2,15 +2,17 @@ package racingcar.engine
 
 import random.RandomNumberGenerator
 
-const val RANDOM_FROM = 0
-const val RANDOM_TO = 10
-const val MOVE_THRESHOLD = 4
-
 class RandomEngine : Engine {
 
     override fun accelerate(): Distance {
         val random = RandomNumberGenerator.generate(RANDOM_FROM, RANDOM_TO)
 
         return Distance(if (random >= MOVE_THRESHOLD) 1 else 0)
+    }
+
+    companion object {
+        private const val RANDOM_FROM = 0
+        private const val RANDOM_TO = 10
+        private const val MOVE_THRESHOLD = 4
     }
 }
