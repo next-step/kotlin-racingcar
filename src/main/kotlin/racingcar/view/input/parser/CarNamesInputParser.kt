fb6f2302263059af13e849c20e61a6b4976347d8
Juatae-Kim <jutaekim@sk.com>
2022-05-16 08:02:14 +0900

    style : Add new lines to increase readability

--- src/main/kotlin/racingcar/view/input/parser/CarNamesInputParser.kt
+++ src/main/kotlin/racingcar/view/input/parser/CarNamesInputParser.kt
@@ -7,7 +7,7 @@ class CarNamesInputParser(private val maxLengthOfName: Int) : InputParser<List<S
     }
 
     override fun parseValue(inputString: String?): List<String> {
-        val names = inputString?.split(",")?.filterNot { it.isBlank() }?.map { it.trim() } ?: listOf()
+        val names = inputString.toBlankRemovedToken(",")
         val namesOverLength = names.filter { it.length > maxLengthOfName }
 
         require(names.isNotEmpty()) { ERROR_MESSAGE_NO_INPUT }
@@ -18,4 +18,9 @@ class CarNamesInputParser(private val maxLengthOfName: Int) : InputParser<List<S
 
     private fun makeOverLengthErrorMessage(names: Collection<String>) =
         "[길이 > $maxLengthOfName]  인 이름이 있습니다 : ${names.joinToString(",", "[", "]")}"
+
+    private fun String?.toBlankRemovedToken(delimiter: String) = this?.split(delimiter)
+        ?.filterNot { it.isBlank() }
+        ?.map { it.trim() }
+        ?: listOf()
 }
