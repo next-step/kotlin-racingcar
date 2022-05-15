bf453c317b789a8a3a45916ba73f534523124c50
minwoo <mwoodev526@gmail.com>
2022-05-16 02:40:28 +0900

    feat: 우승자 출력

--- src/main/kotlin/_3_racing_car/ui/ResultView.kt
+++ src/main/kotlin/_3_racing_car/ui/ResultView.kt
@@ -1,12 +1,19 @@
 package _3_racing_car.ui
 
-data class ResultView(val result: List<Int>) {
+data class ResultView(val result: Map<String, Int>) {
 
     fun print() {
         println("실행 결과")
-        result.forEach {
-            println(MARKED.repeat(it))
+        result.forEach { (name, location) ->
+            println("$name : ${MARKED.repeat(location)}")
         }
+        println("${getWinners(result).joinToString(", ")}가 최종 우승했습니다.")
+    }
+
+    private fun getWinners(result: Map<String, Int>): List<String> {
+        val maxLocation = result.maxByOrNull { it.value }?.value
+
+        return result.filter { it.value == maxLocation }.map { it.key }
     }
 
     companion object {
