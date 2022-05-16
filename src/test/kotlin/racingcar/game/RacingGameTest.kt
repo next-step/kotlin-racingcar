e1234d05cf043b0b7f8c5b12bdb92a607c7a5323
Jake Son <sonjeabin@gmail.com>
2022-05-16 23:20:02 +0900

    test: RacingGame 테스트 보강

--- src/test/kotlin/racingcar/game/RacingGameTest.kt
+++ src/test/kotlin/racingcar/game/RacingGameTest.kt
@@ -1,11 +1,30 @@
 package racingcar.game
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
 import racingcar.car.Car
 import racingcar.car.Position
 
 internal class RacingGameTest : StringSpec({
+
+    "주어진 round 와 동일한 개수의 result 를 반환한다" {
+        // given
+        class StubCar : Car {
+            override fun position(): Position = Position(0)
+            override fun forward(): Car = this
+        }
+
+        val round = 10
+        val racingGame = RacingGame(listOf(StubCar()), round)
+
+        // when
+        val results = racingGame.getResults()
+
+        // then
+        results shouldHaveSize round
+    }
+
     "각 자동차에 대한 round 별 게임 결과를 반환한다" {
         // given
         class StubCar(private val position: Int, private val speed: Int) : Car {
@@ -14,8 +33,8 @@ internal class RacingGameTest : StringSpec({
         }
 
         val cars = listOf(StubCar(0, 10), StubCar(0, 20))
-        val step = 2
-        val racingGame = RacingGame(cars, step)
+        val round = 2
+        val racingGame = RacingGame(cars, round)
 
         // when
         val results = racingGame.getResults()
