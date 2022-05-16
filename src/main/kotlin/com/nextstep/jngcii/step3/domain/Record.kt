fd3672923f6c5f57dcf39bc6b230a8faf9b5d546
jngcii <concotree@gmail.com>
2022-05-16 18:29:14 +0900

    refactor : Record 기능 리팩터링 및 테스트

--- src/main/kotlin/com/nextstep/jngcii/step3/domain/Record.kt
+++ src/main/kotlin/com/nextstep/jngcii/step3/domain/Record.kt
@@ -1,12 +1,14 @@
 package com.nextstep.jngcii.step3.domain
 
 class Record(
-    private val carCount: Int
+    private val carCount: Int,
+    val trialCount: Int
 ) {
     companion object {
         private const val INITIAL_INDEX = 0
 
         private const val INITIAL_COUNT = 0
+        private const val EMPTY_COUNT = 0
         private const val SINGLE_COUNT = 1
     }
 
@@ -14,13 +16,27 @@ class Record(
         .map { INITIAL_COUNT }
         .toMutableList()
 
-    fun write(carIndex: Int, canGo: Boolean) {
-        if (carIndex < INITIAL_INDEX || carIndex >= carCount) {
-            throw IllegalArgumentException("0 <= n < $carCount 의 숫자만 입력 가능합니다.")
-        }
+    fun getGoCount(carIndex: Int): Int {
+        carIndex.validate()
 
-        goCounts[carIndex] = canGo.toCount()
+        return goCounts[carIndex]
     }
 
-    private fun Boolean.toCount() = if (this) 1 else 0
+    fun write(carIndex: Int, canGo: Boolean) {
+        carIndex.validate()
+
+        if (goCounts[carIndex] + 1 > trialCount) {
+            throw IllegalStateException("최대 전진 횟수는 시도 횟수보다 클 수 없습니다.")
+        }
+
+        goCounts[carIndex] += canGo.toCount()
+    }
+
+    private fun Int.validate() {
+        if (this < INITIAL_INDEX || this >= carCount) {
+            throw IllegalArgumentException("0 <= n < $carCount 의 숫자만 입력 가능합니다.")
+        }
+    }
+
+    private fun Boolean.toCount() = if (this) SINGLE_COUNT else EMPTY_COUNT
 }
--- src/test/kotlin/com/nextstep/jngcii/step3/domain/RecordTest.kt
+++ src/test/kotlin/com/nextstep/jngcii/step3/domain/RecordTest.kt
@@ -1,5 +1,7 @@
 package com.nextstep.jngcii.step3.domain
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
@@ -10,18 +12,51 @@ internal class RecordTest {
     @ParameterizedTest
     @CsvSource("3, 0", "3, 1", "3, 2")
     fun `write 성공 테스트`(carCount: Int, writeIndex: Int) {
-        val record = Record(carCount)
+        val record = Record(carCount, 5)
 
         assertDoesNotThrow { record.write(writeIndex, true) }
     }
 
     @ParameterizedTest
     @CsvSource("3, -1", "3, 3")
-    fun `write 실패 테스트`(carCount: Int, writeIndex: Int) {
-        val record = Record(carCount)
+    fun `write 실패 테스트 - carIndex 오류`(carCount: Int, writeIndex: Int) {
+        val record = Record(carCount, 5)
 
         assertThrows<IllegalArgumentException>("0 <= n < $carCount 의 숫자만 입력 가능합니다.") {
             record.write(writeIndex, true)
         }
+    }
+
+    @Test
+    fun `write 실패 테스트 - 최대 전진 횟수 초과`() {
+        val trialCount = 2
+        val record = Record(5, trialCount)
+
+        repeat(trialCount) {
+            record.write(3, true)
+        }
+
+        assertThrows<IllegalStateException>("최대 전진 횟수는 시도 횟수보다 클 수 없습니다.") {
+            record.write(3, true)
+        }
+    }
+
+    @Test
+    fun `canGo=false 일 때 goCount 추가되지 않는것 테스트`() {
+        val trialCount = 3
+        val record = Record(5, trialCount)
+
+        assertThat(record.getGoCount(3)).isEqualTo(0)
+        repeat(trialCount) { record.write(3, false) }
+        assertThat(record.getGoCount(3)).isEqualTo(0)
+    }
+
+    @Test
+    fun `canGo=true 호출 횟수만큼 goCount 적재되는것 테스트`() {
+        val trialCount = 3
+        val record = Record(5, trialCount)
+
+        repeat(2) { record.write(3, true) }
+        assertThat(record.getGoCount(3)).isEqualTo(2)
     }
 }
