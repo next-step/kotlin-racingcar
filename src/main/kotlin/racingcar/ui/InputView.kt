d4a2ad35ce0f0a8bd8f67d87e1cccc52a426c71e
Jake Son <sonjeabin@gmail.com>
2022-05-16 23:06:10 +0900

    refactor: 사용자 입력 검증로직 추가

--- src/main/kotlin/racingcar/ui/InputView.kt
+++ src/main/kotlin/racingcar/ui/InputView.kt
@@ -3,9 +3,13 @@ package racingcar.ui
 object InputView {
     fun run(): GameInput {
         println("자동차 대수는 몇 대인가요?")
-        val carCount = readLine()!!.toInt()
+        val carCount = readLine()?.toIntOrNull()
+        requireNotNull(carCount) { "자동차 대수는 숫자로 입력해주세요." }
+
         println("시도할 횟수는 몇 회인가요?")
-        val round = readLine()!!.toInt()
+        val round = readLine()?.toIntOrNull()
+        requireNotNull(round) { "시도할 횟수는 숫자로 입력해주세요." }
+
         println()
 
         return GameInput(carCount, round)
