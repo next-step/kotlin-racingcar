85dc568b5859c5f86ccb2c2a9a45cfce073d3f40
Gomding <backchida@gmail.com>
2022-05-17 03:00:07 +0900

    refactor(racingcar): RaceHost의 0부터 반복되는 반복문에 repeat 함수를 사용하도록 수정

--- src/main/kotlin/racingcar/RaceHost.kt
+++ src/main/kotlin/racingcar/RaceHost.kt
@@ -5,9 +5,9 @@ object RaceHost {
     fun play(rounds: Int, numberOfParticipants: Int): RacingResults {
         var cars: Cars = Cars.initCars(numberOfParticipants)
         var racingResults = RacingResults()
-        for (i in 1..rounds) {
+        repeat(rounds) {
             cars = cars.move(RandomGasStation.refuels(numberOfParticipants))
-            racingResults = racingResults.add(RacingResult.of(round = i, cars = cars))
+            racingResults = racingResults.add(RacingResult.of(round = it, cars = cars))
         }
         return racingResults
     }
