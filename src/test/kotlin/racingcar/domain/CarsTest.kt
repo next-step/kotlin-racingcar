cb1c96c9394870184a60f7937e394c40f4437c2f
Rok93 <kr.kim@mfort.co.kr>
2022-05-16 14:13:29 +0900

    test: moveAll 함수 테스트 코드 추가

--- src/test/kotlin/racingcar/domain/CarsTest.kt
+++ src/test/kotlin/racingcar/domain/CarsTest.kt
@@ -3,6 +3,9 @@ package racingcar.domain
 import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.data.row
+import io.kotest.inspectors.forAll
+import io.kotest.matchers.shouldBe
 
 class CarsTest : FunSpec({
     test("Cars 객체를 생성한다") {
@@ -22,5 +25,19 @@ class CarsTest : FunSpec({
 
         // when // then
         shouldThrowExactly<IllegalArgumentException> { Cars(cars) }
+    }
+
+    test("자동차들이 전진하거나 전진하지 않는다.") {
+        listOf(
+            row(Cars(listOf(Car(), Car())), 3, 0),
+            row(Cars(listOf(Car(), Car())), 4, 1),
+        ).forAll { (cars, condition, expected) ->
+            // when
+            val actual = cars.moveAll { condition }
+
+            // then
+            actual.value[0].position shouldBe expected
+            actual.value[1].position shouldBe expected
+        }
     }
 })
