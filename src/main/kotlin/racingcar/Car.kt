179a40b9116da28a259a1d7e0093b62cde226d33
vista0212 <kbaek1202@gmail.com>
2022-05-16 10:42:02 +0900

    Car 클래스 관련 린트 수정

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -6,7 +6,7 @@ data class Car(var distance: Int = 0) {
     private val MAX_RANDOM_VALUE = 10
 
     fun moveRandom(number: Int = Random.nextInt(MAX_RANDOM_VALUE)) {
-        if(canMove(number)) {
+        if (canMove(number)) {
             distance += 1
         }
     }
