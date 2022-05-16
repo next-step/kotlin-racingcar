4fc0104a5267bf37d13738809b0d8ca225176244
Jake Son <sonjeabin@gmail.com>
2022-05-16 23:32:00 +0900

    refactor: 입력값 검증 로직 수정

--- src/main/kotlin/racingcar/ui/InputView.kt
+++ src/main/kotlin/racingcar/ui/InputView.kt
@@ -2,11 +2,10 @@ package racingcar.ui
 
 object InputView {
     fun run(): GameInput {
-        println("자동차 대수는 몇 대인가요?")
-        val carCount = readLine()
-        requireNotNull(carCount) { "자동차 이름을 입력해주세요." }
+        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+        val carNames = readLine()
+        requireNotNull(carNames) { "자동차 이름을 입력해주세요." }
 
-        val carNames = readLine()!!
         println("시도할 횟수는 몇 회인가요?")
         val round = readLine()?.toIntOrNull()
         requireNotNull(round) { "시도할 횟수는 숫자로 입력해주세요." }
