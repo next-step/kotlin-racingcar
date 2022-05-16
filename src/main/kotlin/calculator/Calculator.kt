a6cdafca85a8f1db51e82d15ecc3af4af64d0d5b
Molly <dngwoodo@gmail.com>
2022-05-16 23:58:10 +0900

    total, operand 가 숫자가 아닐 때 예외처리

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -8,10 +8,18 @@ object Calculator {
 
         val parsedCalculateFormula = calculatorFormula.split(" ")
         val restParsedCalculateFormula = parsedCalculateFormula.drop(1)
-        var total = parsedCalculateFormula[0].toFloat()
+        var total = try {
+            parsedCalculateFormula[0].toFloat()
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException("유효하지 않은 계산식 입니다.")
+        }
 
         for (i: Int in restParsedCalculateFormula.indices step(2)) {
-            val operand = restParsedCalculateFormula[i + 1].toFloat()
+            val operand = try {
+                restParsedCalculateFormula[i + 1].toFloat()
+            } catch (e: NumberFormatException) {
+                throw IllegalArgumentException("유효하지 않은 계산식 입니다.")
+            }
             val op = restParsedCalculateFormula[i]
 
             Operator.evaluate(total, operand, op).also { total = it }
