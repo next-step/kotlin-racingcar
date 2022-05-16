914f2479c975721155ec544d1015da2d7e17b208
vista0212 <kbaek1202@gmail.com>
2022-05-16 10:41:30 +0900

    Car 클래스 추가 및 테스트 코드 작성

---
+++ src/main/kotlin/racingcar/Car.kt
@@ -0,0 +1,21 @@
+package racingcar
+
+import kotlin.random.Random
+
+data class Car(var distance: Int = 0) {
+    private val MAX_RANDOM_VALUE = 10
+
+    fun moveRandom(number: Int = Random.nextInt(MAX_RANDOM_VALUE)) {
+        if(canMove(number)) {
+            distance += 1
+        }
+    }
+
+    private fun canMove(number: Int): Boolean {
+        if (number >= 4) {
+            return true
+        }
+
+        return false
+    }
+}
