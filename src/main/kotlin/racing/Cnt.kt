ab1f9f1b1858da7678d919ce30da0ea186ab748f
Justin.sg <justin.sg@kakaocorp.com>
2022-05-16 19:36:49 +0900

    step3 자동차, Round 숫자를 사용을 위한 Cnt 추가

---
+++ src/main/kotlin/racing/Cnt.kt
@@ -0,0 +1,6 @@
+package racing
+
+typealias Cnt = Int
+typealias CarCnt = Cnt
+typealias RoundCnt = Cnt
+fun Cnt.toList() = (1..this).toList()
---
+++ src/test/kotlin/racing/CntTest.kt
@@ -0,0 +1,17 @@
+package racing
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class CntTest {
+
+    @Test
+    fun `Int 5 입력되었을때 5개의 원소를 갖는 List 생성`() {
+        assertThat(5.toList()).isEqualTo(listOf(1, 2, 3, 4, 5))
+    }
+
+    @Test
+    fun `Int 0 입력되었을때 5개의 원소를 갖는 emptyList 생성`() {
+        assertThat(0.toList()).isEqualTo(emptyList<Int>())
+    }
+}
