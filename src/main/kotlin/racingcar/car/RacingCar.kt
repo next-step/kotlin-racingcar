6be721989ef1ef028a31f402b5205627f1b49cb2
Jake Son <sonjeabin@gmail.com>
2022-05-16 09:23:25 +0900

    feat: 자동차 이름을 받을 수 있도록 수정

--- src/main/kotlin/racingcar/car/RacingCar.kt
+++ src/main/kotlin/racingcar/car/RacingCar.kt
@@ -4,14 +4,14 @@ import racingcar.engine.Engine
 
 class RacingCar(
     private val engine: Engine,
-    private val position: Position = Position(0),
     private val name: String = "",
+    private val position: Position = Position(0),
 ) : Car {
     override fun forward(): Car {
         val distance = engine.accelerate()
         val newPosition = Position(position.value + distance.value)
 
-        return RacingCar(engine, newPosition)
+        return RacingCar(engine, name, newPosition)
     }
 
     override fun position(): Position = position
--- src/main/kotlin/racingcar/ui/InputView.kt
+++ src/main/kotlin/racingcar/ui/InputView.kt
@@ -3,15 +3,16 @@ package racingcar.ui
 object InputView {
     fun run(): GameInput {
         println("자동차 대수는 몇 대인가요?")
-        val carCount = readLine()?.toIntOrNull()
-        requireNotNull(carCount) { "자동차 대수는 숫자로 입력해주세요." }
+        val carCount = readLine()
+        requireNotNull(carCount) { "자동차 이름을 입력해주세요." }
 
+        val carNames = readLine()!!
         println("시도할 횟수는 몇 회인가요?")
         val round = readLine()?.toIntOrNull()
         requireNotNull(round) { "시도할 횟수는 숫자로 입력해주세요." }
 
         println()
 
-        return GameInput(carCount, round)
+        return GameInput(carNames, round)
     }
 }
