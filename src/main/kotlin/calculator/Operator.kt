6aa56a515aff973f1fbacbb7fc94a19b3da7269a
엄기훈 <pikachom@gmail.com>
2022-05-16 19:33:26 +0900

    refact: step2 리뷰 코멘트 반영(2)
    https://github.com/next-step/kotlin-racingcar/pull/554#discussion_r873521458
    
    - operator 를 enum class 로 변경하고, 검증 부분 위임

---
+++ src/main/kotlin/calculator/Operator.kt
@@ -0,0 +1,16 @@
+package calculator
+
+enum class Operator {
+    PLUS,
+    MINUS,
+    DIVIDE,
+    MULTIPLY;
+}
+fun String.convertToOperatorEnum(): Operator =
+    when(this){
+        "+" -> Operator.PLUS
+        "-" -> Operator.MINUS
+        "/" -> Operator.DIVIDE
+        "*" -> Operator.MULTIPLY
+        else -> throw java.lang.IllegalArgumentException("잘못된 연산자가 입력되었습니다.")
+    }
--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -4,15 +4,15 @@ class StringCalculator(private val input: String?) {
     fun run(): Int {
         require(!input.isNullOrBlank()) { "입력값은 공백일 수 없습니다" }
         val splitted = splitter(input, " ")
-        var operator = "+"
+        var operator = Operator.PLUS
 
         return splitted.foldIndexed(0) { idx, sum, element ->
             if (isEvenIndex(idx)) {
                 val rightTerm = toIntOrThrow(element)
                 calculate(sum, rightTerm, operator)
             } else {
-                require(isValidOperator(element))
-                operator = element
+                operator = element.convertToOperatorEnum()
+
                 sum
             }
         }
@@ -22,24 +22,19 @@ class StringCalculator(private val input: String?) {
 
     private fun splitter(input: String, delimiter: String): List<String> = input.split(delimiter)
 
-    private fun isValidOperator(element: String): Boolean {
-        return element in listOf("+", "-", "*", "/")
-    }
-
     private fun toIntOrThrow(element: String): Int {
         return element.toIntOrNull() ?: throw IllegalArgumentException("$element 는 정수가 아닙니다")
     }
 
-    private fun calculate(leftTerm: Int, rightTerm: Int, operator: String): Int {
+    private fun calculate(leftTerm: Int, rightTerm: Int, operator: Operator): Int {
         return when (operator) {
-            "+" -> leftTerm + rightTerm
-            "-" -> leftTerm - rightTerm
-            "*" -> leftTerm * rightTerm
-            "/" -> if (rightTerm == 0) {
+            Operator.PLUS -> leftTerm + rightTerm
+            Operator.MINUS -> leftTerm - rightTerm
+            Operator.MULTIPLY -> leftTerm * rightTerm
+            Operator.DIVIDE -> if (rightTerm == 0) {
                 throw IllegalArgumentException("/ 뒤에는 0이 올 수 없습니다")
             } else { leftTerm / rightTerm }
 
-            else -> throw IllegalArgumentException("$operator 는 잘못된 연산자입니다")
         }
     }
 }
