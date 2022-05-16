6d552df29d3aa0000752e2287a2abe0ae0958247
glennsm <lovinhut@gmail.com>
2022-05-16 23:02:04 +0900

    사칙연산 enum 활용해서 수정하기

--- src/main/kotlin/Calculation.kt
+++ src/main/kotlin/Calculation.kt
@@ -1,6 +1,6 @@
 class Calculation(private val expr: String) {
     fun run(): Int {
-        isNotBlankOrThrow(expr)
+        checkNotBlankOrThrow(expr)
 
         var target = 0
         var exp = ""
@@ -11,40 +11,21 @@ class Calculation(private val expr: String) {
                 checkExpOrThrow(next)
                 exp = next
             } else {
-                target = cal(target, toIntOrThrow(next), exp)
+                target = compute(target, toIntOrThrow(next), exp)
             }
         }
         return target
     }
 
-    private fun isNotBlankOrThrow(expr: String) = require(expr.isNotBlank())
+    private fun checkNotBlankOrThrow(expr: String) = require(expr.isNotBlank())
 
     private fun checkExpOrThrow(s: String) = require(checkExp(s))
 
     private fun toIntOrThrow(s: String) = s.toIntOrNull() ?: throw IllegalArgumentException()
 
-    private fun cal(v1: Int, v2: Int, exp: String): Int {
-        return when (exp) {
-            "+" -> plus(v1, v2)
-            "-" -> minus(v1, v2)
-            "*" -> multiply(v1, v2)
-            "/" -> divide(v1, v2)
-            else -> throw IllegalArgumentException()
-        }
-    }
-
-    private fun divide(v1: Int, v2: Int): Int {
-        if (v2 == 0) throw IllegalArgumentException()
-        return v1 / v2
-    }
-
-    private fun multiply(v1: Int, v2: Int) = v1 * v2
-
-    private fun minus(v1: Int, v2: Int) = v1 - v2
-
-    private fun plus(v1: Int, v2: Int) = v1 + v2
+    private fun compute(v1: Int, v2: Int, exp: String) = Operation.of(exp).compute(v1, v2)
 
     private fun checkExp(exp: String): Boolean {
-        return exp in listOf("+", "-", "*", "/")
+        return Operation.of(exp) in Operation.values() && Operation.of(exp) != Operation.NONE
     }
 }
--- src/main/kotlin/CalculatorApp.kt
+++
@@ -1,6 +0,0 @@
-import java.util.Scanner
-
-fun main() {
-    print("숫자와 수식을 입력하세요 : ")
-    Calculation(Scanner(System.`in`).nextLine()).run()
-}
