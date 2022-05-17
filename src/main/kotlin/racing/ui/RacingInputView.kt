83cb4303a59371ea62efdf15a6bb0e22ca1f868e
qyu.st <qyu.st@kakaocorp.com>
2022-05-17 17:54:46 +0900

    feat: RacingInputView 기능 추가
    * 각 자동차에 이름을 부여할 수 있음
    * 이름은 5글자를 초과할 수 없음
    * 자동차 이름 쉼표로 파싱

--- src/main/kotlin/racing/ui/RacingInputView.kt
+++ src/main/kotlin/racing/ui/RacingInputView.kt
@@ -2,6 +2,25 @@ package racing.ui
 
 object RacingInputView {
 
+    private const val DELIMITER = ","
+    private val regex = Regex("[\\t\\s]")
+    fun getCarNames(): List<String> {
+        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+        val carNamesStr = readln()
+        return parseCarNamesStr(carNamesStr)
+    }
+
+    fun parseCarNamesStr(carNamesStr: String): List<String> {
+        val result = regex.replace(carNamesStr, "")
+            .split(DELIMITER)
+
+        check( result.all { name -> name.length <= 5} ) {
+            "자동차 이름은 5자를 초과할 수 없습니다."
+        }
+
+        return result
+    }
+
     fun getNumberOfCars(): Int {
         println("자동차 대수는 몇 대인가요?")
         return readln().toInt()
