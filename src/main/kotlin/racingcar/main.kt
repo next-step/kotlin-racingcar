8feb4706f255fe204eaa75b37de2d7c6bb92fb5b
hoori <freedom09@inflab.com>
2022-05-17 10:57:06 +0900

    main 함수 리팩토링

--- src/main/kotlin/racingcar/main.kt
+++ src/main/kotlin/racingcar/main.kt
@@ -6,9 +6,7 @@ import racingcar.view.ResultView
 
 fun main() {
     val inputView = InputView()
-    val carCount: Int = inputView.getCarCount()
-    val moveCount: Int = inputView.getMoveCount()
-    val race: Race = Race(carCount, moveCount)
+    val race = Race(inputView.getCarCount(), inputView.getMoveCount())
     val resultView = ResultView(race)
     resultView.show()
 }
