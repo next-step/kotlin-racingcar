cd0cc51682e31e6b88e8f38227a4af20de3b8805
revfactory <revfactory@gmail.com>
2022-05-17 22:33:02 +0900

    refactor: Refactoring Code
    (cherry picked from commit bac4bb0efa64db6020be82db880081013ac9e42e)

--- src/main/kotlin/step3/model/Car.kt
+++ src/main/kotlin/step3/model/Car.kt
@@ -3,11 +3,9 @@ package step3.model
 import step3.util.NumberGenerator
 import step3.util.RandomGenerator
 
-class Car(val name: String, private val numberGenerator: NumberGenerator = RandomGenerator()) {
-    companion object {
-        const val MIN_RANDOM_VALUE = 0
-        const val MAX_RANDOM_VALUE = 9
-        const val MOVABLE_THRESHOLD = 4
+class Car(val name: String, private val numberGenerator: NumberGenerator = RandomGenerator) {
+    init {
+        require(name.trim().length in NAME_LENGTH_RANGE) { "Invalid Name Length" }
     }
 
     fun move(): Boolean {
@@ -16,5 +14,12 @@ class Car(val name: String, private val numberGenerator: NumberGenerator = Rando
 
     private fun isMovable(): Boolean {
         return MOVABLE_THRESHOLD <= numberGenerator.generate(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE)
+    }
+
+    companion object {
+        const val MIN_RANDOM_VALUE = 0
+        const val MAX_RANDOM_VALUE = 9
+        const val MOVABLE_THRESHOLD = 4
+        val NAME_LENGTH_RANGE = (1..5)
     }
 }
--- src/main/kotlin/step3/util/RandomGenerator.kt
+++ src/main/kotlin/step3/util/RandomGenerator.kt
@@ -1,12 +1,7 @@
 package step3.util
 
-import java.security.SecureRandom
-
-class RandomGenerator : NumberGenerator {
-
-    private val secureRandom = SecureRandom()
-
+object RandomGenerator : NumberGenerator {
     override fun generate(minValue: Int, maxValue: Int): Int {
-        return minValue + secureRandom.nextInt(minValue + (maxValue + 1))
+        return (minValue..maxValue).random()
     }
 }
