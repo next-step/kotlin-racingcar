1bc6721540f0ef83f352cc6e68539391b01afe6c
kwanguuuu <kwangyu326@gmail.com>
2022-05-17 23:29:39 +0900

    [step4-pr] chore: parameterized 테스트로 변경

--- src/test/kotlin/racingcar/car/RacingGameTest.kt
+++ src/test/kotlin/racingcar/car/RacingGameTest.kt
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import racingcar.car.exception.GameOverException
 import racingcar.car.strategy.MoveStrategy
 import racingcar.car.strategy.NameStrategy
@@ -21,15 +23,11 @@ class RacingGameTest {
         }
     }
 
-    @Test
-    fun `게임이 진행 가능 횟수가 0 초과할 때 게임을 할 수 있다`() {
-        val correctRacingGameInit = RacingGame(listOf("car1", "car2"), 1, moveStrategy, nameStrategy)
-        val badRacingGameInitCase = RacingGame(listOf("car1", "car2"), 0, moveStrategy, nameStrategy)
-        val badRacingGameInitCase2 = RacingGame(listOf("car1", "car2"), -1, moveStrategy, nameStrategy)
-
-        assertThat(correctRacingGameInit.isPlayable()).isTrue
-        assertThat(badRacingGameInitCase.isPlayable()).isFalse
-        assertThat(badRacingGameInitCase2.isPlayable()).isFalse
+    @ParameterizedTest
+    @CsvSource(value = ["1, true", "0, false", "-1, false"])
+    fun `게임이 진행 가능 횟수가 0 초과할 때 게임을 할 수 있다`(testTrialCount: Int, result: Boolean) {
+        val correctRacingGameInit = RacingGame(listOf("car1", "car2"), testTrialCount, moveStrategy, nameStrategy)
+        assertThat(correctRacingGameInit.isPlayable()).isEqualTo(result)
     }
 
     @Test
