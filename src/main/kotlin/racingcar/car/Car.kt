11a9f08e934836f8bdc24a67b30a06beed77069b
hoori <nfreedom9@gmail.com>
2022-05-17 17:28:37 +0900

    변수명 수정

--- src/main/kotlin/racingcar/car/Car.kt
+++ src/main/kotlin/racingcar/car/Car.kt
@@ -2,16 +2,17 @@ package racingcar.car
 
 class Car(moveCount: Int) {
     companion object {
-        val range = (0..9)
+        private val range = (0..9)
     }
 
     private val randomInts = RandomList.intListBy(range, moveCount)
-    private val drivingPoints = randomInts.map { i -> DrivingForce.pointBy(i) }
-    private val initPositions = arrayListOf(0)
-    private val positions = drivingPoints.fold(initPositions) { positions, point ->
+    private val drivingPoints = randomInts.map { DrivingForce.pointBy(it) }
+    private val initPosition = 0
+    private val initPositions = arrayListOf(initPosition)
+    private val positionsByStep = drivingPoints.fold(initPositions) { positions, point ->
         positions.add(positions.last() + point)
         positions
     }
 
-    fun positionBy(step: Int): Int = positions[step]
+    fun positionBy(step: Int): Int = positionsByStep[step]
 }
