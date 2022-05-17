38758cbe7c026772fc1e55df261bf3e5f8117c35
엄기훈 <pikachom@gmail.com>
2022-05-18 01:03:09 +0900

    refactor: run 단위테스트 이름 변경
    https://github.com/next-step/kotlin-racingcar/pull/568#discussion_r873682170

--- src/test/kotlin/racing/GameTest.kt
+++ src/test/kotlin/racing/GameTest.kt
@@ -6,12 +6,12 @@ import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.ints.shouldBeInRange
 
 class GameTest : FunSpec({
-    // given
-    val carNumberInput = 3
-    val turnNumberInput = 4
     val game = Game()
-    context("run() 단위테스트") {
-        test("3대, 4턴에 대해 run 실행 시 carList크기는 3, 모든 Car의 currentLocation은 0~4 사이의 수를 가진다") {
+    context("run() 단위테스트 - 정상 범위의 input 입력후 run() 호출 시 게임이 정상적으로 동작한다") {
+        test("3대, 4턴 조건으로 run 실행 시 car는 3대, 모든 car의 currentLocation은 0..4 사이값을 가져야 한다") {
+            //given
+            val carNumberInput = 3
+            val turnNumberInput = 4
             // when
             game.run(carNumberInput, turnNumberInput)
             // then
