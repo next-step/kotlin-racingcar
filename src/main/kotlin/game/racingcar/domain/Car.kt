a13c28f5969f006697bbb22b2209934c31c38fc5
pplenty <newtype26@naver.com>
2022-05-17 20:18:19 +0900

    refactor: position setter 만 private

--- src/main/kotlin/game/racingcar/domain/Car.kt
+++ src/main/kotlin/game/racingcar/domain/Car.kt
@@ -4,17 +4,16 @@ import game.racingcar.strategy.MoveStrategy
 
 class Car(val name: String) {
     private val _blackBox: MutableList<Int> = mutableListOf()
-    private var _position: Int = 0
 
     val blackBox: List<Int>
         get() = _blackBox.toList()
-    val position: Int
-        get() = _position
+    var position: Int = 0
+        private set
 
     fun move(moveStrategy: MoveStrategy): Int {
-        _position = moveStrategy.move(_position)
-        _blackBox.add(_position)
-        return _position
+        position = moveStrategy.move(position)
+        _blackBox.add(position)
+        return position
     }
 
     fun history(time: Int) = blackBox[time]
