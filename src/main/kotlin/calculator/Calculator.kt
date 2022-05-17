c7652519745d9010411e6864d4e0c84fdfc1ea8d
Molly <dngwoodo@gmail.com>
2022-05-17 23:54:54 +0900

    op -> symbol 로 변수명 변경

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -1,27 +1,20 @@
 package calculator
 
 object Calculator {
-    fun calculate(calculatorFormula: String?): Float {
-        validateFor(calculatorFormula)
-
-        calculatorFormula as String
+    fun calculate(calculatorFormula: String): Float {
         val parsedCalculateFormula = calculatorFormula.split(" ")
         val restParsedCalculateFormula = parsedCalculateFormula.drop(1)
         var total = parsedCalculateFormula[0].toFloatOrThrow()
 
         for (i in restParsedCalculateFormula.indices step (2)) {
             val operand = restParsedCalculateFormula[i + 1].toFloatOrThrow()
-            val op = restParsedCalculateFormula[i]
+            val symbol = restParsedCalculateFormula[i]
 
-            total = Operator.find(op).evaluate(total, operand)
+            total = Operator.find(symbol).evaluate(total, operand)
         }
 
         return total
     }
 
     private fun String.toFloatOrThrow(): Float = toFloatOrNull() ?: throw IllegalArgumentException("유효하지 않은 계산식 입니다.")
-
-    private fun validateFor(calculatorFormula: String?) {
-        require(!calculatorFormula.isNullOrEmpty()) { "유효하지 않은 계산식 입니다." }
-    }
 }
