d67182f955f7c252e64cba1579066dca9a9a3ea0
Molly <dngwoodo@gmail.com>
2022-05-17 13:47:14 +0900

    불필요한 ICar 인터페이스 삭제

--- src/main/kotlin/racingCar/Car.kt
+++ src/main/kotlin/racingCar/Car.kt
@@ -1,13 +1,9 @@
 package racingCar
 
-interface ICar {
-    var position: Int
-    fun moveFor(tryCount: Int)
-}
-class Car(private val Random: IRandom): ICar {
-    override var position: Int = 0
+class Car(private val Random: IRandom) {
+    var position: Int = 0
 
-    override fun moveFor(tryCount: Int) {
+    fun moveFor(tryCount: Int) {
         for (i: Int in 1..tryCount) {
             position = if (Random.generate() >= 4) position + 1 else position
         }
