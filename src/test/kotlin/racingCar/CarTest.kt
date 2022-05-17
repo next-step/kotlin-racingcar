dc1829f4b8b9385f33f28d2cac8b9c08f9b6c2c5
Molly <dngwoodo@gmail.com>
2022-05-17 23:44:20 +0900

    불필요한 given/when/then 주석문 제거

--- src/test/kotlin/racingCar/CarTest.kt
+++ src/test/kotlin/racingCar/CarTest.kt
@@ -6,25 +6,19 @@ import io.kotest.matchers.shouldBe
 class CarTest : DescribeSpec({
     describe("move") {
         it("position 을 1만큼 증가시킨다.") {
-            // given
             val car = Car()
 
-            // when
             car.move(5)
 
-            // then
             car.position shouldBe 1
         }
 
         context("랜덤으로 생성한 숫자가 4 미만일 때") {
             it("position 을 증가시키지 않는다.") {
-                // given
                 val car = Car()
 
-                // when
                 car.move(3)
 
-                // then
                 car.position shouldBe 0
             }
         }
