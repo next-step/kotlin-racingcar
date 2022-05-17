1b4b9399dc4afd8487886d85e3a1ef8f106c128d
inpyo-jeon <inpyo.developer@gmail.com>
2022-05-18 01:28:44 +0900

    [Step4] 불필요한 코틀린 문법 제거

--- src/main/kotlin/racing/view/InputView.kt
+++ src/main/kotlin/racing/view/InputView.kt
@@ -7,30 +7,14 @@ class InputView {
     fun readInRoundSize(): Int {
         println("시도할 횟수는 몇 회인가요?")
 
-        var roundSize = 0
-        kotlin.runCatching {
-            readln().toInt()
-        }.fold(
-            onSuccess = { roundSize = it },
-            onFailure = { exception -> throw exception }
-        )
-
-        return roundSize
+        return readln().toInt()
     }
 
     fun readInCarNames(): List<String> {
         println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)")
 
-        var carNames: List<String> = emptyList()
-        kotlin.runCatching {
-            val tokens = readln().split(",")
-            validateCarNames(tokens)
-            tokens
-        }.fold(
-            onSuccess = { carNames = it },
-            onFailure = { exception -> throw exception }
-        )
-
+        val carNames = readln().split(",")
+        validateCarNames(carNames)
         return carNames
     }
 
