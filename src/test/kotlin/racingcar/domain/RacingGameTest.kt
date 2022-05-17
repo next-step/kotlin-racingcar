5ec0eb495639fc370011451c2cea95f667955385
link-js <link.js@kakaocorp.com>
2022-05-17 21:16:54 +0900

    refactor(test) : 테스트 코드 단일화

--- src/test/kotlin/racingcar/domain/RacingGameTest.kt
+++ src/test/kotlin/racingcar/domain/RacingGameTest.kt
@@ -3,6 +3,8 @@ package racingcar.domain
 import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 import racingcar.constants.Message
 
 /**
@@ -10,19 +12,11 @@ import racingcar.constants.Message
  * Created by Jaesungchi on 2022.05.07..
  */
 class RacingGameTest {
-    @Test
-    fun `음수 횟수가 입력되면 에러가 발생한다`() {
-        val racingGame = RacingGame(listOf(), -3)
+    @ParameterizedTest
+    @ValueSource(ints = [-3, 0])
+    fun `양수가 아닌 숫자가 입력되면 에러가 발생한다`(source: Int) {
         Assertions.assertThatExceptionOfType(IllegalArgumentException::class.java).isThrownBy {
-            racingGame.startGame()
-        }.withMessageMatching(Message.ExceptionMessage.ENTERED_INVALID_NUMBER)
-    }
-
-    @Test
-    fun `0이 입력되면 에러가 발생한다`() {
-        val racingGame = RacingGame(listOf(Car("link")), 0)
-        Assertions.assertThatExceptionOfType(IllegalArgumentException::class.java).isThrownBy {
-            racingGame.startGame()
+            RacingGame(listOf(), source)
         }.withMessageMatching(Message.ExceptionMessage.ENTERED_INVALID_NUMBER)
     }
 
