c3c8d6a0aad44bcddf5fde113dcf3badd9cdbfac
aeomhs(엄현식) <aeomhs@gmail.com>
2022-05-17 12:52:31 +0900

    test: Winner 테스트 케이스 작성

--- src/main/kotlin/camp/nextstep/edu/racingcar/racing/Winner.kt
+++ src/main/kotlin/camp/nextstep/edu/racingcar/racing/Winner.kt
@@ -5,6 +5,21 @@ class Winner private constructor(winner: Car) {
     val id = winner.id
     val name = winner.name
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Winner
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id
+    }
+
     companion object {
         fun of(racing: Racing): Set<Winner> {
             val racingResults = racing.carRacingEvents().map { it.key to CarRaceEvent.merge(it.value).marks() }
---
+++ src/test/kotlin/camp/nextstep/edu/racingcar/racing/WinnerTest.kt
@@ -0,0 +1,26 @@
+package camp.nextstep.edu.racingcar.racing
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+internal class WinnerTest {
+
+    @Test
+    fun createFromRacingResultTest() {
+        val tim = Car(0, "tim", InstantEngine(5))
+        val tom = Car(1, "tom", InstantEngine(1))
+        val tam = Car(2, "tam", InstantEngine(3))
+        val tem = Car(3, "tem", InstantEngine(5))
+
+        val racing = Racing.new(listOf(tim, tom, tam, tem), 5)
+
+        racing.start()
+
+        val winnersIds = racing.winners().map { it.id }
+
+        assertEquals(2, winnersIds.size)
+        assertTrue(winnersIds.contains(tim.id))
+        assertTrue(winnersIds.contains(tem.id))
+    }
+}
