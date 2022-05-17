7c2485e619a43307ff959c4ace078ad2b3d6dde0
kwanguuuu <kwangyu326@gmail.com>
2022-05-18 00:00:17 +0900

    [step5] refactor: MoveResults의 farthestDistanceCarNames 삭제, 테스트 코드 삭제

--- src/main/kotlin/racingcar/dto/MoveResults.kt
+++ src/main/kotlin/racingcar/dto/MoveResults.kt
@@ -1,15 +1,8 @@
 package racingcar.dto
 
-class MoveResults(
+data class MoveResults(
     val result: List<MoveResult>
 ) {
-    val farthestDistanceCarNames: List<String>
-        get() {
-            val farthestDistance = result.maxOf { it.moveDistance }
-            return result
-                .filter { it.moveDistance == farthestDistance }
-                .map { it.carName }
-        }
 
     data class MoveResult(
         val carName: String,
