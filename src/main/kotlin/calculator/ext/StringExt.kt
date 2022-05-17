e9e540059000f44b0b3b28249440bec2e9fc16f7
yohan <cd80@kakao.com>
2022-05-17 21:55:29 +0900

    refactor: 불필요한 키워드 제거

--- src/main/kotlin/calculator/ext/StringExt.kt
+++ src/main/kotlin/calculator/ext/StringExt.kt
@@ -2,4 +2,4 @@ package calculator.ext
 
 import calculator.study.Utils
 
-fun String.isNumeric(): Boolean = this.matches(Utils.NUMERIC_REGX)
+fun String.isNumeric(): Boolean = matches(Utils.NUMERIC_REGX)
