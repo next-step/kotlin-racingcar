9c83cc216ede8697ef304b7f016ce7d932508620
Rok93 <kr.kim@mfort.co.kr>
2022-05-17 19:51:56 +0900

    refactor: 코드컨벤션에 맞춰 초기화 블록, 부생성자 위치 수정

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -8,11 +8,11 @@ class Car(
     val name: Name,
     val position: Int = INITIAL_POSITION,
 ) {
-    constructor(name: String) : this(Name(name))
-
     init {
         check(position >= INITIAL_POSITION) { "자동차의 처음 position은 0이상의 숫자만 가능합니다. position = $position" }
     }
+
+    constructor(name: String) : this(Name(name))
 
     fun moveOrNot(condition: Int): Car {
         if (condition >= MOVEMENT_CONDITION_STANDARD) {
