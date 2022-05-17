12d9f7e8306d3499682deb5099cb74eb8e16769b
Molly <dngwoodo@gmail.com>
2022-05-17 23:46:48 +0900

    파라미터 이름을 movingPoint -> condition 으로 변경

--- src/main/kotlin/racingCar/Car.kt
+++ src/main/kotlin/racingCar/Car.kt
@@ -3,7 +3,7 @@ package racingCar
 class Car() {
     var position: Int = 0
 
-    fun move(movingPoint: Int) {
-        position = if (movingPoint >= 4) position + 1 else position
+    fun move(condition: Int) {
+        position = if (condition >= 4) position + 1 else position
     }
 }
