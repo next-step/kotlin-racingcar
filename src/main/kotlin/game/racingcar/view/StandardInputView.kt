9d1e656215862d76dbda0e1665796754509fabf5
pplenty <newtype26@naver.com>
2022-05-18 01:24:28 +0900

    refactor: 피드백 반영 - lazy 패턴 적용

--- src/main/kotlin/game/racingcar/view/StandardInputView.kt
+++ src/main/kotlin/game/racingcar/view/StandardInputView.kt
@@ -3,24 +3,21 @@ package game.racingcar.view
 import game.racingcar.util.requireOrThrow
 
 class StandardInputView : InputView {
-    private lateinit var carNames: List<String>
-    private var numberOfLap: Int = 0
-
-    override fun init() {
+    private val carNames: List<String> by lazy {
         println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
-        carNames = readln().split(",")
+        readln().split(",")
             .requireOrThrow("자동차 이름은 빈 값일 수 없습니다.") { it.isNotBlank() }
             .map { it.take(NAME_MAX_LENGTH) }
-
-        println("시도할 횟수는 몇 회인가요?")
-        numberOfLap = readln().toInt()
     }
 
-    override fun numberOfLap(): Int {
-        return numberOfLap
+    private val numberOfLap: Int by lazy {
+        println("시도할 횟수는 몇 회인가요?")
+        readln().toInt()
     }
 
     override fun carNames(): List<String> = carNames
+
+    override fun numberOfLap(): Int = numberOfLap
 
     companion object {
         const val NAME_MAX_LENGTH = 5
