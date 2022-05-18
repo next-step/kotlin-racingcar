962415cea7872e7647e586cfa42b3c758294dbc8
hoori <freedom09@inflab.com>
2022-05-19 00:42:38 +0900

    GameBoard 를 통한 실행으로 변경

--- src/main/kotlin/racingcar/main.kt
+++ src/main/kotlin/racingcar/main.kt
@@ -1,12 +1,6 @@
 package racingcar
 
-import racingcar.race.Race
-import racingcar.view.InputView
-import racingcar.view.ResultView
-
 fun main() {
-    val inputView = InputView()
-    val race = Race(inputView.getCarCount(), inputView.getMoveCount())
-    val resultView = ResultView(race)
-    resultView.show()
+    val gameBoard = GameBoard()
+    gameBoard.showResult()
 }
