31a07f7f15faf945abb1ee3775b8a327cec1586b
ujusy <lsyeon1997@ajou.ac.kr>
2022-05-18 21:29:02 +0900

    refactor: 랜덤 인터페이스 추가에 따라 테스트 수정 및 run 테스트 케이스 추가

--- src/test/kotlin/carRacing/GameTest.kt
+++ src/test/kotlin/carRacing/GameTest.kt
@@ -1,9 +1,11 @@
 package carRacing
 
-import carRacing.controller.Game
-import carRacing.controller.Game.Companion.INVALID_CAR_LIST
-import carRacing.controller.Game.Companion.INVALID_NUM_MOVEMENT
 import carRacing.model.Car
+import carRacing.model.Game
+import carRacing.model.Game.Companion.INVALID_CAR_LIST
+import carRacing.model.Game.Companion.INVALID_NUM_MOVEMENT
+import carRacing.util.Random
+import carRacing.util.Random.TestRandomGenerator.Companion.TEST_DEFAULT_VALUE
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
@@ -17,7 +19,7 @@ class GameTest : FreeSpec({
             val carNameList: List<String> = listOf()
             val numMovement = 5
             val exception = shouldThrow<IllegalArgumentException> {
-                Game(carNameList, numMovement)
+                Game(carNameList, numMovement, Random.TestRandomGenerator(TEST_DEFAULT_VALUE))
             }
             exception.message shouldBe INVALID_CAR_LIST
         }
@@ -26,7 +28,7 @@ class GameTest : FreeSpec({
             val carNameList = listOf("uju")
             val numMovement = 0
             val exception = shouldThrow<IllegalArgumentException> {
-                Game(carNameList, numMovement)
+                Game(carNameList, numMovement, Random.TestRandomGenerator(TEST_DEFAULT_VALUE))
             }
             exception.message shouldBe INVALID_NUM_MOVEMENT
         }
@@ -37,7 +39,7 @@ class GameTest : FreeSpec({
         "랜덤 값이 4 이상일 경우 자동차의 포지션 +1이 되어야한다." {
             val carName = "test"
             val car = Car(carName)
-            val game = Game(listOf(carName), 1)
+            val game = Game(listOf(carName), 1, Random.TestRandomGenerator(TEST_DEFAULT_VALUE))
 
             game.move(car, 5)
 
@@ -47,7 +49,7 @@ class GameTest : FreeSpec({
         "랜덤 값이 4 미만일 경우 자동차는 움직이지 않아야한다." {
             val carName = "test"
             val car = Car(carName)
-            val game = Game(listOf(carName), 1)
+            val game = Game(listOf(carName), 1, Random.TestRandomGenerator(2))
 
             game.move(car, 1)
 
@@ -57,19 +59,35 @@ class GameTest : FreeSpec({
 
     "run" - {
 
-        "오류 없이 실행되어야한다." {
+        "모든 자동차가 움직인 기록이어야한다." {
             // given
             val carNameList = listOf("uju", "test")
             val numMovement = 5
-            val game = Game(carNameList, numMovement)
+            val game = Game(carNameList, numMovement, Random.TestRandomGenerator(TEST_DEFAULT_VALUE))
 
             // when
             game.run()
 
             // then
             game.recordList.size shouldBe numMovement
-            game.recordList.forEach {
-                it.positions.size shouldBe carNameList.size
+            game.recordList[0].positions.forEach {
+                it shouldBe 1
+            }
+        }
+
+        "모든 자동차가 움직이지 않은 기록이어야한다." {
+            // given
+            val carNameList = listOf("uju", "test")
+            val numMovement = 5
+            val game = Game(carNameList, numMovement, Random.TestRandomGenerator(1))
+
+            // when
+            game.run()
+
+            // then
+            game.recordList.size shouldBe numMovement
+            game.recordList[0].positions.forEach {
+                it shouldBe 0
             }
         }
     }
@@ -79,7 +97,7 @@ class GameTest : FreeSpec({
         "우승한 자동차 리스트를 반환한다." {
             val carNameList = listOf("uju", "test")
             val numMovement = 3
-            val game = Game(carNameList, numMovement)
+            val game = Game(carNameList, numMovement, Random.TestRandomGenerator(TEST_DEFAULT_VALUE))
             game.run()
 
             val carList = game.winner()
