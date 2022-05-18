0d307674315bbe95cbdc575395d55327682ccdba
ujusy <lsyeon1997@ajou.ac.kr>
2022-05-18 21:29:23 +0900

    refactor: 랜덤 인터페이스 인스턴스 테스트 추가

--- src/test/kotlin/carRacing/RandomGeneratorTest.kt
+++ src/test/kotlin/carRacing/RandomGeneratorTest.kt
@@ -1,8 +1,10 @@
 package carRacing
 
-import carRacing.util.RandomGenerator
+import carRacing.util.Random
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.datatest.withData
 import io.kotest.matchers.ints.shouldBeBetween
+import io.kotest.matchers.shouldBe
 
 class RandomGeneratorTest : FreeSpec({
 
@@ -10,7 +12,17 @@ class RandomGeneratorTest : FreeSpec({
 
         "0 ~ 9 사이의 랜던함 정수 값이 나와야한다." {
             (1..10000).forEach { _ ->
-                RandomGenerator.value().shouldBeBetween(0, 9)
+                Random.RandomGenerator().value().shouldBeBetween(0, 9)
+            }
+        }
+
+        "테스트하려는 값이 나와야한다." {
+            withData(
+                1 to 1,
+                3 to 3,
+                5 to 5
+            ) { (input, output) ->
+                Random.TestRandomGenerator(input).value() shouldBe output
             }
         }
     }
