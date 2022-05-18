1c302936c2991fce394475b6170834228028d777
austen.lee <austen.lee@kakaocorp.com>
2022-05-19 00:33:42 +0900

    step5 test : RacingGame 테스트 간소화. 세부 로직 테스트는 각각의 클래스에서!

--- src/test/kotlin/racinggame/domain/RacingGameTest.kt
+++ src/test/kotlin/racinggame/domain/RacingGameTest.kt
@@ -2,29 +2,25 @@ package racinggame.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertAll
 import racinggame.domain.car.Car
 import racinggame.domain.engine.Engine
 import racinggame.domain.model.RacingGameInput
 
 class RacingGameTest {
     @Test
-    fun `RacingGame을 시작하면 자동차들의 각 단계별 전진 정도를 모두 구할 수 있다`() {
+    fun `RacingGame을 시작하면 시도 횟수만큼의 stepResult를 얻을 수 있다`() {
+        // given
         val names = listOf("one", "two", "three")
         val racingGameInput = RacingGameInput(names, 5)
         val engine = Engine { 1 }
         val racingGame = RacingGame.of(racingGameInput) { name ->
             Car(name, engine)
         }
+
+        // when
         val result = racingGame.start()
-        var proceedLevel = 1
-        result.stepResults.forEach { racingStepResult ->
-            assertAll(
-                racingStepResult.racingGameStatusList.map { individualRacingStepStatus ->
-                    { assertThat(individualRacingStepStatus.proceedLevel).isEqualTo(proceedLevel) }
-                }
-            )
-            proceedLevel++
-        }
+
+        // then
+        assertThat(result.stepResults.size).isEqualTo(5)
     }
 }
