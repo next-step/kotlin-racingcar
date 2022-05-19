c11ee0680cd5ea23edb594f016ae91bfff7bdc13
hoori <freedom09@inflab.com>
2022-05-19 19:08:28 +0900

    테스트 코드 리팩토링

--- src/test/kotlin/racingcar/car/randomListTest.kt
+++ src/test/kotlin/racingcar/car/randomListTest.kt
@@ -6,19 +6,17 @@ import org.junit.jupiter.api.Test
 class RandomListTest {
     @Test
     fun `주어진 size 만큼 랜덤리스트가 생성된다`() {
-        val range = -100..100
         for (size in 0..200) {
-            Assertions.assertThat(randomListBy(range, size).size).isEqualTo(size)
+            Assertions.assertThat(randomListBy(-100..100, size).size).isEqualTo(size)
         }
     }
 
     @Test
     fun `랜덤리스트의 모든 엘리먼트는 range 에 포함된 값이다`() {
         for (min in -10..10)
-            for (max in min + 10..20) {
+            for (max in min + 10..min + 20) {
                 val range = min..max
-                val size = 100
-                randomListBy(range, size).forEach { Assertions.assertThat(range.contains(it)).isTrue }
+                randomListBy(range, 100).forEach { Assertions.assertThat(range.contains(it)).isTrue }
             }
     }
 }
