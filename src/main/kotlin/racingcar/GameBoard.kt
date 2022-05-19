749224ac31ac458a9dabff73106038c8f302943d
hoori <freedom09@inflab.com>
2022-05-19 10:48:48 +0900

    gameBoard.showResult 함수 실행 전 resultView 를 구성하여 중복 호출시 resultView 를 재사용 하도록 수정

--- src/main/kotlin/racingcar/GameBoard.kt
+++ src/main/kotlin/racingcar/GameBoard.kt
@@ -7,8 +7,7 @@ import racingcar.view.ResultView
 class GameBoard(minCarCount: Int = 1, minMoveCount: Int = 1) {
     private val inputView = InputView(minCarCount, minMoveCount)
     private val race = Race(inputView.carCount, inputView.moveCount)
+    private val resultView = ResultView(race)
 
-    fun showResult() {
-        ResultView(race).showResult()
-    }
+    fun showResult() = resultView.showResult()
 }
