41471aa03e68d27c448b9d36ecdf93a286fe8635
dongchul <sorkehdcjf@gmail.com>
2022-05-20 07:20:28 +0900

    Step5 - 리팩터링 리뷰요청 드립니다. (#627)
    * feat: 자동차 이름 5글자 유효성 검사 추가
    
    * test : 이름 5글자 제한 테스트

--- src/main/kotlin/car/domain/vo/Name.kt
+++ src/main/kotlin/car/domain/vo/Name.kt
@@ -4,5 +4,10 @@ package car.domain.vo
 value class Name(val value: String) {
     init {
         require(value.isNotEmpty()) { "이름은 비어 있을수 없습니다." }
+        require(value.length in NAME_RANGE) { "이름은 5자를 초과할수 없습니다." }
+    }
+
+    companion object {
+        private val NAME_RANGE = 1..5
     }
 }
