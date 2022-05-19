8d7d80f40cc1ce1a9f0dcdbdb0a681a44ab64963
keira <keira.j@kakaocorp.com>
2022-05-20 00:23:56 +0900

    feat: test코드 패키지 이동, 수정
    - car 도메인 테스트 추가

--- src/test/kotlin/racing/domain/RacingGameTest.kt
+++ src/test/kotlin/racing/domain/RacingGameTest.kt
@@ -8,7 +8,7 @@ import racing.domain.RacingGame.getWinner
 class RacingGameTest {
 
     @Test
-    fun `random하게 선택된 숫자에 따라 적절히 move 혹은 stop 하는가`() {
+    fun `자동차 별로 random하게 선택된 숫자에 따라 적절히 움직여야 한다`() {
         val cars = listOf(Car("자동차1"), Car("자동차2"), Car("자동차3"))
         val numberOfTries = 3
         var strategy = object : MovingStrategy {
