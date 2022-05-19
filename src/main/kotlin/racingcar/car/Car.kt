f87092871730c906a34432ed2a3c079cd257e475
hoori <freedom09@inflab.com>
2022-05-19 19:11:11 +0900

    항상 동일한 프로퍼티를 동반객체 프로퍼티로 변경

--- src/main/kotlin/racingcar/car/Car.kt
+++ src/main/kotlin/racingcar/car/Car.kt
@@ -3,7 +3,6 @@ package racingcar.car
 class Car(moveCount: Int) {
     private val drivingForces = randomListBy(drivingForcePool, moveCount)
     private val drivingPoints = drivingForces.map { drivingPointBy(it) }
-    private val initPosition = 0
     private val initPositions = arrayListOf(initPosition)
     private val positionsByStep = drivingPoints.fold(initPositions) { positions, point ->
         positions.add(positions.last() + point)
@@ -14,5 +13,6 @@ class Car(moveCount: Int) {
 
     companion object {
         private val drivingForcePool = (0..9)
+        private const val initPosition = 0
     }
 }
