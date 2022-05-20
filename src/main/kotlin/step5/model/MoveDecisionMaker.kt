8b33a2315c4b5e037daf2c64decb88084d39cf68
jinwoo-k <br2k@daum.net>
2022-05-21 03:11:58 +0900

    refact: step5 - (step3, 5) 코드리뷰 수정사항 반영
    * companion object 클래스 하단 이동
    * 내부에서만 사용되는 생성자와 프로퍼티는 private으로 변경

--- src/main/kotlin/step5/model/MoveDecisionMaker.kt
+++ src/main/kotlin/step5/model/MoveDecisionMaker.kt
@@ -3,12 +3,13 @@ package step5.model
 import kotlin.random.Random
 
 class MoveDecisionMaker(private val random: Random = Random) {
-    companion object {
-        const val MAX_RANDOM_VALUE = 9
-        const val MOVE_DECISION_CRITERIA = 4
-    }
 
     fun getRandomDigit(): Int = random.nextInt(MAX_RANDOM_VALUE + 1)
 
     fun canMove(digit: Int = getRandomDigit()): Boolean = digit > MOVE_DECISION_CRITERIA
+
+    companion object {
+        private const val MAX_RANDOM_VALUE = 9
+        private const val MOVE_DECISION_CRITERIA = 4
+    }
 }
