d96d4135a7ccc324864e3801222a99ad4771b31a
Jake Son <sonjeabin@gmail.com>
2022-05-21 09:37:11 +0900

    refactor: 매직넘버 상수화

--- src/main/kotlin/racingcar/ui/GameInput.kt
+++ src/main/kotlin/racingcar/ui/GameInput.kt
@@ -9,7 +9,7 @@ data class GameInput(private val nameInput: String, val round: Int) {
     fun toRacingCars(): List<Car> {
         require(round > 0) { "round 는 1 이상이어야 합니다" }
 
-        val names = nameInput.split(',').filter { it.isNotBlank() }
+        val names = nameInput.split(CAR_NAME_DELIMITER).filter { it.isNotBlank() }
 
         require(names.isNotEmpty()) { "자동차는 1대 이상이어야 합니다" }
         require(names.all { it.length <= MAXIMUM_CAR_NAME_LENGTH }) { "자동차 이름은 ${MAXIMUM_CAR_NAME_LENGTH}자를 넘을 수 없습니다" }
@@ -19,5 +19,6 @@ data class GameInput(private val nameInput: String, val round: Int) {
 
     companion object {
         private const val MAXIMUM_CAR_NAME_LENGTH = 5
+        private const val CAR_NAME_DELIMITER = ','
     }
 }
