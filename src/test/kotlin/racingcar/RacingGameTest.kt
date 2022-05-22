fc51ac06557eefc9b3110eb842cdeff696f2278b
baekseungyeol <beck33333@naver.com>
2022-05-22 19:45:53 +0900

    refactor: RacingGame 테스트 코드 개선

--- src/test/kotlin/racingcar/RacingGameTest.kt
+++ src/test/kotlin/racingcar/RacingGameTest.kt
@@ -2,54 +2,41 @@ package racingcar
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 import racingcar.model.Car
 import racingcar.model.CarName
 import racingcar.model.Cars
 import racingcar.model.MovingStrategy
-import racingcar.model.RacingDataPerTry
 import racingcar.model.RacingGame
-import racingcar.model.RacingGameResult
 import racingcar.model.TryNumber
 
 internal class RacingGameTest {
 
+    private val movingStrategy = MovingStrategy { true }
+
     @Test
     fun `자동차들과 수행할 횟수를 받아서 레이싱 게임을 진행한 후 결과를 반환한다`() {
+        // given
         val tryNumber = TryNumber(5)
-        val movingStrategy = MovingStrategy { true }
-        val cars = Cars(
-            listOf(
-                Car(CarName("sam"), 5, movingStrategy),
-                Car(CarName("kaka"), 2, movingStrategy),
-                Car(CarName("luci"), 4, movingStrategy),
-                Car(CarName("andy"), 5, movingStrategy)
-            )
-        )
-
-        val expectedResult = RacingGameResult(
-            results = List(tryNumber.number) { currentTryNumber ->
-                RacingDataPerTry(
-                    cars = Cars(
-                        listOf(
-                            Car(CarName("sam"), 5 + currentTryNumber + 1, movingStrategy),
-                            Car(CarName("kaka"), 2 + currentTryNumber + 1, movingStrategy),
-                            Car(CarName("luci"), 4 + currentTryNumber + 1, movingStrategy),
-                            Car(CarName("andy"), 5 + currentTryNumber + 1, movingStrategy)
-                        )
-                    ),
-                )
-            },
-            winners = Cars(
-                listOf(
-                    Car(CarName("sam"), 5 + tryNumber.number, movingStrategy),
-                    Car(CarName("andy"), 5 + tryNumber.number, movingStrategy)
-                )
-            ),
-        )
-
+        val cars = Cars("sam" to 5, "kaka" to 2, "luci" to 4, "andy" to 5)
         val racingGame = RacingGame(tryNumber = tryNumber, cars = cars)
+
+        // when
         val result = racingGame.startGame()
 
-        assertThat(result).isEqualTo(expectedResult)
+        // then : 모든 차량이 5칸씩 전진 함
+        val actualCars = result.results.last().cars
+        val actualWinnerCars = result.winners
+        val expectedCars = Cars("sam" to 10, "kaka" to 7, "luci" to 9, "andy" to 10)
+        val expectedWinnerCars = Cars("sam" to 10, "andy" to 10)
+
+        assertAll(
+            { assertThat(actualCars).isEqualTo(expectedCars) },
+            { assertThat(actualWinnerCars).isEqualTo(expectedWinnerCars) }
+        )
     }
+
+    private fun Cars(vararg nameToPositions: Pair<String, Int>): Cars = nameToPositions
+        .map { (name, position) -> Car(CarName(name), position, movingStrategy) }
+        .let(::Cars)
 }
