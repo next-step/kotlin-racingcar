9e8d87bba9d4abe0159eb100a9d0f8ab965dab91
Gomding <backchida@gmail.com>
2022-05-22 13:20:45 +0900

    refactor(racingcar) : RaceHost play 함수에서 가독성을 위한 변수 할당

--- src/main/kotlin/racingcar/RaceHost.kt
+++ src/main/kotlin/racingcar/RaceHost.kt
@@ -5,9 +5,10 @@ object RaceHost {
     fun play(rounds: Rounds, numberOfParticipants: NumberOfParticipants): RacingResults {
         var cars: Cars = Cars.initCars(numberOfParticipants.value)
         var racingResults = RacingResults()
-        repeat(rounds.value) {
-            cars = cars.moveForwardAll(RandomGasStation.refuels(numberOfParticipants.value))
-            racingResults = racingResults.add(RacingResult.of(round = it + 1, cars = cars))
+        (1..rounds.value).forEach() { round ->
+            val refuels = RandomGasStation.refuels(numberOfParticipants.value)
+            cars = cars.moveForwardAll(refuels = refuels)
+            racingResults = racingResults.add(RacingResult.of(round = round, cars = cars))
         }
         return racingResults
     }
