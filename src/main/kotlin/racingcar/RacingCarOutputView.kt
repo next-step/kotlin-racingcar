8813b0609e13e4bfd6c09f81fb9626a855cf8142
Gomding <backchida@gmail.com>
2022-05-22 12:59:07 +0900

    refactor(racingcar) : Output에서 필요없이 같은 인덱스의 RacingResult를 두번 가져오는 로직을 제거

--- src/main/kotlin/racingcar/RacingCarOutputView.kt
+++ src/main/kotlin/racingcar/RacingCarOutputView.kt
@@ -4,18 +4,14 @@ object RacingCarOutputView {
 
     fun output(racingResults : RacingResults) {
         for(i in racingResults.value.indices) {
-            outputRoundInfo(racingResults.value[i].round)
             outputRacingResult(racingResults.value[i])
             println()
         }
         println("====== 경주 종료 ======")
     }
 
-    private fun outputRoundInfo(round: Int) {
-        println("라운드 - [$round] 결과")
-    }
-
     private fun outputRacingResult(racingResult: RacingResult) {
+        println("라운드 - [${racingResult.round}] 결과")
         racingResult.distancesMoved.forEach() {
             println(MOVED_SYMBOL.repeat(it))
         }
