8a3ba02f8e3cd1a2105066bb69119dff302f6b29
Gomding <backchida@gmail.com>
2022-05-22 13:46:47 +0900

    refactor(racingcar) : InputView에서 자동차 이름들을 입력받도록 수정

--- src/main/kotlin/racingcar/RacingCarInputView.kt
+++ src/main/kotlin/racingcar/RacingCarInputView.kt
@@ -3,26 +3,23 @@ package racingcar
 object RacingCarInputView {
     fun input(): RacingCarRequest {
         print("경주에 참여할 자동차 대수는 몇 대인가요? ")
-        val numberOfParticipantsInput = readLine()
+        val carNamesInput = readLine()
         print("경주는 몇 라운드를 진행할 것인가요? ")
         val roundsInput = readLine()
-        return RacingCarRequest.of(numberOfParticipantsInput, roundsInput)
+        return RacingCarRequest.of(carNames = carNamesInput, rounds = roundsInput)
     }
 }
 
-class RacingCarRequest(val numberOfParticipants: Int, val rounds: Int) {
+class RacingCarRequest(val carNames: String, val rounds: Int) {
     companion object {
-        fun of(numberOfParticipants: String?, rounds: String?): RacingCarRequest {
-            validateNumberOfParticipants(numberOfParticipants)
-            validateRounds(rounds)
-            return RacingCarRequest(numberOfParticipants!!.toInt(), rounds!!.toInt())
+        fun of(carNames: String?, rounds: String?): RacingCarRequest {
+            validateCarNames(carNamesInput = carNames)
+            validateRounds(roundsInput = rounds)
+            return RacingCarRequest(carNames!!, rounds!!.toInt())
         }
 
-        private fun validateNumberOfParticipants(numberOfParticipantsInput: String?) {
-            require(!numberOfParticipantsInput.isNullOrBlank()) { "자동차 대수 입력값은 빈 값이거나, 공백일 수 없습니다." }
-            val numberOfParticipants = numberOfParticipantsInput.toIntOrNull()
-                ?: throw IllegalArgumentException("자동차 대수 입력값은 정수여야 합니다.")
-            require(numberOfParticipants > 0) { "자동차 대수 입력값은 0보다 큰 값이어야 합니다." }
+        private fun validateCarNames(carNamesInput: String?) {
+            require(!carNamesInput.isNullOrBlank()) { "자동차 이름은 입력값은 빈 값이거나, 공백일 수 없습니다." }
         }
 
         private fun validateRounds(roundsInput: String?) {
