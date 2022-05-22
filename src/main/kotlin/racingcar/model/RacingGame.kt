ec0703dfa0f9b89cfb3b52c466fd5ac0fb79fc79
baekseungyeol <beck33333@naver.com>
2022-05-22 18:06:02 +0900

    refactor: 가독성을 위해 함수 호출 순서 변경

--- src/main/kotlin/racingcar/model/RacingGame.kt
+++ src/main/kotlin/racingcar/model/RacingGame.kt
@@ -4,11 +4,6 @@ class RacingGame(
     val tryNumber: TryNumber,
     val cars: Cars
 ) {
-
-    private fun race(cars: Cars): Cars {
-        return cars.moveForwardCars()
-    }
-
     fun startGame(): RacingGameResult {
         val racingGameResult = mutableListOf<RacingDataPerTry>()
         val finishedCars = startGame(cars, tryNumber, racingGameResult)
@@ -18,12 +13,20 @@ class RacingGame(
         )
     }
 
-    private tailrec fun startGame(cars: Cars, tryNumber: TryNumber, racingGameResult: MutableList<RacingDataPerTry>): Cars {
+    private tailrec fun startGame(
+        cars: Cars,
+        tryNumber: TryNumber,
+        racingGameResult: MutableList<RacingDataPerTry>
+    ): Cars {
         if (tryNumber.isZero()) {
             return cars
         }
         val movedCars = race(cars)
         racingGameResult.add(RacingDataPerTry(cars = movedCars))
         return startGame(movedCars, tryNumber.minus(), racingGameResult)
+    }
+
+    private fun race(cars: Cars): Cars {
+        return cars.moveForwardCars()
     }
 }
