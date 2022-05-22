bd9402de3c40ab5f7cc6b13a19af25491d4663eb
glennsm <lovinhut@gmail.com>
2022-05-22 21:44:06 +0900

    refactor: pr내용 반영

--- src/main/kotlin/racingcar/domain/RacingGame.kt
+++ src/main/kotlin/racingcar/domain/RacingGame.kt
@@ -1,26 +1,13 @@
 package racingcar.domain
 
 class RacingGame(carCnt: Int, val tryCnt: Int) {
-    var cars = mutableListOf<Car>()
-
-    init {
-        repeat(carCnt) { cars.add(Car()) }
-    }
+    var cars = List(carCnt) { Car() }
 
     fun execute(method: () -> Int): List<CarHistory> {
-        val histories: MutableList<CarHistory> = mutableListOf()
-        cars.forEach {
-            val carHistory = it.move(method())
-            histories.add(carHistory)
-        }
-        return histories.toList()
+        return cars.map { it.move(method()) }
     }
 
     fun play(method: () -> Int): List<List<CarHistory>> {
-        val recordList: MutableList<List<CarHistory>> = mutableListOf()
-        repeat(tryCnt) {
-            recordList.add(execute(method))
-        }
-        return recordList.toList()
+        return List(tryCnt) { execute(method) }
     }
 }
