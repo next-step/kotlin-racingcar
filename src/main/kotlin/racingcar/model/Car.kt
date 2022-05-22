e4d51fec08f0de78d31efaa3bad537aec820216c
baekseungyeol <beck33333@naver.com>
2022-05-22 18:05:35 +0900

    refactor: 불필요 equalsAndHashcode 재정의 제거

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -17,25 +17,6 @@ data class Car(
 
     private fun stop() = copy()
 
-    // movingStrategy 제외
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Car
-
-        if (name != other.name) return false
-        if (position != other.position) return false
-
-        return true
-    }
-
-    // movingStrategy 제외
-    override fun hashCode(): Int {
-        var result = name.hashCode()
-        result = 31 * result + position
-        return result
-    }
     companion object {
         internal const val DEFAULT_POSITION = 0
         internal const val MAX_BOUND = 10
