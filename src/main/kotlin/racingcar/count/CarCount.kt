63db496e16e5bea0b56bdece177bf4f4a4bad02f
hoori <freedom09@inflab.com>
2022-05-23 23:37:38 +0900

    자동차 수와 이동횟수에 대한 최소값 조건 추가

--- src/main/kotlin/racingcar/count/CarCount.kt
+++ src/main/kotlin/racingcar/count/CarCount.kt
@@ -2,6 +2,7 @@ package racingcar.count
 
 class CarCount(val value: Int, minCount: Int = 1) {
     init {
+        require(minCount >= 1) { "최소 자동차 수는 1 이상이어야 합니다." }
         require(value >= minCount) { "자동차 수는 최소 $minCount 이상이어야 합니다." }
     }
 }
--- src/main/kotlin/racingcar/count/MoveCount.kt
+++ src/main/kotlin/racingcar/count/MoveCount.kt
@@ -2,6 +2,7 @@ package racingcar.count
 
 class MoveCount(val value: Int, minCount: Int = 1) {
     init {
+        require(minCount >= 1) { "최소이동횟수는 1 이상이어야 합니다" }
         require(value >= minCount) { "이동횟수는 최소 $minCount 이상이어야 합니다" }
     }
 }
