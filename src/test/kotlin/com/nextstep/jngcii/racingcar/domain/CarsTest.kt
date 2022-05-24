a40b16c8ad3030dc1d57b96ee1dc1f6d67c9f813
jngcii <concotree@gmail.com>
2022-05-24 14:41:18 +0900

    refactor : MethodSource를 사용해 Cars ParameterizedTest 수정

--- src/test/kotlin/com/nextstep/jngcii/racingcar/domain/CarsTest.kt
+++ src/test/kotlin/com/nextstep/jngcii/racingcar/domain/CarsTest.kt
@@ -2,31 +2,41 @@ package com.nextstep.jngcii.racingcar.domain
 
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 internal class CarsTest {
 
     @ParameterizedTest
-    @CsvSource(
-        "A:10|B:20|C:30, C",
-        "A:30|B:20|C:30, A:C",
-        "A:10|B:10|C:10, A:B:C"
-    )
-    fun `winners 확인 테스트`(carStrings: String, expectedStrings: String) {
-        val carList = carStrings.split(PIPE_DELIMITER).map {
-            val (name, distance) = it.split(COLON_DELIMITER)
-            return@map PrefixCar(name = name, distance = distance.toInt())
-        }
-        val cars = Cars(cars = carList)
-
-        val expected = expectedStrings.split(COLON_DELIMITER)
+    @MethodSource("arguments")
+    fun `winners 확인 테스트`(carA: Car, carB: Car, carC: Car, expected: List<String>) {
+        val cars = Cars(cars = listOf(carA, carB, carC))
 
         Assertions.assertThat(cars.winners.map { it.name }).isEqualTo(expected)
     }
 
     companion object {
-        private const val PIPE_DELIMITER = "|"
-        private const val COLON_DELIMITER = ":"
+        @JvmStatic
+        fun arguments() = listOf(
+            Arguments.of(
+                PrefixCar(name = "A", distance = 10),
+                PrefixCar(name = "B", distance = 20),
+                PrefixCar(name = "C", distance = 30),
+                listOf("C")
+            ),
+            Arguments.of(
+                PrefixCar(name = "A", distance = 30),
+                PrefixCar(name = "B", distance = 20),
+                PrefixCar(name = "C", distance = 30),
+                listOf("A", "C")
+            ),
+            Arguments.of(
+                PrefixCar(name = "A", distance = 10),
+                PrefixCar(name = "B", distance = 10),
+                PrefixCar(name = "C", distance = 10),
+                listOf("A", "B", "C")
+            ),
+        )
     }
 
     private class PrefixCar(
