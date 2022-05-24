cc87aa078f6919b9d216b9d316050df1d5be317f
Gomding <backchida@gmail.com>
2022-05-25 02:29:04 +0900

    refactor(racingcar) : CarNamesTest에 get(), size()에 대한 테스트 추가

--- src/test/kotlin/racingcar/CarNamesTest.kt
+++ src/test/kotlin/racingcar/CarNamesTest.kt
@@ -1,22 +1,29 @@
 package racingcar
 
-import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 internal class CarNamesTest : FunSpec({
 
-    context("자동차 이름들의 문자열을 입력받아 객체를 생성할 때") {
-        test("입력받은 문자열이 빈 값이면, 예외가 발생한다.") {
-            // when then
-            val exception = shouldThrow<IllegalArgumentException> { CarNames.of("") }
-            exception.message shouldBe "차 이름들의 값은 빈 값이거나 공백일 수 없습니다. carNames: "
-        }
+    test("자동차 이름들을 2개 가지고 있는 CarNames 객체에 size() 함수를 호출하면, 2를 반환한다.") {
+        // given
+        val carNames = CarNames(listOf(CarName.of("11"), CarName.of("22")))
 
-        test("입력받은 문자열에 공백이 존재하면, 예외가 발생한다.") {
-            // when then
-            val exception = shouldThrow<IllegalArgumentException> { CarNames.of("  ") }
-            exception.message shouldBe "차 이름들의 값은 빈 값이거나 공백일 수 없습니다. carNames:   "
-        }
+        // when
+        val sut = carNames.size()
+
+        // then
+        sut shouldBe 2
+    }
+
+    test("자동자 이름들 2개를 가지고 있는 carNames 객체에 carNames[0]과 같이 사용해서, 첫 번째 요소를 가져올 수 있다.") {
+        // given
+        val carNames = CarNames(listOf(CarName.of("11"), CarName.of("22")))
+
+        // when
+        val sut = carNames[0]
+
+        // then
+        sut shouldBe CarName.of("11")
     }
 })
