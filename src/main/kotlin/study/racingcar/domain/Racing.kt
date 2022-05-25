ec8efb92a6d02f1b54caa1946a33cc68b0b19a78
salphaon <salphaon@gmail.com>
2022-05-26 00:26:04 +0900

    불필요한 변수 제거

--- src/main/kotlin/study/racingcar/domain/Racing.kt
+++ src/main/kotlin/study/racingcar/domain/Racing.kt
@@ -2,14 +2,9 @@ package study.racingcar.domain
 
 class Racing(internal val cars: List<Car>, val totalRound: Int) {
 
-    var round = 0
-        private set
-
-    constructor(numOfCar: Int, round: Int) : this((1..numOfCar).map { Car() }, round)
+    constructor(numOfCar: Int, totalRound: Int) : this((1..numOfCar).map { Car() }, totalRound)
 
     fun start() {
-        if (round >= totalRound) return
-
         cars.forEach {
             it.move((1..9).random())
         }
