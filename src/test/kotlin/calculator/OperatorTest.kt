ca657259aedc9dc089145182276745169ae66df1
skid901 <tlrudejr1993@gmail.com>
2022-05-26 01:19:35 +0900

    test: verify Operator enum class

--- src/test/kotlin/calculator/OperatorTest.kt
+++ src/test/kotlin/calculator/OperatorTest.kt
@@ -1,5 +1,41 @@
 package calculator
 
-import org.junit.jupiter.api.Assertions.*
+import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.assertThrows
 
-internal class OperatorTest
+internal class OperatorTest : AnnotationSpec() {
+
+    @Test
+    fun `덧셈`() {
+        Operator.operate("+", 2.0, 3.0) shouldBe 5
+    }
+
+    @Test
+    fun `뺄셈`() {
+        Operator.operate("-", 2.0, 3.0) shouldBe -1
+    }
+
+    @Test
+    fun `곱셈`() {
+        Operator.operate("*", 2.0, 3.0) shouldBe 6
+    }
+
+    @Test
+    fun `나눗셈`() {
+        Operator.operate("/", 5.0, 2.0) shouldBe 2.5
+    }
+
+    @Test
+    fun `나눗셈 시 우항 피연산자가 0 일 경우 예외 처리`() {
+        assertThrows<IllegalStateException> { Operator.operate("/", 5.0, 0.0) }
+            .message shouldBe "0 으로 나눌 수 없습니다."
+    }
+
+    @Test
+    fun `연산 불가능한 연산자가 입력된 경우 예외 처리`() {
+        val nonArithmeticOperator = "//"
+        assertThrows<IllegalArgumentException> { Operator.operate(nonArithmeticOperator, 5.0, 2.0) }
+            .message shouldBe "연산 불가능한 연산자가 입력되었습니다.: $nonArithmeticOperator"
+    }
+}
