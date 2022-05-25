fc451221f2324d62192394c6204d93e3b2a4d4f0
Grandelwald <artjr97@naver.com>
2022-05-25 16:43:13 +0900

    2단계 - 문자열 계산기 : 사칙연산 기호 에러처리 함수를 기능 중복으로 삭제

--- src/main/kotlin/study/stringCalculator/StringCalculator.kt
+++ src/main/kotlin/study/stringCalculator/StringCalculator.kt
@@ -5,7 +5,7 @@ class StringCalculator {
     fun calculate(mathExpression: String): Int {
         val stringList = mathExpression.checkEmptyOrNull().split(" ")
         val numberList = stringList.filterIndexed { index, _ -> index % 2 == 0 }.map { it.toInt() }
-        val operatorList = stringList.filterIndexed { index, _ -> index % 2 != 0 }.toList().checkMathSymbol()
+        val operatorList = stringList.filterIndexed { index, _ -> index % 2 != 0 }.toList()
 
         val numberListToOperate = numberList.drop(1)
         val pairList = operatorList.zip(numberListToOperate).map { (operator, number) -> Pair(operator, number) }
@@ -18,15 +18,10 @@ class StringCalculator {
         return this
     }
 
-    private fun List<String>.checkMathSymbol(): List<String> {
-        require(this.all { it in listOf("+", "-", "*", "/") }) { "사칙연산 기호가 아닌 경우" }
-        return this
-    }
-
     private fun calculateEveryPair(pairList: List<Pair<String, Int>>, initValue: Int): Int {
 
         return pairList.fold(initValue) { acc, pair ->
-            val operator = Operator.reverseMap(pair.first) ?: throw IllegalArgumentException("사칙연산이 아닙니다“")
+            val operator = Operator.reverseMap(pair.first) ?: throw IllegalArgumentException("사칙연산 기호가 아닙니다“")
             operator.work(acc, pair.second)
         }
     }
