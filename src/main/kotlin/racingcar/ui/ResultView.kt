1f2ef73442682ed16f6961744df0737b27a08577
glennsm <lovinhut@gmail.com>
2022-05-25 15:17:41 +0900

    feat: 자동차 이름은 쉼표(,)를 기존으로 구분한다

--- src/main/kotlin/racingcar/ui/ResultView.kt
+++ src/main/kotlin/racingcar/ui/ResultView.kt
@@ -8,19 +8,23 @@ class ResultView {
         println(START_TEXT)
         playRecord.forEach { round ->
             round.forEach { carHistory ->
-                printPosition(carHistory.position)
+                printCar(carHistory)
                 newLine()
             }
             newLine()
         }
     }
 
-    private fun printPosition(position: Int) = repeat(position + 1) { print(POSITION_TEXT) }
+    private fun printCar(carHistory: CarHistory) {
+        print(carHistory.name + NAME_POSTFIX_TEXT)
+        repeat(carHistory.position) { print(POSITION_TEXT) }
+    }
     private fun newLine() = println(NEWLINE_TEXT)
 
     companion object {
         const val START_TEXT = "실행결과"
         const val POSITION_TEXT = "-"
         const val NEWLINE_TEXT = ""
+        const val NAME_POSTFIX_TEXT = " : "
     }
 }
