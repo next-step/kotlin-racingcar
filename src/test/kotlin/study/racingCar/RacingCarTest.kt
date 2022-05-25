48eff7f5dcea49aa6d0ec091c239eb4ecb7ea8a4
Grandelwald <artjr97@naver.com>
2022-05-26 03:12:39 +0900

    3단계 - 자동차 경주 : test에 숫자가 4 이상일떄, 4미만일때 movedSteps 증가 테스트 추가

--- src/test/kotlin/study/racingCar/RacingCarTest.kt
+++ src/test/kotlin/study/racingCar/RacingCarTest.kt
@@ -17,7 +17,17 @@ class RacingCarTest {
     fun `무작위 수가 4이상일 경우에만 전진한다`() {
         val car = Car()
         val initialMovedSteps = car.movedSteps
-        println(initialMovedSteps)
+
+        car.moveOneStep(4).movedSteps
         assertThat(car.movedSteps).isEqualTo(initialMovedSteps + 1)
+    }
+
+    @Test
+    fun `무작위 수가 4미만인 경우에는 전진하지 않는다`() {
+        val car = Car()
+        val initialMovedSteps = car.movedSteps
+
+        car.moveOneStep(1).movedSteps
+        assertThat(car.movedSteps).isEqualTo(initialMovedSteps)
     }
 }
--- src/test/kotlin/study/stringCalculator/StringCalculatorTest.kt
+++ src/test/kotlin/study/stringCalculator/StringCalculatorTest.kt
@@ -8,55 +8,54 @@ import org.junit.jupiter.api.Test
 
 class StringCalculatorTest {
     @Test
-    fun `덧셈`(){
+    fun `덧셈`() {
         val stringCalculator = StringCalculator()
         assertThat(stringCalculator.calculate("4 + 2")).isEqualTo(6)
     }
 
     @Test
-    fun `뺄셈`(){
+    fun `뺄셈`() {
         val stringCalculator = StringCalculator()
         assertThat(stringCalculator.calculate("4 - 2")).isEqualTo(2)
     }
 
     @Test
-    fun `나눗셈`(){
+    fun `나눗셈`() {
         val stringCalculator = StringCalculator()
         assertThat(stringCalculator.calculate("4 / 2")).isEqualTo(2)
     }
 
     @Test
-    fun `곱셈`(){
+    fun `곱셈`() {
         val stringCalculator = StringCalculator()
         assertThat(stringCalculator.calculate("4 * 2")).isEqualTo(8)
-
     }
 
     @Test
-    fun `입력값이 null 또는 빈 공백문자인 경우`(){
+    fun `입력값이 null 또는 빈 공백문자인 경우`() {
         val stringCalculator = StringCalculator()
         assertThatIllegalArgumentException().isThrownBy { stringCalculator.calculate("    ") }
     }
 
     @Test
-    fun `사칙얀산기호가 아닌 경우`(){
+    fun `사칙얀산기호가 아닌 경우`() {
         val stringCalculator = StringCalculator()
         assertThatIllegalArgumentException().isThrownBy { stringCalculator.calculate("2 ? 3 ! 4 & 2") }
     }
 
     @Test
-    fun `사칙연산을 모두 포함하는 기능`(){
+    fun `사칙연산을 모두 포함하는 기능`() {
         val stringCalculator = StringCalculator()
-        assertThat(stringCalculator.calculate( "2 + 3 * 4 / 2")).isEqualTo(10)
+        assertThat(stringCalculator.calculate("2 + 3 * 4 / 2")).isEqualTo(10)
     }
 
     @BeforeEach
-    fun beforeTest(){
+    fun beforeTest() {
         println("-------TEST START-------")
     }
 
     @AfterEach
-    fun afterTest(){
+    fun afterTest() {
         println("--------TEST END--------")
     }
 }
