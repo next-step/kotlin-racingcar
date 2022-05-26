532d7e08060f5ec0710321326c8e57f2fb07d8a2
skid901 <tlrudejr1993@gmail.com>
2022-05-27 04:09:46 +0900

    refactor: change instances name and static method in Operator enum class

--- src/main/kotlin/calculator/Operator.kt
+++ src/main/kotlin/calculator/Operator.kt
@@ -2,10 +2,10 @@ package calculator
 
 enum class Operator(private val op: String, private val operationLambda: (leftOperand: Double, rightOperand: Double) -> Double) {
 
-    Addition("+", { leftOperand, rightOperand -> leftOperand + rightOperand }),
-    Subtraction("-", { leftOperand, rightOperand -> leftOperand - rightOperand }),
-    Multiplication("*", { leftOperand, rightOperand -> leftOperand * rightOperand }),
-    Division("/", { leftOperand, rightOperand ->
+    ADDITION("+", { leftOperand, rightOperand -> leftOperand + rightOperand }),
+    SUBTRACTION("-", { leftOperand, rightOperand -> leftOperand - rightOperand }),
+    MULTIPLICATION("*", { leftOperand, rightOperand -> leftOperand * rightOperand }),
+    DIVISION("/", { leftOperand, rightOperand ->
         check(rightOperand != 0.0) { "0 으로 나눌 수 없습니다." }
         leftOperand / rightOperand
     });
@@ -14,9 +14,9 @@ enum class Operator(private val op: String, private val operationLambda: (leftOp
 
         private val OPERATORS_BY_OP = values().associateBy { it.op }
 
-        fun operate(operator: String, leftOperand: Double, rightOperand: Double) = let {
+        fun get(operator: String) = let {
             OPERATORS_BY_OP[operator] ?: throw IllegalArgumentException("연산 불가능한 연산자가 입력되었습니다.: $operator")
-        }.operate(leftOperand, rightOperand)
+        }
     }
 
     fun operate(leftOperand: Double, rightOperand: Double) = operationLambda(leftOperand, rightOperand)
--- src/test/kotlin/calculator/OperatorTest.kt
+++ src/test/kotlin/calculator/OperatorTest.kt
@@ -8,34 +8,34 @@ internal class OperatorTest : AnnotationSpec() {
 
     @Test
     fun `덧셈`() {
-        Operator.operate("+", 2.0, 3.0) shouldBe 5
+        Operator.ADDITION.operate(2.0, 3.0) shouldBe 5
     }
 
     @Test
     fun `뺄셈`() {
-        Operator.operate("-", 2.0, 3.0) shouldBe -1
+        Operator.SUBTRACTION.operate(2.0, 3.0) shouldBe -1
     }
 
     @Test
     fun `곱셈`() {
-        Operator.operate("*", 2.0, 3.0) shouldBe 6
+        Operator.MULTIPLICATION.operate(2.0, 3.0) shouldBe 6
     }
 
     @Test
     fun `나눗셈`() {
-        Operator.operate("/", 5.0, 2.0) shouldBe 2.5
+        Operator.DIVISION.operate(5.0, 2.0) shouldBe 2.5
     }
 
     @Test
     fun `나눗셈 시 우항 피연산자가 0 일 경우 예외 처리`() {
-        assertThrows<IllegalStateException> { Operator.operate("/", 5.0, 0.0) }
+        assertThrows<IllegalStateException> { Operator.DIVISION.operate(5.0, 0.0) }
             .message shouldBe "0 으로 나눌 수 없습니다."
     }
 
     @Test
     fun `연산 불가능한 연산자가 입력된 경우 예외 처리`() {
         val nonArithmeticOperator = "//"
-        assertThrows<IllegalArgumentException> { Operator.operate(nonArithmeticOperator, 5.0, 2.0) }
+        assertThrows<IllegalArgumentException> { Operator.get(nonArithmeticOperator) }
             .message shouldBe "연산 불가능한 연산자가 입력되었습니다.: $nonArithmeticOperator"
     }
 }
