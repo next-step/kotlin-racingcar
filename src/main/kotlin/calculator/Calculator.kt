b0db5efc25d292f4b8187626edfff834918daa10
skid901 <tlrudejr1993@gmail.com>
2022-05-27 04:12:20 +0900

    refactor: extract operation units first-class collection from Calculator class

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -4,37 +4,14 @@ object Calculator {
 
     private const val INITIAL_ACCUMULATOR = 0.0
 
-    private const val DELIMITER = " "
-
     fun calculate(formula: String?): Double {
-        validateFormula(formula)
-
-        val operationUnits = ("+ $formula").split(DELIMITER)
-            .map { it.trim() }
-            .filter { it.isNotBlank() }
-
-        var accumulator = INITIAL_ACCUMULATOR
-        repeat(countOperationPair(operationUnits.size)) {
-            val (operator, operand) = extractOperationPair(it, operationUnits)
-            accumulator = Operator.operate(operator, accumulator, operand)
-        }
-        return accumulator
-    }
-
-    private fun validateFormula(formula: String?) {
         require(!formula.isNullOrBlank()) { "연산 불가능한 입력 입니다." }
-    }
+        val operationUnits = OperationUnits(formula)
 
-    private fun countOperationPair(operationUnitCount: Int) = operationUnitCount / 2
-
-    private fun extractOperationPair(operationTurn: Int, operationUnits: List<String>): Pair<String, Double> {
-        val index = operationTurn * 2
-        val (operator, operand) = operationUnits.subList(index, index + 2)
-
-        try {
-            return Pair(operator, operand.toDouble())
-        } catch (e: NumberFormatException) {
-            throw IllegalArgumentException("연산 불가능한 피연산자가 입력되었습니다.: $operand")
+        var result = INITIAL_ACCUMULATOR
+        for ((operator, operand) in operationUnits) {
+            result = operator.operate(result, operand)
         }
+        return result
     }
 }
