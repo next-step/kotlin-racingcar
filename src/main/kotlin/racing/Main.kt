073ea6524cf72049d1ff4b8a7bddf224b2686a1e
inpyo-jeon <inpyo.developer@gmail.com>
2022-05-27 22:41:38 +0900

    [Step5] refactor : 의존 분리를 위해 호출부 변경

--- src/main/kotlin/racing/Main.kt
+++ src/main/kotlin/racing/Main.kt
@@ -2,6 +2,7 @@ package racing
 
 import racing.domain.common.CarRaceProperty
 import racing.domain.race.CarRace
+import racing.domain.race.WinnerJudge
 import racing.domain.validation.CarRaceValidation
 import racing.view.InputView
 import racing.view.ResultView
@@ -17,6 +18,13 @@ fun main() {
     val carRaceProperty = CarRaceProperty(roundSize, carNames)
 
     val resultView = ResultView(carRaceProperty)
-    val carRace = CarRace(carRaceProperty, resultView)
-    carRace.start()
+    val carRace = CarRace(carRaceProperty)
+
+    repeat(carRaceProperty.roundSize) {
+        carRace.start()
+        resultView.printResultByRound(it)
+    }
+
+    val winnerJudge = WinnerJudge(carRaceProperty)
+    resultView.printWinner(winnerJudge.getWinnerCarNames())
 }
--- src/main/kotlin/racing/domain/race/CarRace.kt
+++ src/main/kotlin/racing/domain/race/CarRace.kt
@@ -1,19 +1,9 @@
 package racing.domain.race
 
 import racing.domain.common.CarRaceProperty
-import racing.view.ResultView
 
-class CarRace(
-    private val carRaceProperty: CarRaceProperty,
-    private val resultView: ResultView,
-) {
+class CarRace(private val carRaceProperty: CarRaceProperty) {
     fun start() {
-        for (round in CarRaceProperty.ROUND_START..carRaceProperty.roundSize) {
-            carRaceProperty.cars.map { it.move() }
-            resultView.printResultByRound(round)
-        }
-
-        val winnerJudge = WinnerJudge(carRaceProperty)
-        resultView.printWinner(winnerJudge.getWinnerCarNames())
+        carRaceProperty.cars.map { it.move() }
     }
 }
