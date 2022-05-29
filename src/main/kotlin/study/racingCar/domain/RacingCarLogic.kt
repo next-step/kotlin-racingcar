1c86a2290912b7f46123133feb90fa80b1c72e9c
Grandelwald <artjr97@naver.com>
2022-05-29 15:05:01 +0900

    4단계 - 자동차 경주(우승자) : [refactor] RacingCarLogic.kt의 cars 생성부 깔끔하게 리팩토링

--- src/main/kotlin/study/racingCar/domain/RacingCarLogic.kt
+++ src/main/kotlin/study/racingCar/domain/RacingCarLogic.kt
@@ -2,7 +2,7 @@ package study.racingCar.domain
 
 class RacingCarLogic {
     fun startRacing(carNames: List<String>, movingTryNum: Int): List<List<Pair<String, Int>>> {
-        val cars = (carNames.indices).map { Car(carNames[it]) }
+        val cars = carNames.map { carName -> Car(carName) }
 
         return (0 until movingTryNum).map {
             cars.map { car ->
