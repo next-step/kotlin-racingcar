60eee27c6648baa3c8bd7a58e6e362a8074467c6
hoori <freedom09@inflab.com>
2022-05-30 22:58:27 +0900

    상수 사용

--- src/main/kotlin/racingcar/car/drivingPoint.kt
+++ src/main/kotlin/racingcar/car/drivingPoint.kt
@@ -2,5 +2,6 @@ package racingcar.car
 
 private const val DATUM_POINT = 4
 private const val FORWARD_POINT = 1
+private const val STOP_POINT = 0
 
-fun drivingPointBy(drivingForce: Int) = if (drivingForce >= DATUM_POINT) FORWARD_POINT else 0
+fun drivingPointBy(drivingForce: Int) = if (drivingForce >= DATUM_POINT) FORWARD_POINT else STOP_POINT
