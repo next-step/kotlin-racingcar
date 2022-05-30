156ed733f7100734e6b4fd86e0ce38f886644be2
molly <molly.d@kakaocorp.com>
2022-05-30 10:37:35 +0900

    feat : lint 적용

--- src/main/kotlin/step4/racingcar/Main.kt
+++ src/main/kotlin/step4/racingcar/Main.kt
@@ -1,11 +1,6 @@
 package step4.racingcar
 
 import step4.racingcar.controller.CarRacingController
-import step4.racingcar.domain.Car
-import step4.racingcar.domain.CarRacing
-import step4.racingcar.domain.RandomNumber
-import step4.racingcar.view.InputView
-import step4.racingcar.view.ResultView
 
 fun main() {
     CarRacingController.play()
