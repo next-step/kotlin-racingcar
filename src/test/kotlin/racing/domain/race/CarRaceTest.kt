f8a162a57300ab2d855077a54e5179371ce54f9f
inpyo-jeon <inpyo.developer@gmail.com>
2022-06-01 01:28:14 +0900

    [Step5] refactor : 코드리뷰 반영 - fexture 클래스 형태로 수정

--- src/test/kotlin/racing/domain/race/CarRaceTest.kt
+++ src/test/kotlin/racing/domain/race/CarRaceTest.kt
@@ -6,6 +6,11 @@ import racing.domain.common.CarRaceProperty
 import racing.domain.utils.RandomNumberStrategy
 
 class CarRaceTest {
+    class RandomNumberFixture(private val number: Int) : RandomNumberStrategy {
+        override fun getRandomNumber(): Int {
+            return number
+        }
+    }
 
     @Test
     fun `이동이 되었는지 테스트`() {
@@ -20,17 +25,11 @@ class CarRaceTest {
 
     @Test
     fun `모든 라운드가 이동되는 조건인 경우에 대한 테스트`() {
-        class RandomNumberMakerForTest(val testNumber: Int) : RandomNumberStrategy {
-            override fun getRandomNumber(): Int {
-                return testNumber
-            }
-        }
-
         val roundSize = 5
         val carNames = listOf("A")
         val carRaceProperty = CarRaceProperty(roundSize, carNames)
         val carRace = CarRace(carRaceProperty)
-        val randomNumber = RandomNumberMakerForTest(10)
+        val randomNumber = RandomNumberFixture(10)
 
         carRace.start(randomNumber)
 
@@ -39,12 +38,6 @@ class CarRaceTest {
 
     @Test
     fun `조건에 따라 이동, 이동하지 않는 경우에 대한 테스트`() {
-        class RandomNumberMakerForTest(val testNumber: Int) : RandomNumberStrategy {
-            override fun getRandomNumber(): Int {
-                return testNumber
-            }
-        }
-
         val roundSize = 5
         val randomNumberByRound = listOf(5, 2, 10, 9, 2)
         val carNames = listOf("A")
@@ -52,7 +45,7 @@ class CarRaceTest {
         val carRace = CarRace(carRaceProperty)
 
         repeat(roundSize) {
-            val randomNumber = RandomNumberMakerForTest(randomNumberByRound[it])
+            val randomNumber = RandomNumberFixture(randomNumberByRound[it])
             carRace.startByRound(it, randomNumber)
         }
 
