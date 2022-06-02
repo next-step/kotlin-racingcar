ed3ef33062986fe4c095b16c8a293602d7cf9eec
hoori <freedom09@inflab.com>
2022-06-03 02:25:34 +0900

    스텝별 위치 표기시 이름과 함께 표기

--- src/main/kotlin/racingcar/view/ResultView.kt
+++ src/main/kotlin/racingcar/view/ResultView.kt
@@ -1,12 +1,16 @@
 package racingcar.view
 
 import racingcar.GameBoard
+import racingcar.car.Car
 
-private fun showPositionBy(position: Int) = println(stringBy(position))
+private fun showPositionBy(name: String, position: Int) = println("$name :${stringBy(position)}")
 
 private fun stringBy(position: Int): String = "-".repeat(position)
 
 class ResultView(private val gameBoard: GameBoard) {
+    private val cars = gameBoard.cars
+    private val longestNameSize = cars.map { it.name }.maxOf { it.length }
+
     fun showAllPositionsByStep() {
         println("\n실행 결과")
         repeat(gameBoard.moveCount) {
@@ -15,5 +19,15 @@ class ResultView(private val gameBoard: GameBoard) {
         }
     }
 
-    private fun showPositionsBy(step: Int) = gameBoard.cars.forEach { showPositionBy(it.positionBy(step)) }
+    private fun showPositionsBy(step: Int) = cars.forEach { showPositionBy(it.displayName, it.positionBy(step)) }
+
+    private val Car.displayName: String
+        get() = displayNames.getOrPut(this) { getDisplayName(this.name) }
+    private val displayNames = mutableMapOf<Car, String>()
+
+    private fun getDisplayName(name: String): String {
+        var displayName = name
+        repeat(longestNameSize - name.length) { displayName += " " }
+        return displayName
+    }
 }
