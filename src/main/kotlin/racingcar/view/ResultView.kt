9c523ed6cbeea1bfec047b7ecee4e5226b90f65d
hoori <freedom09@inflab.com>
2022-06-08 23:00:40 +0900

    리팩토링 (프로퍼티 순서 변경 및 String.repeat 활용)

--- src/main/kotlin/racingcar/view/ResultView.kt
+++ src/main/kotlin/racingcar/view/ResultView.kt
@@ -8,10 +8,17 @@ private fun showPositionBy(name: String, position: Int) = println("$name :${stri
 private fun stringBy(position: Int): String = "-".repeat(position)
 
 class ResultView(gameBoard: GameBoard) {
-    private val cars = gameBoard.cars
-    private val winners = gameBoard.winners
-    private val moveCount = gameBoard.moveCount
-    private val maxNameLength = cars.map { it.name }.maxOf { it.length }
+    private val cars: List<Car> = gameBoard.cars
+    private val winners: List<Car> = gameBoard.winners
+    private val moveCount: Int = gameBoard.moveCount
+    private val maxNameLength: Int = cars.map { it.name }.maxOf { it.length }
+    private val displayNames = mutableMapOf<Car, String>()
+    private val Car.displayName: String
+        get() = displayNames.getOrPut(this) { getDisplayName(this.name) }
+
+    private fun getDisplayName(name: String): String = name + " ".repeat(maxNameLength - name.length)
+
+    private fun showPositionsBy(step: Int) = cars.forEach { showPositionBy(it.displayName, it.positionBy(step)) }
 
     fun showAllPositionsByStep() {
         println("\n실행 결과")
@@ -19,18 +26,6 @@ class ResultView(gameBoard: GameBoard) {
             showPositionsBy(it + 1)
             println()
         }
-    }
-
-    private fun showPositionsBy(step: Int) = cars.forEach { showPositionBy(it.displayName, it.positionBy(step)) }
-
-    private val Car.displayName: String
-        get() = displayNames.getOrPut(this) { getDisplayName(this.name) }
-    private val displayNames = mutableMapOf<Car, String>()
-
-    private fun getDisplayName(name: String): String {
-        var displayName = name
-        repeat(maxNameLength - name.length) { displayName += " " }
-        return displayName
     }
 
     fun showWinners() = println("${winners.joinToString(", ") { it.name }} (이)가 최종 우승했습니다.")
