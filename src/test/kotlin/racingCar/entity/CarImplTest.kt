cd2aac0762c3d43d39cdf217af3388cdfa8407de
erie0210 <2j00923@gmail.com>
2022-06-20 00:11:00 +0900

    feat: 자동차 기능 추가

--- src/test/kotlin/racingCar/entity/CarImplTest.kt
+++ src/test/kotlin/racingCar/entity/CarImplTest.kt
@@ -1,16 +1,49 @@
 package racingCar.entity
 
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
 
 internal class CarImplTest {
 
     @Test
-    fun move() {
+    fun `자동차가 이동하면 distance가 1 증가한다`() {
+        // given
+        val car = CarImpl()
+        val expectedDistance = car.distance
+        val moves = 6
+
+        // when
+        val testCar = car.move(moves)
+
+        // then
+        Assertions.assertThat(testCar.distance).isEqualTo(expectedDistance + 1)
     }
 
     @Test
-    fun getDistance() {
+    fun `move()에 4 이상의 숫자가 들어가면 자동차가 1 이동한다`() {
+        // given
+        val car = CarImpl()
+        val expectedDistance = car.distance
+        val randomNumber = 6
+
+        // when
+        val testCar = car.move(randomNumber)
+
+        // then
+        Assertions.assertThat(testCar.distance).isEqualTo(expectedDistance + 1)
+    }
+
+    @Test
+    fun `move()에 4 미만의 숫자가 들어가면 자동차가 이동하지 않는다`() {
+        // given
+        val car = CarImpl()
+        val expectedDistance = car.distance
+        val randomNumber = 2
+
+        // when
+        val testCar = car.move(randomNumber)
+
+        // then
+        Assertions.assertThat(testCar.distance).isEqualTo(expectedDistance)
     }
 }
