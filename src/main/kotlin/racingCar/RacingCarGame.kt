6fce79b349ec654391c34cac1192684c2004ded0
erie0210 <2j00923@gmail.com>
2022-06-20 00:11:27 +0900

    feat: 자동차 게임 도메인 추가

--- src/main/kotlin/racingCar/RacingCarGame.kt
+++ src/main/kotlin/racingCar/RacingCarGame.kt
@@ -1,4 +1,33 @@
 package racingCar
 
+import racingCar.entity.Car
+import racingCar.entity.CarImpl
+import racingCar.ui.Input
+import racingCar.ui.Result
+
 class RacingCarGame {
+    fun play() {
+        var cars = generateCars(Input().getNumberOfCars())
+        val moves = Input().getNumberOfMoves()
+        repeat(moves) {
+            cars = moveCars(cars)
+        }
+    }
+
+    fun moveCars(cars: List<Car>): List<Car> {
+        val movedCars = cars.map { car: Car -> car.move(generateRandomNumber()) }
+        Result().printCarsDistanc(movedCars)
+        println()
+        return movedCars
+    }
+
+    fun generateCars(numberOfCars: Int): List<Car> {
+        return mutableListOf<Car>().apply {
+            repeat(numberOfCars) { this.add(CarImpl()) }
+        }.toList()
+    }
+
+    fun generateRandomNumber(): Int {
+        return (0..9).random()
+    }
 }
--- src/test/kotlin/racingCar/RacingCarGameTest.kt
+++ src/test/kotlin/racingCar/RacingCarGameTest.kt
@@ -1,16 +1,29 @@
 package racingCar
 
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
 
 internal class RacingCarGameTest {
 
     @Test
-    fun generateCars() {
+    fun `4대의 자동차를 만든다`() {
+        // given
+        val expectedSize = 4
+
+        // when
+        val resultCars = RacingCarGame().generateCars(4)
+
+        // then
+        Assertions.assertThat(resultCars.size).isEqualTo(expectedSize)
     }
 
     @Test
-    fun generateRandomNumber() {
+    fun `0과 9 사이의 중 하나의 숫자를 임의로 생성한다`() {
+        // when
+        val rangeList = (0..9)
+        val testNumber = RacingCarGame().generateRandomNumber()
+
+        // then
+        Assertions.assertThat(rangeList).contains(testNumber)
     }
 }
