f34e5b77f0755b1c927916d11bd9f204ec7ffb88
erie0210 <2j00923@gmail.com>
2022-06-20 00:11:41 +0900

    feat:UI 추가

--- src/main/kotlin/racingCar/ui/Result.kt
+++ src/main/kotlin/racingCar/ui/Result.kt
@@ -1,4 +1,9 @@
 package racingCar.ui
 
+import racingCar.entity.Car
+
 class Result {
+    fun printCarsDistanc(cars: List<Car>) {
+        cars.forEach { car: Car -> println("-".repeat(car.distance)) }
+    }
 }
