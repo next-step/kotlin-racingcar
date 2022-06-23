fe84ab6068f67fcd97476fc4ea3ba2f3394c464f
yjna2316 <yjna2316@gmail.com>
2022-06-23 10:14:18 +0900

    refac: 리뷰 반영 - Car 클래스 - 이동 정책 moveStrategy를 외부에서 주입 받도록 수정

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -1,16 +1,18 @@
 package racingcar.domain
 
+import racingcar.domain.moving.MoveStrategy
+
 class Car(
     val position: Int = 0,
 ) {
-    fun move(number: Int): Int {
-        val moveOneStepOrNot = if (number >= LOWER_BOUND_TO_MOVE) MOVE else STOP
-        return position.plus(moveOneStepOrNot)
+    fun move(moveStrategy: MoveStrategy): Car {
+        return if (moveStrategy.isMovable())
+            Car(position + FORWARD)
+        else
+            this
     }
 
     companion object {
-        const val MOVE = 1
-        const val STOP = 0
-        const val LOWER_BOUND_TO_MOVE = 4
+        const val FORWARD = 1
     }
 }
--- src/test/kotlin/racingcar/domain/CarTest.kt
+++ src/test/kotlin/racingcar/domain/CarTest.kt
@@ -2,20 +2,46 @@ package racingcar.domain
 
 import io.kotest.core.spec.style.AnnotationSpec
 import io.kotest.matchers.shouldBe
-import racingcar.domain.CarFixture.car
+import racingcar.domain.moving.MoveStrategy
 
 class CarTest : AnnotationSpec() {
-    @Test
-    fun `자동차는 0-9사이 무작위 값이 4 이상일때 움직인다`() {
-        val randomNumber = (4..9).random()
-        val resultCarPosition = car.move(randomNumber)
-        resultCarPosition shouldBe car.position.plus(1)
+    private val moveStrategy = object : MoveStrategy {
+        override fun isMovable(): Boolean {
+            return true
+        }
+    }
+
+    private val notMoveStrategy = object : MoveStrategy {
+        override fun isMovable(): Boolean {
+            return false
+        }
     }
 
     @Test
-    fun `자동차는 0-9사이 무작위 값이 4 미만이면 움직이지 않는다`() {
-        val randomNumber = (0..3).random()
-        val resultCarPosition = car.move(randomNumber)
-        resultCarPosition shouldBe car.position
+    fun `자동차 생성시 초기 위치는 0이다`() {
+        // given
+        // when
+        val car = Car()
+        // then
+        car.position shouldBe 0
+    }
+
+    @Test
+    fun `자동차는 한칸 이동한다`() {
+        // given
+        val car = Car(10)
+        // when
+        val movedCar = car.move(moveStrategy)
+        // then
+        movedCar.position shouldBe 11
+    }
+
+    @Test
+    fun `자동차는 이동하지 않는다`() {
+        val car = Car(10)
+        // when
+        val movedCar = car.move(notMoveStrategy)
+        // then
+        movedCar.position shouldBe 10
     }
 }
