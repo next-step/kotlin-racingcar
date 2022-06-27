fb8166f5e5c8927241d1f06161ba63b49f28365f
erie0210 <2j00923@gmail.com>
2022-06-27 10:51:43 +0900

    feat: ui 기능 추가

--- src/main/kotlin/racingCar/ui/Input.kt
+++ src/main/kotlin/racingCar/ui/Input.kt
@@ -10,4 +10,9 @@ class Input {
         println("시도할 횟수는 몇 회인가요?")
         return readln().toInt()
     }
+
+    fun getCarNames(): List<String> {
+        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+        return readln().split(",")
+    }
 }
--- src/main/kotlin/racingCar/ui/Result.kt
+++ src/main/kotlin/racingCar/ui/Result.kt
@@ -1,9 +1,14 @@
 package racingCar.ui
 
+import racingCar.RacingCarGame
 import racingCar.entity.Car
 
 class Result {
     fun printCarsDistanc(cars: List<Car>) {
-        cars.forEach { car: Car -> println("-".repeat(car.distance)) }
+        cars.forEach { car: Car -> println(car.name + ": " + "-".repeat(car.distance)) }
+    }
+
+    fun printWinner(cars: List<Car>) {
+        println(RacingCarGame().getWinner(cars).joinToString() + "가 최종 우승했습니다.")
     }
 }
