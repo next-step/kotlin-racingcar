02f48d4fba27cde94162b4e82b9398a8a154444d
dwayne.knq <dwayne.knq@kakaocorp.com>
2022-07-30 09:16:48 +0900

    feat: 시도할 횟수 output 추가

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -4,6 +4,7 @@ class InputView {
     fun input(): InputCommand {
         println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
         val names : List<String> = readln().split(",")
+        println("시도할 횟수는 몇 회인가요?")
         val trial : Int = readln().toIntOrNull() ?: throw IllegalArgumentException()
         return InputCommand(names, trial)
     }
