4280cc453a0646aa1fafff4327128f9e0675c081
Choiyounho <65484721+Choiyounho@users.noreply.github.com>
2022-11-02 00:23:10 +0900

    Step1 : 1단계 코틀린 기초 (#712)
    * docs : ktlint 버전 10.3.0으로 업그레이드
    
    * feat : 코틀린 데이터 클래스 기능 테스트

---
+++ src/test/kotlin/PersonTest.kt
@@ -0,0 +1,56 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class PersonTest {
+
+    private lateinit var person: Person
+
+    @BeforeEach
+    fun setUp() {
+        person = Person("최윤호", 29, null)
+    }
+
+    @Test
+    fun `사람 이름을 확인하는 테스트`() {
+        val expected = "최윤호"
+        assertThat(person.name).isEqualTo(expected)
+    }
+
+    @Test
+    fun `널 타입의 값을 확인`() {
+        assertThat(person.nickname).isEqualTo(null)
+    }
+
+    @Test
+    fun `데이터 클래스의 Equals 확인`() {
+        val anotherPerson = Person("홍길동", 29, null)
+        val samePerson = Person("최윤호", 29, null)
+
+        val isSame = person == anotherPerson
+
+        assertAll(
+            { assertThat(isSame).isFalse() },
+            { assertThat(samePerson).isEqualTo(person) }
+        )
+    }
+
+    @Test
+    fun `데이터 클래스 구조 분해 선언 테스트`() {
+        val (name, age, nickname) = Person("최윤호", 29, null)
+
+        assertAll(
+            { assertThat(name).isEqualTo(person.name) },
+            { assertThat(age).isEqualTo(person.age) },
+            { assertThat(nickname).isEqualTo(person.nickname) }
+        )
+    }
+
+    @Test
+    fun `데이터 클래스 카피 기능 테스트`() {
+        val anotherPerson = Person("최윤호", 29, null)
+
+        assertThat(person.copy()).isEqualTo(anotherPerson)
+    }
+}
