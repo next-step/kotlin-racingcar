87be232873a412412c99f98c89fabb90435afd7a
do.lee <do.lee@woowahan.com>
2022-11-01 21:41:19 +0900

    코틀린 기본문법 테스트 작성 및 린트버전 수정

---
+++ src/test/kotlin/study/PersonTest.kt
@@ -0,0 +1,37 @@
+package study
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class PersonTest {
+    @Test
+    fun `이름 붙인 인자`() {
+        val person = Person(name = "이도원", age = 20, nickname = "leedo1982")
+
+        assertThat(person.name).isEqualTo("이도원")
+        assertThat(person.age).isEqualTo(20)
+        assertThat(person.nickname).isEqualTo("leedo1982")
+    }
+
+    @Test
+    fun `널 타입`() {
+        val person = Person(name = "이도원", age = 20, null)
+
+        assertThat(person.nickname).isNull()
+    }
+
+    @Test
+    fun `기본 인자`() {
+        val person = Person(name = "이도원", age = 20)
+
+        assertThat(person.name).isEqualTo(person.nickname)
+    }
+
+    @Test
+    fun `데이터 클래스`() {
+        val person1 = Person(name = "이도원", age = 20)
+        val person2 = Person(name = "이도원", age = 20)
+
+        assertThat(person1).isEqualTo(person2)
+    }
+}
