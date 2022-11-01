224c3de2669be6a63acb52c4fb3c390840a244ca
wonjun3991 <wonjun.choi@meshkorea.net>
2022-11-01 21:41:38 +0900

    study: learn Kotlin

---
+++ src/test/kotlin/study/PersonTest.kt
@@ -0,0 +1,33 @@
+package study
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class PersonTest {
+    @Test
+    fun `이름 붙인 인자`() {
+        val person = Person(name = "최원준", age = 26, nickname = "Hori")
+        assertThat(person.name).isEqualTo("최원준")
+        assertThat(person.age).isEqualTo(26)
+        assertThat(person.nickname).isEqualTo("Hori")
+    }
+
+    @Test
+    fun `널 타입`() {
+        val person = Person(name = "최원준", age = 26, null)
+        assertThat(person.nickname).isNull()
+    }
+
+    @Test
+    fun `기본 인자`() {
+        val person = Person(name = "최원준", age = 26)
+        assertThat(person.nickname).isEqualTo(person.name)
+    }
+
+    @Test
+    fun `데이터 클래스`() {
+        val person1 = Person(name = "최원준", age = 26)
+        val person2 = Person(name = "최원준", age = 26)
+        assertThat(person1).isEqualTo(person2)
+    }
+}
