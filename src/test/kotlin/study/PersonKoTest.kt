750976e4068b2eab9769c3e5a1e8b4714c015922
James <bookwriterseo@gmail.com>
2022-11-01 23:28:39 +0900

    Fixed name in code

--- src/test/kotlin/study/PersonKoTest.kt
+++ src/test/kotlin/study/PersonKoTest.kt
@@ -6,25 +6,25 @@ import io.kotest.matchers.shouldBe
 
 class PersonKoTest : StringSpec({
     "이름 붙인 인자" {
-        val person = Person(name = "박재성", age = 20, nickname = "제이슨")
-        person.name shouldBe "박재성"
-        person.age shouldBe 20
-        person.nickname shouldBe "제이슨"
+        val person = Person(name = "서준수", age = 22, nickname = "제임스")
+        person.name shouldBe "서준수"
+        person.age shouldBe 22
+        person.nickname shouldBe "제임스"
     }
 
     "널 타입" {
-        val person = Person("박재성", 20, null)
+        val person = Person("서준수", 22, null)
         person.nickname.shouldBeNull()
     }
 
     "기본 인자" {
-        val person = Person("박재성", 20)
+        val person = Person("서준수", 22)
         person.name shouldBe person.nickname
     }
 
     "데이터 클래스" {
-        val person1 = Person("박재성", 20)
-        val person2 = Person("박재성", 20)
+        val person1 = Person("서준수", 22)
+        val person2 = Person("서준수", 22)
         person1 shouldBe person2
     }
 })
--- src/test/kotlin/study/PersonTest.kt
+++ src/test/kotlin/study/PersonTest.kt
@@ -5,7 +5,7 @@ import study.Person
 class PersonTest {
     @Test
     fun `이름 붙인 인자`() {
-        val person = Person(name = "홍종표", age = 26, nickname = "jpoh281")
+        val person = Person(name = "서준수", age = 22, nickname = "제임스")
         assertThat(person.name).isEqualTo("홍종표")
         assertThat(person.age).isEqualTo(26)
         assertThat(person.nickname).isEqualTo("jpoh281")
@@ -13,20 +13,20 @@ class PersonTest {
 
     @Test
     fun `널 타입`() {
-        val person = Person(name = "홍종표", age = 26, nickname = null)
+        val person = Person(name = "서준수", age = 22, nickname = null)
         assertThat(person.nickname).isNull()
     }
 
     @Test
     fun `기본 인자`() {
-        val person = Person(name = "홍종표", age = 26)
+        val person = Person(name = "서준수", age = 22)
         assertThat(person.name).isEqualTo(person.nickname)
     }
 
     @Test
     fun `데이터 클래스`() {
-        val person1 = Person(name = "홍종표", age = 26)
-        val person2 = Person(name = "홍종표", age = 26)
+        val person1 = Person(name = "서준수", age = 22)
+        val person2 = Person(name = "서준수", age = 22)
         assertThat(person1).isEqualTo(person2)
     }
 }
