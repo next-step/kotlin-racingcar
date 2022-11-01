baea39da6ca5d8036686e06f1aef33ca4f2f89dd
rolroralra <rolroralra@gmail.com>
2022-11-01 21:38:42 +0900

    test: basic kotest code

---
+++ src/test/kotlin/com/kotlin/racingcar/domain/RacingCarTest.kt
@@ -0,0 +1,10 @@
+package com.kotlin.racingcar.domain
+
+import io.kotest.core.spec.style.FunSpec
+
+class RacingCarTest : FunSpec({
+
+//    test("test") {
+//        RacingCar("test-car", Distance()) shouldNotBe null
+//    }
+})
