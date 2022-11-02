564e815cd15494d1df9a255828aa40be87113084
EP <kjuioqq8@gmail.com>
2022-11-03 01:15:14 +0900

    refactor: 리터럴 문자열을 상수값ì 으로 변경

--- src/test/kotlin/step2/domain/operator/enums/OperatorTest.kt
+++ src/test/kotlin/step2/domain/operator/enums/OperatorTest.kt
@@ -6,26 +6,47 @@ import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 import step2.domain.operation.enums.Operator
+import step2.domain.operation.model.BinaryInputCommand
 
-class OperatorTest {
+internal class OperatorTest {
+
+    companion object {
+        private const val ADDITION_LITERAL_STRING = "+"
+        private const val SUBTRACTION_LITERAL_STRING = "-"
+        private const val MULTIPLICATION_LITERAL_STRING = "*"
+        private const val DIVISION_LITERAL_STRING = "/"
+    }
 
     @ParameterizedTest
-    @ValueSource(strings = ["+", "-", "*", "/"])
-    internal fun `문자열에서 Operator enum 으로 변환`(operatorString: String) {
+    @ValueSource(strings = [ADDITION_LITERAL_STRING, SUBTRACTION_LITERAL_STRING, MULTIPLICATION_LITERAL_STRING, DIVISION_LITERAL_STRING])
+    fun `문자열에서 Operator enum 으로 변환`(operatorString: String) {
         val operator = Operator.operatorOf(operatorString)
         assertThat(operator).isNotNull
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["0", "__", "++", "!"])
-    internal fun `문자열에서 Operator enum 으로 변환 오류`(operatorString: String) {
+    fun `문자열에서 Operator enum 으로 변환 오류`(operatorString: String) {
         assertThrows<IllegalArgumentException> { Operator.operatorOf(operatorString) }
     }
 
     @Test
-    internal fun `Operator enum 을 Command 로 전환`() {
+    fun `Operator enum 을 Command 로 전환`() {
         Operator.values().forEach {
             assertThat(it.toCommand()).isNotNull
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = [ADDITION_LITERAL_STRING, SUBTRACTION_LITERAL_STRING, MULTIPLICATION_LITERAL_STRING, DIVISION_LITERAL_STRING])
+    fun `Operator Command 동작 확인`(operatorString: String) {
+        val binaryInputCommand = BinaryInputCommand(firstNumber = 1, secondNumber = 2)
+        val result = Operator.operatorOf(operatorString).toCommand().operate(binaryInputCommand)
+        when (operatorString) {
+            ADDITION_LITERAL_STRING -> assertThat(result).isEqualTo(3)
+            SUBTRACTION_LITERAL_STRING -> assertThat(result).isEqualTo(-1)
+            MULTIPLICATION_LITERAL_STRING -> assertThat(result).isEqualTo(2)
+            DIVISION_LITERAL_STRING -> assertThat(result).isEqualTo(0)
         }
     }
 }
