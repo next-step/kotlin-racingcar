67c97cd40ac12918478678ea41adfe4748a7453f
soosue <yunsoo0510@daum.net>
2022-11-02 18:02:53 +0900

    feat: Operator 기능 구현

--- src/main/kotlin/calculator/domain/Operator.kt
+++ src/main/kotlin/calculator/domain/Operator.kt
@@ -1,8 +1,41 @@
 package calculator.domain
 
-class Operator {
+data class Operator(private val value: String) {
+
+    init {
+        validate(value)
+    }
+
+    private fun validate(value: String) {
+        if (PLUS != value
+            && MINUS != value
+            && MULTIPLY != value
+            && DIVIDE != value
+        ) {
+            throw IllegalArgumentException()
+        }
+    }
 
     fun operate(operand1: Operand, operand2: Operand): Operand {
-        TODO("Not yet implemented")
+        if (PLUS == value) {
+            return operand1.sum(operand2)
+        }
+        if (MINUS == value) {
+            return operand1.subtract(operand2)
+        }
+        if (MULTIPLY == value) {
+            return operand1.multiply(operand2)
+        }
+        if (DIVIDE == value) {
+            return operand1.divide(operand2)
+        }
+        throw IllegalStateException()
+    }
+
+    companion object {
+        private const val PLUS = "+"
+        private const val MINUS = "-"
+        private const val MULTIPLY = "*"
+        private const val DIVIDE = "/"
     }
 }
