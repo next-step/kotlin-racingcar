2b7c3860e36b723b69aeeb0d3a5208351c675215
giyeon95 <giyeon15@gmail.com>
2022-11-03 20:20:15 +0900

    feat: Operation 기능 구현

---
+++ src/main/kotlin/calculator/Operation.kt
@@ -0,0 +1,18 @@
+package calculator
+
+enum class Operation(
+    val symbol: String,
+    val calculate: (Int, Int) -> Int
+) {
+    PLUS("+", { a, b -> a + b }),
+    MINUS("-", { a, b -> a - b }),
+    MULTIPLE("*", { a, b -> a * b }),
+    DIVIDE("/", { a, b -> a / b });
+
+    companion object {
+        fun of(inputSymbol: String): Operation {
+            return values().find { operation -> operation.symbol == inputSymbol }
+                ?: throw IllegalArgumentException("사칙연산 기호가 아니에요. $inputSymbol")
+        }
+    }
+}
---
+++ src/test/kotlin/calculator/OperationTest.kt
@@ -0,0 +1,30 @@
+package calculator
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
+
+class OperationTest {
+
+    @CsvSource(value = ["+:12", "-:8", "*:20", "/:5"], delimiter = ':')
+    @ParameterizedTest(name = "사칙연산 `{arguments}` 의 결과가 정상적으로 동작한다.")
+    internal fun addTest(symbol: String, expected: String) {
+        // given
+        val operation = Operation.of(symbol)
+
+        // when
+        val result = operation.calculate(10, 2)
+
+        // then
+        assertThat(result).isEqualTo(expected.toInt())
+    }
+
+    @ValueSource(strings = ["", "a", "2", ")"])
+    @ParameterizedTest(name = " 잘못된 값 `{arguments}` 이 들어가면 예외가 발생한다.")
+    internal fun calculateTest(input: String) {
+        // when, then
+        assertThatIllegalArgumentException().isThrownBy { Operation.of(input) }
+    }
+}
