9f3e0dab11e6f7ff4134605f9be858eb42561556
EP <kjuioqq8@gmail.com>
2022-11-04 00:25:17 +0900

    docs: 학습 테스트 결과 주석 변경

--- src/test/kotlin/step2/study/WindowedStudyTest.kt
+++ src/test/kotlin/step2/study/WindowedStudyTest.kt
@@ -14,7 +14,10 @@ internal class WindowedStudyTest {
         list.windowed(size = 2, step = 2, partialWindows = true) { window: List<Int> ->
             val first = window.first()
 
-            // 마지막 window 에서 window.first()와 window.last()가 동일한 값이 나옴. window.size 가 step 보다 작을 때, 남은 원소를 특수 케이스 패턴으로 처리
+            /**
+             * 마지막 window 에서 window.first()와 window.last()가 동일한 값이 나옴.
+             * window.size 가 step 보다 작을 때, 남은 원소를 특수 케이스 패턴으로 처리 필요
+             */
             val last = if (window.size == 2) window.last() else 6
             assertThat(first).isNotEqualTo(last)
         }
