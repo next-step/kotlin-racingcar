60c357fac9702a9e9bf146246c0bfe7730b7f148
dreamboy1339 <dreamboy1339@gmail.com>
2022-11-03 21:11:41 +0900

    study: Add test code

--- src/test/kotlin/study/PersonTest.kt
+++ src/test/kotlin/study/PersonTest.kt
@@ -3,10 +3,10 @@ package study
 import org.assertj.core.api.AssertionsForClassTypes.assertThat
 import org.junit.jupiter.api.Test
 
-class PersonTest {
+internal class PersonTest {
 
     @Test
-    internal fun `이름 붙인 인자`() {
+    fun `이름 붙인 인자`() {
         val person = Person(name = "황재우", age = 39, nickname = "dreamboy1339")
         assertThat(person.name).isEqualTo("황재우")
         assertThat(person.age).isEqualTo(39)
@@ -14,19 +14,19 @@ class PersonTest {
     }
 
     @Test
-    internal fun `널 타입`() {
+    fun `널 타입`() {
         val person = Person("황재우", 39, null)
         assertThat(person.nickname).isNull()
     }
 
     @Test
-    internal fun `기본 인자`() {
+    fun `기본 인자`() {
         val person = Person("황재우", 39)
-        assertThat(person.name).isEqualTo(person.nickname)
+        assertThat(person.nickname).isEqualTo("황재우")
     }
 
     @Test
-    internal fun `데이터 클래스`() {
+    fun `데이터 클래스`() {
         val person1 = Person("황재우", 39)
         val person2 = Person("황재우", 39)
         assertThat(person1).isEqualTo(person2)
