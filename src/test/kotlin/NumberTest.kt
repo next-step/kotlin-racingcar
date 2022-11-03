af41e627d0747d7ef671efc700e8eef7ce0f8df9
dk-ko <dev.dkko@gmail.com>
2022-11-03 18:09:54 +0900

    feat: add2

--- src/test/kotlin/NumberTest.kt
+++ src/test/kotlin/NumberTest.kt
@@ -7,4 +7,10 @@ internal class NumberTest {
         val result = Number(1).add(Number(1))
         assertEquals(result, Number(2))
     }
+
+    @Test
+    internal fun add2() {
+        val result = Number(1).add(Number(2)).add(Number(3))
+        assertEquals(result, Number(1+2+3))
+    }
 }
