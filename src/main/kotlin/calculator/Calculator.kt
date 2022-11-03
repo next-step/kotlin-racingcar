d157e0c4ae79a6fd36bf5b2a1523bd2fffdeadce
giyeon95 <giyeon15@gmail.com>
2022-11-03 23:16:29 +0900

    refactor: 의미있는 문자열 상수로 추출
            의미있는 문자열은 상수로 분리할 수 있을 것 같아요!
    
    _Originally posted by @ddaaac in https://github.com/next-step/kotlin-racingcar/pull/731#discussion_r1012892338_

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -1,12 +1,14 @@
 package calculator
 
+private const val DELIMITERS = " "
+
 class Calculator {
 
     fun calculate(input: String?): Int {
         requireNotNull(input) { "null은 올 수 없어요" }
         require(input.isNotBlank()) { "빈 값은 올 수 없어요" }
 
-        val values = input.split(" ")
+        val values = input.split(DELIMITERS)
 
         var number = values[0].toInt()
         for (i: Int in 1..values.lastIndex step 2) {
