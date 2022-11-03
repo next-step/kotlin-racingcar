3eb11f185ab997a91b308b806a26cc3d86c0dc50
dk-ko <dev.dkko@gmail.com>
2022-11-03 17:57:48 +0900

    feat: 입력값 공백 검증

---
+++ src/main/kotlin/Calculator.kt
@@ -0,0 +1,8 @@
+import kotlin.IllegalArgumentException
+
+data class Calculator (val context: String){
+    init {
+        if (context.isNullOrBlank()) throw IllegalArgumentException("")
+        context.split(" ")
+    }
+}
