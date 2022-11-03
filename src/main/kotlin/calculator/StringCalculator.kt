14b1ecfae9b4149ca49e398d172a8fa9c6362da9
wrallee <wrallee@naver.com>
2022-11-03 23:29:07 +0900

    feat(Calculator): 사칙연산 기능 구현

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -1,24 +1,19 @@
 package calculator
 
 import java.math.BigDecimal
+import java.math.RoundingMode.HALF_EVEN
+import kotlin.math.max
 
 class StringCalculator {
     companion object {
-        fun add(decimal1: BigDecimal, decimal2: BigDecimal): BigDecimal {
-            TODO("Not yet implemented")
-        }
+        fun add(addend: BigDecimal, augend: BigDecimal): BigDecimal = addend.plus(augend)
 
-        fun subtract(decimal1: BigDecimal, decimal2: BigDecimal): BigDecimal {
-            TODO("Not yet implemented")
-        }
+        fun subtract(minuend: BigDecimal, subtrahend: BigDecimal): BigDecimal = minuend.minus(subtrahend)
 
-        fun multiply(decimal1: BigDecimal, decimal2: BigDecimal): BigDecimal {
-            TODO("Not yet implemented")
-        }
+        fun multiply(multiplier: BigDecimal, multiplicand: BigDecimal): BigDecimal = multiplier.times(multiplicand)
 
-        fun divide(decimal1: BigDecimal, decimal2: BigDecimal): BigDecimal {
-            TODO("Not yet implemented")
-        }
+        fun divide(dividend: BigDecimal, divisor: BigDecimal): BigDecimal =
+            dividend.divide(divisor, max(dividend.scale(), divisor.scale()), HALF_EVEN)
 
         fun calculate(string2: String?) {
             TODO("Not yet implemented")
