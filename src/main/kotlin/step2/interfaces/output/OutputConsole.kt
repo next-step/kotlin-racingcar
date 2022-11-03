f296842a63075f242c4978bd465f85ff33189f71
EP <kjuioqq8@gmail.com>
2022-11-04 00:22:19 +0900

    feat: input, output 콘솔 기능 구현

---
+++ src/main/kotlin/step2/interfaces/output/OutputConsole.kt
@@ -0,0 +1,12 @@
+package step2.interfaces.output
+
+object OutputConsole {
+
+    private const val RESULT = "결과"
+    private const val FINAL_RESULT_COMMENT = "짜잔 $RESULT 가 나왔어요!"
+
+    fun output(result: String) {
+        println(FINAL_RESULT_COMMENT)
+        println("$RESULT: $result")
+    }
+}
