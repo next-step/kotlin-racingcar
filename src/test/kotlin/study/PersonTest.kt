8606d2e32e71aac21266367e77003542bf31976a
Yujin Park <uiuiui118437@gmail.com>
2022-11-03 18:34:45 +0900

    study: study basic kotlin and test code

--- src/test/kotlin/study/PersonTest.kt
+++ src/test/kotlin/study/PersonTest.kt
@@ -6,29 +6,28 @@ import org.junit.jupiter.api.Test
 class PersonTest {
     @Test
     fun `이름 붙인 인자`() {
-        val person = Person(name = "권재용", age = 29, nickname = "jaeykweon")
-
-        assertThat(person.name).isEqualTo("권재용")
-        assertThat(person.age).isEqualTo(29)
-        assertThat(person.nickname).isEqualTo("jaeykweon")
+        val person = Person(name = "박유진", age = 26, nickname = "yuuuzzzin")
+        assertThat(person.name).isEqualTo("박유진")
+        assertThat(person.age).isEqualTo(26)
+        assertThat(person.nickname).isEqualTo("yuuuzzzin")
     }
 
     @Test
     fun `널 타입`() {
-        val person = Person(name = "권재용", age = 29, nickname = null)
+        val person = Person(name = "박유진", age = 26, nickname = null)
         assertThat(person.nickname).isNull()
     }
 
     @Test
     fun `기본 인자`() {
-        val person = Person(name = "권재용", age = 29)
+        val person = Person(name = "박유진", age = 26)
         assertThat(person.name).isEqualTo(person.nickname)
     }
 
     @Test
     fun `데이터 클래스`() {
-        val person1 = Person(name = "권재용", age = 29)
-        val person2 = Person(name = "권재용", age = 29)
+        val person1 = Person(name = "박유진", age = 26)
+        val person2 = Person(name = "박유진", age = 26)
         assertThat(person1).isEqualTo(person2)
     }
 }
