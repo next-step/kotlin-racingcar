6981b294921c10dd2d4696e7505948ea3456915a
misu <jungmisu801@gmail.com>
2022-11-04 22:22:25 +0900

    test: 수식 계산, 예외 케이스 테스트 작성

--- src/test/kotlin/step2/StringCalculatorTest.kt
+++ src/test/kotlin/step2/StringCalculatorTest.kt
@@ -37,4 +37,31 @@ class StringCalculatorTest {
         assertThat(stringCalculator.calculate(input)).isEqualTo(5)
     }
 
+    @Test
+    fun `올바른 수식을 입력한 경우`() {
+        val input = "2 + 3 * 4 / 2"
+        val stringCalculator = StringCalculator()
+
+        assertThat(stringCalculator.calculate(input)).isEqualTo(10)
+    }
+
+    @Test
+    fun `입력값이 null인 경우`() {
+        val input = null
+        val stringCalculator = StringCalculator()
+
+        assertThatThrownBy {
+            stringCalculator.calculate(input)
+        }.isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @Test
+    fun `사칙 연산 기호가 아닌 경우`() {
+        val input = "2 ? 3 * 4 / 2"
+        val stringCalculator = StringCalculator()
+
+        assertThatThrownBy {
+            stringCalculator.calculate(input)
+        }.isInstanceOf(IllegalArgumentException::class.java)
+    }
 }
