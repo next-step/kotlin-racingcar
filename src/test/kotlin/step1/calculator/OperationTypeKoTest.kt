eecd77fb40884d7d6e84cfc681b99784d4905d91
choi-ys <project.log.062@gmail.com>
2022-11-04 19:52:15 +0900

    test: JUnit5 기반으로 작성된 테스트를 kotest로 작성
    - JUnit5의 @ParameterizedTest를 대체하는 kotest의 forAll()을 이용한 여러 파라미터에 대한 테스트 작성
    - JUnit5의 assertThatExceptionOfType()을 대체하는 kotest shouldThrow()를 이용한 예외 발생 테스트 작성

---
+++ src/test/kotlin/step1/calculator/OperationTypeKoTest.kt
@@ -0,0 +1,61 @@
+package step1.calculator
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.forAll
+import io.kotest.data.headers
+import io.kotest.data.row
+import io.kotest.data.table
+import io.kotest.matchers.shouldBe
+
+internal class OperationTypeKoTest : StringSpec({
+    "문자열 연산자에 따른 연산자 타입을 반환한다." {
+        forAll(
+            table(
+                headers("문자열 연산자", "연산자 타입"),
+                row("+", OperationType.PLUS),
+                row("-", OperationType.MINUS),
+                row("*", OperationType.MULTIPLY),
+                row("/", OperationType.DIVIDE)
+            )
+        ) { given: String, expected: OperationType ->
+            OperationType.match(given) shouldBe expected
+        }
+    }
+
+    "정의된 연산자 타입이 아닌 경우 예외가 발생한다." {
+        val given = "%"
+        shouldThrow<IllegalArgumentException> {
+            OperationType.match(given)
+        }
+    }
+
+    "연산자 타입에 해당하는 수식을 연산한다." {
+        val leftTerm = 4.0
+        val rightTerm = 2.0
+        forAll(
+            table(
+                headers("문자열 연산자", "연산 결과 기대값"),
+                row("+", 6.0),
+                row("-", 2.0),
+                row("*", 8.0),
+                row("/", 2.0),
+            )
+        ) { given: String, expected: Double ->
+            OperationType.match(given).calculate(leftTerm, rightTerm) shouldBe expected
+        }
+    }
+
+    "제수가 0인 경우 에외가 발생한다." {
+        val operationType = OperationType.DIVIDE
+        val given = 0.0
+        forAll(
+            row(Double.POSITIVE_INFINITY),
+            row(Double.NEGATIVE_INFINITY)
+        ) { leftTerm: Double ->
+            shouldThrow<IllegalArgumentException> {
+                operationType.calculate(leftTerm, given)
+            }
+        }
+    }
+})
---
+++ src/test/kotlin/step1/calculator/StringCalculatorKoTest.kt
@@ -0,0 +1,72 @@
+package step1.calculator
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
+
+internal class StringCalculatorKoTest : StringSpec({
+    "덧셈 수식을 계산한다." {
+        forAll(
+            row("정수의 덧셈", "1 + 2", 3),
+            row("음수인 실수가 포함된 덧셈", "-1.2 + 2", 0.8)
+        ) { descrption: String, given: String, expected: Number ->
+            StringCalculator(given).calculate() shouldBe expected
+        }
+    }
+
+    "뺄셈 수식을 계산한다." {
+        forAll(
+            row("정수의 뺄셈", "2 - 1", 1),
+            row("음수가 포함된 뺄셈", "-2 - -2", 0),
+            row("음수인 실수가 포함된 뺄셈", "-2.1 - 2", -4.1)
+        ) { description: String, given: String, expected: Number ->
+            StringCalculator(given).calculate() shouldBe expected
+        }
+    }
+
+    "곱셈 수식을 계산한다." {
+        forAll(
+            row("정수의 곱셈", "2 * 1", 2),
+            row("음수의 곱셈", "-2 * -2", 4),
+            row("음수가 포함된 곱셈", "-2 * 2", -4),
+            row("음수인 실수가 포함된 뺄셈", "-2.1 * 2", -4.2)
+        ) { description: String, given: String, expected: Number ->
+            StringCalculator(given).calculate() shouldBe expected
+        }
+    }
+
+    "나눗셈 수식을 계산한다." {
+        forAll(
+            row("정수의 나눗셈", "2 / 2", 1),
+            row("음수의 나눗셈", "-2 / -2", 1),
+            row("음수가 포함된 나눗셈", "-2 / 2", -1),
+            row("음수인 실수가 포함된 나눗셈", "-2.1 / 2", -1.05)
+        ) { description: String, given: String, expected: Number ->
+            StringCalculator(given).calculate() shouldBe expected
+        }
+    }
+
+    "사칙 연산을 모두 포함한 표현식을 계산한다." {
+        val given = "1 - 2 + 3 * 4 / 5"
+        val actual = StringCalculator(given).calculate()
+        actual shouldBe 1.6
+    }
+
+    "입력값이 유효하지 않은 경우 경우 예외가 발생한다." {
+        forAll(
+            row("빈값", ""),
+            row("공백", ""),
+            row("공백으로 구분되지 않은 경우", "1+1"),
+            row("계산 항목이 부족한 표현식", " 1 "),
+            row("계산 항목이 포함되지 않은 표현식", " +++ "),
+            row("계산 항목이 올바르지 않은 표현식", " 1++ "),
+            row("계산 항목이 올바르지 않은 표현식", " +1+ "),
+        ) { description: String, given: String ->
+            shouldThrow<IllegalArgumentException> {
+                StringCalculator(given)
+            }
+        }
+    }
+})
