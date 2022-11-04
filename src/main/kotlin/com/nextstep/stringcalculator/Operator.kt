60311e6d03dea5cbd50531190a9c12450c760fab
chomily <chomily@woowahan.com>
2022-11-04 18:58:21 +0900

    feat: add find function to convert string input into Operator

--- src/main/kotlin/com/nextstep/stringcalculator/Operator.kt
+++ src/main/kotlin/com/nextstep/stringcalculator/Operator.kt
@@ -7,4 +7,11 @@ enum class Operator(val symbol: String, val calculate: (Long, Long) -> Long) {
     DIVIDE("/", { x, y -> x / y });
 
     fun apply(leftNumber: Long, rightNumber: Long): Long = this.calculate(leftNumber, rightNumber)
+
+    companion object {
+        fun find(input: String): Operator {
+            val found = Operator.values().find { it.symbol == input }
+            return found ?: throw IllegalArgumentException("string operator should be one of (+, -, *, /), it was: $input")
+        }
+    }
 }
--- src/test/kotlin/com/nextstep/stringcalculator/OperatorTest.kt
+++ src/test/kotlin/com/nextstep/stringcalculator/OperatorTest.kt
@@ -4,8 +4,10 @@ import com.nextstep.stringcalculator.Operator.DIVIDE
 import com.nextstep.stringcalculator.Operator.MINUS
 import com.nextstep.stringcalculator.Operator.MULTIPLY
 import com.nextstep.stringcalculator.Operator.PLUS
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.throwable.shouldHaveMessage
 
 class OperatorTest : BehaviorSpec({
 
@@ -35,6 +37,52 @@ class OperatorTest : BehaviorSpec({
             then("divide left number by right number") {
                 val result: Long = DIVIDE.apply(10, 2)
                 result shouldBe 5
+            }
+        }
+    }
+
+    given("Operator.find(string input)") {
+        `when`("'+' is passed") {
+            then("PLUS will be returned") {
+                val result: Operator = Operator.find("+")
+                result shouldBe PLUS
+            }
+        }
+
+        `when`("'-' is passed") {
+            then("MINUS will be returned") {
+                val result: Operator = Operator.find("-")
+                result shouldBe MINUS
+            }
+        }
+
+        `when`("'*' is passed") {
+            then("MULTIPLY will be returned") {
+                val result: Operator = Operator.find("*")
+                result shouldBe MULTIPLY
+            }
+        }
+
+        `when`("'/' is passed") {
+            then("DIVIDE will be returned") {
+                val result: Operator = Operator.find("/")
+                result shouldBe DIVIDE
+            }
+        }
+
+        `when`("null or empty string is passed") {
+            then("IllegalArgumentException will be thrown") {
+                shouldThrow<IllegalArgumentException> {
+                    Operator.find(" ")
+                } shouldHaveMessage "string operator should be one of (+, -, *, /), it was:  "
+            }
+        }
+
+        `when`("what is not defined in Operator is passed") {
+            then("IllegalArgumentException will be thrown") {
+                shouldThrow<IllegalArgumentException> {
+                    Operator.find("#")
+                } shouldHaveMessage "string operator should be one of (+, -, *, /), it was: #"
             }
         }
     }
