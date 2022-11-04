c252f22036963723d11258c588009155179e7c5d
chayoung-choi <chayoung.dev@gmail.com>
2022-11-04 17:19:32 +0900

    refactor: StringCalculator 객체화

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -1,46 +1,32 @@
+package calculator
+
 import java.util.StringTokenizer
 
-var result = 0
-fun calculator(str: String?): Int {
-    if (str.isNullOrBlank()) {
-        throw IllegalArgumentException("입력값이 null이거나 빈 공백입니다.")
-    }
+/**
+ * 입력 받은 문자열 계산기
+ */
+class StringCalculator {
 
-    val st = StringTokenizer(str)
-    while (st.hasMoreTokens()) {
-        when (val el = st.nextToken()) {
-            "+" -> plus(st.nextToken())
-            "-" -> minus(st.nextToken())
-            "*" -> multiply(st.nextToken())
-            "/" -> divide(st.nextToken())
-            else -> result = parseInt(el)
+    fun calculator(input: String?): Int {
+
+        require(!input.isNullOrBlank()) { "입력값이 null 혹은 공백일 수 없습니다." }
+
+        val st = StringTokenizer(input)
+        var result = st.nextToken().toInt()
+
+        while (st.hasMoreTokens()) {
+            val symbol = st.nextToken()
+            val nextNumber = parsedInt(st.nextToken())
+            result = Operator.of(symbol).operate(result, nextNumber)
         }
+        return result
     }
-    return result
-}
 
-private fun plus(el: String?) {
-    result += parseInt(el)
-}
-
-private fun minus(el: String?) {
-    result -= parseInt(el)
-}
-
-private fun multiply(el: String?) {
-    result *= parseInt(el)
-}
-
-private fun divide(el: String?) {
-    val num = parseInt(el)
-    if (num == 0) throw IllegalArgumentException("0으로 나눌 수 없습니다.")
-    result /= num
-}
-
-private fun parseInt(el: String?): Int {
-    try {
-        return Integer.parseInt(el)
-    } catch (e: NumberFormatException) {
-        throw IllegalArgumentException("숫자가 아닙니다.")
+    private fun parsedInt(str: String): Int {
+        try {
+            return str.toInt()
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException("잘못된 입력 순서입니다.")
+        }
     }
 }
