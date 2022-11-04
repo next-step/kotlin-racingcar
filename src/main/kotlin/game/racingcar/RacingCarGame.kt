bbd9a98d6c0a0c058a4fa1e928b890d7deef0aba
harris.yi <harrisleesh@gmail.com>
2022-11-04 22:58:55 +0900

    feature: Create RacingCarGame with main function in top level

---
+++ src/main/kotlin/game/racingcar/RacingCarGame.kt
@@ -0,0 +1,31 @@
+package game.racingcar
+
+import game.racingcar.view.InputView
+import game.racingcar.view.OutputView
+
+class RacingCarGame(
+    private val numberOfCars: Int,
+    private val numberOfTrials: Int,
+) {
+    fun play(): MutableList<List<Int>> {
+        val locationSnapshots = mutableListOf<List<Int>>()
+
+        val racingCars = RacingCars(numberOfCars)
+        repeat(numberOfTrials) {
+            with(racingCars) {
+                moveAll()
+                locationSnapshots.add(locationSnapshot())
+            }
+        }
+        return locationSnapshots
+    }
+}
+
+fun main() {
+    val (numberOfCars, numberOfTrials) = InputView.getInputFromConsole()
+
+    val racingCarGame = RacingCarGame(numberOfCars, numberOfTrials)
+    val snapshots = racingCarGame.play()
+
+    OutputView.printOutputToConsole(snapshots)
+}
