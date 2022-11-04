97dadb7546823fb6ebddad292031e160907d5b73
chomily <chomily@woowahan.com>
2022-11-04 20:20:16 +0900

    test: StringCalculator can not receive blank string

--- src/main/kotlin/com/nextstep/stringcalculator/StringCalculator.kt
+++ src/main/kotlin/com/nextstep/stringcalculator/StringCalculator.kt
@@ -1,8 +1,11 @@
 package com.nextstep.stringcalculator
 
 class StringCalculator(private val expression: String) {
-    val elements: List<Element> = expression.split(" ")
-        .mapIndexed { idx, s -> if (isNumber(idx)) Number(s) else Operator.find(s) }
-
+    val elements: List<Element>
+    init {
+        if (expression.isBlank()) throw IllegalArgumentException("Input shouldn't be blank")
+        elements = expression.split(" ")
+            .mapIndexed { idx, s -> if (isNumber(idx)) Number(s) else Operator.find(s) }
+    }
     private fun isNumber(idx: Int) = idx % 2 == 0
 }
--- src/test/kotlin/com/nextstep/stringcalculator/StringCalculatorTest.kt
+++ src/test/kotlin/com/nextstep/stringcalculator/StringCalculatorTest.kt
@@ -1,13 +1,21 @@
 package com.nextstep.stringcalculator
 
 import com.nextstep.stringcalculator.Operator.PLUS
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.collections.shouldContainExactly
+import io.kotest.matchers.throwable.shouldHaveMessage
 
 class StringCalculatorTest : FunSpec({
 
     test("StringCalculator should take a string expression and split it by ' ' and convert into a list of Element instance") {
         val stringCalculator = StringCalculator("1 + 2")
         stringCalculator.elements shouldContainExactly listOf(Number("1"), PLUS, Number("2"))
+    }
+
+    test("If StringCalculator take empty string, throw IllegalArgumentException") {
+        shouldThrow<IllegalArgumentException> {
+            StringCalculator(" ")
+        } shouldHaveMessage "Input shouldn't be blank"
     }
 })
