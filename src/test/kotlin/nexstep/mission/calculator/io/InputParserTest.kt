4b25b4f8cba85229604a4966c1267c22f9698b6b
kimseonghun <seonghun127@woowahan.com>
2022-11-04 21:02:29 +0900

    test: 테스트 코드 추가

--- src/test/kotlin/nexstep/mission/calculator/io/InputParserTest.kt
+++ src/test/kotlin/nexstep/mission/calculator/io/InputParserTest.kt
@@ -1,12 +1,21 @@
 package nexstep.mission.calculator.io
 
+import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.collections.shouldContainInOrder
+import io.kotest.matchers.shouldBe
 
 class InputParserTest : FunSpec({
 
     test("입력으로 들어온 문자열을 문자열 리스트로 변환한다.") {
         val input = "2 + 4 * 10 / 3"
         InputParser.parse(input) shouldContainInOrder listOf("2", "+", "4", "*", "10", "/", "3")
+    }
+
+    test("입력으로 들어온 문자열이 비어있으면 예외를 던진다.") {
+        val exception = shouldThrowExactly<IllegalArgumentException> {
+            InputParser.parse("")
+        }
+        exception.message shouldBe "입력값을 제대로 입력해주세요."
     }
 })
