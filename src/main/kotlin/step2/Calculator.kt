e78dade56ab834500dd9cab6d9574ed9b90d10ed
Jo-GyuHyeon <kuhyun1993@gmail.com>
2022-11-04 16:39:42 +0900

    feat: 문자열 계산기 구현
    - 문자열 계산기 테스트 작성
    사칙 연산을 모두 포함하는 기능 구현

---
+++ src/main/kotlin/step2/Calculator.kt
@@ -0,0 +1,13 @@
+package step2
+
+class Calculator {
+    private fun Double.calculate(expression: Pair<Operator, Double>): Double =
+        expression.let { (operator, operand) ->
+            operator(this, operand)
+        }
+
+    fun calculate(rawExpress: String): Double =
+        Parser().parse(rawExpress).fold(0.0) { acc, expression ->
+            acc.calculate(expression)
+        }
+}
