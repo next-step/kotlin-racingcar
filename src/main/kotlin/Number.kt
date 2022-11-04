cfe0a13d3f5d47e4e9e820f89812f6a5b4480df5
dk-ko <dev.dkko@gmail.com>
2022-11-04 11:37:14 +0900

    conflict

--- src/main/kotlin/Number.kt
+++ src/main/kotlin/Number.kt
@@ -1,11 +1,6 @@
 import java.lang.IllegalArgumentException
 
 data class Number(private val value: String) {
-    fun add(number: Number): Number {
-        val result = value.toInt().plus(number.value.toInt())
-        return Number(result.toString())
-    }
-
     fun operate(beforeOperator: Operator, value: Number): Number {
         return when (beforeOperator) {
             Operator.ADD -> Number(this.value.toInt().plus(value.value.toInt()).toString())
