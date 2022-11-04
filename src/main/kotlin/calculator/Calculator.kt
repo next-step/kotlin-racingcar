a49a21074b4885f61cd4b7d268a6a40f2ea61a74
stkment <stkment@gmail.com>
2022-11-04 23:56:00 +0900

    :necktie: 연산자, 숫자 분리

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -4,11 +4,18 @@ class Calculator {
 
     fun calculate(operation: String?) {
         require(!operation.isNullOrBlank())
-        require(isValid(operation))
+        val (operator, numbers) = partitionFormula(operation)
+        require(operator.size == numbers.size + DEFAULT_OFFSET)
         Operator("")
     }
 
-    private fun isValid(operation: String): Boolean {
-        TODO()
+    private fun partitionFormula(operation: String) =
+        operation.split(DELIMITER_WHITE_SPACE)
+            .partition { it in Operator.OPERATORS }
+            .let { it.first to it.second.mapNotNull { it.toIntOrNull() } }
+
+    companion object {
+        private const val DELIMITER_WHITE_SPACE = " "
+        private const val DEFAULT_OFFSET = 1
     }
 }
