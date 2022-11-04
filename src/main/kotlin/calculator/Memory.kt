7c10d0904c38de16a26d20688ff58d6773698ab5
jpoh281 <hjp4869@naver.com>
2022-11-04 22:40:19 +0900

    refactor: Memory class를 data class로 변경
    https://github.com/next-step/kotlin-racingcar/pull/738#discussion_r1013983451

--- src/main/kotlin/calculator/Memory.kt
+++ src/main/kotlin/calculator/Memory.kt
@@ -1,6 +1,6 @@
 package calculator
 
-class Memory(private var terms: List<String> = emptyList(), var accumulator: Int = 0) {
+data class Memory(private var terms: List<String> = emptyList(), var accumulator: Int = 0) {
 
     fun store(terms: List<String>) {
         this.terms = terms
@@ -18,23 +18,5 @@ class Memory(private var terms: List<String> = emptyList(), var accumulator: Int
     fun reset() {
         terms = emptyList()
         accumulator = 0
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Memory
-
-        if (terms != other.terms) return false
-        if (accumulator != other.accumulator) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = terms.hashCode()
-        result = 31 * result + accumulator
-        return result
     }
 }
