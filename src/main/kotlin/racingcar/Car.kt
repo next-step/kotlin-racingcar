ca359f12430840484f71832afe8d7af52edbc6e0
giyeon95 <giyeon15@gmail.com>
2022-11-06 00:51:05 +0900

    refactor: 매직넘버 제거

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -7,6 +7,8 @@ private const val START = 0
 private const val FORWARD = 4
 private const val LIMIT = 10
 
+private const val MOVE_POS = 1
+
 class Car(
     val pos: Int = 0
 ) {
@@ -14,7 +16,7 @@ class Car(
     fun race(rnd: Random): Car {
         return when (rnd.nextInt(LIMIT)) {
             in START until FORWARD -> Car(pos)
-            in FORWARD until LIMIT -> Car(pos + 1)
+            in FORWARD until LIMIT -> Car(pos + MOVE_POS)
             else -> throw IllegalArgumentException("System error")
         }
     }
--- src/main/kotlin/racingcar/RaceStage.kt
+++ src/main/kotlin/racingcar/RaceStage.kt
@@ -4,13 +4,15 @@ import kotlin.random.Random
 
 private const val INIT_ROUND = 0
 
+private const val MOVE_ROUND = 1
+
 class RaceStage(
     val cars: List<Car>,
     val round: Int
 ) {
 
     private val nextRound
-        get() = round + 1
+        get() = round + MOVE_ROUND
 
     init {
         require(cars.isNotEmpty()) { "경주에 참여하는 자동차는 1개 이상이어야 합니다." }
