3187ffa40681aa60b1d74eb4a1841428a7d04e3a
jpoh281 <hjp4869@naver.com>
2022-11-05 14:24:20 +0900

    refactor: Calculator의 parse를 CPU에게이동
    https://github.com/next-step/kotlin-racingcar/pull/738#pullrequestreview-1168742728
    해당 피드백을 보면서 느낀게, Calculator는 각 부품들을 동작시키는 역할만 해야하고,
    그 외에 어떤일도 하면 안될것같다는 생각이 들었습니다.

--- src/main/kotlin/calculator/CPU.kt
+++ src/main/kotlin/calculator/CPU.kt
@@ -1,8 +1,28 @@
 package calculator
 
 class CPU {
-    fun add(i: Int, j: Int): Int = i + j
-    fun subtract(i: Int, j: Int): Int = i - j
-    fun multiply(i: Int, j: Int): Int = i * j
-    fun divide(i: Int, j: Int): Int = i / j
+    private fun add(i: Int, j: Int): Int = i + j
+    private fun subtract(i: Int, j: Int): Int = i - j
+    private fun multiply(i: Int, j: Int): Int = i * j
+    private fun divide(i: Int, j: Int): Int = i / j
+
+    fun execute(operator: Operator, operand1: Int, operand2: Int): Int {
+        return when (operator) {
+            Operator.ADD -> {
+                add(operand1, operand2)
+            }
+
+            Operator.SUBTRACT -> {
+                subtract(operand1, operand2)
+            }
+
+            Operator.MULTIPLY -> {
+                multiply(operand1, operand2)
+            }
+
+            Operator.DIVIDE -> {
+                divide(operand1, operand2)
+            }
+        }
+    }
 }
--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -17,6 +17,7 @@ class Calculator(
         } else {
             keypad.pressed(input)
         }
+
         val terms = covertExpressionToTerm(expression)
         memory.store(terms)
     }
@@ -27,7 +28,7 @@ class Calculator(
             val operator = Operator.findByValue(memory.fetch(index))
             val operand = memory.fetch(index + 1).toInt()
 
-            val result = parse(operator, accumulator, operand)
+            val result = cpu.execute(operator, accumulator, operand)
             memory.accumulate(result)
         }
 
@@ -37,25 +38,5 @@ class Calculator(
 
     private fun covertExpressionToTerm(expression: String): List<String> {
         return expression.split(" ")
-    }
-
-    private fun parse(operator: Operator, operand1: Int, operand2: Int): Int {
-        return when (operator) {
-            Operator.ADD -> {
-                cpu.add(operand1, operand2)
-            }
-
-            Operator.SUBTRACT -> {
-                cpu.subtract(operand1, operand2)
-            }
-
-            Operator.MULTIPLY -> {
-                cpu.multiply(operand1, operand2)
-            }
-
-            Operator.DIVIDE -> {
-                cpu.divide(operand1, operand2)
-            }
-        }
     }
 }
