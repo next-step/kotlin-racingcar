21bbda97748c723398e356785d9f858ebe4cbcae
wonjun3991 <wonjun.choi@meshkorea.net>
2022-11-05 22:02:01 +0900

    test(Calculator): add Calculator Test
    add Calculator Test By Requirements

---
+++ src/main/kotlin/study/step2/Calculator.kt
@@ -0,0 +1,7 @@
+package study.step2
+
+class Calculator {
+    fun calculate(inputs: String): Int {
+        return 0
+    }
+}
---
+++ src/test/kotlin/study/step2/CalculatorTest.kt
@@ -0,0 +1,73 @@
+package study.step2
+
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class CalculatorTest {
+    @Test
+    fun `덧셈`() {
+        val calc = Calculator()
+        val input = "4 + 7"
+
+        assertThat(11).isEqualTo(calc.calculate(input))
+    }
+
+    @Test
+    fun `뺄셈`() {
+        val calc = Calculator()
+        val input = "7 - 4"
+
+        assertThat(3).isEqualTo(calc.calculate(input))
+    }
+
+    @Test
+    fun `곱셈`() {
+        val calc = Calculator()
+        val input = "3 * 4"
+
+        assertThat(12).isEqualTo(calc.calculate(input))
+    }
+
+    @Test
+    fun `나눗셈`() {
+        val calc = Calculator()
+        val input = "4 / 2"
+
+        assertThat(2).isEqualTo(calc.calculate(input))
+    }
+
+    @Test
+    fun `사칙연산`() {
+        val calc = Calculator()
+        val input = "2 + 3 - 3"
+
+        assertThat(2).isEqualTo(calc.calculate(input))
+    }
+
+    @Test
+    fun `사칙연산의 순서는 계산 우선순위가 아니라 입력 순서를 따른다`() {
+        val calc = Calculator()
+        val input = "3 * 2 + 2 / 2"
+
+        assertThat(4).isEqualTo(calc.calculate(input))
+    }
+
+    @Test
+    fun `입력값이 null이거나 빈 공백 문자일 경우 IllegalArgumentException Throw`() {
+        Assertions.assertThatThrownBy {
+            val calc = Calculator()
+            val input = ""
+            calc.calculate(input)
+        }.isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @Test
+    fun `사칙연산 기호가 아닌 경우 IllegalArgumentException throw`() {
+        Assertions.assertThatThrownBy {
+            val calc = Calculator()
+            val input = "3 & 5"
+            calc.calculate(input)
+        }.isInstanceOf(IllegalArgumentException::class.java)
+    }
+}
