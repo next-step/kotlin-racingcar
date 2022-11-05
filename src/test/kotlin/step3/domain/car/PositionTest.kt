110d05a61be180872ec15ee6c82989b37de79122
EP <kjuioqq8@gmail.com>
2022-11-05 17:46:56 +0900

    test: 위치 객체는 값에 따라 동등성이 보장된다

---
+++ src/test/kotlin/step3/domain/car/PositionTest.kt
@@ -0,0 +1,20 @@
+package step3.domain.car
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+
+internal class PositionTest : StringSpec({
+    "위치 값이 같으면 두 위치는 동등성이 보장된다" {
+        val first = Position()
+        val second = Position()
+        first shouldBe second
+    }
+
+    "위치 값이 다르면 두 위치는 다르게 표현된다" {
+        val first = Position()
+        val second = Position()
+        second.forward()
+        first shouldNotBe second
+    }
+})
