5b1baa3760b8bdc6470310afca74663840e32c03
harris.yi <harrisleesh@gmail.com>
2022-11-05 22:11:05 +0900

    test: Add tests as changed dependency

---
+++ src/test/kotlin/game/racingcar/RacingCarGameTest.kt
@@ -0,0 +1,24 @@
+package game.racingcar
+
+import game.racingcar.move.AlwaysMoveStrategy
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class RacingCarGameTest() {
+
+    @Test
+    fun `RacingCarGame이 AlwaysMoveStrategy로 생성되었을 때 위치 스냅샷이 1씩 증가한다`() {
+        // given
+        val racingCarGame = RacingCarGame(3, 5, AlwaysMoveStrategy())
+
+        // when
+        val locationSnapshots = racingCarGame.play()
+
+        // then
+        locationSnapshots.forEachIndexed { expected, locationOfCars ->
+            locationOfCars.locations.forEach { location ->
+                assertThat(location).isEqualTo(expected + 1)
+            }
+        }
+    }
+}
--- src/test/kotlin/game/racingcar/RacingCarsTest.kt
+++ src/test/kotlin/game/racingcar/RacingCarsTest.kt
@@ -1,5 +1,6 @@
 package game.racingcar
 
+import game.racingcar.move.AlwaysMoveStrategy
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -14,6 +15,19 @@ internal class RacingCarsTest {
         // then
         snapshot.forEach {
             assertThat(it).isEqualTo(0)
+        }
+    }
+
+    @Test
+    fun `RacingCars가 AlwaysMoveStrategy로 생성되었을 때 모두 위치가 1씩 증가한다`() {
+        // given
+        val racingCars = RacingCars(3, AlwaysMoveStrategy())
+        // when
+        racingCars.moveAll()
+        val snapshot = racingCars.locations()
+        // then
+        snapshot.forEach {
+            assertThat(it).isEqualTo(1)
         }
     }
 }
