7eab7d654100341e01a63e96cdb5541965b98bf5
EP <kjuioqq8@gmail.com>
2022-11-05 17:40:06 +0900

    test: 자동차가 움직이면 위치가 변한다

--- src/test/kotlin/step3/domain/car/CarTest.kt
+++ src/test/kotlin/step3/domain/car/CarTest.kt
@@ -2,11 +2,19 @@ package step3.domain.car
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldNotBe
 
 internal class CarTest : StringSpec({
     "자동차는 위치값을 갖는다" {
         val car = Car()
         val initPosition = car.isCurrentPosition()
         initPosition.shouldNotBeNull()
+    }
+
+    "자동차는 움직이면 위치값이 변한다" {
+        val car = Car()
+        val initPosition = car.isCurrentPosition()
+        val postPosition = car.move()
+        initPosition shouldNotBe postPosition
     }
 })
