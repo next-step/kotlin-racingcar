651490723c9cd4281b671bc8ab3416c7a094d7a4
김신영 <rolroralra@gmail.com>
2022-11-05 23:58:35 +0900

    feat: BinaryOperator, ToInt interface segregation
    - BinaryOperator(interface), Operator(enum)
    - ToInt(interface), StringToInt(implementation)

--- src/main/kotlin/step2/StringCalculator.kt
+++ src/main/kotlin/step2/StringCalculator.kt
@@ -1,20 +1,16 @@
 package step2
 
-import java.lang.NumberFormatException
+import step2.converter.StringToInt
+import step2.converter.ToInt
+import step2.operator.Operator
 
 class StringCalculator(
-    private val plus: String = "+",
-    private val minus: String = "-",
-    private val times: String = "*",
-    private val div: String = "/",
-    private val rem: String = "%",
     private val spaceCharacter: String = " ",
-    private val toNumber: (String) -> Int = String::toInt
-) {
+) : ToInt<String> by StringToInt() {
+
     fun calculate(expression: String?): Int {
-        if (expression.isNullOrBlank()) {
-            throw IllegalArgumentException("${ErrorMessage.NULL_OR_BLANK}")
-        }
+        requireNotNull(expression) { "${ErrorMessage.NULL_OR_BLANK}" }
+        require(expression.isNotBlank()) { "${ErrorMessage.NULL_OR_BLANK}" }
 
         val expressionList = expression.split(spaceCharacter).filter { it.isNotBlank() }
 
@@ -22,29 +18,18 @@ class StringCalculator(
     }
 
     private fun calculate(expressionList: List<String>): Int {
-        var accumulator = toValue(expressionList[0])
-        for (i in 1 until expressionList.size step 2) {
-            if (i + 1 >= expressionList.size) {
-                throw IllegalArgumentException("${ErrorMessage.NOT_ENOUGH_OPERAND}")
-            }
+        val numberList = expressionList.slice(expressionList.indices step 2).map { toInt(it) }
+        val operatorList = expressionList.slice(1 until expressionList.size step 2).map { Operator.of(it) }
 
-            accumulator = operator(expressionList[i])(accumulator, toValue(expressionList[i + 1]))
+        require(numberList.size == operatorList.size + 1) { "${ErrorMessage.NOT_ENOUGH_OPERAND}" }
+
+        val numberIterator = numberList.iterator()
+        var accumulator = numberIterator.next()
+
+        for (operator in operatorList) {
+            accumulator = operator.operate(accumulator, numberIterator.next())
         }
 
         return accumulator
-    }
-
-    private fun operator(operator: String): (Int, Int) -> Int =
-        when (operator) {
-            plus -> Int::plus
-            minus -> Int::minus
-            times -> Int::times
-            div -> Int::div
-            rem -> Int::rem
-            else -> throw IllegalArgumentException("${ErrorMessage.INVALID_OPERATOR} [$operator]")
-        }
-
-    private fun toValue(expression: String): Int = try { toNumber(expression) } catch (e: NumberFormatException) {
-        throw IllegalArgumentException("${ErrorMessage.INVALID_NUMBER_FORMAT} [$expression]")
     }
 }
