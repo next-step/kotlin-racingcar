aa78eb1bbbcba46499786af6d00f4633c7bfb513
youngbin-yun <youngbin@koreaspacedata.com>
2022-11-06 17:23:52 +0900

    feat(InputValidator): 입력값 형식을 검증하는 클래스 생성

---
+++ src/main/kotlin/step3_racingcar/InputValidator.kt
@@ -0,0 +1,11 @@
+package step3_racingcar
+
+import step3_racingcar.util.extension.isPositive
+
+class InputValidator {
+    fun inputValidate(input: String?) {
+        require(!input.isNullOrBlank()) { "입력 값은 공백 또는 빈 문자열일 수 없습니다." }
+        val number: Int = requireNotNull(input.toIntOrNull()) { "입력 값은 정수 여야 합니다." }
+        require(number.isPositive()) { "입력 값은 1 이상이어야 합니다." }
+    }
+}
