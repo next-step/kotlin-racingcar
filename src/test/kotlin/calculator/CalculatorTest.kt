c7b7dd86de10c699d9cc16ab2e8557dd9f64c9b8
Yujin Park <uiuiui118437@gmail.com>
2022-11-07 00:22:53 +0900

    feat: 케이스 추가 및 given과 when의 조건 변경

--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -7,9 +7,9 @@ import io.kotest.inspectors.forAll
 import io.kotest.matchers.shouldBe
 
 class CalculatorTest : BehaviorSpec({
-    Given("사용자가 계산기를 사용할 때") {
-        When("입력값이 null이거나 빈 공백 문자를 입력하면") {
-            val invalidInputs = listOf(null, "", " ")
+    val invalidInputs = listOf(null, "", " ")
+    Given("null 또는 빈 공백 문자가") {
+        When("계산기에 입력되면") {
             Then("IllegalArgumentException 예외가 발생한다.") {
                 invalidInputs.forAll {
                     shouldThrow<IllegalArgumentException> {
@@ -18,16 +18,35 @@ class CalculatorTest : BehaviorSpec({
                 }
             }
         }
+    }
 
-        When("수식을 입력하면") {
-            val expressions = listOf(
-                "2 + 3 * 4 / 2" to 10,
-                "3 * 2 - 3 + 4" to 7,
-                "10 / 2 + 3 * 2" to 16
-            )
+    val validExpressions = listOf(
+        "2 + 3 * 4 / 2" to 10,
+        "3 * 2 - 3 + 4" to 7,
+        "10 / 2 + 3 * 2" to 16
+    )
+    Given("유효한 수식이") {
+        When("계산기에 입력되면") {
             Then("결과값을 출력한다.") {
-                expressions.forAll { (expression, result) ->
+                validExpressions.forAll { (expression, result) ->
                     calculate(expression) shouldBe result
+                }
+            }
+        }
+    }
+
+    val invalidExpressions = listOf(
+        "+ 3 * 4 / 2",
+        "3 * 2 -",
+        "10 / 2 + *"
+    )
+    Given("유효하지 않은 수식이") {
+        When("계산기에 입력되면") {
+            Then("예외가 발생한다.") {
+                invalidExpressions.forAll {
+                    shouldThrow<IllegalArgumentException> {
+                        calculate(it)
+                    }
                 }
             }
         }
--- src/test/kotlin/calculator/OperatorTest.kt
+++ src/test/kotlin/calculator/OperatorTest.kt
@@ -7,18 +7,18 @@ import io.kotest.matchers.collections.shouldBeIn
 import io.kotest.matchers.shouldBe
 
 class OperatorTest : BehaviorSpec({
-    Given("사용자가 계산기를 사용할 때") {
-        When("사칙연산자 기호를 입력하면") {
-            val validInputs = listOf("+", "-", "*", "/")
-            Then("기호를 통해 해당 Operator를 찾을 수 있다.") {
+    Given("연산자 기호가") {
+        val validInputs = listOf("+", "-", "*", "/")
+        When("사칙 연산자에 해당하는 기호이면") {
+            Then("해당 Operator를 찾을 수 있다.") {
                 validInputs.forAll {
                     Operator.symbolOf(it) shouldBeIn Operator.values()
                 }
             }
         }
 
-        When("사칙연산자 이외의 기호를 입력하면") {
-            val invalidInputs = listOf("!", "#", "$", "a", "0")
+        val invalidInputs = listOf("!", "#", "$", "a", "0")
+        When("사칙연산자 외의 기호이면") {
             Then("IllegalArgumentException 예외가 발생한다.") {
                 invalidInputs.forAll {
                     shouldThrow<IllegalArgumentException> {
@@ -27,44 +27,67 @@ class OperatorTest : BehaviorSpec({
                 }
             }
         }
+    }
 
-        // 3 + 2 = 5
-        When("축적값과 더하기 연산자, 피연산자가 들어오면") {
-            val accumulation = 3
-            val operator = Operator.symbolOf("+")
-            val operand = 2
-            Then("축적값에 피연산자가 더해진다.") {
-                operator.operate(accumulation, operand) shouldBe 5
+    // 3 + 2 = 5
+    val plusOperator = Operator.symbolOf("+")
+    val plusAccumulation = 3
+    val plusOperand = 2
+    Given("더하기 연산자 기호이고 축적값이 3, 피연산자가 2일 때") {
+        When("연산이 실행되면") {
+            val result = plusOperator.operate(plusAccumulation, plusOperand)
+            Then("결과값은 5이다.") {
+                result shouldBe 5
             }
         }
+    }
 
-        // 5 - 2 = 3
-        When("축적값과 빼기 연산자, 피연산자가 들어오면") {
-            val accumulation = 5
-            val operator = Operator.symbolOf("-")
-            val operand = 2
-            Then("축적값에 피연산자가 빼진다.") {
-                operator.operate(accumulation, operand) shouldBe 3
+    // 5 - 2 = 3
+    val minusOperator = Operator.symbolOf("-")
+    val minusAccumulation = 5
+    val minusOperand = 2
+    Given("빼기 연산자 기호이고 축적값이 5, 피연산자가 2일 때") {
+        When("연산이 실행되면") {
+            val result = minusOperator.operate(minusAccumulation, minusOperand)
+            Then("결과값은 3이다.") {
+                result shouldBe 3
             }
         }
+    }
 
-        // 3 * 2 = 6
-        When("축적값과 곱하기 연산자, 피연산자가 들어오면") {
-            val accumulation = 3
-            val operator = Operator.symbolOf("*")
-            val operand = 2
-            Then("축적값에 피연산자가 곱해진다.") {
-                operator.operate(accumulation, operand) shouldBe 6
+    // 3 * 2 = 6
+    val multiplyOperator = Operator.symbolOf("*")
+    val multiplyAccumulation = 3
+    val multiplyOperand = 2
+    Given("곱하기 연산자 기호이고 축적값이 3, 피연산자가 2일 때") {
+        When("연산이 실행되면") {
+            val result = multiplyOperator.operate(multiplyAccumulation, multiplyOperand)
+            Then("결과값은 6이다.") {
+                result shouldBe 6
             }
         }
+    }
 
-        // 6 / 2 = 3
-        When("축적값과 나누기 연산자, 피연산자가 들어오면") {
-            val accumulation = 6
-            val operator = Operator.symbolOf("/")
-            val operand = 2
-            Then("축적값에 피연산자가 나눠진다.") {
-                operator.operate(accumulation, operand) shouldBe 3
+    // 6 / 2 = 3
+    val divideOperator = Operator.symbolOf("/")
+    val divideAccumulation = 6
+    val divideOperand = 2
+    Given("나누기 연산자 기호이고 축적값이 6, 피연산자가 2일 때") {
+        When("연산이 실행되면") {
+            val result = divideOperator.operate(divideAccumulation, divideOperand)
+            Then("결과값은 3이다.") {
+                result shouldBe 3
+            }
+        }
+    }
+
+    // 6 / 0 -> throw ArithmeticException
+    val divideZeroOperand = 0
+    Given("나누기 연산자 기호이고 축적값이 6, 피연산자가 0일 때") {
+        When("연산이 실행되면") {
+            Then("ArithmeticException 예외가 발생한다.")
+            shouldThrow<ArithmeticException> {
+                divideOperator.operate(divideAccumulation, divideZeroOperand)
             }
         }
     }
