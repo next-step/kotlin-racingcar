1646013a4027132f28d73933ccec51486a2694b4
Shin seongsu <tlstjdtn321@naver.com>
2022-11-07 00:08:31 +0900

    test: 테스트 코드 작성

---
+++ src/test/kotlin/calculator/domain/CalculatorTest.kt
@@ -0,0 +1,31 @@
+package calculator.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
+import org.assertj.core.util.Lists
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class CalculatorTest {
+
+    @DisplayName("계산할 수 없는 수식일때 에러 반환")
+    @Test
+    fun `계산할 수없는 수식일떄 에러반환`() {
+        assertThatIllegalArgumentException()
+            .isThrownBy { Calculator(Lists.newArrayList("1", "+")) }
+    }
+
+    @DisplayName("연산자와 피연산자의 올바른 순서가 아니면 에러 반환")
+    @Test
+    fun `연산자와 피연산자의 순서가 올바르지 않을때 에러를 반환`() {
+        assertThatIllegalArgumentException()
+            .isThrownBy { Calculator(Lists.newArrayList("+", "1")) }
+    }
+
+    @DisplayName("올바른 순서와 수식일때는 정상 반환")
+    @Test
+    fun `올바른 순서와 수식일때는 정상 반환`() {
+        assertThat(Calculator(Lists.newArrayList("1", "+", "1")).execute()).isEqualTo(2.0)
+    }
+
+}
---
+++ src/test/kotlin/calculator/domain/OperandTest.kt
@@ -0,0 +1,24 @@
+package calculator.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class OperandTest {
+
+    @DisplayName("올바른 피연산자 타입이 아닐경우 에러 반환")
+    @Test
+    fun `올바른 피연산자 타입이 아닐경우 에러 반환`() {
+        assertThatIllegalArgumentException()
+            .isThrownBy { Operand.from("+") }
+            .withMessage("올바르지 않은 피연산자 타입입니다")
+    }
+
+    @DisplayName("정상적으로 값이 들어간 케이스")
+    @Test
+    fun `정상적으로 값이 들어간 케이스`() {
+        assertThat(Operand.from("1").number).isEqualTo(1.0)
+    }
+
+}
