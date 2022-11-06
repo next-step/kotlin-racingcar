8d6372d788cdccc32950fcf8e04f12b8512edaeb
stkment <stkment@gmail.com>
2022-11-06 22:07:09 +0900

    :white_check_mark: kotest 테스트케이스 수정

--- src/test/kotlin/calculator/CalculatorKoTest.kt
+++ src/test/kotlin/calculator/CalculatorKoTest.kt
@@ -6,12 +6,10 @@ import calculator.model.DivideFormula
 import calculator.model.DivideResult
 import calculator.model.MinusFormula
 import calculator.model.MinusResult
-import calculator.model.MonkeyTestFormula
 import calculator.model.MultiplyFormula
 import calculator.model.MultiplyResult
 import calculator.model.PlusFormula
 import calculator.model.PlusResult
-import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 
@@ -33,55 +31,44 @@ class CalculatorKoTest : BehaviorSpec() {
                     calculator.calculate(numbers, operators) shouldBe PlusResult
                 }
             }
+        }
 
-            `xwhen`("5090 - 1240 식이 입력 되었다면") {
+        given("계산기에 5090 - 1240 식이 입력 되었고") {
+            beforeEach { expression = Expression(MinusFormula) }
+            `when`("연산식을 계산하면") {
                 then("3850.0 결과가 나온다") {
-//                    calculator.calculate(MinusFormula) shouldBe MinusResult
+                    val (numbers, operators) = expression.partitionExpression()
+                    calculator.calculate(numbers, operators) shouldBe MinusResult
                 }
             }
+        }
 
-            `xwhen`("9 * 9 식이 입력 되었다면") {
+        given("계산기에 9 * 9 식이 입력 되고") {
+            beforeEach { expression = Expression(MultiplyFormula) }
+            `when`("연산식을 계산하면") {
                 then("81.0 결과가 나온다") {
-//                    calculator.calculate(MultiplyFormula) shouldBe MultiplyResult
+                    val (numbers, operators) = expression.partitionExpression()
+                    calculator.calculate(numbers, operators) shouldBe MultiplyResult
                 }
             }
+        }
 
-            `xwhen`("2304123040 / 25 식이 입력 되었다면") {
+        given("계산기에 2304123040 / 25 식이 입력 되고") {
+            beforeEach { expression = Expression(DivideFormula) }
+            `when`("연산식을 계산하면") {
                 then("92164921.6 결과가 나온다") {
-//                    calculator.calculate(DivideFormula) shouldBe DivideResult
+                    val (numbers, operators) = expression.partitionExpression()
+                    calculator.calculate(numbers, operators) shouldBe DivideResult
                 }
             }
+        }
 
-            `xwhen`("2 + 3 * 4 / 2 식이 입력 되었다면") {
+        given("계산기에 2 + 3 * 4 / 2 식이 입력 되었고") {
+            beforeEach { expression = Expression(ComplexOperation) }
+            `when`("연산식을 계산하면") {
                 then("10.0 결과가 나온다") {
-//                    calculator.calculate(ComplexOperation) shouldBe ComplexOperationResult
-                }
-            }
-
-            `xwhen`("입력값이 null 이면") {
-                then("에러가 발생한다") {
-                    val exception = shouldThrow<IllegalArgumentException> {
-//                        calculator.calculate(null)
-                    }
-                    exception.message shouldBe "Failed requirement."
-                }
-            }
-
-            `xwhen`("입력값이 공백이면") {
-                then("에러가 발생한다") {
-                    val exception = shouldThrow<IllegalArgumentException> {
-//                        calculator.calculate(" ")
-                    }
-                    exception.message shouldBe "Failed requirement."
-                }
-            }
-
-            `xwhen`("몽키 테스팅을 했다면") {
-                then("에러가 발생한다") {
-                    val exception = shouldThrow<IllegalArgumentException> {
-//                        calculator.calculate(MonkeyTestFormula)
-                    }
-                    exception.message shouldBe "Failed requirement."
+                    val (numbers, operators) = expression.partitionExpression()
+                    calculator.calculate(numbers, operators) shouldBe ComplexOperationResult
                 }
             }
         }
