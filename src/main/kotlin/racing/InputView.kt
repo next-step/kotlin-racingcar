544904788675e6690a5bd8d641dfc2a36c9db678
stkment <stkment@gmail.com>
2022-11-07 01:20:16 +0900

    :necktie: return 타입 변경

--- src/main/kotlin/racing/InputView.kt
+++ src/main/kotlin/racing/InputView.kt
@@ -2,7 +2,7 @@ package racing
 
 object InputView {
 
-    fun readCarCount(): Int? {
+    fun readCarCount(): Int {
         println("자동차 대수는 몇 대인가요?")
         return readLine()?.toInt() ?: throw IllegalArgumentException("유효한 값이 아닙니다.")
     }
