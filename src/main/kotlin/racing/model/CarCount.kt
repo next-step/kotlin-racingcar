1c4bba009b774bce22d12843a342baad0343a53c
stkment <stkment@gmail.com>
2022-11-06 23:43:39 +0900

    :necktie: 자동차 개수, 경주 횟수 모델 유효성 체크

--- src/main/kotlin/racing/model/CarCount.kt
+++ src/main/kotlin/racing/model/CarCount.kt
@@ -3,4 +3,8 @@ package racing.model
 @JvmInline
 value class CarCount(
     val value: Int
-)
+) {
+    init {
+        require(value > 0) { "잘못된 입력입니다. 0보다 이상의 자동차 숫자를 입력하여야 합니다." }
+    }
+}
--- src/main/kotlin/racing/model/RoundCount.kt
+++ src/main/kotlin/racing/model/RoundCount.kt
@@ -3,4 +3,8 @@ package racing.model
 @JvmInline
 value class RoundCount(
     val value: Int
-)
+) {
+    init {
+        require(value > 0) { "잘못된 입력입니다. 0보다 이상의 경주게임 시도 횟수를 입력하여야 합니다." }
+    }
+}
