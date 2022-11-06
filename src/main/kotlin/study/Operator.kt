7a0cde91ffbe7e08ccfa27051d42e79d5de0c3fe
Raemerrr <foals18@gmail.com>
2022-11-06 13:05:19 +0900

    refactor : Operator.kt - when 분기문 적용 및 연산 책임 부여
    * Operators.kt : Operator 컬렉션을 관리하기 위한 객체 생성

--- src/main/kotlin/study/Operator.kt
+++ src/main/kotlin/study/Operator.kt
@@ -6,24 +6,33 @@ enum class Operator(val description: String) {
     DIVIDE("/"),
     MULTIPLY("*");
 
-    fun isPlus(): Boolean {
-        return this == PLUS
-    }
-
-    fun isMinus(): Boolean {
-        return this == MINUS
-    }
-
-    fun isDivide(): Boolean {
-        return this == DIVIDE
-    }
-
-    fun isMultiply(): Boolean {
-        return this == MULTIPLY
-    }
-
     companion object {
         private val map = values().associateBy(Operator::description)
         fun fromDescription(type: String?): Operator = map[type] ?: throw IllegalArgumentException("존재하지 않는 연산자입니다.")
+    }
+
+    fun calculate(first: StringNumber, second: StringNumber): Long {
+        return when (this) {
+            PLUS -> plus(first, second)
+            MINUS -> minus(first, second)
+            DIVIDE -> divide(first, second)
+            MULTIPLY -> multiply(first, second)
+        }
+    }
+
+    private fun plus(first: StringNumber, second: StringNumber): Long {
+        return first.longValue() + second.longValue()
+    }
+
+    private fun minus(first: StringNumber, second: StringNumber): Long {
+        return first.longValue() - second.longValue()
+    }
+
+    private fun divide(first: StringNumber, second: StringNumber): Long {
+        return first.longValue() / second.longValue()
+    }
+
+    private fun multiply(first: StringNumber, second: StringNumber): Long {
+        return first.longValue() * second.longValue()
     }
 }
