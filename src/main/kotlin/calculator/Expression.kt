b66b578baafe7e64203096f146330dadeba4a6ee
SangHyunGil <zizon5941@gmail.com>
2022-11-06 15:13:24 +0900

    chore: require을 메서드 안에서 처리하도록 수정

--- src/main/kotlin/calculator/Expression.kt
+++ src/main/kotlin/calculator/Expression.kt
@@ -7,9 +7,9 @@ data class Expression(
     private val rawExpression: List<String>
 ) {
     init {
-        require(isMoreThanMinimumSize()) { "계산할 수 있는 최소 입력 값을 입력해주세요." }
-        require(isCorrectSize()) { "홀수개의 연산자와 짝수개의 피연산자가 존재 해야 합니다." }
-        require(isCorrectExpression()) { "수식이 올바르지 않습니다." }
+        isEnoughSize()
+        isCorrectSize()
+        isCorrectExpression()
     }
 
     fun calculate(): Double {
@@ -25,17 +25,22 @@ data class Expression(
         }
     }
 
-    private fun isMoreThanMinimumSize() = rawExpression.size > MIN_SIZE
+    private fun isEnoughSize() =
+        require(rawExpression.size > MIN_SIZE) { "계산할 수 있는 최소 입력 값을 입력해주세요." }
 
-    private fun isCorrectSize() = rawExpression.isOddSize()
+    private fun isCorrectSize() =
+        require(rawExpression.isOddSize()) { "홀수개의 연산자와 짝수개의 피연산자가 존재 해야 합니다." }
 
-    private fun isCorrectExpression() = rawExpression.withIndex()
-        .all { (idx, data) ->
-            when (idx % 2 == 0) {
-                true -> data.isNumeric()
-                false -> Operator.exist(data)
-            }
-        }
+    private fun isCorrectExpression() =
+        require(
+            rawExpression.withIndex()
+                .all { (idx, data) ->
+                    when (idx % 2 == 0) {
+                        true -> data.isNumeric()
+                        false -> Operator.exist(data)
+                    }
+                }
+        ) { "수식이 올바르지 않습니다." }
 
     companion object {
         private const val FIRST_INDEX = 1
