05682cceff4fceea83a05035f5efe5340cf192e7
harris.yi <harrisleesh@gmail.com>
2022-11-06 13:18:16 +0900

    feature: Add name field in RacingCar class

--- src/main/kotlin/game/racingcar/RacingCar.kt
+++ src/main/kotlin/game/racingcar/RacingCar.kt
@@ -2,11 +2,17 @@ package game.racingcar
 
 import game.racingcar.move.MoveStrategy
 import game.racingcar.move.RandomMoveStrategy
+import java.lang.IllegalArgumentException
 
 class RacingCar(
     private val moveStrategy: MoveStrategy = RandomMoveStrategy(),
+    private val name: String = "",
     var location: Int = 0,
 ) {
+    init {
+        if (name.length > 5) throw IllegalArgumentException("이름의 길이는 5자를 초과할 수 없습니다.")
+    }
+
     fun move() {
         if (moveStrategy.movable()) {
             location += 1
