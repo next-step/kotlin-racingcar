23f849eb3b813742c4de4aba53fa143845b072bb
youngbin-yun <youngbin@koreaspacedata.com>
2022-11-07 00:46:40 +0900

    feat(RacingService): RacingService 클래스 생성

--- src/main/kotlin/step3_racingcar/Car.kt
+++ src/main/kotlin/step3_racingcar/Car.kt
@@ -3,11 +3,12 @@ package step3_racingcar
 import step3_racingcar.strategy.ForwardStrategy
 
 class Car(
-    private var distance: Int = 0,
+    var distance: Int = 0,
     private val strategy: ForwardStrategy
 ) {
-    fun move() {
+    fun move(): Car {
         if (strategy.move()) addDistance()
+        return this
     }
 
     private fun addDistance() = this.distance ++
