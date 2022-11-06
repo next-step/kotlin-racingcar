e6ecd0d8f3a870e15b9b27c03501f5deb6dd4668
stkment <stkment@gmail.com>
2022-11-07 01:25:57 +0900

    :necktie: RacingApplication 생성 및 초안 작성

---
+++ src/main/kotlin/racing/RacingApplication.kt
@@ -0,0 +1,27 @@
+package racing
+
+import racing.domain.GoOrStopCarActionImpl
+import racing.domain.RacingCarsFactoryImpl
+import racing.domain.RandomMovingCarConditionNumberImpl
+import racing.model.CarCount
+import racing.model.RoundCount
+
+class RacingApplication {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val count = InputView.readCarCount()
+            val count2 = InputView.readRoundCount()
+
+            val racingGame = RacingGame(
+                carCount = CarCount(count),
+                roundCount = RoundCount(count2),
+                goOrStopCarAction = GoOrStopCarActionImpl(
+                    randomMovingCarConditionNumber = RandomMovingCarConditionNumberImpl()
+                ),
+                racingCarsFactory = RacingCarsFactoryImpl()
+            )
+            racingGame.play()
+        }
+    }
+}
