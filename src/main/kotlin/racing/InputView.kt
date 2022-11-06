a6a430bc2b6662fd5bbe8948713f84dfede44fb5
stkment <stkment@gmail.com>
2022-11-06 23:42:16 +0900

    :necktie: 입력 에러 예외 처리

--- src/main/kotlin/racing/InputView.kt
+++ src/main/kotlin/racing/InputView.kt
@@ -4,11 +4,11 @@ object InputView {
 
     fun readCarCount(): Int? {
         println("자동차 대수는 몇 대인가요?")
-        return readLine()?.toInt()
+        return readLine()?.toInt() ?: throw IllegalArgumentException("유효한 값이 아닙니다.")
     }
 
-    fun readRoundCount(): Int? {
+    fun readRoundCount(): Int {
         println("시도할 횟수는 몇 회인가요?")
-        return readLine()?.toInt()
+        return readLine()?.toInt() ?: throw IllegalArgumentException("유효한 값이 아닙니다.")
     }
 }
