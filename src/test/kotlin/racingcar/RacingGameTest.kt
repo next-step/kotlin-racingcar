dcb9634ed211cf8ca1c8912c52befca08dc1e330
jpoh281 <hjp4869@naver.com>
2022-11-06 23:08:20 +0900

    refactor: 테스트 문구 수정

--- src/test/kotlin/racingcar/RacingGameTest.kt
+++ src/test/kotlin/racingcar/RacingGameTest.kt
@@ -20,7 +20,7 @@ class RacingGameTest : BehaviorSpec({
         val goStop = GoStop(0)
 
         `when`("게임을 시작하면") {
-            then("배열의 길이가 3인 문자열 배열을 반환한다.") {
+            then("배열의 길이가 1이며 -----를 가지고 있는 문자열 배열을 반환한다.") {
                 RacingGame(goStop = goStop).start(setting)[0] shouldBe "-----"
                 RacingGame(goStop = goStop).start(setting).size shouldBe 1
             }
