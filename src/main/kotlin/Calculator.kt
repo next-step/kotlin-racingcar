9a868724a970a26200163263e43e0b41b5c07e3f
ssr <ssr03>
2022-11-07 00:03:39 +0900

    fix:연산자의 로직 Operator enum class 로 이동

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,30 +1,12 @@
-import java.math.BigDecimal
-import java.math.MathContext
 import java.util.Stack
 
 class Calculator {
-    private fun add(i: BigDecimal, j: BigDecimal): BigDecimal =
-        i.add(j)
-
-    private fun subtract(i: BigDecimal, j: BigDecimal): BigDecimal =
-        i.subtract(j)
-
-    private fun multiply(i: BigDecimal, j: BigDecimal): BigDecimal =
-        i.multiply(j)
-
-    private fun divide(i: BigDecimal, j: BigDecimal): BigDecimal =
-        i.divide(j, MathContext.DECIMAL64)
 
     fun calculate(strNum1: String?, strNum2: String?, operation: String): String {
         val i = strNum1?.toBigDecimalOrNull() ?: throw IllegalArgumentException()
         val j = strNum2?.toBigDecimalOrNull() ?: throw IllegalArgumentException()
-        return when (operation) {
-            Operator.ADD.symbol -> add(i, j)
-            Operator.MINUS.symbol -> subtract(i, j)
-            Operator.MULTIPLE.symbol -> multiply(i, j)
-            Operator.DIVIDE.symbol -> divide(i, j)
-            else -> throw IllegalArgumentException()
-        }.toString()
+
+        return Operator.symbolOf(operation).calculate(i, j).toString()
     }
 
     fun calculateStringInput(input: String): String {
--- src/main/kotlin/Operator.kt
+++ src/main/kotlin/Operator.kt
@@ -1,10 +1,31 @@
+import java.math.BigDecimal
+import java.math.MathContext
+
 enum class Operator(val symbol: String) {
-    ADD("+"),
-    MINUS("-"),
-    MULTIPLE("*"),
-    DIVIDE("/");
+    ADD("+") {
+        override fun calculate(i: BigDecimal, j: BigDecimal): BigDecimal =
+            i.add(j)
+    },
+    MINUS("-") {
+        override fun calculate(i: BigDecimal, j: BigDecimal): BigDecimal =
+            i.subtract(j)
+    },
+    MULTIPLE("*") {
+        override fun calculate(i: BigDecimal, j: BigDecimal): BigDecimal =
+            i.multiply(j)
+    },
+    DIVIDE("/") {
+        override fun calculate(i: BigDecimal, j: BigDecimal): BigDecimal =
+            i.divide(j, MathContext.DECIMAL64)
+    };
+
+    abstract fun calculate(i: BigDecimal, j: BigDecimal): BigDecimal
 
     companion object {
+        fun symbolOf(operation: String) =
+            values().firstOrNull { it.symbol == operation }
+                ?: throw IllegalArgumentException()
+
         fun getOperators(): List<String> =
             mutableListOf(
                 ADD.symbol,
