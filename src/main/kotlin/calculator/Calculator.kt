aaaf59e55c40e61f251f87fe2459e557ff0cd3e4
Yujin Park <uiuiui118437@gmail.com>
2022-11-07 01:01:19 +0900

    refactor: calculate 함수의 역할 분리

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -7,22 +7,31 @@ object Calculator {
     private const val PAIR = 2
 
     fun calculate(input: String?): Int {
-        require(!input.isNullOrBlank()) { throw IllegalArgumentException("The input value is null or empty.") }
-
-        val expression = input.split(BLANK)
+        val expression = processInput(input)
         var result = expression.first().toInt()
 
         for (i in INDEX_OF_FIRST_OPERATOR until expression.size step PAIR) {
-            val indexOfOperand = i + 1
-            val operator = Operator.symbolOf(expression[i])
-            val operand = if (indexOfOperand == expression.size) {
-                throw IllegalArgumentException("The input value is invalid")
-            } else {
-                expression[indexOfOperand].toInt()
-            }
+            val (operator, operand) = getOperatorAndOperand(index = i, expression = expression)
             result = operator.operate(result, operand)
         }
 
         return result
+    }
+
+    private fun processInput(input: String?): List<String> {
+        require(!input.isNullOrBlank()) { throw IllegalArgumentException("The input value is null or empty.") }
+        return input.split(BLANK)
+    }
+
+    private fun getOperatorAndOperand(index: Int, expression: List<String>): Pair<Operator, Int> {
+        val indexOfOperand = index + 1
+        val operator = Operator.symbolOf(expression[index])
+        val operand = if (indexOfOperand == expression.size) {
+            throw IllegalArgumentException("The input value is invalid")
+        } else {
+            expression[indexOfOperand].toInt()
+        }
+
+        return Pair(operator, operand)
     }
 }
