618b89e72d314dc1660530e745728b272afcef7f
stkment <stkment@gmail.com>
2022-11-06 18:49:08 +0900

    :necktie: Expression 초기화 구현

--- src/main/kotlin/calculator/Expression.kt
+++ src/main/kotlin/calculator/Expression.kt
@@ -3,7 +3,14 @@ package calculator
 data class Expression(
     private val input: String?
 ) {
+    private val expression: String
 
+    init {
+        require(!input.isNullOrBlank()) { "잘못된 입력입니다. 계산식을 입력해주세요" }
+        require(Regex(OPERATION_REX).containsMatchIn(input)) { "잘못된 입력입니다. 숫자, 사칙연산 만 입력해주세요" }
+        expression = input
+    }
+    
     companion object {
         private const val DELIMITER_WHITE_SPACE = " "
         private const val OPERATION_REX = "^[+\\-*/\\d]*$"
