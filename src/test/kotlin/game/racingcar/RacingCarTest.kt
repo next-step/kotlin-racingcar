1d60028679480ea49ee0ddd3b557120d9540e9c4
harris.yi <harrisleesh@gmail.com>
2022-11-06 13:17:59 +0900

    test: Validate RacingCar name field length

--- src/test/kotlin/game/racingcar/RacingCarTest.kt
+++ src/test/kotlin/game/racingcar/RacingCarTest.kt
@@ -2,7 +2,9 @@ package game.racingcar
 
 import game.racingcar.move.AlwaysMoveStrategy
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
+import java.lang.IllegalArgumentException
 
 internal class RacingCarTest {
     @Test
@@ -14,5 +16,11 @@ internal class RacingCarTest {
         racingCar.move()
         // then
         assertThat(racingCar.location).isEqualTo(preLocation + 1)
+    }
+
+    @Test
+    fun `자동차 이름이 5자를 초과하면 IllegalArgumentException이 발생한다`() {
+        // when & then
+        assertThatThrownBy { RacingCar(AlwaysMoveStrategy(), "harris") }.isInstanceOf(IllegalArgumentException::class.java)
     }
 }
