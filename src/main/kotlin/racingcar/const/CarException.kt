afc58db219b04a2bcc1b8a329dc399a875e6dd36
SangHyunGil <zizon5941@gmail.com>
2022-11-07 00:20:19 +0900

    feat: Exception 상수화

---
+++ src/main/kotlin/racingcar/const/CarException.kt
@@ -0,0 +1,7 @@
+package racingcar.const
+
+object CarException {
+    const val NULL_INPUT_ERROR = "입력에 널 값은 불가합니다."
+    const val NOT_DIGIT_ERROR = "입력에 널 값은 불가합니다."
+    const val MORE_THAN_ONE = "1 이상의 값을 입력해주세요."
+}
