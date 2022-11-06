6b6fa83734c090d1833c73060c8e4f850604bb43
stkment <stkment@gmail.com>
2022-11-07 00:34:22 +0900

    :necktie: 동차 전진 또는 정지 액션 생성 유즈케이스 구현

--- src/main/kotlin/racing/domain/GoOrStopCarActionImpl.kt
+++ src/main/kotlin/racing/domain/GoOrStopCarActionImpl.kt
@@ -5,7 +5,14 @@ import racing.model.CarAction
 class GoOrStopCarActionImpl(
     private val randomMovingCarConditionNumber: RandomMovingCarConditionNumber
 ) : GoOrStopCarAction {
-    override fun castCarAction(): CarAction {
-        TODO("Not yet implemented")
+    override fun castCarAction(): CarAction =
+        if (randomMovingCarConditionNumber.random() >= MOVING_CAR_CONDITION) {
+            CarAction.GO
+        } else {
+            CarAction.STOP
+        }
+
+    companion object {
+        private const val MOVING_CAR_CONDITION = 4
     }
 }
