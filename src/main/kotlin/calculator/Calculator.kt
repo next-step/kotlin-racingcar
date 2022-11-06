20e77d11f1df1954234e0bd8e9b3978e738a35ad
Yujin Park <uiuiui118437@gmail.com>
2022-11-06 23:01:15 +0900

    refactor: 매직 리터럴을 상수로 선언

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -3,6 +3,8 @@ package calculator
 object Calculator {
 
     private const val BLANK = " "
+    private const val INDEX_OF_FIRST_OPERATOR = 1
+    private const val PAIR = 2
 
     fun calculate(input: String?): Int {
         require(!input.isNullOrBlank()) { throw IllegalArgumentException("The input value is null or empty.") }
@@ -10,7 +12,7 @@ object Calculator {
         val expression = input.split(BLANK)
         var result = expression.first().toInt()
 
-        for (i in 1 until expression.size step 2) {
+        for (i in INDEX_OF_FIRST_OPERATOR until expression.size step PAIR) {
             val operator = Operator.symbolOf(expression[i])
             val operand = expression[i + 1].toInt()
             result = operator.operate(result, operand)
