cc0ca1fd871ed106680e0342e3349da564a5912e
namonak <namonak@gmail.com>
2022-11-06 22:18:55 +0900

    feat: add calculation functions
    The following have been implement and test:
      - implement a method that verifies whether the input operand value is an integer
      - implement a method that returns a calculation result with input operands and operators
      - if the input operator is incorrect, raise an IllegalArgumentException

--- src/main/kotlin/step2/StringCalculator.kt
+++ src/main/kotlin/step2/StringCalculator.kt
@@ -1,6 +1,21 @@
 package step2
 
-class StringCalculator {
+import java.util.StringTokenizer
+
+fun main() {
+    println("계산식을 입력해주세요.")
+    val input = readLine() ?: throw IllegalArgumentException("입력값이 없습니다.")
+    println(StringCalculator.calculate(input))
+}
+
+class StringCalculator(input: String) {
+    init {
+        if (input.isBlank()) {
+            throw IllegalArgumentException("입력 값이 비어있습니다.")
+        }
+        calculate(input)
+    }
+
     companion object {
         fun add(operand1: Int, operand2: Int): Int {
             return operand1 + operand2
@@ -16,6 +31,35 @@ class StringCalculator {
 
         fun divide(operand1: Int, operand2: Int): Int {
             return operand1 / operand2
+        }
+
+        fun calculate(s: String) : Int {
+            val st = StringTokenizer(s, " ")
+            var result = validateOperand(st.nextToken())
+
+            while (st.hasMoreTokens()) {
+                val operator = st.nextToken()
+                val operand = validateOperand(st.nextToken())
+
+                result = startCalculation(operator, result, operand)
+            }
+            return result
+        }
+
+        private fun startCalculation(operator: String?, result: Int, operand: Int): Int = when (operator) {
+            "+" -> add(result, operand)
+            "-" -> subtract(result, operand)
+            "*" -> multiply(result, operand)
+            "/" -> divide(result, operand)
+            else -> throw IllegalArgumentException("연산자가 올바르지 않습니다.")
+        }
+
+        fun validateOperand(operand: String) : Int {
+            try {
+                return operand.toInt()
+            } catch (ex: NumberFormatException) {
+                throw NumberFormatException("숫자가 아닙니다.")
+            }
         }
     }
 }
--- src/test/kotlin/step2/StringCalculatorTest.kt
+++ src/test/kotlin/step2/StringCalculatorTest.kt
@@ -3,42 +3,80 @@ package step2
 import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
 
 class StringCalculatorTest {
-    @DisplayName("덧셈 연산")
+    @DisplayName("연속된 입력에 대한 연산 테스트")
+    @Test
+    fun `연속된 입력에 대한 연산 테스트`() {
+        assertThat(StringCalculator.calculate("2 + 3 * 4 / 2")).isEqualTo(10)
+    }
+
+    @DisplayName("빈 문자열에 대한 예외처리 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = ["", " "])
+    fun `빈 문자열에 대한 예외처리 테스트`(input: String) {
+        Assertions.assertThatThrownBy { StringCalculator(input) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @DisplayName("연산 테스트")
+    @ParameterizedTest
+    @CsvSource(value = ["1 + 2:3", "1 - 2:-1", "1 * 2:2", "1 / 2:0"], delimiter = ':')
+    fun `연산 테스트`(input: String, output: Int) {
+        assertThat(StringCalculator.calculate(input)).isEqualTo(output)
+    }
+
+    @DisplayName("연산자 예외처리 테스트")
+    @ParameterizedTest
+    @CsvSource(value = ["1 & 2", "1 ^ 2"])
+    fun `연산자 예외처리 테스트`(input: String) {
+        Assertions.assertThatThrownBy { StringCalculator.calculate(input) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @DisplayName("정수 검증 테스트")
+    @Test
+    fun `정수 검증 테스트`() {
+        Assertions.assertThatThrownBy { StringCalculator.validateOperand("ABC") }
+            .isInstanceOf(NumberFormatException::class.java)
+    }
+
+    @DisplayName("덧셈 연산 테스트")
     @ParameterizedTest
     @CsvSource(value = ["1:2", "0:9", "0:-1", "0:0", "21:17"], delimiter = ':')
-    fun addTest(operand1: Int, operand2: Int) {
+    fun `덧셈 연산 테스트`(operand1: Int, operand2: Int) {
         assertThat(StringCalculator.add(operand1, operand2)).isEqualTo(operand1 + operand2)
     }
 
-    @DisplayName("뺄셈 연산")
+    @DisplayName("뺄셈 연산 테스트")
     @ParameterizedTest
     @CsvSource(value = ["1:2", "0:9", "9:0", "9:9", "0:0"], delimiter = ':')
-    fun subtractTest(operand1: Int, operand2: Int) {
+    fun `뺄셈 연산 테스트`(operand1: Int, operand2: Int) {
         assertThat(StringCalculator.subtract(operand1, operand2)).isEqualTo(operand1 - operand2)
     }
 
-    @DisplayName("곱셈 연산")
+    @DisplayName("곱셈 연산 테스트")
     @ParameterizedTest
     @CsvSource(value = ["1:2", "0:9", "0:-1", "0:0", "21:17"], delimiter = ':')
-    fun multiplyTest(operand1: Int, operand2: Int) {
+    fun `곱셈 연산 테스트`(operand1: Int, operand2: Int) {
         assertThat(StringCalculator.multiply(operand1, operand2)).isEqualTo(operand1 * operand2)
     }
 
-    @DisplayName("나눗셈 연산")
+    @DisplayName("나눗셈 연산 테스트")
     @ParameterizedTest
     @CsvSource(value = ["1:2", "0:9", "9:9"], delimiter = ':')
-    fun divideTest(operand1: Int, operand2: Int) {
+    fun `나눗셈 연산 테스트`(operand1: Int, operand2: Int) {
         assertThat(StringCalculator.divide(operand1, operand2)).isEqualTo(operand1 / operand2)
     }
 
-    @DisplayName("나눗셈 연산 - by zero")
+    @DisplayName("나눗셈 연산 예외처리 테스트")
     @ParameterizedTest
     @CsvSource(value = ["9:0", "0:0"], delimiter = ':')
-    fun divideByZeroTest(operand1: Int, operand2: Int) {
+    fun `나눗셈 연산 예외처리 테스트`(operand1: Int, operand2: Int) {
         Assertions.assertThatThrownBy { StringCalculator.divide(operand1, operand2) }
             .isInstanceOf(ArithmeticException::class.java)
     }
