8ad3fedf8ce0dadd2a11479382446c87609d8cf1
SangHyunGil <zizon5941@gmail.com>
2022-11-07 00:20:33 +0900

    feat: InputView 추가 및 테스트

---
+++ src/main/kotlin/racingcar/io/InputView.kt
@@ -0,0 +1,29 @@
+package racingcar.io
+
+import racingcar.const.CarException
+import racingcar.const.isNumeric
+
+object InputView {
+    private const val READ_MESSAGE = "자동차 대수는 몇 대인가요?"
+    private const val WRITE_MESSAGE = "시도할 횟수는 몇 회인가요?"
+
+    fun read(): Pair<Int, Int> {
+        val numberOfCars = getInput(READ_MESSAGE)
+        val numberOfTry = getInput(WRITE_MESSAGE)
+
+        return Pair(numberOfCars, numberOfTry)
+    }
+
+    private fun getInput(message: String): Int {
+        println(message)
+        val input = Reader.read()
+        isAvailableInput(input)
+        return input!!.toInt()
+    }
+
+    private fun isAvailableInput(input: String?) {
+        require(input != null) { CarException.NULL_INPUT_ERROR }
+        require(input.isNumeric()) { CarException.NOT_DIGIT_ERROR }
+        require(input.toInt() > 0) { CarException.MORE_THAN_ONE }
+    }
+}
