01cda8da131dad543aff9dc221e8d7456b2efd04
SangHyunGil <zizon5941@gmail.com>
2022-11-07 00:26:51 +0900

    chore: 계산기 코드리뷰 반영

--- src/main/kotlin/calculator/CalculatorApplication.kt
+++ src/main/kotlin/calculator/CalculatorApplication.kt
@@ -1,13 +1,10 @@
 package calculator
 
 import calculator.const.InputOutputConst
-import java.util.Scanner
 
 fun main() {
-    val sc = Scanner(System.`in`)
-
     print(InputOutputConst.INPUT_MESSAGE)
-    val input = sc.nextLine()
+    val input = readLine()
 
     val result = Calculator.calculate(input)
     println("${InputOutputConst.OUTPUT_MESSAGE}$result")
