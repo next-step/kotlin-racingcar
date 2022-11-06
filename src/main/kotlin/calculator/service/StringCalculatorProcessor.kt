959da86afc34b98ded0d6b93a8477ee1fb1650bf
wrallee <wrallee@naver.com>
2022-11-06 13:41:45 +0900

    refactor(Calculator): 처리기 초기화 관련 변수명 변경

--- src/main/kotlin/calculator/service/StringCalculatorProcessor.kt
+++ src/main/kotlin/calculator/service/StringCalculatorProcessor.kt
@@ -3,7 +3,7 @@ package calculator.service
 import calculator.model.OperationBlock
 import java.math.BigDecimal
 
-private const val ONE = 1
+private const val PROCESSOR_INIT_COUNT = 1
 
 class StringCalculatorProcessor(
     val input: List<String>
@@ -13,9 +13,9 @@ class StringCalculatorProcessor(
     }
 
     fun process(): BigDecimal {
-        val first = input.first()
-        val leftList = input.drop(ONE)
-        var block = OperationBlock(BigDecimal(first))
+        val initialValue = input.first()
+        val leftList = input.drop(PROCESSOR_INIT_COUNT)
+        var block = OperationBlock(BigDecimal(initialValue))
 
         for (value in leftList) {
             block = block.reduce(value)
