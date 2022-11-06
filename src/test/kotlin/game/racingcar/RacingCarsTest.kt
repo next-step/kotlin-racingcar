1970b27c04751f2708797f81b21a665ee0d41497
harris.yi <harrisleesh@gmail.com>
2022-11-06 12:47:14 +0900

    test: Remove useless test and change test as method combination

--- src/test/kotlin/game/racingcar/RacingCarsTest.kt
+++ src/test/kotlin/game/racingcar/RacingCarsTest.kt
@@ -7,24 +7,11 @@ import org.junit.jupiter.api.Test
 internal class RacingCarsTest {
 
     @Test
-    fun `RacingCars 의 초기 위치 스냅샷은 모두 0이다`() {
-        // given
-        val racingCars = RacingCars(3)
-        // when
-        val snapshot = racingCars.locations()
-        // then
-        snapshot.forEach {
-            assertThat(it).isEqualTo(0)
-        }
-    }
-
-    @Test
     fun `RacingCars가 AlwaysMoveStrategy로 생성되었을 때 모두 위치가 1씩 증가한다`() {
         // given
         val racingCars = RacingCars(3, AlwaysMoveStrategy())
         // when
-        racingCars.moveAll()
-        val snapshot = racingCars.locations()
+        val snapshot = racingCars.moveAll()
         // then
         snapshot.forEach {
             assertThat(it).isEqualTo(1)
