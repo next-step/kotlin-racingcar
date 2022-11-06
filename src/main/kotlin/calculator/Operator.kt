0cfc5c84ac59acc3916ccfe517931a71c6998455
chayoung-choi <chayoung.dev@gmail.com>
2022-11-06 22:19:23 +0900

    refactor: 코드 리뷰 내용 반영
    - Int -> BigDecimal 변경
    - 사칙연산 BigDecimal의 연산 사용

--- src/main/kotlin/calculator/Operator.kt
+++ src/main/kotlin/calculator/Operator.kt
@@ -1,34 +1,33 @@
 package calculator
 
+import java.math.BigDecimal
+
 /**
  * 계산 연산자
  */
 enum class Operator(val symbol: String) {
 
     PLUS("+") {
-        override fun operate(a: Int, b: Int): Int = a + b
+        override fun operate(a: BigDecimal, b: BigDecimal): BigDecimal = a.add(b)
     },
     MINUS("-") {
-        override fun operate(a: Int, b: Int): Int = a - b
+        override fun operate(a: BigDecimal, b: BigDecimal): BigDecimal = a.minus(b)
     },
     MULTIPLY("*") {
-        override fun operate(a: Int, b: Int): Int = a * b
+        override fun operate(a: BigDecimal, b: BigDecimal): BigDecimal = a.multiply(b)
     },
     DIVIDE("/") {
-        override fun operate(a: Int, b: Int): Int {
-            require(b != 0) { "0으로 나눌 수 없습니다." }
-            return a / b
+        override fun operate(a: BigDecimal, b: BigDecimal): BigDecimal {
+            require(b != BigDecimal.ZERO) { "0으로 나눌 수 없습니다." }
+            return a.divide(b)
         }
     };
 
-    abstract fun operate(a: Int, b: Int): Int
+    abstract fun operate(a: BigDecimal, b: BigDecimal): BigDecimal
 
     companion object {
         fun of(symbol: String): Operator {
-            values().forEach {
-                if (it.symbol == symbol) return it
-            }
-            throw IllegalArgumentException("지원하지 않는 연산자입니다.")
+            return values().find { it.symbol == symbol } ?: throw IllegalArgumentException("지원하지 않는 연산자입니다.")
         }
     }
 }
--- src/test/kotlin/calculator/OperatorTest.kt
+++ src/test/kotlin/calculator/OperatorTest.kt
@@ -3,40 +3,43 @@ package calculator
 import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.math.BigDecimal
 
 internal class OperatorTest {
 
     @Test
     fun plus() {
-        assertThat(Operator.PLUS.operate(5, 3)).isEqualTo(8)
-        assertThat(Operator.of("+").operate(5, 3)).isEqualTo(8)
+        assertThat(Operator.PLUS.operate(BigDecimal(5), BigDecimal(3))).isEqualTo(BigDecimal(8))
+        assertThat(Operator.of("+").operate(BigDecimal(5), BigDecimal(3))).isEqualTo(BigDecimal(8))
     }
 
     @Test
     fun minus() {
-        assertThat(Operator.MINUS.operate(5, 3)).isEqualTo(2)
-        assertThat(Operator.of("-").operate(5, 3)).isEqualTo(2)
+        assertThat(Operator.MINUS.operate(BigDecimal(5), BigDecimal(3))).isEqualTo(BigDecimal(2))
+        assertThat(Operator.of("-").operate(BigDecimal(5), BigDecimal(3))).isEqualTo(BigDecimal(2))
     }
 
     @Test
     fun multiply() {
-        assertThat(Operator.MULTIPLY.operate(5, 3)).isEqualTo(15)
-        assertThat(Operator.of("*").operate(5, 3)).isEqualTo(15)
+        assertThat(Operator.MULTIPLY.operate(BigDecimal(5), BigDecimal(3))).isEqualTo(BigDecimal(15))
+        assertThat(Operator.of("*").operate(BigDecimal(5), BigDecimal(3))).isEqualTo(BigDecimal(15))
     }
 
     @Test
     fun divide() {
-        assertThat(Operator.DIVIDE.operate(6, 3)).isEqualTo(2)
-        assertThat(Operator.of("/").operate(6, 3)).isEqualTo(2)
+        assertThat(Operator.DIVIDE.operate(BigDecimal(6), BigDecimal(3))).isEqualTo(BigDecimal(2))
+        assertThat(Operator.of("/").operate(BigDecimal(6), BigDecimal(3))).isEqualTo(BigDecimal(2))
     }
 
     @Test
     fun `0으로 나누면, IllegalArgumentException`() {
-        Assertions.assertThatIllegalArgumentException().isThrownBy { Operator.DIVIDE.operate(6, 0) }
+        Assertions.assertThatIllegalArgumentException()
+            .isThrownBy { Operator.DIVIDE.operate(BigDecimal(6), BigDecimal(0)) }
     }
 
     @Test
     fun `지원하지 않는 연산자 문자이면, IllegalArgumentException`() {
-        Assertions.assertThatIllegalArgumentException().isThrownBy { Operator.of("=").operate(6, 0) }
+        Assertions.assertThatIllegalArgumentException()
+            .isThrownBy { Operator.of("=").operate(BigDecimal(6), BigDecimal(0)) }
     }
 }
