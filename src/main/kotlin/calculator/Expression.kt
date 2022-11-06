8bde2ff7c69f402dbedc30b79c3abea943a81856
riyenas0925 <riyenas0925@gmail.com>
2022-11-06 14:43:36 +0900

    feat: 문자열 계산기의 입력(수식)은 사칙 연산을 제외한 연산자는 들어올 수 없어요.

--- src/main/kotlin/calculator/Expression.kt
+++ src/main/kotlin/calculator/Expression.kt
@@ -6,9 +6,18 @@ class Expression(
     init {
         requireNotNull(expression)
         requireNotBlank(expression)
+        requireValidExpression(expression)
+    }
+
+    companion object {
+        val regex = Regex("[0-9+-/x\\s]+")
     }
 
     private fun requireNotBlank(expression: String) {
         require(expression.isNotBlank())
+    }
+
+    private fun requireValidExpression(expression: String) {
+        require(regex.matches(expression))
     }
 }
