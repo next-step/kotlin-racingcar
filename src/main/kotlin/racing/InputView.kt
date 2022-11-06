bc964f98a67b8890b5016ed05af7d95994d4c783
stkment <stkment@gmail.com>
2022-11-06 23:21:59 +0900

    :lipstick: InputView 추가

---
+++ src/main/kotlin/racing/InputView.kt
@@ -0,0 +1,14 @@
+package racing
+
+object InputView {
+
+    fun readCarCount(): Int? {
+        println("자동차 대수는 몇 대인가요?")
+        return readLine()?.toInt()
+    }
+
+    fun readRoundCount(): Int? {
+        println("시도할 횟수는 몇 회인가요?")
+        return readLine()?.toInt()
+    }
+}
