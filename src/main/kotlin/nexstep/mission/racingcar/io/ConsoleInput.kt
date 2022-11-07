caa79082cd31d2a69913d4f42fb56b9424b420ff
kimseonghun <seonghun127@woowahan.com>
2022-11-07 20:42:34 +0900

    [step4] feat: 자동차 입력을 받을 수 있도록 입력 인터페이스에 함수 추가

--- src/main/kotlin/nexstep/mission/racingcar/io/ConsoleInput.kt
+++ src/main/kotlin/nexstep/mission/racingcar/io/ConsoleInput.kt
@@ -7,6 +7,11 @@ object ConsoleInput : Input {
         return readln().toInt()
     }
 
+    override fun inputRacingCarNames(): List<String> {
+        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+        return readln().split(",")
+    }
+
     override fun inputRound(): Int {
         println("시도할 횟수는 몇 회인가요?")
         return readln().toInt()
--- src/main/kotlin/nexstep/mission/racingcar/io/Input.kt
+++ src/main/kotlin/nexstep/mission/racingcar/io/Input.kt
@@ -4,5 +4,7 @@ interface Input {
 
     fun inputRacingCars(): Int
 
+    fun inputRacingCarNames(): List<String>
+
     fun inputRound(): Int
 }
