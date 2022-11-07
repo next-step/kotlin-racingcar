8ebfc07b52a203b1689b35af4f68b2d5862d564b
adforus_hjp <hjp281@adforus.co.kr>
2022-11-07 12:31:25 +0900

    refactor: 0, 4, 9에 대해서 의미를 부여하는 매직 리터럴 추가
    https://github.com/next-step/kotlin-racingcar/pull/777#discussion_r1014840112

--- src/main/kotlin/racingcar/Movable.kt
+++ src/main/kotlin/racingcar/Movable.kt
@@ -1,6 +1,13 @@
 package racingcar
 
-class Movable(private val baseNumber: Int = 4, private val range: IntRange = IntRange(0, 9)) {
+const val START_DIGIT: Int = 0
+const val MEDIAN_DIGIT: Int = 4
+const val END_DIGIT: Int = 9
+
+class Movable(
+    private val baseNumber: Int = MEDIAN_DIGIT,
+    private val range: IntRange = IntRange(START_DIGIT, END_DIGIT)
+) {
 
     fun canGo(randomNumber: Int = range.random()): Boolean {
         return randomNumber >= baseNumber
