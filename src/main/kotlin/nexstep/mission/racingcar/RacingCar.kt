95850c85eb425b9b9b5674cff5d5b2200c677477
kimseonghun <seonghun127@woowahan.com>
2022-11-07 21:01:45 +0900

    [step4] feat: RacingCar 객체에 Name VO 의존 추가

--- src/main/kotlin/nexstep/mission/racingcar/RacingCar.kt
+++ src/main/kotlin/nexstep/mission/racingcar/RacingCar.kt
@@ -1,8 +1,14 @@
 package nexstep.mission.racingcar
 
+import nexstep.mission.racingcar.vo.Name
+
 private const val MOVEMENT_CONDITION = 4
 
-class RacingCar(var position: Int = 0) {
+class RacingCar(
+    val name: Name,
+    var position: Int = 0
+) {
+    constructor(name: String) : this(Name(name))
 
     fun move(random: Int) {
         if (random >= MOVEMENT_CONDITION) {
--- src/test/kotlin/nexstep/mission/racingcar/RacingCarTest.kt
+++ src/test/kotlin/nexstep/mission/racingcar/RacingCarTest.kt
@@ -6,19 +6,19 @@ import io.kotest.matchers.shouldBe
 class RacingCarTest : FunSpec({
 
     test("경주 자동차를 생성한다. 기본 position은 0이다.") {
-        val racingCar = RacingCar()
+        val racingCar = RacingCar("pobi")
         racingCar.position shouldBe 0
     }
 
     test("4이상일 경우 자동차는 이동한다.") {
-        val racingCar = RacingCar()
+        val racingCar = RacingCar("jason")
         racingCar.move(4)
 
         racingCar.position shouldBe 1
     }
 
     test("4미만 경우 자동차는 이동하지 않는다.") {
-        val racingCar = RacingCar()
+        val racingCar = RacingCar("brown")
         racingCar.move(3)
 
         racingCar.position shouldBe 0
