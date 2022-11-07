bd6a8cb1160ce0e474b5cfb47d64b713f1bc1ab5
EP <kjuioqq8@gmail.com>
2022-11-07 09:12:51 +0900

    refactor: 문자열로 변경하는 역할 포트로 변경

--- src/main/kotlin/calculator/application/port/in/CalculatorInputPort.kt
+++ src/main/kotlin/calculator/application/port/in/CalculatorInputPort.kt
@@ -3,16 +3,16 @@ package calculator.application.port.`in`
 import calculator.application.parser.ParsingProcessor
 import calculator.domain.calculator.Calculator
 
-interface CalculatorInputPort<T> {
-    fun input(inputString: String): T
+interface CalculatorInputPort {
+    fun input(inputString: String): String
 }
 
 class CalculatorInputPortImpl(
     private val parsingProcessor: ParsingProcessor,
     private val calculator: Calculator<Int>
-) : CalculatorInputPort<Int> {
-    override fun input(inputString: String): Int {
+) : CalculatorInputPort {
+    override fun input(inputString: String): String {
         val inputOperationCommandList = parsingProcessor.proceed(inputString)
-        return calculator.calculate(inputOperationCommandList)
+        return calculator.calculate(inputOperationCommandList).toString()
     }
 }
--- src/main/kotlin/calculator/interfaces/adpater/CalculatorConsoleAdapter.kt
+++ src/main/kotlin/calculator/interfaces/adpater/CalculatorConsoleAdapter.kt
@@ -5,11 +5,11 @@ import calculator.interfaces.input.InputConsole
 import calculator.interfaces.output.OutputConsole
 
 class CalculatorConsoleAdapter(
-    private val calculatorInputPort: CalculatorInputPort<Int>
+    private val calculatorInputPort: CalculatorInputPort
 ) {
     fun init() {
         val expression = InputConsole.input()
-        val result = calculatorInputPort.input(expression).toString()
+        val result = calculatorInputPort.input(expression)
         OutputConsole.output(result)
     }
 }
