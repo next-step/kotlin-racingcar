3ef75ac93514e642c2205b0e36e9596b167c9c40
shinaaaa <shin029abc@gmail.com>
2022-11-08 12:51:39 +0900

    feat: 자동차 경주 구현

---
+++ src/main/kotlin/racingcar/application/Race.kt
@@ -0,0 +1,24 @@
+package racingcar.application
+
+import racingcar.domain.Car
+import racingcar.domain.Car.Companion.move
+import racingcar.view.InputView
+import racingcar.view.ResultView
+
+class Race {
+
+    fun main() {
+        val numberOfCars = InputView.registerParticipant()
+        val times = InputView.setNumberOfTimes()
+
+        ResultView.executionResult()
+
+        val cars = Car.registerCars(numberOfCars)
+
+        (1..times).forEach { _ ->
+            cars.map { it.move() }
+                .forEach { ResultView.presentSituation(it) }
+            println()
+        }
+    }
+}
