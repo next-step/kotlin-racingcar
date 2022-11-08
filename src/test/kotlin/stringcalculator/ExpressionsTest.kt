7c165bc79491fd72fb0da215ae8c5153953d4db8
lalize <printf@kakao.com>
2022-11-08 20:23:48 +0900

    refactor: 테스트명에 의도를 드러내도록 수정

--- src/test/kotlin/stringcalculator/ExpressionsTest.kt
+++ src/test/kotlin/stringcalculator/ExpressionsTest.kt
@@ -5,32 +5,32 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 class ExpressionsTest : StringSpec({
-    "단항 테스트" {
+    "단항 연산자를 입력하면 수 타입으로 계산된다" {
         UnaryExpression("1").evaluate() shouldBe 1
         UnaryExpression("+1").evaluate() shouldBe 1
         UnaryExpression("-1").evaluate() shouldBe -1
     }
 
-    "올바르지 않은 단항 테스트" {
+    "지원하지 않는 단항 연산자를 입력하면 예외가 발생한다" {
         shouldThrowExactly<NumberFormatException> { UnaryExpression("--1").evaluate() }
         shouldThrowExactly<NumberFormatException> { UnaryExpression("++1").evaluate() }
         shouldThrowExactly<NumberFormatException> { UnaryExpression("-+1").evaluate() }
         shouldThrowExactly<NumberFormatException> { UnaryExpression("+-1").evaluate() }
     }
 
-    "+ 연산 테스트" {
+    "1 + 2는 3이다" {
         BinaryExpression(UnaryExpression("1"), Operator.PLUS, UnaryExpression("2")).evaluate() shouldBe 3
     }
 
-    "- 연산 테스트" {
+    "1 - 2는 -1이다" {
         BinaryExpression(UnaryExpression("1"), Operator.MINUS, UnaryExpression("2")).evaluate() shouldBe -1
     }
 
-    "/ 연산 테스트" {
+    "1 / 2는 0.5다" {
         BinaryExpression(UnaryExpression("1"), Operator.DIVIDE, UnaryExpression("2")).evaluate() shouldBe 0.5
     }
 
-    "* 연산 테스트" {
+    "1 * 2 는 2이다" {
         BinaryExpression(UnaryExpression("1"), Operator.MULTIPLY, UnaryExpression("2")).evaluate() shouldBe 2
     }
 })
--- src/test/kotlin/stringcalculator/StringCalculatorTest.kt
+++ src/test/kotlin/stringcalculator/StringCalculatorTest.kt
@@ -5,19 +5,19 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 class StringCalculatorTest : StringSpec({
-    "올바른 계산식 테스트" {
+    "올바른 계산식을 입력하면 정상적으로 계산된다" {
         StringCalculator.calculate("2 + 3 * 4 / 2") shouldBe 10
     }
 
-    "NULL을 입력한 경우 예외 테스트" {
+    "NULL을 입력하면 예외가 발생한다" {
         shouldThrowExactly<IllegalArgumentException> { StringCalculator.calculate(null) }
     }
 
-    "공백을 입력한 경우 예외 테스트" {
+    "빈 공백을 입력하면 예외가 발생한다" {
         shouldThrowExactly<IllegalArgumentException> { StringCalculator.calculate(" ") }
     }
 
-    "사칙연산이 아닌 경우 예외 테스트" {
+    "지원하지 않는 연산자를 입력하면 예외가 발생한다" {
         shouldThrowExactly<IllegalArgumentException> { StringCalculator.calculate("1 + 2 % 3") }
     }
 })
