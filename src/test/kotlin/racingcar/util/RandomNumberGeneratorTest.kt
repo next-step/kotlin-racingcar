1e376224f4b93f7f88c757f816cf914ceafff864
SangHyunGil <zizon5941@gmail.com>
2022-11-08 23:13:07 +0900

    chore: RandomNumberGenerator 테스트 수정

--- src/test/kotlin/racingcar/util/RandomNumberGeneratorTest.kt
+++ src/test/kotlin/racingcar/util/RandomNumberGeneratorTest.kt
@@ -5,9 +5,13 @@ import io.kotest.matchers.ints.shouldBeInRange
 import io.kotest.property.checkAll
 
 internal class RandomNumberGeneratorTest : FunSpec({
-    context("랜덤한 값은 항상 0~9 사이로 이루어져있다.") {
-        checkAll<Int, Int> (10) { a, b ->
-            RandomNumberGenerator.generate(a..b) shouldBeInRange 0..9
+    context("랜덤한 값은 항상 주어진 범위 안에 나온다.") {
+        checkAll<Int> { _ ->
+            RandomNumberGenerator.generate(0..10) shouldBeInRange 0..10
+        }
+
+        checkAll<Int> { _ ->
+            RandomNumberGenerator.generate(0..100) shouldBeInRange 0..100
         }
     }
 })
