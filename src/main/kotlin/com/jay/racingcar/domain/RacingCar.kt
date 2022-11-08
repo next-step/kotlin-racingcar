0e6f1e4200a28ef577f053a6692efc43ed89d70e
jay <jay@woowahan.com>
2022-11-09 00:03:42 +0900

    refactor: RacingCar 속성 제한자 변경

--- src/main/kotlin/com/jay/racingcar/domain/RacingCar.kt
+++ src/main/kotlin/com/jay/racingcar/domain/RacingCar.kt
@@ -2,7 +2,7 @@ package com.jay.racingcar.domain
 
 class RacingCar(
     private val movingStrategy: MovingStrategy,
-    var position: Int = 0
+    internal var position: Int = 0
 ) {
     fun move() {
         if (movingStrategy.canMove()) position++
