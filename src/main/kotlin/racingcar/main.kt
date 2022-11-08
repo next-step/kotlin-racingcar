8559a8ec526a17c8a0167c51bb63ddf8e3a874f4
youngbin-yun <youngbin@koreaspacedata.com>
2022-11-09 01:40:10 +0900

    refactor(Cars): 일급 컬렉션 생성

--- src/main/kotlin/racingcar/main.kt
+++ src/main/kotlin/racingcar/main.kt
@@ -1,21 +1,18 @@
-import racingcar.service.RacingService
+package racingcar
+
 import racingcar.view.InputView
 import racingcar.view.ResultView
-
-private const val ONE = 1
 
 fun main() {
     val inputView = InputView()
     val resultView = ResultView()
-    val racingService = RacingService()
 
     val carCount = inputView.getCarCount()
     val tryCount = inputView.getTryCount()
 
-    var cars = racingService.makeCar(carCount)
-    (ONE..tryCount).forEach { _ ->
-        var movedCars = racingService.race(cars)
-        resultView.printCarDistance(movedCars)
-        cars = movedCars
+    val cars = Cars(List(carCount) { Car() })
+    repeat(tryCount) {
+        cars.race()
+        resultView.printCarDistance(cars)
     }
 }
