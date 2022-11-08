0a2ce5394efb42da98ae737f5f752706b152175a
dk-ko <dev.dkko@gmail.com>
2022-11-08 20:21:28 +0900

    test: 멈춤 테스트

--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -17,4 +17,18 @@ class CarTest {
 
         assertEquals(movedCar, toMoveCar)
     }
+
+    @Test
+    fun `멈춤 테스트`() {
+        val moveCount = 2
+        var toMoveCar = Car()
+        val stopCar = Car()
+
+        val moveCondition = 3
+        for (i in 1..moveCount) {
+            toMoveCar.move(moveCondition)
+        }
+
+        assertEquals(stopCar, toMoveCar)
+    }
 }
