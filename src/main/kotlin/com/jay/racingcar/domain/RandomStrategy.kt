e7b8682756d4cd29fc2974989de939b4b2751e65
jay <jay@woowahan.com>
2022-11-09 00:05:22 +0900

    refactor: RandomStrategy내 magic number 제거

--- src/main/kotlin/com/jay/racingcar/domain/RandomStrategy.kt
+++ src/main/kotlin/com/jay/racingcar/domain/RandomStrategy.kt
@@ -2,11 +2,13 @@ package com.jay.racingcar.domain
 
 class RandomStrategy : MovingStrategy {
     override fun canMove(): Boolean {
-        val random = (0..9).random()
+        val random = (RANDOM_START_RANGE..RANDOM_END_RANGE).random()
         return random > MOVING_THRESHOLD
     }
 
     companion object {
         private const val MOVING_THRESHOLD = 4
+        private const val RANDOM_START_RANGE = 0
+        private const val RANDOM_END_RANGE = 9
     }
 }
