7686da1fdcefdf62deba5e82dfe4d9c9f66a1841
Sangmeebee <apfhdznzl@gmail.com>
2022-11-08 20:40:27 +0900

    step2: ParameterizedTest 사용

--- src/main/kotlin/step2/Operator.kt
+++ src/main/kotlin/step2/Operator.kt
@@ -5,7 +5,7 @@ enum class Operator(val sign: String, val calculate: (Int, Int) -> Int) {
     MINUS(sign = "-", calculate = { a, b -> a - b }),
     MULTIPLY(sign = "*", calculate = { a, b -> a * b }),
     DIVIDE(sign = "/", calculate = { a, b -> a / b });
-    
+
     companion object {
         fun of(sign: String): Operator =
             values().find { it.sign == sign } ?: throw IllegalArgumentException("invalid operator")
