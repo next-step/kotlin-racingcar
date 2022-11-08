a7346a546affa262ab1437ea838d2e27787293e6
choi-ys <project.log.062@gmail.com>
2022-11-08 21:38:54 +0900

    fix: lint 깨짐 수정

--- src/main/kotlin/step3/racingcar/utils/CarGenerator.kt
+++ src/main/kotlin/step3/racingcar/utils/CarGenerator.kt
@@ -4,8 +4,8 @@ import step3.racingcar.domain.Car
 import step3.racingcar.domain.Cars
 
 object CarGenerator {
-    fun generate(carCount: Int): Cars =
-        Cars(mutableListOf<Car>().also {
+    fun generate(carCount: Int): Cars = Cars(
+        mutableListOf<Car>().also {
             for (i in 0 until carCount) {
                 it.add(Car())
             }
