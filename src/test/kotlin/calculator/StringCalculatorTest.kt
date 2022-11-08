f37ec43a04e7b8e759a74f7b5b967a875a4a660b
khcho902 <khcho902@gmail.com>
2022-11-09 01:01:55 +0900

    test: 사칙연산 종합, 사칙연산 기호 아닌 경우 테스트 추가

--- src/test/kotlin/calculator/StringCalculatorTest.kt
+++ src/test/kotlin/calculator/StringCalculatorTest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ValueSource
 
 internal class StringCalculatorTest {
 
@@ -48,9 +49,28 @@ internal class StringCalculatorTest {
         assertEquals(expectedResult, actualResult)
     }
 
+    @ParameterizedTest(name = "{0} = {1}")
+    @CsvSource("2 - 3 * 4 / 2=-2", "2 + 3 * 4 / 2=10", "2 - 3 + 4 / 2=1", "2 + 3 * 4 - 2=18", "2 + 3 / 6 * 4 - 2=-2", delimiter = '=')
+    fun `사칙연산 종합 테스트`(input: String, expectedResult: Long) {
+        // when
+        val actualResult = StringCalculator.calculate(input)
+
+        // then
+        assertEquals(expectedResult, actualResult)
+    }
+
     @ParameterizedTest(name = "input = [{0}]")
     @MethodSource("blankOrNullStrings")
     fun `입력값 null 또는 빈 공백문자 테스트`(input: String?) {
+        // when & then
+        assertThatThrownBy {
+            StringCalculator.calculate(input)
+        }.isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @ParameterizedTest(name = "input = [{0}]")
+    @ValueSource(strings = ["1 2 3", "1 & 1", "1 + 2 = 1", "3 @ 1", "3 % 1"])
+    fun `사칙연산 기호가 아닌 경우`(input: String) {
         // when & then
         assertThatThrownBy {
             StringCalculator.calculate(input)
