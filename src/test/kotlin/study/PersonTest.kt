dcb5ca27a67232def7a30b5dd1a7d2fcd56ec1a0
Shinyoung Kim <rolroralra@gmail.com>
2022-11-08 20:21:01 +0900

    study: basic kotest code with klint (#685)
    * test: basic kotest code
    
    * test: basic kotest code

---
+++ src/test/kotlin/study/PersonTest.kt
@@ -0,0 +1,36 @@
+package study
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class PersonTest {
+    @Test
+    fun `이름 붙인 인자`() {
+        val person = Person("shinyoung Kim", 30, "rolroralra")
+        assertThat(person.name).isEqualTo("shinyoung Kim")
+        assertThat(person.age).isEqualTo(30)
+        assertThat(person.nickname).isEqualTo("rolroralra")
+    }
+
+    @Test
+    fun `널 타입`() {
+        val person = Person("shinyoung Kim", 20, null)
+        assertThat(person.nickname).isNull()
+    }
+
+    @Test
+    fun `기본 인자`() {
+        val person = Person("shinyoung Kim", 30)
+        assertThat(person.name).isEqualTo("shinyoung Kim")
+        assertThat(person.age).isEqualTo(30)
+        assertThat(person.nickname).isEqualTo("nickname")
+    }
+
+    @Test
+    fun `데이터 클래스`() {
+        val person = Person("shinyoung Kim", 30)
+        val person2 = Person("shinyoung Kim", 30)
+
+        assertThat(person).isEqualTo(person2)
+    }
+}
