7721fd5fe3c5c30fb353e22f530455eb1842af85
giyeon95 <giyeon15@gmail.com>
2022-11-09 22:17:28 +0900

    refactor: object 키워드를 이용하여 View 클래스 개선
    싱글톤 패턴을 구현하고 싶으셨던 것 같아요. 코틀린은 이를 위해 [object](https://kotlinlang.org/docs/object-declarations.html#object-declarations-overview) 키워드를 제공합니다 :)
    
    _Originally posted by @ddaaac in https://github.com/next-step/kotlin-racingcar/pull/760#discussion_r1015684445_

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -1,18 +1,16 @@
 package racingcar.view
 
-class InputView private constructor() {
+object InputView {
 
-    companion object {
-        fun inputCarCount(): Int {
-            println("자동차 대수는 몇 대인가요?")
+    fun inputCarCount(): Int {
+        println("자동차 대수는 몇 대인가요?")
 
-            return readLine()!!.toInt()
-        }
+        return readLine()!!.toInt()
+    }
 
-        fun inputRacingFullRound(): Int {
-            println("시도할 횟수는 몇 회인가요?")
+    fun inputRacingFullRound(): Int {
+        println("시도할 횟수는 몇 회인가요?")
 
-            return readLine()!!.toInt()
-        }
+        return readLine()!!.toInt()
     }
 }
--- src/main/kotlin/racingcar/view/ResultView.kt
+++ src/main/kotlin/racingcar/view/ResultView.kt
@@ -5,18 +5,15 @@ import racingcar.RaceStage
 private const val SCORE_SYMBOL = "-"
 private const val LINE_BREAK = "\n"
 
-class ResultView private constructor() {
+object ResultView {
+    fun printResultIntroWording() {
+        println()
+        println("실행 결과")
+    }
 
-    companion object {
-        fun printResultIntroWording() {
-            println()
-            println("실행 결과")
-        }
-
-        fun printResult(raceStage: RaceStage) {
-            val progress = raceStage.cars.joinToString(separator = LINE_BREAK) { car -> SCORE_SYMBOL.repeat(car.pos) }
-            println(progress)
-            println()
-        }
+    fun printResult(raceStage: RaceStage) {
+        val progress = raceStage.cars.joinToString(separator = LINE_BREAK) { car -> SCORE_SYMBOL.repeat(car.pos) }
+        println(progress)
+        println()
     }
 }
