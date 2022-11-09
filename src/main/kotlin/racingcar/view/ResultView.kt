5eda8d34af0a88e9dbfbfab5fbb678933f72e5bd
shinaaaa <shin029abc@gmail.com>
2022-11-09 11:07:04 +0900

    refactor: InputView, ResultView를 object 타입으로 변경

--- src/main/kotlin/racingcar/view/ResultView.kt
+++ src/main/kotlin/racingcar/view/ResultView.kt
@@ -1,14 +1,11 @@
 package racingcar.view
 
-class ResultView {
+object ResultView {
+    fun executionResult() {
+        println("실행 결과")
+    }
 
-    companion object {
-        fun executionResult() {
-            println("실행 결과")
-        }
-
-        fun presentSituation(position: Int) {
-            println("-".repeat(position))
-        }
+    fun presentSituation(position: Int) {
+        println("-".repeat(position))
     }
 }
