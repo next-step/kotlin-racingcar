eff0858b86f1ebe1930fa8584fe2798806e07e1f
shinaaaa <shin029abc@gmail.com>
2022-11-09 11:10:47 +0900

    refactor: 변경된 InputView의 함수에 맞게 수정

--- src/main/kotlin/racingcar/application/Application.kt
+++ src/main/kotlin/racingcar/application/Application.kt
@@ -1,21 +1,25 @@
 package racingcar.application
 
+import racingcar.RandomGear
 import racingcar.domain.Car
-import racingcar.domain.Car.Companion.move
+import racingcar.domain.Car.Companion.drive
 import racingcar.view.InputView
 import racingcar.view.ResultView
 
 class Application {
 
     fun main() {
-        val numberOfCars = InputView.registerParticipant()
-        val times = InputView.setNumberOfTimes()
+        InputView.registerParticipant()
+        val numberOfCars = InputView.getParticipant()
+
+        InputView.setNumberOfTimes()
+        val numberOfTimes = InputView.getNumberOfTimes()
 
         ResultView.executionResult()
 
         val cars = Car.registerCars(numberOfCars)
 
-        (1..times).forEach { _ ->
+        (1..numberOfTimes).forEach { _ ->
             cars.map { it.move() }
                 .forEach { ResultView.presentSituation(it) }
             println()
