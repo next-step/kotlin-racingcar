0a7a3345c59189089cdf809cb1dd07a93c7c70b6
shinaaaa <shin029abc@gmail.com>
2022-11-09 13:26:08 +0900

    test: ResultView 테스트 케이스 추가

---
+++ src/test/kotlin/racingcar/ResultViewTest.kt
@@ -0,0 +1,33 @@
+package racingcar
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import racingcar.view.ResultView
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+
+class ResultViewTest {
+
+    private val standardOut = System.out
+    private val outputStreamCaptor = ByteArrayOutputStream()
+
+    @BeforeEach
+    fun setUp() {
+        System.setOut(PrintStream(outputStreamCaptor))
+    }
+
+    @AfterEach
+    fun tearDown() {
+        System.setOut(standardOut)
+    }
+
+    @Test
+    @DisplayName("현재 위치가 5일경우 ----- 값을 반환해야함")
+    fun `Returns a ------value if the current location is 5`() {
+        ResultView.presentSituation(5)
+        assertThat(outputStreamCaptor.toString().trim { it <= ' ' }).isEqualTo("-----")
+    }
+}
