b8439df4cfe739bee6f93253e226267d56bdc8d0
harris.yi <harris.yi@kakaostyle.com>
2022-11-09 20:44:24 +0900

    feature: Validate empty name and Change location access modifier

--- src/main/kotlin/game/racingcar/RacingCar.kt
+++ src/main/kotlin/game/racingcar/RacingCar.kt
@@ -2,15 +2,16 @@ package game.racingcar
 
 import game.racingcar.move.MoveStrategy
 import game.racingcar.move.RandomMoveStrategy
-import java.lang.IllegalArgumentException
 
 class RacingCar(
-    val name: String = "",
+    val name: String,
     private val moveStrategy: MoveStrategy = RandomMoveStrategy(),
-    var location: Int = 0,
 ) {
+    var location: Int = 0
+        private set
     init {
         if (name.length > 5) throw IllegalArgumentException("이름의 길이는 5자를 초과할 수 없습니다.")
+        if (name.isEmpty()) throw IllegalArgumentException("이름은 빈 문자열일 수 없습니다.")
     }
 
     fun move() {
