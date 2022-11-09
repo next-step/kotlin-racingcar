59579169c177803e9e8e69ab04e4538a41069ce0
adforus_hjp <hjp281@adforus.co.kr>
2022-11-09 12:37:34 +0900

    refactor: 사용되는 범위를 생각해서 default -> private 변경
    https://github.com/next-step/kotlin-racingcar/pull/777#discussion_r1016701653

--- src/main/kotlin/racingcar/Movable.kt
+++ src/main/kotlin/racingcar/Movable.kt
@@ -1,8 +1,8 @@
 package racingcar
 
-const val START_DIGIT: Int = 0
-const val MEDIAN_DIGIT: Int = 4
-const val END_DIGIT: Int = 9
+private const val START_DIGIT: Int = 0
+private const val MEDIAN_DIGIT: Int = 4
+private const val END_DIGIT: Int = 9
 
 class Movable(
     private val baseNumber: Int = MEDIAN_DIGIT,
