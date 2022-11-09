f09f0e97935b0b4e53e315039756e17b10b8666d
giyeon95 <giyeon15@gmail.com>
2022-11-09 22:22:12 +0900

    refactor: readLine()!! -> readln() 으로 개선
    코틀린 1.6부터 추가된 새로운 함수입니다!
    +) 입력값이 Int가 아닐 경우에 커스텀 예외를 제공하고 싶다면 `toIntOrNull`을 활용할 수 있을 것 같아요. 이처럼 코틀린은 제공하는 함수들에 null/non-null을 리턴하는 함수 두 개를 모두 제공하는 경우가 많습니다. 상황에 맞게 사용해주시면 될 것 같아요.
    
    참고로, non-null assertion 연산자를 !!처럼 불편한 모양으로 정한 건, 사용할 때 한 번 더 생각해보고 불편하게 사용하라는 코틀린 개발진들의 의도가 들어가있다고 합니다 😄 코틀린이 제공하는 기본 함수에 이를 사용하려고 한다면 non-nullable한 리턴값을 갖는 동일한 함수가 있는지 확인해봐도 좋을 것 같아요.
    
    _Originally posted by @ddaaac in https://github.com/next-step/kotlin-racingcar/pull/760#discussion_r1015689624_

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -5,12 +5,12 @@ object InputView {
     fun inputCarCount(): Int {
         println("자동차 대수는 몇 대인가요?")
 
-        return readLine()!!.toInt()
+        return readln().toInt()
     }
 
     fun inputRacingFullRound(): Int {
         println("시도할 횟수는 몇 회인가요?")
 
-        return readLine()!!.toInt()
+        return readln().toInt()
     }
 }
