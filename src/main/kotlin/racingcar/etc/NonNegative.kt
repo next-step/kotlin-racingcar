fe3f037c1b8986e9443a552daa9147f96063fd20
Raemerrr <foals18@gmail.com>
2022-11-09 11:52:29 +0900

    feat: 음수가 아닌 숫자 객체 생성
    * 0 보다 작은 경우 검증
    * 더하기 기능 추가
    * 음수가 아닌 숫자 비교 로직 추가
    * 테스트 코드 추가

---
+++ src/main/kotlin/racingcar/etc/NonNegative.kt
@@ -0,0 +1,39 @@
+package racingcar.etc
+
+class NonNegative {
+
+    companion object {
+        private const val MINIMUM_NUMBER = 0
+        private const val FORWARD_VALUE = 1
+    }
+
+    private var value: Number
+
+    constructor() {
+        this.value = 0
+    }
+
+    constructor(text: String) {
+        this.value = StringAsNumber(text)
+        validateNegative()
+    }
+
+    fun toInt(): Int {
+        return this.value.toInt()
+    }
+
+    fun plus() {
+        this.value = this.value.toInt().plus(FORWARD_VALUE)
+    }
+
+    operator fun compareTo(other: NonNegative): Int =
+        when {
+            this.value.toInt() > other.value.toInt() -> 1
+            this.value.toInt() < other.value.toInt() -> -1
+            else -> 0
+        }
+
+    private fun validateNegative() {
+        require(MINIMUM_NUMBER <= this.value.toInt()) { "$MINIMUM_NUMBER 보다 작은 수를 입력할 수 없습니다." }
+    }
+}
