a96c144f54b8052b519c8227db8ab6664b55eb07
chayoung-choi <chayoung.dev@gmail.com>
2022-11-09 21:20:25 +0900

    refactor: 자동차에 관한 함수만 유지

--- src/main/kotlin/racing/domain/Car.kt
+++ src/main/kotlin/racing/domain/Car.kt
@@ -3,24 +3,21 @@ package racing.domain
 import racing.domain.strategy.MovingStrategy
 import java.util.UUID
 
-class Car(private val movingStrategy: MovingStrategy, private var position: Int = 0) {
+class Car(
+    private val movingStrategy: MovingStrategy
+) {
+
+    val carId = UUID.randomUUID()
+    var position: Int = 0
+        private set
+
+    fun move() {
+        if (movingStrategy.isMove()) {
+            position += ONE_MOVE_VALUE
+        }
+    }
 
     companion object {
-        const val POSITION_MARK = "-"
         const val ONE_MOVE_VALUE = 1
     }
-
-    private val carId = UUID.randomUUID()
-
-    fun getPosition(): Int = position
-
-    fun move(): Boolean {
-        if (movingStrategy.move()) {
-            position += ONE_MOVE_VALUE
-            return true
-        }
-        return false
-    }
-
-    fun getPositionMark(): String = POSITION_MARK.repeat(position)
 }
--- src/test/kotlin/racing/domain/CarTest.kt
+++ src/test/kotlin/racing/domain/CarTest.kt
@@ -1,9 +1,7 @@
 package racing.domain
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assumptions.assumingThat
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.RepeatedTest
 import org.junit.jupiter.api.Test
 import racing.domain.strategy.RandomMovingStrategy
 
@@ -13,8 +11,6 @@ internal class CarTest {
 
     companion object {
         private val RANDOM_STRATEGY = RandomMovingStrategy()
-        private const val ONE_MOVE_VALUE = Car.ONE_MOVE_VALUE
-        private const val POSITION_MARK = Car.POSITION_MARK
     }
 
     @BeforeEach
@@ -22,22 +18,8 @@ internal class CarTest {
         car = Car(RANDOM_STRATEGY)
     }
 
-    @RepeatedTest(5)
-    fun `자동차 최초 정상 움직임`() {
-        val initPosition = car.getPosition()
-        val isMove = car.move()
-        assumingThat(isMove) {
-            assertThat(car.getPosition()).isEqualTo(ONE_MOVE_VALUE)
-        }
-
-        assumingThat(!isMove) {
-            assertThat(car.getPosition()).isEqualTo(initPosition)
-        }
-    }
-
     @Test
-    fun `이동에 따른 마크 정상 출력`() {
-        (0..10).forEach { _ -> car.move() }
-        assertThat(car.getPositionMark()).isEqualTo(POSITION_MARK.repeat(car.getPosition()))
+    fun `자동차 최초 position은 0이다`() {
+        assertThat(car.position).isEqualTo(0)
     }
 }
