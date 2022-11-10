20423264a8ba89a9855a5919090f377f5b21ea6e
lalize <printf@kakao.com>
2022-11-10 19:21:40 +0900

    feat: 자동차 경주 시작 기능 추가

--- src/main/kotlin/racingcar/CarRacing.kt
+++ src/main/kotlin/racingcar/CarRacing.kt
@@ -7,4 +7,17 @@ class CarRacing(
     init {
         require(carRacers.isNotEmpty()) { "자동차 경주에 참여할 대상이 없습니다." }
     }
+
+    fun start(count: Int = 1) {
+        require(count > 0) { "시도할 횟수는 1이상 이어야 합니다." }
+        check(status.isReady()) { "자동차 경주를 시작할 수 없습니다." }
+
+        repeat(count) { race() }
+
+        status = Status.FINISHED
+    }
+
+    private fun race() {
+        carRacers.forEach { it.drive() }
+    }
 }
--- src/test/kotlin/racingcar/CarRacingTest.kt
+++ src/test/kotlin/racingcar/CarRacingTest.kt
@@ -3,6 +3,7 @@ package racingcar
 import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.assertions.throwables.shouldThrowAny
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.inspectors.forAll
 
 class CarRacingTest : StringSpec({
     "참여 인원이 존재하면 자동차 경주를 생성할 수 있다" {
@@ -11,5 +12,32 @@ class CarRacingTest : StringSpec({
 
     "참여 인원이 없으면 자동차 경주를 생성할 수 없다" {
         shouldThrowAny { CarRacing(carRacers = emptyList()) }
+    }
+
+    "종료된 경주는 다시 시작할 수 없다" {
+        val carRacing = CarRacing(
+            carRacers = listOf(CarRacer(StandardCar(), RandomCarControl())),
+            status = Status.FINISHED
+        )
+
+        shouldThrowAny { carRacing.start() }
+    }
+
+    "시도할 횟수가 0이하면 경주를 시작할 수 없다" {
+        listOf(0, -1, -2)
+            .forAll {
+                val carRacing = CarRacing(carRacers = listOf(CarRacer(StandardCar(), RandomCarControl())))
+
+                shouldThrowAny { carRacing.start(it) }
+            }
+    }
+
+    "시도할 횟수가 1이상이고 준비상태면 경주를 시작할 수 있다" {
+        listOf(1, 2, 3)
+            .forAll {
+                val carRacing = CarRacing(carRacers = listOf(CarRacer(StandardCar(), RandomCarControl())))
+
+                shouldNotThrowAny { carRacing.start(it) }
+            }
     }
 })
