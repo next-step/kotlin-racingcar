59a749f8573fd678695eb4a475e1e7bbc0164bb5
chomily <chomily@woowahan.com>
2022-11-10 20:58:02 +0900

    refactor: Car#move accept NumberGenerator as parameter, add Movement Enum

--- src/main/kotlin/com/nextstep/racingcar/domain/Car.kt
+++ src/main/kotlin/com/nextstep/racingcar/domain/Car.kt
@@ -4,8 +4,9 @@ private const val MOVE_CONDITION = 4
 
 class Car {
 
-    fun move(number: Int): Boolean {
+    fun move(numberGenerator: NumberGenerator): Movement {
+        val number = numberGenerator.generate().toInt()
         require(number in 0..9) { "Number should be greater than or equal 0 and less than or equal 9" }
-        return number >= MOVE_CONDITION
+        return if (number >= MOVE_CONDITION) Movement.MOVE else Movement.NONE
     }
 }
--- src/test/kotlin/com/nextstep/racingcar/domain/CarTest.kt
+++ src/test/kotlin/com/nextstep/racingcar/domain/CarTest.kt
@@ -1,22 +1,29 @@
 package com.nextstep.racingcar.domain
 
+import com.nextstep.racingcar.domain.Movement.MOVE
+import com.nextstep.racingcar.domain.Movement.NONE
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.data.forAll
 import io.kotest.data.row
 import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
 
 class CarTest : StringSpec({
     val car = Car()
+    val randomIntGenerator = mockk<RandomIntGenerator>()
 
     "Only if number parameter is 4 or more, move fun returns true" {
+
         forAll(
-            row(0, false),
-            row(3, false),
-            row(4, true),
-            row(9, true)
-        ) { number, move ->
-            car.move(number) shouldBe move
+            row(0, NONE),
+            row(3, NONE),
+            row(4, MOVE),
+            row(9, MOVE)
+        ) { number, movement ->
+            every { randomIntGenerator.generate() } returns number
+            car.move(randomIntGenerator) shouldBe movement
         }
     }
 
@@ -25,7 +32,8 @@ class CarTest : StringSpec({
             row(-1),
             row(10)
         ) { number ->
-            shouldThrow<IllegalArgumentException> { car.move(number) }
+            every { randomIntGenerator.generate() } returns number
+            shouldThrow<IllegalArgumentException> { car.move(randomIntGenerator) }
         }
     }
 })
