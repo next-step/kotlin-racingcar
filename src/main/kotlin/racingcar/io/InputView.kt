3e014efc2600d431554b596ce92d54933d096de9
Jo-GyuHyeon <kuhyun1993@gmail.com>
2022-11-10 15:51:57 +0900

    feat: 구분자를 포함한 문자열 입력 추가
    - 구분자로 나뉜 글자들의 최대 글자수에 대한 체크

--- src/main/kotlin/racingcar/io/InputView.kt
+++ src/main/kotlin/racingcar/io/InputView.kt
@@ -3,13 +3,28 @@ package racingcar.io
 object InputView {
 
     fun requestPositiveNumber(): Int {
-        val input: String? = readLine()
-        require(!input.isNullOrBlank()) { "빈값을 입력 할 수 없습니다." }
-
+        val input: String = this.requestString()
         val number = input.toIntOrNull()
+
         require(number != null) { "숫자를 입력 해주세요." }
         require(number > 0) { "양수를 입력 해주세요." }
 
         return number
+    }
+
+    fun requestString(delimiters: String, maxElementSize: Int = 5): List<String> {
+        val input: String = this.requestString()
+        val split = input.split(delimiters)
+
+        require(split.all { it.length <= maxElementSize })
+
+        return split
+    }
+
+    private fun requestString(): String {
+        val input: String? = readLine()
+        require(!input.isNullOrBlank()) { "빈값을 입력 할 수 없습니다." }
+
+        return input
     }
 }
