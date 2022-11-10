a68bf865d309f7d158a1c252c073d685a3c7e95e
wrallee <wrallee@naver.com>
2022-11-10 10:24:17 +0900

    feat(RacingCar): 자동차 이동 기능 구현

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -1,11 +1,20 @@
 package racingcar.model
 
 private const val DEFAULT_POSITION = 1
+private const val MOVE_THRESHOLD = 4
+private const val MINIMUM_SCORE = 0
+private const val MAXIMUM_SCORE = 9
 
-class Car(position: Int = DEFAULT_POSITION) {
-    var position: Int = position
+class Car(var position: Int = DEFAULT_POSITION) {
+
+    init {
+        require(position >= DEFAULT_POSITION)
+    }
 
     fun move(score: Int) {
-        TODO("Not yet implemented")
+        require(score in (MINIMUM_SCORE..MAXIMUM_SCORE))
+        if (score >= MOVE_THRESHOLD) {
+            position++
+        }
     }
 }
