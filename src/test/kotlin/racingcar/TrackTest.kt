6df92f08c552c695bb385e3c2746d43274826ffd
Jo-GyuHyeon <kuhyun1993@gmail.com>
2022-11-10 15:56:33 +0900

    feat: 우승자 선출 기능 추가
    - Car 불변객체 적용
    - 변경된 IO 적용
    - 우승자 선출 테스트 케이스 추가

---
+++ src/test/kotlin/racingcar/TrackTest.kt
@@ -0,0 +1,39 @@
+package racingcar
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldHaveAtLeastSize
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
+
+internal class TrackTest : StringSpec({
+    val track = Track(Random())
+
+    "우승자를 선출한다." {
+        val maxPosition = 10
+        val minPosition = 0
+        val cars = listOf(
+            Car("winner", maxPosition),
+            Car("loser", minPosition)
+        )
+
+        val winners: List<Car> = track.getWinner(cars)
+
+        winners shouldHaveSize 1
+        winners.first().name shouldBe "winner"
+    }
+
+    "우승자는 1명 이상일 수 있다." {
+        val maxPosition = 10
+        val minPosition = 0
+        val cars = listOf(
+            Car("winner", maxPosition),
+            Car("co-winner", maxPosition),
+            Car("loser", minPosition)
+        )
+
+        val winners = track.getWinner(cars)
+
+        winners shouldHaveAtLeastSize 1
+        winners shouldHaveSize 2
+    }
+})
