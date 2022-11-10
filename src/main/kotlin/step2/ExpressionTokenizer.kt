71d60d3162cf5e14e6a38492353145c2c4c603c7
relkimm <bbonggu92@gmail.com>
2022-11-10 20:37:27 +0900

    refactor: 함수 한줄인 경우 {} 대신 = 으로 표현

--- src/main/kotlin/step2/ExpressionTokenizer.kt
+++ src/main/kotlin/step2/ExpressionTokenizer.kt
@@ -3,10 +3,9 @@ package step2
 object ExpressionTokenizer {
     private const val SPACE = " "
 
-    fun tokenize(expression: String): List<Token> {
-        return expression
+    fun tokenize(expression: String): List<Token> =
+        expression
             .split(SPACE)
             .filter { !it.isNullOrBlank() }
             .map { Token.of(value = it) }
-    }
 }
