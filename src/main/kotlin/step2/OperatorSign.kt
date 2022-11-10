13f48836b2e681478e163492b16493e9087c03b1
relkimm <bbonggu92@gmail.com>
2022-11-10 21:47:15 +0900

    fix: 오타 수정

--- src/main/kotlin/step2/OperatorSign.kt
+++ src/main/kotlin/step2/OperatorSign.kt
@@ -13,7 +13,7 @@ enum class OperatorSign(
     companion object {
         fun getBySign(sign: String): OperatorSign {
             return OperatorSign.values().find { it.sign == sign }
-                ?: throw throw IllegalArgumentException("연산 기호는 +, -, *, / 만 입력할 수 있습니다.")
+                ?: throw IllegalArgumentException("연산 기호는 +, -, *, / 만 입력할 수 있습니다.")
         }
     }
 }
