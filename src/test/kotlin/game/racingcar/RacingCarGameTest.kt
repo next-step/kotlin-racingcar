2b9468656ad41707c2c934bd32798e0e97b45f10
harris.yi <harris.yi@kakaostyle.com>
2022-11-11 23:18:52 +0900

    test: Change given phrase as RacingCarGame constructor changes

--- src/test/kotlin/game/racingcar/RacingCarGameTest.kt
+++ src/test/kotlin/game/racingcar/RacingCarGameTest.kt
@@ -1,6 +1,7 @@
 package game.racingcar
 
 import game.racingcar.domain.RacingCarGame
+import game.racingcar.domain.RacingCars
 import game.racingcar.move.AlwaysMoveStrategy
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -10,7 +11,7 @@ internal class RacingCarGameTest() {
     @Test
     fun `RacingCarGame이 AlwaysMoveStrategy로 생성되었을 때 위치 스냅샷이 1씩 증가한다`() {
         // given
-        val racingCarGame = RacingCarGame(listOf("haris", "horis", "huris"), 5, AlwaysMoveStrategy())
+        val racingCarGame = RacingCarGame(5, RacingCars(listOf("haris", "horis", "huris"), AlwaysMoveStrategy()))
 
         // when
         val locationSnapshots = racingCarGame.play()
