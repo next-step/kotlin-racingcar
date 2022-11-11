b2466a525e05f7b73cbcc8d932ebc776251e4d3b
Minkyung <mminkyung.k@gmail.com>
2022-11-11 19:11:50 +0900

    test: CarFactory with kotest string spec

---
+++ src/test/kotlin/step3/CarFactorySpec.kt
@@ -0,0 +1,23 @@
+package step3
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.inspectors.shouldForAll
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.types.shouldBeInstanceOf
+
+class CarFactorySpec : StringSpec({
+    "지정한 수만큼 Car 인스턴스를 반환한다" {
+        // arrange
+        val size = 5
+        val carFactory = CarFactory()
+
+        // act
+        val cars = carFactory.create(size)
+
+        // assert
+        cars shouldHaveSize size
+        cars.shouldForAll {
+            it.shouldBeInstanceOf<Car>()
+        }
+    }
+})
