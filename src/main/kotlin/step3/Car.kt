359f3fabf0ae0988c54d742d0a72dd2013876ef2
relkimm <bbonggu92@gmail.com>
2022-11-11 23:56:17 +0900

    refactor: Car 주행 가능 여부 판단할 수 있는 로직 별도 메서드로 분리

--- src/main/kotlin/step3/Car.kt
+++ src/main/kotlin/step3/Car.kt
@@ -4,8 +4,16 @@ class Car {
     var position = 0
         private set
     fun move(fuel: Int) {
-        if (fuel >= 4) {
+        if (canMove(fuel = fuel)) {
             this.position++
         }
+    }
+
+    private fun canMove(fuel: Int): Boolean {
+        return fuel >= CAN_MOVE_FUEL
+    }
+
+    companion object {
+        private const val CAN_MOVE_FUEL = 4
     }
 }
