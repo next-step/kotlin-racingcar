c3ad11b94ba51ac8a4831a5f037007fa3daff316
lalize <printf@kakao.com>
2022-11-11 22:22:15 +0900

    refactor: Record 클래스내 불필요한 필드 제거

--- src/main/kotlin/racingcar/Record.kt
+++ src/main/kotlin/racingcar/Record.kt
@@ -2,12 +2,10 @@ package racingcar
 
 data class Record(
     val round: Int,
-    val carRacer: CarRacer,
     val position: Position
 ) {
     constructor(round: Int, carRacer: CarRacer) : this(
         round = round,
-        carRacer = carRacer,
         position = carRacer.position
     )
 }
