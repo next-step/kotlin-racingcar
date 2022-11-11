edb88b0fe39d0bd8e7e027d6f81b8025f8a65c4b
harris.yi <harris.yi@kakaostyle.com>
2022-11-11 23:17:22 +0900

    feature: Change parameters of RacingCarGame constructor includes RacingCars Already made from main

--- src/main/kotlin/game/racingcar/RacingCarMain.kt
+++ src/main/kotlin/game/racingcar/RacingCarMain.kt
@@ -1,13 +1,16 @@
 package game.racingcar
 
 import game.racingcar.domain.RacingCarGame
+import game.racingcar.domain.RacingCars
+import game.racingcar.move.RandomMoveStrategy
 import game.racingcar.view.InputView
 import game.racingcar.view.OutputView
 
 fun main() {
     val (carNames, numberOfTrials) = InputView.getInputFromConsole()
 
-    val racingCarGame = RacingCarGame(carNames, numberOfTrials)
+    val racingCars = RacingCars(carNames, RandomMoveStrategy())
+    val racingCarGame = RacingCarGame(numberOfTrials, racingCars)
     val racingCarPlayResult = racingCarGame.play()
 
     OutputView.printOutputToConsole(racingCarPlayResult)
--- src/main/kotlin/game/racingcar/domain/RacingCarGame.kt
+++ src/main/kotlin/game/racingcar/domain/RacingCarGame.kt
@@ -2,16 +2,12 @@ package game.racingcar.domain
 
 import game.racingcar.dto.CarLocationSnapshot
 import game.racingcar.dto.RacingCarPlayResult
-import game.racingcar.move.MoveStrategy
-import game.racingcar.move.RandomMoveStrategy
 
 class RacingCarGame(
-    private val carNames: List<String>,
     private val numberOfTrials: Int,
-    private val moveStrategy: MoveStrategy = RandomMoveStrategy()
+    private val racingCars: RacingCars
 ) {
     fun play(): RacingCarPlayResult {
-        val racingCars = RacingCars(carNames, moveStrategy)
         return (1..numberOfTrials)
             .map { racingCars.moveAll() }
             .map { CarLocationSnapshot(it) }
