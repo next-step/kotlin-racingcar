476d2e4ab60dcec3f736baf3d4d0b0efb6cf0bf1
shinaaaa <shin029abc@gmail.com>
2022-11-11 14:19:04 +0900

    test: CarTest의 테스트 케이스를 명시적으로 변경

--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -12,9 +12,7 @@ class CarTest {
     @DisplayName("전진 조건일 경우 자동차의 현재 위치가 1 증가함")
     fun `Increase by 1 if forward`() {
         val cars = Car.registerCars(1)
-        Gear.FORWARD.range = 0..9
-        Gear.STOP.range = -1..-1
-        cars.first().drive(RandomGear.getGear())
+        cars.first().drive(Gear.FORWARD)
         assertThat(cars.first().currentPosition).isEqualTo(1)
     }
 
@@ -22,9 +20,7 @@ class CarTest {
     @DisplayName("정지 조건일 경우 자동차의 현재 위치 고정")
     fun `Fixed if stop`() {
         val cars = Car.registerCars(1)
-        Gear.FORWARD.range = -1..-1
-        Gear.STOP.range = 0..9
-        cars.first().drive(RandomGear.getGear())
+        cars.first().drive(Gear.STOP)
         assertThat(cars.first().currentPosition).isEqualTo(0)
     }
 }
