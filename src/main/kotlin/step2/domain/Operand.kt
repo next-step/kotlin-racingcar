964ea1cb0eb31164dfe451febf11846f02a81cc5
namonak <namonak@gmail.com>
2022-11-11 12:19:27 +0900

    refactor(step2): change the Operand class to a data class

--- src/main/kotlin/step2/domain/Operand.kt
+++ src/main/kotlin/step2/domain/Operand.kt
@@ -1,19 +1,9 @@
 package step2.domain
 
-class Operand(operand: Int) {
+data class Operand(val operand: Int) {
     val value: Int = operand
 
     init {
         require(operand >= 0) { "음수는 입력할 수 없습니다." }
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return javaClass.hashCode()
     }
 }
