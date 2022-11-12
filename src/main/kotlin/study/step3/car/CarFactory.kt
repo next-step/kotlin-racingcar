25de07c5680f1cccf3dc894a9a5ac16542e47bc8
wonjun3991 <wonjun.choi@meshkorea.net>
2022-11-12 23:38:03 +0900

    fix(CarFactory): Use Initialize a List in Kotlin

--- src/main/kotlin/study/step3/car/CarFactory.kt
+++ src/main/kotlin/study/step3/car/CarFactory.kt
@@ -2,6 +2,8 @@ package study.step3.car
 
 object CarFactory {
     fun createCars(count: Int): List<Car> {
-        return (1..count).map { Car() }
+        return List(count) {
+            Car()
+        }
     }
 }
