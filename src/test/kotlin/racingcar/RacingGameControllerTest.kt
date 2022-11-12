5c284d29ae87185e5dafb6f7a375b474bf7d6e84
0923kdh <0923kdh@gmail.com>
2022-11-13 00:38:24 +0900

    chore: 클래스 이름 변경

--- src/test/kotlin/racingcar/RacingGameControllerTest.kt
+++ src/test/kotlin/racingcar/RacingGameControllerTest.kt
@@ -3,7 +3,7 @@ package racingcar
 import io.kotest.core.spec.style.StringSpec
 import racingcar.io.ParticipateCarName
 
-internal class RacingGameTest : StringSpec({
+internal class RacingGameControllerTest : StringSpec({
     "경주에 참여하는 자동차 이름, 시도할 횟수가 주어지면 게임을 진행할 수 있다." {
         val nameOfCars = listOf<ParticipateCarName>(
             ParticipateCarName("car1"),
