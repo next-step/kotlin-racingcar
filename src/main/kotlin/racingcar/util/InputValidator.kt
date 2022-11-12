5c8b63dc13da96890d6862eaf964d7fbe9ee61b5
SangHyunGil <zizon5941@gmail.com>
2022-11-12 15:20:27 +0900

    feat: InputValidator 테스트 구현

---
+++ src/main/kotlin/racingcar/util/InputValidator.kt
@@ -0,0 +1,11 @@
+package racingcar.util
+
+object InputValidator {
+    fun validateCarNameStr(input: String?) {
+        // TODO
+    }
+
+    fun validateNumberOfTryStr(input: String?) {
+        // TODO
+    }
+}
---
+++ src/test/kotlin/racingcar/util/InputValidatorTest.kt
@@ -0,0 +1,73 @@
+package racingcar.util
+
+import io.kotest.assertions.throwables.shouldNotThrowAny
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import racingcar.const.CarException
+
+internal class InputValidatorTest: BehaviorSpec({
+    Given("차의 이름을 받는 입력에 대해, ") {
+        When("null 값이 들어오면, ") {
+            then("예외를 반환한다.") {
+                val exception = shouldThrow<IllegalArgumentException> {
+                    InputValidator.validateCarNameStr(null)
+                }
+                exception.message shouldBe CarException.NULL_INPUT_ERROR
+            }
+        }
+
+        When("5자리를 초과한다면, ") {
+            Then("예외를 반환한다.") {
+                val exception = shouldThrow<IllegalArgumentException> {
+                    InputValidator.validateCarNameStr("SangHyun")
+                }
+                exception.message shouldBe CarException.MORE_THAN_FIVE_WORD_ERROR
+            }
+        }
+
+        When("정상적이라면, ") {
+            Then("정상 수행된다.") {
+                shouldNotThrowAny {
+                    InputValidator.validateCarNameStr("SH")
+                }
+            }
+        }
+    }
+
+    Given("시도 횟수를 받는 입력에 대해, ") {
+        When("null 값이 들어오면, ") {
+            Then("예외를 반환한다.") {
+                val exception = shouldThrow<IllegalArgumentException> {
+                    InputValidator.validateNumberOfTryStr(null)
+                }
+                exception.message shouldBe CarException.NULL_INPUT_ERROR
+            }
+        }
+
+        When("숫자가 아니라면, ") {
+            Then("예외를 반환한다.") {
+                val exception = shouldThrow<IllegalArgumentException> {
+                    InputValidator.validateNumberOfTryStr("SH")
+                }
+                exception.message shouldBe CarException.NOT_DIGIT_ERROR
+            }
+        }
+
+        When("1 이상이 아니라면, ") {
+            Then("예외를 반환한다.") {
+                val exception = shouldThrow<IllegalArgumentException> {
+                    InputValidator.validateNumberOfTryStr("0")
+                }
+                exception.message shouldBe CarException.MORE_THAN_ONE
+            }
+        }
+        When("정상적이라면, ") {
+            Then("정상 수행된다.") {
+                shouldNotThrowAny {
+                    InputValidator.validateNumberOfTryStr("3")
+                }
+            }
+        }
+    }
+})
