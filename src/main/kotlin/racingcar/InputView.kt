a1bac92943c901358577eddc2d64c23935e3df60
dk-ko <dev.dkko@gmail.com>
2022-11-12 17:38:48 +0900

    feat: 자동차 이름 길이 제한

--- src/main/kotlin/racingcar/InputView.kt
+++ src/main/kotlin/racingcar/InputView.kt
@@ -5,7 +5,19 @@ class InputView {
         return readln().toInt()
     }
 
-    fun inputString(): String {
-        return readln()
+    fun inputCarNames(): List<String> {
+        val input = readln()
+        val carNames = input.trim().split(",")
+
+        for (carName in carNames) {
+            if (carName.length > NAME_LENGTH_CONDITION)
+                throw IllegalArgumentException("자동자 이름은 ${NAME_LENGTH_CONDITION}자를 넘을 수 없습니다.")
+        }
+
+        return carNames
+    }
+
+    companion object {
+        const val NAME_LENGTH_CONDITION = 5
     }
 }
--- src/main/kotlin/racingcar/RacingCarApplication.kt
+++ src/main/kotlin/racingcar/RacingCarApplication.kt
@@ -5,8 +5,7 @@ fun main() {
     val inputView = InputView()
 
     println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
-    val inputString = inputView.inputString()
-    val carNames = inputString.split(",")
+    val carNames = inputView.inputCarNames()
 
     val carList = mutableListOf<Car>()
     for (carName in carNames) {
