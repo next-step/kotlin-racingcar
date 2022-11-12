f0c68db07a6ec1d275302d8f1197f4ac87b3a203
이상인/wefu0000 <wefu.child@gmail.com>
2022-11-13 01:15:56 +0900

    refactor: ktlintcheck 사항 반영

--- src/main/kotlin/calculator/OperatorType.kt
+++ src/main/kotlin/calculator/OperatorType.kt
@@ -6,8 +6,7 @@ import calculator.operation.Division
 import calculator.operation.Multiplication
 import calculator.operation.Subtraction
 
-enum class OperatorType(val command: String,
-    val operator: ArithmeticOperations) {
+enum class OperatorType(val command: String, val operator: ArithmeticOperations) {
     PLUS("+", Addition()),
     MINUS("-", Subtraction()),
     MULTIPLY("*", Multiplication()),
