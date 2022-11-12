cdf0d022ad93f6895868ed9bef28d3708db8628c
이상인/wefu0000 <wefu.child@gmail.com>
2022-11-12 23:35:43 +0900

    refactor: 리뷰 사항 반영
    require 를 통해 IllegalArgumentException 발생

--- src/main/kotlin/calculator/ValidCheck.kt
+++ src/main/kotlin/calculator/ValidCheck.kt
@@ -8,17 +8,14 @@ object ValidCheck {
 
     @Throws(IllegalArgumentException::class)
     fun checkSplitArrayCount(split: List<String>?): Boolean {
-        if ((split?.size ?: 0) < 3) throw IllegalArgumentException("문자열에 숫자나 연산자 개수 부족")
-        if ((split?.size ?: 0) % 2 == 0) throw IllegalArgumentException("문자열에 숫자나 연산자 개수가 맞지 않음")
-
+        require((split?.size ?: 0) < 3) { "문자열에 숫자나 연산자 개수 부족" }
+        require((split?.size ?: 0) % 2 == 0) { "문자열에 숫자나 연산자 개수 부족" }
         return true
     }
 
     @Throws(IllegalArgumentException::class)
     fun checkEmptyString(input: String?): Boolean {
-        if (input.isNullOrBlank())
-            throw IllegalArgumentException("입력값이 null 이거나 빈 공백 문자")
-
+        require(!input.isNullOrBlank()) { "입력값이 null 이거나 빈 공백 문자" }
         return true
     }
 }
