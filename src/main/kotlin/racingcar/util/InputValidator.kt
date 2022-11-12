dd0fd282fc4b277ce01be3e90678c1129831344b
SangHyunGil <zizon5941@gmail.com>
2022-11-12 16:40:54 +0900

    feat: 빈 값에 대해서도 예외를 반환하도록 구현

--- src/main/kotlin/racingcar/util/InputValidator.kt
+++ src/main/kotlin/racingcar/util/InputValidator.kt
@@ -5,17 +5,17 @@ import racingcar.extension.isNumeric
 
 object InputValidator {
     fun validateCarNameStr(input: String?) {
-        require(input != null) { CarException.NULL_INPUT_ERROR }
+        require(!input.isNullOrBlank()) { CarException.NULL_INPUT_ERROR }
         require(input.length <= 5) { CarException.MORE_THAN_FIVE_WORD_ERROR }
     }
 
     fun validateNumberOfTryStr(input: String?) {
-        require(input != null) { CarException.NULL_INPUT_ERROR }
+        require(!input.isNullOrBlank()) { CarException.NULL_INPUT_ERROR }
         require(input.isNumeric()) { CarException.NOT_DIGIT_ERROR }
         require(input.toInt() > 0) { CarException.MORE_THAN_ONE }
     }
 
     fun validateParserStr(input: String?) {
-        require(input != null) { CarException.NULL_INPUT_ERROR }
+        require(!input.isNullOrBlank()) { CarException.NULL_INPUT_ERROR }
     }
 }
