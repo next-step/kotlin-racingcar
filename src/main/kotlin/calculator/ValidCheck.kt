7e371a30f2991cb6ed4e85c62ae4a54623348aea
이상인/wefu0000 <wefu.child@gmail.com>
2022-11-12 23:38:08 +0900

    refactor: 리뷰 사항 반영
    ValidCheck boolean return 제거

--- src/main/kotlin/calculator/ValidCheck.kt
+++ src/main/kotlin/calculator/ValidCheck.kt
@@ -2,20 +2,19 @@ package calculator
 
 object ValidCheck {
     @Throws(IllegalArgumentException::class)
-    fun checkValid(input: String?): Boolean {
-        return checkEmptyString(input) && checkSplitArrayCount(input?.split(" "))
+    fun checkValid(input: String?) {
+        checkEmptyString(input)
+        checkSplitArrayCount(input?.split(" "))
     }
 
     @Throws(IllegalArgumentException::class)
-    fun checkSplitArrayCount(split: List<String>?): Boolean {
+    fun checkSplitArrayCount(split: List<String>?) {
         require((split?.size ?: 0) < 3) { "문자열에 숫자나 연산자 개수 부족" }
         require((split?.size ?: 0) % 2 == 0) { "문자열에 숫자나 연산자 개수 부족" }
-        return true
     }
 
     @Throws(IllegalArgumentException::class)
-    fun checkEmptyString(input: String?): Boolean {
+    fun checkEmptyString(input: String?) {
         require(!input.isNullOrBlank()) { "입력값이 null 이거나 빈 공백 문자" }
-        return true
     }
 }
