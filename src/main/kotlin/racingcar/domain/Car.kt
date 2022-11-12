dd2c6c0f5760ee796f58abbb7521a936cec49aa0
jysohn0825 <jysohn0825@naver.com>
2022-11-12 21:39:20 +0900

    feat: Add name property in Car domain
    - add name property and valid method
    - add test code for valid method

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -1,13 +1,21 @@
 package racingcar.domain
 
+import java.lang.IllegalArgumentException
+
 class Car(
-    var route: Int = 0
+    var route: Int = 0,
+    val name: String = ""
 ) {
     fun move() {
         this.route += 1
     }
 
+    fun valid() {
+        require(name.length < MAX_CAR_NAME_LENGTH) { IllegalArgumentException("차 이름은 5자를 넘으면 안 됩니다.") }
+    }
+
     companion object {
+        val MAX_CAR_NAME_LENGTH = 5
         fun generateCar(count: Int) = buildList {
             repeat(count) { add(Car()) }
         }
--- src/test/kotlin/racingcar/domain/CarTest.kt
+++ src/test/kotlin/racingcar/domain/CarTest.kt
@@ -2,6 +2,8 @@ package racingcar.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.IllegalArgumentException
 
 class CarTest {
 
@@ -9,5 +11,11 @@ class CarTest {
     fun `입력 값에 따른 Car 클래스 생성 개수 확인`() {
         val carList = Car.generateCar(1)
         assertThat(carList.size).isEqualTo(1)
+    }
+
+    @Test
+    fun `차 이름이 다섯 자리가 넘어가면 에러 발생하는지 확인`() {
+        val car = Car(1, "12345")
+        assertThrows<IllegalArgumentException> { car.valid() }
     }
 }
