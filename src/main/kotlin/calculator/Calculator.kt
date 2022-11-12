277bd88c9abaf7b4f565d18bcd96850fb8210763
ajy9844 <ajy09844@gmail.com>
2022-11-12 17:08:45 +0900

    test(calculator): add test for string calculator

---
+++ src/main/kotlin/calculator/Calculator.kt
@@ -0,0 +1,8 @@
+package calculator
+
+interface Calculator {
+    fun plus(a: Int, b: Int): Int
+    fun minus(a: Int, b: Int): Int
+    fun times(a: Int, b: Int): Int
+    fun div(a: Int, b: Int): Int
+}
