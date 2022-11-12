e6ae6515ec0160c49707d4c1708162ea8d275d9c
jysohn0825 <jysohn0825@naver.com>
2022-11-12 23:39:14 +0900

    refactor: Change ResultView

--- src/main/kotlin/racingcar/RacingGame.kt
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -15,10 +15,4 @@ class RacingGame {
 
         resultView.getRaceResult(race, cars)
     }
-
-    private fun ResultView.getRaceResult(race: Race, cars: List<Car>) {
-        this.printResultTitle()
-        repeat(race.round) { this.printCarsInfo(race.run(cars)) }
-        this.printWinner(race, cars)
-    }
 }
--- src/main/kotlin/racingcar/view/ResultView.kt
+++ src/main/kotlin/racingcar/view/ResultView.kt
@@ -4,18 +4,25 @@ import racingcar.domain.Car
 import racingcar.domain.Race
 
 class ResultView {
-    fun printResultTitle() {
+
+    fun getRaceResult(race: Race, cars: List<Car>) {
+        this.printResultTitle()
+        repeat(race.round) { this.printCarsInfo(race.run(cars)) }
+        this.printWinner(race, cars)
+    }
+
+    private fun printResultTitle() {
         println()
         print("실행 결과")
     }
 
-    fun printWinner(race: Race, carsInfo: List<Car>) {
+    private fun printWinner(race: Race, carsInfo: List<Car>) {
         println()
         val winners = race.findWinner(carsInfo).joinToString(",") { it.name }
         println("${winners}가 최종 우승했습니다.")
     }
 
-    fun printCarsInfo(carsInfo: List<Car>) {
+    private fun printCarsInfo(carsInfo: List<Car>) {
         println()
         carsInfo.forEach {
             printCarInfo(it.name)
