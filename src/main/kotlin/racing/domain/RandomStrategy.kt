21918a9f5fc3fe0b54b074232a73205fd4453cae
soo <yunsoo0510@daum.net>
2022-11-13 01:44:18 +0900

    refactor: 코틀린의 Random 사용하기

--- src/main/kotlin/racing/domain/RandomStrategy.kt
+++ src/main/kotlin/racing/domain/RandomStrategy.kt
@@ -1,17 +1,16 @@
 package racing.domain
 
-import java.util.*
+import kotlin.random.Random.Default.nextInt
 
 class RandomStrategy : MoveStrategy {
     private val randomNumber: Int
-        private get() = random.nextInt(10)
+        private get() = nextInt(10)
 
     override fun canMove(): Boolean {
         return randomNumber >= MOVE_BOUNDARY
     }
 
     companion object {
-        private val random = Random()
         private const val MOVE_BOUNDARY = 4
 
         val instance = RandomStrategy()
