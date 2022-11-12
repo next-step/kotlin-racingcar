a1ae19484bffa80738d87b5ef57992730ed495f9
SangHyunGil <zizon5941@gmail.com>
2022-11-12 15:22:40 +0900

    feat: InputValidator 구현

--- src/main/kotlin/racingcar/util/InputValidator.kt
+++ src/main/kotlin/racingcar/util/InputValidator.kt
@@ -1,11 +1,17 @@
 package racingcar.util
 
+import racingcar.const.CarException
+import racingcar.extension.isNumeric
+
 object InputValidator {
     fun validateCarNameStr(input: String?) {
-        // TODO
+        require(input != null) { CarException.NULL_INPUT_ERROR }
+        require(input.length <= 5) { CarException.MORE_THAN_FIVE_WORD_ERROR }
     }
 
     fun validateNumberOfTryStr(input: String?) {
-        // TODO
+        require(input != null) { CarException.NULL_INPUT_ERROR }
+        require(input.isNumeric()) { CarException.NOT_DIGIT_ERROR }
+        require(input.toInt() > 0) { CarException.MORE_THAN_ONE }
     }
 }
