8c824c66ae44340f96cc52c6266bd5c2e92daf46
giyeon95 <giyeon15@gmail.com>
2022-11-12 21:58:20 +0900

    test: 테스트코드 리뷰 반영

--- src/test/kotlin/racingcar/RaceStageTest.kt
+++ src/test/kotlin/racingcar/RaceStageTest.kt
@@ -18,11 +18,12 @@ internal class RaceStageTest {
     @DisplayName("자동차가 갯수만큼 생성된다.")
     internal fun createTest() {
         // given
+        val carCount = 5
 
         // when
-        val raceStage = RaceStage.create(5)
+        val raceStage = RaceStage.create(carCount)
 
         // then
-        assertThat(raceStage.cars).hasSize(5)
+        assertThat(raceStage.cars).hasSize(carCount)
     }
 }
