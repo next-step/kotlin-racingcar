5bbd55395ead40f1afa2ab91f3e62a126a0af444
0923kdh <0923kdh@gmail.com>
2022-11-12 14:15:02 +0900

    chore: 불필요 코드 제거

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -13,16 +13,3 @@ class Car(
         }
     }
 }
-
-// class Car(position: Int = 0) {
-//     var position: Int = position
-//         private set
-//
-//     fun move(foreword: Int) {
-//         if (foreword >= FOREWORD_CRITERIA) this.position++
-//     }
-//
-//     companion object {
-//         private const val FOREWORD_CRITERIA = 4
-//     }
-// }
