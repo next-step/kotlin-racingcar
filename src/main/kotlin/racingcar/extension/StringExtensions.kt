7fa548984fc16d8305d8489384f2e1abc44e775f
SangHyunGil <zizon5941@gmail.com>
2022-11-12 15:37:59 +0900

    feat: 공백 제거 구현

--- src/main/kotlin/racingcar/extension/StringExtensions.kt
+++ src/main/kotlin/racingcar/extension/StringExtensions.kt
@@ -1,3 +1,6 @@
 package racingcar.extension
 
+private val DELIMITER_REPLACE_REGEX = "\\s+".toRegex()
+
 fun String.isNumeric() = this.all { it.isDigit() }
+fun String.removeWhitespaces() = this.replace(DELIMITER_REPLACE_REGEX, "")
