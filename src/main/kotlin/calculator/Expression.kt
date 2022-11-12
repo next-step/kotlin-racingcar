64ca7166498c449959499da9ddd9003f3a5000f2
Choiyounho <maxosa78@gmail.com>
2022-11-12 21:11:57 +0900

    refactor : 수식 클래스 내 프린트문 제거

--- src/main/kotlin/calculator/Expression.kt
+++ src/main/kotlin/calculator/Expression.kt
@@ -20,7 +20,6 @@ class Expression(
     }
 
     private fun validateExpression(expressionContents: List<String>) {
-        println(expressionContents.size)
         validateEmptyExpression(expressionContents)
         validateNumber(expressionContents)
         validateOperator(expressionContents)
