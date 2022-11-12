92ec7ee07c3f9c977f3506df2d1f83f6816781dd
tax1116 <tax941116@gmail.com>
2022-11-13 01:36:15 +0900

    review: - rename operator to opr - convert magic number to constant

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -1,15 +1,19 @@
 package calculator
 
 class Calculator {
+    companion object {
+        const val EXPRESSION_DELIMITER = " "
+    }
+
     fun calculate(expression: String): Int {
-        val exprArr = expression.split(" ")
+        val exprArr = expression.split(EXPRESSION_DELIMITER)
 
         var result = exprArr[0].toIntOrNull() ?: throw IllegalArgumentException()
 
         for (id: Int in 1 until exprArr.size step 2) {
-            val operator = Operator.valueOf(sign = exprArr[id][0]) ?: throw IllegalArgumentException()
+            val opr = Operator.valueOf(sign = exprArr[id][0]) ?: throw IllegalArgumentException()
             val operand = exprArr[id + 1].toIntOrNull() ?: throw IllegalArgumentException()
-            result = operator.operate(result, operand)
+            result = opr.operate(result, operand)
         }
 
         return result
