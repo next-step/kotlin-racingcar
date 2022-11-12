113b2a36f3f12977bcd3b9501ba93dbafa5104d8
SangHyunGil <zizon5941@gmail.com>
2022-11-12 22:02:09 +0900

    feat: 우승자 추출할 수 있도록 구현

--- src/main/kotlin/racingcar/io/ResultView.kt
+++ src/main/kotlin/racingcar/io/ResultView.kt
@@ -1,15 +1,27 @@
 package racingcar.io
 
+import racingcar.CarRecord
 import racingcar.GameResult
 import racingcar.Record
 
 object ResultView {
     private const val TRACE = "-"
+    private const val EXPLAIN_WINNER_MESSAGE = "가 최종 우승했습니다."
+    private const val SEPARATOR = ","
 
     fun print(gameResult: GameResult) {
         gameResult.forEach { record ->
             printOneRound(record)
         }
+        printWinner(gameResult)
+    }
+
+    private fun printWinner(gameResult: GameResult) {
+        val frontRunners = gameResult
+            .getLastRoundRecord()
+            .findFrontRunner()
+        val winnerNames = extractWinnerNames(frontRunners)
+        println("$winnerNames$EXPLAIN_WINNER_MESSAGE")
     }
 
     private fun printOneRound(record: Record) {
@@ -17,5 +29,9 @@ object ResultView {
             println("$name : ${TRACE.repeat(position + 1)}")
         }
         println()
+    }
+
+    private fun extractWinnerNames(frontRunners: List<CarRecord>): String {
+        return frontRunners.joinToString(SEPARATOR) { it.name }
     }
 }
