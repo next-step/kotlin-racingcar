d4926579a2fd4559b106d98cdecc89f3120c01b6
SangHyunGil <zizon5941@gmail.com>
2022-11-12 16:42:39 +0900

    feat: Parser 구현

--- src/main/kotlin/racingcar/util/Parser.kt
+++ src/main/kotlin/racingcar/util/Parser.kt
@@ -1,7 +1,13 @@
 package racingcar.util
 
+import racingcar.extension.removeWhitespaces
+
 object Parser {
+    private val COMMA_SPLIT_REGEX = ",".toRegex()
+
     fun parse(rawString: String?): List<String> {
-        return emptyList()
+        InputValidator.validateParserStr(rawString)
+        val removeWhiteString = rawString!!.removeWhitespaces()
+        return removeWhiteString.split(COMMA_SPLIT_REGEX)
     }
 }
