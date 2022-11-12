3293a0345f2008b9254bd138493ada5002cc905b
namonak <namonak@gmail.com>
2022-11-13 01:43:47 +0900

    feat: modify the position property of the Position class to have an integer value greater than or equal to 0

--- src/main/kotlin/step3/domain/car/status/Position.kt
+++ src/main/kotlin/step3/domain/car/status/Position.kt
@@ -1,6 +1,10 @@
 package step3.domain.car.status
 
 data class Position(var position: Int) {
+    init {
+        require(position >= 0) { "자동차의 위치는 0 이상이어야 합니다." }
+    }
+
     fun move() {
         position++
     }
--- src/test/kotlin/step3/domain/car/status/PositionTest.kt
+++ src/test/kotlin/step3/domain/car/status/PositionTest.kt
@@ -1,6 +1,7 @@
 package step3.domain.car.status
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
@@ -10,6 +11,14 @@ class PositionTest {
     fun `Position 생성 테스트`() {
         val position = Position(0)
         assertThat(position).isEqualTo(Position(0))
+    }
+
+    @DisplayName("Position 이동 예외처리 테스트")
+    @Test
+    fun `Position 이동 예외처리 테스트`() {
+        assertThatThrownBy { Position(-1) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining("자동차의 위치는 0 이상이어야 합니다.")
     }
 
     @DisplayName("Position 이동 테스트")
