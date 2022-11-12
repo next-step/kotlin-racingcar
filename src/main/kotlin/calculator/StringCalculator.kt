15d44f4d9a5fe2a0fe9f6e30fb93370c217a8c01
khcho902 <khcho902@gmail.com>
2022-11-12 17:49:44 +0900

    refactor: splitInput 메소드 분리

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -2,23 +2,28 @@ package calculator
 
 object StringCalculator {
 
+    private const val DELIMITER = " "
+
     fun calculate(input: String?): Long {
+        val splitInput: List<String> = splitInput(input)
 
-        require(!input.isNullOrBlank()) { "입력값은 null이나 빈 공백 문자일 수 없습니다." }
-
-        val split: List<String> = input.split(" ")
-        require(split.size % 2 != 0) { "피연산자, 연산자의 개수의 합은 홀수여야 합니다." }
-
-        var result = split[0].toLongOrThrow()
-        for (i in 1 until split.size step 2) {
-
-            val operator = ArithmeticOperator.of(split[i])
-            val operand = split[i + 1].toLongOrThrow()
-
+        var result = splitInput[0].toLongOrThrow()
+        for (i in 1 until splitInput.size step 2) {
+            val operator = ArithmeticOperator.of(splitInput[i])
+            val operand = splitInput[i + 1].toLongOrThrow()
             result = operator.operate(result, operand)
         }
 
         return result
+    }
+
+    private fun splitInput(input: String?): List<String> {
+        require(!input.isNullOrBlank()) { "입력값은 null이나 빈 공백 문자일 수 없습니다." }
+
+        val splitInput: List<String> = input.split(DELIMITER)
+        require(splitInput.size % 2 != 0) { "피연산자, 연산자의 개수의 합은 홀수여야 합니다." }
+
+        return splitInput
     }
 
     private fun String.toLongOrThrow(): Long {
