1665f61bbbf26ee5d060dfa5b563da60f63ca471
relkimm <bbonggu92@gmail.com>
2022-11-13 20:20:56 +0900

    refactor: Oil data class 에서 class 로 변경

--- src/main/kotlin/step3/Oil.kt
+++ src/main/kotlin/step3/Oil.kt
@@ -1,6 +1,6 @@
 package step3
 
-data class Oil(val amount: Int) {
+class Oil(val amount: Int) {
     operator fun compareTo(oil: Oil): Int {
         return this.amount.compareTo(oil.amount)
     }
