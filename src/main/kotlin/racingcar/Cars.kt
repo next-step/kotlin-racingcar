84449a14e3055c4714063a94e039af5d74203c17
youngbin-yun <youngbin@koreaspacedata.com>
2022-11-14 00:54:25 +0900

    feat(Cars): 우승자를 반환하는 메서드 추가
    - Cars.kt 에서 우승자를 반환하는 메서드 추가
    - X대의 자동차 중 점수가 가장 높은 대수 만큼 우승자가 나와야 한다. 테스트 추가
    - 모든 자동차가 전진에 실패하였다면 우승자는 없다. 테스트 추가

--- src/main/kotlin/racingcar/Cars.kt
+++ src/main/kotlin/racingcar/Cars.kt
@@ -10,9 +10,20 @@ class Cars(
     fun game(count: Int) {
         repeat(count) {
             this.race()
-            ResultView().printCarDistance(this)
+            ResultView.printCarDistance(this)
+        }
+    }
+
+    fun getWinners(): List<Car> {
+        val max: Int = getMaxDistance()
+
+        return if (max == 0) {
+            emptyList()
+        } else {
+            cars.filter { it.getDistance() == max }
         }
     }
 
     private fun race() = cars.map { it.moveOrNot() }
+    private fun getMaxDistance() = cars.maxOf { it.getDistance() }
 }
--- src/test/kotlin/racingcar/CarsTest.kt
+++ src/test/kotlin/racingcar/CarsTest.kt
@@ -19,4 +19,34 @@ class CarsTest : BehaviorSpec({
             }
         }
     }
+
+    given("3대의 자동차가 (2대는 무조건 전진전략, 1대는 무조건 움직이지않는 전략을 가짐)") {
+        val car1 = Car(name = "a", strategy = { true })
+        val car2 = Car(name = "b", strategy = { true })
+        val car3 = Car(name = "c", strategy = { false })
+        val cars = Cars(listOf(car1, car2, car3))
+
+        `when`("3번의 경주를 해서 2대가 가장 높은 점수(distance)를 가졌다면") {
+            cars.game(3)
+
+            then("2명의 우승자가 나온다.") {
+                cars.getWinners().size shouldBe 2
+            }
+        }
+    }
+
+    given("3대의 자동차가 (모든 자동차가 움직이지 않는 전략을 가짐)") {
+        val car1 = Car(name = "a", strategy = { false })
+        val car2 = Car(name = "b", strategy = { false })
+        val car3 = Car(name = "c", strategy = { false })
+        val cars = Cars(listOf(car1, car2, car3))
+
+        `when`("3번의 경주를 했을 때") {
+            cars.game(3)
+
+            then("우승자는 0명이어야 한다.") {
+                cars.getWinners().size shouldBe 0
+            }
+        }
+    }
 })
