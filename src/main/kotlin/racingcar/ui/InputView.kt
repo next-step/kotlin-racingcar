fafef46c429c024aee9f3cc9b62a57efe997caec
dajeong <galnotepink@gmail.com>
2022-11-13 21:28:48 +0900

    [Step 4] 자동차 이름을 문자열 타입으로 입력 받는 기능

--- src/main/kotlin/racingcar/ui/InputView.kt
+++ src/main/kotlin/racingcar/ui/InputView.kt
@@ -5,24 +5,23 @@ import racingcar.racingcar.CarRacingRequest
 class InputView {
     fun getRequest(): CarRacingRequest {
         println(INPUT_PARTICIPANTS_GUIDE)
-        val totalParticipants = readln().toInt()
-        validateInput(totalParticipants)
+        val carNameString = readln()
 
         println(INPUT_TRY_COUNT_GUIDE)
         val turnCount = readln().toInt()
-        validateInput(turnCount)
+        validateTurnCount(turnCount)
 
-        return CarRacingRequest(totalParticipants, turnCount)
-    }
-
-    private fun validateInput(input: Int) {
-        if (input < 0) {
-            throw IllegalArgumentException("음수를 입력하였습니다. 0보다 큰 숫자를 입력해주세요.")
-        }
+        return CarRacingRequest(carNames, turnCount)
     }
 
     private companion object {
-        const val INPUT_PARTICIPANTS_GUIDE = "자동차 대수는 몇 대 인가요?"
+        const val INPUT_PARTICIPANTS_GUIDE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
         const val INPUT_TRY_COUNT_GUIDE = "시도할 횟수는 몇 회인가요?"
+
+        private fun validateTurnCount(input: Int) {
+            if (input < 0) {
+                throw IllegalArgumentException("시도 회수에 음수를 입력하였습니다. 0보다 큰 숫자를 입력해주세요.")
+            }
+        }
     }
 }
