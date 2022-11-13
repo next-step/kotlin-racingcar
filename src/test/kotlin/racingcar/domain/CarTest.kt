ed9a14b9a24b52cf773d5f839fe36fe29f1507f2
Choiyounho <maxosa78@gmail.com>
2022-11-13 20:16:42 +0900

    test : 자동차 테스트 코드 작성

---
+++ src/test/kotlin/racingcar/domain/CarTest.kt
@@ -0,0 +1,31 @@
+package racingcar.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class CarTest {
+
+    @Test
+    fun `자동차의 전진을 하여 위치가 변동되는 지 확인`() {
+        // given
+        var car = Car()
+
+        // when
+        val actual = car.moveCar().position
+
+        // then
+        assertThat(actual).isEqualTo(Position(2))
+    }
+
+    @Test
+    fun `자동차가 여러번 전진을 했을 때 위치가 변동되는 지 확인`() {
+        // given
+        var car = Car()
+
+        // when
+        val actual = car.moveCar().moveCar().moveCar().moveCar().position
+
+        // then
+        assertThat(actual).isEqualTo(Position(5))
+    }
+}
