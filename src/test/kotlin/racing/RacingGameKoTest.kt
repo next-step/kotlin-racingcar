8bb73911b591747b708ca13d5b4066f40d3bb5fa
stkment <stkment@gmail.com>
2022-11-14 02:09:35 +0900

    :white_check_mark: 테스트케이스 수정

--- src/test/kotlin/racing/RacingGameKoTest.kt
+++ src/test/kotlin/racing/RacingGameKoTest.kt
@@ -9,6 +9,7 @@ import racing.domain.RacingCarsFactory
 import racing.domain.RacingCarsFactoryImpl
 import racing.domain.RandomMoveStrategy
 import racing.model.CarCount
+import racing.model.Driver
 import racing.model.RoundCount
 
 internal class RacingGameKoTest : BehaviorSpec() {
@@ -19,7 +20,6 @@ internal class RacingGameKoTest : BehaviorSpec() {
     private lateinit var racingCar: RacingCar
 
     private var roundCount: RoundCount? = null
-    private var carCount: CarCount? = null
 
     init {
         beforeEach {
@@ -34,16 +34,16 @@ internal class RacingGameKoTest : BehaviorSpec() {
 
         afterEach {
             roundCount = null
-            carCount = null
         }
 
-        given("유저가 차량 3대, 시도횟수 5회 입력을 하였고") {
+        given("드라이버 세 명과, 시도횟수 5회 입력을 하였고") {
+            val round = 5
+            val drivers = listOf("똘기", "떵이", "호치", "새초미")
             beforeEach {
-                carCount = CarCount(3)
-                roundCount = RoundCount(5)
+                roundCount = RoundCount(round)
                 racingGame = RacingGame(
                     racingCarGarage = RacingCarGarage(
-                        carCount = requireNotNull(carCount),
+                        drivers = drivers.map { Driver(it) },
                         racingCarsFactory = racingCarsFactory
                     ),
                     racingCar = racingCar
@@ -57,8 +57,8 @@ internal class RacingGameKoTest : BehaviorSpec() {
                     }
                 }
                 then("차량 3대의 5 라운드 경주 결과를 확인 할 수 있다.") {
-                    racingRecordBoard.results.size shouldBe 5
-                    racingRecordBoard.results.first().size shouldBe 3
+                    racingRecordBoard.results.size shouldBe round
+                    racingRecordBoard.results.first().result.size shouldBe drivers.size
                 }
             }
         }
