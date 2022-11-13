f2a095f02de59253cafda8dbd16a59c9d06e722c
Sangmeebee <apfhdznzl@gmail.com>
2022-11-14 04:34:46 +0900

    feat: Output UI에 차의 소유자가 있으면 소유자를 출력하는 로직 추가 & 승리자를 출력하는 로직 추가

--- src/main/kotlin/racingcar/ui/ResultView.kt
+++ src/main/kotlin/racingcar/ui/ResultView.kt
@@ -1,15 +1,26 @@
 package racingcar.ui
 
 import racingcar.model.Car
+import racingcar.model.Owner
 
 object ResultView {
 
-    private const val RESULT_INTRO_RESOURCE = "실행결과"
+    private const val INTRO_RESOURCE = "실행결과"
+    private const val WINNER_RESOURCE = "가 최종 우승했습니다."
 
-    fun printIntro() = println(RESULT_INTRO_RESOURCE)
+    fun printIntro() = println(INTRO_RESOURCE)
 
     fun printResult(cars: List<Car>) {
-        cars.forEach { car -> println(car.track) }
+        cars.forEach { car ->
+            if (car is Owner) {
+                print("${car.name} : ")
+            }
+            println(car.track)
+        }
         println()
+    }
+
+    fun printWinner(winner: List<String>) {
+        println(winner.joinToString() + WINNER_RESOURCE)
     }
 }
