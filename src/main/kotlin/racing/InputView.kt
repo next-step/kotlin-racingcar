96fba93b72328506e61ccb9b36500706d289dae4
stkment <stkment@gmail.com>
2022-11-14 00:08:17 +0900

    :necktie: 차량 드라이버 입력 받는것으로 수정

--- src/main/kotlin/racing/InputView.kt
+++ src/main/kotlin/racing/InputView.kt
@@ -2,9 +2,9 @@ package racing
 
 object InputView {
 
-    fun readCarCount(): Int? {
-        println("자동차 대수는 몇 대인가요?")
-        return readLine()?.toInt()
+    fun readCarDrivers(): List<String> {
+        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+        return readLine()?.split(",") ?: emptyList()
     }
 
     fun readRoundCount(): Int? {
