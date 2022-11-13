e55b1694cf95f6f8e29637f22faafe4d770845ca
dajeong <galnotepink@gmail.com>
2022-11-13 21:30:41 +0900

    [Step 4] 입력받은 문자열을 쉼표(,)로 분리

--- src/main/kotlin/racingcar/ui/InputView.kt
+++ src/main/kotlin/racingcar/ui/InputView.kt
@@ -7,6 +7,8 @@ class InputView {
         println(INPUT_PARTICIPANTS_GUIDE)
         val carNameString = readln()
 
+        val carNames = splitCarNames(carNameString)
+
         println(INPUT_TRY_COUNT_GUIDE)
         val turnCount = readln().toInt()
         validateTurnCount(turnCount)
@@ -17,6 +19,10 @@ class InputView {
     private companion object {
         const val INPUT_PARTICIPANTS_GUIDE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
         const val INPUT_TRY_COUNT_GUIDE = "시도할 횟수는 몇 회인가요?"
+
+        private fun splitCarNames(input: String): List<String> {
+            return input.split(",")
+        }
 
         private fun validateTurnCount(input: Int) {
             if (input < 0) {
