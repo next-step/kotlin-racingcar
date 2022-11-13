e725d582ba548b0dccabed91a05fc92cd581df66
stkment <stkment@gmail.com>
2022-11-14 02:09:56 +0900

    :necktie: 우승자 결과 화면에 출력

--- src/main/kotlin/racing/RacingApplication.kt
+++ src/main/kotlin/racing/RacingApplication.kt
@@ -4,22 +4,24 @@ import racing.domain.GoOrStopCarActionImpl
 import racing.domain.RacingCarImpl
 import racing.domain.RacingCarsFactoryImpl
 import racing.domain.RandomMoveStrategy
-import racing.model.CarCount
+import racing.model.Driver
 import racing.model.RoundCount
 
 class RacingApplication {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            val carCount = InputView.readCarCount()?.let { CarCount(it) }
-                ?: throw IllegalArgumentException("유효한 값이 아닙니다.")
-            val roundCount = InputView.readRoundCount()?.let { RoundCount(it) }
-                ?: throw IllegalArgumentException("유효한 값이 아닙니다.")
+            val drivers = InputView.readCarDrivers()
+                .let { it.map { driver -> Driver(driver) } }
+            require(drivers.isNotEmpty()) { "유효한 입력 값이 아닙니다." }
+            val roundCount = InputView.readRoundCount()
+                ?.let { RoundCount(it) }
+            requireNotNull(roundCount) { "유효한 입력 값이 아닙니다." }
 
             val racingRecordBoard = RacingRecordBoard()
             val racingGame = RacingGame(
                 racingCarGarage = RacingCarGarage(
-                    carCount = carCount,
+                    drivers = drivers,
                     racingCarsFactory = RacingCarsFactoryImpl()
                 ),
                 racingCar = RacingCarImpl(
@@ -32,7 +34,7 @@ class RacingApplication {
                 val raceResult = racingGame.race()
                 racingRecordBoard.add(raceResult)
             }
-            ResultView.printRaceResult(racingRecordBoard.results)
+            ResultView.printRaceResult(racingRecordBoard.results, racingRecordBoard.winners)
         }
     }
 }
--- src/main/kotlin/racing/ResultView.kt
+++ src/main/kotlin/racing/ResultView.kt
@@ -1,16 +1,18 @@
 package racing
 
+import racing.model.Driver
 import racing.model.RacingResult
 
 object ResultView {
     private const val CAR_MILEAGE_INDICATOR = "-"
 
-    fun printRaceResult(results: List<RacingResult>) {
+    fun printRaceResult(results: List<RacingResult>, winners: List<Driver>) {
         println("실행 결과")
         results.forEach { racingResult ->
             printRoundRaceResult(racingResult)
             println()
         }
+        printWinners(winners)
     }
 
     private fun printRoundRaceResult(racingResult: RacingResult) =
@@ -18,4 +20,8 @@ object ResultView {
             .forEach {
                 println(CAR_MILEAGE_INDICATOR.repeat(it.mileage))
             }
+
+    private fun printWinners(winners: List<Driver>) {
+        println("${winners.joinToString(", ") { it.name }}가 최종 우승했습니다.")
+    }
 }
