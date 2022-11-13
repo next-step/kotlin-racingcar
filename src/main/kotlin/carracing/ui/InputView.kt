1bf097012d77fba7f4371d28325a3fcb32c61d83
dldudwns72 <dldudwns72@gmail.com>
2022-11-14 02:25:15 +0900

    refactor : UI 관련 사항 리팩토링

--- src/main/kotlin/carracing/ui/InputView.kt
+++ src/main/kotlin/carracing/ui/InputView.kt
@@ -1,15 +1,20 @@
 package carracing.ui
 
-// object 는 싱글톤으로 동작된다.
+import java.lang.IllegalArgumentException
+
+const val INPUT_EXCEPTION_MESSAGE = "0 이상의 자연수를 입력해주세요"
+const val INPUT_CAR_MESSAGE = "자동차 대수는 몇 대인가요?"
+const val INPUT_ATTEMPT_MESSAGE = "시도할 횟수는 몇 회인가요?"
+
 object InputView {
 
     fun getCarCount(): Int {
-        println("자동차 대수는 몇 대인가요?")
-        return readLine()!!.toInt()
+        println(INPUT_CAR_MESSAGE)
+        return readLine()?.toIntOrNull() ?: throw IllegalArgumentException(INPUT_EXCEPTION_MESSAGE)
     }
 
     fun getAttemptCount(): Int {
-        println("시도할 횟수는 몇 회인가요?")
-        return readLine()!!.toInt()
+        println(INPUT_ATTEMPT_MESSAGE)
+        return readLine()?.toIntOrNull() ?: throw IllegalArgumentException(INPUT_EXCEPTION_MESSAGE)
     }
 }
--- src/main/kotlin/carracing/ui/OutputView.kt
+++ src/main/kotlin/carracing/ui/OutputView.kt
@@ -1,12 +1,19 @@
 package carracing.ui
 
-import carracing.domain.Cars
+import carracing.domain.CarsFactory
 import carracing.domain.Racing
 
+const val CAR_SYMBOL = "-"
+
 object OutputView {
+
+    fun gameRoundPrint(position: Int) {
+        println(CAR_SYMBOL.repeat(position))
+    }
+
     fun racingResult(carCount: Int, attemptCount: Int) {
         println("실행 결과")
-        Racing(Cars.of(carCount), attemptCount).race()
+        Racing(CarsFactory.of(carCount), attemptCount).race()
     }
 
     fun lineChange() {
