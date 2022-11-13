cfa0509469c28eb66ad7f3381525437071bd8809
misu <jungmisu801@gmail.com>
2022-11-13 21:57:31 +0900

    fix: 요구사항 변경에 따른, InpputView, OutputView 수정

--- src/main/kotlin/racing/view/OutputView.kt
+++ src/main/kotlin/racing/view/OutputView.kt
@@ -1,12 +1,18 @@
 package racing.view
 
+import racing.Name
+
 class OutputView {
     fun printResultString() {
         println("실행 결과")
     }
 
-    fun printResult(results: List<Int>) {
-        results.map { i -> "-".repeat(i) }.forEach(::println)
+    fun printResult(results: List<Pair<Name, Int>>) {
+        results.map { it.first.name + " : " + "-".repeat(it.second) }.forEach(::println)
         println()
+    }
+
+    fun printWinners(winners: List<Name>) {
+        println(winners.map { it.name }.reduce { n1, n2 -> "$n1,$n2" } + "가 최종 우승했습니다.")
     }
 }
