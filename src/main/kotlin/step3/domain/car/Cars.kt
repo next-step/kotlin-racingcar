19f62e5c3341b6d0f40441a1c129087530f0533d
namonak <namonak@gmail.com>
2022-11-13 10:50:20 +0900

    refactor: remove the duplicate declaration of the List<Car> collection property

--- src/main/kotlin/step3/domain/car/Cars.kt
+++ src/main/kotlin/step3/domain/car/Cars.kt
@@ -3,9 +3,7 @@ package step3.domain.car
 import step3.domain.car.action.MovingStrategyImpl
 import step3.domain.car.status.Position
 
-data class Cars(val car: List<Car>) {
-    private val cars = car
-
+data class Cars(private val cars: List<Car>) {
     init {
         require(cars.isNotEmpty()) { "자동차는 1대 이상이어야 합니다." }
     }
