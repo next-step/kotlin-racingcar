f0bfe6cdc6660172db9e070fcf153665b4b27c8f
stkment <stkment@gmail.com>
2022-11-13 22:56:28 +0900

    :necktie: RacingGame refactor

--- src/main/kotlin/racing/RacingGame.kt
+++ src/main/kotlin/racing/RacingGame.kt
@@ -1,29 +1,14 @@
 package racing
 
-import racing.domain.GoOrStopCarAction
+import racing.domain.RacingCar
 import racing.model.Car
-import racing.model.RoundCount
 
 class RacingGame(
-    private val roundCount: RoundCount,
     private val racingCarGarage: RacingCarGarage,
-    private val goOrStopCarAction: GoOrStopCarAction,
+    private val racingCar: RacingCar,
 ) {
-    private val _results: MutableList<List<Car>> = mutableListOf()
-    val results: List<List<Car>>
-        get() = _results
-
-    fun race() {
-        val roundCount = this.roundCount.value
-        repeat(roundCount) {
-            val cars = racingCarGarage.cars
-
-            val raceCarResult = cars.map {
-                val action = goOrStopCarAction.castCarAction()
-                it.copy(mileage = it.mileage + action.value)
-            }
-            _results.add(raceCarResult)
-            racingCarGarage.parkCars(raceCarResult)
-        }
-    }
+    fun race(): List<Car> = racingCarGarage.cars
+        .map(racingCar::driveCar)
+        .let { it }
+        .also(racingCarGarage::parkCars)
 }
