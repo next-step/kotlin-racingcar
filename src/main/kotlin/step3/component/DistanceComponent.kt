6ea5ef8e29e43518faafe99762dad996058ce650
relkimm <bbonggu92@gmail.com>
2022-11-13 20:25:10 +0900

    refactor: 주행 거리를 그리는 로직 repeat 함수로 변경

--- src/main/kotlin/step3/component/DistanceComponent.kt
+++ src/main/kotlin/step3/component/DistanceComponent.kt
@@ -4,8 +4,6 @@ class DistanceComponent(
     val position: Int,
 ) : Component {
     override fun render() {
-        (1..position).forEach { _ ->
-            print("-")
-        }
+        repeat(position) { print("-") }
     }
 }
