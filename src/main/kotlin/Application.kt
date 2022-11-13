91f99d9d236ee036a54a901702299cc7bab3fdb0
James <bookwriterseo@gmail.com>
2022-11-13 16:43:10 +0900

    feat: 입력 값 wrapper 추가

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,3 +1,5 @@
+import racingcar.domain.InputNumber
+import racingcar.view.InputView
 
 fun main() {
     /*
@@ -5,4 +7,8 @@ fun main() {
     val input = CalculationExpression(InputView().getUserInput())
     println(Calculator().calculate(input))
      */
+
+    // code for racing car
+    val inputNumberOfCar = InputNumber(InputView().getNumberOfCar()).number
+    val inputNumberOfTrying = InputNumber(InputView().getNumberOfTrying()).number
 }
