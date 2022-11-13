af799b73f0b9a55fb0dc03a6e79c62bfb33c596f
이상인/wefu0000 <wefu.child@gmail.com>
2022-11-14 01:07:18 +0900

    feat: 자동차 경주 로직 구현

---
+++ src/main/kotlin/racingcar/RacingCarMove.kt
@@ -0,0 +1,26 @@
+package racingcar
+
+import java.security.SecureRandom
+
+object RacingCarMove {
+
+    fun move(moveCountList: ArrayList<Int>) {
+        moveCountList.forEachIndexed { index, _ ->
+            if (checkMoveValue(createRandomInt())){
+                moveCountList[index] ++
+            }
+        }
+    }
+
+    fun createRandomInt(): Int = SecureRandom.getInstanceStrong().nextInt(10)
+
+    fun checkMoveValue(value: Int): Boolean = value >= 4
+
+    fun getMoveString(moveCount: Int): String {
+        return StringBuilder().apply {
+            for (i in 0 until moveCount) {
+                append("-")
+            }
+        }.toString()
+    }
+}
