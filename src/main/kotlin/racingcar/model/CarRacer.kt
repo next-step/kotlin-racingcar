b1093df8285212104228512751ec88ab12d27e9d
dreamboy1339 <dreamboy1339@gmail.com>
2022-11-13 12:23:46 +0900

    study(step3): 경주자 로직 구현

--- src/main/kotlin/racingcar/model/CarRacer.kt
+++ src/main/kotlin/racingcar/model/CarRacer.kt
@@ -1,7 +1,23 @@
 package racingcar.model
 
+import kotlin.random.Random
+
 /**
  * 자동차 경주자
  */
 class CarRacer {
+
+    var path: String = ""
+        private set
+
+    fun move(moveCount: Int) {
+        if (moveCount < 4) return
+        path += CHARACTER_MOVE
+    }
+
+    fun generateCount() = Random(System.currentTimeMillis()).nextInt(0, 10)
+
+    companion object {
+        const val CHARACTER_MOVE = "-"
+    }
 }
