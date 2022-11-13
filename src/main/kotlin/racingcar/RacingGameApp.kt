9599b66f68ee450dc894da6d93b415f100342076
Sangmeebee <apfhdznzl@gmail.com>
2022-11-14 04:36:55 +0900

    feat: 제작한 클래스를 이용하여 우승자를 뽑는 레이싱 게임 기능 제작 완료

--- src/main/kotlin/racingcar/RacingGameApp.kt
+++ src/main/kotlin/racingcar/RacingGameApp.kt
@@ -1,21 +1,12 @@
 package racingcar
 
 import racingcar.controller.RacingGameController
-import racingcar.domain.racing_game.DefaultRacingGame
-import racingcar.domain.random.DefaultRandomGenerator
-import racingcar.domain.racing_game.RacingGame
-import racingcar.model.Car
-import racingcar.model.DefaultCar
+import racingcar.domain.racing_game.RacingGameFactory
+import racingcar.model.GameMode
 import racingcar.ui.InputView
 
 fun main() {
-    val racingGameController = setRacingGame()
+    val racingGame = RacingGameFactory().create(GameMode.COMPETE)
+    val racingGameController = RacingGameController(racingGame)
     racingGameController.doRacingGame(executeNum = InputView.requestExecuteNum())
-}
-
-private fun setRacingGame(): RacingGameController {
-    val carNum = InputView.requestCarNum()
-    val cars = List<Car>(carNum) { DefaultCar() }
-    val racingGame: RacingGame = DefaultRacingGame(DefaultRandomGenerator(), cars)
-    return RacingGameController(racingGame)
 }
