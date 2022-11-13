514ad22b068317b623bcbb5b11df59c7bb7307ea
riyenas0925 <riyenas0925@gmail.com>
2022-11-13 22:41:52 +0900

    test: 레이싱 게임 관련 mock 테스트 추가

---
+++ src/test/kotlin/racingcar/RacingGameTest.kt
@@ -0,0 +1,22 @@
+package racingcar
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+
+class RacingGameTest: StringSpec({
+    "레이싱 게임은 매 횟수마다 자동차의 현재 위치를 보여줘요" {
+        val mockNumberGenerator = mockk<NumberGenerator>()
+        every { mockNumberGenerator.rand() } returns 0
+
+        val racingGame = RacingGame(
+            cars = listOf(Car(0)),
+            numberGenerator = mockNumberGenerator
+        )
+
+        val car = racingGame.round()[0]
+
+        car.currentLocation() shouldBe 0
+    }
+})
