2247d75b93ba5989913393efc2007e5e6c25b157
Yujin Park <uiuiui118437@gmail.com>
2022-11-13 14:10:21 +0900

    feat: result 로직을 담당할 ResultView 생성

---
+++ src/main/kotlin/racingcar/ui/ResultView.kt
@@ -0,0 +1,22 @@
+package racingcar.ui
+
+import racingcar.domain.Car
+
+object ResultView {
+    private const val MARK = "-"
+
+    fun printResultMessage() {
+        println("실행 결과")
+    }
+
+    fun printTraceOfRound(round: Int, cars: List<Car>) {
+        println("[ROUND ${round + 1}]")
+        cars.forEach { car ->
+            repeat(car.distance) {
+                print(MARK)
+            }
+            println()
+        }
+        println()
+    }
+}
