036c8c8316fdd09619e9c8e4a41cd73a2e06035a
Choiyounho <maxosa78@gmail.com>
2022-11-15 00:30:52 +0900

    refactor : 전진 조건과 상수 수정

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -6,12 +6,23 @@ class Car(
 ) {
 
     fun move(): Car {
-        return Car(Position(position.value + POSITION_MOVE))
+        return if (isMove())
+            Car(Position(position.value + POSITION_MOVE))
+        else this
+    }
+
+    private fun isMove(): Boolean {
+        return (TERMS_OF_RACING_MIN..TERMS_OF_RACING_MAX).random() > TERMS_OF_RACING
     }
 
     companion object {
         private const val POSITION_FIRST = 1
         private const val POSITION_MOVE = 1
+
+        private const val TERMS_OF_RACING_MIN = 0
+        private const val TERMS_OF_RACING_MAX = 10
+
+        private const val TERMS_OF_RACING = 4
     }
 
 }
