a359176bcb8c17b16321152f27fff3e1869f4a87
lalize <printf@kakao.com>
2022-11-14 23:42:07 +0900

    refactor: 자동차 경주 결과 방어적 복사

--- src/main/kotlin/racingcar/RaceResult.kt
+++ src/main/kotlin/racingcar/RaceResult.kt
@@ -1,6 +1,8 @@
 package racingcar
 
-class RaceResult(private val records: List<Record>) {
+class RaceResult(records: List<Record>) {
+    private val records: List<Record> = ArrayList(records)
+
     fun toMap(): Map<Round, List<Record>> {
         return records.groupBy { it.round }
     }
