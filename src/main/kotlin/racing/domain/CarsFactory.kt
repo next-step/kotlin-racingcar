b719d45a57316fcc91bc9c9a81abbfc7aed0ef43
chayoung-choi <chayoung.dev@gmail.com>
2022-11-14 11:08:34 +0900

    refactor: CarsFactory 변경

--- src/main/kotlin/racing/domain/CarsFactory.kt
+++ src/main/kotlin/racing/domain/CarsFactory.kt
@@ -3,10 +3,7 @@ package racing.domain
 import racing.domain.strategy.MovingStrategy
 import racing.domain.strategy.RandomMovingStrategy
 
-object Cars {
-    const val NAME_SEPARATOR = ","
-
-    fun splitNameOfCars(nameOfCars: String) = nameOfCars.split(NAME_SEPARATOR)
+object CarsFactory {
 
     fun createCars(nameList: List<String>, movingStrategy: MovingStrategy = RandomMovingStrategy): List<Car> {
         return nameList.map { Car(it, movingStrategy) }
--- src/test/kotlin/racing/domain/GameTest.kt
+++ src/test/kotlin/racing/domain/GameTest.kt
@@ -8,16 +8,16 @@ internal class GameTest {
 
     @Test
     fun `레이싱 게임 정상 동작`() {
-        val nameOfCars = "pobi,crong,honux"
-        val cars = Cars.createCars(Cars.splitNameOfCars(nameOfCars))
+        val nameList = listOf("pobi", "crong", "honux")
+        val cars = CarsFactory.createCars(nameList)
         val game = Game(cars, 5)
         game.play()
     }
 
     @Test
     fun `위치가 동일하면 전부 우승 자동차이다`() {
-        val nameList = Cars.splitNameOfCars("pobi,crong,honux")
-        val cars = Cars.createCars(nameList)
+        val nameList = listOf("pobi", "crong", "honux")
+        val cars = CarsFactory.createCars(nameList)
         val game = Game(cars, 0)
         game.play()
         assertThat(game.getWinner(cars).size).isEqualTo(nameList.size)
@@ -25,8 +25,8 @@ internal class GameTest {
 
     @RepeatedTest(5)
     fun `우승 자동차 수는 최소 1 이상이다`() {
-        val nameList = Cars.splitNameOfCars("pobi,crong,honux")
-        val cars = Cars.createCars(nameList)
+        val nameList = listOf("pobi", "crong", "honux")
+        val cars = CarsFactory.createCars(nameList)
         val game = Game(cars, 5)
         game.play()
         assertThat(game.getWinner(cars).size).isGreaterThanOrEqualTo(1)
