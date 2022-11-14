1a7c421081edeea991535aa24e4b950594593655
namonak <namonak@gmail.com>
2022-11-15 00:26:22 +0900

    feat(step4): add a Racers class
    The following have been implement and test:
     - throw an IllegalArgumentException if it is an empty string
     - throw an IllegalArgumentException if the string doesn't contain a comma

---
+++ src/main/kotlin/racingcar/input/value/Racers.kt
@@ -0,0 +1,10 @@
+package racingcar.input.value
+
+data class Racers(private val racers: String) {
+    val value = racers.split(",")
+
+    init {
+        require(racers.isNotEmpty()) { "자동차 이름은 1글자 이상이어야 합니다." }
+        require(racers.contains(",")) { "자동차 이름은 쉼표(,)로 구분해 주세요." }
+    }
+}
