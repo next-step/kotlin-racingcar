3ddd056f6cab0479b9c75ef035f9bf3301e83de1
choi-ys <project.log.062@gmail.com>
2022-11-14 11:23:54 +0900

    refactor: step2 2차 코드리뷰 피드백 적용
    - 확장 함수로 표현한 관계 연산자(<,<=,>,>=)를 직접 명시하여 가독성 개선
    - 예외가 발생한 요청값 확인이 용이하도록 예외 메세지에 예외 발생 파라미터 명시
    - kotlin elvis operator를 이용하여 객체값이 null인 경우 기본 동작 처리 정의

--- src/main/kotlin/step2/calculator/Expansion.kt
+++ src/main/kotlin/step2/calculator/Expansion.kt
@@ -1,6 +1,3 @@
 package step2.calculator
 
-fun Double.isNotZero(): Boolean = this != 0.0
 fun Double.hasNoRemainder(): Boolean = this % 1 == 0.0
-
-fun Int.overThan(target: Int): Boolean = this >= target
--- src/main/kotlin/step2/calculator/ExpressionParser.kt
+++ src/main/kotlin/step2/calculator/ExpressionParser.kt
@@ -2,7 +2,7 @@ package step2.calculator
 
 object ExpressionParser {
     private const val MINIMUM_EXPRESSION_LENGTH = 3
-    private const val LESS_THAN_MINIMUM_EXPRESSION_LENGTH_ERROR_MESSAGE = "최소 두개의 항과 1개의 연산자를 입력하세요"
+    private const val LESS_THAN_MINIMUM_EXPRESSION_LENGTH_ERROR_MESSAGE = "올바른 수식이 아닙니다. [%s], 최소 두개의 항과 1개의 연산자를 입력하세요"
     private const val DELIMITER = " "
 
     fun parse(expression: String): List<String> {
@@ -12,6 +12,6 @@ object ExpressionParser {
 
     private fun validateExpression(expression: String) {
         val expressionLength: Int = expression.trim().length
-        require(expressionLength.overThan(MINIMUM_EXPRESSION_LENGTH)) { LESS_THAN_MINIMUM_EXPRESSION_LENGTH_ERROR_MESSAGE }
+        require(expressionLength >= MINIMUM_EXPRESSION_LENGTH) { LESS_THAN_MINIMUM_EXPRESSION_LENGTH_ERROR_MESSAGE.format(expression) }
     }
 }
