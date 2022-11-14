38aa8eaae64687718f67f36e67c85faed96986bb
namonak <namonak@gmail.com>
2022-11-14 12:06:27 +0900

    refactor(step3): change the InputResultDto class to a data class

--- src/main/kotlin/racingcar/input/InputResultDto.kt
+++ src/main/kotlin/racingcar/input/InputResultDto.kt
@@ -3,7 +3,7 @@ package racingcar.input
 import racingcar.input.value.NumberOfCars
 import racingcar.input.value.PlayTime
 
-class InputResultDto(private val numberOfCars: NumberOfCars, private val playTime: PlayTime) {
+data class InputResultDto(private val numberOfCars: NumberOfCars, private val playTime: PlayTime) {
     fun getNumberOfCars(): Int {
         return numberOfCars.getNumberOfCars()
     }
