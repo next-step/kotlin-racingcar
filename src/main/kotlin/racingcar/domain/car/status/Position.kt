823f93f3ec93e027801381839135208cb40bab1f
namonak <namonak@gmail.com>
2022-11-14 12:51:31 +0900

    refactor(step3): define the DEFAULT_POSITION constant value in the Position class

--- src/main/kotlin/racingcar/domain/car/status/Position.kt
+++ src/main/kotlin/racingcar/domain/car/status/Position.kt
@@ -1,8 +1,10 @@
 package racingcar.domain.car.status
 
+private const val DEFAULT_POSITION = 0
+
 data class Position(private var position: Int) {
     init {
-        require(position >= 0) { "자동차의 위치는 0 이상이어야 합니다." }
+        require(position >= DEFAULT_POSITION) { "자동차의 위치는 0 이상이어야 합니다." }
     }
 
     fun move() {
