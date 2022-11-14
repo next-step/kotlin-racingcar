3ac9d048686533ac40657895234f65ce0f25a250
lalize <printf@kakao.com>
2022-11-14 09:15:54 +0900

    feat: 자동차를 출력할 때 이름도 같이 출력하도록 수정

--- src/main/kotlin/racingcar/OutputView.kt
+++ src/main/kotlin/racingcar/OutputView.kt
@@ -4,7 +4,7 @@ object OutputView {
     fun writeResult(result: RaceResult) {
         println("실행 결과")
         result.forEach {
-            it.value.forEach { record -> println("-".repeat(record.position.value)) }
+            it.value.forEach { record -> println("${record.name} : ${"-".repeat(record.position.value)}") }
             println()
         }
     }
