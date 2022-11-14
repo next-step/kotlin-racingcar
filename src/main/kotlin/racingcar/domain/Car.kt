7f74fde56692ab4922fd663c82ecd09e1c2750ff
shinaaaa <shin029abc@gmail.com>
2022-11-14 09:06:28 +0900

    refactor: 변수명 첫글자를 소문자로 변경

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -19,7 +19,7 @@ class Car(val name: String) : Moveable {
     }
 
     companion object {
-        fun registerCars(NamesOfCars: List<String>) = Array(NamesOfCars.size) { Car(NamesOfCars[it]) }
+        fun registerCars(namesOfCars: List<String>) = Array(namesOfCars.size) { Car(namesOfCars[it]) }
 
         fun getFurthestCars(cars: Array<Car>): List<Car> {
             val max = cars.maxOf { it.currentPosition }
