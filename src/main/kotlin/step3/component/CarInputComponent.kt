7514a71100bdda1ce70c05ec3d330c0b86f93894
relkimm <bbonggu92@gmail.com>
2022-11-14 21:22:41 +0900

    feat: 자동차 이름을 입력받을 수 있도록 적용

--- src/main/kotlin/step3/component/CarInputComponent.kt
+++ src/main/kotlin/step3/component/CarInputComponent.kt
@@ -6,7 +6,7 @@ import step3.ui.Input
 import step3.ui.Span
 
 class CarInputComponent : Component {
-    private val span = Span(text = "자동차 대수는 몇 대인가요?", block = true)
+    private val span = Span(text = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).", block = true)
     private val input = Input()
 
     init { this.input.addCommandListener(this::onCommand) }
