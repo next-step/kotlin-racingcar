a74396dbd7cbbe4ccd3d62ee049e0146eb312f38
wonjun3991 <wonjun.choi@meshkorea.net>
2022-11-14 15:56:38 +0900

    feature(input): get nameList by cars

--- src/main/kotlin/study/racingcar/io/InputView.kt
+++ src/main/kotlin/study/racingcar/io/InputView.kt
@@ -1,6 +1,11 @@
 package study.racingcar.io
 
 object InputView {
+    fun getCarsName(): List<String> {
+        println("주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+        return readln().split(",")
+    }
+
     fun getCountOfCars(): Int {
         println("자동차 대수는 몇 대인가요?")
         return readln().toInt()
