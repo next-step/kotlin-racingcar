3754b39fdfec281d91f00e4f631cb08ef7ccd674
lalize <printf@kakao.com>
2022-11-14 09:52:41 +0900

    refactor: 불필요한 필드 제거

--- src/main/kotlin/racingcar/CarRacing.kt
+++ src/main/kotlin/racingcar/CarRacing.kt
@@ -4,8 +4,7 @@ class CarRacing(
     private val count: Int = 1,
     private val carRacers: List<CarRacer>
 ) {
-    private val records: MutableList<Record> = mutableListOf()
-    private var result: RaceResult? = null
+    private var cache: RaceResult? = null
 
     init {
         require(count > 0) { "시도할 횟수는 1이상 이어야 합니다." }
@@ -13,21 +12,27 @@ class CarRacing(
     }
 
     fun race(): RaceResult {
-        if (result != null) {
-            return result as RaceResult
+        if (cache != null) {
+            return cache as RaceResult
         }
 
-        repeat(count) { race(it) }
+        val records = mutableListOf<Record>()
 
-        result = RaceResult(records)
+        repeat(count) { records.addAll(race(it)) }
 
-        return result as RaceResult
+        cache = RaceResult(records)
+
+        return cache as RaceResult
     }
 
-    private fun race(round: Round) {
+    private fun race(round: Round): List<Record> {
+        val records = mutableListOf<Record>()
+
         carRacers.forEach {
             it.drive()
             records.add(Record(round, it))
         }
+
+        return records
     }
 }
