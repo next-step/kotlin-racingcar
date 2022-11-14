15850d636e01f7650715c805a712cc678c0ce7cd
ajy9844 <ajy09844@gmail.com>
2022-11-15 02:39:57 +0900

    refactor(validator): make error if input is not arithmetic operation

--- src/main/kotlin/calculator/Validator.kt
+++ src/main/kotlin/calculator/Validator.kt
@@ -1,13 +1,18 @@
 package calculator
 
 class Validator {
-    fun validate(s: String?): Boolean {
+    fun validate(s: String?) {
         isNullOrBlank(s)
-        return true
+        if (s != null) isArithmeticOperation(s)
     }
 
     private fun isNullOrBlank(s: String?) {
         if (s.isNullOrBlank()) throw IllegalArgumentException()
     }
+
+    private fun isArithmeticOperation(s: String) {
+        val s1 = s.replace(" ", "")
+        val regex = Regex("[^+\\-*/0-9]")
+        if (regex.containsMatchIn(s1)) throw IllegalArgumentException()
     }
 }
--- src/test/kotlin/calculator/ValidatorTest.kt
+++ src/test/kotlin/calculator/ValidatorTest.kt
@@ -20,4 +20,17 @@ class ValidatorTest {
         // then
         assertThat(thrown).isInstanceOf(IllegalArgumentException::class.java)
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["2 & 3", "2 ^ 8 - 10"])
+    fun `입력값이 사칙연산 기호가 아닌 경우 오류가 발생한다`(input: String) {
+        // given
+        val validator = Validator()
+
+        // when
+        val thrown = catchThrowable { validator.validate(input) }
+
+        // then
+        assertThat(thrown).isInstanceOf(IllegalArgumentException::class.java)
+    }
 }
