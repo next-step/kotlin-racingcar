de575d6dfb72fe54f31e31e94d47b38691020514
ajy9844 <ajy09844@gmail.com>
2022-11-15 03:59:06 +0900

    refactor(calculator): add calculator

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -1,8 +1,37 @@
 package calculator
 
-interface Calculator {
-    fun plus(a: Int, b: Int): Int
-    fun minus(a: Int, b: Int): Int
-    fun times(a: Int, b: Int): Int
-    fun div(a: Int, b: Int): Int
+class Calculator(private val operator: Operator) {
+    fun calculate(s: String): Int {
+        val (opd, opt) = parse(s)
+
+        var result = opd[0]
+        for (i in 0..opd.size - 2) {
+            operator.find(opt[i])
+            result = operator.operate(result, opd[i + 1])
+        }
+
+        return result
+    }
+
+    private fun parse(s: String): Pair<List<Int>, List<Char>> {
+        val opd = mutableListOf<Int>()
+        val opt = mutableListOf<Char>()
+
+        val token = s.split(" ")
+        token.forEach { t ->
+            if (isInt(t)) opd.add(t.toInt())
+            else opt.add(t.first())
+        }
+
+        return opd to opt
+    }
+
+    private fun isInt(s: String): Boolean {
+        return try {
+            s.toInt()
+            true
+        } catch (e: NumberFormatException) {
+            false
+        }
+    }
 }
