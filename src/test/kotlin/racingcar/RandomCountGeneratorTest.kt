9ec444542c8130755d695cd6b008647b6f7494df
dreamboy1339 <dreamboy1339@gmail.com>
2022-11-15 21:26:05 +0900

    study(step3): 테스트 코드 수정

--- src/test/kotlin/racingcar/RandomCountGeneratorTest.kt
+++ src/test/kotlin/racingcar/RandomCountGeneratorTest.kt
@@ -6,11 +6,11 @@ import io.kotest.matchers.ints.shouldBeLessThanOrEqual
 
 internal class RandomCountGeneratorTest : StringSpec({
     "랜덤하게 9 보다 작거나 같은 카운트를 생성한다" {
-        val count = RandomCountGenerator.generateCount()
+        val count = RandomCountGenerator().generateCount()
         count shouldBeLessThanOrEqual 9
     }
     "랜덤하게 0 보다 크거나 같은 카운트를 생성한다" {
-        val count = RandomCountGenerator.generateCount()
+        val count = RandomCountGenerator().generateCount()
         count shouldBeGreaterThanOrEqual 0
     }
 })
