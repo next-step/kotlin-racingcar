11e3701c8871cee889672ddc38555fd59101516c
soosue <yunsoo0510@daum.net>
2022-11-15 14:04:40 +0900

    refactor: 불명확한 함수의 이름을 이해하기 쉽게 companion object 로 추출함
    - Math.max(a: Int, b: Int) 를 참고

--- src/main/kotlin/racing/domain/CarInfo.kt
+++ src/main/kotlin/racing/domain/CarInfo.kt
@@ -3,5 +3,7 @@ package racing.domain
 data class CarInfo(val name: Name, val position: Position) {
     constructor(name: String, position: Position) : this(Name(name), position)
 
-    fun max(other: CarInfo) = if (this.position.max(other.position) == this.position) this else other
+    companion object {
+        fun max(a: CarInfo, b: CarInfo) = if (a.position.max(b.position) == a.position) a else b
+    }
 }
--- src/main/kotlin/racing/domain/CarInfos.kt
+++ src/main/kotlin/racing/domain/CarInfos.kt
@@ -1,7 +1,7 @@
 package racing.domain
 
 data class CarInfos(val carInfoList: List<CarInfo>) {
-    fun findMaxPosition() =
-        carInfoList.reduce { carInfo1, carInfo2 -> carInfo1.max(carInfo2) }.position
-    
+    fun findMaxPosition(): Position =
+        carInfoList.reduce { carInfo1, carInfo2 -> CarInfo.max(carInfo1, carInfo2) }.position
+
 }
