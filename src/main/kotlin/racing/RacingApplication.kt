f13f66639e63c3e402aa082e2b5ba3f35e9050d8
stkment <stkment@gmail.com>
2022-11-15 21:36:10 +0900

    :necktie: main함수 리팩토링

--- src/main/kotlin/racing/RacingApplication.kt
+++ src/main/kotlin/racing/RacingApplication.kt
@@ -1,40 +1,31 @@
 package racing
 
 import racing.domain.GoOrStopCarActionImpl
-import racing.domain.RacingCarImpl
 import racing.domain.RacingCarsFactoryImpl
+import racing.domain.RacingImpl
 import racing.domain.RandomMoveStrategy
-import racing.model.Driver
-import racing.model.RoundCount
 
 class RacingApplication {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            val drivers = InputView.readCarDrivers()
-                .let { it.map { driver -> Driver(driver) } }
-            require(drivers.isNotEmpty()) { "유효한 입력 값이 아닙니다." }
-            val roundCount = InputView.readRoundCount()
-                ?.let { RoundCount(it) }
-            requireNotNull(roundCount) { "유효한 입력 값이 아닙니다." }
-
-            val racingRecordBoard = RacingRecordBoard()
             val racingGame = RacingGame(
+                inputView = InputView,
+                resultView = ResultView,
                 racingCarGarage = RacingCarGarage(
-                    drivers = drivers,
                     racingCarsFactory = RacingCarsFactoryImpl()
                 ),
-                racingCar = RacingCarImpl(
+                racing = RacingImpl(
                     goOrStopCarAction = GoOrStopCarActionImpl(
                         moveStrategy = RandomMoveStrategy()
                     )
-                )
+                ),
+                racingRecordBoard = RacingRecordBoard()
             )
-            repeat(roundCount.value) {
-                val raceResult = racingGame.race()
-                racingRecordBoard.add(raceResult)
-            }
-            ResultView.printRaceResult(racingRecordBoard.results, racingRecordBoard.winners)
+            val (drivers, roundCount) = racingGame.getRaceInfo()
+            racingGame.start(drivers, roundCount)
+            val (result, winners) = racingGame.getRacingResultAndWinners()
+            racingGame.printResult(result, winners)
         }
     }
 }
