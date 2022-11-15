d4e5b42526f2baa4278f949b81ec5f29885c64b1
youngbin-yun <youngbin@koreaspacedata.com>
2022-11-16 03:46:26 +0900

    feat(TryCount): TryCount init 블록 추가

--- src/main/kotlin/racingcar/domain/TryCount.kt
+++ src/main/kotlin/racingcar/domain/TryCount.kt
@@ -3,18 +3,21 @@ package racingcar.domain
 import racingcar.util.extension.isPositive
 
 @JvmInline
-value class TryCount private constructor(private val count: Int) {
+value class TryCount(private val count: Int) {
     fun getCount() = count
+
+    init {
+        require(count.isPositive()) { "입력 값은 1 이상이어야 합니다." }
+    }
 
     companion object {
         @JvmStatic
-        fun from(stringCount: String) = TryCount(validate(stringCount))
+        fun from(stringCount: String): TryCount {
+            return TryCount(numberValidate(stringCount))
+        }
 
-        @JvmStatic
-        private fun validate(stringCount: String): Int {
-            val count: Int = requireNotNull(stringCount.toIntOrNull()) { "입력 값은 정수 여야 합니다." }
-            require(count.isPositive()) { "입력 값은 1 이상이어야 합니다." }
-            return count
+        private fun numberValidate(stringCount: String): Int {
+            return requireNotNull(stringCount.toIntOrNull()) { "입력 값은 정수 여야 합니다." }
         }
     }
 }
