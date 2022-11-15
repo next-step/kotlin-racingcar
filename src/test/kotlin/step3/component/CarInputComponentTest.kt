76f6cea703ced9d9453fb28fb818465ebde4af3d
relkimm <bbonggu92@gmail.com>
2022-11-15 23:06:26 +0900

    fix: 깨지는 테스트 케이스 수정

--- src/test/kotlin/step3/component/CarInputComponentTest.kt
+++ src/test/kotlin/step3/component/CarInputComponentTest.kt
@@ -6,14 +6,14 @@ import step3.CarStore
 
 class CarInputComponentTest : FunSpec({
     context("커멘드 이벤트 리스너 테스트") {
-        context("자동차 대수 입력 이벤트가 발생하면") {
+        context("자동차 이름 입력 이벤트가 발생하면") {
             test("입력한 대수만큼 자동차를 생성하고 스토어에 저장한다.") {
                 val sut = CarInputComponent()
                 // when
-                sut.onCommand("5")
+                sut.onCommand("relkimm, seulgeun, hyeokzza, duck")
                 // then
                 val actual = CarStore.findAll()
-                actual shouldHaveSize 5
+                actual shouldHaveSize 4
             }
         }
     }
