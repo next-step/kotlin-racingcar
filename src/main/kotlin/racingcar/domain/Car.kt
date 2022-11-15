272d97fb467a62d164882c7da47158cda871a530
Yujin Park <uiuiui118437@gmail.com>
2022-11-15 18:09:14 +0900

    feat: Car를 불변객체로 구현

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -2,11 +2,8 @@ package racingcar.domain
 
 data class Car(
     val name: String,
+    val distance: Int = 0
 ) {
-    private var _distance = 0
-    val distance get() = _distance
 
-    fun takeAction() {
-        _distance += Action.execute().distance
-    }
+    fun takeAction(): Car = this.copy(distance = Action.execute().distance)
 }
