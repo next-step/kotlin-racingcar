93bc9eaa60ad6c01c1a31d5ede275c3109205706
곽진규 <jinkyu.gwak@riiid.co>
2022-11-15 12:27:30 +0900

    refactor: 매직 리터럴 수정

--- src/main/kotlin/study/racing/entity/RacingCar.kt
+++ src/main/kotlin/study/racing/entity/RacingCar.kt
@@ -5,18 +5,20 @@ import study.racing.PowerSource
 class RacingCar(
     private val source: PowerSource
 ) {
-    var currentPosition: Int = 0
+    var currentPosition: Int = START_POSITION
         private set
 
     fun moveForward() {
         if (canMoveForward) {
-            currentPosition += 1
+            currentPosition += MOVE_INTERVAL
         }
     }
 
     private val canMoveForward: Boolean get() = source.getPower() >= POWER_THRESHOLD
 
     companion object {
+        private const val START_POSITION = 0
+        private const val MOVE_INTERVAL = 1
         private const val POWER_THRESHOLD = 4
     }
 }
