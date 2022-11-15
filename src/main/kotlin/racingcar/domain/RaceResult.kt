fb37f16ea089a50c8059799b044c6e712c5fc55f
lalize <printf@kakao.com>
2022-11-15 18:16:14 +0900

    refactor: 가변리스트 캐스팅 방지

--- src/main/kotlin/racingcar/domain/RaceResult.kt
+++ src/main/kotlin/racingcar/domain/RaceResult.kt
@@ -1,7 +1,9 @@
 package racingcar.domain
 
+import java.util.Collections
+
 class RaceResult(records: List<Record>) {
-    val records: List<Record> = records.toList()
+    val records: List<Record> = Collections.unmodifiableList(records.toList())
 
     fun finalRecords(): List<Record> {
         val finalRound = records.maxOf { it.round }
