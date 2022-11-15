d7816b0f38cc8897c0d1ff38a367ec8e8eea63f0
stkment <stkment@gmail.com>
2022-11-15 21:28:26 +0900

    :necktie: RacingGame 테스트 케이스 리팩토링

--- src/test/kotlin/racing/RacingGameKoTest.kt
+++ src/test/kotlin/racing/RacingGameKoTest.kt
@@ -3,10 +3,10 @@ package racing
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import racing.domain.GoOrStopCarActionImpl
-import racing.domain.RacingCar
-import racing.domain.RacingCarImpl
+import racing.domain.Racing
 import racing.domain.RacingCarsFactory
 import racing.domain.RacingCarsFactoryImpl
+import racing.domain.RacingImpl
 import racing.domain.RandomMoveStrategy
 import racing.model.Driver
 import racing.model.RoundCount
@@ -16,47 +16,37 @@ internal class RacingGameKoTest : BehaviorSpec() {
     private lateinit var racingGame: RacingGame
     private lateinit var racingCarsFactory: RacingCarsFactory
     private lateinit var racingRecordBoard: RacingRecordBoard
-    private lateinit var racingCar: RacingCar
-
-    private var roundCount: RoundCount? = null
+    private lateinit var racing: Racing
 
     init {
         beforeEach {
             racingCarsFactory = RacingCarsFactoryImpl()
             racingRecordBoard = RacingRecordBoard()
-            racingCar = RacingCarImpl(
+            racing = RacingImpl(
                 goOrStopCarAction = GoOrStopCarActionImpl(
                     moveStrategy = RandomMoveStrategy()
                 )
             )
         }
 
-        afterEach {
-            roundCount = null
-        }
-
-        given("드라이버 세 명과, 시도횟수 5회 입력을 하였고") {
-            val round = 5
-            val drivers = listOf("똘기", "떵이", "호치", "새초미")
-            beforeEach {
-                roundCount = RoundCount(round)
-                racingGame = RacingGame(
-                    racingCarGarage = RacingCarGarage(
-                        drivers = drivers.map { Driver(it) },
-                        racingCarsFactory = racingCarsFactory
-                    ),
-                    racingCar = racingCar
-                )
-            }
+        given("드라이버 4 명과, 시도횟수 5회 입력을 하였고") {
+            val roundCount = RoundCount(5)
+            val drivers = listOf("똘기", "떵이", "호치", "새초미").map { Driver(it) }
             `when`("경주를 시작하였을 때") {
                 beforeEach {
-                    repeat(requireNotNull(roundCount).value) {
-                        val result = racingGame.race()
-                        racingRecordBoard.add(result)
-                    }
+                    racingGame = RacingGame(
+                        inputView = InputView,
+                        resultView = ResultView,
+                        racingCarGarage = RacingCarGarage(
+                            racingCarsFactory = racingCarsFactory
+                        ),
+                        racing = racing,
+                        racingRecordBoard = racingRecordBoard
+                    )
+                    racingGame.start(drivers, roundCount)
                 }
                 then("차량 3대의 5 라운드 경주 결과를 확인 할 수 있다.") {
-                    racingRecordBoard.results.size shouldBe round
+                    racingRecordBoard.results.size shouldBe roundCount.value
                     racingRecordBoard.results.first().result.size shouldBe drivers.size
                 }
             }
