3fd5408848494d96f37c7681182cfdbd7c04da28
namonak <namonak@gmail.com>
2022-11-15 11:59:33 +0900

    refactor: delete unnecessary classes and test code

--- src/main/kotlin/racingcar/input/InputResultDto.kt
+++
@@ -1,9 +0,0 @@
-package racingcar.input
-
-import racingcar.input.value.NumberOfCars
-import racingcar.input.value.PlayTime
-
-data class InputResultDto(private val NumberOfCars: NumberOfCars, private val playTime: PlayTime) {
-    val getNumberOfCars get() = NumberOfCars.value
-    val getPlayTime get() = playTime.value
-}
--- src/main/kotlin/racingcar/input/value/NumberOfCars.kt
+++
@@ -1,9 +0,0 @@
-package racingcar.input.value
-
-private const val MINIMUM_NUM_OF_CARS = 1
-
-data class NumberOfCars(val value: Int) {
-    init {
-        require(value > MINIMUM_NUM_OF_CARS) { "자동차 대수는 1보다 커야 합니다." }
-    }
-}
