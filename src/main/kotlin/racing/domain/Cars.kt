b62a9a7cff81370e1d7ba19422a2032eaf9183c7
soosue <yunsoo0510@daum.net>
2022-11-15 13:45:30 +0900

    refactor: 클래스 멤버들 선언순서 적용
    1. 프로퍼티와 초기화블록
    2. 부생성자들
    3. 메소드 선언들
    4. 협력 객체

--- src/main/kotlin/racing/domain/Cars.kt
+++ src/main/kotlin/racing/domain/Cars.kt
@@ -2,11 +2,11 @@ package racing.domain
 
 class Cars(private val cars: List<Car>) {
 
-    constructor(carNames: String, moveStrategy: MoveStrategy = RandomStrategy.instance)
-            : this(carNames.split(CAR_NAMES_SPLIT_DELIMITER).map { name -> Car(name, moveStrategy) })
-
     val carInfos: CarInfos
         get() = CarInfos(cars.map { CarInfo(it.name, it.position) })
+
+    constructor(carNames: String, moveStrategy: MoveStrategy = RandomStrategy.instance)
+            : this(carNames.split(CAR_NAMES_SPLIT_DELIMITER).map { name -> Car(name, moveStrategy) })
 
     fun move() {
         cars.map { car -> car.move() }
