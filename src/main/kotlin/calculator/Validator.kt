e587f5f634a1315343f87ad5b7d991eaaced03df
ajy9844 <ajy09844@gmail.com>
2022-11-15 18:35:35 +0900

    refactor(validator): rename isXX() to validateXX()

--- src/main/kotlin/calculator/Validator.kt
+++ src/main/kotlin/calculator/Validator.kt
@@ -1,18 +1,17 @@
 package calculator
 
 class Validator {
-    fun validate(s: String?) {
-        isNullOrBlank(s)
-        if (s != null) isArithmeticOperation(s)
+    fun validate(input: String?) {
+        validateNullOrBlank(input)
+        if (input != null) validateArithmeticOperation(input)
     }
 
-    private fun isNullOrBlank(s: String?) {
+    private fun validateNullOrBlank(s: String?) {
         if (s.isNullOrBlank()) throw IllegalArgumentException()
     }
 
-    private fun isArithmeticOperation(s: String) {
-        val s1 = s.replace(" ", "")
-        val regex = Regex("[^+\\-*/0-9]")
-        if (regex.containsMatchIn(s1)) throw IllegalArgumentException()
+    private fun validateArithmeticOperation(s: String) {
+        val regex = Regex("[^+\\-*/0-9\\s]")
+        if (regex.containsMatchIn(s)) throw IllegalArgumentException()
     }
 }
