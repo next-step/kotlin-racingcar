3ad2c0729122d39ab599bb87a2b38c4a27c988f2
namonak <namonak@gmail.com>
2022-11-16 16:29:31 +0900

    refactor(step4): delete the unnecessary RacingGame class

--- src/main/kotlin/racingcar/RacingGame.kt
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -2,10 +2,6 @@ package racingcar
 
 import racingcar.controller.Controller
 
-class RacingGame {
-    companion object {
-        fun main(args: Array<String>) {
-            Controller().start()
-        }
-    }
+fun main() {
+    Controller().start()
 }
