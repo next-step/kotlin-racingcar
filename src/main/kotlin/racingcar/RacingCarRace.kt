0abe152438c1df0259d167572916d0bd87898d48
이상인/wefu0000 <wefu.child@gmail.com>
2022-11-17 00:28:41 +0900

    refactor: 리뷰 사항 반영
    readln 사용
    InputView 입력에 따른 메소드 분리

--- src/main/kotlin/racingcar/RacingCarRace.kt
+++ src/main/kotlin/racingcar/RacingCarRace.kt
@@ -1,11 +1,15 @@
 package racingcar
 
+import racingcar.data.RacingCarInfo
 import racingcar.ui.InputView
 import racingcar.ui.ResultView
 
 class RacingCarRace {
     fun race() {
-        val carVo = InputView().execute()
+        val inputView = InputView()
+        val carCount = inputView.inputCarCount()
+        val tryCount = inputView.inputTryCount()
+        val carVo = RacingCarInfo(carCount, tryCount, ArrayList(Array(carCount) { 0 }.toList()))
 
         for (i in 0 until carVo.tryCount) {
             RacingCarMove.move(carVo.resultMoveList)
--- src/main/kotlin/racingcar/ui/InputView.kt
+++ src/main/kotlin/racingcar/ui/InputView.kt
@@ -1,15 +1,13 @@
 package racingcar.ui
 
-import racingcar.data.RacingCarInfo
-
 internal class InputView {
-    fun execute(): RacingCarInfo {
+    fun inputCarCount(): Int {
         println("자동차 대수는 몇 대인가요?")
-        val carCount = readLine()?.toInt() ?: 0
+        return readln().toInt()
+    }
 
-        println("시도할 횟수는 몇 회인가요?")
-        val tryCount = readLine()?.toInt() ?: 0
-
-        return RacingCarInfo(carCount, tryCount, ArrayList(Array(carCount) { 0 }.toList()))
+    fun inputTryCount(): Int {
+        println("자동차 대수는 몇 대인가요?")
+        return readln().toInt()
     }
 }
