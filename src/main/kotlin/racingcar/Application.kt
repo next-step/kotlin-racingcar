210db9d3d882838d53d16787fd800e1c03657ba8
lalize <printf@kakao.com>
2022-11-16 19:53:06 +0900

    refactor: 애플리케이션 로직을 컨트롤러로 이동

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,24 +1,7 @@
 package racingcar
 
-import racingcar.domain.CarRacer
-import racingcar.domain.CarRacing
-import racingcar.domain.Name
-import racingcar.domain.RandomCarControl
-import racingcar.domain.StandardCar
-import racingcar.domain.Winner
-import racingcar.view.InputView
-import racingcar.view.OutputView
+import racingcar.controller.RacingCarController
 
 fun main() {
-    try {
-        val carRacing = CarRacing(carRacers = createCarRacers(), count = InputView.readTryCount())
-
-        OutputView.writeResult(carRacing.race())
-        OutputView.writeWinner(Winner(carRacing))
-    } catch (e: RuntimeException) {
-        OutputView.writeError(e)
-    }
+    RacingCarController.race()
 }
-
-private fun createCarRacers(): List<CarRacer> = InputView.readCarNames()
-    .map { CarRacer(StandardCar(name = Name(it)), RandomCarControl()) }
---
+++ src/main/kotlin/racingcar/controller/RacingCarController.kt
@@ -0,0 +1,26 @@
+package racingcar.controller
+
+import racingcar.domain.CarRacer
+import racingcar.domain.CarRacing
+import racingcar.domain.Name
+import racingcar.domain.RandomCarControl
+import racingcar.domain.StandardCar
+import racingcar.domain.Winner
+import racingcar.view.InputView
+import racingcar.view.OutputView
+
+object RacingCarController {
+    fun race() {
+        try {
+            val carRacing = CarRacing(carRacers = createCarRacers(), count = InputView.readTryCount())
+
+            OutputView.writeResult(carRacing.race())
+            OutputView.writeWinner(Winner(carRacing))
+        } catch (e: RuntimeException) {
+            OutputView.writeError(e)
+        }
+    }
+
+    private fun createCarRacers(): List<CarRacer> = InputView.readCarNames()
+        .map { CarRacer(StandardCar(name = Name(it)), RandomCarControl()) }
+}
