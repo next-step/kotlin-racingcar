831e70a3e3b007a550524d1c2a284bf2d437d1a5
seongbeen.kim <seongbeen.kim@linecorp.com>
2022-11-16 22:17:39 +0900

    feat: Add object for position

---
+++ src/main/kotlin/racingcar/domain/MoveStrategy.kt
@@ -0,0 +1,14 @@
+package racingcar.domain
+
+import kotlin.random.Random
+
+fun interface MoveStrategy {
+    fun canMove(): Boolean
+}
+
+private const val MAX_RANDOM_THRESHOLD = 10
+private const val MIN_MOVE_CONDITION = 4
+
+interface RandomStrategy : MoveStrategy {
+    override fun canMove(): Boolean = Random.nextInt(MAX_RANDOM_THRESHOLD) >= MIN_MOVE_CONDITION
+}
---
+++ src/main/kotlin/racingcar/domain/Position.kt
@@ -0,0 +1,19 @@
+package racingcar.domain
+
+data class Position(private val position: Int) {
+    init {
+        if (position < MIN_THRESHOLD) throw IllegalArgumentException("The minimum value for position should be greater than $MIN_THRESHOLD")
+    }
+
+    fun moveForward(moveStrategy: MoveStrategy): Position {
+        return if (moveStrategy.canMove()) {
+            Position(position + 1)
+        } else {
+            this
+        }
+    }
+
+    companion object {
+        private const val MIN_THRESHOLD = 0
+    }
+}
