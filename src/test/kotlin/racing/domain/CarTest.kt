e5b6abe4db91e95f8c60ad927b9769f36a2e2d94
khcho902 <khcho902@gmail.com>
2022-11-17 02:06:57 +0900

    test: CaRacing, Car에 대한 테스트코드 추가

---
+++ src/test/kotlin/racing/domain/CarTest.kt
@@ -0,0 +1,50 @@
+package racing.domain
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class CarTest {
+
+    @Test
+    fun `자동차의 초기 위치는 0이다`() {
+        // given & when
+        val car = Car(forwardByOneMoveStrategy)
+
+        // then
+        assertEquals(0, car.position)
+    }
+
+    @Test
+    fun `앞으로 1씩 이동하는 자동차 테스트`() {
+        // given
+        val car = Car(forwardByOneMoveStrategy)
+
+        // when
+        car.move()
+
+        // then
+        assertEquals(1, car.position)
+    }
+
+    @Test
+    fun `절대 이동하지 않는 자동차 테스트`() {
+        // given
+        val car = Car(stopMoveStrategy)
+
+        // when
+        car.move()
+
+        // then
+        assertEquals(0, car.position)
+    }
+
+    companion object {
+        val forwardByOneMoveStrategy = object : MoveStrategy {
+            override fun getMoveDistance() = 1
+        }
+
+        val stopMoveStrategy = object : MoveStrategy {
+            override fun getMoveDistance() = 0
+        }
+    }
+}
