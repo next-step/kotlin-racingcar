ac0eb9f09d2556eeb9e6bf2695d37f2c3928b886
Shin seongsu <tlstjdtn321@naver.com>
2022-11-18 04:31:37 +0900

    fix: 함수명 수정

--- src/main/kotlin/racing/ui/OutputView.kt
+++ src/main/kotlin/racing/ui/OutputView.kt
@@ -3,11 +3,10 @@ package racing.ui
 object OutputView {
     private const val DASH_LINE = "-"
 
-    fun printOutputLetter() = println("실행 결과")
+    fun printTitle() = println("실행 결과")
 
-    fun printCarCollection(positions: List<Int>) = positions.forEach {
+    fun printCarPosition(positions: List<Int>) = positions.forEach {
         printCarLine(it)
-        println()
     }
 
     private fun printCarLine(position: Int) = println(DASH_LINE.repeat(position))
