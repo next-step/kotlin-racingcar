86390b221cc6af249d43283f62e075a5fb60c901
Shin seongsu <tlstjdtn321@naver.com>
2022-11-18 04:28:37 +0900

    fix: 인터페이스 타입 명시

--- src/main/kotlin/racing/application/RacingGame.kt
+++ src/main/kotlin/racing/application/RacingGame.kt
@@ -1,6 +1,7 @@
 package racing.application
 
 import racing.domain.Car
+import racing.domain.CarMoveStrategy
 import racing.domain.Cars
 import racing.domain.RandomStrategy
 import racing.ui.OutputView
@@ -10,16 +11,16 @@ class RacingGame(
     val numberOfCar: Int
 ) {
 
-    private val carMoveStrategy = RandomStrategy()
+    private val carMoveStrategy: CarMoveStrategy = RandomStrategy()
 
     fun runRace() {
         val cars = Cars(List(numberOfCar) { Car() })
 
-        OutputView.printOutputLetter()
+        OutputView.printTitle()
 
         repeat(tryCount) {
-            cars.tryMoveCars(carMoveStrategy.carMove(tryCount))
-            OutputView.printCarCollection(cars.getCarsPosition())
+            cars.tryMoveCars(carMoveStrategy.values(tryCount))
+            OutputView.printCarPosition(cars.getCarsPosition())
         }
     }
 }
