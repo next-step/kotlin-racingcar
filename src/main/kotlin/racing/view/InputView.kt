beec321a55b14c97f62c2bf1aefea6635381b4e4
khcho902 <khcho902@gmail.com>
2022-11-17 23:40:14 +0900

    fix: not-null assertion(!!) -> safe calls(.?)

--- src/main/kotlin/racing/view/InputView.kt
+++ src/main/kotlin/racing/view/InputView.kt
@@ -5,7 +5,7 @@ object InputView {
     fun inputNumOfCars(): Int {
         println("자동차 대수는 몇 대인가요?")
 
-        val numOfCars = readLine()!!.toIntOrNull() ?: throw IllegalArgumentException("자동차 대수는 숫자값으로 입력해주세요.")
+        val numOfCars = readLine()?.toIntOrNull() ?: throw IllegalArgumentException("자동차 대수는 숫자값으로 입력해주세요.")
         require(numOfCars > 0) { "자동차 대수는 0보다 커야합니다." }
 
         return numOfCars
@@ -14,7 +14,7 @@ object InputView {
     fun inputNumOfMove(): Int {
         println("시도할 횟수는 몇 회인가요?")
 
-        val numOfMove = readLine()!!.toIntOrNull() ?: throw IllegalArgumentException("시도할 횟수는 숫자값으로 입력해주세요.")
+        val numOfMove = readLine()?.toIntOrNull() ?: throw IllegalArgumentException("시도할 횟수는 숫자값으로 입력해주세요.")
         require(numOfMove > 0) { "시도할 횟수는 0보다 커야합니다." }
 
         return numOfMove
