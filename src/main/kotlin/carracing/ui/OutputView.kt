8f015755a65cd242ced9f0d06f5e57996b78d82c
dldudwns72 <dldudwns72@gmail.com>
2022-11-17 21:33:27 +0900

    feat : 자동차 이름 입력 및 우승자 출력 UI 로직 추가

--- src/main/kotlin/carracing/ui/OutputView.kt
+++ src/main/kotlin/carracing/ui/OutputView.kt
@@ -3,33 +3,38 @@ package carracing.ui
 import carracing.domain.Car
 import carracing.domain.Cars
 import carracing.domain.Racing
+import carracing.domain.Winners
 
 const val CAR_SYMBOL = "-"
 const val GAME_RESULT_MESSAGE = "실행 결과"
+const val CAR_NAME_DELIMITER = ","
 
 object OutputView {
 
     private fun printCar(cars: Cars) {
         for (element in cars) {
-            gameRoundPrint(element.position)
+            gameRoundPrint(element)
         }
     }
 
-    private fun gameRoundPrint(position: Int) {
-        println(CAR_SYMBOL.repeat(position))
+    private fun gameRoundPrint(car: Car) {
+        println("${car.name} :  ${CAR_SYMBOL.repeat(car.position)}")
     }
 
     private fun lineChange() {
         println("")
     }
 
-    fun racingResult(carCount: Int, attemptCount: Int) {
+    fun racingResult(carNames: String, attemptCount: Int) {
         println(GAME_RESULT_MESSAGE)
-        val cars = Cars((1..carCount).map { Car() })
+
+        val cars = Cars(carNames.split(CAR_NAME_DELIMITER).map { Car(it) })
 
         for (round in 1..attemptCount) {
             printCar(Racing(cars).race())
             lineChange()
         }
+
+        println("${Winners(cars).getWinners()}가 최종 우승 하였습니다.")
     }
 }
