716be5e2502885fedfa7bcd61f8e66435b19a70c
yongju <odetsuo@gmail.com>
2022-11-17 12:20:13 +0900

    feat: 연산 기호에 따라 사칙 연산을 수행한다

--- src/main/kotlin/stringcalculator/domain/Calculator.kt
+++ src/main/kotlin/stringcalculator/domain/Calculator.kt
@@ -1,8 +1,13 @@
 package stringcalculator.domain
 
-enum class Calculator(private val sign: String) {
+import java.util.function.BinaryOperator
 
-    ADD("+");
+enum class Calculator(private val sign: String, private val operation: BinaryOperator<Int>) {
+
+    ADD("+", { a, b -> a + b }),
+    SUBTRACT("-", { a, b -> a - b }),
+    MULTIPLY("*", { a, b -> a * b }),
+    DIVIDE("/", { a, b -> a / b });
 
     companion object {
         fun of(sign: String): Calculator {
@@ -10,5 +15,9 @@ enum class Calculator(private val sign: String) {
                 ?.let { return it }
                 .run { throw IllegalArgumentException("연산할 수 없는 기호입니다. 기호: $sign") }
         }
+    }
+
+    fun calculate(left: Int, right: Int): Int {
+        return operation.apply(left, right)
     }
 }
