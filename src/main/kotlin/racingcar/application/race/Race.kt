5e8c6167bf5a0ff523d5fd1f6211c123b554f21b
EP <kjuioqq8@gmail.com>
2022-11-17 23:56:43 +0900

    feat: 우승자 로직 구현

--- src/main/kotlin/racingcar/application/race/Race.kt
+++ src/main/kotlin/racingcar/application/race/Race.kt
@@ -4,6 +4,7 @@ import racingcar.application.model.RaceInputCommand
 import racingcar.application.port.output.OutputPort
 import racingcar.application.race.factory.CarFactory
 import racingcar.domain.car.ParticipatingCars
+import java.util.StringJoiner
 
 class Race(
     private val outputPort: OutputPort,
@@ -21,6 +22,7 @@ class Race(
     private fun race(participatingCars: ParticipatingCars, attemptCount: Int) {
         participatingCars.initRace()
         participatingCars.startRace(attemptCount)
+        participatingCars.winnersNameList().winnersAnnounce()
     }
 
     private fun ParticipatingCars.initRace() {
@@ -48,6 +50,18 @@ class Race(
         }
         outputPort.outputNewLine()
         Thread.sleep(PER_RACE_ROUND_SECONDS)
+    }
+
+    private fun List<String>.winnersAnnounce() {
+        outputPort.output("우승자 발표")
+        outputPort.outputNewLine()
+        outputPort.output("두구두구두구----")
+        outputPort.outputNewLine()
+        Thread.sleep(PER_RACE_ROUND_SECONDS)
+
+        val stringJoiner = StringJoiner(",")
+        this.map { stringJoiner.add(it) }
+        outputPort.output("${stringJoiner}가 최종 우승했습니다 !!!")
     }
 
     private companion object {
--- src/main/kotlin/racingcar/domain/car/ParticipatingCars.kt
+++ src/main/kotlin/racingcar/domain/car/ParticipatingCars.kt
@@ -15,4 +15,16 @@ class ParticipatingCars(
             outputFunction(it.currentPosition().toInt(), it.name)
         }
     }
+
+    fun winnersNameList(): List<String> {
+        return winners().map { it.name }.toList()
+    }
+
+    private fun winners(): List<Car> {
+        val winnerPosition = this.carList.sortedWith(
+            compareBy { p -> p.currentPosition().toInt() }
+        ).last().currentPosition()
+
+        return this.carList.filter { it.currentPosition() == winnerPosition }.toList()
+    }
 }
