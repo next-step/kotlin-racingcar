bf508aa006f4c18082a09236a56001f4c39f79c1
youngbin-yun <youngbin@koreaspacedata.com>
2022-11-17 22:53:56 +0900

    refactor: 상수화된 변수를 스트링 템플릿에도 적용

--- src/main/kotlin/racingcar/domain/Name.kt
+++ src/main/kotlin/racingcar/domain/Name.kt
@@ -1,11 +1,13 @@
 package racingcar.domain
 
 @JvmInline
-value class Name(private val name: String) {
+value class Name(val name: String) {
     fun getName() = name
 
     init {
-        require(name.length <= MAXIMUM_NAME_LENGTH) { "이름은 10자 이하여야 합니다." }
+        require(name.length <= MAXIMUM_NAME_LENGTH) {
+            "이름은 $MAXIMUM_NAME_LENGTH 자 이하여야 합니다."
+        }
     }
 
     companion object {
