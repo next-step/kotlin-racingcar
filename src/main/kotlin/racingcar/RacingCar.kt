9cfa21f2064c3a8ea156d6bce4b262834b61c8f5
이상인/wefu0000 <wefu.child@gmail.com>
2022-11-19 01:52:31 +0900

    fix: 자동차 생성 개수 오류 수정

--- src/main/kotlin/racingcar/RacingCar.kt
+++ src/main/kotlin/racingcar/RacingCar.kt
@@ -5,7 +5,7 @@ import java.security.SecureRandom
 private const val MAXIMUM_BOUND = 10
 
 class RacingCar(private val cars: List<Car>, private val tryCount: Int) {
-    constructor(carCount: Int, tryCount: Int) : this((0..carCount).map { Car() }, tryCount)
+    constructor(carCount: Int, tryCount: Int) : this((0 until carCount).map { Car() }, tryCount)
 
     fun start(): List<Car> {
         for (i in 0 until tryCount) {
