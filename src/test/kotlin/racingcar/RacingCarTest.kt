cf9fb35d64e2ee269239e1e16ec9b8b8e3c3b584
이상인/wefu0000 <wefu.child@gmail.com>
2022-11-19 01:52:07 +0900

    refactor: 리뷰 사항 반영
    단위 테스트 작성

--- src/test/kotlin/racingcar/RacingCarTest.kt
+++ src/test/kotlin/racingcar/RacingCarTest.kt
@@ -13,15 +13,25 @@ class RacingCarTest {
 
     @Test
     fun `전진 성공 테스트`() {
-        val car = Car()
-
-        assertTrue(car.move(6) == 1)
+        assertTrue(Car().move(6) == 1)
     }
 
     @Test
     fun `전진 실패 테스트`() {
-        val car = Car()
-        car.move(3)
-        assertTrue(car.move(3) == 0)
+        assertTrue(Car().move(3) == 0)
+    }
+
+    @Test
+    fun `자동차 경주 자동차 생성 테스트`() {
+        val carCount = 3
+
+        assertTrue(RacingCar(carCount, 0).start().size == carCount)
+    }
+
+    @Test
+    fun `자동차 경주 시도 횟에 따른 이동 히스토리 생성 테스트`() {
+        val tryCount = 5
+
+        assertTrue(RacingCar(1, tryCount).start()[0].moveHistory.size == tryCount)
     }
 }
