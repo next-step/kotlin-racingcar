bb615782c6cd191dde2f7c271b88525555f0e87d
이상인/wefu0000 <wefu.child@gmail.com>
2022-11-19 00:36:18 +0900

    refactor: 리뷰 사항 반영
    Car 움직임은 스스로 판단

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,11 +1,13 @@
 package racingcar
 
+private const val MINIMUM_BOUND = 4
+
 class Car(moveCount: Int = 0) {
     var moveCount = moveCount
         private set
 
     fun move(moveNumber: Int) {
-        if (RacingCarUtil.checkMoveValue(moveNumber)) {
+        if (moveNumber >= MINIMUM_BOUND) {
             moveCount ++
         }
     }
--- src/main/kotlin/racingcar/RacingCarUtil.kt
+++ src/main/kotlin/racingcar/RacingCarUtil.kt
@@ -4,9 +4,6 @@ import java.security.SecureRandom
 
 object RacingCarUtil {
     private const val MAXIMUM_BOUND = 10
-    private const val MINIMUM_BOUND = 4
 
     fun createRandomInt(): Int = SecureRandom.getInstanceStrong().nextInt(MAXIMUM_BOUND)
-
-    fun checkMoveValue(value: Int): Boolean = value >= MINIMUM_BOUND
 }
