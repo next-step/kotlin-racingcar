b85e84043076e749beff8db87075d1b6589b06f8
ajy9844 <ajy09844@gmail.com>
2022-11-20 06:36:58 +0900

    feat(car): add move() that move when value is greater than or equal to 4

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,5 +1,15 @@
 package racingcar
 
 class Car {
+    var position: Int = DEFAULT_POSITION
+        private set
 
+    fun move(value: Int) {
+        if (value >= FORWARD_NUMBER) position++
+    }
+
+    companion object {
+        private const val DEFAULT_POSITION = 0
+        private const val FORWARD_NUMBER = 4
+    }
 }
