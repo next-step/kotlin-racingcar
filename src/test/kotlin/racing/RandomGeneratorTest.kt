e966f5d4d286708971120ae27ee3e193e087adbc
AI2팀-이정환 <eedys@ex-em.com>
2022-11-19 20:35:57 +0900

    <refactor> : 피드백적용

--- src/test/kotlin/racing/RandomGeneratorTest.kt
+++ src/test/kotlin/racing/RandomGeneratorTest.kt
@@ -8,10 +8,8 @@ class RandomGeneratorTest {
 
     @RepeatedTest(value = 5)
     fun `랜덤숫자 발행 테스트`() {
-        for (i in 1..9) {
-            val randomNumber = RandomGenerator.generate()
-            assertThat(randomNumber).isGreaterThanOrEqualTo(0)
-            assertThat(randomNumber).isLessThanOrEqualTo(9)
-        }
+        val randomNumber = RandomGenerator.generate()
+        assertThat(randomNumber).isGreaterThanOrEqualTo(0)
+        assertThat(randomNumber).isLessThanOrEqualTo(9)
     }
 }
