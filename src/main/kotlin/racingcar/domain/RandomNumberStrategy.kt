aee6eb4434431168049087fc262dc108fd962d29
dk-ko <dev.dkko@gmail.com>
2022-11-20 08:02:18 +0900

    style: lint

--- src/main/kotlin/racingcar/domain/RandomNumberStrategy.kt
+++ src/main/kotlin/racingcar/domain/RandomNumberStrategy.kt
@@ -2,7 +2,7 @@ package racingcar.domain
 
 private val RANDOM_NUMBER_RANGE = 0..9
 
-class RandomNumberStrategy: Strategy {
+class RandomNumberStrategy : Strategy {
     override fun moveStrategy(): Int {
         return (RANDOM_NUMBER_RANGE).random()
     }
