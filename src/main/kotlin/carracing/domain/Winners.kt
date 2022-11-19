a6b03e44cd733788a47c46e287b7f8898d54f525
dldudwns72 <dldudwns72@gmail.com>
2022-11-20 02:53:26 +0900

    refactor : winner 반환 변경 리팩토링

--- src/main/kotlin/carracing/domain/Winners.kt
+++ src/main/kotlin/carracing/domain/Winners.kt
@@ -1,14 +1,9 @@
 package carracing.domain
 
-const val WINNER_DELIMITER = ","
-
 class Winners(private val cars: Cars) {
 
-    fun getWinners(): String {
+    fun getWinners(): List<Car> {
         val maxPosition = cars.maxOf { it.position }
-
-        val winners = cars.filter { it.position == maxPosition }
-
-        return winners.joinToString(WINNER_DELIMITER) { it.name }
+        return cars.filter { it.position == maxPosition }
     }
 }
--- src/test/kotlin/study/carracing/WinnersTest.kt
+++ src/test/kotlin/study/carracing/WinnersTest.kt
@@ -2,6 +2,7 @@ package study.carracing
 
 import carracing.domain.Car
 import carracing.domain.Cars
+import carracing.domain.Name
 import carracing.domain.Racing
 import carracing.domain.Winners
 import org.assertj.core.api.Assertions.assertThat
@@ -11,22 +12,27 @@ class WinnersTest {
 
     @Test
     fun `단일 우승자 반환 테스트`() {
-        val car1 = Car("lee", moveStrategy = StandardStrategy(2))
-        val car2 = Car("kim", moveStrategy = StandardStrategy(3))
-        val car3 = Car("park", moveStrategy = StandardStrategy(6))
+        val car1 = Car(Name("lee"), moveStrategy = StandardStrategy(2))
+        val car2 = Car(Name("kim"), moveStrategy = StandardStrategy(3))
+        val car3 = Car(Name("park"), moveStrategy = StandardStrategy(6))
         val cars = Cars(listOf(car1, car2, car3))
         val winners = Winners(Racing(cars).race()).getWinners()
-
-        assertThat(winners).isEqualTo("park")
+        val winnerNames = winners.joinToString(",") {
+            it.getName()
+        }
+        assertThat(winnerNames).isEqualTo("park")
     }
 
     @Test
     fun `다중 우승자 반환 테스트`() {
-        val car1 = Car("lee", moveStrategy = StandardStrategy(3))
-        val car2 = Car("kim", moveStrategy = StandardStrategy(5))
-        val car3 = Car("park", moveStrategy = StandardStrategy(6))
+        val car1 = Car(Name("lee"), moveStrategy = StandardStrategy(3))
+        val car2 = Car(Name("kim"), moveStrategy = StandardStrategy(5))
+        val car3 = Car(Name("park"), moveStrategy = StandardStrategy(6))
         val cars = Cars(listOf(car1, car2, car3))
         val winners = Winners(Racing(cars).race()).getWinners()
-        assertThat(winners).isEqualTo("kim,park")
+        val winnerNames = winners.joinToString(",") {
+            it.getName()
+        }
+        assertThat(winnerNames).isEqualTo("kim,park")
     }
 }
