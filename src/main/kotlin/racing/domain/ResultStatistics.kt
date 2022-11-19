20bec37c2ab722d341ed5267ab67936d6c2214c2
AI2팀-이정환 <eedys@ex-em.com>
2022-11-19 21:01:15 +0900

    <refactor> : 실행결과 출력 수정

--- src/main/kotlin/racing/domain/ResultStatistics.kt
+++ src/main/kotlin/racing/domain/ResultStatistics.kt
@@ -3,7 +3,7 @@ package racing.domain
 class ResultStatistics(private val cars: Cars) {
 
     fun toResult(): String {
-        return cars.map { "${it.name}: ${toMark(it)}" }.joinToString(separator = System.lineSeparator()).trimIndent()
+        return cars.map { "${it.name} : ${toMark(it)}" }.joinToString(separator = System.lineSeparator()).trimIndent()
     }
 
     private fun toMark(car: Car): String {
