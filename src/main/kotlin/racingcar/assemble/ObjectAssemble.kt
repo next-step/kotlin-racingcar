d2c90cd1bdae17eefe0ad674ef54d85ebf190af9
EP <kjuioqq8@gmail.com>
2022-11-19 09:52:14 +0900

    refactor: 객체 조립 구조 변경

--- src/main/kotlin/racingcar/assemble/ObjectAssemble.kt
+++ src/main/kotlin/racingcar/assemble/ObjectAssemble.kt
@@ -3,6 +3,7 @@ package racingcar.assemble
 import racingcar.application.port.input.InputPort
 import racingcar.application.port.output.OutputPort
 import racingcar.application.race.Race
+import racingcar.application.race.ScoreBoard
 import racingcar.application.race.factory.CarFactory
 import racingcar.domain.car.engine.implement.RandomMovingEngine
 import racingcar.interfaces.ui.input.RacingCarInputAdapter
@@ -13,7 +14,8 @@ object ObjectAssemble {
     private fun racingCarInputAdapter() = RacingCarInputAdapter(inputPort = inputPort())
     private fun inputPort() = InputPort(race = race())
 
-    private fun race() = Race(outputPort = outputPort(), carFactory = carFactory())
+    private fun race() = Race(scoreBoard = scoreBoard(), carFactory = carFactory())
+    private fun scoreBoard() = ScoreBoard(outputPort = outputPort())
     private fun carFactory() = CarFactory(movingEngine = movingEngine())
     private fun movingEngine() = RandomMovingEngine()
 
--- src/test/kotlin/racingcar/domain/car/ParticipatingCarsTest.kt
+++ src/test/kotlin/racingcar/domain/car/ParticipatingCarsTest.kt
@@ -25,7 +25,7 @@ class ParticipatingCarsTest : FunSpec({
     }
 
     test("레이스 초기화시 출력 함수를 인자로 넘길 수 있다") {
-        val outputFunction: (String) -> Unit = { carName -> println("$carName 출력완료") }
-        participatingCars.initRace(outputFunction)
+        val outputFunction: (Int, String) -> Unit = { position: Int, carName: String -> println("$position $carName 출력완료") }
+        participatingCars.move(outputFunction = outputFunction)
     }
 })
