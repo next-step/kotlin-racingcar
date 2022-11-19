934ffbb1afbd1f44a1e7aa3e1f0c400c964ee013
wisotge <rnalstn0507@gmail.com>
2022-11-20 07:48:13 +0900

    refactor: 변수 네이밍 변경

--- src/main/kotlin/racingcar/RacingCarGame.kt
+++ src/main/kotlin/racingcar/RacingCarGame.kt
@@ -9,10 +9,10 @@ class RacingCarGame {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            val raceSettingInput = InputView.getInput()
+            val racingCarGameInput = InputView.getInput()
 
-            val numberOfCarInput = raceSettingInput.numberOfCar
-            val totalCountOfRoundInput = raceSettingInput.totalCountOfRound
+            val numberOfCarInput = racingCarGameInput.numberOfCar
+            val totalCountOfRoundInput = racingCarGameInput.totalCountOfRound
 
             if (numberOfCarInput < 1 || totalCountOfRoundInput < 1) throw IllegalArgumentException("올바르지 않은 게임 세팅값입니다.")
 
