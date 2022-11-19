52ae783a8312836aec3d4526ab27aaed07770f23
ajy9844 <ajy09844@gmail.com>
2022-11-20 06:35:03 +0900

    feat(game): add random() that generate random number between 0 and 9

--- src/main/kotlin/racingcar/RacingGame.kt
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -1,6 +1,7 @@
 package racingcar
 
 import java.util.Collections
+import kotlin.random.Random
 
 class RacingGame {
     var carList: List<Car> = listOf()
@@ -8,5 +9,13 @@ class RacingGame {
 
     fun set(numberOfCars: Int) {
         carList = Collections.nCopies(numberOfCars, Car())
+    }
+
+    private fun random(): Int {
+        return Random.nextInt(MAX_RANDOM_NUMBER)
+    }
+
+    companion object {
+        private const val MAX_RANDOM_NUMBER: Int = 9
     }
 }
