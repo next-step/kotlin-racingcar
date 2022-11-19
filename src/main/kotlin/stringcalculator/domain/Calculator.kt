4ed4411f920a52dc7a7544c5a55fdc4f25b9d2ca
testrace <odetsuo@gmail.com>
2022-11-19 17:42:33 +0900

    2단계 - 문자열 계산기 (#894)
    * docs: 요구사항 정리
    
    * feat: 연산 기호에 해당하는 계산기를 찾는다
    
    * feat: 연산 기호에 따라 사칙 연산을 수행한다
    
    * feat: 0으로 나누는 경우 연산하지 않는다
    
    * feat: 연산 결과의 소수점은 무시한다
    
    * feat: 연산 결과의 소수점은 무시한다
    
    * feat: 입력 순으로 연산하여 출력한다

---
+++ src/main/kotlin/stringcalculator/domain/Calculator.kt
@@ -0,0 +1,27 @@
+package stringcalculator.domain
+
+import java.util.function.BinaryOperator
+
+enum class Calculator(private val sign: String, private val operation: BinaryOperator<Int>) {
+
+    ADD("+", { a, b -> a + b }),
+    SUBTRACT("-", { a, b -> a - b }),
+    MULTIPLY("*", { a, b -> a * b }),
+    DIVIDE("/", { a, b -> a / b });
+
+    companion object {
+        fun of(sign: String): Calculator {
+            values().find { it.sign == sign }
+                ?.let { return it }
+                .run { throw IllegalArgumentException("연산할 수 없는 기호입니다. 기호: $sign") }
+        }
+    }
+
+    fun calculate(left: Int, right: Int): Int {
+        if (this == DIVIDE && right == 0) {
+            throw IllegalArgumentException("0으로 나눌 수 없습니다.")
+        }
+
+        return operation.apply(left, right)
+    }
+}
---
+++ src/test/kotlin/stringcalculator/domain/CalculatorTest.kt
@@ -0,0 +1,50 @@
+package stringcalculator.domain
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import stringcalculator.domain.Calculator.ADD
+import stringcalculator.domain.Calculator.DIVIDE
+import stringcalculator.domain.Calculator.MULTIPLY
+import stringcalculator.domain.Calculator.SUBTRACT
+
+class CalculatorTest : StringSpec({
+
+    "사칙연산 기호로 해당하는 계산기를 찾는다" {
+        listOf(
+            "+" to ADD,
+            "-" to SUBTRACT,
+            "*" to MULTIPLY,
+            "/" to DIVIDE
+        ).forEach { (sign, calculator) ->
+            Calculator.of(sign) shouldBe calculator
+        }
+    }
+
+    "올바르지 않는 기호로 계산기를 찾으면 예외가 발생한다" {
+        shouldThrow<IllegalArgumentException> {
+            Calculator.of("a")
+        }
+    }
+
+    "두 개의 숫자로 연산을 수행한다" {
+        listOf(
+            ADD to 4,
+            SUBTRACT to 0,
+            MULTIPLY to 4,
+            DIVIDE to 1
+        ).forEach { (calculator, expected) ->
+            calculator.calculate(2, 2) shouldBe expected
+        }
+    }
+
+    "0으로 나누면 예외가 발생한다" {
+        shouldThrow<IllegalArgumentException> {
+            DIVIDE.calculate(2, 0)
+        }
+    }
+
+    "연산 결과의 소수점은 무시한다" {
+        DIVIDE.calculate(1, 3) shouldBe 0
+    }
+})
