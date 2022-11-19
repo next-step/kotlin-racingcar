fea37bf94b1a6131bf75d035091983a101d54715
Minkyung <mminkyung.k@gmail.com>
2022-11-19 17:59:57 +0900

    test: 자동차들에게 0-9 사이 랜덤한 값 주어지고, 4 이상일 때만 전진할 수 있다

--- src/test/kotlin/racingcar/domain/car/CarSpec.kt
+++ src/test/kotlin/racingcar/domain/car/CarSpec.kt
@@ -2,6 +2,8 @@ package racingcar.domain.car
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.ints.shouldBeInRange
+import io.kotest.matchers.shouldBe
 import racingcar.exception.InvalidCarNameLengthInput
 
 class CarSpec : StringSpec({
@@ -11,5 +13,26 @@ class CarSpec : StringSpec({
         shouldThrow<InvalidCarNameLengthInput> {
             Car(invalidCarName)
         }
+    }
+
+    "주어진 수가 4 이상일 때만 움직인다" {
+        val car = Car("pony")
+        val result = car.move(4)
+
+        result shouldBe 1
+    }
+
+    "주어진 수가 4 미만일 때 움직이지 않는다" {
+        val car = Car("pony")
+        val result = car.move(3)
+
+        result shouldBe 0
+    }
+
+    "0과 9사이의 수를 랜덤하게 반환한다" {
+        val car = Car("pony")
+        val result = car.getRandomNumber()
+
+        result shouldBeInRange 0..9
     }
 })
