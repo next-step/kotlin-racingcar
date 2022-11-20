f79be883ae05a1a454f7bc0008cc1bd382d81ee1
ajy9844 <ajy09844@gmail.com>
2022-11-20 10:33:45 +0900

    feat(view): add result view

--- src/main/kotlin/racingcar/InputView.kt
+++ src/main/kotlin/racingcar/InputView.kt
@@ -1,12 +1,15 @@
 package racingcar
 
 class InputView {
-    fun view(): Pair<Int, Int> {
-        println("자동차 대수는 몇 대인가요?")
-        val numberOfCars = readLine()!!.toInt()
-        println("시도할 횟수는 몇 회인가요?")
-        val count = readLine()!!.toInt()
+    companion object {
+        fun view(): Pair<Int, Int> {
+            println("자동차 대수는 몇 대인가요?")
+            val numberOfCars = readLine()!!.toInt()
+            println("시도할 횟수는 몇 회인가요?")
+            val count = readLine()!!.toInt()
+            println("실행 결과")
 
-        return numberOfCars to count
+            return numberOfCars to count
+        }
     }
 }
