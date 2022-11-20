57b1c77a25d94b098db5718a636f9aea8ae52569
riyenas0925 <riyenas0925@gmail.com>
2022-11-20 20:11:48 +0900

    feat: 레이싱 게임은 자동차의 이름을 쉽표로 구분해요.

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -2,13 +2,20 @@ package racingcar.view
 
 import racingcar.domain.Car
 
+private const val VALID_CAR_NAME_LENGTH = 5
+
 object InputView {
     fun readCars(): List<Car> {
-        println("자동차 대수는 몇 대인가요?")
+        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+        val names = parseCarName(readln())
 
         val list = mutableListOf<Car>()
-        repeat(readln().toInt()) {
-            list.add(Car())
+        for(name in names) {
+            require(isValidLength(name)) { "글자수는 5자를 넘을 수 없어요" }
+
+            list.add(
+                Car(name = name)
+            )
         }
 
         return list
@@ -17,5 +24,13 @@ object InputView {
     fun readRoundCount(): Int {
         println("시도할 횟수는 몇 회인가요?")
         return readln().toInt()
+    }
+
+    private fun isValidLength(name: String): Boolean {
+        return name.length <= VALID_CAR_NAME_LENGTH
+    }
+
+    private fun parseCarName(input :String): List<String> {
+        return input.split(",")
     }
 }
--- src/main/kotlin/racingcar/view/ResultView.kt
+++ src/main/kotlin/racingcar/view/ResultView.kt
@@ -6,6 +6,7 @@ object ResultView {
     fun print(cars: List<Car>) {
         for (car in cars) {
             val location = car.location
+            print(car.name + " : ")
             repeat(location) { print("-") }
             println()
         }
