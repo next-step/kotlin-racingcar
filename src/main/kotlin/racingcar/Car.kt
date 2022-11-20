9ab2acd2bd20aaaefc432a76a6758c7ffd198827
riyenas0925 <riyenas0925@gmail.com>
2022-11-20 21:39:56 +0900

    refactor: 자동차 이름 길이 검증을 Car 클래스로 변경

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -4,11 +4,23 @@ class Car(
     private var _location: Int = 0,
     val name: String,
 ) {
+    init {
+        require(isValidLength(name)) { "글자수는 5자를 넘을 수 없어요" }
+    }
+
     val location
         get() = _location
     fun move(number: Int) {
         val rule = MovingJudgeRule.judge(number)
         val strategy = rule.strategy
         _location = strategy.move(_location)
+    }
+
+    private fun isValidLength(name: String): Boolean {
+        return name.length <= VALID_CAR_NAME_LENGTH
+    }
+
+    companion object {
+        private const val VALID_CAR_NAME_LENGTH = 5
     }
 }
--- src/main/kotlin/racingcar/InputView.kt
+++ src/main/kotlin/racingcar/InputView.kt
@@ -1,7 +1,5 @@
 package racingcar
 
-private const val VALID_CAR_NAME_LENGTH = 5
-
 object InputView {
     fun readCars(): List<Car> {
         println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
@@ -9,8 +7,6 @@ object InputView {
 
         val list = mutableListOf<Car>()
         for (name in names) {
-            require(isValidLength(name)) { "글자수는 5자를 넘을 수 없어요" }
-
             list.add(
                 Car(name = name)
             )
@@ -22,10 +18,6 @@ object InputView {
     fun readRoundCount(): Int {
         println("시도할 횟수는 몇 회인가요?")
         return readln().toInt()
-    }
-
-    private fun isValidLength(name: String): Boolean {
-        return name.length <= VALID_CAR_NAME_LENGTH
     }
 
     private fun parseCarName(input: String): List<String> {
