c10c76c084dea16b6819bd9e95cb0141a2bef324
dajeong <galnotepink@gmail.com>
2022-11-20 22:56:43 +0900

    [Step 5] 리뷰 반영 - maxDistance 결과 생성시 구하도록

--- src/main/kotlin/racingcar/domain/carRacing/CarRacing.kt
+++ src/main/kotlin/racingcar/domain/carRacing/CarRacing.kt
@@ -7,7 +7,6 @@ import racingcar.domain.carRacing.dto.TurnRecord
 
 class CarRacing(private val cars: List<Car>) {
     private val records: MutableList<TurnRecord> = mutableListOf()
-    private var maxDistance = 0
 
     fun start(indicatorGenerator: IndicatorGenerator, turnCount: Int) {
         val indicators = indicatorGenerator.generate(cars.size, turnCount)
@@ -17,7 +16,6 @@ class CarRacing(private val cars: List<Car>) {
             val turnRecord = cars
                 .onEachIndexed { index, car ->
                     car.go(indicator.findByCarIndex(index))
-                    recordMaxDistance(car)
                 }
                 .map {
                     CarRecord(it, it.currentDistance())
@@ -28,10 +26,6 @@ class CarRacing(private val cars: List<Car>) {
 
     private fun turnForResult(turn: Int) = turn + 1
 
-    private fun recordMaxDistance(car: Car) {
-        maxDistance = maxDistance.coerceAtLeast(car.currentDistance())
-    }
-
     fun result(): CarRacingResult {
         return CarRacingResult(
             winners = determineWinners(),
@@ -40,9 +34,13 @@ class CarRacing(private val cars: List<Car>) {
     }
 
     private fun determineWinners(): List<Car> {
-        val finalTurnRecord = records.last()
-        return finalTurnRecord.results
+        val maxDistance = maxDistance()
+        return lastRecord().results
             .filter { it.distance == maxDistance }
             .map { it.car }
     }
+
+    private fun maxDistance() = lastRecord().results.maxOf { it.distance }
+
+    private fun lastRecord() = records.last()
 }
