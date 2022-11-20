306af615c34b6ab0e1920b20af96a52b0d387c22
hojihun5516 <hojihun5516@daum.net>
2022-11-20 17:58:26 +0900

    feat: [Step 4] - 이름 추가 및 우승자 출력
    * 자동차 이름을 받는 함수 추가
    * 우승자를 알려주기 위한 출력 메시지 추가

--- src/main/kotlin/race/Car.kt
+++ src/main/kotlin/race/Car.kt
@@ -1,11 +1,17 @@
 package race
 
-class Car {
+class Car private constructor(val name: String) {
     fun move(randomNumber: Int): Boolean {
         return randomNumber >= THRESHOLD
     }
 
     companion object {
         private const val THRESHOLD = 4
+        private const val CAR_NAME_LENGTH_BY_RULE = 5
+        fun from(name: String): Car {
+            if (name.length > CAR_NAME_LENGTH_BY_RULE)
+                throw IllegalArgumentException("차의 이름 길이는 $CAR_NAME_LENGTH_BY_RULE 글자를 넘으면 안됩니다")
+            return Car(name)
+        }
     }
 }
