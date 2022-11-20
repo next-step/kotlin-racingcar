ba294c502c1d514b71d42aca62304c0e8e8042b3
dk-ko <dev.dkko@gmail.com>
2022-11-20 14:53:53 +0900

    refactor: 코드리뷰반영

--- src/main/kotlin/racingcar/domain/Cars.kt
+++ src/main/kotlin/racingcar/domain/Cars.kt
@@ -2,25 +2,16 @@ package racingcar.domain
 
 data class Cars(private val cars: List<Car>) {
     fun move(condition: () -> Int): List<Car> {
-        val of = mutableListOf<Car>()
-
-        cars.forEach {
-            of.add(it.move(condition()))
+        return cars.map {
+            it.move(condition())
         }
-        return of
     }
 
     fun findMaxStatusValue(): Int = cars.maxOf { it.status }
 
     fun statistic(max: Int): List<Car> {
-        val result = mutableListOf<Car>()
-        cars.forEach { car ->
-            run {
-                if (car.isWinner(max)) {
-                    result += car
-                }
-            }
+        return cars.filter {
+            it.isWinner(max)
         }
-        return result
     }
 }
