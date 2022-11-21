ba2f4a647f200b7a6156f54c570545dd13432c8f
Choiyounho <maxosa78@gmail.com>
2022-11-22 00:02:43 +0900

    refactor : 테스트 코드 함수명 변경

--- src/test/kotlin/racingcar/domain/CarTest.kt
+++ src/test/kotlin/racingcar/domain/CarTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 internal class CarTest {
 
     @Test
-    fun `자동차의 전진 조건에 만족하여 전진을 하는 경우`() {
+    fun `자동차의 전진 조건인 랜덤 숫자 4이상이 되어 만족하여 전진을 하는 경우`() {
         // given
         val car = Car()
         val terms = 4
@@ -19,7 +19,7 @@ internal class CarTest {
     }
 
     @Test
-    fun `자동차 전진 조건에 만족하지 못하여 제자리에 있는 경우`() {
+    fun `자동차 전진 조건에 만족하지 못한 랜덤 숫자가 4미만이라 제자리에 있는 경우`() {
         // given
         val car = Car()
         val terms = 1
