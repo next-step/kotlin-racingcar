b4497de5438eff0c669f385699e5547685fcc83f
relkimm <bbonggu92@gmail.com>
2022-11-22 00:09:50 +0900

    refactor: DistanceComponent 네이밍 변경

--- src/main/kotlin/racingcar/view/component/CarDistanceComponent.kt
+++ src/main/kotlin/racingcar/view/component/CarDistanceComponent.kt
@@ -3,10 +3,10 @@ package racingcar.view.component
 import racingcar.domain.Position
 import racingcar.view.ui.Span
 
-class DistanceComponent(
+class CarDistanceComponent(
     private val position: Position,
 ) : Component {
     override fun render() {
-        repeat(position.value) { Span(text = "-").draw() }
+        repeat(this.position.value) { Span(text = "-").draw() }
     }
 }
