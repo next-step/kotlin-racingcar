ee967ec59dd9bc018dca205997c2b64ddba27682
relkimm <bbonggu92@gmail.com>
2022-11-21 23:40:25 +0900

    feat: 라운드 관련 UI 를 표시하는 RoundComponent 추가

--- src/main/kotlin/racingcar/view/component/RacingResultComponent.kt
+++ src/main/kotlin/racingcar/view/component/RacingResultComponent.kt
@@ -2,20 +2,13 @@ package racingcar.view.component
 
 import racingcar.view.store.CarGroupStore
 import racingcar.view.store.RoundStore
-import racingcar.view.ui.Br
 
 class RacingResultComponent : Component {
     override fun render() {
         val carGroup = CarGroupStore.getState()
         val rounds = RoundStore.getState()
 
-        rounds.forEach { round ->
-            val roundResult = round.start(carGroup = carGroup)
-            RoundOrderComponent(id = round.id).render()
-            RoundResultComponent(roundResult = roundResult).render()
-            Br().draw()
-            Thread.sleep(1000)
-        }
+        rounds.forEach { round -> RoundComponent(round = round).render() }
         val winners = carGroup.getWinners()
         WinnerComponent(winners = winners).render()
     }
---
+++ src/main/kotlin/racingcar/view/component/RoundComponent.kt
@@ -0,0 +1,24 @@
+package racingcar.view.component
+
+import racingcar.domain.Round
+import racingcar.domain.RoundResult
+import racingcar.view.store.CarGroupStore
+import racingcar.view.ui.Br
+
+class RoundComponent(private val round: Round) : Component {
+
+    private fun roundStart(): RoundResult {
+        val carGroup = CarGroupStore.getState()
+        val roundResult = round.start(carGroup = carGroup)
+        CarGroupStore.setState(state = carGroup)
+        return roundResult
+    }
+
+    override fun render() {
+        val roundResult = this.roundStart()
+        RoundOrderComponent(id = round.id).render()
+        RoundResultComponent(roundResult = roundResult).render()
+        Br().draw()
+        Thread.sleep(1000)
+    }
+}
