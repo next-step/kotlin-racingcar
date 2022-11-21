0969d56788b2714fdeaa9713e024aab6ac0f2d55
AI2팀-이정환 <eedys@ex-em.com>
2022-11-21 23:35:03 +0900

    <conflict> : rebase 과정에서 충돌해결

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -2,43 +2,21 @@ package calculator
 
 class StringCalculator {
 
-    fun calculate(input: String): Num {
+    fun calculate(mathematical: String): Num {
 
-        val data = removeSpace(input)
-        verifyOperator(data)
-
-        val expression = Expression(data)
-        val queue = expression.apply()
+        val expression = Expression(mathematical)
+        val queue = expression.compute()
 
         var result = Num(queue.poll())
 
         while (queue.isNotEmpty()) {
-            val operator = queue.poll()
-            val operand = queue.poll()
-            val o = Operator(operator.first())
-            result = o.calculate(result, Num(operand))
+            val operator = Operator(queue.poll().first())
+            val second = Num(queue.poll())
+            result = calculate(result, second, operator)
         }
 
         return result
     }
 
-    private fun removeSpace(input: String): String {
-        return input.replace(" ", "")
-    }
-
-    private fun verifyOperator(data: String) {
-        val splitData = data.split("+", "-", "*", "/")
-
-        if (splitData.size != data.length - (data.length / 2)) {
-            throw IllegalArgumentException()
-        }
-
-        for (c in splitData) {
-            try {
-                Integer.parseInt(c)
-            } catch (e: NumberFormatException) {
-                throw IllegalArgumentException()
-            }
-        }
-    }
+    private fun calculate(first: Num, second: Num, operator: Operator) = operator.calculate(first, second)
 }
