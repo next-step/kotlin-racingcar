78083fdc86e7435cde81ec89d8c951e51982787e
choi-ys <project.log.062@gmail.com>
2022-11-22 16:26:26 +0900

    refactor: 리턴 타입이 생략된 멤버 변수와 메서드의 리턴 타입 명시
    - 반환 타입을 명시하여 직관성을 높힘으로써 의도하지 않은 타입 추론으로 인해 발생하는 여러 현상 방어

--- src/main/kotlin/step3/racingcar/domain/Cars.kt
+++ src/main/kotlin/step3/racingcar/domain/Cars.kt
@@ -2,6 +2,6 @@ package step3.racingcar.domain
 
 class Cars(count: Int) {
     private val elements: List<Car> = List(count) { Car() }
-    fun elements() = elements
-    fun size() = elements.size
+    fun elements(): List<Car> = elements
+    fun size(): Int = elements.size
 }
