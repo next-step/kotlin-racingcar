f5c1beea686262c4ce4a871bbed5ed14da2a1ff4
jysohn0825 <jysohn0825@naver.com>
2022-11-22 20:52:39 +0900

    Step5 (#916)
    * kotlin basic grammar
    
    * Add Calculator Production Code
    
    without test code
    
    * Add Test Code
    
    add test code
    optimizing production & test code
    
    * Fix Operaotr enum
    
    - fix operaotr enum method (find method occur IllegalArguementException, add high order function)
    - change validCheck method, not if branch but require method
    
    * Revert "Fix Operaotr enum"
    
    This reverts commit 7d92bc5eac560224e6b10632dba70cb33d3645a1.
    
    * Fix Operator enum
    
    change operaotr enum (add high order function, add Exception)
    change method name
    add test code
    
    * Add RacingCar classes
    
    - make domain package (Car, Race)
    - make view package (InputView, ResultView)
    - make RacingGame class
    
    * Add Car method
    
    add Car method also test code
    
    * Add Race
    
    add race method
    
    * Add View
    
    - add InputView
    - add ResultView
    
    * Make Racig Car Game
    
    - add main method
    - add Racing Game class
    
    * Refactoring Step3
    
    - add README.md
    - change method (Car, Race, *View)
    
    * Revert "Refactoring Step3"
    
    This reverts commit 69963eec575f2cd39c6f4a05ebee48fe87406d12.
    
    * doc: Add README.md
    
    - README.md 추가 (3단계, 4단계)
    
    * refactor: Refactoring Step3 code
    
    - change All class
    
    * feat: Add name property in Car domain
    
    - add name property and valid method
    - add test code for valid method
    
    * feat: Print Car name
    
    * feat: Seperate Car by comma
    
    - add generate Car method by name
    - add test code
    
    * refactor: Change ResultView
    
    - seperate printResult for low coupling
    
    * feat: Add winner logic
    
    - add print winner logic
    - add find winner test code
    - add find winner logic
    
    * refactor: Change ResultView
    
    * refactor: Change ResultView
    
    * refactor: change method
    
    - change ResultView for seperate Race
    - add Rule interface and class
    - add test code
    
    * refactor: movingRule 수정

--- src/main/kotlin/racingcar/service/MovingRule.kt
+++ src/main/kotlin/racingcar/service/MovingRule.kt
@@ -4,8 +4,6 @@ interface MovingRule {
     fun isMoving(condition: Int = RANDOM_CONDITION_NUM): Boolean
 
     companion object {
-        private const val RANDOM_CONDITION_NUM = -1
+        private const val RANDOM_CONDITION_NUM = 4
     }
 }
-
-
