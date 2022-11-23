cceb01df2d54d9b1e5fb7bf26262ef91e9a1a743
yongju <odetsuo@gmail.com>
2022-11-23 10:09:19 +0900

    feat: 자동차 경주 게임 실행

--- src/main/kotlin/racing/RacingApplication.kt
+++ src/main/kotlin/racing/RacingApplication.kt
@@ -1,6 +1,7 @@
 package racing
 
 import racing.ui.InputView
+import racing.ui.ResultView
 
 fun main() {
     RacingApplication.run()
@@ -11,5 +12,12 @@ object RacingApplication {
     fun run() {
         val numberOfCars = InputView.inputNumberOfCars()
         val countOfTry = InputView.inputCountOfTry()
+        val racingGame = RacingGame(numberOfCars, countOfTry)
+
+        ResultView.printResult()
+        while (racingGame.isRacing()) {
+            racingGame.race()
+            ResultView.print(racingGame.positions())
+        }
     }
 }
---
+++ src/main/kotlin/racing/RacingGame.kt
@@ -0,0 +1,23 @@
+package racing
+
+import racing.domain.Cars
+import racing.domain.Movable
+import racing.domain.RandomMove
+
+class RacingGame(
+    private val numberOfCars: Int = 2,
+    private var countOfTry: Int = 1,
+    private val movable: Movable = RandomMove()
+) {
+
+    private val cars: Cars = Cars(numberOfCars)
+
+    fun race() = run {
+        cars.move(movable)
+        countOfTry--
+    }
+
+    fun isRacing() = countOfTry > 0
+
+    fun positions() = cars.positions()
+}
