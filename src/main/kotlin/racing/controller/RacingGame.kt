fb54b5609f2367a07fb7ae3c61847c981774fb8b
shinseongsu <tlstjdtn321@naver.com>
2022-11-24 08:51:15 +0900

    refactor: controller로 view 패키지가 의존되도록 수정

--- src/main/kotlin/racing/controller/RacingGame.kt
+++ src/main/kotlin/racing/controller/RacingGame.kt
@@ -1,19 +1,19 @@
-package racing.application
+package racing.controller
 
 import racing.domain.Car
-import racing.domain.CarName
 import racing.domain.Cars
 import racing.domain.RandomStrategy
-import racing.ui.OutputView.printCarNameAndPosition
-import racing.ui.OutputView.printTitle
-import racing.ui.OutputView.printWinnerCars
+import racing.view.InputView
+import racing.view.OutputView.printCarNameAndPosition
+import racing.view.OutputView.printTitle
+import racing.view.OutputView.printWinnerCars
 
-class RacingGame(
-    val tryCount: Int,
-    val carNames: List<CarName>
-) {
+class RacingGame{
 
     fun runRace() {
+        val carNames = InputView.getCarNames()
+        val tryCount = InputView.getTryCount()
+
         val cars = Cars(carNames.map { Car(it) }, RandomStrategy())
         printTitle()
 
