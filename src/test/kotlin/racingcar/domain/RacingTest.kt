ba788969aea326b7ced45997dd3bd7123718b4a2
김찬수 <cskim96@crscube.co.kr>
2022-11-24 16:18:55 +0900

    :recycle: test(domain)

--- src/test/kotlin/racingcar/domain/RacingTest.kt
+++ src/test/kotlin/racingcar/domain/RacingTest.kt
@@ -20,4 +20,19 @@ class RacingTest {
                 )
             )
     }
+
+    @Test
+    fun `레이싱 게임 시작에서 , 우승자를 선출합니다`() {
+        val racing = Racing(listOf("pobi", "crong", "kcs")) { true }
+        racing.play(1)
+
+        val winners = racing.findWinners()
+
+        assertThat(winners)
+            .containsExactly(
+                Car(Position(1), CarName("pobi")),
+                Car(Position(1), CarName("crong")),
+                Car(Position(1), CarName("kcs"))
+            )
+    }
 }
