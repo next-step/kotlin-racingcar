919d910c3eb0ce3e0b40128c0533187bdac29baf
Minkyung <mminkyung.k@gmail.com>
2022-11-24 19:23:21 +0900

    refactor: 타입 변경

--- src/main/kotlin/racingcar/view/PromptService.kt
+++ src/main/kotlin/racingcar/view/PromptService.kt
@@ -1,9 +1,10 @@
 package racingcar.view
 
 import racingcar.domain.car.CarName
+import racingcar.domain.car.CarRacingResult
 
 interface PromptService {
     fun getNameOfCars(assignedNames: String? = null): Set<CarName>
     fun getNumberOfMoves(assignedNumber: Int? = null): Int
-    fun showResult(result: List<Map<String, Int>>, winners: Set<CarName>)
+    fun showResult(result: List<CarRacingResult>, winners: Set<CarName>)
 }
