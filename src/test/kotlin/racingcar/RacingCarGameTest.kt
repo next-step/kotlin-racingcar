925e158fd580d17076820e61f439ea9d423de7e9
Yujin Park <uiuiui118437@gmail.com>
2022-11-26 00:49:31 +0900

    feat: RacingCarGame은 게임 진행에 대한 테스트를 할 수 있도록 수정

--- src/test/kotlin/racingcar/RacingCarGameTest.kt
+++ src/test/kotlin/racingcar/RacingCarGameTest.kt
@@ -2,27 +2,24 @@ package racingcar
 
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
-import racingcar.domain.Car
 import racingcar.domain.RacingCarGame
+import racingcar.domain.model.CarInfo
+import racingcar.domain.model.GameInfo
 
 class RacingCarGameTest : BehaviorSpec({
 
-    val racingCarGame = RacingCarGame()
-    val car1 = Car(name = "pobi", distance = 3)
-    val car2 = Car(name = "crong", distance = 5)
-    val car3 = Car(name = "honux", distance = 5)
-    val cars = listOf(car1, car2, car3)
-
-    Given("자동차 경주 게임이 끝나") {
-        When("최댓값을 구해 우승자를 정하면") {
-            val result = racingCarGame.pickWinner(
-                max = cars.maxOf { car ->
-                    car.distance
-                },
-                cars = cars
-            )
-            Then("최댓값과 같은 거리 값을 가진 자동차가 우승한다.") {
-                result shouldBe listOf(car2, car3)
+    Given("3개의 자동차 정보를 받고") {
+        val gameInfo = GameInfo(
+            carInfo = CarInfo(
+                names = listOf("pobi", "crong", "honux"),
+                count = 3
+            ),
+            trialCount = 3
+        )
+        When("게임을 시작하면") {
+            val game = RacingCarGame(gameInfo)
+            Then("3개의 자동차가 생성된다.") {
+                game.cars.size shouldBe 3
             }
         }
     }
