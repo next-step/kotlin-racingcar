44b6882b1f4c914b76ec8ae10d100f81a6209b02
seongbeen.kim <seongbeen.kim@linecorp.com>
2022-11-26 19:48:16 +0900

    refactor: Put constants in companion object

--- src/main/kotlin/racingcar/domain/MoveStrategy.kt
+++ src/main/kotlin/racingcar/domain/MoveStrategy.kt
@@ -6,9 +6,11 @@ fun interface MoveStrategy {
     fun canMove(): Boolean
 }
 
-private const val MAX_RANDOM_THRESHOLD = 10
-private const val MIN_MOVE_CONDITION = 4
-
 class RandomStrategy : MoveStrategy {
     override fun canMove(): Boolean = Random.nextInt(MAX_RANDOM_THRESHOLD) >= MIN_MOVE_CONDITION
+
+    companion object {
+        private const val MAX_RANDOM_THRESHOLD = 10
+        private const val MIN_MOVE_CONDITION = 4
+    }
 }
