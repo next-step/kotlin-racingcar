ab1d55dc13940e63ab426a02b5955e68c2340141
seongbeen.kim <seongbeen.kim@linecorp.com>
2022-11-27 08:55:55 +0900

    refactor: Implement Comparable for position

--- src/main/kotlin/racingcar/vo/Position.kt
+++ src/main/kotlin/racingcar/vo/Position.kt
@@ -2,7 +2,7 @@ package racingcar.vo
 
 import racingcar.domain.MoveStrategy
 
-data class Position(val position: Int = 0) {
+data class Position(val position: Int = 0) : Comparable<Position> {
     init {
         require(position >= MIN_THRESHOLD) { "The minimum value for position should be greater than $MIN_THRESHOLD" }
     }
@@ -18,4 +18,11 @@ data class Position(val position: Int = 0) {
     companion object {
         private const val MIN_THRESHOLD = 0
     }
+
+    override fun compareTo(other: Position): Int =
+        when {
+            this.position > other.position -> 1
+            this.position < other.position -> -1
+            else -> 0
+        }
 }
