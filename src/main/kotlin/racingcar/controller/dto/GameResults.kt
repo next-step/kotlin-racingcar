530ca7650f03b09fc9082480e73ac0948bd0fdfd
seongbeen.kim <seongbeen.kim@linecorp.com>
2022-11-26 19:45:50 +0900

    refactor: Remove toList()

--- src/main/kotlin/racingcar/controller/dto/GameResults.kt
+++ src/main/kotlin/racingcar/controller/dto/GameResults.kt
@@ -5,6 +5,6 @@ import racingcar.domain.Car
 data class GameResults private constructor(val carPositions: List<Int>) {
 
     companion object {
-        fun of(cars: List<Car>) = GameResults(cars.map { it.position.position }.toList())
+        fun of(cars: List<Car>) = GameResults(cars.map { it.position.position })
     }
 }
