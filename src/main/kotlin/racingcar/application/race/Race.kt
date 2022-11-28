1858b11d130b66d40c8da01da1140792af48b0c1
EP <kjuioqq8@gmail.com>
2022-11-29 00:06:42 +0900

    refactor: race 로직 변경

--- src/main/kotlin/racingcar/application/race/Race.kt
+++ src/main/kotlin/racingcar/application/race/Race.kt
@@ -1,45 +1,34 @@
 package racingcar.application.race
 
-import racingcar.application.model.RaceInputCommand
-import racingcar.application.race.event.model.CarData
-import racingcar.application.race.event.model.EventState
-import racingcar.application.race.event.model.RoundEventCarData
-import racingcar.application.race.event.model.WinEventCarData
-import racingcar.application.race.event.publisher.EventPublisher
 import racingcar.application.race.factory.CarFactory
 import racingcar.domain.car.ParticipatingCars
 
 class Race(
-    private val carFactory: CarFactory,
-    private val eventPublisher: EventPublisher
+    private val carFactory: CarFactory
 ) {
 
-    fun proceed(raceInputCommand: RaceInputCommand) {
-        val attemptCount = raceInputCommand.attemptCount
-        val participatingCars = carFactory.createByCarNames(carNames = raceInputCommand.carNames)
-        race(participatingCars, attemptCount)
+    fun getParticipatingCars(carNames: List<String>): ParticipatingCars {
+        return carFactory.createByCarNames(carNames = carNames)
     }
 
-    private fun race(participatingCars: ParticipatingCars, attemptCount: Int) {
-        eventPublisher.roundEvent().publish(participatingCars.currentEventData(EventState.INIT))
-        repeat(attemptCount) {
-            participatingCars.move()
-            eventPublisher.roundEvent().publish(participatingCars.currentEventData(EventState.PROCESS))
-        }
-        eventPublisher.winEvent().publish(participatingCars.announceWinners())
+    fun currentRace(participatingCars: ParticipatingCars): RoundCarData {
+        return participatingCars.currentData()
     }
 
-    private fun ParticipatingCars.currentEventData(eventState: EventState): RoundEventCarData {
-        return RoundEventCarData(
+    fun win(participatingCars: ParticipatingCars): WinCarData {
+        return participatingCars.announceWinners()
+    }
+
+    private fun ParticipatingCars.currentData(): RoundCarData {
+        return RoundCarData(
             this.currentPosition().map {
                 CarData(name = it.first, position = it.second)
-            },
-            eventState = eventState
+            }
         )
     }
 
-    private fun ParticipatingCars.announceWinners(): WinEventCarData {
-        return WinEventCarData(
+    private fun ParticipatingCars.announceWinners(): WinCarData {
+        return WinCarData(
             this.winners().map {
                 CarData(name = it.first, position = it.second)
             }
