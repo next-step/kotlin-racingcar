e97ab4b798f26ee431c3afd96088c2c0236439a7
wisotge <rnalstn0507@gmail.com>
2022-11-28 20:48:08 +0900

    fix: 잘못 구조회된 service 패키지 제거, object 클래스를 domain 하위로 이동

--- src/test/kotlin/racingcar/domain/RandomNumberGeneratorTest.kt
+++ src/test/kotlin/racingcar/domain/RandomNumberGeneratorTest.kt
@@ -1,10 +1,10 @@
-package racingcar.service
+package racingcar.domain
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.ints.shouldBeInRange
 
-class RandomNumberGenerateServiceTest : FunSpec({
+class RandomNumberGeneratorTest : FunSpec({
     test("getRandomNumber 함수는 0에서 9사이의 랜덤한 정수를 반환해야 한다.") {
-        RandomNumberGenerateService.getRandomNumber() shouldBeInRange 0..9
+        RandomNumberGenerator.getRandomNumber() shouldBeInRange 0..9
     }
 })
