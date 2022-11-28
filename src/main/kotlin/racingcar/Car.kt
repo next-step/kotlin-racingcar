dbfd8bcb9cb95cdcab600084a7b027f4adb7685a
이상인/wefu0000 <wefu.child@gmail.com>
2022-11-28 18:08:03 +0900

    refactor: Car 생성자 public 변경

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,6 +1,6 @@
 package racingcar
 
-class Car private constructor(private var moveCount: Int = 0, val name: String) {
+class Car(private var moveCount: Int = 0, val name: String) {
     constructor(name: String) : this(0, name) {
         require(name.length <= 5) { "name length 5 limited" }
     }
