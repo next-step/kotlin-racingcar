53d376b03918a214ecf43443e7874362fd553a38
이상인/wefu0000 <wefu.child@gmail.com>
2022-11-28 18:09:24 +0900

    refactor: 상수 사용하는 곳으로 이동

--- src/main/kotlin/racingcar/ForwardStrategy.kt
+++ src/main/kotlin/racingcar/ForwardStrategy.kt
@@ -6,11 +6,9 @@ sealed class ForwardStrategy {
     abstract fun forward(): Boolean
 
     object DefaultForward : ForwardStrategy() {
-        override fun forward(): Boolean = SecureRandom.getInstanceStrong().nextInt(MAXIMUM_BOUND) >= MINIMUM_BOUND
-    }
-
-    companion object {
         private const val MINIMUM_BOUND = 4
         private const val MAXIMUM_BOUND = 10
+
+        override fun forward(): Boolean = SecureRandom.getInstanceStrong().nextInt(MAXIMUM_BOUND) >= MINIMUM_BOUND
     }
 }
