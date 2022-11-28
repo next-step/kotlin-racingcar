e867a9bdd90c8e2c83913ad57a20901d238090c9
seongbeen.kim <seongbeen.kim@linecorp.com>
2022-11-28 20:15:34 +0900

    refactor: Add reference type for readability

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -4,8 +4,8 @@ import racingcar.domain.vo.Name
 import racingcar.domain.vo.Position
 
 class Car(name: String, position: Position = Position()) {
-    val name = Name(name)
-    var position = position
+    val name: Name = Name(name)
+    var position: Position = position
         private set
 
     constructor(name: String, position: Int) : this(name, Position(position))
