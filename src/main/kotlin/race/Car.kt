fab19fe838e0c23d2afbaa42d4bf517142220bdd
hojihun5516 <hojihun5516@daum.net>
2022-12-02 07:37:17 +0900

    refactor: Car init
    * Car init 추가
    * copy 함수 추가

--- src/main/kotlin/race/Car.kt
+++ src/main/kotlin/race/Car.kt
@@ -1,12 +1,22 @@
 package race
 
-class Car(val name: String) {
-    fun move(randomNumber: Int): Boolean {
-        return randomNumber >= THRESHOLD
+class Car(val name: String, var movements: MutableList<Boolean> = mutableListOf()) {
+
+    fun move(condition: Int) {
+        movements.add(condition >= THRESHOLD)
+    }
+
+    fun copy(): Car {
+        val target = movements.toList()
+        return Car(name, target.toMutableList())
+    }
+
+    fun getMovedCount(): Int {
+        return this.movements.count { it == true }
     }
 
     init {
-        require(name.length > CAR_NAME_LENGTH_BY_RULE) { "차의 이름 길이는 $CAR_NAME_LENGTH_BY_RULE 글자를 넘으면 안됩니다" }
+        require(name.length < CAR_NAME_LENGTH_BY_RULE) { "차의 이름 길이는 $CAR_NAME_LENGTH_BY_RULE 글자를 넘으면 안됩니다" }
     }
 
     companion object {
