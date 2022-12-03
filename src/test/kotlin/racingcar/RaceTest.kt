560e984e6dc36ad6626d49cf89f34560040fdc43
Yujin Park <uiuiui118437@gmail.com>
2022-12-03 20:59:05 +0900

    feat: 차 리스트를 가지고 게임을 진행하는 레이스 클래스 생성

---
+++ src/test/kotlin/racingcar/RaceTest.kt
@@ -0,0 +1,17 @@
+package racingcar
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import racingcar.domain.Race
+
+class RaceTest : BehaviorSpec({
+    Given("3개의 자동차 이름으로") {
+        val names = listOf("pobi", "crong", "honux")
+        When("레이스가 생성되면") {
+            val race = Race.create(names)
+            Then("3개의 자동차가 생성된다.") {
+                race.cars.size shouldBe 3
+            }
+        }
+    }
+})
