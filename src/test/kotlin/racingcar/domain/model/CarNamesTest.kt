0f862628ca98634c3fb3957b310a192f9bee977e
Jaewoo Hwang <dreamboy1339@gmail.com>
2022-12-13 01:16:51 +0900

    study(step5): CarNamesTest 테스트 코드 추가

---
+++ src/test/kotlin/racingcar/domain/model/CarNamesTest.kt
@@ -0,0 +1,29 @@
+package racingcar.domain.model
+
+import io.kotest.matchers.collections.shouldContainInOrder
+import org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class CarNamesTest {
+
+    @DisplayName("자동차 이름이 있어야 합니다")
+    @Test
+    fun createByEmptyList() {
+        assertThatExceptionOfType(IllegalArgumentException::class.java)
+            .isThrownBy {
+                CarNames()
+            }
+            .withMessage("자동차 이름이 있어야 합니다")
+    }
+
+    @DisplayName("자동차 이름으로 자동차를 생성합니다")
+    @Test
+    fun createCar() {
+        val carNames = CarNames(listOf("aaa", "bbb", "ccc"))
+
+        val cars = carNames.makeCars()
+
+        cars.names() shouldContainInOrder listOf("aaa", "bbb", "ccc")
+    }
+}
