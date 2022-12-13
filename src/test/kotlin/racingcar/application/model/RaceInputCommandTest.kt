8003bf91343a36d0da32ff01c05da7b74d0c1343
EP <kjuioqq8@gmail.com>
2022-12-13 21:12:11 +0900

    refactor: 테스트 코드 변경

--- src/test/kotlin/racingcar/application/model/RaceInputCommandTest.kt
+++ src/test/kotlin/racingcar/application/model/RaceInputCommandTest.kt
@@ -5,10 +5,10 @@ import io.kotest.core.spec.style.StringSpec
 
 class RaceInputCommandTest : StringSpec({
     "레이스에게 커맨드를 전달하는 객체의 자동차 개수는 0보다 커야 한다." {
-        shouldThrow<IllegalArgumentException> { RaceInputCommand(carNames = listOf(), attemptCount = 1) }
+        shouldThrow<IllegalArgumentException> { RaceInputCommand(carNames = listOf(), attemptCount = 1, carCount = 0) }
     }
 
     "레이스에게 커맨드를 전달하는 객체의 시도 횟수는 0보다 커야 한다." {
-        shouldThrow<IllegalArgumentException> { RaceInputCommand(carNames = listOf("ep", "nak"), attemptCount = -1) }
+        shouldThrow<IllegalArgumentException> { RaceInputCommand(carNames = listOf("ep", "nak"), attemptCount = -1, carCount = 2) }
     }
 })
