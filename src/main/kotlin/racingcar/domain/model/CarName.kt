992a2af4ebf12fa87dc174bc212009b451c65b0b
Jaewoo Hwang <dreamboy1339@gmail.com>
2022-12-14 23:39:55 +0900

    study(step5): CarName 을 data class 로 변경

--- src/main/kotlin/racingcar/domain/model/CarName.kt
+++ src/main/kotlin/racingcar/domain/model/CarName.kt
@@ -1,6 +1,6 @@
 package racingcar.domain.model
 
-class CarName(val name: String) {
+data class CarName(val name: String) {
 
     init {
         require(name.length <= 5) { "자동차 이름은 5자를 초과할 수 없습니다." }
