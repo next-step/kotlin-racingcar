45cdd8bd93680b704df61ce07c3ae47e1117f1b4
dldudwns72 <dldudwns72@gmail.com>
2022-12-18 20:37:31 +0900

    refactor : 숫자 상수화 리팩토링

--- src/main/kotlin/carracing/domain/Cars.kt
+++ src/main/kotlin/carracing/domain/Cars.kt
@@ -3,6 +3,10 @@ package carracing.domain
 class Cars(cars: List<Car>) : List<Car> by cars {
 
     init {
-        require(cars.size > 1) { "최소 2대 이상의 자동차를 입력해주세요." }
+        require(cars.size > MIN_CAR_SIZE) { "최소 2대 이상의 자동차를 입력해주세요." }
+    }
+
+    companion object {
+        private const val MIN_CAR_SIZE = 1
     }
 }
