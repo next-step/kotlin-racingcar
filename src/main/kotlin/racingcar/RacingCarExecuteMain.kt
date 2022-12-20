bb5d5571c2e602f0f971ef82be7039f0b72242e8
이상인/wefu0000 <wefu.child@gmail.com>
2022-12-21 00:42:51 +0900

    refactor: 코드 정리

--- src/main/kotlin/racingcar/RacingCarExecuteMain.kt
+++ src/main/kotlin/racingcar/RacingCarExecuteMain.kt
@@ -6,25 +6,7 @@ import racingcar.view.ResultView
 fun main() {
     val inputView = InputView()
     val resultView = ResultView()
-    //
-    // val inputCarNames = inputView.inputCarNames()
-    // val tryCount = inputView.inputTryCount()
-    // val controller = Controller(inputCarNames, tryCount)
-    //
-    // controller.startRacing()
-    // val carNames = controller.getCarNames()
-    //
-    // for (i in 0 until tryCount) {
-    //     carNames.forEach { carName ->
-    //         resultView.printCarName(carName)
-    //         resultView.printCarMove(controller.getMoveInfos(carName)[i])
-    //     }
-    //
-    //     resultView.printLineSpacing()
-    // }
-    //
-    // resultView.printWinningCars(controller.getWinningCarNames())
-
     val controller = Controller(inputView, resultView)
+
     controller.startRacing(controller.getCarNames(), controller.getTryCount())
 }
