e5624ed3f8822c6ad5af790dbe9b3992dac532b5
hojihun5516 <hojihun5516@daum.net>
2022-12-25 17:42:10 +0900

    Input 값을 object로 변경

--- src/main/kotlin/race/Application.kt
+++ src/main/kotlin/race/Application.kt
@@ -1,18 +1,18 @@
 package race
 
-import race.view.Input
+import race.view.Input.getCarNames
+import race.view.Input.getRaceCount
 import race.view.Message.GET_CARS_NAME_MESSAGE
 import race.view.Message.GET_RACE_COUNT_MESSAGE
 import race.view.Output.printResult
 import race.view.Output.printWinner
 
 private val randomNumberGenerator = RandomNumberGenerator()
-private val inputView = Input()
 
 fun main() {
-    val inputCarNames = inputView.getCarNames(GET_CARS_NAME_MESSAGE)
+    val inputCarNames = getCarNames(GET_CARS_NAME_MESSAGE)
     val carNames = inputCarNames.split(",")
-    val raceCount = inputView.getRaceCount(GET_RACE_COUNT_MESSAGE)
+    val raceCount = getRaceCount(GET_RACE_COUNT_MESSAGE)
 
     val race = Race(randomNumberGenerator)
     val record = race.start(carNames, raceCount)
