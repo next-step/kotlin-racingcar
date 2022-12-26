8aa3b97302a78821c74d1056f89a9877938f6d9a
hand <sns2831@gmail.com>
2022-12-27 01:14:55 +0900

    feat: gradle 설정 변경.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.6.21"
     id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
@@ -17,6 +19,7 @@ dependencies {
     testImplementation("org.assertj", "assertj-core", "3.20.2")
 
     testImplementation("io.kotest", "kotest-runner-junit5", "5.2.3")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks {
@@ -32,4 +35,12 @@ tasks {
     ktlint {
         verbose.set(true)
     }
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
