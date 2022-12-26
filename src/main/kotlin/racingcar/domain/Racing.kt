6a5adb165b7803ebb1dc2b5ba6c08ad7a7f9033c
cafemug <hyanghope@gmail.com>
2022-12-26 13:53:21 +0900

    fix: 매직넘버 추가와 move함수 분리

--- src/main/kotlin/racingcar/domain/Racing.kt
+++ src/main/kotlin/racingcar/domain/Racing.kt
@@ -2,21 +2,18 @@ package racingcar.domain
 
 import racingcar.entity.Car
 
+private const val MOVING_NUMBER = 4
 class Racing {
     fun makeCars(num: Int): List<Car> {
-        return (1..num).map { Car("-") }
+        return (1..num).map { Car() }
     }
 
     fun moveCars(cars: List<Car>) {
         cars.asSequence().forEach {
-            if (randomNum() > 4) {
-                move(it)
+            if (randomNum() >= MOVING_NUMBER) {
+                it.move(1)
             }
         }
-    }
-
-    fun move(car: Car) {
-        car.distance = car.distance + "-"
     }
 
     private fun randomNum(): Int {
--- src/main/kotlin/racingcar/entity/Car.kt
+++ src/main/kotlin/racingcar/entity/Car.kt
@@ -1,3 +1,10 @@
 package racingcar.entity
 
-data class Car(var distance: String)
+private const val ROAD = "-"
+data class Car(var _distance: String = ROAD) {
+    var distance = _distance
+        private set
+    fun move(distance: Int) {
+        this.distance = this.distance + ROAD.repeat(distance)
+    }
+}
