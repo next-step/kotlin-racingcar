dbe31e7b8ff32aee999304c97b5bcb0d39375236
seyun(Marco) <ksy90101@gmail.com>
2022-12-31 13:40:25 +0900

    refactor: println으로 변경

--- src/main/kotlin/view/racing/InputView.kt
+++ src/main/kotlin/view/racing/InputView.kt
@@ -3,12 +3,12 @@ package view.racing
 class InputView private constructor() {
     companion object {
         fun carAmountInput(): Int {
-            print("자동차 대수는 몇 대인가요?" + System.lineSeparator())
+            println("자동차 대수는 몇 대인가요?")
             return readln().toInt()
         }
 
         fun trialInput(): Int {
-            print("시도할 횟수는 몇 회인가요?" + System.lineSeparator())
+            println("시도할 횟수는 몇 회인가요?")
             return readln().toInt()
         }
     }
