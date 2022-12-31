b9e443b69bf8264652c09cc4643790ffc4c925db
seyun(Marco) <ksy90101@gmail.com>
2022-12-31 13:39:30 +0900

    refactor: position data class로 변경

--- src/main/kotlin/domain/racing/Position.kt
+++ src/main/kotlin/domain/racing/Position.kt
@@ -1,24 +1,9 @@
 package domain.racing
 
-class Position(var position: Int) {
+data class Position(var position: Int) {
 
     fun move() {
         position++
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Position
-
-        if (position != other.position) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return position
     }
 
     companion object {
