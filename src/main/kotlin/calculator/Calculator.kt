618a0b0142e2ed20371e775df75492e8d874b497
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-06-07 02:02:10 +0900

    refactor: 수식형태검증 분리

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -5,8 +5,12 @@ fun calculate(input: String): Int {
         throw IllegalArgumentException("공백은 계산할 수 없다.")
     }
     val values = input.split(" ")
+    validateCalculateFormat(values)
+    return 0
+}
+
+private fun validateCalculateFormat(values: List<String>) {
     if (values.size % 2 == 0) {
         throw IllegalArgumentException("올바른 형태로 수식을 입력하여야 한다.")
     }
-    return 0
 }
