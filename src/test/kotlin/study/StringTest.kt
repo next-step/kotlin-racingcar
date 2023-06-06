1928441e450e644b4e065dc8b08aa82e0e5049cb
DongSeongSeo <sds901234@naver.com>
2023-06-06 22:13:28 +0900

    1단계 - 코틀린 기초 (#981)
    * feat: 온라인강의 실습
    
    * study: learn kotlin basics
    
    ---------
    
    Co-authored-by: 동성 서 <sds901234@tradlinx.com>

---
+++ src/test/kotlin/study/StringTest.kt
@@ -0,0 +1,12 @@
+package study
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+class StringTest {
+    @Test
+    fun isEmpty() {
+        assertThat("".isEmpty()).isTrue
+        assertThat(" ".isEmpty()).isFalse
+        assertThat("a".isEmpty()).isFalse
+    }
+}
