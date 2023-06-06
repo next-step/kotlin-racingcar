de3f477976b7a5f0a373a5089ef9ba148a13eb12
nsun9505 <nsun9505@naver.com>
2023-06-06 23:36:10 +0900

    study: learn basic kotlin (#993)

---
+++ src/test/kotlin/study/PersonTest.kt
@@ -0,0 +1,33 @@
+package study
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class PersonTest {
+    @Test
+    fun `이름 붙인 인자`() {
+        val actual = Person(name = "남상윤", nickname = "Nam", age = 20)
+        assertThat(actual.name).isEqualTo("남상윤")
+        assertThat(actual.nickname).isEqualTo("Nam")
+        assertThat(actual.age).isEqualTo(20)
+    }
+
+    @Test
+    fun `널 타입`() {
+        val actual = Person("홍길동", 20, null)
+        assertThat(actual.nickname).isNull()
+    }
+
+    @Test
+    fun `기본 인자`() {
+        val actual = Person(name = "홍길동", age = 20)
+        assertThat(actual.nickname).isEqualTo("Guest")
+    }
+
+    @Test
+    fun `데이터 클래스`() {
+        val person1 = Person("남상윤", 20, "Nam")
+        val person2 = Person("남상윤", 20, "Nam")
+        assertThat(person1).isEqualTo(person2)
+    }
+}
