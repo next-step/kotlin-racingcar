035b80143df17bcc4472e9a2f666b6e8fec428f5
1002533 <bf.winfive@sk.com>
2023-06-06 22:07:26 +0900

    step1 : kotlin test

--- src/test/kotlin/study/PersonTest.kt
+++ src/test/kotlin/study/PersonTest.kt
@@ -1,4 +1,33 @@
 package study
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
 class PersonTest {
+    @Test
+    fun `이름 붙인 인자`() {
+        val actual = Person(name = "윤성현", age = 20, nickname = "minhyukseul")
+        assertThat(actual.name).isEqualTo("윤성현")
+        assertThat(actual.nickname).isEqualTo("minhyukseul")
+        assertThat(actual.age).isEqualTo(20)
+    }
+
+    @Test
+    fun `널 타입`() {
+        val actual = Person("윤성현", 20, null)
+        assertThat(actual.nickname).isNull()
+    }
+
+    @Test
+    fun `기본 인자`() {
+        val actual = Person(name = "윤성현", age = 20)
+        assertThat(actual.nickname).isEqualTo("Guest")
+    }
+
+    @Test
+    fun `데이터 클래스`() {
+        val person1 = Person("홍길동", 20, "Hong")
+        val person2 = Person("홍길동", 20, "Hong")
+        assertThat(person1).isEqualTo(person2)
+    }
 }
