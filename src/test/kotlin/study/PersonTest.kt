5f85018a8f591e619b98d4813f3913aae49a207f
yunbaek <yunbaek4564@gmail.com>
2023-06-06 23:08:22 +0900

    test: add Kotlin basic class test

---
+++ src/test/kotlin/study/PersonTest.kt
@@ -0,0 +1,26 @@
+package study
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class PersonTest {
+    @Test
+    fun `이름 붙인 인자`() {
+        val actual = Person(name = "홍길동", age = 20, nickname = "Hong")
+        assertThat(actual.name).isEqualTo("홍길동")
+        assertThat(actual.nickname).isEqualTo("Hong")
+        assertThat(actual.age).isEqualTo(20)
+    }
+
+    @Test
+    fun `널 타입`() {
+        val actual = Person("홍길동", 20, null)
+        assertThat(actual.nickname).isNull()
+    }
+
+    @Test
+    fun `기본 인자`() {
+        val actual = Person(name = "홍길동", age = 20)
+        assertThat(actual.nickname).isEqualTo("Guest")
+    }
+}
