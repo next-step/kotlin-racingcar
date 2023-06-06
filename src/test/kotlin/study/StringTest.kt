9d517f7d4a0bc2b7fa772b9ff85e48a62b335491
genos-lee <genos.lee@kakaocorp.com>
2023-06-06 22:02:29 +0900

    study: learn kotlin basics

--- src/test/kotlin/study/StringTest.kt
+++ src/test/kotlin/study/StringTest.kt
@@ -1,3 +1,13 @@
 package study
 
-class StringTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class StringTest {
+    @Test
+    fun isEmpty() {
+        assertThat("".isEmpty()).isTrue()
+        assertThat(" ".isEmpty()).isFalse()
+        assertThat("a".isEmpty()).isFalse()
+    }
+}
