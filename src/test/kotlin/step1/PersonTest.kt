25efd2f5f50c38a17bbb23589501b9d58dda0bdc
jeff.42(박제헌) <jeff.42@kakaocorp.com>
2023-06-06 21:12:44 +0900

    코틀린 기초: 기본 뼈대에 테스트 코드를 만족하는 코드 작성

--- src/test/kotlin/step1/PersonTest.kt
+++ src/test/kotlin/step1/PersonTest.kt
@@ -8,22 +8,22 @@ internal class PersonTest {
     @Test
     fun `named arguments`() {
         val people = listOf(
-            Person("박재성", 29, "제이슨"),
-            Person("박재성", 29, nickname = "제이슨"),
-            Person(name = "박재성", nickname = "제이슨", age = 29),
+            Person("박제헌", 31, "jeff.42"),
+            Person("박제헌", 31, nickname = "jeff.42"),
+            Person(name = "박제헌", nickname = "jeff.42", age = 31),
         )
         assertThat(people).allSatisfy {
-            assertThat(it.name).isEqualTo("박재성")
+            assertThat(it.name).isEqualTo("박제헌")
             assertThat(it.age).isEqualTo(29)
-            assertThat(it.nickname).isEqualTo("제이슨")
+            assertThat(it.nickname).isEqualTo("jeff.42")
         }
     }
 
     @Test
     fun `default arguments`() {
-        val person = Person("박재성")
+        val person = Person("박제헌")
         assertAll(
-            { assertThat(person.name).isEqualTo("박재성") },
+            { assertThat(person.name).isEqualTo("박제헌") },
             { assertThat(person.age).isNull() },
             { assertThat(person.nickname).isNull() },
         )
@@ -31,8 +31,8 @@ internal class PersonTest {
 
     @Test
     fun `data classes`() {
-        val person1 = Person("박재성", 29, "제이슨")
-        val person2 = Person("박재성", 29, "제이슨")
+        val person1 = Person("박제헌", 32, "jeff.42")
+        val person2 = Person("박제헌", 32, "jeff.42")
         assertThat(person1).isEqualTo(person2)
     }
 }
