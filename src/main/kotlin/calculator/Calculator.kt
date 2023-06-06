33ebc40834ccc045e2142b0e154af087bbcbc463
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-06-07 01:59:24 +0900

    feat: 연산의 형태가 짝수인 경우 예외 추가
    - "1 +"가 아닌 연산 input 값은 항상 홀수개여야 한다.

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -4,5 +4,9 @@ fun calculate(input: String): Int {
     if (input.isBlank()) {
         throw IllegalArgumentException("공백은 계산할 수 없다.")
     }
+    val values = input.split(" ")
+    if (values.size % 2 == 0) {
+        throw IllegalArgumentException("올바른 형태로 수식을 입력하여야 한다.")
+    }
     return 0
 }
--- src/test/kotlin/calculator/CalculatorKtTest.kt
+++ src/test/kotlin/calculator/CalculatorKtTest.kt
@@ -2,6 +2,7 @@ package calculator
 
 import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EmptySource
 
@@ -12,5 +13,13 @@ class CalculatorKtTest {
     fun `공백이 입력되는 경우 예외가 발생한다`(input: String) {
         val exception = shouldThrowExactly<IllegalArgumentException> { calculate(input) }
         exception.message shouldBe "공백은 계산할 수 없다."
+    }
+
+    @Test
+    fun `숫자와 연산자의 조합으로 된 올바른 수식의 형태로 입력되지 않는 경우 예외가 발생한다`() {
+        val input = "1 +"
+
+        val exception = shouldThrowExactly<IllegalArgumentException> { calculate(input) }
+        exception.message shouldBe "올바른 형태로 수식을 입력하여야 한다."
     }
 }
