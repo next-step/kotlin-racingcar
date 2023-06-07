045bfd33282298d0b067fc3d91350c52a2432cd5
genos-lee <genos.lee@kakaocorp.com>
2023-06-08 02:02:37 +0900

    test, refactor : ExpressionsTest kotest로 수정하고 data driven testing 적용

--- src/test/kotlin/next/step/calculator/domain/ExpressionsTest.kt
+++ src/test/kotlin/next/step/calculator/domain/ExpressionsTest.kt
@@ -1,20 +1,28 @@
 package next.step.calculator.domain
 
 import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.datatest.withData
 import io.kotest.matchers.shouldBe
 import next.step.calculator.service.Expressions
-import org.junit.jupiter.api.Test
 
-class ExpressionsTest {
-    @Test
-    fun `expression list에 대해 계산`() {
-        Expressions.evaluate(listOf("2", "+", "3", "*", "4", "/", "2")) shouldBe 10
-    }
+class ExpressionsTest : FunSpec({
+    data class EvaluationExpectation(val expressions: List<String>, val expected: Int)
+    context("evaluate") {
+        withData(
+            nameFn = { "${it.expressions} = ${it.expected}" },
+            EvaluationExpectation(listOf("2", "+", "3"), 5),
+            EvaluationExpectation(listOf("2", "+", "3", "*", "4"), 20),
+            EvaluationExpectation(listOf("2", "/", "1", "*", "4", "+", "2"), 10),
+            EvaluationExpectation(listOf("2", "+", "3", "*", "4", "/", "2", "-", "10"), 0),
+        ) { (expressionList, expected) ->
+            Expressions.evaluate(expressionList) shouldBe expected
+        }
 
-    @Test
-    fun `expression 개수가 모자라면 예외 발생`() {
-        shouldThrow<IllegalArgumentException> {
-            Expressions.evaluate(listOf("2", "+", "3", "*", "4", "/"))
+        test("expression 개수가 모자라면 예외 발생") {
+            shouldThrow<IllegalArgumentException> {
+                Expressions.evaluate(listOf("2", "+", "3", "*", "4", "/"))
+            }
         }
     }
-}
+})
