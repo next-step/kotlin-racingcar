5b645c727cef139d4ad7d9e6124c7b6e6bdaafc6
chad.yoon <chad.yoon@kakaocorp.com>
2023-06-07 23:39:06 +0900

    feat: 사칙 연산 구현
    +, -, *, / 연산자를 지원하는 계산기를 제작 (연산을 불가능하게 하는 예외 케이스 발생 시 에러 발생하도록 구현)
    - 예외 1) 문자열의 수가 3개 미만인 경우 (3개 미만일 경우 최소한의 연산이 불가)
    - 예외 2) 문자열의 수가 짝수일 경우 (문자열의 수가 짝수라는 것은 올바른 수식이 아님을 의미)
    - 예외 3) 숫자 자리에 기호가 존재하는 경우와 기호 자리에 숫자가 존재하는 경우 (이 역시 올바른 수식이 아님을 의미)
    - 예외 4) 숫자 자리에 숫자가 아닌 문자열이 존재하는 경우 (사칙 연산은 숫자로만 가능)
    - 예외 5) 기호 자리에 사칙 연산을 제외한 다른 문자열이 존재하는 경우 (현재의 계산기는 사칙 연산만 지원)

---
+++ src/main/kotlin/ArithmeticCalculator.kt
@@ -0,0 +1,31 @@
+class ArithmeticCalculator {
+
+    @Throws(
+        IllegalArgumentException::class,
+        NumberFormatException::class
+    )
+    fun execute(text: String?): Double {
+        return calculate(
+            text.orEmpty()
+                .split(" ")
+                .filter { it.isNotBlank() }
+        )
+    }
+
+    @Throws(
+        IllegalArgumentException::class,
+        NumberFormatException::class
+    )
+    private fun calculate(input: List<String>): Double {
+        require(input.size >= 3) {
+            "It must consist of at least two numbers and one operator."
+        }
+        require(input.size % 2 == 1) {
+            "The number of input strings must be odd. ex) \"1 + 2\" -> [1], [+], [2]"
+        }
+        return input.drop(1)
+            .chunked(2)
+            .map { (maybeOperator, maybeNumber) -> ArithmeticOperator.of(maybeOperator) to maybeNumber.toDouble() }
+            .fold(input.first().toDouble()) { current, (operator, next) -> operator.execute(current, next) }
+    }
+}
