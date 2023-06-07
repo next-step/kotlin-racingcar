668295011b0bdbdb31bfd643c16e4f56f33ad41c
yunbaek <yunbaek4564@gmail.com>
2023-06-08 07:32:59 +0900

    feat(StringCalculator): 문자열 계산 로직 추가

--- src/main/kotlin/stringcalculator/StringCalculator.kt
+++ src/main/kotlin/stringcalculator/StringCalculator.kt
@@ -1,10 +1,30 @@
 package stringcalculator
 
+private const val OPERATOR_REGEX = "[+\\-*/]"
+
+private const val NUMBER_REGEX = "\\d+"
+
 class StringCalculator {
     companion object {
-        fun calculate(input: String?) {
+        fun calculate(input: String?): Int {
             require(!input.isNullOrBlank())
-            require(input.contains(Regex("[+\\-*/]")))
+            require(input.contains(Regex(OPERATOR_REGEX)))
+
+            val numbers = input.split(Regex(OPERATOR_REGEX)).map { it.toInt() }
+            val operators = input.split(Regex(NUMBER_REGEX)).filter { it.isNotEmpty() }
+
+            require(numbers.size == operators.size + 1)
+
+            var result = numbers[0]
+            for (i in operators.indices) {
+                when (operators[i]) {
+                    "+" -> result += numbers[i + 1]
+                    "-" -> result -= numbers[i + 1]
+                    "*" -> result *= numbers[i + 1]
+                    "/" -> result /= numbers[i + 1]
+                }
+            }
+            return result
         }
     }
 }
