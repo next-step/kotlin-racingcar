91ca4d24dc9e6dbe37c1157b6ef9ec1d903de8ba
tris.2 <tris.2@kakaocorp.com>
2023-06-08 00:44:44 +0900

    feature(step2): 중위 계산기 구현 및 테스트

---
+++ src/main/kotlin/calculator/InfixCalculator.kt
@@ -0,0 +1,66 @@
+package calculator
+
+import calculator.operation.DivideOperation
+import calculator.operation.InfixOperation
+import calculator.operation.MinusOperation
+import calculator.operation.MultiplyOperation
+import calculator.operation.NumberOperation
+import calculator.operation.PlusOperation
+
+class InfixCalculator(private val expression: String?) {
+    init {
+        if (expression == null) {
+            throw IllegalArgumentException("수식은 null 일 수 없습니다.")
+        }
+
+        if (expression.isBlank()) {
+            throw IllegalArgumentException("수식은 공백 일 수 없습니다.")
+        }
+    }
+    fun parse(): InfixOperation {
+        val expressionWithoutSpace = eraseWhiteSpace(expression)
+        validateExpressionFormat(expressionWithoutSpace)
+
+        val operands = expressionWithoutSpace
+            .split("[+\\-*/]".toRegex())
+            .asSequence()
+            .filter { it.isNotBlank() }
+            .toList()
+
+        val operators = expressionWithoutSpace
+            .replace("\\d".toRegex(), "")
+            .asSequence()
+            .map { it.toString() }
+            .toList()
+
+        if (operands.size <= operators.size) throw IllegalArgumentException("연산자가 피연산자 보다 많을 수 없습니다.")
+
+        var result: InfixOperation = NumberOperation(operands[0])
+        for (i in operators.indices) {
+            result = getOperator(operators[i], result, NumberOperation(operands[i + 1]))
+        }
+
+        return result
+    }
+
+    private fun getOperator(
+        input: String,
+        leftOperation: InfixOperation,
+        rightOperation: InfixOperation
+    ): InfixOperation =
+        when (input) {
+            "+" -> PlusOperation(leftOperation, rightOperation)
+            "-" -> MinusOperation(leftOperation, rightOperation)
+            "*" -> MultiplyOperation(leftOperation, rightOperation)
+            "/" -> DivideOperation(leftOperation, rightOperation)
+            else -> throw IllegalArgumentException("지원 하지 않는 연산자입니다.")
+        }
+
+    private fun eraseWhiteSpace(expression: String?) = expression?.replace("\\s".toRegex(), "")!!
+
+    private fun validateExpressionFormat(expression: String) {
+        if (!expression.matches("[\\d+\\-*/]+".toRegex())) {
+            throw IllegalArgumentException("수식은 사칙연산과 숫자만 가능합니다.")
+        }
+    }
+}
---
+++ src/test/kotlin/calculator/InfixCalculatorTest.kt
@@ -0,0 +1,53 @@
+package calculator
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import java.math.BigDecimal
+
+class InfixCalculatorTest {
+    @ParameterizedTest
+    @ValueSource(strings = ["10 # 20", "10 + 20 - 3a", "10 + a - a"])
+    fun `수식에 숫자와 사칙 연산이 아닌 문자가 있을 경우 IllegalArgumentException`(input: String) {
+        Assertions.assertThatThrownBy { InfixCalculator(input).parse() }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("수식은 사칙연산과 숫자만 가능합니다.")
+    }
+
+    @Test
+    fun `연산자가 숫자 수 보다 많거나 같은 경우`() {
+        Assertions.assertThatThrownBy { InfixCalculator("10 / 20 * 30 + 10 - ").parse() }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("연산자가 피연산자 보다 많을 수 없습니다.")
+    }
+
+    @Test
+    fun `숫자가 연산자 보다 많은경우`() {
+        Assertions.assertThatThrownBy { InfixCalculator("10 / 20 * 30 + 10 -").parse() }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("연산자가 피연산자 보다 많을 수 없습니다.")
+    }
+
+    @Test
+    fun `null 인 경우`() {
+        Assertions.assertThatThrownBy { InfixCalculator(null).parse() }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("수식은 null 일 수 없습니다.")
+    }
+
+    @Test
+    fun `공백 문자일 경우`() {
+        Assertions.assertThatThrownBy { InfixCalculator("").parse() }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("수식은 공백 일 수 없습니다.")
+    }
+
+    @Test
+    fun `정상적인 계산식 일 경우`() {
+        val infixOperation = InfixCalculator("2 + 3 * 4 * 2 / 2 - 5 ").parse()
+        val result = infixOperation.operate()
+
+        Assertions.assertThat(result).isEqualTo(BigDecimal(15))
+    }
+}
