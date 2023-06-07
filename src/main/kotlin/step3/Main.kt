09886654a0086feab07f763cf825d6fe8c767c1b
jeff.42(박제헌) <jeff.42@kakaocorp.com>
2023-06-07 23:39:53 +0900

    자동차 경주: Main 작성

---
+++ src/main/kotlin/step3/Main.kt
@@ -0,0 +1,35 @@
+package step3
+
+import step3.entity.RacingCar
+import step3.view.InputView
+import step3.view.OutputView
+import step3.workflow.Race
+
+class Main(
+    private val inputView: InputView = InputView(),
+    private val outputView: OutputView = OutputView(LINE_SYMBOL),
+    private val race: Race = Race(),
+) {
+    operator fun invoke() {
+        val raceCondition = inputView()
+        var racingCars = makeCars(raceCondition.numberOfCars)
+        (0 until raceCondition.numberOfLabs).forEach {
+            racingCars = doLabs(racingCars)
+            outputView(racingCars)
+        }
+    }
+
+    private fun makeCars(numberOfCars: Int): List<RacingCar> =
+        (0 until numberOfCars).map { RacingCar() }
+
+    private fun doLabs(cars: List<RacingCar>): List<RacingCar> =
+        cars.map { race(it) }
+
+    companion object {
+        private val LINE_SYMBOL = "-"
+    }
+}
+
+fun main() {
+    Main().invoke()
+}
