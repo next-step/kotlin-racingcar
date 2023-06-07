9ac3a339a42b214f6d0efafd704ec1e2f56bb88e
sjparkk <sjparkk0820@naver.com>
2023-06-07 23:57:44 +0900

    feat : 연산 기호 수와 연산 숫자 및 연산 기호 validation 추가

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -29,9 +29,21 @@ class StringCalculator {
             numbers.add(currentNumber.toString().toDouble())
         }
 
+        valid(numbers, operators)
+
         return ExtractDTO(
             numbers = numbers,
             operators = operators
         )
+    }
+
+    private fun valid(numbers: MutableList<Double>, operators: MutableList<Char>) {
+        if (numbers.size != operators.size + 1) {
+            throw IllegalArgumentException("연산 기호의 수는 연산 숫자의 수보다 무조건 1개 적어야 합니다")
+        }
+
+        if (operators.any { it != '+' && it != '-' && it != '*' && it != '/' }) {
+            throw IllegalArgumentException("연산 기호의 맞지 않는 기호가 존재합니다")
+        }
     }
 }
