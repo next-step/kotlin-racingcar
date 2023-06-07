a081b8319557d31bcc732efd5c7a205ad24549da
jeff.42(박제헌) <jeff.42@kakaocorp.com>
2023-06-07 23:37:57 +0900

    자동차 경주: inputView 작성

--- src/main/kotlin/step3/view/InputView.kt
+++ src/main/kotlin/step3/view/InputView.kt
@@ -4,13 +4,10 @@ import step3.entity.RaceCondition
 
 class InputView {
     operator fun invoke(): RaceCondition {
-        val numberOfCars = getNumberInput("자동차 대수는 몇 대인가요?")
-        val numberOfLabs = getNumberInput("시도할 횟수는 몇 회인가요?")
+        println("자동차 대수는 몇 대인가요?")
+        val numberOfCars = readLine()
+        println("시도할 횟수는 몇 회인가요?")
+        val numberOfLabs = readLine()
         return RaceCondition.of(numberOfCars, numberOfLabs)
-    }
-
-    private fun getNumberInput(msg: String): String? {
-        println(msg)
-        return readLine()
     }
 }
