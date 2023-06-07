6bd6be7fcb68ba722acfa98735e133f5cea45a6e
nam-sangyun <nsun9505@naver.com>
2023-06-07 23:38:20 +0900

    test(사칙연산)

---
+++ src/test/kotlin/step2/CalculatorTest.kt
@@ -0,0 +1,50 @@
+package step2
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.math.BigDecimal
+
+class CalculatorTest {
+
+    private val calculator = Calculator()
+
+    @Test
+    fun `1 add 1`() {
+        val x = BigDecimal("1")
+        val y = BigDecimal("1")
+
+        val result = calculator.add(x, y)
+
+        assertThat(result).isEqualTo(BigDecimal("2"))
+    }
+
+    @Test
+    fun `2 subtract 1`() {
+        val x = BigDecimal("2")
+        val y = BigDecimal("1")
+
+        val result = calculator.subtract(x, y)
+
+        assertThat(result).isEqualTo(BigDecimal("1"))
+    }
+
+    @Test
+    fun `1 multiply 2`() {
+        val x = BigDecimal("1")
+        val y = BigDecimal("2")
+
+        val result = calculator.multiply(x, y)
+
+        assertThat(result).isEqualTo(BigDecimal("2"))
+    }
+
+    @Test
+    fun `4 divide 2`() {
+        val x = BigDecimal("4")
+        val y = BigDecimal("2")
+
+        val result = calculator.divide(x, y)
+
+        assertThat(result).isEqualTo(BigDecimal("2"))
+    }
+}
