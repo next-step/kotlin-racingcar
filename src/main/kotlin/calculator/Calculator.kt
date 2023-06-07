590932a2beee8ed40be1590a22d770010c3d1e7a
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-06-07 20:55:47 +0900

    refactor: kotlin require 적용

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -1,11 +1,10 @@
 package calculator
 
 fun calculate(input: String): Int {
-    if (input.isBlank()) {
-        throw IllegalArgumentException("공백은 계산할 수 없다.")
-    }
+    require(input.isNotBlank()) { "공백은 계산할 수 없다." }
     val values = input.split(" ")
-    validateCalculateFormat(values)
+    require(values.size % 2 == 0) { "올바른 형태로 수식을 입력하여야 한다." }
+
     val operands = values.filterIndexed { index, _ -> index % 2 == 0 }
         .map { parseToNumber(it) }
     val operators = values.filterIndexed { index, _ -> index % 2 == 1 }
@@ -20,12 +19,6 @@ fun calculate(input: String): Int {
     }
 
     return calculatedNumber
-}
-
-private fun validateCalculateFormat(values: List<String>) {
-    if (values.size % 2 == 0) {
-        throw IllegalArgumentException("올바른 형태로 수식을 입력하여야 한다.")
-    }
 }
 
 private fun parseToNumber(it: String) =
