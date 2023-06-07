d8b43dad8beb6ec6d64002d7a908535344c63af8
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-06-07 21:09:11 +0900

    refactor: 짝수 홀수 검증 메소드 분리

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -3,11 +3,11 @@ package calculator
 fun calculate(input: String): Int {
     require(input.isNotBlank()) { "공백은 계산할 수 없다." }
     val values = input.split(" ")
-    require(values.size % 2 == 0) { "올바른 형태로 수식을 입력하여야 한다." }
+    require(isOdd(values.size)) { "올바른 형태로 수식을 입력하여야 한다." }
 
-    val operands = values.filterIndexed { index, _ -> index % 2 == 0 }
+    val operands = values.filterIndexed { index, _ -> isEven(index) }
         .map { parseToNumber(it) }
-    val operators = values.filterIndexed { index, _ -> index % 2 == 1 }
+    val operators = values.filterIndexed { index, _ -> isOdd(index) }
         .map { Operator.from(it) }
 
     var calculatedNumber = operands.first()
@@ -20,6 +20,10 @@ fun calculate(input: String): Int {
 
     return calculatedNumber
 }
+
+private fun isOdd(number: Int) = (number % 2) == 1
+
+private fun isEven(number: Int) = (number % 2) == 0
 
 private fun parseToNumber(it: String) =
     try {
