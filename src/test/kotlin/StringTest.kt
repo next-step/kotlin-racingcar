8a5568eb222f17fd2b0387dbeeff2e25cd340fb5
chad.yoon <chad.yoon@kakaocorp.com>
2023-06-07 15:27:56 +0900

    study: learn kotlin basics

---
+++ src/test/kotlin/StringTest.kt
@@ -0,0 +1,12 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class StringTest {
+
+    @Test
+    fun isEmpty() {
+        assertThat("".isEmpty()).isTrue()
+        assertThat(" ".isEmpty()).isFalse()
+        assertThat("a".isEmpty()).isFalse()
+    }
+}
