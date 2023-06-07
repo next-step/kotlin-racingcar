d60a190f752c94f5466dfdd308d4b20ca90449a0
genos-lee <genos.lee@kakaocorp.com>
2023-06-08 02:05:46 +0900

    test, refactor : ActionCommandTest에 ShouldSpec 적용

--- src/test/kotlin/next/step/calculator/domain/ActionCommandTest.kt
+++ src/test/kotlin/next/step/calculator/domain/ActionCommandTest.kt
@@ -1,16 +1,14 @@
 package next.step.calculator.domain
 
+import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.shouldBe
-import org.junit.jupiter.api.Test
 
-class ActionCommandTest {
-    @Test
-    fun `커맨드가 Q이면 종료`() {
+class ActionCommandTest : ShouldSpec({
+    should("커맨드가 Q이면 종료") {
         ActionCommand.Q.isQuit() shouldBe true
     }
 
-    @Test
-    fun `전체 액션 커맨드 출력`() {
+    should("전체 액션 커맨드 출력") {
         ActionCommand.descAll() shouldBe "C: 계산, Q: 종료"
     }
-}
+})
