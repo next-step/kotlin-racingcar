850e9bfbbf1b902200c569132d0e964fef053e52
jeff.42(박제헌) <jeff.42@kakaocorp.com>
2023-06-07 23:37:31 +0900

    자동차 경주: RaceCondition 작성

--- src/main/kotlin/step3/entity/RaceCondition.kt
+++ src/main/kotlin/step3/entity/RaceCondition.kt
@@ -1,5 +1,7 @@
 package step3.entity
 
+import java.lang.NumberFormatException
+
 class RaceCondition private constructor(
     val numberOfCars: Int,
     val numberOfLabs: Int,
@@ -10,7 +12,11 @@ class RaceCondition private constructor(
 
         private fun validateNumbers(number: String?): Int {
             require(!number.isNullOrBlank()) { "입력은 반드시 필요합니다. 숫자를 입력해주세요." }
-            val intValue = number.toIntOrNull() ?: throw IllegalArgumentException("숫자만 입력해주세요.")
+            val intValue = try {
+                number.toInt()
+            } catch (e: NumberFormatException) {
+                throw IllegalArgumentException("숫자만 입력해주세요.")
+            }
             require(intValue > 0) { "숫자는 반드시 양수여야 합니다." }
             return intValue
         }
