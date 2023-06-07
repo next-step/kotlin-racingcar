113dcbbe076b9578574f2ec66eb8c559e43f1c70
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-06-07 20:47:07 +0900

    refactor: 연산자 변환 및 연산기능 Operator로 리팩터링

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -9,29 +9,14 @@ fun calculate(input: String): Int {
     val operands = values.filterIndexed { index, _ -> index % 2 == 0 }
         .map { parseToNumber(it) }
     val operators = values.filterIndexed { index, _ -> index % 2 == 1 }
-        .map { parseToOperator(it) }
+        .map { Operator.from(it) }
 
     var calculatedNumber = operands.first()
 
     for (index in operators.indices) {
         val operand = operands[index + 1]
         val operator = operators[index]
-
-        if (operator == "+") {
-            calculatedNumber += operand
-        }
-        if (operator == "-") {
-            calculatedNumber -= operand
-        }
-        if (operator == "*") {
-            calculatedNumber *= operand
-        }
-        if (operator == "/") {
-            if (operand == 0) {
-                throw IllegalArgumentException("숫자는 0으로 나눌 수 없다.")
-            }
-            calculatedNumber /= operand
-        }
+        calculatedNumber = operator.operate(calculatedNumber, operand)
     }
 
     return calculatedNumber
@@ -49,10 +34,3 @@ private fun parseToNumber(it: String) =
     } catch (_: NumberFormatException) {
         throw IllegalArgumentException("올바른 숫자를 입력하여야 한다.")
     }
-
-private fun parseToOperator(it: String): String {
-    if (it != "+" && it != "-" && it != "/" && it != "*") {
-        throw IllegalArgumentException("올바른 연산자를 입력하여야 한다.")
-    }
-    return it
-}
