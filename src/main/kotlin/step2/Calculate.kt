d09fd538fe512159db52f1e6711722cd57dabfbf
jeff.42(박제헌) <jeff.42@kakaocorp.com>
2023-06-07 09:23:16 +0900

    문자열 계산기: 리팩토링

--- src/main/kotlin/step2/Calculate.kt
+++ src/main/kotlin/step2/Calculate.kt
@@ -33,32 +33,4 @@ class Calculate {
             Operator.PLUS -> firstNumber + secondNumber
             Operator.MINUS -> firstNumber - secondNumber
         }
-
-    private fun tokenize(equation: String): List<String> = equation.split(DELIMITER)
-
-    private fun validateNumbers(tokens: List<String>): List<Double> =
-        try {
-            tokens.filterIndexed { index, _ -> index % 2 == 0 }.map { token -> token.toDouble() }
-        } catch (e: NumberFormatException) {
-            throw IllegalArgumentException("숫자 형태를 입력해주세요.")
-        }
-
-    private fun validateOperand(tokens: List<String>): List<Operator> =
-        tokens.filterIndexed { index, _ -> index % 2 != 0 }.map { token -> compareOperand(token) }
-
-    private fun compareOperand(operand: String): Operator =
-        when (operand) {
-            "+" -> Operator.PLUS
-            "-" -> Operator.MINUS
-            "*" -> Operator.MULTIPLY
-            "/" -> Operator.DIVIDE
-            else -> throw IllegalArgumentException("올바른 계산자를 입력해주세요.")
-        }
-}
-
-enum class Operator(val operand: String) {
-    PLUS("+"),
-    MINUS("-"),
-    MULTIPLY("*"),
-    DIVIDE("/");
 }
