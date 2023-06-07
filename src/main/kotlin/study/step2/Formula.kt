72bc6c8fc90f99f8ae0bffebe11718c7c51b28ea
waveofmymind <sjun0913@gmail.com>
2023-06-08 00:17:02 +0900

    Fix: 잘못된 수에 대한 검증 메서드 추가

--- src/main/kotlin/study/step2/Formula.kt
+++ src/main/kotlin/study/step2/Formula.kt
@@ -4,9 +4,16 @@ class Formula(
     input: String?
 ) {
     val expression: List<String>
-
     init {
-        require(!input.isNullOrBlank()) { "잘못된 연산자입니다." }
+        require(!input.isNullOrBlank()) { "잘못된 연산식입니다." }
         expression = input.split(" ")
+        validateExpression()
+    }
+    private fun validateExpression() {
+        expression.forEachIndexed { index, s ->
+            if (index % 2 == 0 && s.toDoubleOrNull() == null) {
+                throw NumberFormatException("유효한 숫자가 아닙니다.")
+            }
+        }
     }
 }
