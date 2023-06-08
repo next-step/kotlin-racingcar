72e2af3377ef1ee1880efbcbfe24f081045d53e6
jinuk17 <jinuk17@hotmail.com>
2023-06-08 11:56:22 +0900

    🚀 1단계 - 코틀린 기초 (#1051)
    * chore : .gitkeep 제거
    
    * feat : named arguments 학습
    
    * feat : nullable types 학습
    
    * feat : default arguments 학습
    
    * feat : data classes 학습
    
    ---------
    
    Co-authored-by: jayden-17 <jayden.17@kakaocorp.com>

---
+++ src/test/kotlin/study/PersonTest.kt
@@ -0,0 +1,51 @@
+package study
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+
+class PersonTest {
+
+    @Test
+    fun `named arguments`() {
+        val people = listOf(
+            Person("김진억", 39, "제이든"),
+            Person("김진억", 39, nickname = "제이든"),
+            Person(name = "김진억", nickname = "제이든", age = 39)
+        )
+
+        assertThat(people).allSatisfy {
+            assertThat(it.name).isEqualTo("김진억")
+            assertThat(it.age).isEqualTo(39)
+            assertThat(it.nickname).isEqualTo("제이든")
+        }
+    }
+
+    @Test
+    fun `nullable types`() {
+        val person = Person("김진억", null, null)
+
+        assertAll(
+            { assertThat(person.name).isEqualTo("김진억") },
+            { assertThat(person.age).isNull() },
+            { assertThat(person.nickname).isNull() }
+        )
+    }
+
+    @Test
+    fun `default arguments`() {
+        val person = Person("김진억")
+        assertAll(
+            { assertThat(person.name).isEqualTo("김진억") },
+            { assertThat(person.age).isNull() },
+            { assertThat(person.nickname).isNull() }
+        )
+    }
+
+    @Test
+    fun `data classes`() {
+        val person1 = Person("김진억", 39, "제이든")
+        val person2 = Person("김진억", 39, "제이든")
+        assertThat(person1).isEqualTo(person2)
+    }
+}
