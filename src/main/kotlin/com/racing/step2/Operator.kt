f404f7c0d97790020f0688634d7d485d96ef330c
goofy-919 <goofy.919@kakaocorp.com>
2023-06-08 22:30:50 +0900

    refactor: Operator 리팩터링
    * Operator와 Calulator 의존성 최소화를 위해 companion object 메서드 추가
    * Calculator 테스트 코드 수정 : 테스트 메서드 명 단순화 + CsvSource 를 이용한 테스트 단순화
    * Operator 클래스 단위테스트 추가

--- src/main/kotlin/com/racing/step2/Operator.kt
+++ src/main/kotlin/com/racing/step2/Operator.kt
@@ -8,4 +8,16 @@ enum class Operator(
     MINUS("-", { x: Int, y: Int -> x - y }),
     CROSS("*", { x: Int, y: Int -> x * y }),
     DIVIDE("/", { x: Int, y: Int -> if (y != 0) x / y else throw IllegalArgumentException("0 으로 나눌 수는 없습니다") });
+
+    companion object {
+        fun from(op: String): Operator {
+            return when (op) {
+                PLUS.op -> PLUS
+                MINUS.op -> MINUS
+                CROSS.op -> CROSS
+                DIVIDE.op -> DIVIDE
+                else -> throw IllegalArgumentException("연산 기호에 오류가 있습니다")
+            }
+        }
+    }
 }
--- src/test/kotlin/com/racing/step2/CalculatorTest.kt
+++ src/test/kotlin/com/racing/step2/CalculatorTest.kt
@@ -1,40 +1,30 @@
 package com.racing.step2
 
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import java.lang.IllegalArgumentException
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class CalculatorTest {
-    @Test
-    fun `일반적인 수학 계산을 할 수 있는 계산기를 만든다`() {
-        // given
-        val input = "3 + 4 - 1 / 3 + 5"
-        val answer = 7
-
+    @ParameterizedTest
+    @CsvSource(
+        "3 + 4 - 1 / 3 + 5,  7",
+        "3 + 13 / 2 + 5,  13",
+        "5 - 1 * 6 - 2,  22",
+        "6 * 7 / 2 + 1, 22"
+    )
+    fun `성공하는 계산 케이스`(input: String, answer: Int) {
         // when
         val calculator = Calculator()
         val result = calculator.exec(input)
 
         // then
-        Assertions.assertThat(result).isEqualTo(answer)
+        assertThat(result).isEqualTo(answer)
     }
 
     @Test
-    fun `일반적인 수학 계산을 할 수 있는 계산기를 만드는 케이스`() {
-        // given
-        val input = "3 + 10 - 1 / 2 + 5"
-        val answer = 11
-
-        // when
-        val calculator = Calculator()
-        val result = calculator.exec(input)
-
-        // then
-        Assertions.assertThat(result).isEqualTo(answer)
-    }
-
-    @Test
-    fun `일반적인 수학 계산처럼 보이지만 0 으로는 나눌 수 없다`() {
+    fun `0 으로는 나눌 시 예외`() {
         // given
         val input = "3 + 10 - 1 / 0 + 5"
 
@@ -42,14 +32,13 @@ class CalculatorTest {
         val calculator = Calculator()
 
         // then
-        Assertions.assertThatThrownBy {
+        assertThrows<IllegalArgumentException> {
             calculator.exec(input)
         }
-            .isInstanceOf(IllegalArgumentException::class.java)
     }
 
     @Test
-    fun `입력값이 null이거나 빈 공백 문자일 경우 IllegalArgumentException throw 반환한다`() {
+    fun `입력값이 null이거나 빈 공백 문자일 경우 예외`() {
         // given
         val input = ""
         val input2 = " "
@@ -58,19 +47,17 @@ class CalculatorTest {
         val calculator = Calculator()
 
         // then
-        Assertions.assertThatThrownBy {
+        assertThrows<IllegalArgumentException> {
             calculator.exec(input)
         }
-            .isInstanceOf(IllegalArgumentException::class.java)
 
-        Assertions.assertThatThrownBy {
+        assertThrows<IllegalArgumentException> {
             calculator.exec(input2)
         }
-            .isInstanceOf(IllegalArgumentException::class.java)
     }
 
     @Test
-    fun `사칙연산 기호가 아닌 경우 IllegalArgumentException throw 반환한다`() {
+    fun `사칙연산 기호가 아닌 경우 예외`() {
         // given
         val input = "intput string"
 
@@ -78,9 +65,8 @@ class CalculatorTest {
         val calculator = Calculator()
 
         // then
-        Assertions.assertThatThrownBy {
+        assertThrows<IllegalArgumentException> {
             calculator.exec(input)
         }
-            .isInstanceOf(IllegalArgumentException::class.java)
     }
 }
