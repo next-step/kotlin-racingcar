1ec601d9c8b24be81009f4f38d205bfc8ea2b61f
김태훈 <sksskaw@naver.com>
2023-06-09 00:02:00 +0900

    테스트 코드 CsvSource 활용 전환

--- src/test/kotlin/StringCalculateTest.kt
+++ src/test/kotlin/StringCalculateTest.kt
@@ -1,12 +1,11 @@
-
 import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
 
 class StringCalculateTest {
     @ParameterizedTest
-    @ValueSource(strings = [ "2 + 3 * 4 / 2" ])
+    @ValueSource(strings = ["2 + 3 * 4 / 2"])
     fun testSquares(input: String) {
 
         val calculator = StringCalculate()
@@ -15,93 +14,63 @@ class StringCalculateTest {
         Assertions.assertEquals(10, result)
     }
 
-    @Test
-    fun `덧셈`() {
-        val input = "5 + 6 + 9 + 10"
-
+    @ParameterizedTest
+    @CsvSource(
+        "5 + 6 + 9 + 10, 30",
+        "15 + 100 + 1500 + 1, 1616"
+    )
+    fun `덧셈`(input: String, expected: Int) {
         val calculator = StringCalculate()
         val result = calculator.calculate(input)
 
-        Assertions.assertEquals(30, result)
+        Assertions.assertEquals(expected, result)
     }
 
-    @Test
-    fun `여러 자릿수 덧셈`() {
-        val input = "15 + 100 + 1500 + 1"
-
+    @ParameterizedTest
+    @CsvSource(
+        "10 - 5 - 3 - 1, 1",
+        "100 - 10 - 20 - 5, 65"
+    )
+    fun `뺄셈`(input: String, expected: Int) {
         val calculator = StringCalculate()
         val result = calculator.calculate(input)
 
-        Assertions.assertEquals(1616, result)
+        Assertions.assertEquals(expected, result)
     }
 
-    @Test
-    fun `뺄셈`() {
-        val input = "10 - 5 - 3 - 1"
-
+    @ParameterizedTest
+    @CsvSource(
+        "5 * 5 * 5, 125",
+        "100 * 5 * 2, 1000"
+    )
+    fun `곱셈`(input: String, expected: Int) {
         val calculator = StringCalculate()
         val result = calculator.calculate(input)
 
-        Assertions.assertEquals(1, result)
+        Assertions.assertEquals(expected, result)
     }
 
-    @Test
-    fun `여러 자릿수 뺄셈`() {
-        val input = "100 - 10 - 20 - 5"
-
+    @ParameterizedTest
+    @CsvSource(
+        "50 / 5 / 2, 5",
+        "100 / 5 / 3, 6"
+    )
+    fun `나눗셈`(input: String, expected: Int) {
         val calculator = StringCalculate()
         val result = calculator.calculate(input)
 
-        Assertions.assertEquals(65, result)
+        Assertions.assertEquals(expected, result)
     }
 
-    @Test
-    fun `곱셈`() {
-        val input = "5 * 5 * 5"
-
+    @ParameterizedTest
+    @CsvSource(
+        "50 + 9 * 5 * 3 - 111 / 2, 387",
+        "33 + 1 * 2 * 2 - 1111 / 2, -487"
+    )
+    fun `복합 계산`(input: String, expected: Int) {
         val calculator = StringCalculate()
         val result = calculator.calculate(input)
 
-        Assertions.assertEquals(125, result)
-    }
-
-    @Test
-    fun `여러 자릿수 곱셈`() {
-        val input = "100 * 5 * 2"
-
-        val calculator = StringCalculate()
-        val result = calculator.calculate(input)
-
-        Assertions.assertEquals(1000, result)
-    }
-
-    @Test
-    fun `나눗셈`() {
-        val input = "50 / 5 / 2"
-
-        val calculator = StringCalculate()
-        val result = calculator.calculate(input)
-
-        Assertions.assertEquals(5, result)
-    }
-
-    @Test
-    fun `여러 자릿수 나눗셈`() {
-        val input = "100 / 5 / 3"
-
-        val calculator = StringCalculate()
-        val result = calculator.calculate(input)
-
-        Assertions.assertEquals(6, result)
-    }
-
-    @Test
-    fun `복합 계산`() {
-        val input = "50 + 9 * 5 * 3 - 111 / 2"
-
-        val calculator = StringCalculate()
-        val result = calculator.calculate(input)
-
-        Assertions.assertEquals(387, result)
+        Assertions.assertEquals(expected, result)
     }
 }
