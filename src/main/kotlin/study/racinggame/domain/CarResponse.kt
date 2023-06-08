efcb0b23e9402876dd4ceb5299509c80e33459a6
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2023-06-09 00:48:58 +0900

    refactor(CarResponse): 불변객체의 변수도 불변으로 변경

--- src/main/kotlin/study/racinggame/domain/CarResponse.kt
+++ src/main/kotlin/study/racinggame/domain/CarResponse.kt
@@ -1,7 +1,7 @@
 package study.racinggame.domain
 
 data class CarResponse(
-    var position: Int = 0,
+    val position: Int = 0
 ) {
     fun getPositionRepresentation(): String {
         return "-".repeat(position)
