9db0527c14603254fbea50b3c9fdb739e7878d6f
sjjeong <sjjeong1225@gmail.com>
2023-06-08 23:17:12 +0900

    입력값 순서를 보장하는 테스트코드 추가

--- src/main/kotlin/study/step2/Calculator.kt
+++ src/main/kotlin/study/step2/Calculator.kt
@@ -8,7 +8,9 @@ object Calculator {
     fun calculate(input: String?): Int {
         requireNotNull(input) { "input must not be null" }
         require(input.isNotBlank()) { "input must not be blank" }
-        return input.parse().join()
+        return input.parse()
+            .validate()
+            .join()
     }
 
     private fun String.parse(): List<Equation> {
@@ -25,6 +27,14 @@ object Calculator {
                 }
             }
         }
+    }
+
+    private fun List<Equation>.validate(): List<Equation> {
+        require(size % 2 == 1) { "input must be odd" }
+        require(first() is Equation.Number) { "input must start with number" }
+        require(last() is Equation.Number) { "input must end with number" }
+        require(indexOfFirst { it is Operator } % 2 == 1) { "input must be number-operator-number" }
+        return this
     }
 
     private fun List<Equation>.join(): Int {
