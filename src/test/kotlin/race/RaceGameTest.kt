f356ad02c1838c2ff35e96a1fa1356d9f4a9a006
peter <peter.shlee@kakaocorp.com>
2023-06-08 12:50:18 +0900

    feat<step3>: 자동차 경주 테스트 작성

---
+++ src/test/kotlin/race/RaceGameTest.kt
@@ -0,0 +1,50 @@
+package race
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.RepeatedTest
+import org.junit.jupiter.api.Test
+
+class RaceGameTest {
+    @Test
+    fun `입력된 자동차 수와 RaceResult의 자동차 수가 동일해야 한다`() {
+        val raceStartInformation = RaceStartInformation(numberOfCars = 3, numberOfAttempts = 5)
+
+        CarRaceGame().runCarRace(raceStartInformation)
+            .positions
+            .map {
+                it.positions
+                    .size
+            }.forEach {
+                assertThat(it).isEqualTo(raceStartInformation.numberOfCars)
+            }
+    }
+
+    @Test
+    fun `RaceResult 내 positions 길이는 입력된 시도 횟수 + 1 이어야 한다`() {
+        val raceStartInformation = RaceStartInformation(numberOfCars = 3, numberOfAttempts = 5)
+
+        CarRaceGame().runCarRace(raceStartInformation)
+            .positions
+            .size
+            .let {
+                assertThat(it).isEqualTo(raceStartInformation.numberOfAttempts + 1)
+            }
+    }
+
+    @RepeatedTest(10)
+    fun `차량의 위치는 이전 위치와 같거나 +1 이어야 한다`() {
+        val raceStartInformation = RaceStartInformation(numberOfCars = 3, numberOfAttempts = 5)
+
+        CarRaceGame().runCarRace(raceStartInformation)
+            .positions
+            .reduce { prevPosition, currentPosition ->
+                prevPosition.positions
+                    .zip(currentPosition.positions)
+                    .forEach {
+                        val distance = it.second.position - it.first.position
+                        assertThat(distance == 0 || distance == 1).isTrue()
+                    }
+                currentPosition
+            }
+    }
+}
