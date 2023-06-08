9818ee264cc42fa1521d162f16432a413435e9a3
Yongho Choi <73802331+yongho2157@users.noreply.github.com>
2023-06-08 22:46:03 +0900

    Step2: 문자열 계산기 (#1049)
    Co-authored-by: yonghoChoi <studyyongho2157@gmail.com>

---
+++ src/test/kotlin/study/CalculatorTest.kt
@@ -0,0 +1,47 @@
+package study
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+
+class CalculatorTest {
+
+    @Test
+    fun `두 수의 합`() {
+        assertThat(Calculator().calculate("1 + 1")).isEqualTo(2)
+    }
+
+    @Test
+    fun `두 수의 차`() {
+        assertThat(Calculator().calculate("10 - 7")).isEqualTo(3)
+    }
+
+    @Test
+    fun `두 수의 곱`() {
+        assertThat(Calculator().calculate("5 * 3")).isEqualTo(15)
+    }
+
+    @Test
+    fun `두 수의 나눗셈`() {
+        assertThat(Calculator().calculate("4 / 2")).isEqualTo(2)
+    }
+
+    @Test
+    fun `사칙 연산 모두 포함`() {
+        assertThat(Calculator().calculate("1 + 5 * 7 / 2")).isEqualTo(21)
+    }
+
+    @Test
+    fun `입력 값 널이거나 빈 공백`() {
+        assertThatThrownBy {
+            Calculator().calculate("")
+        }.isInstanceOf(IllegalArgumentException::class.java).hasMessage("Input value is null or empty")
+    }
+
+    @Test
+    fun `사칙연산 기호가 아닌 경우`() {
+        assertThatThrownBy {
+            Calculator().calculate("4 / 2 # 3 ")
+        }.isInstanceOf(IllegalArgumentException::class.java).hasMessage("Not an arithmetic operator")
+    }
+}
