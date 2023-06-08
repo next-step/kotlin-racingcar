3e445646df4ebe0679cab1ba73cc614b4ed899a0
Songyi Kim <52441906+songyi00@users.noreply.github.com>
2023-06-08 10:06:47 +0900

    [Step2] 문자열 계산기 (#1031)
    * chore: data class 사용으로 equals, hashcode 제거
    
    * feat: 문자열 사칙 연산 계산기 구현
    
    구현 목록 1: 사칙 연산 구현
    구현 목록 2: 입력값 null 이거나 공백인 경우 예외처리
    구현 목록 3: 사칙 연산 기호가 아닌 경우 예외처리
    
    * test: 테스트 코드 작성
    
    * refactor: 미션 별 패키지 분리

---
+++ src/main/kotlin/step1/Person.kt
@@ -0,0 +1,7 @@
+package step1
+
+data class Person(
+    val name: String,
+    val age: Int,
+    var nickname: String? = "Guest"
+)
--- src/test/kotlin/step1/PersonTest.kt
+++ src/test/kotlin/step1/PersonTest.kt
@@ -1,4 +1,4 @@
-package study
+package step1
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
