204693e00bed74b407f3c8c3b405bd5a630a6771
andy.dec <andy.dec@kakaostyle.com>
2023-06-08 23:06:05 +0900

    feat: 자동차 대수 대신 이름을 입력하도록 개발

--- src/main/kotlin/step3/view/InputView.kt
+++ src/main/kotlin/step3/view/InputView.kt
@@ -2,18 +2,18 @@ package step3.view
 
 object InputView {
 
-    private const val ENTER_CAR_COUNT: String = "자동차 대수는 몇 대인가요?"
+    private const val ENTER_CAR_COUNT: String = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
     private const val ENTER_ROUND: String = "시도할 횟수는 몇 회인가요?"
 
     fun readStartInput(): RaceGameInput {
         println(message = ENTER_CAR_COUNT)
-        val carCount = readln()
+        val carNames = readln()
 
         println(message = ENTER_ROUND)
         val round = readln()
 
         return RaceGameInput(
-            carCountInput = carCount,
+            carNamesInput = carNames,
             roundInput = round,
         )
     }
--- src/main/kotlin/step3/view/RaceGameInput.kt
+++ src/main/kotlin/step3/view/RaceGameInput.kt
@@ -3,17 +3,25 @@ package step3.view
 import step3.model.RaceGameErrorCode
 
 data class RaceGameInput(
-    val carCount: Int,
+    val carNames: List<String>,
     val round: Int,
 ) {
 
-    constructor(carCountInput: String, roundInput: String) : this(
-        carCount = requireNotNull(value = carCountInput.toIntOrNull()) {
-            RaceGameErrorCode.INVALID_CAR_COUNT_INPUT.message(carCountInput)
-        },
+    constructor(carNamesInput: String, roundInput: String) : this(
+        carNames = carNamesInput.split(DELIMITER)
+            .also {
+                require(value = it.none { name -> name.length > MAX_NAME_SIZE }) {
+                    RaceGameErrorCode.INVALID_CAR_NAME_INPUT.message(MAX_NAME_SIZE.toString())
+                }
+            },
 
         round = requireNotNull(value = roundInput.toIntOrNull()) {
             RaceGameErrorCode.INVALID_ROUND_INPUT.message(roundInput)
         },
     )
+
+    companion object {
+        private const val DELIMITER = ','
+        private const val MAX_NAME_SIZE = 5
+    }
 }
