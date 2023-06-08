01d6cdee74d1540ca7ad646ea78e176e7cb58ffb
andy.dec <andy.dec@kakaostyle.com>
2023-06-08 23:23:41 +0900

    test: 자동차 생성할 때 숫자대신 이름을 받을 수 있도록 TC 작성

--- src/test/kotlin/step3/domain/car/CarTest.kt
+++ src/test/kotlin/step3/domain/car/CarTest.kt
@@ -8,24 +8,33 @@ class CarTest : DescribeSpec({
 
     describe(name = "자동차를 생성할 때") {
         val position = 3
+        val name = "andy"
 
-        context(name = "위치를 명시적으로 지정하면") {
-            val car = CarFactory.createCar(position = position)
+        context(name = "위치와 이름을 지정하면") {
+            val car = CarFactory.createCar(position = position, name = name)
 
             it(name = "지정한 위치로 생성된다.") {
                 car.move(moveFormula = NotMoveFormula) shouldBe position
+            }
+
+            it(name = "지정한 이름으로 생성된다.") {
+                car.name shouldBe name
             }
         }
     }
 
     describe(name = "자동차들을 생성할 때") {
-        val count = 3
+        val carNames = listOf("test1", "test2", "test3")
 
-        context(name = "개수를 명시적으로 지정하면") {
-            val cars = CarFactory.createCars(count = count)
+        context(name = "자동차 이름 리스트를 지정하면") {
+            val cars = CarFactory.createCars(carNames = carNames)
 
-            it(name = "지정한 개수만큼 생성된다.") {
-                cars.size shouldBe count
+            it(name = "지정한 이름 순서대로 생성된다.") {
+                cars.size shouldBe carNames.size
+
+                cars.forEachIndexed { index, car ->
+                    car.name shouldBe carNames[index]
+                }
             }
         }
     }
