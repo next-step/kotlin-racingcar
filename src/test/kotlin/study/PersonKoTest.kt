68b09176ebb11f9ef454656eb43f6fc3c02490de
ohsung <ohsung555@gmail.com>
2023-06-08 23:44:02 +0900

    feat : 문자열 계산기

---
+++ src/test/kotlin/study/PersonKoTest.kt
@@ -0,0 +1,14 @@
+package study
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class PersonKoTest : StringSpec({
+    "이름 붙인 인자" {
+        val actual = Person(name = "Ohsung", age = 20, nickname = "oh")
+        actual.name shouldBe "Ohsung"
+        actual.age shouldBe 20
+        actual.nickname shouldBe "oh"
+    }
+}) {
+}
