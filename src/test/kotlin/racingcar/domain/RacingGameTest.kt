efc87394fdeb7c7e23b804962d387b722fab850f
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-06-08 18:29:14 +0900

    feat: 자동차 대수 1미만인 경우 예외 추가 및 테스트

--- src/test/kotlin/racingcar/domain/RacingGameTest.kt
+++ src/test/kotlin/racingcar/domain/RacingGameTest.kt
@@ -7,10 +7,18 @@ import org.junit.jupiter.api.Test
 class RacingGameTest {
 
     @Test
-    fun `자동차 대수가 1보다 작으면 예외가 발생한다`() {
+    fun `시도 횟수가 1보다 작으면 예외가 발생한다`() {
         val exception = shouldThrowExactly<IllegalArgumentException> {
             RacingGame(attemptCount = 0, cars = listOf(Car()))
         }
-        exception.message shouldBe "게임 생성에 필요한 시도횟수는 1회 이상이어야 한다."
+        exception.message shouldBe "게임 생성에 필요한 시도 횟수는 1회 이상이어야 한다."
+    }
+
+    @Test
+    fun `자동차 대수가 1보다 작으면 예외가 발생한다`() {
+        val exception = shouldThrowExactly<IllegalArgumentException> {
+            RacingGame(attemptCount = 1, cars = listOf())
+        }
+        exception.message shouldBe "게임 생성에 필요한 자동차 대수는 1대 이상이어야 한다."
     }
 }
