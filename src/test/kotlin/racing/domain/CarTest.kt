8f9374fd96a346547f9d10203e2a12e467dd3316
lhs0610 <lhs0610@midasin.com>
2023-06-08 10:50:13 +0900

    test(CarTest): kotest spec 변경
    - DescribeSpec -> BehaviorSpec으로 변경

--- src/test/kotlin/racing/domain/CarTest.kt
+++ src/test/kotlin/racing/domain/CarTest.kt
@@ -1,26 +1,23 @@
 package racing.domain
 
-import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 
-class CarTest : DescribeSpec({
-    describe(name = "자동차 객체가 전진을 수행할 때") {
-        context(name = "전진 조건이 true를 반환하면") {
-            val actualCar = Car()
+class CarTest : BehaviorSpec({
+    given(name = "자동차 객체가 전진을 수행할 때") {
+        `when`(name = "전진 조건이 true를 반환하면") {
+            val actual = Car().apply { move { true } }
 
-            actualCar.move { true }
-
-            it(name = "위치 정보는 증가한다.") {
-                actualCar shouldBe Car(Distance(1L))
+            then(name = "위치 정보는 증가한다.") {
+                actual shouldBe Car(Distance(1L))
             }
         }
 
-        context(name = "전진 조건이 false를 반환하면") {
-            val actualCar = Car()
-            actualCar.move { false }
+        `when`(name = "전진 조건이 false를 반환하면") {
+            val actual = Car().apply { move { true } }
 
-            it(name = "위치 정보는 그대로 유지된다.") {
-                actualCar shouldBe Car(Distance.ZERO)
+            then(name = "위치 정보는 그대로 유지된다.") {
+                actual shouldBe Car(Distance.ZERO)
             }
         }
     }
