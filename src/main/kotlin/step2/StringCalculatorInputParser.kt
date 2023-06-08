e12f4d662b7730fb0aa850ec794215cb54636019
nam-sangyun <nsun9505@naver.com>
2023-06-08 21:38:59 +0900

    refactor(상수화)

--- src/main/kotlin/step2/StringCalculatorInputParser.kt
+++ src/main/kotlin/step2/StringCalculatorInputParser.kt
@@ -2,9 +2,11 @@ package step2
 
 class StringCalculatorInputParser {
 
+    private val parserDelimiters: Array<String> = arrayOf(" ")
+
     fun parse(input: String?): List<String> {
         require(!input.isNullOrBlank()) { "Invalid input : $input" }
 
-        return input.split(" ").toList()
+        return input.split(delimiters = parserDelimiters).toList()
     }
 }
