e3b43b70be1fc879b451fe2596181e6d0c6a90e6
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-06-08 18:15:45 +0900

    feat: 시도 횟수 입력기능 구현

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -6,7 +6,18 @@ fun inputCarCount(): Int {
         .getOrElse { retryInputCarCount() }
 }
 
+fun inputRaceCount(): Int {
+    println("시도할 횟수는 몇 회인가요?")
+    return runCatching { readlnOrNull()!!.toInt() }
+        .getOrElse { retryInputRaceCount() }
+}
+
 private fun retryInputCarCount(): Int {
     println("자동차 대수를 잘못 입력했습니다. 다시 입력해주세요.")
     return inputCarCount()
+}
+
+fun retryInputRaceCount(): Int {
+    println("시도할 횟수를 잘못 입력했습니다. 다시 입력해주세요.")
+    return inputRaceCount()
 }
