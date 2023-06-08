ac750d9ac678cd8f7e5d8756922d5051b674170c
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-06-08 18:54:45 +0900

    feat: movable private 변경

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -2,7 +2,7 @@ package racingcar.domain
 
 class Car(
     var position: Int = 0,
-    val movable: () -> Boolean,
+    private val movable: () -> Boolean,
 ) {
     fun move(): Int {
         if (movable.invoke()) {
