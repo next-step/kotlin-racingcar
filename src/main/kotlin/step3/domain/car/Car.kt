4671c49c808189546ce265e12be75b9797134ad9
ljw0829 <olo_jinwon@naver.com>
2023-06-08 13:08:44 +0900

    feat: 리뷰 내용 반영 (도메인 객체 data class -> class로 변경)

--- src/main/kotlin/step3/domain/car/Car.kt
+++ src/main/kotlin/step3/domain/car/Car.kt
@@ -3,7 +3,7 @@ package step3.domain.car
 import step3.domain.game.formula.MoveFormula
 import java.util.concurrent.atomic.AtomicInteger
 
-data class Car internal constructor(
+class Car internal constructor(
     private val position: AtomicInteger = AtomicInteger(),
 ) {
 
@@ -11,5 +11,16 @@ data class Car internal constructor(
         position.incrementAndGet()
     } else {
         position.get()
+    }
+
+    override fun equals(other: Any?): Boolean = when {
+        this === other -> true
+        javaClass != other?.javaClass -> false
+        position != (other as Car).position -> false
+        else -> true
+    }
+
+    override fun hashCode(): Int {
+        return position.hashCode()
     }
 }
