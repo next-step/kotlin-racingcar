5e42cc0b8f0c7f9735e1ed063e66460fd2e6e54b
김민범 <44355413+mbkim95@users.noreply.github.com>
2023-06-08 11:04:58 +0900

    [Step1] 코틀린 기초 (#1035)
    * study: learn kotlin basics
    
    * study: learn kotest
    
    ---------
    
    Co-authored-by: tony.mb <tony.mb@kakaocorp.com>

---
+++ src/test/kotlin/study/StringTest.kt
@@ -0,0 +1,13 @@
+package study
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class StringTest {
+    @Test
+    fun isEmpty() {
+        assertThat("".isEmpty()).isTrue()
+        assertThat(" ".isEmpty()).isFalse()
+        assertThat("a".isEmpty()).isFalse()
+    }
+}
