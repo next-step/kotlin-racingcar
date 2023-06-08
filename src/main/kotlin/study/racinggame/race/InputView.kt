a53b34b1387144d465c481deedd1ccc0b3920b96
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2023-06-09 00:49:49 +0900

    refactor(InputView): setCommand -> 최초 생성 메서드로 변경

--- src/main/kotlin/study/racinggame/race/InputView.kt
+++ src/main/kotlin/study/racinggame/race/InputView.kt
@@ -2,22 +2,23 @@ package study.racinggame.race
 
 import study.racinggame.race.reader.InputReader
 
-class InputView(
-    private val inputReader: InputReader,
-) {
+class InputView {
 
     private var carCount: Int = 0
     private var raceTryCnt: Int = 0
+    private lateinit var reader: InputReader
 
-    fun setCommand() {
+    fun initInputView(inputReader: InputReader): InputView {
+        reader = inputReader
         carCount = getIntInput("자동차 대수는 몇 대인가요?", "자동차 대수를 숫자로 입력해주세요.")
         raceTryCnt = getIntInput("시도할 횟수는 몇 회인가요?", "시도할 횟수를 숫자로 입력해주세요.")
         println()
+        return this
     }
 
     private fun getIntInput(prompt: String, errorMsg: String): Int {
         println(prompt)
-        val input = inputReader.raedLine()
+        val input = reader.raedLine()
         require(input?.toIntOrNull() != null) { errorMsg }
         return input!!.toInt()
     }
