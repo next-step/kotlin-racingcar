64bf451ab2d08a2760f487de57cb6167b826b1fd
andy.dec <andy.dec@kakaostyle.com>
2023-06-08 23:05:49 +0900

    test: 자동차 대수 대신 이름을 입력하는 테스트 코드 작성

--- src/main/kotlin/step3/model/RaceGameErrorCode.kt
+++ src/main/kotlin/step3/model/RaceGameErrorCode.kt
@@ -7,10 +7,10 @@ enum class RaceGameErrorCode(val message: (String) -> String) {
     NOT_REMAINING_ROUND(
         message = { input -> "라운드 수가 남아있지 않습니다. 남은 라운드 수 : $input" },
     ),
-    INVALID_CAR_COUNT_INPUT(
-        message = { input -> "자동차 대수는 숫자형이어야 합니다. 입력값 : $input" },
-    ),
     INVALID_ROUND_INPUT(
         message = { input -> "라운드는 숫자형이어야 합니다. 입력값 : $input" },
+    ),
+    INVALID_CAR_NAME_INPUT(
+        message = { input -> "자동차 이름은 정해진 글자 수를 초과할 수 없습니다. 제한값 : $input" },
     ),
 }
--- src/test/kotlin/step3/view/RaceGameInputTest.kt
+++ src/test/kotlin/step3/view/RaceGameInputTest.kt
@@ -9,40 +9,54 @@ import step3.model.RaceGameErrorCode
 class RaceGameInputTest : DescribeSpec({
 
     describe(name = "레이스 게임을 위한 Input 객체를 만들 때") {
-        val expect = "123"
+        val names = "andy"
+        val round = "123"
 
         context(name = "숫자 문자열을 입력하면") {
             val actual = RaceGameInput(
-                carCountInput = expect,
-                roundInput = expect,
+                carNamesInput = names,
+                roundInput = round,
             )
 
             it(name = "숫자가 객체 내 저장된다.") {
-                actual.carCount shouldBe expect.toIntOrNull()
-                actual.round shouldBe expect.toIntOrNull()
+                actual.carNames.firstOrNull() shouldBe names
+                actual.round shouldBe round.toIntOrNull()
             }
         }
 
-        val invalidString = "안녕하세요"
+        val fiveNames = "andy,pobi,crong,honux,ace"
+
+        context(name = "5대의 자동차를 입력하면") {
+            val actual = RaceGameInput(
+                carNamesInput = fiveNames,
+                roundInput = round,
+            )
+
+            it(name = "다섯 대의 자동차가 저장된다.") {
+                actual.carNames.size shouldBe 5
+            }
+        }
+
+        val invalidString = "안녕하세요를레이요"
         val validNumberString = "123"
 
-        context(name = "자동차 대수에 숫자가 아닌 문자열을 입력하면") {
+        context(name = "자동차 대수에 5글자를 초과하는 문자열을 입력하면") {
             val exception = shouldThrow<IllegalArgumentException> {
                 RaceGameInput(
-                    carCountInput = invalidString,
+                    carNamesInput = invalidString,
                     roundInput = validNumberString,
                 )
             }
 
-            it(name = "자동차 대수는 숫자형이어야 한다는 에러가 발생한다.") {
-                exception shouldHaveMessage RaceGameErrorCode.INVALID_CAR_COUNT_INPUT.message(invalidString)
+            it(name = "자동차 이름은 정해진 글자 수를 초과할 수 없다는 에러가 발생한다.") {
+                exception shouldHaveMessage RaceGameErrorCode.INVALID_CAR_NAME_INPUT.message("5")
             }
         }
 
         context(name = "라운드에 숫자가 아닌 문자열을 입력하면") {
             val exception = shouldThrow<IllegalArgumentException> {
                 RaceGameInput(
-                    carCountInput = validNumberString,
+                    carNamesInput = validNumberString,
                     roundInput = invalidString,
                 )
             }
