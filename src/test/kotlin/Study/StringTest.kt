bddd3935d48a63bad28499b3ae30d638addf8c35
oliveunji <39396725+oliveunji@users.noreply.github.com>
2023-06-08 21:51:16 +0900

    Step1: 코틀린 기초 학습 (#1027)
    * study: Kotlin 클래스 학습
    
    * study: Kotlin 클래스 학습

---
+++ src/test/kotlin/Study/StringTest.kt
@@ -0,0 +1,11 @@
+package study
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class StringTest {
+    @Test
+    fun isEmpty() {
+        assertThat("".isEmpty()).isTrue
+    }
+}
