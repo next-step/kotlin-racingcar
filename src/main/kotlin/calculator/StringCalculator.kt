14c9b37b6f4913fe3036d69a7467073dc4c89e16
sjparkk <sjparkk0820@naver.com>
2023-06-08 21:17:17 +0900

    refactor : require 함수 이용 및 맞지 않는 연산 기호 사용 시 예외 처리 변경

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -65,18 +65,16 @@ class StringCalculator {
             }
 
             val operator = OperatorsEnum.fromChar(operators[i])
-            currentNum = operator?.calculate(currentNum, numbers[i + 1])!!
+            currentNum = operator?.calculate(currentNum, numbers[i + 1])
+                ?: throw throw IllegalArgumentException("연산 기호의 맞지 않는 기호가 존재합니다")
         }
         return currentNum
     }
 
     private fun valid(numbers: MutableList<Double>, operators: MutableList<Char>) {
-        if (numbers.size != operators.size + 1) {
-            throw IllegalArgumentException("연산 기호의 수는 연산 숫자의 수보다 무조건 1개 적어야 합니다")
-        }
 
-        if (operators.any { it != '+' && it != '-' && it != '*' && it != '/' }) {
-            throw IllegalArgumentException("연산 기호의 맞지 않는 기호가 존재합니다")
+        require(numbers.size == operators.size + 1) {
+            "연산 기호의 수는 연산 숫자의 수보다 무조건 1개 적어야 합니다"
         }
     }
 }
