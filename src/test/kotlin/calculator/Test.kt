1169fe4a1b102a0f4cb9e7d5203b3523b36de77f
yoo-jaein <y_jaein@naver.com>
2023-06-09 16:58:45 +0900

    feat: 계산기 기능에 대한 테스트 코드 작성

---
+++ src/test/kotlin/calculator/Test.kt
@@ -0,0 +1,55 @@
+package calculator
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
+import org.junit.jupiter.api.Test
+
+class Test {
+    @Test
+    fun 덧셈() {
+        val input = "1 + 2 + 3 + 4"
+        assertThat(Calculator(input).run()).isEqualTo(10.0)
+    }
+
+    @Test
+    fun 뺄셈() {
+        val input = "30 - 2 - 2"
+        assertThat(Calculator(input).run()).isEqualTo(26.0)
+    }
+
+    @Test
+    fun 곱셈() {
+        val input = "2 * 5"
+        assertThat(Calculator(input).run()).isEqualTo(10.0)
+    }
+
+    @Test
+    fun 나눗셈() {
+        val input = "100 / 10"
+        assertThat(Calculator(input).run()).isEqualTo(10.0)
+    }
+
+    @Test
+    fun `사칙 연산을 모두 포함`() {
+        val input = "2 + 3 * 4 / 2"
+        assertThat(Calculator(input).run()).isEqualTo(10.0)
+    }
+
+    @Test
+    fun `입력값이 빈 공백 문자일 경우 IllegalArgumentException`() {
+        assertThatExceptionOfType(IllegalArgumentException::class.java)
+            .isThrownBy {
+                val input = ""
+                Calculator(input).run()
+            }
+    }
+
+    @Test
+    fun `사칙연산 기호가 아닌 경우 IllegalArgumentException`() {
+        assertThatExceptionOfType(IllegalArgumentException::class.java)
+            .isThrownBy {
+                val input = "2 - 3 & 2"
+                Calculator(input).run()
+            }
+    }
+}
