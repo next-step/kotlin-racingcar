a02a29b483920b63935700a9ca89bfbdbbc98b1d
jonghyunhub <chuwhdgus@gmail.com>
2023-06-09 23:05:53 +0900

    feat : RacingGameApplication 추가

---
+++ src/main/kotlin/racingcar/RacingGameApplication.kt
@@ -0,0 +1,15 @@
+package racingcar
+
+import racingcar.domain.RacingGame
+
+fun main() {
+    RacingGameApplication().run()
+}
+class RacingGameApplication(private val racingGame: RacingGame) {
+
+    constructor():this(racingGame = RacingGame())
+
+    fun run() {
+        racingGame.start()
+    }
+}
