a02c2459ca81625c7e65f87d82d544259b44565c
1002533 <bf.winfive@sk.com>
2023-06-09 10:25:56 +0900

    step3: 구조 변경에 따른 수정

--- src/main/kotlin/InputView.kt
+++ src/main/kotlin/InputView.kt
@@ -1,26 +1,29 @@
-class InputView {
+object InputView {
 
-    var carNumber = 0
-    var actionCount = 0
+    const val DEFAULT_INPUT = 0
+    const val MINIMUM_INPUT = 1
 
-    fun doInput() {
+    fun doInput(): Pair<Int, Int> {
+        var carNumber = 0
+        var actionCount = 0
         println("자동차 대수는 몇 대인가요?")
         runCatching {
-            carNumber = readlnOrNull()?.toInt()?:0
+            carNumber = readlnOrNull()?.toInt() ?: DEFAULT_INPUT
         }.getOrElse {
             throw NumberFormatException("숫자 입력해야함")
         }
-        if(carNumber < 1) {
-            throw IllegalArgumentException("자동차 대수는 1대 이상이어야 함")
+        if (carNumber < MINIMUM_INPUT) {
+            throw IllegalArgumentException("자동차 대수는 ${MINIMUM_INPUT}대 이상이어야 함")
         }
         println("시도할 횟수는 몇 회인가요?")
         runCatching {
-            actionCount = readlnOrNull()?.toInt()?:0
+            actionCount = readlnOrNull()?.toInt() ?: DEFAULT_INPUT
         }.getOrElse {
             throw NumberFormatException("숫자 입력해야함")
         }
-        if(carNumber < 1) {
-            throw IllegalArgumentException("시도할 횟수는 1회 이상이어야 함")
+        if (actionCount < MINIMUM_INPUT) {
+            throw IllegalArgumentException("시도할 횟수는 ${MINIMUM_INPUT}회 이상이어야 함")
         }
+        return Pair(carNumber, actionCount)
     }
 }
