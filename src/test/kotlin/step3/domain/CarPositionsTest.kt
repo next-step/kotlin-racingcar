10e81fbe65416b9cd81567f8ee054b596e98ca06
judy.seo <judy.seo@kakaocorp.com>
2023-06-09 23:15:04 +0900

    test: 테스트 description 수정

--- src/test/kotlin/step3/domain/CarPositionsTest.kt
+++ src/test/kotlin/step3/domain/CarPositionsTest.kt
@@ -4,13 +4,13 @@ import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.collections.shouldContainExactly
 
 class CarPositionsTest : BehaviorSpec({
-    given("[moveBy 테스트] 자동차의 초기 위치가 0일 때") {
+    given("[moveBy 테스트] 자동차의 초기 위치가 0일 ") {
         val positions = CarPositions.of(size = 3, initialPosition = 0)
 
         `when`("movable이 true인 경우") {
             val movable: () -> Boolean = { true }
 
-            then("위치가 한칸씩 이동된다.") {
+            then("전진한다.") {
                 val result = positions.moveBy(movable)
                 result.getPositions() shouldContainExactly listOf(1, 1, 1)
 
@@ -20,7 +20,7 @@ class CarPositionsTest : BehaviorSpec({
         `when`("movable이 false인 경우") {
             val movable: () -> Boolean = { false }
 
-            then("위치는 변하지 않는다") {
+            then("멈춘다.") {
                 val result = positions.moveBy(movable)
                 result.getPositions() shouldContainExactly listOf(0, 0, 0)
             }
