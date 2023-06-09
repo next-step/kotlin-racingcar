7218f3f1ba4d7db80b2f26954cb58b0ef16afe5b
sjparkk <sjparkk0820@naver.com>
2023-06-09 20:46:58 +0900

    feat : View 관련 클래스 추가

---
+++ src/main/kotlin/racing/view/InputView.kt
@@ -0,0 +1,14 @@
+package racing.view
+
+class InputView {
+
+    companion object {
+        fun printInputCar() {
+            println("자동차 대수는 몇 대인가요?")
+        }
+
+        fun printInputCount() {
+            println("시도할 횟수는 몇 회인가요?")
+        }
+    }
+}
---
+++ src/main/kotlin/racing/view/ResultView.kt
@@ -0,0 +1,10 @@
+package racing.view
+
+class ResultView {
+
+    companion object {
+        fun printResult() {
+            println("실행 결과")
+        }
+    }
+}
