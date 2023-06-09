bd40e3481c5bd83a8eabc9a5e69c663d674db3ce
jonghoon ok <jonghoone94@gmail.com>
2023-06-09 15:39:31 +0900

    Step1 코틀린 기초 (#1075)
    * feat: Person 생성
    
    Person 클래스 및 테스트 작성
    
    * feat: Person 생성
    
    오타 수정
    
    ---------
    
    Co-authored-by: theo-94 <theo.94@kakaocorp.com>

---
+++ src/main/kotlin/study/Person.kt
@@ -0,0 +1,8 @@
+package study
+
+//Person.java를 코틀린으로 변환
+data class Person (
+    val name: String,
+    val age: Int,
+    var nickname: String? = "Guest"
+ )
---
+++ src/test/kotlin/study/PersonTest.kt
@@ -0,0 +1,38 @@
+package study
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+
+internal class PersonTest{
+    @Test
+    fun `named arguments`() {
+        val people = listOf(
+            Person("옥종훈", 29, "테오"),
+            Person("옥종훈", 29, nickname = "테오"),
+            Person(name = "옥종훈", nickname = "테오", age = 29)
+        )
+        assertThat(people).allSatisfy {
+            assertThat(it.name).isEqualTo("옥종훈")
+            assertThat(it.age).isEqualTo(29)
+            assertThat(it.nickname).isEqualTo("테오")
+        }
+    }
+
+    @Test
+    fun `default arguments`() {
+        val person = Person("옥종훈", 20)
+        assertAll(
+            { assertThat(person.name).isEqualTo("옥종훈") },
+            { assertThat(person.age).isEqualTo(20) },
+            { assertThat(person.nickname).isEqualTo("Guest") }
+        )
+    }
+
+    @Test
+    fun `data classes`() {
+        val person1 = Person("옥종훈", 29, "테오")
+        val person2 = Person("옥종훈", 29, "테오")
+        assertThat(person1).isEqualTo(person2)
+    }
+}
