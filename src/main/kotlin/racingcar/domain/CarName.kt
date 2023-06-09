c34c9015356a780dd7389806e34259d236ba0128
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-06-10 02:39:08 +0900

    refactor: 매직넘버 상수화

--- src/main/kotlin/racingcar/domain/CarName.kt
+++ src/main/kotlin/racingcar/domain/CarName.kt
@@ -6,6 +6,10 @@ value class CarName(
 ) {
     init {
         require(name.isNotBlank()) { "자동차의 이름은 공백이 들어올 수 없다" }
-        require(name.length < 5) { "자동차의 이름을 5자를 초과할 수 없다" }
+        require(name.length < LIMIT_LENGTH) { "자동차의 이름을 $LIMIT_LENGTH 자를 초과할 수 없다" }
+    }
+
+    companion object {
+        private const val LIMIT_LENGTH = 5
     }
 }
