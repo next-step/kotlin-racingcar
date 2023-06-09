9f1f1dd3a324ada4cae6541aa591355d1894d2a5
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-06-10 02:38:12 +0900

    refactor: CarName 반영

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -4,14 +4,9 @@ import java.util.Random
 
 class Car(
     var position: Int = INIT_POSITION,
-    val name: String,
+    val name: CarName,
     private val movable: () -> Boolean,
 ) {
-    init {
-        require(name.isNotBlank()) { "자동차의 이름은 공백이 들어올 수 없다" }
-        require(name.length < 5) { "자동차의 이름을 5자를 초과할 수 없다" }
-    }
-
     fun move(): Int {
         if (movable()) {
             return ++position
@@ -25,6 +20,6 @@ class Car(
         private const val MOVABLE_VALUE = 4
         private val random = Random()
 
-        fun createRandomMoveCar() = Car(name = "tmp") { random.nextInt(MAX_RANDOM_MOVE_INPUT) >= MOVABLE_VALUE }
+        fun createRandomMoveCar() = Car(name = CarName("tmp")) { random.nextInt(MAX_RANDOM_MOVE_INPUT) >= MOVABLE_VALUE }
     }
 }
--- src/test/kotlin/racingcar/domain/CarTest.kt
+++ src/test/kotlin/racingcar/domain/CarTest.kt
@@ -7,27 +7,27 @@ class CarTest {
 
     @Test
     fun `자동차는 현재 위치와 이름을 가진다`() {
-        val actual = Car(position = 1, name = "pobi") { true }
+        val actual = Car(position = 1, name = CarName("pobi")) { true }
         actual.position shouldBe 1
-        actual.name shouldBe "pobi"
+        actual.name shouldBe CarName("pobi")
     }
 
     @Test
     fun `자동차의 초기 위치는 0이다`() {
-        val actual = Car(name = "pobi") { true }
+        val actual = Car(name = CarName("pobi")) { true }
         actual.position shouldBe 0
     }
 
     @Test
     fun `movable이 true면 전진한다`() {
-        val car = Car(1, "pobi") { true }
+        val car = Car(1, CarName("pobi")) { true }
         val actual = car.move()
         actual shouldBe 2
     }
 
     @Test
     fun `movable이 false면 움직이지 않는다`() {
-        val car = Car(1, "pobi") { false }
+        val car = Car(1, CarName("pobi")) { false }
         val actual = car.move()
         actual shouldBe 1
     }
