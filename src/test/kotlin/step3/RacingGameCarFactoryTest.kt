5aae6e9a19cdded1c978985dbb76e64a2efcb012
nam-sangyun <nsun9505@naver.com>
2023-06-10 00:50:36 +0900

    test(자동차 생성)

---
+++ src/test/kotlin/step3/RacingGameCarFactoryTest.kt
@@ -0,0 +1,25 @@
+package step3
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class RacingGameCarFactoryTest {
+
+    @ParameterizedTest
+    @CsvSource(
+        "1, 1",
+        "3, 3",
+        "5, 5",
+        "100, 100"
+    )
+    fun `check generated cars count`(totalCarCount: Long, expected: Long) {
+        val cars = RacingGameCarFactory.createRandomDriveCars(
+            totalCarCount = totalCarCount,
+            randomNumberGenerator = RandomNumberGenerator(randomLimit = 1, randomBase = 1),
+            minDriveRandomNumber = 1
+        )
+
+        Assertions.assertThat(cars).hasSize(expected.toInt())
+    }
+}
