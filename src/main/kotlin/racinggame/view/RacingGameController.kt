f96832e672e553368eb0a7fd6b3e1fe90a391140
chad.yoon <chad.yoon@kakaocorp.com>
2023-06-09 16:21:38 +0900

    feat: 게임 횟수 입력 받는 기능 구현

--- src/main/kotlin/racinggame/view/RacingGameController.kt
+++ src/main/kotlin/racinggame/view/RacingGameController.kt
@@ -6,16 +6,34 @@ class RacingGameController(
 ) {
 
     fun start() {
-        outputView.displayMessage(RacingGameMessage.PARTICIPANTS_INPUT)
-        val participantsCount = receivePositiveIntegerFromViewOrNull()
-        if (participantsCount == null) {
-            outputView.displayMessage(RacingGameMessage.FINISH_WITH_NOT_POSITIVE_INTEGER)
+        val participantsCount = receiveParticipantsCountOrNull() ?: run {
+            displayErrorFinishMessage()
+            return
         }
+
+        val gamePlayCount = receiveGamePlayCountOrNull() ?: run {
+            displayErrorFinishMessage()
+            return
+        }
+    }
+
+    private fun receiveParticipantsCountOrNull(): Int? {
+        outputView.displayMessage(RacingGameMessage.PARTICIPANTS_INPUT)
+        return receivePositiveIntegerFromViewOrNull()
+    }
+
+    private fun receiveGamePlayCountOrNull(): Int? {
+        outputView.displayMessage(RacingGameMessage.GAME_PLAY_COUNT_INPUT)
+        return receivePositiveIntegerFromViewOrNull()
     }
 
     private fun receivePositiveIntegerFromViewOrNull(): Int? {
         return inputView.readLine()
             .toIntOrNull()
             ?.takeIf { it > 0 }
+    }
+
+    private fun displayErrorFinishMessage() {
+        outputView.displayMessage(RacingGameMessage.FINISH_WITH_NOT_POSITIVE_INTEGER)
     }
 }
--- src/main/kotlin/racinggame/view/RacingGameMessage.kt
+++ src/main/kotlin/racinggame/view/RacingGameMessage.kt
@@ -5,5 +5,6 @@ enum class RacingGameMessage(
 ) {
 
     PARTICIPANTS_INPUT("자동차 대수는 몇 대인가요?"),
+    GAME_PLAY_COUNT_INPUT("시도할 횟수는 몇 회인가요?"),
     FINISH_WITH_NOT_POSITIVE_INTEGER("양의 정수를 입력하지 않아 프로그램을 종료합니다."),
 }
