fb7210b742b84af74685c7ad788a9b98af1289c6
catsbi <killfine1988@gmail.com>
2023-06-10 10:43:55 +0900

    refactor(MoveStrategy): 람다 매개변수 위치 변경
    - 람다 매개변수를 끝으로 위치 변경

--- src/main/kotlin/racing/domain/strategy/StopOrStraightMoveStrategy.kt
+++ src/main/kotlin/racing/domain/strategy/StopOrStraightMoveStrategy.kt
@@ -5,8 +5,8 @@ import racing.domain.Distance
 
 class StopOrStraightMoveStrategy(private val additionalDistance: Distance) : MoveStrategy {
 
-    override fun move(movableStrategy: MovableStrategy, baseDistance: Distance): Distance {
-        return totalDistance(baseDistance).takeIf { movableStrategy.movable() == DirectionType.STRAIGHT } ?: Distance.ZERO
+    override fun move(baseDistance: Distance, movableStrategy: MovableStrategy): Distance {
+        return totalDistance(baseDistance).takeIf { movableStrategy.movable() == DirectionType.STRAIGHT } ?: baseDistance
     }
 
     private fun totalDistance(baseDistance: Distance) = additionalDistance + baseDistance
---
+++ src/test/kotlin/racing/domain/strategy/StopOrStraightMoveStrategyTest.kt
@@ -0,0 +1,36 @@
+package racing.domain.strategy
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import racing.domain.DirectionType
+import racing.domain.DirectionType.BACK
+import racing.domain.DirectionType.LEFT
+import racing.domain.DirectionType.RIGHT
+import racing.domain.DirectionType.STOP
+import racing.domain.Distance
+
+class StopOrStraightMoveStrategyTest : BehaviorSpec({
+    Given("전진 혹은 멈춤 전략은") {
+        When("이동 속도가 1이고") {
+            val strategy = StopOrStraightMoveStrategy(Distance(1L))
+
+            And("이동 타입이 STRAIGHT 인 경우") {
+                val actual = strategy.move(movableStrategy = { DirectionType.STRAIGHT }, baseDistance = Distance.ZERO)
+
+                Then("이동 거리는 1만큼 증가한다") {
+                    actual shouldBe Distance(1L)
+                }
+            }
+
+            listOf(STOP, BACK, LEFT, RIGHT).forEach {
+                And("이동 타입이 $it 인 경우") {
+                    val actual = strategy.move(movableStrategy = { it }, baseDistance = Distance.ZERO)
+
+                    Then("멈춰 있는다.") {
+                        actual shouldBe Distance.ZERO
+                    }
+                }
+            }
+        }
+    }
+})
