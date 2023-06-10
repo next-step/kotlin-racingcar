2643575d589995c3724493b521a3d5c25a903c7b
nam-sangyun <nsun9505@naver.com>
2023-06-11 00:55:22 +0900

    refactor(object로 변경)

--- src/main/kotlin/step3/RacingGameCarFactory.kt
+++ src/main/kotlin/step3/RacingGameCarFactory.kt
@@ -1,20 +1,18 @@
 package step3
 
-class RacingGameCarFactory {
+object RacingGameCarFactory {
 
-    companion object {
-        fun createRandomDriveCars(
-            totalCarCount: Long,
-            randomNumberGenerator: RandomNumberGenerator,
-            minDriveRandomNumber: Long
-        ) = (1..totalCarCount)
-            .map {
-                RandomDriveCar(
-                    carNumber = it,
-                    randomNumberGenerator = randomNumberGenerator,
-                    minDriveRandomNumber = minDriveRandomNumber,
-                )
-            }
-            .toList()
-    }
+    fun createRandomDriveCars(
+        totalCarCount: Long,
+        randomNumberGenerator: RandomNumberGenerator,
+        minDriveRandomNumber: Long
+    ) = (1..totalCarCount)
+        .map {
+            RandomDriveCar(
+                carNumber = it,
+                randomNumberGenerator = randomNumberGenerator,
+                minDriveRandomNumber = minDriveRandomNumber,
+            )
+        }
+        .toList()
 }
