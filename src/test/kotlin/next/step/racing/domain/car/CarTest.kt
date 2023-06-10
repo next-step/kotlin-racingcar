8261e228ab4a655254244604bec9109c7ff15bc8
genos-lee <genos.lee@kakaocorp.com>
2023-06-11 01:13:27 +0900

    feature : Car에 name property 추가하고 pos를 가변적으로 바꾸어 상태를 가지게 변경, 특정 위치에 있는지 확인하는 메소드와 name 제공 메소드 추가

--- src/test/kotlin/next/step/racing/domain/car/CarTest.kt
+++ src/test/kotlin/next/step/racing/domain/car/CarTest.kt
@@ -1,4 +1,4 @@
-package next.step.racing.domain
+package next.step.racing.domain.car
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.datatest.withData
@@ -6,9 +6,17 @@ import io.kotest.matchers.shouldBe
 import next.step.racing.util.DrivingStrategy
 
 class CarTest : FunSpec({
+    val name = CarName("GV80")
     context("position") {
+        val car = Car(name = name, pos = CarPosition(1))
         test("차의 시작 위치는 0") {
-            Car(Position(1)).position() shouldBe Position(1)
+            car.position() shouldBe 1
+        }
+        test("차가 지정된 위치에 있으면 참") {
+            car.isAt(CarPosition(1)) shouldBe true
+        }
+        test("차가 지정된 위치에 없으면 거짓") {
+            car.isAt(CarPosition(2)) shouldBe false
         }
     }
 
@@ -19,7 +27,10 @@ class CarTest : FunSpec({
             StrategyExpected(10, { 1 }, 11),
             StrategyExpected(20, { 5 }, 25)
         ) { (pos, strategy, expected) ->
-            Car(Position(pos)).move(strategy) shouldBe Car(Position(expected))
+            Car(name = name, pos = CarPosition(pos)).move(strategy) shouldBe Car(
+                name = name,
+                pos = CarPosition(expected)
+            )
         }
     }
 })
