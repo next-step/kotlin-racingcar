8154abd8819bd3ff0c51ce395537abf9bbddcc05
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2023-06-10 17:43:38 +0900

    refactor: Car 의 필드의 position 을 불변으로 변경
     - 기존 코드를 안건드리면서 수정하도록 유저가 입력한 값은 불변으로 수정
     - 이 도메인의 포지션 값을 postion() 메소드를 통해서만 접근 가능하도록 변경

--- src/main/kotlin/study/racinggame/domain/Car.kt
+++ src/main/kotlin/study/racinggame/domain/Car.kt
@@ -1,13 +1,14 @@
 package study.racinggame.domain
 
 class Car(
-    var position: Int = 0
+    _position: Int = 0
 ) {
-    fun moveForward(randomNumber: Int) {
-        if (randomNumber > FORWARD_THRESHOLD) position += 1
-    }
+
+    private var position = _position
 
     fun moveForward(moveAble: Boolean) {
         if (moveAble) position += 1
     }
+
+    fun position() = position
 }
