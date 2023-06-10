04f87269f5bc63a924ae2f67e3d5546d51c78734
yoo-jaein <y_jaein@naver.com>
2023-06-10 17:11:55 +0900

    refactor: 연산자 클래스 when 문 개선

--- src/main/kotlin/calculator/Operator.kt
+++ src/main/kotlin/calculator/Operator.kt
@@ -20,13 +20,11 @@ enum class Operator(
 
     companion object {
         fun of(newSign: Char): Operator {
-            return when (newSign) {
-                PLUS.sign -> PLUS
-                SUBTRACT.sign -> SUBTRACT
-                MULTIPLY.sign -> MULTIPLY
-                DIVIDE.sign -> DIVIDE
-                else -> throw IllegalArgumentException("사칙연산 기호가 아닙니다.")
-            }
+            return findBySign(newSign) ?: throw IllegalArgumentException("사칙연산 기호가 아닙니다.")
+        }
+
+        private fun findBySign(newSign: Char): Operator? {
+            return Operator.values().find { it.sign == newSign }
         }
     }
 }
