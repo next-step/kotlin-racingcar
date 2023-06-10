3d54284c619f14798069c2963dbb736753fbbe57
minhyukseul <mioyo2@gmail.com>
2023-06-10 20:34:25 +0900

    리팩토링 작업 진행

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,3 +1,4 @@
+
 object Calculator {
 
     private const val SPERATOR = " "
@@ -17,69 +18,10 @@ object Calculator {
     }
 
     private fun recursiveCalculator(formulaList: List<String>): String {
-        if (formulaList.size == LAST_INDEX) {
-            return formulaList[NUMBER_POSITION]
+        return if (formulaList.size == LAST_INDEX) {
+            formulaList[NUMBER_POSITION]
+        } else {
+            Operator.getOperator(formulaList[OPERATOR_POSITION]).calculate(recursiveCalculator(formulaList.drop(NEXT_PART_INDEX)), formulaList[NUMBER_POSITION])
         }
-
-        val operator = formulaList[OPERATOR_POSITION]
-        val number = formulaList[NUMBER_POSITION]
-
-        return when (Operator.findOperator(operator)) {
-            Operator.PLUS -> {
-                plus(recursiveCalculator(formulaList.slice(NEXT_PART_INDEX until formulaList.size)), number)
-            }
-            Operator.MINUS -> {
-                minus(recursiveCalculator(formulaList.slice(NEXT_PART_INDEX until formulaList.size)), number)
-            }
-            Operator.TIMES -> {
-                times(recursiveCalculator(formulaList.slice(NEXT_PART_INDEX until formulaList.size)), number)
-            }
-            Operator.DIVIDE -> {
-                divide(recursiveCalculator(formulaList.slice(NEXT_PART_INDEX until formulaList.size)), number)
-            }
-            else -> {
-                throw IllegalArgumentException("Cannot find operator")
-            }
-        }
-    }
-    fun plus(preNumber: String, postNumber: String): String {
-        if (!isNum(preNumber) || !isNum(postNumber)) {
-            throw IllegalArgumentException("Value must be Number.")
-        }
-        return (preNumber.toInt() + postNumber.toInt()).toString()
-    }
-
-    fun minus(preNumber: String, postNumber: String): String {
-        if (!isNum(preNumber) || !isNum(postNumber)) {
-            throw IllegalArgumentException()
-        }
-        return (preNumber.toInt() - postNumber.toInt()).toString()
-    }
-
-    fun times(preNumber: String, postNumber: String): String {
-        if (!isNum(preNumber) || !isNum(postNumber)) {
-            throw IllegalArgumentException()
-        }
-        return (preNumber.toInt() * postNumber.toInt()).toString()
-    }
-
-    fun divide(preNumber: String, postNumber: String): String {
-        if (!isNum(preNumber) || !isNum(postNumber)) {
-            throw IllegalArgumentException()
-        }
-        return (preNumber.toInt() / postNumber.toInt()).toString()
-    }
-
-    private fun isNum(numberString: String): Boolean {
-        numberString.forEach {
-            char ->
-
-            val charConvertedToCode = char.code
-
-            if (charConvertedToCode > 57 || charConvertedToCode < 48) {
-                return false
-            }
-        }
-        return true
     }
 }
--- src/test/kotlin/study/CalculatorKoTest.kt
+++ src/test/kotlin/study/CalculatorKoTest.kt
@@ -6,16 +6,16 @@ import io.kotest.matchers.shouldBe
 
 class CalculatorKoTest : StringSpec({
     "덧셈" {
-        Calculator.plus("2", "3") shouldBe "5"
+        Operator.getOperator("+").calculate("2", "3") shouldBe "5"
     }
     "뺄셈" {
-        Calculator.minus("5", "3") shouldBe "2"
+        Operator.getOperator("-").calculate("5", "3") shouldBe "2"
     }
     "곱셈" {
-        Calculator.times("2", "3") shouldBe "6"
+        Operator.getOperator("*").calculate("2", "3") shouldBe "6"
     }
     "나눗셈" {
-        Calculator.divide("6", "3") shouldBe "2"
+        Operator.getOperator("/").calculate("6", "3") shouldBe "2"
     }
     "계산식 테스트" {
         Calculator.calculate("2 + 3 * 4 / 2") shouldBe "10"
