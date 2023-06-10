f46728bbb839609be95af9c9c0571ac965fc96af
genos-lee <genos.lee@kakaocorp.com>
2023-06-10 14:35:16 +0900

    refactor : return 구문 생략

--- src/main/kotlin/next/step/racing/domain/Cars.kt
+++ src/main/kotlin/next/step/racing/domain/Cars.kt
@@ -5,22 +5,10 @@ import next.step.racing.service.DrivingStrategy
 data class Cars(val cars: List<Car>) {
     companion object {
         private val DEFAULT_POSITION = Position(0)
-        fun from(count: Int, pos: Position = DEFAULT_POSITION): Cars {
-            return Cars((1..count).map { Car(pos) })
-        }
+        fun from(count: Int, pos: Position = DEFAULT_POSITION): Cars = Cars((1..count).map { Car(pos) })
     }
 
-    fun move(drivingStrategy: DrivingStrategy): Cars {
-        return Cars(
-            cars.map { car ->
-                car.move(drivingStrategy)
-            }
-        )
-    }
+    fun move(drivingStrategy: DrivingStrategy): Cars = Cars(cars.map { car -> car.move(drivingStrategy) })
 
-    fun positions(): List<Int> {
-        return cars.map { car ->
-            car.position()
-        }
-    }
+    fun positions(): List<Int> = cars.map { car -> car.position() }
 }
--- src/main/kotlin/next/step/racing/domain/Position.kt
+++ src/main/kotlin/next/step/racing/domain/Position.kt
@@ -1,7 +1,5 @@
 package next.step.racing.domain
 
 data class Position(val x: Int) {
-    fun move(dx: Int): Position {
-        return Position(x + dx)
-    }
+    fun move(dx: Int): Position = Position(x + dx)
 }
