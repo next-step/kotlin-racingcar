54fd1888c818735f14d59d1a6cd5b2ba2ebb0e22
kyudong3 <kyudong3@gmail.com>
2023-06-10 22:38:56 +0900

    feat: enum 클래스로 옮긴 로직에 따라 수정

--- src/main/kotlin/study/step2/ArithmeticCalculator.kt
+++ src/main/kotlin/study/step2/ArithmeticCalculator.kt
@@ -1,54 +1,25 @@
 package study.step2
 
-import study.step2.Operator.ADD
-import study.step2.Operator.DIV
-import study.step2.Operator.MUL
-import study.step2.Operator.NONE
-import study.step2.Operator.SUB
-
 class ArithmeticCalculator {
 
-    private fun add(operand1: Int, operand2: Int) = operand1 + operand2
-    private fun sub(operand1: Int, operand2: Int) = operand1 - operand2
-    private fun mul(operand1: Int, operand2: Int) = operand1 * operand2
-    private fun div(operand1: Int, operand2: Int) = operand1 / operand2
-
-    fun calculate(input: String?): Int {
-        requireNotNull(input)
-        require(input.isNotEmpty())
+    fun calculate(inputExpression: String?): Int {
+        requireNotNull(inputExpression)
+        require(inputExpression.isNotEmpty())
 
         var result = 0
-        var operator = NONE
+        var inputOperator = ""
 
-        input.split(" ").forEach {
-            it.toIntOrNull()?.let { operand ->
-                result = operate(operator, operand, result)
-            } ?: run {
-                operator = setOperator(it)
+        inputExpression.split(" ").forEachIndexed { index, input ->
+            when {
+                index == 0 ->
+                    result = input.toInt()
+                index.isEven() ->
+                    result = ArithmeticOperator.operate(inputOperator, result, input.toInt())
+                else ->
+                    inputOperator = ArithmeticOperator.getSymbol(input)
             }
         }
 
         return result
     }
-
-    private fun operate(
-        inputOperator: Operator,
-        operand: Int,
-        result: Int
-    ) = when (inputOperator) {
-        ADD -> add(result, operand)
-        SUB -> sub(result, operand)
-        MUL -> mul(result, operand)
-        DIV -> div(result, operand)
-        NONE -> operand
-    }
-
-    private fun setOperator(inputOperator: String) =
-        when (inputOperator) {
-            "+" -> ADD
-            "-" -> SUB
-            "*" -> MUL
-            "/" -> DIV
-            else -> throw IllegalArgumentException()
-        }
 }
