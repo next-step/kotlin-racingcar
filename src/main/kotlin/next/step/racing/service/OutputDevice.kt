9ce2117352b05e3be1552663625d0b797989a955
genos-lee <genos.lee@kakaocorp.com>
2023-06-11 01:21:29 +0900

    feature : OutputDevice에서 기록과 우승자를 출력하도록 수정

--- src/main/kotlin/next/step/racing/service/OutputDevice.kt
+++ src/main/kotlin/next/step/racing/service/OutputDevice.kt
@@ -1,17 +1,29 @@
 package next.step.racing.service
 
+import next.step.racing.domain.car.CarRecord
+import next.step.racing.service.to.RacingResult
+
 object OutputDevice {
     private const val UNKNOWN_ERR_MSG = "알 수 없는 에러가 발생했습니다."
     private const val RESULT_TITLE = "실행 결과"
-    fun showResult(allPositions: List<List<Int>>) {
+    fun showResult(result: RacingResult) {
         println()
         println(RESULT_TITLE)
-        for (positions in allPositions) {
-            for (position in positions) {
-                println("-".repeat(position))
+        showRecords(result.records)
+        showWinners(result.winners)
+    }
+
+    private fun showRecords(allRecords: List<List<CarRecord>>) {
+        for (records in allRecords) {
+            for (record in records) {
+                println("${record.name} : ${"-".repeat(record.pos)}")
             }
             println()
         }
+    }
+
+    private fun showWinners(winners: List<String>) {
+        println("${winners.joinToString(", ")}가 최종 우승했습니다.")
     }
 
     fun showError(msg: String?) {
