34fb2ab3ae8fc8e99634cc174ba36ee013259aef
sjparkk <sjparkk0820@naver.com>
2023-06-10 23:55:26 +0900

    test : 4이상인 경우와 아닌 경우에 자동차 이동 여부 테스트 추가

--- src/test/kotlin/racing/RacingTest.kt
+++ src/test/kotlin/racing/RacingTest.kt
@@ -3,6 +3,7 @@ package racing
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import racing.domain.Car
 import racing.view.InputView
 import racing.view.ResultView
 
@@ -26,5 +27,23 @@ class RacingTest {
     @CsvSource("3")
     fun `자동차 입력 수 만큼 객체 생성 검증`(carCount: Int) {
         assertThat(Racing().createCars(carCount).size).isEqualTo(carCount)
+    }
+
+    @ParameterizedTest
+    @CsvSource("5")
+    fun `4 이상의 랜덤 숫자가 나와 자동차 이동 검증`(randomNumber: Int) {
+
+        val car = Car()
+        Racing().moveAndStop(car, randomNumber)
+        assertThat(car.position).isEqualTo(1)
+    }
+
+    @ParameterizedTest
+    @CsvSource("3")
+    fun `4 이상의 랜덤 숫자가 나오지 않을 시 자동차 정지 검증`(randomNumber: Int) {
+
+        val car = Car()
+        Racing().moveAndStop(car, randomNumber)
+        assertThat(car.position).isEqualTo(0)
     }
 }
