df5c8ce36c5edf8cb157b81aed15579154d44c02
maenguin <maenguin924@gmail.com>
2023-06-10 21:19:32 +0900

    step3 : Main 수정

--- src/main/kotlin/step3/Main.kt
+++ src/main/kotlin/step3/Main.kt
@@ -1,30 +1,15 @@
 package step3
 
-import kotlin.random.Random
+import step3.domain.RacingSimulator
+import step3.view.InputView
+import step3.view.ResultView
 
 fun main() {
-    println("자동차 대수는 몇 대인가요?")
-    val carCount = readln().toInt()
-    println("시도할 횟수는 몇 회인가요?")
-    val tryCount = readln().toInt()
-    println("실행 결과")
-    val cars = List(carCount) { Car() }
-    repeat(tryCount) { index ->
-        println("try ${index + 1}")
-        cars.forEach { car ->
-            car.move()
-            car.printPosition()
-        }
-    }
-}
+    val inputView = InputView()
+    val resultView = ResultView()
+    val racingSimulator = RacingSimulator()
 
-class Car(private var position: Int = 0) {
-    fun move() {
-        val random = Random.nextInt(1, 10)
-        position += if (random >= 4) 1 else 0
-    }
-
-    fun printPosition() {
-        println("-".repeat(position))
-    }
+    val racingRequest = inputView.inputRacingInfo()
+    val racingRecord = racingSimulator.race(racingRequest)
+    resultView.outputRacingResult(racingRecord)
 }
