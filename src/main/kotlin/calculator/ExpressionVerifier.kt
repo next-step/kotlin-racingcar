9ca3c6f49fc4672389e15952ec9cb79f8b5381ed
mbkim95 <mbkim95@naver.com>
2023-06-10 21:15:56 +0900

    refactor: fix the code in the ExpressionVerifier to be concise

--- src/main/kotlin/calculator/ExpressionVerifier.kt
+++ src/main/kotlin/calculator/ExpressionVerifier.kt
@@ -8,16 +8,12 @@ object ExpressionVerifier {
         val elements = ExpressionParser.parse(input)
         for (i in elements.indices) {
             if (i % 2 == 0) {
-                try {
-                    elements[i].toInt()
-                } catch (e: NumberFormatException) {
-                    throw IllegalArgumentException("Input is incorrect")
-                }
+                elements[i].toBigDecimalOrNull() ?: throw IllegalArgumentException("Input is incorrect")
             } else {
-                when (elements[i]) {
-                    "+", "-", "*", "/" -> continue
-                    else -> throw IllegalArgumentException("Input is incorrect")
+                if (elements[i] in setOf("+", "-", "*", "/")) {
+                    continue
                 }
+                throw IllegalArgumentException("Input is incorrect")
             }
         }
     }
