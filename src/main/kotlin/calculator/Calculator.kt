6858f853a6870763e36cc437e1bfe8836b1e75b2
Wordbe <seonghojin3@gmail.com>
2023-06-10 17:06:51 +0900

    feat(Operator): Operator 객체 분리

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -2,8 +2,8 @@ package calculator
 
 object Calculator {
     private const val FIRST_OPERAND_INDEX = 0
-    private const val FIRST_OPERATOR_INDEX = 0
-    private const val OPERATION_UNIT = 0
+    private const val FIRST_OPERATOR_INDEX = 1
+    private const val OPERATION_UNIT = 2
 
     fun calculate(expression: String): Int {
         require(expression.isNotBlank()) { "입력값이 공백입니다." }
@@ -12,24 +12,11 @@ object Calculator {
 
         var result = parts[FIRST_OPERAND_INDEX].toInt()
         for (i in FIRST_OPERATOR_INDEX until parts.size step OPERATION_UNIT) {
-            val operator = parts[i]
+            val operator = Operator.of(parts[i])
             val nextNumber = parts[i + 1].toInt()
-            result = operate(result, operator, nextNumber)
+            result = operator.operate(result, nextNumber)
         }
 
         return result
-    }
-
-    private fun operate(operand1: Int, operator: String, operand2: Int): Int {
-        return when (operator) {
-            "+" -> operand1 + operand2
-            "-" -> operand1 - operand2
-            "*" -> operand1 * operand2
-            "/" -> {
-                require(operand2 != 0) { "0으로 나눌 수 없습니다." }
-                operand1 / operand2
-            }
-            else -> throw IllegalArgumentException("사칙연산 기호가 올바르지 않습니다. : $operator")
-        }
     }
 }
---
+++ src/main/kotlin/calculator/Operator.kt
@@ -0,0 +1,25 @@
+package calculator
+
+enum class Operator(
+    private val symbol: String,
+    val operate: (Int, Int) -> Int,
+) {
+    ADD("+", { operand1, operand2 -> operand1 + operand2 }),
+    SUBTRACT("-", { operand1, operand2 -> operand1 - operand2 }),
+    MULTIPLY("*", { operand1, operand2 -> operand1 * operand2 }),
+    DIVIDE("/", { operand1, operand2 ->
+        require(operand2 != 0) { "0으로 나눌 수 없습니다." }
+        operand1 / operand2
+    }),
+    ;
+
+    companion object {
+        fun of(symbol: String): Operator {
+            return values().filter { it.symbol == symbol }
+                .run {
+                    require(isNotEmpty()) { "사칙연산 기호가 올바르지 않습니다. : $symbol" }
+                    first()
+                }
+        }
+    }
+}
