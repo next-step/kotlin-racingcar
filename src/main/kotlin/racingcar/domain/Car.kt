2724dc269c4fbe267caa372e8ae0057f6a32b589
jonghyunhub <chuwhdgus@gmail.com>
2023-06-10 15:43:17 +0900

    refact : Car 객체 움직일 조건 함수화 및 조건 숫자 상수화

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -2,13 +2,14 @@ package racingcar.domain
 
 class Car {
     private var location = 1
+    private val BASE_NUMBER_TO_MOVE = 4
 
     fun isMoveOrStop() {
         val randomNum = generateRandomNum()
-        if (randomNum >= 4) {
-            location++
-        }
+        if (conditionToMove(randomNum)) location++
     }
+
+    private fun conditionToMove(randomNum: Int) = randomNum >= BASE_NUMBER_TO_MOVE
 
     private fun generateRandomNum(): Int {
         return (0..9).random()
