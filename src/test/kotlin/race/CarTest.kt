754474c3d222f0a33b8a54f8407b90a42329ae26
yoo-jaein <y_jaein@naver.com>
2023-06-10 19:28:32 +0900

    feat: 자동차 클래스에 대한 테스트 코드 작성

---
+++ src/test/kotlin/race/CarTest.kt
@@ -0,0 +1,19 @@
+package race
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class CarTest {
+    @Test
+    fun `기본 위치는 0`() {
+        val car = Car()
+        assertThat(car.getPosition()).isEqualTo(0)
+    }
+
+    @Test
+    fun `한 번 이동한 경우 위치는 0(후진) 또는 1(직진)`() {
+        val car = Car()
+        car.move()
+        assertThat(car.getPosition()).isIn(0, 1)
+    }
+}
