f9b4fb25d66c0ecd1e8944d57531ff4e271f86ff
genos-lee <genos.lee@kakaocorp.com>
2023-06-11 02:23:47 +0900

    refactor : 차 이름 최대 글자 상수로 사용

--- src/main/kotlin/next/step/racing/domain/car/CarName.kt
+++ src/main/kotlin/next/step/racing/domain/car/CarName.kt
@@ -5,6 +5,10 @@ value class CarName(val name: String) {
 
     init {
         require(name.isNotBlank()) { "차 이름은 공백이 아니어야 합니다." }
-        require(name.length <= 5) { "차 이름은 5자 이하여야 합니다." }
+        require(name.length <= CAR_NAME_MAX_LENGTH) { "차 이름은 5자 이하여야 합니다." }
+    }
+
+    companion object {
+        private const val CAR_NAME_MAX_LENGTH = 5
     }
 }
