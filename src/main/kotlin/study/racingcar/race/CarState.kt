4442d3e54f0a7e1b129301706eaa6a07fecf43a0
waveofmymind <sjun0913@gmail.com>
2023-06-10 13:32:41 +0900

    Feat: 메소드 책임 클래스 이동

--- src/main/kotlin/study/racingcar/race/CarState.kt
+++ src/main/kotlin/study/racingcar/race/CarState.kt
@@ -3,8 +3,4 @@ package study.racingcar.race
 data class CarState(
     val name: String,
     val position: Int
-) {
-    fun getVisualPosition(): String = "-".repeat(position)
-
-    fun getPositionLength(): Int = position
-}
+)
