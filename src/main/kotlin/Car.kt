b6a707e3f36c1b4c3f1e1e50f66f90d40bc7283e
김태훈 <sksskaw@naver.com>
2023-06-11 03:45:56 +0900

    car 구현

--- src/main/kotlin/Car.kt
+++ src/main/kotlin/Car.kt
@@ -1,1 +1,13 @@
-class Car
+class Car(
+    val FORWARD_CONDITIONS: Int = 4,
+    var distance: Int = 0
+) {
+    fun move() {
+        if (checkForwardCondition())
+            distance++
+    }
+
+    private fun checkForwardCondition(): Boolean {
+        return RandomNumberGenerator.getRandomNumber() >= FORWARD_CONDITIONS
+    }
+}
