b003c5c5f1f5182159e490964fdeb48045e2d13c
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2023-06-10 21:29:22 +0900

    refactor: Race에서 View들을 분리, 이제는 트랙과 우승자 찾기만 책임짐

--- src/main/kotlin/study/racinggame/domain/Race.kt
+++ src/main/kotlin/study/racinggame/domain/Race.kt
@@ -1,34 +1,21 @@
 package study.racinggame.domain
 
 import study.racinggame.race.RaceRule
-import study.racinggame.race.view.DrawView
-import study.racinggame.race.view.InputView
 
 class Race(
-    private val inputView: InputView,
+    private val carStorage: List<Car>,
     private val raceRule: RaceRule
 ) {
 
-    private var carStorage: List<Car> = inputView.carNames()
-        .map { carName -> Car(name = carName) }
-    private val drawView: DrawView = DrawView()
-
-    fun startRace() {
-        repeat(inputView.raceTryCnt()) {
-            moveCarsIfPossible()
-            val carResponses = carStorage.map(CarResponse.Companion::of)
-
-            drawView.drawCarState(carResponses)
-        }
+    fun runTrack(): List<CarResponse> {
+        moveCarsIfPossible()
+        return carStorage.map(CarResponse.Companion::of)
     }
 
-    fun noticeRaceWinner() {
+    fun getRaceWinner(): String {
         val maxValue: Int = carStorage.maxOf { each -> each.position() }
-
-        val winnerName: String = carStorage.filter { each -> each.position() == maxValue }
+        return carStorage.filter { each -> each.position() == maxValue }
             .joinToString(", ") { winner -> winner.name }
-
-        drawView.drawRaceWinner(winnerName)
     }
 
     fun carStorage() = carStorage
--- src/main/kotlin/study/racinggame/race/view/InputView.kt
+++ src/main/kotlin/study/racinggame/race/view/InputView.kt
@@ -1,25 +1,26 @@
 package study.racinggame.race.view
 
+import study.racinggame.domain.Car
 import study.racinggame.race.reader.InputReader
 
 class InputView {
 
-    private var carNames = listOf<String>()
+    private var carStorage = listOf<Car>()
     private var raceTryCnt: Int = 0
     private lateinit var reader: InputReader
 
     fun initInputView(inputReader: InputReader): InputView {
         reader = inputReader
-        carNames = getCarNames("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+        carStorage = getCarNames("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
         raceTryCnt = getIntInput("시도할 횟수는 몇 회인가요?", "시도할 횟수를 숫자로 입력해주세요.")
         println()
         return this
     }
 
-    private fun getCarNames(prompt: String): List<String> {
+    private fun getCarNames(prompt: String): List<Car> {
         println(prompt)
         val input = reader.raedLine()
-        return input.split(",")
+        return input.split(",").map { carName -> Car(name = carName) }
     }
 
     private fun getIntInput(prompt: String, errorMsg: String): Int {
@@ -29,8 +30,8 @@ class InputView {
         return input.toInt()
     }
 
-    fun carNames(): List<String> {
-        return carNames
+    fun carStorage(): List<Car> {
+        return carStorage
     }
 
     fun raceTryCnt(): Int {
