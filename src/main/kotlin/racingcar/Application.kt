d577e6976ea04200b0b9f1458f65fbb19bef6ebd
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-06-10 15:56:44 +0900

    feat: 우승자 이름 출력 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -5,6 +5,7 @@ import racingcar.view.inputAttemptCount
 import racingcar.view.inputCarCount
 import racingcar.view.printCurrentResult
 import racingcar.view.printResultTitle
+import racingcar.view.printWinner
 
 fun main() {
     val racingGame = createRandomMoveCarRacingGame(carCount = inputCarCount(), attemptCount = inputAttemptCount())
@@ -13,4 +14,5 @@ fun main() {
     while (racingGame.isRunnable()) {
         printCurrentResult(racingGame.run())
     }
+    printWinner(racingGame.winner())
 }
--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -2,10 +2,15 @@ package racingcar.view
 
 private const val RESULT_TITLE = "실행 결과"
 private const val POSITION = "-"
+private const val WINNER_DELIMITER = ","
 
 fun printResultTitle() = println(RESULT_TITLE)
 
 fun printCurrentResult(carPositions: List<Pair<String, Int>>) {
     carPositions.forEach { println("${it.first} : ${POSITION.repeat(it.second)}") }
     println()
+}
+
+fun printWinner(winner: List<String>) {
+    println("${winner.joinToString(WINNER_DELIMITER)}가 최종 우승했습니다.")
 }
