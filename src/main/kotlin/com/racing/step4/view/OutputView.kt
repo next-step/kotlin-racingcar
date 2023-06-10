85b4e081c4e88a90c18f2b4342eb3a102204c34e
goofy-919 <goofy.919@kakaocorp.com>
2023-06-11 00:30:41 +0900

    feat: OutputView 구현

---
+++ src/main/kotlin/com/racing/step4/view/OutputView.kt
@@ -0,0 +1,14 @@
+package com.racing.step4.view
+
+class OutputView {
+    fun showRacingResultDashboard(stageResultList: List<String>) {
+        println("실행 결과")
+        stageResultList.forEach {
+            println(it)
+        }
+    }
+
+    fun showWinner(winners: String) {
+        print(winners + "가 최종 우승했습니다.")
+    }
+}
