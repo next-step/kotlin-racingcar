74ecb17098cc84b50da17209b479488b2dfba59e
qktlf789456 <54522339+qktlf789456@users.noreply.github.com>
2023-06-11 00:24:18 +0900

    feat: RaceOutputViewer

---
+++ src/main/kotlin/racingcar/view/RaceOutputViewer.kt
@@ -0,0 +1,15 @@
+package racingcar.view
+
+import racingcar.domain.RaceStatus
+
+interface RaceOutputViewer {
+    fun view(raceStatus: RaceStatus)
+}
+
+class ConsoleRaceOutputViewer : RaceOutputViewer {
+    override fun view(raceStatus: RaceStatus) {
+        raceStatus.cars.forEach {
+            println("-".repeat(it.location.location.toInt()))
+        }
+    }
+}
