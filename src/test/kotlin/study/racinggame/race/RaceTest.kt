152d4098d42afa4f10d71ad920c378833fc30325
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2023-06-10 21:31:22 +0900

    test: 우승자의 이름을 가져오는 기능 테스트 추가

--- src/test/kotlin/study/racinggame/race/RaceTest.kt
+++ src/test/kotlin/study/racinggame/race/RaceTest.kt
@@ -2,32 +2,42 @@ package study.racinggame.race
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import study.racinggame.domain.Car
 import study.racinggame.domain.Race
-import study.racinggame.race.mock.MockInputReader
 import study.racinggame.race.mock.MockRandom
-import study.racinggame.race.view.InputView
 
 class RaceTest : StringSpec({
-    val mockInputReader = MockInputReader(listOf("3", "5"))
-    val inputView = InputView().initInputView(mockInputReader)
-
     "모든 차가 앞으로 나가는 경우 테스트" {
+        val testCars = listOf(Car(name = "test1"), Car(name = "test2"))
         val mockRaceRule = RaceRule(MockRandom(Int.MAX_VALUE))
-        val race = Race(inputView, mockRaceRule)
-        race.startRace()
+        val race = Race(testCars, mockRaceRule)
+        race.runTrack()
 
         for (car in race.carStorage()) {
-            car.position() shouldBe 5
+            car.position() shouldBe 2
         }
     }
 
     "모든 차가 앞으로 나가지 못한 테스트" {
+        val testCars = listOf(Car(name = "test1"), Car(name = "test2"))
         val mockRaceRule = RaceRule(MockRandom(Int.MIN_VALUE))
-        val race = Race(inputView, mockRaceRule)
-        race.startRace()
+        val race = Race(testCars, mockRaceRule)
+        race.runTrack()
 
         for (car in race.carStorage()) {
-            car.position() shouldBe 0
+            car.position() shouldBe 1
         }
+    }
+
+    "우승자의 이름을 가져올 수 있다." {
+        val testCars = listOf(Car(777, "test1"), Car(0, "test2"))
+        val race = Race(testCars, RaceRule())
+        race.getRaceWinner() shouldBe "test1"
+    }
+
+    "우승자가 여러명이면 전부 가져온다." {
+        val testCars = listOf(Car(777, "test1"), Car(777, "test2"), Car(777, "test3"))
+        val race = Race(testCars, RaceRule())
+        race.getRaceWinner() shouldBe "test1, test2, test3"
     }
 })
