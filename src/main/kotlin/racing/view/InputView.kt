92c0a4602be637106d645f12ac5e8d767a137175
sjparkk <sjparkk0820@naver.com>
2023-06-10 23:05:34 +0900

    refactor : object 로 변경 (static 메소드만 존재)

--- src/main/kotlin/racing/view/InputView.kt
+++ src/main/kotlin/racing/view/InputView.kt
@@ -1,16 +1,14 @@
 package racing.view
 
-class InputView {
+object InputView {
 
-    companion object {
-        fun printInputCar(): Int {
-            println("자동차 대수는 몇 대 인가요?")
-            return readlnOrNull()?.toInt() ?: throw IllegalArgumentException("입력 값은 null 값이 올 수 없습니다")
-        }
+    fun printInputCar(): Int {
+        println("자동차 대수는 몇 대 인가요?")
+        return readlnOrNull()?.toInt() ?: throw IllegalArgumentException("입력 값은 null 값이 올 수 없습니다")
+    }
 
-        fun printInputCount(): Int {
-            println("시도할 횟수는 몇 회 인가요?")
-            return readlnOrNull()?.toInt() ?: throw IllegalArgumentException("입력 값은 null 값이 올 수 없습니다")
-        }
+    fun printInputCount(): Int {
+        println("시도할 횟수는 몇 회 인가요?")
+        return readlnOrNull()?.toInt() ?: throw IllegalArgumentException("입력 값은 null 값이 올 수 없습니다")
     }
 }
--- src/main/kotlin/racing/view/ResultView.kt
+++ src/main/kotlin/racing/view/ResultView.kt
@@ -1,14 +1,12 @@
 package racing.view
 
-class ResultView {
+object ResultView {
 
-    companion object {
-        fun printResult() {
-            println("실행 결과")
-        }
+    fun printResult() {
+        println("실행 결과")
+    }
 
-        fun printDistance(distance: String) {
-            println(distance)
-        }
+    fun printDistance(distance: String) {
+        println(distance)
     }
 }
