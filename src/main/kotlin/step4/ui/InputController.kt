c34842fa19fa4e9ce72e61d4f3bd50ff89891547
hjjae2 <hjjae2@gmail.com>
2023-06-11 01:08:26 +0900

    [STEP4] (UI) InputController 클래스 작성

---
+++ src/main/kotlin/step4/ui/InputController.kt
@@ -0,0 +1,25 @@
+package step4.ui
+
+object InputController {
+    fun input(): InputValue {
+        val nameOfCars = printAndReadString("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)").split(",")
+        val numberOfTrials = printAndReadInt("시도할 회수는 몇 회 인가요?")
+
+        return InputValue(nameOfCars, numberOfTrials)
+    }
+
+    private fun printAndReadString(message: String): String {
+        println(message)
+        return readlnOrNull() ?: ""
+    }
+
+    private fun printAndReadInt(message: String): Int {
+        println(message)
+        return readlnOrNull()?.toInt() ?: 0
+    }
+}
+
+class InputValue(
+    val nameOfCars: List<String>,
+    val numberOfTrials: Int,
+)
