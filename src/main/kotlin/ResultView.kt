1e102668d8b3155d729e109edc36e659e11d7724
김태훈 <sksskaw@naver.com>
2023-06-11 06:09:10 +0900

    안내 메세지 view 클래스 구현

--- src/main/kotlin/ResultView.kt
+++ src/main/kotlin/ResultView.kt
@@ -1,1 +1,9 @@
-class ResultView
+class ResultView {
+    fun carsDistanceDisplay(distance: Int) {
+        var displayString = ""
+        for (i in 1..distance) {
+            displayString = "$displayString-"
+        }
+        println(displayString)
+    }
+}
