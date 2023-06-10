f3616b129df2aa13decbc41f546eafd1e642320f
chad.yoon <chad.yoon@kakaocorp.com>
2023-06-10 21:05:34 +0900

    feat: 엔진 테스트를 위해 랜덤 값을 생성하기 위한 랜덤 generator를 엔진에 주입

--- src/main/kotlin/racinggame/domain/car/engine/RandomEnergyEngine.kt
+++ src/main/kotlin/racinggame/domain/car/engine/RandomEnergyEngine.kt
@@ -1,9 +1,11 @@
 package racinggame.domain.car.engine
 
-class RandomEnergyEngine : Engine {
+class RandomEnergyEngine(
+    private val randomNumberGenerator: RandomNumberGenerator
+) : Engine {
 
     override fun execute(): DrivableDistance {
-        val energy = (MINIMUM_ENERGY..MAXIMUM_ENERGY).random()
+        val energy = randomNumberGenerator.generate(MINIMUM_ENERGY..MAXIMUM_ENERGY)
         return DrivableDistance(if (energy >= MINIMUM_REQUIRE_ENERGY) FIXED_DRIVABLE_DISTANCE else 0)
     }
 
