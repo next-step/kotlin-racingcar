16d0c38b494e3299ce8b551b35fa2aabef1f77e3
antaeseon <ants412@naver.com>
2023-06-10 23:47:41 +0900

    feat: 계산기 구현

---
+++ src/main/kotlin/calculator/Operator.kt
@@ -0,0 +1,19 @@
+package calculator
+
+enum class Operator(
+    val opName: String,
+    val exec: (Int, Int) -> Int
+) {
+
+    PLUS("+", { x, y -> x + y }),
+    MINUS("-", { x, y -> x - y }),
+    MULTIPLY("*", { x, y -> x * y }),
+    DIVIDE("/", { x, y -> x / y });
+
+    companion object {
+        fun of(symbol: String): Operator {
+            return values().find { it.opName == symbol }
+                ?: throw IllegalArgumentException("사칙연산 기호를 사용하지 않았습니다.")
+        }
+    }
+}
