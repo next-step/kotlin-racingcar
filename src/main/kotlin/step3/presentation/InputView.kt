07dadd7fe72f3748bc8ffd94a370127bfde38ba7
Yongho Choi <73802331+yongho2157@users.noreply.github.com>
2023-06-10 20:40:15 +0900

    Step3: 자동차 경주 (#1096)
    * Step2: 피드백 수정
    
    * Step2: Calculator main 함수 제거
    
    * Step3: 자동차 경주
    
    ---------
    
    Co-authored-by: yonghoChoi <studyyongho2157@gmail.com>

---
+++ src/main/kotlin/step3/presentation/InputView.kt
@@ -0,0 +1,10 @@
+package step3.presentation
+
+class InputView {
+
+    fun inputData(): Int = try {
+        readln().toInt()
+    } catch (e: NumberFormatException) {
+        throw NumberFormatException("Cannot convert the character to an integer.")
+    }
+}
--- src/test/kotlin/study/Calculator.kt
+++ src/test/kotlin/study/Calculator.kt
@@ -3,13 +3,19 @@ package study
 class Calculator {
 
     fun calculate(expression: String): Int {
-        if (expression.isEmpty()) throw IllegalArgumentException("Input value is null or empty")
+        if (expression.isEmpty()) throw IllegalArgumentException("Input value is empty")
         val tokenList = expression.split(" ").toList()
-        var result = tokenList[0].toInt()
+        var result: Int
+
+        try {
+            result = tokenList[0].toInt()
+        } catch (e: NumberFormatException) {
+            throw NumberFormatException("The first value is not a number.")
+        }
 
         for (i in 1 until tokenList.size - 1 step 2) {
             if (tokenList[i].isEmpty() || tokenList[i + 1].isEmpty()) throw IllegalArgumentException("Input value is null or empty")
-            result = Calculator().executeOperation(
+            result = executeOperation(
                 number1 = result, operator = tokenList[i], number2 = tokenList[i + 1].toInt()
             )
         }
