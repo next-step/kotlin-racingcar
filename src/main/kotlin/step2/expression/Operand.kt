8163d193d2fb2a077e0a01b735b84d73c71c66a9
maenguin <maenguin924@gmail.com>
2023-06-10 12:55:13 +0900

    step2 : 피드백 반영 - Operand class에 data 클래스 적용

--- src/main/kotlin/step2/expression/Operand.kt
+++ src/main/kotlin/step2/expression/Operand.kt
@@ -7,7 +7,7 @@ import java.math.BigDecimal
  *
  * '연산자'에 의해 연산이 호출될때 실제로 계산하는 역할을 담당합니다.
  */
-class Operand(operand: String) : ExpressionItem {
+data class Operand(private val operand: String) : ExpressionItem {
     private val value: BigDecimal = BigDecimal(operand)
 
     operator fun plus(target: Operand): Operand {
@@ -27,20 +27,5 @@ class Operand(operand: String) : ExpressionItem {
 
     override fun toString(): String {
         return value.toString()
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Operand
-
-        if (value != other.value) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return value.hashCode()
     }
 }
