9c2d0f24e3c81f405114c6f439cee22be2b5e93a
jonghyunhub <chuwhdgus@gmail.com>
2023-06-11 23:38:03 +0900

    refact : racingGame 구조 수정

---
+++ src/main/kotlin/racingcar/domain/RacingGame.kt
@@ -0,0 +1,36 @@
+package racingcar.domain
+
+import racingcar.view.InputView
+
+/**
+ * 자동차 게임 지휘자 객체
+ */
+class RacingGame {
+
+    private val inputView: InputView = InputView()
+    private val round: Round = Round()
+    private val cars: ArrayList<Car> = ArrayList()
+
+    fun start() {
+        val (carNumber, roundNumber) = getInput()
+        initCars(cars = cars, carNumber = carNumber)
+        startRound(cars = cars, roundNumber = roundNumber)
+    }
+
+    private fun getInput(): Pair<Int, Int> {
+        val carNumber = inputView.getCarNumber()
+        val roundNumber = inputView.getRoundNumber()
+        return Pair(carNumber, roundNumber)
+    }
+
+    private fun initCars(cars: ArrayList<Car>, carNumber: Int) {
+        for (i in 0 until carNumber)
+            cars.add(Car())
+    }
+
+    private fun startRound(cars: ArrayList<Car>, roundNumber: Int) {
+        for (i in 0 until roundNumber) {
+            round.executeRound(cars)
+        }
+    }
+}
