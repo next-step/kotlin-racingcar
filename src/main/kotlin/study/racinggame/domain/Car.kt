17a2d124a7a47ddede519a913673474bae9f04ea
동성 서 <sds901234@tradlinx.com>
2023-06-11 17:26:21 +0900

    feat: 자동차 게임 우승자 기능 추가
    - Car 클래스 생성 시, 이름에 대한 Validation 추가

--- src/main/kotlin/study/racinggame/domain/Car.kt
+++ src/main/kotlin/study/racinggame/domain/Car.kt
@@ -3,6 +3,12 @@ package study.racinggame.domain
 class Car(val name: String, private val engine: CarEngine) {
     private var position: Int = 1
 
+    init {
+        if (name.isBlank() || name.length > 5) {
+            throw IllegalArgumentException()
+        }
+    }
+
     fun move(): CarPosition {
         position += engine.run()
         return CarPosition(name, position)
--- src/test/kotlin/study/racinggame/domain/CarTest.kt
+++ src/test/kotlin/study/racinggame/domain/CarTest.kt
@@ -1,7 +1,9 @@
 package study.racinggame.domain
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import java.lang.IllegalArgumentException
 
 class BrokenEngine : CarEngine {
     override fun run(): Int {
@@ -17,27 +19,51 @@ class NormalEngine : CarEngine {
 
 class CarTest : StringSpec({
 
+    "자동차 이름이 빈 문자열이면 생성 시 오류가 발생 한다." {
+        // given
+        val name = ""
+
+        // when & then
+        shouldThrow<IllegalArgumentException> { Car(name, NormalEngine()) }
+    }
+
+    "자동차 이름이 공백 문자열이면 생성 시 오류가 발생 한다." {
+        // given
+        val name = "  "
+
+        // when & then
+        shouldThrow<IllegalArgumentException> { Car(name, NormalEngine()) }
+    }
+
+    "자동차 이름이 5글자를 초과하면 생성 시 오류가 발생 한다." {
+        // given
+        val name = ""
+
+        // when & then
+        shouldThrow<IllegalArgumentException> { Car(name, NormalEngine()) }
+    }
+
     "자동차 엔진이 움직이는데 성공하면 자동차의 위치가 변경 된다." {
         // given
-        val car = Car("Porsche", NormalEngine())
+        val car = Car("1st", NormalEngine())
 
         // when
         val currentPosition = car.move()
 
         // then
-        currentPosition.name shouldBe "Porsche"
+        currentPosition.name shouldBe "1st"
         currentPosition.position shouldBe 2
     }
 
     "자동차 엔진이 움직이는데 실패하면 자동차의 위치가 변경 되지 않는다." {
         // given
-        val car = Car("Morning", BrokenEngine())
+        val car = Car("2nd", BrokenEngine())
 
         // when
         val currentPosition = car.move()
 
         // then
-        currentPosition.name shouldBe "Morning"
+        currentPosition.name shouldBe "2nd"
         currentPosition.position shouldBe 1
     }
 })
