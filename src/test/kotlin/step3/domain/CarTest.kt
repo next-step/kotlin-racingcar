3fe1dfb3e42b34ff6f362c1d935ea23225d9ac64
maenguin <maenguin924@gmail.com>
2023-06-11 15:46:07 +0900

    step4 : step3 피드백 반영 - @CsvSource로 테스트 통합

--- src/test/kotlin/step3/domain/CarTest.kt
+++ src/test/kotlin/step3/domain/CarTest.kt
@@ -2,28 +2,24 @@ package step3.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
+import org.junit.jupiter.params.provider.CsvSource
 
 internal class CarTest {
     @ParameterizedTest
-    @ValueSource(ints = [4, 5, 6])
-    internal fun `무작위 값이 4 이상일 경우 전진`(randomValue: Int) {
+    @CsvSource(
+        "1, 0",
+        "2, 0",
+        "3, 0",
+        "4, 1",
+        "5, 1",
+        "6, 1",
+    )
+    internal fun `무작위 값이 4 이상일 경우 전진`(randomValue: Int, expectedPosition: Int) {
         // given : 최초 위치가 0인 자동차 준비
         val sut = Car()
         // when : 무작위 값이 4이상 일때 전진 시도
         sut.move(randomValue)
-        // then : 전진 성공, 위치 1로 변경
-        assertThat(sut.position).isEqualTo(1)
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = [1, 2, 3])
-    internal fun `무작위 값이 4 미만일 경우 전진 실패`(randomValue: Int) {
-        // given : 최초 위치가 0인 자동차 준비
-        val sut = Car()
-        // when : 무작위 값이 4 미만일때 전진 시도
-        sut.move(randomValue)
-        // then : 전진 실패, 위치 0
-        assertThat(sut.position).isEqualTo(0)
+        // then : 전진 성공시 position 1 아니면 position 0
+        assertThat(sut.position).isEqualTo(expectedPosition)
     }
 }
