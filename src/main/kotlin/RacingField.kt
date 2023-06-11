32d051856e6b00bc77babbdaeca11a8fe1c7a20b
김태훈 <sksskaw@naver.com>
2023-06-11 19:54:27 +0900

    RacingField의 ResultView 의존성 제거

--- src/main/kotlin/RacingField.kt
+++ src/main/kotlin/RacingField.kt
@@ -2,8 +2,6 @@ class RacingField(
     val cars: MutableList<Car> = mutableListOf(),
     private var gameCount: Int = 0
 ) {
-    private val resultView = ResultView()
-
     fun createCars(count: Int) {
         for (i in 1..count) {
             cars.add(Car())
@@ -19,11 +17,8 @@ class RacingField(
     }
 
     fun gameStart() {
-        resultView.resultMessage()
-
         for (i in 1..gameCount) {
             moveCars()
-            resultView.carDistanceDisplay(cars)
         }
     }
 
