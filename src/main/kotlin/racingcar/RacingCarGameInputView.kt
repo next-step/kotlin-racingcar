0702e499b4c847e8d17c845a651ef5c4de529f03
nam-sangyun <nsun9505@naver.com>
2023-06-11 15:19:12 +0900

    feature(자동차 이름 입력)

--- src/main/kotlin/racingcar/RacingCarGameInputView.kt
+++ src/main/kotlin/racingcar/RacingCarGameInputView.kt
@@ -7,8 +7,18 @@ class RacingCarGameInputView {
         readln().toLong()
     }
 
+    val carNames: List<String> by lazy {
+        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)를 기준으로 구분).")
+        val inputCarNames = readlnOrNull()
+        inputCarNames?.split(delimiters = arrayOf(INPUT_DELIMITERS)) ?: emptyList()
+    }
+
     val totalRound: Long by lazy {
         println("시도할 횟수는 몇 대인가요?")
         readln().toLong()
+    }
+
+    companion object {
+        const val INPUT_DELIMITERS = " "
     }
 }
