c6fc4dc0d8b3dafc0cdcb3cedebb83ad84454673
nam-sangyun <nsun9505@naver.com>
2023-06-11 16:13:30 +0900

    refactor(출력메서드 분리)

--- src/main/kotlin/racingcar/RacingCarGameResultView.kt
+++ src/main/kotlin/racingcar/RacingCarGameResultView.kt
@@ -6,23 +6,39 @@ class RacingCarGameResultView {
         println()
         println(RESULT_TITLE)
 
+        printRoundResults(gameResult, totalRound)
+
+        printGameWinners(winnerNames = gameResult.winnerNames)
+    }
+
+    private fun printRoundResults(gameResult: RacingCarGameResult, totalRound: Long) {
         val racingCarRoundResult = gameResult.gameRoundResults
 
         racingCarRoundResult.sortedBy { it.round }.forEach { roundResult ->
-            roundResult.carDriveResults.sortedBy { it.carNumber }.forEach {
-                print("${it.carName} : ")
-                repeat(it.position.toInt()) {
-                    print(OUTPUT_CHARACTER)
-                }
-                println()
-            }
+            printRoundResult(roundResult, totalRound)
+        }
+    }
 
-            if (roundResult.round < totalRound) {
-                println()
-            }
+    private fun printRoundResult(roundResult: RacingCarRoundResult, totalRound: Long) {
+        roundResult.carDriveResults.sortedBy { it.carNumber }.forEach {
+            printCarRoundResult(it)
         }
 
-        printGameWinners(winnerNames = gameResult.winnerNames)
+        printRoundResultLastLine(roundResult, totalRound)
+    }
+
+    private fun printRoundResultLastLine(roundResult: RacingCarRoundResult, totalRound: Long) {
+        if (roundResult.round < totalRound) {
+            println()
+        }
+    }
+
+    private fun printCarRoundResult(result: RacingCarDriveResult) {
+        print("${result.carName} : ")
+        repeat(result.position.toInt()) {
+            print(OUTPUT_CHARACTER)
+        }
+        println()
     }
 
     private fun printGameWinners(winnerNames: List<String>) {
