487b4e0701af574b6afe8d4836e006b83b986cea
konempty <jameswebb@naver.com>
2023-06-11 21:03:54 +0900

    refactor: 0으로 나눴을때 에러 처리

--- src/main/kotlin/calculator/Operator.kt
+++ src/main/kotlin/calculator/Operator.kt
@@ -4,7 +4,12 @@ enum class Operator(private val value: String, val operate: (Long, Long) -> Long
     PLUS("+", { operand1, operand2 -> operand1 + operand2 }),
     MINUS("-", { operand1, operand2 -> operand1 - operand2 }),
     MUL("*", { operand1, operand2 -> operand1 * operand2 }),
-    DIV("/", { operand1, operand2 -> operand1 / operand2 });
+    DIV("/", { operand1, operand2 ->
+        if (operand2 == 0L) {
+            throw IllegalArgumentException("0으로 나눌 수 없습니다.")
+        }
+        operand1 / operand2
+    });
 
     operator fun invoke(operand1: Long, operand2: Long): Long = operate(operand1, operand2)
 
