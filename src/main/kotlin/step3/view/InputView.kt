90d529013dda11f1446de55412d8657ac7bef8d8
Songyi Kim <ksl2950@naver.com>
2023-06-11 16:56:11 +0900

    feat: 요구사항 변경에 따라 view 동작 수정

--- src/main/kotlin/step3/view/InputView.kt
+++ src/main/kotlin/step3/view/InputView.kt
@@ -1,9 +1,9 @@
 package step3.view
 
 object InputView {
-    fun requestNumberOfCars(): Int {
-        println("자동차 대수는 몇 대인가요?")
-        return readln().toInt()
+    fun requestNameOfCars(): List<String> {
+        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+        return readln().split(",").toList()
     }
 
     fun requestCountOfTrial(): Int {
--- src/main/kotlin/step3/view/OutputView.kt
+++ src/main/kotlin/step3/view/OutputView.kt
@@ -1,14 +1,25 @@
 package step3.view
 
+import step3.domain.Car
+
 object OutputView {
     fun printResult() {
-        println("실행 결과")
+        println("\n실행 결과")
     }
 
-    fun printByResults(results: List<Int>) {
-        results.map { result ->
-            println("-".repeat(result))
+    fun printPositionOfCars(cars: List<Car>) {
+        cars.map { car ->
+            println("${car.name} : ${"-".repeat(car.getPosition())}")
         }
         println()
+    }
+
+    fun printWinner(results: List<Car>) {
+        val maxPosition = results.maxBy { it.getPosition() }.getPosition()
+
+        val winners = results.filter { it.getPosition() == maxPosition }
+            .map { it.name }
+
+        println("${winners.joinToString(", ")}가 최종 우승했습니다.")
     }
 }
