ee4ea6320d1bebf5fe9d1f38bcdec5d65f3e33fc
devyonghee <devyonghee@gmail.com>
2023-06-12 02:44:31 +0900

    fix(CarName): 위치 수정

--- src/main/kotlin/racingcar/model/CarName.kt
+++ src/main/kotlin/racingcar/model/CarName.kt
@@ -7,11 +7,11 @@ value class CarName(private val name: String) {
         require(name.length <= MAX_NAME_LENGTH) { "car name must be less than $MAX_NAME_LENGTH characters. but provided name(`$name`)" }
     }
 
-    companion object {
-        private const val MAX_NAME_LENGTH: Int = 5
-    }
-
     override fun toString(): String {
         return name
+    }
+
+    companion object {
+        private const val MAX_NAME_LENGTH: Int = 5
     }
 }
