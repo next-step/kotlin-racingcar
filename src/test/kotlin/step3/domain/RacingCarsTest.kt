237efd061cfdee2774f73a9bf0ca44c90aa19bb1
maenguin <maenguin924@gmail.com>
2023-06-11 17:55:17 +0900

    step4 : step3 피드백 반영 - RacingCars와 RacingSimulator 테스트 추가

---
+++ src/test/kotlin/step3/domain/RacingCarsTest.kt
@@ -0,0 +1,25 @@
+package step3.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import step3.domain.strategy.AlwaysMoveCondition
+import step3.domain.strategy.NeverMoveCondition
+
+internal class RacingCarsTest {
+    @Test
+    internal fun `레이싱 테스트`() {
+        // given : 항상 전진하는 1개의 자동차와 항상 전진하지 않는 1개의 자동차 준비
+        val cars = listOf(
+            Car(AlwaysMoveCondition()),
+            Car(NeverMoveCondition())
+        )
+        val racingCars = RacingCars(cars)
+
+        // when : 2번의 전진 시도를 가지고 레이싱 수행
+        val racingRecord = racingCars.race(2)
+
+        // then : 항상 전진하는 자동차는 2번 전진, 항상 전진하지 않는 자동차는 0번 전진
+        val expectedRacingRecord = listOf(listOf(1, 0), listOf(2, 0))
+        assertThat(racingRecord.record).containsExactlyElementsOf(expectedRacingRecord)
+    }
+}
