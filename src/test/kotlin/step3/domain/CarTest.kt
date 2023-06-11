5da899bdb8622ac6d9353cf912f8d19614c49b68
Songyi Kim <ksl2950@naver.com>
2023-06-11 16:56:56 +0900

    test: 도메인 별 테스트 코드 작성

--- src/test/kotlin/step3/domain/CarTest.kt
+++ src/test/kotlin/step3/domain/CarTest.kt
@@ -2,16 +2,38 @@ package step3.domain
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import step3.fake.FakeNumberGenerator
 
 class CarTest : FunSpec({
-    test("car 는 전진할 수 있다.") {
+    test("전진 조건을 충족하는 car 는 전진할 수 있다.") {
         // given
-        val car = Car(1)
+        val fakeNumberGenerator = FakeNumberGenerator(5)
+        val carMoveCondition = CarMoveCondition(fakeNumberGenerator)
+        val carName = "song2"
+        val currentPosition = 2
+        val expectedPosition = 3
+        val car = Car(carName, currentPosition, carMoveCondition)
 
         // when
         car.move()
 
         // then
-        car.getPosition() shouldBe Car.FIRST_POSITION + 1
+        car.getPosition() shouldBe expectedPosition
+    }
+
+    test("전진 조건을 충족하지 않는 car 는 전진할 수 없다.") {
+        // given
+        val fakeNumberGenerator = FakeNumberGenerator(3)
+        val carMoveCondition = CarMoveCondition(fakeNumberGenerator)
+        val carName = "song2"
+        val currentPosition = 2
+        val expectedPosition = 2
+        val car = Car(carName, currentPosition, carMoveCondition)
+
+        // when
+        car.move()
+
+        // then
+        car.getPosition() shouldBe expectedPosition
     }
 })
--- src/test/kotlin/step3/fake/FakeNumberGenerator.kt
+++ src/test/kotlin/step3/fake/FakeNumberGenerator.kt
@@ -2,14 +2,8 @@ package step3.fake
 
 import step3.domain.NumberGenerator
 
-class NumberGeneratorGreaterThan4 : NumberGenerator {
+class FakeNumberGenerator(private val number: Int) : NumberGenerator {
     override fun generate(): Int {
-        return 4
-    }
-}
-
-class NumberGeneratorLessThan4 : NumberGenerator {
-    override fun generate(): Int {
-        return 2
+        return number
     }
 }
