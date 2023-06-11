4992693699a1db7fb0f009caa8836a5dc57516a3
choihwan2 <choihwan2@naver.com>
2023-06-12 02:21:30 +0900

    Add Calculator interface

---
+++ src/main/kotlin/step2/Calculator.kt
@@ -0,0 +1,8 @@
+package step2
+
+interface Calculator {
+    fun plus(first: Double, second: Double): Double
+    fun minus(first: Double, second: Double): Double
+    fun divide(first: Double, second: Double): Double
+    fun multiply(first: Double, second: Double): Double
+}
