ab20d95d90c09b9d394fddbcf85c75f21bd77c6d
yoo-jaein <y_jaein@naver.com>
2023-06-12 01:00:19 +0900

    refactor: 코드 블록 순서 변경

--- src/main/kotlin/race/model/Car.kt
+++ src/main/kotlin/race/model/Car.kt
@@ -4,17 +4,11 @@ class Car(
     private val name: String,
     private val moveCondition: MoveCondition,
 ) {
-    init {
-        validate()
-    }
-
     var position: Int = 0
         private set
 
-    private fun validate() {
-        if (5 < name.length) {
-            throw IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.")
-        }
+    init {
+        validate()
     }
 
     fun copyRecord(): CarRecord {
@@ -24,6 +18,12 @@ class Car(
     fun move() {
         if (moveCondition.canGoStraight()) {
             straight()
+        }
+    }
+
+    private fun validate() {
+        if (5 < name.length) {
+            throw IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.")
         }
     }
 
