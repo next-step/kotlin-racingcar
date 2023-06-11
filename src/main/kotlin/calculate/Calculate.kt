1371ed023b1811e39c0755b43f557bafdc78bb64
livenow14 <xowjd41@naver.com>
2023-06-11 16:53:18 +0900

    [step2] refactor: 코드리뷰 반영
    - kotest 버전업
    - 타입추론 사용
    - Kotlin Collection의 확장 함수 사용
    - 네이밍 변경 operatate -> operate
    - 테스트 삭제 (나눗셈 - 대상값이 0일때)
    - Kotlin에서 제공하는 require, check 함수를 활용

--- src/main/kotlin/calculate/Calculate.kt
+++ src/main/kotlin/calculate/Calculate.kt
@@ -2,7 +2,6 @@ package calculate
 
 import java.util.ArrayDeque
 import java.util.Deque
-import java.util.stream.Collectors
 
 class Calculate(private val validations: List<CalculateValidation>) {
 
@@ -11,9 +10,9 @@ class Calculate(private val validations: List<CalculateValidation>) {
     fun calculate(input: String): Int {
         validateInput(input)
         val operators = input.split("\\d+".toRegex()).filter { it.isNotBlank() }
-        val numbers: Deque<Int> = extractAndConvertNumber(input, operators)
+        val numbers = extractAndConvertNumber(input, operators)
         for (operator in operators) {
-            val operatedValue = operatate(numbers, operator)
+            val operatedValue = operate(numbers, operator)
             numbers.addFirst(operatedValue)
         }
         return numbers.pop()
@@ -26,14 +25,14 @@ class Calculate(private val validations: List<CalculateValidation>) {
     }
 
     private fun extractAndConvertNumber(input: String, operators: List<String>): Deque<Int> {
-        var result = input.split(*operators.toTypedArray())
-        return result.stream()
+        val result = input.split(*operators.toTypedArray())
+        return result
             .map(String::trim)
             .map(String::toInt)
-            .collect(Collectors.toCollection { ArrayDeque() })
+            .toCollection(ArrayDeque())
     }
 
-    private fun operatate(numbers: Deque<Int>, operator: String): Int {
+    private fun operate(numbers: Deque<Int>, operator: String): Int {
         val first = numbers.pop()
         val second = numbers.pop()
         return when (operator) {
@@ -46,9 +45,6 @@ class Calculate(private val validations: List<CalculateValidation>) {
     }
 
     private fun divide(first: Int, second: Int): Int {
-        if (first == 0) {
-            return 0
-        }
         if (second == 0) {
             throw IllegalArgumentException("0으로 나눌 수 없습니다.")
         }
