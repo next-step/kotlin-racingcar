0620c1d716e8b28360960c5377e50e0442530682
oliveunji <eunjikim2u@gmail.com>
2023-06-11 22:45:36 +0900

    feat: 브랜치 셋업, 입출력 구현

---
+++ src/main/kotlin/racingCar/RacingCar.kt
@@ -0,0 +1,10 @@
+package racingCar
+
+fun main() {
+    println("자동차 대수는 몇 대인가요?")
+    val numberOfCar = readLine()
+    println("시도할 횟수는 몇 회인가요?")
+    val count = readLine()
+
+    print("$numberOfCar, $count")
+}
