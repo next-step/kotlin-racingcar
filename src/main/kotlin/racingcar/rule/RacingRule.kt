4d81acd383e73c7878ba1b64f97e33d42926b2ad
mbkim95 <mbkim95@naver.com>
2023-06-11 20:55:36 +0900

    feat: implement the logic to determine the winner

--- src/main/kotlin/racingcar/rule/RacingRule.kt
+++ src/main/kotlin/racingcar/rule/RacingRule.kt
@@ -1,5 +1,9 @@
 package racingcar.rule
 
+import racingcar.model.Car
+
 interface RacingRule {
     fun canMove(): Boolean
+
+    fun getWinners(cars: List<Car>): List<String>
 }
--- src/test/kotlin/racingcar/TestDouble.kt
+++ src/test/kotlin/racingcar/TestDouble.kt
@@ -1,11 +1,16 @@
 package racingcar
 
+import racingcar.model.Car
 import racingcar.rule.RacingRule
 
 class AlwaysMoveRule : RacingRule {
     override fun canMove(): Boolean = true
+
+    override fun getWinners(cars: List<Car>): List<String> = cars.map { it.name }
 }
 
 class NeverMoveRule : RacingRule {
     override fun canMove(): Boolean = false
+
+    override fun getWinners(cars: List<Car>): List<String> = cars.map { it.name }
 }
