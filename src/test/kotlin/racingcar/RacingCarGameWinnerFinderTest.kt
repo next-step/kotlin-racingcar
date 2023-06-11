ac086be619b3545bd45b405318a52b8bb3daea26
nam-sangyun <nsun9505@naver.com>
2023-06-11 16:45:27 +0900

    test(자동차 우승자 찾기)

---
+++ src/test/kotlin/racingcar/RacingCarGameWinnerFinderTest.kt
@@ -0,0 +1,30 @@
+package racingcar
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class RacingCarGameWinnerFinderTest {
+
+    @Test
+    fun `check winner names`() {
+        val lastRound: Long = 5
+        val winnerName = "austin"
+        val winnerPosition = 10
+        val carNames = listOf(winnerName, "b", "c")
+
+        val carDriveResults = carNames.map {
+            RacingCarDriveResult(
+                carNumber = 1,
+                position = (if (it == winnerName) winnerPosition else winnerPosition - 1).toLong(),
+                carName = it
+            )
+        }
+
+        val gameRoundResults = listOf(RacingCarRoundResult(round = lastRound, carDriveResults = carDriveResults))
+
+        val result =
+            RacingCarGameWinnerFinder.findWinnerNames(gameRoundResults = gameRoundResults, lastRound = lastRound)
+
+        assertThat(result).isEqualTo(listOf(winnerName))
+    }
+}
