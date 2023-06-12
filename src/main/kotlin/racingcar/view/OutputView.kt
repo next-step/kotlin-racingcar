e9e218b44e7779f414b896c9d20e4ac019f40a2e
jonghyunhub <chuwhdgus@gmail.com>
2023-06-12 10:58:04 +0900

    feat : OutputViewProtocol 추가

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -2,13 +2,19 @@ package racingcar.view
 
 import racingcar.domain.Car
 
-class OutputView {
+class OutputView : OutputViewProtocol {
 
-    fun printRound(cars: ArrayList<Car>) {
-        for (car in cars) {
-            car.printLocation()
+    // fun printRound(cars: ArrayList<Car>) {
+    //     for (car in cars) {
+    //         car.printLocation()
+    //     }
+    //     println()
+    // }
+
+    override fun printValue(value: Int) {
+        for (i in 0 until value) {
+            print("-")
         }
         println()
     }
-
 }
---
+++ src/main/kotlin/racingcar/view/OutputViewProtocol.kt
@@ -0,0 +1,5 @@
+package racingcar.view
+
+interface OutputViewProtocol {
+    fun printValue(value: Int)
+}
