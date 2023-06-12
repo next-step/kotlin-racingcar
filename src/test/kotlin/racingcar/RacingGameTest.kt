370b90fb2f6c415b2495b9754fd2210bf32541b6
celine.kk <celine.kk@kakaocorp.com>
2023-06-12 18:46:42 +0900

    feat. 레이싱게임 테스트 반대의 결과 추가

--- src/test/kotlin/racingcar/RacingGameTest.kt
+++ src/test/kotlin/racingcar/RacingGameTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 
 internal class RacingGameTest {
     @Test
-    internal fun `모든 자동차가 전진이 나왔을 때 score가 전부 1이 되었는지 확인`() {
+    internal fun `모든 자동차의 게임결과가 전진이 나왔을 때 score가 전부 1이 되었는지 확인`() {
         val records = RacingGame.play(
             3,
             GameRecords(CarFactory.manufacture(3)),
@@ -22,6 +22,25 @@ internal class RacingGameTest {
             assertThat(records.cars[0].score).isEqualTo(1)
             assertThat(records.cars[1].score).isEqualTo(1)
             assertThat(records.cars[2].score).isEqualTo(1)
+        }
+    }
+
+    @Test
+    internal fun `모든 자동차의 게임 결과가 전진이 나오지 않았을 때 score가 전부 0이 되었는지 확인`() {
+        val records = RacingGame.play(
+            2,
+            GameRecords(CarFactory.manufacture(2)),
+            object : MoveStrategy {
+                override fun move(): Boolean {
+                    return false
+                }
+            }
+        )
+
+        SoftAssertions.assertSoftly {
+            assertThat(records.cars.size).isEqualTo(2)
+            assertThat(records.cars[0].score).isEqualTo(0)
+            assertThat(records.cars[1].score).isEqualTo(0)
         }
     }
 }
