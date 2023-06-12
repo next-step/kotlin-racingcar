353cd60e612f225f02742db884e7be511782ff09
김태훈 <sksskaw@naver.com>
2023-06-12 21:06:39 +0900

    StringParserTest 테스트 코드 작성

---
+++ src/test/kotlin/StringParserTest.kt
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import java.lang.NumberFormatException
+
+class StringParserTest {
+    @ParameterizedTest
+    @CsvSource(
+        "10, NUMBER",
+        "*, OPERATOR"
+    )
+    fun parseStringTest(input: String, expected: StringType) {
+        val result = StringParser.parseString(input)
+        Assertions.assertEquals(expected, result)
+    }
+
+    @Test
+    fun numberFormatExceptionTest() {
+        Assertions.assertThrows(NumberFormatException::class.java) {
+            StringParser.parseString("45.11")
+        }
+    }
+}
