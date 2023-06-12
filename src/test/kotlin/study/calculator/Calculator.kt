fcc1ba2026d2d3f027aac126cf08e3be4ee8492f
송시은 <sieun.song@woowahan.com>
2023-06-12 22:36:17 +0900

    feat: implement calculator

---
+++ src/test/kotlin/study/calculator/Calculator.kt
@@ -0,0 +1,33 @@
+package study.calculator
+
+class Calculator {
+    fun calculate(question: String?): Int {
+        if (question.isNullOrEmpty()) throw IllegalArgumentException("question was null or empty.")
+
+        val splits = question.split(" ")
+        var result = splits[0].toInt()
+
+        splits.forEachIndexed { index, s ->
+            when (s) {
+                "+" -> plus(result, splits[index + 1].toInt())
+                "-" -> minus(result, splits[index + 1].toInt())
+                "*" -> multiple(result, splits[index + 1].toInt())
+                "/" -> divider(result, splits[index + 1].toInt())
+                else -> {
+                    s.toIntOrNull() ?: throw IllegalArgumentException("question allows only integer or +, -, * and /.")
+                    result
+                }
+            }.let { result = it }
+        }
+
+        return result
+    }
+
+    private fun plus(num1: Int, num2: Int) = num1 + num2
+
+    private fun minus(num1: Int, num2: Int) = num1 - num2
+
+    private fun multiple(num1: Int, num2: Int) = num1 * num2
+
+    private fun divider(num1: Int, num2: Int) = num1 / num2
+}
