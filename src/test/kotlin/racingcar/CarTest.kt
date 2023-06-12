5247cc5ffd52c4b04b3ee0b327b4b02793c84d27
celine.kk <celine.kk@kakaocorp.com>
2023-06-12 14:48:16 +0900

    feat. Car() CarFactory()에 대한 테스트 생성

--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -1,17 +1,20 @@
-package racing
+package racingcar
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import racingcar.RacingGame
 
-class RacingGameTest {
+class CarTest {
     @Test
     internal fun `랜덤값이 4이상일 때 전진이 나온다`() {
-        assertThat(RacingGame.isMove(4)).isEqualTo(1)
+        val car = Car()
+        car.move(4)
+        assertThat(car.score).isEqualTo(1)
     }
 
     @Test
     internal fun `랜덤값이 4이상일 때는 전진하지 않는다`() {
-        assertThat(RacingGame.isMove(3)).isEqualTo(0)
+        val car = Car()
+        car.move(3)
+        assertThat(car.score).isEqualTo(0)
     }
 }
