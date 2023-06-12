26db42808088c379219e4ecc57f2d803cf99f0d1
peter <peter.shlee@kakaocorp.com>
2023-06-12 18:22:09 +0900

    refactor(step4): 테스트 추가

--- src/test/kotlin/race/RaceGameTest.kt
+++ src/test/kotlin/race/RaceGameTest.kt
@@ -69,25 +69,57 @@ class RaceGameTest {
         }
 
         val raceStartInformation = RaceStartInformation(nameOfCars = listOf("1"), numberOfAttempts = 1)
+        val carAtPosition1 = Car(1, "")
 
-        val after = CarRaceGame(number4Generator).runCarRace(raceStartInformation)
+        val carAfterRace = CarRaceGame(number4Generator).runCarRace(raceStartInformation)
             .turns
             .map {
                 it.cars
                     .first()
             }.last()
 
-        assertThat(after.isDraw(Car(1, ""))).isTrue()
+        assertThat(carAfterRace.isDraw(carAtPosition1)).isTrue()
     }
 
     @Test
-    fun `RaceResult의 frontrunner() 함수는 맨 앞의 차량들을 반환한다`() {
-        val frontrunner1 = Car(position = 3, name = "")
-        val frontrunner2 = Car(position = 3, name = "")
+    fun `RaceResult의 frontrunners() 함수는 맨 앞의 차량을 반환한다`() {
+        val frontrunner = Car(position = 3, name = "")
         val car1 = Car(position = 0, name = "")
         val car2 = Car(position = 1, name = "")
-        val cars = listOf(frontrunner1, car1, car2, frontrunner2)
+        val cars = listOf(frontrunner, car1, car2)
+
+        assertThat(RaceResult(listOf(Turn(cars))).frontrunners()).isEqualTo(setOf(frontrunner))
+    }
+
+    @Test
+    fun `선두 차량이 여러대일 경우 RaceResult의 frontrunners() 함수는 선두 차량들을 모두 반환한다`() {
+        val frontrunner1 = Car(position = 3, name = "")
+        val frontrunner2 = Car(position = 3, name = "")
+        val frontrunner3 = Car(position = 3, name = "")
+        val car1 = Car(position = 0, name = "")
+        val cars = listOf(frontrunner1, car1, frontrunner2, frontrunner3)
 
         assertThat(RaceResult(listOf(Turn(cars))).frontrunners()).isEqualTo(setOf(frontrunner2, frontrunner1))
+    }
+
+    @Test
+    fun `두 차량의 position이 같으면 isDraw()는 true를 리턴한다`() {
+        val car1 = Car(position = 1, name = "")
+        val car2 = Car(position = 1, name = "")
+        assertThat(car1.isDraw(car2)).isTrue()
+    }
+
+    @Test
+    fun `두 차량의 position이 다르면 isDraw()는 false를 리턴한다`() {
+        val looser = Car(position = 0, name = "")
+        val winner = Car(position = 1, name = "")
+        assertThat(looser.isDraw(winner)).isFalse()
+    }
+
+    @Test
+    fun `Car_winner()는 두 차량 중 position이 더 큰 차량을 리턴한다`() {
+        val looser = Car(position = 0, name = "")
+        val winner = Car(position = 1, name = "")
+        assertThat(Car.winner(looser, winner)).isEqualTo(winner)
     }
 }
