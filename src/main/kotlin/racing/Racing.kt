9e276e00578ede6c75ec508f86896b508c8473ca
sjparkk <sjparkk0820@naver.com>
2023-06-13 00:54:45 +0900

    fix : 이전 racingGame 제거 후 새로운 요구 사항의 racingGame 으로 수정

--- src/main/kotlin/racing/Racing.kt
+++ src/main/kotlin/racing/Racing.kt
@@ -8,27 +8,6 @@ class Racing {
 
     fun racingGame() {
 
-        val carCount = InputView.printInputCar()
-        val tryCount = InputView.printInputCount()
-
-        ResultView.printResult()
-
-        val cars = CarFactory().createCarsByInputCount(carCount)
-        carRacing(tryCount, cars)
-    }
-
-    private fun carRacing(tryCount: Int, cars: List<Car>) {
-        repeat(tryCount) {
-            cars.forEach { car ->
-                car.move()
-                ResultView.printDistance(car.position)
-            }
-            ResultView.printEnter()
-        }
-    }
-
-    fun determineRaceWinner() {
-
         val inputCarName = InputView.printInputCarNames()
 
         ResultView.printResult()
@@ -46,6 +25,16 @@ class Racing {
             cars.forEach { car ->
                 car.move()
                 ResultView.printNameAndDistance(car)
+            }
+            ResultView.printEnter()
+        }
+    }
+
+    private fun carRacing(tryCount: Int, cars: List<Car>) {
+        repeat(tryCount) {
+            cars.forEach { car ->
+                car.move()
+                ResultView.printDistance(car.position)
             }
             ResultView.printEnter()
         }
