1827114e045c1d9538aec9a17d17ff829051c269
hjjae2 <hjjae2@gmail.com>
2023-06-13 00:19:56 +0900

    [STEP5] domain 클래스(Car, Game) 작성

---
+++ src/main/kotlin/step5/domain/Car.kt
@@ -0,0 +1,34 @@
+package step5.domain
+
+class Car(
+    val name: String,
+    val moveForwardCondition: () -> Boolean = { RANGE_FOR_MOVING_CONDITION.random() >= THRESHOLD_OF_MOVABLE },
+) {
+    var position: Int = INIT_POSITION
+        private set
+
+    init {
+        require(name.length in RANGE_FOR_NAME_LENGTH) { "자동차 이름은 1자 이상, 5자 이하만 가능합니다." }
+    }
+
+    fun moveForwardRandomly() {
+        if (moveForwardCondition()) position += MOVE_FORWARD_VALUE
+    }
+
+    companion object {
+        private const val INIT_POSITION = 0
+        private const val MOVE_FORWARD_VALUE = 1
+        private const val THRESHOLD_OF_MOVABLE = 4
+        private val RANGE_FOR_MOVING_CONDITION = (0..9)
+        private val RANGE_FOR_NAME_LENGTH = (1..5)
+    }
+}
+
+class CarDto(
+    val name: String,
+    val position: Int,
+) {
+    companion object {
+        fun of(car: Car): CarDto = CarDto(car.name, car.position)
+    }
+}
---
+++ src/main/kotlin/step5/domain/Game.kt
@@ -0,0 +1,48 @@
+package step5.domain
+
+class Game(
+    nameOfCars: List<String>,
+    private val numberOfTrials: Int,
+) {
+    private val cars: List<Car>
+    private val gameLogs: MutableList<List<CarDto>> = mutableListOf()
+    private val isPlayed: Boolean
+        get() = gameLogs.isNotEmpty()
+
+    init {
+        require(nameOfCars.isNotEmpty()) { "자동차는 1대 이상이어야 합니다." }
+        require(numberOfTrials > 0) { "시도 횟수는 1회 이상이어야 합니다." }
+
+        cars = nameOfCars.map { Car(it.trim()) }
+    }
+
+    fun process() {
+        repeat(numberOfTrials) {
+            gameLogs.add(doProcess())
+        }
+    }
+
+    private fun doProcess(): List<CarDto> {
+        cars.forEach { it.moveForwardRandomly() }
+
+        return cars.map { CarDto.of(it) }
+    }
+
+    fun getWinners(): List<CarDto> {
+        check(isPlayed) { "게임이 진행되지 않았습니다." }
+        check(cars.isNotEmpty()) { "자동차가 존재하지 않습니다." }
+
+        val longestPosition = cars.maxByOrNull { it.position }?.position
+            ?: return emptyList()
+
+        return cars
+            .filter { it.position == longestPosition }
+            .map { CarDto.of(it) }
+    }
+
+    fun getGameLogs(): List<List<CarDto>> {
+        check(isPlayed) { "게임이 진행되지 않았습니다." }
+
+        return gameLogs
+    }
+}
