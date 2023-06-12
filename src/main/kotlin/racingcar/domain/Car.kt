2e8af2aece9aa2422629f8376178048f7a6687c9
jonghyunhub <chuwhdgus@gmail.com>
2023-06-12 09:14:57 +0900

    feat : Car generateRandom 메서드 검증 로직 추가

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -1,8 +1,12 @@
 package racingcar.domain
 
+import java.lang.IllegalArgumentException
+
 class Car {
     private var location = 0
     private val BASE_NUMBER_TO_MOVE = 4
+    private val RANDOM_MINIMUM = 0
+    private val RANDOM_MAXIMUM = 9
 
     fun isMoveOrStop() {
         val randomNum = generateRandomNum()
@@ -12,7 +16,12 @@ class Car {
     private fun conditionToMove(randomNum: Int) = randomNum >= BASE_NUMBER_TO_MOVE
 
     private fun generateRandomNum(): Int {
-        return (0..9).random()
+
+        val randomValue: Int = (RANDOM_MINIMUM..RANDOM_MAXIMUM).random()
+        if (randomValue < RANDOM_MINIMUM || randomValue > RANDOM_MAXIMUM) {
+            throw IllegalArgumentException()
+        }
+        return randomValue
     }
 
     fun printLocation() {
