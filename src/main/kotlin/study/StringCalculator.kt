216a3739738a101b51fb97c0af48fede68780042
june.kim <junho85@gmail.com>
2023-06-12 18:45:40 +0900

    feat: 문자열 계산기 구현

---
+++ src/main/kotlin/study/StringCalculator.kt
@@ -0,0 +1,29 @@
+package study
+
+class StringCalculator {
+    fun calculate(input: String?): Int {
+        if (input.isNullOrBlank()) {
+            throw IllegalArgumentException("입력값이 null이거나 빈 공백 문자입니다.")
+        }
+
+        val numbersAndOperands = input.split(" ")
+        return numbersAndOperands
+            .drop(1)
+            .chunked(2)
+            .fold(numbersAndOperands.first().toInt()) { acc, window ->
+                val operator = window[0]
+                val right = window[1].toInt()
+                calculate(acc, operator, right)
+            }
+    }
+
+    private fun calculate(left: Int, operator: String, right: Int): Int {
+        return when (operator) {
+            "+" -> left + right
+            "-" -> left - right
+            "*" -> left * right
+            "/" -> left / right
+            else -> throw IllegalArgumentException("지원하지 않는 연산자입니다.")
+        }
+    }
+}
