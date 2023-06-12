0a1f521e36869cbb1eda3d45619cc2917c0d777b
theo-94 <theo.94@kakaocorp.com>
2023-06-12 18:44:38 +0900

    피드백 반영 : require 사용
    코틀린 라이브러리의 require 사용하여 검증

--- src/main/kotlin/step2/Validator.kt
+++ src/main/kotlin/step2/Validator.kt
@@ -5,17 +5,13 @@ class Validator {
      * 입력값이 null이거나 빈 공백 문자일 경우 IllegalArgumentException throw
      */
     fun validateInput(input: String?) {
-        if (input == null || input.trim().isEmpty()) {
-            throw IllegalArgumentException("입력값이 null이거나 빈 공백 문자입니다.")
-        }
+        require(input == null || input.trim().isEmpty()) {"입력값이 null이거나 빈 공백 문자입니다."}
     }
 
     /**
      * 사칙연산 기호가 아닌 경우 IllegalArgumentException throw
      */
     fun validateOperator(operator: String) {
-        if (!operator.matches("[+\\-*/]".toRegex())) {
-            throw IllegalArgumentException("주어진 문자열은 사칙연산 기호가 아닙니다.")
-        }
+        require(!operator.matches("[+\\-*/]".toRegex())) {"주어진 문자열은 사칙연산 기호가 아닙니다."}
     }
 }
