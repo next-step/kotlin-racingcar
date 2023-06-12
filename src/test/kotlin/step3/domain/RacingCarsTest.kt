6e32b6c3fb567ae8f9d932a0e2e6a0054324cb9c
maenguin <maenguin924@gmail.com>
2023-06-12 12:18:21 +0900

    step5 : 피드백 반영 - 테스트 깨짐 픽스

--- src/test/kotlin/step3/domain/RacingCarsTest.kt
+++ src/test/kotlin/step3/domain/RacingCarsTest.kt
@@ -19,8 +19,14 @@ internal class RacingCarsTest {
         val racingRecord = sut.race(2)
 
         // then : 항상 전진하는 자동차는 2번 전진, 항상 전진하지 않는 자동차는 0번 전진
-        val firstAttempt = RacingAttempt(listOf(CarState("car1", 1), CarState("car2", 0)))
-        val secondAttempt = RacingAttempt(listOf(CarState("car1", 2), CarState("car2", 0)))
+        val firstAttempt = RacingAttempt(
+            carStates = listOf(CarState("car1", 1), CarState("car2", 0)),
+            order = 0
+        )
+        val secondAttempt = RacingAttempt(
+            carStates = listOf(CarState("car1", 2), CarState("car2", 0)),
+            order = 1
+        )
         val expectedRacingRecord = RacingRecord(listOf(firstAttempt, secondAttempt))
         assertThat(racingRecord).isEqualTo(expectedRacingRecord)
 
