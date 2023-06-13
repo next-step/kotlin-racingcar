a49e9c808cda09e709044436c43b45b657cf7678
chad.yoon <chad.yoon@kakaocorp.com>
2023-06-13 22:15:12 +0900

    style: value class는 단 한개의 값만 존재할 수 있으므로, git change 내역을 깔끔하게 표현하기 위해 개행해서 작성했던 코드를 한줄로 변경

--- src/main/kotlin/racinggame/domain/car/CarName.kt
+++ src/main/kotlin/racinggame/domain/car/CarName.kt
@@ -1,9 +1,7 @@
 package racinggame.domain.car
 
 @JvmInline
-value class CarName(
-    val value: String,
-) {
+value class CarName(val value: String) {
     init {
         require(value.isNotBlank() && value.length <= MAX_CAR_NAME_LENGTH) {
             "The name cannot be blank and must be up to $MAX_CAR_NAME_LENGTH characters. you input name : $value"
