5b7650cae185adbcfb770fc6b2b9c1af015f2fe8
celine.kk <celine.kk@kakaocorp.com>
2023-06-13 23:07:26 +0900

    change. RacingGame 테스트 수정

--- src/test/kotlin/racingcar/RacingGameTest.kt
+++ src/test/kotlin/racingcar/RacingGameTest.kt
@@ -7,8 +7,8 @@ import org.junit.jupiter.api.Test
 internal class RacingGameTest {
     @Test
     internal fun `모든 자동차의 게임결과가 전진이 나왔을 때 score가 전부 1이 되었는지 확인`() {
-        val records = RacingGame.play(
-            RacingGame(CarFactory.manufacture(listOf("aaa", "bbb", "abc"))),
+        val game = RacingGame(CarFactory.manufacture(listOf("aaa", "bbb", "abc")))
+            game.play(
             object : MoveStrategy {
                 override fun move(): Boolean {
                     return true
@@ -17,17 +17,17 @@ internal class RacingGameTest {
         )
 
         SoftAssertions.assertSoftly {
-            assertThat(records.cars.size).isEqualTo(3)
-            assertThat(records.cars[0].score).isEqualTo(1)
-            assertThat(records.cars[1].score).isEqualTo(1)
-            assertThat(records.cars[2].score).isEqualTo(1)
+            assertThat(game.cars.size).isEqualTo(3)
+            assertThat(game.cars[0].score).isEqualTo(1)
+            assertThat(game.cars[1].score).isEqualTo(1)
+            assertThat(game.cars[2].score).isEqualTo(1)
         }
     }
 
     @Test
     internal fun `모든 자동차의 게임 결과가 전진이 나오지 않았을 때 score가 전부 0이 되었는지 확인`() {
-        val records = RacingGame.play(
-            RacingGame(CarFactory.manufacture(listOf("aaa", "bbb"))),
+        val game = RacingGame(CarFactory.manufacture(listOf("aaa", "bbb")))
+        game.play(
             object : MoveStrategy {
                 override fun move(): Boolean {
                     return false
@@ -36,9 +36,49 @@ internal class RacingGameTest {
         )
 
         SoftAssertions.assertSoftly {
-            assertThat(records.cars.size).isEqualTo(2)
-            assertThat(records.cars[0].score).isEqualTo(0)
-            assertThat(records.cars[1].score).isEqualTo(0)
+            assertThat(game.cars.size).isEqualTo(2)
+            assertThat(game.cars[0].score).isEqualTo(0)
+            assertThat(game.cars[1].score).isEqualTo(0)
+        }
+    }
+
+    @Test
+    internal fun `가장 큰 score를 가진 자동차가 우승자로 결정난다`() {
+        val game = RacingGame(CarFactory.manufacture(listOf("aaa", "bbb", "abc")))
+        game.cars[0].move(object : MoveStrategy {
+            override fun move(): Boolean {
+                return true
+            }
+        })
+
+        SoftAssertions.assertSoftly {
+            assertThat(game.cars[0].score).isEqualTo(1)
+            assertThat(game.cars[1].score).isEqualTo(0)
+            assertThat(game.cars[2].score).isEqualTo(0)
+            assertThat(game.getWinners().joinToString (","){ it.name }).isEqualTo("aaa")
+        }
+    }
+
+    @Test
+    internal fun `우승자는 2명이상일 수 있다`() {
+        val game = RacingGame(CarFactory.manufacture(listOf("aaa", "bbb", "abc")))
+        game.cars[0].move(object : MoveStrategy {
+            override fun move(): Boolean {
+                return true
+            }
+        })
+
+        game.cars[1].move(object : MoveStrategy {
+            override fun move(): Boolean {
+                return true
+            }
+        })
+
+        SoftAssertions.assertSoftly {
+            assertThat(game.cars[0].score).isEqualTo(1)
+            assertThat(game.cars[1].score).isEqualTo(1)
+            assertThat(game.cars[2].score).isEqualTo(0)
+            assertThat(game.getWinners().joinToString (","){ it.name }).isEqualTo("aaa,bbb")
         }
     }
 }
