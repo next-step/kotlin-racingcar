e219c576661aeb1b70eea0202674221cfa62dfb8
celine.kk <celine.kk@kakaocorp.com>
2023-06-13 20:41:46 +0900

    feat. 자동차 이름을 입력 받는다.

--- src/main/kotlin/racingcar/InputView.kt
+++ src/main/kotlin/racingcar/InputView.kt
@@ -1,12 +1,12 @@
 package racingcar
 
-private const val CARNUM_STRING = "자동차 대수는 몇 대인가요?"
+private const val CARNAME_STRING = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
 
 private const val TRYNUM_STRING = "시도할 횟수는 몇 회인가요?"
 
 object InputView {
-    fun getCarNum(): Int {
-        println(CARNUM_STRING)
+    fun getCarName(): Int {
+        println(CARNAME_STRING)
         return readln().toInt()
     }
 
