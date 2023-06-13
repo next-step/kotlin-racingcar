5d5b4209610908fd95c26e2c2fca5a14c1d28384
yunbaek <yunbaek@gmail.com>
2023-06-13 10:41:12 +0900

    refactor(Operator): 메서드 기능 분리
    - 연산자 찾는 기능과 계산 기능 분리

--- src/main/kotlin/stringcalculator/Operator.kt
+++ src/main/kotlin/stringcalculator/Operator.kt
@@ -2,27 +2,24 @@ package stringcalculator
 
 enum class Operator(
     private val operatorSign: String,
-    private val operation: (Int, Int)
-    -> Int
+    private val operation: (Int, Int) -> Int
 ) {
     PLUS("+", { a, b -> a + b }),
     MINUS("-", { a, b -> a - b }),
     TIMES("*", { a, b -> a * b }),
     DIVIDE("/", { a, b ->
-        if (b == 0) {
-            throw IllegalArgumentException("Division by zero is not allowed")
-        }
+        require(b == 0) { "Division by zero is not allowed" }
         a / b
     });
 
-    private fun calculate(a: Int, b: Int): Int = operation(a, b)
-
     companion object {
-        fun executeOperation(operatorSign: String, a: Int, b: Int): Int {
-            val operator = values().find { it.operatorSign == operatorSign }
+        fun findBy(operatorSign: String): Operator {
+            return values().find { it.operatorSign == operatorSign }
                 ?: throw IllegalArgumentException("Invalid operator: $operatorSign")
-
-            return operator.calculate(a, b)
         }
+    }
+
+    fun calculate(a: Int, b: Int): Int {
+        return operation(a, b)
     }
 }
