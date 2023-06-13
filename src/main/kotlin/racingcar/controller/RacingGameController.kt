f0a4d1a0d54c4b4f30add14984c86479cb5e1981
gray <operasystem6@naver.com>
2023-06-13 11:40:22 +0900

    feat: controller 구현

---
+++ src/main/kotlin/racingcar/controller/RacingGameController.kt
@@ -0,0 +1,35 @@
+package racingcar.controller
+
+import racingcar.domain.Round
+import racingcar.domain.condition.RandomCondition
+import racingcar.service.RacingGameService
+import racingcar.view.InputView
+import racingcar.view.OutputView
+
+class RacingGameController(
+    private val racingGameService: RacingGameService
+) {
+    fun run() {
+        OutputView.printRequestCarCount()
+        val inputCarNames = InputView.inputName()
+        OutputView.printRequestAttemptCount()
+        val inputAttemptCount = InputView.inputNumber()
+        OutputView.printExecutionResult()
+
+        init(inputCarNames)
+        play(Round(inputAttemptCount))
+    }
+
+    private fun init(carNames: String) {
+        racingGameService.initCars(carNames, RandomCondition)
+    }
+
+    private fun play(round: Round) {
+        repeat(round.attemptCount) {
+            racingGameService.play()
+            OutputView.print()
+        }
+        val winnerNames = racingGameService.getWinners().joinToString { it.carName.name }
+        OutputView.printWinnerNames(winnerNames)
+    }
+}
