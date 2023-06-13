942757c2a7f6b88843a78040d091814ea5e3c9da
yoo-jaein <y_jaein@naver.com>
2023-06-13 21:33:49 +0900

    refactor(test): 테스트 클래스 패키지 이동

--- src/test/kotlin/race/domain/RaceResultTest.kt
+++ src/test/kotlin/race/domain/RaceResultTest.kt
@@ -1,4 +1,4 @@
-package race
+package race.domain
 
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
--- src/test/kotlin/race/domain/RaceTest.kt
+++ src/test/kotlin/race/domain/RaceTest.kt
@@ -1,4 +1,4 @@
-package race
+package race.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
