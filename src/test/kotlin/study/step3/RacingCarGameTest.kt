e6dca10c60ed260a3a90ad9b04e8727538e443e1
kyudong3 <kyudong3@gmail.com>
2023-06-14 01:37:10 +0900

    review: Test 코드에 로직이 들어가지 않도록 수정

--- src/test/kotlin/study/step3/RacingCarGameTest.kt
+++ src/test/kotlin/study/step3/RacingCarGameTest.kt
@@ -1,7 +1,10 @@
 package study.step3
 
+import io.kotest.matchers.shouldBe
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class RacingCarGameTest {
 
@@ -9,7 +12,7 @@ class RacingCarGameTest {
     fun `입력한 자동차 대수만큼 자동차들이 준비되었는지 확인`() {
         val actualInputCarCount = 3
         val actualRacingRequest = RacingRequest(actualInputCarCount, 5)
-        val game = RacingCarGame(actualRacingRequest)
+        val game = RacingCarGame(actualRacingRequest, CarRandomMove())
         game.play(actualRacingRequest)
 
         assertThat(game.getCurrentCars().size).isEqualTo(3)
@@ -19,7 +22,7 @@ class RacingCarGameTest {
     fun `입력한 시도 횟수만큼 자동차들의 상태가 저장되었는지 확인`() {
         val actualInputTryCount = 5
         val actualRacingRequest = RacingRequest(3, actualInputTryCount)
-        val game = RacingCarGame(actualRacingRequest)
+        val game = RacingCarGame(actualRacingRequest, CarRandomMove())
         game.play(actualRacingRequest)
 
         game.getCurrentCars().forEach { car ->
@@ -27,14 +30,19 @@ class RacingCarGameTest {
         }
     }
 
-    @Test
-    fun `0에서 9사이 무작위 값을 구해 4 이상일 경우 전진`() {
+    @ValueSource(ints = [4, 5, 6, 7, 8, 9])
+    @ParameterizedTest
+    fun `0에서 9사이 무작위 값을 구해 4 이상일 경우 전진`(condition: Int) {
         val actualCar = Car()
-        val randomValue = RandomGenerator().generateRandomValue()
+        actualCar.move(condition)
+        actualCar.getStateList().last() shouldBe 1
+    }
 
-        actualCar.move(randomValue >= 4)
-        if (randomValue >= 4) {
-            assertThat(actualCar.getStateList().last()).isEqualTo(1)
-        }
+    @ValueSource(ints = [0, 1, 2, 3])
+    @ParameterizedTest
+    fun `0에서 9사이 무작위 값을 구해 4 미만일 경우 정지`(condition: Int) {
+        val actualCar = Car()
+        actualCar.move(condition)
+        actualCar.getStateList().last() shouldBe 0
     }
 }
