b3b9215035e5d171efc16f9a8af8b4572136c948
hyunju-seo <hyunju6889@naver.com>
2023-06-14 00:54:54 +0900

    refactor: 자동차 이름 최대 길이 상수 적용

--- src/main/kotlin/carGame/domain/CarName.kt
+++ src/main/kotlin/carGame/domain/CarName.kt
@@ -3,6 +3,10 @@ package carGame.domain
 @JvmInline
 value class CarName(val value: String) {
     init {
-        require(value.length <= 5) { "자동차 이름은 5자를 초과할 수 없습니다." }
+        require(value.length <= MAX_NAME_LENGTH) { "자동차 이름은 ${MAX_NAME_LENGTH}자를 초과할 수 없습니다." }
+    }
+
+    companion object {
+        const val MAX_NAME_LENGTH = 5
     }
 }
