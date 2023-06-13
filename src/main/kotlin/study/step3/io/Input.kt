15b8cdc297551c0b959d3570c26caeed2d5533a3
sjjeong <sjjeong1225@gmail.com>
2023-06-13 20:11:37 +0900

    readlnOrNull()로 함수 변경

--- src/main/kotlin/study/step3/io/Input.kt
+++ src/main/kotlin/study/step3/io/Input.kt
@@ -3,10 +3,10 @@ package study.step3.io
 class Input {
 
     fun requestNumberOfCar(callback: (Int) -> Unit) {
-        readLine()?.toIntOrNull()?.let(callback)
+        readlnOrNull()?.toIntOrNull()?.let(callback)
     }
 
     fun requestNumberOfRound(callback: (Int) -> Unit) {
-        readLine()?.toIntOrNull()?.let(callback)
+        readlnOrNull()?.toIntOrNull()?.let(callback)
     }
 }
