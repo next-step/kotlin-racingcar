3f3882eb6d3b2cb765ee3feafd2f16095570d719
김태훈 <sksskaw@naver.com>
2023-06-14 22:34:39 +0900

    테스트에 inputMessage 제거
    시도할 횟수 입력받는 기능 구현
    
    양수만 입력되도록 구현

--- src/main/kotlin/racingCar2/InputIO.kt
+++ src/main/kotlin/racingCar2/InputIO.kt
@@ -5,4 +5,11 @@ class InputIO {
         requireNotNull(inputString) { "자동차 이름을 입력해 주세요" }
         return inputString.split(",")
     }
+
+    fun inputGameCount(inputNumber: Int? = readlnOrNull()?.toInt()): Int {
+        requireNotNull(inputNumber) { "시도할 횟수를 입력해 주세요." }
+        check(inputNumber > 0) { "시도할 횟수는 양수만 입력해 주세요." }
+
+        return inputNumber
+    }
 }
