62706fa6d6fd7e7f96b4dda5d25bc0142697fe57
김태훈 <sksskaw@naver.com>
2023-06-14 22:08:12 +0900

    자동차 이름 입력받아 객체 생성하는 기능 구현

--- src/main/kotlin/racingCar2/RacingField.kt
+++ src/main/kotlin/racingCar2/RacingField.kt
@@ -1,3 +1,9 @@
 package racingCar2
 
-class RacingField
+class RacingField(
+    private val cars: List<Car>,
+) {
+    constructor(vararg carNames: String) : this(cars = List(carNames.size) { Car(carNames[it]) })
+
+    val carNames: List<String> get() = cars.map { it.name }
+}
--- src/test/kotlin/racingCar2/RacingFieldTest.kt
+++ src/test/kotlin/racingCar2/RacingFieldTest.kt
@@ -1,3 +1,14 @@
 package racingCar2
 
-class RacingFieldTest
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class RacingFieldTest {
+    @Test
+    fun `경주할 자동자의 이름을 입력받고 이름의 수만큼 자동차가 생성된다`() {
+        val carNames = listOf("BMW", "Benz", "Audi")
+        val racingField = RacingField(*carNames.toTypedArray())
+
+        racingField.carNames shouldBe carNames
+    }
+}
