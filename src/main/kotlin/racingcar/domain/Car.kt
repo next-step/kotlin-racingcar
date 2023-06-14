2df3969950f9322ee3e2e382888a9275bb6a02e4
gray <operasystem6@naver.com>
2023-06-14 18:41:26 +0900

    refactor: Car 객체를 data class -> class로 변경

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -2,7 +2,7 @@ package racingcar.domain
 
 import racingcar.domain.condition.Condition
 
-data class Car(
+class Car(
     var position: Int,
     val carName: CarName,
     private val condition: Condition
