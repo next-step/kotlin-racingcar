6cd63ca164bb37ab4e859010c9427313cfdc4b8b
김태훈 <sksskaw@naver.com>
2023-06-14 21:06:33 +0900

    자동차의 처음 위치는 0이다. 기능 구현

--- src/main/kotlin/racingCar2/Car.kt
+++ src/main/kotlin/racingCar2/Car.kt
@@ -2,8 +2,8 @@ package racingCar2
 
 class Car(
     private val _name: String,
-    private var _position: Int
 ) {
+
     init {
         check(name.length < MAX_NAME_LENGTH) { "자동차의 이름은 5자를 초과할 수 없습니다." }
     }
@@ -11,13 +11,15 @@ class Car(
     companion object {
         const val MAX_NAME_LENGTH = 5
         const val FORWARD_CONDITIONS = 4
+        const val DEFAULT_POSITION = 0
     }
+
+    private var _position: Int = DEFAULT_POSITION
+    val name: String get() = _name
+    val position: Int get() = _position
 
     fun move(number: Int) {
         if (number > FORWARD_CONDITIONS)
             _position++
     }
-
-    val name: String get() = _name
-    val position: Int get() = _position
 }
--- src/test/kotlin/racingCar2/CarTest.kt
+++ src/test/kotlin/racingCar2/CarTest.kt
@@ -9,25 +9,31 @@ import org.junit.jupiter.params.provider.CsvSource
 class CarTest {
     @Test
     fun `자동차는 이름과 위치를 가진다`() {
-        val car = Car("김태훈", 1)
+        val car = Car("김태훈")
         car.name shouldBe "김태훈"
-        car.position shouldBe 1
+        car.position shouldBe 0
+    }
+
+    @Test
+    fun `자동차의 처음 위치는 0이다`() {
+        val car = Car("김태훈")
+        car.position shouldBe 0
     }
 
     @Test
     fun `자동차의 이름은 5자를 초과할 수 없다`() {
         shouldThrow<IllegalStateException> {
-            Car("잘생긴 김태훈", 1)
+            Car("잘생긴 김태훈")
         }
     }
 
     @ParameterizedTest
     @CsvSource(
-        "10, 2",
-        "3, 1"
+        "10, 1",
+        "3, 0"
     )
     fun `랜덤 숫자가 4 이상이면 자동차는 이동할 수 있다`(randomNumber: Int, expected: Int) {
-        val car = Car("훈", 1)
+        val car = Car("훈")
         car.move(randomNumber)
         car.position shouldBe expected
     }
