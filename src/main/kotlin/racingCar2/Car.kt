6c2c0df02c79c755cbc8b998c267d73f9ed46aad
김태훈 <sksskaw@naver.com>
2023-06-14 20:51:46 +0900

    자동차 이름 체크 기능 구현

--- src/main/kotlin/racingCar2/Car.kt
+++ src/main/kotlin/racingCar2/Car.kt
@@ -4,6 +4,15 @@ class Car(
     private val _name: String,
     private val _position: Int
 ) {
+    init {
+        check(name.length < MAX_NAME_LENGTH) { "자동차의 이름은 5자를 초과할 수 없습니다." }
+    }
+
+    companion object {
+        const val MAX_NAME_LENGTH = 5
+        const val FORWARD_CONDITIONS = 4
+    }
+
     val name: String get() = _name
     val position: Int get() = _position
 }
--- src/test/kotlin/racingCar2/CarTest.kt
+++ src/test/kotlin/racingCar2/CarTest.kt
@@ -1,6 +1,7 @@
 package racingCar2
 
 import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class CarTest {
@@ -9,5 +10,12 @@ class CarTest {
         val car = Car("김태훈", 1)
         car.name shouldBe "김태훈"
         car.position shouldBe 1
+    }
+
+    @Test
+    fun `자동차의 이름은 5자를 초과할 수 없다`() {
+        Assertions.assertThrows(IllegalStateException::class.java) {
+            Car("잘생긴 김태훈", 1)
+        }
     }
 }
