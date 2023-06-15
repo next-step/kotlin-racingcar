2d044417939eb88ce0bd6b4ba53a51bcb247c273
Songyi Kim <ksl2950@naver.com>
2023-06-15 23:42:39 +0900

    refactor: random number 생성시 커스텀 getter 사용

--- src/main/kotlin/step3/domain/RandomNumberGenerator.kt
+++ src/main/kotlin/step3/domain/RandomNumberGenerator.kt
@@ -3,14 +3,14 @@ package step3.domain
 import java.util.Random
 
 class RandomNumberGenerator : NumberGenerator {
+    private val randomNumber: Int
+        get() = Random().nextInt(MAX)
 
-    private val random = Random()
+    override fun generate(): Int {
+        return randomNumber
+    }
 
     companion object {
         private const val MAX = 10
-    }
-
-    override fun generate(): Int {
-        return random.nextInt(MAX)
     }
 }
