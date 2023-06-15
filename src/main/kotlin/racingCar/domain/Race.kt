c19c10244e739ea4e0db02b98d0bcb07d2e4ab95
oliveunji <39396725+oliveunji@users.noreply.github.com>
2023-06-15 09:18:52 +0900

    Update src/main/kotlin/racingCar/domain/Race.kt
    Co-authored-by: Hyeon9mak <jinha3507@gmail.com>

--- src/main/kotlin/racingCar/domain/Race.kt
+++ src/main/kotlin/racingCar/domain/Race.kt
@@ -5,7 +5,7 @@ import kotlin.random.Random
 private const val RANDOM_NUM_RANGE = 10
 private const val REFERENCE_VAL = 4
 fun racingRule(): Boolean {
-    return Random.nextInt(RANDOM_NUM_RANGE) >= REFERENCE_VAL
+    return Random.nextInt(0..9) >= REFERENCE_VAL
 }
 
 class Race(private val cars: List<Car>) {
