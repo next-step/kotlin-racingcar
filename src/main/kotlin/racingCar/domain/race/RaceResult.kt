812c5c85ec6bbc641c8641afd205ef89fed4f081
pooh <pooh.duck@kakaocorp.com>
2023-06-16 20:18:38 +0900

    chore: 주석 삭제

--- src/main/kotlin/racingCar/domain/race/RaceResult.kt
+++ src/main/kotlin/racingCar/domain/race/RaceResult.kt
@@ -5,21 +5,6 @@ import racingCar.domain.car.CarStatus
 data class RaceResult(val roundResults: List<RaceRoundResult>) {
     val roundCount: Int = roundResults.size
     val winners: List<CarStatus> = findWinners(roundResults.last())
-// 기존 코드
-//    init {
-//        val tempWinners: MutableList<CarStatus> = mutableListOf()
-//        var curMaxPosition = 0
-//        for (elem in roundResults.last().carStatuses) {
-//            if (curMaxPosition < elem.value.position) {
-//                tempWinners.clear()
-//                tempWinners.add(elem.value)
-//                curMaxPosition = elem.value.position
-//            } else if (curMaxPosition == elem.value.position) {
-//                tempWinners.add(elem.value)
-//            }
-//        }
-//        winners = tempWinners.toList()
-//    }
 
     private fun findWinners(result: RaceRoundResult): List<CarStatus> = with(result.carStatuses) {
         val winningDistance = maxOfOrNull { it.value.position } ?: 0
