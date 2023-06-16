03219c23beb4a5bb4ba8af13d9b784c82f2c28b3
impactrudia <impactyoung@gmail.com>
2023-06-16 22:38:04 +0900

    feat : 사칙연산 기호가 아닌 경우 IllegalArgumentException throw

---
+++ src/main/kotlin/calculrate/Operator.kt
@@ -0,0 +1,19 @@
+package calculrate
+
+enum class Operator(val char: String) {
+    PLUS("+") {
+        override fun operation(first: Int, second: Int): Int = first + second
+    };
+
+    abstract fun operation(first: Int, second: Int): Int
+
+    companion object {
+        fun of(symbol: String): Operator {
+            val operator = values().firstOrNull { it.char == symbol }
+            require(operator != null) {
+                "사칙연산 기호가 아닌 경우"
+            }
+            return operator
+        }
+    }
+}
--- src/test/kotlin/calculrate/CalculatorTest.kt
+++ src/test/kotlin/calculrate/CalculatorTest.kt
@@ -1,6 +1,7 @@
 package calculrate
 
 import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.NullAndEmptySource
 
@@ -11,6 +12,13 @@ class CalculatorTest {
     fun `입력값이 Null 이거나 빈 공백 문자일 경우 IllegalArgumentException throw`(input: String?) {
         Assertions.assertThatThrownBy {
             Calculator.calculate(input)
+        }.isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @Test
+    fun `사칙연산 기호가 아닌 경우 IllegalArgumentException throw`() {
+        Assertions.assertThatThrownBy {
+            Operator.of("!")
         }.isInstanceOf(IllegalArgumentException::class.java)
     }
 }
