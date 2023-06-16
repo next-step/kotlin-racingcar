ad8e71a844a701d630aa1744d9c5d5fe4a2c9301
songyi00 <ksl2950@naver.com>
2023-06-16 22:13:42 +0900

    refactor: controller에 view 의존성 주입

--- src/main/kotlin/step3/GameApplication.kt
+++ src/main/kotlin/step3/GameApplication.kt
@@ -1,8 +1,10 @@
 package step3
 
 import step3.controller.GameController
+import step3.view.InputView
+import step3.view.OutputView
 
 fun main() {
-    val gameController = GameController()
+    val gameController = GameController(InputView(), OutputView())
     gameController.start()
 }
