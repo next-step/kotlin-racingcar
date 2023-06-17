edf9168595eabd334b56721ba53eaf9f9d5d2d80
Songyi Kim <ksl2950@naver.com>
2023-06-17 12:24:56 +0900

    test: car 이름 크기 검증 테스트코드 추가

--- src/test/kotlin/racingame/domain/CarTest.kt
+++ src/test/kotlin/racingame/domain/CarTest.kt
@@ -1,10 +1,17 @@
 package racingame.domain
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import racingame.fake.FakeNumberGenerator
+import java.lang.IllegalArgumentException
 
 class CarTest : FunSpec({
+
+    test("자동차 이름이 5자를 넘어가는 경우 예외가 발생한다.") {
+        shouldThrow<IllegalArgumentException> { Car("가나다라마바사") }
+    }
+
     test("4 이상의 값을 가지는 car 는 전진할 수 있다.") {
         // given
         val fakeNumberGenerator = FakeNumberGenerator(5)
