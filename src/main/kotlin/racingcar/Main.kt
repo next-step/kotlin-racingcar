ec1006b79b1413d2b4fd33af350ce52cb2839a1d
turner.lee <turner.lee@kakaocorp.com>
2023-06-18 23:58:52 +0900

    refactor: 불필요한 변수와 생성자는 함수로 통합

--- src/main/kotlin/racingcar/Main.kt
+++ src/main/kotlin/racingcar/Main.kt
@@ -6,9 +6,9 @@ import racingcar.view.ResultView
 fun main() {
     val carsSize = InputView.receiveCarsSize()
     val tryCount = InputView.receiveTryCount()
-    val cars = Cars.from(carsSize)
 
-    val result = RacingGame(cars, tryCount).run()
+    val cars = Cars.from(carsSize)
+    val result = RacingGame.run(cars, tryCount)
 
     ResultView.init()
     ResultView.printResult(result)
--- src/main/kotlin/racingcar/RacingGame.kt
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -1,12 +1,8 @@
 package racingcar
 
-class RacingGame(private val cars: Cars, private val tryCount: Int) {
-    private val gameResult: MutableList<PositionHistory> = mutableListOf()
-
-    fun run(): RacingGameResult {
-        repeat(tryCount) {
-            gameResult.add(PositionHistory(cars.moveAll()))
-        }
+object RacingGame {
+    fun run(cars: Cars, tryCount: Int): RacingGameResult {
+        val gameResult = List(tryCount) { PositionHistory(cars.moveAll()) }
 
         return RacingGameResult(gameResult)
     }
