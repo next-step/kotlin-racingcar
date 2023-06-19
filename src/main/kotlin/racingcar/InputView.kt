e69a8dafb5c733399f0e86cb0f7f193093a9f5db
송시은 <sieun.song@woowahan.com>
2023-06-19 18:46:42 +0900

    refactor: modify NPE to NumberFormatException

--- src/main/kotlin/racingcar/InputView.kt
+++ src/main/kotlin/racingcar/InputView.kt
@@ -3,11 +3,11 @@ package racingcar
 object InputView {
     fun getNumberOfCars(): Int {
         println("자동차 대수는 몇 대인가요?")
-        return readln().toIntOrNull() ?: throw NullPointerException("number of cars was null")
+        return readln().toIntOrNull() ?: throw NumberFormatException("number of cars was null")
     }
 
     fun getNumberOfRounds(): Int {
         println("시도할 횟수는 몇 회인가요?")
-        return readlnOrNull()?.toIntOrNull() ?: throw NullPointerException("number of rounds was null")
+        return readln().toIntOrNull() ?: throw NumberFormatException("number of rounds was null")
     }
 }
