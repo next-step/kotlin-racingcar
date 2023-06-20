91cd228e41504c16e59e2881a87d67c33aa7bd19
yunbaek <yunbaek@gmail.com>
2023-06-20 14:47:33 +0900

    feat(InputView): 자동차 이름 입력받는 기능 추가

--- src/main/kotlin/racingcar/game/RacingCarGame.kt
+++ src/main/kotlin/racingcar/game/RacingCarGame.kt
@@ -1,6 +1,8 @@
 package racingcar.game
 
 import racingcar.car.Cars
+import racingcar.car.CarsGenerator
+import racingcar.car.RandomMoveStrategy
 import racingcar.car.RoundCount
 import racingcar.view.InputView
 import racingcar.view.ResultView
@@ -10,25 +12,23 @@ class RacingCarGame(
     private var resultView: ResultView = ResultView()
 ) {
 
-    private lateinit var cars: Cars
-
     fun play() {
-        getCars()
-        val roundCount = RoundCount(inputView.inputCarsCount())
+        val cars = getCars()
+        val roundCount = RoundCount(inputView.inputRoundCount())
         repeat(roundCount.count) {
             cars.move()
-            printResult()
+            printResult(cars)
         }
     }
 
-    private fun printResult() {
+    private fun printResult(cars: Cars) {
         val result: List<String> = cars.getPathStrings()
         resultView.printResult(result)
     }
 
-    private fun getCars() {
-        val carCounts = inputView.inputCarsCount()
-        // todo : inputView.inputCarNames()를 통해 자동차 이름을 입력받아 Cars를 생성하세요.
-        // cars = Cars("", carCounts)
+    private fun getCars(): Cars {
+        val carNames = inputView.inputCarsNames()
+        val carsGenerator = CarsGenerator(carNames, RandomMoveStrategy())
+        return carsGenerator.generate()
     }
 }
