5be548e6adb49f04d3e3031d7b2d4ecd5f6aa13d
yunbaek <yunbaek@gmail.com>
2023-06-21 08:40:42 +0900

    fix(CarsTest): 깨진 테스트 수정

--- src/test/kotlin/racingcar/car/CarsTest.kt
+++ src/test/kotlin/racingcar/car/CarsTest.kt
@@ -6,20 +6,20 @@ import org.junit.jupiter.api.Test
 
 class CarsTest {
 
-//    @Test
-//    fun `자동차들을 생성할 수 있다`() {
-//        assertThatCode {
-//            val generator = CarsGenerator(CarNames(listOf("a", "b", "c"))) { true }
-//            generator.generate()
-//        }.doesNotThrowAnyException()
-//    }
-//
-//    @Test
-//    fun `자동차들의 이동경로를 문자열로 반환한다`() {
-//        val generator = CarsGenerator(CarNames(listOf("a", "b", "c"))) { true }
-//        val cars = generator.generate()
-//        cars.move()
-//        assertThat(cars.getPathStrings())
-//            .containsExactly("--", "--", "--")
-//    }
+    @Test
+    fun `자동차들을 생성할 수 있다`() {
+        assertThatCode {
+            val generator = CarsGenerator("a,b,c,d,e") { true }
+            generator.generate()
+        }.doesNotThrowAnyException()
+    }
+
+    @Test
+    fun `자동차들의 이동경로를 문자열로 반환한다`() {
+        val generator = CarsGenerator("a,b,c,d,e") { true }
+        val cars = generator.generate()
+        cars.move()
+        assertThat(cars.getPathStrings())
+            .containsExactly("a : --", "b : --", "c : --", "d : --", "e : --")
+    }
 }
