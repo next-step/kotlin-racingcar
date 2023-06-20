f3e10dc5f61b8270b4585b1ece3c1a7dc8817b35
yunbaek <yunbaek@gmail.com>
2023-06-20 14:47:10 +0900

    feat(CarsGenerator): 자동차 생성 책임 위임
    - Cars -> CarsGenerator

--- src/main/kotlin/racingcar/car/CarsGenerator.kt
+++ src/main/kotlin/racingcar/car/CarsGenerator.kt
@@ -1,12 +1,25 @@
 package racingcar.car
 
-class CarsGenerator(private val carNames: CarNames, private val moveStrategy: MoveStrategy) {
+class CarsGenerator(
+    private val inputCarNames: String,
+    private val moveStrategy: MoveStrategy
+) {
 
     fun generate(): Cars {
-        val cars = mutableListOf<Car>()
-        carNames.names.forEach { name ->
-            cars.add(Car(name, moveStrategy))
-        }
+        val carNames = getCarNames()
+        val cars = carNames.map { Car(it.name, moveStrategy) }
         return Cars(cars)
+    }
+
+    private fun getCarNames(): List<CarName> {
+        val carNamesString = inputCarNames.split(CAR_NAME_DELIMITER)
+        require(carNamesString.isNotEmpty()) { "자동차 이름은 1개 이상이어야 합니다." }
+        val carNames = carNamesString.map { CarName(it.trim()) }.toList()
+        require(carNames.distinct().size == carNames.size) { "자동차 이름은 중복되지 않아야 합니다." }
+        return carNames
+    }
+
+    companion object {
+        private const val CAR_NAME_DELIMITER = ","
     }
 }
