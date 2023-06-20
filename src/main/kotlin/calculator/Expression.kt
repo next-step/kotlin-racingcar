3ea21071e46561b4f40d0b3c7c7fc89066c32d84
sang5c <skah321@gmail.com>
2023-06-20 10:01:07 +0900

    refactor(Expression): 기본 구분자 상수화

--- src/main/kotlin/calculator/Expression.kt
+++ src/main/kotlin/calculator/Expression.kt
@@ -1,5 +1,6 @@
 package calculator
 
+
 class Expression(private val operators: List<Operator>, private val numbers: List<Int>) {
 
     fun getResult(): Int {
@@ -9,10 +10,12 @@ class Expression(private val operators: List<Operator>, private val numbers: Lis
     }
 
     companion object {
+        private const val DEFAULT_DELIMITER = " "
+
         fun from(expression: String): Expression {
             require(expression.isNotEmpty()) { "입력값이 없습니다." }
 
-            val values = expression.split(" ")
+            val values = expression.split(DEFAULT_DELIMITER)
             require(validLength(values.size)) { "잘못된 식입니다" }
 
             val operators = values.filterIndexed { index, _ -> index % 2 == 1 }
