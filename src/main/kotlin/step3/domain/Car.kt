17ee625cd155c7e31434563b9e73bfb325792255
theo-94 <theo.94@kakaocorp.com>
2023-06-21 14:25:33 +0900

    피드백 반영 : Car 클래스 생성자 변경
    position을 받도록 함

--- src/main/kotlin/step3/domain/Car.kt
+++ src/main/kotlin/step3/domain/Car.kt
@@ -2,10 +2,9 @@ package step3.domain
 
 class Car(
     private val carMover: CarMover,
-    val name: String=""
-) {
+    val name: String="",
     var position: Int = 0
-
+) {
     fun move() {
         if (carMover.movable()) {
             this.position += DISTANCE
