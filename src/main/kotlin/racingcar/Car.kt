213154b10138a4e5e7326a4cd08455fd8b4a223a
june.kim <junho85@gmail.com>
2023-06-22 17:47:18 +0900

    PR리뷰 반영 - require 적용 https://github.com/next-step/kotlin-racingcar/pull/1226#discussion_r1234861371

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -5,9 +5,7 @@ class Car(var name: String) {
         private set
 
     init {
-        if (name.length > 5) {
-            throw IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.")
-        }
+        require(name.length > 5) { "자동차 이름은 5자를 초과할 수 없습니다." }
     }
 
     fun move(condition: Int) {
