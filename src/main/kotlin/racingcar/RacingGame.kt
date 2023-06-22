cca78f8f1471fcff62be0b1e5ab2ae13826605b7
june.kim <junho85@gmail.com>
2023-06-22 19:52:09 +0900

    PR리뷰 반영 - readln https://github.com/next-step/kotlin-racingcar/pull/1226#discussion_r1234882666

--- src/main/kotlin/racingcar/RacingGame.kt
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -32,12 +32,12 @@ class RandomConditionGenerator : ConditionGenerator
 class InputView {
     fun inputCarNames(): List<String> {
         println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
-        return readLine()!!.split(",")
+        return readln().split(",")
     }
 
     fun inputTryNum(): Int {
         println("시도할 횟수는 몇 회 인가요?")
-        return readLine()!!.toInt()
+        return readln().toInt()
     }
 }
 
