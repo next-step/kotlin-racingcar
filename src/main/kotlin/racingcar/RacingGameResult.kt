94e30e48cc99c53e739a50385db0a6a3e271640d
turner.lee <turner.lee@kakaocorp.com>
2023-06-22 17:42:26 +0900

    refactor: 우승자 계산하는 로직을 ResultView에서 RacingGameResult 도메인으로 이동

--- src/main/kotlin/racingcar/RacingGameResult.kt
+++ src/main/kotlin/racingcar/RacingGameResult.kt
@@ -1,3 +1,11 @@
 package racingcar
 
-data class RacingGameResult(val positionHistories: List<PositionHistory>)
+data class RacingGameResult(val positionHistories: List<PositionHistory>) {
+    fun getWinnerNames(): List<String> {
+        val finalHistory = positionHistories.last()
+        val winnerPosition = finalHistory.positions.maxBy { it.position }.position
+        val winnerNames = finalHistory.positions.filter { it.position == winnerPosition }.map { it.name }
+
+        return winnerNames
+    }
+}
--- src/main/kotlin/racingcar/view/ResultView.kt
+++ src/main/kotlin/racingcar/view/ResultView.kt
@@ -13,10 +13,7 @@ object ResultView {
     fun printResult(result: RacingGameResult) {
         result.positionHistories.forEach { printResult(it) }
 
-        val finalHistory = result.positionHistories.last()
-        val winnerPosition = finalHistory.positions.maxBy { it.position }.position
-        val winnerNames = finalHistory.positions.filter { it.position == winnerPosition }.map { it.name }
-
+        val winnerNames = result.getWinnerNames()
         println("${winnerNames.joinToString(", ")}가 최종 우승했습니다.")
     }
 
