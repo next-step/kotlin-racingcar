a350d014268f9d1541fa669142a1a6b4f580390f
turner.lee <turner.lee@kakaocorp.com>
2023-06-22 17:37:36 +0900

    refactor: 불필요한 줄바꿈 제거

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -4,8 +4,7 @@ object InputView {
     fun receiveCarNames(): List<String> {
         println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
 
-        return receive()
-            .split(",")
+        return receive().split(",")
     }
 
     fun receiveTryCount(): Int {
