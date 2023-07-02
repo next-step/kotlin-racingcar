4355a2d0d7f859a7fecebe6b939577dba63a9cd4
yonghoChoi <studyyongho2157@gmail.com>
2023-07-02 15:54:13 +0900

    CarTest, WinnerTest 수정

--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -11,7 +11,7 @@ import org.junit.jupiter.params.provider.ValueSource
 class CarTest {
     @Test
     fun `자동차는 이름과 위치를 가지고 있다`() {
-        val car = Car("홍길동") { 0 }
+        val car = Car("홍길동")
         car.name shouldBe "홍길동"
         car.position shouldBe 0
     }
@@ -19,23 +19,23 @@ class CarTest {
     @Test
     fun `자동차 이름은 5자를 초과할 수 없다`() {
         shouldThrow<IllegalArgumentException> {
-            Car("가나다마사") { 0 }
+            Car("가나다마사")
         }
     }
 
     @ParameterizedTest
     @ValueSource(ints = [4, 5, 6, 7, 8, 9])
     fun `자동차 전진 성공`(number: Int) {
-        val car = Car("car1") { number }
-        car.move()
+        val car = Car("car1")
+        car.move(number)
         assertThat(car.position).isEqualTo(1)
     }
 
     @ParameterizedTest
     @ValueSource(ints = [1, 2, 3])
     fun `자동차 전진 실패`(number: Int) {
-        val car = Car("car1") { number }
-        car.move()
+        val car = Car("car1")
+        car.move(number)
         assertThat(car.position).isEqualTo(0)
     }
 }
--- src/test/kotlin/racingcar/WinnerTest.kt
+++ src/test/kotlin/racingcar/WinnerTest.kt
@@ -9,17 +9,21 @@ class WinnerTest {
 
     @Test
     fun `우승자 확인`() {
-        val cars = listOf(Car("A") { 5 }, Car("B") { 0 })
+        val carA = Car("A")
+        val carB = Car("B")
+        carA.move(5)
+        carB.move(2)
+        val cars = listOf(carA, carB)
         assertThat(Winner(cars).getWinner()[0].name).isEqualTo("A")
     }
 
     @Test
     fun `우승자는 한 명 이상일 수 있다`() {
-        val cars = listOf(Car("A") { 5 }, Car("B") { 6 }, Car("C") { 9 })
+        val cars = listOf(Car("A"), Car("B"), Car("C"))
 
-        cars[0].move()
-        cars[1].move()
-        cars[2].move()
+        cars[0].move(5)
+        cars[1].move(6)
+        cars[2].move(8)
 
         assertThat(Winner(cars).getWinner().size).isEqualTo(3)
     }
