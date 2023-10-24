68c204fdbc7cae87c4f6c26940b89fd3358a2a6d
baek0318 <peachberry0318@gmail.com>
2023-10-25 01:28:40 +0900

    step2 : 검증을 위한 테스트 코드 작성

---
+++ src/test/kotlin/step2/CalculatorTest.kt
@@ -0,0 +1,31 @@
+package step2
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class CalculatorTest: StringSpec({
+
+    "사칙연산 테스트" {
+        val calculator = Calculator("2 + 3 * 4 / 2")
+        val result = calculator.calculate()
+        result shouldBe 10
+    }
+
+    "0이 포함된 사칙연산 테스트" {
+        val calculator = Calculator("0 + 0 * 0 / 2")
+        val result = calculator.calculate()
+        result shouldBe 0
+    }
+
+    "음수가 포함된 사칙연산 테스트" {
+        val calculator = Calculator("2 + 3 * -4 / 2")
+        val result = calculator.calculate()
+        result shouldBe -10
+    }
+
+    "한가지 연산자만 포함된 사칙연산 테스트" {
+        val calculator = Calculator("2 + 4 + 10")
+        val result = calculator.calculate()
+        result shouldBe 16
+    }
+})
