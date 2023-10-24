a7b5606f66b73f4bca66c4f74f054d3e4963164b
ALbertIM0427 <guang7226961@naver.com>
2023-10-24 23:36:25 +0900

    test: 사칙 연산을 모두 포함하는 기능 구현 테스트 추가

--- src/test/kotlin/step2/StringCalculatorTest.kt
+++ src/test/kotlin/step2/StringCalculatorTest.kt
@@ -90,4 +90,64 @@ class StringCalculatorTest {
         // then
         assertThat(actual).isEqualTo(expect)
     }
+
+    @ParameterizedTest
+    @CsvSource(value = ["2 - 3 - 4:-5", "3 - 5 - 7:-9", "1 - 3 - 5:-7"], delimiter = ':')
+    fun `뺄셈 2개 이상`(input: String, expect: Int) {
+        // when
+        val actual = StringCalculator.calculate(input)
+
+        // then
+        assertThat(actual).isEqualTo(expect)
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["2 * 3 * 4:24", "3 * 5 * 7:105", "1 * 3 * 5:15"], delimiter = ':')
+    fun `곱셈 2개 이상`(input: String, expect: Int) {
+        // when
+        val actual = StringCalculator.calculate(input)
+
+        // then
+        assertThat(actual).isEqualTo(expect)
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["2 / 3 / 4:0", "3 / 5 / 7:0", "1 / 3 / 5:0"], delimiter = ':')
+    fun `나눗셈 2개 이상`(input: String, expect: Int) {
+        // when
+        val actual = StringCalculator.calculate(input)
+
+        // then
+        assertThat(actual).isEqualTo(expect)
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["2 + 3 - 4:1", "3 - 5 + 7:5", "1 + 3 - 5:-1"], delimiter = ':')
+    fun `덧셈, 뺄셈 혼합`(input: String, expect: Int) {
+        // when
+        val actual = StringCalculator.calculate(input)
+
+        // then
+        assertThat(actual).isEqualTo(expect)
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["2 * 3 / 4:1", "3 / 5 * 7:0", "1 * 3 / 5:0"], delimiter = ':')
+    fun `곱셈, 나눗셈 혼합`(input: String, expect: Int) {
+        // when
+        val actual = StringCalculator.calculate(input)
+
+        // then
+        assertThat(actual).isEqualTo(expect)
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["2 + 3 * 4 / 2:10", "2 + 3 * 4:20", "3 * 5 - 7:8", "1 + 3 / 5:0"], delimiter = ':')
+    fun `덧셈, 뺄셈, 곱셈, 나눗셈 혼합`(input: String, expect: Int) {
+        // when
+        val actual = StringCalculator.calculate(input)
+
+        // then
+        assertThat(actual).isEqualTo(expect)
+    }
 }
