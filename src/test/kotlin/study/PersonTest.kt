23c78398ca6dbd6a9d8733fb7252286595cb86c9
Youngtae Kim <47503412+10000001a@users.noreply.github.com>
2023-10-24 22:33:50 +0900

    [study] kotlin 기본 문법 학습 테스트 (#1281)

---
+++ src/test/kotlin/study/PersonTest.kt
@@ -0,0 +1,22 @@
+package study
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class PersonTest {
+    @Test
+    fun `이름 붙인 인자`() {
+        val actual = Person(name = "홍길동", 20)
+        assertThat(actual.name).isEqualTo("홍길동")
+        assertThat(actual.age).isEqualTo(20)
+        assertThat(actual.nickname).isEqualTo("홍")
+    }
+
+    @Test
+    fun `널 타입`() {
+        val actual = Person("홍길동", 20)
+        assertThat(actual.name).isEqualTo("홍길동")
+        assertThat(actual.age).isEqualTo(20)
+        assertThat(actual.nickname).isNull()
+    }
+}
