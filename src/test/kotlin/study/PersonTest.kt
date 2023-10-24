dcbd119d0ebcb4d137a9da2f2c39c778c6d5e64a
AKMunny, Dean <37904970+sodp5@users.noreply.github.com>
2023-10-24 22:29:57 +0900

    study: 학습 테스트 (#1274)

---
+++ src/test/kotlin/study/PersonTest.kt
@@ -0,0 +1,45 @@
+package study
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class PersonTest {
+    @Test
+    fun `이름 붙인 인자`() {
+        val actual = Person(name = "홍길동", 20, nickname = "홍")
+        assertThat(actual.name).isEqualTo("홍길동")
+        assertThat(actual.age).isEqualTo(20)
+        assertThat(actual.nickname).isEqualTo("홍")
+    }
+
+    @Test
+    fun `널 타입`() {
+        val actual = Person("홍길동", 20, null)
+        assertThat(actual.name).isEqualTo("홍길동")
+        assertThat(actual.age).isEqualTo(20)
+        assertThat(actual.nickname).isNull()
+    }
+
+    @Test
+    fun `기본 인자`() {
+        val actual = Person("홍길동", 20)
+        assertThat(actual.name).isEqualTo("홍길동")
+        assertThat(actual.age).isEqualTo(20)
+        assertThat(actual.nickname).isEqualTo("홍길동")
+    }
+
+    @Test
+    fun `데이터 클래스`() {
+        val person1 = Person("홍길동", 20)
+        val person2 = Person("홍길동", 20)
+        assertThat(person1).isEqualTo(person2)
+    }
+
+    @Test
+    fun `복사`() {
+        val person = Person(name = "홍길동", age = 20)
+        val person2 = person.copy(nickname = "홍")
+        assertThat(person2.name).isEqualTo("홍길동")
+        assertThat(person2.nickname).isEqualTo("홍")
+    }
+}
