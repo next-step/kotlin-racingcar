3bfb8cb0f68843360ad650a6e96c0b995825716d
JunHyun Lee <dlwnsgus777@naver.com>
2023-10-25 23:39:35 +0900

    fix: 깨진 테스트 수정

--- src/test/kotlin/StringCalculatorTest.kt
+++ src/test/kotlin/StringCalculatorTest.kt
@@ -19,7 +19,7 @@ class StringCalculatorTest {
     fun `입력값으로 입렵 문자열 첫번째가 숫자가 아니면 예외가 발생한다`() {
         val calculator = StringCalculator()
         assertThatThrownBy {
-            calculator.calculate("n+1")
+            calculator.calculate("n + 1")
         }.isInstanceOf(IllegalArgumentException::class.java)
     }
 
@@ -27,15 +27,15 @@ class StringCalculatorTest {
     fun `입력값으로 사칙연산 기호 오른쪽이 숫자가 아니면 예외가 발생한다`() {
         val calculator = StringCalculator()
         assertThatThrownBy {
-            calculator.calculate("1+n")
+            calculator.calculate("1 + n")
         }.isInstanceOf(IllegalArgumentException::class.java)
     }
 
     @Test
-    fun `사칙연산이 주어지지 않을 경우 예외가 발생한다`(input: String, result: Int) {
+    fun `사칙연산이 주어지지 않을 경우 예외가 발생한다`() {
         val calculator = StringCalculator()
         assertThatThrownBy {
-            calculator.calculate("1]n")
+            calculator.calculate("1 ] n")
         }.isInstanceOf(IllegalArgumentException::class.java)
     }
 
