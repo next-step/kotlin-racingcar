eb7de2f66d6670a608220138e3f74902327a5f7e
JunHyun Lee <dlwnsgus777@naver.com>
2023-10-25 23:52:19 +0900

    refactor: 에러 문자열 상수 처리

--- src/main/kotlin/StringCalculator.kt
+++ src/main/kotlin/StringCalculator.kt
@@ -1,14 +1,20 @@
 class StringCalculator {
+    companion object {
+        private const val INPUT_VALID_ERROR_MESSAGE: String = "입력값이 null이거나 빈 공백 문자입니다."
+        private const val BAD_INPUT_VALUE_MESSAGE: String = "입력값이 잘못되었습니다."
+        private const val NOT_OPERATOR_MESSAGE: String = "사칙연산 기호가 아닙니다."
+    }
+
     fun calculate(input: String): Int {
         if (input.isBlank()) {
-            throw IllegalArgumentException("입력값이 null이거나 빈 공백 문자입니다.")
+            throw IllegalArgumentException(INPUT_VALID_ERROR_MESSAGE)
         }
         val splitInput = input.split(" ")
-        var result = splitInput.first().toIntOrNull() ?: throw IllegalArgumentException("입력값이 잘못되었습니다.")
+        var result = splitInput.first().toIntOrNull() ?: throw IllegalArgumentException(BAD_INPUT_VALUE_MESSAGE)
 
         for (index in 1 until splitInput.size step 2) {
             val operator = splitInput[index]
-            val targetNumber = splitInput[index + 1].toIntOrNull() ?: throw IllegalArgumentException("입력값이 잘못되었습니다.")
+            val targetNumber = splitInput[index + 1].toIntOrNull() ?: throw IllegalArgumentException(BAD_INPUT_VALUE_MESSAGE)
 
             result = calculation(result, targetNumber, operator)
         }
@@ -22,7 +28,7 @@ class StringCalculator {
             "-" -> firstNumber - secondNumber
             "/" -> firstNumber / secondNumber
             "*" -> firstNumber * secondNumber
-            else -> throw IllegalArgumentException("사칙연산 기호가 아닙니다")
+            else -> throw IllegalArgumentException(NOT_OPERATOR_MESSAGE)
         }
     }
 }
