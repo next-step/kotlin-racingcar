0b98bf08a0dd03430b473a43624cd1ba637bde51
bsgreentea <>
2023-10-26 00:40:35 +0900

    feat: Calculator 클래스 선언 및 사칙연산을 위한 메서드 구성

---
+++ src/main/kotlin/week1/calculator/Calculator.kt
@@ -0,0 +1,25 @@
+package week1.calculator
+
+class Calculator {
+
+    // 계산 시 연산자의 우선순위를 고려하지 않는다.
+    fun calculate(input: String): Int {
+        return 0
+    }
+
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun subtract(a: Int, b: Int): Int {
+        return a - b
+    }
+
+    fun multiply(a: Int, b: Int): Int {
+        return a * b
+    }
+
+    fun divide(a: Int, b: Int): Int {
+        return a / b
+    }
+}
