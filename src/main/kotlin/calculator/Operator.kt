5a6e73732f3ebfa052125f7cb3f29c2535e8dad2
korjun1993 <corojoon93@naver.com>
2023-10-26 01:28:05 +0900

    rebase: rebase step4 on step3

--- src/main/kotlin/calculator/Operator.kt
+++ src/main/kotlin/calculator/Operator.kt
@@ -1,26 +1,16 @@
 package calculator
 
-enum class Operator(val symbol: String, private val operation: IntOperation) {
-    PLUS("+", IntOperation { number1, number2 -> number1 + number2 }),
-    MINUS("-", IntOperation { number1, number2 -> number1 - number2 }),
-    MULTIPLY("*", IntOperation { number1, number2 -> number1 * number2 }),
-    DIVIDE("/", IntOperation { number1, number2 -> number1 / number2 });
+enum class Operator(val symbol: String) {
+    PLUS("+"),
+    MINUS("-"),
+    MULTIPLY("*"),
+    DIVIDE("/");
 
-    fun operate(number1: Int, number2: Int): Int {
-        validate(number2)
-        return operation.operate(number1, number2)
-    }
-
-    private fun validate(number2: Int) {
-        if (this == DIVIDE && number2 == 0) {
-            throw IllegalArgumentException("0으로 나눌 수 없습니다")
+    fun apply(number1: Int, number2: Int) =
+        when (this) {
+            PLUS -> number1 + number2
+            MINUS -> number1 - number2
+            MULTIPLY -> number1 * number2
+            DIVIDE -> number1 / number2
         }
-    }
-
-    companion object {
-        fun findOperator(symbol: String): Operator {
-            return Operator.values().firstOrNull { it.symbol == symbol }
-                ?: throw IllegalArgumentException("사칙 연산 기호가 아닙니다")
-        }
-    }
 }
