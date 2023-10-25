cd4420c9a3f5dc78d1d47c61e9dfb439e2b60725
eunwoo song <presentsong@naver.com>
2023-10-25 23:56:16 +0900

    feat: 곱셈 기능 구현

--- src/main/kotlin/calculator/Operator.kt
+++ src/main/kotlin/calculator/Operator.kt
@@ -3,4 +3,5 @@ package calculator
 enum class Operator(val symbol: String, val operation: (Int, Int) -> Int) {
     PLUS("+", { x, y -> x + y }),
     MINUS("-", { x, y -> x - y }),
+    TIMES("*", { x, y -> x * y }),
 }
--- src/test/kotlin/calculator/OperatorTest.kt
+++ src/test/kotlin/calculator/OperatorTest.kt
@@ -17,4 +17,10 @@ internal class OperatorTest {
     fun `뺄셈 연산이 잘 계산된다`(x: Int, y: Int, result: Int) {
         assertEquals(result, Operator.MINUS.operation(x, y))
     }
+
+    @ParameterizedTest
+    @CsvSource(value = ["1,2,2", "2,3,6", "3,4,12"])
+    fun `곱셈 연산이 잘 계산된다`(x: Int, y: Int, result: Int) {
+        assertEquals(result, Operator.TIMES.operation(x, y))
+    }
 }
