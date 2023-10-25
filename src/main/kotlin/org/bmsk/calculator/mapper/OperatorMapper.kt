a54f0864d8b559e004c8b6d4bfed92834f757225
bmsk <vv99911@gmail.com>
2023-10-26 01:04:14 +0900

    [Feat] Operator 클래스 추가
    - Mapper 함수 작성

---
+++ src/main/kotlin/org/bmsk/calculator/mapper/OperatorMapper.kt
@@ -0,0 +1,13 @@
+package org.bmsk.calculator.mapper
+
+import org.bmsk.calculator.model.Operator
+
+internal fun String.toOperator(): Operator {
+    return when(this) {
+        Operator.Plus.symbol -> Operator.Plus
+        Operator.Minus.symbol -> Operator.Minus
+        Operator.Times.symbol -> Operator.Times
+        Operator.Division.symbol -> Operator.Division
+        else -> throw IllegalArgumentException("$this.toOperator(): ${this}는 정의되지 않은 연산자.")
+    }
+}
---
+++ src/main/kotlin/org/bmsk/calculator/model/Operator.kt
@@ -0,0 +1,8 @@
+package org.bmsk.calculator.model
+
+enum class Operator(val symbol: String) {
+    Plus("+"),
+    Minus("-"),
+    Times("*"),
+    Division("/"),
+}
