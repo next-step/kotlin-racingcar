db7aac3a946e9da454bd67b18839cb32c2536038
ALbertIM0427 <guang7226961@naver.com>
2023-10-25 20:51:34 +0900

    refactor: DisplayName 제거

--- src/test/kotlin/calculator/CalculatorParserTest.kt
+++ src/test/kotlin/calculator/CalculatorParserTest.kt
@@ -3,16 +3,14 @@ package calculator
 import calculator.CalculatorParser.getFirstValue
 import calculator.CalculatorParser.getNextValue
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
 class CalculatorParserTest {
 
-    @DisplayName("입력값을 공백을 기준으로 분리한다.")
     @ParameterizedTest
     @CsvSource(value = ["2 + 3 * 4 / 2:7", "3 + 4 / 5:5", "3 + 1:3"], delimiter = ':')
-    fun split(input: String, expect: Int) {
+    fun `입력값을 공백을 기준으로 분리한다`(input: String, expect: Int) {
         // when
         val actual = CalculatorParser.split(input)
 
@@ -20,10 +18,9 @@ class CalculatorParserTest {
         assertThat(actual.size).isEqualTo(expect)
     }
 
-    @DisplayName("연산자의 위치를 기준으로 다음 값을 int로 변환하고 가져온다.")
     @ParameterizedTest
     @CsvSource(value = ["1:3", "2:4", "3:5"], delimiter = ':')
-    fun getNextValue(currentIndex: Int, expect: Int) {
+    fun `연산자의 위치를 기준으로 다음 값을 int로 변환하고 가져온다`(currentIndex: Int, expect: Int) {
         // given
         val input = listOf("1", "2", "3", "4", "5")
 
@@ -34,10 +31,9 @@ class CalculatorParserTest {
         assertThat(actual).isEqualTo(expect)
     }
 
-    @DisplayName("첫번째 값을 int로 변환하고 가져온다.")
     @ParameterizedTest
     @CsvSource(value = ["1", "2", "3", "4", "5"], delimiter = ':')
-    fun getFirstValue(first: String) {
+    fun `첫번째 값을 int로 변환하고 가져온다`(first: String) {
         // given
         val input = listOf(first, "2", "3", "4", "5")
 
