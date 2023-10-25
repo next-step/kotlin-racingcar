ed1628e01ae8de6f4fa9c49b03672d7ac86b4bd3
Jooan Kim <69291010+wycj2009@users.noreply.github.com>
2023-10-26 00:26:45 +0900

    Step1: 코틀린 기초 학습 (#1306)

---
+++ src/test/kotlin/study/PersonTest.kt
@@ -0,0 +1,49 @@
+package study
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+
+class PersonTest {
+
+    @Test
+    fun `named arguments`() {
+        val people = listOf(
+            Person("박재성", 29, "제이슨"),
+            Person("박재성", 29, nickname = "제이슨"),
+            Person(name = "박재성", nickname = "제이슨", age = 29)
+        )
+        assertThat(people).allSatisfy {
+            assertThat(it.name).isEqualTo("박재성")
+            assertThat(it.age).isEqualTo(29)
+            assertThat(it.nickname).isEqualTo("제이슨")
+        }
+    }
+
+    @Test
+    fun `nullable types`() {
+        val person = Person("박재성", null, null)
+        assertAll(
+            { assertThat(person.name).isEqualTo("박재성") },
+            { assertThat(person.age).isNull() },
+            { assertThat(person.nickname).isNull() }
+        )
+    }
+
+    @Test
+    fun `default arguments`() {
+        val person = Person("박재성")
+        assertAll(
+            { assertThat(person.name).isEqualTo("박재성") },
+            { assertThat(person.age).isNull() },
+            { assertThat(person.nickname).isNull() }
+        )
+    }
+
+    @Test
+    fun `data classes`() {
+        val person1 = Person("박재성", 29, "제이슨")
+        val person2 = Person("박재성", 29, "제이슨")
+        assertThat(person1).isEqualTo(person2)
+    }
+}
