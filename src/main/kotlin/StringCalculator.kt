ab43dcfa20f3e4579649bd83444f059d4dd6c8ef
JunHyun Lee <dlwnsgus777@naver.com>
2023-10-25 23:22:25 +0900

    feat: 입력값 검증 기능 개발

--- src/main/kotlin/StringCalculator.kt
+++ src/main/kotlin/StringCalculator.kt
@@ -3,11 +3,14 @@ class StringCalculator {
         if (input.isBlank()) {
             throw IllegalArgumentException("입력값이 null이거나 빈 공백 문자입니다.")
         }
+        val splitInput = input.split(" ")
+        var result = splitInput.first().toIntOrNull() ?: throw IllegalArgumentException("입력값이 잘못되었습니다.")
 
-        input.split(" ").forEach {
-            println(it)
+        for (index in 1 until splitInput.size step 2) {
+            val operator = splitInput[index]
+            val targetNumber = splitInput[index + 1].toIntOrNull() ?: throw IllegalArgumentException("입력값이 잘못되었습니다.")
         }
 
-        return 0
+        return result
     }
 }
--- src/test/kotlin/StringCalculatorTest.kt
+++ src/test/kotlin/StringCalculatorTest.kt
@@ -10,4 +10,20 @@ class StringCalculatorTest {
             calculator.calculate("")
         }.isInstanceOf(IllegalArgumentException::class.java)
     }
+
+    @Test
+    fun `입력값으로 입렵 문자열 첫번째가 숫자가 아니면 오류가 발생한다`() {
+        val calculator = StringCalculator()
+        assertThatThrownBy {
+            calculator.calculate("n+1")
+        }.isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @Test
+    fun `입력값으로 사칙연산 기호 오른쪽이 숫자가 아니면 오류가 발생한다`() {
+        val calculator = StringCalculator()
+        assertThatThrownBy {
+            calculator.calculate("1+n")
+        }.isInstanceOf(IllegalArgumentException::class.java)
+    }
 }
