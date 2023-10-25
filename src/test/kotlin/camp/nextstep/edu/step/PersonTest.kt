623152765dff5e43d29307ae4469cd8ae3686960
kimueibin <at365@naver.com>
2023-10-26 01:47:52 +0900

    chore : 원활한 step별 학습관리를 위한 이전 step 패키지 이동

--- src/test/kotlin/camp/nextstep/edu/step/PersonTest.kt
+++ src/test/kotlin/camp/nextstep/edu/step/PersonTest.kt
@@ -1,5 +1,6 @@
 package camp.nextstep.edu.step
 
+import camp.nextstep.edu.step.person.Person
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
