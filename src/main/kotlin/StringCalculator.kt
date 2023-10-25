c8722283591c1f4613246c54c11d8ef3b76d642a
장진혁/Client개발팀/원스토어 <ironelder@onestorecorp.com>
2023-10-25 14:16:07 +0900

    [Step2] 문자열계산기 : 계산 클래스 추가
    사칙연산 , null , 공백 검사를 추가한 문자열 계산 클래스 추가

---
+++ src/main/kotlin/StringCalculator.kt
@@ -0,0 +1,36 @@
+class StringCalculator constructor(private val calculatorString: String?) {
+
+    private val numberArgs by lazy {
+        require(!calculatorString.isNullOrEmpty()) { throw IllegalArgumentException()}
+        calculatorString.split("").filter { it.isNotBlank() }.filter { it.toIntOrNull() != null }.map { it.toInt() }
+    }
+    private val operatorArgs by lazy {
+        require(!calculatorString.isNullOrEmpty()) { throw IllegalArgumentException()}
+        calculatorString.split("").filter { it.isNotBlank() }.filter { it.toIntOrNull() == null }.toMutableList()
+    }
+
+    @Throws(IllegalArgumentException::class)
+    fun calculating(): Int = numberArgs.reduce { acc, next ->
+        when (operatorArgs.removeFirst()) {
+            "+" -> plus(acc, next)
+            "-" -> minus(acc, next)
+            "*" -> multiple(acc, next)
+            "/" -> divide(acc, next)
+            else -> throw IllegalArgumentException()
+        }
+    }
+
+    private fun plus(first: Int, second: Int): Int = first + second
+    private fun minus(first: Int, second: Int): Int = first - second
+    private fun multiple(first: Int, second: Int): Int = first * second
+    private fun divide(first: Int, second: Int): Int = first / second
+
+}
+
+fun main() {
+
+    StringCalculator("2 + 3 * 4 / 2").apply {
+        println("result = ${calculating()}")
+    }
+
+}
