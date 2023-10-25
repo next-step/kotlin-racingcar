68ac578867940534d43d835dc6859c480a58ca27
ALbertIM0427 <guang7226961@naver.com>
2023-10-25 20:59:01 +0900

    test: StringCalculator 테스트 구현

--- src/test/kotlin/calculator/StringCalculatorTest.kt
+++ src/test/kotlin/calculator/StringCalculatorTest.kt
@@ -2,42 +2,17 @@ package calculator
 
 import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
-import org.junit.jupiter.params.provider.ValueSource
 
 class StringCalculatorTest {
 
     @ParameterizedTest
-    @ValueSource(strings = ["", " "])
-    fun `입력값이 빈 공백 문자일 경우 IllegalArgumentException throw`(input: String) {
+    @CsvSource(value = ["2:3:^", "3:5:&", "1:3:|"], delimiter = ':')
+    fun `사칙연산 기호가 아닌 경우 예외를 던진다`(currentResult: Int, nextValue: Int, operator: String) {
         Assertions.assertThatThrownBy {
             // when
-            ExpressionCalculator.calculate(input)
-        }
-            // then
-            .isInstanceOf(IllegalArgumentException::class.java)
-            .message().isEqualTo(ErrorMessage.NULL_OR_BLANK_ERROR_MSG.message)
-    }
-
-    @Test
-    fun `입력값이 null일 경우 IllegalArgumentException throw`() {
-        Assertions.assertThatThrownBy {
-            // when
-            ExpressionCalculator.calculate(null)
-        }
-            // then
-            .isInstanceOf(IllegalArgumentException::class.java)
-            .message().isEqualTo(ErrorMessage.NULL_OR_BLANK_ERROR_MSG.message)
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = ["2 ^ 3", "3 & 5", "1 | 3"])
-    fun `사칙연산 기호가 아닌 경우 IllegalArgumentException throw`(input: String) {
-        Assertions.assertThatThrownBy {
-            // when
-            ExpressionCalculator.calculate(input)
+            StringCalculator.applyOperation(currentResult, nextValue, operator)
         }
             // then
             .isInstanceOf(IllegalArgumentException::class.java)
@@ -45,47 +20,12 @@ class StringCalculatorTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = ["2 + 3 * 4 / 2:10", "-2 + 3 * 4:4", "3 * -5 - 7:-22", "1 + 3 / 5:0"], delimiter = ':')
-    fun `덧셈, 뺄셈, 곱셈, 나눗셈 혼합`(input: String, expect: Int) {
+    @CsvSource(value = ["2:3:+:5", "3:5:-:-2", "1:3:*:3", "3:5:/:0"], delimiter = ':')
+    fun `덧셈, 뺄셈, 곱셈, 나눗셈`(currentResult: Int, nextValue: Int, operator: String, expect: Int) {
         // when
-        val actual = ExpressionCalculator.calculate(input)
+        val actual = StringCalculator.applyOperation(currentResult, nextValue, operator)
 
         // then
         assertThat(actual).isEqualTo(expect)
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = ["a + b", "! + 4", "a + 4", "2 + b", "2 + 3 + b", "2 + 3 + 4 + b", "2 + 3 + 4 + 5 + b"])
-    fun `값의 위치에 수자 아닐 때 IllegalArgumentException throw`(input: String) {
-        Assertions.assertThatThrownBy {
-            // when
-            ExpressionCalculator.calculate(input)
-        }
-            // then
-            .isInstanceOf(NumberFormatException::class.java)
-    }
-
-    @ParameterizedTest
-    @CsvSource(value = ["2 + 3 +", "3 + 5 + 44 55", "1 + 3 + 4 *", "1 + 3 + 4 /"])
-    fun `문자 파싱 후 길이가 짝수일 경우 IllegalArgumentException throw`(input: String) {
-        Assertions.assertThatThrownBy {
-            // when
-            ExpressionCalculator.calculate(input)
-        }
-            // then
-            .isInstanceOf(IllegalArgumentException::class.java)
-            .message().isEqualTo(ErrorMessage.ODD_SIZE_REQUIREMENT_MSG.message)
-    }
-
-    @ParameterizedTest
-    @CsvSource(value = ["2 +", "3", "1 +"])
-    fun `길이가 3보다 작을 경우 IllegalArgumentException throw`(input: String) {
-        Assertions.assertThatThrownBy {
-            // when
-            ExpressionCalculator.calculate(input)
-        }
-            // then
-            .isInstanceOf(IllegalArgumentException::class.java)
-            .message().isEqualTo(ErrorMessage.MINIMUM_SIZE_REQUIREMENT_MSG.message)
     }
 }
