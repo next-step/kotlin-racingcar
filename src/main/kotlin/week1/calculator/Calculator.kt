741a12ebe4537ecd51093c803fb588b83d23d1fd
bsgreentea <bsgreentea1@gmail.com>
2023-10-26 02:12:39 +0900

    feat: 입력 식 계산 메서드 구현

--- src/main/kotlin/week1/calculator/Calculator.kt
+++ src/main/kotlin/week1/calculator/Calculator.kt
@@ -4,7 +4,37 @@ class Calculator {
 
     // 계산 시 연산자의 우선순위를 고려하지 않는다.
     fun calculate(input: String?): Int {
-        return 0
+        checkInput(input)
+        checkNotNull(input)
+
+        val numbers = mutableListOf<Int>()
+        val operators = mutableListOf<OperatorType>()
+        input.split(' ').forEach {
+            println("it = [$it]")
+            if (it.length == 1 && isOperatorType(it)) {
+                OperatorType.from(it)?.let { type ->
+                    operators.add(type)
+                }
+            } else {
+                numbers.add(it.toInt())
+            }
+        }
+
+        var result = numbers.first()
+        operators.forEachIndexed { idx, operatorType ->
+            val a = result
+            val b = numbers[idx + 1]
+
+            result = when (operatorType) {
+                OperatorType.ADD -> add(a, b)
+                OperatorType.SUBTRACT -> subtract(a, b)
+                OperatorType.MULTIPLY -> multiply(a, b)
+                OperatorType.DIVIDE -> divide(a, b)
+            }
+        }
+
+        println(result)
+        return result
     }
 
     fun add(a: Int, b: Int): Int {
@@ -27,8 +57,8 @@ class Calculator {
         if (input.isNullOrBlank()) throw IllegalArgumentException()
     }
 
-    fun checkOperatorType(candidate: String) {
-        OperatorType.valueOf(candidate)
+    fun isOperatorType(candidate: String): Boolean {
+        return OperatorType.values().any { it.value == candidate }
     }
 
 }
--- src/main/kotlin/week1/calculator/OperatorType.kt
+++ src/main/kotlin/week1/calculator/OperatorType.kt
@@ -1,10 +1,17 @@
 package week1.calculator
 
 enum class OperatorType(
-    val type: String,
+    val value: String,
 ) {
     ADD("+"),
     SUBTRACT("-"),
     MULTIPLY("*"),
     DIVIDE("/"),
+    ;
+
+    companion object {
+        fun from(value: String): OperatorType? {
+            return OperatorType.values().firstOrNull { it.value == value }
+        }
+    }
 }
