266cd7b9527cd82f7e7ecda9511f3817c41acae4
dong <dong.22@kakaoenterprise.com>
2023-10-25 23:14:47 +0900

    feat : 입력값 검증부분 추가

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -1,7 +1,24 @@
 package calculator
 
 class StringCalculator {
+
     fun calculate(input: String?): Int {
+        val inputString: String = validate(input)
         return -1
+    }
+
+    private fun validate(input: String?): String {
+        assertNotNull(input)
+        assertNotEmpty(input)
+
+        return input!!
+    }
+
+    private fun assertNotEmpty(input: String?) {
+        TODO("Not yet implemented")
+    }
+
+    private fun assertNotNull(input: String?) {
+        TODO("Not yet implemented")
     }
 }
