75ce782a3a9d095cc6be043f36a33e07e0b7114b
송은우 <80899085+be-student@users.noreply.github.com>
2023-10-25 22:41:25 +0900

    feat: person 학습 테스트 작성 (#1308)

---
+++ src/test/kotlin/study/PersonTest.kt
@@ -0,0 +1,38 @@
+package study
+
+import org.assertj.core.api.Java6Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertAll
+import org.junit.jupiter.api.Test
+
+internal class PersonTest {
+    @Test
+    fun `named arguments`() {
+        val people = listOf(
+            Person("박재성", 29, "제이슨"),
+            Person("박재성", 29, nickname = "제이슨"),
+            Person(name = "박재성", nickname = "제이슨", age = 29)
+        )
+        assertThat(people).allSatisfy {
+            assertThat(it.name).isEqualTo("박재성")
+            assertThat(it.age).isEqualTo(29)
+            assertThat(it.nickname).isEqualTo("제이슨")
+        }
+    }
+
+    @Test
+    fun `nullable types`() {
+        val person = Person("박재성", null, null)
+        assertAll(
+            { assertThat(person.name).isEqualTo("박재성") },
+            { assertThat(person.age).isNull() },
+            { assertThat(person.nickname).isNull() }
+        )
+    }
+
+    @Test
+    fun `data classes`() {
+        val person1 = Person("박재성", 29, "제이슨")
+        val person2 = Person("박재성", 29, "제이슨")
+        assertThat(person1).isEqualTo(person2)
+    }
+}
