828f8a1de916430437dd07e00fc68af97fe15df4
Yunho-Park <chicken0980@gmail.com>
2023-10-25 16:16:00 +0900

    study: learn kotlin basics (#1246)
    * study: learn kotlin basics
    
    * 누락된 파일 추가
    
    * 데이터클래스 테스트 추가
    
    ---------
    
    Co-authored-by: chacha.roni <chacha.roni@kakaocorp.com>

---
+++ src/test/kotlin/study/Person.kt
@@ -0,0 +1,5 @@
+package study
+
+data class Person(val name: String, val age: Int, var nickname: String? = name) {
+    constructor(name: String, age: Int) : this(name, age, name)
+}
---
+++ src/test/kotlin/study/PersonTest.kt
@@ -0,0 +1,50 @@
+package study
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class PersonTest {
+
+    @Test
+    fun `이름 붙인 인자`() {
+        val person = Person(nickname = "길동", name = "홍길동", age = 100)
+        assert(person.name == "홍길동")
+        assert(person.age == 100)
+        assert(person.nickname == "길동")
+    }
+
+    @Test
+    fun `null 타입`() {
+        val person = Person(nickname = null, name = "홍길동", age = 100)
+        assert(person.name == "홍길동")
+        assertThat(person.age).isEqualTo(100)
+        assertThat(person.nickname).isNull()
+    }
+
+    @Test
+    fun `기본 인자`() {
+        val person = Person(name = "홍길동", age = 100)
+        assertThat(person.name).isEqualTo("홍길동")
+        assertThat(person.age).isEqualTo(100)
+        assertThat(person.nickname).isEqualTo("홍길동")
+    }
+
+    @Test
+    fun `데이터 클래스`() {
+        val person1 = Person("홍길동", 100)
+        val person2 = Person("홍길동", 100, "홍")
+        assertThat(person1).isNotEqualTo(person2)
+        person2.nickname = person2.name
+        assertThat(person1).isEqualTo(person2)
+    }
+
+    @Test
+    fun copy() {
+        val person = Person(name = "홍길동", age = 100)
+
+        val person2 = person.copy(nickname = "홍")
+        assertThat(person2.name).isEqualTo("홍길동")
+        assertThat(person2.age).isEqualTo(100)
+        assertThat(person2.nickname).isEqualTo("홍")
+    }
+}
