c88410ef1ac9ecfced3b6fbed5ad4f3e6d26dd25
yeongun <gjduddns@gmail.com>
2023-10-25 20:26:21 +0900

    feat: exception 테스트 추가

--- src/test/kotlin/step2/StringCalculatorTest.kt
+++ src/test/kotlin/step2/StringCalculatorTest.kt
@@ -1,5 +1,6 @@
 package step2
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
@@ -15,5 +16,38 @@ class StringCalculatorTest : StringSpec({
 
         // then
         actual shouldBe 10
+    }
+
+    "문자열 계산기 테스트 - 입력값이 null이면 에러가 발생한다." {
+        // given
+        val stringCalculator = StringCalculator()
+        val input = null
+
+        // when & then
+        shouldThrow<IllegalArgumentException> {
+            stringCalculator.calculate(input)
+        }
+    }
+
+    "문자열 계산기 테스트 - 입력값이 공백이면 에러가 발생한다." {
+        // given
+        val stringCalculator = StringCalculator()
+        val input = " "
+
+        // when & then
+        shouldThrow<IllegalArgumentException> {
+            stringCalculator.calculate(input)
+        }
+    }
+
+    "문자열 계산기 테스트 - 올바른 사칙연산을 입력하지 않으면 에러가 발생한다." {
+        // given
+        val stringCalculator = StringCalculator()
+        val input = "2 ? 3"
+
+        // when & then
+        shouldThrow<IllegalArgumentException> {
+            stringCalculator.calculate(input)
+        }
     }
 })
