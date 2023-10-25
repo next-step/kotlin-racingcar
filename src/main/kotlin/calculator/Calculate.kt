28ef091e52750771175b1d2821b034e3e8c8e153
Alex <kpmyung@gamil.com>
2023-10-25 11:54:18 +0900

    feat: 사칙연산 기능 개발

---
+++ src/main/kotlin/calculator/Calculate.kt
@@ -0,0 +1,17 @@
+package calculator
+
+fun add(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun subtract(a: Int, b: Int): Int {
+    return a - b
+}
+
+fun multiply(a: Int, b: Int): Int {
+    return a * b
+}
+
+fun divide(a: Int, b: Int): Int {
+    return a / b
+}
