25a36b377e76de16a6956bb450ad60c5c1362329
JunHyun Lee <dlwnsgus777@naver.com>
2023-10-25 23:32:27 +0900

    feat: 사칙연산 기능 개발

--- src/main/kotlin/StringCalculator.kt
+++ src/main/kotlin/StringCalculator.kt
@@ -9,8 +9,20 @@ class StringCalculator {
         for (index in 1 until splitInput.size step 2) {
             val operator = splitInput[index]
             val targetNumber = splitInput[index + 1].toIntOrNull() ?: throw IllegalArgumentException("입력값이 잘못되었습니다.")
+
+            result = calculation(result, targetNumber, operator)
         }
 
         return result
+    }
+
+    private fun calculation(firstNumber: Int, secondNumber: Int, operate: String): Int {
+        return when (operate) {
+            "+" -> firstNumber + secondNumber
+            "-" -> firstNumber - secondNumber
+            "/" -> firstNumber / secondNumber
+            "*" -> firstNumber * secondNumber
+            else -> throw IllegalArgumentException("사칙연산 기호가 아닙니다")
+        }
     }
 }
--- src/test/kotlin/StringCalculatorTest.kt
+++ src/test/kotlin/StringCalculatorTest.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 class StringCalculatorTest {
 
     @Test
-    fun `입력값으로 공백문자가 전달되면 오류가 발생한다`() {
+    fun `입력값으로 공백문자가 전달되면 예외가 발생한다`() {
         val calculator = StringCalculator()
         assertThatThrownBy {
             calculator.calculate("")
@@ -12,7 +12,7 @@ class StringCalculatorTest {
     }
 
     @Test
-    fun `입력값으로 입렵 문자열 첫번째가 숫자가 아니면 오류가 발생한다`() {
+    fun `입력값으로 입렵 문자열 첫번째가 숫자가 아니면 예외가 발생한다`() {
         val calculator = StringCalculator()
         assertThatThrownBy {
             calculator.calculate("n+1")
@@ -20,10 +20,18 @@ class StringCalculatorTest {
     }
 
     @Test
-    fun `입력값으로 사칙연산 기호 오른쪽이 숫자가 아니면 오류가 발생한다`() {
+    fun `입력값으로 사칙연산 기호 오른쪽이 숫자가 아니면 예외가 발생한다`() {
         val calculator = StringCalculator()
         assertThatThrownBy {
             calculator.calculate("1+n")
+        }.isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @Test
+    fun `사칙연산이 주어지지 않을 경우 예외가 발생한다`(input: String, result: Int) {
+        val calculator = StringCalculator()
+        assertThatThrownBy {
+            calculator.calculate("1]n")
         }.isInstanceOf(IllegalArgumentException::class.java)
     }
 }
