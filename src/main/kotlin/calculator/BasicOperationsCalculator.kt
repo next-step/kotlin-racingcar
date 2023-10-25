82f8ace41de739998f64cfa53e64ab22dbbeb3a2
Jihoon, Shin <jihoon9030@gmail.com>
2023-10-25 18:16:12 +0900

    feat : BasicOperationCalculator 의 기본 4가지 연산을 정의합니다.

---
+++ src/main/kotlin/calculator/BasicOperationsCalculator.kt
@@ -0,0 +1,8 @@
+package calculator
+
+class BasicOperationsCalculator {
+    fun add(a: Double, b: Double) : Double = a+b
+    fun subtract(a: Double, b: Double) : Double = a-b
+    fun multiply(a: Double, b: Double) : Double = a*b
+    fun divide(a: Double, b: Double) : Double = a/b
+}
