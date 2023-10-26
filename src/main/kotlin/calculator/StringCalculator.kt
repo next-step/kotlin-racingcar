3e2da397bd26be34aeccdf6745b97a04fda72072
yw9594 <kyw9509@gmail.com>
2023-10-26 18:16:06 +0900

    feat(step2): add string calculator

---
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -0,0 +1,56 @@
+package calculator
+
+class StringCalculator {
+    companion object {
+        private const val DELIMITER = " "
+        private const val PLUS = "+"
+        private const val MINUS = "-"
+        private const val MULTIPLY = "*"
+        private const val DIVIDE = "/"
+
+        fun calculate(expression: String?): Int {
+            val tokens = StringExpression
+                .validate(expression)
+                .split(DELIMITER)
+
+            val firstNumber = tokens
+                .first()
+                .toInt()
+
+            return tokens
+                .drop(1)
+                .chunked(2)
+                .fold(firstNumber) { acc, cur ->
+                    applyOperation(
+                        number1 = acc,
+                        number2 = cur[1].toInt(),
+                        operator = cur[0]
+                    )
+                }
+        }
+
+        private fun applyOperation(number1: Int, number2: Int, operator: String): Int {
+            return when (operator) {
+                PLUS -> number1 + number2
+                MINUS -> number1 - number2
+                MULTIPLY -> number1 * number2
+                DIVIDE -> number1 / number2
+                else -> throw IllegalArgumentException("unknown operation type.")
+            }
+        }
+    }
+}
+
+class StringExpression {
+    companion object {
+        private val formatRegex = """^[0-9]+(?: [+\-*/] [0-9]+)*$""".toRegex()
+
+        fun validate(expression: String?): String {
+            if (expression.isNullOrBlank()) throw IllegalArgumentException("expression is null or blank.")
+
+            if (!formatRegex.matches(expression)) throw IllegalArgumentException("invalid expression format.")
+
+            return expression
+        }
+    }
+}
