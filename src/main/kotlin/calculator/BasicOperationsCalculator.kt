ac9084a5c8d80ca5681a54eb54169968b1c084f7
Jihoon, Shin <jihoon9030@gmail.com>
2023-10-26 15:24:44 +0900

    feat : BasicOperationsCalculator 에서 symbol 과 input 2개가 주어졌을 때, 각각을 계산하는 코드를 작성합니다.

--- src/main/kotlin/calculator/BasicOperationsCalculator.kt
+++ src/main/kotlin/calculator/BasicOperationsCalculator.kt
@@ -1,8 +1,30 @@
 package calculator
 
+import java.lang.IllegalArgumentException
+
 class BasicOperationsCalculator {
     fun add(a: Double, b: Double) : Double = a+b
     fun subtract(a: Double, b: Double) : Double = a-b
     fun multiply(a: Double, b: Double) : Double = a*b
     fun divide(a: Double, b: Double) : Double = a/b
+
+    fun operation(a: Double, b: Double, symbol : String): Double {
+        if (symbol == "+"){
+            return add(a, b)
+        }
+
+        if (symbol == "-"){
+            return subtract(a, b)
+        }
+
+        if (symbol == "*"){
+            return multiply(a, b)
+        }
+
+        if (symbol == "/"){
+            return divide(a, b)
+        }
+
+        throw IllegalArgumentException("Not Valid Form")
+    }
 }
