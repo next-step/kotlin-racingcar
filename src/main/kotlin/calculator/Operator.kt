155e64d0e63bda36241e5ef42f387b05b433aa9b
jaylene shin <jaylene.shin@kakaocorp.com>
2023-10-27 01:53:27 +0900

    feat: enum 초기화값 변경

--- src/main/kotlin/calculator/Operator.kt
+++ src/main/kotlin/calculator/Operator.kt
@@ -1,5 +1,14 @@
 package calculator
 
-enum class Operator {
-    PLUS, MINUS, MULTIPLY, DIVIDE
+enum class Operator(val symbol: String, val calcFunc: (Int, Int) -> Int) {
+    PLUS("+", { operand1, operand2 -> operand1 + operand2}),
+    MINUS("-", { operand1, operand2 -> operand1 - operand2}),
+    MULTIPLY("*", { operand1, operand2 -> operand1 * operand2}),
+    DIVIDE("/", { operand1, operand2 -> operand1 / operand2});
+
+    companion object {
+        fun fromSymbol(symbol: String): Operator {
+            return values().find { it.symbol == symbol } ?: throw IllegalArgumentException("Unsupported operator symbol: $symbol")
+        }
+    }
 }
