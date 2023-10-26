aac8697bfa7cd7c2e38ac59e06671a805aa1d694
inhyeokk <rkddlsgur983@gmail.com>
2023-10-26 14:26:47 +0900

    step2: ParameterizedTest 적용

--- src/test/kotlin/calculator/StringCalculatorTest.kt
+++ src/test/kotlin/calculator/StringCalculatorTest.kt
@@ -3,29 +3,32 @@ package calculator
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class StringCalculatorTest {
-    @Test
-    fun `덧셈`() {
-        val input = "2 + 3"
+
+    @ParameterizedTest
+    @ValueSource(strings = ["2 + 3"])
+    fun `덧셈`(input: String) {
         assertThat(StringCalculator.calculate(input)).isEqualTo(5)
     }
 
-    @Test
-    fun `뺄셈`() {
-        val input = "2 - 3"
+    @ParameterizedTest
+    @ValueSource(strings = ["2 - 3"])
+    fun `뺄셈`(input: String) {
         assertThat(StringCalculator.calculate(input)).isEqualTo(-1)
     }
 
-    @Test
-    fun `곱셈`() {
-        val input = "3 * 4"
+    @ParameterizedTest
+    @ValueSource(strings = ["3 * 4"])
+    fun `곱셈`(input: String) {
         assertThat(StringCalculator.calculate(input)).isEqualTo(12)
     }
 
-    @Test
-    fun `나눗셈`() {
-        val input = "4 / 2"
+    @ParameterizedTest
+    @ValueSource(strings = ["4 / 2"])
+    fun `나눗셈`(input: String) {
         assertThat(StringCalculator.calculate(input)).isEqualTo(2)
     }
 
@@ -37,33 +40,33 @@ class StringCalculatorTest {
         }.isInstanceOf(IllegalArgumentException::class.java)
     }
 
-    @Test
-    fun `입력 값이 빈 공백 문자일 경우 IllegalArgumentException throw`() {
-        val input = "   "
+    @ParameterizedTest
+    @ValueSource(strings = ["   "])
+    fun `입력 값이 빈 공백 문자일 경우 IllegalArgumentException throw`(input: String) {
         assertThatThrownBy {
             StringCalculator.calculate(input)
         }.isInstanceOf(IllegalArgumentException::class.java)
     }
 
-    @Test
-    fun `사칙 연산 기호가 아닌 경우 IllegalArgumentException throw`() {
-        val input = "2 & 3"
+    @ParameterizedTest
+    @ValueSource(strings = ["2 & 3"])
+    fun `사칙 연산 기호가 아닌 경우 IllegalArgumentException throw`(input: String) {
         assertThatThrownBy {
             StringCalculator.calculate(input)
         }.isInstanceOf(IllegalArgumentException::class.java)
     }
 
-    @Test
-    fun `올바른 계산식이 아닌 경우 IllegalArgumentException throw`() {
-        val input = "2 + 3 * 4 /"
+    @ParameterizedTest
+    @ValueSource(strings = ["2 + 3 * 4 /"])
+    fun `올바른 계산식이 아닌 경우 IllegalArgumentException throw`(input: String) {
         assertThatThrownBy {
             StringCalculator.calculate(input)
         }.isInstanceOf(IllegalArgumentException::class.java)
     }
 
-    @Test
-    fun `사칙 연산을 모두 포함하는 기능 구현`() {
-        val input = "2 + 3 * 4 / 2"
+    @ParameterizedTest
+    @ValueSource(strings = ["2 + 3 * 4 / 2"])
+    fun `사칙 연산을 모두 포함하는 기능 구현`(input: String) {
         assertThat(StringCalculator.calculate(input)).isEqualTo(10)
     }
 }
