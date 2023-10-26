e4f28312b1892a04458e264646e7dde48411617a
inhyeokk <rkddlsgur983@gmail.com>
2023-10-26 14:42:23 +0900

    step2: validate 메서드 분리

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -31,6 +31,11 @@ class StringCalculator private constructor(
 
     companion object {
         fun calculate(input: String?): Int {
+            val pair = validate(input)
+            return StringCalculator(pair.first, pair.second).calculate()
+        }
+
+        private fun validate(input: String?): Pair<MutableList<Int>, List<Operator>> {
             if (input.isNullOrBlank()) throw IllegalArgumentException("입력 값이 null 이거나 빈 공백 문자입니다.")
             val splits = input.split(" ").map {
                 if (!it.matches(Regex(CALCULATOR_REGEX))) throw IllegalArgumentException("입력 값에 사칙 연산이 아닌 기호가 포함되었습니다.")
@@ -39,7 +44,7 @@ class StringCalculator private constructor(
             val numbers = splits.filter { it.matches(Regex(NUMBER_REGEX)) }.mapTo(mutableListOf()) { it.toInt() }
             val operators = splits.filter { it.matches(Regex(OPERATOR_REGEX)) }.mapNotNull { Operator.from(it) }
             if (numbers.size != operators.size + 1) throw IllegalArgumentException("올바른 계산식이 아닙니다.")
-            return StringCalculator(numbers, operators).calculate()
+            return numbers to operators
         }
     }
 }
