fd2657b386795c06dd52560f4e9808da8d5056e3
Factoriall <47839550+Factoriall@users.noreply.github.com>
2023-10-26 22:53:10 +0900

    2단계: 문자열 계산기 (#1303)
    * step2: create calculator code
    
    * fix: rename package study to step2
    
    * fix: use ParameterizedTest
    
    * step2: create Input data class
    
    * step2: create Input data class
    
    * step2: add isFormulaValid logic
    
    * step2: create Arithmetic enum class
    
    * step2: add splitList, isFormulaValid method using Arithmetic enum class

---
+++ src/main/kotlin/step2/Arithmetic.kt
@@ -0,0 +1,5 @@
+package step2
+
+enum class Arithmetic(val expr: String) {
+    PLUS("+"), MINUS("-"), TIMES("*"), DIVIDE("/")
+}
---
+++ src/test/kotlin/step2/CalculatorTest.kt
@@ -0,0 +1,65 @@
+package step2
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.NullAndEmptySource
+import org.junit.jupiter.params.provider.ValueSource
+import java.lang.IllegalArgumentException
+
+class CalculatorTest {
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = [" ", "   ", "\t", "\n"])
+    fun `입력값이 null이거나 빈 공백문자인 경우 예외를 던진다`(text: String?) {
+        val calculator = Calculator()
+
+        assertThatThrownBy { calculator.calculate(Input(text)) }.isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["+", "1 + 2 *", "1 2 3", "1 / / 2"])
+    fun `값이 잘 안 들어간 경우 예외를 던진다`(candidate: String) {
+        val calculator = Calculator()
+
+        assertThatThrownBy { calculator.calculate(Input(candidate)) }.isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @Test
+    fun `더하기`() {
+        val calculator = Calculator()
+
+        assertThat(calculator.calculate(Input("1 + 2"))).isEqualTo(3)
+    }
+
+    @Test
+    fun `빼기`() {
+        val calculator = Calculator()
+
+        assertThat(calculator.calculate(Input("1 - 2"))).isEqualTo(-1)
+    }
+
+    @Test
+    fun `곱하기`() {
+        val calculator = Calculator()
+
+        assertThat(calculator.calculate(Input("12 * 4"))).isEqualTo(48)
+    }
+
+    @Test
+    fun `나누기`() {
+        val calculator = Calculator()
+
+        assertThat(calculator.calculate(Input("12 / 4"))).isEqualTo(3)
+    }
+
+    @Test
+    fun `복잡한 식`() {
+        val calculator = Calculator()
+
+        assertThat(calculator.calculate(Input("2 + 3 * 4 / 2"))).isEqualTo(10)
+        assertThat(calculator.calculate(Input("2 * 3 * 4 / 2 - 1"))).isEqualTo(11)
+        assertThat(calculator.calculate(Input("2 + 3 * 4 / 2 - 1"))).isEqualTo(9)
+    }
+}
