5d528acd23eb5b0af5ae4eaad94aaf46d225fd09
kimueibin <at365@naver.com>
2023-10-27 01:51:22 +0900

    refactor : 최종 결과 객체 네이밍 변경에 의한 Command 인터페이스 내 네이밍 변경

--- src/main/kotlin/camp/nextstep/edu/step/calculator/command/CalculateCommand.kt
+++ src/main/kotlin/camp/nextstep/edu/step/calculator/command/CalculateCommand.kt
@@ -1,10 +1,10 @@
 package camp.nextstep.edu.step.calculator.command
 
+import camp.nextstep.edu.step.calculator.domain.FinalResult
 import camp.nextstep.edu.step.calculator.domain.Requester
-import camp.nextstep.edu.step.calculator.domain.Result
 
 interface CalculateCommand {
 
-    fun calculate(requester: Requester): Result
+    fun calculate(requester: Requester): FinalResult
 
 }
