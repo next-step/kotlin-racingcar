96b5e3de2fcce4fc2fa838c523cf0df05247c772
Jaeyeon <59726665+jaeyeonme@users.noreply.github.com>
2023-10-26 22:23:31 +0900

    [step2] 문자열 계산기 구현 (#1297)
    * step2 : Implementation of four basic operations and test logic
    
    * step2 : Modify test code and modify logic
    
    * step2 : Modify test code and modify logic
    
    * step2 : ./gradlew ktlintFormat
    
    * step2 : 메서드 분리 연산 로직을 적용하여 로직을 수정 및 테스틑 코드 추가
    
    * step2 : 반환값 추가

---
+++ src/test/kotlin/study/step2/Calculator.kt
@@ -0,0 +1,35 @@
+package study.step2
+
+class Calculator() {
+
+    fun calculate(input: String): Int {
+        input.takeIf { it.isNotBlank() } ?: throw IllegalArgumentException("입력값이 비어있습니다.")
+
+        val elements = input.split(" ")
+        var result = elements[0].toIntOrNull() ?: throw IllegalArgumentException("올바르지 않는 입력값입니다.")
+
+        for (i in 1 until elements.size step 2) {
+            val operation = elements[i]
+            val operand = elements[i + 1].toIntOrNull() ?: throw IllegalArgumentException("올바르지 않는 입력값입니다.")
+
+            result = calculate(operation, result, operand)
+        }
+        return result
+    }
+
+    private fun calculate(operator: String, result: Int, operand: Int): Int = when (operator) {
+        "+" -> add(result, operand)
+        "-" -> subtract(result, operand)
+        "*" -> multiply(result, operand)
+        "/" -> divide(result, operand)
+        else -> throw IllegalArgumentException("연산자는 +, -, *, / 중 하나여야 합니다.")
+    }
+
+    private fun add(a: Int, b: Int) = a + b
+    private fun subtract(a: Int, b: Int) = a - b
+    private fun multiply(a: Int, b: Int) = a * b
+    private fun divide(a: Int, b: Int): Int {
+        b.takeIf { it != 0 } ?: throw IllegalArgumentException("0으로 나눌 수 없습니다.")
+        return a / b
+    }
+}
---
+++ src/test/kotlin/study/step2/CalculatorTest.kt
@@ -0,0 +1,84 @@
+package study.step2
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class CalculatorTest {
+
+    val calculator = Calculator()
+
+    @Test
+    fun `덧셈`() {
+        val input = "1 + 2"
+        val expected = 3
+        val actual = calculator.calculate(input)
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `뺄셈`() {
+        val input = "2 - 1"
+        val expected = 1
+        val actual = calculator.calculate(input)
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `곱셈`() {
+        val input = "2 * 3"
+        val expected = 6
+        val actual = calculator.calculate(input)
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `나눗셈`() {
+        val input = "6 / 3"
+        val expected = 2
+        val actual = calculator.calculate(input)
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `나눗셈_예외 테스트`() {
+        val input = "6 / 0"
+        assertThatThrownBy { calculator.calculate(input) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("0으로 나눌 수 없습니다.")
+    }
+
+    @Test
+    fun `복잡한 연산 테스트`() {
+        val input = "2 + 3 * 4 / 2"
+        val expected = 10
+        val acutal = calculator.calculate(input)
+        assertThat(acutal).isEqualTo(expected)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["", " "])
+    fun `공백 문자열 예외 테스트`(input: String) {
+        assertThatThrownBy { calculator.calculate(input) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("입력값이 비어있습니다.")
+    }
+
+    @Test
+    fun `올바르지 않은 입력값 예외 테스트`() {
+        val input = "a + 2"
+        assertThatThrownBy { calculator.calculate(input) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("올바르지 않는 입력값입니다.")
+    }
+
+    @Test
+    fun `올바르지 않은 연산자 예외 테스트`() {
+        val input = "1 $#@$) 2"
+        assertThatThrownBy { calculator.calculate(input) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("연산자는 +, -, *, / 중 하나여야 합니다.")
+    }
+}
