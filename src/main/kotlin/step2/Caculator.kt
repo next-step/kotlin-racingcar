0594bc4f77e6c5780040fe82505bc9b610c38332
wycj2009 <wycj2009@naver.com>
2023-10-27 04:50:55 +0900

    Step2: 문자열 사칙 연산 계산기 구현
    - Caculator, CaculatorTest 생성

---
+++ src/main/kotlin/step2/Caculator.kt
@@ -0,0 +1,75 @@
+package step2
+
+import java.util.LinkedList
+import java.util.Queue
+
+object Caculator {
+
+    /**
+     * Calculates if the [input] is a correct mathematical expression.
+     *
+     * @param input String of mathematical expression.
+     * @throws IllegalArgumentException If the [input] is not a correct mathematical expression.
+     */
+    fun calculate(input: String?): Double {
+        val expression: Queue<Token> = input.toExpression()
+
+        var result: Double = 0.0
+        var curSymbol: Token.Symbol? = null
+        while (expression.peek() != null) {
+            when (val curToken: Token = expression.poll()) {
+                is Token.Number -> {
+                    result = when (curSymbol) {
+                        Token.Symbol.PLUS -> result + curToken.value
+                        Token.Symbol.MINUS -> result - curToken.value
+                        Token.Symbol.TIMES -> result * curToken.value
+                        Token.Symbol.DIVISION -> result / curToken.value
+                        null -> curToken.value
+                    }
+                    if (!result.isFinite()) throw IllegalArgumentException()
+                    curSymbol = null
+                }
+                is Token.Symbol -> {
+                    curSymbol = curToken
+                }
+            }
+        }
+
+        return result
+    }
+
+    @Throws(IllegalArgumentException::class)
+    private fun String?.toExpression(): Queue<Token> {
+        if (this.isNullOrEmpty()) throw IllegalArgumentException()
+
+        return this.split(" ").filter {
+            it.isNotEmpty()
+        }.mapIndexed { i: Int, s: String ->
+            s.toTokenOrNull()?.let {
+                if ((i % 2 == 0 && it is Token.Number || i % 2 == 1 && it is Token.Symbol).not()) throw IllegalArgumentException()
+                it
+            } ?: throw IllegalArgumentException()
+        }.let {
+            LinkedList(it)
+        }
+    }
+
+    private fun String.toTokenOrNull(): Token? {
+        return this.toDoubleOrNull()?.let {
+            Token.Number(it)
+        } ?: when (this) {
+            Token.Symbol.PLUS.strValue -> Token.Symbol.PLUS
+            Token.Symbol.MINUS.strValue -> Token.Symbol.MINUS
+            Token.Symbol.TIMES.strValue -> Token.Symbol.TIMES
+            Token.Symbol.DIVISION.strValue -> Token.Symbol.DIVISION
+            else -> null
+        }
+    }
+
+    private sealed interface Token {
+        class Number(val value: Double) : Token
+        enum class Symbol(val strValue: String) : Token {
+            PLUS("+"), MINUS("-"), TIMES("*"), DIVISION("/")
+        }
+    }
+}
---
+++ src/test/kotlin/step2/CaculatorTest.kt
@@ -0,0 +1,35 @@
+package step2
+
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class CaculatorTest {
+
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            "-4 + 5=1.0",
+            "2 - 5.5=-3.5",
+            "4 * 9=36.0",
+            "8 / 8=1.0",
+            "3 + 9 / -6=-2.0",
+            "7 - 6 + 2.5=3.5",
+            "4 * -7 - 1=-29.0",
+            "5 / -1 * 2=-10.0",
+            "6 + 9 / -5 + 5=2.0",
+            "6 - 4 * -7 + 5=-9.0",
+            "-1 * 0 - 1 + 0=-1.0",
+            "-4 / 8 + -1 + -5=-6.5",
+            "  6 + 9   / -5 + 5=2.0",
+            "6 - 4 *   -7 + 5  =-9.0",
+            "   -1    * 0  -    1 + 0=-1.0",
+            "     -4   /   8  +  -1 +  -5  =-6.5"
+        ],
+        delimiter = '='
+    )
+    fun test(input: String, result: Double) {
+        assertThat(Caculator.calculate(input)).isEqualTo(result)
+    }
+}
