fccb86a2b473c09655915e2208c75ba27dc34cd1
Celina <luv.mins99@gmail.com>
2023-10-27 01:00:04 +0900

    feat: 오류 메시지 enum class 생성

---
+++ src/main/kotlin/calculator/enums/ErrorMessages.kt
@@ -0,0 +1,9 @@
+package calculator.enums
+
+enum class ErrorMessages(val message: String) {
+    INVALID_INPUT_EXCEPTION("유효하지 않은 입력입니다."),
+    INVALID_OPERATOR_EXCEPTION("유효하지 않은 연산자입니다."),
+    INVALID_NUMBER_INPUT_EXCEPTION("유효하지 않은 숫자 입력입니다."),
+    NOT_SUPPORTED_OPERATOR("지원하지 않는 연산자입니다.")
+    ;
+}
