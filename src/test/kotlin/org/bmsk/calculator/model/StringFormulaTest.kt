3ae9771df25c9a529fcf265744342a010085ead0
bmsk <vv99911@gmail.com>
2023-10-27 01:41:12 +0900

    [Test] 테스트 코드에 상수화된 예외 메세지와 다른 예외 사항 추가

--- src/test/kotlin/org/bmsk/calculator/model/StringFormulaTest.kt
+++ src/test/kotlin/org/bmsk/calculator/model/StringFormulaTest.kt
@@ -4,36 +4,66 @@ import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.throwable.shouldHaveMessage
+import org.bmsk.calculator.ErrorMessage.BLANK_FORMULA
+import org.bmsk.calculator.ErrorMessage.DIVISION_BY_ZERO
+import org.bmsk.calculator.ErrorMessage.EVEN_POSITION_MUST_BE_NUMBER
+import org.bmsk.calculator.ErrorMessage.FORMULA_MUST_END_WITH_NUMBER
+import org.bmsk.calculator.ErrorMessage.FORMULA_MUST_START_WITH_NUMBER
+import org.bmsk.calculator.ErrorMessage.ODD_POSITION_MUST_BE_OPERATOR
 
 class StringFormulaTest : FunSpec({
-    test("plus") {
-        StringFormula("1 + 2 + 3").calculate() shouldBe 6f
-    }
+    context("Run All Test") {
 
-    test("minus") {
-        StringFormula("1 - 2 - 3").calculate() shouldBe -4f
-    }
+        test("plus") {
+            StringFormula("1 + 2 + 3").calculate() shouldBe 6f
+        }
 
-    test("multiply") {
-        StringFormula("1 * 2 * 3").calculate() shouldBe 6f
-    }
+        test("minus") {
+            StringFormula("1 - 2 - 3").calculate() shouldBe -4f
+        }
 
-    test("divide") {
-        StringFormula("3 / 2 / 1").calculate() shouldBe 1.5f
-    }
+        test("multiply") {
+            StringFormula("1 * 2 * 3").calculate() shouldBe 6f
+        }
 
-    test("require IsNotEmpty") {
-        shouldThrow<IllegalArgumentException> {
-            StringFormula("").calculate()
-        }.shouldHaveMessage("공백은 계산할 수 없음")
-    }
+        test("divide") {
+            StringFormula("3 / 2 / 1").calculate() shouldBe 1.5f
+        }
 
-    test("적절하지 않은 문자열이 포함되었다면 예외") {
-        shouldThrow<IllegalArgumentException> {
-            StringFormula("4 * 12 = 48").calculate()
-        }.shouldHaveMessage("적절하지 않은 문자열: =")
-        shouldThrow<IllegalArgumentException> {
-            StringFormula("x + x").calculate()
-        }.shouldHaveMessage("적절하지 않은 문자열: x")
+        test("0으로 나눌 경우 예외") {
+            shouldThrow<IllegalArgumentException> {
+                StringFormula("3 / 0").calculate()
+            }.shouldHaveMessage(DIVISION_BY_ZERO)
+        }
+
+        test("빈 문자열인 경우") {
+            shouldThrow<IllegalArgumentException> {
+                StringFormula("").calculate()
+            }.shouldHaveMessage(BLANK_FORMULA)
+        }
+
+        test("적절하지 않은 문자열이 포함된 경우") {
+            shouldThrow<IllegalArgumentException> {
+                StringFormula("4 * 12 = 48").calculate()
+            }.shouldHaveMessage(ODD_POSITION_MUST_BE_OPERATOR)
+            shouldThrow<IllegalArgumentException> {
+                StringFormula("1 + x + 1").calculate()
+            }.shouldHaveMessage(EVEN_POSITION_MUST_BE_NUMBER)
+            shouldThrow<IllegalArgumentException> {
+                StringFormula("x + x").calculate()
+            }.shouldHaveMessage(FORMULA_MUST_START_WITH_NUMBER)
+            shouldThrow<IllegalArgumentException> {
+                StringFormula("1 + x").calculate()
+            }.shouldHaveMessage(FORMULA_MUST_END_WITH_NUMBER)
+        }
+
+        test("마지막에 수가 아니라 문자가 오는 경우") {
+            shouldThrow<IllegalArgumentException> {
+                StringFormula("1 + ").calculate()
+            }.shouldHaveMessage(FORMULA_MUST_END_WITH_NUMBER)
+            shouldThrow<IllegalArgumentException> {
+                StringFormula("1 +").calculate()
+            }.shouldHaveMessage(FORMULA_MUST_END_WITH_NUMBER)
+        }
     }
 })
