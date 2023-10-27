fa5933fc9c2b39f80bc2abf3a00a33a3ce9e0924
kimueibin <at365@naver.com>
2023-10-28 00:55:08 +0900

    test : 계산 팩토리 내 수식 기호 예외 테스트 코드 작성

--- src/test/kotlin/camp/nextstep/edu/step/calculator/factory/CalculateSimpleFactoryTest.kt
+++ src/test/kotlin/camp/nextstep/edu/step/calculator/factory/CalculateSimpleFactoryTest.kt
@@ -2,9 +2,12 @@ package camp.nextstep.edu.step.calculator.factory
 
 import camp.nextstep.edu.step.calculator.domain.MedianResult
 import camp.nextstep.edu.step.calculator.domain.Requester
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.DisplayName
+import java.lang.IllegalArgumentException
 
 @DisplayName("계산 팩토리는")
 class CalculateSimpleFactoryTest : BehaviorSpec({
@@ -21,6 +24,24 @@ class CalculateSimpleFactoryTest : BehaviorSpec({
                 branchProcessingResult shouldBe MedianResult.of(medianResult = 10L)
                 functionalProcessingResult shouldBe MedianResult.of(medianResult = 10L)
                 branchProcessingResult shouldBe functionalProcessingResult
+            }
+        }
+    }
+
+    Given("잘못된 문자열 수식이 포함되어있으면") {
+        val wrongStringExpression = Requester(expression = "2 a 3 * 4").splitExpression()
+
+        When("팩토리 내 계산을 수행하면") {
+            val branchProcessingException = shouldThrow<IllegalArgumentException> {
+                stubCalculateSimpleFactory.calculateByStringExpressionNumbers(numbers = wrongStringExpression)
+            }
+            val functionalProcessingException = shouldThrow<IllegalArgumentException> {
+                stubCalculateSimpleFactory.calculateStringExpressionNumbersByOperators(numbers = wrongStringExpression)
+            }
+
+            Then("예외를 발생시킨다") {
+                branchProcessingException.message shouldBe "지원하지 않는 연산자입니다."
+                functionalProcessingException.message shouldBe "지원하지 않는 연산자입니다."
             }
         }
     }
