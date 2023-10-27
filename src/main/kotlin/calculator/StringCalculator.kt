3fd5644badf5d26d208c0d225f019e8b48aaae81
jeongdalma <jeongdalma@gmail.com>
2023-10-28 00:06:33 +0900

    feat(StringCalculator): 문자열 계산기 입력 유효성 검증 추가

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -1,32 +1,28 @@
 package calculator
 
 import java.lang.IllegalArgumentException
-import java.lang.NumberFormatException
 import kotlin.jvm.Throws
 
-class StringCalculator {
+object StringCalculator {
 
-    companion object {
+    private val EXPRESSION = """^(\d+\s*[+\-*/]\s*)+\d+$""".toRegex()
 
-        @Throws(IllegalArgumentException::class)
-        fun calc(input: String) : Any? {
-            val split = input.split(" ")
-            var acc: Int = 0
-            var operator = Operator.ADD
-            for (char: String in split) {
-                when(val parseResult = parse(char)) {
-                    is Operator -> operator = parseResult
-                    is Int -> acc = if(acc == 0) parseResult else operator(acc, parseResult)
-                        ?: return null
-                }
+    @Throws(IllegalArgumentException::class)
+    fun calc(input: String) : Any {
+        if (isAvailableInput(input)) throw IllegalArgumentException("유효하지 않은 입력입니다.")
+
+        val split = input.split(" ")
+        var acc: Int = -1
+        var operator = Operator.ADD
+        for (char: String in split) {
+            when(val parseResult = parse(char)) {
+                is Operator -> operator = parseResult
+                is Int -> acc = if(acc == -1) parseResult else operator(acc, parseResult)
+                    ?: throw IllegalArgumentException("나누기 연산에서 피연산자 중 0이 존재합니다.")
             }
-            return acc
         }
-
-        private fun parse(input: String) = try {
-            Integer.parseInt(input)
-        } catch (e: NumberFormatException) {
-            Operator.of(input)
-        }
+        return acc
     }
+    private fun isAvailableInput(input: String): Boolean = !input.matches(EXPRESSION)
+    private fun parse(input: String) = input.toIntOrNull() ?: Operator.of(input)
 }
