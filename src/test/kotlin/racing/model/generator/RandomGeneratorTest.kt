6f00486283d8aa594924169eaaf410fe23c15198
dong <dong.22@kakaoenterprise.com>
2023-10-27 15:56:19 +0900

    test : 1~9 사이의 랜덤값이 나오는것을 검증하는 단위테스트 작성

--- src/test/kotlin/racing/model/generator/RandomGeneratorTest.kt
+++ src/test/kotlin/racing/model/generator/RandomGeneratorTest.kt
@@ -1,10 +1,17 @@
 package racing.model.generator
 
-import io.kotest.assertions.fail
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.ints.shouldBeInRange
 
 class RandomGeneratorTest : StringSpec({
     "에서 9 사이에서 무작위 값이 출력되어야한다" {
-        fail("")
+        val generator = RandomGenerator()
+        generator.generate() shouldBeInRange IntRange(1, 9)
+        generator.generate() shouldBeInRange IntRange(1, 9)
+        generator.generate() shouldBeInRange IntRange(1, 9)
+        generator.generate() shouldBeInRange IntRange(1, 9)
+        generator.generate() shouldBeInRange IntRange(1, 9)
+        generator.generate() shouldBeInRange IntRange(1, 9)
+        generator.generate() shouldBeInRange IntRange(1, 9)
     }
 })
