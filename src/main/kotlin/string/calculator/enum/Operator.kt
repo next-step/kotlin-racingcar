4cf24e659536ab8a0cdb58e9a828b536c187a952
hyotaek-jang <hyotaek.jang@linecorp.com>
2023-10-27 09:19:32 +0900

    refactor: refactor code

--- src/main/kotlin/string/calculator/enum/Operator.kt
+++ src/main/kotlin/string/calculator/enum/Operator.kt
@@ -9,14 +9,12 @@ enum class Operator {
     companion object {
         const val NOT_SUPPORTED_OPERATOR = "지원하지 않는 연산자입니다."
 
-        fun of(operator: String): Operator {
-            return when (operator) {
-                "+" -> PLUS
-                "-" -> MINUS
-                "*" -> MULTIPLY
-                "/" -> DIVIDE
-                else -> throw IllegalArgumentException(NOT_SUPPORTED_OPERATOR)
-            }
+        fun of(operator: String): Operator = when (operator) {
+            "+" -> PLUS
+            "-" -> MINUS
+            "*" -> MULTIPLY
+            "/" -> DIVIDE
+            else -> throw IllegalArgumentException(NOT_SUPPORTED_OPERATOR)
         }
     }
 }
