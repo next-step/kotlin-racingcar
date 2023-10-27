a8b439084433b2e30dbf65dc80219ef306548304
ALbertIM0427 <guang7226961@naver.com>
2023-10-27 21:05:01 +0900

    feat: 자동차는 조건이 4이상 9일 때 한칸 전진하는 기능 구현

--- src/main/kotlin/game/Car.kt
+++ src/main/kotlin/game/Car.kt
@@ -2,7 +2,15 @@ package game
 
 class Car(var position: Int = 0) {
 
-    fun move(movement: Int) {
-        this.position += movement
+    fun move(moveCondition: Int) {
+        when {
+            canMove(moveCondition) -> advance()
+        }
     }
+
+    private fun advance() {
+        position += 1
+    }
+
+    private fun canMove(moveCondition: Int) = moveCondition in 4..9
 }
--- src/test/kotlin/game/CarTest.kt
+++ src/test/kotlin/game/CarTest.kt
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.provider.CsvSource
 class CarTest {
     @ParameterizedTest
     @CsvSource(value = ["1:4:2", "4:5:5", "0:9:1"], delimiter = ':')
-    fun `자동차는 4이상 9이하일때 전진한다`(position: Int, moveCondition: Int, expected: Int) {
+    fun `자동차는 4이상 9이하일때 한칸 전진한다`(position: Int, moveCondition: Int, expected: Int) {
         // given
         var car = Car(position)
 
