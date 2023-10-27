c9c6f5eaea10e49cd42061d3139d386aec78c0c8
ALbertIM0427 <guang7226961@naver.com>
2023-10-27 20:59:48 +0900

    test: 자동차는 4이상 9이하 일 때 이동하는 테스트 구현

--- src/test/kotlin/game/CarTest.kt
+++ src/test/kotlin/game/CarTest.kt
@@ -6,15 +6,28 @@ import org.junit.jupiter.params.provider.CsvSource
 
 class CarTest {
     @ParameterizedTest
-    @CsvSource(value = ["1:4:5", "4:5:9", "0:9:9"], delimiter = ':')
-    fun `자동차는 4이상 9이하일때 전진한다`(position: Int, movement: Int, expected: Int) {
+    @CsvSource(value = ["1:4:2", "4:5:5", "0:9:1"], delimiter = ':')
+    fun `자동차는 4이상 9이하일때 전진한다`(position: Int, moveCondition: Int, expected: Int) {
         // given
         var car = Car(position)
 
         // when
-        car.move(movement)
+        car.move(moveCondition)
 
         // then
         assertThat(car.position).isEqualTo(expected)
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["1:3", "4:10"], delimiter = ':')
+    fun `자동차는 4미만 9초과하면 전진하지 않는다`(position: Int, moveCondition: Int) {
+        // given
+        var car = Car(position)
+
+        // when
+        car.move(moveCondition)
+
+        // then
+        assertThat(car.position).isEqualTo(position)
     }
 }
