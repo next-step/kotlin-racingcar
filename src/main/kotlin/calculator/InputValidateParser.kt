a515704b1e186dcd3337c34b8d55ead9bc6dbdef
jaylene shin <jaylene.shin@kakaocorp.com>
2023-10-27 13:08:35 +0900

    feat: inputValidateParser 구현

--- src/main/kotlin/calculator/InputValidateParser.kt
+++ src/main/kotlin/calculator/InputValidateParser.kt
@@ -1,11 +1,18 @@
 package calculator
 
-class InputValidateParser {
-    fun verify(input: String) {
-        TODO("validate input and throw exception if invalid")
+object InputValidateParser {
+    private const val validInputRegex = "[\\d+\\-*/\\s]*"
+
+    fun validate(input: String?) {
+        require(!input.isNullOrBlank(), { "입력값은 null이거나 빈 공백 문자일 수 없다." })
+        require(input.matches(Regex(validInputRegex)), { "입력값은 숫자, 사칙연산자, 공백만 들어올 수 있다." })
     }
 
-    fun parse(input: String): List<String> {
-        TODO("string to chunk string and collect to list")
+    fun parse(input: String, delimiterRegex: String = "\\s+"): List<String> {
+        this.validate(input)
+        return input
+            .split(Regex(delimiterRegex))
+            .map { it.trim() }
+            .filter { it.isNotEmpty() }
     }
 }
