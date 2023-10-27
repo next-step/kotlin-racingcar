62cb00e25d44c024f12a99b580ee410d04ac50c4
bmsk <vv99911@gmail.com>
2023-10-28 02:22:40 +0900

    [Refact] Car 클래스의 움직임 정책 기본값을 삭제

--- src/main/kotlin/org/bmsk/racingcar/domain/model/Car.kt
+++ src/main/kotlin/org/bmsk/racingcar/domain/model/Car.kt
@@ -4,9 +4,9 @@ import org.bmsk.racingcar.domain.policy.MovementPolicy
 import org.bmsk.racingcar.model.Position
 
 data class Car(
+    val movementPolicy: MovementPolicy,
     val name: String = "",
     val position: Position = Position(xPos = 0),
-    val movementPolicy: MovementPolicy = MovementPolicy.RandomMovement,
 ) {
     fun getTheMovedCar(): Car = this.copy(
         name = name,
