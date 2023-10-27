b9d75e63a01d0641de7ba73c9facd0e1259d1fa7
hyotaek-jang <hyotaek.jang@linecorp.com>
2023-10-28 08:29:20 +0900

    refactor: refactor Operator.of

--- src/main/kotlin/string/calculator/domain/Operator.kt
+++ src/main/kotlin/string/calculator/domain/Operator.kt
@@ -1,20 +1,15 @@
 package string.calculator.domain
 
-enum class Operator {
-    PLUS,
-    MINUS,
-    MULTIPLY,
-    DIVIDE;
+enum class Operator(val symbol: String) {
+    PLUS("+"),
+    MINUS("-"),
+    MULTIPLY("*"),
+    DIVIDE("/");
 
     companion object {
         private const val NOT_SUPPORTED_OPERATOR = "지원하지 않는 연산자입니다."
 
-        fun of(operator: String): Operator = when (operator) {
-            "+" -> PLUS
-            "-" -> MINUS
-            "*" -> MULTIPLY
-            "/" -> DIVIDE
-            else -> throw IllegalArgumentException(NOT_SUPPORTED_OPERATOR)
-        }
+        fun of(operator: String): Operator = values().firstOrNull { it.symbol == operator }
+            ?: throw IllegalArgumentException(NOT_SUPPORTED_OPERATOR)
     }
 }
