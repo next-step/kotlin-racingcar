d4d71c9a30a71ba6a15567335234592bebe29b02
bmsk <vv99911@gmail.com>
2023-10-28 02:21:16 +0900

    [Refact] 무작위 움직임 정책을 object에서 class로 변경함
    - RandomGenerator를 주입하기 위함

--- src/main/kotlin/org/bmsk/racingcar/domain/policy/MovementPolicy.kt
+++ src/main/kotlin/org/bmsk/racingcar/domain/policy/MovementPolicy.kt
@@ -1,13 +1,16 @@
 package org.bmsk.racingcar.domain.policy
 
+import org.bmsk.racingcar.domain.random.DefaultRandomGenerator
+import org.bmsk.racingcar.domain.random.RandomGenerator
 import org.bmsk.racingcar.model.Position
 
 sealed interface MovementPolicy {
     fun move(position: Position): Position
 
-    object RandomMovement : MovementPolicy {
+    class RandomMovement(private val randomGenerator: RandomGenerator = DefaultRandomGenerator()) : MovementPolicy {
+
         override fun move(position: Position): Position {
-            val distance = (0..9).random()
+            val distance = randomGenerator.generate()
             return if (distance >= 4) {
                 position.copy(xPos = position.xPos + 1)
             } else {
