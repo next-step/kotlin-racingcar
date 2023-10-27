bb6d213a2bf701ed3942bfa1f4e7a42447b7ef75
ALbertIM0427 <guang7226961@naver.com>
2023-10-27 21:40:23 +0900

    refactor: 입력 관련 기능을 기능으로 분리

--- src/main/kotlin/game/ConsoleInputReader.kt
+++
@@ -1,7 +0,0 @@
-package game
-
-object ConsoleInputReader {
-    fun readInput(): String {
-        return readlnOrNull() ?: throw IllegalArgumentException(ErrorMessage.INVALID_INPUT_ERROR_MSG.message)
-    }
-}
--- src/main/kotlin/game/RacingCarGame.kt
+++ src/main/kotlin/game/RacingCarGame.kt
@@ -2,14 +2,9 @@ package game
 
 class RacingCarGame() {
 
-    private val carCountQuestion = "자동차 대수는 몇 대인가요?"
-    private val retryCountQuestion = "시도할 회수는 몇 회인가요?"
-
     fun start() {
-        ConsoleTextPrinter.printText(carCountQuestion)
-        val carCount = ConsoleInputReader.readInput().toInt()
-        ConsoleTextPrinter.printText(retryCountQuestion)
-        val retryCount = ConsoleInputReader.readInput().toInt()
+        val carCount = UserInputHandler.askForCarCount()
+        val retryCount = UserInputHandler.askForRetryCount()
         val cars = Cars.fromCarCount(carCount, RandomMoveConditionGenerator())
         cars.advance(retryCount)
     }
