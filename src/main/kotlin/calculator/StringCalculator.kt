20dc84a79e8fc78fed1e9e03b4b373f719c03f14
jaylene shin <jaylene.shin@kakaocorp.com>
2023-10-27 13:10:22 +0900

    feat: 최종 stringCalculator 앱 구현

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -1,8 +1,27 @@
 package calculator
 
 class StringCalculator {
-    fun calculate(input: String): String {
-        TODO("define input, output")
-        TODO("add, substract, multiply, divide the string input")
+    fun calculate(input: String): String{
+        val inputList: List<String> = InputValidateParser.parse(input)
+        val chunkStringCalculator = ChunkStringCalculator(inputList)
+
+        var result: String = ""
+        while (chunkStringCalculator.isContinued()) {
+            result = chunkStringCalculator.calculate()
+        }
+
+        if (chunkStringCalculator.isValidResult()) {
+            return result
+        } else {
+            throw IllegalArgumentException("(피)연산자 부족으로 계산에 실패했다.")
+        }
     }
+}
+
+fun main() {
+    ConsolePrinter.printMsg("계산할 식을 입력해주세요.")
+    val stringInput = ConsoleReader.readInput()
+    val stringCalculator = StringCalculator()
+    val stringOutput = stringCalculator.calculate(stringInput)
+    ConsolePrinter.printMsg("계산 결과는 $stringOutput 입니다.")
 }
--- src/test/kotlin/calculator/StringCalculatorTest.kt
+++ src/test/kotlin/calculator/StringCalculatorTest.kt
@@ -1,10 +1,63 @@
 package calculator
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class StringCalculatorTest {
-    @Test
-    fun calculate() {
-        TODO("Not yet implemented")
+
+    private val stringCalculator = StringCalculator()
+
+    @ParameterizedTest
+    @CsvSource(
+        "1 + 2, 3",
+        "1 + 2 + 3, 6",
+        "1 + 2 + 3 + 4, 10",
+    )
+    fun `덧셈 연산 정상 수행 확인`(input: String, expected: String) {
+        assertEquals(expected, stringCalculator.calculate(input))
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "1 - 2, -1",
+        "1 - 2 - 3, -4",
+        "1 - 2 - 3 - 4, -8"
+    )
+    fun `뺄셈 연산 정상 수행 확인`(input: String, expected: String) {
+        assertEquals(expected, stringCalculator.calculate(input))
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "1 * 2, 2",
+        "1 * 2 * 3, 6",
+        "1 * 2 * 3 * 4, 24"
+    )
+    fun `곱셈 연산 정상 수행 확인`(input: String, expected: String) {
+        assertEquals(expected, stringCalculator.calculate(input))
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "1 / 0, 0",
+        "2 / 1, 2",
+        "3 / 2 / 1, 1",
+        "12 / 4 / 3 / 2, 0"
+    )
+    fun `나눗셈 연산 정상 수행 확인`(input: String, expected: String) {
+        assertEquals(expected, stringCalculator.calculate(input))
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "1 + 2 * 3 / 4, 2",
+        "1 / 0 * 3 + 4 - 5, -1",
+        "5 / 4 * 3 * 3 / 1, 9",
+        "10 - 2 - 3 * 5 * 5 / 3 + 2, 43"
+    )
+    fun `사칙 연산 정상 수행 확인`(input: String, expected: String) {
+        assertEquals(expected, stringCalculator.calculate(input))
     }
 }
