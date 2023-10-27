f987ea45ee53f207a62df1f6d49d61f59545c44d
moses.lee(이상후)/kakao <moses.lee@kakaocorp.com>
2023-10-27 19:00:41 +0900

    feat: 입력값 UI
    - 입력값이 유효한지 검증 & 값 반환
    
    # step3

---
+++ src/test/kotlin/study/racingcar/application/InputViewTest.kt
@@ -0,0 +1,11 @@
+package study.racingcar.application
+
+import org.junit.jupiter.api.Test
+
+class InputViewTest {
+
+    // TODO : stdin 으로 값을 받는 객체의 경우, 테스트를 어떻게 해야할까?
+    @Test
+    fun getInput() {
+    }
+}
