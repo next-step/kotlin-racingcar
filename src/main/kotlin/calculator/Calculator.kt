d807dd000bff3359a2a1c12dda1fc026134f09cb
배재현 <jimbae@woowahan.com>
2023-10-27 21:43:06 +0900

    fix: 오류 수정

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -3,11 +3,11 @@ package calculator
 class Calculator(private val formulas: List<String>) {
 
     init {
-        require (formulas.isEmpty() || formulas[0] == "") {
+        if (formulas.isEmpty() || formulas[0] == "") {
             throw IllegalArgumentException("수식을 입력해 주세요.")
         }
 
-        require (!Number.isNumber(formulas[formulas.size-1])) {
+        if (!Number.isNumber(formulas[formulas.size-1])) {
             throw IllegalArgumentException("수식의 마지막은 숫자여야 합니다.")
         }
 
@@ -18,13 +18,13 @@ class Calculator(private val formulas: List<String>) {
     }
 
     private fun validateIsNumber(index: Int, formula: String) {
-        require (index % 2 != 1 && !Number.isNumber(formula)) {
+        if (index % 2 != 1 && !Number.isNumber(formula)) {
             throw IllegalArgumentException("수식을 확인해 주세요.")
         }
     }
 
     private fun validateIsArithmetic(index: Int, formula: String) {
-        require (index % 2 != 0 && !Arithmetic.isArithmetic(formula)) {
+        if (index % 2 != 0 && !Arithmetic.isArithmetic(formula)) {
             throw IllegalArgumentException("수식을 확인해 주세요.")
         }
     }
