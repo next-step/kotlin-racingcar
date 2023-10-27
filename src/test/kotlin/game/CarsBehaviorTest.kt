c5904826d798951a2d49ab485be494956ddd9dd4
ALbertIM0427 <guang7226961@naver.com>
2023-10-28 00:26:06 +0900

    test: kotest 사용해서 bahavior 검증

--- src/test/kotlin/game/CarsBehaviorTest.kt
+++ src/test/kotlin/game/CarsBehaviorTest.kt
@@ -4,20 +4,32 @@ import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 
 class CarsBehaviorTest : BehaviorSpec({
-    Given("자동차 경주 게임을 시작하면") {
-        When("자동차 수와 시도할 횟수를 입력받으면") {
-            val carCount = 3
-            val retryCount = 5
-            val cars = Cars.fromCarCount(carCount, FixedMoveConditionGenerator(4))
-            // when
-            cars.advance(retryCount)
-            val positions = cars.carList.map(Car::position)
-            then("자동차 수만큼 자동차가 생성되고 시도할 횟수만큼 자동차가 이동한다") {
-                // then
-                positions.forEach { position ->
-                    position shouldBe retryCount
+    val testConditions = listOf(
+        TestCondition(4, 8, 3),
+        TestCondition(3, 4, 4),
+        TestCondition(5, 8, 9),
+        TestCondition(6, 4, 10),
+    )
+
+    testConditions.forEach { (carCount, retryCount, moveConditionCount) ->
+        Given("자동차 수 ${carCount}와 시도할 횟수 $retryCount 입력받고") {
+            When("고정 이동조건 ${moveConditionCount}이면") {
+                val cars = Cars.fromCarCount(carCount, FixedMoveConditionGenerator(moveConditionCount))
+                cars.advance(retryCount)
+                val positions = cars.carList.map(Car::position)
+                val expect = expectedPosition(moveConditionCount, retryCount)
+                Then("자동차는 ${carCount}개 생성되고 모든 자동차는 ${expect}만큼 이동한다") {
+                    positions.forEach { position ->
+                        position shouldBe expect
+                    }
                 }
             }
         }
     }
-})
+}) {
+    data class TestCondition(val carCount: Int, val retryCount: Int, val moveConditionCount: Int)
+}
+
+fun expectedPosition(moveConditionCount: Int, retryCount: Int): Int {
+    return if (moveConditionCount in 4..9) retryCount else 0
+}
