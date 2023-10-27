9b402e2435ec4388ca3a33755cd05777661ff920
jeongdalma <jeongdalma@gmail.com>
2023-10-27 19:07:09 +0900

    refactor: Operation 필드 제거 및 이름 변경
    block 필드를 제거하고 연산자 오버로딩으로 변경
    Operation 이름에서 Operator로 이름 변경

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -9,14 +9,15 @@ class StringCalculator {
     companion object {
 
         @Throws(IllegalArgumentException::class)
-        fun calc(input: String) : Any {
+        fun calc(input: String) : Any? {
             val split = input.split(" ")
             var acc: Int = 0
-            var block: (Int, Int) -> Int = Operation.ADD.block
+            var operator = Operator.ADD
             for (char: String in split) {
                 when(val parseResult = parse(char)) {
-                    is Operation -> block = parseResult.block
-                    is Int -> acc = if(acc == 0) parseResult else block(acc, parseResult)
+                    is Operator -> operator = parseResult
+                    is Int -> acc = if(acc == 0) parseResult else operator(acc, parseResult)
+                        ?: return null
                 }
             }
             return acc
@@ -25,7 +26,7 @@ class StringCalculator {
         private fun parse(input: String) = try {
             Integer.parseInt(input)
         } catch (e: NumberFormatException) {
-            Operation.of(input)
+            Operator.of(input)
         }
     }
 }
--- src/test/kotlin/calculator/OperatorTest.kt
+++ src/test/kotlin/calculator/OperatorTest.kt
@@ -3,28 +3,28 @@ package calculator
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-class OperationTest {
+class OperatorTest {
 
     private val number1 = 20
     private val number2 = 10
 
     @Test
     fun `더하기 연산`() {
-        assertThat(Operation.of("+").block(number1, number2)).isEqualTo(30)
+        assertThat(Operator.of("+")(number1, number2)).isEqualTo(30)
     }
 
     @Test
     fun `빼기 연산`() {
-        assertThat(Operation.of("-").block(number1, number2)).isEqualTo(10)
+        assertThat(Operator.of("-")(number1, number2)).isEqualTo(10)
     }
 
     @Test
     fun `곱하기 연산`() {
-        assertThat(Operation.of("*").block(number1, number2)).isEqualTo(200)
+        assertThat(Operator.of("*")(number1, number2)).isEqualTo(200)
     }
 
     @Test
     fun `나누기 연산`() {
-        assertThat(Operation.of("/").block(number1, number2)).isEqualTo(2)
+        assertThat(Operator.of("/")(number1, number2)).isEqualTo(2)
     }
 }
