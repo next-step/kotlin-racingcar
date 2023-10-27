c59d905e1f591101c17eaeed9ee6df48700b53f8
배재현 <jimbae@woowahan.com>
2023-10-27 21:29:57 +0900

    doc: 의미있는 에러 메시지 사용

--- src/main/kotlin/calculator/Arithmetic.kt
+++ src/main/kotlin/calculator/Arithmetic.kt
@@ -9,7 +9,7 @@ enum class Arithmetic(val symbols: String, val operation: (Number, Number) -> Nu
     companion object {
         fun of(symbols: String): Arithmetic {
             return values()
-                .find { arithmetic -> arithmetic.symbols == symbols } ?: throw NoSuchElementException()
+                .find { arithmetic -> arithmetic.symbols == symbols } ?: throw NoSuchElementException("+, -, *, /만 입력 가능합니다.")
         }
 
         fun isArithmetic(symbols: String): Boolean = values().any { it.symbols == symbols }
