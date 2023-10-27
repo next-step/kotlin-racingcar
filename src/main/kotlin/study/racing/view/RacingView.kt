caa75f36df77449e142ec4a699e9890e602a136e
LAPTOP-A8710DGI\kdh85 <modudamba@naver.com>
2023-10-28 00:30:06 +0900

    step3: 자동차 경주 view 수정 * 해당 클래스는 생성자 없이 정적팩토리 메소드로 사용하고 있는 관계로 class -> object로 변경하면 코드를 더 줄일 수 있고 구조상 적합하다고 판단되어 수정.

--- src/main/kotlin/study/racing/view/RacingView.kt
+++ src/main/kotlin/study/racing/view/RacingView.kt
@@ -1,39 +1,34 @@
 package study.racing.view
 
-import java.lang.StringBuilder
+object RacingView {
 
-class RacingView {
+    private const val PROGRESS_BAR = "-"
 
-    companion object {
-
-        private const val PROGRESS_BAR = "-"
-
-        fun inputRacingParameters(message: String): Int {
-            try {
-                println(message)
-                return readln().toInt()
-            } catch (e: Exception) {
-                throw IllegalArgumentException("1이상의 숫자만 입력해 주세요.")
-            }
+    fun inputRacingParameters(message: String): Int {
+        try {
+            println(message)
+            return readln().toInt()
+        } catch (e: Exception) {
+            throw IllegalArgumentException("1이상의 숫자만 입력해 주세요.")
         }
+    }
 
-        fun printRacingResult(racingResult: List<Int>) {
-            racingResult.forEach { distance ->
-                println(drawingRacingProgress(distance))
-            }
-            roundDivisionBlankLine()
+    fun printRacingResult(racingResult: List<Int>) {
+        racingResult.forEach { distance ->
+            println(drawingRacingProgress(distance))
         }
+        roundDivisionBlankLine()
+    }
 
-        private fun roundDivisionBlankLine() {
-            println()
-        }
+    private fun roundDivisionBlankLine() {
+        println()
+    }
 
-        private fun drawingRacingProgress(distance: Int): StringBuilder {
-            val progress = StringBuilder()
-            repeat(distance) {
-                progress.append(PROGRESS_BAR)
-            }
-            return progress
+    private fun drawingRacingProgress(distance: Int): StringBuilder {
+        val progress = StringBuilder()
+        repeat(distance) {
+            progress.append(PROGRESS_BAR)
         }
+        return progress
     }
 }
