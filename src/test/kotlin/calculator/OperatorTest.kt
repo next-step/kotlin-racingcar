7a4afee8648eab1e9cd08e5f68bdeb14a9b423ac
jeongdalma <jeongdalma@gmail.com>
2023-10-27 19:11:16 +0900

    test: Operator 테스트 코드 수정 및 kotest 적용

--- src/test/kotlin/calculator/OperatorTest.kt
+++ src/test/kotlin/calculator/OperatorTest.kt
@@ -1,30 +1,59 @@
 package calculator
 
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
 
-class OperatorTest {
+class OperationTest: BehaviorSpec({
 
-    private val number1 = 20
-    private val number2 = 10
+    Given("정수인 두 개의 피연산자를") {
+        val operand1 = 10
+        val operand2 = 20
 
-    @Test
-    fun `더하기 연산`() {
-        assertThat(Operator.of("+")(number1, number2)).isEqualTo(30)
+        When("더하기 연산을 한다면") {
+            val add = Operator.of("+")(operand1, operand2)
+            Then("두 정수를 더한 결과를 반환한다.") {
+                add shouldBe 30
+            }
+        }
+
+        When("빼기 연산을 한다면") {
+            val sub = Operator.of("-")(operand1, operand2)
+            Then("두 정수를 뺀 결과를 반환한다.") {
+                sub shouldBe -10
+            }
+        }
+
+        When("곱하기 연산을 한다면") {
+            val multi = Operator.of("*")(operand1, operand2)
+            Then("두 정수를 곱한 결과를 반환한다.") {
+                multi shouldBe 200
+            }
+        }
+
+        When("나누기 연산을 한다면") {
+            val div = Operator.of("/")(operand2, operand1)
+            Then("두 번째 피연산자로 첫 번째 피연산자를 나눈 결과를 반환한다.") {
+                div shouldBe 2
+            }
+        }
+
+        When("소수가 반환될 나누기 연산을 한다면") {
+            val div = Operator.of("/")(operand1, operand2)
+            Then("0을 반환한다.") {
+                div shouldBe 0
+            }
+        }
     }
 
-    @Test
-    fun `빼기 연산`() {
-        assertThat(Operator.of("-")(number1, number2)).isEqualTo(10)
-    }
+    Given("0과 정수인 피연산자 두 개를") {
+        val operand1 = 10
+        val operandZero = 0
 
-    @Test
-    fun `곱하기 연산`() {
-        assertThat(Operator.of("*")(number1, number2)).isEqualTo(200)
+        When("나누기 연산을 한다면") {
+            val div = Operator.of("/")(operand1, operandZero)
+            Then("null을 반환한다.") {
+                div shouldBe null
+            }
+        }
     }
-
-    @Test
-    fun `나누기 연산`() {
-        assertThat(Operator.of("/")(number1, number2)).isEqualTo(2)
-    }
-}
+})
