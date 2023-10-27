6f5013d259d242d2e005c00162bcb2e03c477987
ALbertIM0427 <guang7226961@naver.com>
2023-10-27 20:01:59 +0900

    refactor: 생성자 대신 팩토리 함수 구현

--- src/main/kotlin/game/Cars.kt
+++ src/main/kotlin/game/Cars.kt
@@ -1,18 +1,23 @@
 package game
 
-class Cars(private val cars: List<Car>) {
+class Cars(
+    private val cars: List<Car>,
+    private val advanceCountGenerator: AdvanceCountGenerator = AdvanceCountGenerator()
+) {
 
     private fun advance() {
-        cars.forEach(fun(car: Car) {
-            val movement = AdvanceCountGenerator().generateRandomValue()
+        cars.forEach { car ->
+            val movement = advanceCountGenerator.generateRandomValue()
             car.move(movement)
             val currentPosition = CarPositionRenderer.makePosition(car)
             ConsoleTextPrinter.printText(currentPosition)
-        })
+        }
         println()
     }
 
     fun advance(retryCount: Int) = repeat(retryCount) { advance() }
 
-    constructor(cars: Int) : this(1.rangeTo(cars).map { Car() })
+    companion object {
+        fun fromCarCount(carCount: Int): Cars = Cars(1.rangeTo(carCount).map { Car() })
+    }
 }
