57185057aa8d2d86c8768ca018eb918f9c8cc480
yeongun <gjduddns@gmail.com>
2023-10-29 06:54:20 +0900

    refactor: InputView, ResultView 모두 object로 변경

--- src/main/kotlin/racingcar/ui/InputView.kt
+++ src/main/kotlin/racingcar/ui/InputView.kt
@@ -3,16 +3,14 @@ package racingcar.ui
 private const val INPUT_COUNT_OF_RACER_MESSAGE = "자동차 대수는 몇 대인가요?"
 private const val INPUT_COUNT_OF_RACING_MESSAGE = "시도할 횟수는 몇 회인가요?"
 
-class InputView {
-    companion object {
-        fun inputCountOfRacer(): Int {
-            println(INPUT_COUNT_OF_RACER_MESSAGE)
-            return readln().toInt()
-        }
+object InputView {
+    fun inputCountOfRacer(): Int {
+        println(INPUT_COUNT_OF_RACER_MESSAGE)
+        return readln().toInt()
+    }
 
-        fun inputCountOfRacing(): Int {
-            println(INPUT_COUNT_OF_RACING_MESSAGE)
-            return readln().toInt()
-        }
+    fun inputCountOfRacing(): Int {
+        println(INPUT_COUNT_OF_RACING_MESSAGE)
+        return readln().toInt()
     }
 }
--- src/main/kotlin/racingcar/ui/ResultView.kt
+++ src/main/kotlin/racingcar/ui/ResultView.kt
@@ -2,19 +2,17 @@ package racingcar.ui
 
 import racingcar.domain.Racer
 
-class ResultView {
-    companion object {
-        fun printResult(racers: MutableList<Racer>) {
-            for (racer in racers) {
-                for (j in 1..racer.count)
-                    print("-")
-                println()
-            }
+object ResultView {
+    fun printResult(racers: MutableList<Racer>) {
+        for (racer in racers) {
+            for (j in 1..racer.count)
+                print("-")
             println()
         }
-        fun printInitMessage() {
-            println()
-            println("실행 결과")
-        }
+        println()
+    }
+    fun printInitMessage() {
+        println()
+        println("실행 결과")
     }
 }
