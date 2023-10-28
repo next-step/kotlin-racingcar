3a6ed23af51bc921b943907cd42ec0dd38c8dff4
ALbertIM0427 <guang7226961@naver.com>
2023-10-29 00:27:01 +0900

    feat: 입력한 내용을 자동차 이름 리스트로 파싱하는 기능을 구현

---
+++ src/main/kotlin/game/CarNameParser.kt
@@ -0,0 +1,7 @@
+package game
+
+object CarNameParser {
+    fun parse(input: String): List<String> {
+        return input.split(",")
+    }
+}
---
+++ src/test/kotlin/game/CarNameParserTest.kt
@@ -0,0 +1,30 @@
+package game
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+class CarNameParserTest {
+
+    @ParameterizedTest
+    @MethodSource("provideCarNames")
+    fun `자동차 이름을 쉼표를 기준으로 분리한다`(input: String, expected: List<String>) {
+        val carNames = CarNameParser.parse(input)
+        assertThat(carNames).containsExactly(*expected.toTypedArray())
+    }
+
+    companion object {
+        @JvmStatic
+        fun provideCarNames(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of("apple,banana", listOf("apple", "banana")),
+                Arguments.of("pobi,crong,honux", listOf("pobi", "crong", "honux")),
+                Arguments.of("a,b,c,d", listOf("a", "b", "c", "d")),
+                Arguments.of("14,15,16,17", listOf("14", "15", "16", "17")),
+                Arguments.of("`~1!2@3#4$5%6^7&8*9(0).;:'\\|\"[{}]", listOf("`~1!2@3#4\$5%6^7&8*9(0).;:'\\|\"[{}]"))
+            )
+        }
+    }
+}
