a94967c7a655581cc3f1f0f2e463b29f9741f085
1stpie <1stpie@naver.com>
2023-10-28 20:28:53 +0900

    feat(core): add Car functions

--- src/main/kotlin/study/racingcar/Car.kt
+++ src/main/kotlin/study/racingcar/Car.kt
@@ -1,3 +1,17 @@
 package study.racingcar
 
-class Car
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+class Car(position: Int = 0) {
+    var position = position
+        private set
+
+    fun move(randomValue: Int = Random.nextInt(0..10)) {
+        if (randomValue >= 4) {
+            position += 1
+        }
+    }
+
+    fun displayPosition(): String = "-".repeat(position)
+}
