6b1c3bfa181afabe45686bc2f03042b828591c96
JunHyun Lee <dlwnsgus777@naver.com>
2023-10-28 22:02:21 +0900

    test: 깨지는 테스트 수정

--- src/test/kotlin/racingcar/domain/CarTest.kt
+++ src/test/kotlin/racingcar/domain/CarTest.kt
@@ -4,19 +4,20 @@ import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 
 class CarTest {
-    val testCondition = DriveCondition { true }
+    val successTestCondition = DriveCondition { true }
+    val failTestCondition = DriveCondition { false }
     val startDistance: Int = 1
     @Test
     fun `자동차는 조건에 맞으면 전진할 수 있다`() {
-        val car = Car(1, testCondition)
-        car.drive(1)
+        val car = Car(1, successTestCondition)
+        car.drive()
         Assertions.assertThat(car.distance).isEqualTo(startDistance + 1)
     }
 
     @Test
     fun `자동차는 조건에 맞지 않으면 전진할 수 있다`() {
-        val car = Car(1, testCondition)
-        car.drive(2)
+        val car = Car(1, failTestCondition)
+        car.drive()
         Assertions.assertThat(car.distance).isEqualTo(startDistance)
     }
 }
