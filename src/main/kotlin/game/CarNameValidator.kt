d8931501f139c6f7ba06ddc2afbfd4295ae912b5
ALbertIM0427 <guang7226961@naver.com>
2023-10-29 00:39:37 +0900

    feat: 이름을 검증하는 기능 구현
    자동차 이름은 5자를 초과할 수 없다

---
+++ src/main/kotlin/game/CarNameValidator.kt
@@ -0,0 +1,7 @@
+package game
+
+object CarNameValidator {
+    fun validate(carNames: List<String>) {
+        carNames.forEach { carName -> carName.length in 1..5 || throw IllegalArgumentException("자동차 이름은 5자를 넘을 수 없습니다.") }
+    }
+}
