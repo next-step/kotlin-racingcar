446bd74ac374eb3e10627bc7341c69d396713d83
moses.lee(이상후)/kakao <moses.lee@kakaocorp.com>
2023-10-28 17:35:34 +0900

    refactor: Interpreter 메서드 분리
    - interpret 함수 코드 분리
    - 검증 / 계산 로직 인터프리터 안에서 수행 유지
    - 토큰 객체 적용
    
    # step2

--- src/test/kotlin/study/racingcar/InterpreterTest.kt
+++ src/test/kotlin/study/racingcar/InterpreterTest.kt
@@ -3,18 +3,15 @@ package study.racingcar
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class InterpreterTest {
-    @Test
-    fun `계산 가능한 문자열인지 확인`() {
+    @ParameterizedTest
+    @ValueSource(strings = ["1 +", "1 + 2 2", "1 + 2 +", "+", "+ 1", "1+"])
+    fun `계산 가능한 문자열인지 확인`(input: String) {
         val interpreter = Interpreter(Parser())
-        assertThrows(IllegalOperationException::class.java) { interpreter.semanticAnalysis("1 +") }
-        assertThrows(IllegalOperationException::class.java) { interpreter.semanticAnalysis("1 + 2 2") }
-        assertThrows(IllegalOperationException::class.java) { interpreter.semanticAnalysis("1 + 2 +") }
-        assertThrows(IllegalOperationException::class.java) { interpreter.semanticAnalysis("+") }
-        assertThrows(IllegalOperationException::class.java) { interpreter.semanticAnalysis("+ 1") }
-        assertThrows(IllegalOperationException::class.java) { interpreter.semanticAnalysis("1+") }
-        // assertThrows(IllegalOperationException::class.java) { interpreter.semanticAnalysis("+1") }
+        assertThrows(IllegalOperationException::class.java) { interpreter.checkSemantic(input) }
     }
 
     @Test
@@ -22,6 +19,6 @@ class InterpreterTest {
         val interpreter = Interpreter(Parser())
         assertEquals(3, interpreter.interpret("1 + 2"))
         assertEquals(1, interpreter.interpret("1"))
-        assertThrows(IllegalArgumentException::class.java) { interpreter.interpret("1+") }
+        assertThrows(IllegalOperationException::class.java) { interpreter.interpret("1+") }
     }
 }
