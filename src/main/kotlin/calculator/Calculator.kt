9e17c05ccb90f56ef00690562988002ab3e608f8
claude.do <claude.do@kakaocorp.com>
2023-10-29 00:04:37 +0900

    필요없는 코드 제거

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -12,8 +12,7 @@ class Calculator(
         val pairNumbersAndOperators = parser.apply(input).getOrThrow()
         val inputNumbers = pairNumbersAndOperators.first
         val inputOperators = pairNumbersAndOperators.second.map { Op.match(it) }
-        println(inputNumbers)
-        println(inputOperators)
+
         return inputNumbers.reduceIndexed {
             index, front, back ->
             inputOperators[index - 1].run(front, back)
