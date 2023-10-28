7899f527de5b35fd4ccf0d9da98e569a9f0348b9
yeongun <gjduddns@gmail.com>
2023-10-29 08:01:01 +0900

    refactor: Racer를 data 클래스로 변경

--- src/main/kotlin/racingcar/domain/Racer.kt
+++ src/main/kotlin/racingcar/domain/Racer.kt
@@ -4,7 +4,7 @@ private const val INIT_COUNT = 0
 private const val MOVE_FORWARD_START_NUM = 4
 private const val MOVE_FORWARD_COUNT = 1
 
-class Racer(
+data class Racer(
     val count: Int = INIT_COUNT
 ) {
     fun run(num: Int): Racer {
