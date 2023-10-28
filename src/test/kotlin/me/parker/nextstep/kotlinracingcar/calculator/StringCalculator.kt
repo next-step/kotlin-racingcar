7009f785c5fcd957a965a1cf2aa6159a466010f0
parker1609 <psbum1609@gmail.com>
2023-10-28 10:01:40 +0900

    refactor: Object 키워드 사용하는 것으로 변경

--- src/test/kotlin/me/parker/nextstep/kotlinracingcar/calculator/StringCalculator.kt
+++ src/test/kotlin/me/parker/nextstep/kotlinracingcar/calculator/StringCalculator.kt
@@ -2,85 +2,83 @@ package me.parker.nextstep.kotlinracingcar.calculator
 
 import org.assertj.core.util.Strings
 
-class StringCalculator {
-    companion object {
-        private const val EMPTY_STRING = ""
+object StringCalculator {
+    private const val EMPTY_STRING = ""
 
-        fun calculate(input: String): Int {
-            if (Strings.isNullOrEmpty(input))
-                throw IllegalArgumentException("입력값이 비어있습니다.")
+    fun calculate(input: String): Int {
+        if (Strings.isNullOrEmpty(input))
+            throw IllegalArgumentException("입력값이 비어있습니다.")
 
-            val tokens: List<String> = separateOperandAndOperator(input)
+        val tokens: List<String> = separateOperandAndOperator(input)
 
-            return calculateFormula(tokens)
+        return calculateFormula(tokens)
+    }
+
+    private fun calculateFormula(formulaTokens: List<String>): Int {
+        var result = formulaTokens[0].toInt()
+        for ((index, token) in formulaTokens.withIndex()) {
+            if (index == 0) continue
+
+            if (isEvenNumber(index)) {
+                val operator = formulaTokens[index - 1]
+                result = OperatorType.calculate(operator, result, token.toInt())
+            }
         }
 
-        private fun calculateFormula(formulaTokens: List<String>): Int {
-            var result = formulaTokens[0].toInt()
-            for ((index, token) in formulaTokens.withIndex()) {
-                if (index == 0) continue
+        return result
+    }
 
-                if (isEvenNumber(index)) {
-                    val operator = formulaTokens[index - 1]
-                    result = OperatorType.calculate(operator, result, token.toInt())
-                }
+    private fun separateOperandAndOperator(input: String): List<String> {
+        val tokens = input.split(EMPTY_STRING)
+            .filter { !Strings.isNullOrEmpty(it) && it != " " }
+
+        val results = mutableListOf<String>()
+
+        var number = EMPTY_STRING
+        for (token in tokens) {
+            if (isNumber(token)) {
+                number += token
             }
 
-            return result
+            if (isNumber(token).not() && number.isNotEmpty()) {
+                results.add(number)
+                number = EMPTY_STRING
+            }
+
+            if (isNumber(token).not()) {
+                results.add(token)
+            }
+        }
+        results.add(number)
+
+        if (isValidFormulaOrder(results).not())
+            throw IllegalArgumentException("계산식이 올바르지 않습니다.")
+
+        return results
+    }
+
+    private fun isValidFormulaOrder(arguments: MutableList<String>): Boolean {
+        for ((index, arg) in arguments.withIndex()) {
+            if (isEvenNumber(index) && isNumber(arg).not())
+                return false
+            if (isEvenNumber(index).not() && OperatorType.exist(arg).not())
+                return false
+
+            if (index < arguments.size - 1 && isNumber(arg) && isNumber(arguments[index + 1]))
+                return false
         }
 
-        private fun separateOperandAndOperator(input: String): List<String> {
-            val tokens = input.split(EMPTY_STRING)
-                .filter { !Strings.isNullOrEmpty(it) && it != " " }
+        return arguments.size > 1
+    }
 
-            val results = mutableListOf<String>()
+    private fun isEvenNumber(num: Int) = num % 2 == 0
 
-            var number = EMPTY_STRING
-            for (token in tokens) {
-                if (isNumber(token)) {
-                    number += token
-                }
-
-                if (isNumber(token).not() && number.isNotEmpty()) {
-                    results.add(number)
-                    number = EMPTY_STRING
-                }
-
-                if (isNumber(token).not()) {
-                    results.add(token)
-                }
-            }
-            results.add(number)
-
-            if (isValidFormulaOrder(results).not())
-                throw IllegalArgumentException("계산식이 올바르지 않습니다.")
-
-            return results
-        }
-
-        private fun isValidFormulaOrder(arguments: MutableList<String>): Boolean {
-            for ((index, arg) in arguments.withIndex()) {
-                if (isEvenNumber(index) && isNumber(arg).not())
-                    return false
-                if (isEvenNumber(index).not() && OperatorType.exist(arg).not())
-                    return false
-
-                if (index < arguments.size - 1 && isNumber(arg) && isNumber(arguments[index + 1]))
-                    return false
-            }
-
-            return arguments.size > 1
-        }
-
-        private fun isEvenNumber(num: Int) = num % 2 == 0
-
-        private fun isNumber(str: String): Boolean {
-            return try {
-                str.toInt()
-                true
-            } catch (e: NumberFormatException) {
-                false
-            }
+    private fun isNumber(str: String): Boolean {
+        return try {
+            str.toInt()
+            true
+        } catch (e: NumberFormatException) {
+            false
         }
     }
 }
