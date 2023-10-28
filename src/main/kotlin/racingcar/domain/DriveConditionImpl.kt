64363c155e30f2d8587a75cd06de1fb6ac1acf27
JunHyun Lee <dlwnsgus777@naver.com>
2023-10-28 22:15:47 +0900

    refactor: class layout에 맞게 수정

--- src/main/kotlin/racingcar/domain/DriveConditionImpl.kt
+++ src/main/kotlin/racingcar/domain/DriveConditionImpl.kt
@@ -4,12 +4,12 @@ import java.util.Random
 
 class DriveConditionImpl : DriveCondition {
 
-    companion object {
-        const val IS_DRIVE: Int = 4
-    }
-
     override fun checkDriveCondition(): Boolean {
         var randomNumber = Random()
         return randomNumber.nextInt(10) == IS_DRIVE
+    }
+
+    companion object {
+        const val IS_DRIVE: Int = 4
     }
 }
