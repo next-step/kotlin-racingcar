be8c87277fcb7946e3fbd567a1e35ad68aec5f61
stevy.sung <stevy.sung@kakaocorp.com>
2023-10-28 15:24:18 +0900

    test: Output 모듈 테스트 작성

--- src/main/kotlin/step3/carRacing/model/Car.kt
+++ src/main/kotlin/step3/carRacing/model/Car.kt
@@ -7,9 +7,7 @@ class CarGenerator {
     }
 }
 
-class Car {
-    var curPosition = 0
-
+class Car(var curPosition: Int = 0) {
     fun tryRace(randomNumber: Number) {
         if (randomNumber in 4 until 10) { // NOTE: 4 이상 일 경우 이동
             curPosition++
