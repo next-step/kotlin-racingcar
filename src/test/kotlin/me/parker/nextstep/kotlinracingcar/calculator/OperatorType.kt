901c6a6d80b156e06181886af2e4449aeb1564a8
parker1609 <psbum1609@gmail.com>
2023-10-28 09:49:39 +0900

    refactor: OperatorType 이 추가되거나 이름이 변경되었을 때, DRY 원칙을 지키는 호출 방식으로 변경

--- src/test/kotlin/me/parker/nextstep/kotlinracingcar/calculator/OperatorType.kt
+++ src/test/kotlin/me/parker/nextstep/kotlinracingcar/calculator/OperatorType.kt
@@ -11,13 +11,12 @@ enum class OperatorType(val symbol: String, val calculate: (Int, Int) -> Int) {
 
     companion object {
         fun calculate(operator: String, lValue: Int, rValue: Int): Int {
-            return when (operator) {
-                ADD.symbol -> ADD.calculate(lValue, rValue)
-                SUBTRACT.symbol -> SUBTRACT.calculate(lValue, rValue)
-                MULTIPLY.symbol -> MULTIPLY.calculate(lValue, rValue)
-                DIVIDE.symbol -> DIVIDE.calculate(lValue, rValue)
-                else -> throw IllegalArgumentException("올바르지 않은 연산자입니다.")
-            }
+            return OperatorType.from(operator).calculate(lValue, rValue)
+        }
+
+        private fun from(operator: String): OperatorType {
+            return values().find { it.symbol == operator }
+                ?: throw IllegalArgumentException("올바르지 않은 연산자입니다.")
         }
 
         fun exist(operator: String): Boolean {
