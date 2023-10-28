ded1a5488ab48769e2cc817a4e117f714c303929
ALbertIM0427 <guang7226961@naver.com>
2023-10-29 01:40:18 +0900

    test: 자동차 위치와 비교 테스트 구현

--- src/test/kotlin/game/CarTest.kt
+++ src/test/kotlin/game/CarTest.kt
@@ -5,9 +5,10 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
 
-private const val i1 = 4
-
 class CarTest {
+
+    private val advanceCount = 4
+
     @ParameterizedTest
     @ValueSource(ints = [4, 5, 6, 7, 8, 9])
     fun `자동차는 4이상 9이하일때 한칸 전진한다`(moveCondition: Int) {
@@ -36,11 +37,11 @@ class CarTest {
 
     @ParameterizedTest
     @CsvSource(value = ["pobi,3,pobi : ---", "crong,2,crong : --", "honux,1,honux : -"])
-    fun `자동차들을 자동차이름과 이동거리를 그린다`(name: String, retryCount: Int, expected: String) {
+    fun `자동차 이름과 이동거리를 그린다`(name: String, retryCount: Int, expected: String) {
         // given
         var car = Car.of(name)
         for (i in 1..retryCount) {
-            val advanceCount = 4
+
             car.move(advanceCount)
         }
 
@@ -49,5 +50,19 @@ class CarTest {
 
         // then
         assertThat(actual).contains(expected)
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["0:false", "1:true", "2:false"], delimiter = ':')
+    fun `자동차 위치와 비교하여 같으면 true를 반환하고 다르면 false를 반환한다`(position: Int, expected: Boolean) {
+        // given
+        var car = Car.of("honux")
+        car.move(advanceCount)
+
+        // when
+        val actual = car.isSamePosition(position)
+
+        // then
+        assertThat(actual).isEqualTo(expected)
     }
 }
