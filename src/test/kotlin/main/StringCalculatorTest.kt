2702d310f38526e8adc6410b21527cb2c20fd5bf
chacha.roni <chacha.roni@kakaocorp.com>
2023-10-28 15:44:10 +0900

    부적합 문자열 테스트 리팩토링

--- src/test/kotlin/main/StringCalculatorTest.kt
+++ src/test/kotlin/main/StringCalculatorTest.kt
@@ -2,6 +2,8 @@ package main
 
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.NullAndEmptySource
 import java.lang.IllegalArgumentException
 
 class StringCalculatorTest {
@@ -10,16 +12,10 @@ class StringCalculatorTest {
         Assertions.assertThat(StringCalculator().run("1 + 3 / 2 * 4 - 1")).isEqualTo(7)
     }
 
-    @Test
-    fun `빈 문자열 전달 테스트`() {
-        Assertions.assertThatThrownBy { StringCalculator().run("  ") }
-            .isInstanceOf(IllegalArgumentException::class.java)
-            .hasMessageContaining("Formula should not be null or empty")
-    }
-
-    @Test
-    fun `Null 전달 테스트`() {
-        Assertions.assertThatThrownBy { StringCalculator().run(null) }
+    @ParameterizedTest
+    @NullAndEmptySource
+    fun `Null or Empty String 전달 테스트`(formula: String?) {
+        Assertions.assertThatThrownBy { StringCalculator().run(formula) }
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessageContaining("Formula should not be null or empty")
     }
