fb1e8af0b541aa1370a598a493c497196022526f
moses.lee(이상후)/kakao <moses.lee@kakaocorp.com>
2023-10-28 17:36:23 +0900

    refactor: Parser 역할 축소
    - 토큰의 검증 메서드 -> Token 으로 이동
    
    # step2

--- src/test/kotlin/study/racingcar/ParserTest.kt
+++ src/test/kotlin/study/racingcar/ParserTest.kt
@@ -1,47 +1,31 @@
 package study.racingcar
 
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.NullSource
-import org.junit.jupiter.params.provider.ValueSource
+import org.junit.jupiter.params.provider.NullAndEmptySource
 
 class ParserTest {
     @Test
     fun `문자열 파싱 확인`() {
         val parser = Parser()
-        val parsed = parser.parse("2 + 3 * 4 / 2")
-        assertEquals(listOf("2", "+", "3", "*", "4", "/", "2"), parsed)
+        val input = "2 + 3"
+        val parsed = parser.parse(input)
+
+        assertEquals(
+            parsed[0].operand + " " + parsed[1].operator + " " + parsed[2].operand,
+            input
+        )
     }
 
     @ParameterizedTest
-    @NullSource
-    @ValueSource(strings = [""])
+    @NullAndEmptySource
     fun `빈 공백 문자이거나, null 일 경우 Throw Exception`(input: String?) {
         val parser = Parser()
 
         assertThrows(IllegalArgumentException::class.java) {
             parser.parse(input)
-        }
-    }
-
-    @Test
-    fun `계산 가능한 숫자인지 확인`() {
-        val parser = Parser()
-        assertThat(parser.isInterpretableNumber("1")).isEqualTo(true)
-        assertThat(parser.isInterpretableNumber("1234556677898")).isEqualTo(false)
-        assertThat(parser.isInterpretableNumber("1+")).isEqualTo(false)
-        // assertThat(parser.isInterpretableNumber("+1")).isEqualTo(false) TODO: "+1" 을 toInt 로 변환 가능한 이슈
-    }
-
-    @Test
-    fun `계산 가능한 연산자인지 확인`() {
-        val parser = Parser()
-        assertThat(parser.getInterpretableOperator("+")).isEqualTo("+")
-        assertThrows(IllegalArgumentException::class.java) {
-            parser.getInterpretableOperator("+-")
         }
     }
 }
