e4e4f1aff9352543fb8c4272f31a10b8d86ee4db
OYJ <oyj7677@gmail.com>
2023-10-29 06:59:15 +0900

    Refactoring: Add error check, Create package and move

--- src/test/kotlin/study/calculator/FormulaParser.kt
+++ src/test/kotlin/study/calculator/FormulaParser.kt
@@ -1,6 +1,6 @@
-package study
+package study.calculator
 
-import FormulaParser
+import calculator.FormulaParser
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
@@ -13,15 +13,16 @@ class FormulaParser {
             arrayOf("2 + 3 * 4 / 2 *", IllegalArgumentException()),
             arrayOf("4 - 1 + 3 * +", NumberFormatException()),
             arrayOf("19 * 2 + 18 ) 20", IllegalArgumentException()),
-            arrayOf("      ", IllegalArgumentException())
+            arrayOf("      ", IllegalArgumentException()),
+            arrayOf("19 * 2 + 18 / 0", IllegalArgumentException()),
         )
     }
 
     @ParameterizedTest
     @MethodSource("calculateErrorTestData")
     fun `, 잘못된 수식을 calculate()에 넣어 호출하면, 에러를 배출한다`(formula: String, expected: Exception) {
-        // when : 잘못된 수식을 FormulaParser().parse(formula)에 넣어 호출한다.
-        val actual = runCatching { FormulaParser().parse(formula) }.exceptionOrNull()
+        // when : 잘못된 수식을 calculator.FormulaParser().parse(formula)에 넣어 호출한다.
+        val actual = runCatching { FormulaParser.parse(formula) }.exceptionOrNull()
 
         // then : 에러를 배출한다.
         Assertions.assertThat(actual).isInstanceOf(expected::class.java)
