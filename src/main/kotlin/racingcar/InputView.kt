1f9aac26e49ab9b21665c1cf3a10975647817700
dean <sodp5@naver.com>
2023-10-28 18:10:55 +0900

    자동차 대수와 시도할 회수 구하는 기능 추가

---
+++ src/main/kotlin/racingcar/InputView.kt
@@ -0,0 +1,15 @@
+package racingcar
+
+class InputView {
+    fun getCarCount(): Int {
+        println("자동차 대수는 몇 대인가요?")
+
+        return readln().toInt()
+    }
+
+    fun getTryMoveCount(): Int {
+        println("시도할 횟수는 몇 회인가요?")
+
+        return readln().toInt()
+    }
+}
