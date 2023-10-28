acf5ad40ff0ac244b63519fe8bf83cc570628a6a
ALbertIM0427 <guang7226961@naver.com>
2023-10-29 02:04:39 +0900

    feat: 시도할 회수만큼 자동차들을 움직이는 기능 구현

--- src/main/kotlin/game/CarFleet.kt
+++ src/main/kotlin/game/CarFleet.kt
@@ -7,6 +7,12 @@ class CarFleet private constructor(private val cars: List<Car>) {
         return cars.filter { it.isSamePosition(maxPosition) }.map { it.name }
     }
 
+    fun advance(retryCount: Int, moveConditionGenerator: MoveConditionGenerator) {
+        repeat(retryCount) {
+            cars.forEach { car -> car.move(moveConditionGenerator.generate()) }
+        }
+    }
+
     companion object {
         fun of(carNames: List<String>): CarFleet {
             return CarFleet(carNames.map { Car.of(it) })
--- src/test/kotlin/game/CarFleetTest.kt
+++ src/test/kotlin/game/CarFleetTest.kt
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ValueSource
 import java.util.stream.Stream
 
 class CarFleetTest {
@@ -29,6 +30,25 @@ class CarFleetTest {
 
         // then
         assertThat(actual).containsExactlyElementsOf(expected)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, 2, 3])
+    fun `시도한 횟수 만큼 모든 자동차들을 전진시킨다`(retryCount: Int) {
+        // given
+        val pobi = Car.of("pobi")
+        val crong = Car.of("crong")
+        val honux = Car.of("honux")
+        val cars = listOf(pobi, crong, honux)
+        val carFleet = CarFleet.from(cars)
+
+        // when
+        carFleet.advance(retryCount, FixedMoveConditionGenerator(4))
+
+        // then
+        assertThat(pobi.getPosition()).isEqualTo(retryCount)
+        assertThat(crong.getPosition()).isEqualTo(retryCount)
+        assertThat(honux.getPosition()).isEqualTo(retryCount)
     }
 
     companion object {
