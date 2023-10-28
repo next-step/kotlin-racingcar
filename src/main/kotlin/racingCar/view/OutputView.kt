626cda4d6e27e2295ae4b9ad5fa0376933eb0ea4
Ho-Ya-Ho <rkdgh1056@gmail.com>
2023-10-28 22:24:52 +0900

    전진하는 자동차 출력할 때 자동차 이름 같이 출력하도록 수정

--- src/main/kotlin/racingCar/view/OutputView.kt
+++ src/main/kotlin/racingCar/view/OutputView.kt
@@ -4,17 +4,18 @@ import racingCar.domain.Car
 
 object OutputView {
 
-    private const val ASK_CAR_COUNT = "자동차 대수는 몇 대인가요?"
+    private const val ENTER_CARS_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
     private const val ASK_TRY_COUNT = "시도할 횟수는 몇 회인가요?"
     private const val EXECUTE_RESULT = "\n실행 결과"
     private const val CAR = "="
+    private const val COLON = " : "
 
-    fun printCarCount() = println(ASK_CAR_COUNT)
+    fun printCarName() = println(ENTER_CARS_NAME)
     fun printTryCount() = println(ASK_TRY_COUNT)
     fun printExecuteResult() = println(EXECUTE_RESULT)
 
     fun printCar(carList: List<Car>) {
-        carList.forEach { car: Car -> println(CAR.repeat(car.moveCount)) }
+        carList.forEach { car: Car -> println(car.name + COLON + CAR.repeat(car.moveCount)) }
         println()
     }
 }
