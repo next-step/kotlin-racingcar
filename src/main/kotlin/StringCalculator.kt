27a5a3569eaadfcc7072383f8b53787efc4d3f37
장진혁/Client개발팀/원스토어 <ironelder@onestorecorp.com>
2023-10-28 16:29:55 +0900

    [Step2][ironelder] 중복되는 검사 하나로 통합

--- src/main/kotlin/StringCalculator.kt
+++ src/main/kotlin/StringCalculator.kt
@@ -1,18 +1,18 @@
 class StringCalculator(private val calculatorString: String?) {
 
+    private val tokens = calculatorString?.split(" ")?.filter { it.isNotBlank() } ?: emptyList()
+    private val numberArgs: List<Int>
+    private val operatorArgs: MutableList<String>
+
     init {
         require(!calculatorString.isNullOrBlank()) { throw IllegalArgumentException() }
+        numberArgs = tokens.filter { it.toIntOrNull() != null }.map { it.toInt() }
+        operatorArgs = tokens.filter { it.toIntOrNull() == null }.toMutableList()
+        check(numberArgs.size > operatorArgs.size) { throw IllegalArgumentException() }
     }
 
-    private val numberArgs by lazy {
-        calculatorString?.split(" ")?.filter { it.isNotBlank() }?.filter { it.toIntOrNull() != null }?.map { it.toInt() }
-    }
-    private val operatorArgs by lazy {
-        calculatorString?.split(" ")?.filter { it.isNotBlank() }?.filter { it.toIntOrNull() == null }?.toMutableList()
-    }
-
-    fun calculating(): Int? = numberArgs?.reduce { acc, next ->
-        when (operatorArgs?.removeFirst()) {
+    fun calculating(): Int = numberArgs.reduce { acc, next ->
+        when (operatorArgs.removeFirst()) {
             "+" -> plus(acc, next)
             "-" -> minus(acc, next)
             "*" -> multiple(acc, next)
@@ -28,7 +28,6 @@ class StringCalculator(private val calculatorString: String?) {
 }
 
 fun main() {
-
     StringCalculator("2 + 3 * 4 / 2").apply {
         println("result = ${calculating()}")
     }
