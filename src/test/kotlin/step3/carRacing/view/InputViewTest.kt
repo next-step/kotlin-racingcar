8322331489266b46787933fba9170a4927084d86
stevy.sung <stevy.sung@kakaocorp.com>
2023-10-28 15:29:39 +0900

    test: 모킹 변수 이름 변경

--- src/test/kotlin/step3/carRacing/view/InputViewTest.kt
+++ src/test/kotlin/step3/carRacing/view/InputViewTest.kt
@@ -3,15 +3,16 @@ package step3.carRacing.view
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
-class TestInputReader : InputReader {
-    override fun readInput(): Int {
-        return 5
-    }
-}
+
 
 class InputViewTest : StringSpec({
     "인풋을 통해 자동차의 대수 몇번의 이동시도를 할것인지 입력 받는다" {
-        val (carCount, tryCount) = InputView.getInputInfo(TestInputReader())
+        class MockInputReader : InputReader {
+            override fun readInput(): Int {
+                return 5
+            }
+        }
+        val (carCount, tryCount) = InputView.getInputInfo(MockInputReader())
         carCount shouldBe 5
         tryCount shouldBe 5
     }
