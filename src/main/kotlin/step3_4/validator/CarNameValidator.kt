e51d67943bc1bc923ae28ab8897b81f195ba2865
Ahn-seokjoo <tjrwn0716@naver.com>
2023-10-29 00:54:34 +0900

    [refactoring] car name validator 조건 추가
    아무것도 입력없이 엔터쳤을 때 에러

--- src/main/kotlin/step3_4/validator/CarNameValidator.kt
+++ src/main/kotlin/step3_4/validator/CarNameValidator.kt
@@ -2,9 +2,11 @@ package step3_4.validator
 
 object CarNameValidator {
     fun checkValidate(carNameList: List<String>) {
+        carNameList.find { it.isBlank() }?.run { error(PLEASE_ENTER_THE_NAME) }
         carNameList.forEach { require(it.length <= CAR_NAME_MAXIMUM_LENGTH) { CAR_NAME_CAN_NOT_EXCEED_MAXIMUM_LENGTH } }
     }
 
     private const val CAR_NAME_MAXIMUM_LENGTH = 5
     private const val CAR_NAME_CAN_NOT_EXCEED_MAXIMUM_LENGTH = "이름을 5자 이상 초과할 수 없어요."
+    private const val PLEASE_ENTER_THE_NAME = "이름을 제대로 입력해 주세요."
 }
