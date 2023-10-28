f52731eed232265b7a76b6388e0a6da7b611edcf
Hyunjun Jeong <jeongdalma@gmail.com>
2023-10-28 15:03:02 +0900

    자동차 경주 2단계: 문자열 계산기 (#1315)
    * feat: 문자열 계산기 구현
    
    * test: 문자열 계산기 테스트 작성
    
    * refactor: Operation 필드 제거 및 이름 변경
    
    block 필드를 제거하고 연산자 오버로딩으로 변경
    Operation 이름에서 Operator로 이름 변경
    
    * test: Operator 테스트 코드 수정 및 kotest 적용
    
    * feat(StringCalculator): 문자열 계산기 입력 유효성 검증 추가
    
    * test(StringCalculatorTest): 문자열 계산기 테스트 코드 추가
    
    * refactor: 입력 유효성 검증 함수 이름 수정

---
+++ src/main/kotlin/calculator/Operator.kt
@@ -0,0 +1,28 @@
+package calculator
+
+enum class Operator(
+    private val symbol: String
+) {
+
+    ADD("+") {
+        override fun invoke(n1: Int, n2: Int): Int = n1 + n2
+     },
+    SUB("-") {
+        override fun invoke(n1: Int, n2: Int): Int = n1 - n2
+     },
+    MULTI("*") {
+        override fun invoke(n1: Int, n2: Int): Int = n1 * n2
+   },
+    DIV("/") {
+        override fun invoke(n1: Int, n2: Int): Int? =
+            if(n1 == 0 || n2 == 0) null else n1 / n2
+    }
+    ;
+
+    abstract operator fun invoke(n1: Int, n2: Int) : Int?
+
+    companion object {
+        fun of(char: String): Operator = values().find { it.symbol == char }
+            ?: throw IllegalArgumentException("유효하지 않은 연산자 입니다.")
+    }
+}
---
+++ src/test/kotlin/calculator/StringCalculatorTest.kt
@@ -0,0 +1,49 @@
+package calculator
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+import java.lang.IllegalArgumentException
+
+class StringCalculatorTest: StringSpec ({
+
+    "숫자,기호,숫자 순서로 입력된 문자열은 정상적으로 계산한다." {
+        val inputs = listOf(
+            "5 - 1 + 3 * 10 / 2" to 35,
+            "1 - 11 + 0 / 2 * 5" to -25,
+            "10 / 2 + 3 * 5 - 39" to 1,
+            "1 + 3" to 4
+        )
+        inputs.map {
+            val (input, output) = it
+            StringCalculator.calc(input) shouldBe output
+        }
+    }
+
+    "숫자,기호,숫자로 순서로 입력된 문자열이지만 나누기 연산에서 피연산자 중에서 0이 포함되어 있으면 예외를 던진다." {
+        shouldThrow<IllegalArgumentException> {
+            StringCalculator.calc("10 - 2 - 8 / 10")
+        }.message shouldBe "나누기 연산에서 피연산자 중 0이 존재합니다."
+    }
+
+    "숫자,기호,숫자 순서가 아닌 문자열이 입력된다면 예외를 던진다." {
+        val inputs = listOf(
+            "1 + 2 * 10 *",
+            "11 22",
+            "* 1",
+            "+",
+            "-",
+            "/",
+            "*"
+        )
+        inputs.map {
+            shouldThrow<IllegalArgumentException> {
+                StringCalculator.calc(it)
+            }.message shouldBe "유효하지 않은 입력입니다."
+        }
+    }
+})
