78d87b9c82e202d47f5bcacc544700185af8fb7b
명기범 <kpmyung@oliveyoung.co.kr>
2023-10-29 23:54:07 +0900

    refactor: step-2 최종 피드백 반영(nullable 타입 변환, 최상위 함수 제거)

--- src/test/kotlin/calculator/OperatorTest.kt
+++ src/test/kotlin/calculator/OperatorTest.kt
@@ -9,25 +9,26 @@ class OperatorTest {
 
     @Test
     fun `덧셈 테스트`() {
-        val actual = add(2, 7)
+
+        val actual = Operator.PLUS.operation(2, 7)
         assertThat(actual).isEqualTo(9)
     }
 
     @Test
     fun `뺄셈 테스트`() {
-        val actual = subtract(7, 2)
+        val actual = Operator.MINUS.operation(7, 2)
         assertThat(actual).isEqualTo(5)
     }
 
     @Test
     fun `곱셈 테스트`() {
-        val actual = multiply(7, 2)
+        val actual = Operator.MULTIPLY.operation(2, 7)
         assertThat(actual).isEqualTo(14)
     }
 
     @Test
     fun `나눗셈 테스트`() {
-        val actual = divide(7, 2)
+        val actual = Operator.DIVIDE.operation(7, 2)
         assertThat(actual).isEqualTo(3)
     }
 }
