230f4fa78109c9bb4fb9228a9a0f46467be8d85c
bongbong <kbk5190@naver.com>
2023-10-29 17:15:49 +0900

    feat: inputValidator 분리

--- src/main/kotlin/racingcar/domain/configuration/CarRacingConfiguration.kt
+++ src/main/kotlin/racingcar/domain/configuration/CarRacingConfiguration.kt
@@ -1,16 +1,14 @@
 package racingcar.domain.configuration
 
+import racingcar.domain.CarRacingInputValidator
 import racingcar.domain.car.Car
 import racingcar.domain.car.CarGenerator
 import racingcar.view.CarRacingInput
 
 class CarRacingConfiguration(
-    val carCount: Int,
-    val roundCount: Int,
+    private val cars: List<Car>,
+    private val rounds: IntRange
 ) {
-    private val cars: List<Car> by lazy { setCars() }
-    private val rounds: IntRange by lazy { setRounds() }
-
     fun getCarRacingGameConfiguration(): CarRacingGameConfiguration =
         CarRacingGameConfiguration(
             cars = cars,
@@ -23,21 +21,20 @@ class CarRacingConfiguration(
             roundRange = rounds,
         )
 
-    private fun setCars(): List<Car> = CarGenerator.createInOrder(carCount)
-
-    private fun setRounds(): IntRange = (CAR_RACING_INITIAL_ROUND..roundCount)
-
     companion object {
         private const val CAR_RACING_INITIAL_ROUND = 1
-        fun of(input: CarRacingInput): CarRacingConfiguration = CarRacingConfiguration(
-            carCount = validateCount(input.carCount),
-            roundCount = validateCount(input.tryCount),
-        )
-
-        private fun validateCount(input: Int): Int {
-            require(input > 0) { "갯수는 0보다 커야 합니다" }
-            return input
+        fun of(input: CarRacingInput): CarRacingConfiguration {
+            val carCount = CarRacingInputValidator.validateCount(input.carCount)
+            val roundCount = CarRacingInputValidator.validateCount(input.tryCount)
+            return CarRacingConfiguration(
+                cars = setCars(carCount),
+                rounds = setRounds(roundCount),
+            )
         }
+
+        private fun setCars(carCount: Int): List<Car> = CarGenerator.createInOrder(carCount)
+
+        private fun setRounds(roundCount: Int): IntRange = (CAR_RACING_INITIAL_ROUND..roundCount)
     }
 }
 
