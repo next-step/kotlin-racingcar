2c2000f26e9a7efd497adb5e4be9013779e018b9
dokdogalmaegi <clik202012@gmail.com>
2023-10-29 19:46:33 +0900

    Refac & Add: 재사용 변수 할당, OperatorTest

--- src/test/kotlin/calculator/StringCalculatorTest.kt
+++ src/test/kotlin/calculator/StringCalculatorTest.kt
@@ -5,11 +5,12 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class StringCalculatorTest {
+
+    private val stringCalculator: StringCalculator = StringCalculator()
+
     @Test
     fun `입력값이 Null이라면 IllegalArgumentException이 발생합니다`() {
         val input: String? = null
-
-        val stringCalculator: StringCalculator = StringCalculator()
 
         assertThrows<IllegalArgumentException> {
             stringCalculator.calculate(input)
@@ -20,8 +21,6 @@ class StringCalculatorTest {
     fun `입력값이 빈 문자열이라면 IllegalArgumentException이 발생합니다`() {
         val input: String = ""
 
-        val stringCalculator: StringCalculator = StringCalculator()
-
         assertThrows<IllegalArgumentException> {
             stringCalculator.calculate(input)
         }
@@ -30,8 +29,6 @@ class StringCalculatorTest {
     @Test
     fun `입력값이 공백이라면 IllegalArgumentException이 발생합니다`() {
         val input: String = " "
-
-        val stringCalculator: StringCalculator = StringCalculator()
 
         assertThrows<IllegalArgumentException> {
             stringCalculator.calculate(input)
@@ -42,16 +39,12 @@ class StringCalculatorTest {
     fun `입력값이 숫자 하나라면 해당 숫자를 반환합니다`() {
         val input: String = "1"
 
-        val stringCalculator: StringCalculator = StringCalculator()
-
         assertThat(stringCalculator.calculate(input)).isEqualTo(1)
     }
 
     @Test
     fun `처음 입력값이 숫자가 아니라면 IllegalArgumentException이 발생합니다`() {
         val input: String = "+ 1"
-
-        val stringCalculator: StringCalculator = StringCalculator()
 
         assertThrows<IllegalArgumentException> {
             stringCalculator.calculate(input)
@@ -62,16 +55,12 @@ class StringCalculatorTest {
     fun `입력값이 숫자 두개와 덧셈 기호라면 두 숫자를 더한 값을 반환합니다`() {
         val input: String = "1 + 2"
 
-        val stringCalculator: StringCalculator = StringCalculator()
-
         assertThat(stringCalculator.calculate(input)).isEqualTo(3)
     }
 
     @Test
     fun `입력값에 계산식이 여러개라면 모든 계산식을 계산한 결과를 반환합니다`() {
         val input: String = "1 + 2 * 3 / 3"
-
-        val stringCalculator: StringCalculator = StringCalculator()
 
         assertThat(stringCalculator.calculate(input)).isEqualTo(3)
     }
