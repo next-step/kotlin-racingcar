d83a65be344324fb1454c880231ad8a5e387b1e1
jyunkim <jyunkim13@gmail.com>
2023-10-29 20:06:18 +0900

    feat: add validation

--- src/main/kotlin/racingcar/domain/car/CarFactory.kt
+++ src/main/kotlin/racingcar/domain/car/CarFactory.kt
@@ -2,6 +2,7 @@ package racingcar.domain.car
 
 object CarFactory {
     fun createCars(total: Int): List<Car> {
+        require(total >= 0) { "Argument should be non negative number." }
         return List(total) { Car() }
     }
 }
--- src/test/kotlin/racingcar/domain/car/CarFactoryTest.kt
+++ src/test/kotlin/racingcar/domain/car/CarFactoryTest.kt
@@ -1,7 +1,9 @@
 package racingcar.domain.car
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.throwable.shouldHaveMessage
 
 class CarFactoryTest : BehaviorSpec({
     Given("생성할 자동차 수가 주어졌을 때") {
@@ -12,6 +14,20 @@ class CarFactoryTest : BehaviorSpec({
 
             Then("해당 수의 자동차 리스트가 반환된다") {
                 result shouldHaveSize totalCarCount
+            }
+        }
+    }
+
+    Given("생성할 자동차 수가 음수이면") {
+        val totalCarCount = -1
+
+        When("자동차를 생성할 때") {
+            val result = shouldThrow<IllegalArgumentException> {
+                CarFactory.createCars(totalCarCount)
+            }
+
+            Then("IllegalArgumentException이 발생한다") {
+                result shouldHaveMessage "Argument should be non negative number."
             }
         }
     }
