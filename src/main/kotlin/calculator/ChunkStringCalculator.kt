c5956546a916d10852f0e8a964e1b75bc8feba9f
jaylene shin <jaylene.shin@kakaocorp.com>
2023-10-29 13:00:02 +0900

    fix: enum 사용 축약

--- src/main/kotlin/calculator/ChunkStringCalculator.kt
+++ src/main/kotlin/calculator/ChunkStringCalculator.kt
@@ -1,7 +1,5 @@
 package calculator
 
-import java.util.LinkedList
-
 class ChunkStringCalculator {
     companion object {
         fun calculate(operand1: String, operator: String, operand2: String): String {
@@ -12,14 +10,7 @@ class ChunkStringCalculator {
                 throw NumberFormatException("숫자형이 아닌 피연산자가 들어왔다.")
             }
 
-            val chunkResult = when (Operator.fromSymbol(operator)) {
-                Operator.PLUS -> Operator.PLUS.calcFunc(operand1.toInt(), operand2.toInt())
-                Operator.MINUS -> Operator.MINUS.calcFunc(operand1.toInt(), operand2.toInt())
-                Operator.MULTIPLY -> Operator.MULTIPLY.calcFunc(operand1.toInt(), operand2.toInt())
-                Operator.DIVIDE -> Operator.DIVIDE.calcFunc(operand1.toInt(), operand2.toInt())
-            }.toString() // wholeElements: List<String> 를 지워라
-
-            return chunkResult
+            return Operator.fromSymbol(operator).calcFunc(operand1.toInt(), operand2.toInt()).toString()
         }
     }
 }
