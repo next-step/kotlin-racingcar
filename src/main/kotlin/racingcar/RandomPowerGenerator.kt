517044db1b8669d013f50877b1c4ef5892206b05
eunwoo song <presentsong@naver.com>
2023-10-29 23:05:56 +0900

    refactor: 코틀린의 랜덤을 사용하도록 변경

--- src/main/kotlin/racingcar/RandomPowerGenerator.kt
+++ src/main/kotlin/racingcar/RandomPowerGenerator.kt
@@ -1,15 +1,14 @@
 package racingcar
 
-import java.util.concurrent.ThreadLocalRandom
+import kotlin.random.Random
 
 object RandomPowerGenerator : PowerGenerator {
 
     private const val MAX_POWER = 10
     private const val MIN_POWER = 0
 
-    private val threadLocalRandom = ThreadLocalRandom.current()
 
     override fun generate(): Int {
-        return threadLocalRandom.nextInt(MIN_POWER, MAX_POWER)
+        return Random.nextInt(MIN_POWER, MAX_POWER)
     }
 }
