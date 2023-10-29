9c6c02908aceb40e3662bded9121026d46a5ebb1
dokdogalmaegi <clik202012@gmail.com>
2023-10-29 18:48:43 +0900

    Add: 입력값 null or empty 테스트

--- src/test/kotlin/calculator/StringCalculatorTest.kt
+++ src/test/kotlin/calculator/StringCalculatorTest.kt
@@ -5,8 +5,19 @@ import org.junit.jupiter.api.assertThrows
 
 class StringCalculatorTest {
     @Test
-    fun `입력값이 없으면 IllegalArgumentException이 발생합니다`() {
+    fun `입력값이 Null이라면 IllegalArgumentException이 발생합니다`() {
         val input: String? = null;
+
+        val stringCalculator: StringCalculator = StringCalculator()
+
+        assertThrows<IllegalArgumentException> {
+            stringCalculator.calculate(input)
+        }
+    }
+
+    @Test
+    fun `입력값이 빈 문자열이라면 IllegalArgumentException이 발생합니다`() {
+        val input: String? = "";
 
         val stringCalculator: StringCalculator = StringCalculator()
 
