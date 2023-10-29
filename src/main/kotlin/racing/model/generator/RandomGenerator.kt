1e017810be54ecc2d423bc646a8b9ff5a219c1b6
dong <dong.22@kakaoenterprise.com>
2023-10-29 15:04:09 +0900

    refactor : 매직넘버를 분리하기

--- src/main/kotlin/racing/model/generator/RandomGenerator.kt
+++ src/main/kotlin/racing/model/generator/RandomGenerator.kt
@@ -3,8 +3,10 @@ package racing.model.generator
 import kotlin.random.Random
 
 class RandomGenerator : Generator {
+    private val rangeFrom = 1
+    private val rangeUntil = 10
     private val random = Random(System.currentTimeMillis())
     override fun generate(): Int {
-        return random.nextInt(1, 10)
+        return random.nextInt(rangeFrom, rangeUntil)
     }
 }
--- src/main/kotlin/racing/model/move/MoveRandomStrategy.kt
+++ src/main/kotlin/racing/model/move/MoveRandomStrategy.kt
@@ -5,11 +5,12 @@ import racing.model.generator.Generator
 class MoveRandomStrategy(
     private val generator: Generator,
 ) : MoveStrategy {
+    private val threshold = 4
     override fun movable(): Boolean {
         return movingPolicy(generator.generate())
     }
 
     private fun movingPolicy(value: Int): Boolean {
-        return value >= 4
+        return value >= threshold
     }
 }
