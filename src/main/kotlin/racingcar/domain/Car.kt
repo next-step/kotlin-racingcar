20e321253ba88a8e62d288937d5656f344d4e663
JunHyun Lee <dlwnsgus777@naver.com>
2023-10-29 22:09:57 +0900

    refactor: 외부에서 수정하지 못하도록 private set 사용

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -2,6 +2,7 @@ package racingcar.domain
 
 class Car(val id: Int, private val driveCondition: DriveCondition) {
     var distance: Int = 1
+        private set
 
     fun drive() {
         if (driveCondition.checkDriveCondition()) {
