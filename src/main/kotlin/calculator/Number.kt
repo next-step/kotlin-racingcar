3b53b6431feab3421cf665644c1311b27545e0d3
jimbaemon <jimbae@kakao.com>
2023-10-29 21:20:51 +0900

    refactor: value class 로 변경

--- src/main/kotlin/calculator/Number.kt
+++ src/main/kotlin/calculator/Number.kt
@@ -1,6 +1,7 @@
 package calculator
 
-data class Number(val number: Int) {
+@JvmInline
+value class Number(val number: Int) {
 
     constructor(number: String) : this(number.toIntOrNull() ?: throw IllegalArgumentException("숫자가 아닌 문자열 입니다."))
 
