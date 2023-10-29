00e919e58605e828b00698e107bb89cc1e1e23f7
yeongun <gjduddns@gmail.com>
2023-10-29 13:31:11 +0900

    feat: [Step4] 자동차 이름 제약사항인 5를 상수로 변경

--- src/main/kotlin/racingcar/ui/InputView.kt
+++ src/main/kotlin/racingcar/ui/InputView.kt
@@ -2,14 +2,14 @@ package racingcar.ui
 
 private const val INPUT_RACING_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
 private const val INPUT_COUNT_OF_RACING_MESSAGE = "시도할 횟수는 몇 회인가요?"
-
 private const val RACING_CAR_NAME_DELIMITER = ","
+private const val RACING_CAR_NAME_LENGTH_LIMIT = 5
 
 object InputView {
     fun inputNames(): List<String> {
         println(INPUT_RACING_CAR_NAME_MESSAGE)
         val inputNames = readln().split(RACING_CAR_NAME_DELIMITER)
-        val limitOverNames = inputNames.filter { inputName -> inputName.length > 5 }
+        val limitOverNames = inputNames.filter { inputName -> inputName.length > RACING_CAR_NAME_LENGTH_LIMIT }
         return printLimitOverNames(limitOverNames, inputNames)
     }
 
@@ -19,7 +19,7 @@ object InputView {
     ) = if (limitOverNames.isEmpty()) {
         inputNames
     } else {
-        println(limitOverNames.joinToString(RACING_CAR_NAME_DELIMITER) + " : 자동차 이름은 5글자를 초과하여 입력할 수 없습니다.")
+        println(limitOverNames.joinToString(RACING_CAR_NAME_DELIMITER) + " : 자동차 이름은 " + RACING_CAR_NAME_LENGTH_LIMIT + "글자를 초과하여 입력할 수 없습니다.")
         inputNames()
     }
 
