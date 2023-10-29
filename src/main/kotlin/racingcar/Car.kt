9fbf110e9de5bf52d1734865adfa3d4de7967e56
eunwoo song <presentsong@naver.com>
2023-10-29 16:14:23 +0900

    refactor: 변수 네이밍 수정

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -2,14 +2,16 @@ package racingcar
 
 class Car(val position: Int) {
 
-    constructor() : this(0)
+
+    constructor() : this(INITIAL_POSITION)
 
     private companion object {
-        const val MIN_POWER = 4
+        const val INITIAL_POSITION = 0
+        const val MINIMUM_MOVE_POWER = 4
     }
 
     fun move(power: Int): Car {
-        if (power >= MIN_POWER) {
+        if (power >= MINIMUM_MOVE_POWER) {
             return Car(position + 1)
         }
         return this
--- src/main/kotlin/racingcar/RandomPowerGenerator.kt
+++ src/main/kotlin/racingcar/RandomPowerGenerator.kt
@@ -4,9 +4,12 @@ import java.util.concurrent.ThreadLocalRandom
 
 object RandomPowerGenerator : PowerGenerator {
 
+    private const val MAX_POWER = 10
+    private const val MIN_POWER = 0
+
     private val threadLocalRandom = ThreadLocalRandom.current()
 
     override fun generate(): Int {
-        return threadLocalRandom.nextInt(0, 10)
+        return threadLocalRandom.nextInt(MIN_POWER, MAX_POWER)
     }
 }
