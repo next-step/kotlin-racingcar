f1dbbe6599b146a593c3031d425fc140bc613678
dean <sodp5@naver.com>
2023-10-29 13:25:19 +0900

    MoveCondition 추가

---
+++ src/test/kotlin/racingcar/MinimumAboveNumberMoveConditionTest.kt
@@ -0,0 +1,27 @@
+package racingcar
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class MinimumAboveNumberMoveConditionTest {
+    @ParameterizedTest
+    @ValueSource(ints = [4, 5, 6, 7, 8, 9])
+    fun `숫자가 4 이상인 경우 이동할 수 있다`(number: Int) {
+        val condition = MinimumAboveNumberMoveCondition(number)
+
+        val actual = condition.canForward()
+
+        assertThat(actual).isTrue
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, 2, 3])
+    fun `숫자가 4 미만인 경우 이동할 수 없다`(number: Int) {
+        val condition = MinimumAboveNumberMoveCondition(number)
+
+        val actual = condition.canForward()
+
+        assertThat(actual).isFalse
+    }
+}
