6a0e692c15c61902e5e6e0c7863f33ccb9834825
eunwoo song <presentsong@naver.com>
2023-10-29 23:05:26 +0900

    feat: 0, 9 범위 바깥을 거르는 로직 추가

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -4,14 +4,23 @@ class Car(val position: Int) {
     constructor() : this(INITIAL_POSITION)
 
     fun move(power: Int): Car {
-        if (power >= MINIMUM_MOVE_POWER) {
+        validatePower(power)
+        if (power >= MINIMUM_AMOUNT_TO_MOVE) {
             return Car(position + 1)
         }
         return this
     }
 
+    private fun validatePower(power: Int) {
+        require(power in MINIMUM_MOVE_POWER..MAXIMUM_MOVE_POWER) {
+            "자동차의 움직임은 0 이상 9 이하의 값만 가능합니다."
+        }
+    }
+
     private companion object {
         const val INITIAL_POSITION = 0
-        const val MINIMUM_MOVE_POWER = 4
+        const val MINIMUM_AMOUNT_TO_MOVE = 4
+        const val MAXIMUM_MOVE_POWER = 9
+        const val MINIMUM_MOVE_POWER = 0
     }
 }
