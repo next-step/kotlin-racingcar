3cfdf2cb40924178980acb31f560377f6487aaef
1stpie <1stpie@naver.com>
2023-10-29 14:42:49 +0900

    feat(core): add Race functions

--- src/main/kotlin/study/racingcar/Race.kt
+++ src/main/kotlin/study/racingcar/Race.kt
@@ -1,3 +1,25 @@
 package study.racingcar
 
-class Race
+class Race(
+    numberOfCars: Int,
+    rounds: Int,
+    private val randomValueGenerator: () -> Int = { (0..9).random() }
+) {
+    var numberOfCars: Int = numberOfCars
+        private set
+
+    var rounds: Int = rounds
+        private set
+
+    var cars: List<Car> = List(numberOfCars) { Car() }
+        private set
+
+    fun run() {
+        repeat(rounds) {
+            cars.forEach { car ->
+                val randomValue = randomValueGenerator()
+                car.move(randomValue)
+            }
+        }
+    }
+}
