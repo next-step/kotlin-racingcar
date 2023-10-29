7bfecc3815a371271c1ad75800aa1aeb9754b3dd
eunwoo song <presentsong@naver.com>
2023-10-29 22:32:58 +0900

    chore: 메서드 순서 변경 및 공백 수정

--- src/main/kotlin/racingcar/Cars.kt
+++ src/main/kotlin/racingcar/Cars.kt
@@ -1,10 +1,15 @@
 package racingcar
 
 class Cars(val cars: List<Car>, private val powerGenerator: PowerGenerator) {
+    fun move(): Cars {
+        val movedCars = mutableListOf<Car>()
+        for (car in cars) {
+            movedCars.add(car.move(powerGenerator.generate()))
+        }
+        return Cars(movedCars, powerGenerator)
+    }
 
     companion object {
-
-        @JvmStatic
         fun initializeWithNumberOfCars(numberOfCars: Int, powerGenerator: PowerGenerator): Cars {
             val cars = mutableListOf<Car>()
             for (i in 1..numberOfCars) {
@@ -12,13 +17,5 @@ class Cars(val cars: List<Car>, private val powerGenerator: PowerGenerator) {
             }
             return Cars(cars, powerGenerator)
         }
-    }
-
-    fun move(): Cars {
-        val movedCars = mutableListOf<Car>()
-        for (car in cars) {
-            movedCars.add(car.move(powerGenerator.generate()))
-        }
-        return Cars(movedCars, powerGenerator)
     }
 }
