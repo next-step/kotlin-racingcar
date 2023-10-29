30356555325672c968875d31f222cf73251f44f2
eunwoo song <presentsong@naver.com>
2023-10-29 16:11:16 +0900

    fix: 깨진 테스트 수정

--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -8,14 +8,14 @@ internal class CarTest {
     @Test
     fun `자동차는 특정 숫자 이상이 들어오면 움직인다`() {
         val car = Car(0)
-        car.move(4)
-        assertThat(car.position).isOne()
+        val result = car.move(4)
+        assertThat(result.position).isOne()
     }
 
     @Test
     fun `자동차는 특정 숫자 미만이 들어오면 움직이지 않는다`() {
         val car = Car(0)
-        car.move(3)
-        assertThat(car.position).isZero()
+        val result = car.move(3)
+        assertThat(result.position).isZero()
     }
 }
