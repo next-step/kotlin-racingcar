bed37817a1e35069dba64283a1fbc6dcbb8409e3
stevy.sung <stevy.sung@kakaocorp.com>
2023-10-30 07:37:30 +0900

    refactor: InputView 함수화

--- src/main/kotlin/step4/carRacingWinner/view/InputView.kt
+++ src/main/kotlin/step4/carRacingWinner/view/InputView.kt
@@ -17,22 +17,19 @@ class SystemInputReader : InputReader {
 
 object InputView {
 
-    private fun getCarInfo(inputReader: InputReader): List<Car> {
-        println("경주할 자동차의 이름을 입력하세요 (',' 구분 필요, 5자 이하)")
-        val carInput = inputReader.readInput()
+    private fun getInput(inputReader: InputReader, inputString: String): String {
+        println(inputString)
+        return inputReader.readInput()
+    }
 
-        val cars: List<Car> = carInput.split(",").let {
+    private fun getCarInfo(inputReader: InputReader): List<Car> =
+        getInput(inputReader, "경주할 자동차의 이름을 입력하세요 (',' 구분 필요, 5자 이하)").split(",").let {
             InputRule.validateCarNames(it)
         }.let {
             CarGenerator.generate(it)
         }
-        return cars
-    }
 
-    private fun getTryCount(inputReader: InputReader): Int {
-        println("시도할 회수는 몇회인가요?")
-        return inputReader.readInput().toInt()
-    }
+    private fun getTryCount(inputReader: InputReader): Int = getInput(inputReader, "시도할 회수는 몇회인가요?").toInt()
 
     fun getInputInfo(inputReader: InputReader): Pair<List<Car>, Int> =
         Pair(getCarInfo(inputReader), getTryCount(inputReader))
