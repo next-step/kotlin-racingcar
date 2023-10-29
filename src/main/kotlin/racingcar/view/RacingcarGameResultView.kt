29988a4da94d6b880499bc46e007f29b23b01318
yw9594 <kyw9509@gmail.com>
2023-10-30 01:40:17 +0900

    feat(step3): add result view class for racing car game

---
+++ src/main/kotlin/racingcar/view/RacingcarGameResultView.kt
@@ -0,0 +1,15 @@
+package racingcar.view
+
+import racingcar.model.Car
+
+class RacingcarGameResultView {
+    fun printCarsDistance(cars: List<Car>) {
+        cars.forEach { printCarDistance(it) }
+    }
+
+    fun printCarDistance(car: Car) {
+        val distance = car.currentDistance() + 1
+
+        println("-".repeat(distance))
+    }
+}
