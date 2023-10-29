be72219c53a666e2b8412334a1335d0e7a4663b6
stevy.sung <stevy.sung@kakaocorp.com>
2023-10-30 08:01:15 +0900

    chore: readme작성

--- src/main/kotlin/step4/carRacingWinner/model/CarRacing.kt
+++ src/main/kotlin/step4/carRacingWinner/model/CarRacing.kt
@@ -1,5 +1,6 @@
 package step4.carRacingWinner.model
 
+
 import step4.carRacingWinner.RaceRule
 import step4.carRacingWinner.view.InputView
 import step4.carRacingWinner.view.OutputView
@@ -11,7 +12,7 @@ class CarRacing {
     fun start() {
         val (cars, tryCount) = InputView.getInputInfo(inputReader = SystemInputReader())
         val raceOrganizer = RaceOrganizer(cars)
-        raceOrganizer.startRace(tryCount) { RaceRule.over4CanGo() }
+        raceOrganizer.startRace(tryCount, RaceRule.over4CanGo)
         OutputView.renderRace(raceOrganizer.getRaceResult(), SystemPrint)
     }
 }
