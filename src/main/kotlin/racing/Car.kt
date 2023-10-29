ed93364327194dbaf106def611850e886d79190b
Jihoon, Shin <jihoon9030@gmail.com>
2023-10-29 14:08:42 +0900

    feat : 프로퍼티로 car position 을 받도록 변경

--- src/main/kotlin/racing/Car.kt
+++ src/main/kotlin/racing/Car.kt
@@ -2,8 +2,8 @@ package racing
 
 import kotlin.random.Random
 
-class Car {
-    private var position = 0
+class Car(var position: Int = 0) {
+
     fun moveForward(randomValue: () -> Int = { Random.nextInt(10) }) {
         val forwardSteps = randomValue()
         if (forwardSteps >= 4) {
