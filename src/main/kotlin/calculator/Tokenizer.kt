2af42f94bd42a9ba0ee26a79a5c60b1dd22d1f34
jaylene shin <jaylene.shin@kakaocorp.com>
2023-10-29 11:51:59 +0900

    fix: 리뷰반영-const 대문자 치환, 이름 변경

--- src/main/kotlin/calculator/Tokenizer.kt
+++ src/main/kotlin/calculator/Tokenizer.kt
@@ -1,11 +1,11 @@
 package calculator
 
 object Tokenizer {
-    private const val validInputRegex = "[\\d+\\-*/\\s]*"
+    private const val TOKEN_REGEX = "[\\d+\\-*/\\s]*"
 
     fun validate(input: String?) {
         require(!input.isNullOrBlank(), { "입력값은 null이거나 빈 공백 문자일 수 없다." })
-        require(input.matches(Regex(validInputRegex)), { "입력값은 숫자, 사칙연산자, 공백만 들어올 수 있다." })
+        require(input.matches(Regex(TOKEN_REGEX)), { "입력값은 숫자, 사칙연산자, 공백만 들어올 수 있다." })
     }
 
     fun tokenize(input: String, pattern: String = "\\s+"): List<String> {
