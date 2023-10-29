79e4a39a5883cc8aa3a57367575636745912ba7d
inhyeokk <rkddlsgur983@gmail.com>
2023-10-29 23:15:34 +0900

    step4-review: 테스트 코드 수정

--- src/test/kotlin/racing/CarRacingTest.kt
+++ src/test/kotlin/racing/CarRacingTest.kt
@@ -8,7 +8,7 @@ class CarRacingTest : BehaviorSpec({
     given("경주할 자동차의 이름과 몇 번의 이동을 할 것인지를 전달하여") {
         val (carNames, tryCount) = "pobi,crong,honux".split(",") to 5
         `when`("자동차 경주를 하면") {
-            val (results, winners) = CarRacing.race(carNames, tryCount)
+            val (results, winners) = CarRacing().race(carNames, tryCount)
             then("그에 맞는 크기의 결과를 담은 이차원 배열이 생성되는가") {
                 results shouldBeSameSizeAs List(tryCount) { carNames.map { Car(it) } }
             }
