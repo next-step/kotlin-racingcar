5352d0726e78b9763513a987eab095a26ce72114
jiwon <ljwon77@daum.net>
2023-10-29 19:14:19 +0900

    feat: 레이싱 결과 저장 및 출력 기능 추가

---
+++ src/main/kotlin/racingcar/ui/RaceResultStorage.kt
@@ -0,0 +1,27 @@
+package racingcar.ui
+
+import racingcar.domain.Cars
+import racingcar.domain.RaceResult
+
+class RaceResultStorage(
+    results: List<CarMove> = emptyList()
+) : RaceResult {
+
+    private val _results: MutableList<CarMove> = results.toMutableList()
+
+    override fun save(cars: Cars) {
+        val carMoves = cars.getPositions().map {
+            CarMove(it.position)
+        }
+        ResultView.printRaceResult(carMoves)
+        _results.addAll(carMoves)
+        Thread.sleep(500) // 자동차 경주 사이의 결과를 0.5초씩 딜레이시켜 실시간으로 출력되는 것처럼 보이도록 만들기
+    }
+
+    val results
+        get() = _results.toList()
+}
+
+data class CarMove(
+    val position: Int,
+)
---
+++ src/main/kotlin/racingcar/ui/ResultView.kt
@@ -0,0 +1,19 @@
+package racingcar.ui
+
+object ResultView {
+
+    private const val RESULT_MESSAGE = "실행 결과"
+    private const val CAR_TRACK = "-"
+
+    fun printResultMessage() {
+        println(RESULT_MESSAGE)
+    }
+
+    fun printRaceResult(carMoves: List<CarMove>) {
+        carMoves.forEach {
+            val moveResult = CAR_TRACK.repeat(it.position)
+            println(moveResult)
+        }
+        println(System.lineSeparator())
+    }
+}
