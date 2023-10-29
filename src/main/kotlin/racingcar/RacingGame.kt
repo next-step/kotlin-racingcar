f4157051bb212b8de1336d471f9ca80748d869af
jaylene shin <jaylene.shin@kakaocorp.com>
2023-10-29 17:55:11 +0900

    feature: 게임 클래스 생성

---
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -0,0 +1,36 @@
+package racingcar
+
+import racingcar.domain.Car
+import racingcar.view.InputView
+import racingcar.view.ResultView
+
+class RacingGame private constructor(
+    private val numOfCars: Int,
+    private val numOfAttempts: Int
+) {
+    fun setUpCars(): List<Car> {
+        return List(numOfCars) { Car.ofRandom() }
+    }
+
+    fun start(cars: List<Car>): List<Car> {
+        ResultView.printResultText()
+        repeat(numOfAttempts) {
+            cars.forEach { it.move() }
+            ResultView.printResultState(cars)
+        }
+        return cars
+    }
+
+    companion object of {
+        fun of(numOfCars: Int, numOfAttempts: Int): RacingGame {
+            return RacingGame(numOfCars, numOfAttempts)
+        }
+    }
+}
+
+fun main() {
+    val inputs = InputView.gatherUserInput()
+    val racingGame = RacingGame.of(inputs.first, inputs.second)
+    val racingCars = racingGame.setUpCars()
+    racingGame.start(racingCars)
+}
