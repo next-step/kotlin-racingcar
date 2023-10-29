17ad774327be0c1f0ee71aa55cd5328d6bec8512
yw9594 <kyw9509@gmail.com>
2023-10-30 01:46:47 +0900

    feat(step3): add default car movement default condition

--- src/main/kotlin/racingcar/CarMovementManager.kt
+++ src/main/kotlin/racingcar/CarMovementManager.kt
@@ -1,9 +1,10 @@
 package racingcar
 
 import racingcar.model.Car
+import kotlin.random.Random.Default.nextInt
 
 class CarMovementManager(
-    private val condition: () -> Boolean
+    private val condition: () -> Boolean = { nextInt(0, 10) >= 4 }
 ) {
     fun moveCarsWithCondition(cars: List<Car>) {
         cars
