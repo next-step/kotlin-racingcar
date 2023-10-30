0860a7eacee648858b9c20dc12e07be3eb80d87a
moses.lee(이상후)/kakao <moses.lee@kakaocorp.com>
2023-10-30 17:17:48 +0900

    feat : kotlin mock 라이브러리 추가
    - Reader 인터페이스를 사용하여 stdin input test 코드 추가
    
    # step4

--- src/main/kotlin/study/racingcar/application/InputView.kt
+++ src/main/kotlin/study/racingcar/application/InputView.kt
@@ -1,18 +1,14 @@
 package study.racingcar.application
 
-import kotlin.jvm.Throws
-
 /**
- * 기능
- *    - 값을 입력 받는다.
+ * 값을 입력 받는다.
  * */
-data class InputValue(val carTotalNum: Int, val tryCount: Int)
 
-object InputView {
-
-    // TODO-리뷰 요청 : private 함수로 역할을 나눠서 분리했지만, 결과적으로 getInput 함수의 역할이 많습니다. SRP 에 위반되는건 아닌지 궁금합니다.
+class InputView(
+    val reader: Reader) {
     /**
      * stdin 으로 입력을 받고, 입력받은 값을 검증해서 InputValue 로 반환한다.
+     * @param 입력을 받는 Reader 객체
      * @throws IllegalArgumentException 입력 값이 null 이거나 적절한 정수형의 숫자가 아닌 경우
      * */
     fun getInput(): InputValue {
@@ -37,24 +33,18 @@ object InputView {
             throw IllegalArgumentException("입력 값이 정수형의 숫자가 아닙니다. 숫자를 입력해주세요.")
         }
 
-        if (num < 1) {
-            throw IllegalArgumentException("입력 값을 확인해주세요. 1 이상의 숫자만 입력 가능합니다.")
-        }
-
-        return num
-    }
 
     /**
      * 입력으로 받은 문자열을 정수형으로 변환해서 반환한다.
      * @param prompt 입력을 유도하는 문자열
      * @throws IllegalArgumentException 입력 값이 null 이거나 적절한 정수형의 숫자가 아닌 경우
      * */
-    private fun inputNumber(prompt: String): Int {
+    fun inputNumber(prompt: String): Int {
         while (true) {
             try {
                 print(prompt)
-                val input: String = readlnOrNull() ?: throw IllegalArgumentException("입력 값이 null 입니다.")
-                return input.checkIsProperIntFormat()
+                val input: String = reader.read() ?: throw IllegalArgumentException("입력 값이 null 입니다.")
+                return Number(input).checkIsProperIntFormat()
             } catch (e: Exception) {
                 println("[$e] 입력 숫자를 확인해주세요.")
             }
--- src/test/kotlin/study/racingcar/application/InputViewTest.kt
+++ src/test/kotlin/study/racingcar/application/InputViewTest.kt
@@ -1,11 +1,23 @@
 package study.racingcar.application
 
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
 
 class InputViewTest {
 
-    // TODO : stdin 으로 값을 받는 객체의 경우, 테스트를 어떻게 해야할까?
     @Test
+    @DisplayName("stdin reader 를 사용했을때, 입력값을 받아올 수 있는지 확인")
     fun getInput() {
+        // given
+        val reader = mock<Reader>()
+
+        // when
+        whenever(reader.read()).thenReturn("123")
+        val result: Int = InputView(reader).inputNumber("Enter a number: ")
+
+        // then
+        assert(result == 123)
     }
 }
