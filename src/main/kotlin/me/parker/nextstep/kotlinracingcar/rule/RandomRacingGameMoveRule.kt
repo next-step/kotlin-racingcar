25ad4b194c09872938a88ca77dccf171b65a7dcc
parker1609 <psbum1609@gmail.com>
2023-10-30 18:28:50 +0900

    refactor: 게임 룰에서 랜덤 숫자 생성 유틸 클래스 사용하는 것으로 변경

--- src/main/kotlin/me/parker/nextstep/kotlinracingcar/rule/RandomRacingGameMoveRule.kt
+++ src/main/kotlin/me/parker/nextstep/kotlinracingcar/rule/RandomRacingGameMoveRule.kt
@@ -1,11 +1,15 @@
 package me.parker.nextstep.kotlinracingcar.rule
 
+import me.parker.nextstep.kotlinracingcar.utils.RandomNumberGenerator
+
 class RandomRacingGameMoveRule : RacingGameMoveRule {
     companion object {
+        private const val MIN_BOUND = 0
+        private const val MAX_BOUND = 9
         private const val FORWARD_STANDARD = 4
     }
 
     override fun isForward(): Boolean {
-        return (Math.random() * 10).toInt() >= FORWARD_STANDARD
+        return RandomNumberGenerator.generate(MIN_BOUND, MAX_BOUND) >= FORWARD_STANDARD
     }
 }
