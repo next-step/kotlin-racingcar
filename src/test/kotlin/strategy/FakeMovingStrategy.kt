7e17e77eb5e83a42f6b65d1fe09a82104fd3eb61
seongmo-jeon <seongmo.jeon@navercorp.com>
2023-10-31 00:47:28 +0900

    feat: indent는 최대 1까지만 하도록 수정

--- src/test/kotlin/strategy/FakeMovingStrategy.kt
+++ src/test/kotlin/strategy/FakeMovingStrategy.kt
@@ -2,6 +2,6 @@ package strategy
 
 import racingcar.strategy.MovingStrategy
 
-class FakeMovingStrategy(private val canMove: Boolean): MovingStrategy {
+class FakeMovingStrategy(private val canMove: Boolean) : MovingStrategy {
     override fun canMove(): Boolean = canMove
 }
