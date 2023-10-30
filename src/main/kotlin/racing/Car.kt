7cdf0b4da4c61db46a9743f9342b74074b416c1f
Jihoon, Shin <jihoon9030@gmail.com>
2023-10-30 20:45:56 +0900

    feat : Value 를 외부에서 주입받을 수 있도록 합니다

--- src/main/kotlin/racing/Car.kt
+++ src/main/kotlin/racing/Car.kt
@@ -1,15 +1,12 @@
 package racing
 
-import kotlin.random.Random
-
 class Car(private var position: Int = START_POSITION) {
 
     companion object {
         const val START_POSITION = 1
     }
 
-    fun moveForward(randomValue: () -> Int = { Random.nextInt(10) }) {
-        val forwardSteps = randomValue()
+    fun moveForward(forwardSteps: Int) {
         if (forwardSteps >= 4) {
             position += 1
         }
