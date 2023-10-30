460ec6570377ac2e8d42b24bd4ea4570d51fb4a9
bmsk <vv99911@gmail.com>
2023-10-30 20:49:43 +0900

    [Refact] 움직임 정측을 서로 다른 파일로 관리

--- src/main/kotlin/org/bmsk/racingcar/domain/policy/MovementPolicy.kt
+++ src/main/kotlin/org/bmsk/racingcar/domain/policy/MovementPolicy.kt
@@ -1,25 +1,9 @@
 package org.bmsk.racingcar.domain.policy
 
-import org.bmsk.racingcar.domain.random.DefaultRandomGenerator
-import org.bmsk.racingcar.domain.random.RandomGenerator
 import org.bmsk.racingcar.model.Position
 
 sealed interface MovementPolicy {
     fun move(position: Position): Position
 
-    class RandomMovement(private val randomGenerator: RandomGenerator = DefaultRandomGenerator()) : MovementPolicy {
-
-        override fun move(position: Position): Position {
-            val distance = randomGenerator.generate()
-            return if (distance >= DISTANCE_CONDITIONS) {
-                position.copy(xPos = position.xPos + 1)
-            } else {
-                position
-            }
-        }
-
-        companion object {
-            private const val DISTANCE_CONDITIONS = 4
-        }
-    }
+    interface RandomMovement : MovementPolicy
 }
