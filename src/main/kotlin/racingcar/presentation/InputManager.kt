9c5490c1008913a4f0b920b22769ce584ada76de
JunHyun Lee <dlwnsgus777@naver.com>
2023-10-30 23:02:00 +0900

    refact: 불필요 로직 제거

--- src/main/kotlin/racingcar/presentation/InputManager.kt
+++ src/main/kotlin/racingcar/presentation/InputManager.kt
@@ -3,10 +3,6 @@ package racingcar.presentation
 import racingcar.domain.CarNames
 
 class InputManager {
-    fun inputNumberOfCar(): Int {
-        println(INPUT_NUMBER_OF_CARS_MESSAGE)
-        return readln().toInt()
-    }
 
     fun inputCarNames(): CarNames {
         println(INPUT_NAME_OF_CARS_MESSAGE)
@@ -20,9 +16,7 @@ class InputManager {
     }
 
     companion object {
-        const val INPUT_NUMBER_OF_CARS_MESSAGE = "자동차 대수는 몇 대인가요?"
         const val INPUT_NAME_OF_CARS_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
         const val INPUT_NUMBER_OF_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?"
-        const val NAME_VALID_ERROR_MESSAGE = "입력값이 공백이거나 Null일 수 없습니다"
     }
 }
