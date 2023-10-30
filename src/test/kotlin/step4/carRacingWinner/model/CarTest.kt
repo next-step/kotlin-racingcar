52a3498a83bbd05c5f66e7f1f4143b6188c46599
stevy.sung <stevy.sung@kakaocorp.com>
2023-10-30 18:52:34 +0900

    test: BDD로 테스트 짜보기

---
+++ src/test/kotlin/step4/carRacingWinner/model/CarTest.kt
@@ -0,0 +1,23 @@
+package step4.carRacingWinner.model
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import step3.carRacing.model.Car
+
+class CarTest : BehaviorSpec({
+    Given("자동차가 생성된다.") {
+        val car = Car()
+        When("경기 규칙에 의해 race 를 성공 했을 때") {
+            car.tryRace { true }
+            Then("현재 위치는 1이 된다") {
+                car.getCurPosition() shouldBe 1
+            }
+        }
+        When("경기 규칙에 의해 race 를 실패 했을 때") {
+            car.tryRace { false }
+            Then("현재 위치는 그대로 1이 된다") {
+                car.getCurPosition() shouldBe 1
+            }
+        }
+    }
+})
---
+++ src/test/kotlin/step4/carRacingWinner/model/RaceOrganizerTest.kt
@@ -0,0 +1,20 @@
+package step4.carRacingWinner.model
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+
+class RaceOrganizerTest : BehaviorSpec({
+    Given("stevy 라는 이름의 차량을 포함한 4대의 차량이 생성 되었다.") {
+        val carList = listOf<Car>(Car(0, "stevy"), Car(0, "jisu"), Car(0, "tomi"), Car(0, "kuku"))
+        val raceOrganizer = RaceOrganizer(carList)
+        When("경기 규칙은 stevy 만 앞으로 갈 수 있고 5번의 경기를 한다.") {
+            raceOrganizer.startRace(5) {
+                it.getCarName() == "stevy"
+            }
+            Then("5라운드 때 stevy 차량의 위치는 5여야 한다") {
+                raceOrganizer.getRaceResult()
+                    .first { it.round == 5 }.carData.first { it.name === "stevy" }.curPosition shouldBe 5
+            }
+        }
+    }
+})
