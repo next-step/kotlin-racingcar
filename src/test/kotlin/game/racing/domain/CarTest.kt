446f4ffb26d1b52d27a902f49fa6d9ffa19126d0
hyotaek-jang <hyotaek.jang@linecorp.com>
2023-10-30 21:54:14 +0900

    feat: add Car

---
+++ src/test/kotlin/game/racing/domain/CarTest.kt
@@ -0,0 +1,23 @@
+package game.racing.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+internal class CarTest {
+    @ParameterizedTest
+    @ValueSource(ints = [4, 5, 6, 7, 8, 9])
+    fun `4 이상의 값이 입력되면 이동한다`(value: Int) {
+        val car = Car()
+
+        assertThat(car.isMovePossible(value)).isTrue()
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [0, 1, 2, 3])
+    fun `4 이하의 값이 입력되면 이동하지 않는다`(value: Int) {
+        val car = Car()
+
+        assertThat(car.isMovePossible(value)).isFalse()
+    }
+}
