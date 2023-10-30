783ed6a85a09caaa28abaa311ea4220a127f4156
Celina <luv.mins99@gmail.com>
2023-10-31 01:29:28 +0900

    test: 자동차 객체 테스트 추가

--- src/test/kotlin/racingcar/model/CarTest.kt
+++ src/test/kotlin/racingcar/model/CarTest.kt
@@ -2,27 +2,34 @@ package racingcar.model
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import org.assertj.core.api.Assertions
 
 class CarTest : StringSpec({
 
     "자동차 객체 생성시 위치가 0인 객체가 생성 된다" {
-        val car = Car()
+        val car = Car("jason")
 
         car.position shouldBe 0
     }
 
     "자동차 객체의 move 메서드로 postion이 1 증가 한다" {
-        val car = Car()
+        val car = Car("pobi")
         car.move()
 
         car.position shouldBe 1
     }
 
     "자동차 객체를 3번 움직이게 하면 위치가 3이된다" {
-        val car = Car()
+        val car = Car("car")
         repeat(3) {
             car.move()
         }
         car.position shouldBe 3
+    }
+
+    "자동차 이름은 5자를 초과할 수 없다" {
+        Assertions.assertThatThrownBy {
+            Car("racingcar")
+        }.isInstanceOf(IllegalArgumentException::class.java)
     }
 })
