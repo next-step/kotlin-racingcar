d597e0ff9b8695f6f59f1c70c719f2d169fb5840
[김정식] <wjdtlr0920@gmail.com>
2023-10-30 19:16:59 +0900

    Fix: Main Function Changed to require user input

--- src/main/kotlin/step2/main/Main.kt
+++ src/main/kotlin/step2/main/Main.kt
@@ -3,5 +3,6 @@ package step2.main
 import step2.calculator.Calculator
 
 fun main() {
-    print(Calculator.calculate("2 + 3 * 4 / 2"))
+    val result = Calculator.calculate(readln())
+    println(result)
 }
