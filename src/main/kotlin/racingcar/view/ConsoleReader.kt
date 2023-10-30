d7ac22f61ec30ab521cf6f350b35cca073ae732d
jaylene shin <jaylene.shin@kakaocorp.com>
2023-10-30 15:31:50 +0900

    fix: 확장 함수 사용, 에러 메시지 변경

--- src/main/kotlin/racingcar/view/ConsoleReader.kt
+++ src/main/kotlin/racingcar/view/ConsoleReader.kt
@@ -3,12 +3,6 @@ package racingcar.view
 object ConsoleReader {
     fun readInput(): Int {
         val input = readlnOrNull() ?: throw IllegalArgumentException("입력값은 공백이거나 빈 문자열일 수 없습니다.")
-        val inputNum: Int
-        try {
-            inputNum = input.toInt()
-        } catch (e: NumberFormatException) {
-            throw IllegalArgumentException("입력값은 0 이상의 숫자여야 합니다.")
-        }
-        return inputNum
+        return input.toIntOrNull() ?: throw IllegalArgumentException("숫자가 아닌 입력값은 들어올 수 없습니다.")
     }
 }
