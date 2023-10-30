eb511e94d5ba097d1b87f5b48f84b7bf3d2b4313
jaylene shin <jaylene.shin@kakaocorp.com>
2023-10-30 17:14:41 +0900

    fix: uesrInput 도메인 추가

--- src/main/kotlin/racingcar/domain/UserInput.kt
+++ src/main/kotlin/racingcar/domain/UserInput.kt
@@ -3,4 +3,9 @@ package racingcar.domain
 data class UserInput(
     val numOfCars: Int,
     val numOfAttempts: Int
-)
+) {
+    init {
+        require(numOfCars >= 0) {"자동차 대수는 0 이상의 숫자여야 합니다. (입력된 값: $numOfCars)"}
+        require(numOfAttempts >= 0) {"게임 횟수는 0 이상의 숫자여야 합니다. (입력된 값: $numOfAttempts)"}
+    }
+}
