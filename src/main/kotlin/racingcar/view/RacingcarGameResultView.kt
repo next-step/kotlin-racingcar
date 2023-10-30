5ec38c6bdd641c7e97d1be60f2ed6f9f6b7bd5d1
yw9594 <kyw9509@gmail.com>
2023-10-30 18:21:30 +0900

    feat(step3): change racing car result view template

--- src/main/kotlin/racingcar/view/RacingcarGameResultView.kt
+++ src/main/kotlin/racingcar/view/RacingcarGameResultView.kt
@@ -5,9 +5,10 @@ import racingcar.model.Car
 class RacingcarGameResultView {
     fun printCarsDistance(cars: List<Car>) {
         cars.forEach { printCarDistance(it) }
+        println()
     }
 
-    fun printCarDistance(car: Car) {
+    private fun printCarDistance(car: Car) {
         val distance = car.currentDistance() + 1
 
         println("-".repeat(distance))
