a91df6c0fd3e9fd6b397f6ec8a64b0442e1eec73
Jihoon, Shin <jihoon9030@gmail.com>
2023-10-30 22:34:58 +0900

    feat : RacingCircuit 에서 winner 를 가리는 Race 방식을 정의합니다

--- src/main/kotlin/racing/RacingCircuit.kt
+++ src/main/kotlin/racing/RacingCircuit.kt
@@ -8,15 +8,26 @@ class RacingCircuit {
         val inputView = InputView()
         val resultView = ResultView()
         val (carNumber, tryNumber) = inputView.getInputView()
-
-        val racingCars = List(carNumber) { Car() }
-        val cars = Cars(racingCars)
+        val cars = Cars(carNumber)
 
         println("레이스 스타트")
-        RandomCarRace().move(cars, tryNumber, resultView)
+        RandomCarRace().move(cars, tryNumber, resultView::makeResultView)
+    }
+
+    fun raceWithWinner(){
+        val inputView = InputView()
+        val resultView = ResultView()
+        val (carNames, tryNumber) = inputView.getInputViewWithName()
+
+        val cars = Cars(carNames)
+
+        println("레이스 스타트")
+        RandomCarRace().move(cars, tryNumber, resultView::makeResultViewWithName)
+        resultView.makeWinnerView(cars)
     }
 }
 
 fun main(){
-    RacingCircuit().race()
+    val racingCircuit = RacingCircuit()
+    racingCircuit.raceWithWinner()
 }
