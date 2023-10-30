42f29bfe60d96ae724500a7c90c64109090ef8bd
dong <dong.22@kakaoenterprise.com>
2023-10-30 15:50:20 +0900

    refactor : 상수를 const 화

--- src/main/kotlin/racing/model/generator/RandomGenerator.kt
+++ src/main/kotlin/racing/model/generator/RandomGenerator.kt
@@ -3,10 +3,14 @@ package racing.model.generator
 import kotlin.random.Random
 
 class RandomGenerator : Generator {
-    private val rangeFrom = 1
-    private val rangeUntil = 10
+
     private val random = Random(System.currentTimeMillis())
     override fun generate(): Int {
-        return random.nextInt(rangeFrom, rangeUntil)
+        return random.nextInt(LOWER, UPPER)
+    }
+
+    companion object {
+        private const val LOWER = 1
+        private const val UPPER = 10
     }
 }
