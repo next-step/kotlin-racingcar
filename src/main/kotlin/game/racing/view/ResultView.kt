7a150f724671364524d1fbfb41389c3c5478e17d
hyotaek-jang <hyotaek.jang@linecorp.com>
2023-10-30 22:58:11 +0900

    refactor: refactor printCarPosition

--- src/main/kotlin/game/racing/view/ResultView.kt
+++ src/main/kotlin/game/racing/view/ResultView.kt
@@ -5,11 +5,23 @@ import game.racing.domain.Car
 object ResultView {
     private const val RESULT_MESSAGE = "실행 결과"
     private const val POSITION_DELIMITER = "-"
+    private const val ZERO_POSITION = "X"
+
+    fun printResultIntroduction() {
+        println(RESULT_MESSAGE)
+    }
+
+    private fun printCarPosition(car: Car) {
+        if (car.position == Car.MIN_POSITION) {
+            println(ZERO_POSITION)
+            return
+        }
+        println(POSITION_DELIMITER.repeat(car.position))
+    }
 
     fun printResult(cars: List<Car>) {
-        println(RESULT_MESSAGE)
         cars.forEach {
-            println(POSITION_DELIMITER.repeat(it.position))
+            printCarPosition(it)
         }
         println()
     }
