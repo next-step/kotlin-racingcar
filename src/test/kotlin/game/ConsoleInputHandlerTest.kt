bbdfca87a9f4c768d07034d0c7a2c694eb002956
ALbertIM0427 <guang7226961@naver.com>
2023-10-30 16:26:15 +0900

    test: 회수 받는 테스트 추가

--- src/test/kotlin/game/ConsoleInputHandlerTest.kt
+++ src/test/kotlin/game/ConsoleInputHandlerTest.kt
@@ -9,26 +9,58 @@ import org.mockito.Mockito.`when`
 class ConsoleInputHandlerTest {
 
     @Test
-    fun `입력 받은 내용을 그대로 반환환다`() {
+    fun `입력받은 자동차 이름을 그대로 반환환다`() {
+        // given
         val inputReader = mock(InputWrapper::class.java)
         val expect = "car1,car2,car3"
         `when`(inputReader.readlnOrNull()).thenReturn(expect)
-
         val consoleInputHandler = ConsoleInputHandler(inputReader)
-        assertThat(consoleInputHandler.askForCarNames()).isEqualTo(expect)
+
+        // when
+        val askForCarNames = consoleInputHandler.askForCarNames()
+
+        // then
+        assertThat(askForCarNames).isEqualTo(expect)
     }
 
     @Test
-    fun `입력 받은 내용이 null이면 예외를 던진다`() {
+    fun `입력받은 자동차 이름이 null이면 예외를 던진다`() {
+        // given
         val inputReader = mock(InputWrapper::class.java)
         `when`(inputReader.readlnOrNull()).thenReturn(null)
-
         val consoleInputHandler = ConsoleInputHandler(inputReader)
-        assertThatThrownBy { consoleInputHandler.askForCarNames() }.isInstanceOf(IllegalArgumentException::class.java)
+
+        // when,then
+        assertThatThrownBy { consoleInputHandler.askForCarNames() }
+            .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessage(ErrorMessage.INVALID_INPUT_ERROR_MSG.message)
     }
 
     @Test
-    fun askForRetryCount() {
+    fun `입력 받은 시도 횟수을 그대로 반환한다`() {
+        // given
+        val inputReader = mock(InputWrapper::class.java)
+        val expect = "5"
+        `when`(inputReader.readlnOrNull()).thenReturn(expect)
+        val consoleInputHandler = ConsoleInputHandler(inputReader)
+
+        // when
+        val askForRetryCount = consoleInputHandler.askForRetryCount()
+
+        // then
+        assertThat(askForRetryCount).isEqualTo(expect)
+    }
+
+    @Test
+    fun `입력 받은 시도 횟수가 null이면 예외를 던진다`() {
+        // given
+        val inputReader = mock(InputWrapper::class.java)
+        `when`(inputReader.readlnOrNull()).thenReturn(null)
+        val consoleInputHandler = ConsoleInputHandler(inputReader)
+
+        // when,then
+        assertThatThrownBy { consoleInputHandler.askForRetryCount() }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage(ErrorMessage.INVALID_INPUT_ERROR_MSG.message)
     }
 }
