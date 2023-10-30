ab367b735bfccb706a0ae71577c3b7a6a760d6e2
Jihoon, Shin <jihoon9030@gmail.com>
2023-10-30 18:44:17 +0900

    fix : BasicOperationsCaclulator 에 쓰지 않는 operation function 을 제거합니다

--- src/main/kotlin/calculator/BasicOperationsCalculator.kt
+++ src/main/kotlin/calculator/BasicOperationsCalculator.kt
@@ -1,11 +1,6 @@
 package calculator
 
 class BasicOperationsCalculator {
-    private fun add(a: Double, b: Double): Double = a + b
-    private fun subtract(a: Double, b: Double): Double = a - b
-    private fun multiply(a: Double, b: Double): Double = a * b
-    private fun divide(a: Double, b: Double): Double = a / b
-
     fun operation(a: Double, b: Double, symbol: String): Double {
         val operator = Operator.fromSymbol(symbol)
         return operator.operate(a, b)
