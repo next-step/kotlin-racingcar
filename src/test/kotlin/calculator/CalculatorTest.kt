abc1c32e1575422f3b37fa0298e3e003e977c042
zelda-jeong <zelda.jeong@kakaocorp.com>
2023-10-31 07:08:03 +0900

    step2 문자열 계산기 테스트코드 추가

---
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -0,0 +1,30 @@
+package calculator
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class CalculatorTest {
+    @Test
+    fun `input 값에 이상이 없는 경우`() {
+        val input = "2 + 3 * 4 / 2"
+        val expect = 10
+        val calculator = Calculator()
+        val actual = calculator.calculate(input)
+        assertThat(actual).isEqualTo(expect)
+    }
+
+    @Test
+    fun `입력값이 공백인 경우`() {
+        val input = " "
+        val calculator = Calculator()
+        assertThrows<IllegalArgumentException> ("입력값이 null이거나 빈 공백 문자입니다.") { calculator.calculate(input) }
+    }
+
+    @Test
+    fun `사칙연산 기호가 아닌 경우`() {
+        val input = "4 5 3 * 2"
+        val calculator = Calculator()
+        assertThrows<IllegalArgumentException> ("사칙연산 기호가 아닙니다.") { calculator.calculate(input) }
+    }
+}
