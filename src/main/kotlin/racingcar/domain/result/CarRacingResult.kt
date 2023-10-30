84ca4f8a3d9d5428f7c5771541d7655b707ef1bf
bongbong <kbk5190@naver.com>
2023-10-30 23:35:11 +0900

    feat: 자동차 경주 우승자 가리기

--- src/main/kotlin/racingcar/domain/result/CarRacingResult.kt
+++ src/main/kotlin/racingcar/domain/result/CarRacingResult.kt
@@ -2,6 +2,14 @@ package racingcar.domain.result
 
 import racingcar.domain.car.Car
 
-data class CarRacingResult(
+class CarRacingResult(
     val cars: List<Car>,
-)
+) {
+    fun getFarthestCar(): List<Car> {
+        val maxPosition = getMaxPosition()
+        return cars.filter { it.position == maxPosition }
+    }
+
+    private fun getMaxPosition(): Int =
+        cars.maxOf { it.position }
+}
---
+++ src/test/kotlin/result/CarRacingResultTest.kt
@@ -0,0 +1,46 @@
+package result
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.collections.shouldBeIn
+import io.kotest.matchers.shouldBe
+import racingcar.domain.car.Car
+import racingcar.domain.car.CarName
+import racingcar.domain.result.CarRacingResult
+
+class CarRacingResultTest : FunSpec({
+    test("가장 멀리간 차를 반환한다") {
+        val farthestCarName = CarName("test1")
+        val farthestPosition = 10
+        val cars = listOf(
+            Car(farthestCarName, farthestPosition),
+            Car(CarName("test3"), 3),
+            Car(CarName("test4"), 3)
+        )
+
+        val result = CarRacingResult(cars).getFarthestCar()
+
+        result.size shouldBe 1
+        result[0].name shouldBe farthestCarName
+        result[0].position shouldBe farthestPosition
+    }
+
+    test("가장 멀리간 차가 여러 대인 경우 여러 대를 반환한다") {
+        val farthestNames = listOf(CarName("test1"), CarName("test2"))
+        val farthestNames2 = listOf("test1", "test2")
+        val farthestPosition = 10
+        val cars = listOf(
+            Car(farthestNames[0], farthestPosition),
+            Car(farthestNames[1], farthestPosition),
+            Car(CarName("test3"), 3),
+            Car(CarName("test4"), 3)
+        )
+
+        val result = CarRacingResult(cars).getFarthestCar()
+
+        result.size shouldBe 2
+        result[0].name.shouldBeIn(farthestNames)
+        result[1].name.shouldBeIn(farthestNames)
+        result[0].position shouldBe farthestPosition
+        result[1].position shouldBe farthestPosition
+    }
+})
