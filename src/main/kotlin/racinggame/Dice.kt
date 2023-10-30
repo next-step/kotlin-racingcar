e58c288ed0d260f5e1d5f3b23ef8ce1a954ff1af
baek0318 <peachberry0318@gmail.com>
2023-10-30 19:12:37 +0900

    step4 : Dice.pitch가 DiceResult를 반환하도록 변경

--- src/main/kotlin/racinggame/Dice.kt
+++ src/main/kotlin/racinggame/Dice.kt
@@ -1,15 +1,32 @@
 package racinggame
 
-import kotlin.random.Random
-
 interface Dice {
-    fun pitch(): Int
+    fun pitch(): DiceResult
 }
 
 class RandomDice : Dice {
-    override fun pitch() = Random.nextInt() % 10
+    override fun pitch() = ((0..10).random()).let {
+        DiceResult.getResult(it)
+    }
 }
 
 class ForwardDice : Dice {
-    override fun pitch() = 4
+    override fun pitch() = (4).let {
+        DiceResult.getResult(it)
+    }
+}
+
+enum class DiceResult(val apply: (Int) -> Int) {
+    STOP({ it }),
+    FORWARD({ it + 1 });
+
+    companion object {
+        fun getResult(diceResult: Int) = diceResult.let {
+            if (it >= 4) {
+                FORWARD
+            } else {
+                STOP
+            }
+        }
+    }
 }
