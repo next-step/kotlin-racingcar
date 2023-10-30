83ae9f4666ba3fb6b7208e13be40dba03ee0415d
baek0318 <peachberry0318@gmail.com>
2023-10-31 00:55:42 +0900

    step4 : 이름에 대해서 입력을 받을 수 있도록 수정

--- src/main/kotlin/racinggame/InputView.kt
+++ src/main/kotlin/racinggame/InputView.kt
@@ -2,13 +2,13 @@ package racinggame
 
 object InputView {
 
-    fun getCarCount(): Int {
-        println("자동차 대수는 몇 대인가요?")
-        return readln().toInt()
-    }
-
     fun getTryCount(): Int {
         println("시도할 횟수는 몇 회인가요?")
         return readln().toInt()
+    }
+
+    fun getCarNames(): List<String> {
+        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+        return readln().split(",")
     }
 }
