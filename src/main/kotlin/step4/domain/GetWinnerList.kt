ff96df69378e642c4b230340db802ff5baf4aaff
Factoriall <skarlgus0910@gmail.com>
2023-10-31 00:29:35 +0900

    step4: remove redundant class

--- src/main/kotlin/step4/domain/GetWinnerList.kt
+++
@@ -1,8 +0,0 @@
-package step4.domain
-
-object GetWinnerList {
-    fun getWinnerList(racingCars: List<RacingCar>): List<RacingCar> {
-        val maxMove = racingCars.maxOf { it.moveCount }
-        return racingCars.filter { it.moveCount == maxMove }
-    }
-}
