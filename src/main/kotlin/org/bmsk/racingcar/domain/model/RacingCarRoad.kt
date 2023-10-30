91aa9677992b8885096d625d287da115feff1cae
bmsk <vv99911@gmail.com>
2023-10-31 01:12:36 +0900

    [Refact] RacingCarRoad에 제약 조건 추가
    빈 자동차 리스트로 초기화 시 예외처리

--- src/main/kotlin/org/bmsk/racingcar/domain/model/RacingCarRoad.kt
+++ src/main/kotlin/org/bmsk/racingcar/domain/model/RacingCarRoad.kt
@@ -1,8 +1,14 @@
 package org.bmsk.racingcar.domain.model
 
+import org.bmsk.racingcar.domain.error.RacingCarErrorMessage
+
 data class RacingCarRoad(
     val cars: List<Car>,
 ) {
+    init {
+        require(cars.isNotEmpty()) { RacingCarErrorMessage.LIST_OF_CARS_CANNOT_BE_EMPTY.message }
+    }
+
     fun getMovedRacingCarRoad(): RacingCarRoad {
         return RacingCarRoad(
             cars = cars.map { it.getMovedCar() },
