f8622669424dd9353c42bd167166210eddbaad1f
bmsk <vv99911@gmail.com>
2023-10-30 21:42:43 +0900

    [Test] CarTest를 더 직관적이도록 개선

--- src/test/kotlin/org/bmsk/racingcar/domain/model/CarTest.kt
+++ src/test/kotlin/org/bmsk/racingcar/domain/model/CarTest.kt
@@ -1,8 +1,8 @@
 package org.bmsk.racingcar.domain.model
 
-import io.kotest.core.spec.style.WordSpec
+import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
-import org.bmsk.racingcar.domain.policy.MovementPolicy
+import org.bmsk.racingcar.domain.policy.RandomMovementImpl
 import org.bmsk.racingcar.domain.random.RandomGenerator
 import org.bmsk.racingcar.model.Position
 
@@ -10,22 +10,25 @@ class FakeRandomGenerator(private val value: Int) : RandomGenerator {
     override fun generate(): Int = value
 }
 
-class CarTest : WordSpec({
-    "Car 클래스는" should {
-        "랜덤 움직임 정책의" should {
-            "랜덤으로 만들어진 수가" should {
-                "4 이상인 경우에만 전진한다." {
-                    var testPosition = Position(xPos = 0)
-                    for (i in 0..9) {
-                        val randomMovementPolicy = MovementPolicy.RandomMovement(FakeRandomGenerator(i))
-                        val newPosition = randomMovementPolicy.move(testPosition)
+class CarTest : FunSpec({
+    context("Car 클래스는") {
+        context("랜덤 움직임 정책의") {
+            context("랜덤으로 만들어진 수가") {
+                val testPosition = Position(xPos = 0)
 
-                        if (i >= 4) {
-                            newPosition.xPos shouldBe testPosition.xPos + 1
-                            testPosition = newPosition
-                        } else {
-                            newPosition.xPos shouldBe testPosition.xPos
-                        }
+                test("0 .. 3인 경우에는 전진하지 않는다.") {
+                    for (i in 0..3) {
+                        val randomMovementPolicy = RandomMovementImpl(FakeRandomGenerator(i))
+                        val newPosition = randomMovementPolicy.move(testPosition)
+                        newPosition.xPos shouldBe testPosition.xPos
+                    }
+                }
+
+                test("4 이상인 경우에는 전진한다.") {
+                    for (i in 4..9) {
+                        val randomMovementPolicy = RandomMovementImpl(FakeRandomGenerator(i))
+                        val newPosition = randomMovementPolicy.move(testPosition)
+                        newPosition.xPos shouldBe testPosition.xPos + 1
                     }
                 }
             }
