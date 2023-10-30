d56652f1216fc5495bc779b5b75ff1f37dc2bf97
Jihoon, Shin <jihoon9030@gmail.com>
2023-10-30 20:29:54 +0900

    fix : 1 이라는 position 을  START_POSITION 으로 지정하여 가독성을 높입니다

--- src/main/kotlin/racing/Car.kt
+++ src/main/kotlin/racing/Car.kt
@@ -2,7 +2,11 @@ package racing
 
 import kotlin.random.Random
 
-class Car(private var position: Int = 1) {
+class Car(private var position: Int = START_POSITION) {
+
+    companion object {
+        const val START_POSITION = 1
+    }
 
     fun moveForward(randomValue: () -> Int = { Random.nextInt(10) }) {
         val forwardSteps = randomValue()
