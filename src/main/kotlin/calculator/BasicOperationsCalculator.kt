6b786c1c3f579f1657ea5876e7396ef7082fb4cd
Jihoon, Shin <jihoon9030@gmail.com>
2023-10-30 18:42:51 +0900

    fix : BasicOperationsCalculator 가 Operator 를 사용하도록 변경합니다

--- src/main/kotlin/calculator/BasicOperationsCalculator.kt
+++ src/main/kotlin/calculator/BasicOperationsCalculator.kt
@@ -7,13 +7,7 @@ class BasicOperationsCalculator {
     private fun divide(a: Double, b: Double): Double = a / b
 
     fun operation(a: Double, b: Double, symbol: String): Double {
-        val operation = Operation.fromSymbol(symbol)
-
-        return when (operation) {
-            Operation.ADD -> add(a, b)
-            Operation.SUBTRACT -> subtract(a, b)
-            Operation.MULTIPLY -> multiply(a, b)
-            Operation.DIVIDE -> divide(a, b)
-        }
+        val operator = Operator.fromSymbol(symbol)
+        return operator.operate(a, b)
     }
 }
