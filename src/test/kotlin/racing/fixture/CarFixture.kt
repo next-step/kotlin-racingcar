877a312a3fa3943ef0ddc7068bf1b5cefc20adb6
dong <dong.22@kakaoenterprise.com>
2023-10-30 15:30:11 +0900

    test : 테스트 픽스쳐 추가

--- src/test/kotlin/racing/fixture/CarFixture.kt
+++ src/test/kotlin/racing/fixture/CarFixture.kt
@@ -15,6 +15,26 @@ class CarFixture {
             )
         )
 
+        val carFixtureStep1: ArrayDeque<Car> = ArrayDeque(
+            listOf(
+                Car("까망베르", 0),
+                Car("적양파", 1),
+                Car("살치살", 0),
+                Car("신선양상추", 0),
+                Car("빨간토마토", 1),
+            )
+        )
+
+        val carFixtureStep2: ArrayDeque<Car> = ArrayDeque(
+            listOf(
+                Car("까망베르", 1),
+                Car("적양파", 2),
+                Car("살치살", 3),
+                Car("신선양상추", 4),
+                Car("빨간토마토", 5),
+            )
+        )
+
         val carFixtureOfAfterFinish: ArrayDeque<Car> = ArrayDeque(
             listOf(
                 Car("까망베르", 2),
