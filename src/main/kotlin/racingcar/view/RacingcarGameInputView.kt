7a711f94d840736702eddab619b66df66460d3b8
yw9594 <kyw9509@gmail.com>
2023-10-30 18:29:35 +0900

    feat(step3): add input value natural number check logic

--- src/main/kotlin/racingcar/view/RacingcarGameInputView.kt
+++ src/main/kotlin/racingcar/view/RacingcarGameInputView.kt
@@ -6,13 +6,13 @@ class RacingcarGameInputView {
     fun getInput(): RacingcarGameInput {
         val carNumber = getInput("자동차 대수는 몇 대인가요?")?.toIntOrNull()
 
-        require(carNumber != null) {
+        require(carNumber != null && carNumber > 0) {
             "자동차 수는 자연수만 가능합니다."
         }
 
         val roundNumber = getInput("시도할 횟수는 몇 회인가요?")?.toIntOrNull()
 
-        require(roundNumber != null) {
+        require(roundNumber != null && roundNumber > 0) {
             "시도 횟수는 자연수만 가능합니다."
         }
 
