7f56cb07baccb91b929fce5572eacb27d0c4d8ed
Jihoon, Shin <jihoon9030@gmail.com>
2023-10-30 22:01:58 +0900

    test : Cars 객체에서 winner 를 찾는 테스트 코드를 작성합니다

--- src/test/kotlin/racing/CarsTest.kt
+++ src/test/kotlin/racing/CarsTest.kt
@@ -4,11 +4,11 @@ import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 
 class CarsTest : BehaviorSpec ({
-    given("Cars 객체는"){
+    given("Cars 객체는") {
         val cars = Cars(3)
-        `when`("4 미만의 일정한 값이 주어졌을 때"){
+        `when`("4 미만의 일정한 값이 주어졌을 때") {
             cars.moveAll { 3 }
-            then("모두 함께 정지한다."){
+            then("모두 함께 정지한다.") {
                 cars.carList.forEach { car ->
                     car.currentPosition shouldBe 1
                 }
@@ -23,6 +23,21 @@ class CarsTest : BehaviorSpec ({
                 }
             }
         }
+    }
+
+    given("Car의 모임인 Cars 객체에서") {
+        val cars = Cars(
+            listOf(Car(5), Car(4), Car(3), Car(5))
+        )
+        `when`("가장 멀리 가 있는 자동차는") {
+            val winner: List<Car> = cars.getWinner()
+            then("position 이 5인 객체들이다.") {
+                winner.forEach { car ->
+                    car.currentPosition shouldBe 5
+                }
+            }
+        }
+
     }
 
 })
