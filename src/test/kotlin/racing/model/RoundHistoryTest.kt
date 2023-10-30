b4affd665cfb5062f067576b111a9d55c64eda3e
dong <dong.22@kakaoenterprise.com>
2023-10-30 23:48:39 +0900

    test : 테스트코드 구현

--- src/test/kotlin/racing/model/RoundHistoryTest.kt
+++ src/test/kotlin/racing/model/RoundHistoryTest.kt
@@ -1,10 +1,19 @@
 package racing.model
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 
 class RoundHistoryTest : StringSpec({
 
     "자동차 이름은 중복되선 안된다, 중복시 exception throw" {
-        TODO()
+        val results = ArrayDeque(
+            listOf(
+                Car("Honux", 1),
+                Car("Honux", 2)
+            )
+        )
+        shouldThrow<IllegalArgumentException> {
+            RoundHistory(results)
+        }
     }
 })
