7da4e0cf7e1b8602ef868d6374b515f8a2bce3a4
kimueibin <at365@naver.com>
2023-10-31 14:29:39 +0900

    refactor : 자동차 객체 ( Car ) 내 기본 property 와 move 함수 수정
    기본 프로퍼티 제거와 이동 전략 함수 수정, display 함수 제거

--- src/main/kotlin/camp/nextstep/edu/step/racing/domain/Car.kt
+++ src/main/kotlin/camp/nextstep/edu/step/racing/domain/Car.kt
@@ -1,21 +1,21 @@
 package camp.nextstep.edu.step.racing.domain
 
+import camp.nextstep.edu.step.racing.strategy.CarMoveStrategy
+
 data class Car(
-    val carName: String = "",
+    val carName: String,
     var position: Int = 0
 ) {
     init {
         require(carName.isNotBlank()) { "자동차의 이름이 비어있습니다." }
     }
 
-    fun moveForward(isMove: Boolean) {
-        if (isMove) {
+    fun moveForward(carMoveStrategy: CarMoveStrategy) {
+        val carMoveStatus = carMoveStrategy.isMovable()
+
+        if (carMoveStatus) {
             this.position += 1
         }
-    }
-
-    fun displayCarsNameAndPosition() {
-        println("${this.carName} : ${"-".repeat(this.position)}")
     }
 
 }
