ee2181a1bd6e52a06137cef84aaada16428e0ca9
dokdogalmaegi <dokdogalmaegi@gmail.com>
2023-10-31 15:25:03 +0900

    Remove & Add: 함수 변경으로 인해 테스트 삭제, start 함수 생성

--- src/main/kotlin/carRacing/domain/Racing.kt
+++ src/main/kotlin/carRacing/domain/Racing.kt
@@ -3,13 +3,23 @@ package carRacing.domain
 import carRacing.serviceimpl.SimpleCarController
 
 class Racing {
-    fun process(carCount: Int): List<Car> {
-        val carList: List<Car> = SimpleCarController().createCars(carCount)
+    fun start(carCount: Int, tryCount: Int): List<Car> {
+        var carList: List<Car> = SimpleCarController().createCars(carCount)
 
-        for (car in carList) {
-            car.move()
+        for (i in 0 until tryCount) {
+            carList = process(carList)
         }
 
         return carList
+    }
+
+    private fun process(carList: List<Car>): List<Car> {
+        val copyCarList = carList.toList()
+
+        for (car in copyCarList) {
+            car.move()
+        }
+
+        return copyCarList
     }
 }
