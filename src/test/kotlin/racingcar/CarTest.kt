b9aa0d24770cd51aa43403108c5ac4aee177cb2d
Jeong Myeonggu <audrn6689@gmail.com>
2023-10-31 18:30:51 +0900

    step4 - Car Test 코드 추가 및 수정

--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -1,5 +1,6 @@
 package racingcar
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
@@ -16,20 +17,32 @@ class CarTest : StringSpec({
     }
 
     "전진하는 조건을 만족하면 자동차는 전진한다. " {
-        val car = Car.create()
+        val carName = "정명구"
+        val car = Car.create(carName)
         every { Random.nextInt(Car.getRandomRange()) } returns Car.getMoveThreshold()
 
         car.move()
 
-        car.display() shouldBe "-"
+        car.display() shouldBe "$carName : -"
     }
 
     "전진하는 조건을 만족하지 못하면 자동차는 전진하지 않는다" {
-        val car = Car.create()
+        val carName = "정명구"
+        val car = Car.create(carName)
         every { Random.nextInt(Car.getRandomRange()) } returns Car.getMoveThreshold() - 1
 
         car.move()
 
-        car.display() shouldBe ""
+        car.display() shouldBe "$carName : "
+    }
+
+    "이름의 길이가 지정한 범위 보다 크면 예외가 발생한다." {
+        val carName = "정명구정명구"
+        val nameRange = Car.getNameRange()
+
+        nameRange shouldBe 5
+        shouldThrow<IllegalArgumentException> {
+            Car.create(carName)
+        }
     }
 })
