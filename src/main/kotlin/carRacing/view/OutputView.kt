e0866ddb089b39a457dcfd17360b802c9f64e32a
dokdogalmaegi <dokdogalmaegi@gmail.com>
2023-10-31 15:13:34 +0900

    Implement: printMessageInBox 함수 구현

--- src/main/kotlin/carRacing/view/OutputView.kt
+++ src/main/kotlin/carRacing/view/OutputView.kt
@@ -1,3 +1,17 @@
 package carRacing.view
 
-class OutputView
+class OutputView {
+    fun printMessageInBox(message: String) {
+        println("====================================")
+        println(message)
+        println("====================================")
+    }
+
+    fun printMessagesInBox(vararg messages: String) {
+        println("====================================")
+        for (message in messages) {
+            println(message)
+        }
+        println("====================================")
+    }
+}
