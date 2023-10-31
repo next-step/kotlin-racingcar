2442c2c042ae0445523c8fd54c0a2350dbf90586
dokdogalmaegi <dokdogalmaegi@gmail.com>
2023-10-31 14:34:24 +0900

    Refac & Add: 중복 로직 통합, Message Enum 추가

--- src/main/kotlin/carRacing/domain/MessageGenerator.kt
+++ src/main/kotlin/carRacing/domain/MessageGenerator.kt
@@ -1,23 +1,28 @@
 package carRacing.domain
 
 import carRacing.enum.ErrorCode
+import carRacing.enum.Message
 
 val VARIABLE_PATTERN: Regex = Regex("\\[\\d+\\]")
 
 class MessageGenerator {
-    fun getErrorMessage(errorCode: ErrorCode, vararg variables: String): String {
-        var message = errorCode.message
+    fun getErrorMessage(errorCode: ErrorCode, vararg variables: String): String = formatMessage(errorCode.message, *variables)
+
+    fun getMessage(message: Message, vararg variables: String): String = formatMessage(message.message, *variables)
+
+    private fun getVariableCount(message: String): Int = VARIABLE_PATTERN.findAll(message).count()
+
+    private fun formatMessage(message: String, vararg variables: String): String {
+        var formattedMessage = message
 
         if (getVariableCount(message) != variables.size) {
             throw IllegalArgumentException(getErrorMessage(ErrorCode.MISMATCHED_VARIABLE_COUNT))
         }
 
         for ((idx, variable) in variables.withIndex()) {
-            message = message.replace("[$idx]", variable)
+            formattedMessage = formattedMessage.replace("[$idx]", variable)
         }
 
-        return message
+        return formattedMessage
     }
-
-    private fun getVariableCount(message: String): Int = VARIABLE_PATTERN.findAll(message).count()
 }
