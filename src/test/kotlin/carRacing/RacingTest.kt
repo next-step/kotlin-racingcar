f760ed0a5863b05202a1eb2485d43171cc7ea192
dokdogalmaegi <clik202012@gmail.com>
2023-10-31 18:29:28 +0900

    Modify: Racing 테스트 이름 변경

--- src/test/kotlin/carRacing/RacingTest.kt
+++ src/test/kotlin/carRacing/RacingTest.kt
@@ -4,6 +4,7 @@ import carRacing.domain.Car
 import carRacing.domain.Racing
 import carRacing.testcontroller.TestCarController
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
@@ -11,7 +12,7 @@ class RacingTest {
 
     @ParameterizedTest
     @CsvSource("2, 1", "2, 2", "2, 3")
-    fun `Racing 결과를 반환`(carCount: Int, tryCount: Int) {
+    fun `Racing 결과에서 반환하는 자동차 List의 길이는 carCount와 동일하다`(carCount: Int, tryCount: Int) {
         val moveRacing = Racing(TestCarController())
         val result: List<Car> = moveRacing.getRaceResult(carCount, tryCount)
 
