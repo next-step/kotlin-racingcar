22fcebdf192397b70ea565dd13cfa421eb996529
Jihoon, Shin <jihoon9030@gmail.com>
2023-10-31 16:33:35 +0900

    test-fix : csvSource 를 이용하여 test-code 의 분기를 없앱니다

--- src/test/kotlin/calculator/BasicOperationsCalculatorTest.kt
+++ src/test/kotlin/calculator/BasicOperationsCalculatorTest.kt
@@ -1,33 +1,19 @@
 package calculator
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
+import org.junit.jupiter.params.provider.CsvSource
 
 class BasicOperationsCalculatorTest {
 
     @ParameterizedTest()
-    @ValueSource(strings = ["+", "-", "*", "/"])
-    fun operation(symbol: String) {
-        // given
-        val firstNum: Double = 10.0
-        val secondNum: Double = 5.0
-
-        // when
+    @CsvSource("10.0,5.0,+,15.0", "10.0,5.0,-,5.0",  "10.0,5.0,*,50.0",  "10.0,5.0,/,2.0")
+    fun operation(firstNum: Double, secondNum: Double, symbol: String, expected: Double) {
+        // given when
         val basicOperationsCalculator = BasicOperationsCalculator()
         val result = basicOperationsCalculator.operation(firstNum, secondNum, symbol)
 
         // then
-        assertAll(
-            {
-                when (symbol) {
-                    "+" -> assertThat(result).isEqualTo(15.0)
-                    "-" -> assertThat(result).isEqualTo(5.0)
-                    "*" -> assertThat(result).isEqualTo(50.0)
-                    "/" -> assertThat(result).isEqualTo(2.0)
-                }
-            }
-        )
+        assertThat(result).isEqualTo(expected)
     }
 }
