f659fd363194ad6575524b4eaed3ad28967956f2
didrlgus <rlgusdid@naver.com>
2023-11-01 18:34:10 +0900

    feat: 사칙연산 계산기 구현

---
+++ src/main/kotlin/calculator/Calculator.kt
@@ -0,0 +1,27 @@
+package calculator
+
+class Calculator(
+    private val number1: Int,
+    private val operator: Char,
+    private val number2: Int,
+) {
+
+    fun calculate(): Int {
+        return when (operator) {
+            '+' -> {
+                number1 + number2
+            }
+            '-' -> {
+                number1 - number2
+            }
+            '*' -> {
+                number1 * number2
+            }
+            '/' -> {
+                require(number2 != 0) { "0으로 나눌 수 없습니다." }
+                number1 / number2
+            }
+            else -> throw IllegalArgumentException("올바르지 않은 연산자 입니다.")
+        }
+    }
+}
