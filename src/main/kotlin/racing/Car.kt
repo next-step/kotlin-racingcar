6ac808a6bedcf1c9ca88def532ec8eff0b9ed1c5
inhyeokk <rkddlsgur983@gmail.com>
2023-11-01 21:40:47 +0900

    step5: 상수 적용

--- src/main/kotlin/racing/Car.kt
+++ src/main/kotlin/racing/Car.kt
@@ -1,12 +1,13 @@
 package racing
 
+private const val MAXIMUM_NAME_LENGTH = 5
 private const val DEFAULT_POSITION = 1
 private const val MOVABLE_MINIMUM = 4
 private const val MOVING_DISTANCE = 1
 
 data class Car(val name: String, private var _position: Int = DEFAULT_POSITION) {
     init {
-        require(name.length <= 5) {
+        require(name.length <= MAXIMUM_NAME_LENGTH) {
             "자동차 이름은 5자를 초과할 수 없습니다."
         }
     }
