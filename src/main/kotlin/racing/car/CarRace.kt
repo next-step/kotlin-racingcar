2e1f8e764fe73e5d006886604675e6a3d363d76d
Jihoon, Shin <jihoon9030@gmail.com>
2023-11-01 19:58:58 +0900

    fix : repeat 을 활용하여 가독성을 높입니다

--- src/main/kotlin/racing/car/CarRace.kt
+++ src/main/kotlin/racing/car/CarRace.kt
@@ -5,7 +5,7 @@ import racing.ui.ResultViewAction
 
 class CarRace {
     fun randomMove(cars: Cars, tryNumber: Int, resultViewAction: ResultViewAction) {
-        for (i in 1..tryNumber) {
+        repeat(tryNumber) {
             cars.moveAll(RandomMoveProvider())
             resultViewAction(cars)
         }
