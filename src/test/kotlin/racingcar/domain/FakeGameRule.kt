02fce44ba78f7d71acd0219d298da785baacde6c
wooddy.kim <wooddy.kim@kakaocorp.com>
2023-11-02 00:43:39 +0900

    test: 플레이어 수, 게임룰에 따른 테스트 케이스 추가

--- src/test/kotlin/racingcar/domain/FakeGameRule.kt
+++ src/test/kotlin/racingcar/domain/FakeGameRule.kt
@@ -1,5 +1,15 @@
 package racingcar.domain
 
-class FakeGameRule(private val direction: MovingDirection) : GameRule {
+class FixedGameRule(private val direction: MovingDirection) : GameRule {
     override fun getMovingDirection(): MovingDirection = this.direction
+}
+
+class ConditionGameRule(private val predicate: (count: Int) -> Boolean) : GameRule {
+
+    var count = 0;
+
+    override fun getMovingDirection(): MovingDirection {
+        count += 1
+        return if (predicate(count)) MovingDirection.FORWARD else MovingDirection.STOP
+    }
 }
--- src/test/kotlin/racingcar/domain/RacingCarTest.kt
+++ src/test/kotlin/racingcar/domain/RacingCarTest.kt
@@ -7,8 +7,7 @@ import racingcar.test.RacingCarGenerator
 class RacingCarTest : ExpectSpec({
 
     context("자동차에게 FORWARD 를 반환하는 게임룰을 설정하면") {
-        val gameRule = FakeGameRule(MovingDirection.FORWARD)
-        val racingCar = RacingCarGenerator.create(rule = gameRule)
+        val racingCar = RacingCarGenerator.create(direction = MovingDirection.FORWARD)
 
         expect("자동차의 포지션은 1 증가한다.") {
             racingCar.move()
@@ -17,8 +16,7 @@ class RacingCarTest : ExpectSpec({
     }
 
     context("자동차에게 STOP 을 반환하는 게임룰을 설정하면") {
-        val gameRule = FakeGameRule(MovingDirection.STOP)
-        val racingCar = RacingCarGenerator.create(rule = gameRule)
+        val racingCar = RacingCarGenerator.create(direction = MovingDirection.STOP)
 
         expect("자동차의 포지션은 증가하지 않는다.") {
             racingCar.move()
