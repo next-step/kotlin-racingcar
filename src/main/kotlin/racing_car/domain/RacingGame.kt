bbe4631a9ebd76b29a6e0aa9f4060c7e4cf3a7b6
khcho902 <khcho902@gmail.com>
2023-11-02 00:04:37 +0900

    feat: RacingGame 클래스 생성시 유효하지 않은 입력값 에러처리

--- src/main/kotlin/racing_car/domain/RacingGame.kt
+++ src/main/kotlin/racing_car/domain/RacingGame.kt
@@ -6,7 +6,13 @@ class RacingGame(
 ) {
     private val racingRound: Int = racingRound
     private var currentRacingRound: Int = 0
-    val cars: List<Car> = List(numberOfCars) { Car() }
+    val cars: List<Car>
+
+    init {
+        require(numberOfCars >= 2) { "경주에 필요한 자동차 대수는 2대 이상입니다." }
+        require(racingRound >= 1) { "경주는 1번 이상 시도되어야합니다." }
+        cars = List(numberOfCars) { Car() }
+    }
 
     val isContinuable: Boolean
         get() = currentRacingRound < racingRound
