b4d65cd9b029f124a3f4724218416d1b4505d250
bsgreentea <bsgreentea1@gmail.com>
2023-11-01 09:46:25 +0900

    refactor: 실행 순서 수정해라
    - 문구 출력 순서 변경

--- src/main/kotlin/week1/racingcar/InputView.kt
+++ src/main/kotlin/week1/racingcar/InputView.kt
@@ -4,8 +4,8 @@ class InputView {
     fun startInput(): InputString {
         println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
         val carListInput = readlnOrNull()
-        val timesInput = readlnOrNull()
         println("시도할 횟수는 몇 회인가요?")
+        val timesInput = readlnOrNull()
         return InputString(carListInput, timesInput)
     }
 
