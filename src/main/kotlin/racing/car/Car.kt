072a654abd01e377ffeb6a20f325fcbec242f298
Jihoon, Shin <jihoon9030@gmail.com>
2023-11-01 12:59:24 +0900

    fix : 앞으로 나아가기 위한 임계값을 상수로 분리합니다

--- src/main/kotlin/racing/car/Car.kt
+++ src/main/kotlin/racing/car/Car.kt
@@ -4,12 +4,13 @@ class Car(position: Int = START_POSITION, val name: String = "kotlin") {
     var position: Int = position
         private set
     fun moveForward(forwardSteps: Int) {
-        if (forwardSteps >= 4) {
+        if (forwardSteps >= FORWARD_THRESHOLD) {
             this.position += 1
         }
     }
 
     companion object {
-        const val START_POSITION = 1
+        private const val START_POSITION = 1
+        private const val FORWARD_THRESHOLD = 4
     }
 }
