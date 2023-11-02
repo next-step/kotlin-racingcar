5e77a5b9fefc70eb20d3ce90b0d817b7850d1f43
baek0318 <peachberry0318@gmail.com>
2023-11-02 22:37:24 +0900

    step5 : Result를 사라지게하고 OutPutView로 로직을 이동 - 특정 View를 위한 로직을 지우고 OutPutView가 알아서 파싱하도록 변경

--- src/main/kotlin/racinggame/domain/RacingGameResult.kt
+++ src/main/kotlin/racinggame/domain/RacingGameResult.kt
@@ -1,35 +1,11 @@
 package racinggame.domain
 
 data class RacingGameResult(
+    val winners: List<Car>,
     val racingResult: List<RoundResult>
-) : Result<List<List<String>>> {
-    override fun translate() = racingResult.map { round ->
-        round.translate()
-    }
-}
+)
 
 data class RoundResult(
     val result: List<Car>
-) : Result<List<String>> {
-    override fun translate() = result.map { car ->
-        (0..car.position).map {
-            "-"
-        }.reduce { s1, s2 ->
-            s1 + s2
-        }.let {
-            "${car.name} : $it"
-        }
-    }
-}
+)
 
-data class PlayWinners(
-    val winners: List<Car>
-) : Result<String> {
-    override fun translate() = winners.joinToString(", ") { car ->
-        car.name
-    }
-}
-
-interface Result<T> {
-    fun translate(): T
-}
--- src/main/kotlin/racinggame/view/OutputView.kt
+++ src/main/kotlin/racinggame/view/OutputView.kt
@@ -1,21 +1,30 @@
 package racinggame.view
 
-import racinggame.domain.PlayWinners
-import racinggame.domain.RacingGameResult
+import racinggame.domain.Car
+import racinggame.domain.RoundResult
 
 object OutputView {
 
-    fun printRacingResult(racingResult: RacingGameResult) {
+    fun printRacingResult(racingResult: List<RoundResult>) {
         println("실행 결과")
-        racingResult.translate().forEach { result ->
-            result.forEach {
-                println(it)
+        racingResult.map { round ->
+            round.result.map { car ->
+                (0..car.position).map {
+                    "-"
+                }.reduce { s1, s2 ->
+                    s1 + s2
+                }.let {
+                    println("${car.name} : $it")
+                }
             }
             println()
         }
     }
 
-    fun printWinners(winners: PlayWinners) {
-        println("${winners.translate()} 가 최종 우승했습니다.")
-    }
+    fun printWinners(winners: List<Car>) =
+        winners.joinToString(", ") { car ->
+            car.name
+        }.let {
+            println("$it 가 최종 우승했습니다.")
+        }
 }
