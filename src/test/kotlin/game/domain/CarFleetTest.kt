06745460a82e81ac11ed9bd599c2637ec8946821
ALbertIM0427 <guang7226961@naver.com>
2023-11-02 20:07:52 +0900

    test: 부족한 테스트 구현

--- src/test/kotlin/game/domain/CarFleetTest.kt
+++ src/test/kotlin/game/domain/CarFleetTest.kt
@@ -1,3 +1,17 @@
 package game.domain
 
-class CarFleetTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class CarFleetTest {
+
+    @Test
+    fun `포함되여 있는 모든 자동차가 한번씩 조건에 따라 움직인다`() {
+        // given
+        val carFleet = CarFleet(Cars(Car.from(listOf("a", "b", "c"))), FixedMoveConditionGenerator(5))
+        // when
+        val advancedCarFleet = carFleet.advanceAll()
+        // then
+        assertThat(advancedCarFleet.cars.carList.all { it.position == 1 }).isTrue()
+    }
+}
---
+++ src/test/kotlin/game/domain/GameControllerTest.kt
@@ -0,0 +1,26 @@
+package game.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+
+class GameControllerTest {
+
+    @Test
+    fun `시도 횟수 만큼 CarFleet를 전진시키고 횟수만큼 history에 저장한다`() {
+        // given
+        val carFleet = CarFleet(Cars(Car.from(listOf("a", "b", "c"))), FixedMoveConditionGenerator(5))
+        val gameController = GameController()
+
+        // when
+        val history = gameController.playGame(3, carFleet)
+
+        // then
+        assertAll(
+            { assertThat(history.rounds.size).isEqualTo(3) },
+            { assertThat(history.rounds[0].all { it.position == 1 }).isTrue() },
+            { assertThat(history.rounds[1].all { it.position == 2 }).isTrue() },
+            { assertThat(history.rounds[2].all { it.position == 3 }).isTrue() },
+        )
+    }
+}
