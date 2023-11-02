f208d62f1ad183e1d41860322246ea185776d151
dong <dong.22@kakaoenterprise.com>
2023-11-02 17:10:14 +0900

    refactor : 오브젝트 클래스로 리팩토링

--- src/test/kotlin/racing/fixture/CarFixture.kt
+++ src/test/kotlin/racing/fixture/CarFixture.kt
@@ -2,47 +2,45 @@ package racing.fixture
 
 import racing.model.Car
 
-class CarFixture {
-    companion object {
+object CarFixture {
 
-        val carFixtureOfBeforeStart: ArrayDeque<String> = ArrayDeque(
-            listOf(
-                "치즈",
-                "양파",
-                "소고기",
-                "양상추",
-                "토마토",
-            )
+    val carFixtureOfBeforeStart: ArrayDeque<String> = ArrayDeque(
+        listOf(
+            "치즈",
+            "양파",
+            "소고기",
+            "양상추",
+            "토마토",
         )
+    )
 
-        val carFixtureStep1: ArrayDeque<Car> = ArrayDeque(
-            listOf(
-                Car("치즈", 0),
-                Car("양파", 1),
-                Car("소고기", 0),
-                Car("신선양상추", 0),
-                Car("빨간토마토", 1),
-            )
+    val carFixtureStep1: ArrayDeque<Car> = ArrayDeque(
+        listOf(
+            Car("치즈", 0),
+            Car("양파", 1),
+            Car("소고기", 0),
+            Car("신선양상추", 0),
+            Car("빨간토마토", 1),
         )
+    )
 
-        val carFixtureStep2: ArrayDeque<Car> = ArrayDeque(
-            listOf(
-                Car("치즈", 1),
-                Car("양파", 2),
-                Car("소고기", 3),
-                Car("신선양상추", 4),
-                Car("빨간토마토", 5),
-            )
+    val carFixtureStep2: ArrayDeque<Car> = ArrayDeque(
+        listOf(
+            Car("치즈", 1),
+            Car("양파", 2),
+            Car("소고기", 3),
+            Car("신선양상추", 4),
+            Car("빨간토마토", 5),
         )
+    )
 
-        val carFixtureOfAfterFinish: ArrayDeque<Car> = ArrayDeque(
-            listOf(
-                Car("치즈", 2),
-                Car("양파", 4),
-                Car("소고기", 6),
-                Car("신선양상추", 8),
-                Car("빨간토마토", 10),
-            )
+    val carFixtureOfAfterFinish: ArrayDeque<Car> = ArrayDeque(
+        listOf(
+            Car("치즈", 2),
+            Car("양파", 4),
+            Car("소고기", 6),
+            Car("신선양상추", 8),
+            Car("빨간토마토", 10),
         )
-    }
+    )
 }
