eaaa667e43aa88db34c7e5635efb18c5f8c89638
jiwon <ljwon77@daum.net>
2023-11-03 01:33:25 +0900

    test: 자동차에 대한 테스트 픽스처 생성 및 불필요한 테스트 제거

--- src/test/kotlin/racingCar/helper/CarFixture.kt
+++ src/test/kotlin/racingCar/helper/CarFixture.kt
@@ -2,9 +2,13 @@ package racingCar.helper
 
 import racingCar.domain.Car
 import racingCar.domain.CarName
+import racingCar.domain.CarPosition
 import racingCar.util.NumberGenerator
 
 object CarFixture {
+
+    fun makeCar(name: String, position: Int): Car =
+        Car(name = CarName(name), position = CarPosition(position))
 
     fun makeCars(carCount: Int) = List(carCount) {
         Car(name = CarName("테스트$it"))
