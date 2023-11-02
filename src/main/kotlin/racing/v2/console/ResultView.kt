35089ca5623b908c0962869352c48f4778a3c1bf
ALEX <kpmyung@gmail.com>
2023-11-02 15:35:33 +0900

    feat: 게임 완료 시 우승자를 출력하는 기능을 추가한다
    Signed-off-by: ALEX <kpmyung@gmail.com>

--- src/main/kotlin/racing/v2/console/ResultView.kt
+++ src/main/kotlin/racing/v2/console/ResultView.kt
@@ -3,11 +3,21 @@ package racing.v2.console
 import racing.v2.domain.Car
 
 object ResultView {
+    const val RESULT_PRINT_START_MESSAGE = "실행 결과"
+
+    fun printResultStart() {
+        println(RESULT_PRINT_START_MESSAGE)
+    }
+    
     fun printCarPosition(cars: List<Car>) {
-        println("실행 결과")
         cars.forEach { car ->
             println("${car.name} : ${"-".repeat(car.position)}")
         }
         println()
+    }
+
+    fun printWinner(winners: List<Car>) {
+        val winnerNames = winners.joinToString(", ") { it.name }
+        println(winnerNames + "이(가) 최종 우승했습니다.")
     }
 }
--- src/main/kotlin/racing/v2/domain/CarGame.kt
+++ src/main/kotlin/racing/v2/domain/CarGame.kt
@@ -5,15 +5,24 @@ import kotlin.random.Random
 
 class CarGame(val cars: List<Car>, val tryCount: Int) {
     fun start() {
+        ResultView.printResultStart()
+        
         repeat(tryCount) {
             moveAllCarsOnce()
             ResultView.printCarPosition(cars)
         }
+
+        ResultView.printWinner(findWinners())
     }
 
     private fun moveAllCarsOnce() {
         cars.forEach {
             it.moveForward(CarDrivingPolicy, Random.nextInt(10))
         }
+    }
+
+    private fun findWinners(): List<Car> {
+        val maxPosition = cars.maxOf { it.position }
+        return cars.filter { it.position == maxPosition }
     }
 }
