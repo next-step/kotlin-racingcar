90e072faf87aa04e89aad6e06c8c348c2ee867e6
didrlgus <rlgusdid@naver.com>
2023-11-02 12:10:00 +0900

    feat: CalculatorController 추가

---
+++ src/main/kotlin/calculator/controller/CalculatorController.kt
@@ -0,0 +1,44 @@
+package calculator.controller
+
+import calculator.domain.Calculator
+
+class CalculatorController(
+    private val words: List<String>,
+) {
+
+    fun getResult(): Int {
+        var result = getFirstOperand()
+
+        for (i in 1 until words.size step 2) {
+            val operator = getOperator(i)
+            val operand = getNextOperand(i)
+            result = Calculator(result, operator, operand).calculate()
+        }
+
+        return result
+    }
+
+    private fun getFirstOperand(): Int {
+        try {
+            return Integer.parseInt(words[0])
+        } catch (e: Exception) {
+            throw IllegalArgumentException("피연산자는 숫자만 가능합니다.")
+        }
+    }
+
+    private fun getOperator(i: Int): Char {
+        try {
+            return words[i].single()
+        } catch (e: Exception) {
+            throw IllegalArgumentException("숫자와 연산자 사이에는 공백이 있어야 합니다.")
+        }
+    }
+
+    private fun getNextOperand(i: Int): Int {
+        try {
+            return Integer.parseInt(words[i + 1])
+        } catch (e: Exception) {
+            throw IllegalArgumentException("반드시 연산하고자 하는 숫자를 입력해야 합니다.")
+        }
+    }
+}
