30adac84c1fbdc02195cef9a764f56b3c80069b4
1stpie <1stpie@naver.com>
2023-11-02 19:23:52 +0900

    test(core): change test to ParameterizedTest

--- src/test/kotlin/study/racingcar/CarTest.kt
+++ src/test/kotlin/study/racingcar/CarTest.kt
@@ -1,23 +1,25 @@
 package study.racingcar
 
 import org.assertj.core.api.Java6Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class CarTest {
-    @Test
-    fun `car moves if random value is 4 or more`() {
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            "0, 0",
+            "1, 0",
+            "2, 0",
+            "3, 0",
+            "4, 1",
+            "5, 1",
+            "6, 1",
+        ],
+    )
+    fun `car moves if random value is 4 or more`(randomValue: Int, expectedPosition: Int) {
         val car = Car()
-        var movedCar = car.move(4)
-        assertThat(movedCar.position).isEqualTo(1)
-
-        movedCar = movedCar.move(5)
-        assertThat(movedCar.position).isEqualTo(2)
-    }
-
-    @Test
-    fun `car stays if random value is less than 4`() {
-        val car = Car()
-        val movedCar = car.move(3)
-        assertThat(movedCar.position).isEqualTo(0)
+        val movedCar = car.move(randomValue)
+        assertThat(movedCar.position).isEqualTo(expectedPosition)
     }
 }
--- src/test/kotlin/study/racingcar/RaceTest.kt
+++ src/test/kotlin/study/racingcar/RaceTest.kt
@@ -1,15 +1,26 @@
 package study.racingcar
 
 import org.assertj.core.api.Java6Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class RaceTest {
 
-    @Test
-    fun `race runs for given rounds`() {
-        val random = 5
-        val race = Race(3, 5) { random }
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            "3, 3, 0",
+            "3, 6, 0",
+            "3, 9, 0",
+            "4, 3, 3",
+            "4, 6, 6",
+            "4, 9, 9",
+            "6, 100, 100",
+        ],
+    )
+    fun `race runs for given rounds`(randomValue: Int, rounds: Int, expectedPosition: Int) {
+        val race = Race(3, rounds) { randomValue }
         race.run {}
-        assertThat(race.cars.list.all { it.position == 5 }).isTrue
+        assertThat(race.cars.list.all { it.position == expectedPosition }).isTrue
     }
 }
