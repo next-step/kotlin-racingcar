2251cb65f697b3a8f6976ac63af3ab66dae49ee4
jaylene shin <jaylene.shin@kakaocorp.com>
2023-11-02 20:00:56 +0900

    1. UserInput 객체 생성시 유효성 검사를 함께 수행한다. 2. 유저 입력에 대한 유효성을 검증하는 코드를 작성한다.

--- src/main/kotlin/racingCarWinner/domain/UserInput.kt
+++ src/main/kotlin/racingCarWinner/domain/UserInput.kt
@@ -3,4 +3,13 @@ package racingCarWinner.domain
 data class UserInput(
     val carNames: List<String>,
     val numOfAttempts: Int,
-)
+) {
+    init {
+        require(carNames.map { it.trim().length }.all { it in 1..5 }) {
+            "자동차 이름은 1~5자만 가능합니다."
+        }
+        require(numOfAttempts > 0) {
+            "시도 횟수는 1 이상만 가능합니다."
+        }
+    }
+}
