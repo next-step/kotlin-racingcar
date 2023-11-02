7d5ce865a78b93b804578bb250ce29429af83532
1stpie <1stpie@naver.com>
2023-11-02 18:49:23 +0900

    refactor(core): remove unnecessary backing property

--- src/main/kotlin/study/racingcar/Cars.kt
+++ src/main/kotlin/study/racingcar/Cars.kt
@@ -2,15 +2,13 @@ package study.racingcar
 
 data class Cars(private val cars: List<Car>) {
 
-    private val _cars: List<Car> = cars.toList()
-
-    val list: List<Car> get() = _cars.toList()
+    val list: List<Car> get() = cars.toList()
 
     fun moveAll(randomValueGenerator: () -> Int): Cars {
         return Cars(
             cars.map { car ->
                 Car(car.position).apply { move(randomValueGenerator()) }
-            }
+            },
         )
     }
 
