5da44c7e8dfb2e507bebf14014afc33aa2eb0817
moses.lee(이상후)/kakao <moses.lee@kakaocorp.com>
2023-11-02 21:47:14 +0900

    fix: mock 제거
    - reader 인터페이스 구현한 MockReader
    
    # step5

--- src/test/kotlin/study/racingcar/application/InputViewTest.kt
+++ src/test/kotlin/study/racingcar/application/InputViewTest.kt
@@ -1,26 +1,30 @@
 package study.racingcar.application
 
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
-import org.mockito.kotlin.mock
-import org.mockito.kotlin.whenever
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class InputViewTest {
 
     // TODO-review : 외부의존성인 Input 에 대해서 테스트 코드를 작성하기 위해 사용한 방식이 적합한지 코멘트 부탁드립니다!
-    //  - Reader 인터페이스를 주입
-    //  - mock 사용하는 방식
-    @Test
-    @DisplayName("stdin reader 를 사용했을때, 입력값을 받아올 수 있는지 확인")
-    fun getInput() {
+    //  - MockReader 클래스 구현
+    //  - null & empty 는 fun inputString() 에서 검증하고 있기 때문에, 테스트 코드 작성 필요 x
+
+    class MockReader(private val input: String) : Reader {
+        override fun read(): String? {
+            return input
+        }
+    }
+
+    @ValueSource(strings = ["123", "456", "789"])
+    @ParameterizedTest
+    fun `mock reader 를 사용했을때, 입력값을 받아올 수 있는지 확인`(input: String) {
         // given
-        val reader = mock<Reader>()
+        val reader = MockReader(input)
 
         // when
-        whenever(reader.read()).thenReturn("123")
         val result: Int = InputView(reader).inputNumber("Enter a number: ")
 
         // then
-        assert(result == 123)
+        assert(result.toString() == input)
     }
 }
