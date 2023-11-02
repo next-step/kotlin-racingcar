da960c704aefd6932b0ecf0347a5f8db0bf8d616
1stpie <1stpie@naver.com>
2023-11-02 19:02:36 +0900

    refactor(core): change const number to private const value

--- src/main/kotlin/study/racingcar/Car.kt
+++ src/main/kotlin/study/racingcar/Car.kt
@@ -2,10 +2,14 @@ package study.racingcar
 
 data class Car(val position: Int = 0) {
     fun move(randomValue: Int): Car {
-        return if (randomValue >= 4) {
+        return if (randomValue >= MOVABLE_BASE_NUMBER) {
             copy(position = position + 1)
         } else {
             this
         }
+    }
+
+    private companion object {
+        const val MOVABLE_BASE_NUMBER = 4
     }
 }
