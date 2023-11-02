77a8888440c0b1a765a68d44840bb9d4d33ac3ca
1stpie <1stpie@naver.com>
2023-11-02 19:14:23 +0900

    test(core): add Cars moveAll function Test

---
+++ src/test/kotlin/study/racingcar/CarsTest.kt
@@ -0,0 +1,28 @@
+package study.racingcar
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class CarsTest {
+
+    @Test
+    fun `moveAll should move all cars based on the provided random values`() {
+        val initialCars = listOf(
+            Car(),
+            Car(),
+        )
+
+        val cars = Cars(initialCars)
+        val randomValueGenerator = { 5 }
+
+        val movedCars = cars.moveAll(randomValueGenerator)
+
+        val expectedPositions = listOf(
+            initialCars[0].move(randomValueGenerator()),
+            initialCars[1].move(randomValueGenerator()),
+        ).map { it.position }
+        val actualPositions = movedCars.list.map { it.position }
+
+        assertEquals(expectedPositions, actualPositions, "Cars should move based on the provided random values")
+    }
+}
