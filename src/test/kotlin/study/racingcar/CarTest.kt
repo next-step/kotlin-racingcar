f78ab8ac898e515c8cfc014b2fd87c143eba40c0
1stpie <1stpie@naver.com>
2023-11-02 19:10:50 +0900

    test(core): change car move function test

--- src/test/kotlin/study/racingcar/CarTest.kt
+++ src/test/kotlin/study/racingcar/CarTest.kt
@@ -7,22 +7,17 @@ class CarTest {
     @Test
     fun `car moves if random value is 4 or more`() {
         val car = Car()
-        car.move(4)
-        assertThat(car.position).isEqualTo(1)
-        car.move(5)
-        assertThat(car.position).isEqualTo(2)
+        var movedCar = car.move(4)
+        assertThat(movedCar.position).isEqualTo(1)
+
+        movedCar = movedCar.move(5)
+        assertThat(movedCar.position).isEqualTo(2)
     }
 
     @Test
     fun `car stays if random value is less than 4`() {
         val car = Car()
-        car.move(3)
-        assertThat(car.position).isEqualTo(0)
-    }
-
-    @Test
-    fun `display position`() {
-        val car = Car(3)
-        assertThat(car.displayPosition()).isEqualTo("---")
+        val movedCar = car.move(3)
+        assertThat(movedCar.position).isEqualTo(0)
     }
 }
