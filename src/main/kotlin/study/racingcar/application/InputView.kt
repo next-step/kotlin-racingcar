976912aa60b417f45189f29be206c051196da14f
moses.lee(이상후)/kakao <moses.lee@kakaocorp.com>
2023-11-02 21:49:59 +0900

    feat: inputString 파서 별도 객체 분리
    # step5

--- src/main/kotlin/study/racingcar/application/InputView.kt
+++ src/main/kotlin/study/racingcar/application/InputView.kt
@@ -3,10 +3,12 @@ package study.racingcar.application
 /**
  * 값을 입력 받는다.
  * */
-data class InputValue(val carNames: List<CarName>, val tryCount: Int)
+data class InputValue(val carNames: CarNames, val tryCount: Int)
 
 class InputView(
-    val reader: Reader) {
+    private val _reader: Reader
+) {
+    private val reader: Reader = _reader
 
     /**
      * stdin 으로 입력을 받고, 입력받은 값을 검증해서 InputValue 로 반환한다.
@@ -14,7 +16,7 @@ class InputView(
      * @throws IllegalArgumentException 입력 값이 null 이거나 적절한 정수형의 숫자가 아닌 경우
      * */
     fun getInput(): InputValue {
-        val carNames: List<CarName> = inputString("자동차 이름을 입력하세요: ")
+        val carNames: CarNames = inputString("자동차 이름을 입력하세요: ")
         val tryCount: Int = inputNumber("시도할 횟수를 입력하세요: ")
 
         val inputValue = InputValue(carNames, tryCount)
@@ -27,12 +29,12 @@ class InputView(
      * @param prompt N 개의 자동차 이름 문자열을 유도하는 문자열
      * @throws IllegalArgumentException 입력 값이 null 이거나 적절한 문자열이 아닌 경우
      * */
-    fun inputString(prompt: String) : List<CarName>{
+    fun inputString(prompt: String): CarNames {
         while (true) {
             try {
                 print(prompt)
                 val input: String = reader.read() ?: throw IllegalArgumentException("입력 값이 null 입니다.")
-                return CarNames(input).getProperFormatCarNames()
+                return CarNameParser(input).getCarNames()
             } catch (e: Exception) {
                 println("[$e] 입력 문자열을 확인해주세요.")
             }
