0411c295c494cf05f4162a4ed984611b16a1675c
chacha.roni <chacha.roni@kakaocorp.com>
2023-11-03 22:25:30 +0900

    피드백 반영 - 자동차명 최대길이 상수 처리

--- src/main/kotlin/race/domain/SimpleCar.kt
+++ src/main/kotlin/race/domain/SimpleCar.kt
@@ -6,8 +6,8 @@ class SimpleCar(val name: String) {
     private var location = 0
 
     init {
-        if (name.length > 5) {
-            throw IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다")
+        if (name.length > NAME_MAX_LENGTH) {
+            throw IllegalArgumentException("자동차 이름은 ${NAME_MAX_LENGTH}자를 초과할 수 없습니다")
         }
     }
 
@@ -19,5 +19,6 @@ class SimpleCar(val name: String) {
 
     companion object {
         const val MOVE_THRESHOLD = 4
+        const val NAME_MAX_LENGTH = 5
     }
 }
--- src/test/kotlin/race/domain/SimpleCarTest.kt
+++ src/test/kotlin/race/domain/SimpleCarTest.kt
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
+import race.domain.SimpleCar.Companion.NAME_MAX_LENGTH
 import java.lang.IllegalArgumentException
 
 class SimpleCarTest {
@@ -13,7 +14,7 @@ class SimpleCarTest {
     fun `name 정책위반 테스트`(name: String) {
         Assertions.assertThatThrownBy { SimpleCar(name) }
             .isInstanceOf(IllegalArgumentException::class.java)
-            .hasMessageContaining("자동차 이름은 5자를 초과할 수 없습니다")
+            .hasMessageContaining("자동차 이름은 ${NAME_MAX_LENGTH}자를 초과할 수 없습니다")
     }
 
     @Test
