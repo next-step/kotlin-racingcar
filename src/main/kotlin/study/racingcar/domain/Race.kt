40dff634c23f47bff36de349b7dc9bd129d4790a
Jaeyeon <59726665+jaeyeonme@users.noreply.github.com>
2023-11-03 15:43:37 +0900

    [step5] 🚀 자동차 경주(리팩토링) (#1478)
    * step5 : 자동차 경주 (리팩토링)
    
    * ./gradlew ktlintFormat
    
    * [step5] 코드리뷰 반영

--- src/main/kotlin/study/racingcar/domain/Race.kt
+++ src/main/kotlin/study/racingcar/domain/Race.kt
@@ -1,7 +1,7 @@
 package study.racingcar.domain
 
+import study.racingcar.dto.RacingCarState
 import study.racingcar.inteface.MoveStrategy
-import study.racingcar.ui.ResultView
 
 class Race private constructor(
     val cars: List<Car>,
@@ -13,15 +13,25 @@ class Race private constructor(
         require(moveCount > 0) { "시도할 횟수는 1회 이상이어야 합니다." }
     }
 
-    fun run() {
+    fun run(): List<RacingCarState> {
+        val result = mutableListOf<RacingCarState>()
         repeat(moveCount) {
             cars.forEach { car ->
                 val shouldMove = moveStrategy.shouldMove()
                 car.move(shouldMove)
             }
-            ResultView.displayCars(cars)
+            result.addAll(cars.map { it.toRacingCarState() })
         }
+        return result
     }
+
+    fun getWinners(): List<RacingCarState> {
+        val maxPosition = cars.maxByOrNull { it.position }?.position ?: 0
+        val winningCars = cars.filter { it.position == maxPosition }
+        return winningCars.map { it.toRacingCarState() }
+    }
+
+    private fun Car.toRacingCarState() = RacingCarState(this.name.name, this.position)
 
     companion object {
         fun create(cars: List<Car>, moveCount: Int, moveStrategy: MoveStrategy): Race {
--- src/test/kotlin/study/racingcar/domain/CarTest.kt
+++ src/test/kotlin/study/racingcar/domain/CarTest.kt
@@ -24,6 +24,25 @@ class CarTest {
     }
 
     @Test
+    @DisplayName("자동차 이름이 1자 이상 5자 이하일 때 객체 생성이 성공해야 한다")
+    fun `should create CarName object when name length is between 1 and 5`() {
+        val validNames = listOf("1", "12", "123", "1234", "12345")
+
+        validNames.forEach { name ->
+            val carName = CarName(name)
+            assertEquals(name, carName.name)
+        }
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 빈 문자열일 때 예외를 던져야 한다")
+    fun `should throw exception when name is blank`() {
+        assertThrows<IllegalArgumentException> {
+            CarName("")
+        }
+    }
+
+    @Test
     @DisplayName("자동차 이름이 5자를 초과할 때 예외를 던져야 한다.")
     fun `should throw exception when car name is longer than 5 characters`() {
         assertThrows<IllegalArgumentException> {
