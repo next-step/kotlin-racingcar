1ae48bb015b863fa12f395da3f27f2886d0b5f97
moses.lee(이상후)/kakao <moses.lee@kakaocorp.com>
2023-11-03 14:53:02 +0900

    chore : 주석 추가
    # step5

--- src/test/kotlin/study/racingcar/domain/RacingGameTest.kt
+++ src/test/kotlin/study/racingcar/domain/RacingGameTest.kt
@@ -10,6 +10,8 @@ import study.racingcar.view.InputView
 
 class RacingGameTest {
 
+    // TODO-review : 컨트롤러(오케스트레이션) 객체인 RacingGame 을 테스트할 경우, run 이 정상동작 함 이외는 테스트 할 수 없네요.
+    //  도메인 로직의 밖의 영역이니,, 테스트를 할 수 없는 것이 맞는 것 같기도 하고요. 코멘트 부탁드려요!
     @Test
     fun `racingGame 이 정상 동작한다`() {
         // given
