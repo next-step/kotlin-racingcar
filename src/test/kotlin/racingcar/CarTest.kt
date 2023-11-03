096b90dbde84757b705691e56bb6a1f5192878c8
Jeong Myeonggu <audrn6689@gmail.com>
2023-11-03 16:40:13 +0900

    step4 - 자동차 경주 테스트 코드 작성

--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -2,47 +2,40 @@ package racingcar
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.inspectors.forAll
 import io.kotest.matchers.shouldBe
-import io.mockk.every
-import io.mockk.mockkObject
-import io.mockk.unmockkObject
-import kotlin.random.Random
 
 class CarTest : StringSpec({
-    beforeTest {
-        mockkObject(Random)
-    }
-    afterTest {
-        unmockkObject(Random)
-    }
 
     "전진하는 조건을 만족하면 자동차는 전진한다. " {
-        val carName = "정명구"
-        val car = Car.create(carName)
-        every { Random.nextInt(Car.getRandomRange()) } returns Car.getMoveThreshold()
+        val range = listOf(4, 5, 6, 7, 8, 9)
+        range.forAll {
+            val carName = "정명구"
+            val car = Car(carName)
 
-        car.move()
+            car.move(it)
 
-        car.display() shouldBe "$carName : -"
+            car.moveCount shouldBe 1
+        }
     }
 
     "전진하는 조건을 만족하지 못하면 자동차는 전진하지 않는다" {
-        val carName = "정명구"
-        val car = Car.create(carName)
-        every { Random.nextInt(Car.getRandomRange()) } returns Car.getMoveThreshold() - 1
+        val range = listOf(1, 2, 3)
+        range.forAll {
+            val carName = "정명구"
+            val car = Car(carName)
 
-        car.move()
+            car.move(it)
 
-        car.display() shouldBe "$carName : "
+            car.moveCount shouldBe 0
+        }
     }
 
     "이름의 길이가 지정한 범위 보다 크면 예외가 발생한다." {
         val carName = "정명구정명구"
-        val nameRange = Car.getNameRange()
 
-        nameRange shouldBe 5
         shouldThrow<IllegalArgumentException> {
-            Car.create(carName)
+            Car(carName)
         }
     }
 })
--- src/test/kotlin/racingcar/RacingCarServiceTest.kt
+++ src/test/kotlin/racingcar/RacingCarServiceTest.kt
@@ -1,21 +1,55 @@
 package racingcar
 
 import io.kotest.core.spec.style.StringSpec
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.verify
+import io.kotest.matchers.collections.shouldContainExactly
+import io.kotest.matchers.shouldBe
+
+class AlwaysMoveStrategy : MoveStrategy {
+    override fun move(): Int = Car.MOVE_THRESHOLD
+}
+
+class NeverMoveStrategy : MoveStrategy {
+    override fun move(): Int = 0
+}
 
 class RacingCarServiceTest : StringSpec({
-    "사용자 입력에 따라 수행을 반복합니다." {
-        val mockInputProvider = mockk<InputProvider>()
-        val mockResultProvider = mockk<ResultProvider>()
+    "레이싱카 서비스는 승자가 한 명일 때 승자를 올바르게 결정해야 합니다." {
+        val movingCarNames = listOf("T1")
+        val stopCarNames = listOf("S1", "S2")
 
-        every { mockInputProvider.getIntInput("자동차 대수는 몇 대 인가요?") } returns 2
-        every { mockInputProvider.getIntInput("시도할 횟수는 몇 회 인가요?") } returns 3
+        val movingCars = Cars(movingCarNames.map { Car(it) }, AlwaysMoveStrategy())
+        val stopCars = Cars(stopCarNames.map { Car(it) }, NeverMoveStrategy())
 
-        val sut = RacingCarService(mockInputProvider, mockResultProvider)
-        sut.run()
+        repeat(5) {
+            movingCars.moveCars()
+            stopCars.moveCars()
+        }
 
-        verify(exactly = 3) { mockResultProvider.printItems(any<List<Car>>()) }
+        val allCars = Cars(movingCars.getCars() + stopCars.getCars())
+
+        val winners = allCars.getWinners()
+
+        winners.size shouldBe movingCars.getCars().size
+        winners.map { it.name } shouldContainExactly movingCarNames
+    }
+
+    "레이싱카 서비스는 승자가 여러 명일 때 승자를 올바르게 결정해야 합니다." {
+        val movingCarNames = listOf("M1", "M2", "M3")
+        val stopCarNames = listOf("S1", "S2")
+
+        val movingCars = Cars(movingCarNames.map { Car(it) }, AlwaysMoveStrategy())
+        val stopCars = Cars(stopCarNames.map { Car(it) }, NeverMoveStrategy())
+
+        repeat(5) {
+            movingCars.moveCars()
+            stopCars.moveCars()
+        }
+
+        val allCars = Cars(movingCars.getCars() + stopCars.getCars())
+
+        val winners = allCars.getWinners()
+
+        winners.size shouldBe movingCars.getCars().size
+        winners.map { it.name } shouldContainExactly movingCarNames
     }
 })
