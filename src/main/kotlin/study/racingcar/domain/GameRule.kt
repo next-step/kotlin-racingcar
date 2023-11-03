8b783251303c121e128b14747dd80aac7cd81422
moses.lee(이상후)/kakao <moses.lee@kakaocorp.com>
2023-11-03 14:32:09 +0900

    fix: random 함수 위치 수정
    - GameRule 의 역할로 고정
    
    # step5

--- src/main/kotlin/study/racingcar/domain/GameRule.kt
+++ src/main/kotlin/study/racingcar/domain/GameRule.kt
@@ -1,10 +1,12 @@
 package study.racingcar.domain
 
+/**
+ * getResult 의 구현에 따라, 랜덤 숫자를 기준으로 어떤 룰을 적용하느냐가 달라짐.
+ * */
 interface GameRule {
-
-    // TODO-review : interface 로 규격을 만들고 싶었습니다.
-    //  하지만, 만약 새로운 게임룰은 결과를 받을때, int 가 아닌 다른 타입을 받아야 한다면, interface 에 새로운 fun 이 추가되어야 합니다.
-    //  이렇듯 새로운 기능이 추가될때마다 interface 를 수정하는건 best-practice 가 아닌것 같은데요.
-    //  이를 해결하기 위한 방법이 있을까요? 조언 부탁드립니다.
     fun getResult(num: Int): Boolean
+
+    fun getRandomNumber(): Int {
+        return (0..9).random()
+    }
 }
