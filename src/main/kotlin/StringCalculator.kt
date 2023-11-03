7734152a30a48bc1ea382f75b81419540da3f5b5
장진혁/Client개발팀/원스토어 <ironelder@onestorecorp.com>
2023-11-03 14:58:29 +0900

    [step2] calculate를 연속으로 할 경우 값이 변하는 이슈 수정

--- src/main/kotlin/StringCalculator.kt
+++ src/main/kotlin/StringCalculator.kt
@@ -2,24 +2,25 @@ class StringCalculator(private val calculatorString: String?) {
 
     private val tokens = calculatorString?.split(" ")?.filter { it.isNotBlank() } ?: emptyList()
     private val numberArgs: List<Int>
-    private val operatorArgs: MutableList<String>
+    private val operatorArgs: List<String>
 
     init {
         require(!calculatorString.isNullOrBlank()) { throw IllegalArgumentException() }
         numberArgs = tokens.filter { it.toIntOrNull() != null }.map { it.toInt() }
-        operatorArgs = tokens.filter { it.toIntOrNull() == null }.toMutableList()
+        operatorArgs = tokens.filter { it.toIntOrNull() == null }
         check(numberArgs.size > operatorArgs.size) { throw IllegalArgumentException() }
     }
 
-    fun calculating(): Int = numberArgs.reduce { acc, next ->
-        when (operatorArgs.removeFirst()) {
-            "+" -> plus(acc, next)
-            "-" -> minus(acc, next)
-            "*" -> multiple(acc, next)
-            "/" -> divide(acc, next)
-            else -> throw IllegalArgumentException()
+    fun calculating(operators: MutableList<String> = operatorArgs.toMutableList()): Int =
+        numberArgs.reduce { acc, next ->
+            when (operators.removeFirst()) {
+                "+" -> plus(acc, next)
+                "-" -> minus(acc, next)
+                "*" -> multiple(acc, next)
+                "/" -> divide(acc, next)
+                else -> throw IllegalArgumentException()
+            }
         }
-    }
 
     private fun plus(first: Int, second: Int): Int = first + second
     private fun minus(first: Int, second: Int): Int = first - second
@@ -28,7 +29,8 @@ class StringCalculator(private val calculatorString: String?) {
 }
 
 fun main() {
-    StringCalculator("2 + 3 * 4 / 2").apply {
-        println("result = ${calculating()}")
-    }
+    val test = StringCalculator("2 + 3 + 4 + 6")
+    println("${test.calculating()}")
+    println("${test.calculating()}")
+    println("${test.calculating()}")
 }
