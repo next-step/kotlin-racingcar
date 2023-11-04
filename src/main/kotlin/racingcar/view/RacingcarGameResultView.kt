15901d471e1d6870fee1e75a8a5eb7557076e741
yw9594 <kyw9509@gmail.com>
2023-11-04 19:36:52 +0900

    refactor(step4): change hard coding string to constant

--- src/main/kotlin/racingcar/view/RacingcarGameResultView.kt
+++ src/main/kotlin/racingcar/view/RacingcarGameResultView.kt
@@ -3,6 +3,9 @@ package racingcar.view
 import racingcar.model.Car
 
 object RacingcarGameResultView {
+    private const val CAR_DISTANCE_CHARACTER = "-"
+    private const val WINNER_DISPLAY_SEPARATOR = ", "
+
     fun printGameStart() {
         println("실행 결과")
     }
@@ -15,11 +18,11 @@ object RacingcarGameResultView {
     private fun printCarDistance(car: Car) {
         val distance = car.getDistance() + 1
 
-        println("${car.name} : " + "-".repeat(distance))
+        println("${car.name} : " + CAR_DISTANCE_CHARACTER.repeat(distance))
     }
 
     fun printGameWinners(cars: List<Car>) {
-        val winnersNameString = cars.joinToString(", ") { it.name }
+        val winnersNameString = cars.joinToString(WINNER_DISPLAY_SEPARATOR) { it.name }
 
         println("${winnersNameString}가 최종 우승했습니다.\n")
     }
