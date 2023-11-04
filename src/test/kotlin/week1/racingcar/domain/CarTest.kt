726231f7b172e51a96c1035d2a26aec4e39e2210
bsgreentea <bsgreentea1@gmail.com>
2023-11-05 00:53:59 +0900

    test: Car 클래스에 대한 테스트 코드를 작성한다.
    - 테스트용으로 최대한 진행할 수 있는 게임 회차를 두고, 게임 진행 시 최대 주행거리가 이를 넘어가는지 않는 값이 맞는지 확인

---
+++ src/test/kotlin/week1/racingcar/domain/CarTest.kt
@@ -0,0 +1,37 @@
+package week1.racingcar.domain
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
+
+class CarTest {
+    @Test
+    fun `생성자 확인`() {
+        val car1 = Car("James")
+        car1.name shouldBe "James"
+
+        assertThrows(IllegalArgumentException::class.java) {
+            Car("Thomas")
+        }
+    }
+
+    @Test
+    fun `한 턴 진행`() {
+        val car1 = Car("K5")
+        repeat(DEFAULT_TEST_TIMES) {
+            car1.moveOrNot()
+        }
+        assert(car1.mileage <= DEFAULT_TEST_TIMES)
+
+        val car2 = Car("G80")
+        repeat(MAX_TEST_TIMES) {
+            car2.moveOrNot()
+        }
+        assert(car1.mileage <= MAX_TEST_TIMES)
+    }
+
+    companion object {
+        private const val DEFAULT_TEST_TIMES = 1
+        private const val MAX_TEST_TIMES = 5
+    }
+}
