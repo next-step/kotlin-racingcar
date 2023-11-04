85a0286511b4b3800ea7ac805a7ac7106075dcb9
yw9594 <kyw9509@gmail.com>
2023-11-04 19:40:04 +0900

    refactor(step4): split input method per responsibility

--- src/main/kotlin/racingcar/view/RacingcarGameInputView.kt
+++ src/main/kotlin/racingcar/view/RacingcarGameInputView.kt
@@ -5,21 +5,29 @@ import racingcar.model.RacingcarGameInput
 
 object RacingcarGameInputView {
     fun getInput(): RacingcarGameInput {
-        val carNamesString = getInput("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
-        val carNames = RacingcarGameInputValidator.validateCarNamesString(carNamesString)
-            .split(",")
-            .map { it.trim() }
-            .map { RacingcarGameInputValidator.validateCarName(it) }
-
-        val roundNumberString = getInput("시도할 횟수는 몇 회인가요?")
-        val roundNumber = RacingcarGameInputValidator
-            .validateRoundNumberString(roundNumberString)
-            .toInt()
+        val carNames = getCarNames()
+        val roundNumber = getRoundNumber()
 
         return RacingcarGameInput(
             carNames = carNames,
             roundNumber = roundNumber
         )
+    }
+
+    private fun getCarNames(): List<String> {
+        val carNamesString = getInput("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+
+        return RacingcarGameInputValidator.validateCarNamesString(carNamesString)
+            .split(",")
+            .map { it.trim() }
+            .map { RacingcarGameInputValidator.validateCarName(it) }
+    }
+
+    private fun getRoundNumber(): Int {
+        val roundNumberString = getInput("시도할 횟수는 몇 회인가요?")
+        return RacingcarGameInputValidator
+            .validateRoundNumberString(roundNumberString)
+            .toInt()
     }
 
     private fun getInput(message: String): String? {
