51b170293ad20740d33ebe891e275fc4094a661a
celine.yerim <celine.yerim@kakaocorp.com>
2023-11-04 21:36:57 +0900

    test: 자동차 경주 테스트 수정

--- src/test/kotlin/carracing/RandomGameTest.kt
+++ src/test/kotlin/carracing/RandomGameTest.kt
@@ -8,8 +8,10 @@ class RandomGameTest {
 
     @Test
     fun `랜덤 게임 테스트`() {
-        val cars = listOf(Car())
+        val name = "carName"
+        val cars = listOf(Car(name))
         RandomGame().execute(cars)
         assertThat(cars.first().position).isIn(0, 1)
+        assertThat(cars.first().name).isEqualTo(name)
     }
 }
