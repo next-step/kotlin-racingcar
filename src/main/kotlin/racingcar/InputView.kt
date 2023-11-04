07348818030e7303e7f1ce242864791728a097d5
sendkite <sendkite1@gmail.com>
2023-11-04 16:29:59 +0900

    feat: validate input

--- src/main/kotlin/racingcar/InputView.kt
+++ src/main/kotlin/racingcar/InputView.kt
@@ -7,23 +7,21 @@ data class InputView(
     fun inputGameCount(): Unit {
         println("시도할 회수는 몇회인가요?")
         val input = readln().toInt()
-        validate()
-
+        validate(input)
         this.gameCount = input
     }
 
     fun inputCarNumber() {
         println("자동차 대수는 몇 대 인가요?")
         val input = readln().toInt()
-        validate()
+        validate(input)
 
         this.cars = List(input) {
             Car(moveCondition = RandomNumberHolderImpl())
         }
     }
 
-    private fun validate() {
-        require(gameCount < 0) { throw IllegalArgumentException("음수는 입력할 수 없습니다.") }
-        require(gameCount > 100) { throw IllegalArgumentException("100회 이상은 입력할 수 없습니다.") }
+    fun validate(input: Int) {
+        require(input > 0) { throw IllegalArgumentException("음수는 입력할 수 없습니다.") }
     }
 }
--- src/test/kotlin/racingcar/RacingGameTest.kt
+++ src/test/kotlin/racingcar/RacingGameTest.kt
@@ -1,7 +1,10 @@
 package racingcar
 
 import io.kotest.matchers.shouldBe
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class RacingGameTest {
 
@@ -25,5 +28,14 @@ class RacingGameTest {
 
         racingGame.gameCount shouldBe 5
         racingGame.cars.size shouldBe 3
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [-12, 0, -1])
+    fun `실패 - 0 이하의 게임횟수, 자동차 수 입력`(input: Int) {
+
+        assertThatThrownBy { InputView().validate(input) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("음수는 입력할 수 없습니다.")
     }
 }
