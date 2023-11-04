275defe9c1668497406d6426c28bbd340daaa977
Heedong <59307414+ruthetum@users.noreply.github.com>
2023-11-04 15:03:19 +0900

    refactor(racingcar): 미사용 코드 제거

--- src/main/kotlin/racingcar/view/ResultView.kt
+++ src/main/kotlin/racingcar/view/ResultView.kt
@@ -7,11 +7,7 @@ class ResultView {
     companion object {
         private const val POSITION_SYMBOL = "-"
 
-        fun moveAndPrintResult(cars: Cars) {
-            moveAndPrintResult(cars, 1)
-        }
-
-        fun moveAndPrintResult(cars: Cars, tryCount: Int) {
+        fun moveAndPrintResult(cars: Cars, tryCount: Int = 1) {
             println("\n실행 결과")
             for (i in 1..tryCount) {
                 cars.move()
