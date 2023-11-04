56a6197750014f83df58f08a27408c124647ab8a
bsgreentea <bsgreentea1@gmail.com>
2023-11-05 00:30:45 +0900

    refactor: 각 패키지의 역할에 맞게 코드를 수정한다.
    - 입출력은 RaceController에서 다루도록 수정
    - 자동차 리스트와 게임 횟수는 RacingGame의 생성자에서 받도록 변경

--- src/main/kotlin/week1/racingcar/controller/RaceController.kt
+++ src/main/kotlin/week1/racingcar/controller/RaceController.kt
@@ -1,14 +1,24 @@
 package week1.racingcar.controller
 
+import week1.racingcar.domain.Car
 import week1.racingcar.domain.RacingGame
+import week1.racingcar.view.InputView
+import week1.racingcar.view.ResultView
 
 class RaceController {
 
     fun startRacing() {
+        val raceInfo = InputView.getValidateRaceInfo()
+        val racingGame = RacingGame(raceInfo.cars.map { Car(it) }, raceInfo.timesToRace)
+
+        repeat(racingGame.timesToRace) {
+            val carsAfterDriving = racingGame.drive()
+            ResultView.showResult(carsAfterDriving)
+        }
+        ResultView.showWinner(racingGame.getWinners())
     }
 }
 
 fun main() {
-    RacingGame().startGame()
-    RaceController()
+    RaceController().startRacing()
 }
--- src/main/kotlin/week1/racingcar/domain/RacingGame.kt
+++ src/main/kotlin/week1/racingcar/domain/RacingGame.kt
@@ -1,44 +1,17 @@
 package week1.racingcar.domain
 
-import week1.racingcar.view.InputView
-import week1.racingcar.view.ResultView
-
-class RacingGame {
-
-    private val cars: List<String>
-    private val timesToRace: Int
-
-    init {
-        InputView.getValidateRaceInfo().also {
-            cars = it.cars
-            timesToRace = it.timesToRace
-        }
-    }
-
-    fun startGame() {
-        val cars = cars.map { Car(it) }
-        race(cars, timesToRace)
-    }
-
-    private fun race(cars: List<Car>, timesToRace: Int) {
-        repeat(timesToRace) {
-            drive(cars)
-            showResult(cars)
-        }
-        ResultView.showWinner(getWinners(cars))
-    }
-
-    private fun drive(cars: List<Car>) {
+class RacingGame(
+    private val cars: List<Car>,
+    val timesToRace: Int,
+) {
+    fun drive(): List<Car> {
         cars.forEach {
             it.moveOrNot()
         }
+        return cars
     }
 
-    private fun showResult(cars: List<Car>) {
-        ResultView.showResult(cars)
-    }
-
-    fun getWinners(cars: List<Car>): List<String> {
+    fun getWinners(): List<String> {
         val farthestMileage = cars.map { it.mileage }
             .maxOf { it }
         return cars.filter { it.isSameMileage(farthestMileage) }
