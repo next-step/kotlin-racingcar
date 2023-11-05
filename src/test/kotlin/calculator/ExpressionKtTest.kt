55519479e506d93a0fef58ec4d24f867190a42d5
korjun1993 <corojoon93@naver.com>
2023-11-05 19:29:42 +0900

    test: 피드백 반영 - NullAndEmptySource 사용

--- src/test/kotlin/calculator/ExpressionKtTest.kt
+++ src/test/kotlin/calculator/ExpressionKtTest.kt
@@ -2,13 +2,13 @@ package calculator
 
 import io.kotest.assertions.throwables.shouldThrow
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.NullSource
+import org.junit.jupiter.params.provider.NullAndEmptySource
 import org.junit.jupiter.params.provider.ValueSource
 
 class ExpressionKtTest {
 
     @ParameterizedTest
-    @NullSource
+    @NullAndEmptySource
     @ValueSource(strings = ["", " ", "  ", "\n"])
     fun `표현식이 공백이거나 널이면 IllegalArgumentException 발생한다`(expression: String?) {
         shouldThrow<IllegalArgumentException> {
