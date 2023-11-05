fb136e47371d7d2be75e5a68a2b95500ac043311
ALEX <kpmyung@gmail.com>
2023-11-05 17:26:20 +0900

    refactor: 콘솔 출력 메시지와 입력 받는 행위는 입력 뷰 객체로 응집한다
    Signed-off-by: ALEX <kpmyung@gmail.com>

--- src/main/kotlin/racing/v2/Application.kt
+++ src/main/kotlin/racing/v2/Application.kt
@@ -3,11 +3,11 @@ package racing.v2
 import racing.v2.console.InputView
 
 fun main() {
-    println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
-    val carsStr: String? = readLine()
+    InputView.printCarNamesPrompt()
+    val carsStr = InputView.getUserInput()
 
-    println("시도할 횟수는 몇 회인가요?")
-    val tryCountStr: String? = readLine()
+    InputView.printTryCountPrompt()
+    val tryCountStr = InputView.getUserInput()
 
     val carGame = InputView.createCarGame(carsStr, tryCountStr)
     carGame.start()
