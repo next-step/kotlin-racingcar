0098ba50b44521846beb228c2aa8444353bbbedf
korjun1993 <corojoon93@naver.com>
2023-11-05 19:29:13 +0900

    test: 피드백 반영 - 0으로 나누는 케이스 테스트

--- src/test/kotlin/calculator/CalculatorKtTest.kt
+++ src/test/kotlin/calculator/CalculatorKtTest.kt
@@ -1,6 +1,8 @@
 package calculator
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -10,7 +12,16 @@ class CalculatorKtTest {
     @ParameterizedTest
     @MethodSource("calculators")
     fun `숫자와 연산기호로 구성된 표현식의 연산을 수행한다`(expression: String, expect: Int) {
-        calculate(expression) shouldBe expect
+        val calculator = Calculator()
+        calculator.calculate(expression) shouldBe expect
+    }
+
+    @Test
+    fun `숫자를 0으로 나누면 예외가 발생한다`() {
+        val calculator = Calculator()
+        shouldThrow<IllegalArgumentException> {
+            calculator.calculate("10 / 0")
+        }
     }
 
     companion object {
