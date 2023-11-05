6f285f4d5481ec9582cfbdf25c6e6ce5edf5bae9
mkkim90 <minkyun.kim@toss.im>
2023-11-06 01:06:54 +0900

    [step3] feat : 자동차 경주 시뮬레이션

---
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -0,0 +1,30 @@
+package racingcar
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class CarTest {
+
+    @ValueSource(ints = [5, 6, 7])
+    @ParameterizedTest
+    fun `자동차는 위치를 가진다`(position: Int) {
+        val car = Car(position)
+        car.position shouldBe position
+    }
+
+    @Test
+    fun `자동차의 초기 위치를 0이다`() {
+        val car = Car()
+        car.position shouldBe 0
+    }
+
+    @ValueSource(ints = [4, 5, 6, 7, 8, 9])
+    @ParameterizedTest
+    fun `자동차는 무작위 값이 4 이상일 경우 움직인다`(condition: Int) {
+        val car = Car()
+        car.move(condition)
+        car.position shouldBe 1
+    }
+}
