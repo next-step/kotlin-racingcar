9bf10bf960bf828eb88768b4c94b02e6802c6d67
1stpie <1stpie@naver.com>
2023-11-05 14:34:12 +0900

    test(core): refactor test cases

--- src/test/kotlin/study/racingcar/domain/CarsTest.kt
+++ src/test/kotlin/study/racingcar/domain/CarsTest.kt
@@ -1,3 +1,30 @@
 package study.racingcar.domain
 
-class CarsTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class CarsTest {
+    private val alwaysMoveGenerator = object : RandomValueGenerator {
+        override fun invoke(): Int = 5
+    }
+
+    @Test
+    fun `moveAll should move all cars based on the provided random values`() {
+        val racingGame = RacingGame(listOf("hong", "lee"), 5, alwaysMoveGenerator)
+        val initialCars = racingGame.cars
+        val initialCarList = racingGame.cars.list
+        val movedCars = initialCars.moveAll(alwaysMoveGenerator)
+
+        val expectedPositions = listOf(
+            initialCarList[0].move(alwaysMoveGenerator()),
+            initialCarList[1].move(alwaysMoveGenerator())
+        ).map { it.position }
+        val actualPositions = movedCars.list.map { it.position }
+
+        assertEquals(
+            expectedPositions,
+            actualPositions,
+            "Cars should move based on the provided random values"
+        )
+    }
+}
--- src/test/kotlin/study/racingcar/domain/RacingGameTest.kt
+++ src/test/kotlin/study/racingcar/domain/RacingGameTest.kt
@@ -3,7 +3,6 @@ package study.racingcar.domain
 import org.assertj.core.api.AssertionsForInterfaceTypes
 import org.assertj.core.api.Java6Assertions.assertThat
 import org.assertj.core.api.Java6Assertions.assertThatThrownBy
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
@@ -31,7 +30,7 @@ class RacingGameTest {
             override fun invoke(): Int = randomValue
         }
         val racingGame = RacingGame(listOf("hong", "lee", "kim"), rounds, valueGenerator)
-        racingGame.run {}
+        racingGame.raceStart {}
         assertThat(racingGame.cars.list.all { it.position == expectedPosition }).isTrue
     }
 
@@ -41,44 +40,6 @@ class RacingGameTest {
             RacingGame(listOf("hong", "lee", "kim"), -5)
         }.isInstanceOf(IllegalArgumentException::class.java)
             .hasMessage("Number of rounds must be positive")
-    }
-
-    @ParameterizedTest
-    @CsvSource(
-        value = [
-            "0, 0",
-            "1, 0",
-            "2, 0",
-            "3, 0",
-            "4, 1",
-            "5, 1",
-            "6, 1"
-        ]
-    )
-    fun `car moves if random value is 4 or more`(randomValue: Int, expectedPosition: Int) {
-        val racingGame = RacingGame(listOf("hong", "lee", "kim"), 5, alwaysMoveGenerator)
-        val car = Car("test")
-        val movedCar = racingGame.move(car, randomValue)
-        assertThat(movedCar.position).isEqualTo(expectedPosition)
-    }
-
-    @Test
-    fun `moveAll should move all cars based on the provided random values`() {
-        val racingGame = RacingGame(listOf("hong", "lee"), 5, alwaysMoveGenerator)
-        val initialCars = racingGame.cars.copy()
-        val movedCars = racingGame.moveAll(initialCars, alwaysMoveGenerator)
-
-        val expectedPositions = listOf(
-            racingGame.move(initialCars.list[0], alwaysMoveGenerator()),
-            racingGame.move(initialCars.list[1], alwaysMoveGenerator())
-        ).map { it.position }
-        val actualPositions = movedCars.list.map { it.position }
-
-        assertEquals(
-            expectedPositions,
-            actualPositions,
-            "Cars should move based on the provided random values"
-        )
     }
 
     @Test
