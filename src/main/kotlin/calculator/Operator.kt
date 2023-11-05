4b663b51900f90163815b97a5fedfbc2f084c255
korjun1993 <corojoon93@naver.com>
2023-11-05 19:25:20 +0900

    fix: 피드백 반영 - Operator 수정
    1. 함수형 인터페이스 적용
    2. 0으로 나눌때 예외 발생
    3. findOperator 정적메서드로 선언

--- src/main/kotlin/calculator/Operator.kt
+++ src/main/kotlin/calculator/Operator.kt
@@ -1,21 +1,26 @@
 package calculator
 
-enum class Operator(val symbol: String) {
-    PLUS("+"),
-    MINUS("-"),
-    MULTIPLY("*"),
-    DIVIDE("/");
+enum class Operator(val symbol: String, private val operation: IntOperation) {
+    PLUS("+", IntOperation { number1, number2 -> number1 + number2 }),
+    MINUS("-", IntOperation { number1, number2 -> number1 - number2 }),
+    MULTIPLY("*", IntOperation { number1, number2 -> number1 * number2 }),
+    DIVIDE("/", IntOperation { number1, number2 -> number1 / number2 });
 
-    fun apply(number1: Int, number2: Int) =
-        when (this) {
-            PLUS -> number1 + number2
-            MINUS -> number1 - number2
-            MULTIPLY -> number1 * number2
-            DIVIDE -> number1 / number2
+    fun operate(number1: Int, number2: Int): Int {
+        validate(number2)
+        return operation.operate(number1, number2)
+    }
+
+    private fun validate(number2: Int) {
+        if (this == DIVIDE && number2 == 0) {
+            throw IllegalArgumentException("0으로 나눌 수 없습니다")
         }
-}
+    }
 
-fun String.findOperator(): Operator {
-    return Operator.values().firstOrNull() { it.symbol == this }
-        ?: throw IllegalArgumentException("사칙 연산 기호가 아닙니다")
+    companion object {
+        fun findOperator(symbol: String): Operator {
+            return Operator.values().firstOrNull { it.symbol == symbol }
+                ?: throw IllegalArgumentException("사칙 연산 기호가 아닙니다")
+        }
+    }
 }
