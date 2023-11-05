41b63c936785833c9bd908c9dac962ad317a9c3b
ironelder <ironelder@gmail.com>
2023-11-05 17:42:11 +0900

    [Step3] 테스트 항목 추가

--- src/test/kotlin/study/RacingCarGameTest.kt
+++ src/test/kotlin/study/RacingCarGameTest.kt
@@ -1,3 +1,15 @@
 package study
 
-class RacingCarGameTest
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import racinggame.RacingCarGame
+
+class RacingCarGameTest {
+
+    @Test
+    fun `테스트 인자`() {
+        assertThrows<IllegalStateException> { RacingCarGame() }
+        assertThrows<IllegalStateException> { RacingCarGame().playing() }
+    }
+    
+}
