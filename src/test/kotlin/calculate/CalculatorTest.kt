4fdb9e184b109f405593c596377c15e24597c0ab
mkkim90 <minkyun.kim@toss.im>
2023-11-05 16:57:01 +0900

    [step2] test refactor: 실패할 수 있는 테스트를 짬

--- src/test/kotlin/calculate/CalculatorTest.kt
+++ src/test/kotlin/calculate/CalculatorTest.kt
@@ -1,6 +1,7 @@
 package calculate
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
@@ -16,5 +17,29 @@ class CalculatorTest {
     )
     fun `계산 테스트`(input: String, result: Int) {
         assertThat(Calculator.calculate(input)).isEqualTo(result)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "+ + 2,7",
+        "+ + 3 * 4 / 2,10",
+        "2 * +,6",
+        "2 * + / 2,3",
+        "4 + + + + - 4,14"
+    )
+    fun `숫자가 아닌 값이 들어오면 예외가 발생한다`(input: String) {
+        assertThrows<NumberFormatException> { Calculator.calculate(input) }
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "5 2 2,7",
+        "2 & 3 * 4 / 2,10",
+        "2 ^ 3,6",
+        "2 # 3 / 2,3",
+        "4 % 4 ( 10 - 4,14"
+    )
+    fun `사칙연산기호가 아닌 값이 들어오면 예외가 발생한다`(input: String) {
+        assertThrows<IllegalArgumentException> { Calculator.calculate(input) }
     }
 }
