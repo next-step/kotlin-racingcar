4c715b8258093dc55a29c1221a278842b66ac060
hyotaek-jang <hyotaek.jang@linecorp.com>
2023-11-08 00:38:10 +0900

    refactor: refactor test

--- src/test/kotlin/game/racing/domain/CarTest.kt
+++ src/test/kotlin/game/racing/domain/CarTest.kt
@@ -12,4 +12,22 @@ internal class CarTest {
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessageContaining(Car.NOT_VALID_CAR_NAME_MESSAGE)
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = [0, 1, 2, 3])
+    fun `4보다 작으면 자동차는 이동하지 않는다`(speed: Int) {
+        val car = Car("TEST")
+        car.moveOrStayBySpeed(speed)
+
+        Assertions.assertThat(car.position).isEqualTo(Car.MIN_POSITION)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [4, 5, 6, 7, 8, 9])
+    fun `4보다 크거나 같으면 자동차는 이동한다`(speed: Int) {
+        val car = Car("TEST")
+        car.moveOrStayBySpeed(speed)
+
+        Assertions.assertThat(car.position).isEqualTo(Car.MIN_POSITION + 1)
+    }
 }
--- src/test/kotlin/game/racing/domain/RacingManagerTest.kt
+++ src/test/kotlin/game/racing/domain/RacingManagerTest.kt
@@ -2,42 +2,20 @@ package game.racing.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
 import org.junit.jupiter.params.provider.ValueSource
-import java.util.stream.Stream
 
 internal class RacingManagerTest {
     @ParameterizedTest
-    @MethodSource("provideParameters")
-    fun `자동차들의 위치를 이동시킨다`(carNames: String, speed: Int) {
-        val cars = CarFactory.generateCarByCarNames(carNames)
-        val racingManager = RacingManager(cars)
-        val speedList = List(cars.size) { speed }
-        racingManager.moveCars(speedList)
-
-        assertThat(cars).allMatch {
-            if (speed >= Car.MIN_MOVE_THRESHOLD) it.position == 1 else it.position == 0
-        }
-    }
-
-    @ParameterizedTest
     @ValueSource(strings = ["WIN,TEST2,TEST3", "TEST1,TEST2,WIN,TEST4", "TEST1,WIN,TEST3,TEST4,TEST5"])
     fun `우승자를 반환한다`(carNames: String) {
-        val cars = CarFactory.generateCarByCarNames(carNames)
+        val cars = carNames.split(",").toTypedArray().map { Car(it) }
         val racingManager = RacingManager(cars)
-        val speedList = cars.map { if (it.name == "WIN") Car.MIN_MOVE_THRESHOLD else Car.MIN_MOVE_THRESHOLD - 1 }
-        racingManager.moveCars(speedList)
-
-        assertThat(racingManager.getWinners()).allMatch { it.position == Car.MIN_POSITION + 1 }.allMatch { it.name == "WIN" }
-    }
-
-    companion object {
-        @JvmStatic
-        fun provideParameters(): Stream<Arguments> {
-            return Stream.of(
-                Arguments.of("TEST,TEST1", 1), Arguments.of("TEST,TEST1", 4), Arguments.of("TEST,TEST1", 7)
-            )
+        cars.forEach {
+            if (it.name == "WIN") {
+                it.moveOrStayBySpeed(Car.MIN_MOVE_THRESHOLD)
+            }
         }
+
+        assertThat(racingManager.determineWinner()).allMatch { it.position == Car.MIN_POSITION + 1 }.allMatch { it.name == "WIN" }
     }
 }
