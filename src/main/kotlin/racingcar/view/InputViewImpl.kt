e2c1413d65e2a4c4ca6b69a2e6e8df29870a9fcc
Heedong <59307414+ruthetum@users.noreply.github.com>
2023-11-07 14:15:18 +0900

    feat(racingcar): view 인터페이스, 구현체 추가

---
+++ src/main/kotlin/racingcar/view/InputViewImpl.kt
@@ -0,0 +1,14 @@
+package racingcar.view
+
+class InputViewImpl : InputView {
+
+    override fun inputCarNameList(): List<String> {
+        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+        return readln().split(",")
+    }
+
+    override fun inputTryCount(): Int {
+        println("시도할 횟수는 몇 회인가요?")
+        return readln().toInt()
+    }
+}
