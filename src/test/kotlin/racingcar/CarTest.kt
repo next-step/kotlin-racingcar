220a63e6eba67297853d747403b53f2b463bb949
korjun1993 <corojoon93@naver.com>
2023-11-08 12:20:42 +0900

    fix: 피드백 반영 - 전진/정지 테스트 통합

--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -1,22 +1,27 @@
 package racingcar
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.data.row
 import io.kotest.datatest.withData
 import io.kotest.matchers.shouldBe
 
 class CarTest : FunSpec({
-    context("자동차에 4 이상의 힘을 가하면 전진한다") {
-        withData(listOf(4, 5, 6, 7, 8, 9)) { power ->
+    context("자동차에 4 이상의 힘을 가하면 전진 혹은 정지한다") {
+        withData(
+            row(0, 0),
+            row(1, 0),
+            row(2, 0),
+            row(3, 0),
+            row(4, 1),
+            row(5, 1),
+            row(6, 1),
+            row(7, 1),
+            row(8, 1),
+            row(9, 1),
+        ) { (power, expected) ->
             val car = Car(engine = { power })
             car.move()
-            car.location shouldBe 1
-        }
-    }
-    context("자동차에 4 미만의 힘을 가하면 움직이지 않는다") {
-        withData(listOf(0, 1, 2, 3)) { power ->
-            val car = Car(engine = { power })
-            car.move()
-            car.location shouldBe 0
+            car.location shouldBe expected
         }
     }
 })
