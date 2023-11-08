3b8691fc407070f4183df4bd6665ce05efac5095
korjun1993 <corojoon93@naver.com>
2023-11-08 12:20:25 +0900

    fix: 피드백 반영 - Car, Wheel 합치기

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,16 +1,25 @@
 package racingcar
 
 data class Car(
-    var location: Int = 0,
+    var location: Int = CAR_DEFAULT_LOCATION,
     private val engine: Engine = RandomPowerEngine(),
-    private val wheel: Wheel = Wheel()
 ) {
     fun move() {
         val power = engine.powerUp()
-        location += wheel.nextLocation(power)
+        location += if (isMovablePower(power)) 1 else 0
     }
 
     override fun toString(): String {
-        return "-".repeat(location)
+        return CAR_LOCATION_PATH_SYMBOL.repeat(location)
+    }
+
+    private fun isMovablePower(power: Int) =
+        power in MOVABLE_MIN_POWER..MOVABLE_MAX_POWER
+
+    companion object {
+        private const val CAR_DEFAULT_LOCATION = 0
+        private const val MOVABLE_MAX_POWER = 9
+        private const val MOVABLE_MIN_POWER = 4
+        private const val CAR_LOCATION_PATH_SYMBOL = "-"
     }
 }
