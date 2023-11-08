a1122d5a9fbfec5f7c0bdc1ce813525486b63170
korjun1993 <corojoon93@naver.com>
2023-11-08 12:07:09 +0900

    fix: 피드백 반영 - Operator 수정
    1. 람다식 단축
    2. validate 함수를 제거하고 DIVIDE operation에 유효성 검증 로직 추가

--- src/main/kotlin/calculator/Operator.kt
+++ src/main/kotlin/calculator/Operator.kt
@@ -1,20 +1,16 @@
 package calculator
 
 enum class Operator(val symbol: String, private val operation: IntOperation) {
-    PLUS("+", IntOperation { number1, number2 -> number1 + number2 }),
-    MINUS("-", IntOperation { number1, number2 -> number1 - number2 }),
-    MULTIPLY("*", IntOperation { number1, number2 -> number1 * number2 }),
-    DIVIDE("/", IntOperation { number1, number2 -> number1 / number2 });
+    PLUS("+", { number1, number2 -> number1 + number2 }),
+    MINUS("-", { number1, number2 -> number1 - number2 }),
+    MULTIPLY("*", { number1, number2 -> number1 * number2 }),
+    DIVIDE("/", { number1, number2 ->
+        if (number2 == 0) throw IllegalArgumentException("0으로 나눌 수 없습니다")
+        else number1 / number2
+    });
 
     fun operate(number1: Int, number2: Int): Int {
-        validate(number2)
         return operation.operate(number1, number2)
-    }
-
-    private fun validate(number2: Int) {
-        if (this == DIVIDE && number2 == 0) {
-            throw IllegalArgumentException("0으로 나눌 수 없습니다")
-        }
     }
 
     companion object {
