01c66192f6e2c058e14b67803f5c0f6650043442
jylim <jylim@crscube.co.kr>
2023-11-08 14:37:28 +0900

    Refac: CarRacingMain

--- src/main/kotlin/carRacing/CarRacingMain.kt
+++ src/main/kotlin/carRacing/CarRacingMain.kt
@@ -11,21 +11,20 @@ import carRacing.view.OutputView
 
 fun main() {
     val messageGenerator: MessageGenerator = MessageGenerator()
+    val outputView: OutputView = OutputView()
 
     val inputView = InputView()
     val carNameList: List<String> = inputView.readLineList(messageGenerator.getMessage(Message.CAR_NAME_QUESTION))
     val tryCount: Int = inputView.readLineNumber(messageGenerator.getMessage(Message.TRY_COUNT_QUESTION))
 
     println()
-    println(messageGenerator.getMessage(Message.RESULT))
+    outputView.printWithMessage(Message.RESULT)
 
     val carList: List<Car> = RandomMovementCarFactory().createCarList(carNameList)
 
     val racing = Racing(carList)
     val racingResult: RacingResult = racing.getRaceResult(tryCount)
-
-    val outputView: OutputView = OutputView()
     racingResult.getRacingResultHistory().forEach(outputView::printCarNameAndPosition)
 
-    println(messageGenerator.getMessage(Message.WINNER, racingResult.getWinnerNames().joinToString(", ")))
+    outputView.printWithMessage(Message.WINNER, racingResult.getWinnerNames().joinToString(", "))
 }
--- src/main/kotlin/carRacing/view/OutputView.kt
+++ src/main/kotlin/carRacing/view/OutputView.kt
@@ -1,8 +1,12 @@
 package carRacing.view
 
 import carRacing.domain.Car
+import carRacing.enum.Message
 
 class OutputView {
+
+    private val messageGenerator: MessageGenerator = MessageGenerator()
+
     fun printMessageInBox(message: String) {
         println("====================================")
         println(message)
@@ -33,5 +37,9 @@ class OutputView {
 
     private fun printCarNameAndPosition(car: Car) {
         println("${car.name} : ${car.getPosition()}")
+    }
+
+    fun printWithMessage(message: Message, vararg messages: String) {
+        println(messageGenerator.getMessage(message, *messages))
     }
 }
