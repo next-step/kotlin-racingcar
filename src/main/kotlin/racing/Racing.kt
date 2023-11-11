618e8f2a437c81aef42d4ed187bb78b3ae1dc437
Jiwon Kwon <jiwonkwon419@gmail.com>
2023-11-11 22:02:59 +0900

    step4 refactor: 불필요한 move 함수 삭제 filter와 forEach로 대체

--- src/main/kotlin/racing/Racing.kt
+++ src/main/kotlin/racing/Racing.kt
@@ -5,15 +5,9 @@ import racing.ui.ResultView
 class Racing(val cars: List<Car>, val tryCount: Int) {
     fun startRacing(movable: () -> Boolean): List<Car> {
         for (i in 0 until this.tryCount) {
-            this.cars.forEach { car -> move(movable, car) }
+            this.cars.filter { movable() }.forEach { it.move() }
         }
         ResultView.printResult(this.cars)
         return this.cars
-    }
-
-    private fun move(movable: () -> Boolean, car: Car) {
-        if (movable()) {
-            car.move()
-        }
     }
 }
