700c666fb75439ec302b39c7bf8bf0c915dbf308
Jiwon Kwon <jiwonkwon419@gmail.com>
2023-11-11 21:13:30 +0900

    step4 test: racing 테스트 케이스 추가

--- src/main/kotlin/racing/Racing.kt
+++ src/main/kotlin/racing/Racing.kt
@@ -2,7 +2,7 @@ package racing
 
 import racing.ui.ResultView
 
-class Racing(private val cars: List<Car>, private val tryCount: Int) {
+class Racing(val cars: List<Car>, val tryCount: Int) {
     fun startRacing(movable: () -> Boolean) {
         for (i in 0 until this.tryCount) {
             this.cars.forEach { car -> move(movable, car) }
--- src/test/kotlin/racing/RacingTest.kt
+++ src/test/kotlin/racing/RacingTest.kt
@@ -4,8 +4,37 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 class RacingTest : FunSpec({
+    test("racing prepare시 position은 0으로 초기화") {
+        // Given
+        val names: List<String> = listOf("Jiwon", "Alex", "Bob")
+        val cars = CarNameMapper.map(names)
 
-    test("racing start시 position이 1씩 증가") {
+        // When
+        val racing = Racing(cars, 5)
+
+        // Then
+        for (car in racing.cars) {
+            car.position shouldBe 0
+        }
+    }
+
+    test("racing start시 movable flase면 정지") {
+        // Given
+        val names: List<String> = listOf("Jiwon", "Alex", "Bob")
+        val cars = CarNameMapper.map(names)
+        val movable = { false }
+        val racing = Racing(cars, 5)
+
+        // When
+        racing.startRacing(movable)
+
+        // Then
+        for (car in cars) {
+            car.position shouldBe 0
+        }
+    }
+
+    test("racing start시 movable true면 position이 1씩 증가") {
         // Given
         val names: List<String> = listOf("Jiwon", "Alex", "Bob")
         val cars = CarNameMapper.map(names)
@@ -17,7 +46,7 @@ class RacingTest : FunSpec({
 
         // Then
         for (car in cars) {
-            car.position shouldBe 1
+            car.position shouldBe 5
         }
     }
 })
