ba0a2dd1c8103f4a043f64692d4514d2186dee2a
korjun1993 <corojoon93@naver.com>
2023-11-12 00:11:53 +0900

    fix: 피드백 반영 - lateinit 키워드를 통한 테스트 픽스처 초기화 반복

--- src/test/kotlin/racingcar/RacingGameTest.kt
+++ src/test/kotlin/racingcar/RacingGameTest.kt
@@ -9,14 +9,14 @@ import racingcar.domain.RacingGame
 class RacingGameTest : ShouldSpec({
 
     val tryCount = 10
-    val cars: List<Car> = listOf(
-        Car(name = "pobi"),
-        Car(name = "crong"),
-        Car(name = "honux")
-    )
+    lateinit var cars: List<Car>
 
     beforeTest {
-        cars.forAll { it.location = Car.CAR_DEFAULT_LOCATION }
+        cars = listOf(
+            Car(name = "pobi"),
+            Car(name = "crong"),
+            Car(name = "honux")
+        )
     }
 
     context("N대의 자동차를 M번 이동하는 게임이 시작되면") {
@@ -34,14 +34,16 @@ class RacingGameTest : ShouldSpec({
 
         context("게임 횟수마다 자동차가 전진한다면") {
             should("각 자동차의 위치는 초기 위치로부터 tryCount 만큼 이동한 위치이다") {
-                RacingGame(cars, tryCount).run { (4..9).random() }
-                cars.forAll { it.location shouldBe tryCount }
+                RacingGame(cars, tryCount).run { 4 }
+                cars[0].location shouldBe tryCount
+                cars[1].location shouldBe tryCount
+                cars[2].location shouldBe tryCount
             }
         }
 
         context("게임 횟수마다 자동차가 정지한다면") {
             should("각 자동차의 위치는 초기 위치이다") {
-                RacingGame(cars, tryCount).run { (0..3).random() }
+                RacingGame(cars, tryCount).run { 0 }
                 cars.forAll { it.location shouldBe 0 }
             }
         }
