dd1616d9b3cb9933ed4f4f20127a40f649a15fb8
hyotaek-jang <hyotaek.jang@linecorp.com>
2023-11-11 09:49:02 +0900

    refactor: add moveCars test

--- src/test/kotlin/game/racing/domain/RacingManagerTest.kt
+++ src/test/kotlin/game/racing/domain/RacingManagerTest.kt
@@ -1,6 +1,7 @@
 package game.racing.domain
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -17,5 +18,14 @@ internal class RacingManagerTest {
         }
 
         assertThat(racingManager.determineWinner()).allMatch { it.position == Car.MIN_POSITION + 1 }.allMatch { it.name == "WIN" }
+    }
+
+    @Test
+    fun `자동차가 움직인다`() {
+        val cars = arrayOf(Car("TEST1"), Car("TEST2"), Car("TEST3"))
+        val racingManager = RacingManager(cars.toList())
+        racingManager.moveCars { Car.MIN_MOVE_THRESHOLD }
+
+        assertThat(cars).allMatch { it.position == Car.MIN_POSITION + 1 }
     }
 }
