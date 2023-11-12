6424d396605629f6c48ed89d34b69d9c67c10e52
khcho902 <khcho902@gmail.com>
2023-11-12 14:46:13 +0900

    refactor: Cars 일급 컬렉션 추가(객체지향 생활 체조 8번)

--- src/main/kotlin/racing_car/domain/RacingGame.kt
+++ src/main/kotlin/racing_car/domain/RacingGame.kt
@@ -1,39 +1,27 @@
 package racing_car.domain
 
 class RacingGame(
-    private val cars: List<Car>,
+    cars: List<Car>,
     racingRound: Int,
 ) {
     private val roundInfo: RoundInfo = RoundInfo(totalRound = racingRound)
-
-    init {
-        require(cars.size >= 2) { "경주에 필요한 자동차 대수는 2대 이상입니다." }
-    }
+    private val cars: Cars = Cars(cars)
 
     val carInfos: List<CarInfo>
-        get() = cars.map { it.carInfo }
+        get() = cars.carInfos
 
     val isContinuable: Boolean
         get() = roundInfo.isContinuable
 
-    val isFinish: Boolean
-        get() = roundInfo.isFinish
-
     fun move() {
-        check(isContinuable) { "시도회수를 초과하였습니다." }
-        cars.forEach(Car::move)
+        check(roundInfo.isContinuable) { "시도회수를 초과하였습니다." }
+        cars.moveAll()
         roundInfo.increaseCurrentRound()
     }
 
     fun judgeWinners(): List<String> {
-        check(isFinish) { "아직 경주가 끝나지 않았습니다." }
-
-        val winners = cars.filter { car ->
-            cars.all { otherCar ->
-                car.isWin(otherCar) || car.isDraw(otherCar)
-            }
-        }
-
-        return winners.map(Car::name)
+        check(roundInfo.isFinish) { "아직 경주가 끝나지 않았습니다." }
+        val winningCars = cars.getWinningCars()
+        return winningCars.map(Car::name)
     }
 }
