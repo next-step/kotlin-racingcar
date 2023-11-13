7d19b0c78e88ec10a438de7baaaf2591f1b100b6
zelda-jeong <zelda.jeong@kakaocorp.com>
2023-11-13 17:31:28 +0900

    step4: step3 리뷰 반영 (Car 초기 위치 테스트 추가)

--- src/test/kotlin/racing/CarTest.kt
+++ src/test/kotlin/racing/CarTest.kt
@@ -4,6 +4,17 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class CarTest {
+    
+    @Test
+    fun `자동차의 첫 위치는 0이다`() {
+        val car = object : Car() {
+            override fun moveOrNot(): Boolean {
+                return true
+            }
+        }
+
+        assertThat(car.position).isEqualTo(0)
+    }
 
     @Test
     fun `랜덤 결과 움직이는 경우`() {
@@ -12,7 +23,7 @@ class CarTest {
                 return true
             }
         }
-        assertThat(car.position).isEqualTo(0)
+        
         car.move()
         assertThat(car.position).isEqualTo(1)
     }
@@ -24,7 +35,7 @@ class CarTest {
                 return false
             }
         }
-        assertThat(car.position).isEqualTo(0)
+        
         car.move()
         assertThat(car.position).isEqualTo(0)
     }
