4edbb7850caccb431729902b85993012f49ed095
david-sang <david.sang@kakaocorp.com>
2023-11-13 13:56:07 +0900

    feat(step5): IllegalStateException 확인하는 부분 check로 변경

--- src/main/kotlin/carracing/domain/Racing.kt
+++ src/main/kotlin/carracing/domain/Racing.kt
@@ -8,14 +8,12 @@ class Racing(
         private set
     val winners: List<String> by lazy { decideWinner() }
 
-    private fun decideWinner() = when (isFinish) {
-        true -> {
-            val maxCount = cars.maxOf { it.movingCount }
-            cars
-                .filter { it.movingCount == maxCount }
-                .map { it.name }
-        }
-        false -> throw IllegalStateException("'winners' can only be determined after racing finish")
+    private fun decideWinner(): List<String> {
+        check(isFinish) { "'winners' can only be determined after racing finish" }
+        val maxCount = cars.maxOf { it.movingCount }
+        return cars
+            .filter { it.movingCount == maxCount }
+            .map { it.name }
     }
 
     fun playRound() {
@@ -35,9 +33,7 @@ class Racing(
             .toList()
 
     fun finish(): List<String> {
-        if (isFinish) {
-            throw IllegalStateException("racing has already finished")
-        }
+        check(!isFinish) { "racing has already finished" }
         isFinish = true
         return winners
     }
