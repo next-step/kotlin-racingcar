f751a706d1e0e9da6411c023d94effeff4162b56
zelda-jeong <zelda.jeong@kakaocorp.com>
2023-11-13 17:30:59 +0900

    step4: step3 리뷰 반영 (상수화)

--- src/main/kotlin/racing/Car.kt
+++ src/main/kotlin/racing/Car.kt
@@ -3,16 +3,23 @@ package racing
 import kotlin.random.Random
 
 open class Car(
-    var position: Int = 0
+    var position: Int = INIT_POSITION
 ) {
     fun move() {
         if (moveOrNot()) {
-            position += 1
+            position += POSITION_INCREMENT
         }
     }
 
     open fun moveOrNot(): Boolean {
-        return Random.nextInt(10) > 4
+        return Random.nextInt(RANDOM_LIMIT) > MOVE_POSITION_CONDITION
+    }
+    
+    companion object {
+        private const val INIT_POSITION = 0
+        private const val POSITION_INCREMENT = 1
+        private const val RANDOM_LIMIT = 10
+        private const val MOVE_POSITION_CONDITION = 4
     }
 
 }
