96e529de1d695418c3dd5acb80a8f12d124c4786
zelda-jeong <zelda.jeong@kakaocorp.com>
2023-11-17 06:16:05 +0900

    step4 테스트코드 수정

--- src/test/kotlin/racing/CarTest.kt
+++ src/test/kotlin/racing/CarTest.kt
@@ -7,46 +7,39 @@ class CarTest {
     
     @Test
     fun `자동차의 첫 위치는 0이다`() {
-        val car = object : Car() {
-            override fun moveOrNot(): Boolean {
-                return true
-            }
-        }
+        val car = Car()
 
         assertThat(car.position).isEqualTo(0)
     }
 
     @Test
     fun `랜덤 결과 움직이는 경우`() {
-        val car = object : Car() {
-            override fun moveOrNot(): Boolean {
-                return true
-            }
-        }
+        val car = Car()
+        val condition = 8
         
-        car.move()
+        car.move(condition)
         assertThat(car.position).isEqualTo(1)
     }
 
     @Test
     fun `랜덤 결과 안 움직이는 경우`() {
-        val car = object : Car() {
-            override fun moveOrNot(): Boolean {
-                return false
-            }
-        }
+        val car = Car()
+        val condition = 1
         
-        car.move()
+        car.move(condition)
         assertThat(car.position).isEqualTo(0)
     }
     
     @Test
-    fun `자동차 이름`() {
-        val car = Car()
-        assertThat(car.name).isEqualTo("이름 없음")
-        
+    fun `자동차 이름을 설정한다`() {
         val name = "지훈"
         val carWithName = Car(name)
         assertThat(carWithName.name).isEqualTo(name)
+    }
+    
+    @Test
+    fun `자동차는 이름을 주지 않더라도 기본 이름을 가진다`() {
+        val car = Car()
+        assertThat(car.name).isEqualTo("이름 없음")
     }
 }
