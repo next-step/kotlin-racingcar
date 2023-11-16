dfa9bb96a2dd7b507128e4ec48c4812b95eed6ea
zelda-jeong <zelda.jeong@kakaocorp.com>
2023-11-17 06:15:37 +0900

    step4 viewCar 함수 분리

--- src/main/kotlin/racing/ResultView.kt
+++ src/main/kotlin/racing/ResultView.kt
@@ -3,16 +3,20 @@ package racing
 class ResultView {
     fun view(cars: List<Car>) {
         cars.forEach {
-            print("${it.name} : ")
-            repeat(it.position) {
-                print("-")
-            }
-            println()
+            viewCar(it)
         }
         println()
     }
     
     fun viewWinner(names: List<String>) {
         println("${names.joinToString(", ")}(이)가 최종 우승했습니다.")
+    }
+    
+    private fun viewCar(car: Car) {
+        print("${car.name} : ")
+        repeat(car.position) {
+            print("-")
+        }
+        println()
     }
 }
