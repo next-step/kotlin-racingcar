ca7def0ef902b4b637674d76471cd568852efdfd
zelda-jeong <zelda.jeong@kakaocorp.com>
2023-11-19 16:23:27 +0900

    step5 랜덤값 받는 함수 분리

--- src/main/kotlin/racing/Application.kt
+++ src/main/kotlin/racing/Application.kt
@@ -1,17 +1,24 @@
 package racing
 
+import racing.game.Game
+import racing.view.InputView
+import racing.view.ResultView
+import kotlin.random.Random
+
 fun main() {
+    val randomLimit = 10
     val inputView = InputView()
     val resultView = ResultView()
     val cars = inputView.getNames("자동차 이름을 입력하세요")
     val tryCount = inputView.get("시도할 횟수는 몇 회인가요?")
-    
+
     val game = Game(cars)
 
     repeat(tryCount) {
-        val cars = game.run()
-        resultView.view(cars)
+        val conditions = cars.map { Random.nextInt(randomLimit) }
+        val runners = game.run(conditions)
+        resultView.view(runners)
     }
-    
+
     resultView.viewWinner(game.end())
 }
