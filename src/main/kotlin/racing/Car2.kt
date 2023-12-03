eb0d2e4bcb8a9e357199c0ec7e71efc2548b2515
OYJ <oyj7677@gmail.com>
2023-12-03 19:09:28 +0900

    Feat: 자동차 전진 기능 구현
    Refactor: 자동차 위치 값 Backing Property 적용

--- src/main/kotlin/racing/Car2.kt
+++ src/main/kotlin/racing/Car2.kt
@@ -1,7 +1,17 @@
 package racing
 
-class Car2(val name: String, position: Int) {
+class Car2(val name: String, var _positon: Int) {
+
     init {
-        require(name.length <= 5) {"자동차 이름은 5자 이하로 해주세요."}
+        require(name.length <= 5) { "자동차 이름은 5자 이하로 해주세요." }
+    }
+
+    val position
+        get() = _positon
+
+
+
+    fun move() {
+        _positon++
     }
 }
--- src/test/kotlin/racing/Car2Test.kt
+++ src/test/kotlin/racing/Car2Test.kt
@@ -6,14 +6,30 @@ import org.junit.jupiter.api.Test
 class Car2Test {
     @Test
     fun `자동차 이름을 입력받고, 자동차 객체 생성 시 이름이 5자를 초과할 때, 예외를 던진다`() {
-        // given :
+        // given : 자동차 이름을 입력받는다.
         val name = "oyjoyj"
         val position = 0
 
-        // when :
-        val actual = runCatching { Car2(name = name, position = position) }.exceptionOrNull()
+        // when : 자동차 객체 생성 시 이름이 5자를 초과할 때
+        val actual = runCatching { Car2(name = name, _positon = position) }.exceptionOrNull()
 
-        // then :
+        // then : 예외를 던진다
         assertThat(actual).isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @Test
+    fun `,자동차 전진 요청 시, position이 1 증가한다`() {
+        // given :
+        val name = "oyj"
+        val position = 0
+        val car = Car2(name = name, _positon = position)
+
+        // when : 자동차 전진 요청(기본 위치는 0)
+        car.move()
+        car.move()
+        val actual = car.position
+
+        // then : 위치가 1 증가한다.
+        assertThat(actual).isEqualTo(2)
     }
 }
