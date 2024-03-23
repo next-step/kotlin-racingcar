e1e4230d591f255107bf76cfa21261661361ce1f
김병화 <binghe819@woowahan.com>
2024-03-23 16:42:42 +0900

    feat: 5단계 구현

--- src/main/kotlin/racingcar/application/RacingService.kt
+++ src/main/kotlin/racingcar/application/RacingService.kt
@@ -6,6 +6,9 @@ import racingcar.domain.RacingGame
 import racingcar.domain.RandomEngine
 import racingcar.presentation.OutputView
 
+/**
+ * MVC 패턴상에선 RacingService가 Controller 역할. (도메인들의 비즈니스 처리를 위임하고 순서를 관장하는 역할)
+ */
 class RacingService {
 
     fun playNewRacingGameToEnd(listOfCarsName: List<String>, countOfRacingRound: Int) {
