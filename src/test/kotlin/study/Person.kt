5d4981632edc9158f86015add15aefc583fe2b55
Sara Han <sarahan774@gmail.com>
2024-11-12 22:19:22 +0900

    [study] kotlin basics

---
+++ src/test/kotlin/study/Person.kt
@@ -0,0 +1,27 @@
+package study
+
+class Person(
+    val name: String,
+    val age: Int,
+    var nickname: String? = name,
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Person
+
+        if (name != other.name) return false
+        if (age != other.age) return false
+        if (nickname != other.nickname) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + age
+        result = 31 * result + (nickname?.hashCode() ?: 0)
+        return result
+    }
+}
