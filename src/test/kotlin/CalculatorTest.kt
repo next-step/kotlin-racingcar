6bc2bf6d86de0c946f83191ce79dea053ef4cfc5
김태현(TaeHyeon, Kim) <thk98k@gmail.com>
2024-11-14 21:08:46 +0900

    test: 문자열 사칙 연산 계산기 테스트 코드 작성

---
+++ src/test/kotlin/CalculatorTest.kt
@@ -0,0 +1,47 @@
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+
+class CalculatorTest : DescribeSpec({
+    lateinit var sut: Calculator
+    beforeTest { sut = Calculator() }
+
+    describe("A calculator that performs arithmetic operations based on the user's input string") {
+        context("Not Null and Not Empty String") {
+            it("should correctly perform the operations in the given sequence") {
+                val input = "2 + 3 * 4 / 2"
+                sut.calculate(input) shouldBe 10
+            }
+        }
+
+        context("null String") {
+            it("should throw an exception") {
+                val input: String? = null
+
+                shouldThrow<IllegalArgumentException> {
+                    sut.calculate(input)
+                }
+            }
+        }
+
+        context("empty String") {
+            it("should throw an exception") {
+                val input = ""
+
+                shouldThrow<IllegalArgumentException> {
+                    sut.calculate(input)
+                }
+            }
+        }
+
+        context("not operational symbols") {
+            it("should throw an exception") {
+                val input = "2 + 3 * 4 & 2"
+
+                shouldThrow<IllegalArgumentException> {
+                    sut.calculate(input)
+                }
+            }
+        }
+    }
+})
