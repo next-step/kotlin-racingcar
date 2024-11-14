8c2adc534d4e5bb44385e501ca4db0e0d34d9b4c
김태현(TaeHyeon, Kim) <thk98k@gmail.com>
2024-11-14 22:15:21 +0900

    fix: isEmpty -> isBlack 변경

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,6 +1,25 @@
 class Calculator {
     fun calculate(input: String?) {
-        val express = input.orEmpty()
-        require(express.isNotEmpty()) { "Input must have a value" }
+        require(!input.isNullOrBlank()) { "Input must have a value" }
     }
+
+    fun sum(
+        a: Int,
+        b: Int,
+    ): Int = a + b
+
+    fun subtract(
+        a: Int,
+        b: Int,
+    ): Int = a - b
+
+    fun multiply(
+        a: Int,
+        b: Int,
+    ): Int = a * b
+
+    fun divide(
+        a: Int,
+        b: Int,
+    ): Int = a / b
 }
