ee758a469452f11a985433b71010c332b6c1b5d8
김태현(TaeHyeon, Kim) <thk98k@gmail.com>
2024-11-16 02:11:52 +0900

    test: 입력 유효성 검사 테스트

--- src/test/kotlin/UserInputTest.kt
+++ src/test/kotlin/UserInputTest.kt
@@ -1,39 +1,70 @@
 import exception.NotImplementedTestException
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.assertThrows
+import util.validInputToInt
 
 class UserInputTest : DescribeSpec({
+
     describe("receives input from user") {
         context("input is null") {
             it("should throw an exception") {
-                throw NotImplementedTestException()
+                val str: String? = null
+
+                val exception =
+                    assertThrows<IllegalArgumentException> {
+                        str.validInputToInt()
+                    }
+
+                exception.message shouldBe "유효한 값을 입력해주세요"
             }
         }
 
         context("input is blank") {
             it("should throw an exception") {
-                throw NotImplementedTestException()
+                val str = "  "
+
+                val exception =
+                    assertThrows<IllegalArgumentException> {
+                        str.validInputToInt()
+                    }
+                exception.message shouldBe "유효한 값을 입력해주세요"
             }
         }
 
         context("input less than 1") {
             it("should throw an exception") {
-                throw NotImplementedTestException()
+                val exception =
+                    assertThrows<IllegalArgumentException> {
+                        "0".validInputToInt()
+                    }
+                exception.message shouldBe "유효한 값을 입력해주세요"
             }
         }
 
         context("when the input is not an Int type") {
             it("throws an exception for a String input") {
-                throw NotImplementedTestException()
+                val exception =
+                    assertThrows<IllegalArgumentException> {
+                        "str".validInputToInt()
+                    }
+
+                exception.message shouldBe "유효한 값을 입력해주세요"
             }
 
             it("throws an exception for a Double input") {
-                throw NotImplementedTestException()
+                val exception =
+                    assertThrows<IllegalArgumentException> {
+                        "1.1".validInputToInt()
+                    }
+
+                exception.message shouldBe "유효한 값을 입력해주세요"
             }
         }
 
         context("input is a positive number") {
-            it("returns the input as is") {
-                throw NotImplementedTestException()
+            it("test") {
+                "10".validInputToInt() shouldBe 10
             }
         }
     }
