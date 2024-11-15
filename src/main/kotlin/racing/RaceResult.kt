5190014f35851a04924fb648950685257fe68088
sian <chicori3@naver.com>
2024-11-15 23:29:53 +0900

    test: 테스트 검증용 로직 추가

--- src/main/kotlin/racing/RaceResult.kt
+++ src/main/kotlin/racing/RaceResult.kt
@@ -1,5 +1,7 @@
 package racing
 
+import org.jetbrains.annotations.TestOnly
+
 class RaceResult {
     private val results = mutableListOf<Result>()
 
@@ -9,5 +11,15 @@ class RaceResult {
 
     fun getResults(): List<Result> {
         return results.toList()
+    }
+
+    @TestOnly
+    fun getRoundCount(): Int {
+        return results.map(Result::round).distinct().size
+    }
+
+    @TestOnly
+    fun getCarCount(): Int {
+        return results.map(Result::carId).distinct().size
     }
 }
