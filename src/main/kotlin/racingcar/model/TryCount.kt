df4a6bada746dc7a6262cade4f98ee039dcc685a
KimDabomi <cdabomi@nate.com>
2024-11-16 17:21:34 +0900

    [step3] refactor:saveTryCount 삭제

--- src/main/kotlin/racingcar/model/TryCount.kt
+++ src/main/kotlin/racingcar/model/TryCount.kt
@@ -5,18 +5,10 @@ import racingcar.model.exception.TryCountException
 private const val TRY_COUNT_MIN = 1
 
 class TryCount {
-    fun getTryCount(userTryCount: Int): Int {
-        val tryCount = saveTryCount(userTryCount)
+    fun getTryCount(tryCount: Int): Int {
         if (tryCount < TRY_COUNT_MIN) {
             throw TryCountException()
         }
         return tryCount
-    }
-
-    private fun saveTryCount(userTryCount: Int): Int {
-        if (userTryCount < TRY_COUNT_MIN) {
-            throw TryCountException()
-        }
-        return userTryCount
     }
 }
