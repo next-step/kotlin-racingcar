a63494f1708eb56ce7a46e96c79a87bca90ec31c
SeokHo-Ham <forkyy.dev@gmail.com>
2024-11-16 14:08:47 +0900

    refactor: fold 메서드 적용, 상수화 적용, 예외 메세지 수정"

--- src/main/kotlin/stringCalculator/StringCalculator.kt
+++ src/main/kotlin/stringCalculator/StringCalculator.kt
@@ -2,26 +2,27 @@ package stringCalculator
 
 class StringCalculator {
     companion object {
-        const val STEP_SIZE = 2
+        const val SPACE_SYMBOL = " "
     }
 
     fun calculate(expression: String?): Int {
         val expressionValues = convertExpressionToList(expression)
-        var result = validateIsNumberAndConvert(expressionValues[0])
-        for (i in 1 until expressionValues.size - 1 step STEP_SIZE) {
-            val operator = Operator.findByOperator(expressionValues[i])
-            val rightValue = validateIsNumberAndConvert(expressionValues[i + 1])
-            result = operator.apply(result, rightValue)
-        }
-        return result
+        return expressionValues
+            .drop(1)
+            .chunked(2)
+            .fold(validateIsNumberAndConvert(expressionValues[0])) { acc, (op, right) ->
+                val operator = Operator.findByOperator(op)
+                val rightValue = validateIsNumberAndConvert(right)
+                operator.apply(acc, rightValue)
+            }
     }
 
     private fun convertExpressionToList(expression: String?): List<String> {
         if (expression.isNullOrEmpty()) throw IllegalArgumentException("null 또는 공백은 전달할 수 없습니다.")
-        return expression.split(" ")
+        return expression.split(SPACE_SYMBOL)
     }
 
     private fun validateIsNumberAndConvert(str: String): Int {
-        return str.toIntOrNull() ?: throw IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다.")
+        return str.toIntOrNull() ?: throw IllegalArgumentException("${str}은 숫자가 아닙니다.")
     }
 }
