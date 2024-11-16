be13880685626fa9255740f4645b8b7f86c944df
dino.shin <troas96@naver.com>
2024-11-16 15:37:13 +0900

    feat: 자동차 이름(CarName) 구현

---
+++ src/test/kotlin/racingcar/CarNameTest.kt
@@ -0,0 +1,25 @@
+package racingcar
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Assertions.assertThrows
+
+class CarNameTest : StringSpec({
+    "이름 (CarName)은 5자를 초과할 수 없고 공백이 될 수 없다." {
+        forAll(
+            row(""),
+            row("dinosaur"),
+            row("GoodDinoBadDino"),
+            row(" Dino "),
+            row("한글여섯글자"),
+        ) { name ->
+            val exception =
+                assertThrows(IllegalArgumentException::class.java) {
+                    CarName(name)
+                }
+            exception.message shouldBe "자동차 이름은 1자 이상, 5자 이하만 가능합니다."
+        }
+    }
+})
