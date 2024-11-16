95aa0ec32869ee1ac82fcbadb077b7f449b21ef3
dino.shin <troas96@naver.com>
2024-11-17 00:21:50 +0900

    refactor: CarName value class로 변경

--- src/main/kotlin/racingcar/CarName.kt
+++ src/main/kotlin/racingcar/CarName.kt
@@ -1,6 +1,7 @@
 package racingcar
 
-class CarName(val value: String) {
+@JvmInline
+value class CarName(val value: String) {
     init {
         require(value.length in 1..5) { "자동차 이름은 1자 이상, 5자 이하만 가능합니다." }
     }
