40ee8f473ea3afe8c8c1133f20617e46ac25cd9c
wooobo <irooori89@gmail.com>
2024-11-16 19:37:47 +0900

    refactor: 예외 테스트 수정

--- src/test/kotlin/race/NameTest.kt
+++ src/test/kotlin/race/NameTest.kt
@@ -1,13 +1,13 @@
 package race
 
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.provider.EmptySource
 
 class NameTest {
     @Test
     fun `5자 초과시 예외발생`() {
-        assertThrows<IllegalArgumentException> {
+        assertThatIllegalArgumentException().isThrownBy {
             Name("1234567")
         }
     }
@@ -15,7 +15,7 @@ class NameTest {
     @Test
     @EmptySource
     fun `빈 문자열 입력시 예외발생`() {
-        assertThrows<IllegalArgumentException> {
+        assertThatIllegalArgumentException().isThrownBy {
             Name("")
         }
     }
