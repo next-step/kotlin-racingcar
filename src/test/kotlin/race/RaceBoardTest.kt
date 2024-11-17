b3b86de8ece19d64c35a4f140afb9c4aefd37d9e
wooobo <irooori89@gmail.com>
2024-11-17 14:43:04 +0900

    test: 테스트 리팩토링

--- src/test/kotlin/race/RaceBoardTest.kt
+++ src/test/kotlin/race/RaceBoardTest.kt
@@ -11,20 +11,10 @@ class RaceBoardTest {
     @MethodSource("winnerTestData")
     fun `start 우승자를 알수있다`(
         cars: Cars,
-        manualMoveConditionIterator: ManualMoveConditionIterator,
+        conditions: List<isMove>,
         winners: List<String>,
     ) {
-        val moveConditions =
-            MoveConditions(
-                listOf(
-                    MoveCondition(PositiveNumber(1)),
-                    MoveCondition(PositiveNumber(1)),
-                    MoveCondition(PositiveNumber(1)),
-                    MoveCondition(PositiveNumber(5)),
-                    MoveCondition(PositiveNumber(5)),
-                    MoveCondition(PositiveNumber(5)),
-                ),
-            )
+        val moveConditions = MoveConditions(conditions)
         val raceBoard = RaceBoard(cars, moveConditions)
 
         val actual = raceBoard.start()
@@ -44,13 +34,7 @@ class RaceBoardTest {
                             Car("c", 3),
                         ),
                     ),
-                    ManualMoveConditionIterator(
-                        listOf(
-                            MoveCondition(PositiveNumber(1)),
-                            MoveCondition(PositiveNumber(2)),
-                            MoveCondition(PositiveNumber(5)),
-                        ),
-                    ),
+                    listOf(false, false, true),
                     listOf("c"),
                 ),
                 Arguments.of(
@@ -61,13 +45,7 @@ class RaceBoardTest {
                             Car("c", 3),
                         ),
                     ),
-                    ManualMoveConditionIterator(
-                        listOf(
-                            MoveCondition(PositiveNumber(1)),
-                            MoveCondition(PositiveNumber(5)),
-                            MoveCondition(PositiveNumber(5)),
-                        ),
-                    ),
+                    listOf(false, true, true),
                     listOf("b", "c"),
                 ),
                 Arguments.of(
@@ -78,13 +56,7 @@ class RaceBoardTest {
                             Car("c", 1),
                         ),
                     ),
-                    ManualMoveConditionIterator(
-                        listOf(
-                            MoveCondition(PositiveNumber(5)),
-                            MoveCondition(PositiveNumber(5)),
-                            MoveCondition(PositiveNumber(5)),
-                        ),
-                    ),
+                    listOf(true, true, true),
                     listOf("a", "b", "c"),
                 ),
             )
