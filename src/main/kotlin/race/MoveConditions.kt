3fb57d43613dc0eeb6e9b94ea5bab56073c80f52
wooobo <irooori89@gmail.com>
2024-11-17 14:41:35 +0900

    refactor: MoveCondition object 로 변경

--- src/main/kotlin/race/MoveConditions.kt
+++ src/main/kotlin/race/MoveConditions.kt
@@ -1,5 +1,7 @@
 package race
 
-data class MoveConditions(private val values: List<MoveCondition>) : List<MoveCondition> by values {
-    constructor(count: Int, generator: NumberGenerator) : this(List(count) { MoveCondition(generator()) })
+typealias isMove = Boolean
+
+data class MoveConditions(private val values: List<isMove>) : List<isMove> by values {
+    constructor(count: Int, generator: NumberGenerator) : this(List(count) { MoveCondition.shouldMove(generator()) })
 }
