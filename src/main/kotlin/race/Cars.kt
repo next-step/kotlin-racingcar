457c56a8186b7dbd30d8470d74b29af4edd52a2d
wooobo <irooori89@gmail.com>
2024-11-17 13:27:24 +0900

    refactor: `Car, Cars` data clas,class 변경

--- src/main/kotlin/race/Cars.kt
+++ src/main/kotlin/race/Cars.kt
@@ -1,8 +1,8 @@
 package race
 
-class Cars(private val cars: List<Car>) : List<Car> by cars {
+data class Cars(val values: List<Car>) : List<Car> by values {
     init {
-        require(cars.isNotEmpty()) { "최소 1대 이상 입력 해주세요." }
+        require(values.isNotEmpty()) { "최소 1대 이상 입력 해주세요." }
     }
 
     constructor(names: Names) : this(names.map { Car(it, PositiveNumber(1)) })
@@ -11,11 +11,7 @@ class Cars(private val cars: List<Car>) : List<Car> by cars {
         indexedValue: Int,
         next: MoveCondition,
     ) {
-        move(cars[indexedValue], next)
-    }
-
-    fun copy(): Cars {
-        return Cars(cars.map { it.copy() })
+        move(values[indexedValue], next)
     }
 
     private fun move(
