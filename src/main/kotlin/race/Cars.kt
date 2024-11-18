0bc489430287625f9416f330982723bda2f215bd
wooobo <irooori89@gmail.com>
2024-11-18 16:36:33 +0900

    feat: Cars 입력값 구체화 전달

--- src/main/kotlin/race/Cars.kt
+++ src/main/kotlin/race/Cars.kt
@@ -2,7 +2,7 @@ package race
 
 data class Cars(val values: List<Car>) : List<Car> by values {
     init {
-        require(values.isNotEmpty()) { "최소 1대 이상 입력 해주세요." }
+        require(values.isNotEmpty()) { "최소 1대 이상 입력 해주세요. 입력 값: $values" }
     }
 
     constructor(names: Names) : this(names.map { Car(it, PositiveNumber.ONE) })
--- src/test/kotlin/race/CarsTest.kt
+++ src/test/kotlin/race/CarsTest.kt
@@ -1,8 +1,10 @@
 package race
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 
 class CarsTest {
     @Test
@@ -26,8 +28,13 @@ class CarsTest {
 
     @Test
     fun `Cars는 최소 1대 이상 입력해야 한다`() {
-        assertThatIllegalArgumentException().isThrownBy {
-            Cars(Names(""))
-        }
+        assertAll(
+            { assertThatIllegalArgumentException().isThrownBy { Cars(Names("")) } },
+            {
+                assertThatExceptionOfType(
+                    IllegalArgumentException::class.java,
+                ).isThrownBy { Cars(listOf()) }.withMessage("최소 1대 이상 입력 해주세요. 입력 값: []")
+            },
+        )
     }
 }
