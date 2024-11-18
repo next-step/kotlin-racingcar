b6c79aade684d513bdca5b3af0c4f850b7332b66
이상준 <sangjun927@skplanet.com>
2024-11-18 11:35:58 +0900

    refactor: 리뷰에 따른 코드 개선
    https://github.com/next-step/kotlin-racingcar/pull/1645#pullrequestreview-2441290157

--- src/main/kotlin/study/racing/Car.kt
+++ src/main/kotlin/study/racing/Car.kt
@@ -1,6 +1,20 @@
 package study.racing
 
+import kotlin.random.Random
+
 /**
  * @author 이상준
  */
-data class Car(var position: Int = 1)
+class Car {
+    private var position: Int = 0
+
+    fun getPosition(): Int {
+        return this.position
+    }
+
+    fun move() {
+        if (Random.nextInt(GameRule.MIN_RANDOM_POSITION, GameRule.MAX_RANDOM_POSITION) > 3) {
+            this.position += 1
+        }
+    }
+}
--- src/main/kotlin/study/racing/RacingCarGame.kt
+++ src/main/kotlin/study/racing/RacingCarGame.kt
@@ -1,18 +1,14 @@
 package study.racing
 
-import kotlin.random.Random
-
 /**
  * @author 이상준
  */
 class RacingCarGame(
     private val gameSettings: GameSettings,
 ) {
-    private val random = Random
-    private var carList = mutableListOf<Car>()
+    private val carList = mutableListOf<Car>()
 
     private fun init() {
-        this.carList = mutableListOf()
         repeat(gameSettings.carCount) {
             this.carList.add(Car())
         }
@@ -20,7 +16,7 @@ class RacingCarGame(
 
     private fun playGame() {
         carList.forEach {
-            it.position += move()
+            it.move()
             gameMessageView(it)
         }
     }
@@ -34,12 +30,8 @@ class RacingCarGame(
         }
     }
 
-    private fun move(): Int {
-        return if (random.nextInt(GameRule.MIN_RANDOM_POSITION, GameRule.MAX_RANDOM_POSITION) > 3) 1 else 0
-    }
-
     private fun gameMessageView(car: Car) {
-        repeat(car.position) {
+        repeat(car.getPosition()) {
             print(GameRule.RACING_CAR_MOVE_TEXT)
         }
         println()
