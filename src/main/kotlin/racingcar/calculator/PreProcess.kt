edf1e554225dd6dca6f877236fe63216480aba32
std-freejia <std.freejia@gmail.com>
2024-11-23 12:51:28 +0900

    fix: PreProcess 에서 띄어쓰기 문자를 상수처리하도록 변경, !연산자 대신 not() 메서드 쓰도록 변경

--- src/main/kotlin/racingcar/calculator/PreProcess.kt
+++ src/main/kotlin/racingcar/calculator/PreProcess.kt
@@ -3,14 +3,15 @@ package racingcar.calculator
 /** 전처리: 연산자 배열과 숫자 배열로 분리한다 */
 class PreProcess {
     companion object {
-        val validOperator = "+-*/"
+        private const val SPACE = " "
+        private val validOperator = "+-*/"
 
         fun splitBySpace(s: String): List<String> {
-            return s.split(" ")
+            return s.split(SPACE)
         }
 
         fun extractNumbers(input: List<String>): List<Double> {
-            return input.filter { !validOperator.contains(it) }
+            return input.filter { validOperator.contains(it).not() }
                 .map { it.toDouble() }
         }
 
