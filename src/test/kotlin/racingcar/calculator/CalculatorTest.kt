d0a6ed1f33a9ea529aac5397a1f662617ac107af
std-freejia <std.freejia@gmail.com>
2024-11-23 12:50:00 +0900

    fix: Calculator 의 변수들을 노출할 필요가 없으므로 private 을 달아놨음. 테스트코드 작성을 위해 getter 추가함

--- src/test/kotlin/racingcar/calculator/CalculatorTest.kt
+++ src/test/kotlin/racingcar/calculator/CalculatorTest.kt
@@ -5,11 +5,15 @@ import org.junit.jupiter.api.Test
 
 class CalculatorTest {
     @Test
-    fun `계산할 연산식 입력받고 계산기 초기화`() {
+    fun `계산할 연산식 입력받고 숫자만 추출`() {
         val cal = Calculator("3 + 5 - 1")
-        assertThat(cal.inputStringList).isEqualTo(listOf("3", "+", "5", "-", "1"))
-        assertThat(cal.numbers).isEqualTo(listOf(3.0, 5.0, 1.0))
-        assertThat(cal.operators).isEqualTo(listOf("+", "-"))
+        assertThat(cal.getNumbers()).isEqualTo(listOf(3.0, 5.0, 1.0))
+    }
+
+    @Test
+    fun `계산할 연산식 입력받고 연산자만 추출`() {
+        val cal = Calculator("3 + 5 - 1")
+        assertThat(cal.getOperators()).isEqualTo(listOf("+", "-"))
     }
 
     @Test
