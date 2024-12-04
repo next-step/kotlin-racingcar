098e28c2d4fca6944a8012feba1d24fbd9bff8ad
std-freejia <std.freejia@gmail.com>
2024-12-04 16:21:39 +0900

    feat: 입력 뷰, 출력 뷰

---
+++ src/main/kotlin/racingcar/car/InputView.kt
@@ -0,0 +1,15 @@
+package racingcar.car
+
+class InputView {
+    companion object {
+        fun getCarCount(): Int {
+            println("자동차 대수는 몇 대인가요?")
+            return readLine()!!.toInt()
+        }
+
+        fun getTryCount(): Int {
+            println("시도할 횟수는 몇 회인가요?")
+            return readLine()!!.toInt()
+        }
+    }
+}
