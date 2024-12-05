9751a23e04152c026b2b9b351299230a4ad54ee5
std-freejia <std.freejia@gmail.com>
2024-12-05 14:35:49 +0900

    fix: var의 경우, getter, setter 자동 생성 되므로 getPoint() 제거

--- src/main/kotlin/racingcar/car/Car.kt
+++ src/main/kotlin/racingcar/car/Car.kt
@@ -1,14 +1,10 @@
 package racingcar.car
 
 class Car {
-    private var point = 0
+    var point = 0
 
     fun move(x: Int): Car {
         point += x
         return this
-    }
-
-    fun getPoint(): Int {
-        return point
     }
 }
