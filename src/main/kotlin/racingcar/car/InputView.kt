94c86cc23d8f929bc273d80d571f587b49c8fe16
std-freejia <std.freejia@gmail.com>
2024-12-07 10:56:38 +0900

    fix: 사용자 입력 받을 때 requireNotNull 로 체크

--- src/main/kotlin/racingcar/car/InputView.kt
+++ src/main/kotlin/racingcar/car/InputView.kt
@@ -4,12 +4,12 @@ class InputView {
     companion object {
         fun getCarCount(): Int {
             println("자동차 대수는 몇 대인가요?")
-            return readLine()!!.toInt()
+            return requireNotNull(readLine()).toInt()
         }
 
         fun getTryCount(): Int {
             println("시도할 횟수는 몇 회인가요?")
-            return readLine()!!.toInt()
+            return requireNotNull(readLine()).toInt()
         }
     }
 }
