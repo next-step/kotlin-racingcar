37d0bff8e412c3b05ce7c7065ce6a585aefd0b83
std-freejia <std.freejia@gmail.com>
2024-12-07 10:59:21 +0900

    fix: object 키워드로 객체를 정의함과 동시에 생성하도록 변경

--- src/main/kotlin/racingcar/car/RandomMachine.kt
+++ src/main/kotlin/racingcar/car/RandomMachine.kt
@@ -2,11 +2,9 @@ package racingcar.car
 
 import kotlin.random.Random
 
-class RandomMachine {
-    companion object {
-        fun getRandomNumber(): Int {
-            val randomNumber = Random.nextInt(0, 10)
-            return if (randomNumber >= 4) randomNumber else 0
-        }
+object RandomMachine {
+    fun getRandomNumber(): Int {
+        val randomNumber = Random.nextInt(0, 10)
+        return if (randomNumber >= 4) randomNumber else 0
     }
 }
