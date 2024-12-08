edc9b01f66e84b5a7b6cf061c765ca5d6a87842b
이창환 <54737136+2chang5@users.noreply.github.com>
2024-12-08 16:49:16 +0900

    [Step5]자동차 경주 제출합니다. (#1777)
    * feat: IntRange 매번 생성되지 않도록 수정
    
    * feat: Junit으로 동일한 테스트 작성

--- src/main/kotlin/step3/domain/RandomMovePolicy.kt
+++ src/main/kotlin/step3/domain/RandomMovePolicy.kt
@@ -2,11 +2,12 @@ package step3.domain
 
 class RandomMovePolicy : MovePolicy {
     override fun shouldMove(): Boolean {
-        val randomInt = (0..9).random()
+        val randomInt = randomIntRange.random()
         return randomInt >= MOVE_STANDARD
     }
 
     companion object {
+        private val randomIntRange = 0..9
         private const val MOVE_STANDARD = 4
     }
 }
--- src/test/kotlin/step3/domain/CarsTest.kt
+++ src/test/kotlin/step3/domain/CarsTest.kt
@@ -2,6 +2,9 @@ package step3.domain
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
 class CarsTest : StringSpec({
     lateinit var dummyPolicy: MovePolicy
@@ -46,4 +49,54 @@ class CarsTest : StringSpec({
 
         cars.getWinner().sorted() shouldBe listOf("이창환", "이민수", "이창환").sorted()
     }
-})
+}) {
+    lateinit var dummyPolicy: MovePolicy
+
+    @BeforeEach
+    fun setUp() {
+        dummyPolicy =
+            object : MovePolicy {
+                override fun shouldMove(): Boolean = true
+            }
+    }
+
+    @Test
+    fun `이창환 1등임을 판별한다`() {
+        val cars =
+            Cars(
+                Car(name = "이창환", location = 3, movePolicy = dummyPolicy),
+                Car(name = "이민수", location = 1, movePolicy = dummyPolicy),
+                Car(name = "송상현", location = 2, movePolicy = dummyPolicy),
+            )
+
+        val winner = cars.getWinner().sorted()
+        assertEquals(listOf("이창환").sorted(), winner)
+    }
+
+    @Test
+    fun `이창환 이민수 공동 1등임을 판별한다`() {
+        val cars =
+            Cars(
+                Car(name = "이창환", location = 3, movePolicy = dummyPolicy),
+                Car(name = "이민수", location = 3, movePolicy = dummyPolicy),
+                Car(name = "송상현", location = 2, movePolicy = dummyPolicy),
+            )
+
+        val winner = cars.getWinner().sorted()
+        assertEquals(listOf("이민수", "이창환").sorted(), winner)
+    }
+
+    @Test
+    fun `이창환 이창환 이민수 공동 1등임을 판별한다`() {
+        val cars =
+            Cars(
+                Car(name = "이창환", location = 3, movePolicy = dummyPolicy),
+                Car(name = "이창환", location = 3, movePolicy = dummyPolicy),
+                Car(name = "이민수", location = 3, movePolicy = dummyPolicy),
+                Car(name = "송상현", location = 2, movePolicy = dummyPolicy),
+            )
+
+        val winner = cars.getWinner().sorted()
+        assertEquals(listOf("이창환", "이민수", "이창환").sorted(), winner)
+    }
+}
