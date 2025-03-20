8a12167f16b892f4e4045b7fbd6e4937a803c19f
june-777 <wlwhswnsrl96@gmail.com>
2025-03-20 22:33:26 +0100

    feat: validate blank

--- src/main/kotlin/CalculatorParser.kt
+++ src/main/kotlin/CalculatorParser.kt
@@ -1,12 +1,19 @@
 class CalculatorParser {
 
     fun parse(input: String): List<String> {
+        validateBlank(input)
 
         val split = input.trim()
             .split(" ")
             .toList()
 
         return split
+    }
+
+    private fun validateBlank(input: String) {
+        if (input.isBlank()) {
+            throw IllegalArgumentException("Input is blank")
+        }
     }
 
 }
