a813a30b4982f657be23e7e4f0a7c9b9eecf7155
MingyumKim <mingyum.kim@deliveryhero.com>
2025-03-21 16:56:51 +0100

    fix: check edge case (0 or negative) and fix errors

--- README.md
+++ README.md
@@ -20,3 +20,4 @@ Functional Requirements
 - [x] Enter how many rounds will be played
 - [x] Generate number for each round and move car randomly
 - [x] Print the current state of cars for each round
+- [x] Check edge case and fix errors
--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -3,13 +3,21 @@ package view
 class InputView {
     fun enterAmountOfCars(): Int {
         println("How many cars in the race?")
-        return (readlnOrNull() ?:
-        throw IllegalArgumentException("[ERROR] Enter valid number for amount of cars")).toInt()
+        val cars = (readlnOrNull()
+            ?: throw IllegalArgumentException("[ERROR] Enter valid number for amount of cars")).toInt()
+        if (cars <= 0) {
+            throw IllegalArgumentException("[ERROR] Enter positive number for amount of cars")
+        }
+        return cars
     }
 
     fun enterAmountOfRounds(): Int {
         println("How many rounds will be played?")
-        return (readlnOrNull() ?:
-        throw IllegalArgumentException("[ERROR] Enter valid number for amount of rounds")).toInt()
+        val rounds = (readlnOrNull()
+            ?: throw IllegalArgumentException("[ERROR] Enter valid number for amount of rounds")).toInt()
+        if (rounds <= 0) {
+            throw IllegalArgumentException("[ERROR] Enter positive number for amount of rounds")
+        }
+        return rounds
     }
 }
