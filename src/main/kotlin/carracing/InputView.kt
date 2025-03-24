a30249b44689495c288576cb3f4b2cea18fbecd4
june-777 <wlwhswnsrl96@gmail.com>
2025-03-24 23:25:50 +0100

    feat: implement input view

---
+++ src/main/kotlin/carracing/InputView.kt
@@ -0,0 +1,14 @@
+package carracing
+
+class InputView {
+
+    fun inputTotalCar(): Int {
+        println("How many cars are in the race?")
+        return readLine()!!.toInt()
+    }
+
+    fun inputTotalRound(): Int {
+        println("How many rounds will be played?")
+        return readLine()!!.toInt()
+    }
+}
