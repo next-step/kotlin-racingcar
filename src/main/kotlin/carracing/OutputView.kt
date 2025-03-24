d65bee1f4e46c632f80112b947485cad046f6353
june-777 <wlwhswnsrl96@gmail.com>
2025-03-24 23:25:54 +0100

    feat: implement output view

---
+++ src/main/kotlin/carracing/OutputView.kt
@@ -0,0 +1,10 @@
+package carracing
+
+class OutputView(val outputViewResolver: OutputViewResolver) {
+
+    fun printRaceResult(positions: List<Int>) {
+        val raceResult = outputViewResolver.makeCarPositionsView(positions)
+        println(raceResult)
+    }
+
+}
