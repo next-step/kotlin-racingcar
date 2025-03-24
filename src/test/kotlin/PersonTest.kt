a22f600fcfd347da857ddaff53c3f0d3b666c051
MingyumKim <mingyum.kim@deliveryhero.com>
2025-03-24 23:54:43 +0100

    style: reformat indent of tap size

--- src/test/kotlin/PersonTest.kt
+++ src/test/kotlin/PersonTest.kt
@@ -3,46 +3,46 @@ import org.junit.jupiter.api.Assertions.assertAll
 import org.junit.jupiter.api.Test
 
 class PersonTest {
-	data class Person(val name: String, val age: Int?, var nickname: String = name)
+    data class Person(val name: String, val age: Int?, var nickname: String = name)
 
-	@Test
-	fun `named arguments`() {
-		val people = listOf(
-			Person("MingyumKim", 20, "Anna"),
-			Person("MingyumKim", 20, nickname = "Anna"),
-			Person(name = "MingyumKim", nickname = "Anna", age = 20)
-		)
-		assertThat(people).allSatisfy {
-			assertThat(it.name).isEqualTo("MingyumKim")
-			assertThat(it.age).isEqualTo(20)
-			assertThat(it.nickname).isEqualTo("Anna")
-		}
-	}
+    @Test
+    fun `named arguments`() {
+        val people = listOf(
+            Person("MingyumKim", 20, "Anna"),
+            Person("MingyumKim", 20, nickname = "Anna"),
+            Person(name = "MingyumKim", nickname = "Anna", age = 20)
+        )
+        assertThat(people).allSatisfy {
+            assertThat(it.name).isEqualTo("MingyumKim")
+            assertThat(it.age).isEqualTo(20)
+            assertThat(it.nickname).isEqualTo("Anna")
+        }
+    }
 
-	@Test
-	fun `nullable types`() {
-		val person = Person("MingyumKim", null, "Anna")
-		assertAll(
-			{ assertThat(person.name).isEqualTo("MingyumKim") },
-			{ assertThat(person.age).isNull() },
-			{ assertThat(person.nickname).isEqualTo("Anna") }
-		)
-	}
+    @Test
+    fun `nullable types`() {
+        val person = Person("MingyumKim", null, "Anna")
+        assertAll(
+            { assertThat(person.name).isEqualTo("MingyumKim") },
+            { assertThat(person.age).isNull() },
+            { assertThat(person.nickname).isEqualTo("Anna") }
+        )
+    }
 
-	@Test
-	fun `default arguments`() {
-		val person = Person("MingyumKim", null)
-		assertAll(
-			{ assertThat(person.name).isEqualTo("MingyumKim") },
-			{ assertThat(person.age).isNull() },
-			{ assertThat(person.nickname).isEqualTo("MingyumKim") }
-		)
-	}
+    @Test
+    fun `default arguments`() {
+        val person = Person("MingyumKim", null)
+        assertAll(
+            { assertThat(person.name).isEqualTo("MingyumKim") },
+            { assertThat(person.age).isNull() },
+            { assertThat(person.nickname).isEqualTo("MingyumKim") }
+        )
+    }
 
-	@Test
-	fun `data classes`() {
-		val person1 = Person("MingyumKim", 20, "Anna")
-		val person2 = Person("MingyumKim", 20, "Anna")
-		assertThat(person1).isEqualTo(person2)
-	}
+    @Test
+    fun `data classes`() {
+        val person1 = Person("MingyumKim", 20, "Anna")
+        val person2 = Person("MingyumKim", 20, "Anna")
+        assertThat(person1).isEqualTo(person2)
+    }
 }
--- src/test/kotlin/model/RacingCarTest.kt
+++ src/test/kotlin/model/RacingCarTest.kt
@@ -5,19 +5,19 @@ import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
 class RacingCarTest {
-	@Test
-	fun `car is not moving when generated number is 3`() {
-		val racingCar = RacingCar(moved = 2, generator = { 3 }, "Anna")
-		racingCar.moveRandomly()
-		assertThat(racingCar.getCurrentState()).isEqualTo(2)
-	}
+    @Test
+    fun `car is not moving when generated number is 3`() {
+        val racingCar = RacingCar(moved = 2, generator = { 3 }, "Anna")
+        racingCar.moveRandomly()
+        assertThat(racingCar.getCurrentState()).isEqualTo(2)
+    }
 
-	@Test
-	fun `car is moving when generated number is 5`() {
-		val racingCar = RacingCar(moved = 3, generator = { 5 }, "Anna")
-		racingCar.moveRandomly()
-		assertThat(racingCar.getCurrentState()).isEqualTo(4)
-	}
+    @Test
+    fun `car is moving when generated number is 5`() {
+        val racingCar = RacingCar(moved = 3, generator = { 5 }, "Anna")
+        racingCar.moveRandomly()
+        assertThat(racingCar.getCurrentState()).isEqualTo(4)
+    }
 
     @Test
     fun `error occurred when car name exceeds limits`() {
