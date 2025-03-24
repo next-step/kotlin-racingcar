4e5953bfbecc1e2e45626c0ec436abd4da61a127
june-777 <wlwhswnsrl96@gmail.com>
2025-03-24 23:24:25 +0100

    feat: implement car "first-class collection"

---
+++ src/main/kotlin/carracing/Cars.kt
@@ -0,0 +1,30 @@
+package carracing
+
+class Cars private constructor(private val cars: List<Car>) {
+
+    companion object {
+        fun create(cars: List<Car>): Cars {
+            return Cars(cars)
+        }
+
+        fun create(totalCar: Int, initialPosition: Int, minConditionToMove: Int): Cars {
+            val cars = mutableListOf<Car>()
+            for (count in 0 until totalCar) {
+                cars.add(Car(initialPosition, minConditionToMove))
+            }
+            return Cars(cars)
+        }
+
+    }
+
+    fun moveAllCars(numberGenerator: NumberGenerator) {
+        for (car in cars) {
+            val power = numberGenerator.generate()
+            car.move(power)
+        }
+    }
+
+    fun getCarPositions(): List<Int> {
+        return cars.map { it.getPosition() }
+    }
+}
