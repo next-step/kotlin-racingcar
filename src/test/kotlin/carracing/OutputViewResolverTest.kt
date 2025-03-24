034e86eada32113cf51e2a7da91b25099b776451
june-777 <wlwhswnsrl96@gmail.com>
2025-03-24 23:28:25 +0100

    test: out put view

---
+++ src/test/kotlin/carracing/OutputViewResolverTest.kt
@@ -0,0 +1,27 @@
+package carracing
+
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class OutputViewResolverTest {
+
+    val outputViewResolver = OutputViewResolver()
+
+    @Test
+    fun `hyphen count should match with position`() {
+        // given, when
+        val viewResult = outputViewResolver.makeCarPositionsView(listOf(1, 2, 5))
+        // then
+        assertThat(
+            viewResult.equals(
+                """
+            -
+            --
+            -----
+        """
+            )
+        )
+    }
+}
