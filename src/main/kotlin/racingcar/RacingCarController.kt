cf0d86c3461a2931e87921cd2ef977af50837582
krrong <kangsj9665@gmail.com>
2025-03-25 16:29:57 +0100

    refactor: split the run function

--- src/main/kotlin/racingcar/RacingCarController.kt
+++ src/main/kotlin/racingcar/RacingCarController.kt
@@ -6,18 +6,23 @@ import racingcar.view.OutputView
 
 class RacingCarController(private val inputView: InputView, private val outputView: OutputView) {
     fun run() {
-        val carNumber = inputView.getCarNumber() ?: 0
+        val cars = createCars()
         val roundNumber = inputView.getRoundNumber() ?: 0
 
-        val cars = List(carNumber) { Car() }
+        repeat(roundNumber) { playRound(cars) }
 
-        repeat(roundNumber) {
-            cars.forEach { car ->
-                car.move(getRandomNumber())
-            }
-            outputView.printResult(cars)
+        outputView.printResult(cars)
+    }
+
+    private fun createCars(): List<Car> {
+        val carNumber = inputView.getCarNumber() ?: 0
+        return List(carNumber) { Car() }
+    }
+
+    private fun playRound(cars: List<Car>) {
+        cars.forEach { car ->
+            car.move(getRandomNumber())
         }
-
         outputView.printResult(cars)
     }
 
