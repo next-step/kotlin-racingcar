d282f08e3e1e7542d602590befcdc972d4aaa25e
krrong <khk9664@naver.com>
2025-03-29 11:37:08 +0100

    refactor: extract range to constant

--- src/main/kotlin/racingcar/domain/numberpicker/RandomNumberPicker.kt
+++ src/main/kotlin/racingcar/domain/numberpicker/RandomNumberPicker.kt
@@ -1,14 +1,13 @@
 package racingcar.domain.numberpicker
 
 class RandomNumberPicker : NumberPicker {
-    private val range = (MINIMUM_VALUE..MAXIMUM_VALUE)
-
     override fun getNumber(): Int {
-        return range.random()
+        return RANGE.random()
     }
 
     companion object {
         private const val MINIMUM_VALUE = 0
         private const val MAXIMUM_VALUE = 9
+        private val RANGE = (MINIMUM_VALUE..MAXIMUM_VALUE)
     }
 }
