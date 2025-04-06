ec4e6601bf83c00ef12de010725271e2ff9451e3
sh1erz <krebriseva@gmail.com>
2025-04-06 20:24:33 +0200

    Apply small suggestions

--- src/main/kotlin/carracing/game/domain/data/Car.kt
+++ src/main/kotlin/carracing/game/domain/data/Car.kt
@@ -4,11 +4,13 @@ private const val INITIAL_POSITION = 0
 private const val MIN_NUMBER_TO_MOVE = 4
 
 data class Car(
-    var position: Int = INITIAL_POSITION,
+    private var _position: Int = INITIAL_POSITION,
 ) {
+    val position: Int get() = _position
+
     fun move(number: Int) {
         if (number >= MIN_NUMBER_TO_MOVE) {
-            position++
+            _position++
         }
     }
 }
