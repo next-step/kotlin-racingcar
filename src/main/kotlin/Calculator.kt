49671fcf7fedfa7d5016759b438dad34d9a3e080
euijinkk <gytks4@naver.com>
2026-01-16 16:50:02 +0900

    feat: 더하기 기능 구현

---
+++ src/main/kotlin/Calculator.kt
@@ -0,0 +1,5 @@
+class Calculator {
+    fun add(num1: Int, num2: Int): Int {
+        return num1 + num2
+    }
+}
---
+++ src/test/kotlin/CalculatorTest.kt
@@ -0,0 +1,13 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class CalculatorTest {
+    @Test
+    fun 더하기() {
+        val calculator = Calculator();
+
+        val actual = calculator.add(1, 2);
+
+        assertThat(actual).isEqualTo(3)
+    }
+}
