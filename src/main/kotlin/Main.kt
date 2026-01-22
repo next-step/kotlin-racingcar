aab9b413bb7169d93de4eba9da5d39b26481d044
euijinkk <gytks4@naver.com>
2026-01-22 23:57:32 +0900

    feat: 프로그램 구현체 추가

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,11 @@
 fun main(args: Array<String>) {
     println("계산기 프로그램을 시작합니다.")
     println("숫자와 연산자를 입력하세요")
-    val input = readLine()
+    val input = readLine() ?: throw IllegalArgumentException("입력이 잘못되었습니다.")
 
+    val calculator = Calculator()
+    val calculatorController = CalculatorController(calculator)
 
+    val result = calculatorController.calc((input))
+    println(result)
 }
