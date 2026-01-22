37ca44e3b01fa1f5b4929a870358d84109eefbaf
euijinkk <gytks4@naver.com>
2026-01-22 22:10:26 +0900

    refactor: 테스트 Nested 구조로 수정

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,21 +1,53 @@
 class Calculator {
+    companion object {
+        const val PLUS = '+'
+        const val MINUS = '-'
+        const val MULTIPLY = '*'
+        const val DIVIDE = '/'
+    }
+
+
     fun add(num1: Int, num2: Int): Int {
         return num1 + num2
     }
 
-    fun selectExpression(exp: Char): (Int, Int) -> Int {
+    fun minus(num1: Int, num2: Int): Int {
+        return num1 - num2
+    }
+
+    fun multiply(num1: Int, num2: Int): Int {
+        return num1 * num2
+    }
+
+    fun divide(num1: Int, num2: Int): Int {
+        return num1 / num2
+    }
+
+    private fun selectExpression(exp: Char): (Int, Int) -> Int {
         when (exp) {
-            '+' -> {
+            PLUS -> {
                 return ::add
             }
 
+            MINUS -> {
+                return this::minus
+            }
+
+            MULTIPLY -> {
+                return ::multiply
+            }
+
+            DIVIDE -> {
+                return ::divide
+            }
+
             else -> {
-                return ::add
+                throw IllegalArgumentException("$PLUS, $MINUS, $MULTIPLY, $DIVIDE 연산자를 사용하세요")
             }
         }
     }
 
-    fun calcTwoNums(num1: Int, expression: Char, num2: Int): Int {
+    private fun calcTwoNums(num1: Int, expression: Char, num2: Int): Int {
         val applyExpression = selectExpression(expression)
         return applyExpression(num1, num2)
     }
