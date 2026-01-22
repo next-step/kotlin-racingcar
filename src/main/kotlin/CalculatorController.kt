593167f250b5d7922d9d9094bdf8ba3e53afb4c7
euijinkk <gytks4@naver.com>
2026-01-22 23:37:12 +0900

    refactor: selectExpression when 에서 값 return

--- src/main/kotlin/CalculatorController.kt
+++ src/main/kotlin/CalculatorController.kt
@@ -7,28 +7,12 @@ class CalculatorController(private val calculator: Calculator) {
         const val DIVIDE = '/'
     }
 
-    private fun selectExpression(exp: Char): (Int, Int) -> Int {
-        when (exp) {
-            PLUS -> {
-                return calculator::add
-            }
-
-            MINUS -> {
-                return calculator::minus
-            }
-
-            MULTIPLY -> {
-                return calculator::multiply
-            }
-
-            DIVIDE -> {
-                return calculator::divide
-            }
-
-            else -> {
-                throw IllegalArgumentException("$PLUS, $MINUS, $MULTIPLY, $DIVIDE 연산자를 사용하세요")
-            }
-        }
+    private fun selectExpression(exp: Char): (Int, Int) -> Int = when (exp) {
+        PLUS -> calculator::add
+        MINUS -> calculator::minus
+        MULTIPLY -> calculator::multiply
+        DIVIDE -> calculator::divide
+        else -> throw IllegalArgumentException("$PLUS, $MINUS,$MULTIPLY, $DIVIDE 연산자를 사용하세요")
     }
 
     private fun calcTwoNums(num1: Int, expression: Char, num2: Int): Int {
