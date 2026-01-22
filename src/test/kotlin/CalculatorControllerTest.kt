509d7f37faac04935f051c61973c97a6a1b3483d
euijinkk <gytks4@naver.com>
2026-01-22 23:28:34 +0900

    refactor: Controller 테스트 케이스 상세화

--- src/test/kotlin/CalculatorControllerTest.kt
+++ src/test/kotlin/CalculatorControllerTest.kt
@@ -2,20 +2,10 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class CalculatorControllerTest {
-    @Test
-    fun 더하기를_수행한다() {
-        val input = "2 + 3"
 
-        val calculator = Calculator();
-        val calculatorController = CalculatorController(calculator);
-
-        val actual = calculatorController.calc(input);
-
-        assertThat(actual).isEqualTo(5)
-    }
 
     @Test
-    fun 두자리수_더하기를_수행한다() {
+    fun 하나의_연산자를_받아서_연산을_수행한다() {
         val input = "14 + 25"
 
         val calculator = Calculator();
@@ -28,13 +18,13 @@ class CalculatorControllerTest {
 
     @Test
     fun 여러개의_연산자를_받아서_연산을_수행한다() {
-        val input = "14 + 25 + 5"
+        val input = "10 + 20 * 5 / 15"
 
         val calculator = Calculator();
         val calculatorController = CalculatorController(calculator);
 
         val actual = calculatorController.calc(input);
 
-        assertThat(actual).isEqualTo(44)
+        assertThat(actual).isEqualTo(10)
     }
 }
