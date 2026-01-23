8a33bf5dba97b8392d1cc093a0fddc4cb74ac420
euijinkk <gytks4@naver.com>
2026-01-23 23:24:53 +0900

    feat: GameView 라운드 결과 시각화

--- src/test/kotlin/GameView.kt
+++ src/test/kotlin/GameView.kt
@@ -1,3 +1,15 @@
+package game.view
+
 class GameView {
 
+    fun showResult(list: List<Int>): String {
+        val result = StringBuilder()
+
+        for (item in list) {
+            result.append("-".repeat(item))
+            result.append("\n")
+        }
+
+        return result.toString()
+    }
 }
---
+++ src/test/kotlin/GameViewTest.kt
@@ -0,0 +1,18 @@
+import game.view.GameView
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class GameViewTest {
+    @Test
+    fun 라운드_진행_결과_시각화() {
+        val gameView = GameView()
+
+        val result = gameView.showResult(listOf(1, 2, 3))
+
+        val resultToList = result.split("\n").filter { it.isNotEmpty() }
+
+        resultToList.forEach { it ->
+            assertThat(it).isEqualTo("-".repeat(it.length))
+        }
+    }
+}
